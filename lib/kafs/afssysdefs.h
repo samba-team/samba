ecaf61d7ac834fcc02e28cc9769994cc3c089466
Björn Groenvall <bg@sics.se>
1995-06-20 16:19:19 +0000

    Initial revision
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@22 ec53bebd-3082-4978-b11e-865c3cabbd6b

---
+++ lib/kafs/afssysdefs.h
@@ -0,0 +1,31 @@
+/*
+ * This section is for machines using single entry point AFS syscalls!
+ * or
+ * This section is for machines using multiple entry point AFS syscalls!
+ */
+
+#if defined(sun) && !defined(__svr4__)
+#define AFS_SYSCALL	31
+#endif
+
+#if defined(sun) && defined(__svr4__)
+#define AFS_SYSCALL	105
+#endif
+
+#if defined(hpux)
+#define AFS_SYSCALL	49
+#endif
+
+#if defined(_AIX)
+#define AFS_SYSCALL	38
+#endif
+
+#if defined(sgi)
+#define AFS_PIOCTL      (64+1000)
+#define AFS_SETPAG      (65+1000)
+#endif
+
+#if defined(__osf__)
+#define AFS_SYSCALL	232
+#define AFS_SYSCALL2	258
+#endif
