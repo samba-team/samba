07ebff2bbfe24827d00f2c87bb0d9d3dc9c6ec48
Björn Groenvall <bg@sics.se>
1995-09-04 21:41:38 +0000

    Initial revision
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@101 ec53bebd-3082-4978-b11e-865c3cabbd6b

---
+++ lib/roken/iruserok.c
@@ -0,0 +1,18 @@
+#include "bsd_locl.h"
+
+#ifndef HAVE_IRUSEROK
+
+int     __check_rhosts_file = 1;
+char    *__rcmd_errstr = 0;
+
+
+/*
+ * Returns 0 if ok, -1 if not ok.
+ */
+int
+iruserok(u_int32_t raddr, int superuser, const char *ruser, const char *luser)
+{
+  return -1;
+}
+
+#endif /* !HAVE_IRUSEROK */
