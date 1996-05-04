40a567fcac8dade13b1e8f037a50b064a492a8b2
Unknown User d91-jda <d91-jda@nada.kth.se>
1996-05-04 02:15:56 +0000

    Include top-level config.h.
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@464 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- appl/ftp/acconfig.h
+++ appl/ftp/acconfig.h
@@ -2,18 +2,11 @@
  * $Id$
  */
 
+#include "../../config.h"
+
 @TOP@
 
 @BOTTOM@
-
-/* define if struct utmp contains ut_host */
-#undef HAVE_UT_HOST
-
-#ifdef __STDC__
-#define RCSID(msg) static const char *rcsid[] = { (char *)rcsid, "\0100(#)" msg }
-#else
-#define RCSID(msg) static char *rcsid[] = { (char *)rcsid, msg }
-#endif
 
 #ifndef WTMP_PATH
 #define WTMP_PATH "/var/adm/wtmp"
