2063f6352dd151220c6ca01a6f0c3c12d2228c7c
Assar Westerlund <assar@sics.se>
1996-05-10 22:16:46 +0000

    New function k_getpwnam that should work with and without shadow passwords.
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@516 ec53bebd-3082-4978-b11e-865c3cabbd6b

---
+++ lib/roken/k_getpwnam.c
@@ -0,0 +1,28 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+RCSID("$Id$");
+#endif /* HAVE_CONFIG_H */
+
+#include "roken.h"
+#ifdef HAVE_SHADOW_H
+#include <shadow.h>
+#endif
+
+struct passwd *
+k_getpwnam (char *user)
+{
+     struct passwd *p;
+
+     p = getpwnam (user);
+#ifdef HAVE_GETSPNAM
+     if(p)
+     {
+	  struct spwd *spwd;
+
+	  spwd = getspnam (user);
+	  if (spwd)
+	       p->pw_passwd = spwd->sp_pwdp;
+     }
+#endif
+     return p;
+}
