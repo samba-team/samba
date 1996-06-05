35f4cd6e665c84aa460c1b773f93761aff5da08f
Assar Westerlund <assar@sics.se>
1996-06-05 08:20:52 +0000

    *** empty log message ***
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@560 ec53bebd-3082-4978-b11e-865c3cabbd6b

---
+++ lib/roken/verify.c
@@ -0,0 +1,22 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+RCSID("$Id$");
+#endif
+
+#include "roken.h"
+
+int
+verify_unix_user(char *user, char *password)
+{
+    struct passwd *pw;
+    
+    pw = k_getpwnam(user);
+    if(pw == NULL)
+	return -1;
+    if(strlen(pw->pw_passwd) == 0 && strlen(password) == 0)
+	return 0;
+    if(strcmp(crypt(password, pw->pw_passwd), pw->pw_passwd) == 0)
+        return 0;
+    return -1;
+}
+
