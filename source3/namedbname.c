3545a9b8734bfabdff760253dd73216aad25b925
Samba Release Account <samba-bugs@samba.org>
1996-07-07 13:29:56 +0000

    added the recently shuffled and updated source files missed in the previous commit (see previous log message for details)
    fixed bug in nameservreply.c: wrong macro in use (RSSVAL not IVAL!).
    
    did another make proto
    
    lkcl
    (This used to be commit d78b319062144d14a54408fce77ccc313ad58ee3)

---
+++ source3/namedbname.c
@@ -0,0 +1,547 @@
+/* 
+   Unix SMB/Netbios implementation.
+   Version 1.9.
+   NBT netbios routines and daemon - version 2
+   Copyright (C) Andrew Tridgell 1994-1996
+   
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+   
+   Module name: namedbname.c
+
+   Revision History:
+
+   14 jan 96: lkcl@pires.co.uk
+   added multiple workgroup domain master support
+
+   04 jul 96: lkcl@pires.co.uk
+   created module namedbname containing name database functions
+*/
+
+#include "includes.h"
+
+extern int DEBUGLEVEL;
+
+extern pstring scope;
+extern struct in_addr ipzero;
+extern struct in_addr ipgrp;
+
+extern struct subnet_record *subnetlist;
+
+#define WINS_LIST "wins.dat"
+
+
+/****************************************************************************
+  true if two netbios names are equal
+****************************************************************************/
+BOOL name_equal(struct nmb_name *n1,struct nmb_name *n2)
+{
+  return n1->name_type == n2->name_type &&
+		 strequal(n1->name ,n2->name ) &&
+         strequal(n1->scope,n2->scope);
+}
+
+
+/****************************************************************************
+  true if the netbios name is ^1^2__MSBROWSE__^2^1
+
+  note: this name is registered if as a master browser or backup browser
+  you are responsible for a workgroup (when you announce a domain by
+  broadcasting on your local subnet, you announce it as coming from this
+  name: see announce_host()).
+
+  **************************************************************************/
+BOOL ms_browser_name(char *name, int type)
+{
+  return strequal(name,MSBROWSE) && type == 0x01;
+}
+
+
+/****************************************************************************
+  add a netbios name into the namelist
+  **************************************************************************/
+static void add_name(struct subnet_record *d, struct name_record *n)
+{
+  struct name_record *n2;
+
+  if (!d) return;
+
+  if (!d->namelist)
+  {
+    d->namelist = n;
+    n->prev = NULL;
+    n->next = NULL;
+    return;
+  }
+
+  for (n2 = d->namelist; n2->next; n2 = n2->next) ;
+
+  n2->next = n;
+  n->next = NULL;
+  n->prev = n2;
+}
+
+
+/****************************************************************************
+  remove a name from the namelist. The pointer must be an element just 
+  retrieved
+  **************************************************************************/
+void remove_name(struct subnet_record *d, struct name_record *n)
+{
+  struct name_record *nlist;
+  if (!d) return;
+
+  nlist = d->namelist;
+
+  while (nlist && nlist != n) nlist = nlist->next;
+
+  if (nlist)
+  {
+    if (nlist->next) nlist->next->prev = nlist->prev;
+    if (nlist->prev) nlist->prev->next = nlist->next;
+    free(nlist);
+  }
+}
+
+
+/****************************************************************************
+  find a name in a namelist.
+  **************************************************************************/
+struct name_record *find_name(struct name_record *n,
+			struct nmb_name *name,
+			int search, struct in_addr ip)
+{
+	struct name_record *ret;
+  
+	for (ret = n; ret; ret = ret->next)
+	{
+		if (name_equal(&ret->name,name))
+		{
+			/* self search: self names only */
+			if ((search&FIND_SELF) == FIND_SELF && ret->source != SELF)
+				continue;
+	  
+			/* zero ip is either samba's ip or a way of finding a
+			   name without needing to know the ip address */
+			if (zero_ip(ip) || ip_equal(ip, ret->ip))
+			{
+				return ret;
+			}
+		}
+	}
+    return NULL;
+}
+
+
+/****************************************************************************
+  find a name in the domain database namelist 
+  search can be any of:
+  FIND_SELF - look exclusively for names the samba server has added for itself
+  FIND_LOCAL - look for names in the local subnet record.
+  FIND_WINS - look for names in the WINS record
+  **************************************************************************/
+struct name_record *find_name_search(struct subnet_record **d,
+			struct nmb_name *name,
+			int search, struct in_addr ip)
+{
+	if (d == NULL) return NULL; /* bad error! */
+	
+    if ((search & FIND_LOCAL) == FIND_LOCAL)
+	{
+		if (*d != NULL)
+        {
+			return find_name((*d)->namelist, name, search, ip);
+		}
+        else
+        {
+			DEBUG(4,("local find_name_search with a NULL subnet pointer\n"));
+            return NULL;
+		}
+	}
+
+	if ((search & FIND_WINS) != FIND_WINS) return NULL;
+
+	if (*d == NULL)
+	{
+		/* find WINS subnet record */
+		*d = find_subnet(ipgrp);
+    }
+
+	if (*d == NULL) return NULL;
+
+	return find_name((*d)->namelist, name, search, ip);
+}
+
+
+/****************************************************************************
+  dump a copy of the name table
+  **************************************************************************/
+void dump_names(void)
+{
+  struct name_record *n;
+  struct subnet_record *d;
+  fstring fname, fnamenew;
+  time_t t = time(NULL);
+  
+  FILE *f;
+  
+  strcpy(fname,lp_lockdir());
+  trim_string(fname,NULL,"/");
+  strcat(fname,"/");
+  strcat(fname,WINS_LIST);
+  strcpy(fnamenew,fname);
+  strcat(fnamenew,".");
+  
+  f = fopen(fnamenew,"w");
+  
+  if (!f)
+  {
+    DEBUG(4,("Can't open %s - %s\n",fnamenew,strerror(errno)));
+  }
+  
+  DEBUG(3,("Dump of local name table:\n"));
+  
+  for (d = subnetlist; d; d = d->next)
+   for (n = d->namelist; n; n = n->next)
+    {
+      if (f && ip_equal(d->bcast_ip, ipgrp) && n->source == REGISTER)
+      {
+        fstring data;
+
+      /* XXXX i have little imagination as to how to output nb_flags as
+         anything other than as a hexadecimal number :-) */
+
+        sprintf(data, "%s#%02x %s %ld %2x",
+	       n->name.name,n->name.name_type, /* XXXX ignore the scope for now */
+	       inet_ntoa(n->ip),
+	       n->death_time,
+	       n->nb_flags);
+	    fprintf(f, "%s\n", data);
+      }
+
+	  DEBUG(3,("%15s ", inet_ntoa(d->bcast_ip)));
+	  DEBUG(3,("%15s ", inet_ntoa(d->mask_ip)));
+      DEBUG(3,("%s %15s TTL=%15d NBFLAGS=%2x\n",
+	       namestr(&n->name),
+	       inet_ntoa(n->ip),
+           n->death_time?n->death_time-t:0,
+	       n->nb_flags));
+    }
+
+  fclose(f);
+  unlink(fname);
+  chmod(fnamenew,0644);
+  rename(fnamenew,fname);   
+
+  DEBUG(3,("Wrote wins database %s\n",fname));
+}
+
+
+/****************************************************************************
+load a netbios name database file
+****************************************************************************/
+void load_netbios_names(void)
+{
+  struct subnet_record *d = find_subnet(ipgrp);
+  fstring fname;
+
+  FILE *f;
+  pstring line;
+
+  if (!d) return;
+
+  strcpy(fname,lp_lockdir());
+  trim_string(fname,NULL,"/");
+  strcat(fname,"/");
+  strcat(fname,WINS_LIST);
+
+  f = fopen(fname,"r");
+
+  if (!f) {
+    DEBUG(2,("Can't open wins database file %s\n",fname));
+    return;
+  }
+
+  while (!feof(f))
+    {
+      pstring name_str, ip_str, ttd_str, nb_flags_str;
+
+      pstring name;
+      int type = 0;
+      int nb_flags;
+      time_t ttd;
+	  struct in_addr ipaddr;
+
+	  enum name_source source;
+
+      char *ptr;
+	  int count = 0;
+
+      char *p;
+
+      if (!fgets_slash(line,sizeof(pstring),f)) continue;
+
+      if (*line == '#') continue;
+
+	ptr = line;
+
+	if (next_token(&ptr,name_str    ,NULL)) ++count;
+	if (next_token(&ptr,ip_str      ,NULL)) ++count;
+	if (next_token(&ptr,ttd_str     ,NULL)) ++count;
+	if (next_token(&ptr,nb_flags_str,NULL)) ++count;
+
+	if (count <= 0) continue;
+
+	if (count != 4) {
+	  DEBUG(0,("Ill formed wins line"));
+	  DEBUG(0,("[%s]: name#type ip nb_flags abs_time\n",line));
+	  continue;
+	}
+
+      /* netbios name. # divides the name from the type (hex): netbios#xx */
+      strcpy(name,name_str);
+
+      p = strchr(name,'#');
+
+      if (p) {
+	    *p = 0;
+	    sscanf(p+1,"%x",&type);
+      }
+
+      /* decode the netbios flags (hex) and the time-to-die (seconds) */
+	  sscanf(nb_flags_str,"%x",&nb_flags);
+	  sscanf(ttd_str,"%ld",&ttd);
+
+	  ipaddr = *interpret_addr2(ip_str);
+
+      if (ip_equal(ipaddr,ipzero)) {
+         source = SELF;
+      }
+      else
+      {
+         source = REGISTER;
+      }
+
+      DEBUG(4, ("add WINS line: %s#%02x %s %ld %2x\n",
+	       name,type, inet_ntoa(ipaddr), ttd, nb_flags));
+
+      /* add all entries that have 60 seconds or more to live */
+      if (ttd - 60 < time(NULL) || ttd == 0)
+      {
+        time_t t = (ttd?ttd-time(NULL):0) / 3;
+
+        /* add netbios entry read from the wins.dat file. IF it's ok */
+        add_netbios_entry(d,name,type,nb_flags,t,source,ipaddr,True,True);
+      }
+    }
+
+  fclose(f);
+}
+
+
+/****************************************************************************
+  remove an entry from the name list
+  ****************************************************************************/
+void remove_netbios_name(struct subnet_record *d,
+			char *name,int type, enum name_source source,
+			 struct in_addr ip)
+{
+  struct nmb_name nn;
+  struct name_record *n;
+
+  make_nmb_name(&nn, name, type, scope);
+  n = find_name_search(&d, &nn, FIND_LOCAL, ip);
+  
+  if (n && n->source == source) remove_name(d,n);
+}
+
+
+/****************************************************************************
+  add an entry to the name list.
+
+  this is a multi-purpose function.
+
+  it adds samba's own names in to its records on each interface, keeping a
+  record of whether it is a master browser, domain master, or WINS server.
+
+  it also keeps a record of WINS entries.
+
+  ****************************************************************************/
+struct name_record *add_netbios_entry(struct subnet_record *d,
+		char *name, int type, int nb_flags, 
+		int ttl, enum name_source source, struct in_addr ip,
+		BOOL new_only,BOOL wins)
+{
+  struct name_record *n;
+  struct name_record *n2=NULL;
+  int search = 0;
+  BOOL self = source == SELF;
+
+  /* add the name to the WINS list if the name comes from a directed query */
+  search |= wins ? FIND_WINS : FIND_LOCAL;
+  /* search for SELF names only */
+  search |= self ? FIND_SELF : 0;
+
+  if (!self)
+  {
+    if (!wins && type != 0x1b)
+    {
+       /* the only broadcast (non-WINS) names we are adding are ours
+          (SELF) and PDC type names */
+       return NULL;
+    }
+  }
+
+  n = (struct name_record *)malloc(sizeof(*n));
+  if (!n) return(NULL);
+
+  bzero((char *)n,sizeof(*n));
+
+  make_nmb_name(&n->name,name,type,scope);
+
+  if ((n2 = find_name_search(&d, &n->name, search, new_only?ipzero:ip)))
+  {
+    free(n);
+    if (new_only || (n2->source==SELF && source!=SELF)) return n2;
+    n = n2;
+  }
+
+  if (ttl)
+     n->death_time = time(NULL)+ttl*3;
+  n->refresh_time = time(NULL)+GET_TTL(ttl);
+
+  n->ip = ip;
+  n->nb_flags = nb_flags;
+  n->source = source;
+  
+  if (!n2) add_name(d,n);
+
+  DEBUG(3,("Added netbios name %s at %s ttl=%d nb_flags=%2x\n",
+	        namestr(&n->name),inet_ntoa(ip),ttl,nb_flags));
+
+  return(n);
+}
+
+
+/*******************************************************************
+  expires old names in the namelist
+  ******************************************************************/
+void expire_names(time_t t)
+{
+	struct name_record *n;
+	struct name_record *next;
+	struct subnet_record *d;
+
+	/* expire old names */
+	for (d = subnetlist; d; d = d->next)
+	{
+	  for (n = d->namelist; n; n = next)
+		{
+		  if (n->death_time && n->death_time < t)
+		{
+		  DEBUG(3,("Removing dead name %s\n", namestr(&n->name)));
+		  
+		  next = n->next;
+		  
+		  if (n->prev) n->prev->next = n->next;
+		  if (n->next) n->next->prev = n->prev;
+		  
+		  if (d->namelist == n) d->namelist = n->next; 
+		  
+		  free(n);
+		}
+		  else
+		{
+		  next = n->next;
+		}
+		}
+	}
+}
+
+
+/***************************************************************************
+  reply to a name query
+  ****************************************************************************/
+struct name_record *search_for_name(struct subnet_record **d,
+					struct nmb_name *question,
+				    struct in_addr ip, int Time, int search)
+{
+  int name_type = question->name_type;
+  char *qname = question->name;
+  BOOL dns_type = name_type == 0x20 || name_type == 0;
+  
+  struct name_record *n;
+  
+  DEBUG(3,("Search for %s from %s - ", namestr(question), inet_ntoa(ip)));
+  
+  /* first look up name in cache */
+  n = find_name_search(d,question,search,ip);
+  
+  if (*d == NULL) return NULL;
+
+  /* now try DNS lookup. */
+  if (!n)
+    {
+      struct in_addr dns_ip;
+      unsigned long a;
+      
+      /* only do DNS lookups if the query is for type 0x20 or type 0x0 */
+      if (!dns_type)
+	{
+	  DEBUG(3,("types 0x20 0x1b 0x0 only: name not found\n"));
+	  return NULL;
+	}
+      
+      /* look it up with DNS */      
+      a = interpret_addr(qname);
+      
+      putip((char *)&dns_ip,(char *)&a);
+      
+      if (!a)
+	{
+	  /* no luck with DNS. We could possibly recurse here XXXX */
+	  DEBUG(3,("no recursion.\n"));
+      /* add the fail to our WINS cache of names. give it 1 hour in the cache */
+	  add_netbios_entry(*d,qname,name_type,NB_ACTIVE,60*60,DNSFAIL,dns_ip,
+						True, True);
+	  return NULL;
+	}
+      
+      /* add it to our WINS cache of names. give it 2 hours in the cache */
+      n = add_netbios_entry(*d,qname,name_type,NB_ACTIVE,2*60*60,DNS,dns_ip,
+						True,True);
+      
+      /* failed to add it? yikes! */
+      if (!n) return NULL;
+    }
+  
+  /* is our entry already dead? */
+  if (n->death_time)
+    {
+      if (n->death_time < Time) return False;
+    }
+  
+  /* it may have been an earlier failure */
+  if (n->source == DNSFAIL)
+    {
+      DEBUG(3,("DNSFAIL\n"));
+      return NULL;
+    }
+  
+  DEBUG(3,("OK %s\n",inet_ntoa(n->ip)));      
+  
+  return n;
+}
+
+
---
+++ source3/namedbsubnet.c
@@ -0,0 +1,340 @@
+/* 
+   Unix SMB/Netbios implementation.
+   Version 1.9.
+   NBT netbios routines and daemon - version 2
+   Copyright (C) Andrew Tridgell 1994-1996
+   
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+   
+   Revision History:
+
+   14 jan 96: lkcl@pires.co.uk
+   added multiple workgroup domain master support
+
+   04 jul 96: lkcl@pires.co.uk
+   created module namedbsubnet containing subnet database functions
+
+*/
+
+#include "includes.h"
+#include "smb.h"
+
+extern int ClientNMB;
+extern int ClientDGRAM;
+
+extern int DEBUGLEVEL;
+
+extern struct in_addr ipgrp;
+extern struct in_addr ipzero;
+
+extern pstring myname;
+
+BOOL updatedlists = True;
+int updatecount = 0;
+
+/* local interfaces structure */
+extern struct interface *local_interfaces;
+
+/* remote interfaces structure */
+extern struct interface *remote_interfaces;
+
+/* this is our domain/workgroup/server database */
+struct subnet_record *subnetlist = NULL;
+
+
+/****************************************************************************
+  add a domain into the list
+  **************************************************************************/
+static void add_subnet(struct subnet_record *d)
+{
+  struct subnet_record *d2;
+
+  if (!subnetlist)
+  {
+    subnetlist = d;
+    d->prev = NULL;
+    d->next = NULL;
+    return;
+  }
+
+  for (d2 = subnetlist; d2->next; d2 = d2->next);
+
+  d2->next = d;
+  d->next = NULL;
+  d->prev = d2;
+}
+
+
+/****************************************************************************
+  find a subnet in the subnetlist 
+  **************************************************************************/
+struct subnet_record *find_subnet(struct in_addr bcast_ip)
+{   
+  struct subnet_record *d;
+  struct in_addr wins_ip = ipgrp;
+  
+  /* search through subnet list for broadcast/netmask that matches
+     the source ip address. a subnet 255.255.255.255 represents the
+     WINS list. */
+  
+  for (d = subnetlist; d; d = d->next)
+    {
+        if (ip_equal(bcast_ip, wins_ip))
+	    {
+           if (ip_equal(bcast_ip, d->bcast_ip))
+           {
+               return d;
+           }
+        }
+        else if (same_net(bcast_ip, d->bcast_ip, d->mask_ip))
+	    {
+	      return(d);
+	    }
+    }
+  
+  return (NULL);
+}
+
+
+/****************************************************************************
+  finds the appropriate subnet structure. directed packets (non-bcast) are
+  assumed to come from a point-to-point (P or M node), and so the subnet we
+  return in this instance is the WINS 'pseudo-subnet' with ip 255.255.255.255
+  ****************************************************************************/
+struct subnet_record *find_req_subnet(struct in_addr ip, BOOL bcast)
+{
+  if (bcast)
+  {
+    /* identify the subnet the broadcast request came from */
+    return find_subnet(*iface_bcast(ip));
+  }
+  /* find the subnet under the pseudo-ip of 255.255.255.255 */
+  return find_subnet(ipgrp);
+}
+
+
+/****************************************************************************
+  create a domain entry
+  ****************************************************************************/
+static struct subnet_record *make_subnet(struct in_addr bcast_ip, struct in_addr mask_ip)
+{
+  struct subnet_record *d;
+  d = (struct subnet_record *)malloc(sizeof(*d));
+  
+  if (!d) return(NULL);
+  
+  bzero((char *)d,sizeof(*d));
+  
+  DEBUG(4, ("making domain %s ", inet_ntoa(bcast_ip)));
+  DEBUG(4, ("%s\n", inet_ntoa(mask_ip)));
+  
+  d->bcast_ip = bcast_ip;
+  d->mask_ip  = mask_ip;
+  d->workgrouplist = NULL;
+  d->my_interface = False; /* True iff the interface is on the samba host */
+  
+  add_subnet(d);
+  
+  return d;
+}
+
+
+/****************************************************************************
+  add the remote interfaces from lp_remote_interfaces() and lp_interfaces()
+  to the netbios subnet database.
+  ****************************************************************************/
+void add_subnet_interfaces(void)
+{
+	struct interface *i;
+
+	/* loop on all local interfaces */
+	for (i = local_interfaces; i; i = i->next)
+	{
+		/* add the interface into our subnet database */
+		if (!find_subnet(i->bcast))
+		{
+		    struct subnet_record *d = make_subnet(i->bcast,i->nmask);
+			if (d)
+			{
+				/* short-cut method to identifying local interfaces */
+				d->my_interface = True;
+			}
+		}
+	}
+
+	/* loop on all remote interfaces */
+	for (i = remote_interfaces; i; i = i->next)
+	{
+		/* add the interface into our subnet database */
+		if (!find_subnet(i->bcast))
+		{
+		    make_subnet(i->bcast,i->nmask);
+		}
+	}
+
+	/* add the pseudo-ip interface for WINS: 255.255.255.255 */
+	if (lp_wins_support())
+    {
+		struct in_addr wins_bcast = ipgrp;
+		struct in_addr wins_nmask = ipzero;
+		make_subnet(wins_bcast, wins_nmask);
+    }
+}
+
+
+
+/****************************************************************************
+  add the default workgroup into my domain
+  **************************************************************************/
+void add_my_subnets(char *group)
+{
+  struct interface *i;
+
+  /* add or find domain on our local subnet, in the default workgroup */
+  
+  if (*group == '*') return;
+
+	/* the coding choice is up to you, andrew: i can see why you don't want
+       global access to the local_interfaces structure: so it can't get
+       messed up! */
+    for (i = local_interfaces; i; i = i->next)
+    {
+      add_subnet_entry(i->bcast,i->nmask,group, True, False);
+    }
+}
+
+
+/****************************************************************************
+  add a domain entry. creates a workgroup, if necessary, and adds the domain
+  to the named a workgroup.
+  ****************************************************************************/
+struct subnet_record *add_subnet_entry(struct in_addr bcast_ip, 
+				       struct in_addr mask_ip,
+				       char *name, BOOL add, BOOL lmhosts)
+{
+  struct subnet_record *d;
+
+  /* XXXX andrew: struct in_addr ip appears not to be referenced at all except
+     in the DEBUG comment. i assume that the DEBUG comment below actually
+     intends to refer to bcast_ip? i don't know.
+
+  struct in_addr ip = ipgrp;
+
+  */
+
+  if (zero_ip(bcast_ip)) 
+    bcast_ip = *iface_bcast(bcast_ip);
+  
+  /* add the domain into our domain database */
+  if ((d = find_subnet(bcast_ip)) ||
+      (d = make_subnet(bcast_ip, mask_ip)))
+    {
+      struct work_record *w = find_workgroupstruct(d, name, add);
+	  extern pstring ServerComment;
+      
+      if (!w) return NULL;
+
+      /* add WORKGROUP(1e) and WORKGROUP(00) entries into name database
+	 or register with WINS server, if it's our workgroup */
+      if (strequal(lp_workgroup(), name) && d->my_interface)
+	{
+	  add_my_name_entry(d,name,0x1e,NB_ACTIVE|NB_GROUP);
+	  add_my_name_entry(d,name,0x0 ,NB_ACTIVE|NB_GROUP);
+	}
+      /* add samba server name to workgroup list */
+      if ((strequal(lp_workgroup(), name) && d->my_interface) || lmhosts)
+      {
+	    add_server_entry(d,w,myname,w->ServerType,0,ServerComment,True);
+      }
+      
+      DEBUG(3,("Added domain name entry %s at %s\n", name,inet_ntoa(bcast_ip)));
+      return d;
+    }
+  return NULL;
+}
+
+
+/*******************************************************************
+  write out browse.dat
+  ******************************************************************/
+void write_browse_list(void)
+{
+  struct subnet_record *d;
+  
+  pstring fname,fnamenew;
+  FILE *f;
+  
+  if (!updatedlists) return;
+  
+  dump_names();
+  dump_workgroups();
+  
+  updatedlists = False;
+  updatecount++;
+  
+  strcpy(fname,lp_lockdir());
+  trim_string(fname,NULL,"/");
+  strcat(fname,"/");
+  strcat(fname,SERVER_LIST);
+  strcpy(fnamenew,fname);
+  strcat(fnamenew,".");
+  
+  f = fopen(fnamenew,"w");
+  
+  if (!f)
+    {
+      DEBUG(4,("Can't open %s - %s\n",fnamenew,strerror(errno)));
+      return;
+    }
+  
+  for (d = subnetlist; d ; d = d->next)
+    {
+      struct work_record *work;
+      for (work = d->workgrouplist; work ; work = work->next)
+	{
+	  struct server_record *s;
+	  for (s = work->serverlist; s ; s = s->next)
+	    {
+	      fstring tmp;
+	      
+	      /* don't list domains I don't have a master for */
+	      if ((s->serv.type & SV_TYPE_DOMAIN_ENUM) && !s->serv.comment[0])
+		{
+		  continue;
+		}
+	      
+	      /* output server details, plus what workgroup/domain
+		 they're in. without the domain information, the
+		 combined list of all servers in all workgroups gets
+		 sent to anyone asking about any workgroup! */
+	      
+	      sprintf(tmp, "\"%s\"", s->serv.name);
+	      fprintf(f, "%-25s ", tmp);
+	      fprintf(f, "%08x ", s->serv.type);
+	      sprintf(tmp, "\"%s\" ", s->serv.comment);
+	      fprintf(f, "%-30s", tmp);
+	      fprintf(f, "\"%s\"\n", work->work_group);
+	    }
+	}
+    }
+  
+  fclose(f);
+  unlink(fname);
+  chmod(fnamenew,0644);
+  rename(fnamenew,fname);   
+  DEBUG(3,("Wrote browse list %s\n",fname));
+}
+
+
