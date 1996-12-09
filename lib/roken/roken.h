56a0d1df6c66acebbd612b8919bffcd4dbdb3de6
Assar Westerlund <assar@sics.se>
1996-12-09 01:38:21 +0000

    more ifdefs for include files
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@1086 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/roken/roken.h
+++ lib/roken/roken.h
@@ -153,8 +153,17 @@ int setegid(int egid);
 #define strrchr(p, ch) rindex(p, ch)
 #endif
 
-#include <time.h>
+#ifdef TIME_WITH_SYS_TIME
 #include <sys/time.h>
+#include <time.h>
+#else
+#ifdef HAVE_SYS_TIME_H
+#include <sys/time.h>
+#else
+#include <time.h>
+#endif /* !HAVE_SYS_TIME_H */
+#endif /* !TIME_WITH_SYS_TIME */
+
 time_t tm2time (struct tm tm, int local);
 
 int unix_verify_user(char *user, char *password);
@@ -187,6 +196,7 @@ void mini_inetd (int port);
 #define min(a,b) (((a)<(b))?(a):(b))
 #endif
 
+#ifdef HAVE_SYSLOG_H
 #include <syslog.h>
 /* Misc definitions for old syslogs */
 
@@ -211,6 +221,7 @@ void vsyslog(int pri, const char *fmt, va_list ap);
 #endif
 #ifndef LOG_AUTHPRIV
 #define LOG_AUTHPRIV LOG_AUTH
+#endif
 #endif
 
 #ifdef NEED_OPTARG_DECLARATION
