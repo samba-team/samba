64a25a4676ddfbfffd3a970ccd8589a2fe0e24c5
Assar Westerlund <assar@sics.se>
1996-12-17 20:34:57 +0000

    Use otp_locl.h
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@1131 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/otp/otp_parse.c
+++ lib/otp/otp_parse.c
@@ -41,14 +41,7 @@
 RCSID("$Id$");
 #endif
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-
-#include "protos.h"
-#include "roken.h"
-#include "otp.h"
+#include "otp_locl.h"
 
 struct e {
   char *s;
