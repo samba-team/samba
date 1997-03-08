cd9dbf4d4fe72490fbe892ab89c581b9a3b68a43
Assar Westerlund <assar@sics.se>
1997-03-08 04:34:09 +0000

    more general
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@1278 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- kuser/kfoo.c
+++ kuser/kfoo.c
@@ -4,15 +4,21 @@
 #if 1
 main(int argc, char **argv)
 {
-    int ret;
     krb5_context context;
     krb5_ccache cache;
     krb5_creds in, *out;
+    int ret;
+
+    if (argc != 2) {
+      printf ("argc != 2\n");
+      return 1;
+    }
+
     krb5_init_context(&context);
     krb5_cc_default(context, &cache);
     memset(&in, 0, sizeof(in));
     krb5_cc_get_principal(context, cache, &in.client);
-    krb5_parse_name(context, "host/emma.pdc.kth.se@FOO.SE", &in.server);
+    krb5_parse_name(context, argv[1], &in.server);
     in.times.endtime = time(NULL) + 4711;
     ret = krb5_get_credentials(context, 0, cache, &in, &out);
     
