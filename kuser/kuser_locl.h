3a7dce096c3b2f4e6a45fd397645e3a3cca10460
Johan Danielsson <joda@pdc.kth.se>
1997-03-08 10:45:29 +0000

    *** empty log message ***
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@1310 ec53bebd-3082-4978-b11e-865c3cabbd6b

---
+++ kuser/kuser_locl.h
@@ -0,0 +1,18 @@
+/* $Id$ */
+
+#ifndef __KUSER_LOCL_H__
+#define __KUSER_LOCL_H__
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pwd.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <krb5.h>
+
+#endif /* __KUSER_LOCL_H__ */
