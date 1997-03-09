22ecf0527aa7b214efa46ad9d3ab70c3dfbf6513
Assar Westerlund <assar@sics.se>
1997-03-09 20:01:22 +0000

    HAVE_TIMEZONE
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@1321 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- acconfig.h
+++ acconfig.h
@@ -9,6 +9,12 @@
 #undef HAVE_U_INT32_T
 #undef HAVE_U_INT64_T
 
+/* Define this if your `struct tm' has a field `tm_gmtoff' */
+#undef HAVE_STRUCT_TM_TM_GMTOFF
+
+/* Define this if you have a variable `timezone' */
+#undef HAVE_TIMEZONE
+
 #undef VOID_RETSIGTYPE
 
 #ifdef VOID_RETSIGTYPE
