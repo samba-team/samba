990c5107ec38897a332e4e87c08841eaa7668443
Johan Danielsson <joda@pdc.kth.se>
1997-03-14 01:54:23 +0000

    Changes reflecting new hdb library.
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@1391 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- kdc/kdc_locl.h
+++ kdc/kdc_locl.h
@@ -10,22 +10,17 @@
 #endif
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 #include <fcntl.h>
-#include <db.h>
 #include <krb5.h>
+#include <hdb_err.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
 
 #include "kdc.h"
+#include "hdb.h"
 
-struct entry{
-    char *principal;
-    char *key;
-    char *kvno;
-    char *max_life;
-    char *max_renew;
-};
 
 #endif /* __KDC_LOCL_H__ */
