b218c58430eff5e44ce0af12b526fa45427246a7
Assar Westerlund <assar@sics.se>
1997-03-30 09:10:24 +0000

    reindent
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@1510 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/roken/err.h
+++ lib/roken/err.h
@@ -15,15 +15,24 @@ extern const char *__progname;
 #define __attribute__(x)
 #endif
 
-void warnerr(int doexit, int eval, int doerrno, const char *fmt, va_list ap) __attribute__ ((noreturn, format (printf, 4, 0)));
+void warnerr(int doexit, int eval, int doerrno, const char *fmt, va_list ap)
+     __attribute__ ((noreturn, format (printf, 4, 0)));
 
-void verr(int eval, const char *fmt, va_list ap) __attribute__ ((noreturn, format (printf, 2, 0)));
-void err(int eval, const char *fmt, ...)  __attribute__ ((noreturn, format (printf, 2, 3)));
-void verrx(int eval, const char *fmt, va_list ap) __attribute__ ((noreturn, format (printf, 2, 0)));
-void errx(int eval, const char *fmt, ...)  __attribute__ ((noreturn, format (printf, 2, 3)));
-void vwarn(const char *fmt, va_list ap) __attribute__ ((format (printf, 1, 0)));
-void warn(const char *fmt, ...)  __attribute__ ((format (printf, 1, 2)));
-void vwarnx(const char *fmt, va_list ap) __attribute__ ((format (printf, 1, 0)));
-void warnx(const char *fmt, ...)  __attribute__ ((format (printf, 1, 2)));;
+void verr(int eval, const char *fmt, va_list ap)
+     __attribute__ ((noreturn, format (printf, 2, 0)));
+void err(int eval, const char *fmt, ...)
+     __attribute__ ((noreturn, format (printf, 2, 3)));
+void verrx(int eval, const char *fmt, va_list ap)
+     __attribute__ ((noreturn, format (printf, 2, 0)));
+void errx(int eval, const char *fmt, ...)
+     __attribute__ ((noreturn, format (printf, 2, 3)));
+void vwarn(const char *fmt, va_list ap)
+     __attribute__ ((format (printf, 1, 0)));
+void warn(const char *fmt, ...)
+     __attribute__ ((format (printf, 1, 2)));
+void vwarnx(const char *fmt, va_list ap)
+     __attribute__ ((format (printf, 1, 0)));
+void warnx(const char *fmt, ...)
+     __attribute__ ((format (printf, 1, 2)));;
 
 #endif /* __ERR_H__ */
