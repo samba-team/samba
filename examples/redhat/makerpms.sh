b24c38d904e5ad339a97d9dac24d82fe1c81278e
John Terpstra <jht@samba.org>
1997-05-11 12:24:00 +0000

    Getting ready for release of RedHat Linux RPM build for 1.9.17 (This used to be commit 15ec54b6c6ba3ba19117fb7ae5aaafa4345f0236)

--- examples/redhat/makerpms.sh
+++ examples/redhat/makerpms.sh
@@ -3,11 +3,7 @@
 RPMDIR=/usr/src/redhat
 cp -a *.spec $RPMDIR/SPECS
 cp -a *.patch smb.* samba.log $RPMDIR/SOURCES
-# cp -a ../../../samba-1.9.16p11.tar.gz $RPMDIR/SOURCES
 cd $RPMDIR/SOURCES
-rm -rf samba-1.9.16p11
+rm -rf samba-1.9.17a1
 cd $RPMDIR/SPECS
-rpm -ba -v samba-1.9.16p11.spec
-#cd ..
-#mkdir $RPMDIR/distrib
-#cp -avf RPMS SRPMS distrib
+rpm -ba -v samba-1.9.17a1.spec
