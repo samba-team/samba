9ea6a88057b4220448ba61fc687486d82eb7373f
Assar Westerlund <assar@sics.se>
1997-07-06 21:05:01 +0000

    removed krb5_get_lrealm
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@1977 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/krb5/misc.c
+++ lib/krb5/misc.c
@@ -1,28 +1,3 @@
 #include "krb5_locl.h"
 
 RCSID("$Id$");
-
-krb5_error_code
-krb5_get_lrealm(char **realm)
-{
-  FILE *F;
-  char s[128];
-  char *p, *q;
-  F = fopen("/etc/krb5.conf", "r");
-  while(fgets(s, 128, F)){
-    if((p = strstr(s, "default_realm"))){
-      p = strchr(p, '=');
-      p++;
-      while(isspace(*p)) p++;
-      q = p;
-      while(isalnum(*p) || strchr("_.-", *p)) p++;
-      *p=0;
-      *realm = strdup(q);
-      fclose(F);
-      return 0;
-    }
-  }
-  fclose(F);
-  *realm = 0;
-  return 0;
-}
