ace05c5084666e92aaf903444a3664ab8eaa7e64
Assar Westerlund <assar@sics.se>
1997-07-07 09:19:51 +0000

    _PATH* extern variables prototypes
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@2003 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- appl/rsh/rsh_locl.h
+++ appl/rsh/rsh_locl.h
@@ -15,10 +15,23 @@
 #include <netdb.h>
 #include <errno.h>
 #include <syslog.h>
+#include <paths.h>
 #include <krb.h>
 #include <prot.h>
 #include <krb5.h>
 #include <roken.h>
+
+#ifndef _PATH_NOLOGIN
+#define _PATH_NOLOGIN   "/etc/nologin"
+#endif
+
+#ifndef _PATH_BSHELL
+#define _PATH_BSHELL	"/bin/sh"
+#endif
+
+#ifndef _PATH_DEFPATH
+#define _PATH_DEFPATH	"/usr/bin:/bin"
+#endif
 
 /*
  *
@@ -26,7 +39,19 @@
 
 enum auth_method { AUTH_KRB4, AUTH_KRB5 };
 
+extern enum auth_method auth_method;
+extern int do_encrypt;
+extern krb5_context context;
+extern krb5_keyblock *keyblock;
+extern des_key_schedule schedule;
+extern des_cblock iv;
+
 #define KCMD_VERSION "KCMDV0.1"
 
 #define USERNAME_SZ 16
 #define COMMAND_SZ 1024
+
+#define RSH_BUFSIZ 10240
+
+ssize_t do_read (int fd, void *buf, size_t sz);
+ssize_t do_write (int fd, void *buf, size_t sz);
