f3cd39f9d8e7ca60fc181406f90abf2b08fd6303
Assar Westerlund <assar@sics.se>
1997-07-09 01:36:58 +0000

    const stuff
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@2084 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/krb5/set_default_realm.c
+++ lib/krb5/set_default_realm.c
@@ -6,15 +6,18 @@ krb5_error_code
 krb5_set_default_realm(krb5_context context,
 		       char *realm)
 {
+    const char *foo;
     char *tmp;
 
     if (realm == NULL)
-	realm = krb5_config_get_string (context->cf,
-					"libdefaults",
-					"default_realm",
-					NULL);
+	foo = krb5_config_get_string (context->cf,
+				      "libdefaults",
+				      "default_realm",
+				      NULL);
+    else
+	foo = realm;
 
-    tmp = strdup (realm);
+    tmp = strdup (foo);
     if (tmp == NULL)
 	return ENOMEM;
     context->default_realm = tmp;
