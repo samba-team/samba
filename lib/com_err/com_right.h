50f86f863a65cbac8225ca1332baeff08b73f4f3
Johan Danielsson <joda@pdc.kth.se>
1997-07-10 03:32:05 +0000

    Remove error_list.
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@2119 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/com_err/com_right.h
+++ lib/com_err/com_right.h
@@ -7,11 +7,7 @@ struct error_table {
     char const * const * msgs;
     long base;
     int n_msgs;
-};
-
-struct error_list {
-    struct error_list *next;
-    const struct error_table * table;
+    struct error_table *next;
 };
 
 #endif /* __ERROR_H__ */
