fad86630ce2e7fc63b6f9f6360e47c58e7271f10
Johan Danielsson <joda@pdc.kth.se>
1997-07-14 16:14:41 +0000

    Remove <asn1_locl.h>, add <stdlib.h>.
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@2306 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/asn1/libasn1.h
+++ lib/asn1/libasn1.h
@@ -3,10 +3,10 @@
 #ifndef __LIBASN1_H__
 #define __LIBASN1_H__
 
-#include "asn1_locl.h"
+#include <stdlib.h>
+#include <errno.h>
 #include "asn1.h"
 #include "der.h"
 #include "asn1_err.h"
-#include <errno.h>
 
 #endif /* __LIBASN1_H__ */
