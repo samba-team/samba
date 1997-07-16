b2e14e1b918c791d2787c67190ab3741e946204b
Johan Danielsson <joda@pdc.kth.se>
1997-07-16 01:23:45 +0000

    Make db_fetch take a krb5_principal.
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@2347 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- kdc/misc.c
+++ kdc/misc.c
@@ -5,14 +5,14 @@ RCSID("$Id$");
 struct timeval now;
 
 hdb_entry*
-db_fetch(krb5_context context, PrincipalName *principal, char *realm)
+db_fetch(krb5_context context, krb5_principal principal)
 {
     HDB *db;
     hdb_entry *ent;
     krb5_error_code ret;
 
     ent = malloc(sizeof(*ent));
-    principalname2krb5_principal(&ent->principal, *principal, realm);
+    krb5_copy_principal(context, principal, &ent->principal);
     hdb_open(context, &db, NULL, O_RDONLY, 0);
     ret = db->fetch(context, db, ent);
     db->close(context, db);
