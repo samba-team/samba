940fc6080bd6859a3bdd899580c20f76fc68ac36
Johan Danielsson <joda@pdc.kth.se>
1997-07-16 01:24:48 +0000

    Update prototypes.
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@2348 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- kdc/kdc_locl.h
+++ kdc/kdc_locl.h
@@ -13,6 +13,7 @@
 #include <string.h>
 #include <errno.h>
 #include <signal.h>
+#include <stdarg.h>
 #ifdef HAVE_SYS_TYPES_H
 #include <sys/types.h>
 #endif
@@ -25,6 +26,9 @@
 #ifdef HAVE_NETINET_IN_H
 #include <netinet/in.h>
 #endif
+#ifdef HAVE_ARPA_INET_H
+#include <arpa/inet.h>
+#endif
 #ifdef HAVE_NETDB_H
 #include <netdb.h>
 #endif
@@ -35,19 +39,23 @@
 
 #include "hdb.h"
 
+extern sig_atomic_t exit_flag;
+
 extern struct timeval now;
 #define kdc_time (now.tv_sec)
 
-hdb_entry *db_fetch (krb5_context, PrincipalName *, char *);
+hdb_entry *db_fetch (krb5_context, krb5_principal);
 
 krb5_error_code mk_des_keyblock (EncryptionKey *);
 
-krb5_error_code tgs_rep(krb5_context, KDC_REQ *, krb5_data *);
-krb5_error_code as_rep(krb5_context, KDC_REQ *, krb5_data *);
+krb5_error_code tgs_rep(krb5_context, KDC_REQ *, krb5_data *, const char*);
+krb5_error_code as_rep(krb5_context, KDC_REQ *, krb5_data *, const char*);
 
 int maybe_version4(unsigned char*, int);
 krb5_error_code do_version4();
 
 void loop (krb5_context);
+
+void kdc_log(int, const char *fmt, ...);
 
 #endif /* __KDC_LOCL_H__ */
