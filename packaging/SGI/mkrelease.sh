8fde65239d0a9462fb044670220cd3c63bd887cb
Jeremy Allison <jra@samba.org>
1997-09-05 02:28:41 +0000

    This is really Herb adding the SGI packaging files.

---
+++ packaging/SGI/mkrelease.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# create the catman versions of the manual pages
+#
+echo Making manual pages
+./mkman
+
+# build the sources
+#
+echo Making binaries
+cd ../../source
+# make -f ../packaging/SGI/Makefile clean
+make -f ../packaging/SGI/Makefile all
+cd ../packaging/SGI
+
+# generate the packages
+#
+echo Generating Inst Packages
+
+if [ ! -d bins ]; then
+   mkdir bins
+fi
+
+/usr/sbin/gendist -rbase / -sbase ../.. -idb samba.idb -spec samba.spec -dist ./bins -all
+
