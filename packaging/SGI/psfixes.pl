f492bd51a61cbe6dd4f1971491b760fc7ebd2554
Herb Lewis <herb@samba.org>
1997-10-24 18:41:08 +0000

    update SGI build files to latest source

---
+++ packaging/SGI/psfixes.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+
+$found_vm = 0;
+
+while (<>) {
+  if (not $found_vm) {
+    if (not /^%/) {
+	if (/^%%Title:/) {
+	    s/.$//;
+	    print;
+	}
+	else {
+	    if (/^\/VM?/) {
+		print "/VM? { pop } bind def\n";
+		$found_vm = 1;
+	    }
+	    else {
+		print;
+	    }
+	}
+    }
+  }
+  else {
+    if (/def/) {
+	$found_vm = 0;
+    }
+  }
+}
