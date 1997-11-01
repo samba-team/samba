d8b896a90fb693187a11456e46971c873e9f0d60
Andrew Tridgell <tridge@samba.org>
1997-11-01 08:13:54 +0000

    change a debug level

--- source/lib/interface.c
+++ source/lib/interface.c
@@ -302,9 +302,9 @@ static void interpret_interfaces(char *s, struct interface **interfaces,
       last_iface->next = iface;
     }
     last_iface = iface;
-    DEBUG(1,("Added %s ip=%s ",description,inet_ntoa(iface->ip)));
-    DEBUG(1,("bcast=%s ",inet_ntoa(iface->bcast)));
-    DEBUG(1,("nmask=%s\n",inet_ntoa(iface->nmask)));	     
+    DEBUG(2,("Added %s ip=%s ",description,inet_ntoa(iface->ip)));
+    DEBUG(2,("bcast=%s ",inet_ntoa(iface->bcast)));
+    DEBUG(2,("nmask=%s\n",inet_ntoa(iface->nmask)));	     
   }
 
   if (*interfaces) return;
