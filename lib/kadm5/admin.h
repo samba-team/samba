8616f5f70ca6186489b06870def729a53e2fedb1
Johan Danielsson <joda@pdc.kth.se>
1997-11-09 02:40:56 +0000

    x
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@3841 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/kadm5/admin.h
+++ lib/kadm5/admin.h
@@ -181,7 +181,7 @@ typedef struct _kadm5_policy_ent_t {
 #define KADM5_PRIV_DELETE	(1 << 3)
 #define KADM5_PRIV_LIST		(1 << 4)
 #define KADM5_PRIV_CPW		(1 << 5)
-#define KADM5_PRIV_ALL		(0x7fffffff) /* XXX ~0 breaks in parse_flags */
+#define KADM5_PRIV_ALL		(KADM5_PRIV_GET | KADM5_PRIV_ADD | KADM5_PRIV_MODIFY | KADM5_PRIV_DELETE | KADM5_PRIV_LIST | KADM5_PRIV_CPW)
 
 typedef struct {
     int XXX;
