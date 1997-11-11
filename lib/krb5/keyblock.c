2e2be5b4e777f1b271e90caa9b46320fabd0bf1b
Johan Danielsson <joda@pdc.kth.se>
1997-11-11 01:38:34 +0000

    Rename krb5_free_keyblock to krb5_free_keyblock_contents, and reimplement krb5_free_keyblock.
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@3911 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/krb5/keyblock.c
+++ lib/krb5/keyblock.c
@@ -39,11 +39,19 @@
 #include "krb5_locl.h"
 
 void
-krb5_free_keyblock(krb5_context context,
-		   krb5_keyblock *keyblock)
+krb5_free_keyblock_contents(krb5_context context,
+			    krb5_keyblock *keyblock)
 {
     memset(keyblock->keyvalue.data, 0, keyblock->keyvalue.length);
     krb5_data_free (&keyblock->keyvalue);
+}
+
+void
+krb5_free_keyblock(krb5_context context,
+		   krb5_keyblock *keyblock)
+{
+    krb5_free_keyblock_contents(context, keyblock);
+    free(keyblock);
 }
 
 krb5_error_code
