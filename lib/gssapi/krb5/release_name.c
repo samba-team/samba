bc2ef14eec79428527759b101117deb3bc8c4f7b
Assar Westerlund <assar@sics.se>
1997-12-14 22:10:55 +0000

    stuppid warning
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@4195 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/gssapi/krb5/release_name.c
+++ lib/gssapi/krb5/release_name.c
@@ -45,8 +45,6 @@ OM_uint32 gss_release_name
             gss_name_t * input_name
            )
 {
-  krb5_error_code kret;
-
   gssapi_krb5_init ();
   krb5_free_principal(gssapi_krb5_context,
 		      *input_name);
