09e035e1d1de9ab44e8a1c17d38046e2fcaa2526
Assar Westerlund <assar@sics.se>
1997-12-15 00:26:02 +0000

    more prototypes
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@4202 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- kadmin/kadmin_locl.h
+++ kadmin/kadmin_locl.h
@@ -108,7 +108,15 @@ DECL(merge);
 
 #define KADMIN_APPL_VERSION "KADM0.0" /* XXX get from kadm5_locl.h */
 
+/* util.c */
+
+void timeval2str(time_t t, char *str, size_t len);
+void deltat2str(unsigned t, char *str, size_t len);
+unsigned str2deltat(const char *str);
+void attr2str(krb5_flags attributes, char *str, size_t len);
+krb5_flags str2attr(const char *str, krb5_flags orig);
 void get_response(const char*, const char*, char*, size_t);
 unsigned get_deltat(const char*, const char*);
+int edit_entry(kadm5_principal_ent_t ent, int *mask);
 
 #endif /* __ADMIN_LOCL_H__ */
