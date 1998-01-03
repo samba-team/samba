4c8e214011e748c07c42d6d0c9df676416d5e641
Johan Danielsson <joda@pdc.kth.se>
1998-01-03 21:17:34 +0000

    Add value for AFS salts.
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@4259 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/krb5/krb5.h
+++ lib/krb5/krb5.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997 Kungliga Tekniska Högskolan
+ * Copyright (c) 1997, 1998 Kungliga Tekniska Högskolan
  * (Royal Institute of Technology, Stockholm, Sweden). 
  * All rights reserved. 
  *
@@ -128,7 +128,8 @@ typedef HostAddresses krb5_addresses;
 typedef enum krb5_keytype { 
     KEYTYPE_NULL = 0,
     KEYTYPE_DES = 1,
-    KEYTYPE_DES3 = 7
+    KEYTYPE_DES3 = 7,
+    KEYTYPE_DES_AFS3 = 0x100000
 } krb5_keytype;
 
 typedef EncryptionKey krb5_keyblock;
