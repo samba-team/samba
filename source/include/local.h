596ce5e272b71718ce47a8b6b0b6538d8c846e20
Herb Lewis <herb@samba.org>
1998-03-23 18:31:38 +0000

    Added missing close comment. Causes warnings on SGI compilers

--- source/include/local.h
+++ source/include/local.h
@@ -1,5 +1,5 @@
 /* Copyright (C) 1995-1998 Samba-Team */
-/* Copyright (C) 1998 John H Terpstra <jht@aquasoft.com.au>
+/* Copyright (C) 1998 John H Terpstra <jht@aquasoft.com.au> */
 
 /* local definitions for file server */
 #ifndef _LOCAL_H
