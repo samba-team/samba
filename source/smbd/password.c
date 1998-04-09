5d956abb4f4ed22671dfb1c7cb51489ab280463f
John Terpstra <jht@samba.org>
1998-04-09 14:13:20 +0000

    Added const cast to struct args to get rid of compile time warning.

--- source/smbd/password.c
+++ source/smbd/password.c
@@ -468,7 +468,7 @@ static char *PAM_password;
  * echo off means password.
  */
 static int PAM_conv (int num_msg,
-                     struct pam_message **msg,
+                     const struct pam_message **msg,
                      struct pam_response **resp,
                      void *appdata_ptr) {
   int replies = 0;
