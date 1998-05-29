32dc0ec99c3db390298428d40dcbdd03c0bf82c4
Johan Danielsson <joda@pdc.kth.se>
1998-05-29 22:19:47 +0000

    import _krb_time_to_life, to avoid runtime dependencies on libkrb with some shared library implementations
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@4992 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/krb5/convert_creds.c
+++ lib/krb5/convert_creds.c
@@ -37,15 +37,85 @@
  */
 
 #include "krb5_locl.h"
-#ifdef KRB4
-#include <krb.h>
-
 RCSID("$Id$");
 
 static krb5_error_code
 check_ticket_flags(TicketFlags f)
 {
     return 0; /* maybe add some more tests here? */
+}
+
+/* include this here, to avoid dependencies on libkrb */
+
+#define		MAX_KTXT_LEN	1250
+
+#define 	ANAME_SZ	40
+#define		REALM_SZ	40
+#define		SNAME_SZ	40
+#define		INST_SZ		40
+
+struct ktext {
+    unsigned int length;		/* Length of the text */
+    unsigned char dat[MAX_KTXT_LEN];	/* The data itself */
+    u_int32_t mbz;		/* zero to catch runaway strings */
+};
+
+struct credentials {
+    char    service[ANAME_SZ];	/* Service name */
+    char    instance[INST_SZ];	/* Instance */
+    char    realm[REALM_SZ];	/* Auth domain */
+    des_cblock session;		/* Session key */
+    int     lifetime;		/* Lifetime */
+    int     kvno;		/* Key version number */
+    struct ktext ticket_st;	/* The ticket itself */
+    int32_t    issue_date;	/* The issue time */
+    char    pname[ANAME_SZ];	/* Principal's name */
+    char    pinst[INST_SZ];	/* Principal's instance */
+};
+
+
+#define TKTLIFENUMFIXED 64
+#define TKTLIFEMINFIXED 0x80
+#define TKTLIFEMAXFIXED 0xBF
+#define TKTLIFENOEXPIRE 0xFF
+#define MAXTKTLIFETIME	(30*24*3600)	/* 30 days */
+#ifndef NEVERDATE
+#define NEVERDATE ((time_t)0x7fffffffL)
+#endif
+
+static const int _tkt_lifetimes[TKTLIFENUMFIXED] = {
+   38400,   41055,   43894,   46929,   50174,   53643,   57352,   61318,
+   65558,   70091,   74937,   80119,   85658,   91581,   97914,  104684,
+  111922,  119661,  127935,  136781,  146239,  156350,  167161,  178720,
+  191077,  204289,  218415,  233517,  249664,  266926,  285383,  305116,
+  326213,  348769,  372885,  398668,  426234,  455705,  487215,  520904,
+  556921,  595430,  636601,  680618,  727680,  777995,  831789,  889303,
+  950794, 1016537, 1086825, 1161973, 1242318, 1328218, 1420057, 1518247,
+ 1623226, 1735464, 1855462, 1983758, 2120925, 2267576, 2424367, 2592000
+};
+
+static int
+_krb_time_to_life(time_t start, time_t end)
+{
+    int i;
+    time_t life = end - start;
+
+    if (life > MAXTKTLIFETIME || life <= 0) 
+	return 0;
+#if 0    
+    if (krb_no_long_lifetimes) 
+	return (life + 5*60 - 1)/(5*60);
+#endif
+    
+    if (end >= NEVERDATE)
+	return TKTLIFENOEXPIRE;
+    if (life < _tkt_lifetimes[0]) 
+	return (life + 5*60 - 1)/(5*60);
+    for (i=0; i<TKTLIFENUMFIXED; i++)
+	if (life <= _tkt_lifetimes[i])
+	    return i + TKTLIFEMINFIXED;
+    return 0;
+    
 }
 
 krb5_error_code
@@ -87,8 +157,8 @@ krb524_convert_creds_kdc(krb5_context context,
 				      v4creds->realm);
 	if(ret) goto out;
 	v4creds->issue_date = v5creds->times.authtime;
-	v4creds->lifetime = krb_time_to_life(v4creds->issue_date,
-					     v5creds->times.endtime);
+	v4creds->lifetime = _krb_time_to_life(v4creds->issue_date,
+					      v5creds->times.endtime);
 	ret = krb5_524_conv_principal(context, v5creds->client, 
 				      v4creds->pname, 
 				      v4creds->pinst, 
@@ -100,4 +170,3 @@ out:
     krb5_storage_free(sp);
     return ret;
 }
-#endif
