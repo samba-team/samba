c40201c60289300051a309924788df745616434c
Jeremy Allison <jra@samba.org>
1998-06-05 22:20:15 +0000

    includes.h: Added BSDI and NetBSD fixes. shmem_sysv.c: Added strerror debug print code. Jeremy.

--- source/include/includes.h
+++ source/include/includes.h
@@ -506,6 +506,7 @@ char *mktemp(char *); /* No standard include */
 #include <netinet/tcp.h>
 #define SIGNAL_CAST (void (*)())
 #define USE_DIRECT
+#define QSORT_CAST (int (*)(const void *, const void *))
 #endif
 
 
@@ -519,6 +520,8 @@ char *mktemp(char *); /* No standard include */
 #include <strings.h>
 #endif /* NetBSD1_3 */
 #include <netinet/tcp.h>
+#include <netinet/in_systm.h> 
+#include <netinet/ip.h> 
 /* you may not need this */
 #define NO_GETSPNAM
 #define SIGNAL_CAST (void (*)())
