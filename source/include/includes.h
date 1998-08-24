e94ab03a4f97ff7ee7bb938245cf054a00f950fd
Luke Leighton <lkcl@samba.org>
1998-08-24 18:53:37 +0000

    resource.h on FreeBSD 2.1 requires sys/time.h to come first.

--- source/include/includes.h
+++ source/include/includes.h
@@ -49,6 +49,17 @@
 
 #include <sys/types.h>
 
+#ifdef TIME_WITH_SYS_TIME
+#include <sys/time.h>
+#include <time.h>
+#else
+#ifdef HAVE_SYS_TIME_H
+#include <sys/time.h>
+#else
+#include <time.h>
+#endif
+#endif
+
 #ifdef HAVE_SYS_RESOURCE_H
 #include <sys/resource.h>
 #endif
@@ -86,17 +97,6 @@
 
 #ifdef HAVE_MALLOC_H
 #include <malloc.h>
-#endif
-
-#ifdef TIME_WITH_SYS_TIME
-#include <sys/time.h>
-#include <time.h>
-#else
-#ifdef HAVE_SYS_TIME_H
-#include <sys/time.h>
-#else
-#include <time.h>
-#endif
 #endif
 
 #ifdef HAVE_FCNTL_H
