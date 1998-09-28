c8c89585a30310482ce65a792f814c1937d225e4
Richard Sharpe <sharpe@samba.org>
1998-09-28 09:22:06 +0000

    The previous fix for WINS on the WINS server was wrong. It caused nmbd to exit :-(
    We now set the variable szWINSserver after the smb.conf file
    has been processed, but only of we are a WINS server.
    (This used to be commit 17212991cf0f49c5afc77cbd82dc6ce8a13f1405)

--- source3/param/loadparm.c
+++ source3/param/loadparm.c
@@ -809,9 +809,7 @@ static void init_globals(void)
   /* %N is the NIS auto.home server if -DAUTOHOME is used, else same as %L */
   string_set(&Globals.szLogonHome, "\\\\%N\\%U");
   string_set(&Globals.szLogonPath, "\\\\%N\\%U\\profile");
-  /* Set szWINSserver to 127.0.0.1 so that we have something reasonable */
-  /* in case it is not set in the smb.conf                              */
-  string_set(&Globals.szWINSserver, "127.0.0.1");
+
   string_set(&Globals.szNameResolveOrder, "lmhosts host wins bcast");
 
   Globals.bLoadPrinters = True;
@@ -2483,6 +2481,15 @@ BOOL lp_load(char *pszFname,BOOL global_only, BOOL save_defaults, BOOL add_ipc)
   set_default_server_announce_type();
 
   bLoaded = True;
+
+  /* Now we check bWINSsupport and set szWINSserver to 127.0.0.1 */
+  /* if bWINSsupport is True                                     */
+
+  if (Globals.bWINSsupport) {
+
+    string_set(&Globals.szWINSserver, "127.0.0.1");
+
+  }
 
   return (bRetval);
 }
