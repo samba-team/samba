e3fd861590dd27cc643c2d8d0cb5a5651c84d9ac
Andrew Tridgell <tridge@samba.org>
1998-10-03 16:34:57 +0000

    drat.
    on Linux 2.0 libc detects the lack of getdents in the kernel and used
    opendir() etc. so we need to implement those ...
    
    it would be needed for a port to others OSes anyway I suppose.

---
+++ source/smbwrapper/readdir.c
@@ -0,0 +1,36 @@
+/* 
+   Unix SMB/Netbios implementation.
+   Version 2.0
+   SMB wrapper functions
+   Copyright (C) Andrew Tridgell 1998
+   
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+
+#include "wrapper.h"
+
+#ifdef linux
+__asm__(".globl __readdir; __readdir = readdir");
+#endif
+
+ struct dirent *readdir(DIR *dir)
+{
+	if (smbw_dirp(dir)) {
+		return smbw_readdir(dir);
+	}
+
+	return real_readdir(dir);
+}
