784b1590c8c305d6876a14fcaa6fc17b9daac6d0
Andrew Tridgell <tridge@samba.org>
1998-10-13 08:27:28 +0000

    smbsh launch program is now in C

---
+++ source/smbwrapper/smbsh.c
@@ -0,0 +1,62 @@
+/* 
+   Unix SMB/Netbios implementation.
+   Version 2.0
+   SMB wrapper functions - frontend
+   Copyright (C) Andrew Tridgell 1998
+   
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include "includes.h"
+
+int main(int argc, char *argv[])
+{
+	char *p, *u;
+	char *libd = BINDIR;
+	
+	pstring line;
+
+	printf("Username: ");
+	u = fgets_slash(line, sizeof(line)-1, stdin);
+
+	p = getpass("Password: ");
+
+	setenv("SMBW_USER", u, 1);
+	setenv("SMBW_PASSWORD", p, 1);
+	setenv("PS1", "smbsh$ ", 1);
+
+	sys_getwd(line);
+
+	setenv("PWD", line, 1);
+
+	slprintf(line,sizeof(line)-1,"%s/smbwrapper.so", libd);
+	setenv("LD_PRELOAD", line, 1);
+
+	slprintf(line,sizeof(line)-1,"%s/smbwrapper.32.so", libd);
+
+	if (file_exist(line, NULL)) {
+		slprintf(line,sizeof(line)-1,"%s/smbwrapper.32.so:DEFAULT", libd);
+		setenv("_RLD_LIST", line, 1);
+		slprintf(line,sizeof(line)-1,"%s/smbwrapper.so:DEFAULT", libd);
+		setenv("_RLD32_LIST", line, 1);
+	} else {
+		slprintf(line,sizeof(line)-1,"%s/smbwrapper.so:DEFAULT", libd);
+		setenv("_RLD_LIST", line, 1);
+	}
+
+	execl("/bin/sh","sh",NULL);
+	printf("launch failed!\n");
+	return 1;
+}	
