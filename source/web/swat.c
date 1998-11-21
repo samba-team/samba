c54bd16ccabff81eb8dd75e8eeaef3749ddfe97f
Jeremy Allison <jra@samba.org>
1998-11-21 02:30:46 +0000

    One more cut'n'paste bug. (Ooops). Jeremy.

--- source/web/swat.c
+++ source/web/swat.c
@@ -600,7 +600,7 @@ static BOOL change_password(const char *remote_machine, char *user_name,
 		return ret;
 	}
 
-	if(!initialise_password_db()) {
+	if(!initialize_password_db()) {
 		printf("Can't setup password database vectors.\n<p>");
 		return False;
 	}
