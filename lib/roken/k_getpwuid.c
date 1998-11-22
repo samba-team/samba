d934a8c12f3c1a9a42fae054d9b0c565e2f87202
Assar Westerlund <assar@sics.se>
1998-11-22 09:23:04 +0000

    (k_getpwuid): check for `struct spwd'
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@5218 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/roken/k_getpwuid.c
+++ lib/roken/k_getpwuid.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1995, 1996, 1997 Kungliga Tekniska Högskolan
+ * Copyright (c) 1995, 1996, 1997, 1998 Kungliga Tekniska Högskolan
  * (Royal Institute of Technology, Stockholm, Sweden).
  * All rights reserved.
  * 
@@ -52,7 +52,7 @@ k_getpwuid (uid_t uid)
      struct passwd *p;
 
      p = getpwuid (uid);
-#ifdef HAVE_GETSPUID
+#if defined(HAVE_GETSPUID) && defined(HAVE_STRUCT_SPWD)
      if (p)
      {
 	  struct spwd *spwd;
