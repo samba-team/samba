ced486c8415c7ece457edecb3246f7277a57d60b
Luke Leighton <lkcl@samba.org>
1998-11-24 16:47:49 +0000

    sorting out difference between aliases and groups in the cases where unix groups are not explicitly mapped.
    i.e as a PDC or BDC you can have domain groups, as a member of a domain
    you cannot.
    
    as a member of a domain, unmapped unix groups are assumed to be aliases,
    and as a PDC or BDC, unmapped unix groups are assumed to be unix groups.
    
    there is _one_ other check needed with aliases to be added: unmapped unix
    groups that have the same name as an NT group on the PDC (for which i will
    need to write an LsaLookupNames call) should be assumed to be domain groups
    on the PDC.
    (This used to be commit 53b49b44e13a4ca9818ebc947372b1374831b568)

--- source3/lib/util_sid.c
+++ source3/lib/util_sid.c
@@ -134,7 +134,10 @@ BOOL sid_split_rid(DOM_SID *sid, uint32 *rid)
 	if (sid->num_auths > 0)
 	{
 		sid->num_auths--;
-		(*rid) = sid->sub_auths[sid->num_auths];
+		if (rid != NULL)
+		{
+			(*rid) = sid->sub_auths[sid->num_auths];
+		}
 		return True;
 	}
 	return False;
