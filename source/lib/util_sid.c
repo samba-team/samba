ab43a0a7c55a142df400f11a0bb0d478ed7cf5fa
Jeremy Allison <jra@samba.org>
1998-11-25 02:12:53 +0000

    Fixed parameter strings to slprintf for sid components. Jeremy.

--- source/lib/util_sid.c
+++ source/lib/util_sid.c
@@ -39,11 +39,11 @@ char *sid_to_string(pstring sidstr_out, DOM_SID *sid)
               (sid->id_auth[3] << 16) +
               (sid->id_auth[2] << 24);
 
-  slprintf(sidstr_out, sizeof(pstring) - 1, "S-%u-%u", sid->sid_rev_num, ia);
+  slprintf(sidstr_out, sizeof(pstring) - 1, "S-%u-%lu", (unsigned int)sid->sid_rev_num, (unsigned long)ia);
 
   for (i = 0; i < sid->num_auths; i++)
   {
-    slprintf(subauth, sizeof(subauth)-1, "-%u", sid->sub_auths[i]);
+    slprintf(subauth, sizeof(subauth)-1, "-%lu", (unsigned long)sid->sub_auths[i]);
     pstrcat(sidstr_out, subauth);
   }
 
@@ -76,7 +76,7 @@ BOOL string_to_sid(DOM_SID *sidout, char *sidstr)
   }
 
   /* Get the revision number. */
-  sidout->sid_rev_num = (uint32)strtoul(tok, NULL, 10);
+  sidout->sid_rev_num = (uint8)strtoul(tok, NULL, 10);
 
   if (!next_token(&p, tok, "-", sizeof(tok))) {
     DEBUG(0,("string_to_sid: Sid %s is not in a valid format.\n", sidstr));
