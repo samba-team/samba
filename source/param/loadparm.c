2703372ce4dec0e6368fccb743f144d5771b51d6
Herb Lewis <herb@samba.org>
1999-02-23 00:13:34 +0000

    output [global] section header for testparm and swat.

--- source/param/loadparm.c
+++ source/param/loadparm.c
@@ -2234,7 +2234,7 @@ Display the contents of the global structure.
 static void dump_globals(FILE *f)
 {
 	int i;
-	fprintf(f, "# Global parameters\n");
+	fprintf(f, "[global]\n");
 	
 	for (i=0;parm_table[i].label;i++)
 		if (parm_table[i].class == P_GLOBAL &&
