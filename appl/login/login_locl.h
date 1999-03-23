87e483ec7871d6e7012528474d7947f78ae809d6
Johan Danielsson <joda@pdc.kth.se>
1999-03-23 13:17:38 +0000

    _PATH_UTMP
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@5710 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- appl/login/login_locl.h
+++ appl/login/login_locl.h
@@ -90,6 +90,13 @@
 #define _PATH_WTMP "/var/adm/wtmp"
 #endif
 #endif
+#ifndef _PATH_UTMP
+#ifdef UTMP_FILE
+#define _PATH_UTMP UTMP_FILE
+#else
+#define _PATH_UTMP "/var/adm/utmp"
+#endif
+#endif
 
 char *clean_ttyname (char*);
 char *make_id (char*);
