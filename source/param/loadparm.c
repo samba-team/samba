ea6946d48fd195858643eedc6970664d02bf7392
Herb Lewis <herb@samba.org>
1999-03-24 19:49:47 +0000

    make swat and testparm print out global section name

--- source/param/loadparm.c
+++ source/param/loadparm.c
@@ -2249,7 +2249,7 @@ Display the contents of the global structure.
 static void dump_globals(FILE *f)
 {
 	int i;
-	fprintf(f, "# Global parameters\n");
+	fprintf(f, "# Global parameters\n[global]\n");
 	
 	for (i=0;parm_table[i].label;i++)
 		if (parm_table[i].class == P_GLOBAL &&
