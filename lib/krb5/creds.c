7c2cf730587305f9ce3083fef411a2f02653e3ac
Assar Westerlund <assar@sics.se>
1999-05-03 02:01:41 +0000

    (krb5_compare_creds): just verify that the keytypes/enctypes are compatible, not that they are the same
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@6064 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/krb5/creds.c
+++ lib/krb5/creds.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 1998 Kungliga Tekniska Högskolan
+ * Copyright (c) 1997, 1998, 1999 Kungliga Tekniska Högskolan
  * (Royal Institute of Technology, Stockholm, Sweden). 
  * All rights reserved. 
  *
@@ -140,7 +140,9 @@ krb5_compare_creds(krb5_context context, krb5_flags whichfields,
     else
 	match = krb5_principal_compare(context, mcreds->server, creds->server);
     if(match && (whichfields & KRB5_TC_MATCH_KEYTYPE) &&
-       mcreds->session.keytype != creds->session.keytype)
+       !krb5_enctypes_comptible_keys (context,
+				      mcreds->session.keytype,
+				      creds->session.keytype))
 	match = FALSE;
     return match;
 }
