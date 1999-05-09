bd4b92f446952ec8928cd3decdaa3f6bbd9ee429
Andrew Tridgell <tridge@samba.org>
1999-05-09 09:55:24 +0000

    fixed typo

--- source/utils/torture.c
+++ source/utils/torture.c
@@ -99,7 +99,7 @@ static BOOL open_connection(struct cli_state *c)
 		return False;
 	}
 
-	cli->timeout = 120000; /* set a really long timeout (2 minutes) */
+	c->timeout = 120000; /* set a really long timeout (2 minutes) */
 
 	if (!cli_session_request(c, &calling, &called)) {
 		printf("%s rejected the session\n",host);
