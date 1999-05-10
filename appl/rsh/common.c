98760591be56955dea8e16d760b8d1ae246fe4ff
Assar Westerlund <assar@sics.se>
1999-05-10 03:50:07 +0000

    (do_write, do_read): always return -1 for failure (net_write, net_read): remove.  they already exist in libroken
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@6194 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- appl/rsh/common.c
+++ appl/rsh/common.c
@@ -59,14 +59,14 @@ do_read (int fd,
 
 	    ret = krb5_net_read (context, &fd, &len, 4);
 	    if (ret != 4)
-		return ret;
+		return -1;
 	    len = ntohl(len);
 	    outer_len = krb5_get_wrapped_length (context, crypto, len);
 	    if (outer_len > sz)
 		abort ();
 	    ret = krb5_net_read (context, &fd, buf, outer_len);
 	    if (ret != outer_len)
-		return ret;
+		return -1;
 
 	    status = krb5_decrypt(context, crypto, KRB5_KU_OTHER_ENCRYPTED, 
 				  buf, outer_len, &data);
@@ -74,7 +74,7 @@ do_read (int fd,
 	    if (status)
 		errx (1, "%s", krb5_get_err_text (context, status));
 	    memcpy (buf, data.data, len);
-	    free (data.data);
+	    krb5_data_free (&data);
 	    return len;
 	} else {
 	    abort ();
@@ -121,42 +121,4 @@ do_write (int fd, void *buf, size_t sz)
 	}
     } else
 	return write (fd, buf, sz);
-}
-
-ssize_t
-net_write (int fd,
-	   const void *buf,
-	   size_t len)
-{
-  char *cbuf = (char *)buf;
-  ssize_t count;
-  size_t rem = len;
-
-  while (rem > 0) {
-    count = write (fd, cbuf, rem);
-    if (count < 0)
-      return count;
-    cbuf += count;
-    rem -= count;
-  }
-  return len;
-}
-
-ssize_t
-net_read (int fd,
-	  void *buf,
-	  size_t len)
-{
-  char *cbuf = (char *)buf;
-  ssize_t count;
-  size_t rem = len;
-
-  while (rem > 0) {
-    count = read (fd, cbuf, rem);
-    if (count <= 0)
-      return count;
-    cbuf += count;
-    rem -= count;
-  }
-  return len;
 }
