9ad534b2eb9e61334cff1bd49fd98c8e89d29af7
Jeremy Allison <jra@samba.org>
1999-05-26 19:06:44 +0000

    Added patch from Branko Cibej <branko.cibej@hermes.si> to allow smbwrapper to be configured on HPUX. Added fixes for glibc2.1 bug. Jeremy.

--- source/tests/fcntl_lock64.c
+++ source/tests/fcntl_lock64.c
@@ -1,4 +1,4 @@
-/* test whether fcntl locking works on this system */
+/* test whether 64 bit fcntl locking really works on this system */
 
 #if defined(HAVE_UNISTD_H)
 #include <unistd.h>
@@ -27,21 +27,17 @@ static int sys_waitpid(pid_t pid,int *status,int options)
 #endif /* USE_WAITPID */
 }
 
-#define DATA "conftest.fcntl"
-
-#ifndef SEEK_SET
-#define SEEK_SET 0
-#endif
+#define DATA "conftest.fcntl64"
 
 /* lock a byte range in a open file */
 int main(int argc, char *argv[])
 {
-	struct flock lock;
+	struct flock64 lock;
 	int fd, pid, ret, status=1;
 
 	if (!(pid=fork())) {
 		sleep(2);
-		fd = open(DATA, O_RDONLY);
+		fd = open64(DATA, O_RDONLY);
 
 		if (fd == -1) exit(1);
 
@@ -54,40 +50,42 @@ int main(int argc, char *argv[])
 		lock.l_type = F_WRLCK;
 		
 		/* check if a lock applies */
-		ret = fcntl(fd,F_GETLK,&lock);
+		ret = fcntl(fd,F_GETLK64,&lock);
 
 		if ((ret == -1) ||
 		    (lock.l_type == F_UNLCK)) {
+/*            printf("No lock conflict\n"); */
 			exit(1);
 		} else {
+/*            printf("lock conflict\n"); */
 			exit(0);
 		}
 	}
 
-	fd = open(DATA, O_RDWR|O_CREAT|O_TRUNC, 0600);
+	fd = open64(DATA, O_RDWR|O_CREAT|O_TRUNC, 0600);
 
 	lock.l_type = F_WRLCK;
 	lock.l_whence = SEEK_SET;
-	lock.l_start = 0;
+	lock.l_start = 0x100000000;
 	lock.l_len = 4;
 	lock.l_pid = getpid();
 
 	/* set a 4 byte write lock */
-	fcntl(fd,F_SETLK,&lock);
+	fcntl(fd,F_SETLK64,&lock);
 
 	sys_waitpid(pid, &status, 0);
 
-	unlink(DATA);
-
 #if defined(WIFEXITED) && defined(WEXITSTATUS)
-    if(WIFEXITED(status)) {
-        status = WEXITSTATUS(status);
-    } else {
-        status = 1;
-    }
+	if(WIFEXITED(status)) {
+		status = WEXITSTATUS(status);
+	} else {
+		status = 1;
+	}
 #else /* defined(WIFEXITED) && defined(WEXITSTATUS) */
 	status = (status == 0) ? 0 : 1;
 #endif /* defined(WIFEXITED) && defined(WEXITSTATUS) */
+
+	unlink(DATA);
 
 	exit(status);
 }
