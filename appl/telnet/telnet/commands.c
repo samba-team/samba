f0391c5536372caa48ff1ac72191ed491a09debc
Assar Westerlund <assar@sics.se>
1999-06-15 02:45:37 +0000

    (tn): use get_default_username
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@6313 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- appl/telnet/telnet/commands.c
+++ appl/telnet/telnet/commands.c
@@ -2316,18 +2316,8 @@ tn(int argc, char **argv)
 #endif
     } while (connected == 0);
     cmdrc(hostp, hostname);
-    if (autologin && user == NULL) {
-	struct passwd *pw;
-
-	user = getenv("USER");
-	if (user == NULL ||
-	    ((pw = k_getpwnam((char *)user)) && pw->pw_uid != getuid())) {
-		if ((pw = k_getpwuid(getuid())))
-			user = pw->pw_name;
-		else
-			user = NULL;
-	}
-    }
+    if (autologin && user == NULL)
+	user = get_default_username ();
     if (user) {
 	env_define((unsigned char *)"USER", (unsigned char *)user);
 	env_export((unsigned char *)"USER");
