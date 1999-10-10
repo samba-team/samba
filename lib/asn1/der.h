d4dcfc71993e41e4312fb06adb2fcabf2ddc8d6a
Assar Westerlund <assar@sics.se>
1999-10-10 04:48:28 +0000

    const-ize the prototypes
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@7125 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/asn1/der.h
+++ lib/asn1/der.h
@@ -73,42 +73,49 @@ time_t timegm (struct tm *);
 
 void time2generalizedtime (time_t t, octet_string *s);
 
-int der_get_int (unsigned char *p, size_t len, int *ret, size_t *size);
-int der_get_length (unsigned char *p, size_t len, size_t *val, size_t *size);
-int der_get_general_string (unsigned char *p, size_t len, 
+int der_get_int (const unsigned char *p, size_t len, int *ret, size_t *size);
+int der_get_length (const unsigned char *p, size_t len,
+		    size_t *val, size_t *size);
+int der_get_general_string (const unsigned char *p, size_t len, 
 			    general_string *str, size_t *size);
-int der_get_octet_string (unsigned char *p, size_t len, 
+int der_get_octet_string (const unsigned char *p, size_t len, 
 			  octet_string *data, size_t *size);
-int der_get_tag (unsigned char *p, size_t len, 
+int der_get_tag (const unsigned char *p, size_t len, 
 		 Der_class *class, Der_type *type,
 		 int *tag, size_t *size);
 
-int der_match_tag (unsigned char *p, size_t len, 
+int der_match_tag (const unsigned char *p, size_t len, 
 		   Der_class class, Der_type type,
 		   int tag, size_t *size);
-int der_match_tag_and_length (unsigned char *p, size_t len,
+int der_match_tag_and_length (const unsigned char *p, size_t len,
 			      Der_class class, Der_type type, int tag,
 			      size_t *length_ret, size_t *size);
 
-int decode_integer (unsigned char*, size_t, int*, size_t*);
-int decode_general_string (unsigned char*, size_t, general_string*, size_t*);
-int decode_octet_string (unsigned char*, size_t, octet_string*, size_t*);
-int decode_generalized_time (unsigned char*, size_t, time_t*, size_t*);
-
+int decode_integer (const unsigned char*, size_t, int*, size_t*);
+int decode_general_string (const unsigned char*, size_t,
+			   general_string*, size_t*);
+int decode_octet_string (const unsigned char*, size_t, octet_string*, size_t*);
+int decode_generalized_time (const unsigned char*, size_t, time_t*, size_t*);
 
 int der_put_int (unsigned char *p, size_t len, int val, size_t*);
 int der_put_length (unsigned char *p, size_t len, size_t val, size_t*);
-int der_put_general_string (unsigned char *p, size_t len, general_string *str, size_t*);
-int der_put_octet_string (unsigned char *p, size_t len, octet_string *data, size_t*);
+int der_put_general_string (unsigned char *p, size_t len,
+			    const general_string *str, size_t*);
+int der_put_octet_string (unsigned char *p, size_t len,
+			  const octet_string *data, size_t*);
 int der_put_tag (unsigned char *p, size_t len, Der_class class, Der_type type,
 		 int tag, size_t*);
 int der_put_length_and_tag (unsigned char*, size_t, size_t, 
 			    Der_class, Der_type, int, size_t*);
 
-int encode_integer (unsigned char *p, size_t len, int *data, size_t*);
-int encode_general_string (unsigned char *p, size_t len, general_string *data, size_t*);
-int encode_octet_string (unsigned char *p, size_t len, octet_string *k, size_t*);
-int encode_generalized_time (unsigned char *p, size_t len, time_t *t, size_t*);
+int encode_integer (unsigned char *p, size_t len,
+		    const int *data, size_t*);
+int encode_general_string (unsigned char *p, size_t len, 
+			   const general_string *data, size_t*);
+int encode_octet_string (unsigned char *p, size_t len,
+			 const octet_string *k, size_t*);
+int encode_generalized_time (unsigned char *p, size_t len,
+			     const time_t *t, size_t*);
 
 void free_integer (int *num);
 void free_general_string (general_string *str);
@@ -116,10 +123,10 @@ void free_octet_string (octet_string *k);
 void free_generalized_time (time_t *t);
 
 size_t length_len (size_t len);
-size_t length_integer (int *data);
-size_t length_general_string (general_string *data);
-size_t length_octet_string (octet_string *k);
-size_t length_generalized_time (time_t *t);
+size_t length_integer (const int *data);
+size_t length_general_string (const general_string *data);
+size_t length_octet_string (const octet_string *k);
+size_t length_generalized_time (const time_t *t);
 
 int copy_general_string (const general_string *from, general_string *to);
 int copy_octet_string (const octet_string *from, octet_string *to);
