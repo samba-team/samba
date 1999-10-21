d6ff0b76510e54c50ff95afc9758730e28702b3f
Johan Danielsson <joda@pdc.kth.se>
1999-10-21 17:24:18 +0000

    (krb4_decode): syslog failure reason
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@7218 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- appl/ftp/ftp/krb4.c
+++ appl/ftp/ftp/krb4.c
@@ -87,6 +87,7 @@ krb4_decode(void *app_data, void *buf, int len, int level)
 			(struct sockaddr_in *)REMOTE_ADDR,
 			(struct sockaddr_in *)LOCAL_ADDR, &m);
     if(e){
+	syslog(LOG_ERR, "krb4_decode: %s", krb_get_err_text(e));
 	return -1;
     }
     memmove(buf, m.app_data, m.app_length);
