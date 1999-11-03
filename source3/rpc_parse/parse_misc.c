59a7e6cef89a9bd519df64978f8c8f113d66d0ef
Luke Leighton <lkcl@samba.org>
1999-11-03 20:01:07 +0000

    had to move unistr2_dup(), unistr2_free() and unistr2_copy() into util_unistr.c in order to get bin/testparm to compile. (This used to be commit e718ce9c4a3598483e38b8c32bdf2924593edc1e)

--- source3/rpc_parse/parse_misc.c
+++ source3/rpc_parse/parse_misc.c
@@ -745,40 +745,6 @@ BOOL make_buf_unistr2(UNISTR2 *str, uint32 *ptr, char *buf)
 }
 
 /*******************************************************************
-copies a UNISTR2 structure.
-********************************************************************/
-BOOL copy_unistr2(UNISTR2 *str, const UNISTR2 *from)
-{
-	/* set up string lengths. add one if string is not null-terminated */
-	str->uni_max_len = from->uni_max_len;
-	str->undoc       = from->undoc;
-	str->uni_str_len = from->uni_str_len;
-
-	/* copy the string */
-	memcpy(str->buffer, from->buffer, sizeof(from->buffer));
-
-	return True;
-}
-
-/*******************************************************************
-duplicates a UNISTR2 structure.
-********************************************************************/
-UNISTR2 *unistr2_dup(const UNISTR2 *name)
-{
-	UNISTR2 *copy = (UNISTR2*)malloc(sizeof(*copy));
-	copy_unistr2(copy, name);
-	return copy;
-}
-
-/*******************************************************************
-frees a UNISTR2 structure.
-********************************************************************/
-void unistr2_free(UNISTR2 *name)
-{
-	free(name);
-}
-
-/*******************************************************************
 creates a STRING2 structure.
 ********************************************************************/
 BOOL make_string2(STRING2 *str, char *buf, int len)
