98c42764fba365d612a8ae4b3172b03367066112
Luke Leighton <lkcl@samba.org>
1999-11-20 22:05:31 +0000

    oops, #ifdef'd cli_shutdown out, as the fun has _already_ started: NT refuses to play nice, and establish a trust relationship.

--- source/smbd/password.c
+++ source/smbd/password.c
@@ -1247,10 +1247,9 @@ BOOL domain_client_validate( char *user, char *domain, char *server_list,
 	}
 #endif /* 0 */
 
-#if 0
 	cli_nt_session_close(&cli, nt_pipe_fnum);
 	cli_ulogoff(&cli);
 	cli_shutdown(&cli);
-#endif
+
 	return True;
 }
