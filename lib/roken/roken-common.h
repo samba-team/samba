b71c4993aa5e03fb1918b653f663c25a7226cd12
Assar Westerlund <assar@sics.se>
1999-12-03 03:59:49 +0000

    remove commented-out stuff
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@7472 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/roken/roken-common.h
+++ lib/roken/roken-common.h
@@ -136,39 +136,6 @@
 #define NO_ADDRESS NO_DATA
 #endif
 
-#if 0
-
-struct addrinfo {
-    int    ai_flags;
-    int    ai_family;
-    int    ai_socktype;
-    int    ai_protocol;
-    size_t ai_addrlen;
-    char  *ai_canonname;
-    struct sockaddr *ai_addr;
-    struct addrinfo *ai_next;
-};
-
-#define EAI_ADDRFAMILY	1	/* address family for nodename not supported */
-#define EAI_AGAIN	2	/* temporary failure in name resolution */
-#define EAI_BADFLAGS	3	/* invalid value for ai_flags */
-#define EAI_FAIL	4	/* non-recoverable failure in name resolution */
-#define EAI_FAMILY	5	/* ai_family not supported */
-#define EAI_MEMORY	6	/* memory allocation failure */
-#define EAI_NODATA	7	/* no address associated with nodename */
-#define EAI_NONAME	8	/* nodename nor servname provided, or not known */
-#define EAI_SERVICE	9	/* servname not supported for ai_socktype */
-#define EAI_SOCKTYPE   10	/* ai_socktype not supported */
-#define EAI_SYSTEM     11	/* system error returned in errno */
-
-/* flags for getaddrinfo() */
-
-#define AI_PASSIVE	0x01
-#define AI_CANONNAME	0x02
-#define AI_NUMERICHOST	0x04
-
-#endif
-
 /*
  * constants for inet_ntop
  */
