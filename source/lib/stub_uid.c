9ef8fddaa6080e4942bc2268b89e61c3fb1ccd3d
Luke Leighton <lkcl@samba.org>
2000-01-03 00:36:33 +0000

    added stub become_root() / unbecome_root() functions.  they are used quite frequently (swat, rpcclient and others) so as i needed stub (dummy, do nada) become_root() in rpc_client/cli_connect, i thought it best to put them in a file.

---
+++ source/lib/stub_uid.c
@@ -0,0 +1,32 @@
+/* 
+   Unix SMB/Netbios implementation.
+   Version 1.9.
+   SMB Utilities
+   Copyright (C) Andrew Tridgell 1997-1999
+   
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include "includes.h"
+
+/* we need these because we link to locking*.o */
+void become_root(BOOL save_dir)
+{
+}
+
+void unbecome_root(BOOL restore_dir)
+{
+}
+
