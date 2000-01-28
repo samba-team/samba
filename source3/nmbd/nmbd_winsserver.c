40122f6e32627f2afaad0e66eb2f5f6eaa56a4e0
Jeremy Allison <jra@samba.org>
2000-01-28 23:47:31 +0000

    Modified auto alignment of ud[] struct on stack to be declared as an array of pointers. This should cause alignment on a correct boundary.. Spotted by Darren Reed <darrenr@telnetmedia.com>. Jeremy. (This used to be commit 91f4d0675333d7c5d7bb5ff077faaf83e1fd9bfe)

--- source3/nmbd/nmbd_winsserver.c
+++ source3/nmbd/nmbd_winsserver.c
@@ -838,7 +838,7 @@ is one of our (WINS server) names. Denying registration.\n", nmb_namestr(questio
 
   if( namerec != NULL )
   {
-    char ud[sizeof(struct userdata_struct) + sizeof(struct packet_struct *)];
+    long *ud[(sizeof(struct userdata_struct) + sizeof(struct packet_struct *))/sizeof(long *) + 1];
     struct userdata_struct *userdata = (struct userdata_struct *)ud;
 
     /*
@@ -1146,7 +1146,7 @@ is one of our (WINS server) names. Denying registration.\n", nmb_namestr(questio
 
   if(namerec != NULL)
   {
-    char ud[sizeof(struct userdata_struct) + sizeof(struct packet_struct *)];
+    long *ud[(sizeof(struct userdata_struct) + sizeof(struct packet_struct *))/sizeof(long *) + 1];
     struct userdata_struct *userdata = (struct userdata_struct *)ud;
 
     /*
