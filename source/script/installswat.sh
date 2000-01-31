7c23f0a7300cede9815ed9068889992d4f4f406a
Tim Potter <tpot@samba.org>
2000-01-31 04:27:52 +0000

    Initial version of including the O'Reilly Using Samba book as part of swat.

--- source/script/installswat.sh
+++ source/script/installswat.sh
@@ -1,22 +1,24 @@
-#!/bin/sh
+#!/bin/sh -x
 #fist version March 1998, Andrew Tridgell
 
 SWATDIR=$1
 SRCDIR=$2/
+BOOKDIR=$3
 
 echo Installing SWAT in $SWATDIR
-
 echo Installing the Samba Web Administration Tool
 
 for d in $SWATDIR $SWATDIR/help $SWATDIR/images $SWATDIR/include; do
-if [ ! -d $d ]; then
-  mkdir $d
-if [ ! -d $d ]; then
-  echo Failed to make directory $d, does $USER have privileges?
-  exit 1
-fi
-fi
+    if [ ! -d $d ]; then
+        mkdir $d
+        if [ ! -d $d ]; then
+            echo Failed to make directory $d, does $USER have privileges?
+            exit 1
+        fi
+    fi
 done
+
+# Install images
 
 for f in $SRCDIR../swat/images/*.gif; do
       FNAME=$SWATDIR/images/`basename $f`
@@ -25,12 +27,23 @@ for f in $SRCDIR../swat/images/*.gif; do
       chmod 0644 $FNAME
 done
 
+# Install html help
+
 for f in $SRCDIR../swat/help/*.html; do
       FNAME=$SWATDIR/help/`basename $f`
       echo $FNAME
+      if [ "x$BOOKDIR" = "x" ]; then
+        cat $f | sed 's/@BOOKDIR@.*$//' > $f.tmp
+      else
+        cat $f | sed 's/@BOOKDIR@//' > $f.tmp
+      fi
+      f=$f.tmp
       cp $f $FNAME || echo Cannot install $FNAME. Does $USER have privileges?
+      rm -f $f
       chmod 0644 $FNAME
 done
+
+# Install html documentation
 
 for f in $SRCDIR../docs/htmldocs/*.html; do
       FNAME=$SWATDIR/help/`basename $f`
@@ -39,12 +52,59 @@ for f in $SRCDIR../docs/htmldocs/*.html; do
       chmod 0644 $FNAME
 done
 
+# Install "server-side" includes
+
 for f in $SRCDIR../swat/include/*.html; do
       FNAME=$SWATDIR/include/`basename $f`
       echo $FNAME
       cp $f $FNAME || echo Cannot install $FNAME. Does $USER have privileges?
       chmod 0644 $FNAME
 done
+
+# Install Using Samba book
+
+if [ "x$BOOKDIR" != "x" ]; then
+
+    # Create directories
+
+    for d in $BOOKDIR $BOOKDIR/figs $BOOKDIR/gifs; do
+        if [ ! -d $d ]; then
+            mkdir $d
+            if [ ! -d $d ]; then
+                echo Failed to make directory $d, does $USER have privileges?
+                exit 1
+            fi
+        fi
+    done
+
+    # HTML files
+
+    for f in $SRCDIR../docs/htmldocs/using_samba/*.html; do
+        FNAME=$BOOKDIR/`basename $f`
+        echo $FNAME
+        cp $f $FNAME || echo Cannot install $FNAME. Does $USER have privileges?
+        chmod 0644 $FNAME
+    done
+
+    # Figures
+
+    for f in $SRCDIR../docs/htmldocs/using_samba/figs/*.gif; do
+        FNAME=$BOOKDIR/figs/`basename $f`
+        echo $FNAME
+        cp $f $FNAME || echo Cannot install $FNAME. Does $USER have privileges?
+        chmod 0644 $FNAME
+    done
+
+    # Gifs
+
+    for f in $SRCDIR../docs/htmldocs/using_samba/gifs/*.gif; do
+        FNAME=$BOOKDIR/gifs/`basename $f`
+        echo $FNAME
+        cp $f $FNAME || echo Cannot install $FNAME. Does $USER have privileges?
+        chmod 0644 $FNAME
+    done
+
+fi
 
 cat << EOF
 ======================================================================
