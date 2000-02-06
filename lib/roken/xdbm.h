9e913399af65897c9564d6e170ce598d3b45d922
Assar Westerlund <assar@sics.se>
2000-02-06 05:03:27 +0000

    hopefully catch a few more declarations by including <ndbm.h> even if <db.h> was found
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@7852 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/roken/xdbm.h
+++ lib/roken/xdbm.h
@@ -41,7 +41,9 @@
 #if defined(HAVE_DB_H)
 #define DB_DBM_HSEARCH 1
 #include <db.h>
-#elif defined(HAVE_NDBM_H)
+#endif
+
+#if defined(HAVE_NDBM_H)
 #include <ndbm.h>
 #elif defined(HAVE_DBM_H)
 #include <dbm.h>
