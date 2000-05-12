75859d689ed115a36af9274e53b873cbd93e0fc0
Tim Potter <tpot@samba.org>
2000-05-12 05:18:42 +0000

    simple build script for appliance rpm (This used to be commit 40b8095a9a367d2fee1e58d12affc5883d4adc0a)

---
+++ examples/appliance/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+for dir in SOURCES RPMS/i386 SRPMS BUILD; do
+    mkdir -p /tmp/$dir
+done
+
+tar cfz /tmp/SOURCES/samba-appliance-0.1-src.tar.gz samba-appliance-0.1
+rpm -ba appliance.spec
