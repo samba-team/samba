b022e6ab9785588fd4123ae4d819936fb5bfdbdc
Andrew Tridgell <tridge@samba.org>
2000-05-12 05:43:02 +0000

    fixed tar command line

--- examples/appliance/build.sh
+++ examples/appliance/build.sh
@@ -4,5 +4,5 @@ for dir in SOURCES RPMS/i386 SRPMS BUILD; do
     mkdir -p /tmp/$dir
 done
 
-tar --exclude=CVS cfz /tmp/SOURCES/samba-appliance-0.1-src.tar.gz samba-appliance-0.1
+tar --exclude=CVS -czf /tmp/SOURCES/samba-appliance-0.1-src.tar.gz samba-appliance-0.1
 rpm -ba appliance.spec
