9ba818277fc5ea67a18082e9ead4d76cf931484a
Assar Westerlund <assar@sics.se>
2000-06-05 01:09:45 +0000

    bump copyright
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@8313 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/hdb/db3.c
+++ lib/hdb/db3.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 1998, 1999 Kungliga Tekniska Högskolan
+ * Copyright (c) 1997 - 2000 Kungliga Tekniska Högskolan
  * (Royal Institute of Technology, Stockholm, Sweden). 
  * All rights reserved. 
  *
