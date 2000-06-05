e42f2fc397ab195391a6f78452782efb89ebf0cf
Tim Potter <tpot@samba.org>
2000-06-05 12:58:47 +0000

    Updated samba appliance example.

--- examples/appliance/build.sh
+++ examples/appliance/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-tar --exclude=CVS -czf /usr/src/redhat/SOURCES/samba-appliance-0.1-src.tar.gz samba-appliance-0.1
+tar --exclude=CVS -czf /usr/src/redhat/SOURCES/samba-appliance-0.2-src.tar.gz samba-appliance-0.2
 rpm -ba appliance.spec
