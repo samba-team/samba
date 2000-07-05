7f158ffcc5b8b42a06650df4042783d96b0a28a1
Elrond <elrond@samba.org>
2000-07-05 15:02:16 +0000

    Just made the output a little nicer. ;)

--- source/script/mkproto.sh
+++ source/script/mkproto.sh
@@ -4,6 +4,18 @@ LANG=C; export LANG
 LC_ALL=C; export LC_ALL
 LC_COLLATE=C; export LC_COLLATE
 
+if (echo "testing\c"; echo 1,2,3) | grep c >/dev/null; then
+  # Stardent Vistra SVR4 grep lacks -e, says ghazi@caip.rutgers.edu.
+  if (echo -n testing; echo 1,2,3) | sed s/-n/xn/ | grep xn >/dev/null; then
+    ac_n= ac_c='
+' ac_t='	'
+  else
+    ac_n=-n ac_c= ac_t=
+  fi
+else
+  ac_n= ac_c='\c' ac_t=
+fi
+
 if [ $# -lt 3 ]
 then
   echo "Usage: $0 awk [-h headerdefine] outputheader proto_obj"
@@ -27,15 +39,16 @@ headertmp="$header.$$.tmp~"
 
 proto_src="`echo $@ | tr ' ' '\n' | sed -e 's/\.o/\.c/g' | sort | uniq | egrep -v 'ubiqx/|wrapped'`"
 
-echo creating $header
+echo $ac_n "creating ${header}... " $ac_c
 
 ${awk} $headeropt \
   -f script/mkproto.awk $proto_src > $headertmp
 
 if cmp -s $header $headertmp 2>/dev/null
 then
-  echo "$header unchanged"
+  echo "$ac_t""unchanged."
   rm $headertmp
 else
+  echo "$ac_t""done."
   mv $headertmp $header
 fi
