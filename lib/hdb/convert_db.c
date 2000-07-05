904ae57b0438063c77cff1c8943226b489d96195
Johan Danielsson <joda@pdc.kth.se>
2000-07-05 13:10:46 +0000

    cleanup
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@8547 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/hdb/convert_db.c
+++ lib/hdb/convert_db.c
@@ -132,7 +132,6 @@ main(int argc, char **argv)
     krb5_error_code ret;
     krb5_context context;
     HDB *db, *new;
-    EncryptionKey key;
     int optind = 0;
     int master_key_set = 0;
     
@@ -157,23 +156,17 @@ main(int argc, char **argv)
     if(ret != 0)
 	krb5_err(context, 1, ret, "hdb_create");
 
-    ret = hdb_read_master_key(context, mkeyfile, &key);
-    if(ret == 0) {
-	if(key.keytype == KEYTYPE_DES)
-	    key.keytype = ETYPE_DES_CBC_MD5;
-    
-	ret = hdb_set_master_key(context, db, key);
-	if (ret)
-	    krb5_err(context, 1, ret, "hdb_set_master_key");
-	master_key_set = 1;
-    }
+    ret = hdb_set_master_keyfile(context, db, mkeyfile);
+    if (ret)
+	krb5_err(context, 1, ret, "hdb_set_master_keyfile");
+    master_key_set = 1;
     ret = hdb_create(context, &new, new_database);
     if(ret != 0)
 	krb5_err(context, 1, ret, "hdb_create");
     if (master_key_set) {
-	ret = hdb_set_master_key(context, new, key);
+	ret = hdb_set_master_keyfile(context, new, mkeyfile);
 	if (ret)
-	    krb5_err(context, 1, ret, "hdb_set_master_key");
+	    krb5_err(context, 1, ret, "hdb_set_master_keyfile");
     }
     ret = db->open(context, db, O_RDONLY, 0);
     if(ret == HDB_ERR_BADVERSION) {
