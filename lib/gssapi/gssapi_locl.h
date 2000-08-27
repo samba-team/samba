c7001612cd64c187326ff185ba660d9005cddb2e
Assar Westerlund <assar@sics.se>
2000-08-27 04:19:00 +0000

    (gssapi_krb5_encapsulate, gss_krb5_getsomekey): update prototypes add assert.h
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@9002 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/gssapi/gssapi_locl.h
+++ lib/gssapi/gssapi_locl.h
@@ -38,6 +38,7 @@
 
 #include <krb5_locl.h>
 #include <gssapi.h>
+#include <assert.h>
 
 extern krb5_context gssapi_krb5_context;
 
@@ -59,7 +60,7 @@ gssapi_krb5_verify_8003_checksum (
 
 OM_uint32
 gssapi_krb5_encapsulate(
-			krb5_data *in_data,
+			const krb5_data *in_data,
 			gss_buffer_t output_token,
 			u_char *type);
 
@@ -86,7 +87,7 @@ gssapi_krb5_verify_header(u_char **str,
 
 OM_uint32
 gss_krb5_getsomekey(const gss_ctx_id_t context_handle,
-		    des_cblock *key);
+		    krb5_keyblock **key);
 
 krb5_error_code
 gss_address_to_krb5addr(OM_uint32 gss_addr_type,
