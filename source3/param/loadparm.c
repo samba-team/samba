4326894f6a10c55a1b95b94023952c4e30439074
Gerald Carter <jerry@samba.org>
2000-08-29 14:33:39 +0000

    needed to use strwicmp() in smbclient code, so I moved it to util_str.c and made it non-static
    --jerry
    (This used to be commit dfdca21bd90b9c83f195d580ec9d774f1be8f9cb)

--- source3/param/loadparm.c
+++ source3/param/loadparm.c
@@ -1786,37 +1786,6 @@ BOOL lp_add_printer(char *pszPrintername, int iDefaultService)
 	return (True);
 }
 
-
-/***************************************************************************
-Do a case-insensitive, whitespace-ignoring string compare.
-***************************************************************************/
-static int strwicmp(char *psz1, char *psz2)
-{
-	/* if BOTH strings are NULL, return TRUE, if ONE is NULL return */
-	/* appropriate value. */
-	if (psz1 == psz2)
-		return (0);
-	else if (psz1 == NULL)
-		return (-1);
-	else if (psz2 == NULL)
-		return (1);
-
-	/* sync the strings on first non-whitespace */
-	while (1)
-	{
-		while (isspace(*psz1))
-			psz1++;
-		while (isspace(*psz2))
-			psz2++;
-		if (toupper(*psz1) != toupper(*psz2) || *psz1 == '\0'
-		    || *psz2 == '\0')
-			break;
-		psz1++;
-		psz2++;
-	}
-	return (*psz1 - *psz2);
-}
-
 /***************************************************************************
 Map a parameter's string representation to something we can use. 
 Returns False if the parameter string is not recognised, else TRUE.
