2a63cfe6b9601ce773505c7f1216b86113ccfd08
Assar Westerlund <assar@sics.se>
2000-10-04 06:07:52 +0000

    fix superfluous semicolons in some of the __attribute__s
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@9094 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- appl/ftp/ftp/security.h
+++ appl/ftp/ftp/security.h
@@ -94,17 +94,17 @@ void delete_ftp_command(void);
 
 
 int sec_fflush (FILE *);
-int sec_fprintf (FILE *, const char *, ...);
+int sec_fprintf (FILE *, const char *, ...)
     __attribute__ ((format (printf, 2,3)));
 int sec_getc (FILE *);
 int sec_putc (int, FILE *);
 int sec_read (int, void *, int);
 int sec_read_msg (char *, int);
-int sec_vfprintf (FILE *, const char *, va_list);
+int sec_vfprintf (FILE *, const char *, va_list)
     __attribute__ ((format (printf, 2,0)));
 int sec_fprintf2(FILE *f, const char *fmt, ...)
     __attribute__ ((format (printf, 2,3)));
-int sec_vfprintf2(FILE *, const char *, va_list);
+int sec_vfprintf2(FILE *, const char *, va_list)
     __attribute__ ((format (printf, 2,0)));
 int sec_write (int, char *, int);
 
