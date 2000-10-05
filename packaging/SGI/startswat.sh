2fbe2e91486cbd7a9cc7e877d0739ae31c5397a8
Herb Lewis <herb@samba.org>
2000-10-05 14:57:25 +0000

    add some paranoia checks

--- packaging/SGI/startswat.sh
+++ packaging/SGI/startswat.sh
@@ -3,6 +3,10 @@
 # add SWAT deamon to inetd.conf
 #
 cp /etc/inetd.conf /etc/inetd.conf.O
+
+if [ $? ]; then exit 1; fi
+if [ ! -r /etc/inetd.conf.O -o ! -w /etc/inetd.conf ]; then exit 1; fi
+
 sed -e "/^swat/D" -e "/^#SWAT/D" /etc/inetd.conf.O > /etc/inetd.conf
 echo '#SWAT services' >> /etc/inetd.conf
 echo swat stream tcp  nowait  root    /usr/samba/bin/swat swat >> /etc/inetd.conf
@@ -11,6 +15,10 @@ echo swat stream tcp  nowait  root    /usr/samba/bin/swat swat >> /etc/inetd.con
 # add SWAT service port to /etc/services
 #
 cp /etc/services /etc/services.O
+
+if [ $? ]; then exit 1; fi
+if [ ! -r /etc/services.O -o ! -w /etc/services ]; then exit 1; fi
+
 sed -e "/^swat/D" -e "/^#SWAT/D" /etc/services.O > /etc/services
 echo '#SWAT services' >> /etc/services
 echo 'swat              901/tcp                         # SWAT' >> /etc/services
