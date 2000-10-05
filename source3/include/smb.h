bbd7f7bf0fd4f6cda41989c3371d7bf18f49a592
Jeremy Allison <jra@samba.org>
2000-10-05 18:50:18 +0000

    Herb's fixes for profiling & compiler warnings. Jeremy. (This used to be commit 7914e9351abb5271ebb4990c3b1fe495d15a4eda)

--- source3/include/smb.h
+++ source3/include/smb.h
@@ -622,8 +622,7 @@ struct current_user
 
 #define NUM_FLUSH_REASONS 8 /* Keep this in sync with the enum below. */
 enum flush_reason_enum { SEEK_FLUSH, READ_FLUSH, WRITE_FLUSH, READRAW_FLUSH,
-                         OPLOCK_RELEASE_FLUSH, CLOSE_FLUSH, SYNC_FLUSH, SIZECHANGE_FLUSH };     
-
+			OPLOCK_RELEASE_FLUSH, CLOSE_FLUSH, SYNC_FLUSH, SIZECHANGE_FLUSH };
 /* Defines for the sent_oplock_break field above. */
 #define NO_BREAK_SENT 0
 #define EXCLUSIVE_BREAK_SENT 1
