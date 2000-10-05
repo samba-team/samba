d4d55488397832df35b558564c263a307b0bb629
Jeremy Allison <jra@samba.org>
2000-10-05 22:51:57 +0000

    Fix for null passwords being allowed bug. Jeremy.

--- source/smbd/password.c
+++ source/smbd/password.c
@@ -514,10 +514,14 @@ BOOL pass_check_smb(char *user, char *domain,
 		return(False);
 	}
 
-	if (lm_pwd[0] == '\0' && (smb_pass->acct_ctrl & ACB_PWNOTREQ) && lp_null_passwords())
-	{
-		DEBUG(3,("Account for user '%s' has no password and null passwords are allowed.\n", smb_pass->smb_name));
-		return(True);
+	if (smb_pass->acct_ctrl & ACB_PWNOTREQ) {
+		if (lp_null_passwords()) {
+			DEBUG(3,("Account for user '%s' has no password and null passwords are allowed.\n", smb_pass->smb_name));
+			return(True);
+		} else {
+			DEBUG(3,("Account for user '%s' has no password and null passwords are NOT allowed.\n", smb_pass->smb_name));
+			return(False);
+		}		
 	}
 
 	if (smb_password_ok(smb_pass, chal, lm_pwd, nt_pwd))
