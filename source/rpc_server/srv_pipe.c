15d7f16bdc2ff4f2ae82871eb9f318ba45cf4d1c
Tim Potter <tpot@samba.org>
2000-10-11 04:54:37 +0000

    Don't initialise groups twice.

--- source/rpc_server/srv_pipe.c
+++ source/rpc_server/srv_pipe.c
@@ -463,7 +463,6 @@ failed authentication on named pipe %s.\n", domain, pipe_user_name, wks, p->name
 
 	/* Set up pipe user group membership. */
 	initialise_groups(pipe_user_name, p->pipe_user.uid, p->pipe_user.gid);
-	initialize_groups(pipe_user_name, p->pipe_user.uid, p->pipe_user.gid);
 	get_current_groups( &p->pipe_user.ngroups, &p->pipe_user.groups);
 
 	/* Create an NT_USER_TOKEN struct for this user. */
