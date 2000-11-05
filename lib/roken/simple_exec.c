10fed5bc8968e020e71a41954add069671cb40ed
Johan Danielsson <joda@pdc.kth.se>
2000-11-05 16:41:06 +0000

    simple_execl
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@9135 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/roken/simple_exec.c
+++ lib/roken/simple_exec.c
@@ -148,3 +148,20 @@ simple_execle(const char *file, ... /* ,char *const envp[] */)
     free(argv);
     return ret;
 }
+
+int
+simple_execl(const char *file, ...) 
+{
+    va_list ap;
+    char **argv;
+    int ret;
+
+    va_start(ap, file);
+    argv = vstrcollect(&ap);
+    va_end(ap);
+    if(argv == NULL)
+	return -1;
+    ret = simple_execve(file, argv, environ);
+    free(argv);
+    return ret;
+}
