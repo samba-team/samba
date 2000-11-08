ee1a5e70e9675b5c0d73995328d463c0ea08d43f
Johan Danielsson <joda@pdc.kth.se>
2000-11-08 23:30:32 +0000

    (sec_read): fix bug in previous (from Jacques A. Vidrine <n@nectar.com>)
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@9148 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- appl/ftp/ftp/security.c
+++ appl/ftp/ftp/security.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1998 - 2000 Kungliga Tekniska Högskolan
+ * Copyright (c) 1998-2000 Kungliga Tekniska Högskolan
  * (Royal Institute of Technology, Stockholm, Sweden).
  * All rights reserved.
  * 
@@ -237,7 +237,7 @@ sec_read(int fd, void *data, int length)
 	ret = sec_get_data(fd, &in_buffer, data_prot);
 	if (ret < 0)
 	    return -1;
-	if(ret == 0 || in_buffer.size == 0) {
+	if(ret == 0 && in_buffer.size == 0) {
 	    if(rx)
 		in_buffer.eof_flag = 1;
 	    return rx;
