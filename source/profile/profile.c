4bfb8be8f68b79d7f542c7b65a358180418ee05f
Herb Lewis <herb@samba.org>
2000-11-10 14:40:19 +0000

    add INFO: to debug message printed by set debug level message clearer messages for set profiling options messages

--- source/profile/profile.c
+++ source/profile/profile.c
@@ -54,20 +54,23 @@ void profile_message(int msg_type, pid_t src, void *buf, size_t len)
 	case 0:		/* turn off profiling */
 		do_profile_flag = False;
 		do_profile_times = False;
+		DEBUG(1,("INFO: Profiling turned OFF from pid %d\n", (int)src));
 		break;
 	case 1:		/* turn on counter profiling only */
 		do_profile_flag = True;
 		do_profile_times = False;
+		DEBUG(1,("INFO: Profiling counts turned ON from pid %d\n", (int)src));
 		break;
 	case 2:		/* turn on complete profiling */
 		do_profile_flag = True;
 		do_profile_times = True;
+		DEBUG(1,("INFO: Full profiling turned ON from pid %d\n", (int)src));
 		break;
 	case 3:		/* reset profile values */
 		memset((char *)profile_p, 0, sizeof(*profile_p));
+		DEBUG(1,("INFO: Profiling values cleared from pid %d\n", (int)src));
 		break;
 	}
-	DEBUG(1,("Profile level set to %d from pid %d\n", level, (int)src));
 }
 
 /*******************************************************************
