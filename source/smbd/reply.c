5fc9b689142768b10f3abbe556610cad8baceaa4
Jeremy Allison <jra@samba.org>
2000-12-12 00:04:29 +0000

    Extra part of fix that Gerald missed (sorry). Jeremy.

--- source/smbd/reply.c
+++ source/smbd/reply.c
@@ -1045,7 +1045,7 @@ int reply_chkpth(connection_struct *conn, char *inbuf,char *outbuf, int dum_size
   mode = SVAL(inbuf,smb_vwv0);
 
   if (check_name(name,conn)) {
-    if(VALID_STAT(sbuf))
+    if (VALID_STAT(sbuf) || vfs_stat(conn,name,&sbuf) == 0)
       ok = S_ISDIR(sbuf.st_mode);
   }
 
