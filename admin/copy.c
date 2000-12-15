37bcf8ded8d57a0698f1ce0bb2fbcbccc50d066d
Johan Danielsson <joda@pdc.kth.se>
2000-12-15 17:44:49 +0000

    merge srvconvert with copy
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@9404 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- admin/copy.c
+++ admin/copy.c
@@ -35,15 +35,60 @@
 
 RCSID("$Id$");
 
-int
-kt_copy (int argc, char **argv)
+static int
+kt_copy_int (const char *from, const char *to)
 {
     krb5_error_code ret;
-    int help_flag = 0;
-    int optind = 0;
     krb5_keytab src_keytab, dst_keytab;
     krb5_kt_cursor cursor;
     krb5_keytab_entry entry;
+
+    ret = krb5_kt_resolve (context, from, &src_keytab);
+    if (ret) {
+	krb5_warn (context, ret, "resolving src keytab `%s'", from);
+	return 0;
+    }
+
+    ret = krb5_kt_resolve (context, to, &dst_keytab);
+    if (ret) {
+	krb5_kt_close (context, src_keytab);
+	krb5_warn (context, ret, "resolving dst keytab `%s'", to);
+	return 0;
+    }
+
+    ret = krb5_kt_start_seq_get (context, src_keytab, &cursor);
+    if (ret) {
+	krb5_warn (context, ret, "krb5_kt_start_seq_get %s", keytab_string);
+	goto fail;
+    }
+
+    while((ret = krb5_kt_next_entry(context, src_keytab,
+				    &entry, &cursor)) == 0) {
+	char name_str[128];
+	krb5_unparse_name_fixed (context, entry.principal, 
+				 name_str, sizeof(name_str));
+	if (verbose_flag)
+	    printf ("copying %s\n", name_str);
+	ret = krb5_kt_add_entry (context, dst_keytab, &entry);
+	krb5_kt_free_entry (context, &entry);
+	if (ret) {
+	    krb5_warn (context, ret, "krb5_kt_add_entry(%s)", name_str);
+	    break;
+	}
+    }
+    krb5_kt_end_seq_get (context, src_keytab, &cursor);
+
+fail:
+    krb5_kt_close (context, src_keytab);
+    krb5_kt_close (context, dst_keytab);
+    return 0;
+}
+
+int
+kt_copy (int argc, char **argv)
+{
+    int help_flag = 0;
+    int optind = 0;
 
     struct getargs args[] = {
 	{ "help", 'h', arg_flag, NULL}
@@ -53,6 +98,7 @@ kt_copy (int argc, char **argv)
     int i = 0;
 
     args[i++].value = &help_flag;
+    args[i++].value = &verbose_flag;
 
     if(getarg(args, num_args, argc, argv, &optind)) {
 	arg_printusage(args, num_args, "ktutil copy",
@@ -74,46 +120,56 @@ kt_copy (int argc, char **argv)
 	return 0;
     }
 
-    ret = krb5_kt_resolve (context, argv[0], &src_keytab);
-    if (ret) {
-	krb5_warn (context, ret, "resolving src keytab `%s'", argv[0]);
+    return kt_copy_int(argv[0], argv[1]);
+}
+
+/* convert a version 4 srvtab to a version 5 keytab */
+
+#ifndef KEYFILE
+#define KEYFILE "/etc/srvtab"
+#endif
+
+int
+srvconv(int argc, char **argv)
+{
+    int help_flag = 0;
+    char *srvtab = KEYFILE;
+    int optind = 0;
+    char kt4[1024], kt5[1024];
+
+    struct getargs args[] = {
+	{ "srvtab", 's', arg_string, NULL},
+	{ "help", 'h', arg_flag, NULL}
+    };
+
+    int num_args = sizeof(args) / sizeof(args[0]);
+    int i = 0;
+
+    args[i++].value = &srvtab;
+    args[i++].value = &help_flag;
+
+    if(getarg(args, num_args, argc, argv, &optind)){
+	arg_printusage(args, num_args, "ktutil srvconvert", "");
+	return 1;
+    }
+    if(help_flag){
+	arg_printusage(args, num_args, "ktutil srvconvert", "");
 	return 0;
     }
 
-    ret = krb5_kt_resolve (context, argv[1], &dst_keytab);
-    if (ret) {
-	krb5_kt_close (context, src_keytab);
-	krb5_warn (context, ret, "resolving dst keytab `%s'", argv[1]);
-	return 0;
+    argc -= optind;
+    argv += optind;
+
+    if (argc != 0) {
+	arg_printusage(args, num_args, "ktutil srvconvert", "");
+	return 1;
     }
 
-    ret = krb5_kt_start_seq_get (context, src_keytab, &cursor);
-    if (ret) {
-	krb5_warn (context, ret, "krb5_kt_start_seq_get %s", keytab_string);
-	goto fail;
-    }
+    snprintf(kt4, sizeof(kt4), "krb4:%s", srvtab);
 
-    while((ret = krb5_kt_next_entry(context, src_keytab,
-				    &entry, &cursor)) == 0) {
-	ret = krb5_kt_add_entry (context, dst_keytab, &entry);
-	if (verbose_flag) {
-	    char *name_str;
+    if(keytab_string != NULL)
+	return kt_copy_int(kt4, keytab_string);
 
-	    krb5_unparse_name (context, entry.principal, &name_str);
-	    printf ("copying %s\n", name_str);
-	    free (name_str);
-	}
-
-	krb5_kt_free_entry (context, &entry);
-	if (ret) {
-	    krb5_warn (context, ret, "krb5_kt_add_entry");
-	    break;
-	}
-    }
-    krb5_kt_end_seq_get (context, src_keytab, &cursor);
-
-fail:
-    krb5_kt_close (context, src_keytab);
-    krb5_kt_close (context, dst_keytab);
-    return 0;
+    krb5_kt_default_name(context, kt5, sizeof(kt5));
+    return kt_copy_int(kt4, kt5);
 }
