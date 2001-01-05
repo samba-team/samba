79b73e62ee23bc28a169ec4cee1740ba688e972d
David O'Neill <dmo@samba.org>
2001-01-05 17:59:31 +0000

    Sync up packaging updates that didn't make it to HEAD. (This used to be commit c6f7358535eb96112e055d60d39c1595601d3056)

--- packaging/SGI/startswat.sh
+++ packaging/SGI/startswat.sh
@@ -2,8 +2,12 @@
 #
 # add SWAT deamon to inetd.conf
 #
-cp /etc/inetd.conf /etc/inetd.O
-sed -e "/^swat/D" -e "/^#SWAT/D" /etc/inetd.O > /etc/inetd.conf
+cp /etc/inetd.conf /etc/inetd.conf.O
+
+if [ $? -ne 0 ]; then exit 1; fi
+if [ ! -r /etc/inetd.conf.O -o ! -w /etc/inetd.conf ]; then exit 1; fi
+
+sed -e "/^swat/D" -e "/^#SWAT/D" /etc/inetd.conf.O > /etc/inetd.conf
 echo '#SWAT services' >> /etc/inetd.conf
 echo swat stream tcp  nowait  root    /usr/samba/bin/swat swat >> /etc/inetd.conf
 
@@ -11,6 +15,10 @@ echo swat stream tcp  nowait  root    /usr/samba/bin/swat swat >> /etc/inetd.con
 # add SWAT service port to /etc/services
 #
 cp /etc/services /etc/services.O
+
+if [ $? -ne 0 ]; then exit 1; fi
+if [ ! -r /etc/services.O -o ! -w /etc/services ]; then exit 1; fi
+
 sed -e "/^swat/D" -e "/^#SWAT/D" /etc/services.O > /etc/services
 echo '#SWAT services' >> /etc/services
 echo 'swat              901/tcp                         # SWAT' >> /etc/services
