51d37dc8a19817e05549eaf27330b7e29b3208e3
John Reilly <jreilly@samba.org>
2001-01-17 18:39:08 +0000

    Thanks to Jeremy for solving this one. - Change fill_printq_info() to fix corrupted 9X/ME printer comment

--- source/smbd/lanman.c
+++ source/smbd/lanman.c
@@ -737,11 +737,10 @@ static void fill_printq_info(connection_struct *conn, int snum, int uLevel,
 		PACKS(desc,"z","");		/* pszSepFile */
 		PACKS(desc,"z","WinPrint");	/* pszPrProc */
 		PACKS(desc,"z","");		/* pszParms */
-		if (!status || !status->message[0]) {
-			PACKS(desc,"z",Expand(conn,snum,lp_comment(snum))); /* pszComment */
+		PACKS(desc,"z",NULL);		/* pszComment - don't ask.... JRA */
+		if (!status) {
 			PACKI(desc,"W",LPSTAT_OK); /* fsStatus */
 		} else {
-			PACKS(desc,"z",status->message); /* pszComment */
 			PACKI(desc,"W",printq_status(status->status)); /* fsStatus */
 		}
 		PACKI(desc,(uLevel == 3 ? "W" : "N"),count);	/* cJobs */
@@ -772,7 +771,7 @@ static int get_printerdrivernumber(int snum)
 	char **lines = NULL;
 	pstring gen_line;
 	pstring drivername;
-	
+
 	/*
 	 * Check in the tdb *first* before checking the legacy
 	 * files. This allows an NT upload to take precedence over
