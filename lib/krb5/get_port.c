918f97ddd51438a26949b0ac151ed4a9ec0a46f4
Johan Danielsson <joda@pdc.kth.se>
2001-01-27 19:24:34 +0000

    don't warn if the port name can't be found, nobody cares anyway
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@9521 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/krb5/get_port.c
+++ lib/krb5/get_port.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997 Kungliga Tekniska Högskolan
+ * Copyright (c) 1997-2001 Kungliga Tekniska Högskolan
  * (Royal Institute of Technology, Stockholm, Sweden). 
  * All rights reserved. 
  *
@@ -44,8 +44,10 @@ krb5_getportbyname (krb5_context context,
     struct servent *sp;
 
     if ((sp = roken_getservbyname (service, proto)) == NULL) {
+#if 0
 	krb5_warnx(context, "%s/%s unknown service, using default port %d", 
 		   service, proto, default_port);
+#endif
 	return htons(default_port);
     } else
 	return sp->s_port;
