c711de132dcd8c19e5622286d9ce063596e14a5b
Jeremy Allison <jra@samba.org>
2001-02-24 00:39:23 +0000

    This patch (donated by Marc Jacobsen of HP) should make Sun and HP very happy :-). Probably Veritas too... :-). It allows Samba as a domain member to authenticate against an AS/U server such as the older HP PDC product or PD Netlink. It does this by trying a netlogon with info level 3 and then falling back to info level 2 if the PDC returns invalid info level. Jeremy.

--- source/include/rpc_netlogon.h
+++ source/include/rpc_netlogon.h
@@ -39,32 +39,63 @@
 #define SEC_CHAN_DOMAIN  4
 
 #if 0
-/* JRATEST.... */
+/* I think this is correct - it's what gets parsed on the wire. JRA. */
 /* NET_USER_INFO_2 */
 typedef struct net_user_info_2
 {
-        uint32 ptr_user_info;
+	uint32 ptr_user_info;
 
-        NTTIME logon_time;            /* logon time */
-        NTTIME logoff_time;           /* logoff time */
-        NTTIME kickoff_time;          /* kickoff time */
-        NTTIME pass_last_set_time;    /* password last set time */
-        NTTIME pass_can_change_time;  /* password can change time */
-        NTTIME pass_must_change_time; /* password must change time */
+	NTTIME logon_time;            /* logon time */
+	NTTIME logoff_time;           /* logoff time */
+	NTTIME kickoff_time;          /* kickoff time */
+	NTTIME pass_last_set_time;    /* password last set time */
+	NTTIME pass_can_change_time;  /* password can change time */
+	NTTIME pass_must_change_time; /* password must change time */
 
-....
+	UNIHDR hdr_user_name;    /* username unicode string header */
+	UNIHDR hdr_full_name;    /* user's full name unicode string header */
+	UNIHDR hdr_logon_script; /* logon script unicode string header */
+	UNIHDR hdr_profile_path; /* profile path unicode string header */
+	UNIHDR hdr_home_dir;     /* home directory unicode string header */
+	UNIHDR hdr_dir_drive;    /* home directory drive unicode string header */
+
+	uint16 logon_count;  /* logon count */
+	uint16 bad_pw_count; /* bad password count */
+
 	uint32 user_id;       /* User ID */
 	uint32 group_id;      /* Group ID */
-....
+	uint32 num_groups;    /* num groups */
+	uint32 buffer_groups; /* undocumented buffer pointer to groups. */
+	uint32 user_flgs;     /* user flags */
+
+	uint8 user_sess_key[16]; /* unused user session key */
+
+	UNIHDR hdr_logon_srv; /* logon server unicode string header */
+	UNIHDR hdr_logon_dom; /* logon domain unicode string header */
+
+	uint32 buffer_dom_id; /* undocumented logon domain id pointer */
+	uint8 padding[40];    /* unused padding bytes.  expansion room */
+
+	UNISTR2 uni_user_name;    /* username unicode string */
+	UNISTR2 uni_full_name;    /* user's full name unicode string */
+	UNISTR2 uni_logon_script; /* logon script unicode string */
+	UNISTR2 uni_profile_path; /* profile path unicode string */
+	UNISTR2 uni_home_dir;     /* home directory unicode string */
+	UNISTR2 uni_dir_drive;    /* home directory drive unicode string */
+
 	uint32 num_groups2;        /* num groups */
 	DOM_GID *gids; /* group info */
 
-	UNIHDR hdr_logon_srv; /* logon server unicode string header */
+	UNISTR2 uni_logon_srv; /* logon server unicode string */
 	UNISTR2 uni_logon_dom; /* logon domain unicode string */
-	DOM_SID2 dom_sid;
-	
+
+	DOM_SID2 dom_sid;           /* domain SID */
+
+	uint32 num_other_groups;        /* other groups */
+	DOM_GID *other_gids; /* group info */
+	DOM_SID2 *other_sids; /* undocumented - domain SIDs */
+
 } NET_USER_INFO_2;
-/* ! JRATEST.... */
 #endif
 
 /* NET_USER_INFO_3 */
