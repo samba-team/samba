ef9185aa4742708b3d13f604c214ca82252ce9eb
Johan Danielsson <joda@pdc.kth.se>
2001-02-28 20:03:47 +0000

    (krb5_init_ets): use krb5_add_et_list
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@9726 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/krb5/context.c
+++ lib/krb5/context.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997 - 2000 Kungliga Tekniska Högskolan
+ * Copyright (c) 1997 - 2001 Kungliga Tekniska Högskolan
  * (Royal Institute of Technology, Stockholm, Sweden). 
  * All rights reserved. 
  *
@@ -287,9 +287,9 @@ void
 krb5_init_ets(krb5_context context)
 {
     if(context->et_list == NULL){
-	initialize_krb5_error_table_r(&context->et_list);
-	initialize_asn1_error_table_r(&context->et_list);
-	initialize_heim_error_table_r(&context->et_list);
+	krb5_add_et_list(context, initialize_krb5_error_table_r);
+	krb5_add_et_list(context, initialize_asn1_error_table_r);
+	krb5_add_et_list(context, initialize_heim_error_table_r);
     }
 }
 
