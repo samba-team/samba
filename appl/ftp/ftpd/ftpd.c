b1cd585ea9ea671638a2d1e2b094edfa5d21a477
Assar Westerlund <assar@sics.se>
2001-03-25 23:55:08 +0000

    (send_file_list): use GLOB_LIMIT if defined
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@9755 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- appl/ftp/ftpd/ftpd.c
+++ appl/ftp/ftpd/ftpd.c
@@ -2161,7 +2161,11 @@ send_file_list(char *whichf)
   char buf[MaxPathLen];
 
   if (strpbrk(whichf, "~{[*?") != NULL) {
-    int flags = GLOB_BRACE|GLOB_NOCHECK|GLOB_QUOTE|GLOB_TILDE;
+    int flags = GLOB_BRACE|GLOB_NOCHECK|GLOB_QUOTE|GLOB_TILDE
+#ifdef GLOB_LIMIT
+	| GLOB_LIMIT
+#endif
+	;
 
     memset(&gl, 0, sizeof(gl));
     freeglob = 1;
