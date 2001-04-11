0ca9f5c023df2ee498dcd1bdb2f29abc632a5d60
Jeremy Allison <jra@samba.org>
2001-04-11 21:19:25 +0000

    Fix for core dump in security = share code with new share security db. Jeremy. (This used to be commit 20b13bafdff2fd7be9219ed164e7fe91b597298d)

--- source3/include/proto.h
+++ source3/include/proto.h
@@ -1879,6 +1879,12 @@ BOOL pm_process( char *FileName,
 
 BOOL pdb_generate_sam_sid(void);
 
+/*The following definitions come from  passdb/pampass.c  */
+
+BOOL PAM_session(BOOL instance, const connection_struct *conn, char *tty);
+BOOL pam_passcheck(char * user, char * password);
+BOOL pam_passcheck( char * user, char * password );
+
 /*The following definitions come from  passdb/pass_check.c  */
 
 void dfs_unlogin(void);
@@ -3768,7 +3774,7 @@ BOOL api_srvsvc_rpc(pipes_struct *p);
 
 BOOL share_info_db_init(void);
 void map_generic_share_sd_bits(SEC_DESC *psd);
-BOOL share_access_check(int snum, uint16 vuid, uint32 desired_access);
+BOOL share_access_check(connection_struct *conn, int snum, uint16 vuid, uint32 desired_access);
 uint32 _srv_net_srv_get_info(pipes_struct *p, SRV_Q_NET_SRV_GET_INFO *q_u, SRV_R_NET_SRV_GET_INFO *r_u);
 uint32 _srv_net_file_enum(pipes_struct *p, SRV_Q_NET_FILE_ENUM *q_u, SRV_R_NET_FILE_ENUM *r_u);
 uint32 _srv_net_conn_enum(pipes_struct *p, SRV_Q_NET_CONN_ENUM *q_u, SRV_R_NET_CONN_ENUM *r_u);
