25dd457912d350f6aa312b1b55005b316ac07765
Andrew Tridgell <tridge@samba.org>
2001-04-13 14:37:00 +0000

    added S_OFF_T_R() for pure 32 bit systems

--- source/libsmb/clifile.c
+++ source/libsmb/clifile.c
@@ -543,10 +543,8 @@ BOOL cli_lock64(struct cli_state *cli, int fnum,
 
 	p = smb_buf(cli->outbuf);
 	SIVAL(p, 0, cli->pid);
-	SIVAL(p, 4, (offset>>32));
-	SIVAL(p, 8, (offset&0xffffffff));
-	SIVAL(p, 12, (len>>32));
-	SIVAL(p, 16, (len&0xffffffff));
+	S_OFF_T_R(p, 0, offset);
+	S_OFF_T_R(p, 12, len);
 	cli_send_smb(cli);
 
         cli->timeout = (timeout == -1) ? 0x7FFFFFFF : (timeout + 2*1000);
@@ -590,10 +588,8 @@ BOOL cli_unlock64(struct cli_state *cli, int fnum, SMB_BIG_UINT offset, SMB_BIG_
 
 	p = smb_buf(cli->outbuf);
 	SIVAL(p, 0, cli->pid);
-	SIVAL(p, 4, (offset>>32));
-	SIVAL(p, 8, (offset&0xffffffff));
-	SIVAL(p, 12, (len>>32));
-	SIVAL(p, 16, (len&0xffffffff));
+	S_OFF_T_R(p, 4, offset);
+	S_OFF_T_R(p, 12, len);
 
 	cli_send_smb(cli);
 	if (!cli_receive_smb(cli)) {
