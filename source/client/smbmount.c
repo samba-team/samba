8051406588987005f621cb095067d3628638d250
John Terpstra <jht@samba.org>
2001-04-14 18:39:32 +0000

    Patch from Mandrakesoft to ensure we close all files.

--- source/client/smbmount.c
+++ source/client/smbmount.c
@@ -95,7 +95,9 @@ static void daemonize(void)
 static void close_our_files(int client_fd)
 {
 	int i;
-	for (i = 0; i < 256; i++) {
+
+	getrlimit(RLIMIT_NOFILE,&limits);
+	for (1 = 0; i< limits.rlim_max, i++) {
 		if (i == client_fd) continue;
 		close(i);
 	}
