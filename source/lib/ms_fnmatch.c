e222e359460c2fe1b704cba66540965689b16c66
Andrew Tridgell <tridge@samba.org>
2001-04-18 02:35:53 +0000

    removed some unused test code

--- source/lib/ms_fnmatch.c
+++ source/lib/ms_fnmatch.c
@@ -24,14 +24,7 @@
 */  
 
 
-#if FNMATCH_TEST
-#include <stdio.h>
-#include <stdlib.h>
-#else
 #include "includes.h"
-#endif
-
-
 
 /* 
    bugger. we need a separate wildcard routine for older versions
@@ -201,59 +194,3 @@ int ms_fnmatch(char *pattern, char *string)
 	
 	return -1;
 }
-
-
-#if FNMATCH_TEST
-
-static int match_one(char *pattern, char *file)
-{
-	if (strcmp(file,"..") == 0) file = ".";
-	if (strcmp(pattern,".") == 0) return -1;
-
-	return ms_fnmatch(pattern, file);
-}
-
-static char *match_test(char *pattern, char *file, char *short_name)
-{
-	static char ret[4];
-	strncpy(ret, "---", 3);
-
-	if (match_one(pattern, ".") == 0) ret[0] = '+';
-	if (match_one(pattern, "..") == 0) ret[1] = '+';
-	if (match_one(pattern, file) == 0 || 
-	    (*short_name && match_one(pattern, short_name)==0)) ret[2] = '+';
-	return ret;
-}
-
- int main(int argc, char *argv[])
-{
-	int ret;
-	char ans[4], mask[100], file[100], mfile[100];
-	char *ans2;
-	int n, i=0;
-	char line[200];
-
-	if (argc == 3) {
-		ret = ms_fnmatch(argv[1], argv[2]);
-		if (ret == 0) 
-			printf("YES\n");
-		else printf("NO\n");
-		return ret;
-	}
-	mfile[0] = 0;
-
-	while (fgets(line, sizeof(line)-1, stdin)) {
-		n = sscanf(line, "%3s %s %s %s\n", ans, mask, file, mfile);
-		if (n < 3) continue;
-		ans2 = match_test(mask, file, mfile);
-		if (strcmp(ans2, ans)) {
-			printf("%s %s %d mask=[%s]  file=[%s]  mfile=[%s]\n",
-			       ans, ans2, i, mask, file, mfile);
-		}
-		i++;
-		mfile[0] = 0;
-	}
-	return 0;
-}
-#endif /* FNMATCH_TEST */
-
