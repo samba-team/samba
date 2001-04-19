88b39cfb989086417ac472e4466613f43197b869
Johan Danielsson <joda@pdc.kth.se>
2001-04-19 07:26:52 +0000

    fix argument count check after getarg change; spotted by mark@MCS.VUW.AC.NZ
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@9790 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- kadmin/del_enctype.c
+++ kadmin/del_enctype.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1999-2000 Kungliga Tekniska Högskolan
+ * Copyright (c) 1999-2001 Kungliga Tekniska Högskolan
  * (Royal Institute of Technology, Stockholm, Sweden). 
  * All rights reserved. 
  *
@@ -73,7 +73,7 @@ del_enctype(int argc, char **argv)
 	usage ();
 	return 0;
     }
-    if(argc - optind < 3 || help_flag) {
+    if(argc - optind < 2 || help_flag) {
 	usage ();
 	return 0;
     }
