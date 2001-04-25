ce4d3cd4fa5b98187f63c2b48e8d31be16090df5
Jeremy Allison <jra@samba.org>
2001-04-25 19:03:57 +0000

    Re-ran autoheader; autoconf to fix pam_smb build. Jeremy. (This used to be commit c6f25e84d3e15319d263386c101727d787cf0679)

--- source3/acconfig.h
+++ source3/acconfig.h
@@ -61,6 +61,7 @@
 #undef WITH_NISPLUS
 #undef WITH_TDBPWD
 #undef WITH_PAM
+#undef WITH_PAM_SMBPASS
 #undef WITH_NISPLUS_HOME
 #undef WITH_AUTOMOUNT
 #undef WITH_SMBMOUNT
@@ -146,3 +147,4 @@
 #undef HAVE_XFS_ACLS
 #undef HAVE_AIX_ACLS
 #undef HAVE_NO_ACLS
+#undef HAVE_LIBPAM
