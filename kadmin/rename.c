dab1829c35639abc7725568643a44708d78f0527
Johan Danielsson <joda@pdc.kth.se>
2001-05-04 13:07:03 +0000

    fix argument number test
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@9835 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- kadmin/rename.c
+++ kadmin/rename.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997-2000 Kungliga Tekniska Högskolan
+ * Copyright (c) 1997-2001 Kungliga Tekniska Högskolan
  * (Royal Institute of Technology, Stockholm, Sweden). 
  * All rights reserved. 
  *
@@ -62,7 +62,7 @@ rename_entry(int argc, char **argv)
 	usage ();
 	return 0;
     }
-    if(argc - optind < 3 || help_flag) {
+    if(argc - optind != 2 || help_flag) {
 	usage ();
 	return 0;
     }
