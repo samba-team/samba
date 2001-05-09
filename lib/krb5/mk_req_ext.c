27753b359bed414c7488da342530bf3abed8f4e1
Assar Westerlund <assar@sics.se>
2001-05-09 07:15:00 +0000

    (krb5_mk_req_internal): new krb5_create_checksum
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@9871 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/krb5/mk_req_ext.c
+++ lib/krb5/mk_req_ext.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997 - 2000 Kungliga Tekniska Högskolan
+ * Copyright (c) 1997 - 2001 Kungliga Tekniska Högskolan
  * (Royal Institute of Technology, Stockholm, Sweden). 
  * All rights reserved. 
  *
@@ -99,6 +99,7 @@ krb5_mk_req_internal(krb5_context context,
 	  /* this is to make DCE secd (and older MIT kdcs?) happy */
 	  ret = krb5_create_checksum(context, 
 				     NULL,
+				     0,
 				     CKSUMTYPE_RSA_MD4,
 				     in_data->data,
 				     in_data->length,
@@ -112,6 +113,7 @@ krb5_mk_req_internal(krb5_context context,
 	  ret = krb5_create_checksum(context, 
 				     crypto,
 				     checksum_usage,
+				     0,
 				     in_data->data,
 				     in_data->length,
 				     &c);
