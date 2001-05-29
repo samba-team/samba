91ab57c202db78b9fd64032bb2a798db178ab3aa
Assar Westerlund <assar@sics.se>
2001-05-29 13:12:21 +0000

    (main): initialize numerr
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@10015 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/roken/base64-test.c
+++ lib/roken/base64-test.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1999 - 2000 Kungliga Tekniska Högskolan
+ * Copyright (c) 1999 - 2001 Kungliga Tekniska Högskolan
  * (Royal Institute of Technology, Stockholm, Sweden).
  * All rights reserved.
  * 
@@ -42,7 +42,7 @@ RCSID("$Id$");
 int
 main(int argc, char **argv)
 {
-    int numerr;
+    int numerr = 0;
     int numtest = 1;
     struct test {
 	void *data;
