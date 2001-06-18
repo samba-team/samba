ad4db1f4e6f1ebb669bd35397d42d60b7d148b2c
Johan Danielsson <joda@pdc.kth.se>
2001-06-18 01:24:29 +0000

    (do_list): make sure list of keys is NULL terminated; similar to patch sent by Chris Chiappa
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@10086 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- admin/list.c
+++ admin/list.c
@@ -122,11 +122,11 @@ do_list(const char *keytab_string)
 			 ((unsigned char*)entry.keyblock.keyvalue.data)[i]);
 	    CHECK_MAX(key);
 	}
-	kp->next = NULL;
 	*kie = kp;
 	kie = &kp->next;
 	krb5_kt_free_entry(context, &entry);
     }
+    *kie = NULL; /* termiate list */
     ret = krb5_kt_end_seq_get(context, keytab, &cursor);
 
     printf("%-*s  %-*s  %-*s", max_version, "Vno", 
