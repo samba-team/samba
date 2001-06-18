79230887c1bc33c7971520c38bc5ad6961de01ba
Assar Westerlund <assar@sics.se>
2001-06-18 02:50:46 +0000

    (gss_krb5_get_remotekey, gss_krb5_get_localkey): add prototypes
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@10098 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/gssapi/krb5/gssapi_locl.h
+++ lib/gssapi/krb5/gssapi_locl.h
@@ -90,8 +90,12 @@ gssapi_krb5_verify_header(u_char **str,
 			  char *type);
 
 OM_uint32
-gss_krb5_getsomekey(const gss_ctx_id_t context_handle,
-		    krb5_keyblock **key);
+gss_krb5_get_remotekey(const gss_ctx_id_t context_handle,
+		       krb5_keyblock **key);
+
+OM_uint32
+gss_krb5_get_localkey(const gss_ctx_id_t context_handle,
+		      krb5_keyblock **key);
 
 krb5_error_code
 gss_address_to_krb5addr(OM_uint32 gss_addr_type,
