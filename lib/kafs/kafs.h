2a1e1a05d82221f91e7408d5da26aaa0bf2cd769
Assar Westerlund <assar@sics.se>
2001-07-15 07:24:33 +0000

    add missing slash (noted by felipe@nada.kth.se)
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@10343 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/kafs/kafs.h
+++ lib/kafs/kafs.h
@@ -186,7 +186,7 @@ krb5_error_code krb5_realm_of_cell __P((const char *cell, char **realm));
 #define _PATH_ARLA_CELLSERVDB 	_PATH_ARLA_VICE "CellServDB"
 #define _PATH_ARLA_THESECELLS	_PATH_ARLA_VICE "TheseCells"
 
-#define _PATH_OPENAFS_DEBIAN_VICE		"/etc/openafs"
+#define _PATH_OPENAFS_DEBIAN_VICE		"/etc/openafs/"
 #define _PATH_OPENAFS_DEBIAN_THISCELL		_PATH_OPENAFS_DEBIAN_VICE "ThisCell"
 #define _PATH_OPENAFS_DEBIAN_CELLSERVDB 	_PATH_OPENAFS_DEBIAN_VICE "CellServDB"
 #define _PATH_OPENAFS_DEBIAN_THESECELLS		_PATH_OPENAFS_DEBIAN_VICE "TheseCells"
