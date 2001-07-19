a5214a7bbc434ac8b93745ba1c4ca4251f20a241
Assar Westerlund <assar@sics.se>
2001-07-19 16:00:42 +0000

    (output_data): make sure of not forwarding `nfrontp' too far, thereby allowing writes after the end of `netobuf'
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@10354 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- appl/telnet/telnetd/global.c
+++ appl/telnet/telnetd/global.c
@@ -93,7 +93,7 @@ int
 output_data (const char *format, ...)
 {
   va_list args;
-  size_t remaining, ret;
+  int remaining, ret;
 
   va_start(args, format);
   remaining = BUFSIZ - (nfrontp - netobuf);
@@ -101,7 +101,7 @@ output_data (const char *format, ...)
 		   remaining,
 		   format,
 		   args);
-  nfrontp += ret;
+  nfrontp += min(ret, remaining-1);
   va_end(args);
   return ret;
 }
