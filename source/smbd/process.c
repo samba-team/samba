23a2ffc40e588e881f38176c19cb97154f9ed9d1
Herb Lewis <herb@samba.org>
2001-08-22 04:53:03 +0000

    force checking of log size after 200 smbd in a row or after 30 sec with no smbds. This should fix one case of logs not getting rotated properly.
    We may need to test this to make sure there is no big performance hit from
    the extra stat call.

--- source/smbd/process.c
+++ source/smbd/process.c
@@ -1169,9 +1169,10 @@ machine %s in domain %s.\n", global_myname, global_myworkgroup ));
 
   /*
    * Now we are root, check if the log files need pruning.
+   * Force a log file check.
    */
-  if(need_to_check_log_size())
-      check_log_size();
+  force_check_log_size();
+  check_log_size();
 
   /*
    * Modify the select timeout depending upon
