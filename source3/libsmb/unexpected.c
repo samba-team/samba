1772584c35189cba517c26bbde3205447f875952
Andrew Tridgell <tridge@samba.org>
2001-09-06 05:45:07 +0000

    actually obey the "use mmap" smb.conf option (This used to be commit b36c98036bcbaa5545c9637cb632361122033cfd)

--- source3/libsmb/unexpected.c
+++ source3/libsmb/unexpected.c
@@ -151,7 +151,7 @@ struct packet_struct *receive_unexpected(enum packet_type packet_type, int id,
 {
 	TDB_CONTEXT *tdb2;
 
-	tdb2 = tdb_open(lock_path("unexpected.tdb"), 0, 0, O_RDONLY, 0);
+	tdb2 = tdb_open_log(lock_path("unexpected.tdb"), 0, 0, O_RDONLY, 0);
 	if (!tdb2) return NULL;
 
 	matched_packet = NULL;
