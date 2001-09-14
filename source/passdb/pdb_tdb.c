b619458dde158c37a4420a28a7e1e1f70e7a18d1
Gerald Carter <jerry@samba.org>
2001-09-14 15:33:09 +0000

    merges from 2.2

--- source/passdb/pdb_tdb.c
+++ source/passdb/pdb_tdb.c
@@ -21,7 +21,7 @@
 
 #include "includes.h"
 
-#ifdef WITH_TDBPWD
+#ifdef WITH_TDBSAM
 
 #define PDB_VERSION		"20010830"
 #define PASSDB_FILE_NAME	"/passdb.tdb"
@@ -817,4 +817,4 @@ BOOL pdb_add_sam_account (SAM_ACCOUNT *newpwd)
 #else
 	/* Do *NOT* make this function static. It breaks the compile on gcc. JRA */
 	void samtdb_dummy_function(void) { } /* stop some compilers complaining */
-#endif /* WITH_TDBPWD */
+#endif /* WITH_TDBSAM */
