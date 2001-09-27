6dbfe6a550719c7123a6ccbd00b68cd665f6e400
Jeremy Allison <jra@samba.org>
2001-09-27 21:56:50 +0000

    Merged some of AB's HEAD changes - fix memory leaks. Jeremy.

--- source/rpc_server/srv_samr_nt.c
+++ source/rpc_server/srv_samr_nt.c
@@ -85,38 +85,39 @@ static uint32 get_sampwd_entries(SAM_USER_INFO_21 *pw_buf, int start_idx,
 	while (((not_finished = pdb_getsampwent(pwd)) != False) 
 		&& (*num_entries) < max_num_entries) 
 	{
-        	int user_name_len;
+		int user_name_len;
 
 		if (start_idx > 0) {
-		/* skip the requested number of entries.
-		   not very efficient, but hey... */
-		start_idx--;
-		continue;
+			/* skip the requested number of entries.
+			   not very efficient, but hey... */
+			pdb_reset_sam(pwd);
+			start_idx--;
+			continue;
         }
 
-	user_name_len = strlen(pdb_get_username(pwd))+1;
-	init_unistr2(&pw_buf[(*num_entries)].uni_user_name, pdb_get_username(pwd), user_name_len);
-	init_uni_hdr(&pw_buf[(*num_entries)].hdr_user_name, user_name_len);
-	pw_buf[(*num_entries)].user_rid = pwd->user_rid;
-	memset((char *)pw_buf[(*num_entries)].nt_pwd, '\0', 16);
+		user_name_len = strlen(pdb_get_username(pwd))+1;
+		init_unistr2(&pw_buf[(*num_entries)].uni_user_name, pdb_get_username(pwd), user_name_len);
+		init_uni_hdr(&pw_buf[(*num_entries)].hdr_user_name, user_name_len);
+		pw_buf[(*num_entries)].user_rid = pwd->user_rid;
+		memset((char *)pw_buf[(*num_entries)].nt_pwd, '\0', 16);
 
-        /* Now check if the NT compatible password is available. */
-        if (pdb_get_nt_passwd(pwd))
-		memcpy( pw_buf[(*num_entries)].nt_pwd , pdb_get_nt_passwd(pwd), 16);
+		/* Now check if the NT compatible password is available. */
+		if (pdb_get_nt_passwd(pwd))
+			memcpy( pw_buf[(*num_entries)].nt_pwd , pdb_get_nt_passwd(pwd), 16);
 
-        pw_buf[(*num_entries)].acb_info = pdb_get_acct_ctrl(pwd);
+		pw_buf[(*num_entries)].acb_info = pdb_get_acct_ctrl(pwd);
 
-        DEBUG(5, ("entry idx: %d user %s, rid 0x%x, acb %x",
-                  (*num_entries), pdb_get_username(pwd), pdb_get_user_rid(pwd), pdb_get_acct_ctrl(pwd) ));
+		DEBUG(5, ("entry idx: %d user %s, rid 0x%x, acb %x",
+			(*num_entries), pdb_get_username(pwd), pdb_get_user_rid(pwd), pdb_get_acct_ctrl(pwd) ));
 
-        if (acb_mask == 0 || (pwd->acct_ctrl & acb_mask)) {
-		DEBUG(5,(" acb_mask %x accepts\n", acb_mask));
-		(*num_entries)++;
-        }
-        else
-		DEBUG(5,(" acb_mask %x rejects\n", acb_mask));
+		if (acb_mask == 0 || (pwd->acct_ctrl & acb_mask)) {
+			DEBUG(5,(" acb_mask %x accepts\n", acb_mask));
+			(*num_entries)++;
+		} else
+			DEBUG(5,(" acb_mask %x rejects\n", acb_mask));
 
 		(*total_entries)++;
+		pdb_reset_sam(pwd);
 	}
 
 	pdb_endsampwent();
@@ -829,7 +830,7 @@ static uint32 get_group_alias_entries(DOMAIN_GRP *d_grp, DOM_SID *sid, uint32 st
 		 * libc grent structures overwrites by called functions */
 		grp = glist = getgrent_list();
 		if (grp == NULL)
-			return False;
+			return NT_STATUS_NO_MEMORY;
 
 		for (;(num_entries < max_entries) && (grp != NULL); grp = grp->next) {
 			int i;
