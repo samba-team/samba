33fb598731f95ff380149402f9942544bf4e1a0e
Tim Potter <tpot@samba.org>
2001-10-02 03:43:26 +0000

    Added copyright and include guards. (This used to be commit 9efa93556e3ed444b075a06d2706111b89a0e099)

--- source3/include/secrets.h
+++ source3/include/secrets.h
@@ -1,5 +1,26 @@
+/*
+ * Unix SMB/Netbios implementation. 
+ * Version 3.0
+ * secrets.tdb file format info
+ * Copyright (C) Andrew Tridgell              2000
+ * 
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 675
+ * Mass Ave, Cambridge, MA 02139, USA.  
+ */
 
-
+#ifndef _SECRETS_H
+#define _SECRETS_H
 
 #define SECRETS_MACHINE_ACCT_PASS "SECRETS/$MACHINE.ACC"
 #define SECRETS_DOMAIN_SID    "SECRETS/SID"
@@ -10,3 +31,4 @@ struct machine_acct_pass {
 	time_t mod_time;
 };
 
+#endif /* _SECRETS_H */
