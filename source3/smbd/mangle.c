d02b5fefba169ad6cdcbbaf01769113481692f38
Simo Sorce <idra@samba.org>
2001-10-08 11:58:25 +0000

    tim suggested to add a copyright note :) (This used to be commit c0bb484e0fcdfece62d361aaaebd90cf15548992)

--- source3/smbd/mangle.c
+++ source3/smbd/mangle.c
@@ -966,7 +966,27 @@ BOOL name_map_mangle(char *OutName, BOOL need83, BOOL cache83, int snum)
 
 
 
-
+/* -------------------------------------------------------------------- */
+/* 
+   Unix SMB/Netbios implementation.
+   Version 3.0
+   Name mangling with persistent tdb
+   Copyright (C) Simo Sorce <idra@samba.org> 2001
+   
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
 
 #if 1
 #define MANGLE_TDB_VERSION		"20010927"
