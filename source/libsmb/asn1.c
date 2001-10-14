1bcdf9106afacbe18437e87178bc1f219695c1e9
Andrew Tridgell <tridge@samba.org>
2001-10-14 09:25:19 +0000

    minor Realloc() fix - pedantic

--- source/libsmb/asn1.c
+++ source/libsmb/asn1.c
@@ -32,11 +32,14 @@ BOOL asn1_write(ASN1_DATA *data, const void *p, int len)
 {
 	if (data->has_error) return False;
 	if (data->length < data->ofs+len) {
-		data->data = Realloc(data->data, data->ofs+len);
-		if (!data->data) {
+		uint8 *p;
+		p = Realloc(data->data, data->ofs+len);
+		if (!p) {
+			SAFE_FREE(data->data);
 			data->has_error = True;
 			return False;
 		}
+		data->data = p;
 		data->length = data->ofs+len;
 	}
 	memcpy(data->data + data->ofs, p, len);
