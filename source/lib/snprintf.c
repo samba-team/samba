57b8e6d742addd9fa90da918ec4343d47562f0b7
Jeremy Allison <jra@samba.org>
2001-10-24 23:52:29 +0000

    Added || -> && fix from "Orwig, Paul" <PORWIG@PacificLife.com>. Jeremy.

--- source/lib/snprintf.c
+++ source/lib/snprintf.c
@@ -748,14 +748,14 @@ static void dopr_outch(char *buffer, size_t *currlen, size_t maxlen, char c)
 	(*currlen)++;
 }
 
-#if !defined(HAVE_VSNPRINTF) || !defined(HAVE_C99_VSNPRINTF)
+#if !defined(HAVE_VSNPRINTF) && !defined(HAVE_C99_VSNPRINTF)
  int vsnprintf (char *str, size_t count, const char *fmt, va_list args)
 {
 	return dopr(str, count, fmt, args);
 }
 #endif
 
-#if !defined(HAVE_SNPRINTF) || !defined(HAVE_C99_VSNPRINTF)
+#if !defined(HAVE_SNPRINTF) && !defined(HAVE_C99_SNPRINTF)
  int snprintf(char *str,size_t count,const char *fmt,...)
 {
 	size_t ret;
