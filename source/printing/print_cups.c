f018aa44732121ba7bc030912bc9d604fb73bb49
Jeremy Allison <jra@samba.org>
2001-11-17 03:20:21 +0000

    Forgot to set copyright correctly. Jeremy.

--- source/printing/print_cups.c
+++ source/printing/print_cups.c
@@ -1,7 +1,7 @@
 /*
  * Support code for the Common UNIX Printing System ("CUPS")
  *
- * Copyright 1999-2001 by Easy Software Products
+ * Copyright 1999-2001 by Michael R Sweet.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
