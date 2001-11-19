f4f65ea9a3c9c99871bb76381541b7f1703c60c8
Martin Pool <mbp@samba.org>
2001-11-19 02:24:05 +0000

    Source file defining global variables for configuration paths.  This has two purposes:
     - If ./configure is re-run to configure Samba for a new set of path
       or prefix, then when Samba is re-built it really will use the new
       paths.
    
     - This is a step towards making sure all programs can be consistently
       told to use a different configuration file or prefix directory,
       which is required for testing.  Possibly not true at the moment.
    (This used to be commit f54c64babd31abf9f5bbf960316d4b9502b9477d)

---
+++ source3/dynconfig.c
@@ -0,0 +1,45 @@
+/* 
+   Unix SMB/Netbios implementation.
+   Copyright (C) 2001 by Martin Pool <mbp@samba.org>
+   
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include "includes.h"
+
+/**
+ * @file dynconfig.c
+ *
+ * @brief Compile-time configured constants.
+ *
+ * This file should be the only file that depends on path
+ * configuration (--prefix, etc), so that if ./configure is re-run,
+ * all programs will be appropriately updated.  Everything else in
+ * Samba should import extern variables from here, rather than relying
+ * on preprocessor macros.
+ *
+ * Eventually some of these may become even more variable, so that
+ * they can for example consistently be set across the whole of Samba
+ * by command-line parameters, config file entries, or environment
+ * variables.
+ **/
+
+char const *dyn_SBINDIR = SBINDIR,
+	*dyn_BINDIR = BINDIR,
+	*dyn_SWATDIR = SWATDIR;
+
+pstring dyn_CONFIGFILE = CONFIGFILE; /**< Location of smb.conf file. **/
+
+pstring dyn_LOGFILEBASE; /**< Log file directory. **/
