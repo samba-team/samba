1dd255b06d4c8669d839a387d4c63ff3475ab1ab
Andrew Tridgell <tridge@samba.org>
2001-11-20 11:32:16 +0000

    forgot this file

---
+++ source/include/ads.h
@@ -0,0 +1,29 @@
+/*
+  header for ads (active directory) library routines
+
+  basically this is a wrapper around ldap
+*/
+
+typedef struct {
+	void *ld;
+	char *realm;
+	char *ldap_server;
+	char *kdc_server;
+	int ldap_port;
+	char *bind_path;
+} ADS_STRUCT;
+
+
+#define UF_DONT_EXPIRE_PASSWD           0x10000
+#define UF_MNS_LOGON_ACCOUNT            0x20000
+#define UF_SMARTCARD_REQUIRED           0x40000
+#define UF_TRUSTED_FOR_DELEGATION       0x80000
+#define UF_NOT_DELEGATED               0x100000
+#define UF_USE_DES_KEY_ONLY            0x200000
+#define UF_DONT_REQUIRE_PREAUTH        0x400000
+
+#define UF_TEMP_DUPLICATE_ACCOUNT       0x0100
+#define UF_NORMAL_ACCOUNT               0x0200
+#define UF_INTERDOMAIN_TRUST_ACCOUNT    0x0800
+#define UF_WORKSTATION_TRUST_ACCOUNT    0x1000
+#define UF_SERVER_TRUST_ACCOUNT         0x2000
