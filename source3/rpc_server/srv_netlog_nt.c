97346ea795f43a9d9487d9c1a63af4016a72e753
Andrew Bartlett <abartlet@samba.org>
2001-11-25 02:30:30 +0000

    Unless the error is exactly NT_STATUS_OK, we might not have a server info, so we need to bail here. (This used to be commit ea0331354e5968aa0a25c0b12379a56c72d7946b)

--- source3/rpc_server/srv_netlog_nt.c
+++ source3/rpc_server/srv_netlog_nt.c
@@ -620,7 +620,7 @@ NTSTATUS _net_sam_logon(pipes_struct *p, NET_Q_SAM_LOGON *q_u, NET_R_SAM_LOGON *
 
 	/* Check account and password */
     
-	if (NT_STATUS_IS_ERR(status)) {
+	if (!NT_STATUS_IS_OK(status)) {
 		free_server_info(&server_info);
 		return status;
 	}
