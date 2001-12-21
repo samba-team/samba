2bfeee924cbe86a97cc70064be920af4e2776432
Tim Potter <tpot@samba.org>
2001-12-21 02:23:38 +0000

    Append to log.winbindd instead of overwriting it so we are consistent with smbd/nmbd behaviour. (This used to be commit 54d276561524213302e7bb2d759d7d4082fd6e8a)

--- source3/nsswitch/winbindd.c
+++ source3/nsswitch/winbindd.c
@@ -730,6 +730,7 @@ int main(int argc, char **argv)
 {
 	extern pstring global_myname;
 	extern fstring global_myworkgroup;
+	extern BOOL append_log;
 	pstring logfile;
 	int accept_sock;
 	BOOL interactive = False;
@@ -741,6 +742,12 @@ int main(int argc, char **argv)
  	CatchSignal(SIGUSR1, SIG_IGN);
 
 	fault_setup((void (*)(void *))fault_quit );
+
+	/* Append to log file by default as we are a single process daemon
+	   program. */
+
+	append_log = True;
+
 	snprintf(logfile, sizeof(logfile), "%s/log.winbindd", dyn_LOGFILEBASE);
 	lp_set_logfile(logfile);
 
