bb70b7811f91290f8c23e745681951a543c15ed4
Gerald Carter <jerry@samba.org>
2002-01-02 23:11:24 +0000

    removed stuff I wasn't using (This used to be commit 27ffce157a655e267f225126f586847e5a0b8614)

--- source3/include/passdb.h
+++ source3/include/passdb.h
@@ -23,55 +23,6 @@
 #ifndef _PASSDB_H
 #define _PASSDB_H
 
-/*
- * This next constant specifies the version number of the VFS interface
- * this smbd will load. Increment this if *ANY* changes are made to the
- * passdb_ops below.
- */
-
-#define SMB_PASSDB_MAJOR_VERSION 1
-#define SMB_PASSDB_MINOR_VERSION 0
-
-/* passdb operations structure */
-struct passdb_ops {
-
-	/* Iteration  Functions*/
-	BOOL (*setsampwent) (BOOL update);
-	void (*endsampwent) (void);
-	SAM_ACCOUNT* (*getsampwent) (void);
-
-	/* Lookup Functions */
-	SAM_ACCOUNT* (*getsampwuid) (uid_t uid);
-	SAM_ACCOUNT* (*getsampwrid) (uint32 rid);
-	SAM_ACOCUNT* (*getsampwnam) (char* username);
-
-	/* Modify the SAM database */
-	BOOL (*update_sam_account) (SAM_ACCOUNT* sampass, BOOL override);
-	BOOL (*delete_sam_account) (char* username);
-	BOOL (*add_sam_account) (SAM_ACCOUNT* sampass);
-
-	/* authenticate a user */
-	SAM_ACCOUNT* (*logon_user) (char* username, char* domain, char* lm_pw,
-		int lm_pw_len, char* nt_pw, int nt_pw_len, char* clear_pass);
-};
-
-
-
-#define SMB_UIDMAP_MAJOR_VERSION 1
-#define SMB_UIDMAP_MINOR_VERSION 0
-
-typedef enum sid_type {SID_USER_TYPE, SID_GROUP_TYPE} SMB_SID_T
-
-/* uid mapping structure */
-struct uidmap_ops {
-
-	/* From NT to UNIX */
-	int (*sid_to_id) (DOM_SID* sid, SMB_SID_T type);
-
-	/* From UNIX to NT */
-	DOM_SID* (*id_to_sid) (int id, SMB_SID_T type);
-
-};
 
 
 #endif /* _PASSDB_H */
