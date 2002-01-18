7d05175494227bf30d098e04ec91c4f0a7b7184c
Jeremy Allison <jra@samba.org>
2002-01-18 03:26:53 +0000

    Ensure (C) message is output on startup. Jeremy.

--- source/nmbd/nmbd.c
+++ source/nmbd/nmbd.c
@@ -770,8 +770,8 @@ static void usage(char *pname)
 
   reopen_logs();
 
-  DEBUG( 1, ( "Netbios nameserver version %s started.\n", VERSION ) );
-  DEBUGADD( 1, ( "Copyright Andrew Tridgell 1994-1998\n" ) );
+  DEBUG( 0, ( "Netbios nameserver version %s started.\n", VERSION ) );
+  DEBUGADD( 0, ( "Copyright Andrew Tridgell and the Samba Team 1994-2002\n" ) );
 
   if ( !reload_nmbd_services(False) )
     return(-1);
