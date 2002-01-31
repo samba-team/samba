9930cf97330dd93985c5558cec6b24406e90c228
Andrew Tridgell <tridge@samba.org>
2002-01-31 09:37:26 +0000

    this fixes the problem of not being able to add a SD to a file on a non-domain Samba server from a NT4 client.
    Note that this exactly reverses a change by Jeremy on the 18th of
    December 2001, reverting the code back to what JF originally wrote. I
    have looked carefully with a sniffer and JFs original NULL sid is
    correct (ie. it matches what NT4 does) and also fixes the problem.
    
    Sending a blank sid (which is what jeremy's patch did) causes NT4 to
    give a classic "parameter is incorrect error" and prevents the
    addition of new ACLs.

--- source/rpc_server/srv_lsa_nt.c
+++ source/rpc_server/srv_lsa_nt.c
@@ -441,7 +441,6 @@ NTSTATUS _lsa_enum_trust_dom(pipes_struct *p, LSA_Q_ENUM_TRUST_DOM *q_u, LSA_R_E
 
 NTSTATUS _lsa_query_info(pipes_struct *p, LSA_Q_QUERY_INFO *q_u, LSA_R_QUERY_INFO *r_u)
 {
-	extern DOM_SID global_sid_nonexistent;
 	struct lsa_info *handle;
 	LSA_INFO_UNION *info = &r_u->dom;
 	DOM_SID domain_sid;
@@ -494,7 +493,7 @@ NTSTATUS _lsa_query_info(pipes_struct *p, LSA_Q_QUERY_INFO *q_u, LSA_R_QUERY_INF
 				break;
 			case ROLE_STANDALONE:
 				name = global_myworkgroup;
-				sid = &global_sid_nonexistent;
+				sid = NULL;
 				break;
 			default:
 				return NT_STATUS_CANT_ACCESS_DOMAIN_INFO;
