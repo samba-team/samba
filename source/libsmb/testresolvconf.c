9762e1dfe167f691ad2f028222e38641b0a22237
Martin Pool <mbp@samba.org>
2002-02-14 21:41:13 +0000

    Welcome, mbp, to the charset_initialise club :-/

--- source/libsmb/testresolvconf.c
+++ source/libsmb/testresolvconf.c
@@ -8,6 +8,8 @@ int main(void)
 {
 	char name_server_arr[3][16];
 
+	charset_initialise();
+
 	if (parse_resolvconf(name_server_arr)) {
 		printf("parse succeeded\n"
 		       "first nameserver: %s\n",
