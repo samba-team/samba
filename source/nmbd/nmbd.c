b19db1075cb781475cbf62854e4cc8283585d0dd
Jeremy Allison <jra@samba.org>
2002-03-02 01:01:41 +0000

    This code is so obviously correct it needs to be in app-head. Jeremy.

--- source/nmbd/nmbd.c
+++ source/nmbd/nmbd.c
@@ -56,51 +56,51 @@ time_t StartupTime = 0;
 extern struct in_addr ipzero;
 
 /**************************************************************************** **
-  catch a sigterm
+ Handle a SIGTERM in band.
  **************************************************************************** */
-static void sig_term(int sig)
+
+static void terminate(void)
 {
-  BlockSignals(True,SIGTERM);
-  
-  DEBUG(0,("Got SIGTERM: going down...\n"));
-  
-  /* Write out wins.dat file if samba is a WINS server */
-  wins_write_database(False);
-  
-  /* Remove all SELF registered names. */
-  release_my_names();
-  
-  /* Announce all server entries as 0 time-to-live, 0 type. */
-  announce_my_servers_removed();
+        DEBUG(0,("Got SIGTERM: going down...\n"));
 
-  /* If there was an async dns child - kill it. */
-  kill_async_dns_child();
+        /* Write out wins.dat file if samba is a WINS server */
+        wins_write_database(False);
 
-  exit(0);
+        /* Remove all SELF registered names. */
+        release_my_names();
 
-} /* sig_term */
+        /* Announce all server entries as 0 time-to-live, 0 type. */
+        announce_my_servers_removed();
+
+        /* If there was an async dns child - kill it. */
+        kill_async_dns_child();
+
+        exit(0);
+}
 
 /**************************************************************************** **
- catch a sighup
+ Catch a SIGTERM signal.
  **************************************************************************** */
-static VOLATILE SIG_ATOMIC_T reload_after_sighup = False;
+
+static VOLATILE sig_atomic_t got_sig_term;
+
+static void sig_term(int sig)
+{
+        got_sig_term = 1;
+        sys_select_signal();
+}
+
+/**************************************************************************** **
+ Catch a SIGHUP signal.
+ **************************************************************************** */
+
+static VOLATILE sig_atomic_t reload_after_sighup;
 
 static void sig_hup(int sig)
 {
-  BlockSignals( True, SIGHUP );
-
-  DEBUG( 0, ( "Got SIGHUP dumping debug info.\n" ) );
-
-  write_browse_list( 0, True );
-
-  dump_all_namelists();
-
-  reload_after_sighup = True;
-
-  BlockSignals(False,SIGHUP);
-
-} /* sig_hup */
-
+        reload_after_sighup = 1;
+        sys_select_signal();
+}
 
 #if DUMP_CORE
 /**************************************************************************** **
@@ -337,6 +337,15 @@ static void process(void)
       return;
 
     /*
+     * Handle termination inband.
+     */
+
+    if (got_sig_term) {
+      got_sig_term = 0;
+      terminate();
+    }
+
+    /*
      * Process all incoming packets
      * read above. This calls the success and
      * failure functions registered when response
@@ -489,11 +498,14 @@ static void process(void)
      */
 
     if(reload_after_sighup) {
-	    reload_nmbd_services( True );
-	    reopen_logs();
-	    if(reload_interfaces(0))
-			return;
-	    reload_after_sighup = False;
+      DEBUG( 0, ( "Got SIGHUP dumping debug info.\n" ) );
+      write_browse_list( 0, True );
+      dump_all_namelists();
+      reload_nmbd_services( True );
+      reopen_logs();
+      if(reload_interfaces(0))
+        return;
+      reload_after_sighup = 0;
     }
 
     /* check for new network interfaces */
