0843c37abd0a1520fc3117f91fc6284d9475c87e
Andrew Tridgell <tridge@samba.org>
2002-03-10 23:18:44 +0000

    failed timed locks always give LOCK_CONFLICT not LOCK_NOT_GRANTED (This used to be commit ec71c1a66f9fd4b9cb4cad5a9b5b17e20de7aeb1)

--- source3/smbd/blocking.c
+++ source3/smbd/blocking.c
@@ -175,6 +175,12 @@ static void generic_blocking_lock_error(blocking_lock_record *blr, NTSTATUS stat
 	char *inbuf = blr->inbuf;
 	construct_reply_common(inbuf, outbuf);
 
+	/* whenever a timeout is given w2k maps LOCK_NOT_GRANTED to
+	   FILE_LOCK_CONFLICT! (tridge) */
+	if (NT_STATUS_EQUAL(status, NT_STATUS_LOCK_NOT_GRANTED)) {
+		status = NT_STATUS_FILE_LOCK_CONFLICT;
+	}
+
 	ERROR_NT(status);
 	if (!send_smb(smbd_server_fd(),outbuf))
 		exit_server("generic_blocking_lock_error: send_smb failed.");
