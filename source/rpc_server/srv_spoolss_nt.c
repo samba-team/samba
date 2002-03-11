65257499c29111f2a48fb83e5fe986299165937e
Tim Potter <tpot@samba.org>
2002-03-11 03:01:25 +0000

    Updated copyright.

--- source/rpc_server/srv_spoolss_nt.c
+++ source/rpc_server/srv_spoolss_nt.c
@@ -4,9 +4,10 @@
  *  RPC Pipe client / server routines
  *  Copyright (C) Andrew Tridgell              1992-2000,
  *  Copyright (C) Luke Kenneth Casson Leighton 1996-2000,
- *  Copyright (C) Jean François Micouleau      1998-2000.
- *  Copyright (C) Jeremy Allison		    2001.
- *  Copyright (C) Gerald Carter		       2000-2001.
+ *  Copyright (C) Jean François Micouleau      1998-2000,
+ *  Copyright (C) Jeremy Allison		    2001,
+ *  Copyright (C) Gerald Carter		       2000-2001,
+ *  Copyright (C) Tim Potter                   2001-2002.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
