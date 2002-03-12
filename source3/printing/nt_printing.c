92e59edc410a1de09a3d1eaba09cd46f4db1367f
Tim Potter <tpot@samba.org>
2002-03-12 00:16:03 +0000

    Merge of enumprinters vs getprinter naming patch from 2.2 (This used to be commit dfd51bc8d0714473880bf50369f7994304c1d83f)

--- source3/printing/nt_printing.c
+++ source3/printing/nt_printing.c
@@ -2485,8 +2485,11 @@ static WERROR get_a_printer_2_default(NT_PRINTER_INFO_LEVEL_2 **info_ptr, fstrin
 	snum = lp_servicenumber(sharename);
 
 	slprintf(info.servername, sizeof(info.servername)-1, "\\\\%s", get_called_name());
-	slprintf(info.printername, sizeof(info.printername)-1, "\\\\%s\\%s", 
-		 get_called_name(), sharename);
+	strupper(info.servername);
+	slprintf(info.printername, sizeof(info.printername)-1, "\\\\%s\\", 
+		 get_called_name());
+	strupper(info.printername);
+	fstrcat(info.printername, sharename);
 	fstrcpy(info.sharename, sharename);
 	fstrcpy(info.portname, SAMBA_PRINTER_PORT_NAME);
 	fstrcpy(info.drivername, lp_printerdriver(snum));
@@ -2602,10 +2605,9 @@ static WERROR get_a_printer_2(NT_PRINTER_INFO_LEVEL_2 **info_ptr, fstring sharen
 	info.attributes |= (PRINTER_ATTRIBUTE_SHARED | PRINTER_ATTRIBUTE_NETWORK); 
 
 	/* Restore the stripped strings. */
-	slprintf(info.servername, sizeof(info.servername)-1, "\\\\%s", get_called_name());
-	slprintf(printername, sizeof(printername)-1, "\\\\%s\\%s", get_called_name(),
-			info.printername);
-	fstrcpy(info.printername, printername);
+	slprintf(info.servername, sizeof(info.servername)-1, "\\\\%s",
+		 get_called_name());
+	strupper(info.servername);
 
 	len += unpack_devicemode(&info.devmode,dbuf.dptr+len, dbuf.dsize-len);
 
