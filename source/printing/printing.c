f50160d3c6feda54d9d04c1bfdd9ae273eaa7f6d
John Reilly <jreilly@samba.org>
2002-03-19 19:45:45 +0000

    Fixed page count/job size macros.
    printing/printing.c        add print job page count (%c) and print job file size in bytes (%z)
    
    JohnR cr276

--- source/printing/printing.c
+++ source/printing/printing.c
@@ -1158,6 +1158,7 @@ BOOL print_job_end(int jobid, BOOL normal_close)
 	pstring print_directory;
 	char *wd, *p;
 	pstring jobname;
+	fstring job_page_count, job_size;
 
 	if (!pjob)
 		return False;
@@ -1208,6 +1209,8 @@ BOOL print_job_end(int jobid, BOOL normal_close)
 
 	pstrcpy(jobname, pjob->jobname);
 	pstring_sub(jobname, "'", "_");
+	slprintf(job_page_count, sizeof(job_page_count), "%d", pjob->page_count);
+	slprintf(job_size, sizeof(job_size), "%d", pjob->size);
 
 	/* send it to the system spooler */
 	ret = print_run_command(snum, 
@@ -1215,6 +1218,8 @@ BOOL print_job_end(int jobid, BOOL normal_close)
 			  "%s", p,
   			  "%J", jobname,
 			  "%f", p,
+			  "%z", job_size,
+			  "%c", job_page_count,
 			  NULL);
 
 	chdir(wd);
