1d582af3c0dcbfad9a158f3ed38219c95424b045
Tim Potter <tpot@samba.org>
2002-03-24 23:25:05 +0000

    Spelling fixes. (This used to be commit a5ac2ac4ada48ee3be061a32ba40bd8c4b3b3865)

--- source3/lib/util_sec.c
+++ source3/lib/util_sec.c
@@ -414,7 +414,7 @@ main()
 #endif
 
 /****************************************************************************
-Check if we are setuid root.  Used in libsmb and smbpasswd parinoia checks.
+Check if we are setuid root.  Used in libsmb and smbpasswd paranoia checks.
 ****************************************************************************/
 BOOL is_setuid_root(void) 
 {
