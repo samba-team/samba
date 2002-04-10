d30443051d9ef9f133df313bca4ff7c482920201
Tim Potter <tpot@samba.org>
2002-04-10 00:40:10 +0000

    Merge from HEAD:
    >Moved definition of winbind username/password secrets into secrets.h
    
    Also added copyright notice and guards.

--- source/nsswitch/winbindd.h
+++ source/nsswitch/winbindd.h
@@ -203,10 +203,4 @@ typedef struct {
 #define SETENV(name, value, overwrite) ;
 #endif
 
-/* Authenticated user info is stored in secrets.tdb under these keys */
-
-#define SECRETS_AUTH_USER      "SECRETS/AUTH_USER"
-#define SECRETS_AUTH_DOMAIN      "SECRETS/AUTH_DOMAIN"
-#define SECRETS_AUTH_PASSWORD  "SECRETS/AUTH_PASSWORD"
-
 #endif /* _WINBINDD_H */
