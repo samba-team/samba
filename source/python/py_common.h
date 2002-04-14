14e4d889a236dd9c2ba6db68b3133e44195b8a47
Tim Potter <tpot@samba.org>
2002-04-14 00:58:16 +0000

    Moved open_pipe_creds() function to common file.

--- source/python/py_common.h
+++ source/python/py_common.h
@@ -27,8 +27,19 @@ void py_samba_init(void);
 PyObject *py_werror_tuple(WERROR werror);
 PyObject *py_ntstatus_tuple(NTSTATUS ntstatus);
 
-PyObject *py_setup_logging(PyObject *self, PyObject *args);
+PyObject *py_setup_logging(PyObject *self, PyObject *args, PyObject *kw);
 PyObject *get_debuglevel(PyObject *self, PyObject *args);
 PyObject *set_debuglevel(PyObject *self, PyObject *args);
+
+/* Return a cli_state struct opened on the SPOOLSS pipe.  If credentials
+   are passed use them. */
+
+typedef struct cli_state *(cli_pipe_fn)(
+	struct cli_state *cli, char *system_name,
+	struct ntuser_creds *creds);
+
+struct cli_state *open_pipe_creds(char *system_name, PyObject *creds, 
+				  cli_pipe_fn *connect_fn,
+				  struct cli_state *cli);
 
 #endif /* _PY_COMMON_H */
