80b22f643394b2362be2a5a0a3a8b192c133a88f
Andrew Bartlett <abartlet@samba.org>
2002-05-19 02:28:14 +0000

    Keep the compiler happy (This used to be commit 84ea2a434b510ed49838a04a4b30bd2fc9ec5673)

--- source3/lib/messages.c
+++ source3/lib/messages.c
@@ -95,7 +95,7 @@ void debuglevel_message(int msg_type, pid_t src, void *buf, size_t len)
 	char *debug_level_classes;
 	DEBUG(1,("INFO: Received REQ_DEBUGLEVEL message from PID %u\n",(unsigned int)src));
 
-	if (debug_level_classes = debug_list_class_names_and_levels()) {
+	if ((debug_level_classes = debug_list_class_names_and_levels())) {
 	/*{ debug_level_classes = "test:1000";*/
 		message_send_pid(src, MSG_DEBUGLEVEL, debug_level_classes, strlen(debug_level_classes) + 1, True);
 		SAFE_FREE(debug_level_classes);
