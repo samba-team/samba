dd1bd8d0c8c09db23e944466a0443a5ad03bea26
Andrew Bartlett <abartlet@samba.org>
2002-06-25 01:40:44 +0000

    Add a couple more DEBUG()s to winbindd.
    Andrew Bartlett
    (This used to be commit 3b2464ffdad5e64a05e227b50116cb59f6d34204)

--- source3/nsswitch/winbindd_group.c
+++ source3/nsswitch/winbindd_group.c
@@ -84,8 +84,8 @@ static BOOL fill_grent_mem(struct winbindd_domain *domain,
 	status = domain->methods->lookup_groupmem(domain, mem_ctx, group_rid, &num_names, 
 						  &rid_mem, &names, &name_types);
 	if (!NT_STATUS_IS_OK(status)) {
-		DEBUG(1, ("could not lookup membership for group rid %d in domain %s\n", 
-			  group_rid, domain->name));
+		DEBUG(1, ("could not lookup membership for group rid %d in domain %s (error: %s)\n", 
+			  group_rid, domain->name, nt_errstr(status)));
 
 		goto done;
 	}
@@ -363,8 +363,10 @@ enum winbindd_result winbindd_setgrent(struct winbindd_cli_state *state)
 		/* Create a state record for this domain */
 		
 		if ((domain_state = (struct getent_state *)
-		     malloc(sizeof(struct getent_state))) == NULL)
+		     malloc(sizeof(struct getent_state))) == NULL) {
+			DEBUG(1, ("winbindd_setgrent: malloc failed for domain_state!\n"));
 			return WINBINDD_ERROR;
+		}
 		
 		ZERO_STRUCTP(domain_state);
 		
@@ -411,8 +413,10 @@ static BOOL get_sam_group_entries(struct getent_state *ent)
 		return False;
 
 	if (!(mem_ctx = talloc_init_named("get_sam_group_entries(%s)",
-					  ent->domain_name)))
+					  ent->domain_name))) {
+		DEBUG(1, ("get_sam_group_entries: could not create talloc context!\n")); 
 		return False;
+	}
 		
 	/* Free any existing group info */
 
@@ -435,6 +439,7 @@ static BOOL get_sam_group_entries(struct getent_state *ent)
 						  &sam_grp_entries);
 	
 	if (!NT_STATUS_IS_OK(status)) {
+		DEBUG(3, ("get_sam_group_entries: could not enumerate domain groups! Error: %s", nt_errstr(status)));
 		result = False;
 		goto done;
 	}
