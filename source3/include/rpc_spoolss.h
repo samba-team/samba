fca23fab8ac733ab7a4bf867c9c02c81481c2dd6
Gerald Carter <jerry@samba.org>
2002-07-11 13:27:32 +0000

    added copyright (This used to be commit 0bb3d0f1ca9504d2430471558e1a0125d72ee82f)

--- source3/include/rpc_spoolss.h
+++ source3/include/rpc_spoolss.h
@@ -5,6 +5,7 @@
    Copyright (C) Andrew Tridgell              1992-2000,
    Copyright (C) Luke Kenneth Casson Leighton 1996-2000,
    Copyright (C) Jean Francois Micouleau      1998-2000.
+   Copyright (C) Gerald Carter                2001-2002.
    
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
