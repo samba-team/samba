775b918b8c63b1fcd9a8db1743505ab718978c19
Andrew Tridgell <tridge@samba.org>
2002-07-15 03:51:53 +0000

    don't report the faiilure of non-blocking locks. They are supposed to fail sometimes, thats why they are non-blocking :)

--- source/tdb/tdb.c
+++ source/tdb/tdb.c
@@ -189,7 +189,7 @@ static int tdb_brlock(TDB_CONTEXT *tdb, tdb_off offset,
 	} while (ret == -1 && errno == EINTR);
 
 	if (ret == -1) {
-		if (!probe) {
+		if (!probe && lck_type != F_SETLK) {
 			TDB_LOG((tdb, 5,"tdb_brlock failed (fd=%d) at offset %d rw_type=%d lck_type=%d\n", 
 				 tdb->fd, offset, rw_type, lck_type));
 		}
