370fa2629fb665fd7ce6f290a162ba1ece745cea
Jeremy Allison <jra@samba.org>
2002-07-18 23:19:22 +0000

    Needs fixing in all_string_sub too. Jeremy.

--- source/lib/util_str.c
+++ source/lib/util_str.c
@@ -1182,7 +1182,7 @@ void string_sub(char *s,const char *pattern,const char *insert, size_t len)
 		len = ls;
 	
 	while (lp <= ls && (p = strstr(s,pattern))) {
-		if (ls + (li-lp) >= len) {
+		if (ls + (li-lp) > len) {
 			DEBUG(0,("ERROR: string overflow by %d in string_sub(%.50s, %d)\n", 
 				 (int)(ls + (li-lp) - len),
 				 pattern, (int)len));
@@ -1244,7 +1244,7 @@ void all_string_sub(char *s,const char *pattern,const char *insert, size_t len)
 		len = ls;
 	
 	while (lp <= ls && (p = strstr(s,pattern))) {
-		if (ls + (li-lp) >= len) {
+		if (ls + (li-lp) > len) {
 			DEBUG(0,("ERROR: string overflow by %d in all_string_sub(%.50s, %d)\n", 
 				 (int)(ls + (li-lp) - len),
 				 pattern, (int)len));
