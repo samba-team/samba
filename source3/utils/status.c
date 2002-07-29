11785c7bc83b153e8d177928283474bbb0e5f3a9
Tim Potter <tpot@samba.org>
2002-07-29 09:25:23 +0000

    Use common popt definition for -d option. (This used to be commit 8c17904848a6206ab35652625ff5f3afcf6bcb0d)

--- source3/utils/status.c
+++ source3/utils/status.c
@@ -551,7 +551,7 @@ static int traverse_sessionid(TDB_CONTEXT *tdb, TDB_DATA kbuf, TDB_DATA dbuf, vo
 		{"brief",	'b', POPT_ARG_NONE, 	&brief},
 		{"profile",	'P', POPT_ARG_NONE,	&profile_only},
 		{"byterange",	'B', POPT_ARG_NONE,	&show_brl},
-		{"debug",	'd', POPT_ARG_STRING,	&new_debuglevel},
+		{ NULL, 0, POPT_ARG_INCLUDE_TABLE, popt_common_debug },
 		{ 0, 0, 0, 0}
 	};
 
