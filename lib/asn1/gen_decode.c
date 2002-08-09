dca68aa10e2d24239ab77227e90b843b608dba04
Johan Danielsson <joda@pdc.kth.se>
2002-08-09 15:37:34 +0000

    remove unused variable
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@11068 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/asn1/gen_decode.c
+++ lib/asn1/gen_decode.c
@@ -314,9 +314,8 @@ generate_type_decode (const Symbol *s)
     fprintf (codefile,
 	     "size_t ret = 0, reallen;\n"
 	     "size_t l;\n"
-	     "int i, e;\n\n");
+	     "int e;\n\n");
     fprintf (codefile, "memset(data, 0, sizeof(*data));\n");
-    fprintf (codefile, "i = 0;\n"); /* hack to avoid `unused variable' */
     fprintf (codefile, "reallen = 0;\n"); /* hack to avoid `unused variable' */
 
     decode_type ("data", s->type);
