5e5cd2874527dd9a213c4bfcf98a425c39f3f2e2
Andrew Bartlett <abartlet@samba.org>
2002-08-17 07:21:01 +0000

    Quick hack to get around the inadequacy of pdb_smbpasswd.  This should make the build farm happy again, and allow the 'guest account' to be added to smbpasswd.
    Andrew Bartlett

--- source/passdb/pdb_smbpasswd.c
+++ source/passdb/pdb_smbpasswd.c
@@ -1149,7 +1149,7 @@ static BOOL build_smb_pass (struct smb_passwd *smb_pw, const SAM_ACCOUNT *sampas
 		uid = pdb_get_uid(sampass);
 
 		/* If the user specified a RID, make sure its able to be both stored and retreived */
-		if (rid && uid != fallback_pdb_user_rid_to_uid(rid)) {
+		if (rid && rid != DOMAIN_USER_RID_GUEST && uid != fallback_pdb_user_rid_to_uid(rid)) {
 			DEBUG(0,("build_sam_pass: Failing attempt to store user with non-uid based user RID. \n"));
 			return False;
 		}
