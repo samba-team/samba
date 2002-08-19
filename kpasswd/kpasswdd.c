d0af49b56ee4533ef46ec99e7fc92b50467cfdec
Johan Danielsson <joda@pdc.kth.se>
2002-08-19 15:07:31 +0000

    include <kadm5/private.h>
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@11130 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- kpasswd/kpasswdd.c
+++ kpasswd/kpasswdd.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997-2001 Kungliga Tekniska Högskolan
+ * Copyright (c) 1997-2002 Kungliga Tekniska Högskolan
  * (Royal Institute of Technology, Stockholm, Sweden). 
  * All rights reserved. 
  *
@@ -35,8 +35,11 @@
 RCSID("$Id$");
 
 #include <kadm5/admin.h>
-
+#ifdef HAVE_SYS_UN_H
+#include <sys/un.h>
+#endif
 #include <hdb.h>
+#include <kadm5/private.h>
 
 static krb5_context context;
 static krb5_log_facility *log_facility;
