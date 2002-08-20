a71ea58806699026f6d969b1ed4502486411cbb3
Simo Sorce <idra@samba.org>
2002-08-20 20:43:39 +0000

    fix typos thanks Josef Zlomek

--- source/client/client.c
+++ source/client/client.c
@@ -300,7 +300,7 @@ static BOOL do_this_one(file_info *finfo)
 
 	if (*fileselection && 
 	    !mask_match(finfo->name,fileselection,False)) {
-		DEBUG(3,("match_match %s failed\n", finfo->name));
+		DEBUG(3,("mask_match %s failed\n", finfo->name));
 		return False;
 	}
 
@@ -681,7 +681,7 @@ static void do_get(char *rname,char *lname)
 	}
 
 	DEBUG(2,("getting file %s of size %.0f as %s ", 
-		 lname, (double)size, lname));
+		 rname, (double)size, lname));
 
 	if(!(data = (char *)malloc(read_size))) { 
 		DEBUG(0,("malloc fail for size %d\n", read_size));
