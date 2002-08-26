0cf44e300ea22d2a1471522bf13ca5354aab310e
Assar Westerlund <assar@sics.se>
2002-08-26 13:28:36 +0000

    (find_db_spec): const-correctness in parameters to krb5_config_get_next
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@11231 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/kadm5/context_s.c
+++ lib/kadm5/context_s.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997 - 2000 Kungliga Tekniska Högskolan
+ * Copyright (c) 1997 - 2002 Kungliga Tekniska Högskolan
  * (Royal Institute of Technology, Stockholm, Sweden). 
  * All rights reserved. 
  *
@@ -124,14 +124,15 @@ set_config(kadm5_server_context *ctx,
 static kadm5_ret_t
 find_db_spec(kadm5_server_context *ctx)
 {
-    krb5_config_binding *top_binding = NULL;
+    const krb5_config_binding *top_binding = NULL;
     krb5_config_binding *db_binding;
     krb5_config_binding *default_binding = NULL;
     krb5_context context = ctx->context;
 
     while((db_binding = (krb5_config_binding *)
 	   krb5_config_get_next(context,
-				NULL, &top_binding, 
+				NULL,
+				&top_binding, 
 				krb5_config_list, 
 				"kdc", 
 				"database",
