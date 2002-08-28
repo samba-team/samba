739bc21429fee6ceb7430eb6c5e63882f6d4f030
Volker Lendecke <vlendec@samba.org>
2002-08-28 04:19:15 +0000

    Merge from 2_2.
    Volker

--- docs/docbook/scripts/strip-links.pl
+++ docs/docbook/scripts/strip-links.pl
@@ -1,13 +1,15 @@
 #!/usr/bin/perl
 
-## small script to stirp the <URL:...> tags from 
+## small script to strip the <URL:...> tags from
 ## manpages generated from docbook2man.  we'll leave
 ## the <URL:ftp://...> and <URL:mailto:...> links for now
 
 while (<STDIN>) {
 
 	chomp ($_);
-	$_ =~ s/\s*<URL:.*>\s*//g;
+	$_ =~ s/\s*<URL:.*html.*>\s+/ /g;
+	$_ =~ s/\s*<URL:.*html.*>\S//g;
+	$_ =~ s/\s*<URL:.*html.*>$//g;
 	print "$_\n";
 
 }
