e1c8ad0a50bb119dd8d1f627b4501dbe02b4bebe
Gerald Carter <jerry@samba.org>
2002-08-30 11:56:47 +0000

    fixing broken dependencies due to REGISTRY_VALUE's.  Merge from HEAD.

--- source/registry/reg_objects.c
+++ source/registry/reg_objects.c
@@ -22,9 +22,6 @@
 
 #include "includes.h"
 
-#undef DBGC_CLASS
-#define DBGC_CLASS DBGC_RPC_SRV
-
 
 /***********************************************************************
  Init the talloc context held by a REGSUBKEY_CTR structure
