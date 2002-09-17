dfba05e08a6041f9822be10240e322462155a138
Jeremy Allison <jra@samba.org>
2002-09-17 20:54:52 +0000

    Reverted my earlier change. We must be protected by the pidfile lock before doing secrets_init(). Jeremy.

--- source/nsswitch/winbindd.c
+++ source/nsswitch/winbindd.c
@@ -795,8 +795,11 @@ int main(int argc, char **argv)
 
         fstrcpy(global_myworkgroup, lp_workgroup());
 
-	if (!interactive)
+	if (!interactive) {
 		become_daemon();
+		pidfile_create("winbindd");
+	}
+
 
 #if HAVE_SETPGID
 	/*
@@ -870,10 +873,6 @@ int main(int argc, char **argv)
 		DEBUG(0, ("failed to create socket\n"));
 		return 1;
 	}
-
-	/* Only create the pidfile when we're ready to receive requests. */
-	if (!interactive)
-		pidfile_create("winbindd");
 
 	/* Loop waiting for requests */
 
