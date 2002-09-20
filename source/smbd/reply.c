3373ec5deef5c9a1760c047a5b8a839efa82b521
Jeremy Allison <jra@samba.org>
2002-09-20 18:35:12 +0000

    @ is a valid character in domain names. Jeremy.

--- source/smbd/reply.c
+++ source/smbd/reply.c
@@ -860,7 +860,7 @@ int reply_sesssetup_and_X(connection_struct *conn, char *inbuf,char *outbuf,int 
 
   /* don't allow strange characters in usernames or domains */
   alpha_strcpy(user, user, ". _-$", sizeof(user));
-  alpha_strcpy(domain, domain, ". _-", sizeof(domain));
+  alpha_strcpy(domain, domain, ". _-@", sizeof(domain));
   if (strstr(user, "..") || strstr(domain,"..")) {
 	  return ERROR_BOTH(NT_STATUS_LOGON_FAILURE,ERRSRV,ERRbadpw);
   }
