1e4ac9615d26bfbf90e3a45f088564f448f689ce
Richard Sharpe <sharpe@samba.org>
2002-09-28 03:38:37 +0000

    Commit a fix to smbclient so that it will connect to EMCs and NetApp's machines.

--- source/client/client.c
+++ source/client/client.c
@@ -2436,9 +2436,24 @@ static struct cli_state *do_connect(const char *server, const char *share)
 
 	if (!cli_send_tconX(c, sharename, "?????",
 			    password, strlen(password)+1)) {
-		d_printf("tree connect failed: %s\n", cli_errstr(c));
-		cli_shutdown(c);
-		return NULL;
+		pstring share;
+
+		/*
+		 * Some servers require \\server\share for the share
+		 * while others are happy with share as we gave above
+		 * Lets see if we give it the long form if it works
+		 */
+		pstrcpy(share, "\\\\");
+		pstrcat(share, server);
+		pstrcat(share, "\\");
+		pstrcat(share, sharename);
+		if (!cli_send_tconX(c, share, "?????", password,
+					strlen(password) + 1)) {
+
+			d_printf("tree connect failed: %s\n", cli_errstr(c));
+			cli_shutdown(c);
+			return NULL;
+		}
 	}
 
 	DEBUG(4,(" tconx ok\n"));
