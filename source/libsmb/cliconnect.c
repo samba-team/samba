6f4218c8fe43b93d8ff4452ed567f5c5d7d8bc3d
Gerald Carter <jerry@samba.org>
2002-10-17 22:00:01 +0000

    don't upper ase the username when connecting to a server

--- source/libsmb/cliconnect.c
+++ source/libsmb/cliconnect.c
@@ -284,7 +284,7 @@ static BOOL cli_session_setup_nt1(struct cli_state *cli, char *user,
 	p = smb_buf(cli->outbuf);
 	memcpy(p,pword,passlen); p += passlen;
 	memcpy(p,ntpword,ntpasslen); p += ntpasslen;
-	p += clistr_push(cli, p, user, -1, STR_TERMINATE|STR_UPPER|STR_CONVERT);
+	p += clistr_push(cli, p, user, -1, STR_TERMINATE|STR_CONVERT);
 	p += clistr_push(cli, p, workgroup, -1, STR_TERMINATE|STR_UPPER|STR_CONVERT);
 	p += clistr_push(cli, p, "Unix", -1, STR_TERMINATE|STR_CONVERT);
 	p += clistr_push(cli, p, "Samba", -1, STR_TERMINATE|STR_CONVERT);
