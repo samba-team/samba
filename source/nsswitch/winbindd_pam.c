dd1b95e517cec1a75b62c7df2963398054deda20
Tim Potter <tpot@samba.org>
2002-11-01 22:22:12 +0000

    Missed one...

--- source/nsswitch/winbindd_pam.c
+++ source/nsswitch/winbindd_pam.c
@@ -172,7 +172,7 @@ enum winbindd_result winbindd_pam_auth_crap(struct winbindd_cli_state *state)
 	if (!secrets_fetch_trust_account_password(
                 lp_workgroup_unix(), trust_passwd, &last_change_time)) {
 		DEBUG(0, ("winbindd_pam_auth: could not fetch trust account "
-                          "password for domain %s\n", lp_workgroup()));
+                          "password for domain %s\n", lp_workgroup_unix()));
 		result = NT_STATUS_CANT_ACCESS_DOMAIN_INFO;
 		goto done;
 	}
