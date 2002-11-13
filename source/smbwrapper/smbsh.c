6ab12ca80d12fcecf6ad3a534bd73b482d991112
Jeremy Allison <jra@samba.org>
2002-11-13 21:47:21 +0000

    Fix smbwrapper from the global_* changes. Jeremy.

--- source/smbwrapper/smbsh.c
+++ source/smbwrapper/smbsh.c
@@ -36,7 +36,7 @@ static void smbsh_usage(void)
 int main(int argc, char *argv[])
 {
 	char *p, *u;
-	char *libd = dyn_BINDIR;
+	const char *libd = dyn_BINDIR;
 	pstring line, wd;
 	int opt;
 	extern char *optarg;
