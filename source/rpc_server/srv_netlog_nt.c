58fa6bfee8ba35cc182c18c980e0a4040ddd7d09
Andrew Tridgell <tridge@samba.org>
2002-11-20 00:53:24 +0000

    merged the %U changes to 3.0

--- source/rpc_server/srv_netlog_nt.c
+++ source/rpc_server/srv_netlog_nt.c
@@ -566,6 +566,7 @@ NTSTATUS _net_sam_logon(pipes_struct *p, NET_Q_SAM_LOGON *q_u, NET_R_SAM_LOGON *
                  nt_workstation, nt_domain));
    	
 	pstrcpy(current_user_info.smb_name, nt_username);
+	sub_set_smb_name(nt_username);
      
 	/*
 	 * Convert to a UNIX username.
