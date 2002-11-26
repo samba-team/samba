3c2d396aa8d674970b2cc043fd32d2648fcd004e
Jeremy Allison <jra@samba.org>
2002-11-26 17:40:25 +0000

    Ensure data is not used uninitialised. Jeremy.

--- source/libsmb/namecache.c
+++ source/libsmb/namecache.c
@@ -161,7 +161,7 @@ BOOL namecache_fetch(const char *name, int name_type, struct in_addr **ip_list,
 		     int *num_names)
 {
 	TDB_DATA key, value;
-	struct nc_value *data;
+	struct nc_value *data = NULL;
 	time_t now;
 	int i;
 
