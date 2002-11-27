c827bc9299b582f96a539b938ccf516817b1e03e
Tim Potter <tpot@samba.org>
2002-11-27 01:34:31 +0000

    Convert password to dos codepage before calling SMBNTencrypt() so that i18n passwords work for winbindd plaintext authentication.
    CR#1041

--- source/nsswitch/winbindd_pam.c
+++ source/nsswitch/winbindd_pam.c
@@ -67,8 +67,8 @@ enum winbindd_result winbindd_pam_auth(struct winbindd_cli_state *state)
 		
 		generate_random_buffer(chal, 8, False);
 		SMBencrypt( (const uchar *)state->request.data.auth.pass, chal, local_lm_response);
-		
-		SMBNTencrypt((const uchar *)state->request.data.auth.pass, chal, local_nt_response);
+		SMBNTencrypt((const uchar *)unix_to_dos_static(state->request.data.auth.pass), chal, 
+			     local_nt_response);
 
 		lm_resp = data_blob_talloc(mem_ctx, local_lm_response, sizeof(local_lm_response));
 		nt_resp = data_blob_talloc(mem_ctx, local_nt_response, sizeof(local_nt_response));
