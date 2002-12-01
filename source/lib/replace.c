24fb89da51f54cc547f7f963b094b0f3178632e4
Andrew Bartlett <abartlet@samba.org>
2002-12-01 03:07:42 +0000

    Merge from HEAD, struct in_addr is always in network byte order, so we don't need a little-endian case here.

--- source/lib/replace.c
+++ source/lib/replace.c
@@ -328,13 +328,8 @@ char *rep_inet_ntoa(struct in_addr ip)
 {
 	unsigned char *p = (unsigned char *)&ip.s_addr;
 	static char buf[18];
-#if WORDS_BIGENDIAN
 	slprintf(buf, 17, "%d.%d.%d.%d", 
 		 (int)p[0], (int)p[1], (int)p[2], (int)p[3]);
-#else /* WORDS_BIGENDIAN */
-	slprintf(buf, 17, "%d.%d.%d.%d", 
-		 (int)p[3], (int)p[2], (int)p[1], (int)p[0]);
-#endif /* WORDS_BIGENDIAN */
 	return buf;
 }
 #endif /* REPLACE_INET_NTOA */
