e6e3b79f74fb17c34adda153e86ccf2f103bc869
Simo Sorce <idra@samba.org>
2002-12-09 12:06:15 +0000

    make vfs recycle use vfs facilities correctly!
    WARNING: every vfs module maintainer should look at these changes to understand how to properly write a vfs module in head!!
    
    also make installmodules work correctly when the directory structure is not yet in place.
    
    Simo.
    (This used to be commit 6dfa46098679681a6d2c931395d6eeae2a978466)

--- source3/script/installmodules.sh
+++ source3/script/installmodules.sh
@@ -7,6 +7,16 @@ shift
 shift
 shift
 
+for d in $BASEDIR $LIBDIR; do
+if [ ! -d $d ]; then
+mkdir $d
+if [ ! -d $d ]; then
+  echo Failed to make directory $d
+  exit 1
+fi
+fi
+done
+
 for p in $*; do
  p2=`basename $p`
  echo Installing $p as $LIBDIR/$p2
