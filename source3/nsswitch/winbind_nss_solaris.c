6aeddce85eb23fef4320fdd3a8b5ce9e72f9754a
Jeremy Allison <jra@samba.org>
2003-01-31 18:33:35 +0000

    Ensure Luke Howard's (C) is added. Jeremy. (This used to be commit 4af9e8052186f0d2e5ef10879990ec92dc7c5feb)

--- source3/nsswitch/winbind_nss_solaris.c
+++ source3/nsswitch/winbind_nss_solaris.c
@@ -1,9 +1,29 @@
 /*
   Solaris NSS wrapper for winbind 
   - Shirish Kalele 2000
-
+  
   Based on Luke Howard's ldap_nss module for Solaris 
   */
+
+/*
+  Copyright (C) 1997-2003 Luke Howard.
+  This file is part of the nss_ldap library.
+
+  The nss_ldap library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General Public License as
+  published by the Free Software Foundation; either version 2 of the
+  License, or (at your option) any later version.
+
+  The nss_ldap library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Library General Public License for more details.
+
+  You should have received a copy of the GNU Library General Public
+  License along with the nss_ldap library; see the file COPYING.LIB.  If not,
+  write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+  Boston, MA 02111-1307, USA.
+*/
 
 #include <stdlib.h>
 #include <sys/types.h>
