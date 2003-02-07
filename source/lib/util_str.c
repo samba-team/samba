de306bde5e7a7d39ec72a2d471ceb35feb4ffb6b
Jeremy Allison <jra@samba.org>
2003-02-07 06:16:49 +0000

    Merge tridge's changes from HEAD. No CR#. Jeremy.

--- source/lib/util_str.c
+++ source/lib/util_str.c
@@ -958,9 +958,11 @@ char *safe_strcat(char *dest, const char *src, size_t maxlength)
 	if (src_len + dest_len > maxlength) {
 		DEBUG(0,("ERROR: string overflow by %d in safe_strcat [%.50s]\n",
 			(int)(src_len + dest_len - maxlength), src));
-		if (dest_len >= maxlength)
-			return dest;
-		src_len = maxlength - dest_len;
+		if (maxlength > dest_len) {
+			memcpy(&dest[dest_len], src, maxlength - dest_len);
+		}
+		dest[maxlength] = 0;
+		return NULL;
 	}
       
 	memcpy(&dest[dest_len], src, src_len);
