2ef72f95003cebf2ff56e47f16fb8be0e8032042
Andrew Tridgell <tridge@samba.org>
2003-02-11 22:53:57 +0000

    removed a duplicate copy of smb_io_sid_array()

--- source/rpc_parse/parse_misc.c
+++ source/rpc_parse/parse_misc.c
@@ -1194,52 +1194,6 @@ BOOL smb_io_sid_array(const char *desc, SID_ARRAY *array, prs_struct *ps, int de
 	return True;
 }
 
-
-/*******************************************************************
- Reads or writes a SID_ARRAY structure.
-********************************************************************/
-BOOL smb_io_sid_array(const char *desc, SID_ARRAY *array, prs_struct *ps, int depth)
-{
-	int i;
-
-	prs_debug(ps, depth, desc, "smb_io_sid_array");
-	depth++;
-
-	if(!prs_uint32("ref_id", ps, depth, &array->ref_id))
-		return False;
-
-	if (! array->ref_id) {
-		return True;
-	}
-
-	if(!prs_uint32("count", ps, depth, &array->count))
-		return False;
-
-	if (array->count == 0) {
-		return True;
-	}
-
-	if (UNMARSHALLING(ps)) {
-		array->sids = talloc_zero(get_talloc_ctx(), array->count * sizeof(array->sids[0]));
-	}
-	if (! array->sids) {
-		return False;
-	}
-
-	for (i=0;i<array->count;i++) {
-		if(!prs_uint32("ref_id", ps, depth, &array->sids[i].ref_id))
-			return False;
-	}
-
-	for (i=0;i<array->count;i++) {
-		if (!smb_io_dom_sid2("sid", &array->sids[i].sid, ps, depth)) 
-			return False;
-	}
-	
-	return True;
-}
-
-
 /*******************************************************************
  Inits a DOM_RID2 structure.
 ********************************************************************/
