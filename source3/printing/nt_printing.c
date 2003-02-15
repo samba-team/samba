940fcdf09575788781e0b92b9080fff527363fd4
Gerald Carter <jerry@samba.org>
2003-02-15 23:33:30 +0000

    * set PRINTER_ATTRIBUTE_RAW_ONLY; CR 1736 * never save a pointer to an automatic variable (they go away)   implement a deep copy for SPOOLSS_NOTIFY_MSG to correct   messages being sent that have junk for strings;   fix in response to changes for CR 1504 (This used to be commit ffda9e2480414c7ed6156958f516e0d1f3c61350)

--- source3/printing/nt_printing.c
+++ source3/printing/nt_printing.c
@@ -3132,7 +3132,7 @@ static WERROR get_a_printer_2_default(NT_PRINTER_INFO_LEVEL_2 **info_ptr, fstrin
 	fstrcpy(info.printprocessor, "winprint");
 	fstrcpy(info.datatype, "RAW");
 
-	info.attributes = PRINTER_ATTRIBUTE_SHARED | PRINTER_ATTRIBUTE_NETWORK;      /* attributes */
+	info.attributes = PRINTER_ATTRIBUTE_SAMBA;
 
 	info.starttime = 0; /* Minutes since 12:00am GMT */
 	info.untiltime = 0; /* Minutes since 12:00am GMT */
@@ -3224,7 +3224,7 @@ static WERROR get_a_printer_2(NT_PRINTER_INFO_LEVEL_2 **info_ptr, fstring sharen
 			info.parameters);
 
 	/* Samba has to have shared raw drivers. */
-	info.attributes |= (PRINTER_ATTRIBUTE_SHARED | PRINTER_ATTRIBUTE_NETWORK); 
+	info.attributes |= PRINTER_ATTRIBUTE_SAMBA;
 
 	/* Restore the stripped strings. */
 	slprintf(info.servername, sizeof(info.servername)-1, "\\\\%s", get_called_name());
