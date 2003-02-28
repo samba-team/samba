37e39f5be4ae0156565b009ae210f2c95e906142
Martin Pool <mbp@samba.org>
2003-02-28 03:34:18 +0000

    Add simple test harness that exposes StrCaseCmp for testing. (This used to be commit ab77beca041595bc3bd70de3a6e1b49159a4964b)

---
+++ source3/torture/t_strcmp.c
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2003 by Martin Pool
+ *
+ * Test harness for StrCaseCmp
+ */
+
+#include "includes.h"
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3) {
+		fprintf(stderr, "usage: %s STRING1 STRING2\nCompares two strings\n",
+			argv[0]);
+		return 2;
+	}
+	
+	printf("%d\n", StrCaseCmp(argv[1], argv[2]));
+	
+	return 0;
+}
