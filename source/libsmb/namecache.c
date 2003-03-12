1a16eaa38557e2f9b6e268ae1a8a5d30550cbd48
Jeremy Allison <jra@samba.org>
2003-03-12 00:53:23 +0000

    Janitor for tpot's memleak fix. Jeremy.

--- source/libsmb/namecache.c
+++ source/libsmb/namecache.c
@@ -161,7 +161,7 @@ BOOL namecache_fetch(const char *name, int name_type, struct in_addr **ip_list,
 		     int *num_names)
 {
 	TDB_DATA key, value;
-	struct nc_value *data;
+	struct nc_value *data = NULL;
 	time_t now;
 	int i;
 
@@ -196,6 +196,7 @@ BOOL namecache_fetch(const char *name, int name_type, struct in_addr **ip_list,
 
 		tdb_delete(namecache_tdb, key);
 
+		SAFE_FREE(value.dptr);
 		value = tdb_null;
 
 		goto done;
@@ -210,6 +211,7 @@ BOOL namecache_fetch(const char *name, int name_type, struct in_addr **ip_list,
 
 		tdb_delete(namecache_tdb, key);
 
+		SAFE_FREE(value.dptr);
 		value = tdb_null;
 
 		goto done;
