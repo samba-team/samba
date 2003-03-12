17f01f6959fb79ba11a56d6869ec9789e3ee2f74
Martin Pool <mbp@samba.org>
2003-03-12 02:58:45 +0000

    Update usage message.

--- source/torture/t_strcmp.c
+++ source/torture/t_strcmp.c
@@ -9,7 +9,8 @@
 int main(int argc, char *argv[])
 {
 	if (argc != 3) {
-		fprintf(stderr, "usage: %s STRING1 STRING2\nCompares two strings\n",
+		fprintf(stderr, "usage: %s STRING1 STRING2\n"
+			"Compares two strings, prints the results of StrCaseCmp\n",
 			argv[0]);
 		return 2;
 	}
