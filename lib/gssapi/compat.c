81370af455c2c189b56c90742391a1a045bc8e6f
Love Hörnquist Åstrand <lha@kth.se>
2003-03-16 17:49:43 +0000

    (check_compat): make sure ret have a defined value
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@11750 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/gssapi/compat.c
+++ lib/gssapi/compat.c
@@ -40,7 +40,7 @@ static krb5_error_code
 check_compat(OM_uint32 *minor_status, gss_name_t name, 
 	     const char *option, krb5_boolean *compat)
 {
-    krb5_error_code ret;
+    krb5_error_code ret = 0;
     char **p, **q;
     krb5_principal match;
 
