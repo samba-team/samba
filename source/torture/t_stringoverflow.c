cea126f62ad411f5efbebc7c5d39297fd8ef9efb
Martin Pool <mbp@samba.org>
2003-03-18 05:31:52 +0000

    The new string macros catch a bug at compile that previously only trapped at runtime, which is great.  So we have to try a little harder to provoke an overflow -- which is still caught nicely in developer mode.

--- source/torture/t_stringoverflow.c
+++ source/torture/t_stringoverflow.c
@@ -3,6 +3,7 @@
  int main(void)
 {
 	fstring dest;
+	char *ptr = dest;
 
 	printf("running on valgrind? %d\n", RUNNING_ON_VALGRIND);
 
@@ -10,7 +11,13 @@
 	 * will actually fit, but this is still wrong because you
 	 * can't pstrcpy into an fstring.  This should trap in a
 	 * developer build. */
+
+#if 0
+	/* As of CVS 20030318, this will be trapped at compile time! */
 	pstrcpy(dest, "hello");
+#endif /* 0 */
+
+	pstrcpy(ptr, "hello!");
 
 	return 0;
 }
