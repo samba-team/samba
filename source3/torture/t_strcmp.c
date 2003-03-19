54925dcc6061f4aeb9f05b10c607065a0e511d9e
Martin Pool <mbp@samba.org>
2003-03-19 02:59:18 +0000

    Run test 10000 times to make measurement easier. (This used to be commit e051789ac6cc05f8a643ea81e887d3b969cf3a27)

--- source3/torture/t_strcmp.c
+++ source3/torture/t_strcmp.c
@@ -8,14 +8,19 @@
 
 int main(int argc, char *argv[])
 {
+	int i, ret;
+	
 	if (argc != 3) {
 		fprintf(stderr, "usage: %s STRING1 STRING2\n"
 			"Compares two strings, prints the results of StrCaseCmp\n",
 			argv[0]);
 		return 2;
 	}
-	
-	printf("%d\n", StrCaseCmp(argv[1], argv[2]));
+
+	for (i = 0; i < 10000; i++)
+		ret = StrCaseCmp(argv[1], argv[2]);
+
+	printf("%d\n", ret);
 	
 	return 0;
 }
