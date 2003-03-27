6026e47cfe493625c6ed017ecae3cc785a6d0347
Alexander Bokovoy <ab@samba.org>
2003-03-27 18:28:55 +0000

    1. Fix  generate-file-list.sh to produce a list sorted by parameter name, not parameter section. 2. Convert base parameters to new meta-information scheme

--- docs/docbook/smbdotconf/generate-file-list.sh
+++ docs/docbook/smbdotconf/generate-file-list.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "<variablelist>"
-find . -type f -name '*.xml' -mindepth 2 | sort |
+find . -type f -name '*.xml' -mindepth 2 | sort -t/ -k3 |
         while read ; do  
                 echo "<xi:include href='$REPLY' parse='xml' xmlns:xi='http://www.w3.org/2001/XInclude'/>"
         done
