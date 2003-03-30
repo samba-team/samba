089383ba6b1ac8489639d466f2ea892614df0c4f
Gerald Carter <jerry@samba.org>
2003-03-30 23:04:17 +0000

    update copyright notice since it we are now almost 4 months into 2003

--- source/nmbd/nmbd.c
+++ source/nmbd/nmbd.c
@@ -793,7 +793,7 @@ static void usage(char *pname)
   reopen_logs();
 
   DEBUG( 0, ( "Netbios nameserver version %s started.\n", VERSION ) );
-  DEBUGADD( 0, ( "Copyright Andrew Tridgell and the Samba Team 1994-2002\n" ) );
+  DEBUGADD( 0, ( "Copyright Andrew Tridgell and the Samba Team 1994-2003\n" ) );
 
   if ( !reload_nmbd_services(False) )
     return(-1);
