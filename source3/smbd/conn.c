96b5d5bdfdfe368cb1a84c9b25b2de97c1370af0
Jelmer Vernooij <jelmer@samba.org>
2003-03-31 15:06:34 +0000

    - Support absolute paths in vfs and charset modules - Fix typo in Makefile.in - Fix compatibility with older vfs modules (from patch by metze) - Build some modules shared by default and some static (and fall   back to static when dlopen() is not available) (This used to be commit aa36f462d95f8a3a3a81a89c210b98a6f9fd295f)

--- source3/smbd/conn.c
+++ source3/smbd/conn.c
@@ -201,10 +201,11 @@ void conn_free(connection_struct *conn)
 	/* Free vfs_connection_struct */
 	handle = conn->vfs_private;
 	while(handle) {
+		/* Only call dlclose for the old modules */
 		if (handle->handle) {
 			/* Close dlopen() handle */
 			done_fptr = (void (*)(connection_struct *))sys_dlsym(handle->handle, "vfs_done");
-			
+
 			if (done_fptr == NULL) {
 				DEBUG(3, ("No vfs_done() symbol found in module with handle %p, ignoring\n", handle->handle));
 			} else {
@@ -212,7 +213,6 @@ void conn_free(connection_struct *conn)
 			}
 			sys_dlclose(handle->handle);
 		}
-
 		DLIST_REMOVE(conn->vfs_private, handle);
 		thandle = handle->next;
 		SAFE_FREE(handle);
