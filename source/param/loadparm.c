e99a40a711fad336305bc06beb62ba5668d0ed78
Volker Lendecke <vlendec@samba.org>
2003-04-11 16:32:39 +0000

    Set the default schannel params to False until we know more.
    Volker

--- source/param/loadparm.c
+++ source/param/loadparm.c
@@ -1356,8 +1356,8 @@ static void init_globals(void)
 	Globals.paranoid_server_security = True;
 	Globals.bEncryptPasswords = True;
 	Globals.bUpdateEncrypt = False;
-	Globals.clientSchannel = Auto;
-	Globals.serverSchannel = Auto;
+	Globals.clientSchannel = False;
+	Globals.serverSchannel = False;
 	Globals.bReadRaw = True;
 	Globals.bWriteRaw = True;
 	Globals.bReadPrediction = False;
