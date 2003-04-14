63a1b7637f11688f36ca8302714e1591927c51e4
Martin Pool <mbp@samba.org>
2003-04-14 05:00:00 +0000

    Merge test cases from rsync. (This used to be commit a0d841a59ea745bf48b0d705ad6c81e4172552ea)

--- source3/lib/snprintf.c
+++ source3/lib/snprintf.c
@@ -873,9 +873,9 @@ static void dopr_outch(char *buffer, size_t *currlen, size_t maxlen, char c)
 		"-16.16f",
 		NULL
 	};
-	/* XXX: What does "0203.9" even mean?  An octal floating point number??? */
-	double fp_nums[] = { 6442452944.1234, -1.5, 134.21, 91340.2, 341.1234, 0203.9, 0.96, 0.996, 
-			     0.9996, 1.996, 4.136, 5.030201, 0, 0.00205};
+	double fp_nums[] = { 6442452944.1234, -1.5, 134.21, 91340.2, 341.1234, 203.9, 0.96, 0.996, 
+			     0.9996, 1.996, 4.136, 5.030201, 0.00205,
+			     /* END LIST */ 0};
 	char *int_fmt[] = {
 		"%-1.5d",
 		"%1.5d",
