d9a34fec6582733ae01dcb3b0b8fa408c23194aa
Jelmer Vernooij <jelmer@samba.org>
2003-04-29 14:09:50 +0000

    Change order of parameters to smb_register_passdb() (This used to be commit 63cf5637c16dc770f0ab2f73ff00577c7cd426ee)

--- source3/passdb/pdb_unix.c
+++ source3/passdb/pdb_unix.c
@@ -127,5 +127,5 @@ NTSTATUS pdb_init_unixsam(PDB_CONTEXT *pdb_context, PDB_METHODS **pdb_method, co
 
 NTSTATUS pdb_unix_init(void)
 {
-	return smb_register_passdb("unixsam", pdb_init_unixsam, PASSDB_INTERFACE_VERSION);
+	return smb_register_passdb(PASSDB_INTERFACE_VERSION, "unixsam", pdb_init_unixsam);
 }
