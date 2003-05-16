1480c7e8c7a84c34181118c449c50ca99fdcbc6b
Simo Sorce <idra@samba.org>
2003-05-16 06:20:57 +0000

    add metze's patch for smb_register functions

--- source/smbd/vfs.c
+++ source/smbd/vfs.c
@@ -135,12 +135,9 @@ static struct vfs_ops default_vfs = {
 static struct vfs_init_function_entry *vfs_find_backend_entry(const char *name)
 {
 	struct vfs_init_function_entry *entry = backends;
-	pstring stripped;
-
-	module_path_get_name(name, stripped);
  
 	while(entry) {
-		if (strequal(entry->name, stripped)) return entry;
+		if (strcmp(entry->name, name)==0) return entry;
 		entry = entry->next;
 	}
 
