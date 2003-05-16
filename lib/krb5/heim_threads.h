ec8cfb3623e2f0ff19fd2fb9649914f5765d1d6e
Love Hörnquist Åstrand <lha@kth.se>
2003-05-16 22:23:48 +0000

    add missing argument to mutex_init
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@12308 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/krb5/heim_threads.h
+++ lib/krb5/heim_threads.h
@@ -58,7 +58,7 @@
 
 #define HEIMDAL_MUTEX mutex_t
 #define HEIMDAL_MUTEX_INITIALIZER MUTEX_INITIALIZER
-#define HEIMDAL_MUTEX_init(m) mutex_init(m)
+#define HEIMDAL_MUTEX_init(m) mutex_init(m, NULL)
 #define HEIMDAL_MUTEX_lock(m) mutex_lock(m)
 #define HEIMDAL_MUTEX_unlock(m) mutex_unlock(m)
 #define HEIMDAL_MUTEX_destroy(m) mutex_destroy(m)
@@ -70,7 +70,7 @@
 
 #define HEIMDAL_MUTEX pthread_mutex_t
 #define HEIMDAL_MUTEX_INITIALIZER PTHREAD_MUTEX_INITIALIZER
-#define HEIMDAL_MUTEX_init(m) pthread_mutex_init(m)
+#define HEIMDAL_MUTEX_init(m) pthread_mutex_init(m, NULL)
 #define HEIMDAL_MUTEX_lock(m) pthread_mutex_lock(m)
 #define HEIMDAL_MUTEX_unlock(m) pthread_mutex_unlock(m)
 #define HEIMDAL_MUTEX_destroy(m) pthread_mutex_destroy(m)
