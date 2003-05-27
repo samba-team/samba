a6cb33c5bf0e26d8092c06a2cc01a6678109fb68
Tim Potter <tpot@samba.org>
2003-05-27 00:16:43 +0000

    Merge from 3.0: Fix set_local_machine_name() for both *SMBSERVER and *SMBSERV as per comments in loadparm.c

--- source/lib/substitute.c
+++ source/lib/substitute.c
@@ -40,6 +40,17 @@ void set_local_machine_name(const char* local_name, BOOL perm)
 	static BOOL already_perm = False;
 	fstring tmp_local_machine;
 
+	/*
+	 * Windows NT/2k uses "*SMBSERVER" and XP uses "*SMBSERV"
+	 * arrggg!!! 
+	 */
+
+	if (strcasecmp(local_name, "*SMBSERVER")==0) 
+		return;
+
+	if (strcasecmp(local_name, "*SMBSERV")==0) 
+		return;
+
 	if (already_perm)
 		return;
 
