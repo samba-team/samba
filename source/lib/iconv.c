30a1b517fca6554c3beb5cc33fa0299d1376a542
Tim Potter <tpot@samba.org>
2003-05-27 00:09:34 +0000

    Jelmer was really excited about copyrights this year.

--- source/lib/iconv.c
+++ source/lib/iconv.c
@@ -2,7 +2,7 @@
    Unix SMB/CIFS implementation.
    minimal iconv implementation
    Copyright (C) Andrew Tridgell 2001
-   Copyright (C) Jelmer Vernooij 2002,2003,2003,2003,2003
+   Copyright (C) Jelmer Vernooij 2002,2003
    
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
