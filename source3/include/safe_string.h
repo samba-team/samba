d3688454185adc97bcdfd821b8c2641163aa392a
Andrew Tridgell <tridge@samba.org>
2003-06-11 05:34:14 +0000

    on AIX FD_ZERO() is defined in terms of bzero(), so we can't have the "don't use bzero" macros.
    In general I think it would be better to have a separate script that
    checks for deprecated functions like these using grep rather than
    using these cpp tricks. They just get us into trouble.
    (This used to be commit 2a227c880db0f233fb1f6dae5851450ea6020f3b)

--- source3/include/safe_string.h
+++ source3/include/safe_string.h
@@ -32,11 +32,6 @@
 #endif /* bcopy */
 #define bcopy(src,dest,size) __ERROR__XX__NEVER_USE_BCOPY___;
 
-#ifdef bzero
-#undef bzero
-#endif /* bzero */
-#define bzero(dest,size) __ERROR__XX__NEVER_USE_BZERO_USE_MEMSET_INSTEAD___;
-
 #ifdef strcpy
 #undef strcpy
 #endif /* strcpy */
