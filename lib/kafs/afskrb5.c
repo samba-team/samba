b5984d2675afc92d0878ded76a39f4b3e71c80a2
Love Hörnquist Åstrand <lha@kth.se>
2003-06-21 22:31:09 +0000

    (v5_convert): rename one of the two c to cred4
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@12385 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/kafs/afskrb5.c
+++ lib/kafs/afskrb5.c
@@ -135,16 +135,16 @@ v5_convert(krb5_context context, krb5_ccache id,
     else if(strcasecmp(val, "yes") == 0 ||
 	    strcasecmp(val, "true") == 0 ||
 	    atoi(val)) {
-	struct credentials c;
+	struct credentials cred4;
 	
 	if (id == NULL)
-	    ret = krb524_convert_creds_kdc(context, cred, &c);
+	    ret = krb524_convert_creds_kdc(context, cred, &cred4);
 	else
-	    ret = krb524_convert_creds_kdc_ccache(context, id, cred, &c);
+	    ret = krb524_convert_creds_kdc_ccache(context, id, cred, &cred4);
 	if (ret)
 	    goto out;
 
-	ret = _kafs_v4_to_kt(&c, uid, kt);
+	ret = _kafs_v4_to_kt(&cred4, uid, kt);
     } else 
 	ret = v5_to_kt(cred, uid, kt, 0);
 
