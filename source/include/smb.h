6956eb9a0b878f6fae37e4de14573cccd2af2156
John Terpstra <jht@samba.org>
2003-07-28 05:49:51 +0000

    Corrected description of SWAT FLAGS since they have changed as a result of the cleanup of loadparm and swat.c

--- source/include/smb.h
+++ source/include/smb.h
@@ -705,15 +705,14 @@ struct bitmap {
 	unsigned int n;
 };
 
-/* Note: Parameters flagged as basic will be visible in all views. */
-/*       Parameters NOT flagged as basic will not be preset to default value is not set in smb.conf */
-#define FLAG_BASIC 	0x0001 /* fundamental options preset to default value if not set in smb.conf */
+/* The following flags are used in SWAT */
+#define FLAG_BASIC 	0x0001 /* Display only in BASIC view */
 #define FLAG_SHARE 	0x0002 /* file sharing options */
 #define FLAG_PRINT 	0x0004 /* printing options */
 #define FLAG_GLOBAL 	0x0008 /* local options that should be globally settable in SWAT */
 #define FLAG_WIZARD 	0x0010 /* Parameters that the wizard will operate on */
-#define FLAG_ADVANCED 	0x0020 /* Parameters that will be visible in advanced view and developer view */
-#define FLAG_DEVELOPER 	0x0040 /* Parameters that will be visible only in developer view */
+#define FLAG_ADVANCED 	0x0020 /* Parameters that will be visible in advanced view */
+#define FLAG_DEVELOPER 	0x0040 /* No longer used */
 #define FLAG_DEPRECATED 0x1000 /* options that should no longer be used */
 #define FLAG_HIDE  	0x2000 /* options that should be hidden in SWAT */
 #define FLAG_DOS_STRING 0x4000 /* convert from UNIX to DOS codepage when reading this string. */
