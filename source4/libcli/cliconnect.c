a59229614ebeb17f3e4db2edcb70b71f7bf31982
Herb Lewis <herb@samba.org>
2003-08-15 21:37:42 +0000

    don't dereference null pointer (This used to be commit ba5d1cde98f9146ffc889ac89ec79331e2bfae18)

--- source4/libcli/cliconnect.c
+++ source4/libcli/cliconnect.c
@@ -211,7 +211,7 @@ struct cli_state *cli_state_init(void)
 ****************************************************************************/
 void cli_shutdown(struct cli_state *cli)
 {
-	if (!cli) return;
+	if (!cli || !cli->tree) return;
 	cli->tree->reference_count++;
 	cli_tree_close(cli->tree);
 	if (cli->mem_ctx) {
