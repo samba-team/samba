95b1d4933b0de63613fe041c273d413d86909b4b
Jelmer Vernooij <jelmer@samba.org>
2003-08-15 00:04:20 +0000

    Add utility that takes a samba log file (at least level 5 for headers and 10 for data contents as well) and creates a packet trace readable by ethereal.
    What does not work yet:
     - SMB data contents (log level 5)
     - SMB data contents beyond the 512 byte range (log level 99 or something?)

--- source/utils/log2pcaphex.c
+++ source/utils/log2pcaphex.c
@@ -1,25 +1,25 @@
 /* 
-Unix SMB/CIFS implementation.
-Utility to extract pcap files from samba (log level 10) log files
+   Unix SMB/CIFS implementation.
+   Utility to extract pcap files from samba (log level 10) log files
 
-Copyright (C) Jelmer Vernooij 2003
-Thanks to Tim Potter for the genial idea
+   Copyright (C) Jelmer Vernooij 2003
+   Thanks to Tim Potter for the genial idea
 
-Example use: log2pcaphex < samba-log-file | text2pcap -T 139,139 - foo.pcap
+   Example use: log2pcaphex < samba-log-file | text2pcap -T 139,139 - foo.pcap
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
 #include "includes.h"
