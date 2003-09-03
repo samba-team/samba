8b66e46474db2a54559f9cdda0a0fcb80353854a
Love Hörnquist Åstrand <lha@kth.se>
2003-09-03 11:47:30 +0000

    s/des_read_pw_string/UI_UTIL_read_pw_string/
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@12759 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- kadmin/ank.c
+++ kadmin/ank.c
@@ -129,7 +129,7 @@ add_one_principal (const char *name,
 	krb5_unparse_name(context, princ_ent, &princ_name);
 	asprintf (&prompt, "%s's Password: ", princ_name);
 	free (princ_name);
-	ret = des_read_pw_string (pwbuf, sizeof(pwbuf), prompt, 1);
+	ret = UI_UTIL_read_pw_string (pwbuf, sizeof(pwbuf), prompt, 1);
 	free (prompt);
 	if (ret)
 	    goto out;
