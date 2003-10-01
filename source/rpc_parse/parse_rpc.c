641652cad97b761ba11d4e89b7c9ad098c7dd1c2
Andrew Bartlett <abartlet@samba.org>
2003-10-01 23:04:34 +0000

    Remove duplicate function (now in rpc_parse/parse_prs.c) and fix a RPC debug (I renamed the element of the structure).
    Andrew Bartlett

--- source/rpc_parse/parse_rpc.c
+++ source/rpc_parse/parse_rpc.c
@@ -1168,26 +1168,6 @@ BOOL smb_io_rpc_auth_netsec_neg(const char *desc, RPC_AUTH_NETSEC_NEG *neg,
 	return True;
 }
 
-
-/*******************************************************************
-creates an RPC_AUTH_NETSEC_CHK structure.
-********************************************************************/
-BOOL init_rpc_auth_netsec_chk(RPC_AUTH_NETSEC_CHK * chk,
-			      const uchar sig[8],
-			      const uchar packet_digest[8],
-			      const uchar seq_num[8], const uchar confounder[8])
-{
-	if (chk == NULL)
-		return False;
-
-	memcpy(chk->sig, sig, sizeof(chk->sig));
-	memcpy(chk->packet_digest, packet_digest, sizeof(chk->packet_digest));
-	memcpy(chk->seq_num, seq_num, sizeof(chk->seq_num));
-	memcpy(chk->confounder, confounder, sizeof(chk->confounder));
-
-	return True;
-}
-
 /*******************************************************************
 reads or writes an RPC_AUTH_NETSEC_CHK structure.
 ********************************************************************/
@@ -1203,7 +1183,7 @@ BOOL smb_io_rpc_auth_netsec_chk(const char *desc, RPC_AUTH_NETSEC_CHK * chk,
 	prs_uint8s(False, "sig  ", ps, depth, chk->sig, sizeof(chk->sig));
 	prs_uint8s(False, "seq_num", ps, depth, chk->seq_num, sizeof(chk->seq_num));
 	prs_uint8s(False, "packet_digest", ps, depth, chk->packet_digest, sizeof(chk->packet_digest));
-	prs_uint8s(False, "data8", ps, depth, chk->confounder, sizeof(chk->confounder));
+	prs_uint8s(False, "confounder", ps, depth, chk->confounder, sizeof(chk->confounder));
 
 	return True;
 }
