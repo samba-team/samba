820903ef5a062b4b9824c33ee035c68a39c8eeb0
Gerald Carter <jerry@samba.org>
2003-10-10 16:21:39 +0000

    removing docs from HEAD

--- docs/docbook/smbdotconf/generate-file-list.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo "<variablelist>"
-for I in `find . -type f -name '*.xml' -mindepth 2 | sort -t/ -k3 | xargs`
-do 
-	echo "<xi:include href='$I' parse='xml' xmlns:xi='http://www.w3.org/2001/XInclude'/>"
-done
-                
-echo "</variablelist>"
