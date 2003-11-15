ab5939e79600841c5e7e31e9da8742bf2da4a184
Andrew Tridgell <tridge@samba.org>
2003-11-15 21:25:03 +0000

    simple method for auto-building rpc files if idl changes, and auto-building proto.h if it doesn't exist. This should make life a little easier for developers

--- source/script/build_idl.sh
+++ source/script/build_idl.sh
@@ -1,10 +1,14 @@
-#!/bin/bash
+#!/bin/sh
+
+FULLBUILD=$1
 
 for f in librpc/idl/*.idl; do
-    echo Processing $f
     base=`basename $f .idl`
     ndr=librpc/ndr/ndr_$base
-    pidl.pl --output $ndr --parse --header --parser --client librpc/rpc/rpc_$base.c $f || exit 1
+    if [ x$FULLBUILD = xFULL -o "$f" -nt $ndr.c ]; then
+      echo Processing $f
+      pidl.pl --output $ndr --parse --header --parser --client librpc/rpc/rpc_$base.c $f || exit 1
+    fi
 done
 
 exit 0
