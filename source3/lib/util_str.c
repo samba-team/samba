a63010bae7710a4d24ebbae5f232fa792f005112
Rafal Szczesniak <mimir@samba.org>
2003-11-19 22:56:02 +0000

    Added useful information to debug lines. Patch by metze.
    rafal
    (This used to be commit 2eef3c7bc182bb2c0c483190570ee1a297047ad2)

--- source3/lib/util_str.c
+++ source3/lib/util_str.c
@@ -574,7 +574,7 @@ char *safe_strcpy_fn(const char *fn, int line, char *dest,const char *src, size_
 	size_t len;
 
 	if (!dest) {
-		DEBUG(0,("ERROR: NULL dest in safe_strcpy\n"));
+		DEBUG(0,("ERROR: NULL dest in safe_strcpy, called from [%s][%d]\n", fn, line));
 		return NULL;
 	}
 
@@ -610,7 +610,7 @@ char *safe_strcat_fn(const char *fn, int line, char *dest, const char *src, size
 	size_t src_len, dest_len;
 
 	if (!dest) {
-		DEBUG(0,("ERROR: NULL dest in safe_strcat\n"));
+		DEBUG(0,("ERROR: NULL dest in safe_strcat, called from [%s][%d]\n", fn, line));
 		return NULL;
 	}
 
@@ -654,7 +654,7 @@ char *alpha_strcpy_fn(const char *fn, int line, char *dest, const char *src, con
 #endif
 
 	if (!dest) {
-		DEBUG(0,("ERROR: NULL dest in alpha_strcpy\n"));
+		DEBUG(0,("ERROR: NULL dest in alpha_strcpy, called from [%s][%d]\n", fn, line));
 		return NULL;
 	}
 
@@ -696,7 +696,7 @@ char *StrnCpy_fn(const char *fn, int line,char *dest,const char *src,size_t n)
 #endif
 
 	if (!dest) {
-		DEBUG(0,("ERROR: NULL dest in StrnCpy\n"));
+		DEBUG(0,("ERROR: NULL dest in StrnCpy, called from [%s][%d]\n", fn, line));
 		return(NULL);
 	}
 
