670c0cbe034e1e2ac5aed751b6c4fbd925fe3221
Andrew Tridgell <tridge@samba.org>
2003-11-19 05:53:05 +0000

    change to AuditEventsInfo struct from lukeh

--- source/librpc/ndr/ndr_lsa.h
+++ source/librpc/ndr/ndr_lsa.h
@@ -187,13 +187,12 @@ struct lsa_AuditLogInfo {
 };
 
 struct lsa_AuditSettings {
-	uint32 count;
-	uint32 *settings;
 };
 
 struct lsa_AuditEventsInfo {
 	uint32 auditing_mode;
-	struct lsa_AuditSettings *settings;
+	uint32 *settings;
+	uint32 count;
 };
 
 struct lsa_DomainInfo {
