2ed87a52ccc343c281e29f5a18f3f99fe06beb28
Andrew Tridgell <tridge@samba.org>
2003-11-20 22:46:01 +0000

    we now use a copy of pidl inside the samba4 source tree at source/build/pidl/
    I imported the code using direct respository manipulation to preserve
    the commit history

--- source/script/build_idl.sh
+++ source/script/build_idl.sh
@@ -5,12 +5,14 @@ FULLBUILD=$1
 [ -d librpc/gen_ndr ] || mkdir -p librpc/gen_ndr || exit 1
 [ -d librpc/gen_rpc ] || mkdir -p librpc/gen_rpc || exit 1
 
+( cd build/pidl && make ) || exit 1
+
 for f in librpc/idl/*.idl; do
     base=`basename $f .idl`
     ndr=librpc/gen_ndr/ndr_$base
     if [ x$FULLBUILD = xFULL -o "$f" -nt $ndr.c ]; then
       echo Processing $f
-      pidl.pl --output $ndr --parse --header --parser --client librpc/gen_rpc/rpc_$base.c $f || exit 1
+      build/pidl/pidl.pl --output $ndr --parse --header --parser --client librpc/gen_rpc/rpc_$base.c $f || exit 1
     fi
 done
 
