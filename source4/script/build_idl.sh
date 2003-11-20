b0083691759c9d0561a5e9d436d927a44c82b388
Andrew Tridgell <tridge@samba.org>
2003-11-20 11:09:53 +0000

    moved the pidl auto-generated files out of CVS (This used to be commit 49c72d42766a55e2833c004e721a47115000626b)

--- source4/script/build_idl.sh
+++ source4/script/build_idl.sh
@@ -2,12 +2,15 @@
 
 FULLBUILD=$1
 
+[ -d librpc/gen_ndr ] || mkdir -p librpc/gen_ndr || exit 1
+[ -d librpc/gen_rpc ] || mkdir -p librpc/gen_rpc || exit 1
+
 for f in librpc/idl/*.idl; do
     base=`basename $f .idl`
-    ndr=librpc/ndr/ndr_$base
+    ndr=librpc/gen_ndr/ndr_$base
     if [ x$FULLBUILD = xFULL -o "$f" -nt $ndr.c ]; then
       echo Processing $f
-      pidl.pl --output $ndr --parse --header --parser --client librpc/rpc/rpc_$base.c $f || exit 1
+      pidl.pl --output $ndr --parse --header --parser --client librpc/gen_rpc/rpc_$base.c $f || exit 1
     fi
 done
 
