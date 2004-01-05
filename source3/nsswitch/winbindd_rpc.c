614c18d24bc1bb19c884138c8c16099d1b8484d2
Andrew Bartlett <abartlet@samba.org>
2004-01-05 02:04:37 +0000

    rpc_client/cli_lsarpc.c: rpc_parse/parse_lsa.c: nsswitch/winbindd_rpc.c: nsswitch/winbindd.h:  - Add const
    libads/ads_ldap.c:
     - Cleanup function for use
    
    nsswitch/winbindd_ads.c:
     - Use new utility function ads_sid_to_dn
     - Don't search for 'dn=', rather call the ads_search_retry_dn()
    
    nsswitch/winbindd_ads.c:
    include/rpc_ds.h:
    rpc_client/cli_ds.c:
     - Fixup braindamage in cli_ds_enum_domain_trusts():
        - This function was returning a UNISTR2 up to the caller, and
          was doing nasty (invalid, per valgrind) things with memcpy()
        - Create a new structure that represents this informaiton in a useful way
          and use talloc.
    
    Andrew Bartlett
    (This used to be commit 06c3f15aa166bb567d8be0a8bc4b095b167ab371)

--- source3/nsswitch/winbindd_rpc.c
+++ source3/nsswitch/winbindd_rpc.c
@@ -324,7 +324,7 @@ static NTSTATUS name_to_sid(struct winbindd_domain *domain,
 */
 static NTSTATUS sid_to_name(struct winbindd_domain *domain,
 			    TALLOC_CTX *mem_ctx,
-			    DOM_SID *sid,
+			    const DOM_SID *sid,
 			    char **name,
 			    enum SID_NAME_USE *type)
 {
