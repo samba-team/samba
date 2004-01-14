17518018c0264d2a8e4afbc712a22ef4a1a00003
Andrew Bartlett <abartlet@samba.org>
2004-01-14 01:14:22 +0000

    Finish adding cracklib support - this adds the configure test to enable it, on machines that actually have a working cracklib, for which we have the correct path to the dictionary.
    Andrew Bartlett

--- source/tests/crack.c
+++ source/tests/crack.c
@@ -1,3 +1,10 @@
+#include <crack.h>
+
+#ifndef HAVE_CRACKLIB_DICTPATH
+#ifndef CRACKLIB_DICTPATH
+#define CRACKLIB_DICTPATH SAMBA_CRACKLIB_DICTPATH
+#endif
+#endif
 
 int main(int argc, char **argv) {
 	FascistCheck("Foo", CRACKLIB_DICTPATH);
