902d4a647a88d1def09d5b1eacb06ab1561f3dec
Gerald Carter <jerry@samba.org>
2004-02-02 01:46:30 +0000

    janitor duty (merges from 3.0) and cleanup compiler warning on SuSE 9 in the end mapper code

--- source/nsswitch/winbindd_group.c
+++ source/nsswitch/winbindd_group.c
@@ -1098,7 +1098,7 @@ enum winbindd_result winbindd_getusersids(struct winbindd_cli_state *state)
 	enum winbindd_result result = WINBINDD_ERROR;
 	unsigned int i;
 	TALLOC_CTX *mem_ctx;
-	char *ret;
+	char *ret = NULL;
 	uint32 num_groups;
 	unsigned ofs, ret_size = 0;
 
@@ -1144,7 +1144,7 @@ enum winbindd_result winbindd_getusersids(struct winbindd_cli_state *state)
 	ofs = 0;
 	for (i = 0; i < num_groups; i++) {
 		const char *s = sid_string_static(user_grpsids[i]);
-		safe_strcpy(ret + ofs, s, ret_size - ofs);
+		safe_strcpy(ret + ofs, s, ret_size - ofs - 1);
 		ofs += strlen(ret+ofs) + 1;
 	}
 
