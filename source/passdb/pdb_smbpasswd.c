09839de27c9741051b9ebd751c4adcd1c1481cf8
Stefan Metzmacher <metze@samba.org>
2004-02-11 12:45:47 +0000

    as I renamed the PDB subsystem to PASSDB, we need to rename the static init functions of the modules too:-)
    metze

--- source/passdb/pdb_smbpasswd.c
+++ source/passdb/pdb_smbpasswd.c
@@ -1580,7 +1580,7 @@ static NTSTATUS pdb_init_smbpasswd_nua(PDB_CONTEXT *pdb_context, PDB_METHODS **p
 	return NT_STATUS_OK;
 }
 
-NTSTATUS pdb_smbpasswd_init(void)
+NTSTATUS passdb_smbpasswd_init(void)
 {
 	NTSTATUS ret;
 	struct passdb_ops ops;
