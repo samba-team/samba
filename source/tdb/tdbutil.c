ed26b550b5f24741db6be38542f341be18741e62
Rafal Szczesniak <mimir@samba.org>
2004-02-27 22:11:31 +0000

    Added copyrights I forgot about looong ago...
    rafal

--- source/tdb/tdbutil.c
+++ source/tdb/tdbutil.c
@@ -1,7 +1,8 @@
 /* 
    Unix SMB/CIFS implementation.
    tdb utility functions
-   Copyright (C) Andrew Tridgell 1992-1998
+   Copyright (C) Andrew Tridgell   1992-1998
+   Copyright (C) Rafal Szczesniak  2002
    
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
