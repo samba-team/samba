cd4be77fc72ae8c5848fdad32f0f0e83dccf70e0
Love Hörnquist Åstrand <lha@kth.se>
2004-03-09 17:40:05 +0000

    (fcc_store_cred): use [libdefaults]fcc-mit-ticketflags=boolean to decide what format to write the fcc in. Default to mit version (aka heimdal 0.7)
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@13486 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/krb5/fcache.c
+++ lib/krb5/fcache.c
@@ -405,7 +405,12 @@ fcc_store_cred(krb5_context context,
 	sp = krb5_storage_from_fd(fd);
 	krb5_storage_set_eof_code(sp, KRB5_CC_END);
 	storage_set_flags(context, sp, FCACHE(id)->version);
-	ret = krb5_store_creds(sp, creds);
+	if (krb5_config_get_bool_default(context, NULL, TRUE,
+					 "libdefaults",
+					 "fcc-mit-ticketflags"))
+	    ret = _krb5_store_creds_heimdal_0_7(sp, creds);
+	else
+	    ret = _krb5_store_creds_heimdal_pre_0_7(sp, creds);
 	krb5_storage_free(sp);
     }
     fcc_unlock(context, fd);
