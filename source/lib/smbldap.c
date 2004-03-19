b574099f0ef967f0796c4b84f254c5a9ac893fe1
Gerald Carter <jerry@samba.org>
2004-03-19 14:53:55 +0000

    syncing with 3.0

--- source/lib/smbldap.c
+++ source/lib/smbldap.c
@@ -100,6 +100,7 @@ ATTRIB_MAP_ENTRY attrib_map_v30[] = {
 	{ LDAP_ATTR_MUNGED_DIAL,	"sambaMungedDial"	},
 	{ LDAP_ATTR_BAD_PASSWORD_COUNT,	"sambaBadPasswordCount" },
 	{ LDAP_ATTR_BAD_PASSWORD_TIME,	"sambaBadPasswordTime" 	},
+	{ LDAP_ATTR_MOD_TIMESTAMP,	"modifyTimestamp"	},
 	{ LDAP_ATTR_LIST_END,		NULL 			}
 };
 
@@ -1394,3 +1395,4 @@ char *smbldap_get_dn(LDAP *ld, LDAPMessage *entry)
 	ldap_memfree(utf8_dn);
 	return unix_dn;
 }
+
