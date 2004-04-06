bc145a276cea4d20db8f95f1927717e4a9813a94
Jim McDonough <jmcd@samba.org>
2004-04-06 22:02:51 +0000

    r92: Fix lsalookupnames.  Previously we'd fail if we didn't find the name, but we never checked if it was a domain user and didn't find a local one.

--- source/lib/util_sid.c
+++ source/lib/util_sid.c
@@ -201,8 +201,13 @@ void split_domain_name(const char *fullname, char *domain, char *name)
 		fstrcpy(domain, full_name);
 		fstrcpy(name, p+1);
 	} else {
-		fstrcpy(domain, global_myname());
-		fstrcpy(name, full_name);
+		if(!lp_domain_logons()) {
+			fstrcpy(domain, global_myname());
+			fstrcpy(name, full_name);
+		} else {
+			fstrcpy(domain, lp_workgroup());
+			fstrcpy(name, full_name);
+		}
 	}
 
 	DEBUG(10,("split_domain_name:name '%s' split into domain :'%s' and user :'%s'\n",
