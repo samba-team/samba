768cbc9228846ded6ddb04559d38eb643ac71f18
Stefan Metzmacher <metze@samba.org>
2004-04-14 13:00:22 +0000

    r211: svk has different revision numbers, so use "3.9.0-SVK-build-276-metze@dragon" as version when the working copy is checked out with svk
    metze

--- source/script/mkversion.sh
+++ source/script/mkversion.sh
@@ -51,15 +51,22 @@ fi
 
 
 if test x"${SAMBA_VERSION_IS_SVN_SNAPSHOT}" = x"yes";then
-    TMP_SVN_REVISION=`(svn info ${SOURCE_DIR} 2>/dev/null || svk info ${SOURCE_DIR} 2>/dev/null) |grep Revision: |sed -e 's/Revision: \([0-9]*\).*/\1/'`
-    TMP_SVN_REVISION_STR=""
-    if test -n "${TMP_SVN_REVISION}";then
-	TMP_SVN_REVISION_STR="-${TMP_SVN_REVISION}"
-    else
-	TMP_SVN_REVISION=0
+    HAVESVN=yes
+    TMP_SVN_REVISION=`(svn info ${SOURCE_DIR} 2>/dev/null || HAVESVN=no) |grep Revision: |sed -e 's/Revision: \([0-9]*\).*/\1/'`
+    if test x"${HAVESVN}" = x"no";then
+	HAVESVK=yes
+	TMP_SVK_REVISION=`(svk info ${SOURCE_DIR} 2>/dev/null || HAVESVK=no) |grep Revision: |sed -e 's/Revision: \([0-9]*\).*/\1/'`
+	TMP_SVK_REVISION_STR="${TMP_SVK_REVISION}-${USER}@${HOSTNAME}"
     fi
-    SAMBA_VERSION_STRING="${SAMBA_VERSION_STRING}-SVN-build${TMP_SVN_REVISION_STR}"
-    echo "#define SAMBA_VERSION_SVN_REVISION ${TMP_SVN_REVISION}" >> $OUTPUT_FILE
+
+    if test x"${HAVESVN}" = x"yes";then
+	    SAMBA_VERSION_STRING="${SAMBA_VERSION_STRING}-SVN-build-${TMP_SVN_REVISION}"
+	    echo "#define SAMBA_VERSION_SVN_REVISION ${TMP_SVN_REVISION}" >> $OUTPUT_FILE
+    elif test x"${HAVESVK}" = x"yes";then
+	    SAMBA_VERSION_STRING="${SAMBA_VERSION_STRING}-SVK-build-${TMP_SVK_REVISION_STR}"
+    else
+	    SAMBA_VERSION_STRING="${SAMBA_VERSION_STRING}-SVN-build-UNKNOWN"
+    fi
 fi
 
 if test -n "${SAMBA_VERSION_VENDOR_SUFFIX}";then
