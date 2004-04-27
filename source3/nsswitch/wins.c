43fb4632c6903abbcbcad371fec45c7f40c83e0e
Gerald Carter <jerry@samba.org>
2004-04-27 13:12:31 +0000

    r376: BUG 1288: resolve any machine netbios name (0x00) and not just servers (0x20) (This used to be commit d96929b5b1d8d149ac023a30964925952587c258)

--- source3/nsswitch/wins.c
+++ source3/nsswitch/wins.c
@@ -100,7 +100,7 @@ static struct in_addr *lookup_byname_backend(const char *name, int *count)
 	*count = 0;
 
 	/* always try with wins first */
-	if (resolve_wins(name,0x20,&address,count)) {
+	if (resolve_wins(name,0x00,&address,count)) {
 		if ( (ret = (struct in_addr *)malloc(sizeof(struct in_addr))) == NULL ) {
 			free( address );
 			return NULL;
@@ -118,7 +118,7 @@ static struct in_addr *lookup_byname_backend(const char *name, int *count)
 	/* uggh, we have to broadcast to each interface in turn */
 	for (j=iface_count() - 1;j >= 0;j--) {
 		struct in_addr *bcast = iface_n_bcast(j);
-		ret = name_query(fd,name,0x20,True,True,*bcast,count, &flags, NULL);
+		ret = name_query(fd,name,0x00,True,True,*bcast,count, &flags, NULL);
 		if (ret) break;
 	}
 
