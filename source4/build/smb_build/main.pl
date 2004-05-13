c2236f1a8bcec64f951ad404f59aaf3bc593f89b
Stefan Metzmacher <metze@samba.org>
2004-05-13 10:35:08 +0000

    r667: remove Data::Dumper dependecy
    metze
    (This used to be commit eae450f219701e156a5bd0fd7096dd7fe7da6e2b)

--- source4/build/smb_build/main.pl
+++ source4/build/smb_build/main.pl
@@ -6,20 +6,6 @@
 ###  Released under the GNU GPL				###
 ###########################################################
 
-use Data::Dumper;
-sub _dump_ctx($)
-{
-	my $ctx = shift;
-
-	open (DUMP,"> config.smb_build.dump");
-	
-	print DUMP Dumper($ctx);
-
-	close(DUMP);
-
-	return;
-}
-
 sub smb_build_main($)
 {
 	check_input($SMB_BUILD_CTX);
@@ -31,8 +17,6 @@ sub smb_build_main($)
 	create_makefile_in($SMB_BUILD_CTX);
 
 	create_smb_build_h($SMB_BUILD_CTX);
-
-	_dump_ctx($SMB_BUILD_CTX);
 
 	return 0;
 }
