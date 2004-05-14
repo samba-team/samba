6210ba54329d3b19e7e72430cfa3b9bd3ceacc13
Andrew Tridgell <tridge@samba.org>
2004-05-14 00:26:37 +0000

    r714: make sure ldb formats are portable between big/little endian machines

--- source/lib/ldb/ldb_tdb/ldb_tdb.h
+++ source/lib/ldb/ldb_tdb/ldb_tdb.h
@@ -43,11 +43,3 @@ struct ltdb_private {
 #define LTDB_FLAG_WILDCARD         (1<<2)
 #define LTDB_FLAG_OBJECTCLASS      (1<<3)
 #define LTDB_FLAG_HIDDEN           (1<<4)
-
-
-#ifndef IVAL
-#define IVAL(p, ofs) (((unsigned *)((char *)(p) + (ofs)))[0])
-#endif
-#ifndef SIVAL
-#define SIVAL(p, ofs, v) do { IVAL(p, ofs) = (v); } while (0)
-#endif
