7fffc124e38a4e5b16d650b1f6d82b24dcfee982
Stefan Metzmacher <metze@samba.org>
2004-05-24 16:02:40 +0000

    r846: convert DCESRV subsystem to a config.mk file
    metze

--- source/rpc_server/config.m4
+++ source/rpc_server/config.m4
@@ -1,27 +1,16 @@
 dnl # DCERPC Server subsystem
 
-SMB_SUBSYSTEM(DCERPC_COMMON,[],
-		[rpc_server/common/server_info.o
-		rpc_server/common/share_info.o])
+SMB_SUBSYSTEM_MK(DCERPC_COMMON,rpc_server/config.mk)
 
-SMB_SUBSYSTEM(SAMDB,[rpc_server/samr/samdb.o],[rpc_server/samr/samr_utils.o],[],[LIBLDB])
+SMB_SUBSYSTEM_MK(SAMDB,rpc_server/config.mk)
 
-SMB_MODULE(dcerpc_rpcecho,DCERPC,STATIC,[rpc_server/echo/rpc_echo.o])
-SMB_MODULE(dcerpc_epmapper,DCERPC,STATIC,[rpc_server/epmapper/rpc_epmapper.o])
-SMB_MODULE(dcerpc_remote,DCERPC,STATIC,[rpc_server/remote/dcesrv_remote.o],[],[],[LIBSMB])
-SMB_MODULE(dcerpc_srvsvc,DCERPC,STATIC,[rpc_server/srvsvc/dcesrv_srvsvc.o],[],[],[DCERPC_COMMON])
-SMB_MODULE(dcerpc_wkssvc,DCERPC,STATIC,[rpc_server/wkssvc/dcesrv_wkssvc.o],[],[],[DCERPC_COMMON])
+SMB_MODULE_MK(dcerpc_rpcecho,DCERPC,STATIC,rpc_server/config.mk)
+SMB_MODULE_MK(dcerpc_epmapper,DCERPC,STATIC,rpc_server/config.mk)
+SMB_MODULE_MK(dcerpc_remote,DCERPC,STATIC,rpc_server/config.mk)
+SMB_MODULE_MK(dcerpc_srvsvc,DCERPC,STATIC,rpc_server/config.mk)
+SMB_MODULE_MK(dcerpc_wkssvc,DCERPC,STATIC,rpc_server/config.mk)
+SMB_MODULE_MK(dcerpc_samr,DCERPC,STATIC,rpc_server/config.mk)
+SMB_MODULE_MK(dcerpc_winreg,DCERPC,STATIC,rpc_server/config.mk)
+SMB_MODULE_MK(dcerpc_netlogon,DCERPC,STATIC,rpc_server/config.mk)
 
-SMB_MODULE(dcerpc_samr,DCERPC,STATIC,
-	[rpc_server/samr/dcesrv_samr.o
-	rpc_server/samr/samr_password.o],
-	[],[],
-	[SAMDB DCERPC_COMMON])
-
-SMB_MODULE(dcerpc_winreg,DCERPC,STATIC,[rpc_server/winreg/rpc_winreg.o],[],[],[REGISTRY])
-SMB_MODULE(dcerpc_netlogon,DCERPC,STATIC,[rpc_server/netlogon/dcerpc_netlogon.o],[],[],[SAMDB DCERPC_COMMON])
-
-SMB_SUBSYSTEM(DCERPC,rpc_server/dcerpc_server.o,
-		[rpc_server/dcerpc_tcp.o
-		rpc_server/dcesrv_auth.o
-		rpc_server/handles.o])
+SMB_SUBSYSTEM_MK(DCERPC,rpc_server/config.mk)
---
+++ source/rpc_server/config.mk
@@ -0,0 +1,117 @@
+# DCERPC Server subsystem
+
+################################################
+# Start SUBSYSTEM DCERPC_COMMON
+[SUBSYSTEM::DCERPC_COMMON]
+ADD_OBJ_FILES = \
+		rpc_server/common/server_info.o \
+		rpc_server/common/share_info.o
+#
+# End SUBSYSTEM DCERPC_COMMON
+################################################
+
+################################################
+# Start SUBSYSTEM SAMDB
+[SUBSYSTEM::SAMDB]
+INIT_OBJ_FILES = \
+		rpc_server/samr/samdb.o
+ADD_OBJ_FILES = \
+		rpc_server/samr/samr_utils.o
+REQUIRED_SUBSYSTEMS = \
+		LIBLDB
+#
+# End SUBSYSTEM SAMDB
+################################################
+
+################################################
+# Start MODULE dcerpc_rpcecho
+[MODULE::dcerpc_rpcecho]
+INIT_OBJ_FILES = \
+		rpc_server/echo/rpc_echo.o
+# End MODULE dcerpc_rpcecho
+################################################
+
+################################################
+# Start MODULE dcerpc_epmapper
+[MODULE::dcerpc_epmapper]
+INIT_OBJ_FILES = \
+		rpc_server/epmapper/rpc_epmapper.o
+# End MODULE dcerpc_epmapper
+################################################
+
+################################################
+# Start MODULE dcerpc_remote
+[MODULE::dcerpc_remote]
+INIT_OBJ_FILES = \
+		rpc_server/remote/dcesrv_remote.o
+REQUIRED_SUBSYSTEMS = \
+		LIBSMB
+# End MODULE dcerpc_remote
+################################################
+
+################################################
+# Start MODULE dcerpc_srvsvc
+[MODULE::dcerpc_srvsvc]
+INIT_OBJ_FILES = \
+		rpc_server/srvsvc/dcesrv_srvsvc.o
+REQUIRED_SUBSYSTEMS = \
+		DCERPC_COMMON
+# End MODULE dcerpc_srvsvc
+################################################
+
+################################################
+# Start MODULE dcerpc_wkssvc
+[MODULE::dcerpc_wkssvc]
+INIT_OBJ_FILES = \
+		rpc_server/wkssvc/dcesrv_wkssvc.o
+REQUIRED_SUBSYSTEMS = \
+		DCERPC_COMMON
+# End MODULE dcerpc_wkssvc
+################################################
+
+################################################
+# Start MODULE dcerpc_samr
+[MODULE::dcerpc_samr]
+INIT_OBJ_FILES = \
+		rpc_server/samr/dcesrv_samr.o
+ADD_OBJ_FILES = \
+		rpc_server/samr/samr_password.o
+REQUIRED_SUBSYSTEMS = \
+		SAMDB \
+		DCERPC_COMMON
+# End MODULE dcerpc_samr
+################################################
+
+################################################
+# Start MODULE dcerpc_winreg
+[MODULE::dcerpc_winreg]
+INIT_OBJ_FILES = \
+		rpc_server/winreg/rpc_winreg.o
+REQUIRED_SUBSYSTEMS = \
+		REGISTRY
+# End MODULE dcerpc_winreg
+################################################
+
+################################################
+# Start MODULE dcerpc_netlogon
+[MODULE::dcerpc_netlogon]
+INIT_OBJ_FILES = \
+		rpc_server/netlogon/dcerpc_netlogon.o
+REQUIRED_SUBSYSTEMS = \
+		SAMDB \
+		DCERPC_COMMON
+# End MODULE dcerpc_netlogon
+################################################
+
+################################################
+# Start SUBSYSTEM DCERPC
+[SUBSYSTEM::DCERPC]
+INIT_OBJ_FILES = \
+		rpc_server/dcerpc_server.o
+ADD_OBJ_FILES = \
+		rpc_server/dcerpc_tcp.o \
+		rpc_server/dcesrv_auth.o \
+		rpc_server/handles.o
+#
+# End SUBSYSTEM DCERPC
+################################################
