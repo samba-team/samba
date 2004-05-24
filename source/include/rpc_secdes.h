672035dfc5881a2273a3433d2880ac6ddc8ba97a
Andrew Tridgell <tridge@samba.org>
2004-05-24 04:53:12 +0000

    r836: get rid of SEC_DESC and related structure definitions

--- source/include/rpc_secdes.h
+++ source/include/rpc_secdes.h
@@ -19,6 +19,8 @@
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
+typedef struct security_descriptor SEC_DESC;
+
 #ifndef _RPC_SECDES_H /* _RPC_SECDES_H */
 #define _RPC_SECDES_H 
 
@@ -113,33 +115,6 @@
 					PROTECTED_SACL_SECURITY_INFORMATION|\
 					PROTECTED_DACL_SECURITY_INFORMATION)
 
-/* SEC_ACCESS */
-typedef struct security_info_info
-{
-	uint32 mask;
-
-} SEC_ACCESS;
-
-/* SEC_ACE */
-typedef struct security_ace_info
-{
-	uint8 type;  /* xxxx_xxxx_ACE_TYPE - e.g allowed / denied etc */
-	uint8 flags; /* xxxx_INHERIT_xxxx - e.g OBJECT_INHERIT_ACE */
-	uint16 size;
-
-	SEC_ACCESS info;
-
-	/* this stuff may be present when type is XXXX_TYPE_XXXX_OBJECT */
-	uint32  obj_flags; /* xxxx_ACE_OBJECT_xxxx e.g present/inherited present etc */
-	struct GUID    obj_guid;  /* object GUID */
-	struct GUID    inh_guid;  /* inherited object GUID */		
-        /* eof object stuff */
-
-	DOM_SID trustee;
-
-} SEC_ACE;
-#define  SEC_ACE_HEADER_SIZE (2 * sizeof(uint8) + sizeof(uint16) + sizeof(uint32))
-
 #ifndef ACL_REVISION
 #define ACL_REVISION 0x3
 #endif
@@ -148,77 +123,9 @@ typedef struct security_ace_info
 #define NT4_ACL_REVISION 0x2
 #endif
 
-#ifndef _SEC_ACL
-/* SEC_ACL */
-typedef struct security_acl_info
-{
-	uint16 revision; /* 0x0003 */
-	uint16 size; /* size in bytes of the entire ACL structure */
-	uint32 num_aces; /* number of Access Control Entries */
-
-	SEC_ACE *ace;
-
-} SEC_ACL;
-#define  SEC_ACL_HEADER_SIZE (2 * sizeof(uint16) + sizeof(uint32))
-#define _SEC_ACL
-#endif
-
 #ifndef SEC_DESC_REVISION
 #define SEC_DESC_REVISION 0x1
 #endif
-
-#ifndef _SEC_DESC
-/* SEC_DESC */
-typedef struct security_descriptor_info
-{
-	uint16 revision; /* 0x0001 */
-	uint16 type;     /* SEC_DESC_xxxx flags */
-
-	uint32 off_owner_sid; /* offset to owner sid */
-	uint32 off_grp_sid  ; /* offset to group sid */
-	uint32 off_sacl     ; /* offset to system list of permissions */
-	uint32 off_dacl     ; /* offset to list of permissions */
-
-	SEC_ACL *dacl; /* user ACL */
-	SEC_ACL *sacl; /* system ACL */
-	DOM_SID *owner_sid; 
-	DOM_SID *grp_sid;
-
-} SEC_DESC;
-#define  SEC_DESC_HEADER_SIZE (2 * sizeof(uint16) + 4 * sizeof(uint32))
-#define _SEC_DESC
-#endif
-
-#ifndef _SEC_DESC_BUF
-/* SEC_DESC_BUF */
-typedef struct sec_desc_buf_info
-{
-	uint32 max_len;
-	uint32 ptr;
-	uint32 len;
-
-	SEC_DESC *sec;
-
-} SEC_DESC_BUF;
-#define _SEC_DESC_BUF
-#endif
-
-/* A type to describe the mapping of generic access rights to object
-   specific access rights. */
-
-typedef struct generic_mapping {
-	uint32 generic_read;
-	uint32 generic_write;
-	uint32 generic_execute;
-	uint32 generic_all;
-} GENERIC_MAPPING;
-
-typedef struct standard_mapping {
-	uint32 std_read;
-	uint32 std_write;
-	uint32 std_execute;
-	uint32 std_all;
-} STANDARD_MAPPING;
 
 
 /* Security Access Masks Rights */
