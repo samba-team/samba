a4a8d71085f2df22bbebf1a39cb98864255ec9e2
Jelmer Vernooij <jelmer@samba.org>
2004-06-16 16:20:05 +0000

    Makefile dependency updates (This used to be commit d519625378fdc5a1c0192766095104fded5b628c)

--- docs/smbdotconf/generate-file-list.sh
+++ docs/smbdotconf/generate-file-list.sh
@@ -2,7 +2,7 @@
 echo "<variablelist>"
 for I in `find . -type f -name '*.xml' -mindepth 2 | sort -t/ -k3 | xargs`
 do 
-	echo "<xi:include href='$I' parse='xml' xmlns:xi='http://www.w3.org/2003/XInclude'/>"
+	echo "<xi:include href='$I' parse='xml'/>"
 done
                 
 echo "</variablelist>"
