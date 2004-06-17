1f31eccdad0d65b06983c96da7cd52e5f713d6d5
Andrew Bartlett <abartlet@samba.org>
2004-06-17 00:31:24 +0000

    r1173: A quick little test to show that we cannot bind twice to a single endpoint.
    Andrew Bartlett
    (This used to be commit e12ad47c69098b6865f5b10527aa44ff322e4b18)

---
+++ source4/torture/rpc/bind.c
@@ -0,0 +1,86 @@
+/* 
+   Unix SMB/CIFS implementation.
+
+   dcerpc torture tests
+
+   Copyright (C) Andrew Tridgell 2003
+   Copyright (C) Andrew Bartlett <abartlet@samba.org 2004
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include "includes.h"
+
+
+
+BOOL torture_multi_bind(int dummy) 
+{
+	struct dcerpc_pipe *p;
+	const char *domain = lp_workgroup();
+	const char *username = lp_parm_string(-1, "torture", "username");
+	const char *password = lp_parm_string(-1, "torture", "password");
+	const char *pipe_uuid = DCERPC_LSARPC_UUID;
+	uint32_t pipe_version = DCERPC_LSARPC_VERSION;
+	struct dcerpc_binding b;
+	struct dcerpc_binding *binding;
+	const char *binding_string = lp_parm_string(-1, "torture", "binding");
+
+	TALLOC_CTX *mem_ctx;
+	mem_ctx = talloc_init("torture_multi_bind");
+	NTSTATUS status;
+	BOOL ret;
+
+	status = dcerpc_parse_binding(mem_ctx, binding_string, &b);
+	if (!NT_STATUS_IS_OK(status)) {
+		DEBUG(0,("Failed to parse dcerpc binding '%s'\n", binding_string));
+		talloc_destroy(mem_ctx);
+		return False;
+	}
+
+	binding = &b;
+
+	status = torture_rpc_connection(&p, 
+					NULL,
+					pipe_uuid,
+					pipe_version);
+	
+	if (!NT_STATUS_IS_OK(status)) {
+		return False;
+	}
+
+	if (username && username[0] && (binding->flags & DCERPC_SCHANNEL_ANY)) {
+		status = dcerpc_bind_auth_schannel(p, pipe_uuid, pipe_version, 
+						   domain, username, password);
+	} else if (username && username[0] && (binding->flags & (DCERPC_SIGN | DCERPC_SEAL))) {
+		status = dcerpc_bind_auth_ntlm(p, pipe_uuid, pipe_version, domain, username, password);
+	} else {    
+		status = dcerpc_bind_auth_none(p, pipe_uuid, pipe_version);
+	}
+
+	if (NT_STATUS_IS_OK(status)) {
+		DEBUG(0,("(incorrectly) allowed re-bind to uuid %s - %s\n", 
+			 pipe_uuid, nt_errstr(status)));
+		ret = False;
+	} else {
+		ret = True;
+	}
+
+	printf("\n");
+	
+	talloc_destroy(mem_ctx);
+	torture_rpc_close(p);
+
+	return ret;
+}
