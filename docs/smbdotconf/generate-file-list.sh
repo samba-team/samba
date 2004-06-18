9eb45e211cbc28bbd28837a17dcec3df29d6f455
Jelmer Vernooij <jelmer@samba.org>
2004-06-18 13:12:52 +0000

    Support running from a different directory (This used to be commit ec0bbb6c2e7ccb9155d4f890cc99563453d209f2)

--- docs/smbdotconf/generate-file-list.sh
+++ docs/smbdotconf/generate-file-list.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+DIR=.
+if [ "x$1" != "x" ]
+then
+	DIR="$1"
+fi
+
 echo "<variablelist>"
-for I in `find . -type f -name '*.xml' -mindepth 2 | sort -t/ -k3 | xargs`
+for I in `find $DIR -type f -name '*.xml' -mindepth 2 | sort -t/ -k3 | xargs`
 do 
 	echo "<xi:include href='$I' parse='xml'/>"
 done
