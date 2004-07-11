fb8ae4518ac22df2bd629050292190b18d1f8afb
Jelmer Vernooij <jelmer@samba.org>
2004-07-11 12:51:01 +0000

    r1451: More missing files... (This used to be commit 7e9884799e4f450b9693b6e29d7490288ebc969e)

---
+++ source4/libcli/auth/config.mk
@@ -0,0 +1,11 @@
+#################################
+# Start SUBSYSTEM GENSEC
+[SUBSYSTEM::LIBCLI_AUTH]
+ADD_OBJ_FILES = libcli/auth/schannel.o \
+		libcli/auth/credentials.o \
+		libcli/auth/session.o \
+		libcli/auth/ntlm_check.o
+REQUIRED_SUBSYSTEMS = \
+		AUTH SCHANNELDB GENSEC
+# End SUBSYSTEM LIBCLI_AUTH
+#################################
