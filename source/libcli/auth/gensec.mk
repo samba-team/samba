531cd0f4da6a4b7090bed29a18ad0b6baecd217f
Jelmer Vernooij <jelmer@samba.org>
2004-07-11 12:38:27 +0000

    r1450: Oops.. Missing files :-)

---
+++ source/libcli/auth/gensec.mk
@@ -0,0 +1,42 @@
+#################################
+# Start SUBSYSTEM GENSEC
+[SUBSYSTEM::GENSEC]
+INIT_OBJ_FILES = libcli/auth/gensec.o
+REQUIRED_SUBSYSTEMS = \
+		AUTH SCHANNELDB
+# End SUBSYSTEM GENSEC
+#################################
+
+################################################
+# Start MODULE gensec_krb5
+[MODULE::gensec_krb5]
+INIT_OBJ_FILES = libcli/auth/gensec_krb5.o 
+ADD_OBJ_FILES = \
+		libcli/auth/clikrb5.o \
+		libcli/auth/kerberos.o \
+		libcli/auth/kerberos_verify.o
+REQUIRED_SUBSYSTEMS = GENSEC
+# End MODULE gensec_krb5
+################################################
+
+################################################
+# Start MODULE gensec_spnego
+[MODULE::gensec_spnego]
+INIT_OBJ_FILES = libcli/auth/spnego.o
+ADD_OBJ_FILES = \
+		libcli/auth/spnego_parse.o
+REQUIRED_SUBSYSTEMS = GENSEC
+# End MODULE gensec_spnego
+################################################
+
+################################################
+# Start MODULE gensec_ntlmssp
+[MODULE::gensec_ntlmssp]
+INIT_OBJ_FILES = libcli/auth/gensec_ntlmssp.o
+ADD_OBJ_FILES = \
+		libcli/auth/ntlmssp.o \
+		libcli/auth/ntlmssp_parse.o \
+		libcli/auth/ntlmssp_sign.o
+REQUIRED_SUBSYSTEMS = GENSEC AUTH
+# End MODULE gensec_ntlmssp
+################################################
