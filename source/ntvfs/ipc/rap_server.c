08140cc1a838b4eaa23c897b280a46c95b7ef3e0
Volker Lendecke <vlendec@samba.org>
2004-07-12 16:35:48 +0000

    r1470: Get the smb_trans2 structure out of the rap_cli_call struct.
    Initial attempt at RAP server infrastructure. Look at rap_server.c for the
    dummy functions that are supposed to implement the core functionality.
    
    ipc_rap.c contains all the data shuffling. _rap_shareenum and _rap_serverenum2
    in ipc_rap.c are (I think) regular enough to be auto-generated.
    
    I did not test all the corner cases yet, but nevertheless I would like some
    comments on the general style.
    
    Volker
    
    P.S: samba-3 smbclient now doesn't freak out anymore, although the results are
    not entirely correct :-)

---
+++ source/ntvfs/ipc/rap_server.c
@@ -0,0 +1,54 @@
+/* 
+   Unix SMB/CIFS implementation.
+   RAP handlers
+
+   Copyright (C) Volker Lendecke 2004
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include "includes.h"
+
+/* At this moment these are just dummy functions, but you might get the
+ * idea. */
+
+NTSTATUS rap_netshareenum(struct smbsrv_request *req,
+			  struct rap_NetShareEnum *r)
+{
+	r->out.status = 0;
+	r->out.available = 2;
+	r->out.info = talloc_array_p(req->mem_ctx,
+				     union rap_shareenum_info, 2);
+
+	strncpy(r->out.info[0].info1.name, "C$", 12);
+	r->out.info[0].info1.pad = 0;
+	r->out.info[0].info1.type = 0;
+	r->out.info[0].info1.comment = talloc_strdup(req->mem_ctx, "Bla");
+	
+	strncpy(r->out.info[1].info1.name, "IPC$", 12);
+	r->out.info[1].info1.pad = 0;
+	r->out.info[1].info1.type = 1;
+	r->out.info[1].info1.comment = talloc_strdup(req->mem_ctx, "Blub");
+	
+	return NT_STATUS_OK;
+}
+
+NTSTATUS rap_netserverenum2(struct smbsrv_request *req,
+				   struct rap_NetServerEnum2 *r)
+{
+	r->out.status = 0;
+	r->out.available = 0;
+	return NT_STATUS_OK;
+}
