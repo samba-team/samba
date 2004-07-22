430cf63b9148441bce42bfb15a8045de5da108f4
Gerald Carter <jerry@samba.org>
2004-07-22 13:39:43 +0000

    r1570: merging changes from 3.0.5

--- source/include/mangle.h
+++ source/include/mangle.h
@@ -8,7 +8,7 @@ struct mangle_fns {
 	BOOL (*is_mangled)(const char *s);
 	BOOL (*is_8_3)(const char *fname, BOOL check_case, BOOL allow_wildcards);
 	void (*reset)(void);
-	BOOL (*check_cache)(char *s);
+	BOOL (*check_cache)(char *s, size_t maxlen);
 	void (*name_map)(char *OutName, BOOL need83, BOOL cache83, int default_case);
 };
 #endif /* _MANGLE_H_ */
