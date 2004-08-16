d3e7a22630aa931d55ed44a695a0b05344bc6098
Stefan Metzmacher <metze@samba.org>
2004-08-16 16:52:57 +0000

    r1836: - as abartlet said to me, we need to contact the users domain pdcfor doing a   password change - add start of libnet_SetPassword - use KRB5 and LDAP instead of ADS as ADS isn't a protocol - add start of lib_rpc_connect()
    metze
    (This used to be commit 05c40dca8ad1ab020aa75282da046f1dbce2a52a)

--- source4/libnet/config.mk
+++ source4/libnet/config.mk
@@ -1,6 +1,8 @@
 #################################
 # Start SUBSYSTEM LIBNET
 [SUBSYSTEM::LIBNET]
-ADD_OBJ_FILES = libnet/libnet_password.o
+ADD_OBJ_FILES = \
+		libnet/libnet_passwd.o \
+		libnet/libnet_rpc.o
 # End SUBSYSTEM LIBNET
 #################################
