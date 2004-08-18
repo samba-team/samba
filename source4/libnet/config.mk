226478521b93dcdd60d268757b3b2fce14549b72
Stefan Metzmacher <metze@samba.org>
2004-08-18 11:01:06 +0000

    r1878: implemet libnet_context_{init,destroy}
    and implement libnet_Change_Password_generic()
    
    metze
    (This used to be commit d48050ae906ecf8d4f78c224042f8f9fe584ad39)

--- source4/libnet/config.mk
+++ source4/libnet/config.mk
@@ -1,6 +1,8 @@
 #################################
 # Start SUBSYSTEM LIBNET
 [SUBSYSTEM::LIBNET]
+INIT_OBJ_FILES = \
+		libnet/libnet.o
 ADD_OBJ_FILES = \
 		libnet/libnet_passwd.o \
 		libnet/libnet_rpc.o
