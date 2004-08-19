8b80b46b8b76ead72c17dc65f2de4c0cc0b263c7
Jeremy Allison <jra@samba.org>
2004-08-19 22:47:05 +0000

    r1939: Ensure with my new change we set extended security in flags2 in the negprot reply. Jeremy.

--- source/smbd/negprot.c
+++ source/smbd/negprot.c
@@ -250,6 +250,9 @@ static int reply_nt1(char *inbuf, char *outbuf)
 		negotiate_spnego = True;
 		capabilities |= CAP_EXTENDED_SECURITY;
 		add_to_common_flags2(FLAGS2_EXTENDED_SECURITY);
+		/* Ensure FLAGS2_EXTENDED_SECURITY gets set in this reply (already
+			partially constructed. */
+		SSVAL(outbuf,smb_flg2, SVAL(outbuf,smb_flg2) | FLAGS2_EXTENDED_SECURITY);
 	}
 	
 	capabilities |= CAP_NT_SMBS|CAP_RPC_REMOTE_APIS|CAP_UNICODE;
