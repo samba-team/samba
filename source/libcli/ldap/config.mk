8be31e5c854e4462163b97b897ff41de95f181c4
Simo Sorce <idra@samba.org>
2004-08-20 07:39:19 +0000

    r1944: put ldif functions in a separate file

--- source/libcli/ldap/config.mk
+++ source/libcli/ldap/config.mk
@@ -1,6 +1,7 @@
 #################################
 # Start SUBSYSTEM LIBCLI_LDAP
 [SUBSYSTEM::LIBCLI_LDAP]
-ADD_OBJ_FILES = libcli/ldap/ldap.o
+ADD_OBJ_FILES = libcli/ldap/ldap.o \
+		libcli/ldap/ldap_ldif.o
 # End SUBSYSTEM LIBCLI_LDAP
 #################################
