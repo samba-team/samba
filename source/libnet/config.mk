72e3b351d0169366aa88c5445ffa555da6efd1d0
Stefan Metzmacher <metze@samba.org>
2004-08-25 12:11:28 +0000

    r2065: add libnet_RemoteTOD() call with levels GENERIC and SRVSVC
    metze

--- source/libnet/config.mk
+++ source/libnet/config.mk
@@ -5,6 +5,7 @@ INIT_OBJ_FILES = \
 		libnet/libnet.o
 ADD_OBJ_FILES = \
 		libnet/libnet_passwd.o \
+		libnet/libnet_time.o \
 		libnet/libnet_rpc.o
 # End SUBSYSTEM LIBNET
 #################################
