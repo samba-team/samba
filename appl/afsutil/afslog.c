0a57899d043031bcdc778440cd5e942ba38f3593
Love Hörnquist Åstrand <lha@kth.se>
2004-09-03 12:11:40 +0000

    use negative string help string for arg_negative_flag Pointed out by Harald Barth
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@14185 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- appl/afsutil/afslog.c
+++ appl/afsutil/afslog.c
@@ -70,10 +70,10 @@ struct getargs args[] = {
     { "realm",	'k', arg_string, &realm, "realm for afs cell", "realm" },
     { "unlog",	'u', arg_flag, &unlog_flag, "remove tokens" },
 #ifdef KRB4
-    { "v4",	 0, arg_negative_flag, &use_krb4, "use Kerberos 4" },
+    { "v4",	 0, arg_negative_flag, &use_krb4, "don't use Kerberos 4" },
 #endif
 #ifdef KRB5
-    { "v5",	 0, arg_negative_flag, &use_krb5, "use Kerberos 5" },
+    { "v5",	 0, arg_negative_flag, &use_krb5, "don't use Kerberos 5" },
 #endif
 #if 0
     { "create-user", 0, arg_flag, &create_user, "create user if not found" },
