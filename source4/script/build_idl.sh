ff19c6ac0d4a966022e59986ced52857f75f94df
Tim Potter <tpot@samba.org>
2004-09-08 12:51:46 +0000

    r2255: Don't do experimental swig stuff by default. (This used to be commit db191572ff9351ae84dc0c3d6302fa60b61cd179)

--- source4/script/build_idl.sh
+++ source4/script/build_idl.sh
@@ -4,7 +4,7 @@ FULLBUILD=$1
 
 [ -d librpc/gen_ndr ] || mkdir -p librpc/gen_ndr || exit 1
 
-PIDL="$PERL ./build/pidl/pidl.pl --output librpc/gen_ndr/ndr_ --parse --header --parser --server --swig"
+PIDL="$PERL ./build/pidl/pidl.pl --output librpc/gen_ndr/ndr_ --parse --header --parser --server"
 TABLES="$PERL ./build/pidl/tables.pl --output librpc/gen_ndr/tables"
 
 if [ x$FULLBUILD = xFULL ]; then
