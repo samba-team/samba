de72d898778635af20a1f6a394fe6c837e2ed149
Gerald Carter <jerry@samba.org>
2004-09-23 18:11:08 +0000

    r2567: Patches from Lars Mueller <lmuelle at suse dot de>:
    trivial fix for autoconf and autoheader versions with a
    letter in the version string.  This happens in our current beta named
    distribution tree.
    
    trivial patch to fix the build with the upcoming libtool
    version.  It will be mandatory to use --mode while using libtool.
    (This used to be commit 80d591f8cc62d513eb99112e6533b93ee901d27d)

--- source3/autogen.sh
+++ source3/autogen.sh
@@ -16,7 +16,7 @@ AUTOCONFFOUND="0"
 ##
 for i in $TESTAUTOHEADER; do
 	if which $i > /dev/null 2>&1; then
-		if [ `$i --version | head -n 1 | cut -d.  -f 2` -ge 53 ]; then
+		if [ `$i --version | head -n 1 | cut -d.  -f 2 | tr -d [:alpha:]` -ge 53 ]; then
 			AUTOHEADER=$i
 			AUTOHEADERFOUND="1"
 			break
@@ -30,7 +30,7 @@ done
 
 for i in $TESTAUTOCONF; do
 	if which $i > /dev/null 2>&1; then
-		if [ `$i --version | head -n 1 | cut -d.  -f 2` -ge 53 ]; then
+		if [ `$i --version | head -n 1 | cut -d.  -f 2 | tr -d [:alpha:]` -ge 53 ]; then
 			AUTOCONF=$i
 			AUTOCONFFOUND="1"
 			break
