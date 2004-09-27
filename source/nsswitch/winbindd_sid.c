5654aa8d1d961058b2d4824446714d33b09c7718
Volker Lendecke <vlendec@samba.org>
2004-09-27 14:44:48 +0000

    r2692: Increase a debug level for a quite frequent operation.
    Optimization for 'idmap backend = ldap': When asking sid2id for the wrong
    type, don't ask ldap when we have the opposite mapping in the local tdb.
    
    Volker

--- source/nsswitch/winbindd_sid.c
+++ source/nsswitch/winbindd_sid.c
@@ -219,7 +219,7 @@ enum winbindd_result winbindd_sid_to_uid(struct winbindd_cli_state *state)
 	if (NT_STATUS_IS_OK(result))
 		return WINBINDD_OK;
 
-	DEBUG(1, ("Could not get uid for sid %s\n", state->request.data.sid));
+	DEBUG(4, ("Could not get uid for sid %s\n", state->request.data.sid));
 	return WINBINDD_ERROR;
 }
 
@@ -329,7 +329,7 @@ enum winbindd_result winbindd_sid_to_gid(struct winbindd_cli_state *state)
 	if (NT_STATUS_IS_OK(result))
 		return WINBINDD_OK;
 
-	DEBUG(1, ("Could not get gid for sid %s\n", state->request.data.sid));
+	DEBUG(4, ("Could not get gid for sid %s\n", state->request.data.sid));
 	return WINBINDD_ERROR;
 }
 
