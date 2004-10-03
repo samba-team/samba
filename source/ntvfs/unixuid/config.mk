079e82c223639b6e591c0b5e1f4fa0a33cd84567
Andrew Tridgell <tridge@samba.org>
2004-10-03 06:46:29 +0000

    r2794: a very simple version of the unixuid NTVFS pass-thru module. In conjunction with the posix backend this gives us a way to correctly setup the unix security context in Samba4.
    I chose the following method to determine the unix uid's and gid's to
    use given the list of SIDs from the login process
    
     - look for a "UnixID" field in the sam record. If present, then use it
       (check if the record is of the right type as well)
    
     - if UnixID is not present, then look for the "UnixName" sam
       field. If it is present then use getpwnam() or getgrnam() to find
       the unix id.
    
     - if UnixID and UnixName are not present, then look for a unix
       account of the right type called by the same name as the sAMAccountName field.
    
     - if none of the above work then fail the operation with NT_STATUS_ACCESS_DENIED
    
    obviously these steps only work well with a local SAM. It will need to
    be more sophisticated in future.
    
    I did not put any cache in place at all. That will need to be added
    for decent performance.

---
+++ source/ntvfs/unixuid/config.mk
@@ -0,0 +1,7 @@
+################################################
+# Start MODULE ntvfs_unixuid
+[MODULE::ntvfs_unixuid]
+INIT_OBJ_FILES = \
+		ntvfs/unixuid/vfs_unixuid.o
+# End MODULE ntvfs_unixuid
+################################################
