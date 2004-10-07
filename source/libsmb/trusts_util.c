9428cf10a44433ed6d798b306a7c5fa81a06df2d
Rafal Szczesniak <mimir@samba.org>
2004-10-07 13:44:10 +0000

    r2840: Big patch. Switch from secrets_* functions to new trust passwords api integrated with passdb interface.
    Trust passwords migration function.
    
    - use necessary trust password structure initialisation
    - set trust password depending on whether it is plaintext (ADS)
      or 16 bytes of password hash (NT)
    - typo fix in comment
    - use pdb_set_tp_* functions instead of directly accessing structure
      members
    - count NT_STATUS_USER_EXISTS code as migrated password
    
    
    rafal

--- source/libsmb/trusts_util.c
+++ source/libsmb/trusts_util.c
@@ -64,10 +64,24 @@ NTSTATUS trust_pw_change_and_store_it(struct cli_state *cli, TALLOC_CTX *mem_ctx
 				      unsigned char orig_trust_passwd_hash[16],
 				      uint32 sec_channel_type)
 {
+	SAM_TRUST_PASSWD *trust = NULL;
 	unsigned char new_trust_passwd_hash[16];
 	char *new_trust_passwd;
 	char *str;
 	NTSTATUS nt_status;
+	
+	/* Get trust password before updating it */
+	nt_status = pdb_init_trustpw_talloc(mem_ctx, &trust);
+	if (!NT_STATUS_IS_OK(nt_status)) {
+		DEBUG(0, ("Couldn't init trust password\n"));
+		return nt_status;
+	}
+
+	nt_status = pdb_gettrustpwnam(trust, domain);
+	if (!NT_STATUS_IS_OK(nt_status)) {
+		DEBUG(0, ("Couldn't get trust password for domain [%s]\n", domain));
+		return nt_status;
+	}
 		
 	/* Create a random machine account password */
 	str = generate_random_str(DEFAULT_TRUST_ACCOUNT_PASSWORD_LENGTH);
@@ -85,8 +99,25 @@ NTSTATUS trust_pw_change_and_store_it(struct cli_state *cli, TALLOC_CTX *mem_ctx
 		 * Return the result of trying to write the new password
 		 * back into the trust account file.
 		 */
-		if (!secrets_store_machine_password(new_trust_passwd, domain, sec_channel_type)) {
-			nt_status = NT_STATUS_UNSUCCESSFUL;
+
+		/* copying new password to trust password structure */
+		if (trust->private.flags & PASS_TRUST_NT) {
+			trust->private.pass = data_blob_talloc(mem_ctx, new_trust_passwd_hash,
+							       NT_HASH_LEN);
+		} else if (trust->private.flags & PASS_TRUST_ADS) {
+			trust->private.pass = data_blob_talloc(mem_ctx, new_trust_passwd_hash,
+							       strlen(new_trust_passwd_hash) + 1);
+			trust->private.pass.data[trust->private.pass.length] = '\0';
+		}
+
+		/* trust password flags (according to sec channel type) */
+		sec_channel_type = SCHANNEL_TYPE(trust->private.flags);
+		
+		nt_status = pdb_update_trust_passwd(trust);
+		if (!NT_STATUS_IS_OK(nt_status)) {
+			DEBUG(0, ("Error when updating trust password for domain [%s]\n",
+				  domain));
+			return nt_status;
 		}
 	}
 
@@ -95,7 +126,7 @@ NTSTATUS trust_pw_change_and_store_it(struct cli_state *cli, TALLOC_CTX *mem_ctx
 
 /*********************************************************
  Change the domain password on the PDC.
- Do most of the legwork ourselfs.  Caller must have
+ Do most of the legwork ourselves.  Caller must have
  already setup the connection to the NETLOGON pipe
 **********************************************************/
 
@@ -103,18 +134,33 @@ NTSTATUS trust_pw_find_change_and_store_it(struct cli_state *cli,
 					   TALLOC_CTX *mem_ctx, 
 					   const char *domain) 
 {
+	NTSTATUS nt_status = NT_STATUS_UNSUCCESSFUL;
+	SAM_TRUST_PASSWD *trust = NULL;
 	unsigned char old_trust_passwd_hash[16];
 	char *up_domain;
 	uint32 sec_channel_type = 0;
 
 	up_domain = talloc_strdup(mem_ctx, domain);
-
-	if (!secrets_fetch_trust_account_password(domain,
-						  old_trust_passwd_hash, 
-						  NULL, &sec_channel_type)) {
-		DEBUG(0, ("could not fetch domain secrets for domain %s!\n", domain));
+	
+	nt_status = pdb_init_trustpw_talloc(mem_ctx, &trust);
+	if (!NT_STATUS_IS_OK(nt_status)) {
+		DEBUG(0, ("Could not init trust password\n"));
+		return NT_STATUS_NO_MEMORY;
+	}
+	
+	nt_status = pdb_gettrustpwnam(trust, domain);
+	if (!NT_STATUS_IS_OK(nt_status)) {
+		DEBUG(0, ("could not fetch trust password for domain %s!\n", domain));
 		return NT_STATUS_UNSUCCESSFUL;
 	}
+
+	/* TODO: here should actually be a test verifying whether trust
+	   password is really a NT trust password */
+
+	memcpy(old_trust_passwd_hash, trust->private.pass.data,
+	       trust->private.pass.length);
+	
+	sec_channel_type = SCHANNEL_TYPE(trust->private.flags);
 	
 	return trust_pw_change_and_store_it(cli, mem_ctx, domain,
 					    old_trust_passwd_hash,
@@ -197,16 +243,17 @@ done:
  * @return number of passwords migrated
  */
 
-int migrate_trust_passwords(struct pdb_context *pdb_ctx)
+int migrate_trust_passwords(void)
 {
 	int migrated = 0, i;
+	struct pdb_context *pdb_ctx = NULL;
 	NTSTATUS nt_status;
-	SAM_TRUST_PASSWD trust;
-	const size_t max_name_len = sizeof(trust.private.uni_name)/2;
+	SAM_TRUST_PASSWD *trust = NULL;
 	time_t lct;
 	/* nt workstation trust */
 	const char* dom_name = lp_workgroup();
-	uint8 wks_pass[16];
+	uint8 nt_wks_pass[NT_HASH_LEN];
+	char* ads_wks_pass;
 	uint32 chan = 0;
 	DOM_SID dom_sid;
 	/* nt domain trust */
@@ -214,87 +261,134 @@ int migrate_trust_passwords(struct pdb_context *pdb_ctx)
 	const unsigned int max_trusts = 10;
 	int enum_ctx = 0, num_trusts;
 	TRUSTDOM **trusts;
-	char *trust_name = NULL, *pass;
-	size_t trust_name_len = 0;
+	char *trust_name = NULL, *pass = NULL, nt_dom_pass[NT_HASH_LEN];
 	DOM_SID sid;
-
-	/* sanity-check */
-	if (!pdb_ctx) return 0;
 
 	/* Checking whether passwords have already been migrated */
 	if (secrets_passwords_migrated(False)) return migrated;
 
-	/* NT/ADS Workstation trust passwords */
-	if (secrets_lock_trust_account_password(dom_name, True) &&
-	    secrets_fetch_trust_account_password(dom_name, wks_pass, &lct, &chan)) {
-		memset(&trust, 0, sizeof(trust));
+	nt_status = make_pdb_context_list(&pdb_ctx, lp_passdb_backend());
+	if (!NT_STATUS_IS_OK(nt_status)) return -1;
+	    
+	if (!pdb_ctx) return -1;
 
-		/* flags */
-		switch (lp_security()) {
-		case SEC_DOMAIN: trust.private.flags = PASS_TRUST_NT;
-			break;
-		case SEC_ADS:    trust.private.flags = PASS_TRUST_ADS;
-			break;
-		}
+	mem_ctx = talloc_init("trust password migration");
 
+	nt_status = pdb_init_trustpw_talloc(mem_ctx, &trust);
+	if (!NT_STATUS_IS_OK(nt_status)) return -1;
+
+	/* NT Workstation trust passwords */
+	if (lp_security() == SEC_DOMAIN &&
+	    secrets_lock_trust_account_password(dom_name, True) &&
+	    secrets_fetch_trust_account_password(dom_name, nt_wks_pass, &lct, &chan)) {
+
+		pdb_set_tp_flags(trust, PASS_TRUST_NT);
+
+		/* sec channel type and its corresponding flags */
 		switch (chan) {
-		case SEC_CHAN_WKSTA:
-			trust.private.flags |= PASS_TRUST_MACHINE;
+		case SEC_CHAN_WKSTA:  trust->private.flags |= PASS_TRUST_MACHINE;
 			break;
-		case SEC_CHAN_BDC:
-			trust.private.flags |= PASS_TRUST_SERVER;
+		case SEC_CHAN_BDC:    trust->private.flags |= PASS_TRUST_SERVER;
+			break;
+		case SEC_CHAN_DOMAIN: trust->private.flags |= PASS_TRUST_DOMAIN;
 			break;
 		default:
 			return 0;
 		}
 
-		/* unicode name length */
-		trust.private.uni_name_len = strlen(dom_name);
 		/* unicode name */
-		push_ucs2(NULL, &trust.private.uni_name, dom_name, max_name_len, STR_UPPER);
-		/* password */
-		strncpy(trust.private.pass, wks_pass, sizeof(trust.private.pass));
+		pdb_set_tp_domain_name_c(trust, dom_name);
+		/* password (nt hash) */
+		pdb_set_tp_pass(trust, nt_wks_pass, NT_HASH_LEN);
 		/* last change time */
-		trust.private.mod_time = lct;
+		pdb_set_tp_mod_time(trust, lct);
 		
 		/* domain sid */
 		if (secrets_fetch_domain_sid(dom_name, &dom_sid))
-			sid_copy(&trust.private.domain_sid, &dom_sid);
+			pdb_set_tp_domain_sid(trust, &dom_sid);
 		else
 			return 0;
 
 		/* release mutex on secrets.tdb record */
 		secrets_lock_trust_account_password(dom_name, False);
 		
-		nt_status = pdb_ctx->pdb_add_trust_passwd(pdb_ctx, &trust);
-		migrated++;
+		nt_status = pdb_ctx->pdb_add_trust_passwd(pdb_ctx, trust);
+		if (NT_STATUS_IS_OK(nt_status) ||
+		    NT_STATUS_EQUAL(nt_status, NT_STATUS_USER_EXISTS))
+			migrated++;
+		else
+			return -1;
 	}
 
 	/* NT Domain trust passwords */
-	mem_ctx = talloc_init("trust password migration");
 	do {
 		nt_status = secrets_get_trusted_domains(mem_ctx, &enum_ctx, max_trusts,
 							&num_trusts, &trusts);
-		for (i = 0; i < (num_trusts - enum_ctx); i++) {
-			memset(&trust, 0, sizeof(trust));
-			trust.private.flags = PASS_TRUST_NT | PASS_TRUST_DOMAIN;
-			pull_ucs2_allocate(&trust_name, trusts[i]->name);
-			trust_name_len = strlen_w(trusts[i]->name);
+		for (i = 0; i < num_trusts; i++) {
+			pdb_set_tp_flags(trust, PASS_TRUST_NT | PASS_TRUST_DOMAIN);
+			pull_ucs2_talloc(mem_ctx, &trust_name, trusts[i]->name);
 
 			if (secrets_fetch_trusted_domain_password(trust_name, &pass, &sid, &lct)) {
-				strncpy_w(trust.private.uni_name, trusts[i]->name, trust_name_len);
-				trust.private.uni_name_len = trust_name_len;
-				sid_copy(&trust.private.domain_sid, &sid);
-				trust.private.mod_time = lct;
+				pdb_set_tp_domain_name(trust, trusts[i]->name);
+				E_md4hash(pass, nt_dom_pass);
+				memset(pass, 0, strlen(pass));
+				SAFE_FREE(pass);
+				pdb_set_tp_pass(trust, nt_dom_pass, sizeof(nt_dom_pass));
+				pdb_set_tp_domain_sid(trust, &sid);
+				pdb_set_tp_mod_time(trust, lct);
 			}
 
-			nt_status = pdb_ctx->pdb_add_trust_passwd(pdb_ctx, &trust);
-			migrated++;
-
-			SAFE_FREE(trust_name);
+			nt_status = pdb_ctx->pdb_add_trust_passwd(pdb_ctx, trust);
+			if (NT_STATUS_IS_OK(nt_status) ||
+			    NT_STATUS_EQUAL(nt_status, NT_STATUS_USER_EXISTS))
+				migrated++;
+			else
+				return -1;
 		}
 
-	} while (NT_STATUS_EQUAL(nt_status, NT_STATUS_NO_MORE_ENTRIES));
+	} while (NT_STATUS_EQUAL(nt_status, STATUS_MORE_ENTRIES));
+
+	/* ADS Workstation trust passwords */
+	if (lp_security() == SEC_ADS &&
+	    secrets_lock_trust_account_password(dom_name, True) &&
+	    (ads_wks_pass = secrets_fetch_machine_password(dom_name, &lct, &chan))) {
+
+		pdb_set_tp_flags(trust, PASS_TRUST_ADS);
+
+		/* sec channel type and its corresponding flags */
+		switch (chan) {
+		case SEC_CHAN_WKSTA:  trust->private.flags |= PASS_TRUST_MACHINE;
+			break;
+		case SEC_CHAN_DOMAIN: trust->private.flags |= PASS_TRUST_DOMAIN;
+			break;
+		default:
+			return 0;
+		}
+
+		/* domain name */
+		pdb_set_tp_domain_name_c(trust, dom_name);
+		/* password (plaintext) */
+		pdb_set_tp_pass(trust, ads_wks_pass, strlen(ads_wks_pass) + 1);
+		/* last change time */
+		pdb_set_tp_mod_time(trust, lct);
+		
+		/* domain sid */
+		if (secrets_fetch_domain_sid(dom_name, &dom_sid))
+			pdb_set_tp_domain_sid(trust, &dom_sid);
+		else
+			return 0;
+		
+		/* release mutex on secrets.tdb record */
+		secrets_lock_trust_account_password(dom_name, False);
+		
+		nt_status = pdb_ctx->pdb_add_trust_passwd(pdb_ctx, trust);
+		if (NT_STATUS_IS_OK(nt_status) ||
+		    NT_STATUS_EQUAL(nt_status, NT_STATUS_USER_EXISTS))
+			migrated++;
+		else
+			return -1;
+	}
+
 
 	talloc_destroy(mem_ctx);
 
