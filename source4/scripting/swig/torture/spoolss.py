e3627c2c6eaf8b0ede0b014aacb3478d25faf1b8
Tim Potter <tpot@samba.org>
2004-10-14 07:25:47 +0000

    r2965: Ignore *.pyc files in torture directory.
    Allow test module name to be specified on command line for pytorture
    module.
    
    Start spoolss torture test.
    (This used to be commit 44bab84f765f6a4d37733a353fc6b74b09c1b39a)

---
+++ source4/scripting/swig/torture/spoolss.py
@@ -0,0 +1,24 @@
+import dcerpc
+
+def test_EnumPrinters(pipe):
+
+    r = {}
+    r['flags'] = 0x02
+    r['server'] = None
+    r['level'] = 1
+    r['buffer'] = 392 * '\x00'
+    r['buf_size'] = 392
+
+    result = dcerpc.spoolss_EnumPrinters(pipe, r)
+
+    print result
+
+def runtests(binding, domain, username, password):
+    
+    print 'Testing SPOOLSS pipe'
+
+    pipe = dcerpc.pipe_connect(binding,
+            dcerpc.DCERPC_SPOOLSS_UUID, dcerpc.DCERPC_SPOOLSS_VERSION,
+            domain, username, password)
+
+    test_EnumPrinters(pipe)
