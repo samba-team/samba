cf1a468df4007a5c9a1e49f54e519236b30a2522
Stefan Metzmacher <metze@samba.org>
2004-10-20 20:48:31 +0000

    r3095: - fix a free'ing of msg.dn
    - reenable index tests
    
    metze

--- source/lib/ldb/tests/test-generic.sh
+++ source/lib/ldb/tests/test-generic.sh
@@ -17,14 +17,14 @@ $VALGRIND bin/ldbrename "$OLDDN" "$NEWDN"  || exit 1
 echo "Showing renamed record"
 $VALGRIND bin/ldbsearch '(uid=uham)' || exit 1
 
-#echo "Starting ldbtest"
-#time $VALGRIND bin/ldbtest -r 1000 -s 10  || exit 1
+echo "Starting ldbtest"
+time $VALGRIND bin/ldbtest -r 1000 -s 10  || exit 1
 
-#echo "Adding index"
-#$VALGRIND bin/ldbadd tests/test-index.ldif  || exit 1
+echo "Adding index"
+$VALGRIND bin/ldbadd tests/test-index.ldif  || exit 1
 
-#echo "Starting ldbtest indexed"
-#time $VALGRIND bin/ldbtest -r 1000 -s 5000  || exit 1
+echo "Starting ldbtest indexed"
+time $VALGRIND bin/ldbtest -r 1000 -s 5000  || exit 1
 
 echo "Testing one level search"
 count=`$VALGRIND bin/ldbsearch -b 'ou=Groups,o=University of Michigan,c=US' -s one 'objectclass=*' none |grep ^dn | wc -l`
