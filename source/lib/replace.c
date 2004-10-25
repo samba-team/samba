d5c065fe54566f2f7bc1ee8d878f69d5b8df4c01
Andrew Tridgell <tridge@samba.org>
2004-10-25 23:26:13 +0000

    r3223: continue the effort on LIBREPLACE to try to get the ldb tools to compile on platforms like solaris without having to link to the whole of the Samba4 lib directory.

--- source/lib/replace.c
+++ source/lib/replace.c
@@ -427,6 +427,39 @@ char *rep_inet_ntoa(struct in_addr ip)
 #endif /* HAVE_SYSLOG */
 #endif /* HAVE_VSYSLOG */
 
+/*******************************************************************
+yield the difference between *A and *B, in seconds, ignoring leap seconds
+********************************************************************/
+static int tm_diff(struct tm *a, struct tm *b)
+{
+	int ay = a->tm_year + (1900 - 1);
+	int by = b->tm_year + (1900 - 1);
+	int intervening_leap_days =
+		(ay/4 - by/4) - (ay/100 - by/100) + (ay/400 - by/400);
+	int years = ay - by;
+	int days = 365*years + intervening_leap_days + (a->tm_yday - b->tm_yday);
+	int hours = 24*days + (a->tm_hour - b->tm_hour);
+	int minutes = 60*hours + (a->tm_min - b->tm_min);
+	int seconds = 60*minutes + (a->tm_sec - b->tm_sec);
+
+	return seconds;
+}
+
+/*******************************************************************
+  return the UTC offset in seconds west of UTC, or 0 if it cannot be determined
+  ******************************************************************/
+int get_time_zone(time_t t)
+{
+	struct tm *tm = gmtime(&t);
+	struct tm tm_utc;
+	if (!tm)
+		return 0;
+	tm_utc = *tm;
+	tm = localtime(&t);
+	if (!tm)
+		return 0;
+	return tm_diff(&tm_utc,tm);
+}
 
 #ifndef HAVE_TIMEGM
 /*
