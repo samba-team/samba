7abaf7206b0353bcd7816504660d552a5d2fd160
Andrew Tridgell <tridge@samba.org>
2004-10-25 01:49:54 +0000

    r3176: added a script to run all the filesystem torture tests that are expected to pass. This allows easy testing for regressions when I change core routines in the posix backend.

---
+++ source/script/tests/test_posix.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+# this runs the file serving tests that are expected to pass with the
+# current posix ntvfs backend
+
+if [ $# -lt 3 ]; then
+cat <<EOF
+Usage: test_posix.sh UNC USERNAME PASSWORD <first>
+EOF
+exit 1;
+fi
+
+unc="$1"
+username="$2"
+password="$3"
+start="$4"
+shift 3
+
+testit() {
+   cmdline="$*"
+   if ! $cmdline > test.$$ 2>&1; then
+       cat test.$$;
+       rm -f test.$$;
+       echo "TEST FAILED - $cmdline";
+       exit 1;
+   fi
+   rm -f test.$$;
+}
+
+
+tests="BASE-FDPASS BASE-LOCK1 BASE-LOCK2 BASE-LOCK3 BASE-LOCK4"
+tests="$tests BASE-LOCK5 BASE-LOCK6 BASE-LOCK7 BASE-UNLINK BASE-ATTR"
+tests="$tests BASE-TRANS2 BASE-NEGNOWAIT BASE-DIR"
+tests="$tests BASE-DENY2 BASE-TCON BASE-TCONDEV BASE-RW1"
+tests="$tests BASE-DENY3 BASE-XCOPY"
+tests="$tests BASE-DELETE BASE-PROPERTIES BASE-MANGLE"
+tests="$tests BASE-CHKPATH BASE-SECLEAK"
+tests="$tests RAW-QFSINFO RAW-QFILEINFO RAW-SFILEINFO-BUG"
+tests="$tests RAW-LOCK RAW-SEEK"
+
+
+soon="BASE-DIR1 BASE-DENY1 BASE-VUID BASE-OPEN BASE-DEFER_OPEN BASE-RENAME BASE-OPENATTR BASE-CHARSET"
+soon="$soon RAW-SFILEINFO RAW-SEARCH RAW-CLOSE RAW-OPEN RAW-MKDIR RAW-OPLOCK RAW-NOTIFY RAW-MUX RAW-IOCTL"
+soon="$soon RAW-CHKPATH RAW-UNLINK RAW-READ RAW-WRITE RAW-CONTEXT RAW-RENAME"
+
+for t in $tests; do
+    if [ ! -z "$start" -a "$start" != $t ]; then
+	continue;
+    fi
+    start=""
+    echo Testing $t
+    testit bin/smbtorture $unc -U"$username"%"$password" $t
+done
