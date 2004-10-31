cabec03422f0c7140b56b2d5c4ec8ca663b406fc
Jelmer Vernooij <jelmer@samba.org>
2004-10-31 18:37:59 +0000

    r3413: RemoteActivation updates and fixes Add torture test for RemoteActivation The request is now send correctly and we get back a valid response from Windows but r->in.Interfaces is set to 0 somewhere while parsing the response...

--- source/torture/torture.c
+++ source/torture/torture.c
@@ -2493,6 +2493,7 @@ static struct {
         {"RPC-EPMAPPER", torture_rpc_epmapper, 0},
         {"RPC-WINREG", torture_rpc_winreg, 0},
         {"RPC-OXIDRESOLVE", torture_rpc_oxidresolve, 0},
+        {"RPC-REMACT", torture_rpc_remact, 0},
         {"RPC-MGMT", torture_rpc_mgmt, 0},
         {"RPC-SCANNER", torture_rpc_scanner, 0},
         {"RPC-AUTOIDL", torture_rpc_autoidl, 0},
