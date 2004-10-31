b33f17e11b801a002829a98bcac2c70d64f9c950
Jelmer Vernooij <jelmer@samba.org>
2004-10-31 18:35:47 +0000

    r3412: Add NCACN_SPX and NCADG_IPX transports (This used to be commit ff2f2b4273ce9a49599b385d32fbbd5e8f0d4c23)

--- source4/librpc/rpc/dcerpc.h
+++ source4/librpc/rpc/dcerpc.h
@@ -23,7 +23,7 @@
 enum dcerpc_transport_t {
 	NCACN_NP, NCACN_IP_TCP, NCACN_IP_UDP, NCACN_VNS_IPC, NCACN_VNS_SPP, 
 	NCACN_AT_DSP, NCADG_AT_DDP, NCALRPC, NCACN_UNIX_STREAM, NCADG_UNIX_DGRAM,
-	NCACN_HTTP };
+	NCACN_HTTP, NCADG_IPX, NCACN_SPX };
 
 /*
   this defines a generic security context for signed/sealed dcerpc pipes.
