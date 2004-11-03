e0e6e0f99d9c7645e1809a3bde1a6ab01843de67
Stefan Metzmacher <metze@samba.org>
2004-11-03 16:02:32 +0000

    r3512: - support DsCrackName GUID strings ('{faedf4f9-0de8-4582-b8b6-c475efefbe5a}')
    - resolve the GUID's we got in DsGetDomainControllerInfo in the DsCrackNames test
    
    metze
    (This used to be commit f6310695821a7e750dd37936a6145232d81ab413)

--- source4/torture/rpc/drsuapi.c
+++ source4/torture/rpc/drsuapi.c
@@ -24,8 +24,17 @@
 #include "includes.h"
 #include "librpc/gen_ndr/ndr_drsuapi.h"
 
+struct DsPrivate {
+	struct policy_handle bind_handle;
+	struct GUID domain_guid;
+	struct GUID site_guid;
+	struct GUID computer_guid;
+	struct GUID server_guid;
+	struct GUID ntds_guid;
+};
+
 static BOOL test_DsBind(struct dcerpc_pipe *p, TALLOC_CTX *mem_ctx, 
-		      struct policy_handle *bind_handle)
+		      struct DsPrivate *priv)
 {
 	NTSTATUS status;
 	struct drsuapi_DsBind r;
@@ -33,7 +42,7 @@ static BOOL test_DsBind(struct dcerpc_pipe *p, TALLOC_CTX *mem_ctx,
 
 	r.in.server_guid = NULL;
 	r.in.bind_info = NULL;
-	r.out.bind_handle = bind_handle;
+	r.out.bind_handle = &priv->bind_handle;
 
 	printf("testing DsBind\n");
 
@@ -54,7 +63,7 @@ static BOOL test_DsBind(struct dcerpc_pipe *p, TALLOC_CTX *mem_ctx,
 }
 
 static BOOL test_DsCrackNames(struct dcerpc_pipe *p, TALLOC_CTX *mem_ctx, 
-		      struct policy_handle *bind_handle)
+		      struct DsPrivate *priv)
 {
 	NTSTATUS status;
 	struct drsuapi_DsCrackNames r;
@@ -66,7 +75,7 @@ static BOOL test_DsCrackNames(struct dcerpc_pipe *p, TALLOC_CTX *mem_ctx,
 	const char *FQDN_1779_name;
 
 	ZERO_STRUCT(r);
-	r.in.bind_handle		= bind_handle;
+	r.in.bind_handle		= &priv->bind_handle;
 	r.in.level			= 1;
 	r.in.req.req1.unknown1		= 0x000004e4;
 	r.in.req.req1.unknown2		= 0x00000407;
@@ -265,17 +274,107 @@ static BOOL test_DsCrackNames(struct dcerpc_pipe *p, TALLOC_CTX *mem_ctx,
 		return ret;
 	}
 
+	r.in.req.req1.format_offered	= DRSUAPI_DS_NAME_FORMAT_GUID;
+	r.in.req.req1.format_desired	= DRSUAPI_DS_NAME_FORMAT_FQDN_1779;
+	names[0].str = GUID_string2(mem_ctx, &priv->site_guid);
+
+	printf("testing DsCrackNames with Site GUID '%s' desired format:%d\n",
+			names[0].str, r.in.req.req1.format_desired);
+
+	status = dcerpc_drsuapi_DsCrackNames(p, mem_ctx, &r);
+	if (!NT_STATUS_IS_OK(status)) {
+		const char *errstr = nt_errstr(status);
+		if (NT_STATUS_EQUAL(status, NT_STATUS_NET_WRITE_FAULT)) {
+			errstr = dcerpc_errstr(mem_ctx, p->last_fault_code);
+		}
+		printf("dcerpc_drsuapi_DsCrackNames failed - %s\n", errstr);
+		ret = False;
+	} else if (!W_ERROR_IS_OK(r.out.result)) {
+		printf("DsCrackNames failed - %s\n", win_errstr(r.out.result));
+		ret = False;
+	}
+
+	if (!ret) {
+		return ret;
+	}
+
+	names[0].str = GUID_string2(mem_ctx, &priv->computer_guid);
+
+	printf("testing DsCrackNames with Computer GUID '%s' desired format:%d\n",
+			names[0].str, r.in.req.req1.format_desired);
+
+	status = dcerpc_drsuapi_DsCrackNames(p, mem_ctx, &r);
+	if (!NT_STATUS_IS_OK(status)) {
+		const char *errstr = nt_errstr(status);
+		if (NT_STATUS_EQUAL(status, NT_STATUS_NET_WRITE_FAULT)) {
+			errstr = dcerpc_errstr(mem_ctx, p->last_fault_code);
+		}
+		printf("dcerpc_drsuapi_DsCrackNames failed - %s\n", errstr);
+		ret = False;
+	} else if (!W_ERROR_IS_OK(r.out.result)) {
+		printf("DsCrackNames failed - %s\n", win_errstr(r.out.result));
+		ret = False;
+	}
+
+	if (!ret) {
+		return ret;
+	}
+
+	names[0].str = GUID_string2(mem_ctx, &priv->server_guid);
+
+	printf("testing DsCrackNames with Server GUID '%s' desired format:%d\n",
+			names[0].str, r.in.req.req1.format_desired);
+
+	status = dcerpc_drsuapi_DsCrackNames(p, mem_ctx, &r);
+	if (!NT_STATUS_IS_OK(status)) {
+		const char *errstr = nt_errstr(status);
+		if (NT_STATUS_EQUAL(status, NT_STATUS_NET_WRITE_FAULT)) {
+			errstr = dcerpc_errstr(mem_ctx, p->last_fault_code);
+		}
+		printf("dcerpc_drsuapi_DsCrackNames failed - %s\n", errstr);
+		ret = False;
+	} else if (!W_ERROR_IS_OK(r.out.result)) {
+		printf("DsCrackNames failed - %s\n", win_errstr(r.out.result));
+		ret = False;
+	}
+
+	if (!ret) {
+		return ret;
+	}
+
+	names[0].str = GUID_string2(mem_ctx, &priv->ntds_guid);
+
+	printf("testing DsCrackNames with NTDS GUID '%s' desired format:%d\n",
+			names[0].str, r.in.req.req1.format_desired);
+
+	status = dcerpc_drsuapi_DsCrackNames(p, mem_ctx, &r);
+	if (!NT_STATUS_IS_OK(status)) {
+		const char *errstr = nt_errstr(status);
+		if (NT_STATUS_EQUAL(status, NT_STATUS_NET_WRITE_FAULT)) {
+			errstr = dcerpc_errstr(mem_ctx, p->last_fault_code);
+		}
+		printf("dcerpc_drsuapi_DsCrackNames failed - %s\n", errstr);
+		ret = False;
+	} else if (!W_ERROR_IS_OK(r.out.result)) {
+		printf("DsCrackNames failed - %s\n", win_errstr(r.out.result));
+		ret = False;
+	}
+
+	if (!ret) {
+		return ret;
+	}
+
 	return ret;
 }
 
 static BOOL test_DsGetDCInfo(struct dcerpc_pipe *p, TALLOC_CTX *mem_ctx, 
-		      struct policy_handle *bind_handle)
+		      struct DsPrivate *priv)
 {
 	NTSTATUS status;
 	struct drsuapi_DsGetDomainControllerInfo r;
 	BOOL ret = True;
 
-	r.in.bind_handle = bind_handle;
+	r.in.bind_handle = &priv->bind_handle;
 	r.in.level = 1;
 
 	r.in.req.req1.domain_name = talloc_strdup(mem_ctx, lp_realm());
@@ -321,6 +420,13 @@ static BOOL test_DsGetDCInfo(struct dcerpc_pipe *p, TALLOC_CTX *mem_ctx,
 			"    with dns domain failed - %s\n",
 			r.in.req.req1.level, win_errstr(r.out.result));
 		ret = False;
+	} else {
+		if (r.out.ctr.ctr2.count > 0) {
+			priv->site_guid		= r.out.ctr.ctr2.array[0].site_guid;
+			priv->computer_guid	= r.out.ctr.ctr2.array[0].computer_guid;
+			priv->server_guid	= r.out.ctr.ctr2.array[0].server_guid;
+			priv->ntds_guid		= r.out.ctr.ctr2.array[0].ntds_guid;
+		}
 	}
 
 	r.in.req.req1.level = -1;
@@ -395,14 +501,14 @@ static BOOL test_DsGetDCInfo(struct dcerpc_pipe *p, TALLOC_CTX *mem_ctx,
 }
 
 static BOOL test_DsUnbind(struct dcerpc_pipe *p, TALLOC_CTX *mem_ctx, 
-			struct policy_handle *bind_handle)
+			struct DsPrivate *priv)
 {
 	NTSTATUS status;
 	struct drsuapi_DsUnbind r;
 	BOOL ret = True;
 
-	r.in.bind_handle = bind_handle;
-	r.out.bind_handle = bind_handle;
+	r.in.bind_handle = &priv->bind_handle;
+	r.out.bind_handle = &priv->bind_handle;
 
 	printf("testing DsUnbind\n");
 
@@ -428,7 +534,7 @@ BOOL torture_rpc_drsuapi(void)
         struct dcerpc_pipe *p;
 	TALLOC_CTX *mem_ctx;
 	BOOL ret = True;
-	struct policy_handle bind_handle;
+	struct DsPrivate priv;
 
 	status = torture_rpc_connection(&p, 
 					DCERPC_DRSUAPI_NAME,
@@ -442,19 +548,19 @@ BOOL torture_rpc_drsuapi(void)
 
 	mem_ctx = talloc_init("torture_rpc_drsuapi");
 
-	if (!test_DsBind(p, mem_ctx, &bind_handle)) {
+	if (!test_DsBind(p, mem_ctx, &priv)) {
 		ret = False;
 	}
 
-	if (!test_DsGetDCInfo(p, mem_ctx, &bind_handle)) {
+	if (!test_DsGetDCInfo(p, mem_ctx, &priv)) {
 		ret = False;
 	}
 
-	if (!test_DsCrackNames(p, mem_ctx, &bind_handle)) {
+	if (!test_DsCrackNames(p, mem_ctx, &priv)) {
 		ret = False;
 	}
 
-	if (!test_DsUnbind(p, mem_ctx, &bind_handle)) {
+	if (!test_DsUnbind(p, mem_ctx, &priv)) {
 		ret = False;
 	}
 
