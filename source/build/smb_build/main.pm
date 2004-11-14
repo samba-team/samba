04326d0ea0e1aefe5338d25f003b53e452ee753e
Jelmer Vernooij <jelmer@samba.org>
2004-11-14 22:33:21 +0000

    r3738: Use a hardcoded list of .mk files rather then "find" (breaks on some machines)

--- source/build/smb_build/main.pm
+++ source/build/smb_build/main.pm
@@ -22,7 +22,35 @@ sub smb_build_main($)
 		INPUT => $INPUT
 	);
 
-	my @mkfiles = split /\n/, `find -name "*.mk"`; 
+	my @mkfiles = (
+		"gtk/config.mk", 
+		"smbd/config.mk",
+		"smbd/process_model.mk",
+		"libnet/config.mk",
+		"auth/config.mk",
+		"nsswitch/config.mk",
+		"lib/basic.mk",
+		"lib/dcom/config.mk",
+		"lib/socket/config.mk",
+		"lib/ldb/config.mk",
+		"lib/tdb/config.mk",
+		"lib/registry/config.mk",
+		"lib/messaging/config.mk",
+		"smb_server/config.mk",
+		"rpc_server/config.mk",
+		"ldap_server/config.mk",
+		"libcli/auth/gensec.mk",
+		"libcli/auth/config.mk",
+		"libcli/ldap/config.mk",
+		"libcli/config.mk",
+		"utils/net/config.mk",
+		"utils/config.mk",
+		"ntvfs/posix/config.mk",
+		"ntvfs/config.mk",
+		"ntvfs/unixuid/config.mk",
+		"torture/config.mk",
+		"librpc/config.mk",
+		"client/config.mk");
 
 	for my $mkfile (@mkfiles) {
 		config_mk::import_file($SMB_BUILD_CTX{INPUT}, $mkfile);
