4afb4aaa6d96c5578586ddffa36297c3170984ce
Andrew Tridgell <tridge@samba.org>
2004-11-15 00:15:25 +0000

    r3742: make test-ldap give a sane error message when the openldap schema files are missing and auto-link the files if they are found in /etc/ldap/schema/

--- source/lib/ldb/tests/test-ldap.sh
+++ source/lib/ldb/tests/test-ldap.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
+SCHEMA_NEEDED="core nis cosine inetorgperson openldap"
+
+# setup needed schema files
+for f in $SCHEMA_NEEDED; do
+    if [ ! -r tests/schema/$f.schema ]; then
+	mkdir -p tests/schema
+	if [ -r /etc/ldap/schema/$f.schema ]; then
+	    ln -s /etc/ldap/schema/$f.schema tests/schema/$f.schema
+	    continue;
+	fi
+
+	echo "ERROR: you need the following OpenLDAP schema files in tests/schema/"
+	for f in $SCHEMA_NEEDED; do
+	    echo "  $f.schema"
+	done
+	exit 1
+    fi
+done
+
 tests/init_slapd.sh
 tests/start_slapd.sh
 
