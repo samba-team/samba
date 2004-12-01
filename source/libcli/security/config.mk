f8b34cdacb8cbb4118a3422e174c209159f9f40d
Andrew Tridgell <tridge@samba.org>
2004-12-01 05:22:24 +0000

    r4025: added a sec_access_check() function for checking security descriptors against a users security token and access_mask

--- source/libcli/security/config.mk
+++ source/libcli/security/config.mk
@@ -12,6 +12,7 @@ NOPROTO = YES
 ADD_OBJ_FILES = libcli/security/security_token.o \
 		libcli/security/security_descriptor.o \
 		libcli/security/dom_sid.o \
+		libcli/security/access_check.o \
 		librpc/ndr/ndr_sec.o
 REQUIRED_SUBSYSTEMS = LIB_SECURITY_NDR
 # End SUBSYSTEM LIB_SECURITY
