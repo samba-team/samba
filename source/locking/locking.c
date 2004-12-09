596f23051363f8cc8896119b3eca0663a61a38c3
Jeremy Allison <jra@samba.org>
2004-12-09 01:07:06 +0000

    r4108: As check_self is *always* False in every invokation, remove the logic for it. We still pass Samba4 RAW-LOCK test. Jeremy.

--- source/locking/locking.c
+++ source/locking/locking.c
@@ -62,14 +62,11 @@ static const char *lock_type_name(enum brl_type lock_type)
 
 /****************************************************************************
  Utility function called to see if a file region is locked.
- If check_self is True, then checks on our own fd with the same locking context
- are still made. If check_self is False, then checks are not made on our own fd
- with the same locking context are not made.
 ****************************************************************************/
 
 BOOL is_locked(files_struct *fsp,connection_struct *conn,
 	       SMB_BIG_UINT count,SMB_BIG_UINT offset, 
-	       enum brl_type lock_type, BOOL check_self)
+	       enum brl_type lock_type)
 {
 	int snum = SNUM(conn);
 	BOOL ret;
@@ -82,7 +79,7 @@ BOOL is_locked(files_struct *fsp,connection_struct *conn,
 
 	ret = !brl_locktest(fsp->dev, fsp->inode, fsp->fnum,
 			     global_smbpid, sys_getpid(), conn->cnum, 
-			     offset, count, lock_type, check_self);
+			     offset, count, lock_type);
 
 	DEBUG(10,("is_locked: brl start=%.0f len=%.0f %s for file %s\n",
 			(double)offset, (double)count, ret ? "locked" : "unlocked",
