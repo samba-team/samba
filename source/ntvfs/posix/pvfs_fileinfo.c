d866d7101e1d4065f446ed09bf6c9583a0c87474
Andrew Tridgell <tridge@samba.org>
2004-12-16 12:29:47 +0000

    r4230: now that we set the FLAGS2_EXTENDED_ATTRIBUTES flag, we should mark empty EAs as being of size 4, not size 0

--- source/ntvfs/posix/pvfs_fileinfo.c
+++ source/ntvfs/posix/pvfs_fileinfo.c
@@ -95,7 +95,7 @@ NTSTATUS pvfs_fill_dos_info(struct pvfs_state *pvfs, struct pvfs_filename *name,
 	name->dos.attrib = dos_mode_from_stat(pvfs, &name->st);
 	name->dos.alloc_size = pvfs_round_alloc_size(pvfs, name->st.st_size);
 	name->dos.nlink = name->st.st_nlink;
-	name->dos.ea_size = 0;
+	name->dos.ea_size = 4;
 	name->dos.file_id = (((uint64_t)name->st.st_dev)<<32) | name->st.st_ino;
 	name->dos.flags = 0;
 
