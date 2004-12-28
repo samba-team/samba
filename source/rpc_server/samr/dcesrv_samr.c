9c1df8b9fffe537c63729977d7f205e3e0215b08
Volker Lendecke <vlendec@samba.org>
2004-12-28 23:05:48 +0000

    r4381: Add my copyright

--- source/rpc_server/samr/dcesrv_samr.c
+++ source/rpc_server/samr/dcesrv_samr.c
@@ -4,6 +4,7 @@
    endpoint server for the samr pipe
 
    Copyright (C) Andrew Tridgell 2004
+   Copyright (C) Volker Lendecke 2004
    
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
