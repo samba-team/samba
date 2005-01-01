ac5d3941a564a27ec0312b88b25be59570fc837d
Andrew Tridgell <tridge@samba.org>
2005-01-01 01:40:45 +0000

    r4462: - enable DSSETUP on ncalrpc
    - add DSSETUP to the list of tests run in test_rpc.sh

--- source/script/tests/test_rpc.sh
+++ source/script/tests/test_rpc.sh
@@ -2,7 +2,9 @@
 
 # add tests to this list as they start passing, so we test
 # that they stay passing
-tests="RPC-SCHANNEL RPC-ECHO"
+ncacn_np_tests="RPC-SCHANNEL RPC-ECHO RPC-DSSETUP"
+ncalrpc_tests="RPC-SCHANNEL RPC-ECHO RPC-DSSETUP"
+ncacn_ip_tcp_tests="RPC-SCHANNEL RPC-ECHO"
 
 if [ $# -lt 4 ]; then
 cat <<EOF
@@ -31,6 +33,11 @@ testit() {
 
 for transport in ncalrpc ncacn_np ncacn_ip_tcp; do
  for bindoptions in connect sign seal sign,seal validate padcheck bigendian bigendian,seal; do
+     case $transport in
+	 ncalrpc) tests=$ncalrpc_tests ;;
+	 ncacn_np) tests=$ncacn_np_tests ;;
+	 ncacn_ip_tcp) tests=$ncacn_ip_tcp_tests ;;
+     esac
    for t in $tests; do
     echo Testing $t on $transport with $bindoptions
     testit bin/smbtorture $transport:"$server[$bindoptions]" -U"$username"%"$password" -W $domain $t "$*"
