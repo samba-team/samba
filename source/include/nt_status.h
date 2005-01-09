a20fcb4a18338fb312a983eaadc54e8817785de4
Stefan Metzmacher <metze@samba.org>
2005-01-09 02:30:01 +0000

    r4607: add some usefull macros to make the code more readable
    metze

--- source/include/nt_status.h
+++ source/include/nt_status.h
@@ -64,12 +64,48 @@ typedef uint32_t WERROR;
 	}\
 } while (0)
 
+#define NT_STATUS_IS_OK_RETURN(x) do { \
+	if (NT_STATUS_IS_OK(x)) {\
+		return x;\
+	}\
+} while (0)
+
+#define NT_STATUS_NOT_OK_RETURN(x) do { \
+	if (!NT_STATUS_IS_OK(x)) {\
+		return x;\
+	}\
+} while (0)
+
+#define NT_STATUS_IS_ERR_RETURN(x) do { \
+	if (NT_STATUS_IS_ERR(x)) {\
+		return x;\
+	}\
+} while (0)
+
+#define NT_STATUS_NOT_ERR_RETURN(x) do { \
+	if (!NT_STATUS_IS_ERR(x)) {\
+		return x;\
+	}\
+} while (0)
+
 #define W_ERROR_IS_OK(x) (W_ERROR_V(x) == 0)
 #define W_ERROR_EQUAL(x,y) (W_ERROR_V(x) == W_ERROR_V(y))
 
 #define W_ERROR_HAVE_NO_MEMORY(x) do { \
 	if (!(x)) {\
 		return WERR_NOMEM;\
+	}\
+} while (0)
+
+#define W_ERROR_IS_OK_RETURN(x) do { \
+	if (W_ERROR_IS_OK(x)) {\
+		return x;\
+	}\
+} while (0)
+
+#define W_ERROR_NOT_OK_RETURN(x) do { \
+	if (!W_ERROR_IS_OK(x)) {\
+		return x;\
 	}\
 } while (0)
 
