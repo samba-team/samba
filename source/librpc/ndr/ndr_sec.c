795c19456144f1be5d9e43f913fd876f2072b270
Andrew Tridgell <tridge@samba.org>
2005-01-16 03:44:08 +0000

    r4772: fixed checking of the conformant size for dom_sid2

--- source/librpc/ndr/ndr_sec.c
+++ source/librpc/ndr/ndr_sec.c
@@ -35,7 +35,13 @@ NTSTATUS ndr_pull_dom_sid2(struct ndr_pull *ndr, int ndr_flags, struct dom_sid *
 		return NT_STATUS_OK;
 	}
 	NDR_CHECK(ndr_pull_uint32(ndr, &num_auths));
-	return ndr_pull_dom_sid(ndr, ndr_flags, sid);
+	NDR_CHECK(ndr_pull_dom_sid(ndr, ndr_flags, sid));
+	if (sid->num_auths != num_auths) {
+		return ndr_pull_error(ndr, NDR_ERR_CONFORMANT_SIZE, 
+				      "Bad conformant size %u should be %u", 
+				      num_auths, sid->num_auths);
+	}
+	return NT_STATUS_OK;
 }
 
 /*
