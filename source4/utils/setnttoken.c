4d7e4c80580f9902339680cecee457460fe85218
Andrew Tridgell <tridge@samba.org>
2005-01-17 04:07:33 +0000

    r4794: - disabled the ntacl command line utilities until they are rewritten to use the same   acl format as we use in pvfs (and hopefully use common code too)
    - removed a lot of old cruft from our autoconf tests. This may well break some builds,
      but then we can fix them properly instead of the "if solaris version 5.1.2" crap
    
    This was prompted by someone sending me solaris 10 patches that
    patched the configure script with if statements for several more
    versions of solaris to check for and do special stuff. That is just
    silly.
    (This used to be commit 1ea59d1146f041e9befbb435e901c6d7d497c52c)

--- source4/utils/setnttoken.c
+++ source4/utils/setnttoken.c
@@ -23,60 +23,8 @@
 #include "includes.h"
 #include "system/filesys.h"
 
-#if (defined(HAVE_NO_ACLS) || !defined(HAVE_XATTR_SUPPORT))
-
 int main(int argc, char **argv)
 {
-	printf("ACL support not compiled in.");
+	printf("This utility disabled until rewritten\n");
 	return 1;
 }
-
-#else
-
-int main(int argc, char **argv)
-{
-	char line[255];
-	struct ndr_push *ndr;
-	struct lsa_SidArray sidarray;
-	NTSTATUS status;
-	TALLOC_CTX *mem_ctx;
-
-	static_init_setnttoken;
-
-	setup_logging("setnttoken", DEBUG_STDOUT);
-
-	mem_ctx = talloc_init("setnttoken");
-
-	ndr = ndr_push_init();
-
-	sidarray.num_sids = 0;
-	sidarray.sids = NULL;
-
-	while(fgets(line, sizeof(line), stdin)) {
-		struct dom_sid *sid = dom_sid_parse_talloc(ndr, line);
-
-		if (!sid) {
-			fprintf(stderr, "Invalid sid: %s", line);
-			continue;
-		}
-
-		sidarray.sids = talloc_realloc(mem_ctx, sidarray.sids,
-				(sidarray.num_sids + 1) * sizeof(struct lsa_SidPtr));
-
-		sidarray.sids[sidarray.num_sids].sid =
-			dom_sid_dup(ndr, sid);
-
-		sidarray.num_sids++;
-	}
-
-/*	NDR_PRINT_DEBUG(lsa_SidArray, &sidarray); */
-
-	status = ndr_push_lsa_SidArray(
-		ndr, NDR_SCALARS|NDR_BUFFERS, &sidarray);
-
-	fwrite(ndr->data, 1, ndr->offset, stdout);
-
-	return 0;
-}
-
-#endif /* HAVE_NO_ACLS */
