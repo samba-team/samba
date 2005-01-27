f86521677d7ff16bdc4815f9524e5286026f10f3
Andrew Tridgell <tridge@samba.org>
2005-01-27 06:16:59 +0000

    r5034: - added a type mapping function in pidl, so the type names in our IDL   files don't need to match the type names in the generated headers
    - with this type mapping we no longer need definitions for the
      deprecated "int32", "uint8" etc form of types. We can now force
      everyone to use the standard types int32_t, uint8_t etc.
    
    - fixed all the code that used the deprecated types
    
    - converted the IDL types "int64" and "uint64" to "dlong" and
      "udlong". These are the 4 byte aligned 64 bit integers that
      Microsoft internally define as two 32 bit integers in a
      structure. After discussions with Ronnie Sahlberg we decided that
      calling these "int64" was confusing, as it implied a true 8 byte
      aligned type
    
    - fixed all the cases where we incorrectly used things like
      "NTTIME_hyper" in our C code. The generated API now uses a NTTIME for
      those. The fact that it is hyper-aligned on the wire is not relevant
      to the API, and should remain just a IDL property

--- source/include/smb.h
+++ source/include/smb.h
@@ -196,17 +196,8 @@ typedef struct data_blob {
 	size_t length;
 } DATA_BLOB;
 
-/* 8 byte aligned 'hyper' type from MS IDL */
-typedef uint64_t HYPER_T;
-
 /* 64 bit time (100 nanosec) 1601 - cifs6.txt, section 3.5, page 30, 4 byte aligned */
 typedef uint64_t NTTIME;
-
-/* 64 bit time (100 nanosec) 1601, but 8 byte aligned 'hyper' type */
-#define NTTIME_hyper NTTIME
-
-/* 64 bit time (1 sec) 1601 - in the NDR blob but mapped to NTTIME  */
-#define NTTIME_1sec NTTIME
 
 /* the basic packet size, assuming no words or bytes. Does not include the NBT header */
 #define MIN_SMB_SIZE 35
