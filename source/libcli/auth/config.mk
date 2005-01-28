3351c636af23ad88649e84f4cb88fc1167d5c654
Andrew Tridgell <tridge@samba.org>
2005-01-28 11:25:01 +0000

    r5053: - fix up the library dependencies so that tools that need nbt don't   need to pull in the whole dcerpc subsystem
    - moved smbencrypt.c code into libcli/auth/

--- source/libcli/auth/config.mk
+++ source/libcli/auth/config.mk
@@ -3,7 +3,8 @@
 [SUBSYSTEM::LIBCLI_AUTH]
 ADD_OBJ_FILES = libcli/auth/schannel.o \
 		libcli/auth/credentials.o \
-		libcli/auth/session.o 
+		libcli/auth/session.o \
+		libcli/auth/smbencrypt.o 
 REQUIRED_SUBSYSTEMS = \
 		AUTH SCHANNELDB GENSEC
 # End SUBSYSTEM LIBCLI_AUTH
