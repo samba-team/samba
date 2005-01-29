7d43a2bdfef260ebcb948d6d856345e51721ab6b
Tim Potter <tpot@samba.org>
2005-01-29 04:00:14 +0000

    r5087: Add --swig to pidl flags so that swig .i files are generated with make idl. (This used to be commit 7ba9f329aa8baa8e8c0efe742ad51eb5b3487cbc)

--- source4/script/build_idl.sh
+++ source4/script/build_idl.sh
@@ -4,7 +4,7 @@ FULLBUILD=$1
 
 [ -d librpc/gen_ndr ] || mkdir -p librpc/gen_ndr || exit 1
 
-PIDL="$PERL ./build/pidl/pidl.pl --output librpc/gen_ndr/ndr_ --parse --header --parser --server --client"
+PIDL="$PERL ./build/pidl/pidl.pl --output librpc/gen_ndr/ndr_ --parse --header --parser --server --client --swig"
 
 if [ x$FULLBUILD = xFULL ]; then
       echo Rebuilding all idl files in librpc/idl
