fcc690a274963565d13fc358edc61db57c4111b9
Stefan Metzmacher <metze@samba.org>
2005-01-31 16:07:56 +0000

    r5138: don't create prototypes for samba3's winbind client lib they're in hand declared
    metze

--- source/nsswitch/config.mk
+++ source/nsswitch/config.mk
@@ -2,6 +2,6 @@
 # Start SUBSYSTEM LIB_WINBIND_CLIENT
 [SUBSYSTEM::LIB_WINBIND_CLIENT]
 ADD_OBJ_FILES = nsswitch/wb_common.o
-
+NOPROTO=YES
 # End SUBSYSTEM LIB_WINBIND_CLIENT
 #################################
