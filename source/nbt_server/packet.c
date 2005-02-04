25e2708befcf2b246a657855c5fd6f04a013ba9f
Andrew Tridgell <tridge@samba.org>
2005-02-04 02:05:27 +0000

    r5212: added checking for receiving our own packets as broadcasts

--- source/nbt_server/packet.c
+++ source/nbt_server/packet.c
@@ -36,3 +36,32 @@ void nbtd_bad_packet(struct nbt_name_packet *packet,
 	}
 }
 
+
+/*
+  see if an incoming packet is from one of our own interfaces
+*/
+BOOL nbtd_self_packet(struct nbt_name_socket *nbtsock, 
+		      struct nbt_name_packet *packet, 
+		      const char *src_address, int src_port)
+{
+	struct nbt_interface *iface = talloc_get_type(nbtsock->incoming.private, 
+						      struct nbt_interface);
+	struct nbt_server *nbtsrv = iface->nbtsrv;
+	
+	if (src_port != lp_nbt_port()) {
+		return False;
+	}
+
+	for (iface=nbtsrv->interfaces;iface;iface=iface->next) {
+		if (strcmp(src_address, iface->ip_address) == 0) {
+			return True;
+		}
+	}
+
+	if (nbtsrv->bcast_interface &&
+	    strcmp(src_address, nbtsrv->bcast_interface->ip_address) == 0) {
+		return True;
+	}
+
+	return False;
+}
