6aa6dce3f7c4303d184b3098fbf4619e49a27e45
Andrew Tridgell <tridge@samba.org>
2005-02-10 07:18:00 +0000

    r5306: removed all the unused mutex functions from mutex.c. When (if?) we decide to reinstate the mutex code for the threads process model, I'd like to do it a little differently. At least this gets it out of includes.h for now. (This used to be commit cfee0fb02e10add22b6c436bdfa95d1a8f5f3def)

--- source4/lib/mutex.c
+++ source4/lib/mutex.c
@@ -19,98 +19,15 @@
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 #include "includes.h"
+#include "mutex.h"
 	 
-static smb_mutex_t mutex_list[MUTEX_MAX];
-
 /* the registered mutex handlers */
 static struct {
 	const char *name;
 	struct mutex_ops ops;
 } mutex_handlers;
 
-int smb_mutex_lock_by_id(enum mutex_id id, const char *name)
-{
-	return smb_mutex_lock(&mutex_list[id], name);
-}
-
-int smb_mutex_unlock_by_id(enum mutex_id id, const char *name)
-{
-	return smb_mutex_unlock(&mutex_list[id], name);
-}
-
-int smb_mutex_init(smb_mutex_t *mutex, const char *name)
-{
-	if (mutex_handlers.ops.mutex_init) {
-		return mutex_handlers.ops.mutex_init(mutex, name);
-	}
-	return 0;
-}
-
-int smb_mutex_destroy(smb_mutex_t *mutex, const char *name)
-{
-	if (mutex_handlers.ops.mutex_destroy) {
-		return mutex_handlers.ops.mutex_destroy(mutex, name);
-	}
-	return 0;
-}
-
-int smb_mutex_lock(smb_mutex_t *mutex, const char *name)
-{
-	if (mutex_handlers.ops.mutex_lock) {
-		return mutex_handlers.ops.mutex_lock(mutex, name);
-	}
-	return 0;
-}
-
-int smb_mutex_unlock(smb_mutex_t *mutex, const char *name)
-{
-	if (mutex_handlers.ops.mutex_unlock) {
-		return mutex_handlers.ops.mutex_unlock(mutex, name);
-	}
-	return 0;
-}
-
 /* read/write lock routines */
-
-int smb_rwlock_init(smb_rwlock_t *rwlock, const char *name)
-{
-	if (mutex_handlers.ops.rwlock_init) {
-		return mutex_handlers.ops.rwlock_init(rwlock, name);
-	}
-	return 0;
-}
-
-int smb_rwlock_destroy(smb_rwlock_t *rwlock, const char *name)
-{
-	if (mutex_handlers.ops.rwlock_destroy) {
-		return mutex_handlers.ops.rwlock_destroy(rwlock, name);
-	}
-	return 0;
-}
-
-int smb_rwlock_lock_write(smb_rwlock_t *rwlock, const char *name)
-{
-	if (mutex_handlers.ops.rwlock_lock_write) {
-		return mutex_handlers.ops.rwlock_lock_write(rwlock, name);
-	}
-	return 0;
-}
-
-int smb_rwlock_lock_read(smb_rwlock_t *rwlock, const char *name)
-{
-	if (mutex_handlers.ops.rwlock_lock_read) {
-		return mutex_handlers.ops.rwlock_lock_read(rwlock, name);
-	}
-	return 0;
-}
-
-int smb_rwlock_unlock(smb_rwlock_t *rwlock, const char *name)
-{
-	if (mutex_handlers.ops.rwlock_unlock) {
-		return mutex_handlers.ops.rwlock_unlock(rwlock, name);
-	}
-	return 0;
-}
 
 
 /*
@@ -128,13 +45,6 @@ BOOL register_mutex_handlers(const char *name, struct mutex_ops *ops)
 
 	mutex_handlers.name = name;
 	mutex_handlers.ops = *ops;
-
-	if (mutex_handlers.ops.mutex_init) {
-		enum mutex_id id;
-		for (id=0; id < MUTEX_MAX; id++) {
-			mutex_handlers.ops.mutex_init(&mutex_list[id], "mutex_list");
-		}
-	}
 
 	DEBUG(2,("mutex handler '%s' registered\n", name));
 	return True;
