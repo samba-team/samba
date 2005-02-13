78bbf0580c952e78f2306418526991212a57915b
Stefan Metzmacher <metze@samba.org>
2005-02-13 11:32:11 +0000

    r5373: prepare infrastructure for maing different events implementations possible
    metze

--- source/lib/events/config.mk
+++ source/lib/events/config.mk
@@ -2,7 +2,7 @@
 # Start SUBSYSTEM LIBEVENTS
 [SUBSYSTEM::LIBEVENTS]
 NOPROTO = YES
-INIT_OBJ_FILES = lib/events/events.o
+INIT_OBJ_FILES = lib/events/events_standard.o
 REQUIRED_SUBSYSTEMS = LIBTALLOC
 # End SUBSYSTEM LIBEVENTS
 ##############################
