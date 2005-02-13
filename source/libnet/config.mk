b9dbf17366f543194f420e7e3c56c8086fb7bf1d
Rafal Szczesniak <mimir@samba.org>
2005-02-13 00:50:38 +0000

    r5366: added initial code for adding user accounts to libnet makefile. It doesn't do anything useful yet, but at least doesn't break the build.
    rafal

--- source/libnet/config.mk
+++ source/libnet/config.mk
@@ -7,7 +7,8 @@ ADD_OBJ_FILES = \
 		libnet/libnet_passwd.o \
 		libnet/libnet_time.o \
 		libnet/libnet_rpc.o \
-		libnet/libnet_join.o
+		libnet/libnet_join.o \
+		libnet/libnet_user.o
 REQUIRED_SUBSYSTEMS = RPC_NDR_SAMR RPC_NDR_SRVSVC
 # End SUBSYSTEM LIBNET
 #################################
