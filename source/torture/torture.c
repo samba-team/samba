c5648bb9c89314ace96c3785b120086be3cd6480
Andrew Tridgell <tridge@samba.org>
2005-02-15 04:55:56 +0000

    r5403: a simple WINS benchmarking program

--- source/torture/torture.c
+++ source/torture/torture.c
@@ -2346,6 +2346,7 @@ static struct {
 	{"BENCH-NBENCH",  torture_nbench, 0},
 	{"BENCH-TORTURE", NULL, run_torture},
 	{"BENCH-NBT",     torture_bench_nbt, 0},
+	{"BENCH-WINS",    torture_bench_wins, 0},
 
 	/* RAW smb tests */
 	{"RAW-QFSINFO", torture_raw_qfsinfo, 0},
