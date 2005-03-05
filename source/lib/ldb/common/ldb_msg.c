e21495ab3ee26609fe9cb029e7d3d46306836f02
Andrew Tridgell <tridge@samba.org>
2005-03-05 21:59:48 +0000

    r5665: the data within el2->values can still be used at this point, so don't free

--- source/lib/ldb/common/ldb_msg.c
+++ source/lib/ldb/common/ldb_msg.c
@@ -445,7 +445,6 @@ struct ldb_message *ldb_msg_canonicalize(struct ldb_context *ldb,
 			       sizeof(struct ldb_val) * el2->num_values);
 			el1->num_values += el2->num_values;
 			talloc_free(el2->name);
-			talloc_free(el2->values);
 			if (i+1<msg2->num_elements) {
 				memmove(el2, el2+1, sizeof(struct ldb_message_element) * 
 					(msg2->num_elements - (i+1)));
