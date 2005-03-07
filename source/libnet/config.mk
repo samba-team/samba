f3894e55ea5dbb25405a73224dd8e59f1656276d
Stefan Metzmacher <metze@samba.org>
2005-03-07 12:27:03 +0000

    r5681: fix the build and compiler wanings
    metze

--- source/libnet/config.mk
+++ source/libnet/config.mk
@@ -8,7 +8,8 @@ ADD_OBJ_FILES = \
 		libnet/libnet_time.o \
 		libnet/libnet_rpc.o \
 		libnet/libnet_join.o \
-		libnet/libnet_user.o
+		libnet/libnet_user.o \
+		libnet/userinfo.o
 REQUIRED_SUBSYSTEMS = RPC_NDR_SAMR RPC_NDR_SRVSVC
 # End SUBSYSTEM LIBNET
 #################################
