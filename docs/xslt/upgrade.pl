3c320a23e533e9caf054a05dc730f92d4d8d2396
Jelmer Vernooij <jelmer@samba.org>
2005-03-13 18:25:08 +0000

    More validation fixes; building FO-PDF versions of the HOWTO and the Guide work now!

--- docs/xslt/upgrade.pl
+++ docs/xslt/upgrade.pl
@@ -6,6 +6,7 @@ undef $/;
 while(<>) {
 	s/<smbconfoption><name>(.*?)<\/name><value>(.*?)<\/value><\/smbconfoption>/<smbconfoption name=\"\1\">\2<\/smbconfoption>/g;
 	s/<smbconfoption><name>(.*?)<\/name><\/smbconfoption>/<smbconfoption name=\"\1\"\/>/g;
+	s/<smbconfsection>(.*?)<\/smbconfsection>/<smbconfsection name=\"\1\"\/>/g;
 	s/xmlns:samba=\"http:\/\/samba.org\/common\"/xmlns:samba=\"http:\/\/www.samba.org\/samba\/DTD\/samba-doc\"/g;
 	print $_;
 }
