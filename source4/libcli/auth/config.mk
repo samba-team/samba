b8f395cf78d6983f6a9ee139e99968bde3455795
Andrew Bartlett <abartlet@samba.org>
2005-03-23 09:05:40 +0000

    r5992: Rename schannel.c -> schannel_sign.c.  The rest of the schannel code (from librpc) will be moved into schannel.c soon.
    Andrew Bartlett
    (This used to be commit d6c80ff74b0550641c253316b37f1050c207791c)

--- source4/libcli/auth/config.mk
+++ source4/libcli/auth/config.mk
@@ -1,7 +1,7 @@
 #################################
 # Start SUBSYSTEM GENSEC
 [SUBSYSTEM::LIBCLI_AUTH]
-ADD_OBJ_FILES = libcli/auth/schannel.o \
+ADD_OBJ_FILES = libcli/auth/schannel_sign.o \
 		libcli/auth/credentials.o \
 		libcli/auth/session.o \
 		libcli/auth/smbencrypt.o 
