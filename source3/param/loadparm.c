717845f1093df1205a8d4b0f40480002acb6cafc
Jeremy Allison <jra@samba.org>
2005-03-28 21:31:01 +0000

    r6104: Update dos filetimes to default yes for Office 2000. Jeremy. (This used to be commit d1ff66f6cde844bf0312747c50b402e81846891f)

--- source3/param/loadparm.c
+++ source3/param/loadparm.c
@@ -542,7 +542,7 @@ static service sDefault = {
 	False,			/* bFakeOplocks */
 	False,			/* bDeleteVetoFiles */
 	False,			/* bDosFilemode */
-	False,			/* bDosFiletimes */
+	True,			/* bDosFiletimes */
 	False,			/* bDosFiletimeResolution */
 	False,			/* bFakeDirCreateTimes */
 	True,			/* bBlockingLocks */
