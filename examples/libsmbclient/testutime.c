8e6b4b9867b7507c8a1542a5b3389a08ffba722b
Derrell Lipman <derrell@samba.org>
2005-03-31 16:26:15 +0000

    r6151: additional examples/tests for libsmbclient (This used to be commit a3bd496c921dc775b59be4ff2941f4824ffbec03)

--- examples/libsmbclient/testutime.c
+++ examples/libsmbclient/testutime.c
@@ -8,38 +8,39 @@
 
 int main(int argc, char * argv[]) 
 { 
+    int             ret;
+    int             debug = 0;
+    int             mode = 0666;
     char            buffer[16384]; 
     char            mtime[32];
     char            ctime[32];
     char            atime[32];
     char *          pSmbPath = NULL;
-    char *          pLocalPath = NULL;
-    struct stat     st; 
+    struct stat     st;
+    struct utimbuf  utimbuf;
     
     if (argc == 1)
     {
         pSmbPath = "smb://RANDOM/Public/small";
-        pLocalPath = "/random/home/samba/small";
     }
     else if (argc == 2)
     {
         pSmbPath = argv[1];
-        pLocalPath = NULL;
     }
     else if (argc == 3)
     {
         pSmbPath = argv[1];
-        pLocalPath = argv[2];
+        mode = (int) strtol(argv[2], NULL, 8);
     }
     else
     {
         printf("usage: "
-               "%s [ smb://path/to/file [ /nfs/or/local/path/to/file ] ]\n",
+               "%s [ smb://path/to/file [ octal_mode ] ]\n",
                argv[0]);
         return 1;
     }
 
-    smbc_init(get_auth_data_fn, 0); 
+    smbc_init(get_auth_data_fn, debug); 
     
     if (smbc_stat(pSmbPath, &st) < 0)
     {
@@ -47,24 +48,29 @@ int main(int argc, char * argv[])
         return 1;
     }
     
-    printf("SAMBA\n mtime:%lu/%s ctime:%lu/%s atime:%lu/%s\n",
+    printf("Before\n mtime:%lu/%s ctime:%lu/%s atime:%lu/%s\n",
            st.st_mtime, ctime_r(&st.st_mtime, mtime),
            st.st_ctime, ctime_r(&st.st_ctime, ctime),
            st.st_atime, ctime_r(&st.st_atime, atime)); 
     
-    if (pLocalPath != NULL)
+    utimbuf.actime = st.st_atime - 120;  /* unchangable.  this one wont change */
+    utimbuf.modtime = st.st_mtime - 120; /* this one should succeed */
+    if (smbc_utime(pSmbPath, &utimbuf) < 0)
     {
-        if (stat(pLocalPath, &st) < 0)
-        {
-            perror("stat");
-            return 1;
-        }
-        
-        printf("LOCAL\n mtime:%lu/%s ctime:%lu/%s atime:%lu/%s\n",
-               st.st_mtime, ctime_r(&st.st_mtime, mtime),
-               st.st_ctime, ctime_r(&st.st_ctime, ctime),
-               st.st_atime, ctime_r(&st.st_atime, atime)); 
+        perror("smbc_utime");
+        return 1;
     }
 
+    if (smbc_stat(pSmbPath, &st) < 0)
+    {
+        perror("smbc_stat");
+        return 1;
+    }
+    
+    printf("After\n mtime:%lu/%s ctime:%lu/%s atime:%lu/%s\n",
+           st.st_mtime, ctime_r(&st.st_mtime, mtime),
+           st.st_ctime, ctime_r(&st.st_ctime, ctime),
+           st.st_atime, ctime_r(&st.st_atime, atime)); 
+    
     return 0; 
 }
