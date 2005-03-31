381c3f8b41c12e245f9a35040a5cc674b18b249c
Love Hörnquist Åstrand <lha@kth.se>
2005-03-31 00:37:42 +0000

    const poison
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@14706 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/asn1/main.c
+++ lib/asn1/main.c
@@ -57,8 +57,8 @@ int
 main(int argc, char **argv)
 {
     int ret;
-    char *file;
-    char *name = NULL;
+    const char *file;
+    const char *name = NULL;
     int optind = 0;
 
     setprogname(argv[0]);
