e2f5d36e47cab2e9bc199b743ab6d5a040e6e175
Andrew Tridgell <tridge@samba.org>
2005-05-01 09:58:27 +0000

    r6549: a simple ldap test script

---
+++ source/script/tests/test_ldap.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+SERVER="$1"
+
+# test some simple LDAP operations
+
+echo "Testing RootDSE"
+ldbsearch -b '' -H ldap://$SERVER -s base DUMMY=x dnsHostName highestCommittedUSN || exit 1
+
+echo "Getting defaultNamingContext"
+BASEDN=`ldbsearch -b '' -H ldap://$SERVER -s base DUMMY=x defaultNamingContext | grep ^defaultNamingContext | awk '{print $2}'`
+echo "BASEDN is $BASEDN"
+
+
+echo "Listing Users"
+ldbsearch -H ldap://$SERVER -b "$BASEDN" '(objectclass=user)' sAMAccountName
+
+echo "Listing Groups"
+ldbsearch -H ldap://$SERVER -b "$BASEDN" '(objectclass=group)' sAMAccountName
+
