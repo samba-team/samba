7afff6e9ddb746bb0f77755466e63eea6927fbc8
Volker Lendecke <vlendec@samba.org>
2005-05-07 21:18:27 +0000

    r6654: Make talloc.c 4.0-ready (This used to be commit be6949362f6be0eb5aa1ddf86061bad2a5674073)

--- source3/lib/talloc.c
+++ source3/lib/talloc.c
@@ -29,6 +29,7 @@
 
 #ifdef _SAMBA_BUILD_
 #include "includes.h"
+#if (SAMBA_VERSION_MAJOR<4)
 /* This is to circumvent SAMBA3's paranoid malloc checker. Here in this file
  * we trust ourselves... */
 #ifdef malloc
@@ -36,6 +37,7 @@
 #endif
 #ifdef realloc
 #undef realloc
+#endif
 #endif
 #else
 #include <stdio.h>
