8890dd3ac331cffe83226a356c52df89c917c2b0
Andrew Tridgell <tridge@samba.org>
2005-05-13 06:07:53 +0000

    r6763: added functions in libcli/ldap/ to binary encode some NDR structures into ldap friendly filter strings

--- source/libcli/ldap/config.mk
+++ source/libcli/ldap/config.mk
@@ -3,7 +3,8 @@
 [SUBSYSTEM::LIBCLI_LDAP]
 ADD_OBJ_FILES = libcli/ldap/ldap.o \
 		libcli/ldap/ldap_client.o \
-		libcli/ldap/ldap_ldif.o
+		libcli/ldap/ldap_ldif.o \
+		libcli/ldap/ldap_ndr.o
 NOPROTO=YES
 # End SUBSYSTEM LIBCLI_LDAP
 #################################
