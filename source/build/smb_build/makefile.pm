df1305495ec6be71b7018097dd8c7c3520e2292a
Andrew Tridgell <tridge@samba.org>
2005-05-16 03:04:58 +0000

    r6809: ifeq is not portable in make - jelmer, you'll need to find some other way of doing this if you want detection of socket wrapper :-)

--- source/build/smb_build/makefile.pm
+++ source/build/smb_build/makefile.pm
@@ -154,13 +154,8 @@ test: @DEFAULT_TEST_TARGET@
 
 test-swrap: all
 	export SOCKET_WRAPPER_DIR=.
-ifeq (@HAVE_SOCKET_WRAPPER@,yes)
 	./script/tests/selftest.sh `pwd`/prefix-test
-else
-	echo "Socket wrapper not built in, unable to run test-swrap"
-	exit 1
-endif
-	
+
 test-noswrap: all
 	./script/tests/selftest.sh `pwd`/prefix-test
 
