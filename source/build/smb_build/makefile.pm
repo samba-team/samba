f46c532883e18b8780ff73d3ac0899690eeab3f4
Jelmer Vernooij <jelmer@samba.org>
2005-05-16 21:08:44 +0000

    r6828: More portability fixes

--- source/build/smb_build/makefile.pm
+++ source/build/smb_build/makefile.pm
@@ -85,7 +85,7 @@ sub _prepare_compiler_linker($)
 
 	$output = << '__EOD__';
 CC=@CC@
-CC_FLAGS=-Iinclude -I. -I$(srcdir)/include -I$(srcdir) -D_SAMBA_BUILD_ -Ilib @CFLAGS@ @CPPFLAGS@
+CC_FLAGS=-Iinclude -I. -I$(srcdir)/include -I$(srcdir) -D_SAMBA_BUILD_ -DHAVE_CONFIG_H -Ilib @CFLAGS@ @CPPFLAGS@
 
 LD=@CC@
 LD_FLAGS=@LDFLAGS@ @CFLAGS@
