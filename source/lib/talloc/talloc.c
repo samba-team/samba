fbc87c8bb1b2468c8e2e7d8677bbd153259517f8
Jelmer Vernooij <jelmer@samba.org>
2005-05-16 21:08:44 +0000

    r6828: More portability fixes

--- source/lib/talloc/talloc.c
+++ source/lib/talloc/talloc.c
@@ -27,12 +27,21 @@
 */
 
 
+#ifdef HAVE_CONFIG_H
 #include "config.h"
+#endif
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#ifdef HAVE_SYS_TYPES_H
+#include <sys/types.h>
+#endif
+
+#ifdef HAVE_UNISTD_H
 #include <unistd.h>
+#endif
 
 #ifdef HAVE_STDARG_H
 #include <stdarg.h>
