428389a775e922f804e18c1bf7508ca39eb1dc49
Jelmer Vernooij <jelmer@samba.org>
2005-05-17 15:51:35 +0000

    r6860: Add some allocation and alignment tests, remove uint64 from list of scalars (it doesn't have any push/pull functions anymore either)

--- source/build/pidl/tests/ndr_align.pl
+++ source/build/pidl/tests/ndr_align.pl
@@ -23,11 +23,63 @@ Test::test_idl('align-uint8-uint16', \%settings,
 '
 	struct ndr_push *ndr = ndr_push_init();
 	struct bla r;
-	uint8_t expected[] = { 0x0D, 0x00, 0xbe, 0xef };
+	uint8_t expected[] = { 0x0D, 0x00, 0xef, 0xbe };
 	DATA_BLOB expected_blob = { expected, 4 };
 	DATA_BLOB result_blob;
 	r.x = 13;
 	r.y = 0xbeef;
+
+	if (NT_STATUS_IS_ERR(ndr_push_bla(ndr, NDR_SCALARS|NDR_BUFFERS, &r)))
+		return 1;
+
+	result_blob = ndr_push_blob(ndr);
+	
+	if (!data_blob_equal(&result_blob, &expected_blob)) 
+		return 2;
+');
+
+Test::test_idl('align-uint8-uint32', \%settings,
+'
+	typedef [public] struct { 
+		uint8 x;
+		uint32 y;
+	} bla;
+',
+'
+	struct ndr_push *ndr = ndr_push_init();
+	struct bla r;
+	uint8_t expected[] = { 0x0D, 0x00, 0x00, 0x00, 0xef, 0xbe, 0xef, 0xbe };
+	DATA_BLOB expected_blob = { expected, 8 };
+	DATA_BLOB result_blob;
+	r.x = 13;
+	r.y = 0xbeefbeef;
+
+	if (NT_STATUS_IS_ERR(ndr_push_bla(ndr, NDR_SCALARS|NDR_BUFFERS, &r)))
+		return 1;
+
+	result_blob = ndr_push_blob(ndr);
+	
+	if (!data_blob_equal(&result_blob, &expected_blob)) 
+		return 2;
+');
+
+
+Test::test_idl('align-uint8-hyper', \%settings,
+'
+	typedef [public] struct { 
+		uint8 x;
+		hyper y;
+	} bla;
+',
+'
+	struct ndr_push *ndr = ndr_push_init();
+	struct bla r;
+	uint8_t expected[] = { 0x0D, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+						   0xef, 0xbe, 0xef, 0xbe, 0xef, 0xbe, 0xef, 0xbe };
+	DATA_BLOB expected_blob = { expected, 16 };
+	DATA_BLOB result_blob;
+	r.x = 13;
+	r.y = 0xbeefbeefbeefbeef;
 
 	if (NT_STATUS_IS_ERR(ndr_push_bla(ndr, NDR_SCALARS|NDR_BUFFERS, &r)))
 		return 1;
--- source/script/tests/test_pidl.sh
+++ source/script/tests/test_pidl.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 ./build/pidl/tests/ndr_simple.pl
 ./build/pidl/tests/ndr_align.pl
+./build/pidl/tests/ndr_alloc.pl
 ./build/pidl/tests/ndr_refptr.pl
