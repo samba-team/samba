e6b147ebc6a430213c2ae26c9866bc22457b99da
Derrell Lipman <derrell@samba.org>
2005-05-24 13:31:22 +0000

    r6956: added start of ldb_sqlite3 work

---
+++ source/lib/ldb/ldb_sqlite3/ldb_sqlite3.h
@@ -0,0 +1,25 @@
+#include <sqlite3.h>
+
+struct lsqlite3_private {
+	char **options;
+	const char *basedn;
+        sqlite3 * sqlite;
+        int lock_count;
+	int last_rc;
+        struct {
+                sqlite3_stmt *begin;
+                sqlite3_stmt *commit;
+                sqlite3_stmt *rollback;
+                sqlite3_stmt *newDN;
+                sqlite3_stmt *renameDN;
+                sqlite3_stmt *deleteDN;
+                sqlite3_stmt *newObjectClass;
+                sqlite3_stmt *assignObjectClass;
+                sqlite3_stmt *newAttributeUseDefaults;
+                sqlite3_stmt *newAttribute;
+                sqlite3_stmt *addAttrValuePair;
+                sqlite3_stmt *replaceAttrValuePairs;
+                sqlite3_stmt *deleteAttrValuePairs;
+                sqlite3_stmt *insertSubclass;
+        } queries;
+};
