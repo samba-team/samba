9b8c012392b6810786267a2a744540fba3878f05
Tim Potter <tpot@samba.org>
2005-05-27 07:38:41 +0000

    r7010: Merge libcli/libsmb.mk into libcli/config.mk (This used to be commit d7d48adce9628ee7a0d2f8ac3504745aaeb912b9)

--- source4/libcli/libsmb.mk
+++
@@ -1,9 +0,0 @@
-[SUBSYSTEM::LIBSMB]
-REQUIRED_SUBSYSTEMS = LIBCLI SOCKET
-ADD_OBJ_FILES = libcli/clireadwrite.o \
-		libcli/cliconnect.o \
-		libcli/clifile.o \
-		libcli/clilist.o \
-		libcli/clitrans2.o \
-		libcli/climessage.o \
-		libcli/clideltree.o
