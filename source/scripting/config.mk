e14c00c4c1cc51dac9452c1e950445907d883e46
Tim Potter <tpot@samba.org>
2005-05-29 00:22:34 +0000

    r7062: Merge scripting/swig/config.mk with scripting/config.mk

--- source/scripting/config.mk
+++ source/scripting/config.mk
@@ -6,3 +6,17 @@ OBJ_FILES = \
 REQUIRED_SUBSYSTEMS = EJS LIBBASIC
 # End BINARY SMBSCRIPT
 #######################
+
+#######################
+# Start LIBRARY swig_tdb
+[LIBRARY::swig_tdb]
+REQUIRED_SUBSYSTEMS = LIBTDB
+# End LIBRARY swig_tdb
+#######################
+
+#######################
+# Start LIBRARY swig_dcerpc
+[LIBRARY::swig_dcerpc]
+REQUIRED_SUBSYSTEMS = LIBCLI NDR_MISC LIBBASIC CONFIG RPC_NDR_SAMR RPC_NDR_LSA
+# End LIBRARY swig_dcerpc
+#######################
