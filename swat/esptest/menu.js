86d2ecf0f2a8de8abfdcc5f2aae7d4d969d19339
Andrew Tridgell <tridge@samba.org>
2005-05-30 06:15:01 +0000

    r7088: start on some real structure for the SWAT web pages
    - this imports the basic css pages from samba.org
    
    - i have created some useful ejs scripts in common.js that will be included by all pages
    
    - added a real login page, and a logout button showing who you are logged in as
    
    - added page_header() and page_footer() functions that take a page type, allowing
      for "plain" or "columms" pages
    
    - added some simple menus on the left of the columns page type, with links to the esp
      tests and some useful links for samba4 developers

---
+++ swat/esptest/menu.js
@@ -0,0 +1,11 @@
+/* show a menu for the esp test pages */
+simple_menu(
+	"ESP Tests",
+	"ldb database",       "/esptest/ldb.esp",
+	"html forms",         "/esptest/formtest.esp",
+	"esp includes",       "/esptest/include.esp",
+	"session variables",  "/esptest/session.esp",
+	"loadparm access",    "/esptest/loadparm.esp",
+	"exception handling", "/esptest/exception.esp",
+	);
+
