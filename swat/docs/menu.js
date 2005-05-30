31ae74192328c74e7b609c10558481cd93c3ce09
Andrew Tridgell <tridge@samba.org>
2005-05-30 12:18:49 +0000

    r7105: fixed a typo

--- swat/docs/menu.js
+++ swat/docs/menu.js
@@ -5,5 +5,5 @@ simple_menu(
 	"Recent Checkins", "http://build.samba.org/?tree=samba4&function=Recent+Checkins",
 	"Recent Builds",   "http://build.samba.org/?tree=samba4&function=Recent+Builds",
 	"EJS Information", "http://www.appwebserver.org/products/ejs/ejs.html",
-	"ESP Information", "http://www.appwebserver.org/products/ejs/ejs.html",
+	"ESP Information", "http://www.appwebserver.org/products/esp/esp.html",
 	"HTML 4.01 Spec",  "http://www.w3.org/TR/html401/");
