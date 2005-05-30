cab5384b37aa696d82d89bf52cd68bef68520f4e
Andrew Tridgell <tridge@samba.org>
2005-05-30 06:15:01 +0000

    r7088: start on some real structure for the SWAT web pages
    - this imports the basic css pages from samba.org
    
    - i have created some useful ejs scripts in common.js that will be included by all pages
    
    - added a real login page, and a logout button showing who you are logged in as
    
    - added page_header() and page_footer() functions that take a page type, allowing
      for "plain" or "columms" pages
    
    - added some simple menus on the left of the columns page type, with links to the esp
      tests and some useful links for samba4 developers

---
+++ swat/scripting/common.js
@@ -0,0 +1,56 @@
+/*
+	js functions and code common to all pages
+*/
+
+/* define some global variables for this request */
+global.page = new Object();
+
+/* fill in some defaults */
+global.page.title = "Samba Web Administration Tool";
+
+/*
+  show the page header. page types include "plain" and "column" 
+*/
+function page_header(pagetype, title) {
+	global.page.pagetype = pagetype;
+	global.page.title = title;
+	include("/scripting/header_" + pagetype + ".esp");
+}
+
+/*
+  show the page footer, getting the page type from page.pagetype
+  set in page_header()
+*/
+function page_footer() {
+	include("/scripting/footer_" + global.page.pagetype + ".esp");
+}
+
+/*
+  check if a uri is one of the 'always allowed' pages, even when not logged in
+  This allows the login page to use the same style sheets and images
+*/
+function always_allowed(uri) {
+	var allowed = new Array("/images/favicon.ico", 
+				"/images/linkpad.gif",
+				"/images/logo.png",
+				"/style/main.css",
+				"/style/common.css");
+	for (i in allowed) {
+		if (allowed[i] == uri) {
+			return true;
+		}
+	}
+	return false;
+}
+
+/*
+  display a simple menu. First argument is menu title, followed by
+  pairs of menu item name and link
+*/
+function simple_menu() {
+	write("<i>" + arguments[0] + "</i><br /><ul>\n");
+	for (i = 1; i < arguments.length; i = i + 2) {
+		write("<li><a href=\"" + arguments[i+1] + "\">" + arguments[i] + "</a></li>\n");
+	}
+	write("</ul>\n");
+}
