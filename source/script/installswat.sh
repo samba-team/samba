3ca7b77d77f5a97713ecd36eae8897b347227b04
Stefan Metzmacher <metze@samba.org>
2005-05-30 09:31:59 +0000

    r7100: install lately added files too
    metze

--- source/script/installswat.sh
+++ source/script/installswat.sh
@@ -21,9 +21,14 @@ installdir() {
 installdir . esp
 installdir . ico
 installdir esptest esp
+installdir esptest js
 installdir images png
+installdir images gif
 installdir scripting ejs
 installdir scripting esp
+installdir scripting js
+installdir style css
+installdir docs js
 
 cat << EOF
 ======================================================================
