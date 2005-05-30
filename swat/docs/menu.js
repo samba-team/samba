dded47a3d1e131f04825779b36eac7a28adad05c
Andrew Tridgell <tridge@samba.org>
2005-05-30 06:54:43 +0000

    r7091: added some more useful links

--- swat/docs/menu.js
+++ swat/docs/menu.js
@@ -3,4 +3,7 @@ simple_menu(
 	"Samba Information",
 	"Samba4 development", "http://devel.samba.org/",
 	"Recent Checkins", "http://build.samba.org/?tree=samba4&function=Recent+Checkins",
-	"Recent Builds",   "http://build.samba.org/?tree=samba4&function=Recent+Builds");
+	"Recent Builds",   "http://build.samba.org/?tree=samba4&function=Recent+Builds",
+	"EJS Information", "http://www.appwebserver.org/products/ejs/ejs.html",
+	"ESP Information", "http://www.appwebserver.org/products/ejs/ejs.html",
+	"HTML 4.01 Spec",  "http://www.w3.org/TR/html401/");
