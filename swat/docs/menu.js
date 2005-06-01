edeae99f8f79d960d52cbb1bef9826b3af4b68e5
Deryck Hodge <deryck@samba.org>
2005-06-01 18:47:44 +0000

    r7171: Just learning my way around the new SWAT, and updating some docs/specs in the menu...
    Linking to XHTML specs since we're sending the pages as XHTML.  
    Also, added a link to CSS specs to go along with the CSS quick 
    reference card.

--- swat/docs/menu.js
+++ swat/docs/menu.js
@@ -1,5 +1,5 @@
 /* show a menu for the docs directory */
-var m = MenuObj("Samba Information", 8);
+var m = MenuObj("Samba Information", 9);
 
 m.element[0].label = "Samba4 development";
 m.element[0].link  = "http://devel.samba.org/";
@@ -11,12 +11,14 @@ m.element[3].label = "EJS Information";
 m.element[3].link  = "http://www.appwebserver.org/products/ejs/ejs.html";
 m.element[4].label = "ESP Information";
 m.element[4].link  = "http://www.appwebserver.org/products/esp/esp.html";
-m.element[5].label = "HTML 4.01 Spec";
-m.element[5].link  = "http://www.w3.org/TR/html401/";
+m.element[5].label = "XHTML Spec";
+m.element[5].link  = "http://www.w3.org/TR/xhtml1/";
 m.element[6].label = "JavaScript Spec";
 m.element[6].link  = "http://www.ecma-international.org/publications/files/ecma-st/ECMA-262.pdf";
-m.element[7].label = "CSS2 Reference";
-m.element[7].link  = "http://www.w3schools.com/css/css_reference.asp";
+m.element[7].label = "CSS Specs";
+m.element[7].link = "http://www.w3.org/Style/CSS/#specs";
+m.element[8].label = "CSS1/2 Reference";
+m.element[8].link  = "http://www.w3schools.com/css/css_reference.asp";
 
 display_menu(m);
 
