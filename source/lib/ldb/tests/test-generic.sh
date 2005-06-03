9293de1c354854ace3f8f60a2accc72f4686c6e2
Simo Sorce <idra@samba.org>
2005-06-03 14:20:25 +0000

    r7239: add a little test

--- source/lib/ldb/tests/test-generic.sh
+++ source/lib/ldb/tests/test-generic.sh
@@ -38,6 +38,7 @@ $VALGRIND bin/ldbsearch '(&(uid=uham)(uid=uham)(!(objectclass=xxx)))'  || exit 1
 $VALGRIND bin/ldbsearch '(&(objectclass=person)(uid=uham)(!(uid=uhamxx)))' uid \* \+ dn  || exit 1
 $VALGRIND bin/ldbsearch '(&(uid=uham)(uid=uha*)(title=*))' uid || exit 1
 $VALGRIND bin/ldbsearch '((' uid && exit 1
+$VALGRIND bin/ldbsearch '(objectclass=)' uid || exit 1
 $VALGRIND bin/ldbsearch 'dn=cn=Hampster Ursula,ou=Alumni Association,ou=People,o=University of Michigan,c=US' uid || exit 1
 
 echo "Starting ldbtest indexed"
