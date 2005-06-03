6eb8838190534106c6afedf37ba52cd03d501c8b
Simo Sorce <idra@samba.org>
2005-06-03 14:11:04 +0000

    r7236: uhmm this require to be in too (This used to be commit 3b5fc036f3ffe57282b291454f6c632306e4c8d5)

--- source4/web_server/config.mk
+++ source4/web_server/config.mk
@@ -20,10 +20,8 @@ INIT_OBJ_FILES = \
 		web_server/web_server.o
 ADD_OBJ_FILES = \
 		web_server/http.o \
-		web_server/calls.o \
 		web_server/tls.o \
-		web_server/tlscert.o \
-		web_server/pam.o
-REQUIRED_SUBSYSTEMS = ESP EXT_LIB_GNUTLS EXT_LIB_PAM SMBCALLS PAM_ERRORS
+		web_server/tlscert.o
+REQUIRED_SUBSYSTEMS = ESP EXT_LIB_GNUTLS SMBCALLS
 # End SUBSYSTEM WEB
 #######################
