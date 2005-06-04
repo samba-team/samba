7cbb95d3f55dbaf9ca606655377682841e4c534d
Andrew Tridgell <tridge@samba.org>
2005-06-04 05:35:27 +0000

    r7264: fix up the socket handling for abartlet. Still only udp, but it won't be hard to do tcp as well.

--- source/kdc/config.mk
+++ source/kdc/config.mk
@@ -1,12 +1,12 @@
-# CLDAP server subsystem
+# KDC server subsystem
 
 #######################
-# Start SUBSYSTEM CLDAPD
+# Start SUBSYSTEM KDC
 [SUBSYSTEM::KDC]
 INIT_OBJ_FILES = \
 		kdc/kdc.o \
 		kdc/hdb-ldb.o
 REQUIRED_SUBSYSTEMS = \
 		SOCKET
-# End SUBSYSTEM CLDAPD
+# End SUBSYSTEM KDC
 #######################
