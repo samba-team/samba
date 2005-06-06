e6a2174050bb55afb32d54b8cfe12c7358bf98f2
Andrew Tridgell <tridge@samba.org>
2005-06-06 05:52:07 +0000

    r7322: the beginnings of a in-tree heimdal

---
+++ source/heimdal_build/crypto-headers.h
@@ -0,0 +1,11 @@
+#ifndef __crypto_headers_h__
+#define __crypto_headers_h__
+#include <krb5-types.h>
+#include <des.h>
+#include <md4.h>
+#include <md5.h>
+#include <sha.h>
+#include <rc4.h>
+#include <rc2.h>
+#include <aes.h>
+#endif /* __crypto_headers_h__ */
