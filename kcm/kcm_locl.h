93aadcfbfa5fb0a020c9cea4ed4e3ccb88e092fa
Love Hörnquist Åstrand <lha@kth.se>
2005-06-07 00:31:29 +0000

    (disallow_getting_krbtgt): Declare.
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@15351 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- kcm/kcm_locl.h
+++ kcm/kcm_locl.h
@@ -159,6 +159,7 @@ extern size_t max_request;
 extern sig_atomic_t exit_flag;
 extern int name_constraints;
 extern int detach_from_console;
+extern int disallow_getting_krbtgt;
 
 #if 0
 extern const krb5_cc_ops krb5_kcmss_ops;
