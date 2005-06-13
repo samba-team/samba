b66da46e40a66535d4ac67e4745f25e612438574
Jelmer Vernooij <jelmer@samba.org>
2005-06-13 20:25:32 +0000

    Fix image quality

--- docs/smbdotconf/generate-file-list.sh
+++ docs/smbdotconf/generate-file-list.sh
@@ -9,7 +9,7 @@ OLD=`pwd`
 cd $DIR
 
 echo "<variablelist xmlns:xi=\"http://www.w3.org/2003/XInclude\">"
-for I in `find -mindepth 2 . -type f -name '*.xml' | sort -t/ -k3 | xargs`
+for I in `find . -mindepth 2 -type f -name '*.xml' | sort -t/ -k3 | xargs`
 do 
 	echo "<xi:include href='$I' parse='xml'/>"
 done
