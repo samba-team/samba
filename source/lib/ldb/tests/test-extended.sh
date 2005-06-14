c05c043819f9bacd03a06977ba7708667dc11c41
Andrew Tridgell <tridge@samba.org>
2005-06-14 03:37:43 +0000

    r7564: added a test showing the search expression that w2k is actually giving us that triggered this work

--- source/lib/ldb/tests/test-extended.sh
+++ source/lib/ldb/tests/test-extended.sh
@@ -15,6 +15,22 @@ i1: 0x800000
 dn: testrec3
 i1: 0x101010101
 i1: 7
+
+dn: auser1
+groupType: 2147483648
+samAccountType: 805306368
+
+dn: auser2
+groupType: 2147483648
+samAccountType: 805306369
+
+dn: auser3
+groupType: 2147483649
+samAccountType: 805306370
+
+dn: auser4
+groupType: 2147483649
+samAccountType: 805306369
 EOF
 
 checkcount() {
@@ -45,5 +61,8 @@ checkcount 2 '(i1:1.2.840.113556.1.4.804:=7)'
 checkcount 2 '(i1:1.2.840.113556.1.4.804:=15)'
 checkcount 1 '(i1:1.2.840.113556.1.4.804:=0x800000)'
 checkcount 1 '(i1:1.2.840.113556.1.4.804:=8388608)'
+
+# this is one that w2k gives
+checkcount 3 '(|(|(&(!(groupType:1.2.840.113556.1.4.803=1))(groupType:1.2.840.113556.1.4.803=2147483648)(groupType:1.2.840.113556.1.4.804=10))(samAccountType=805306368))(samAccountType=805306369))'
 
 rm -f $LDB_URL
