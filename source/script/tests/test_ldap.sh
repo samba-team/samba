98913772c7059aa47fa20af967b25b69bce507d9
Andrew Tridgell <tridge@samba.org>
2005-06-20 01:35:25 +0000

    r7772: actually give the auth options to ldbsearch ....

--- source/script/tests/test_ldap.sh
+++ source/script/tests/test_ldap.sh
@@ -26,17 +26,17 @@ for p in $PROTOCOLS; do
  for options in "" "-U$USERNAME%$PASSWORD"; do
     echo "TESTING PROTOCOL $p with options $options"
 
-    testit "RootDSE" bin/ldbsearch $CONFIGURATION --basedn='' -H $p://$SERVER -s base DUMMY=x dnsHostName highestCommittedUSN || failed=`expr $failed + 1`
+    testit "RootDSE" bin/ldbsearch $CONFIGURATION $options --basedn='' -H $p://$SERVER -s base DUMMY=x dnsHostName highestCommittedUSN || failed=`expr $failed + 1`
 
     echo "Getting defaultNamingContext"
-    BASEDN=`bin/ldbsearch -b '' -H $p://$SERVER -s base DUMMY=x defaultNamingContext | grep ^defaultNamingContext | awk '{print $2}'`
+    BASEDN=`bin/ldbsearch $options -b '' -H $p://$SERVER -s base DUMMY=x defaultNamingContext | grep ^defaultNamingContext | awk '{print $2}'`
     echo "BASEDN is $BASEDN"
 
-    testit "Listing Users" bin/ldbsearch $CONFIGURATION -H $p://$SERVER '(objectclass=user)' sAMAccountName || failed=`expr $failed + 1`
+    testit "Listing Users" bin/ldbsearch $options $CONFIGURATION -H $p://$SERVER '(objectclass=user)' sAMAccountName || failed=`expr $failed + 1`
 
-    testit "Listing Groups" bin/ldbsearch $CONFIGURATION -H $p://$SERVER '(objectclass=group)' sAMAccountName || failed=`expr $failed + 1`
+    testit "Listing Groups" bin/ldbsearch $options $CONFIGURATION -H $p://$SERVER '(objectclass=group)' sAMAccountName || failed=`expr $failed + 1`
 
-    nusers=`bin/ldbsearch -H $p://$SERVER $CONFIGURATION '(|(|(&(!(groupType:1.2.840.113556.1.4.803:=1))(groupType:1.2.840.113556.1.4.803:=2147483648)(groupType:1.2.840.113556.1.4.804:=10))(samAccountType=805306368))(samAccountType=805306369))' sAMAccountName | grep ^sAMAccountName | wc -l`
+    nusers=`bin/ldbsearch $options -H $p://$SERVER $CONFIGURATION '(|(|(&(!(groupType:1.2.840.113556.1.4.803:=1))(groupType:1.2.840.113556.1.4.803:=2147483648)(groupType:1.2.840.113556.1.4.804:=10))(samAccountType=805306368))(samAccountType=805306369))' sAMAccountName | grep ^sAMAccountName | wc -l`
     echo "Found $nusers users"
     if [ $nusers -lt 10 ]; then
 	echo "Should have found at least 10 users"
