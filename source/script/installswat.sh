501fa3b52e9b719aa6ac4946e1d01b96f1c78c82
Tim Potter <tpot@samba.org>
2005-06-20 21:20:47 +0000

    r7788: Update install swat message to reflect the fact that swat/README no longer exists.  Bugzilla #2808.

--- source/script/installswat.sh
+++ source/script/installswat.sh
@@ -169,7 +169,7 @@ fi
 
 cat << EOF
 ======================================================================
-The SWAT files have been installed. Remember to read the swat/README
+The SWAT files have been installed. Remember to read the documentation
 for information on enabling and using SWAT
 ======================================================================
 EOF
