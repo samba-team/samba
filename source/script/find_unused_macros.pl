aa5ade43427eaa38e0d0a1d3d7880246b7f8152d
Jelmer Vernooij <jelmer@samba.org>
2005-06-24 22:49:14 +0000

    r7891: Improve output of unused macro find script Remove duplicate find-missing-doc script (already in samba-docs repository)

--- source/script/find_unused_macros.pl
+++ source/script/find_unused_macros.pl
@@ -3,30 +3,36 @@
 # it defines that are used nowhere in the code
 
 # Arguments: C and H files
+# Copyright Jelmer Vernooij <jelmer@samba.org>, GPL
 
-my %defined,%used,%files;
+use strict;
 
-$in = shift;
+my %defined;
+my %used;
+my %files;
 
+my $tmp;
 while($tmp = shift) { 
 	$files{$tmp} = $tmp;
 	open(FI, $tmp);
+	my $ln = 0;
 	while(<FI>) { 
-		$line = $_;
-		$cur = "";
+		$ln++;
+		my $line = $_;
+		my $cur = "";
 		if(/^#define ([A-Za-z0-9_]+)/) {
-			$defined{$1} = $tmp;
+			$defined{$1} = "$tmp:$ln";
 			$cur = $1;
 		}
 
 		$_ = $line;
 		while(/([A-Za-z0-9_]+)/sgm) { 
-			if($cur cmp $1) { $used{$1} = $tmp; }
+			if($cur ne $1) { $used{$1} = "$tmp:$ln"; }
 		}
 	}
 	close FI;
 }
 
 foreach(keys %defined) {
-	if(!$used{$_}) { print "$_\n"; }
+	if(!$used{$_}) { print "$defined{$_}: Macro `$_' is unused\n"; }
 }
