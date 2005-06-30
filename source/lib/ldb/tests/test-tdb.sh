0ef5f563ec8948c2dd088e8304abfcbb51e2a2ef
Andrew Tridgell <tridge@samba.org>
2005-06-30 03:32:43 +0000

    r8009: expanded the ldb test suite. It worried me that some changes I have pending, which I know break lots of features, still passed our test suite! Now they don't.

--- source/lib/ldb/tests/test-tdb.sh
+++ source/lib/ldb/tests/test-tdb.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 
-export LDB_URL="tdb://tdbtest.ldb"
+export LDB_URL="tdbtest.ldb"
 
 rm -f tdbtest.ldb
 
 . tests/test-generic.sh
 
 . tests/test-extended.sh
+
+. tests/test-tdb-features.sh
