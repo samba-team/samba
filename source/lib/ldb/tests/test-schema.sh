f3e3b2ccd93b87c1c240cf7e44106389b089b526
Andrew Tridgell <tridge@samba.org>
2005-07-01 07:23:14 +0000

    r8039: allow ldb test suite to be run outside of the ldb directory

--- source/lib/ldb/tests/test-schema.sh
+++ source/lib/ldb/tests/test-schema.sh
@@ -7,22 +7,22 @@ rm -f schema.ldb
 echo "LDB_URL: $LDB_URL"
 
 echo "Adding schema"
-$VALGRIND bin/ldbadd tests/schema-tests/schema.ldif || exit 1
+$VALGRIND bin/ldbadd $LDBDIR/tests/schema-tests/schema.ldif || exit 1
 
 echo "Adding few test elements (no failure expected here)"
-$VALGRIND bin/ldbadd tests/schema-tests/schema-add-test.ldif || exit 1
+$VALGRIND bin/ldbadd $LDBDIR/tests/schema-tests/schema-add-test.ldif || exit 1
 
 echo "Modifying elements (2 failures expected here)"
 
-$VALGRIND bin/ldbmodify tests/schema-tests/schema-mod-test-1.ldif || exit 1
-$VALGRIND bin/ldbmodify tests/schema-tests/schema-mod-test-2.ldif || exit 1
-$VALGRIND bin/ldbmodify tests/schema-tests/schema-mod-test-3.ldif || exit 1
-$VALGRIND bin/ldbmodify tests/schema-tests/schema-mod-test-4.ldif
+$VALGRIND bin/ldbmodify $LDBDIR/tests/schema-tests/schema-mod-test-1.ldif || exit 1
+$VALGRIND bin/ldbmodify $LDBDIR/tests/schema-tests/schema-mod-test-2.ldif || exit 1
+$VALGRIND bin/ldbmodify $LDBDIR/tests/schema-tests/schema-mod-test-3.ldif || exit 1
+$VALGRIND bin/ldbmodify $LDBDIR/tests/schema-tests/schema-mod-test-4.ldif
 if [ "$?" == "0" ]; then
 	echo "test failed!"
 	exit 1
 fi
-$VALGRIND bin/ldbmodify tests/schema-tests/schema-mod-test-5.ldif
+$VALGRIND bin/ldbmodify $LDBDIR/tests/schema-tests/schema-mod-test-5.ldif
 if [ "$?" == "0" ]; then
 	echo "test failed!"
 	exit 1
