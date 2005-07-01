29e43139952ddcd52034ef54d4fd3d8b333cac94
Gerald Carter <jerry@samba.org>
2005-07-01 03:13:08 +0000

    r8034: fix mistaken commit that broken the virtual registry backends

--- source/registry/reg_frontend.c
+++ source/registry/reg_frontend.c
@@ -31,7 +31,7 @@ extern REGISTRY_OPS shares_reg_ops;
 extern REGISTRY_OPS regdb_ops;		/* these are the default */
 
 /* array of REGISTRY_HOOK's which are read into a tree for easy access */
-#define REG_TDB_ONLY		1
+/* #define REG_TDB_ONLY		1 */
 
 REGISTRY_HOOK reg_hooks[] = {
 #ifndef REG_TDB_ONLY 
