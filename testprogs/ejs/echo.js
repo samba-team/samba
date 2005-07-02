37ad59fdba9766abaae989e40ec8c351e91e6cf1
Andrew Tridgell <tridge@samba.org>
2005-07-02 11:13:12 +0000

    r8074: demonstrate calling echo_AddOne() from ejs

--- testprogs/ejs/echo.js
+++ testprogs/ejs/echo.js
@@ -9,26 +9,30 @@ function irpcObj()
 	return o;
 }
 
-function test_AddOne()
+function test_AddOne(binding)
 {
 	var status;
 	var conn = new Object();
 	var io = irpcObj();
 
-	status = rpc_connect(conn, "ncacn_ip_tcp:localhost", "rpcecho");
-	printVars(status);
-	printVars(conn);
+	status = rpc_connect(conn, binding, "rpcecho");
+	if (status.is_ok != true) {
+	   print("Failed to connect to " + binding + " - " + status.errstr + "\n");
+	   return;
+	}
 
-	io.in.in_data = 3;
-	status = rpc_call(conn, "echo_AddOne", io);
-	printVars(status);
-	printVars(io);
+	for (i=0;i<10;i++) {
+		io.in.in_data = i;
+		status = rpc_call(conn, "echo_AddOne", io);
+		print("AddOne(" + i + ")=" + io.out.out_data + "\n");
+	}
 }
 
+if (ARGV.length == 0) {
+   print("Usage: echo.js <RPCBINDING>\n");
+   exit(0);
+}
 
-print("Starting\n");
+var binding = ARGV[0];
 
-test_AddOne();
-
-
-
+test_AddOne(binding);
