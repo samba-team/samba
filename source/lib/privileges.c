ddda20d7d51677223d1a1b480ebd03e131e8a256
Gerald Carter <jerry@samba.org>
2005-07-04 13:19:05 +0000

    r8141: Update volker's valgrind fix in r8097.  Same effect, just helps me to remember what is going on here better.

--- source/lib/privileges.c
+++ source/lib/privileges.c
@@ -539,12 +539,13 @@ BOOL grant_privilege(const DOM_SID *sid, const SE_PRIV *priv_mask)
 {
 	SE_PRIV old_mask, new_mask;
 	
+	ZERO_STRUCT( old_mask );
+	ZERO_STRUCT( new_mask );
+
 	if ( get_privileges( sid, &old_mask ) )
 		se_priv_copy( &new_mask, &old_mask );
-	else {
-		se_priv_copy( &old_mask, &se_priv_none );
+	else
 		se_priv_copy( &new_mask, &se_priv_none );
-	}
 
 	se_priv_add( &new_mask, priv_mask );
 
