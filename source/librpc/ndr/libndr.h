3597f9962052f1ab079a014618d46cc1b4500364
Stefan Metzmacher <metze@samba.org>
2005-07-08 05:22:31 +0000

    r8227: add STR_LARGE_SIZE flag, to support strings where the size is length+1,
    metze

--- source/librpc/ndr/libndr.h
+++ source/librpc/ndr/libndr.h
@@ -113,7 +113,8 @@ struct ndr_print {
 #define LIBNDR_FLAG_STR_CHARLEN		(1<<11)
 #define LIBNDR_FLAG_STR_UTF8		(1<<12)
 #define LIBNDR_FLAG_STR_FIXLEN15	(1<<13)
-#define LIBNDR_STRING_FLAGS		(0x3FFC)
+#define LIBNDR_FLAG_STR_LARGE_SIZE	(1<<14)
+#define LIBNDR_STRING_FLAGS		(0x7FFC)
 
 
 #define LIBNDR_FLAG_REF_ALLOC    (1<<20)
