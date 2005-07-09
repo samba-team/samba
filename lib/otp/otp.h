faaa2e6d989e1c537e3ba139575745ed47b489e2
Love Hörnquist Åstrand <lha@kth.se>
2005-07-09 14:41:47 +0000

    remove parameter names to avoid shadow warnings
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@15606 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/otp/otp.h
+++ lib/otp/otp.h
@@ -60,9 +60,9 @@ typedef struct {
   OtpAlgID id;
   char *name;
   int hashsize;
-  int (*hash)(const char *s, size_t len, unsigned char *res);
-  int (*init)(OtpKey key, const char *pwd, const char *seed);
-  int (*next)(OtpKey key);
+  int (*hash)(const char *, size_t, unsigned char *);
+  int (*init)(OtpKey, const char *, const char *);
+  int (*next)(OtpKey);
 } OtpAlgorithm;
 
 typedef struct {
@@ -76,26 +76,26 @@ typedef struct {
   char *err;
 } OtpContext;
 
-OtpAlgorithm *otp_find_alg (char *name);
-void otp_print_stddict (OtpKey key, char *str, size_t sz);
-void otp_print_hex (OtpKey key, char *str, size_t sz);
-void otp_print_stddict_extended (OtpKey key, char *str, size_t sz);
-void otp_print_hex_extended (OtpKey key, char *str, size_t sz);
-unsigned otp_checksum (OtpKey key);
-int otp_parse_hex (OtpKey key, const char *);
-int otp_parse_stddict (OtpKey key, const char *);
-int otp_parse_altdict (OtpKey key, const char *, OtpAlgorithm *);
-int otp_parse (OtpKey key, const char *, OtpAlgorithm *);
-int otp_challenge (OtpContext *ctx, char *user, char *str, size_t len);
-int otp_verify_user (OtpContext *ctx, const char *passwd);
-int otp_verify_user_1 (OtpContext *ctx, const char *passwd);
-char *otp_error (OtpContext *ctx);
+OtpAlgorithm *otp_find_alg (char *);
+void otp_print_stddict (OtpKey, char *, size_t);
+void otp_print_hex (OtpKey, char *, size_t);
+void otp_print_stddict_extended (OtpKey, char *, size_t);
+void otp_print_hex_extended (OtpKey, char *, size_t);
+unsigned otp_checksum (OtpKey);
+int otp_parse_hex (OtpKey, const char *);
+int otp_parse_stddict (OtpKey, const char *);
+int otp_parse_altdict (OtpKey, const char *, OtpAlgorithm *);
+int otp_parse (OtpKey, const char *, OtpAlgorithm *);
+int otp_challenge (OtpContext *, char *, char *, size_t);
+int otp_verify_user (OtpContext *, const char *);
+int otp_verify_user_1 (OtpContext *, const char *);
+char *otp_error (OtpContext *);
 
 void *otp_db_open (void);
 void otp_db_close (void *);
-int otp_put (void *, OtpContext *ctx);
-int otp_get (void *, OtpContext *ctx);
-int otp_simple_get (void *, OtpContext *ctx);
-int otp_delete (void *, OtpContext *ctx);
+int otp_put (void *, OtpContext *);
+int otp_get (void *, OtpContext *);
+int otp_simple_get (void *, OtpContext *);
+int otp_delete (void *, OtpContext *);
 
 #endif /* _OTP_H */
