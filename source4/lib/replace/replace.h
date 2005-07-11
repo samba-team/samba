54ffd4fdbf470cb71cbf9a9c9e5aa37f73acc559
Andrew Tridgell <tridge@samba.org>
2005-07-11 04:17:54 +0000

    r8313: moved PRINTF_ATTRIBUTE to replace.h to try to get irix building with heimdal (This used to be commit 7d4e309f02cfcef661ebf3bbe9c227938318077c)

--- source4/lib/replace/replace.h
+++ source4/lib/replace/replace.h
@@ -111,4 +111,16 @@ int asprintf(char **,const char *, ...) PRINTF_ATTRIBUTE(2,3);
 typedef int (*comparison_fn_t)(const void *, const void *);
 #endif
 
+#ifndef PRINTF_ATTRIBUTE
+#if (__GNUC__ >= 3)
+/** Use gcc attribute to check printf fns.  a1 is the 1-based index of
+ * the parameter containing the format, and a2 the index of the first
+ * argument. Note that some gcc 2.x versions don't handle this
+ * properly **/
+#define PRINTF_ATTRIBUTE(a1, a2) __attribute__ ((format (__printf__, a1, a2)))
+#else
+#define PRINTF_ATTRIBUTE(a1, a2)
+#endif
+#endif
+
 #endif
