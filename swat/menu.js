8b1ce8bbc2855dc69274085be291cfad23081b19
Andrew Tridgell <tridge@samba.org>
2005-07-19 09:44:55 +0000

    r8594: more placeholder pages for server status

--- swat/menu.js
+++ swat/menu.js
@@ -1,8 +1,11 @@
 /* show a menu for the esp test pages */
 simple_menu(
 	"Main Menu",
-	"File Server",         session_uri("/smb_server/"),
-	"LDAP Server",         session_uri("/ldap_server/"),
-	"CLDAP Server",         session_uri("/cldap_server/"),
+	"File Server",        session_uri("/smb_server/"),
+	"LDAP Server",        session_uri("/ldap_server/"),
+	"CLDAP Server",       session_uri("/cldap_server/"),
+	"RPC Server",         session_uri("/rpc_server/"),
 	"NBT Server",         session_uri("/nbt_server/"),
+	"WINS Server",        session_uri("/wins_server/"),
+	"Kerberos Server",    session_uri("/kdc_server/"),
 	"ESP Tests",          session_uri("/esptest/"));
