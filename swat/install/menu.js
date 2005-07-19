325edaa66a5b3a8c907a3a521b4ceea26ea0210c
Andrew Tridgell <tridge@samba.org>
2005-07-19 11:06:12 +0000

    r8598: move provisioning to /install/ directory from /esptest/ (This used to be commit 318402ee4e8900e0d8ed1dce1e201f63ac4a908f)

---
+++ swat/install/menu.js
@@ -0,0 +1,5 @@
+/* show a menu for the esp test pages */
+simple_menu(
+	"Installation",
+	"Provisioning",       session_uri("/install/provision.esp"),
+	"New User",           session_uri("/install/newuser.esp"));
