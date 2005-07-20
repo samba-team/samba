24d2107324982d8ad69fb89d13037ba591f49534
Andrew Bartlett <abartlet@samba.org>
2005-07-20 11:43:23 +0000

    r8650: Use the timestamps and a new objectguid module rather than placing boilerplate attributes in every entry in provision.ldif.
    The next step will be to use templates.
    
    Andrew Bartlett
    (This used to be commit 940ed9827f5ab83b668a60a2b0110567dd54c3e2)

--- source4/lib/ldb/common/ldb_modules.c
+++ source4/lib/ldb/common/ldb_modules.c
@@ -189,6 +189,16 @@ int ldb_load_modules(struct ldb_context *ldb, const char *options[])
 			continue;
 		}
 
+		if (strcmp(modules[i], "objectguid") == 0) {
+			current = objectguid_module_init(ldb, options);
+			if (!current) {
+				ldb_debug(ldb, LDB_DEBUG_FATAL, "function 'init_module' in %s fails\n", modules[i]);
+				return -1;
+			}
+			DLIST_ADD(ldb->modules, current);
+			continue;
+		}
+
 #ifdef _SAMBA_BUILD_
 		if (strcmp(modules[i], "samldb") == 0) {
 			current = samldb_module_init(ldb, options);
