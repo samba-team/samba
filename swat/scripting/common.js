a46cedf7ca7290485eda8aa461f4a24835c5c62e
Andrew Tridgell <tridge@samba.org>
2005-07-23 10:58:09 +0000

    r8722: make the menu handling considerably saner and easier to follow. The whole menu hierarchy is now in /menu.js

--- swat/scripting/common.js
+++ swat/scripting/common.js
@@ -89,48 +89,6 @@ function always_allowed(uri) {
 }
 
 /*
-  create a menu object with the defaults filled in, ready for display_menu()
- */
-function MenuObj(name, num_elements)
-{
-	var o = new Object();
-	o.name = name;
-	o.class = "menu";
-	o.style = "simple";
-	o.orientation = "vertical"
-	o.element = new Array(num_elements);
-	for (i in o.element) {
-		o.element[i] = new Object();
-	}
-	return o;
-}
-
-/*
-  display a menu object. Currently only the "simple", "vertical" menu style
-  is supported
-*/
-function display_menu(m) {
-	assert(m.style == "simple" && m.orientation == "vertical");
-	write('<div class="' + m.class + '">\n');
-	write("<i>" + m.name + "</i><br /><ul>\n");
-	for (i = 0; i < m.element.length; i++) {
-		var e = m.element[i];
-		write("<li><a href=\"" + e.link + "\">" + e.label + "</a></li>\n");
-	}
-	write("</ul></div>\n");
-}
-
-function simple_menu() {
-	var i, m = MenuObj(arguments[0], (arguments.length-1)/2);
-	for (i=0;i<m.element.length;i++) {
-		var ndx = i*2;
-		m.element[i].label = arguments[ndx+1];
-		m.element[i].link = arguments[ndx+2];
-	}
-	display_menu(m);
-}
-
-/*
   display a table element
 */
 function table_element(i, o) {
