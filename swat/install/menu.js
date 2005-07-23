fdb322876d573f7e18947e53667b3ee83e3ba226
Andrew Tridgell <tridge@samba.org>
2005-07-23 10:58:09 +0000

    r8722: make the menu handling considerably saner and easier to follow. The whole menu hierarchy is now in /menu.js (This used to be commit c2a450bb9967caa666628f067a8e732227854ce2)

--- swat/install/menu.js
+++
@@ -1,5 +0,0 @@
-/* show a menu for the esp test pages */
-simple_menu(
-	"Installation",
-	"Provisioning",       session_uri("/install/provision.esp"),
-	"New User",           session_uri("/install/newuser.esp"));
