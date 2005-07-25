a04e899bc07825acecd0e1d3bd5e9898b52a0bd9
Rafal Szczesniak <mimir@samba.org>
2005-07-25 22:56:28 +0000

    r8760: Rework monitor messaging code a bit, as Metze once suggested. enum type has now been replaced with unsigned 32-bit field and message data is passed as void pointer.
    This allows various extension implementers to plug their monitor
    messages in more easily.
    
    rafal
    (This used to be commit 4a6ab58133a59d3da3209b3e46c2a8cf848d25e7)

--- source4/libnet/userman.h
+++ source4/libnet/userman.h
@@ -1,9 +1,7 @@
 /* 
    Unix SMB/CIFS implementation.
 
-   Definitions of composite function monitoring messages.
-
-   Copyright (C) Rafal Szczesniak  2005
+   Copyright (C) Rafal Szczesniak 2005
    
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -20,21 +18,11 @@
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
+
 /*
- * Monitor structure and message types definitions. Composite function monitoring
- * allows client application to be notified on function progress. This enables
- * eg. gui client to display progress bars, status messages, etc.
+ * Monitor messages sent from userman.c functions
  */
 
-
-#define  rpc_create_user        (0x00000001)        /* userman.h */
-#define  rpc_open_user          (0x00000002)        /* userinfo.h */
-#define  rpc_query_user         (0x00000003)        /* userinfo.h */
-#define  rpc_close_user         (0x00000004)        /* userinfo.h */
-
-
-struct monitor_msg {
-	uint32_t   type;
-	void       *data;
-	size_t     data_size;
+struct msg_rpc_create_user {
+	uint32_t rid;
 };
