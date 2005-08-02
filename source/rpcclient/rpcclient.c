c7ed1e3463b3e1cf568ff5015b4a053f696fd975
Günther Deschner <gd@samba.org>
2005-08-02 19:46:11 +0000

    r8937: allow to use non-default smb.conf in rpcclient
    Guenther

--- source/rpcclient/rpcclient.c
+++ source/rpcclient/rpcclient.c
@@ -671,11 +671,6 @@ out_free:
 	if (!interactive) 
 		reopen_logs();
 	
-	/* Load smb.conf file */
-
-	if (!lp_load(dyn_CONFIGFILE,True,False,False))
-		fprintf(stderr, "Can't load %s\n", dyn_CONFIGFILE);
-
 	/* Parse options */
 
 	pc = poptGetContext("rpcclient", argc, (const char **) argv,
@@ -714,6 +709,11 @@ out_free:
 
 	if (!init_names())
 		return 1;
+
+	/* Load smb.conf file */
+
+	if (!lp_load(dyn_CONFIGFILE,True,False,False))
+		fprintf(stderr, "Can't load %s\n", dyn_CONFIGFILE);
 
 	/*
 	 * Get password
