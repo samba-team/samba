3909df1756c1d4ef15a041621fafaef351bc259a
Jelmer Vernooij <jelmer@samba.org>
2005-08-03 06:43:06 +0000

    r8994: Clean up some more autogenerated files so I can predict when I'm going to break the build...

--- source/heimdal_build/et_deps.pl
+++ source/heimdal_build/et_deps.pl
@@ -9,6 +9,7 @@ my $basename = basename($file);
 my $header = $file; $header =~ s/\.et$/.h/;
 my $source = $file; $source =~ s/\.et$/.c/;
 print "$source: $file bin/compile_et\n";
+print "\t\@echo \"Compiling error table $file\"\n";
 print "\t\@cd $dirname && ../../../bin/compile_et $basename\n\n";
 
 print "$header: $source\n";
