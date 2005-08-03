136fe3f15e74686882f7c1e5a46fcc0bcf7d51a6
Gerald Carter <jerry@samba.org>
2005-08-03 06:24:28 +0000

    r8992: More make test work.  Adding a way to order tests (might change my mind on this later).

--- source/script/tests/runtests.sh
+++ source/script/tests/runtests.sh
@@ -1,21 +1,28 @@
 #!/bin/sh
 
-DOMAIN=SAMBA-TEST
-export DOMAIN
-
 if [ "x$1" == "x" ]; then
 	echo "$0 <directory>"
 	exit 1
 fi
 
+##
+## create the test directory
+##
 PREFIX=`echo $1 | sed s+//+/+`
-
 mkdir -p $PREFIX || exit $?
 OLD_PWD=`pwd`
 cd $PREFIX || exit $?
 export PREFIX_ABS=`pwd`
 cd $OLD_PWD
 
+##
+## setup the various environment variables we need
+##
+
+DOMAIN=SAMBA-TEST
+
+SRCDIR=`pwd`
+SCRIPTDIR=$SRCDIR/script/tests
 TMPDIR=$PREFIX_ABS/tmp
 LIBDIR=$PREFIX_ABS/lib
 PIDDIR=$PREFIX_ABS/pid
@@ -31,11 +38,17 @@ PATH=`pwd`/bin:$PATH
 rm -rf $PREFIX/*
 mkdir -p $PRIVATEDIR $LIBDIR $PIDDIR $LOCKDIR $TMPDIR $LOGDIR $SOCKET_WRAPPER_DIR
 
-export PREFIX_ABS CONFIGURATION CONFFILE PATH SOCKET_WRAPPER_DIR
+export PREFIX_ABS CONFIGURATION CONFFILE PATH SOCKET_WRAPPER_DIR DOMAIN
 export PRIVATEDIR LIBDIR PIDDIR LOCKDIR TMPDIR LOGDIR
+export SRCDIR SCRIPTDIR
 
-cd script/tests
-for testfile in t_*sh; do
+for testfile in `ls $SCRIPTDIR/t_*sh | sort`; do
+	echo ">>>>>> Starting test driver `basename $testfile` <<<<<"
 	sh $testfile
+	if [ $? = 0 ]; then
+		echo ">>>>> test ok <<<<<"
+	else
+		echo ">>>>> test failed <<<<<"
+	fi
 done
 
