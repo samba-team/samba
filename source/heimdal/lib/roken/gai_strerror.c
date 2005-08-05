f63fc21007d846b08221c374fccaff4f1f67d33f
Stefan Metzmacher <metze@samba.org>
2005-08-05 08:09:43 +0000

    r9105: match the prototype of netdb.h this should fix the build on solaris 10
    lha can that be merged to the main heimdal if that apears to not break
    the build on other platforms
    
    metze

--- source/heimdal/lib/roken/gai_strerror.c
+++ source/heimdal/lib/roken/gai_strerror.c
@@ -40,7 +40,7 @@ RCSID("$Id: gai_strerror.c,v 1.5 2005/04/12 11:28:42 lha Exp $");
 
 static struct gai_error {
     int code;
-    char *str;
+    const char *str;
 } errors[] = {
 {EAI_NOERROR,		"no error"},
 #ifdef EAI_ADDRFAMILY
@@ -65,7 +65,7 @@ static struct gai_error {
  *
  */
 
-char * ROKEN_LIB_FUNCTION
+const char * ROKEN_LIB_FUNCTION
 gai_strerror(int ecode)
 {
     struct gai_error *g;
