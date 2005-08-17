286e2bbd6e22c3fc1e69af53c5ebf24426ccfa5d
Jeremy Allison <jra@samba.org>
2005-08-17 01:16:47 +0000

    r9336: Move arc4 into a separate file and add it to the makefile to fix dependencies. Jeremy.

--- source/libsmb/smbdes.c
+++ source/libsmb/smbdes.c
@@ -358,63 +358,6 @@ void cred_hash3(unsigned char *out, unsigned char *in, const unsigned char *key,
 }
 
 /*****************************************************************
- Initialize state for an arc4 crypt/decrpyt.
- arc4 state is 258 bytes - last 2 bytes are the index bytes.
-*****************************************************************/
-
-void smb_arc4_init(unsigned char arc4_state_out[258], const unsigned char *key, size_t keylen)
-{
-	size_t ind;
-	unsigned char j = 0;
-
-	for (ind = 0; ind < 256; ind++) {
-		arc4_state_out[ind] = (unsigned char)ind;
-	}
-
-	for( ind = 0; ind < 256; ind++) {
-		unsigned char tc;
-
-		j += (arc4_state_out[ind] + key[ind%keylen]);
-
-		tc = arc4_state_out[ind];
-		arc4_state_out[ind] = arc4_state_out[j];
-		arc4_state_out[j] = tc;
-	}
-	arc4_state_out[256] = 0;
-	arc4_state_out[257] = 0;
-}
-
-/*****************************************************************
- Do the arc4 crypt/decrpyt.
- arc4 state is 258 bytes - last 2 bytes are the index bytes.
-*****************************************************************/
-
-void smb_arc4_crypt(unsigned char arc4_state_inout[258], unsigned char *data, size_t len)
-{
-	unsigned char index_i = arc4_state_inout[256];
-	unsigned char index_j = arc4_state_inout[257];
-        size_t ind;
-
-	for( ind = 0; ind < len; ind++) {
-		unsigned char tc;
-		unsigned char t;
-
-		index_i++;
-		index_j += arc4_state_inout[index_i];
-
-		tc = arc4_state_inout[index_i];
-		arc4_state_inout[index_i] = arc4_state_inout[index_j];
-		arc4_state_inout[index_j] = tc;
-
-		t = arc4_state_inout[index_i] + arc4_state_inout[index_j];
-		data[ind] = data[ind] ^ arc4_state_inout[t];
-	}
-
-	arc4_state_inout[256] = index_i;
-	arc4_state_inout[257] = index_j;
-}
-
-/*****************************************************************
  arc4 crypt/decrypt with a 16 byte key.
 *****************************************************************/
 
