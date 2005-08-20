c3a3e184b4f839da4a29e69bc5f7d1e95b0e6073
Jelmer Vernooij <jelmer@samba.org>
2005-08-20 23:28:14 +0000

    r9437: Update PLAN and README for Samba3 compatibility layer Add support for reading tdbsam files

---
+++ source/lib/samba3/config.mk
@@ -0,0 +1,10 @@
+################################################
+# Start SUBSYSTEM LIBSAMBA3
+[SUBSYSTEM::LIBSAMBA3]
+INIT_OBJ_FILES = \
+		lib/samba3/smbpasswd.o \
+		lib/samba3/tdbsam.o
+# End SUBSYSTEM LIBSAMBA3
+################################################
+
+
