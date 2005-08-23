dbaaf81cec59aae01e71bbb639e1937ebee2454d
Gerald Carter <jerry@samba.org>
2005-08-23 22:10:44 +0000

    r9548: more additions to make test; better checks for starting and stopping smbd

--- source/script/tests/t_001.sh
+++ source/script/tests/t_001.sh
@@ -4,27 +4,19 @@
 
 cat >$CONFFILE<<EOF
 [global]
-	netbios name = LOCALHOST
-	workgroup = $DOMAIN
-
-	private dir = $PRIVATEDIR
-	pid directory = $PIDDIR
-	lock directory = $LOCKDIR
-	log file = $LOGDIR/log.%m
-
-	interfaces = lo
-	bind interfaces only = yes
-
-	panic action = $PREFIX_ABS/script/tests/gdb_backtrace /proc/%d/exe %d
+	include = $LIBDIR/common.conf
 
 [test]
 	path = $TMPDIR
 	read only = no
 EOF
 
+##
+## Test code 
+##
 
-smbd $CONFIGURATION || exit $?
-sleep 1
+start_smbd || exit $?
+
 smbclient $CONFIGURATION -L localhost -N -p 139
 
-killall smbd
+stop_smbd
