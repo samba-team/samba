049a7a17b14f1d456ce206b2d2e74220ee50b823
Gerald Carter <jerry@samba.org>
2005-08-25 17:15:23 +0000

    r9618: adding get/put tests from Don Watson

--- source/script/tests/t_001.sh
+++ source/script/tests/t_001.sh
@@ -22,9 +22,27 @@ chmod 1777 $PREFIX_ABS/tmp
 
 start_smbd || exit $?
 
+## share enumeration
+
 smbclient $CONFIGURATION -L localhost -N -p 139
-ret=$?
+check_ret_value $? 
+
+testfile=`echo $CONFIGURATION | awk '{print $2}'`
+filename=`basename $testfile`
+dirname=`dirname $testfile`
+
+
+# file get/put
+
+smbclient //localhost/test $PASSWORD $CONFIGURATION -c "lcd $dirname; put $filename"
+check_ret_value $? 
+
+smbclient //localhost/test $PASSWORD $CONFIGURATION -c "get $filename; rm $filename"
+check_ret_value $? 
+
+diff $filename $testfile 2> /dev/null > /dev/null
+check_ret_value $?
 
 stop_smbd
+exit 0
 
-exit $ret
