278c885b70929c42acaf832dd58118925430a645
Volker Lendecke <vlendec@samba.org>
2005-08-28 09:19:10 +0000

    r9709: Fix two bugs found by Brian Moran: Any request sent to winbind while the child in question is still initializing overwrites domain->dcname. Only overwrite if the parent actually has sent a dcname and thus really knows it. Second, ntlm_auth needs the error code, not just the fact it failed.
    Jerry, the 3_0 part might qualify as a "recommended patch".
    
    Thanks,
    
    Volker

--- source/nsswitch/winbindd_dual.c
+++ source/nsswitch/winbindd_dual.c
@@ -158,9 +158,8 @@ static void async_reply_recv(void *private_data, BOOL success)
 		return;
 	}
 
-	if (state->response->result == WINBINDD_OK)
-		SMB_ASSERT(cache_retrieve_response(child->pid,
-						   state->response));
+	SMB_ASSERT(cache_retrieve_response(child->pid,
+					   state->response));
 
 	DLIST_REMOVE(child->requests, state);
 
@@ -479,12 +478,11 @@ static BOOL fork_domain_child(struct winbindd_child *child)
 			DEBUG(4,("child daemon request %d\n",
 				 (int)state.request.cmd));
 
+			ZERO_STRUCT(state.response);
 			state.request.null_term = '\0';
 			child_process_request(child->domain, &state);
 
-			if (state.response.result == WINBINDD_OK)
-				cache_store_response(sys_getpid(),
-						     &state.response);
+			cache_store_response(sys_getpid(), &state.response);
 
 			SAFE_FREE(state.response.extra_data);
 
