fe1ee4494e172ea8045b4847806241e9dfaefeef
Jelmer Vernooij <jelmer@samba.org>
2005-08-29 23:20:40 +0000

    r9766: Prevent erroneous OOM message ldb_dup_val() sets out->data to NULL if in->length == 0 (This used to be commit 7ecb6988e74f4273b2ca3ea76562117e1be54b08)

--- source4/lib/ldb/common/attrib_handlers.c
+++ source4/lib/ldb/common/attrib_handlers.c
@@ -38,7 +38,7 @@ int ldb_handler_copy(struct ldb_context *ldb, void *mem_ctx,
 		     const struct ldb_val *in, struct ldb_val *out)
 {
 	*out = ldb_val_dup(mem_ctx, in);
-	if (out->data == NULL) {
+	if (in->length > 0 && out->data == NULL) {
 		ldb_oom(ldb);
 		return -1;
 	}
