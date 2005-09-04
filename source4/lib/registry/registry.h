a487b6c19c28100041ceb2b7a5798c236983509c
Jelmer Vernooij <jelmer@samba.org>
2005-09-04 17:26:23 +0000

    r10028: More registry fixes. Remove save_hive() function (there is a flush_key function already). Fixes for the allocation mechanism in the REGF backend (This used to be commit 499d03bc90382bcd33c8c3a1577a58d2c76e5129)

--- source4/lib/registry/registry.h
+++ source4/lib/registry/registry.h
@@ -73,6 +73,9 @@ typedef void (*value_notification_function) (void);
  * Backends should always do case-insensitive compares 
  * (everything is case-insensitive but case-preserving, 
  * just like the FS)
+ *
+ * There is no save function as all operations are expected to 
+ * be atomic.
  */ 
 
 struct hive_operations {
@@ -80,7 +83,6 @@ struct hive_operations {
 
 	/* Implement this one */
 	WERROR (*open_hive) (struct registry_hive *, struct registry_key **);
-	WERROR (*save_hive) (struct registry_hive *, const char *location);
 
 	/* Or this one */
 	WERROR (*open_key) (TALLOC_CTX *, struct registry_key *, const char *name, struct registry_key **);
