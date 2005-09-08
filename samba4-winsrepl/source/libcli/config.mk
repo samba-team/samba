52695dcefc3a66d52f13f69ec5d7b67e7da3b84c
Stefan Metzmacher <metze@samba.org>
2005-09-08 14:43:45 +0000

    r10082:  r12043@SERNOX (orig r10081):  metze | 2005-09-08 16:43:15 +0200  - create a seperate WINSDB subsystem  - use LIBCLI_WREPL for the winsreplication client code  - fix some dependencies
     metze

---
+++ samba4-winsrepl/source/libcli/config.mk
@@ -0,0 +1,107 @@
+[SUBSYSTEM::LIBCLI_UTILS]
+ADD_OBJ_FILES = libcli/util/asn1.o \
+		libcli/util/doserr.o \
+		libcli/util/errormap.o \
+		libcli/util/clierror.o \
+		libcli/util/nterr.o \
+		libcli/util/smbdes.o
+
+[SUBSYSTEM::LIBCLI_LSA]
+ADD_OBJ_FILES = libcli/util/clilsa.o
+REQUIRED_SUBSYSTEMS = RPC_NDR_LSA
+
+[SUBSYSTEM::LIBCLI_COMPOSITE_BASE]
+ADD_OBJ_FILES = \
+	libcli/composite/composite.o
+REQUIRED_SUBSYSTEMS = LIBEVENTS
+
+[SUBSYSTEM::LIBCLI_COMPOSITE]
+ADD_OBJ_FILES = \
+	libcli/composite/loadfile.o \
+	libcli/composite/savefile.o \
+	libcli/composite/connect.o \
+	libcli/composite/sesssetup.o \
+	libcli/composite/fetchfile.o \
+	libcli/composite/appendacl.o \
+	libcli/composite/fsinfo.o 
+REQUIRED_SUBSYSTEMS = LIBCLI_COMPOSITE_BASE
+
+[SUBSYSTEM::LIBCLI_NBT]
+ADD_OBJ_FILES = \
+	libcli/nbt/nbtname.o \
+	libcli/nbt/nbtsocket.o \
+	libcli/nbt/namequery.o \
+	libcli/nbt/nameregister.o \
+	libcli/nbt/namerefresh.o \
+	libcli/nbt/namerelease.o
+REQUIRED_SUBSYSTEMS = NDR_RAW NDR_NBT SOCKET LIBCLI_COMPOSITE_BASE LIBEVENTS \
+	LIB_SECURITY_NDR
+
+[SUBSYSTEM::LIBCLI_DGRAM]
+ADD_OBJ_FILES = \
+	libcli/dgram/dgramsocket.o \
+	libcli/dgram/mailslot.o \
+	libcli/dgram/netlogon.o \
+	libcli/dgram/ntlogon.o \
+	libcli/dgram/browse.o
+NOPROTO=YES
+REQUIRED_SUBSYSTEMS = LIBCLI_NBT
+
+[SUBSYSTEM::LIBCLI_CLDAP]
+ADD_OBJ_FILES = \
+	libcli/cldap/cldap.o
+NOPROTO=YES
+REQUIRED_SUBSYSTEMS = LIBCLI_LDAP
+
+[SUBSYSTEM::LIBCLI_WREPL]
+ADD_OBJ_FILES = \
+	libcli/wins/winsrepl.o
+REQUIRED_SUBSYSTEMS = NDR_WINSREPL SOCKET LIBEVENTS
+
+[SUBSYSTEM::LIBCLI_RESOLVE]
+ADD_OBJ_FILES = \
+	libcli/resolve/resolve.o \
+	libcli/resolve/nbtlist.o \
+	libcli/resolve/bcast.o \
+	libcli/resolve/wins.o \
+	libcli/resolve/host.o
+REQUIRED_SUBSYSTEMS = LIBCLI_NBT
+
+[SUBSYSTEM::LIBCLI]
+REQUIRED_SUBSYSTEMS = LIBCLI_RAW LIBCLI_UTILS LIBCLI_AUTH \
+	LIBCLI_COMPOSITE LIBCLI_NBT LIB_SECURITY LIBCLI_RESOLVE \
+	LIBCLI_DGRAM
+
+[SUBSYSTEM::LIBSMB]
+REQUIRED_SUBSYSTEMS = LIBCLI SOCKET
+ADD_OBJ_FILES = libcli/clireadwrite.o \
+		libcli/cliconnect.o \
+		libcli/clifile.o \
+		libcli/clilist.o \
+		libcli/clitrans2.o \
+		libcli/climessage.o \
+		libcli/clideltree.o
+
+[SUBSYSTEM::LIBCLI_RAW]
+REQUIRED_SUBSYSTEMS = LIBCLI_RAW_KRB5
+OBJ_FILES = libcli/raw/rawfile.o \
+		libcli/raw/smb_signing.o \
+		libcli/raw/clisocket.o \
+		libcli/raw/clitransport.o \
+		libcli/raw/clisession.o \
+		libcli/raw/clitree.o \
+		libcli/raw/rawrequest.o \
+		libcli/raw/rawreadwrite.o \
+		libcli/raw/rawsearch.o \
+		libcli/raw/rawsetfileinfo.o \
+		libcli/raw/raweas.o \
+		libcli/raw/rawtrans.o \
+		libcli/raw/clioplock.o \
+		libcli/raw/rawnegotiate.o \
+		libcli/raw/rawfsinfo.o \
+		libcli/raw/rawfileinfo.o \
+		libcli/raw/rawnotify.o \
+		libcli/raw/rawioctl.o \
+		libcli/raw/rawacl.o \
+		libcli/raw/rawdate.o \
+		libcli/raw/rawlpq.o
---
+++ samba4-winsrepl/source/nbt_server/config.mk
@@ -0,0 +1,53 @@
+# NBTD server subsystem
+
+#######################
+# Start SUBSYSTEM WINSDB
+[SUBSYSTEM::WINSDB]
+ADD_OBJ_FILES = \
+		nbt_server/wins/winsdb.o
+REQUIRED_SUBSYSTEMS = \
+		LIBLDB
+# End SUBSYSTEM WINSDB
+#######################
+
+#######################
+# Start SUBSYSTEM NBTD_WINS
+[SUBSYSTEM::NBTD_WINS]
+ADD_OBJ_FILES = \
+		nbt_server/wins/winsserver.o \
+		nbt_server/wins/winsclient.o \
+		nbt_server/wins/winswack.o
+REQUIRED_SUBSYSTEMS = \
+		LIBCLI_NBT WINSDB
+# End SUBSYSTEM NBTD_WINS
+#######################
+
+#######################
+# Start SUBSYSTEM NBTD_DGRAM
+[SUBSYSTEM::NBTD_DGRAM]
+ADD_OBJ_FILES = \
+		nbt_server/dgram/request.o \
+		nbt_server/dgram/netlogon.o \
+		nbt_server/dgram/ntlogon.o \
+		nbt_server/dgram/browse.o
+REQUIRED_SUBSYSTEMS = \
+		LIBCLI_DGRAM
+# End SUBSYSTEM NBTD_DGRAM
+#######################
+
+#######################
+# Start SUBSYSTEM NBTD
+[SUBSYSTEM::NBTD]
+INIT_OBJ_FILES = \
+		nbt_server/nbt_server.o
+ADD_OBJ_FILES = \
+		nbt_server/interfaces.o \
+		nbt_server/register.o \
+		nbt_server/query.o \
+		nbt_server/nodestatus.o \
+		nbt_server/defense.o \
+		nbt_server/packet.o
+REQUIRED_SUBSYSTEMS = \
+		LIBCLI_NBT NBTD_WINS NBTD_DGRAM
+# End SUBSYSTEM NBTD
+#######################
