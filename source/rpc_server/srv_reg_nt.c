0a1848907f3bb092c6f95059531cf5ff2097da86
Gerald Carter <jerry@samba.org>
2005-09-14 12:49:24 +0000

    r10221: add "free pass for root" in svcctl and default winreg access checks

--- source/rpc_server/srv_reg_nt.c
+++ source/rpc_server/srv_reg_nt.c
@@ -46,6 +46,14 @@ NTSTATUS registry_access_check( SEC_DESC *sec_desc, NT_USER_TOKEN *token,
 		
 	se_map_generic( &access_desired, &reg_generic_map );
 	se_access_check( sec_desc, token, access_desired, access_granted, &result );
+
+	if ( !NT_STATUS_IS_OK(result) ) {
+		if ( geteuid() == sec_initial_uid() ) {
+			DEBUG(5,("registry_access_check: access check bypassed for 'root'\n"));
+			*access_granted = access_desired;
+			return NT_STATUS_OK;
+		}
+	}
 	
 	return result;
 }
