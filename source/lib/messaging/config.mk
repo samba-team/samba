df4a923c64a7fffea3aa01f646b3fba7a83a1b49
Jelmer Vernooij <jelmer@samba.org>
2005-09-19 23:10:28 +0000

    r10332: Fix the build - messaging uses UNIX_PRIVS

--- source/lib/messaging/config.mk
+++ source/lib/messaging/config.mk
@@ -6,6 +6,7 @@ INIT_OBJ_FILES = \
 		lib/messaging/messaging.o
 NOPROTO = YES
 REQUIRED_SUBSYSTEMS = \
-		NDR_IRPC
+		NDR_IRPC \
+		UNIX_PRIVS
 # End SUBSYSTEM MESSAGING
 ################################################
