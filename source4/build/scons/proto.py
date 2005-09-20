920a627ba814d78a1765a6c1c7209ff29acc6153
Jelmer Vernooij <jelmer@samba.org>
2005-09-20 17:49:19 +0000

    r10356: Make the proto generator work with scons (This used to be commit a2268f1dd441059955bee30cf2a9028656c14099)

--- source4/build/scons/proto.py
+++ source4/build/scons/proto.py
@@ -5,22 +5,16 @@ Tool-specific initialization for mkproto (C Proto File generator)
 """
 
 import SCons.Defaults
-import SCons.Scanner.C
 import SCons.Util
 
-c_scanner = SCons.Scanner.C.CScan()
-
 proto_builder = SCons.Builder.Builder(action='$PROTOCOM',
-                                     src_suffix = '.idl',
-                                     suffix='.h',
-                                     scanner = c_scanner)
+                                     src_suffix = '.c',
+                                     suffix='.h')
 
 def generate(env):
-    env['MKPROTO']          = './script/mkproto.sh'
-    env['PROTOCOM']       = '$MKPROTO "$PERL" -h _PROTO_H_ ${TARGETS[0]} $SOURCE'
-    env['BUILDERS']['ProtoHeader'] = proto_builder
+	env['MKPROTO']          = './script/mkproto.sh'
+	env['PROTOCOM']       = '$MKPROTO "$PERL" -h _PROTO_H_ ${TARGETS[0]} $SOURCE'
+	env['BUILDERS']['CProtoHeader'] = proto_builder
 
 def exists(env):
 	return env.Detect('./script/mkproto.sh')
-
-
