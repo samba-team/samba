b638845426ec83d344af32b5bf79f237cf1f0324
Andrew Tridgell <tridge@samba.org>
2005-09-22 04:40:23 +0000

    r10408: now that we are using tdb transactions we don't need any additional locking code in the ldb_tdb backend, except for a single read lock during searches to ensure searches don't cross transaction boundaries
    The tdb transactions code would map these extra locks to noops anyway
    (as locking makes no sense inside a transaction), but the work in
    setting up the locking keys still costs something, and it makes the
    code needlessly complex

--- source/lib/ldb/ldb_tdb/ldb_search.c
+++ source/lib/ldb/ldb_tdb/ldb_search.c
@@ -242,6 +242,42 @@ int ltdb_search_dn1(struct ldb_module *module, const struct ldb_dn *dn, struct l
 	return 1;
 }
 
+/* the lock key for search locking. Note that this is not a DN, its
+   just an arbitrary key to give to tdb. Also note that as we and
+   using transactions for all write operations and transactions take
+   care of their own locks, we don't need to do any locking anywhere
+   other than in ldb_search() */
+#define LDBLOCK	"INT_LDBLOCK"
+
+/*
+  lock the database for read - use by ltdb_search
+*/
+static int ltdb_lock_read(struct ldb_module *module)
+{
+	struct ltdb_private *ltdb = module->private_data;
+	TDB_DATA key;
+
+	key.dptr = discard_const(LDBLOCK);
+	key.dsize = strlen(LDBLOCK);
+
+	return tdb_chainlock_read(ltdb->tdb, key);
+}
+
+/*
+  unlock the database after a ltdb_lock_read()
+*/
+static int ltdb_unlock_read(struct ldb_module *module)
+{
+	struct ltdb_private *ltdb = module->private_data;
+	TDB_DATA key;
+
+	key.dptr = discard_const(LDBLOCK);
+	key.dsize = strlen(LDBLOCK);
+
+	return tdb_chainunlock_read(ltdb->tdb, key);
+}
+
+
 
 /*
   search the database for a single simple dn
