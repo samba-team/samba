6b86347dc86d53a9056f52718abf5d5d28e8e064
Andrew Tridgell <tridge@samba.org>
2005-09-26 02:03:06 +0000

    r10496: - added configure test for sys/time.h
    - print length of failed locks

--- source/lib/tdb/common/lock.c
+++ source/lib/tdb/common/lock.c
@@ -68,8 +68,8 @@ int tdb_brlock_len(struct tdb_context *tdb, tdb_off_t offset,
 		if (!probe && lck_type != F_SETLK) {
 			/* Ensure error code is set for log fun to examine. */
 			tdb->ecode = TDB_ERR_LOCK;
-			TDB_LOG((tdb, 5,"tdb_brlock failed (fd=%d) at offset %d rw_type=%d lck_type=%d\n", 
-				 tdb->fd, offset, rw_type, lck_type));
+			TDB_LOG((tdb, 5,"tdb_brlock failed (fd=%d) at offset %d rw_type=%d lck_type=%d len=%d\n", 
+				 tdb->fd, offset, rw_type, lck_type, len));
 		}
 		return TDB_ERRCODE(TDB_ERR_LOCK, -1);
 	}
