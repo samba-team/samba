ef13569ca94da00cc410318e61505e70f3606674
Andrew Tridgell <tridge@samba.org>
2005-10-07 00:40:29 +0000

    r10790: allow updating of existing ldb opaque values (thanks to abartlet for spotting this)

--- source/lib/ldb/common/ldb.c
+++ source/lib/ldb/common/ldb.c
@@ -350,7 +350,17 @@ const char *ldb_errstring(struct ldb_context *ldb)
 */
 int ldb_set_opaque(struct ldb_context *ldb, const char *name, void *value)
 {
-	struct ldb_opaque *o = talloc(ldb, struct ldb_opaque);
+	struct ldb_opaque *o;
+
+	/* allow updating an existing value */
+	for (o=ldb->opaque;o;o=o->next) {
+		if (strcmp(o->name, name) == 0) {
+			o->value = value;
+			return LDB_SUCCESS;
+		}
+	}
+
+	o = talloc(ldb, struct ldb_opaque);
 	if (o == NULL) {
 		ldb_oom(ldb);
 		return LDB_ERR_OTHER;
