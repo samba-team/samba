ef3845366bc883e735b2008243b7c05a403f42ca
Gerald Carter <jerry@samba.org>
2005-10-07 15:43:32 +0000

    r10822: updating copyright info

--- source/nmbd/nmbd.c
+++ source/nmbd/nmbd.c
@@ -720,7 +720,7 @@ static BOOL open_sockets(BOOL isdaemon, int port)
 	reopen_logs();
 
 	DEBUG( 0, ( "Netbios nameserver version %s started.\n", SAMBA_VERSION_STRING) );
-	DEBUGADD( 0, ( "Copyright Andrew Tridgell and the Samba Team 1994-2004\n" ) );
+	DEBUGADD( 0, ( "%s\n", COPYRIGHT_STARTUP_MESSAGE ) );
 
 	if ( !reload_nmbd_services(False) )
 		return(-1);
