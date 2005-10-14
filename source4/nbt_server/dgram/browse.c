3d7c2443b82b3770858c236bdcd11c3d649ead75
Stefan Metzmacher <metze@samba.org>
2005-10-14 12:56:13 +0000

    r11041:  r10358@SERNOX:  metze | 2005-09-20 21:40:03 +0200  fix some missing nbt_peer_socket cases
     metze
    (This used to be commit bd25231c57f60296ad954d4f333c14d83781a1d1)

--- source4/nbt_server/dgram/browse.c
+++ source4/nbt_server/dgram/browse.c
@@ -31,8 +31,8 @@
 */
 void nbtd_mailslot_browse_handler(struct dgram_mailslot_handler *dgmslot, 
 				  struct nbt_dgram_packet *packet, 
-				  const char *src_address, int src_port)
+				  struct nbt_peer_socket *src)
 {
 	DEBUG(2,("Browse request on '%s' from %s:%d\n", 
-		  dgmslot->mailslot_name, src_address, src_port));
+		  dgmslot->mailslot_name, src->addr, src->port));
 }
