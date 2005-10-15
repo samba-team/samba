17355fbbd4c4a904bb75c1d8ba98948edaf0fe68
Volker Lendecke <vlendec@samba.org>
2005-10-15 22:01:15 +0000

    r11094: Connect to SAM, implement getdcname (This used to be commit a14398715eceecf204caf815a8769ba8214d0576)

--- source4/winbind/config.mk
+++ source4/winbind/config.mk
@@ -11,6 +11,9 @@ INIT_OBJ_FILES = \
 		winbind/wb_samba3_cmd.o \
 		winbind/wb_init_domain.o \
 		winbind/wb_connect_lsa.o \
+		winbind/wb_connect_sam.o \
+		winbind/wb_cmd_lookupname.o \
+		winbind/wb_cmd_getdcname.o \
 		winbind/wb_pam_auth.o \
 		winbind/wb_async_helpers.o
 REQUIRED_SUBSYSTEMS = RPC_NDR_LSA
