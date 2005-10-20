90cf4f8e1a1051a58635e126d56118701875bc5d
Andrew Bartlett <abartlet@samba.org>
2005-10-20 05:08:24 +0000

    r11207: Correct principal search define

--- source/include/secrets.h
+++ source/include/secrets.h
@@ -62,6 +62,6 @@ struct machine_acct_pass {
 #define SECRETS_PRIMARY_DOMAIN_FILTER "(&(flatname=%s)(objectclass=primaryDomain))"
 #define SECRETS_PRIMARY_REALM_FILTER "(&(realm=%s)(objectclass=primaryDomain))"
 #define SECRETS_KRBTGT_SEARCH "(&((|(realm=%s)(flatname=%s))(samAccountName=krbtgt)))"
-#define SECRETS_PRINCIPAL_SEARCH "(&((|(realm=%s)(flatname=%s))(servicePrincipalName=%s)))"
+#define SECRETS_PRINCIPAL_SEARCH "(&(|(realm=%s)(flatname=%s))(servicePrincipalName=%s))"
 
 #endif /* _SECRETS_H */
