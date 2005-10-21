645c2fcae94ab11543f7f5696a606dbff23d0336
Jelmer Vernooij <jelmer@samba.org>
2005-10-21 19:24:13 +0000

    r11247: Add environment class (This used to be commit 90c3f8b3beda525b700688fc4d6ed0584ff2e27c)

--- source4/build/smb_build/main.pl
+++ source4/build/smb_build/main.pl
@@ -13,15 +13,18 @@ use smb_build::input;
 use smb_build::config_mk;
 use smb_build::output;
 use smb_build::dot;
+use smb_build::env;
 use config;
 use strict;
 
 my $INPUT = {};
 
 my $mkfile = smb_build::config_mk::run_config_mk($INPUT, "main.mk");
+my $env = new smb_build::env();
+$env->set_config(\%config::config);
 my $DEPEND = smb_build::input::check($INPUT, \%config::enabled);
 my $OUTPUT = output::create_output($DEPEND);
-makefile::create_makefile($OUTPUT, $mkfile, "Makefile");
+makefile::create_makefile($OUTPUT, $env, $mkfile, "Makefile");
 smb_build_h::create_smb_build_h($OUTPUT, "include/smb_build.h");
 
 open DOTTY, ">samba4-deps.dot";
