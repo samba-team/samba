4cd8e2a96b98ff711905e8c6f416b22440c16062
Jeremy Allison <jra@samba.org>
2005-10-31 20:11:58 +0000

    r11420: Fix issue pointed out by Dina Fine <dina@exanet.com>. We can only tell at parse time from the wire if an incoming name has wildcards or not. If it's a mangled name and we demangle the demangled name may contain wildcard characters. Ensure these are ignored. Jeremy.

--- source/printing/nt_printing.c
+++ source/printing/nt_printing.c
@@ -4792,7 +4792,7 @@ static BOOL delete_driver_files( NT_PRINTER_DRIVER_INFO_LEVEL_3 *info_3, struct 
 			pstrcpy( file, s );
 			driver_unix_convert(file, conn, NULL, &bad_path, &st);
 			DEBUG(10,("deleting driverfile [%s]\n", s));
-			unlink_internals(conn, 0, file);
+			unlink_internals(conn, 0, file, False);
 		}
 	}
 		
@@ -4801,7 +4801,7 @@ static BOOL delete_driver_files( NT_PRINTER_DRIVER_INFO_LEVEL_3 *info_3, struct 
 			pstrcpy( file, s );
 			driver_unix_convert(file, conn, NULL, &bad_path, &st);
 			DEBUG(10,("deleting configfile [%s]\n", s));
-			unlink_internals(conn, 0, file);
+			unlink_internals(conn, 0, file, False);
 		}
 	}
 	
@@ -4810,7 +4810,7 @@ static BOOL delete_driver_files( NT_PRINTER_DRIVER_INFO_LEVEL_3 *info_3, struct 
 			pstrcpy( file, s );
 			driver_unix_convert(file, conn, NULL, &bad_path, &st);
 			DEBUG(10,("deleting datafile [%s]\n", s));
-			unlink_internals(conn, 0, file);
+			unlink_internals(conn, 0, file, False);
 		}
 	}
 	
@@ -4819,7 +4819,7 @@ static BOOL delete_driver_files( NT_PRINTER_DRIVER_INFO_LEVEL_3 *info_3, struct 
 			pstrcpy( file, s );
 			driver_unix_convert(file, conn, NULL, &bad_path, &st);
 			DEBUG(10,("deleting helpfile [%s]\n", s));
-			unlink_internals(conn, 0, file);
+			unlink_internals(conn, 0, file, False);
 		}
 	}
 	
@@ -4835,7 +4835,7 @@ static BOOL delete_driver_files( NT_PRINTER_DRIVER_INFO_LEVEL_3 *info_3, struct 
 				pstrcpy( file, p );
 				driver_unix_convert(file, conn, NULL, &bad_path, &st);
 				DEBUG(10,("deleting dependent file [%s]\n", file));
-				unlink_internals(conn, 0, file );
+				unlink_internals(conn, 0, file, False);
 			}
 			
 			i++;
