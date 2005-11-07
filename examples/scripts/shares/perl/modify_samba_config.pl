26e9c1c1fae8f2fe402374f7cf1d87de48852fa6
Gerald Carter <jerry@samba.org>
2005-11-07 13:45:28 +0000

    r11549: fixing GPL notices

--- examples/scripts/shares/perl/modify_samba_config.pl
+++ examples/scripts/shares/perl/modify_samba_config.pl
@@ -1,12 +1,26 @@
 #!/usr/bin/perl
 
+######################################################################
 ##
-## Simple example of how to implement a '[add|delete] share command' for
-## use with the Windows NT Server Manager. See smb.conf(5) for details
-## on the '[add|delete] share command'
+##  Simple add/delete/change share command script for Samba
 ##
-## Author : Gerald (Jerry) Carter <jerry@samba.org>
+##  Copyright (C) Gerald Carter                2004.
 ##
+##  This program is free software; you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation; either version 2 of the License, or
+##  (at your option) any later version.
+##
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##
+##  You should have received a copy of the GNU General Public License
+##  along with this program; if not, write to the Free Software
+##  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+##
+######################################################################
 
 use POSIX qw(tmpnam);
 
