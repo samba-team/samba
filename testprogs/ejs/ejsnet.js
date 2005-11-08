5a338b04f37f6160f4aaf7d9758aa2d817516eb1
Rafal Szczesniak <mimir@samba.org>
2005-11-08 23:23:16 +0000

    r11587: Trivial testing code giving an idea of how could using subcontexts look like.
    rafal

--- testprogs/ejs/ejsnet.js
+++ testprogs/ejs/ejsnet.js
@@ -1,4 +1,17 @@
 #!/usr/bin/env smbscript
 
 var ctx = NetContext("Administrator", "admin");
-ctx.CreateUser("noname");
+var usr_ctx = ctx.UserMgr("BUILTIN");
+if (usr_ctx == undefined) {
+	print("Couln't get user management context.\n");
+	return -1;
+}
+
+var status = usr_ctx.Create("noname");
+if (status.is_ok != true) {
+	print("Failed to create user account: " + status.errstr + "\n");
+	return -1;
+}
+
+print ("OK\n");
+return 0;
