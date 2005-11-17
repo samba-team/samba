bff1df678a8948d382f4555e83a1df23146a4b12
Jeremy Allison <jra@samba.org>
2005-11-17 22:40:10 +0000

    r11769: Looking at a performance problem enumerating accounts, wondered if changing to support samr_connect5 might help so quickly coded it up. No it doesn't :-(. Don't merge this for 3.0.21 please. Jeremy.

--- source/rpc_server/srv_samr.c
+++ source/rpc_server/srv_samr.c
@@ -679,6 +679,37 @@ static BOOL api_samr_connect4(pipes_struct *p)
 	return True;
 }
 
+/*******************************************************************
+ api_samr_connect5
+ ********************************************************************/
+
+static BOOL api_samr_connect5(pipes_struct *p)
+{
+	SAMR_Q_CONNECT5 q_u;
+	SAMR_R_CONNECT5 r_u;
+	prs_struct *data = &p->in_data.data;
+	prs_struct *rdata = &p->out_data.rdata;
+
+	ZERO_STRUCT(q_u);
+	ZERO_STRUCT(r_u);
+
+	/* grab the samr open policy */
+	if(!samr_io_q_connect5("", &q_u, data, 0)) {
+		DEBUG(0,("api_samr_connect5: unable to unmarshall SAMR_Q_CONNECT5.\n"));
+		return False;
+	}
+
+	r_u.status = _samr_connect5(p, &q_u, &r_u);
+
+	/* store the response in the SMB stream */
+	if(!samr_io_r_connect5("", &r_u, rdata, 0)) {
+		DEBUG(0,("api_samr_connect5: unable to marshall SAMR_R_CONNECT5.\n"));
+		return False;
+	}
+
+	return True;
+}
+
 /**********************************************************************
  api_samr_lookup_domain
  **********************************************************************/
@@ -1492,7 +1523,8 @@ static struct api_struct api_samr_cmds [] =
       {"SAMR_GET_USRDOM_PWINFO" , SAMR_GET_USRDOM_PWINFO, api_samr_get_usrdom_pwinfo},
       {"SAMR_UNKNOWN_2E"        , SAMR_UNKNOWN_2E       , api_samr_unknown_2e       },
       {"SAMR_SET_DOMAIN_INFO"   , SAMR_SET_DOMAIN_INFO  , api_samr_set_dom_info     },
-      {"SAMR_CONNECT4"          , SAMR_CONNECT4         , api_samr_connect4         }
+      {"SAMR_CONNECT4"          , SAMR_CONNECT4         , api_samr_connect4         },
+      {"SAMR_CONNECT5"          , SAMR_CONNECT5         , api_samr_connect5         }
 };
 
 void samr_get_pipe_fns( struct api_struct **fns, int *n_fns )
