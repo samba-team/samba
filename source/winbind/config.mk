772b65f0c069d71a18ceb10b58033194490a695c
Stefan Metzmacher <metze@samba.org>
2005-11-21 12:16:45 +0000

    r11828: split out the async helper function into a new subsystem WB_HELPER to use it in torture tests too
    metze

--- source/winbind/config.mk
+++ source/winbind/config.mk
@@ -21,8 +21,16 @@ INIT_OBJ_FILES = \
 		wb_cmd_userdomgroups.o \
 		wb_cmd_usersids.o \
 		wb_cmd_list_trustdom.o \
-		wb_pam_auth.o \
-		wb_async_helpers.o
-REQUIRED_SUBSYSTEMS = RPC_NDR_LSA
+		wb_pam_auth.o
+REQUIRED_SUBSYSTEMS = WB_HELPER RPC_NDR_LSA RPC_NDR_SAMR
 # End MODULE server_service_winbind
+################################################
+
+################################################
+# Start SUBYSTEM WB_HELPER
+[SUBSYSTEM::WB_HELPER]
+ADD_OBJ_FILES = \
+		wb_async_helpers.o
+REQUIRED_SUBSYSTEMS = RPC_NDR_LSA RPC_NDR_SAMR
+# End SUBSYSTEM WB_HELPER
 ################################################
