d228cb62a7be6ae128d3418aeb0ea466c7329802
Volker Lendecke <vlendec@samba.org>
2005-11-28 20:42:18 +0000

    r11947: Back out passdb:expand_explicit until we find consensus. I'll file this as a bugzilla entry.
    Volker

--- source/passdb/passdb.c
+++ source/passdb/passdb.c
@@ -1751,8 +1751,6 @@ BOOL init_sam_from_buffer_v2(SAM_ACCOUNT *sampass, uint8 *buf, uint32 buflen)
 	uint32 pwHistLen = 0;
 	BOOL ret = True;
 	fstring tmpstring;
-	BOOL expand_explicit = lp_parm_bool(-1, "passdb", "expand_explicit",
-					    False);
 	
 	if(sampass == NULL || buf == NULL) {
 		DEBUG(0, ("init_sam_from_buffer_v2: NULL parameters found!\n"));
@@ -1817,10 +1815,7 @@ BOOL init_sam_from_buffer_v2(SAM_ACCOUNT *sampass, uint8 *buf, uint32 buflen)
 
 	if (homedir) {
 		fstrcpy( tmpstring, homedir );
-		if (expand_explicit) {
-			standard_sub_basic( username, tmpstring,
-					    sizeof(tmpstring) );
-		}
+		standard_sub_basic( username, tmpstring, sizeof(tmpstring) );
 		pdb_set_homedir(sampass, tmpstring, PDB_SET);
 	}
 	else {
@@ -1836,10 +1831,7 @@ BOOL init_sam_from_buffer_v2(SAM_ACCOUNT *sampass, uint8 *buf, uint32 buflen)
 
 	if (logon_script) {
 		fstrcpy( tmpstring, logon_script );
-		if (expand_explicit) {
-			standard_sub_basic( username, tmpstring,
-					    sizeof(tmpstring) );
-		}
+		standard_sub_basic( username, tmpstring, sizeof(tmpstring) );
 		pdb_set_logon_script(sampass, tmpstring, PDB_SET);
 	}
 	else {
@@ -1850,10 +1842,7 @@ BOOL init_sam_from_buffer_v2(SAM_ACCOUNT *sampass, uint8 *buf, uint32 buflen)
 	
 	if (profile_path) {	
 		fstrcpy( tmpstring, profile_path );
-		if (expand_explicit) {
-			standard_sub_basic( username, tmpstring,
-					    sizeof(tmpstring) );
-		}
+		standard_sub_basic( username, tmpstring, sizeof(tmpstring) );
 		pdb_set_profile_path(sampass, tmpstring, PDB_SET);
 	} 
 	else {
