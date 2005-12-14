bdc2a850daa7f0c4636df7548a748309f95fc417
Stefan Metzmacher <metze@samba.org>
2005-12-14 18:24:59 +0000

    r12239: as we only get error from our events system when we wait for read events, we need to initialy ask for read events too, otherwise we'll never get an error back from socket_connect()
    metze
    (This used to be commit 7d94054d0fc954e6d810573430f2c5d35b73125d)

--- source4/lib/socket/connect.c
+++ source4/lib/socket/connect.c
@@ -76,7 +76,7 @@ static void socket_send_connect(struct composite_context *result)
 
 	fde = event_add_fd(result->event_ctx, result,
 			   socket_get_fd(state->sock),
-			   EVENT_FD_WRITE, 
+			   EVENT_FD_READ|EVENT_FD_WRITE, 
 			   socket_connect_handler, result);
 	composite_nomem(fde, result);
 }
