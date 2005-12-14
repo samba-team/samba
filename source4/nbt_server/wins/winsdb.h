2759fc850b8cf5f58207b642e0d2d922d884a813
Stefan Metzmacher <metze@samba.org>
2005-12-14 08:38:25 +0000

    r12228: fix the expire time for RELEASED records
    metze
    (This used to be commit afb0f660a2e4e5ae78c2906e2acf9eba50ea598f)

--- source4/nbt_server/wins/winsdb.h
+++ source4/nbt_server/wins/winsdb.h
@@ -54,6 +54,19 @@ struct wins_server {
 	/* wins server database handle */
 	struct ldb_context *wins_db;
 
-	uint32_t min_ttl;
-	uint32_t max_ttl;
+	/* some configuration */
+	struct {
+		/* 
+		 * the interval (in secs) till an active record will be marked as RELEASED
+		 */
+		uint32_t min_renew_interval;
+		uint32_t max_renew_interval;
+
+		/* 
+		 * the interval (in secs) a record remains in RELEASED state,
+		 * before it will be marked as TOMBSTONE
+		 * (also known as extinction interval)
+		 */
+		uint32_t tombstone_interval;
+	} config;
 };
