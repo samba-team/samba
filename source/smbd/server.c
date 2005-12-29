8d114b832a0848113697dc9087fbf6282fb412d7
Jeremy Allison <jra@samba.org>
2005-12-29 13:10:33 +0000

    r12564: Ensure load_case_tables is always done first. Jeremy.

--- source/smbd/server.c
+++ source/smbd/server.c
@@ -718,6 +718,8 @@ void build_options(BOOL screen);
 	{ NULL }
 	};
 
+	load_case_tables();
+
 #ifdef HAVE_SET_AUTH_PARAMETERS
 	set_auth_parameters(argc,argv);
 #endif
@@ -741,8 +743,6 @@ void build_options(BOOL screen);
 #endif
 
 	sec_init();
-
-	load_case_tables();
 
 	set_remote_machine_name("smbd", False);
 
