7e702939faf70d0c02fa71b315043795bc4a1d84
Stefan Metzmacher <metze@samba.org>
2006-01-08 20:47:48 +0000

    r12777: use TEST_DATA_PREFIX if available
    metze

--- source/lib/ldb/tests/test-tdb.sh
+++ source/lib/ldb/tests/test-tdb.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-
-LDB_URL="tdbtest.ldb"
+if [ -n "$TEST_DATA_PREFIX" ]; then
+	LDB_URL="$TEST_DATA_PREFIX/tdbtest.ldb"
+else
+	LDB_URL="tdbtest.ldb"
+fi
 export LDB_URL
 
 PATH=bin:$PATH
 export PATH
 
-rm -f tdbtest.ldb
-rm -f tdbtest.ldb.1
-rm -f tdbtest.ldb.2
+rm -f $LDB_URL*
 
 if [ -z "$LDBDIR" ]; then
     LDBDIR="."
