bac186a7572cf1fd6c2cf3c3f4287f04fdb005e0
Love Hörnquist Åstrand <lha@kth.se>
2006-01-10 05:47:45 +0000

    Make hmac work.
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@16509 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/des/hmac.c
+++ lib/des/hmac.c
@@ -4,31 +4,6 @@
 #include <string.h>
 #include <hmac.h>
 
-HMAC_CTX *
-HMAC_CTX_create(void)
-{
-    return NULL;
-}
-
-void
-HMAC_CTX_destroy(HMAC_CTX *ctx)
-{
-    HMAC_CTX_cleanup(ctx);
-    if (ctx->buf) {
-	free(ctx->buf);
-	ctx->buf = NULL;
-    }
-    if (ctx->opad) {
-	free(ctx->opad);
-	ctx->opad = NULL;
-    }
-    if (ctx->ipad) {
-	free(ctx->ipad);
-	ctx->ipad = NULL;
-    }
-    free(ctx);
-}
-
 void
 HMAC_CTX_init(HMAC_CTX *ctx)
 {
@@ -38,6 +13,21 @@ HMAC_CTX_init(HMAC_CTX *ctx)
 void
 HMAC_CTX_cleanup(HMAC_CTX *ctx)
 {
+    if (ctx->buf) {
+	memset(ctx->buf, 0, ctx->key_length);
+	free(ctx->buf);
+	ctx->buf = NULL;
+    }
+    if (ctx->opad) {
+	memset(ctx->ipad, 0, ctx->key_length);
+	free(ctx->opad);
+	ctx->opad = NULL;
+    }
+    if (ctx->ipad) {
+	memset(ctx->ipad, 0, ctx->key_length);
+	free(ctx->ipad);
+	ctx->ipad = NULL;
+    }
     EVP_MD_CTX_cleanup(ctx->ctx);
 }
 
@@ -50,7 +40,7 @@ HMAC_size(const HMAC_CTX *ctx)
 void
 HMAC_Init_ex(HMAC_CTX *ctx,
 	     const void *key,
-	     size_t len,
+	     size_t keylen,
 	     const EVP_MD *md,
 	     ENGINE *engine)
 {
@@ -61,14 +51,17 @@ HMAC_Init_ex(HMAC_CTX *ctx,
 	ctx->md = md;
 	if (ctx->buf)
 	    free (ctx->buf);
-	ctx->buf = malloc(EVP_MD_size(ctx->md));
+	ctx->key_length = EVP_MD_size(ctx->md);
+	ctx->buf = malloc(ctx->key_length);
     }
+#if 0
     ctx->engine = engine;
+#endif
 
-    if (len < EVP_MD_size(ctx->md)) {
+    if (keylen > EVP_MD_size(ctx->md)) {
+	EVP_Digest(key, keylen, ctx->buf, NULL, ctx->md, engine);
 	key = ctx->buf;
-	EVP_Digest(key, len, ctx->buf, NULL, ctx->md, engine);
-	len = EVP_MD_size(ctx->md);
+	keylen = EVP_MD_size(ctx->md);
     }
 
     if (ctx->opad)
@@ -76,19 +69,20 @@ HMAC_Init_ex(HMAC_CTX *ctx,
     if (ctx->ipad)
 	free(ctx->ipad);
 
-    ctx->opad = malloc(len);
-    ctx->ipad = malloc(len);
-    ctx->key_length = len;
+    ctx->opad = malloc(EVP_MD_block_size(ctx->md));
+    ctx->ipad = malloc(EVP_MD_block_size(ctx->md));
+    memset(ctx->ipad, 0x36, EVP_MD_block_size(ctx->md));
+    memset(ctx->opad, 0x5c, EVP_MD_block_size(ctx->md));
 
-    for (i = 0, p = ctx->opad; i < len; i++)
-	p[i] = ((const unsigned char *)key)[i] ^ 0x36;
-    for (i = 0, p = ctx->ipad; i < len; i++)
-	p[i] = ((const unsigned char *)key)[i] ^ 0x5c;
+    for (i = 0, p = ctx->ipad; i < keylen; i++)
+	p[i] ^= ((const unsigned char *)key)[i];
+    for (i = 0, p = ctx->opad; i < keylen; i++)
+	p[i] ^= ((const unsigned char *)key)[i];
 
     ctx->ctx = EVP_MD_CTX_create();
 
     EVP_DigestInit_ex(ctx->ctx, ctx->md, ctx->engine);
-    EVP_DigestUpdate(ctx->ctx, ctx->ipad, ctx->key_length);
+    EVP_DigestUpdate(ctx->ctx, ctx->ipad, EVP_MD_block_size(ctx->md));
 }
 
 void
@@ -103,8 +97,8 @@ HMAC_Final(HMAC_CTX *ctx, void *md, unsigned int *len)
     EVP_DigestFinal_ex(ctx->ctx, ctx->buf, NULL);
 
     EVP_DigestInit_ex(ctx->ctx, ctx->md, ctx->engine);
-    EVP_DigestUpdate(ctx->ctx, ctx->opad, ctx->key_length);
-    EVP_DigestUpdate(ctx->ctx, ctx->buf, EVP_MD_size(ctx->md));
+    EVP_DigestUpdate(ctx->ctx, ctx->opad, EVP_MD_block_size(ctx->md));
+    EVP_DigestUpdate(ctx->ctx, ctx->buf, ctx->key_length);
     EVP_DigestFinal_ex(ctx->ctx, md, len);
 }
 
@@ -114,14 +108,12 @@ HMAC(const EVP_MD *md,
      const void *data, size_t data_size, 
      void *hash, unsigned int *hash_len)
 {
-    HMAC_CTX *ctx;
+    HMAC_CTX ctx;
 
-    ctx = HMAC_CTX_create();
-    if (ctx == NULL)
-	return NULL;
-    HMAC_Init_ex(ctx, key, key_size, md, NULL);
-    HMAC_Update(ctx, data, data_size);
-    HMAC_Final(ctx, hash, hash_len);
-    HMAC_CTX_destroy(ctx);
+    HMAC_CTX_init(&ctx);
+    HMAC_Init_ex(&ctx, key, key_size, md, NULL);
+    HMAC_Update(&ctx, data, data_size);
+    HMAC_Final(&ctx, hash, hash_len);
+    HMAC_CTX_cleanup(&ctx);
     return hash;
 }
