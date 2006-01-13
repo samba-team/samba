005c88054f526d9a14d748b665cd6b4853e60a35
Gerald Carter <jerry@samba.org>
2006-01-13 21:22:25 +0000

    r12916: use rpcstr_pull() instead of unistr_to_ascii() when validating share names (This used to be commit c08bc30698eac2f3f5dd8257b4fd7c3e23e6de39)

--- source3/rpc_server/srv_srvsvc_nt.c
+++ source3/rpc_server/srv_srvsvc_nt.c
@@ -1,5 +1,5 @@
- /* 
-  *  Unix SMB/CIFS implementation.
+/* 
+ *  Unix SMB/CIFS implementation.
  *  RPC Pipe client / server routines
  *  Copyright (C) Andrew Tridgell              1992-1997,
  *  Copyright (C) Jeremy Allison					2001.
@@ -2312,11 +2312,7 @@ WERROR _srv_net_name_validate(pipes_struct *p, SRV_Q_NET_NAME_VALIDATE *q_u, SRV
 
 	switch ( q_u->type ) {
 	case 0x9:
-		/* Run the name through alpha_strcpy() to remove any unsafe 
-		   shell characters.  Compare the copied string with the original
-		   and fail if the strings don't match */
-
-		unistr2_to_ascii(sharename, &q_u->uni_name, sizeof(sharename));
+		rpcstr_pull(sharename, q_u->uni_name.buffer, sizeof(sharename), q_u->uni_name.uni_str_len*2, 0);
 		if ( !validate_net_name( sharename, INVALID_SHARENAME_CHARS, sizeof(sharename) ) ) {
 			DEBUG(5,("_srv_net_name_validate: Bad sharename \"%s\"\n", sharename));
 			return WERR_INVALID_NAME;
