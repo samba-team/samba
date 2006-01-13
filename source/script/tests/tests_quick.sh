a841967f9d8b34cc86a7f9e2d38e19c5ee8acf09
Andrew Bartlett <abartlet@samba.org>
2006-01-13 12:48:53 +0000

    r12902: Fix 'make quicktest'.
    Andrew Bartlett

--- source/script/tests/tests_quick.sh
+++ source/script/tests/tests_quick.sh
@@ -1,6 +1,6 @@
 TORTURE_OPTIONS="$TORTURE_OPTIONS --option=torture:quick=yes"
 
-$SRCDIR/script/tests/test_ejs.sh $SERVER $USERNAME $PASSWORD || failed=`expr $failed + $?`
+$SRCDIR/script/tests/test_ejs.sh $DOMAIN $USERNAME $PASSWORD || failed=`expr $failed + $?`
 $SRCDIR/script/tests/test_ldap.sh $SERVER $USERNAME $PASSWORD || failed=`expr $failed + $?`
 $SRCDIR/script/tests/test_nbt.sh $SERVER || failed=`expr $failed + $?`
 $SRCDIR/script/tests/test_quick.sh //$SERVER/cifs $USERNAME $PASSWORD "" || failed=`expr $failed + $?`
