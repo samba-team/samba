902f48ff642e0e189a6ea2db0814f6b39191fcbe
James Peach <jpeach@samba.org>
2006-01-19 00:30:16 +0000

    r13027: Support file change notifications from FAM.

--- source/smbd/notify.c
+++ source/smbd/notify.c
@@ -212,9 +212,15 @@ BOOL change_notify_set(char *inbuf, files_struct *fsp, connection_struct *conn, 
 
 BOOL init_change_notify(void)
 {
+	cnotify = NULL;
+
 #if HAVE_KERNEL_CHANGE_NOTIFY
-	if (lp_kernel_change_notify())
+	if (cnotify == NULL && lp_kernel_change_notify())
 		cnotify = kernel_notify_init();
+#endif
+#if HAVE_FAM_CHANGE_NOTIFY
+	if (cnotify == NULL && lp_fam_change_notify())
+		cnotify = fam_notify_init();
 #endif
 	if (!cnotify) cnotify = hash_notify_init();
 	
