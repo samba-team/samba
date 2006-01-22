219aebe7e4283d2075ca3910ae700a16919120e4
Jelmer Vernooij <jelmer@samba.org>
2006-01-22 22:53:58 +0000

    r13066: Add (very simple) manpages for gepdump and gwcrontab, remove gwsam from installed binaries (as it's not ready for use yet)

--- source/gtk/config.mk
+++ source/gtk/config.mk
@@ -13,39 +13,26 @@ OBJ_FILES = common/gtk-smb.o \
 		common/credentials.o
 REQUIRED_SUBSYSTEMS = CHARSET LIBBASIC EXT_LIB_gtk RPC_NDR_SAMR
 
-################################################
-# Start BINARY gregedit
 [BINARY::gregedit]
 INSTALLDIR = BINDIR
 OBJ_FILES = tools/gregedit.o
 REQUIRED_SUBSYSTEMS = CONFIG REGISTRY GTK_SAMBA
 MANPAGE = man/gregedit.1
-# End BINARY gregedit
-################################################
 
-################################################
-# Start BINARY gepdump 
 [BINARY::gepdump]
 INSTALLDIR = BINDIR
+MANPAGE = man/gepdump.1
 OBJ_FILES = tools/gepdump.o
 REQUIRED_SUBSYSTEMS = CONFIG GTK_SAMBA RPC_NDR_EPMAPPER RPC_NDR_MGMT
-# End BINARY gepdump 
-################################################
 
-################################################
-# Start BINARY gwcrontab
 [BINARY::gwcrontab]
 INSTALLDIR = BINDIR
+MANPAGE = man/gwcrontab.1
 OBJ_FILES = tools/gwcrontab.o
 REQUIRED_SUBSYSTEMS = CONFIG GTK_SAMBA RPC_NDR_ATSVC
-# End BINARY gwcrontab
-################################################
 
-################################################
-# Start BINARY gwsam
+# This binary is disabled for now as it doesn't do anything useful yet...
 [BINARY::gwsam]
-INSTALLDIR = BINDIR
+#INSTALLDIR = BINDIR
 OBJ_FILES = tools/gwsam.o tools/gwsam_user.o
 REQUIRED_SUBSYSTEMS = CONFIG RPC_NDR_SAMR GTK_SAMBA
-# End BINARY gwsam
-################################################
