2f8b1050e1e7b2f04b304156e9e4fa2828d23f94
Andrew Bartlett <abartlet@samba.org>
2006-01-26 05:57:53 +0000

    r13153: Try to move closer to FHS.  This probably breaks everything...
    Andrew Bartlett
    (This used to be commit 2c02a45d5a6c79ced7aea0f7e9d23fae19c2d127)

--- source4/script/installmisc.sh
+++ source4/script/installmisc.sh
@@ -2,20 +2,21 @@
 # install miscellaneous files
 
 SRCDIR="$1"
-LIBDIR="$2"
-BINDIR="$3"
+JSDIR="$2"
+SETUPDIR="$3"
+BINDIR="$4"
 
 cd $SRCDIR || exit 1
 
 echo "Installing js libs"
-mkdir -p $LIBDIR/js || exit 1
-cp scripting/libjs/*.js $LIBDIR/js || exit 1
+mkdir -p $JSDIR || exit 1
+cp scripting/libjs/*.js $JSDIR/js || exit 1
 
 echo "Installing setup templates"
-mkdir -p $LIBDIR/setup || exit 1
-cp setup/*.ldif $LIBDIR/setup || exit 1
-cp setup/*.zone $LIBDIR/setup || exit 1
-cp setup/*.conf $LIBDIR/setup || exit 1
+mkdir -p $SETUPDIR || exit 1
+cp setup/*.ldif $SETUPDIR || exit 1
+cp setup/*.zone $SETUPDIR || exit 1
+cp setup/*.conf $SETUPDIR || exit 1
 
 echo "Installing script tools"
 mkdir -p "$BINDIR"
