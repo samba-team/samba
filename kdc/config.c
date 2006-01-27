ca3745e9a42d7e3c843a5bcb8e67f6bb5b7b8ba3
Love Hörnquist Åstrand <lha@kth.se>
2006-01-27 13:30:57 +0000

    Fix spelling of "Kungliga Tekniska Högskolan".
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@16665 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- kdc/config.c
+++ kdc/config.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997-2005 Kungliga Tekniska Höskolan
+ * Copyright (c) 1997-2005 Kungliga Tekniska Högskolan
  * (Royal Institute of Technology, Stockholm, Sweden). 
  *
  * All rights reserved. 
