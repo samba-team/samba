03a0363850b7b38fc3621dfde38504d66156e728
Stefan Metzmacher <metze@samba.org>
2006-01-30 07:59:53 +0000

    r13227: revert some more stuff from 13208
    metze

--- source/build/smb_build/smb_build_h.pm
+++ source/build/smb_build/smb_build_h.pm
@@ -28,10 +28,7 @@ sub _prepare_build_h($)
 
 	foreach my $key (values %{$depend}) {
 		my $DEFINE = ();
-		next if ($key->{TYPE} ne "LIBRARY" and 
-				 $key->{TYPE} ne "SUBSYSTEM" and 
-			 	 $key->{TYPE} ne "BINARY" and 
-			 	 $key->{TYPE} ne "MODULE");
+		next if ($key->{TYPE} ne "LIBRARY" and $key->{TYPE} ne "SUBSYSTEM");
 		next unless defined($key->{INIT_FUNCTIONS});
 		
 		$DEFINE->{COMMENT} = "$key->{TYPE} $key->{NAME} INIT";
