7978c451bd0f41bc5f342c0508ebd5a3150b69b7
Lars Müller <lmuelle@samba.org>
2006-01-31 10:39:45 +0000

    r13257: Fix python build with older python versions (e.g. 2.2.1) like in United         Linux 1 (UL) aka SuSE Linux Enterprise Server (SLES) 8.

--- source/python/setup.py
+++ source/python/setup.py
@@ -63,9 +63,9 @@ for lib in string.split(samba_libs):
         next_is_flag = 0;
     elif lib == "-Wl,-rpath":
         next_is_path = 1;
-    elif lib[0:2] in ("-l"):
+    elif lib[0:2] == ("-l"):
         libraries.append(lib[2:])
-    elif lib[0:8] in ("-pthread"):
+    elif lib[0:8] == ("-pthread"):
         pass # Skip linker flags
     elif lib[0:2] == "-L":
         library_dirs.append(lib[2:])
