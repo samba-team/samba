e31b339184177b99fe80d6e3781ef8b8fef96b6e
Gerald Carter <jerry@samba.org>
2006-02-22 15:08:05 +0000

    r13635: Some more fixes for 3.0.21c
    svn merge -r13601:13604 $SVNURL/branches/SAMBA_3_0
    svn merge -r13604:13607 $SVNURL/branches/SAMBA_3_0
    svn merge -r13610:13612 $SVNURL/branches/SAMBA_3_0
    svn merge -r13612:13614 $SVNURL/branches/SAMBA_3_0

--- source/libsmb/clientgen.c
+++ source/libsmb/clientgen.c
@@ -357,7 +357,15 @@ struct cli_state *cli_initialise(struct cli_state *cli)
 
 void cli_rpc_pipe_close(struct rpc_pipe_client *cli)
 {
-	if (!cli_close(cli->cli, cli->fnum)) {
+	BOOL ret;
+
+	if (!cli) {
+		return False;
+	}
+
+	ret = cli_close(cli->cli, cli->fnum);
+
+	if (!ret) {
 		DEBUG(0,("cli_rpc_pipe_close: cli_close failed on pipe %s, "
                          "fnum 0x%x "
                          "to machine %s.  Error was %s\n",
