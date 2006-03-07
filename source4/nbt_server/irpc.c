c9e5d884fcc7b83f8b15ae2e690d0c3b4be7dea6
Stefan Metzmacher <metze@samba.org>
2006-03-07 14:13:38 +0000

    r13941: fix the build
    metze
    (This used to be commit d9da948b0f7f9698decc140a0a549d27675d14e4)

--- source4/nbt_server/irpc.c
+++ source4/nbt_server/irpc.c
@@ -23,7 +23,7 @@
 
 #include "includes.h"
 #include "smbd/service_task.h"
-#include "smbd/proto.h"
+#include "smbd/service.h"
 #include "nbt_server/nbt_server.h"
 #include "nbt_server/wins/winsserver.h"
 #include "lib/socket/socket.h"
--- source4/nbt_server/nbt_server.c
+++ source4/nbt_server/nbt_server.c
@@ -22,7 +22,7 @@
 
 #include "includes.h"
 #include "smbd/service_task.h"
-#include "smbd/proto.h"
+#include "smbd/service.h"
 #include "nbt_server/nbt_server.h"
 #include "nbt_server/wins/winsserver.h"
 #include "system/network.h"
