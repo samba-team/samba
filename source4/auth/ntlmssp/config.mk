7d8424ede21185a978a222f609263506c0e20f4e
Stefan Metzmacher <metze@samba.org>
2006-03-09 12:42:45 +0000

    r14064: - split out MSRPC_PARSE into a speperate subsystem - build gensec_ntlmssp always static for now, because torture/auth/ntlmssp.c   needs to access functions from it
    metze
    (This used to be commit 43733c9556c1c92336780206e3f71bdee6e43eee)

--- source4/auth/ntlmssp/config.mk
+++ source4/auth/ntlmssp/config.mk
@@ -1,3 +1,7 @@
+[SUBSYSTEM::MSRPC_PARSE]
+PRIVATE_PROTO_HEADER = msrpc_parse.h
+OBJ_FILES = ntlmssp_parse.o
+
 ################################################
 # Start MODULE gensec_ntlmssp
 [MODULE::gensec_ntlmssp]
@@ -8,8 +12,8 @@ PRIVATE_PROTO_HEADER = proto.h
 OBJ_FILES = ntlmssp.o \
 		ntlmssp_sign.o \
 		ntlmssp_client.o \
-		ntlmssp_parse.o \
 		ntlmssp_server.o
-REQUIRED_SUBSYSTEMS = auth
+REQUIRED_SUBSYSTEMS = auth MSRPC_PARSE
+OUTPUT_TYPE = MERGEDOBJ
 # End MODULE gensec_ntlmssp
 ################################################
