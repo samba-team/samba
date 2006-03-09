d538eac9dbfdd786b533cb5fbbd35ab2fd7358ba
Jelmer Vernooij <jelmer@samba.org>
2006-03-09 22:06:54 +0000

    r14109: Modulify the smbcalls subsystem.

--- source/scripting/ejs/config.mk
+++ source/scripting/ejs/config.mk
@@ -7,32 +7,75 @@ NOPROTO = YES
 # End SUBSYSTEM EJSRPC
 #######################
 
+[MODULE::smbcalls_config]
+OBJ_FILES = smbcalls_config.o
+SUBSYSTEM = smbcalls
+INIT_FUNCTION = smb_setup_ejs_config
+
+[MODULE::smbcalls_ldb]
+OBJ_FILES = smbcalls_ldb.o
+SUBSYSTEM = smbcalls
+INIT_FUNCTION = smb_setup_ejs_ldb
+
+[MODULE::smbcalls_nbt]
+OBJ_FILES = smbcalls_nbt.o
+SUBSYSTEM = smbcalls
+INIT_FUNCTION = smb_setup_ejs_nbt
+
+[MODULE::smbcalls_samba3]
+OBJ_FILES = smbcalls_samba3.o
+SUBSYSTEM = smbcalls
+INIT_FUNCTION = smb_setup_ejs_samba3
+REQUIRED_SUBSYSTEMS = LIBSAMBA3 
+
+
+[MODULE::smbcalls_rand]
+OBJ_FILES = smbcalls_rand.o
+SUBSYSTEM = smbcalls
+INIT_FUNCTION = smb_setup_ejs_random
+
+[MODULE::smbcalls_nss]
+OBJ_FILES = smbcalls_nss.o
+SUBSYSTEM = smbcalls
+INIT_FUNCTION = smb_setup_ejs_nss
+
+[MODULE::smbcalls_data]
+OBJ_FILES = smbcalls_data.o
+SUBSYSTEM = smbcalls
+INIT_FUNCTION = smb_setup_ejs_datablob
+
+[MODULE::smbcalls_auth]
+OBJ_FILES = smbcalls_auth.o
+SUBSYSTEM = smbcalls
+INIT_FUNCTION = smb_setup_ejs_auth
+REQUIRED_SUBSYSTEMS = auth
+
+[MODULE::smbcalls_string]
+OBJ_FILES = smbcalls_string.o
+SUBSYSTEM = smbcalls
+INIT_FUNCTION = smb_setup_ejs_string
+
+[MODULE::smbcalls_sys]
+OBJ_FILES = smbcalls_sys.o
+SUBSYSTEM = smbcalls
+INIT_FUNCTION = smb_setup_ejs_system
+
 #######################
-# Start LIBRARY SMBCALLS
-[SUBSYSTEM::SMBCALLS]
+# Start LIBRARY smbcalls
+[SUBSYSTEM::smbcalls]
 PRIVATE_PROTO_HEADER = proto.h
 OBJ_FILES = \
 		smbcalls.o \
-		smbcalls_config.o \
-		smbcalls_ldb.o \
-		smbcalls_nbt.o \
 		smbcalls_cli.o \
 		smbcalls_rpc.o \
-		smbcalls_auth.o \
 		smbcalls_options.o \
-		smbcalls_nss.o \
-		smbcalls_string.o \
-		smbcalls_data.o \
-		smbcalls_rand.o \
-		smbcalls_sys.o \
 		smbcalls_creds.o \
-		smbcalls_samba3.o \
 		smbcalls_param.o \
 		ejsnet.o \
 		mprutil.o
 REQUIRED_SUBSYSTEMS = \
-		auth EJS LIBBASIC \
-		EJSRPC MESSAGING LIBSAMBA3 \
+		EJS LIBBASIC \
+		EJSRPC MESSAGING \
 		LIBNET LIBSMB LIBPOPT \
 		POPT_CREDENTIALS POPT_SAMBA \
 		dcerpc \
@@ -52,7 +95,7 @@ REQUIRED_SUBSYSTEMS = \
 		RPC_EJS_NETLOGON \
 		RPC_EJS_DRSUAPI \
 		RPC_EJS_IRPC
-# End SUBSYSTEM SMBCALLS
+# End SUBSYSTEM smbcalls
 #######################
 
 #######################
@@ -61,6 +104,6 @@ REQUIRED_SUBSYSTEMS = \
 INSTALLDIR = BINDIR
 OBJ_FILES = \
 		smbscript.o
-REQUIRED_SUBSYSTEMS = EJS LIBBASIC SMBCALLS CONFIG 
+REQUIRED_SUBSYSTEMS = EJS LIBBASIC smbcalls CONFIG 
 # End BINARY SMBSCRIPT
 #######################
