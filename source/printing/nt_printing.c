5df50dbf6d824e5d7b14f695785da99d60c14f4d
Jeremy Allison <jra@samba.org>
2006-03-11 02:12:30 +0000

    r14185: Coverity fix #56. Ensure we can't deref null. Jeremy.

--- source/printing/nt_printing.c
+++ source/printing/nt_printing.c
@@ -3531,7 +3531,7 @@ static void map_to_os2_driver(fstring drivername)
 	}
 
 	lines = file_lines_load(mapfile, &numlines,0);
-	if (numlines == 0) {
+	if (numlines == 0 || lines == NULL) {
 		DEBUG(0,("No entries in OS/2 driver map %s\n",mapfile));
 		return;
 	}
