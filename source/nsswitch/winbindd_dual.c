a32a423a0e9e0d4dd21282fd528bcd3247fddbd1
Günther Deschner <gd@samba.org>
2006-03-13 15:13:35 +0000

    r14321: When we have libnscd and winbindd comes (back) online, try to flush the nscd caches so that NSS-calls can deliver accurate information.
    Guenther

--- source/nsswitch/winbindd_dual.c
+++ source/nsswitch/winbindd_dual.c
@@ -559,6 +559,7 @@ static void child_msg_offline(int msg_type, struct process_id src, void *buf, si
 static void child_msg_online(int msg_type, struct process_id src, void *buf, size_t len)
 {
 	struct winbindd_domain *domain;
+	int ret;
 
 	DEBUG(5,("child_msg_online received.\n"));
 
@@ -569,6 +570,21 @@ static void child_msg_online(int msg_type, struct process_id src, void *buf, siz
 
 	/* Set our global state as online. */
 	set_global_winbindd_state_online();
+
+#ifdef HAVE_NSCD_FLUSH_CACHE
+	/* Flush nscd caches to get accurate new information */
+	ret = nscd_flush_cache("passwd");
+	if (ret) {
+		DEBUG(5,("failed to flush nscd cache for 'passwd' service: %s\n",
+			error_message(ret)));
+	}
+
+	ret = nscd_flush_cache("group");
+	if (ret) {
+		DEBUG(5,("failed to flush nscd cache for 'group' service: %s\n",
+			error_message(ret)));
+	}
+#endif
 
 	/* Mark everything online - delete any negative cache entries
 	   to force an immediate reconnect. */
