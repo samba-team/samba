5368119fd62d267ad253700cb8ef94448e92e6f7
Jeremy Allison <jra@samba.org>
2006-03-13 23:56:08 +0000

    r14358: Try and fix Coverity #169 by making the pointer aliasing clearer. This isn't a bug but a code clarification. Jeremy.

--- source/smbd/posix_acls.c
+++ source/smbd/posix_acls.c
@@ -516,10 +516,12 @@ static size_t count_canon_ace_list( canon_ace *list_head )
 
 static void free_canon_ace_list( canon_ace *list_head )
 {
-	while (list_head) {
-		canon_ace *old_head = list_head;
-		DLIST_REMOVE(list_head, list_head);
-		SAFE_FREE(old_head);
+	canon_ace *list, *next;
+
+	for (list = list_head; list; list = next) {
+		next = list->next;
+		DLIST_REMOVE(list_head, list);
+		SAFE_FREE(list);
 	}
 }
 
