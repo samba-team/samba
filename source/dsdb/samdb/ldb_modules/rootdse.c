82ad81e8753a6997f68ae925b19fd07ab06d76d3
Simo Sorce <idra@samba.org>
2006-03-14 17:34:00 +0000

    r14389: rootdse -> async

--- source/dsdb/samdb/ldb_modules/rootdse.c
+++ source/dsdb/samdb/ldb_modules/rootdse.c
@@ -47,31 +47,21 @@ static int do_attribute(const char * const *attrs, const char *name)
 /*
   add dynamically generated attributes to rootDSE result
 */
-static int rootdse_add_dynamic(struct ldb_module *module, struct ldb_request *req)
+static int rootdse_add_dynamic(struct ldb_module *module, struct ldb_message *msg, const char * const *attrs)
 {
 	struct private_data *priv = talloc_get_type(module->private_data, struct private_data);
-	struct ldb_search *s = &req->op.search;
-	struct ldb_message *msg;
 	struct cli_credentials *server_creds;
-
-	/* this is gross, and will be removed when I change ldb_result not
-	   to be so pointer crazy :-) */
-	if (s->res->msgs == NULL) {
-		return LDB_SUCCESS;
-	}
-
-	msg = s->res->msgs[0];
 
 	msg->dn = ldb_dn_explode(msg, "");
 
-	if (do_attribute(s->attrs, "currentTime")) {
+	if (do_attribute(attrs, "currentTime")) {
 		if (ldb_msg_add_steal_string(msg, "currentTime", 
 					     ldb_timestring(msg, time(NULL))) != 0) {
 			goto failed;
 		}
 	}
 
-	if (do_attribute(s->attrs, "supportedControl")) {
+	if (do_attribute(attrs, "supportedControl")) {
  		int i;
 		for (i = 0; i < priv->num_controls; i++) {
 			char *control = talloc_strdup(msg, priv->controls[i]);
@@ -87,12 +77,12 @@ static int rootdse_add_dynamic(struct ldb_module *module, struct ldb_request *re
 
 	server_creds = talloc_get_type(ldb_get_opaque(module->ldb, "server_credentials"), 
 				       struct cli_credentials);
-	if (server_creds && do_attribute(s->attrs, "supportedSASLMechanisms")) {
+	if (server_creds && do_attribute(attrs, "supportedSASLMechanisms")) {
 		struct gensec_security_ops **backends = gensec_security_all();
 		enum credentials_use_kerberos use_kerberos
 			= cli_credentials_get_kerberos_state(server_creds);
 		struct gensec_security_ops **ops
-			= gensec_use_kerberos_mechs(req, backends, use_kerberos);
+			= gensec_use_kerberos_mechs(msg, backends, use_kerberos);
 		int i;
 		for (i = 0; ops && ops[i]; i++) {
 			if (ops[i]->sasl_name) {
@@ -108,7 +98,7 @@ static int rootdse_add_dynamic(struct ldb_module *module, struct ldb_request *re
 		}
 	}
 
-	if (do_attribute(s->attrs, "highestCommittedUSN")) {
+	if (do_attribute(attrs, "highestCommittedUSN")) {
 		if (module->ldb->sequence_number != NULL && 
 		    ldb_msg_add_fmt(msg, "highestCommittedUSN", 
 				    "%llu", module->ldb->sequence_number(module->ldb)) != 0) {
@@ -118,7 +108,7 @@ static int rootdse_add_dynamic(struct ldb_module *module, struct ldb_request *re
 	
 	/* TODO: lots more dynamic attributes should be added here */
 
-	return 0;
+	return LDB_SUCCESS;
 
 failed:
 	return LDB_ERR_OPERATIONS_ERROR;
@@ -155,11 +145,104 @@ static int rootdse_search_bytree(struct ldb_module *module, struct ldb_request *
 
 	req->op.search.res = s->res;
 
-	if (ret == LDB_SUCCESS) {
-		ret = rootdse_add_dynamic(module, req);
+	if ((ret == LDB_SUCCESS) && (s->res->msgs != NULL)) {
+		ret = rootdse_add_dynamic(module, s->res->msgs[0], s->attrs);
 	}
 
 	talloc_free(tmp_ctx);
+
+	return ret;
+}
+
+struct rootdse_async_context {
+	struct ldb_module *module;
+	void *up_context;
+	int (*up_callback)(struct ldb_context *, void *, struct ldb_async_result *);
+	int timeout;
+
+	const char * const * attrs;
+};
+
+static int rootdse_async_callback(struct ldb_context *ldb, void *context, struct ldb_async_result *ares)
+{
+	struct rootdse_async_context *ac;
+
+	if (!context || !ares) {
+		ldb_set_errstring(ldb, talloc_asprintf(ldb, "NULL Context or Result in callback"));
+		goto error;
+	}
+
+	ac = talloc_get_type(context, struct rootdse_async_context);
+
+	if (ares->type == LDB_REPLY_ENTRY) {
+		/* for each record returned post-process to add any dynamic
+		   attributes that have been asked for */
+		if (rootdse_add_dynamic(ac->module, ares->message, ac->attrs) != LDB_SUCCESS) {
+			goto error;
+		}
+	}
+
+	return ac->up_callback(ldb, ac->up_context, ares);
+
+error:
+	talloc_free(ares);
+	return LDB_ERR_OPERATIONS_ERROR;
+}
+
+static int rootdse_search_async(struct ldb_module *module, struct ldb_request *req)
+{
+	struct rootdse_async_context *ac;
+	struct ldb_request *down_req;
+	int ret;
+
+	/* see if its for the rootDSE */
+	if (req->op.search.scope != LDB_SCOPE_BASE ||
+	    (req->op.search.base && req->op.search.base->comp_num != 0)) {
+		return ldb_next_request(module, req);
+	}
+
+	ac = talloc(req, struct rootdse_async_context);
+	if (ac == NULL) {
+		return LDB_ERR_OPERATIONS_ERROR;
+	}
+
+	ac->module = module;
+	ac->up_context = req->async.context;
+	ac->up_callback = req->async.callback;
+	ac->timeout = req->async.timeout;
+	ac->attrs = req->op.search.attrs;
+
+	down_req = talloc_zero(req, struct ldb_request);
+	if (down_req == NULL) {
+		return LDB_ERR_OPERATIONS_ERROR;
+	}
+
+	down_req->operation = req->operation;
+	/* in our db we store the rootDSE with a DN of cn=rootDSE */
+	down_req->op.search.base = ldb_dn_explode(down_req, "cn=rootDSE");
+	down_req->op.search.scope = LDB_SCOPE_BASE;
+	down_req->op.search.tree = ldb_parse_tree(down_req, "dn=*");
+	if (down_req->op.search.base == NULL || down_req->op.search.tree == NULL) {
+		ldb_oom(module->ldb);
+		talloc_free(down_req);
+		return LDB_ERR_OPERATIONS_ERROR;
+	}
+	down_req->op.search.attrs = req->op.search.attrs;
+	down_req->controls = req->controls;
+	down_req->creds = req->creds;
+
+	down_req->async.context = ac;
+	down_req->async.callback = rootdse_async_callback;
+	down_req->async.timeout = req->async.timeout;
+
+	/* perform the search */
+	ret = ldb_next_request(module, down_req);
+
+	/* do not free down_req as the call results may be linked to it,
+	 * it will be freed when the upper level request get freed */
+	if (ret == LDB_SUCCESS) {
+		req->async.handle = down_req->async.handle;
+	}
 
 	return ret;
 }
@@ -191,8 +274,13 @@ static int rootdse_request(struct ldb_module *module, struct ldb_request *req)
 	switch (req->operation) {
 	case LDB_REQ_SEARCH:
 		return rootdse_search_bytree(module, req);
+
+	case LDB_ASYNC_SEARCH:
+		return rootdse_search_async(module, req);
+		
 	case LDB_REQ_REGISTER:
 		return rootdse_register_control(module, req);
+
 	default:
 		break;
 	}
