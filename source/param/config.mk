e7cb8d767a6dd35f2ec1f02e82431ae9de1a7c97
Jelmer Vernooij <jelmer@samba.org>
2006-03-19 20:11:47 +0000

    r14573: Install samba-config and headers

--- source/param/config.mk
+++ source/param/config.mk
@@ -7,6 +7,5 @@ OBJ_FILES = loadparm.o \
 			generic.o \
 			../lib/version.o
 REQUIRED_SUBSYSTEMS = LIBSAMBA-UTIL DYNCONFIG
-PRIVATE_PROTO_HEADER = proto.h
-
-
+PUBLIC_PROTO_HEADER = proto.h
+PUBLIC_HEADERS = param.h
