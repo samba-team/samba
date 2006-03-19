c2c79711191dc078c0979c79e7b9a570b290434f
Jelmer Vernooij <jelmer@samba.org>
2006-03-19 15:59:12 +0000

    r14562: Use VPATH to solve strange heimdal dependencies (This used to be commit 4d09ffa7f128e881fc891988ea672ae93a7ab45d)

--- source4/heimdal_build/et_deps.pl
+++ source4/heimdal_build/et_deps.pl
@@ -8,7 +8,6 @@ my $basename = basename($file);
 
 my $header = $file; $header =~ s/\.et$/.h/;
 my $source = $file; $source =~ s/\.et$/.c/;
-my $short_header = $header; $short_header =~ s/(.*)\///g;
-print "$short_header $header $source: $file bin/compile_et\n";
+print "$header $source: $file bin/compile_et\n";
 print "\t\@echo \"Compiling error table $file\"\n";
 print "\t\@cd $dirname && ../../../bin/compile_et $basename\n\n";
