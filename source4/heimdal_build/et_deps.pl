356b73920732e409956997197529f7bcaa91f3be
Jelmer Vernooij <jelmer@samba.org>
2006-03-19 03:24:46 +0000

    r14557: Add hack for asn1 files (This used to be commit b4796266c3dcd000a034c94ed2ff249735d5db92)

--- source4/heimdal_build/et_deps.pl
+++ source4/heimdal_build/et_deps.pl
@@ -8,8 +8,7 @@ my $basename = basename($file);
 
 my $header = $file; $header =~ s/\.et$/.h/;
 my $source = $file; $source =~ s/\.et$/.c/;
-print "$source: $file bin/compile_et\n";
+my $short_header = $header; $short_header =~ s/(.*)\///g;
+print "$short_header $header $source: $file bin/compile_et\n";
 print "\t\@echo \"Compiling error table $file\"\n";
 print "\t\@cd $dirname && ../../../bin/compile_et $basename\n\n";
-
-print "$header: $source\n";
