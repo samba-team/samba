38168c5472236dc4d42b5bd59b1da71c99313a37
Günther Deschner <gd@samba.org>
2006-03-23 12:50:25 +0000

    r14674: Further cleanup for cached logins, only dump hashes with DEBUG_PASSWORD.
    Guenther

--- source/nsswitch/winbindd_creds.c
+++ source/nsswitch/winbindd_creds.c
@@ -117,7 +117,9 @@ NTSTATUS winbindd_store_creds(struct winbindd_domain *domain,
 
 		E_md4hash(pass, nt_pass);
 
+#if DEBUG_PASSWORD
 		dump_data(100, (const char *)nt_pass, NT_HASH_LEN);
+#endif
 
 		status = wcache_save_creds(domain, mem_ctx, &cred_sid, nt_pass);
 		if (!NT_STATUS_IS_OK(status)) {
