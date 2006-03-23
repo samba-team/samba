835f8f4339f41619ec890e38569b745c5ef5d44e
Stefan Metzmacher <metze@samba.org>
2006-03-23 14:55:38 +0000

    r14678: - we need to use 127.0.0.2/8 as interface for the server   as nmbd skip interfaces with address 127.0.0.1 - add samba3 smbclient -L tests - add samba3 smbtorture tests
    metze

--- source/script/tests/tests_all.sh
+++ source/script/tests/tests_all.sh
@@ -1,8 +1,11 @@
 
+$SCRIPTDIR/test_smbtorture_s3.sh //$SERVER_IP/tmp $USERNAME $PASSWORD "" || failed=`expr $failed + $?`
+$SCRIPTDIR/test_smbclient_s3.sh $SERVER $SERVER_IP || failed=`expr $failed + $?`
+
 if [ -n "$SMBTORTURE4" ];then
 	echo "Running Tests with Samba4's smbtorture"
 	$SMBTORTURE4 --version
-	$SCRIPTDIR/test_posix_s3.sh //$SERVER/tmp $USERNAME $PASSWORD "" || failed=`expr $failed + $?`
+	$SCRIPTDIR/test_posix_s3.sh //$SERVER_IP/tmp $USERNAME $PASSWORD "" || failed=`expr $failed + $?`
 else
 	echo "Skip Tests with Samba4's smbtorture"
 fi
