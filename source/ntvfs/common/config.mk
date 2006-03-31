33495d19cf0263f6df5ec935f51c68bde84b1d01
Andrew Tridgell <tridge@samba.org>
2006-03-31 03:02:39 +0000

    r14835: split out the config rules for the ntvfs/common/ directory

---
+++ source/ntvfs/common/config.mk
@@ -0,0 +1,13 @@
+################################################
+# Start MODULE ntvfs_common
+[MODULE::ntvfs_common]
+SUBSYSTEM = ntvfs
+PRIVATE_PROTO_HEADER = vfs_common_proto.h
+OBJ_FILES = \
+		brlock.o \
+		opendb.o \
+		notify.o \
+		sidmap.o
+REQUIRED_SUBSYSTEMS = 
+# End MODULE ntvfs_common
+################################################
