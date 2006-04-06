dc754ce95df1ed87013014b2cee26f9d53e5a59c
Andrew Tridgell <tridge@samba.org>
2006-04-06 10:12:36 +0000

    r14945: allow the notify backend to be specified per share

--- source/ntvfs/posix/vfs_posix.c
+++ source/ntvfs/posix/vfs_posix.c
@@ -188,7 +188,8 @@ static NTSTATUS pvfs_connect(struct ntvfs_module_context *ntvfs,
 	pvfs->notify_context = notify_init(pvfs, 
 					   pvfs->ntvfs->ctx->server_id,  
 					   pvfs->ntvfs->ctx->msg_ctx, 
-					   event_context_find(pvfs));
+					   event_context_find(pvfs),
+					   pvfs->ntvfs->ctx->config.snum);
 	if (pvfs->notify_context == NULL) {
 		return NT_STATUS_INTERNAL_DB_CORRUPTION;
 	}
