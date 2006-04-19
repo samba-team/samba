b603ce4fb15ac24c502d54ffce043eb869b14764
Volker Lendecke <vlendec@samba.org>
2006-04-19 15:38:43 +0000

    r15136: Fix join consistency check

--- source/utils/net_ads.c
+++ source/utils/net_ads.c
@@ -721,7 +721,7 @@ int net_ads_join(int argc, const char **argv)
 	const char *short_domain_name = NULL;
 	TALLOC_CTX *ctx = NULL;
 
-	if ((lp_server_role() != ROLE_DOMAIN_MEMBER) || 
+	if ((lp_server_role() != ROLE_DOMAIN_MEMBER) &&
 	    (lp_server_role() != ROLE_DOMAIN_BDC)) {
 		d_printf("can only join as domain member or as BDC\n");
 		return -1;
