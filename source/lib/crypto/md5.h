fdbb741df8dbeba532f49460850c5f2489422639
Jelmer Vernooij <jelmer@samba.org>
2006-04-24 14:53:03 +0000

    r15204: Fix MD5Update prototype

--- source/lib/crypto/md5.h
+++ source/lib/crypto/md5.h
@@ -11,9 +11,9 @@ struct MD5Context {
 	uint8_t in[64];
 };
 
-_PUBLIC_ void MD5Init(struct MD5Context *context);
-_PUBLIC_ void MD5Update(struct MD5Context *context, const uint8_t *buf,
-	       uint_t len);
-_PUBLIC_ void MD5Final(uint8_t digest[16], struct MD5Context *context);
+void MD5Init(struct MD5Context *context);
+void MD5Update(struct MD5Context *context, const uint8_t *buf,
+	       size_t len);
+void MD5Final(uint8_t digest[16], struct MD5Context *context);
 
 #endif /* !MD5_H */
