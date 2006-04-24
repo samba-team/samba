01714b4b483d1362a4a544d11cca943fc6f48f24
Jelmer Vernooij <jelmer@samba.org>
2006-04-24 22:50:19 +0000

    r15224: Check whether -MT is actually supported by the compiler before using automatic dependencies

--- source/include/includes.h
+++ source/include/includes.h
@@ -45,7 +45,7 @@
 #endif
 
 #ifndef PRINTF_ATTRIBUTE
-#if !defined(NO_PRINTF_ATTRIBUTE) && (__GNUC__ >= 3)
+#if __GNUC__ >= 3
 /** Use gcc attribute to check printf fns.  a1 is the 1-based index of
  * the parameter containing the format, and a2 the index of the first
  * argument. Note that some gcc 2.x versions don't handle this
