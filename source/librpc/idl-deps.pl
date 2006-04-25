b4e08b98a6a8ebfc4f22125b4474978e9384971c
Jelmer Vernooij <jelmer@samba.org>
2006-04-25 16:31:12 +0000

    r15247: Get rid of patsubst macro as it is not supported by some makes and replace it with a perl script. This should fix the build on tru64

---
+++ source/librpc/idl-deps.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+use strict;
+
+my %vars = ();
+
+foreach(@ARGV) {
+	push (@{$vars{IDL_FILES}}, $_);
+	my $b = $_; $b =~ s/.*\/(.*?).idl$/\1/;
+	push (@{$vars{IDL_HEADER_FILES}}, "librpc/gen_ndr/$b.h");
+	push (@{$vars{IDL_NDR_HEADER_FILES}}, "librpc/gen_ndr/ndr_$b.h");
+	push (@{$vars{IDL_NDR_PARSE_FILES}}, "librpc/gen_ndr/ndr_$b.c");
+	push (@{$vars{IDL_NDR_CLIENT_C_FILES}}, "librpc/gen_ndr/ndr_$b\_c.c");
+	push (@{$vars{IDL_NDR_CLIENT_HEADER_FILES}}, "librpc/gen_ndr/ndr_$b\_c.h");
+	push (@{$vars{IDL_NDR_SERVER_C_FILES}}, "librpc/gen_ndr/ndr_$b\_s.c");
+	push (@{$vars{IDL_NDR_EJS_C_FILES}}, "librpc/gen_ndr/ndr_$b\_ejs.c");
+	push (@{$vars{IDL_NDR_EJS_H_FILES}}, "librpc/gen_ndr/ndr_$b\_ejs.h");
+}
+
+foreach (keys %vars) {
+	print "$_ = " . join (' ', @{$vars{$_}}) . "\n";
+}
