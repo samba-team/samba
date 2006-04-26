6f8382432f8e6fba4d7c0f629f94ad514f0cee08
Jelmer Vernooij <jelmer@samba.org>
2006-04-26 13:17:43 +0000

    r15278: Fix recreating symlink every time.

--- source/heimdal_build/hcrypto-deps.pl
+++ source/heimdal_build/hcrypto-deps.pl
@@ -5,5 +5,5 @@ foreach (@ARGV) {
 	my $old = $_;
 	my $new = $old; $new =~ s/des/des\/hcrypto/g;
 	my $dir = $old; 
-	print "$new: $old heimdal/lib/des/hcrypto\n";
+	print "$new: heimdal/lib/des/hcrypto\n";
 }
