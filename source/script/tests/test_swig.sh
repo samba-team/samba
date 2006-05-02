ee7dc21bfc4bcaa56293ed2aa14eeeba255b4fba
Tim Potter <tpot@samba.org>
2006-05-02 10:46:03 +0000

    r15390: Make test output a bit prettier.

--- source/script/tests/test_swig.sh
+++ source/script/tests/test_swig.sh
@@ -15,7 +15,10 @@ failed=0
 export PYTHONPATH=lib/tdb/swig:lib/ldb/swig:scripting/swig:$PYTHONPATH
 export LD_LIBRARY_PATH=bin:$LD_LIBRARY_PATH
 
+echo Testing tdb wrappers
 scripting/swig/torture/torture_tdb.py || failed=`expr $failed + 1`
+
+echo Testing ldb wrappers
 scripting/swig/torture/torture_ldb.py || failed=`expr $failed + 1`
 
 testok $0 $failed
