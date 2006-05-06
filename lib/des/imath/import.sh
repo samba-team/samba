3d256d3f4a0737610224faa3d4765d02866eebb1
Love Hörnquist Åstrand <lha@kth.se>
2006-05-06 12:31:07 +0000

    import core of imath 1.1.9
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@17471 ec53bebd-3082-4978-b11e-865c3cabbd6b

---
+++ lib/des/imath/import.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# $Id$
+
+dir=$1
+
+if test ! -f "$dir"/imdrover.c ; then
+    echo $dir doesnt seem to contain imath
+    exit 1
+fi
+
+rm *.[ch]
+
+headers=`grep ^HDRS "$dir"/Makefile |sed 's/^HDRS=//' | sed 's/imdrover.h//'`
+code=`echo $headers | sed 's/\.h/.c/g'`
+
+for a in $headers $code ; do
+    cp "$dir"/"$a" .
+done
+
+echo  "imathsource =			\\"
+for a in $headers $code ; do
+    echo "	imath/$a		\\"
+done | sort
+
