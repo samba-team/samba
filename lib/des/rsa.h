8d323541036722758ff2351e2a61298a49c0cded
Love Hörnquist Åstrand <lha@kth.se>
2006-05-07 11:34:02 +0000

    expose RSA_imath_method
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@17498 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/des/rsa.h
+++ lib/des/rsa.h
@@ -40,6 +40,7 @@
 
 /* symbol renaming */
 #define RSA_null_method hc_RSA_null_method
+#define RSA_imath_method hc_RSA_imath_method
 #define RSA_new hc_RSA_new
 #define RSA_new_method hc_RSA_new_method
 #define RSA_free hc_RSA_free
@@ -128,6 +129,7 @@ struct RSA {
  */
 
 const RSA_METHOD *RSA_null_method(void);
+const RSA_METHOD *RSA_imath_method(void);
 
 /*
  *
