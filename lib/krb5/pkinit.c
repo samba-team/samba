64f9c706ecf1bce374a10f4d741cbaeee59af4ab
Love Hörnquist Åstrand <lha@kth.se>
2006-05-07 12:32:38 +0000

    (_krb5_pk_verify_sign): Use hx509_get_one_cert.
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@17506 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/krb5/pkinit.c
+++ lib/krb5/pkinit.c
@@ -682,32 +682,10 @@ _krb5_pk_verify_sign(krb5_context context,
 	goto out;
     }
 	
-    /* XXX */
-    {
-	hx509_cursor cursor;
-
-	ret = hx509_certs_start_seq(id->hx509ctx,
-				    signer_certs,
-				    &cursor);
-	if (ret) {
-	    krb5_clear_error_string(context);
-	    goto out;
-	}
-	ret = hx509_certs_next_cert(id->hx509ctx,
-				    signer_certs,
-				    cursor,
-				    &(*signer)->cert);
-	if (ret) {
-	    krb5_clear_error_string(context);
-	    goto out;
-	}
-	ret = hx509_certs_end_seq(id->hx509ctx,
-				  signer_certs,
-				  cursor);
-	if (ret) {
-	    krb5_clear_error_string(context);
-	    goto out;
-	}
+    ret = hx509_get_one_cert(id->hx509ctx, signer_certs, &(*signer)->cert);
+    if (ret) {
+	krb5_clear_error_string(context);
+	goto out;
     }
 
 out:
