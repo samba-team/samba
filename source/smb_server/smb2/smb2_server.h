2ea15e9d28bca358989a565576ea7e8d9462e924
Stefan Metzmacher <metze@samba.org>
2006-05-22 14:14:23 +0000

    r15802: merge tridge's fix to the SMB2 server
    metze

--- source/smb_server/smb2/smb2_server.h
+++ source/smb_server/smb2/smb2_server.h
@@ -108,10 +108,7 @@ struct smbsrv_request;
 		smb2srv_send_error(req, NT_STATUS_NO_MEMORY); \
 		return; \
 	} \
-	if (!talloc_reference(req->ntvfs, req)) { \
-		smb2srv_send_error(req, NT_STATUS_NO_MEMORY); \
-		return; \
-	} \
+	talloc_steal(req->tcon->ntvfs, req); \
 	req->ntvfs->frontend_data.private_data = req; \
 } while (0)
 
