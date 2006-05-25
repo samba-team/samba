45b64b0100b6f6fac360a8e0af084e3c866320f7
Andrew Tridgell <tridge@samba.org>
2006-05-25 02:09:00 +0000

    r15879: strtok_r() replacement, for solaris

--- source/lib/replace/replace.c
+++ source/lib/replace/replace.c
@@ -549,3 +549,30 @@ char *strcasestr(const char *haystack, const char *needle)
 	return NULL;
 }
 #endif
+
+#ifndef HAVE_STRTOK_R
+/* based on GLIBC version, copyright Free Software Foundation */
+char *strtok_r(char *s, const char *delim, char **save_ptr)
+{
+	char *token;
+
+	if (s == NULL) s = *save_ptr;
+
+	s += strspn(s, delim);
+	if (*s == '\0') {
+		*save_ptr = s;
+		return NULL;
+	}
+
+	token = s;
+	s = strpbrk(token, delim);
+	if (s == NULL) {
+		*save_ptr = token + strlen(token);
+	} else {
+		*s = '\0';
+		*save_ptr = s + 1;
+	}
+
+	return token;
+}
+#endif
