fd1b550851d9b6c91e5c20a692572d454e61d618
Stefan Metzmacher <metze@samba.org>
2006-06-03 09:51:59 +0000

    r16025: make sure the samba4's smbtorture can be executed
    metze
    (This used to be commit bed220afb816138a3e806c5d20e812667f6994ae)

--- source3/script/tests/tests_all.sh
+++ source3/script/tests/tests_all.sh
@@ -2,9 +2,10 @@
 $SCRIPTDIR/test_smbtorture_s3.sh //$SERVER_IP/tmp $USERNAME $PASSWORD "" || failed=`expr $failed + $?`
 $SCRIPTDIR/test_smbclient_s3.sh $SERVER $SERVER_IP || failed=`expr $failed + $?`
 
-if [ -n "$SMBTORTURE4" ];then
+SMBTORTURE4VERSION=`$SMBTORTURE4 --version`
+if [ -n "$SMBTORTURE4" -a -n "$SMBTORTURE4VERSION" ];then
 	echo "Running Tests with Samba4's smbtorture"
-	$SMBTORTURE4 --version
+	echo $SMBTORTURE4VERSION
 	$SCRIPTDIR/test_posix_s3.sh //$SERVER_IP/tmp $USERNAME $PASSWORD "" || failed=`expr $failed + $?`
 else
 	echo "Skip Tests with Samba4's smbtorture"
