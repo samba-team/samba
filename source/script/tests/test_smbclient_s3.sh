1793cc794a5850a86651c83e74f0311885e62fb7
James Peach <jpeach@samba.org>
2006-06-15 23:47:41 +0000

    r16273: Add smbclient tests to verify that         in interactive mode, smbclient always prompts on stdout         in non-interactive mode, smbclient never prompts

--- source/script/tests/test_smbclient_s3.sh
+++ source/script/tests/test_smbclient_s3.sh
@@ -11,13 +11,67 @@ fi
 
 SERVER="$1"
 SERVER_IP="$2"
+SMBCLIENT="$VALGRIND ${SMBCLIENT:-$SRCDIR/bin/smbclient} $CONFIGURATION"
 
 incdir=`dirname $0`
 . $incdir/test_functions.sh
 
 failed=0
 
-testit "smbclient -L $SERVER_IP" $VALGRIND $SRCDIR/bin/smbclient $CONFIGURATION -L $SERVER_IP -N -p 139 || failed=`expr $failed + 1`
-testit "smbclient -L $SERVER" $VALGRIND $SRCDIR/bin/smbclient $CONFIGURATION -L $SERVER -N -p 139 || failed=`expr $failed + 1`
+# Test that a noninteractive smbclient does not prompt
+test_noninteractive_no_prompt()
+{
+    prompt="smb"
+
+    echo du | \
+	$SMBCLIENT "$@" -U$USERNAME%$PASSWORD //$SERVER/tmp 2>&1 | \
+    grep $prompt
+
+    if [ $? = 0 ] ; then
+	# got a prompt .. fail
+	echo matched interactive prompt in non-interactive mode
+	false
+    else
+	true
+    fi
+}
+
+# Test that an interactive smbclient prompts to stdout
+test_interactive_prompt_stdout()
+{
+    prompt="smb"
+
+    echo du | \
+	CLI_FORCE_INTERACTIVE=yes \
+	$SMBCLIENT "$@" -U$USERNAME%$PASSWORD //$SERVER/tmp 2>/dev/null | \
+    grep $prompt
+
+    if [ $? = 0 ] ; then
+	# got a prompt .. succeed
+	true
+    else
+	echo failed to match interactive prompt on stdout
+	false
+    fi
+}
+
+testit "smbclient -L $SERVER_IP" $SMBCLIENT -L $SERVER_IP -N -p 139 || failed=`expr $failed + 1`
+testit "smbclient -L $SERVER" $SMBCLIENT -L $SERVER -N -p 139 || failed=`expr $failed + 1`
+
+testit "noninteractive smbclient does not prompt" \
+    test_noninteractive_no_prompt || \
+    failed=`expr $failed + 1`
+
+testit "noninteractive smbclient -l does not prompt" \
+   test_noninteractive_no_prompt -l /tmp || \
+    failed=`expr $failed + 1`
+
+testit "interactive smbclient prompts on stdout" \
+   test_interactive_prompt_stdout || \
+    failed=`expr $failed + 1`
+
+testit "interactive smbclient -l prompts on stdout" \
+   test_interactive_prompt_stdout -l /tmp || \
+    failed=`expr $failed + 1`
 
 testok $0 $failed
