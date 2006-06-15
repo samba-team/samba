848ac756f651a4be231e5635580c0fd5f3d3fa0e
James Peach <jpeach@samba.org>
2006-06-15 23:51:20 +0000

    r16274: Fix the smbclient prompting behaviour for both systems that have libreadline and those that don't. We always use the built-in readline replacement for non-interactive mode. Interactive prompts are always emitted to stdout and non-interactive mode never prompts at all.
    Introduce x_fdup to avoid spuriously closing stdout when a logfile is
    specified on the command line and setup_logging is called a second time.

--- source/client/client.c
+++ source/client/client.c
@@ -3422,8 +3422,9 @@ static int do_message_op(void)
         /* set default debug level to 0 regardless of what smb.conf sets */
 	setup_logging( "smbclient", True );
 	DEBUGLEVEL_CLASS[DBGC_ALL] = 1;
-	dbf = x_stderr;
-	x_setbuf( x_stderr, NULL );
+	if ((dbf = x_fdup(x_stderr))) {
+		x_setbuf( dbf, NULL );
+	}
 
 	pc = poptGetContext("smbclient", argc, (const char **) argv, long_options, 
 				POPT_CONTEXT_KEEP_FIRST);
