0b16350fa2da39a66c4479dbf74182b06f7ed91a
Stefan Metzmacher <metze@samba.org>
2006-07-25 20:05:00 +0000

    r17240: move extended operations to a new file
    metze

--- source/ldap_server/config.mk
+++ source/ldap_server/config.mk
@@ -9,7 +9,8 @@ PRIVATE_PROTO_HEADER = proto.h
 OBJ_FILES = \
 		ldap_server.o \
 		ldap_backend.o \
-		ldap_bind.o
+		ldap_bind.o \
+		ldap_extended.o
 PUBLIC_DEPENDENCIES = \
 		LIBCLI_LDAP SAMDB process_model auth GENSEC_SOCKET
 # End SUBSYSTEM SMB
