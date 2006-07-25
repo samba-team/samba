aeb8077b9600ffea6e4e7ee5caca54fbf941eb6e
Stefan Metzmacher <metze@samba.org>
2006-07-25 20:05:00 +0000

    r17240: move extended operations to a new file
    metze
    (This used to be commit 0b16350fa2da39a66c4479dbf74182b06f7ed91a)

--- source4/ldap_server/ldap_backend.c
+++ source4/ldap_server/ldap_backend.c
@@ -25,10 +25,6 @@
 #include "lib/ldb/include/ldb.h"
 #include "lib/ldb/include/ldb_errors.h"
 #include "lib/db_wrap.h"
-#include "lib/tls/tls.h"
-#include "smbd/service_task.h"
-#include "smbd/service_stream.h"
-#include "smbd/service.h"
 
 #define VALID_DN_SYNTAX(dn,i) do {\
 	if (!(dn)) {\
@@ -735,77 +731,6 @@ static NTSTATUS ldapsrv_AbandonRequest(struct ldapsrv_call *call)
 	return NT_STATUS_OK;
 }
 
-
-struct ldapsrv_starttls_context {
-	struct ldapsrv_connection *conn;
-	struct socket_context *tls_socket;
-};
-
-static void ldapsrv_start_tls(void *private) 
-{
-	struct ldapsrv_starttls_context *ctx = talloc_get_type(private, struct ldapsrv_starttls_context);
-	talloc_steal(ctx->conn->connection, ctx->tls_socket);
-	talloc_unlink(ctx->conn->connection, ctx->conn->connection->socket);
-
-	ctx->conn->sockets.tls = ctx->tls_socket;
-	ctx->conn->connection->socket = ctx->tls_socket;
-	packet_set_socket(ctx->conn->packet, ctx->conn->connection->socket);
-}
-
-static NTSTATUS ldapsrv_ExtendedRequest(struct ldapsrv_call *call)
-{
-	struct ldap_ExtendedRequest *req = &call->request->r.ExtendedRequest;
-	struct ldapsrv_reply *reply;
-
-	DEBUG(10, ("Extended\n"));
-
-	reply = ldapsrv_init_reply(call, LDAP_TAG_ExtendedResponse);
-	if (!reply) {
-		return NT_STATUS_NO_MEMORY;
-	}
-
-	ZERO_STRUCT(reply->msg->r);
-
-	/* check if we have a START_TLS call */
-	if (strcmp(req->oid, LDB_EXTENDED_START_TLS_OID) == 0) {
-		struct ldapsrv_starttls_context *ctx;
-		int result = 0;
-		const char *errstr;
-		ctx = talloc(call, struct ldapsrv_starttls_context); 
-
-		if (ctx) {
-			ctx->conn = call->conn;
-			ctx->tls_socket = tls_init_server(call->conn->service->tls_params,
-						 call->conn->connection->socket,
-						 call->conn->connection->event.fde, 
-						 NULL);
-		} 
-
-		if (!ctx || !ctx->tls_socket) {
-			result = LDAP_OPERATIONS_ERROR;
-			errstr = talloc_asprintf(reply, 
-						 "START-TLS: Failed to setup TLS socket");
-		} else {
-			result = LDAP_SUCCESS;
-			errstr = NULL;
-			call->send_callback = ldapsrv_start_tls;
-			call->send_private  = ctx;
-		}
-
-		reply->msg->r.ExtendedResponse.response.resultcode = result;
-		reply->msg->r.ExtendedResponse.response.errormessage = errstr;
-		reply->msg->r.ExtendedResponse.oid = talloc_strdup(reply, req->oid);
-		if (!reply->msg->r.ExtendedResponse.oid) {
-			return NT_STATUS_NO_MEMORY;
-		}
-	}
-
-	/* TODO: OID not recognized, return a protocol error */
-
-	ldapsrv_queue_reply(call, reply);
-	return NT_STATUS_OK;
-}
-
 NTSTATUS ldapsrv_do_call(struct ldapsrv_call *call)
 {
 	switch(call->request->type) {
@@ -833,5 +758,3 @@ NTSTATUS ldapsrv_do_call(struct ldapsrv_call *call)
 		return ldapsrv_unwilling(call, 2);
 	}
 }
-
-
