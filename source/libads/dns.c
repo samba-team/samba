6bb90327142eac9fade4d39fe1611e9a4a66283a
Gerald Carter <jerry@samba.org>
2006-07-25 20:11:03 +0000

    r17242: BUG 3957: make sure to zero memory in the SRV hostlist in case there is not an A record for each SRV name

--- source/libads/dns.c
+++ source/libads/dns.c
@@ -343,7 +343,7 @@ static NTSTATUS ads_dns_lookup_srv( TALLOC_CTX *ctx, const char *name, struct dn
 	DEBUG(4,("ads_dns_lookup_srv: %d records returned in the answer section.\n", 
 		answer_count));
 		
-	if ( (dcs = TALLOC_ARRAY(ctx, struct dns_rr_srv, answer_count)) == NULL ) {
+	if ( (dcs = TALLOC_ZERO_ARRAY(ctx, struct dns_rr_srv, answer_count)) == NULL ) {
 		DEBUG(0,("ads_dns_lookup_srv: talloc() failure for %d char*'s\n", 
 			answer_count));
 		return NT_STATUS_NO_MEMORY;
