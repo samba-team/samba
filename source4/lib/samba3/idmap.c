bca791da85d7b82636fb4e94c5466711ab17af74
Stefan Metzmacher <metze@samba.org>
2006-08-24 09:41:45 +0000

    r17779: this macro is unused
    metze
    (This used to be commit 5389ba19c8ce4cc8f5992b2f9b4fc451df8ee3f2)

--- source4/lib/samba3/idmap.c
+++ source4/lib/samba3/idmap.c
@@ -34,7 +34,9 @@
 #define HWM_USER   "USER HWM"
 
 /* idmap version determines auto-conversion */
+#if 0 /* unused */
 #define IDMAP_VERSION 2
+#endif
 
 /*****************************************************************************
  Initialise idmap database. 
