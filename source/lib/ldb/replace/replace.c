3cfaceefef44bd3d895feb056d6d5e87dbf66c98
Volker Lendecke <vlendec@samba.org>
2006-08-26 17:19:58 +0000

    r17842: After talking to Simo, apply the next attempt to resolve the strnlen problem. Timegm is the same. Simo says this is just a workaround, but it helps for now. Feel free to revert.
    Volker

--- source/lib/ldb/replace/replace.c
+++ source/lib/ldb/replace/replace.c
@@ -23,7 +23,7 @@
 #include "includes.h"
 #include "ldb/include/includes.h"
 
-#ifndef HAVE_STRNLEN
+#if !defined(HAVE_STRNLEN) && !defined(_SAMBA_BUILD_)
 /**
  Some platforms don't have strnlen
 **/
