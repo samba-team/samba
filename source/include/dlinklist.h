7d1c23ebb269af2980442697ab879a813e1ade64
Stefan Metzmacher <metze@samba.org>
2006-08-28 12:15:13 +0000

    r17882: merge change from samba3
    metze

--- source/include/dlinklist.h
+++ source/include/dlinklist.h
@@ -46,7 +46,7 @@ do { \
 		if ((p)->prev) (p)->prev->next = (p)->next; \
 		if ((p)->next) (p)->next->prev = (p)->prev; \
 	} \
-	if ((p) && ((p) != (list))) (p)->next = (p)->prev = NULL; \
+	if ((p) != (list)) (p)->next = (p)->prev = NULL; \
 } while (0)
 
 /* promote an element to the top of the list */
