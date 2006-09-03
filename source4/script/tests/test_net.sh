92b8f4cfb6d32a90e5b4b956824b25fa2d9efe42
Rafal Szczesniak <mimir@samba.org>
2006-09-03 12:30:55 +0000

    r18017: Separate NET tests out of RPC. This way it's easier to run libnet tests only for quick checking.
    rafal
    (This used to be commit a6c9a01e89d837d0ce8e45499ff5d3dfa8fb46f4)

---
+++ source4/script/tests/test_net.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# add tests to this list as they start passing, so we test
+# that they stay passing
+ncacn_np_tests="NET-API-RPCCONN-BIND NET-API-LISTSHARES NET-API-CREATEUSER NET-API-DELETEUSER"
+ncalrpc_tests="NET-API-LISTSHARES NET-API-CREATEUSER NET-API-DELETEUSER"
+ncacn_ip_tcp_tests="NET-API-LISTSHARES NET-API-CREATEUSER NET-API-DELETEUSER"
+
+if [ $# -lt 4 ]; then
+cat <<EOF
+Usage: test_rpc.sh SERVER USERNAME PASSWORD DOMAIN
+EOF
+exit 1;
+fi
+
+server="$1"
+username="$2"
+password="$3"
+domain="$4"
+shift 4
+
+incdir=`dirname $0`
+. $incdir/test_functions.sh
+
+failed=0
+for bindoptions in seal,validate,padcheck bigendian; do
+ for transport in ncalrpc ncacn_np ncacn_ip_tcp; do
+     case $transport in
+	 ncalrpc) tests=$ncalrpc_tests ;;
+	 ncacn_np) tests=$ncacn_np_tests ;;
+	 ncacn_ip_tcp) tests=$ncacn_ip_tcp_tests ;;
+     esac
+   for t in $tests; do
+    name="$t on $transport with $bindoptions"
+    testit "$name" $VALGRIND bin/smbtorture $TORTURE_OPTIONS $transport:"$server[$bindoptions]" -U"$username"%"$password" -W $domain $t "$*" || failed=`expr $failed + 1`
+   done
+ done
+done
+
+testok $0 $failed
