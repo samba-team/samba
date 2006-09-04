d8687ce8497a03c7ac81090397fb386dc2d75e3e
Andrew Tridgell <tridge@samba.org>
2006-09-04 13:35:03 +0000

    r18044: timegm.c needs to be in a separate file

--- source/lib/replace/replace.c
+++ source/lib/replace/replace.c
@@ -468,7 +468,7 @@ char *rep_mkdtemp(char *template)
 {
 	char *dname;
 	
-	if (dname = mktemp(template)) {
+	if ((dname = mktemp(template))) {
 		if (mkdir(dname, 0700) >= 0) {
 			return dname;
 		}
@@ -603,34 +603,3 @@ int rep_setenv(const char *name, const char *value, int overwrite)
 }
 #endif
 
-#if !defined(HAVE_TIMEGM)
-
-static int is_leap(unsigned y)
-{
-	y += 1900;
-	return (y % 4) == 0 && ((y % 100) != 0 || (y % 400) == 0);
-}
-
-time_t timegm(struct tm *tm)
-{
-	static const unsigned ndays[2][12] ={
-		{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
-		{31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}};
-	time_t res = 0;
-	unsigned i;
-	
-	for (i = 70; i < tm->tm_year; ++i)
-		res += is_leap(i) ? 366 : 365;
-	
-	for (i = 0; i < tm->tm_mon; ++i)
-		res += ndays[is_leap(tm->tm_year)][i];
-	res += tm->tm_mday - 1;
-	res *= 24;
-	res += tm->tm_hour;
-	res *= 60;
-	res += tm->tm_min;
-	res *= 60;
-	res += tm->tm_sec;
-	return res;
-}
-#endif
