72088136ff8dc65617290996e3f02c17b5baff4b
Andrew Tridgell <tridge@samba.org>
2006-09-05 23:32:35 +0000

    r18109: move ldb to use the same build methods

--- source/lib/ldb/autogen.sh
+++ source/lib/ldb/autogen.sh
@@ -3,8 +3,12 @@
 rm -rf autom4te.cache
 rm -f configure config.h.in
 
-autoheader || exit 1
-autoconf || exit 1
+IPATHS="-I libreplace -I lib/replace -I ../libreplace -I ../replace"
+IPATHS="$IPATHS -I lib/talloc -I talloc -I ../talloc"
+IPATHS="$IPATHS -I lib/tdb -I tdb -I ../tdb"
+IPATHS="$IPATHS -I lib/popt -I popt -I ../popt"
+autoheader $IPATHS || exit 1
+autoconf $IPATHS || exit 1
 
 rm -rf autom4te.cache
 
