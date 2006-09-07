9695a4bd530d0f49e6a87c6bb56502c6d2d3bdbd
Stefan Metzmacher <metze@samba.org>
2006-09-07 10:25:55 +0000

    r18214: don't add empty elements
    metze

--- source/build/smb_build/output.pm
+++ source/build/smb_build/output.pm
@@ -132,8 +132,22 @@ sub create_output($$)
 			my $elem = $depend->{$_};
 			next if $elem == $part;
 
-			push(@{$part->{PUBLIC_CFLAGS}}, @{$elem->{CPPFLAGS}}) if defined(@{$elem->{CPPFLAGS}});
-			push(@{$part->{PUBLIC_CFLAGS}}, $elem->{CFLAGS}) if defined($elem->{CFLAGS});
+			if (defined(@{$elem->{CPPFLAGS}})) {
+				my $count = 0;
+				foreach (@{$elem->{CPPFLAGS}}) {
+					$count++;
+				}
+				push(@{$part->{PUBLIC_CFLAGS}}, @{$elem->{CPPFLAGS}}) if ($count > 0);
+			}
+
+			next if not defined($elem->{CFLAGS});
+			next if $elem->{CFLAGS} eq "";
+			my $found = 0;
+			foreach my $line (@{$part->{PUBLIC_CFLAGS}}) {
+				$found = 1 if ($line eq $elem->{CFLAGS});
+			}
+			next if ($found == 1);
+			push(@{$part->{PUBLIC_CFLAGS}}, $elem->{CFLAGS});
 		}
 
 
