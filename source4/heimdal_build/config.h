820492ba79aba44ab35dd7af7c0e8cafab61f114
Andrew Tridgell <tridge@samba.org>
2006-09-07 04:52:06 +0000

    r18203: rearranged the overrides in heimdal_build/*.h to avoid the use of heimdal_build/config.h from interfering with libreplace
    This is a workaround for the fact that with the current CFLAGS and
    dependencies system, -Iheimdal_build is put at the front of the
    includes list, which means it overrides the normal config.h
    (This used to be commit 84335903ceaf2242e43a0c5464e33408daec44b4)

--- source4/heimdal_build/config.h
+++ source4/heimdal_build/config.h
@@ -4,41 +4,12 @@
 */
 
 #ifndef HAVE_HEIMDAL_CONFIG_H
-#define HAVE_HEIMDAL_CONFIG_H 1
+#define HAVE_HEIMDAL_CONFIG_H
+
+#define RCSID(msg)
+#define KRB5
 
 #include "include/config.h"
 #include "replace.h"
-
-#define RCSID(msg)
-
-#ifdef VOID_RETSIGTYPE
-#define SIGRETURN(x) return
-#else
-#define SIGRETURN(x) return (RETSIGTYPE)(x)
-#endif
-
-/* path to sysconf - should we force this to samba LIBDIR ? */
-#define SYSCONFDIR "/etc"
-
-/* HDB module dir - set to Samba LIBDIR/hdb ? */
-#define HDBDIR "/usr/heimdal/lib"
-
-/* Maximum values on all known systems */
-#define MaxHostNameLen (64+4)
-#define MaxPathLen (1024+4)
-
-#define VERSIONLIST {"Lorikeet-Heimdal, Modified for Samba4 0.7rc1"}
-
-#define VERSION "Samba"
-
-#define ROKEN_LIB_FUNCTION
-
-/* these should be done with configure tests */
-#define KRB5
-
-#define GETHOSTBYADDR_PROTO_COMPATIBLE
-#define GETSERVBYNAME_PROTO_COMPATIBLE
-#define OPENLOG_PROTO_COMPATIBLE
-#define GETSOCKNAME_PROTO_COMPATIBLE
 
 #endif
