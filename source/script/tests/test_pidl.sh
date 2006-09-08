3c54e2d83a1b496474bce94f4d23f3603594a4bb
Andrew Tridgell <tridge@samba.org>
2006-09-08 04:54:30 +0000

    r18251: skip pidl tests unless the platform has Test::More

--- source/script/tests/test_pidl.sh
+++ source/script/tests/test_pidl.sh
@@ -9,8 +9,12 @@ incdir=`dirname $0`
 
 failed=0
 
-for f in pidl/tests/*.pl; do
-    testit "$f" $PERL $f || failed=`expr $failed + 1`
-done
+if $PERL -e 'eval require Test::More;' > /dev/null 2>&1; then
+  for f in pidl/tests/*.pl; do
+     testit "$f" $PERL $f || failed=`expr $failed + 1`
+  done
+else 
+   echo "Skipping pidl tests - Test::More not installed"
+fi
 
 testok $0 $failed
