db96ce03e99e657c4590d576d99f95da70cef5d5
Andrew Tridgell <tridge@samba.org>
2006-09-11 00:59:31 +0000

    r18351: functions as initialisers in structures doesn't work on some compilers (This used to be commit db694a7c82e4b5071aa7f609de6ecde90a8d42dd)

--- source4/lib/crypto/md4test.c
+++ source4/lib/crypto/md4test.c
@@ -31,50 +31,49 @@ BOOL torture_local_crypto_md4(struct torture_context *torture)
 	BOOL ret = True;
 	uint32_t i;
 	struct {
-		DATA_BLOB data;
-		DATA_BLOB md4;
+		const char *data;
+		const char *md4;
 	} testarray[] = {
 	{
-		.data	= data_blob_string_const(""),
-		.md4	= strhex_to_data_blob("31d6cfe0d16ae931b73c59d7e0c089c0")
+		.data	= "",
+		.md4	= "31d6cfe0d16ae931b73c59d7e0c089c0"
 	},{
-		.data	= data_blob_string_const("a"),
-		.md4	= strhex_to_data_blob("bde52cb31de33e46245e05fbdbd6fb24")
+		.data	= "a",
+		.md4	= "bde52cb31de33e46245e05fbdbd6fb24"
 	},{
-		.data	= data_blob_string_const("abc"),
-		.md4	= strhex_to_data_blob("a448017aaf21d8525fc10ae87aa6729d")
+		.data	= "abc",
+		.md4	= "a448017aaf21d8525fc10ae87aa6729d"
 	},{
-		.data	= data_blob_string_const("message digest"),
-		.md4	= strhex_to_data_blob("d9130a8164549fe818874806e1c7014b")
+		.data	= "message digest",
+		.md4	= "d9130a8164549fe818874806e1c7014b"
 	},{
-		.data	= data_blob_string_const("abcdefghijklmnopqrstuvwxyz"),
-		.md4	= strhex_to_data_blob("d79e1c308aa5bbcdeea8ed63df412da9")
+		.data	= "abcdefghijklmnopqrstuvwxyz",
+		.md4	= "d79e1c308aa5bbcdeea8ed63df412da9"
 	},{
-		.data	= data_blob_string_const("ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-						 "abcdefghijklmnopqrstuvwxyz"
-						 "0123456789"),
-		.md4	= strhex_to_data_blob("043f8582f241db351ce627e153e7f0e4")
+		.data	= "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
+		.md4	= "043f8582f241db351ce627e153e7f0e4"
 	},{
-		.data	= data_blob_string_const("123456789012345678901234567890"
-						 "123456789012345678901234567890"
-						 "12345678901234567890"),
-		.md4	= strhex_to_data_blob("e33b4ddc9c38f2199c3e7b164fcc0536")
+		.data	= "12345678901234567890123456789012345678901234567890123456789012345678901234567890",
+		.md4	= "e33b4ddc9c38f2199c3e7b164fcc0536"
 	}
 	};
 
 	for (i=0; i < ARRAY_SIZE(testarray); i++) {
 		uint8_t md4[16];
 		int e;
+		DATA_BLOB data;
+		DATA_BLOB md4blob;
 
-		mdfour(md4, testarray[i].data.data, testarray[i].data.length);
+		data = data_blob_string_const(testarray[i].data);
+		md4blob  = strhex_to_data_blob(testarray[i].md4);
 
-		e = memcmp(testarray[i].md4.data,
-			   md4,
-			   MIN(testarray[i].md4.length, sizeof(md4)));
+		mdfour(md4, data.data, data.length);
+
+		e = memcmp(md4blob.data, md4, MIN(md4blob.length, sizeof(md4)));
 		if (e != 0) {
 			printf("md4 test[%u]: failed\n", i);
-			dump_data(0, testarray[i].data.data, testarray[i].data.length);
-			dump_data(0, testarray[i].md4.data, testarray[i].md4.length);
+			dump_data(0, data.data, data.length);
+			dump_data(0, md4blob.data, md4blob.length);
 			dump_data(0, md4, sizeof(md4));
 			ret = False;
 		}
