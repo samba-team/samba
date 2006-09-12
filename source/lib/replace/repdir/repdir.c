5279656a15e42e6dd96ddfa181597c71fc993ff3
Stefan Metzmacher <metze@samba.org>
2006-09-12 07:04:12 +0000

    r18418: Tru64 has a different prototype for seekdir() returning int when _OSF_SOURCE is defined
    metze

--- source/lib/replace/repdir/repdir.c
+++ source/lib/replace/repdir/repdir.c
@@ -118,7 +118,11 @@ long telldir(DIR *dir)
 	return d->seekpos + d->ofs;
 }
 
+#ifdef _OSF_SOURCE
+int seekdir(DIR *dir, long ofs)
+#else
 void seekdir(DIR *dir, long ofs)
+#endif
 {
 	struct dir_buf *d = (struct dir_buf *)dir;
 	d->seekpos = lseek(d->fd, ofs & ~(DIR_BUF_SIZE-1), SEEK_SET);
@@ -127,6 +131,9 @@ void seekdir(DIR *dir, long ofs)
 	while (d->ofs < (ofs & (DIR_BUF_SIZE-1))) {
 		if (readdir(dir) == NULL) break;
 	}
+#ifdef _OSF_SOURCE
+	return -1;
+#else
 }
 
 void rewinddir(DIR *dir)
