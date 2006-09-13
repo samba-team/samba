c0d9e7d473b8e3eb2524a9fc29cf88680f994b36
Andrew Tridgell <tridge@samba.org>
2006-09-13 00:10:38 +0000

    r18436: converted ldb to use talloc_move() instead of talloc_steal() when appropriate.
    Note that I also removed the error checks that were being done on the
    result of talloc_steal(). They are pointless as talloc_steal() doesn't
    have any failure modes that wouldn't cause a segv anyway, and they
    tend to clutter the code

--- source/lib/ldb/ldb_sqlite3/ldb_sqlite3.c
+++ source/lib/ldb/ldb_sqlite3/ldb_sqlite3.c
@@ -865,11 +865,7 @@ static int lsql_search_sync_callback(struct ldb_context *ldb, void *context, str
 
 		res->msgs[res->count + 1] = NULL;
 
-		res->msgs[res->count] = talloc_steal(res->msgs, ares->message);
-		if (! res->msgs[res->count]) {
-			goto error;
-		}
-
+		res->msgs[res->count] = talloc_move(res->msgs, ares->message);
 		res->count++;
 	} else {
 		ldb_debug(ldb, LDB_DEBUG_ERROR, "unrecognized async reply in ltdb_search_sync_callback!\n");
