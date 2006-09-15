1ae6306a644a7a5d5acaea539521e034c6342cac
Andrew Tridgell <tridge@samba.org>
2006-09-15 21:05:03 +0000

    r18569: add really simple testing of the 'simple' ntvfs backend, so we know when it breaks. It isn't much good as a template for developers to use unless it works :-)

---
+++ source/script/tests/test_simple.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+# run a quick set of filesystem tests
+
+if [ $# -lt 3 ]; then
+cat <<EOF
+Usage: test_simple.sh UNC USERNAME PASSWORD <first> <smbtorture args>
+EOF
+exit 1;
+fi
+
+unc="$1"
+username="$2"
+password="$3"
+start="$4"
+shift 4
+ADDARGS="$*"
+
+incdir=`dirname $0`
+. $incdir/test_functions.sh
+
+tests="BASE-RW1"
+
+failed=0
+for t in $tests; do
+    if [ ! -z "$start" -a "$start" != $t ]; then
+	continue;
+    fi
+    start=""
+    name="$t"
+    testit "$name" $VALGRIND bin/smbtorture $TORTURE_OPTIONS $ADDARGS $unc -U"$username"%"$password" $t || failed=`expr $failed + 1`
+done
+
+testok $0 $failed
+
