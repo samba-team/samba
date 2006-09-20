d062e101664a90f2f7bf0980449f920aa719ee76
Jelmer Vernooij <jelmer@samba.org>
2006-09-20 17:19:05 +0000

    r18721: Fix base and samba3sam EJS tests to work without installation.

--- source/script/tests/test_ejs.sh
+++ source/script/tests/test_ejs.sh
@@ -21,7 +21,7 @@ DATADIR=../testdata
 PATH=bin:$PATH
 export PATH
 
-testit "base.js" $SCRIPTDIR/base.js || failed=`expr $failed + 1`
+testit "base.js" $SCRIPTDIR/base.js $CONFIGURATION || failed=`expr $failed + 1`
 
 for f in samr.js echo.js; do
     testit "$f" $SCRIPTDIR/$f $CONFIGURATION ncalrpc: -U$USERNAME%$PASSWORD || failed=`expr $failed + 1`
@@ -31,7 +31,7 @@ testit "ejsnet.js" $SCRIPTDIR/ejsnet.js $CONFIGURATION -U$USERNAME%$PASSWORD $DO
 
 testit "ldb.js" $SCRIPTDIR/ldb.js `pwd` $CONFIGURATION || failed=`expr $failed + 1`
 
-testit "samba3sam.js" $SCRIPTDIR/samba3sam.js `pwd` $DATADIR/samba3/ || failed=`expr $failed + 1`
+testit "samba3sam.js" $SCRIPTDIR/samba3sam.js $CONFIGURATION `pwd` $DATADIR/samba3/ || failed=`expr $failed + 1`
 
 testit "winreg" scripting/bin/winreg $CONFIGURATION ncalrpc: 'HKLM' -U$USERNAME%$PASSWORD || failed=`expr $failed + 1`
 
