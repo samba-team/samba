c906122efc4342544abb9cd7f6b138cb62224876
Andrew Bartlett <abartlet@samba.org>
2006-09-22 20:00:49 +0000

    r18829: Print a nice welcome message when we enter the test environment.
    Andrew Bartlett

--- source/script/tests/tests_xterm.sh
+++ source/script/tests/tests_xterm.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-xterm
+xterm -e 'echo -e "Welcome to the Samba4 Test environment\n\
+This matches the client environment used in make test\n\
+smbd is pid `cat $PIDDIR/smbd.pid`\n\
+\n\
+Some useful environment variables:\n\
+AUTH=$AUTH\n\
+TORTURE_OPTIONS=$TORTURE_OPTIONS\n\
+CONFIGURATION=$CONFIGURATION\n\
+SERVER=$SERVER\n\
+NETBIOSNAME=$NETBIOSNAME" && bash'
