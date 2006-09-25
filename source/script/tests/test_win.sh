b451310aefec9cd3bec46d14e76c85cc7ecf601d
Brad Henry <brad@samba.org>
2006-09-25 03:54:46 +0000

    r18884: client_test.exp getting changed to client_tests.exp was an intentional error that I inserted for testing earlier and forgot to take out before commiting. The other error was a real bug. :)

--- source/script/tests/test_win.sh
+++ source/script/tests/test_win.sh
@@ -106,7 +106,7 @@ cat $WINTEST_DIR/common.exp > $TMPDIR/client_test.exp
 cat $WINTEST_DIR/wintest_client.exp >> $TMPDIR/client_test.exp
 
 testit "$test_name" \
-	expect $TMPDIR/client_tests.exp || all_errs`expr $all_errs + 1`
+	expect $TMPDIR/client_test.exp || all_errs=`expr $all_errs + 1`
 
 if [ $old_errs -lt $all_errs ]; then
 	# Restore snapshot to ensure VM is in a known state.
