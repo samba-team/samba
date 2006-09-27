255414e9fe51835e360a173ef145dce1c6e48de0
Andrew Tridgell <tridge@samba.org>
2006-09-27 03:34:50 +0000

    r18939: don't rely on the umask being right in ldb creation. Both Samba3 and Samba4 smbd force the umask to 0, which meant we ended up with ldb being world writable.
    This isn't really an ideal fix, as it means ldb no longer honors umask
    (as it should do, like all good libraries).
    
    Unfortunately the 'proper' fix is too complex for now
    
    this also merges a tiny code style fix from s4 to s3

--- source/lib/ldb/ldb_tdb/ldb_tdb.c
+++ source/lib/ldb/ldb_tdb/ldb_tdb.c
@@ -1036,7 +1036,7 @@ static int ltdb_connect(struct ldb_context *ldb, const char *url,
 
 	/* note that we use quite a large default hash size */
 	ltdb->tdb = ltdb_wrap_open(ltdb, path, 10000, 
-				   tdb_flags, open_flags, 0666, ldb);
+				   tdb_flags, open_flags, 0644, ldb);
 	if (!ltdb->tdb) {
 		ldb_debug(ldb, LDB_DEBUG_ERROR, "Unable to open tdb '%s'\n", path);
 		talloc_free(ltdb);
