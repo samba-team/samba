26f2cb71ebf00b2c6f356da5f32384f7fa083521
Volker Lendecke <vlendec@samba.org>
2006-10-06 14:39:47 +0000

    r19132: Fix some C++ warnings. Is there interest to have them in Samba4 as well?
    I have some problems resolving the last 3 ones in attrib_handlers.c. In line
    251 the function ldb_dn_explode_casefold is called with mem_ctx as the first
    argument. Looking at ldb_dn_explode_casefold I see that the first argument it
    expects is a struct ldb_context. I could certainly add a cast to (struct
    ldb_context *) to that call, but I would assume that this is the wrong fix. Is
    it possible that attrib_handlers.c:251 and :254 should have ldb and not
    mem_ctx as the first argument?
    
    Can anybody from Samba4 clarify this for me and apply the correct fix?
    
    Thanks a lot.
    
    Volker

--- source/lib/ldb/common/ldb_msg.c
+++ source/lib/ldb/common/ldb_msg.c
@@ -797,7 +797,7 @@ int ldb_msg_check_string_attribute(const struct ldb_message *msg, const char *na
 	if (el == NULL)
 		return 0;
 
-	val.data = discard_const(value);
+	val.data = (uint8_t *)discard_const(value);
 	val.length = strlen(value);
 
 	if (ldb_msg_find_val(el, &val))
