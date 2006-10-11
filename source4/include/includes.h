b6582987fe25ef3d54034f2f27a052664a298f3e
Stefan Metzmacher <metze@samba.org>
2006-10-11 11:52:49 +0000

    r19242: merge from samba3:
    handle NO_CONFIG_H in libreplace
    
    metze
    (This used to be commit 909d736a4bf5a7adfcd64eecf6bb2a92211c6f96)

--- source4/include/includes.h
+++ source4/include/includes.h
@@ -21,17 +21,16 @@
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
-#ifndef NO_CONFIG_H /* for some tests */
 #include "lib/replace/replace.h"
 
 /* make sure we have included the correct config.h */
+#ifndef NO_CONFIG_H /* for some tests */
 #ifndef CONFIG_H_IS_FROM_SAMBA
 #error "make sure you have removed all config.h files from standalone builds!"
 #error "the included config.h isn't from samba!"
 #endif
-
 #endif /* NO_CONFIG_H */
- 
+
 #include "local.h"
 
 #ifdef __GNUC__
