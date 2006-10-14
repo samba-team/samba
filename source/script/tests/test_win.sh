b8abe849d001b7685a0ad4c1481db9d906902582
Brad Henry <brad@samba.org>
2006-10-14 14:50:30 +0000

    r19277: This commit adds support to run the smbtorture RPC-ASYNCBIND RPC-ATSVC RPC-DSSETUP RPC-EPMAPPER RPC-INITSHUTDOWN RPC-LSA-GETUSER RPC-MULTIBIND RPC-ROT RPC-SECRETS RPC-SRVSVC RPC-SVCCTL  RPC-WKSSVC tests against windows, either by 'make wintest' or in the build farm.
    I was surprised when RPC-EVENTLOG and RPC-LSA failed, so i'll investigate today.

--- source/script/tests/test_win.sh
+++ source/script/tests/test_win.sh
@@ -88,17 +88,23 @@ else
 	fi
 fi
 
-test_name="RPC-WINREG / WINDOWS SERVER"
-old_errs=$all_errs
+rpc_tests="RPC-WINREG RPC-ASYNCBIND RPC-ATSVC RPC-DSSETUP RPC-EPMAPPER"
+rpc_tests="$rpc_tests RPC-INITSHUTDOWN RPC-LSA-GETUSER RPC-MULTIBIND RPC-ROT"
+rpc_tests="$rpc_tests RPC-SECRETS RPC-SRVSVC RPC-SVCCTL RPC-WKSSVC"
 
-testit "$test_name" $SMBTORTURE_BIN_PATH \
-	-U $SMBTORTURE_USERNAME%$SMBTORTURE_PASSWORD \
-	-W $SMBTORTURE_WORKGROUP \
-	ncacn_np:$SMBTORTURE_REMOTE_HOST \
-	RPC-WINREG || all_errs=`expr $all_errs + 1`
-if [ $old_errs -lt $all_errs ]; then
-	restore_snapshot "\n$test_name failed."
-fi
+for t in $rpc_tests; do
+	test_name="$t / WINDOWS SERVER"
+	old_errs=$all_errs
+
+	testit "$test_name" $SMBTORTURE_BIN_PATH \
+		-U $SMBTORTURE_USERNAME%$SMBTORTURE_PASSWORD \
+		-W $SMBTORTURE_WORKGROUP \
+		ncacn_np:$SMBTORTURE_REMOTE_HOST \
+		$t || all_errs=`expr $all_errs + 1`
+	if [ $old_errs -lt $all_errs ]; then
+		restore_snapshot "\n$test_name failed."
+	fi
+done
 
 test_name="WINDOWS CLIENT / SAMBA SERVER SHARE"
 old_errs=$all_errs
