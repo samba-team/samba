cf5ac5caccc5eafc5b8483e8c72e2d02cbfb31d6
Love Hörnquist Åstrand <lha@kth.se>
2006-10-17 02:16:29 +0000

    Prefix asn1 primitives with der_.
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@18488 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- kdc/kerberos5.c
+++ kdc/kerberos5.c
@@ -65,8 +65,8 @@ set_salt_padata (METHOD_DATA *md, Salt *salt)
     if (salt) {
 	realloc_method_data(md);
 	md->val[md->len - 1].padata_type = salt->type;
-	copy_octet_string(&salt->salt,
-			  &md->val[md->len - 1].padata_value);
+	der_copy_octet_string(&salt->salt,
+			      &md->val[md->len - 1].padata_value);
     }
 }
 
