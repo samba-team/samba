ea8353794d68255b43cb8b0bdfcb50035baf6452
Love Hörnquist Åstrand <lha@kth.se>
2006-10-17 17:13:03 +0000

    Add flags for krb5_unparse_name_flags
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@18538 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/krb5/krb5.h
+++ lib/krb5/krb5.h
@@ -735,6 +735,12 @@ enum {
     KRB5_PRINCIPAL_PARSE_MUST_REALM = 2,
 };
 
+/* flags for krb5_unparse_name_flags */
+enum {
+    KRB5_PRINCIPAL_UNPARSE_SHORT = 1,
+    KRB5_PRINCIPAL_UNPARSE_NO_REALM = 2
+};
+
 struct credentials; /* this is to keep the compiler happy */
 struct getargs;
 struct sockaddr;
