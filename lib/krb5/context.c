6bf38d8279c234febd9c6fdf559f7d85a417163d
Love Hörnquist Åstrand <lha@kth.se>
2006-10-20 22:26:10 +0000

    krb5_set_send_to_kdc_func takes two arguments.
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@18726 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/krb5/context.c
+++ lib/krb5/context.c
@@ -263,7 +263,7 @@ krb5_free_context(krb5_context context)
 	krb5_closelog(context, context->warn_dest);
     krb5_set_extra_addresses(context, NULL);
     krb5_set_ignore_addresses(context, NULL);
-    krb5_set_send_to_kdc_func(context, NULL);
+    krb5_set_send_to_kdc_func(context, NULL, NULL);
     if (context->mutex != NULL) {
 	HEIMDAL_MUTEX_destroy(context->mutex);
 	free(context->mutex);
