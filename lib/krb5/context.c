d9d472cd6a8a75dafc4faa9d01ad4706a2c614e1
Love Hörnquist Åstrand <lha@kth.se>
2006-10-20 20:10:06 +0000

    (krb5_free_context): free send_to_kdc context
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@18690 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/krb5/context.c
+++ lib/krb5/context.c
@@ -263,6 +263,7 @@ krb5_free_context(krb5_context context)
 	krb5_closelog(context, context->warn_dest);
     krb5_set_extra_addresses(context, NULL);
     krb5_set_ignore_addresses(context, NULL);
+    krb5_set_send_to_kdc_func(context, NULL);
     if (context->mutex != NULL) {
 	HEIMDAL_MUTEX_destroy(context->mutex);
 	free(context->mutex);
