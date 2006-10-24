ac83f285a17cedeb8c40e66f79253b57a56a5ceb
Brad Henry <brad@samba.org>
2006-10-24 01:04:20 +0000

    r19470: Move common windows test functions into their own script wintest_functions.sh, and remove them from the main windows test script. (This used to be commit 587196092846dbab009be9063b1680ad73ae9c14)

--- source4/script/tests/test_win.sh
+++ source4/script/tests/test_win.sh
@@ -9,42 +9,10 @@
 
 . script/tests/test_functions.sh
 
+. script/tests/wintest_functions.sh
+
 # This variable is defined in the per-hosts .fns file.
 . $WINTESTCONF
-
-# Setup the windows environment.
-# This was the best way I could figure out including library files
-# for the moment.
-# I was finding that "cat common.exp wintest_setup.exp | expect -f -"
-# fails to run, but exits with 0 status something like 1% of the time.
-
-setup_share_test()
-{
-	echo -e "\nSetting up windows environment."
-	cat $WINTEST_DIR/common.exp > $TMPDIR/setup.exp
-	cat $WINTEST_DIR/wintest_setup.exp >> $TMPDIR/setup.exp
-	expect $TMPDIR/setup.exp
-	err_rtn=$?
-	rm -f $TMPDIR/setup.exp
-}
-
-# Clean up the windows environment after the test has run or failed.
-remove_share_test()
-{
-	echo -e "\nCleaning up windows environment."
-	cat $WINTEST_DIR/common.exp > $TMPDIR/remove.exp
-	cat $WINTEST_DIR/wintest_remove.exp >> $TMPDIR/remove.exp
-	expect $TMPDIR/remove.exp
-	err_rtn=$?
-	rm -f $TMPDIR/remove.exp
-}
-
-restore_snapshot()
-{
-	echo -e $1
-	vmrun revertToSnapshot "$VM_CFG_PATH"
-	echo "Snapshot restored."
-}
 
 # Index variable to count the total number of tests which fail.
 all_errs=0
