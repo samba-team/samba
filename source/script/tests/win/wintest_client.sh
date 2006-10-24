d3cbbc9bd5eeb5db653f13794747e0fffc872125
Brad Henry <brad@samba.org>
2006-10-24 01:40:15 +0000

    r19474: Move the Samba server / Windows client test to its own script.

---
+++ source/script/tests/win/wintest_client.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+. script/tests/test_functions.sh
+
+. script/tests/win/wintest_functions.sh
+
+# This variable is defined in the per-hosts .fns file.
+. $WINTESTCONF
+
+test_name="WINDOWS CLIENT / SAMBA SERVER SHARE"
+
+cat $WINTEST_DIR/common.exp > $TMPDIR/client_test.exp
+cat $WINTEST_DIR/wintest_client.exp >> $TMPDIR/client_test.exp
+
+testit "$test_name" \
+	expect $TMPDIR/client_test.exp || all_errs=`expr $all_errs + 1`
+
+if [ $all_errs -gt 0 ]; then
+	# Restore snapshot to ensure VM is in a known state.
+	restore_snapshot "\n$test_name failed."
+	echo "Snapshot restored."
+fi
+
+rm -f $TMPDIR/client_test.exp
+
+testok $0 $all_errs
