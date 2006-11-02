e8bf421c018ed829b9dba7c0872693080b77d49d
Jeremy Allison <jra@samba.org>
2006-11-02 09:37:52 +0000

    r19533: Add a suffix to the program name if this is a process with a non-default configuration file name. Jeremy.

--- source/lib/pidfile.c
+++ source/lib/pidfile.c
@@ -78,12 +78,23 @@ pid_t pidfile_pid(const char *name)
 }
 
 /* create a pid file in the pid directory. open it and leave it locked */
-void pidfile_create(const char *name)
+void pidfile_create(const char *program_name)
 {
 	int     fd;
 	char    buf[20];
+	char    *short_configfile;
+	pstring name;
 	pstring pidFile;
 	pid_t pid;
+
+	/* Add a suffix to the program name if this is a process with a
+	 * none default configuration file name. */
+	if (strcmp( CONFIGFILE, dyn_CONFIGFILE) == 0) {
+		strncpy( name, program_name, sizeof( name)-1);
+	} else {
+		short_configfile = strrchr( dyn_CONFIGFILE, '/');
+		slprintf( name, sizeof( name)-1, "%s-%s", program_name, short_configfile+1);
+	}
 
 	slprintf(pidFile, sizeof(pidFile)-1, "%s/%s.pid", lp_piddir(), name);
 
