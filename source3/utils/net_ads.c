61a38bd4b83b7f72b479e84daa5ea89164a92f85
Günther Deschner <gd@samba.org>
2006-11-10 12:42:50 +0000

    r19651: Fix interesting bug with the automatic site coverage in Active Directory:
    When having DC-less sites, AD assigns DCs from other sites to that site
    that does not have it's own DC. The most reliable way for us to identify
    the nearest DC - in that and all other cases - is the closest_dc flag in
    the CLDAP reply.
    
    Guenther
    (This used to be commit ff004f7284cb047e738ba3d3ad6602e8aa84e883)

--- source3/utils/net_ads.c
+++ source3/utils/net_ads.c
@@ -314,7 +314,7 @@ retry:
 
 		tried_closest_dc = True; /* avoid loop */
 
-		if (!closest_dc || !site_matches) {
+		if (!ads_closest_dc(ads)) {
 
 			namecache_delete(ads->server.realm, 0x1C);
 			namecache_delete(ads->server.workgroup, 0x1C);
