273d5f866653db0190c334c086bbfbd88d0ac3cb
Jelmer Vernooij <jelmer@samba.org>
2006-11-11 23:14:58 +0000

    r19671: Fix remaining unresolved symbols in shared library build. Fix starting tests in shared library build. (This used to be commit 3b65a0d6e491a57ed216dc0cd9c31d46e0cb6a35)

--- source4/ntptr/config.mk
+++ source4/ntptr/config.mk
@@ -8,7 +8,7 @@ SUBSYSTEM = ntptr
 OBJ_FILES = \
 		simple_ldb/ntptr_simple_ldb.o
 PUBLIC_DEPENDENCIES = \
-		ldb NDR_SPOOLSS
+		ldb NDR_SPOOLSS DCERPC_COMMON
 # End MODULE ntptr_simple_ldb
 ################################################
 
