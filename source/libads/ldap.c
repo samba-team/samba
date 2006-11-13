0ac17477205afb97a33b6035da865a767a90e292
Jeremy Allison <jra@samba.org>
2006-11-13 19:53:15 +0000

    r19700: Janitor for Volker. Once 3.0.23d is out well have to maintain less branches :-). Jeremy.

--- source/libads/ldap.c
+++ source/libads/ldap.c
@@ -1293,7 +1293,7 @@ char *ads_default_ou_string(ADS_STRUCT *ads, const char *wknguid)
 {
 	ADS_STATUS status;
 	void *res = NULL;
-	char *base, *wkn_dn, *ret = NULL, **wkn_dn_exp, **bind_dn_exp;
+	char *base, *wkn_dn = NULL, *ret = NULL, **wkn_dn_exp = NULL, **bind_dn_exp = NULL;
 	const char *attrs[] = {"distinguishedName", NULL};
 	int new_ln, wkn_ln, bind_ln, i;
 
