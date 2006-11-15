317383fd3dc97cb561156504fb46974b3fc41a61
Love Hörnquist Åstrand <lha@kth.se>
2006-11-15 12:16:51 +0000

    Talk about how far the build have progressed.
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@19055 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- packages/mac/mac.sh
+++ packages/mac/mac.sh
@@ -3,7 +3,6 @@
 
 dbase=`dirname $0`
 base=`cd $dbase && pwd`
-echo $base
 config=${base}/../../configure
 
 destdir=`pwd`/destdir
@@ -19,18 +18,24 @@ cd ${builddir} || exit 1
 
 version=`sh ${config} --help 2>/dev/null | head -1 | sed 's/.*Heimdal \([^ ]*\).*/\1/'`
 
+echo "Building Mac universal binary package for Heimdal ${version}"
+echo "Configure"
 env \
   CFLAGS="-arch i386 -arch ppc" \
   LDFLAGS="-arch i386 -arch ppc" \
   ${config} > log || exit 1
+echo "Build"
 make all > /dev/null || exit 1
+echo "Run regression suite"
 make check > /dev/null || exit 1
+echo "Install"
 make install DESTDIR=${destdir} > /dev/null || exit 1 
 
 replace="s,[@]VERSION[@],${version},g"
 
 sed -e $replace < ${base}/Info.plist.in > ./Info.plist
 
+echo "Build package"
 /Developer/Tools/packagemaker \
     -build \
     -ds \
@@ -40,8 +45,11 @@ sed -e $replace < ${base}/Info.plist.in > ./Info.plist
     -i ./Info.plist || exit 1
 
 cd ..
+echo "Build disk image"
 /usr/bin/hdiutil create -volname "Heimdal-${version}" -srcfolder ${imgdir} "heimdal-${version}.dmg" || exit 1
 
+echo "Clean"
 rm -rf ${destdir} ${builddir} ${imgdir} || exit 1
 
+echo "Done!"
 exit 0
