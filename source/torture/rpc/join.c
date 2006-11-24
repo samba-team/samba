857f02d85beddf9f9d689a8d39d0e539756fba5f
Stefan Metzmacher <metze@samba.org>
2006-11-24 15:56:18 +0000

    r19876: test that we can do a session setup and tree connect using the workstation and dc credentials
    I noticed w2k3 has gets NT_STATUS_LOGON_FAILURE after a it has joined a samba4 domain
    
    metze

--- source/torture/rpc/join.c
+++ source/torture/rpc/join.c
@@ -1,5 +1,6 @@
 #include "includes.h"
 #include "libnet/libnet.h"
+#include "libcli/libcli.h"
 
 #include "auth/credentials/credentials.h"
 #include "torture/rpc/rpc.h"
@@ -8,9 +9,12 @@
 
 
 BOOL torture_rpc_join(struct torture_context *torture)
-{  
+{
+	NTSTATUS status;
 	struct test_join *tj;
 	struct cli_credentials *machine_account;
+	struct smbcli_state *cli;
+	const char *host = lp_parm_string(-1, "torture", "host");
 
 	/* Join domain as a member server. */
 	tj = torture_join_domain(TORTURE_NETBIOS_NAME,
@@ -18,10 +22,21 @@ BOOL torture_rpc_join(struct torture_context *torture)
 				 &machine_account);
 
 	if (!tj) {
-		DEBUG(0, ("%s failed to join domain\n",
+		DEBUG(0, ("%s failed to join domain as workstation\n",
 			  TORTURE_NETBIOS_NAME));
 		return False;
 	}
+
+	status = smbcli_full_connection(tj, &cli, host,
+					"IPC$", NULL,
+					machine_account,
+					NULL);
+	if (!NT_STATUS_IS_OK(status)) {
+		DEBUG(0, ("%s failed to connect to IPC$ with workstation credentials\n",
+			  TORTURE_NETBIOS_NAME));
+		return False;	
+	}
+	smbcli_tdis(cli);
         
 	/* Leave domain. */                          
 	torture_leave_domain(tj);
@@ -31,10 +46,22 @@ BOOL torture_rpc_join(struct torture_context *torture)
 				 ACB_SVRTRUST,
 				 &machine_account);
 	if (!tj) {
-		DEBUG(0, ("%s failed to join domain\n",
+		DEBUG(0, ("%s failed to join domain as domain controller\n",
 			  TORTURE_NETBIOS_NAME));
 		return False;
 	}
+
+	status = smbcli_full_connection(tj, &cli, host,
+					"IPC$", NULL,
+					machine_account,
+					NULL);
+	if (!NT_STATUS_IS_OK(status)) {
+		DEBUG(0, ("%s failed to connect to IPC$ with workstation credentials\n",
+			  TORTURE_NETBIOS_NAME));
+		return False;	
+	}
+
+	smbcli_tdis(cli);
 
 	/* Leave domain. */
 	torture_leave_domain(tj);
