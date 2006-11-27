a3e1682ba3196d7c8aec8f2c72f9e12a3707db40
Gerald Carter <jerry@samba.org>
2006-11-27 19:06:58 +0000

    r19921: Add license and copyright info

--- source/include/rpc_perfcount_defs.h
+++ source/include/rpc_perfcount_defs.h
@@ -1,5 +1,26 @@
 #ifndef _RPC_PERFCOUNT_DEFS_H
 #define _RPC_PERFCOUNT_DEFS_H
+/* 
+ *  Unix SMB/CIFS implementation.
+ *  Virtual Windows Registry Layer
+ *
+ *  Copyright (C) Marcin Krzysztof Porwit    2005,
+ *  Copyright (C) Gerald (Jerry) Carter      2005.
+ *  
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
 
 /*
  * The following #defines match what is in winperf.h. 
