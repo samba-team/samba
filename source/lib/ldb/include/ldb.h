c32c009ae42bd70ca6501d3d643cb564eca3f5ff
Volker Lendecke <vlendec@samba.org>
2006-12-05 16:20:39 +0000

    r20046: Add ldb_search_exp_fmt and port comment to 4

--- source/lib/ldb/include/ldb.h
+++ source/lib/ldb/include/ldb.h
@@ -989,9 +989,16 @@ int ldb_search(struct ldb_context *ldb,
 	       const char *expression,
 	       const char * const *attrs, struct ldb_result **res);
 
-int ldb_search_exp_fmt(struct ldb_context *ldb, TALLOC_CTX *mem_ctx, struct ldb_result **result,
-                        struct ldb_dn *base, enum ldb_scope scope, const char * const *attrs,
-                        const char *exp_fmt, ...);
+/*
+ * a useful search function where you can easily define the expression and
+ * that takes a memory context where results are allocated
+*/
+
+int ldb_search_exp_fmt(struct ldb_context *ldb, TALLOC_CTX *mem_ctx,
+		       struct ldb_result **result, struct ldb_dn *base,
+		       enum ldb_scope scope, const char * const *attrs,
+		       const char *exp_fmt, ...);
+
 /*
   like ldb_search() but takes a parse tree
 */
