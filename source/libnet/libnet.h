f66a93ec0b5c0250dd5c49adde163b2aaaaddfbd
Stefan Metzmacher <metze@samba.org>
2006-12-05 11:49:55 +0000

    r20040: uncomplete implementation start for libnet_UnbecomeDC()
    metze

--- source/libnet/libnet.h
+++ source/libnet/libnet.h
@@ -59,6 +59,7 @@ struct libnet_context {
 #include "libnet/libnet_join.h"
 #include "libnet/libnet_site.h"
 #include "libnet/libnet_become_dc.h"
+#include "libnet/libnet_unbecome_dc.h"
 #include "libnet/libnet_vampire.h"
 #include "libnet/libnet_user.h"
 #include "libnet/libnet_share.h"
