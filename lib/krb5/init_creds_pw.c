7bf56b0e3f4fa79deeb1579ab46bf9bfb41f60ec
Love Hörnquist Åstrand <lha@kth.se>
2006-12-14 10:49:56 +0000

    (free_paid): free the krb5_data structure too. Bug report from Stefan Metzmacher.
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@19331 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/krb5/init_creds_pw.c
+++ lib/krb5/init_creds_pw.c
@@ -656,7 +656,7 @@ free_paid(krb5_context context, struct pa_info_data *ppaid)
 {
     krb5_free_salt(context, ppaid->salt);
     if (ppaid->s2kparams)
-	krb5_data_free(ppaid->s2kparams);
+	krb5_free_data(context, ppaid->s2kparams);
 }
 
 
