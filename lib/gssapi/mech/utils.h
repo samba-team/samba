02620cbb4e0f923a3ef4d02f96643137859f5a3c
Love Hörnquist Åstrand <lha@kth.se>
2006-12-18 13:01:40 +0000

    add _gss_free_oid, reverse of _gss_copy_oid
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@19398 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/gssapi/mech/utils.h
+++ lib/gssapi/mech/utils.h
@@ -27,6 +27,7 @@
  *	$Id$
  */
 
+OM_uint32 _gss_free_oid(OM_uint32 *, gss_OID);
 OM_uint32 _gss_copy_oid(OM_uint32 *, const gss_OID, gss_OID);
 OM_uint32 _gss_copy_buffer(OM_uint32 *minor_status,
     const gss_buffer_t from_buf, gss_buffer_t to_buf);
