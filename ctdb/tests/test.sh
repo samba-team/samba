1f44ccb569a38a5e5165af1824f2a2077f8d3c57
Andrew Tridgell <tridge@samba.org>
2006-12-18 16:05:22 +1100

    added a 4 node test (This used to be ctdb commit 982468d0770684ec18cf95e3f254250debf4fd4c)

--- ctdb/tests/test.sh
+++ ctdb/tests/test.sh
@@ -2,8 +2,17 @@
 
 killall -q ctdb_test
 
+echo "Trying 2 nodes"
 bin/ctdb_test --nlist nodes.txt --listen 127.0.0.1:9001 &
 bin/ctdb_test --nlist nodes.txt --listen 127.0.0.2:9001 &
 
+sleep 3
+killall ctdb_test
+
+echo "Trying 4 nodes"
+bin/ctdb_test --nlist tests/4nodes.txt --listen 127.0.0.1:9001 &
+bin/ctdb_test --nlist tests/4nodes.txt --listen 127.0.0.2:9001 &
+bin/ctdb_test --nlist tests/4nodes.txt --listen 127.0.0.3:9001 &
+bin/ctdb_test --nlist tests/4nodes.txt --listen 127.0.0.4:9001 &
 sleep 3
 killall ctdb_test
