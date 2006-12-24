aa88d4cdaf6d10df92854291cbe83ab40370b6ec
Derrell Lipman <derrell@samba.org>
2006-12-24 21:42:02 +0000

    r20346: This is the beginnings of a framework for a complete SWAT.  It will provide for easily adding modules, and using as much common code as possible. Although all modules are loaded to the browser as part of the application, modules are not actually initialized, their finite state machines generated, or their graphical user interfaces built until they are selected to be viewed for the first time.
    I'm checking this in at this early stage because I promised Brad some
    check-ins a week ago, and the flu has kept me unable to do anything for the
    past 10 days.  Now that I'm back in working order, I hope to be building on
    this regularly over the next few days.
    
    Derrell

---
+++ swat/apps/swat/source/class/swat/main/Main.js
@@ -0,0 +1,96 @@
+/*
+#module(swat)
+#require(swat.module.AbstractModule)
+#require(swat.module.stats.Statistics)
+*/
+
+/**
+ * Swat main menu
+ */
+qx.OO.defineClass("swat.main.Main", qx.component.AbstractApplication,
+function()
+{
+  qx.component.AbstractApplication.call(this);
+});
+
+
+/**
+ * The list of supported modules
+ */
+var moduleSystemStatus =
+{
+  "canvas" : null,
+  "fsm"    : null,
+  "class"  : swat.module.stats.Statistics
+};
+
+/*
+var moduleLdbView =
+{
+  "canvas" : null,
+  "fsm"    : null,
+  "class"  : swat.module.ldbview.LdbView
+};
+*/
+
+qx.Class.modules =
+{
+  list :
+  {
+    "System Status" : moduleSystemStatus
+  }
+};
+
+
+/*
+---------------------------------------------------------------------------
+  METHODS
+---------------------------------------------------------------------------
+*/
+
+qx.Proto.initialize = function()
+{
+  var modules = swat.main.Main.modules;
+  var o = new qx.ui.basic.Label("hello world");
+
+  // For each module...
+  for (moduleName in modules.list)
+  {
+    // ... add the module's name to the module object, ...
+    modules.list[moduleName].name = moduleName;
+
+    // ... and call the module's buildInitialFsm() function
+    var module = modules.list[moduleName]["class"].getInstance();
+    module.buildInitialFsm(modules.list[moduleName]);
+  }
+};
+
+
+qx.Proto.main = function()
+{
+  var modules = swat.main.Main.modules;
+
+  // Initialize the gui for the main menu
+  swat.main.Gui.buildGui(modules);
+
+  // Similarly, now that we have a canvas for each module, ...
+  for (moduleName in modules.list)
+  {
+    // ... call the module's buildInitialGui() function
+    var module = modules.list[moduleName]["class"].getInstance();
+    module.buildInitialGui(modules.list[moduleName]);
+  }
+};
+
+
+qx.Proto.finalize = function()
+{
+  var modules = swat.main.Main.modules;
+
+  // Call each module's finalization function
+  for (moduleName in modules.list)
+  {
+    var module = modules.list[moduleName]["class"].getInstance();
+    module.finalize(modules.list[moduleName]);
+  }
+}
