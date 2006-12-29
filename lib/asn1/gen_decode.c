5342643f04cc66fcee93e1a2f873f2a8f7d6bdee
Love Hörnquist Åstrand <lha@kth.se>
2006-12-29 17:30:32 +0000

    update (c)
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@19572 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/asn1/gen_decode.c
+++ lib/asn1/gen_decode.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997 - 2005 Kungliga Tekniska Högskolan
+ * Copyright (c) 1997 - 2006 Kungliga Tekniska Högskolan
  * (Royal Institute of Technology, Stockholm, Sweden). 
  * All rights reserved. 
  *
