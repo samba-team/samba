2e7c59c24470766e37309c7a8bfa4c7b81c57614
Derrell Lipman <derrell@samba.org>
2007-01-03 19:57:40 +0000

    r20515: Continued work on the Web Application Framework.  Until we get all of the functionality of the old scripts incorporated into the new framework, the old scripts need to still be available.  I've reverted to having the old scripts be the default pages, and added an option to access the preview of the new SWAT. (This used to be commit b43620d4b8eff815f4a6dc02522a8dfc9fdcaef4)

--- webapps/qooxdoo-0.6.3-sdk/frontend/framework/tool/massreplace.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-cd `dirname $0`/..
-
-for file in `find source -name "*.js" -o -name "*.css"  -o -name "*.html"`; do
-  echo ">>> Patching: $file"
-  sed -i s:"$1":"$2":g $file
-done
