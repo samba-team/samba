fd7bd254e13953bbf408b49e7009b35dfe7e7d92
Günther Deschner <gd@samba.org>
2007-01-03 16:05:00 +0000

    r20512: Fix typo.
    Guenther
    (This used to be commit 36bd5cb21bdaf35c7dae47f8b8e93822b3f6188c)

--- source3/include/rpc_ds.h
+++ source3/include/rpc_ds.h
@@ -49,7 +49,7 @@
 #define DSROLE_BDC			4
 #define DSROLE_PDC			5
 
-/* Settings for the domainFunctionality attribteu in the rootDSE */
+/* Settings for the domainFunctionality attribute in the rootDSE */
 
 #define DS_DOMAIN_FUNCTION_2000		0
 #define DS_DOMAIN_FUCNTION_2003_MIXED	1
