52fb3170b2720115b69f9b6e2b962ee405054c13
James Peach <jpeach@samba.org>
2007-01-07 00:44:17 +0000

    r20590: Heimdal uses warn(), so we should prototype it here too.

--- source/heimdal_build/err.h
+++ source/heimdal_build/err.h
@@ -1,8 +1,14 @@
+#ifdef HAVE_ERR_H
+#include <err.h>
+#else
+
 #ifndef	_ERR_H
 #define	_ERR_H	1
 
 void err(int eval, const char *format, ...);
 void errx(int eval, const char *format, ...);
 void warnx(const char *format, ...);
+void warn(const char *format, ...);
 
 #endif	/* err.h */
+#endif /* HAVE_ERR_H */
