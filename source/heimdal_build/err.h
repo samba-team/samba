1e8c4847b829217beec62ba46c592da3fae8a73e
James Peach <jpeach@samba.org>
2007-01-07 04:43:04 +0000

    r20592: Use the Heimdal copy of err.h only if the build host doesn't have it.

--- source/heimdal_build/err.h
+++
@@ -1,9 +0,0 @@
-#ifndef	_ERR_H
-#define	_ERR_H	1
-
-void err(int eval, const char *format, ...);
-void errx(int eval, const char *format, ...);
-void warnx(const char *format, ...);
-void warn(const char *format, ...);
-
-#endif	/* err.h */
