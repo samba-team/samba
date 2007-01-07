9162f7fb9bdd1280fc2fd1d71d600ac088ec2f6d
James Peach <jpeach@samba.org>
2007-01-07 04:53:49 +0000

    r20594: Match popt.h declaration.

--- source/lib/popt/popt.c
+++ source/lib/popt/popt.c
@@ -1155,7 +1155,7 @@ const char * poptBadOption(poptContext con, int flags)
     /*@=nullderef@*/
 }
 
-const char *const poptStrerror(const int error)
+const char *poptStrerror(const int error)
 {
     switch (error) {
       case POPT_ERROR_NOARG:
