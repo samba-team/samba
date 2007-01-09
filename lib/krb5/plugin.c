d4b0d11e0a429484dbdcc3cf95fb2e32ce276685
Love Hörnquist Åstrand <lha@kth.se>
2007-01-09 17:46:01 +0000

    update (c)
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@19789 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/krb5/plugin.c
+++ lib/krb5/plugin.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006 Kungliga Tekniska Högskolan
+ * Copyright (c) 2006 - 2007 Kungliga Tekniska Högskolan
  * (Royal Institute of Technology, Stockholm, Sweden). 
  * All rights reserved. 
  *
