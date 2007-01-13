342aad413862d181c76cf5bbfe555bfeb6ce8e93
Love Hörnquist Åstrand <lha@kth.se>
2007-01-13 11:18:33 +0000

    add hx509_env
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@19884 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/hx509/hx509.h
+++ lib/hx509/hx509.h
@@ -50,6 +50,7 @@ typedef struct hx509_request_data *hx509_request;
 typedef struct hx509_error_data *hx509_error;
 typedef struct hx509_peer_info *hx509_peer_info;
 typedef struct hx509_ca_tbs *hx509_ca_tbs;
+typedef struct hx509_env *hx509_env;
 
 typedef void (*hx509_vprint_func)(void *, const char *, va_list);
 
