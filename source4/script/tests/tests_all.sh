f60f9de6a833237767d1988ea67bc5d3368a4354
Jelmer Vernooij <jelmer@samba.org>
2007-01-14 01:39:34 +0000

    r20747: Move cifsdd to blackbox section (This used to be commit 96ac1c62eef29e8c8c1df9719b6785134efe525c)

--- source4/script/tests/tests_all.sh
+++ source4/script/tests/tests_all.sh
@@ -10,7 +10,6 @@
  $SRCDIR/script/tests/test_echo.sh $SERVER $USERNAME $PASSWORD $DOMAIN || totalfailed=`expr $totalfailed + $?`
  $SRCDIR/script/tests/test_posix.sh //$SERVER/tmp $USERNAME $PASSWORD "" || totalfailed=`expr $totalfailed + $?`
  $SRCDIR/script/tests/test_local.sh || totalfailed=`expr $totalfailed + $?`
- $SRCDIR/script/tests/test_pidl.sh || totalfailed=`expr $totalfailed + $?`
  $SRCDIR/script/tests/test_blackbox.sh $SERVER $USERNAME $PASSWORD $DOMAIN $PREFIX || totalfailed=`expr $totalfailed + $?`
- $SRCDIR/script/tests/test_cifsdd.sh $SERVER $USERNAME $PASSWORD $DOMAIN || totalfailed=`expr $totalfailed + $?`
+ $SRCDIR/script/tests/test_pidl.sh || totalfailed=`expr $totalfailed + $?`
  $SRCDIR/script/tests/test_simple.sh //$SERVER/simple $USERNAME $PASSWORD "" || totalfailed=`expr $totalfailed + $?`
