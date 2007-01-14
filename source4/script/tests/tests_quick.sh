5ae1175460b6ac419fc30f0858e115321e689b33
Jelmer Vernooij <jelmer@samba.org>
2007-01-14 03:18:04 +0000

    r20754: Simplify code for calculating total number of failed tests. (This used to be commit f288ca5f6805d04a2da60a26006ca293d9fa7cc0)

--- source4/script/tests/tests_quick.sh
+++ source4/script/tests/tests_quick.sh
@@ -4,9 +4,9 @@ export TORTURE_OPTIONS
 TORTURE_QUICK="yes"
 export TORTURE_QUICK
 
-$SRCDIR/script/tests/test_ejs.sh $DOMAIN $USERNAME $PASSWORD || totalfailed=`expr $totalfailed + $?`
-$SRCDIR/script/tests/test_ldap.sh $SERVER $USERNAME $PASSWORD || totalfailed=`expr $totalfailed + $?`
-$SRCDIR/script/tests/test_nbt.sh $SERVER || totalfailed=`expr $totalfailed + $?`
-$SRCDIR/script/tests/test_quick.sh //$SERVER/cifs $USERNAME $PASSWORD "" || totalfailed=`expr $totalfailed + $?`
-$SRCDIR/script/tests/test_rpc_quick.sh $SERVER $USERNAME $PASSWORD $DOMAIN || totalfailed=`expr $totalfailed + $?`
+$SRCDIR/script/tests/test_ejs.sh $DOMAIN $USERNAME $PASSWORD
+$SRCDIR/script/tests/test_ldap.sh $SERVER $USERNAME $PASSWORD
+$SRCDIR/script/tests/test_nbt.sh $SERVER
+$SRCDIR/script/tests/test_quick.sh //$SERVER/cifs $USERNAME $PASSWORD ""
+$SRCDIR/script/tests/test_rpc_quick.sh $SERVER $USERNAME $PASSWORD $DOMAIN
 #$SRCDIR/script/tests/test_cifsposix.sh //$SERVER/cifsposixtestshare $USERNAME $PASSWORD "" || totalfailed=`expr $totalfailed + $?`
