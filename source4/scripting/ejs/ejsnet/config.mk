64f27395a10fbf9ad95921ff0a8481b7afb0d02d
Stefan Metzmacher <metze@samba.org>
2007-01-15 07:46:36 +0000

    r20792: fix the build, sorry...
    metze
    (This used to be commit e2cdd882b5ab12396244a13e0e58d8ee52f234ab)

--- source4/scripting/ejs/ejsnet/config.mk
+++ source4/scripting/ejs/ejsnet/config.mk
@@ -1,8 +1,9 @@
 [MODULE::smbcalls_net]
-PRIVATE_PROTO_HEADER = proto.h
-INIT_FUNCTION = smb_setup_ejs_net
 OBJ_FILES = \
 		net_ctx.o \
 		net_user.o \
 		mpr_user.o
+SUBSYSTEM = smbcalls
+INIT_FUNCTION = smb_setup_ejs_net
+PRIVATE_PROTO_HEADER = proto.h
 PRIVATE_DEPENDENCIES = LIBSAMBA-NET LIBCLI_SMB CREDENTIALS
