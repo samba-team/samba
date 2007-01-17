5b0bb46664e1b71e09dadf9b916e2c1b22ee1975
Love Hörnquist Åstrand <lha@kth.se>
2007-01-17 16:21:05 +0000

    Check in variable.
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@19964 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/gssapi/mech/gss_verify_mic.c
+++ lib/gssapi/mech/gss_verify_mic.c
@@ -39,6 +39,11 @@ gss_verify_mic(OM_uint32 *minor_status,
 	struct _gss_context *ctx = (struct _gss_context *) context_handle;
 	gssapi_mech_interface m = ctx->gc_mech;
 
+	if (ctx == NULL) {
+	    *minor_status = 0;
+	    return GSS_S_NO_CONTEXT;
+	}
+
 	return (m->gm_verify_mic(minor_status, ctx->gc_ctx,
 		    message_buffer, token_buffer, qop_state));
 }
