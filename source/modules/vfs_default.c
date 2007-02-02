f21cbf218e384c549f4cd87ac0700be3741a33cb
Volker Lendecke <vlendec@samba.org>
2007-02-02 17:48:21 +0000

    r21127: Add a mitigating comment ;-))

--- source/modules/vfs_default.c
+++ source/modules/vfs_default.c
@@ -841,6 +841,14 @@ static NTSTATUS vfswrap_notify_watch(vfs_handle_struct *vfs_handle,
 						      struct notify_event *ev),
 				     void *private_data, void *handle)
 {
+	/*
+	 * So far inotify is the only supported default notify mechanism. If
+	 * another platform like the the BSD's or a proprietary Unix comes
+	 * along and wants another default, we can play the same trick we
+	 * played with Posix ACLs.
+	 *
+	 * Until that is the case, hard-code inotify here.
+	 */
 #ifdef HAVE_INOTIFY
 	if (lp_kernel_change_notify(ctx->conn->params)) {
 		return inotify_watch(ctx, e, callback, private_data, handle);
