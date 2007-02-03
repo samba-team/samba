ea7f403c7c819cc7f21d70324c9be4210de893f2
Love Hörnquist Åstrand <lha@kth.se>
2007-02-03 09:55:07 +0000

    x
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@20159 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/hx509/print.c
+++ lib/hx509/print.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004 - 2006 Kungliga Tekniska Högskolan
+ * Copyright (c) 2004 - 2007 Kungliga Tekniska Högskolan
  * (Royal Institute of Technology, Stockholm, Sweden). 
  * All rights reserved. 
  *
