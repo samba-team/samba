8538af1107a6e894d4941708b77e79fac587e35d
Andrew Tridgell <tridge@samba.org>
2007-02-06 05:26:25 +0000

    r21174: many thanks to Paul Wayper for pointing out that C99 requires a matching va_end() for each va_copy(). This doesn't matter for most architectures, but there could be some obscure ones where it does matter.
    some of this should be ported to Samba3
    (This used to be commit 21eb316473486cb6b73bb3ff9c5f3a44ecd57e4a)

--- source4/lib/util/util_file.c
+++ source4/lib/util/util_file.c
@@ -364,8 +364,8 @@ _PUBLIC_ int vfdprintf(int fd, const char *format, va_list ap) _PRINTF_ATTRIBUTE
 	va_list ap2;
 
 	va_copy(ap2, ap);
-
 	len = vasprintf(&p, format, ap2);
+	va_end(ap2);
 	if (len <= 0) return len;
 	ret = write(fd, p, len);
 	SAFE_FREE(p);
