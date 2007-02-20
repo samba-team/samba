351cfc7d9e961043597218874d84c374027fb177
Stefan Metzmacher <metze@samba.org>
2007-02-20 14:39:13 +0000

    r21463: add a LDAP-UPTODATEVECTOR test, it shows that the "replUpToDateVector" isn't updated by originating changes, and that there's no entry for the local dsa invocation_id
    metze

--- source/torture/ldap/common.c
+++ source/torture/ldap/common.c
@@ -101,17 +101,14 @@ NTSTATUS torture_ldap_close(struct ldap_connection *conn)
 
 NTSTATUS torture_ldap_init(void)
 {
-	struct torture_suite *suite = torture_suite_create(
-										talloc_autofree_context(),
-										"LDAP");
-	torture_suite_add_simple_test(suite, "BENCH-CLDAP", 
-								  torture_bench_cldap);
+	struct torture_suite *suite = torture_suite_create(talloc_autofree_context(), "LDAP");
+	torture_suite_add_simple_test(suite, "BENCH-CLDAP", torture_bench_cldap);
 	torture_suite_add_simple_test(suite, "BASIC", torture_ldap_basic);
 	torture_suite_add_simple_test(suite, "CLDAP", torture_cldap);
 	torture_suite_add_simple_test(suite, "SCHEMA", torture_ldap_schema);
+	torture_suite_add_simple_test(suite, "UPTODATEVECTOR", torture_ldap_uptodatevector);
 
-	suite->description = talloc_strdup(
-							suite, "LDAP and CLDAP tests");
+	suite->description = talloc_strdup(suite, "LDAP and CLDAP tests");
 
 	torture_register_suite(suite);
 
