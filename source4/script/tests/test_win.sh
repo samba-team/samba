48b0dabdbaf7538e0896dd137eec5cc97b50a59d
Brad Henry <brad@samba.org>
2007-02-21 22:50:55 +0000

    r21494: Fix 'make wintest' to support command-line changes to 'vm-get-ip.pl' and the revertsnapshot() shell function. Restructure existing tests to work with the recent build farm changes. Added windows DC testing to 'make wintest'. Some improvements and generalization to the way errors are handled. (This used to be commit de9687404e802d7e7629c9d03f496d3c5bd30397)

--- source4/script/tests/test_win.sh
+++ source4/script/tests/test_win.sh
@@ -7,30 +7,36 @@
 # Copyright Brad Henry <brad@samba.org> 2006
 # Released under the GNU GPL v2 or later.
 
-# Index variable to count the total number of tests which fail.
-all_errs=0
+. script/tests/test_functions.sh
 
-export SMBTORTURE_REMOTE_HOST=`perl -I$WINTEST_DIR $WINTEST_DIR/vm_get_ip.pl`
+export SMBTORTURE_REMOTE_HOST=`perl -I$WINTEST_DIR $WINTEST_DIR/vm_get_ip.pl VM_CFG_PATH`
 if [ -z $SMBTORTURE_REMOTE_HOST ]; then
 	# Restore snapshot to ensure VM is in a known state, then exit.
-	restore_snapshot "Test failed to get the IP address of the windows host."
+	restore_snapshot "Test failed to get the IP address of the windows host." "$VM_CFG_PATH"
 	exit 1
 fi
 
-$WINTEST_DIR/wintest_base.sh $SMBTORTURE_REMOTE_HOST $SMBTORTURE_USERNAME \
-	$SMBTORTURE_PASSWORD $SMBTORTURE_WORKGROUP \
-	|| all_errs=`expr $all_errs + $?`
+name="BASE against Windows 2003"
+testit "$name" $WINTEST_DIR/wintest_base.sh $SMBTORTURE_REMOTE_HOST \
+	$SMBTORTURE_USERNAME $SMBTORTURE_PASSWORD $SMBTORTURE_WORKGROUP
 
-$WINTEST_DIR/wintest_raw.sh $SMBTORTURE_REMOTE_HOST $SMBTORTURE_USERNAME \
-	$SMBTORTURE_PASSWORD $SMBTORTURE_WORKGROUP \
-	|| all_errs=`expr $all_errs + $?`
+name="RAW against Windows 2003"
+testit "$name" $WINTEST_DIR/wintest_raw.sh $SMBTORTURE_REMOTE_HOST \
+	$SMBTORTURE_USERNAME $SMBTORTURE_PASSWORD $SMBTORTURE_WORKGROUP
 
-$WINTEST_DIR/wintest_rpc.sh $SMBTORTURE_REMOTE_HOST $SMBTORTURE_USERNAME \
-	$SMBTORTURE_PASSWORD $SMBTORTURE_WORKGROUP \
-	|| all_errs=`expr $all_errs + $?`
+name="RPC against Windows 2003"
+testit "$name" $WINTEST_DIR/wintest_rpc.sh $SMBTORTURE_REMOTE_HOST \
+	$SMBTORTURE_USERNAME $SMBTORTURE_PASSWORD $SMBTORTURE_WORKGROUP
 
-$WINTEST_DIR/wintest_net.sh $SMBTORTURE_REMOTE_HOST $SMBTORTURE_USERNAME \
-	$SMBTORTURE_PASSWORD $SMBTORTURE_WORKGROUP \
-	|| all_errs=`expr $all_errs + $?`
+name="NET against Windows 2003"
+testit "$name" $WINTEST_DIR/wintest_net.sh $SMBTORTURE_REMOTE_HOST \
+	$SMBTORTURE_USERNAME $SMBTORTURE_PASSWORD $SMBTORTURE_WORKGROUP
 
-$WINTEST_DIR/wintest_client.sh || all_errs=`expr $all_errs + $?`
+name="Windows 2003 against smbd"
+testit "$name" $WINTEST_DIR/wintest_client.sh $SMBTORTURE_REMOTE_HOST
+
+dc_tests="RPC-DRSUAPI RPC-SPOOLSS ncacn_np ncacn_ip_tcp"
+for name in $dc_tests; do
+	testit "$name against Windows 2003 DC" $WINTEST_DIR/wintest_2k3_dc.sh \
+		"$name"
+done
