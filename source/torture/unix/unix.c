7e2a6b1742f23642c53c6de20d4ee7b686832b5e
James Peach <jpeach@samba.org>
2007-03-02 05:58:22 +0000

    r21648: Start a UNIX extensions test set. Add a test for the SMBWhoami query.

---
+++ source/torture/unix/unix.c
@@ -0,0 +1,39 @@
+/*
+   UNIX Extensions test registration.
+
+   Copyright (C) 2006 James Peach
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include "includes.h"
+#include "torture/torture.h"
+#include "torture/unix/proto.h"
+
+NTSTATUS torture_unix_init(void)
+{
+        struct torture_suite *suite =
+                torture_suite_create(talloc_autofree_context(), "UNIX");
+
+        suite->description =
+                talloc_strdup(suite, "CIFS UNIX extensions tests");
+
+	torture_suite_add_simple_test(suite,
+                        "WHOAMI", apple_torture_unix_whoami);
+
+        return (torture_register_suite(suite)) ? NT_STATUS_OK
+                                        : NT_STATUS_UNSUCCESSFUL;
+
+}
