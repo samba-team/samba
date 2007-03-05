0474005d074163f95e03580e5b1da7f7efe4664a
Jelmer Vernooij <jelmer@samba.org>
2007-03-05 00:11:46 +0000

    r21691: Add testsuite for lib/charset (This used to be commit a4184893959bb25541704938ee621e5c575b817d)

---
+++ source4/lib/charset/testsuite.c
@@ -0,0 +1,236 @@
+/* 
+   Unix SMB/CIFS implementation.
+   test suite for the charcnv functions
+
+   Copyright (C) Jelmer Vernooij 2007
+   
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include "includes.h"
+#include "torture/torture.h"
+
+static bool test_toupper_w(struct torture_context *tctx)
+{
+	torture_assert_int_equal(tctx, toupper_w('c'), 'C', "c");
+	torture_assert_int_equal(tctx, toupper_w('Z'), 'Z', "z");
+	torture_assert_int_equal(tctx, toupper_w(0xFFFF4565), 0xFFFF4565, "0xFFFF4565");
+	return true;
+}
+
+static bool test_tolower_w(struct torture_context *tctx)
+{
+	torture_assert_int_equal(tctx, tolower_w('C'), 'c', "c");
+	torture_assert_int_equal(tctx, tolower_w('z'), 'z', "z");
+	torture_assert_int_equal(tctx, tolower_w(0xFFFF4565), 0xFFFF4565, "0xFFFF4565");
+	return true;
+}
+
+static bool test_codepoint_cmpi(struct torture_context *tctx)
+{
+	torture_assert_int_equal(tctx, codepoint_cmpi('a', 'a'), 0, "same char");
+	torture_assert_int_equal(tctx, codepoint_cmpi('A', 'a'), 0, "upcase version");
+	torture_assert_int_equal(tctx, codepoint_cmpi('b', 'a'), 1, "right diff");
+	torture_assert_int_equal(tctx, codepoint_cmpi('a', 'b'), -1, "right diff");
+	return true;
+}
+
+static bool test_strcasecmp_m(struct torture_context *tctx)
+{
+	torture_assert(tctx, strcasecmp_m("foo", "bar") != 0, "different strings");
+	torture_assert(tctx, strcasecmp_m("foo", "foo") == 0, "same case strings");
+	torture_assert(tctx, strcasecmp_m("foo", "Foo") == 0, "different case strings");
+	torture_assert(tctx, strcasecmp_m(NULL, "Foo") != 0, "one NULL");
+	torture_assert(tctx, strcasecmp_m("foo", NULL) != 0, "other NULL");
+	torture_assert(tctx, strcasecmp_m(NULL, NULL) == 0, "both NULL");
+	return true;
+}
+
+
+static bool test_strequal_w(struct torture_context *tctx)
+{
+	torture_assert(tctx, !strequal_w("foo", "bar"), "different strings");
+	torture_assert(tctx, strequal_w("foo", "foo"), "same case strings");
+	torture_assert(tctx, strequal_w("foo", "Foo"), "different case strings");
+	torture_assert(tctx, !strequal_w(NULL, "Foo"), "one NULL");
+	torture_assert(tctx, !strequal_w("foo", NULL), "other NULL");
+	torture_assert(tctx, strequal_w(NULL, NULL), "both NULL");
+	return true;
+}
+
+static bool test_strcsequal_w(struct torture_context *tctx)
+{
+	torture_assert(tctx, !strcsequal_w("foo", "bar"), "different strings");
+	torture_assert(tctx, strcsequal_w("foo", "foo"), "same case strings");
+	torture_assert(tctx, !strcsequal_w("foo", "Foo"), "different case strings");
+	torture_assert(tctx, !strcsequal_w(NULL, "Foo"), "one NULL");
+	torture_assert(tctx, !strcsequal_w("foo", NULL), "other NULL");
+	torture_assert(tctx, strcsequal_w(NULL, NULL), "both NULL");
+	return true;
+}
+
+static bool test_string_replace_w(struct torture_context *tctx)
+{
+	char data[] = "bla";
+	string_replace_w(data, 'b', 'c');
+	torture_assert_str_equal(tctx, data, "cla", "first char replaced");
+	memcpy(data, "bab", 4);
+	string_replace_w(data, 'b', 'c');
+	torture_assert_str_equal(tctx, data, "cac", "other chars replaced");
+	memcpy(data, "bba", 4);
+	string_replace_w(data, 'b', 'c');
+	torture_assert_str_equal(tctx, data, "cca", "other chars replaced");
+	memcpy(data, "blala", 6);
+	string_replace_w(data, 'o', 'c');
+	torture_assert_str_equal(tctx, data, "blala", "no chars replaced");
+	string_replace_w(NULL, 'b', 'c');
+	return true;
+}
+
+static bool test_strncasecmp_m(struct torture_context *tctx)
+{
+	torture_assert(tctx, strncasecmp_m("foo", "bar", 3) != 0, "different strings");
+	torture_assert(tctx, strncasecmp_m("foo", "foo", 3) == 0, "same case strings");
+	torture_assert(tctx, strncasecmp_m("foo", "Foo", 3) == 0, "different case strings");
+	torture_assert(tctx, strncasecmp_m("fool", "Foo", 3) == 0, "different case strings");
+	torture_assert(tctx, strncasecmp_m("fool", "Fool", 40) == 0, "over size");
+	torture_assert(tctx, strncasecmp_m("BLA", "Fool", 0) == 0, "empty");
+	torture_assert(tctx, strncasecmp_m(NULL, "Foo", 3) != 0, "one NULL");
+	torture_assert(tctx, strncasecmp_m("foo", NULL, 3) != 0, "other NULL");
+	torture_assert(tctx, strncasecmp_m(NULL, NULL, 3) == 0, "both NULL");
+	return true;
+}
+
+
+
+static bool test_next_token(struct torture_context *tctx)
+{
+	const char *teststr = "foo bar bla";
+	char buf[20];
+	torture_assert(tctx, next_token(&teststr, buf, " ", 20), "finding token works");
+	torture_assert_str_equal(tctx, buf, "foo", "token matches");
+	torture_assert_str_equal(tctx, teststr, "bar bla", "ptr modified correctly");
+
+	torture_assert(tctx, next_token(&teststr, buf, " ", 20), "finding token works");
+	torture_assert_str_equal(tctx, buf, "bar", "token matches");
+	torture_assert_str_equal(tctx, teststr, "bla", "ptr modified correctly");
+
+	torture_assert(tctx, next_token(&teststr, buf, " ", 20), "finding token works");
+	torture_assert_str_equal(tctx, buf, "bla", "token matches");
+	torture_assert_str_equal(tctx, teststr, "", "ptr modified correctly");
+
+	torture_assert(tctx, !next_token(&teststr, buf, " ", 20), "finding token doesn't work");
+	return true;
+}
+
+static bool test_next_token_seps(struct torture_context *tctx)
+{
+	const char *teststr = ",foo bla";
+	char buf[20];
+	torture_assert(tctx, next_token(&teststr, buf, ",", 20), "finding token works");
+	torture_assert_str_equal(tctx, buf, "foo bla", "token matches");
+	torture_assert_str_equal(tctx, teststr, "", "ptr modified correctly");
+
+	torture_assert(tctx, !next_token(&teststr, buf, ",", 20), "finding token doesn't work");
+	return true;
+}
+
+static bool test_next_token_quotes(struct torture_context *tctx)
+{
+	const char *teststr = "\"foo bar\" bla";
+	char buf[20];
+	torture_assert(tctx, next_token(&teststr, buf, " ", 20), "finding token works");
+	torture_assert_str_equal(tctx, buf, "foo bar", "token matches");
+	torture_assert_str_equal(tctx, teststr, "bla", "ptr modified correctly");
+
+	torture_assert(tctx, next_token(&teststr, buf, " ", 20), "finding token works");
+	torture_assert_str_equal(tctx, buf, "bla", "token matches");
+	torture_assert_str_equal(tctx, teststr, "", "ptr modified correctly");
+
+	torture_assert(tctx, !next_token(&teststr, buf, " ", 20), "finding token doesn't work");
+	return true;
+}
+
+static bool test_next_token_quote_wrong(struct torture_context *tctx)
+{
+	const char *teststr = "\"foo bar bla";
+	char buf[20];
+	torture_assert(tctx, next_token(&teststr, buf, " ", 20), "finding token works");
+	torture_assert_str_equal(tctx, buf, "foo bar bla", "token matches");
+	torture_assert_str_equal(tctx, teststr, "", "ptr modified correctly");
+
+	torture_assert(tctx, !next_token(&teststr, buf, " ", 20), "finding token doesn't work");
+	return true;
+}
+
+static bool test_strlen_m(struct torture_context *tctx)
+{
+	torture_assert_int_equal(tctx, strlen_m("foo"), 3, "simple len");
+	torture_assert_int_equal(tctx, strlen_m("foo\x83l"), 6, "extended len");
+	torture_assert_int_equal(tctx, strlen_m(NULL), 0, "NULL");
+	return true;
+}
+
+static bool test_strlen_m_term(struct torture_context *tctx)
+{
+	torture_assert_int_equal(tctx, strlen_m_term("foo"), 4, "simple len");
+	torture_assert_int_equal(tctx, strlen_m_term("foo\x83l"), 7, "extended len");
+	torture_assert_int_equal(tctx, strlen_m(NULL), 0, "NULL");
+	return true;
+}
+
+static bool test_strhaslower(struct torture_context *tctx)
+{
+	torture_assert(tctx, strhaslower("a"), "one low char");
+	torture_assert(tctx, strhaslower("aB"), "one low, one up char");
+	torture_assert(tctx, !strhaslower("B"), "one up char");
+	torture_assert(tctx, !strhaslower(""), "empty string");
+	torture_assert(tctx, !strhaslower("3"), "one digit");
+	return true;
+}
+
+static bool test_strhasupper(struct torture_context *tctx)
+{
+	torture_assert(tctx, strhasupper("B"), "one up char");
+	torture_assert(tctx, strhasupper("aB"), "one low, one up char");
+	torture_assert(tctx, !strhasupper("a"), "one low char");
+	torture_assert(tctx, !strhasupper(""), "empty string");
+	torture_assert(tctx, !strhasupper("3"), "one digit");
+	return true;
+}
+
+struct torture_suite *torture_local_charset(TALLOC_CTX *mem_ctx)
+{
+	struct torture_suite *suite = torture_suite_create(mem_ctx, "CHARSET");
+
+	torture_suite_add_simple_test(suite, "toupper_w", test_toupper_w);
+	torture_suite_add_simple_test(suite, "tolower_w", test_tolower_w);
+	torture_suite_add_simple_test(suite, "codepoint_cmpi", test_codepoint_cmpi);
+	torture_suite_add_simple_test(suite, "strcasecmp_m", test_strcasecmp_m);
+	torture_suite_add_simple_test(suite, "strequal_w", test_strequal_w);
+	torture_suite_add_simple_test(suite, "strcsequal_w", test_strcsequal_w);
+	torture_suite_add_simple_test(suite, "string_replace_w", test_string_replace_w);
+	torture_suite_add_simple_test(suite, "strncasecmp_m", test_strncasecmp_m);
+	torture_suite_add_simple_test(suite, "next_token", test_next_token);
+	torture_suite_add_simple_test(suite, "next_token_quotes", test_next_token_quotes);
+	torture_suite_add_simple_test(suite, "next_token_seps", test_next_token_seps);
+	torture_suite_add_simple_test(suite, "next_token_quote_wrong", test_next_token_quote_wrong);
+	torture_suite_add_simple_test(suite, "strlen_m", test_strlen_m);
+	torture_suite_add_simple_test(suite, "strlen_m_term", test_strlen_m_term);
+	torture_suite_add_simple_test(suite, "strhaslower", test_strhaslower);
+	torture_suite_add_simple_test(suite, "strhasupper", test_strhasupper);
+
+	return suite;
+}
