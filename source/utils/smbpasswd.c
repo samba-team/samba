424d2187215ea13534b591ee60c52b17a936dbd7
Andrew Bartlett <abartlet@samba.org>
2007-04-04 06:03:54 +0000

    r22071: Make the error message for incorrect use of '-c' show the parameter as used.
    This means that smbpasswd -c user (which in htpasswd creates a new file)
    and smbpasswd -c /not/my/smb.conf should give errors an admin will
    think to chase down.
    
    Andrew Bartlett

--- source/utils/smbpasswd.c
+++ source/utils/smbpasswd.c
@@ -190,7 +190,7 @@ static int process_options(int argc, char **argv, int local_flags)
 
 	if (!lp_load(configfile,True,False,False,True)) {
 		fprintf(stderr, "Can't load %s - run testparm to debug it\n", 
-			dyn_CONFIGFILE);
+			configfile);
 		exit(1);
 	}
 
