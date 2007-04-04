150bb2238ea91ead3bdde0a34ff801b79bc83ec3
Jelmer Vernooij <jelmer@samba.org>
2007-04-04 14:24:44 +0000

    r22077: Import only necessary test variables, more refactoring.

--- source/script/tests/tests_quick.sh
+++ source/script/tests/tests_quick.sh
@@ -7,4 +7,3 @@ $SRCDIR/script/tests/test_ldap.sh $SERVER $USERNAME $PASSWORD
 $SRCDIR/script/tests/test_nbt.sh $SERVER $USERNAME $PASSWORD
 $SRCDIR/script/tests/test_quick.sh //$SERVER/cifs $USERNAME $PASSWORD ""
 $SRCDIR/script/tests/test_rpc_quick.sh $SERVER $USERNAME $PASSWORD $DOMAIN
-#$SRCDIR/script/tests/test_cifsposix.sh //$SERVER/cifsposixtestshare $USERNAME $PASSWORD "" || totalfailed=`expr $totalfailed + $?`
