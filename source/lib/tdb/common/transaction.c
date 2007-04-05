3d281cbf6886f3089e51f5ebd8740bc296672ec7
Andrew Tridgell <tridge@samba.org>
2007-04-05 03:22:08 +0000

    r22081: transaction_brlock() should be static
    thanks to Ted T'so for pointing this out

--- source/lib/tdb/common/transaction.c
+++ source/lib/tdb/common/transaction.c
@@ -358,8 +358,8 @@ static int transaction_expand_file(struct tdb_context *tdb, tdb_off_t size,
 /*
   brlock during a transaction - ignore them
 */
-int transaction_brlock(struct tdb_context *tdb, tdb_off_t offset, 
-		       int rw_type, int lck_type, int probe, size_t len)
+static int transaction_brlock(struct tdb_context *tdb, tdb_off_t offset, 
+			      int rw_type, int lck_type, int probe, size_t len)
 {
 	return 0;
 }
