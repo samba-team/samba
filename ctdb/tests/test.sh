1f3f0ae56d2edae4541d7da8e3e0cd1607c62e70
Ronnie sahlberg <ronniesahlberg@gmail.com>
2007-04-10 12:23:10 +1000

    restore the test script that was updated by mistake in the previous checkin
    (This used to be ctdb commit 5e2e6a5ce439762876f2aa054fdd0d4bb54c403c)

--- ctdb/tests/test.sh
+++ ctdb/tests/test.sh
@@ -2,14 +2,6 @@
 
 killall -q ctdb_test
 
-echo "Trying 2 nodes in daemon mode"
-bin/ctdb_test --nlist tests/nodes.txt --listen 127.0.0.1:9001 --daemon &
-bin/ctdb_test --nlist tests/nodes.txt --listen 127.0.0.2:9001 --daemon &
-
-sleep 30000
-killall ctdb_test
-exit
-
 
 echo "Trying 2 nodes"
 bin/ctdb_test --nlist tests/nodes.txt --listen 127.0.0.1:9001 &
@@ -25,6 +17,13 @@ bin/ctdb_test --nlist tests/4nodes.txt --listen 127.0.0.3:9001 &
 bin/ctdb_test --nlist tests/4nodes.txt --listen 127.0.0.4:9001 &
 sleep 3
 
+killall ctdb_test
+
+echo "Trying 2 nodes in daemon mode"
+bin/ctdb_test --nlist tests/nodes.txt --listen 127.0.0.1:9001 --daemon &
+bin/ctdb_test --nlist tests/nodes.txt --listen 127.0.0.2:9001 --daemon &
+
+sleep 3
 killall ctdb_test
 
 echo "Trying 4 nodes in daemon mode"
