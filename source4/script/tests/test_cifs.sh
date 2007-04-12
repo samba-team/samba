f5bcf91e220e61b86d232974d616c20e40aef320
Andrew Bartlett <abartlet@samba.org>
2007-04-12 00:34:59 +0000

    r22180: Re-add testing of the CIFS backend.
    Andrew Bartlett
    (This used to be commit d76afe147ca87f3678b7eb4930096eb6aaa39e6e)

---
+++ source4/script/tests/test_cifs.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# this runs the file serving tests that are expected to pass with the
+# current posix ntvfs backend, via the ntvfs cifs proxy
+
+ADDARGS="$*"
+
+incdir=`dirname $0`
+. $incdir/test_functions.sh
+
+raw=`bin/smbtorture --list | grep "^RAW-" | xargs`
+base=`bin/smbtorture --list | grep "^BASE-" | xargs`
+tests="$base $raw $smb2"
+
+for t in $tests; do
+    if [ ! -z "$start" -a "$start" != $t ]; then
+	continue;
+    fi
+    start=""
+    plantest "ntvfs/cifs $t" dc $VALGRIND bin/smbtorture $TORTURE_OPTIONS $ADDARGS //\$SERVER/cifs -U"\$USERNAME"%"\$PASSWORD" $t
+done
