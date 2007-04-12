6ec0692dfbbe5203d50d902af2294adc93e71a02
Stefan Metzmacher <metze@samba.org>
2007-04-12 11:35:00 +0000

    r22193: fix compiler warning and formating
    metze

--- source/torture/rpc/wkssvc.c
+++ source/torture/rpc/wkssvc.c
@@ -25,7 +25,7 @@
 #include "torture/rpc/rpc.h"
 
 static bool test_NetWkstaGetInfo(struct torture_context *tctx, 
-								 struct dcerpc_pipe *p)
+				 struct dcerpc_pipe *p)
 {
 	NTSTATUS status;
 	struct wkssvc_NetWkstaGetInfo r;
@@ -38,7 +38,7 @@ static bool test_NetWkstaGetInfo(struct torture_context *tctx,
 
 	for (i=0;i<ARRAY_SIZE(levels);i++) {
 		r.in.level = levels[i];
-		torture_comment(tctx, talloc_asprintf(tctx, "testing NetWkstaGetInfo level %u\n", r.in.level));
+		torture_comment(tctx, "testing NetWkstaGetInfo level %u\n", r.in.level);
 		status = dcerpc_wkssvc_NetWkstaGetInfo(p, tctx, &r);
 		torture_assert_ntstatus_ok(tctx, status, 
 			talloc_asprintf(tctx, "NetWkstaGetInfo level %u failed", r.in.level));
@@ -51,7 +51,7 @@ static bool test_NetWkstaGetInfo(struct torture_context *tctx,
 
 
 static bool test_NetWkstaTransportEnum(struct torture_context *tctx,
-									   struct dcerpc_pipe *p)
+				       struct dcerpc_pipe *p)
 {
 	NTSTATUS status;
 	struct wkssvc_NetWkstaTransportEnum r;
@@ -72,8 +72,7 @@ static bool test_NetWkstaTransportEnum(struct torture_context *tctx,
 
 	status = dcerpc_wkssvc_NetWkstaTransportEnum(p, tctx, &r);
 	torture_assert_ntstatus_ok(tctx, status, "NetWkstaTransportEnum failed");
-	torture_assert_werr_ok(tctx, r.out.result, 
-						   talloc_asprintf(tctx, 
+	torture_assert_werr_ok(tctx, r.out.result, talloc_asprintf(tctx, 
 		"NetWkstaTransportEnum level %u failed", r.in.level));
 
 	return true;
@@ -87,11 +86,11 @@ struct torture_suite *torture_rpc_wkssvc(void)
 	struct torture_tcase *tcase;
 
 	suite = torture_suite_create(talloc_autofree_context(), "WKSSVC");
-	tcase = torture_suite_add_rpc_iface_tcase(suite, "wkssvc", 
-											  &dcerpc_table_wkssvc);
+	tcase = torture_suite_add_rpc_iface_tcase(suite, "wkssvc",
+						  &dcerpc_table_wkssvc);
 
 	torture_rpc_tcase_add_test(tcase, "NetWkstaGetInfo", test_NetWkstaGetInfo);
 	torture_rpc_tcase_add_test(tcase, "NetWkstaTransportEnum", 
-							   test_NetWkstaTransportEnum);
+				   test_NetWkstaTransportEnum);
 	return suite;
 }
