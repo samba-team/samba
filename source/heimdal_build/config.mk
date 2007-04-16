a4598cadf93c8624a8ba0aa03d39c29c0c472e6b
Andrew Bartlett <abartlet@samba.org>
2007-04-16 05:48:11 +0000

    r22238: Because these utilities compile in socket_wrapper.ho, they may need -lsocket on some systems.
    Andrew Bartlett

--- source/heimdal_build/config.mk
+++ source/heimdal_build/config.mk
@@ -494,7 +494,8 @@ OBJ_FILES = \
 	../heimdal/lib/vers/print_version.ho \
 	../lib/socket_wrapper/socket_wrapper.ho \
 	replace.ho
-PRIVATE_DEPENDENCIES = HEIMDAL_ASN1_COMPILE_LEX HEIMDAL_ROKEN_GETPROGNAME_H
+PRIVATE_DEPENDENCIES = HEIMDAL_ASN1_COMPILE_LEX HEIMDAL_ROKEN_GETPROGNAME_H EXT_SOCKET EXT_NSL
+
 # End BINARY asn1_compile
 #######################
 
@@ -520,7 +521,7 @@ OBJ_FILES = ../heimdal/lib/vers/print_version.ho \
 	../heimdal/lib/roken/setprogname.ho \
 	../lib/socket_wrapper/socket_wrapper.ho \
 	replace.ho
-PRIVATE_DEPENDENCIES = HEIMDAL_COM_ERR_COMPILE_LEX HEIMDAL_ROKEN_GETPROGNAME_H
+PRIVATE_DEPENDENCIES = HEIMDAL_COM_ERR_COMPILE_LEX HEIMDAL_ROKEN_GETPROGNAME_H EXT_SOCKET EXT_NSL
 # End BINARY compile_et
 #######################
 
