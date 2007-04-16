9cc94483ded55db77b0bc6c7c8df03ca268b8bda
Andrew Tridgell <tridge@samba.org>
2007-04-16 14:13:10 +1000

    merged from samba4 (This used to be ctdb commit 541814e6ee3dd7f8f1b3fb615213ff5979b6e22f)

---
+++ ctdb/config.mk
@@ -0,0 +1,29 @@
+##################
+[SUBSYSTEM::brlock_ctdb]
+OBJ_FILES = brlock_ctdb.o
+
+##################
+[SUBSYSTEM::opendb_ctdb]
+OBJ_FILES = opendb_ctdb.o
+
+##################
+[SUBSYSTEM::ctdb_tcp]
+OBJ_FILES = \
+		tcp/tcp_init.o \
+		tcp/tcp_io.o \
+		tcp/tcp_connect.o
+
+##################
+[SUBSYSTEM::ctdb]
+OBJ_FILES = \
+		ctdb_cluster.o \
+		common/ctdb.o \
+		common/ctdb_call.o \
+		common/ctdb_message.o \
+		common/ctdb_ltdb.o \
+		common/ctdb_util.o \
+		common/ctdb_io.o \
+		common/ctdb_client.o \
+		common/ctdb_daemon.o
+PUBLIC_DEPENDENCIES = LIBTDB LIBTALLOC
+PRIVATE_DEPENDENCIES = ctdb_tcp
