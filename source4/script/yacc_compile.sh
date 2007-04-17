8934c3f68e0ba949edd38673f10b9943059e4938
Stefan Metzmacher <metze@samba.org>
2007-04-17 13:48:12 +0000

    r22307: when no lex or yacc is found: - just touch $DEST if it's already there and newer than $SRC - touch $DEST if it's already there but older than $SRC and print warnings   to stderr - otherwise return an error
    when lex or yacc is found:
    - just touch $DEST if it's already there and newer than $SRC
    - otherwise regenerate $DEST from $SRC using lex or yacc
    
    I don't like that every 'make' tries to regenerate because the timestamp
    of $DEST is never updated...
    
    metze
    (This used to be commit 10ed5c39692ff4a6b61ec9c6d046b2ee2202fc02)

--- source4/script/yacc_compile.sh
+++ source4/script/yacc_compile.sh
@@ -8,18 +8,30 @@ dir=`dirname $SRC`
 file=`basename $SRC`
 base=`basename $SRC .y`
 if [ -z "$YACC" ]; then
-	echo "yacc not found"
-	exit;
+	# if $DEST is more recent than $SRC, we can just touch
+	# otherwise we touch but print out warnings
+	if [ -r $DEST ]; then
+		if [ x`find $SRC -newer $DEST -print` = x$SRC ]; then
+			echo "warning: yacc not found - cannot generate $SRC => $DEST" >&2
+			echo "warning: yacc not found - only updating the timestamp of $DEST" >&2
+		fi
+		touch $DEST;
+		exit;
+	fi
+	echo "error: yacc not found - cannot generate $SRC => $DEST" >&2
+	exit 1;
 fi
+# if $DEST is more recent than $SRC, we can just touch
 if [ -r $DEST ]; then
 	if [ x`find $SRC -newer $DEST -print` != x$SRC ]; then
+		touch $DEST;
 		exit;
 	fi
 fi
 TOP=`pwd`
 if cd $dir && $YACC -d $file; then
 	if [ -r y.tab.h -a -r y.tab.c ];then
-		echo "move files"
+		#echo "info: move files"
 		sed -e "/^#/!b" -e "/^#/ s|y\.tab\.h|$SRC|" -e "/^#/ s|\"$base.y|\"$SRC|"  y.tab.h > $base.h
 		sed -e "/^#/ s|y\.tab\.c|$SRC|" -e "/^#/ s|\"$base.y|\"$SRC|" y.tab.c > $base.c
 		rm -f y.tab.c y.tab.h
