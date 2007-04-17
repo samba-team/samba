4047b20c368f473081eeb951dff119a6c060c9f7
Stefan Metzmacher <metze@samba.org>
2007-04-17 08:03:01 +0000

    r22297: move ZERO_*, ARRAY_SIZE and PTR_DIFF macros into libreplace
    metze
    (This used to be commit b69c950858d837d7bd734d418129ade69106a00d)

--- source4/lib/util/util.h
+++ source4/lib/util/util.h
@@ -41,31 +41,6 @@ extern const char *panic_action;
 #include "util/byteorder.h"
 #include "lib/util/util_proto.h"
 
-/** 
- * zero a structure 
- */
-#define ZERO_STRUCT(x) memset((char *)&(x), 0, sizeof(x))
-
-/** 
- * zero a structure given a pointer to the structure 
- */
-#define ZERO_STRUCTP(x) do { if ((x) != NULL) memset((char *)(x), 0, sizeof(*(x))); } while(0)
-
-/** 
- * zero a structure given a pointer to the structure - no zero check 
- */
-#define ZERO_STRUCTPN(x) memset((char *)(x), 0, sizeof(*(x)))
-
-/** 
- * pointer difference macro 
- */
-#define PTR_DIFF(p1,p2) ((ptrdiff_t)(((const char *)(p1)) - (const char *)(p2)))
-
-/**
- * work out how many elements there are in a static array 
- */
-#define ARRAY_SIZE(a) (sizeof(a)/sizeof(a[0]))
-
 /**
  * assert macros 
  */
