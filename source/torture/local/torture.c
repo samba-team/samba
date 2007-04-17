e009865f53974fc9c5c777a279ff4b830013c271
Jelmer Vernooij <jelmer@samba.org>
2007-04-17 00:30:01 +0000

    r22286: Import branch that converted the Samba4 test target code to perl and makes the code a bit clearer.

--- source/torture/local/torture.c
+++ source/torture/local/torture.c
@@ -40,44 +40,12 @@ static bool test_tempdir(struct torture_context *tctx)
 	return true;
 }
 
-static bool test_setup_server(struct torture_context *tctx)
-{
-	pid_t pid;
-	TALLOC_CTX *mem_ctx = tctx;
-
-	torture_assert_ntstatus_ok(tctx, torture_setup_server(mem_ctx, 
-									"setupserver-success",
-									"./script/tests/mktestdc.sh",
-									"./bin/smbd", &pid),
-							   "starting smbd failed");
-
-	torture_assert(tctx, pid > 0, "Pid invalid");
-
-	torture_comment(tctx, "Created smbd with pid %d\n", pid);
-
-	kill(pid, SIGINT);
-
-	waitpid(pid, NULL, 0);
-
-	torture_assert_ntstatus_equal(tctx, torture_setup_server(mem_ctx, 
-									"setupserver-fail",
-									"./invalid-script",
-									"./bin/smbd", &pid), 
-								  NT_STATUS_UNSUCCESSFUL,
-							   "invalid script specified");
-
-	torture_assert(tctx, pid == -1, "Pid not -1 after failure");
-	return true;
-}
-
-
 struct torture_suite *torture_local_torture(TALLOC_CTX *mem_ctx)
 {
 	struct torture_suite *suite = torture_suite_create(mem_ctx, 
 													   "TORTURE");
 
 	torture_suite_add_simple_test(suite, "tempdir", test_tempdir);
-	torture_suite_add_simple_test(suite, "setup server", test_setup_server);
 
 	return suite;
 }
