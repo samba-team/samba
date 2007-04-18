1e1176a10505dc24789af392143740c14ceda854
Michael Adam <obnox@samba.org>
2007-04-18 11:12:31 +0000

    r22330: For my own convenience (changes discussed some weeks ago with Jerry, who basically didn't care - now I can commit them myself;-):
    - Add a call to mkversion.sh to ensure sane version number even
      from a plain checkout.
    - Add a "nodocs" switch to allow for calling make-tarball.sh without
      the need of having samba-docs and using-samba around.

--- make-tarball.sh
+++ make-tarball.sh
@@ -4,35 +4,43 @@
 ## You either need to include the using_samba cvs module in the
 ## parent directory or tell the script where to find it 
 ##
-## Usgae:  ./make-tarball.sh
+## Usgae:  ./make-tarball.sh [nodocs]
+
+NODOCS=0
+if [ x"$1" = x"nodocs" ] ; then
+	NODOCS=1
+	echo Not including docs.
+fi
 
 DOCSDIR=../samba-docs/
 USING_SAMBA=../using_samba/
 SRCDIR=`pwd`
 
-if [ ! -d $USING_SAMBA ]; then
-
-	echo Cannot find "Using Samba" directory \(assuming $USING_SAMBA\).  
-	echo Please set the USING_SAMBA variable in this script to the correct
-	echo location.  The html files are available in the using_samba CVS 
-	echo module on cvs.samba.org.  See http://cvs/samba.org/ for details 
-	echo about anonymous CVS access.  Exiting now....
-
-	exit 1
-
+if [ $NODOCS -eq 0 ]; then
+	if [ ! -d $USING_SAMBA ]; then
+	
+		echo Cannot find "Using Samba" directory \(assuming $USING_SAMBA\).  
+		echo Please set the USING_SAMBA variable in this script to the correct
+		echo location.  The html files are available in the using_samba CVS 
+		echo module on cvs.samba.org.  See http://cvs.samba.org/ for details 
+		echo about anonymous CVS access.  Exiting now....
+	
+		exit 1
+	
+	fi
+	
+	if [ ! -d $DOCSDIR ]; then
+	
+		echo Cannot find samba-docs \(assuming $DOCSDIR\).
+		echo Please set the DOCSDIR variable in this script 
+		echo to the correct path.
+	
+		exit 1
+	
+	fi
 fi
 
-if [ ! -d $DOCSDIR ]; then
-
-	echo Cannot find samba-docs \(assuming $DOCSDIR\).
-	echo Please set the DOCSDIR variable in this script 
-	echo to the correct path.
-
-	exit 1
-
-fi
-
-
+( cd source ; sh script/mkversion.sh )
 VERSION=`grep SAMBA_VERSION_OFFICIAL_STRING source/include/version.h | cut -d\" -f2 | sed 's/ /_/g'`
 TARBALLDIR=/tmp/samba-$VERSION
 
@@ -44,8 +52,10 @@ echo Creating the tarball source directory in $TARBALLDIR
 mkdir $TARBALLDIR
 rsync -aC ./ $TARBALLDIR
 /bin/rm -rf $TARBALLDIR/docs/*
-rsync -aC $DOCSDIR/ $TARBALLDIR/docs/ 
-rsync -aC $USING_SAMBA $TARBALLDIR/docs/htmldocs/
+if [ $NODOCS -eq 0 ]; then
+	rsync -aC $DOCSDIR/ $TARBALLDIR/docs/ 
+	rsync -aC $USING_SAMBA $TARBALLDIR/docs/htmldocs/
+fi
 
 echo Creating packaging scripts...
 ( cd $TARBALLDIR/packaging; sh bin/update-pkginfo $VERSION 1 )
