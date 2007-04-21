968050c6ba1f7f9728f24ca36a689f553c347457
Stefan Metzmacher <metze@samba.org>
2007-04-21 23:02:49 +0000

    r22438: get rid of printf format warnings in heimdal code
    metze

--- source/heimdal_build/roken.h
+++ source/heimdal_build/roken.h
@@ -73,6 +73,12 @@
 #define HAVE_INNETGR
 #endif
 
+#if (__GNUC__ >= 3) && (__GNUC_MINOR__ >= 1 )
+#ifndef HAVE___ATTRIBUTE__
+#define HAVE___ATTRIBUTE__
+#endif
+#endif
+
 /*
  * we don't want that roken.h.in includes socket_wrapper
  * we include socket_wrapper via "system/network.h"
