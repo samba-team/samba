2541e0d59048f9d0f522aaebca4c4c6f5019fec5
Volker Lendecke <vlendec@samba.org>
2007-04-22 14:51:07 +0000

    r22458: Fix a comment (This used to be commit f5ccec42287cfdf128bbfcf562a47a852bd0e3fe)

--- source3/smbd/process.c
+++ source3/smbd/process.c
@@ -121,7 +121,7 @@ void remove_deferred_open_smb_message(uint16 mid)
 
 	for (pml = deferred_open_queue; pml; pml = pml->next) {
 		if (mid == SVAL(pml->buf.data,smb_mid)) {
-			DEBUG(10,("remove_sharing_violation_open_smb_message: "
+			DEBUG(10,("remove_deferred_open_smb_message: "
 				  "deleting mid %u len %u\n",
 				  (unsigned int)mid,
 				  (unsigned int)pml->buf.length ));
