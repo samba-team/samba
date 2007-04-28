2597152126251da78b605f1d20c8b00e0e51396b
Stefan Metzmacher <metze@samba.org>
2007-04-28 07:31:19 +0000

    r22544: use the same syntax as in all other files maybe that fixes make test on all sun hosts...
    metze

--- source/script/tests/test_nbt.sh
+++ source/script/tests/test_nbt.sh
@@ -12,7 +12,7 @@ export PATH
 plantest "nmblookup -U \$SERVER \$SERVER" dc bin/nmblookup $TORTURE_OPTIONS -U \$SERVER \$SERVER
 plantest "nmblookup \$SERVER" dc bin/nmblookup $TORTURE_OPTIONS \$SERVER
 
-NBT_TESTS=`bin/smbtorture --list | grep ^NBT`
+NBT_TESTS=`bin/smbtorture --list | grep "^NBT-" | xargs`
 
 for f in $NBT_TESTS; do
     plantest "$f" dc bin/smbtorture $TORTURE_OPTIONS //\$SERVER/_none_ $f -U\$USERNAME%\$PASSWORD 
