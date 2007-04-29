9902f8d6afe9da7ec6b383d6ccfc3449cb5896a6
Stefan Metzmacher <metze@samba.org>
2007-04-29 13:31:13 +0000

    r22570: run the NBT tests against DC and MEMBER
    metze

--- source/script/tests/test_nbt.sh
+++ source/script/tests/test_nbt.sh
@@ -9,11 +9,22 @@ SCRIPTDIR=../testprogs/ejs
 PATH=bin:$PATH
 export PATH
 
-plantest "nmblookup -U \$SERVER \$SERVER" dc bin/nmblookup $TORTURE_OPTIONS -U \$SERVER \$SERVER
-plantest "nmblookup \$SERVER" dc bin/nmblookup $TORTURE_OPTIONS \$SERVER
+TEST_NBT_ENVNAME=$1
+if test x"$TEST_NBT_ENVNAME" = x"";then
+	TEST_NBT_ENVNAME="dc"
+fi
+
+plantest "nmblookup -U \$SERVER_IP \$SERVER" $TEST_NBT_ENVNAME bin/nmblookup $TORTURE_OPTIONS -U \$SERVER_IP \$SERVER
+plantest "nmblookup -U \$SERVER_IP \$NETBIOSNAME" $TEST_NBT_ENVNAME bin/nmblookup $TORTURE_OPTIONS -U \$SERVER_IP \$NETBIOSNAME
+plantest "nmblookup -U \$SERVER_IP \$NETBIOSALIAS" $TEST_NBT_ENVNAME bin/nmblookup $TORTURE_OPTIONS -U \$SERVER_IP \$NETBIOSALIAS
+plantest "nmblookup \$SERVER" $TEST_NBT_ENVNAME bin/nmblookup $TORTURE_OPTIONS \$SERVER
+plantest "nmblookup \$NETBIOSNAME" $TEST_NBT_ENVNAME bin/nmblookup $TORTURE_OPTIONS \$NETBIOSNAME
+plantest "nmblookup \$NETBIOSALIAS" $TEST_NBT_ENVNAME bin/nmblookup $TORTURE_OPTIONS \$NETBIOSALIAS
 
 NBT_TESTS=`bin/smbtorture --list | grep "^NBT-" | xargs`
 
-for f in $NBT_TESTS; do
-    plantest "$f" dc bin/smbtorture $TORTURE_OPTIONS //\$SERVER/_none_ $f -U\$USERNAME%\$PASSWORD 
-done
+if test x"$TEST_NBT_ENVNAME" = x"dc";then
+    for f in $NBT_TESTS; do
+        plantest "$f:$TEST_NBT_ENVNAME" $TEST_NBT_ENVNAME bin/smbtorture $TORTURE_OPTIONS //\$SERVER/_none_ $f -U\$USERNAME%\$PASSWORD 
+    done
+fi
