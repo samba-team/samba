8336e85aad03deb6fffe89c0a770fb05c2319c8e
Stefan Metzmacher <metze@samba.org>
2007-04-29 13:07:36 +0000

    r22569: use the $SERVER_IP for the LDAP-* tests because I'll later change $SERVER from localhost to localdc.
    we should find out why this is needed and add explicit name resolution
    in related code...
    
    metze

--- source/script/tests/test_ldap.sh
+++ source/script/tests/test_ldap.sh
@@ -6,18 +6,18 @@ incdir=`dirname $0`
 
 p=ldap
 for options in "" "--option=socket:testnonblock=true" "-U\$USERNAME%\$PASSWORD --option=socket:testnonblock=true" "-U\$USERNAME%\$PASSWORD"; do
-    plantest "TESTING PROTOCOL $p with options $options" dc ../testprogs/blackbox/test_ldb.sh $p \$SERVER $options
+    plantest "TESTING PROTOCOL $p with options $options" dc ../testprogs/blackbox/test_ldb.sh $p \$SERVER_IP $options
 done
 # see if we support ldaps
 if grep ENABLE_GNUTLS.1 include/config.h > /dev/null; then
     p=ldaps
     for options in "" "-U\$USERNAME%\$PASSWORD"; do
-	plantest "TESTING PROTOCOL $p with options $options" dc ../testprogs/blackbox/test_ldb.sh $p \$SERVER $options
+	plantest "TESTING PROTOCOL $p with options $options" dc ../testprogs/blackbox/test_ldb.sh $p \$SERVER_IP $options
     done
 fi
 for t in LDAP-CLDAP LDAP-BASIC LDAP-SCHEMA LDAP-UPTODATENESS
 do
-	plantest "$t" dc bin/smbtorture $TORTURE_OPTIONS "-U\$USERNAME%\$PASSWORD" //\$SERVER/_none_ $t
+	plantest "$t" dc bin/smbtorture $TORTURE_OPTIONS "-U\$USERNAME%\$PASSWORD" //\$SERVER_IP/_none_ $t
 done
 
 # only do the ldb tests when not in quick mode - they are quite slow, and ldb
