037a3e0fc22730c08efb65583a3837419d7aaa0f
Stefan Metzmacher <metze@samba.org>
2007-05-01 04:12:54 +0000

    r22626: test member server with local and domain credentials
    metze
    (This used to be commit 79c1133467cca54bc6b647ca74744e0be7bb9f81)

--- source4/selftest/test_member.sh
+++ source4/selftest/test_member.sh
@@ -3,4 +3,5 @@
 incdir=`dirname $0`
 . $incdir/test_functions.sh
 
-plantest "RPC-ECHO against member server" member $VALGRIND bin/smbtorture $TORTURE_OPTIONS ncacn_np:"\$SERVER" -U"\$USERNAME"%"\$PASSWORD" -W \$DOMAIN RPC-ECHO "$*"
+plantest "RPC-ECHO against member server with local creds" member $VALGRIND bin/smbtorture $TORTURE_OPTIONS ncacn_np:"\$NETBIOSNAME" -U"\$NETBIOSNAME\\\\\$USERNAME"%"\$PASSWORD" RPC-ECHO "$*"
+plantest "RPC-ECHO against member server with domain creds" member $VALGRIND bin/smbtorture $TORTURE_OPTIONS ncacn_np:"\$NETBIOSNAME" -U"\$DOMAIN\\\\\$DC_USERNAME"%"\$DC_PASSWORD" RPC-ECHO "$*"
