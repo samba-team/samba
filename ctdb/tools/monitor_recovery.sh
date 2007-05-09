1e38ae491f5620651e894cee0e9fcc35196cebc7
Andrew Tridgell <tridge@samba.org>
2007-05-10 08:49:57 +1000

    remove old s3 recovery code fixed vnnmap wire format in recover daemon
    (This used to be ctdb commit e03fab7bfe0cf43f40c49a3d63e75dc44001d8d8)

--- ctdb/tools/monitor_recovery.sh
+++ ctdb/tools/monitor_recovery.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 CTDB_CONTROL=./bin/ctdb_control
-XPOS=0
+export CTDB_CONTROL
 
 $CTDB_CONTROL getnodemap 0 | egrep "^vnn:" | sed -e "s/^vnn://" -e "s/ .*$//" | while read NODE; do
-	xterm -geometry 30x25+$XPOS -e "while true; do sleep 1; clear; $CTDB_CONTROL getnodemap $NODE; $CTDB_CONTROL getvnnmap $NODE; $CTDB_CONTROL getrecmode $NODE; $CTDB_CONTROL getrecmaster $NODE;done" &
-	export XPOS=`expr $XPOS "+" "200"`
+	xterm -geometry 30x25 -e "watch -n1 \"$CTDB_CONTROL getnodemap $NODE; $CTDB_CONTROL getvnnmap $NODE; $CTDB_CONTROL getrecmode $NODE; $CTDB_CONTROL getrecmaster $NODE\"" &
 
 done
 
