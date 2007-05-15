839e4b1fb3627463fd61de21c7a9c8db8b8a3850
Michael Adam <obnox@samba.org>
2007-05-15 09:38:42 +0000

    r22890: Fix cast. (In 3_0_26, TDB_DATA.dptr is still char, not unsigned char.)

--- source/lib/dbwrap_tdb.c
+++ source/lib/dbwrap_tdb.c
@@ -58,7 +58,7 @@ static struct db_record *db_tdb_fetch_locked(struct db_context *db,
 	}
 
 	result->key.dsize = key.dsize;
-	result->key.dptr = (uint8 *)talloc_memdup(result, key.dptr, key.dsize);
+	result->key.dptr = (char *)talloc_memdup(result, key.dptr, key.dsize);
 	if (result->key.dptr == NULL) {
 		DEBUG(0, ("talloc failed\n"));
 		TALLOC_FREE(result);
@@ -93,7 +93,7 @@ static struct db_record *db_tdb_fetch_locked(struct db_context *db,
 	}
 
 	result->value.dsize = value.dsize;
-	result->value.dptr = (uint8 *)talloc_memdup(result, value.dptr,
+	result->value.dptr = (char *)talloc_memdup(result, value.dptr,
 						    value.dsize);
 	if (result->value.dptr == NULL) {
 		DEBUG(3, ("talloc failed\n"));
