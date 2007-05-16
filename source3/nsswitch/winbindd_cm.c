0570ce21ffd6c2a52bf701cd72fbb66ed7b3eb3e
Volker Lendecke <vlendec@samba.org>
2007-05-16 14:45:09 +0000

    r22943: More message_register -> messaging_register (This used to be commit caece8975b0c2bad56d6a6a576bf8ce54626183f)

--- source3/nsswitch/winbindd_cm.c
+++ source3/nsswitch/winbindd_cm.c
@@ -82,13 +82,16 @@ static BOOL get_dcs(TALLOC_CTX *mem_ctx, const struct winbindd_domain *domain,
  Child failed to find DC's. Reschedule check.
 ****************************************************************/
 
-static void msg_failed_to_go_online(int msg_type, struct server_id src,
-				    void *buf, size_t len, void *private_data)
+static void msg_failed_to_go_online(struct messaging_context *msg,
+				    void *private_data,
+				    uint32_t msg_type,
+				    struct server_id server_id,
+				    DATA_BLOB *data)
 {
 	struct winbindd_domain *domain;
-	const char *domainname = (const char *)buf;
+	const char *domainname = (const char *)data->data;
 
-	if (buf == NULL || len == 0) {
+	if (data->data == NULL || data->length == 0) {
 		return;
 	}
 
@@ -118,13 +121,16 @@ static void msg_failed_to_go_online(int msg_type, struct server_id src,
  Actually cause a reconnect from a message.
 ****************************************************************/
 
-static void msg_try_to_go_online(int msg_type, struct server_id src,
-				 void *buf, size_t len, void *private_data)
+static void msg_try_to_go_online(struct messaging_context *msg,
+				 void *private_data,
+				 uint32_t msg_type,
+				 struct server_id server_id,
+				 DATA_BLOB *data)
 {
 	struct winbindd_domain *domain;
-	const char *domainname = (const char *)buf;
+	const char *domainname = (const char *)data->data;
 
-	if (buf == NULL || len == 0) {
+	if (data->data == NULL || data->length == 0) {
 		return;
 	}
 
@@ -184,10 +190,12 @@ static BOOL fork_child_dc_connect(struct winbindd_domain *domain)
 
 	if (child_pid != 0) {
 		/* Parent */
-		message_register(MSG_WINBIND_TRY_TO_GO_ONLINE,
-				 msg_try_to_go_online, NULL);
-		message_register(MSG_WINBIND_FAILED_TO_GO_ONLINE,
-				 msg_failed_to_go_online, NULL);
+		messaging_register(winbind_messaging_context(), NULL,
+				   MSG_WINBIND_TRY_TO_GO_ONLINE,
+				   msg_try_to_go_online);
+		messaging_register(winbind_messaging_context(), NULL,
+				   MSG_WINBIND_FAILED_TO_GO_ONLINE,
+				   msg_failed_to_go_online);
 		message_unblock();
 		return True;
 	}
