8f4655d6423d3f5fa80dc89d75497779b3dcb110
Stefan Metzmacher <metze@samba.org>
2007-05-21 13:33:46 +0000

    r23037: actually fix the asn1 memory leak :-)
    metze

--- source/libcli/util/asn1.c
+++ source/libcli/util/asn1.c
@@ -24,7 +24,7 @@
 /* allocate an asn1 structure */
 struct asn1_data *asn1_init(TALLOC_CTX *mem_ctx)
 {
-	struct asn1_data *ret = talloc_zero(NULL, struct asn1_data);
+	struct asn1_data *ret = talloc_zero(mem_ctx, struct asn1_data);
 	if (ret == NULL) {
 		DEBUG(0,("asn1_init failed! out of memory\n"));
 	}
