95bfdcf9da09b31730d1b6e39219e09e2d7d182e
Stefan Metzmacher <metze@samba.org>
2007-05-25 09:15:09 +0000

    r23137: make it possible to skip the pidl tests with PIDL_TESTS_SKIP=yes
    metze

--- source/selftest/test_pidl.sh
+++ source/selftest/test_pidl.sh
@@ -7,7 +7,9 @@ fi
 incdir=`dirname $0`
 . $incdir/test_functions.sh
 
-if $PERL -e 'eval require Test::More;' > /dev/null 2>&1; then
+if test x"${PIDL_TESTS_SKIP}" = x"yes"; then
+   echo "Skipping pidl tests - PIDL_TESTS_SKIP=yes"
+elif $PERL -e 'eval require Test::More;' > /dev/null 2>&1; then
   for f in pidl/tests/*.pl; do
      plantest "$f" none $PERL $f
   done
