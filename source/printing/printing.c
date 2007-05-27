3452a870d58cdddf03ddf6ee698bca8416e05cbf
Volker Lendecke <vlendec@samba.org>
2007-05-27 16:22:12 +0000

    r23167: Remove an unused parameter

--- source/printing/printing.c
+++ source/printing/printing.c
@@ -1396,7 +1396,7 @@ void start_background_queue(void)
 		/* Child. */
 		DEBUG(5,("start_background_queue: background LPQ thread started\n"));
 
-		claim_connection( NULL, "smbd lpq backend", 0, False, 
+		claim_connection( NULL, "smbd lpq backend", 0, 
 			FLAG_MSG_GENERAL|FLAG_MSG_SMBD|FLAG_MSG_PRINT_GENERAL);
 
 		if (!locking_init(0)) {
