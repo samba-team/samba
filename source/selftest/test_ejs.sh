9471de13ea92ca017019464c4439c4a5528879c0
Rafal Szczesniak <mimir@samba.org>
2007-05-29 06:33:55 +0000

    r23179: I forgot ejsnet.js script is one of tests. Disable it temporarily.
    rafal

--- source/selftest/test_ejs.sh
+++ source/selftest/test_ejs.sh
@@ -15,7 +15,7 @@ export PATH
 plantest "base.js" dc "$SCRIPTDIR/base.js" $CONFIGURATION
 plantest "samr.js" dc "$SCRIPTDIR/samr.js" $CONFIGURATION ncalrpc: -U\$USERNAME%\$PASSWORD
 plantest "echo.js" dc "$SCRIPTDIR/echo.js" $CONFIGURATION ncalrpc: -U\$USERNAME%\$PASSWORD
-plantest "ejsnet.js" dc "$SCRIPTDIR/ejsnet.js" $CONFIGURATION -U\$USERNAME%\$PASSWORD \$DOMAIN ejstestuser
+#plantest "ejsnet.js" dc "$SCRIPTDIR/ejsnet.js" $CONFIGURATION -U\$USERNAME%\$PASSWORD \$DOMAIN ejstestuser
 plantest "ldb.js" none "$SCRIPTDIR/ldb.js" `pwd` $CONFIGURATION
 plantest "samba3sam.js" none $SCRIPTDIR/samba3sam.js $CONFIGURATION `pwd` $DATADIR/samba3/
 plantest "winreg" dc scripting/bin/winreg $CONFIGURATION ncalrpc: 'HKLM' -U\$USERNAME%\$PASSWORD
