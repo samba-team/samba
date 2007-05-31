af1c3c64b0223a545783ac83265f7c64af273612
Andrew Bartlett <abartlet@samba.org>
2007-05-31 05:37:52 +0000

    r23262: Fix mkrelease.sh to work in the right directories (This used to be commit db8d78f187339acefc0ba03b3c33d1128208b084)

--- source4/script/mkrelease.sh
+++ source4/script/mkrelease.sh
@@ -4,9 +4,8 @@ VERSION=$1
 
 svn export . samba-$VERSION || exit 1
 
-cd samba-$VERSION/source
-./autogen.sh || exit 1
-./configure || exit 1
-make dist  || exit 1
-
-tar -zcf samba-$VERSION.tar.gz samba-$VERSION
+( cd samba-$VERSION/source
+ ./autogen.sh || exit 1
+ ./configure || exit 1
+ make dist  || exit 1
+) && tar -zcf samba-$VERSION.tar.gz samba-$VERSION
