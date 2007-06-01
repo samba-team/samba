869d70d9c9b05fc97102317c88bd02fed9aba421
Andrew Tridgell <tridge@samba.org>
2007-06-01 19:04:51 +1000

    convert ctdbd.sh tests to use an event script (This used to be ctdb commit b3eff804fe59238ed37cd692cc04785089a89dc3)

--- ctdb/tests/ctdbd.sh
+++ ctdb/tests/ctdbd.sh
@@ -3,8 +3,10 @@
 killall -q ctdbd
 
 echo "Starting 2 ctdb daemons"
-$VALGRIND bin/ctdbd --nlist direct/nodes.txt
-$VALGRIND bin/ctdbd --nlist direct/nodes.txt
+$VALGRIND bin/ctdbd --nlist direct/nodes.txt --event-script=tests/events --logfile=-
+$VALGRIND bin/ctdbd --nlist direct/nodes.txt --event-script=tests/events --logfile=-
+
+sleep 2
 
 echo "Testing ping"
 $VALGRIND bin/ctdb ping || exit 1
@@ -19,7 +21,7 @@ echo "Testing statisticsreset"
 $VALGRIND bin/ctdb -n all statisticsreset || exit 1
 
 echo "Testing debug"
-$VALGRIND bin/ctdb -n all setdebug 5 || exit 1
+$VALGRIND bin/ctdb -n all setdebug 3 || exit 1
 $VALGRIND bin/ctdb -n all getdebug || exit 1
 $VALGRIND bin/ctdb -n all setdebug 0 || exit 1
 $VALGRIND bin/ctdb -n all getdebug || exit 1
@@ -31,6 +33,15 @@ $VALGRIND bin/ctdb attach test2.tdb || exit 1
 echo "Testing getdbmap"
 $VALGRIND bin/ctdb getdbmap || exit 1
 
-echo "All done"
+echo "Testing status"
+$VALGRIND bin/ctdb status || exit 1
 
+sleep 1
+
+echo "Testing shutdown"
+$VALGRIND bin/ctdb shutdown -n all || exit 1
+
+sleep 1
+
+echo "All done"
 killall -q ctdbd
