76b7361c7e12bd12f5af7d6b5a08b98cac7482a0
Andrew Tridgell <tridge@samba.org>
2007-06-06 12:08:42 +1000

    - added monitoring of rpc ports for nfs, and of Samba ports and directories - added monitoring of the ethernet link state
    When monitoring detects an error, the node loses its public IP address
    
    (This used to be ctdb commit 0af57aead8c983511d25774b4ffe09fa5ff26501)

--- ctdb/common/ctdb_tunables.c
+++ ctdb/common/ctdb_tunables.c
@@ -36,7 +36,7 @@ static const struct {
 	{ "RecoverInterval",   1,  offsetof(struct ctdb_tunable, recover_interval) },
 	{ "ElectionTimeout",   3,  offsetof(struct ctdb_tunable, election_timeout) },
 	{ "TakeoverTimeout",   5,  offsetof(struct ctdb_tunable, takeover_timeout) },
-	{ "MonitorInterval",  60,  offsetof(struct ctdb_tunable, monitor_interval) },
+	{ "MonitorInterval",  15,  offsetof(struct ctdb_tunable, monitor_interval) },
 };
 
 /*
