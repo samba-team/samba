97d5bea2eb4172f26af8cf9a0e83279627e2c700
Andrew Tridgell <tridge@samba.org>
2007-06-12 19:44:54 +1000

    on startup release all IPs, in case we have any left over from a previous run (This used to be ctdb commit 5eb2f8f5f70f567c264d6929e95899b70f0e4ec0)

--- ctdb/server/ctdb_daemon.c
+++ ctdb/server/ctdb_daemon.c
@@ -122,6 +122,9 @@ static void ctdb_main_loop(struct ctdb_context *ctdb)
 				 CTDB_CTRL_FLAG_NOREPLY,
 				 tdb_null, NULL, NULL);
 
+	/* release any IPs we hold from previous runs of the daemon */
+	ctdb_release_all_ips(ctdb);
+
 	ret = ctdb_event_script_callback(ctdb, timeval_zero(), ctdb, 
 					 ctdb_start_transport, NULL, "startup");
 	if (ret != 0) {
