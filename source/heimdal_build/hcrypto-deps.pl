ebf8cd09c3a4e32e824e31178330dde2d867f5e1
Stefan Metzmacher <metze@samba.org>
2007-06-14 12:05:08 +0000

    r23487: fix the build with automatic dependencies
    metze

--- source/heimdal_build/hcrypto-deps.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-use strict;
-
-foreach (@ARGV) {
-	my $old = $_;
-	my $new = $old; $new =~ s/hcrypto/hcrypto\/hcrypto/g;
-	my $dir = $old; 
-	print "$new: heimdal/lib/hcrypto/hcrypto\n";
-}
