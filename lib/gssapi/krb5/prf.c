3a5f0052445b6ee54af45f56280c9ddb22eaa04b
Love Hörnquist Åstrand <lha@kth.se>
2007-06-18 20:28:44 +0000

    (_gsskrb5_pseudo_random): add missing break.
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@21129 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/gssapi/krb5/prf.c
+++ lib/gssapi/krb5/prf.c
@@ -67,6 +67,7 @@ _gsskrb5_pseudo_random(OM_uint32 *minor_status,
     switch(prf_key) {
     case GSS_C_PRF_KEY_FULL:
 	_gsskrb5i_get_acceptor_subkey(ctx, context, &key);
+	break;
     case GSS_C_PRF_KEY_PARTIAL:
 	_gsskrb5i_get_initiator_subkey(ctx, context, &key);
 	break;
