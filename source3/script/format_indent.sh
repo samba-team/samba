386267447b9043eafb8497c0f1fca0f7eead821b
Gerald Carter <jerry@samba.org>
2007-06-25 15:23:19 +0000

    r23599: Add new version of indent script copied from the the Linux kernel tree. (This used to be commit bc422ab5f152c3d7ff730a2e0a80ad3a196a797a)

--- source3/script/format_indent.sh
+++ source3/script/format_indent.sh
@@ -1,21 +1,12 @@
 #!/bin/sh
 
 # -npro	Do no read the '.indent.pro' files.
-# -l80	Set maximum line length for non-comment lines to 80.
-# -bad 	Force blank lines after the declarations.
-# -bap	Force blank lines after procedure bodies.
-# -bbb	Force blank lines before block comments.
-# -br	Put braces on line with if, etc.
-# -ce	Cuddle else and preceeding ‘}’.
-# -ut	Use tabs.
-# -ts8	Set tab size to 8 spaces
+# -kr	Use K&R formatting rules
 # -i8	Set indentation level to 8 spaces.
-# -di1	Put variables in column 1.
-# -brs	Put braces on struct declaration line.
-# -npsl	Put the type of a procedure on the same line as its name.
-# -npcs	Do not put space after the function in function calls.
-# -nprs	Do not put a space after every ’(’ and before every ’)’.
-# -bbo	Prefer to break long lines before boolean operators.
-# -hnl	Prefer to break long lines at the position of newlines in the input.
+# -ts8	Set tab size to 8 spaces
+# -sob	Swallow optional blank lines.
+# -l80	Set the maximum line length at 80 characters.
+# -ss	On one-line for and while statments, force a blank before the semicolon
+# -ncs	Do not put a space after cast operators.
 
-indent -npro -l80 -bad -bap -bbb -br -ce -ut -ts8 -i8 -di1 -brs -npsl -npcs -nprs -bbo -hnl "$@"
+indent -npro -kr -i8 -ts8 -sob -l80 -ss -ncs "$@"
