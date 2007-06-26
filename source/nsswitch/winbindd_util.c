b8107d3905129eb28b35307d8bfb640ab3e574de
Gerald Carter <jerry@samba.org>
2007-06-26 00:52:19 +0000

    r23601: BUG 4579: Don't mark the SAM domain on a Samba DC as an internal domain or else all network connections from the local winbindd will fail

--- source/nsswitch/winbindd_util.c
+++ source/nsswitch/winbindd_util.c
@@ -91,6 +91,9 @@ static BOOL is_internal_domain(const DOM_SID *sid)
 	if (sid == NULL)
 		return False;
 
+	if ( IS_DC )
+		return sid_check_is_builtin(sid);
+
 	return (sid_check_is_domain(sid) || sid_check_is_builtin(sid));
 }
 
@@ -98,6 +101,9 @@ static BOOL is_in_internal_domain(const DOM_SID *sid)
 {
 	if (sid == NULL)
 		return False;
+
+	if ( IS_DC )
+		return sid_check_is_in_builtin(sid);
 
 	return (sid_check_is_in_our_domain(sid) || sid_check_is_in_builtin(sid));
 }
