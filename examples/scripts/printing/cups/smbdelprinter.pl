c14aba93c06348a1f84fc1dd9791ff1159d22c4b
Andrew Tridgell <tridge@samba.org>
2007-07-10 02:46:15 +0000

    r23795: more v2->v3 conversion

--- examples/scripts/printing/cups/smbdelprinter.pl
+++ examples/scripts/printing/cups/smbdelprinter.pl
@@ -5,7 +5,7 @@
 ##   This program is free software; you can redistribute it
 ##   and/or modify it under the terms of the GNU General
 ##   Public License as published by the Free Software Foundation;
-##   ither version 2 of the License, or (at your option) any
+##   Either version 3 of the License, or (at your option) any
 ##   later version.
 ##
 ##   This program is distributed in the hope that it will be useful,
