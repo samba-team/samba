65c0d2d4b968fd3f5ed99b0bdfa386daa85b8cec
Andrew Tridgell <tridge@samba.org>
2007-07-10 01:52:05 +0000

    r23791: found some more v2->v3 conversions

--- source/lib/replace/strptime.c
+++ source/lib/replace/strptime.c
@@ -5,7 +5,7 @@
 
    The GNU C Library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public License as
-   published by the Free Software Foundation; either version 2 of the
+   published by the Free Software Foundation; either version 3 of the
    License, or (at your option) any later version.
 
    The GNU C Library is distributed in the hope that it will be useful,
