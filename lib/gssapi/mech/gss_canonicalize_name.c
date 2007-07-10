dbbf83f2693e0ae08c0f7d3cde8916dfb6de1a89
Love Hörnquist Åstrand <lha@kth.se>
2007-07-10 16:31:27 +0000

    New signature of _gss_find_mn.
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@21476 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/gssapi/mech/gss_canonicalize_name.c
+++ lib/gssapi/mech/gss_canonicalize_name.c
@@ -44,10 +44,9 @@ gss_canonicalize_name(OM_uint32 *minor_status,
 	*minor_status = 0;
 	*output_name = 0;
 
-	mn = _gss_find_mn(name, mech_type);
-	if (!mn) {
-		return (GSS_S_BAD_MECH);
-	}
+	major_status = _gss_find_mn(minor_status, name, mech_type, &mn);
+	if (major_status)
+		return major_status;
 
 	m = mn->gmn_mech;
 	major_status = m->gm_canonicalize_name(minor_status,
