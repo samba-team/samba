727a6cf2cba8da6b40610409b264e86e6908eb0c
Andrew Tridgell <tridge@samba.org>
2007-07-10 04:04:46 +0000

    r23800: LGPL is now called GNU Lesser General Public License not GNU Library General Public License

--- source/lib/replace/strptime.c
+++ source/lib/replace/strptime.c
@@ -4,7 +4,7 @@
    Contributed by Ulrich Drepper <drepper@cygnus.com>, 1996.
 
    The GNU C Library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Library General Public License as
+   modify it under the terms of the GNU Lesser General Public License as
    published by the Free Software Foundation; either version 3 of the
    License, or (at your option) any later version.
 
@@ -13,7 +13,7 @@
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    Library General Public License for more details.
 
-   You should have received a copy of the GNU Library General Public
+   You should have received a copy of the GNU Lesser General Public
    License along with the GNU C Library; see the file COPYING.LIB.  If not, 
    see <http://www.gnu.org/licenses/>.  */
 
--- source/nsswitch/winbind_nss_solaris.c
+++ source/nsswitch/winbind_nss_solaris.c
@@ -10,7 +10,7 @@
   This file is part of the nss_ldap library.
 
   The nss_ldap library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Library General Public License as
+  modify it under the terms of the GNU Lesser General Public License as
   published by the Free Software Foundation; either version 3 of the
   License, or (at your option) any later version.
 
@@ -19,7 +19,7 @@
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
   Library General Public License for more details.
 
-  You should have received a copy of the GNU Library General Public
+  You should have received a copy of the GNU Lesser General Public
   License along with the nss_ldap library; see the file COPYING.LIB.  If not,
   see <http://www.gnu.org/licenses/>.
 */
