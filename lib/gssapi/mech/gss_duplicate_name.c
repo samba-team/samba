16179acf03a5cac219ce21d216b4e0263eb11539
Love Hörnquist Åstrand <lha@kth.se>
2007-07-10 16:32:32 +0000

    New signature of _gss_find_mn.
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@21480 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/gssapi/mech/gss_duplicate_name.c
+++ lib/gssapi/mech/gss_duplicate_name.c
@@ -54,7 +54,9 @@ OM_uint32 gss_duplicate_name(OM_uint32 *minor_status,
 		new_name = (struct _gss_name *) *dest_name;
 		
 		SLIST_FOREACH(mn, &name->gn_mn, gmn_link) {
-			_gss_find_mn(new_name, mn->gmn_mech_oid);
+		    struct _gss_mechanism_name *mn2;
+		    _gss_find_mn(minor_status, new_name, 
+				 mn->gmn_mech_oid, &mn2);
 		}
 	} else {
 		new_name = malloc(sizeof(struct _gss_name));
