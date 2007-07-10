01e3fe7533b5670236c026ec3c6cc1e25655fbc3
Andrew Tridgell <tridge@samba.org>
2007-07-10 04:04:46 +0000

    r23800: LGPL is now called GNU Lesser General Public License not GNU Library General Public License

--- source/nsswitch/wb_common.c
+++ source/nsswitch/wb_common.c
@@ -9,7 +9,7 @@
    
    
    This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Library General Public
+   modify it under the terms of the GNU Lesser General Public
    License as published by the Free Software Foundation; either
    version 3 of the License, or (at your option) any later version.
    
@@ -18,7 +18,7 @@
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    Library General Public License for more details.
    
-   You should have received a copy of the GNU Library General Public License
+   You should have received a copy of the GNU Lesser General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
