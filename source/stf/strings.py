a1f0ee02804f1985acddc7f1f40f4f285b86efe4
Andrew Tridgell <tridge@samba.org>
2007-07-10 03:42:26 +0000

    r23798: updated old Temple Place FSF addresses to new URL

--- source/stf/strings.py
+++ source/stf/strings.py
@@ -15,9 +15,7 @@
 # General Public License for more details.
 # 
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
-# USA
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 # XXX: All this code assumes that the Unix character set is UTF-8,
 # which is the most common setting.  I guess it would be better to
