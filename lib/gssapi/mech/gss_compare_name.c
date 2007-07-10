4d5a754244c32cb97c2f64eec107518be29ff183
Love Hörnquist Åstrand <lha@kth.se>
2007-07-10 16:31:03 +0000

    New signature of _gss_find_mn.
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@21475 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/gssapi/mech/gss_compare_name.c
+++ lib/gssapi/mech/gss_compare_name.c
@@ -57,8 +57,11 @@ gss_compare_name(OM_uint32 *minor_status,
 		struct _gss_mechanism_name *mn2;
 
 		SLIST_FOREACH(mn1, &name1->gn_mn, gmn_link) {
-			mn2 = _gss_find_mn(name2, mn1->gmn_mech_oid);
-			if (mn2) {
+			OM_uint32 major_status;
+
+			major_status = _gss_find_mn(minor_status, name2,
+						    mn1->gmn_mech_oid, &mn2);
+			if (major_status == GSS_S_COMPLETE) {
 				return (mn1->gmn_mech->gm_compare_name(
 						minor_status,
 						mn1->gmn_name,
