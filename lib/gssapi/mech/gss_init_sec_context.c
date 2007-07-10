5fbee5a6acc7e33c13ba3961b4d8f62db34bc727
Love Hörnquist Åstrand <lha@kth.se>
2007-07-10 16:32:19 +0000

    New signature of _gss_find_mn.
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@21479 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/gssapi/mech/gss_init_sec_context.c
+++ lib/gssapi/mech/gss_init_sec_context.c
@@ -109,11 +109,11 @@ gss_init_sec_context(OM_uint32 * minor_status,
 	/*
 	 * Find the MN for this mechanism.
 	 */
-	mn = _gss_find_mn(name, mech_type);
-	if (mn == NULL) {
+	major_status = _gss_find_mn(minor_status, name, mech_type, &mn);
+	if (major_status != GSS_S_COMPLETE) {
 		if (allocated_ctx)
 			free(ctx);
-		return GSS_S_BAD_NAME;
+		return major_status;
 	}
 
 	/*
