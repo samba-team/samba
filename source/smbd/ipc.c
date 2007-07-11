ebe1ddf40ffd1c5935543e3d3fe5349abf553b49
Volker Lendecke <vlendec@samba.org>
2007-07-11 06:18:32 +0000

    r23823: Memory leak fix from Atsushi Nakabayashi <nakabayashi@miraclelinux.com>
    Thanks!
    
    Volker

--- source/smbd/ipc.c
+++ source/smbd/ipc.c
@@ -530,6 +530,8 @@ int reply_trans(connection_struct *conn, char *inbuf,char *outbuf,
 			DEBUG(0,("reply_trans: setup malloc fail for %u "
 				 "bytes !\n", (unsigned int)
 				 (state->setup_count * sizeof(uint16))));
+			SAFE_FREE(state->data);
+			SAFE_FREE(state->param);
 			TALLOC_FREE(state);
 			END_PROFILE(SMBtrans);
 			return(ERROR_DOS(ERRDOS,ERRnomem));
