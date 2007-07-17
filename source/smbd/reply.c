4c40d25fe116daad8843aad3d64476ec22f7c013
Jeremy Allison <jra@samba.org>
2007-07-17 04:47:36 +0000

    r23911: Revert r23910 to try and fix the build farm. I need to look at this more closely tomorrow. Stevef's cthon tests definately show we're not matching Windows behaviour (as his tests pass against Windows but not SAMBA_3_2) but this isn't the fix. Jeremy.

--- source/smbd/reply.c
+++ source/smbd/reply.c
@@ -1862,12 +1862,6 @@ static NTSTATUS can_rename(connection_struct *conn, files_struct *fsp,
 		return NT_STATUS_NO_SUCH_FILE;
 	}
 
-#if 0
-	/* We used to deny renames on the
-	 * source open for non-delete open.
-	 * Turns out this is not the case.
-	 * VL please test but this matches
-	 * Windows behaviour. JRA. */
 	if (S_ISDIR(pst->st_mode)) {
 		return NT_STATUS_OK;
 	}
@@ -1877,9 +1871,6 @@ static NTSTATUS can_rename(connection_struct *conn, files_struct *fsp,
 	}
 
 	return NT_STATUS_ACCESS_DENIED;
-#else
-	return NT_STATUS_OK;
-#endif
 }
 
 /*******************************************************************
