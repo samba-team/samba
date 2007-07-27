9b33009cdeb15021b9b443a01818e5ed6c10e449
Love Hörnquist Åstrand <lha@kth.se>
2007-07-27 14:22:02 +0000

    Always set the ticket options, use KRB5_ADDRESSLESS_DEFAULT as the default value, this make the unset tri-state not so useful.
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@21711 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/krb5/init_creds.c
+++ lib/krb5/init_creds.c
@@ -225,9 +225,8 @@ krb5_get_init_creds_opt_set_default_flags(krb5_context context,
 	krb5_get_init_creds_opt_set_renew_life(opt, t);
 
     krb5_appdefault_boolean(context, appname, realm, "no-addresses", 
-			    FALSE, &b);
-    if (b)
-	krb5_get_init_creds_opt_set_addressless (context, opt, TRUE);
+			    KRB5_ADDRESSLESS_DEFAULT, &b);
+    krb5_get_init_creds_opt_set_addressless (context, opt, b);
 
 #if 0
     krb5_appdefault_boolean(context, appname, realm, "anonymous", FALSE, &b);
