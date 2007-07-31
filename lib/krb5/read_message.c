41c38ef00bae5550522c0c8c8aa0ece09d5d9ae3
Love Hörnquist Åstrand <lha@kth.se>
2007-07-31 20:41:25 +0000

    (krb5_read_message): clear error string.
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@21750 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/krb5/read_message.c
+++ lib/krb5/read_message.c
@@ -52,12 +52,16 @@ krb5_read_message (krb5_context context,
 	krb5_clear_error_string (context);
 	return ret;
     }
-    if(ret < 4)
+    if(ret < 4) {
+	krb5_clear_error_string(context);
 	return HEIM_ERR_EOF;
+    }
     len = (buf[0] << 24) | (buf[1] << 16) | (buf[2] << 8) | buf[3];
     ret = krb5_data_alloc (data, len);
-    if (ret)
+    if (ret) {
+	krb5_clear_error_string(context);
 	return ret;
+    }
     if (krb5_net_read (context, p_fd, data->data, len) != len) {
 	ret = errno;
 	krb5_data_free (data);
