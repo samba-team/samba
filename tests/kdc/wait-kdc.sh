8902278a945e7fddfc3cf491e33e71e4d7fd4d32
Love Hörnquist Åstrand <lha@kth.se>
2007-08-08 07:02:06 +0000

    update (c)
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@21859 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- tests/kdc/wait-kdc.sh
+++ tests/kdc/wait-kdc.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (c) 2006 Kungliga Tekniska Högskolan
+# Copyright (c) 2006 - 2007 Kungliga Tekniska Högskolan
 # (Royal Institute of Technology, Stockholm, Sweden). 
 # All rights reserved. 
 #
