3767e755db7b58aee4ff375896beb00b133cc413
Andrew Tridgell <tridge@samba.org>
2007-08-09 06:34:20 +0000

    r24283: added a dbwrap API for temporary databases, based on the one from the s3 clustering code.
    This commit just establishes the API. The ctdb backend for this will
    come soon.

---
+++ source/lib/dbwrap/dbwrap.c
@@ -0,0 +1,33 @@
+/* 
+   Unix SMB/CIFS implementation.
+
+   database wrapper code
+
+   Copyright (C) Andrew Tridgell 2007
+   Copyright (C) Volker Lendecke 2005
+   
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "includes.h"
+#include "lib/tdb/include/tdb.h"
+#include "lib/dbwrap/dbwrap.h"
+
+/*
+  open a temporary database
+ */
+struct db_context *db_tmp_open(TALLOC_CTX *mem_ctx, const char *name, int tdb_flags)
+{
+	return db_tmp_open_tdb(mem_ctx, name, tdb_flags);
+}
