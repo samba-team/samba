03e71f5032c4b67643d2a6560f4bdd4ff98a527d
Stefan Metzmacher <metze@samba.org>
2007-08-10 09:45:09 +0000

    r24305: merge from SAMBA_3_2: patch from Bjoern JAcke <bj@SerNet.DE>:
    attached patches add EA support for Solaris. If no one disagrees, can 
    someone check this in please?
    
    metze

--- source/include/includes.h
+++ source/include/includes.h
@@ -224,6 +224,10 @@ typedef int ber_int_t;
 #include <sys/attributes.h>
 #endif
 
+#ifndef ENOATTR
+#define ENOATTR ENODATA
+#endif
+
 /* mutually exclusive (SuSE 8.2) */
 #if HAVE_ATTR_XATTR_H
 #include <attr/xattr.h>
