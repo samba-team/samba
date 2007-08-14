73b3ae9c55c5c1b3d20c69ce3571d066556881ed
Volker Lendecke <vlendec@samba.org>
2007-08-14 15:09:49 +0000

    r24415: Convert reply_getatr to the new API (This used to be commit 1a08b97a933e25362707cb0d8ba09d733af4cbbf)

--- source3/smbd/process.c
+++ source3/smbd/process.c
@@ -698,7 +698,7 @@ static const struct smb_message_struct {
 /* 0x05 */ { "SMBflush",NULL,reply_flush,AS_USER},
 /* 0x06 */ { "SMBunlink",NULL,reply_unlink,AS_USER | NEED_WRITE },
 /* 0x07 */ { "SMBmv",NULL,reply_mv,AS_USER | NEED_WRITE },
-/* 0x08 */ { "SMBgetatr",reply_getatr,NULL,AS_USER},
+/* 0x08 */ { "SMBgetatr",NULL,reply_getatr,AS_USER},
 /* 0x09 */ { "SMBsetatr",reply_setatr,NULL,AS_USER | NEED_WRITE},
 /* 0x0a */ { "SMBread",reply_read,NULL,AS_USER},
 /* 0x0b */ { "SMBwrite",reply_write,NULL,AS_USER | CAN_IPC },
