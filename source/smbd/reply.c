1057d58017168dcf9f528551d5a3512cacf792ee
Jeremy Allison <jra@samba.org>
2007-08-16 00:37:07 +0000

    r24476: Fix the mappings in reply_opeXXX calls. Now to test renames. Jeremy.

--- source/smbd/reply.c
+++ source/smbd/reply.c
@@ -1367,6 +1367,16 @@ int reply_open(connection_struct *conn, char *inbuf,char *outbuf, int dum_size, 
 			/* We have re-scheduled this call. */
 			return -1;
 		}
+		if (NT_STATUS_EQUAL(status, NT_STATUS_OBJECT_NAME_COLLISION)) {
+			/*
+			 * We hit an existing file, and if we're returning DOS
+			 * error codes OBJECT_NAME_COLLISION would map to
+			 * ERRDOS/183, we need to return ERRDOS/80, see bug
+			 * 4852.
+			 */
+			return ERROR_BOTH(NT_STATUS_OBJECT_NAME_COLLISION,
+				ERRDOS, ERRfilexists);
+		}
 		return ERROR_NT(status);
 	}
 
@@ -1708,6 +1718,16 @@ int reply_mknew(connection_struct *conn, char *inbuf,char *outbuf, int dum_size,
 			/* We have re-scheduled this call. */
 			return -1;
 		}
+		if (NT_STATUS_EQUAL(status, NT_STATUS_OBJECT_NAME_COLLISION)) {
+			/*
+			 * We hit an existing file, and if we're returning DOS
+			 * error codes OBJECT_NAME_COLLISION would map to
+			 * ERRDOS/183, we need to return ERRDOS/80, see bug
+			 * 4852.
+			 */
+			return ERROR_BOTH(NT_STATUS_OBJECT_NAME_COLLISION,
+				ERRDOS, ERRfilexists);
+		}
 		return ERROR_NT(status);
 	}
  
@@ -1813,6 +1833,16 @@ int reply_ctemp(connection_struct *conn, char *inbuf,char *outbuf, int dum_size,
 		if (open_was_deferred(SVAL(inbuf,smb_mid))) {
 			/* We have re-scheduled this call. */
 			return -1;
+		}
+		if (NT_STATUS_EQUAL(status, NT_STATUS_OBJECT_NAME_COLLISION)) {
+			/*
+			 * We hit an existing file, and if we're returning DOS
+			 * error codes OBJECT_NAME_COLLISION would map to
+			 * ERRDOS/183, we need to return ERRDOS/80, see bug
+			 * 4852.
+			 */
+			return ERROR_BOTH(NT_STATUS_OBJECT_NAME_COLLISION,
+				ERRDOS, ERRfilexists);
 		}
 		return ERROR_NT(status);
 	}
