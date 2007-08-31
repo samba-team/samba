a0c595fc94a09a43f3ed314fb87705806a8a3495
Andrew Bartlett <abartlet@samba.org>
2007-08-31 02:35:16 +0200

    r24821: Update the mkrelease.sh script and mkversion.sh to make releasing correct
    Samba4 tarballs harder to screw up.
    
    Andrew Bartlett
    (This used to be commit b17e1c783216ed0b0975c1a2e7f2a6f95cd1ac33)

--- source4/script/mkrelease.sh
+++ source4/script/mkrelease.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
 
-VERSION=$1
+TMPDIR=`mktemp samba-XXXXX`
+rm $TMPDIR || exit 1
+svn export . $TMPDIR || exit 1
 
-svn export . samba-$VERSION || exit 1
-
-( cd samba-$VERSION/source
+( cd $TMPDIR/source
  ./autogen.sh || exit 1
  ./configure || exit 1
  make dist  || exit 1
-) && tar -zcf samba-$VERSION.tar.gz samba-$VERSION
+) || exit 1
+
+VERSION=`sed -n 's/^SAMBA_VERSION_STRING=//p' $TMPDIR/source/version.h`
+mv $TMPDIR samba-$VERSION || exit 1
+tar -cf samba-$VERSION.tar samba-$VERSION || exit 1
+echo "Now run: "
+echo "gpg --detach-sign --armor samba-$VERSION.tar"
+echo "gzip samba-$VERSION.tar" 
+echo "And then upload "
+echo "samba-$VERSION.tar.gz samba-$VERSION.tar.asc" 
+echo "to pub/samba/samba4/ on samba.org"
+
+
+
