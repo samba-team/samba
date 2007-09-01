c3077a9e4bd3fd61055a1824946f6f2fb4aec583
Jelmer Vernooij <jelmer@samba.org>
2007-09-01 00:36:01 +0000

    r24860: Make it easier to run selftest.pl in a Samba3 source tree. (This used to be commit 53ec879e3168e9f56e774f0b0d70aeebb3795f8e)

--- source4/selftest/test_s3upgrade.sh
+++ source4/selftest/test_s3upgrade.sh
@@ -10,13 +10,11 @@ fi
 SCRIPTDIR=../testprogs/ejs
 DATADIR=../testdata
 
-PATH=bin:$PATH
-export PATH
-
 mkdir -p $PREFIX
 rm -f $PREFIX/*
 
-. selftest/test_functions.sh
+incdir=`dirname $0`
+. $incdir/test_functions.sh
 
 plantest "parse samba3" none bin/smbscript ../testdata/samba3/verify $CONFIGURATION ../testdata/samba3
 #plantest "upgrade" none bin/smbscript setup/upgrade $CONFIGURATION --verify --targetdir=$PREFIX ../testdata/samba3 ../testdata/samba3/smb.conf
