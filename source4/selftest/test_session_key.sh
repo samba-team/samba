78996e64b4c066ce9476448fec4375f47158c7fd
Jelmer Vernooij <jelmer@samba.org>
2007-09-02 12:48:58 +0000

    r24890: Integrate more of the RPC-SECRETS tests. (This used to be commit 84cc1d42ab201d4a8fb0ef06b85d79a3ed8bc1d4)

--- source4/selftest/test_session_key.sh
+++ source4/selftest/test_session_key.sh
@@ -4,10 +4,7 @@ incdir=`dirname $0`
 . $incdir/test_functions.sh
 
 transport="ncacn_np"
-for keyexchange in "yes" "no"; do
- for ntlm2 in "yes" "no"; do
- for lm_key in "yes" "no"; do
-  for ntlmoptions in \
+for ntlmoptions in \
         "-k no --option=usespnego=yes" \
         "-k no --option=usespnego=yes --option=ntlmssp_client:128bit=no" \
         "-k no --option=usespnego=yes --option=ntlmssp_client:56bit=yes" \
@@ -20,12 +17,9 @@ for keyexchange in "yes" "no"; do
         "-k no --option=usespnego=no --option=clientntlmv2auth=yes" \
         "-k no --option=gensec:spnego=no --option=clientntlmv2auth=yes" \
         "-k no --option=usespnego=no"; do
-   name="RPC-SECRETS on $transport with $bindoptions with NTLM2:$ntlm2 KEYEX:$keyexchange LM_KEY:$lm_key $ntlmoptions"
-   plantest "$name" dc $samba4bindir/smbtorture $TORTURE_OPTIONS $transport:"\$SERVER[$bindoptions]" --option=ntlmssp_client:keyexchange=$keyexchange --option=ntlmssp_client:ntlm2=$ntlm2 --option=ntlmssp_client:lm_key=$lm_key $ntlmoptions -U"\$USERNAME"%"\$PASSWORD" -W \$DOMAIN --option=gensec:target_hostname=\$NETBIOSNAME RPC-SECRETS "$*"
-  done
- done
- done
- done
+   name="RPC-SECRETS on $transport with $bindoptions with $ntlmoptions"
+   plantest "$name" dc $samba4bindir/smbtorture $TORTURE_OPTIONS $transport:"\$SERVER[$bindoptions]"  $ntlmoptions -U"\$USERNAME"%"\$PASSWORD" -W \$DOMAIN --option=gensec:target_hostname=\$NETBIOSNAME RPC-SECRETS "$*"
+done
 name="RPC-SECRETS on $transport with $bindoptions with Kerberos"
 plantest "$name" dc $samba4bindir/smbtorture $TORTURE_OPTIONS $transport:"\$SERVER[$bindoptions]" -k yes -U"\$USERNAME"%"\$PASSWORD" -W \$DOMAIN "--option=gensec:target_hostname=\$NETBIOSNAME" RPC-SECRETS "$*"
 name="RPC-SECRETS on $transport with $bindoptions with Kerberos - use target principal"
