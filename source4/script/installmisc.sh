a43dd8b956790985b8541011d41ce21bb76f1c1b
Jelmer Vernooij <jelmer@samba.org>
2007-09-10 15:06:05 +0000

    r25056: Install some extra files. Patch from Stefan Gohmann (This used to be commit 1b13c91375682011e1951c008b83a0780ec20d38)

--- source4/script/installmisc.sh
+++ source4/script/installmisc.sh
@@ -16,6 +16,10 @@ echo "Installing setup templates"
 mkdir -p $SETUPDIR || exit 1
 cp setup/schema-map-* $SETUPDIR || exit 1
 cp setup/DB_CONFIG $SETUPDIR || exit 1
+cp setup/upgrade $SETUPDIR || exit 1
+cp setup/provision-backend $SETUPDIR || exit 1
+cp setup/provision $SETUPDIR || exit 1
+cp setup/newuser $SETUPDIR || exit 1
 cp setup/*.inf $SETUPDIR || exit 1
 cp setup/*.ldif $SETUPDIR || exit 1
 cp setup/*.reg $SETUPDIR || exit 1
