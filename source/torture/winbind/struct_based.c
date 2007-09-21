8bf28eecf274c5f53ab735036ffe09e0df751a9c
Stefan Metzmacher <metze@samba.org>
2007-09-21 12:49:32 +0000

    r25289: add WINBIND-STRUCT-INFO test
    this may needs --option="torture:winbindd separator=+"
    
    metze

--- source/torture/winbind/struct_based.c
+++ source/torture/winbind/struct_based.c
@@ -97,6 +97,31 @@ static bool torture_winbind_struct_ping(struct torture_context *torture)
 	return true;
 }
 
+static bool torture_winbind_struct_info(struct torture_context *torture)
+{
+	struct winbindd_response rep;
+	const char *separator;
+
+	ZERO_STRUCT(rep);
+
+	torture_comment(torture, "Running WINBINDD_INFO (struct based)\n");
+
+	DO_STRUCT_REQ_REP(WINBINDD_INFO, NULL, &rep);
+
+	separator = torture_setting_string(torture,
+					   "winbindd separator",
+					   lp_winbind_separator());
+	torture_assert_int_equal(torture,
+				 rep.data.info.winbind_separator,
+				 *separator,
+				 "winbind separator doesn't match");
+
+	torture_comment(torture, "Samba Version '%s'\n",
+			rep.data.info.samba_version);
+
+	return true;
+}
+
 static bool torture_winbind_struct_netbios_name(struct torture_context *torture)
 {
 	struct winbindd_request req;
@@ -396,6 +421,7 @@ struct torture_suite *torture_winbind_struct_init(void)
 
 	torture_suite_add_simple_test(suite, "INTERFACE_VERSION", torture_winbind_struct_interface_version);
 	torture_suite_add_simple_test(suite, "PING", torture_winbind_struct_ping);
+	torture_suite_add_simple_test(suite, "INFO", torture_winbind_struct_info);
 	torture_suite_add_simple_test(suite, "NETBIOS_NAME", torture_winbind_struct_netbios_name);
 	torture_suite_add_simple_test(suite, "DOMAIN_NAME", torture_winbind_struct_domain_name);
 	torture_suite_add_simple_test(suite, "LIST_TRUSTDOM", torture_winbind_struct_list_trustdom);
