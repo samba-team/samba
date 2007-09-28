5221ebb299081da6a806362212c6a8ceb9cc70a8
Gerald Carter <jerry@samba.org>
2007-09-28 18:15:34 +0000

    r25407: Revert Longhorn join patch as it is not correct for the 3.2 tree.
    The translate_name() used by cli_session_setup_spnego() cann rely
    Winbindd since it is needed by the join process (and hence before
    Winbind can be run).
    (This used to be commit 00a93ed336c5f36643e6e33bd277608eaf05677c)

--- source3/auth/auth_domain.c
+++ source3/auth/auth_domain.c
@@ -124,7 +124,7 @@ machine %s. Error was : %s.\n", dc_name, nt_errstr(result)));
 
 	if (!lp_client_schannel()) {
 		/* We need to set up a creds chain on an unauthenticated netlogon pipe. */
-		uint32 neg_flags = NETLOGON_NEG_SELECT_AUTH2_FLAGS;
+		uint32 neg_flags = NETLOGON_NEG_AUTH2_FLAGS;
 		uint32 sec_chan_type = 0;
 		unsigned char machine_pwd[16];
 
