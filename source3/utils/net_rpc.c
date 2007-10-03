4a952fb0c0bb9d8ebb733b32f3f16596293b320b
Volker Lendecke <vlendec@samba.org>
2007-10-03 12:55:45 +0000

    r25481: Fix some typos, don't panic as non-root on net getdomainsid
    Thanks to Karolin Seeger <ks@sernet.de>
    (This used to be commit 5ae4b2739853e2cd91e839909eb868dbb56be431)

--- source3/utils/net_rpc.c
+++ source3/utils/net_rpc.c
@@ -6621,7 +6621,7 @@ int net_rpc_usage(int argc, const char **argv)
 {
 	d_printf("  net rpc info \t\t\tshow basic info about a domain \n");
 	d_printf("  net rpc join \t\t\tto join a domain \n");
-	d_printf("  net rpc oldjoin \t\t\tto join a domain created in server manager\n");
+	d_printf("  net rpc oldjoin \t\tto join a domain created in server manager\n");
 	d_printf("  net rpc testjoin \t\ttests that a join is valid\n");
 	d_printf("  net rpc user \t\t\tto add, delete and list users\n");
 	d_printf("  net rpc password <username> [<password>] -Uadmin_username%%admin_pass\n");
@@ -6632,7 +6632,7 @@ int net_rpc_usage(int argc, const char **argv)
 	d_printf("  net rpc changetrustpw \tto change the trust account password\n");
 	d_printf("  net rpc getsid \t\tfetch the domain sid into the local secrets.tdb\n");
 	d_printf("  net rpc vampire \t\tsyncronise an NT PDC's users and groups into the local passdb\n");
-	d_printf("  net rpc samdump \t\tdiplay an NT PDC's users, groups and other data\n");
+	d_printf("  net rpc samdump \t\tdisplay an NT PDC's users, groups and other data\n");
 	d_printf("  net rpc trustdom \t\tto create trusting domain's account or establish trust\n");
 	d_printf("  net rpc abortshutdown \tto abort the shutdown of a remote server\n");
 	d_printf("  net rpc shutdown \t\tto shutdown a remote server\n");
