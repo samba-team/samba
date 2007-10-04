03ad70ea52743235a14545e4f84545fd4068531a
Love Hörnquist Åstrand <lha@kth.se>
2007-10-04 07:55:54 +0000

    (krb5_enctype_to_string): make sure string is NULL on failure.
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@21947 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/krb5/crypto.c
+++ lib/krb5/crypto.c
@@ -2768,6 +2768,7 @@ krb5_enctype_to_string(krb5_context context,
     if(e == NULL) {
 	krb5_set_error_string (context, "encryption type %d not supported",
 			       etype);
+	*string = NULL;
 	return KRB5_PROG_ETYPE_NOSUPP;
     }
     *string = strdup(e->name);
