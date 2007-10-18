df3f31d2d57d86144c36dbedec6d322857847285
Stefan Metzmacher <metze@samba.org>
2007-10-18 13:27:48 +0200

    r25697: make *clean doesn't work after a svn up when some *.mk files were removed
    the error is:
    SAMBA_4_0/source> make clean
    make: *** No rule to make target `lib/ldb/samba/config.mk', needed by `Makefile'.  Stop.
    
    the problem is:
    Makefile: config.status $(MK_FILES)
    	./config.status
    
    so now we let the MK_FILES variable empty for the *clean targets
    if gnu make is detected, we should later test if this construct
    is portable to other make implementations and remove the check for gnu make.
    
    metze
    (This used to be commit 4c8e539af1e8f14bb449c92db3376dec12344e5b)

--- source4/build/smb_build/env.pm
+++ source4/build/smb_build/env.pm
@@ -50,6 +50,7 @@ sub _set_config($$)
 	}
 	
 	$self->{developer} = ($self->{config}->{developer} eq "yes");
+	$self->{gnu_make} = ($self->{config}->{GNU_MAKE} eq "yes");
 	$self->{automatic_deps} = ($self->{config}->{automatic_dependencies} eq "yes");
 }
 
