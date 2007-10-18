6a5e6b676a38983c7f9167c1c7d230f69b2689b6
Love Hörnquist Åstrand <lha@kth.se>
2007-10-18 18:23:22 +0000

    (older_enctype): old windows enctypes (arcfour based) "old", this to support windows 2000 clients (unjoined to a domain). From Andy Polyakov.
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@21964 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- kdc/kerberos5.c
+++ kdc/kerberos5.c
@@ -362,6 +362,13 @@ older_enctype(krb5_enctype enctype)
     case ETYPE_DES3_CBC_SHA1:
     case ETYPE_ARCFOUR_HMAC_MD5:
     case ETYPE_ARCFOUR_HMAC_MD5_56:
+    /* 
+     * The following three is "old" windows enctypes and is needed for
+     * windows 2000 hosts.
+     */
+    case ETYPE_ARCFOUR_MD4:
+    case ETYPE_ARCFOUR_HMAC_OLD:
+    case ETYPE_ARCFOUR_HMAC_OLD_EXP:
 	return 1;
     default:
 	return 0;
