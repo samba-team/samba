557b63be064eb8ba4a584f5260533aa7db19e65d
Stefan Metzmacher <metze@samba.org>
2007-11-05 15:38:36 +0100

    r25834: nss_wrapper: add lib/nss_wrapper skeleton
    metze
    (This used to be commit 9fdde8d5f5cc3ea49c0fb4078d7989058462e1d2)

---
+++ source4/lib/nss_wrapper/config.mk
@@ -0,0 +1,10 @@
+##############################
+# Start SUBSYSTEM NSS_WRAPPER
+[LIBRARY::NSS_WRAPPER]
+VERSION = 0.0.1
+SO_VERSION = 0
+DESCRIPTION = Wrapper library for testing nss calls without being root
+PUBLIC_HEADERS = nss_wrapper.h
+OBJ_FILES = nss_wrapper.o
+# End SUBSYSTEM NSS_WRAPPER
+##############################
