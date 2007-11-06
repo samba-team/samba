e3198b3acde1f737266ce2b1de738f08314d530c
Andrew Bartlett <abartlet@samba.org>
2007-11-06 03:46:57 +0100

    r25856: If the search fails, it is not valid to steal 'res'.
    Andrew Bartlett
    (This used to be commit f4d733c3d00c90ac2e02fcc202240ae7c290463e)

--- source4/lib/ldb/common/ldb_modules.c
+++ source4/lib/ldb/common/ldb_modules.c
@@ -322,15 +322,16 @@ int ldb_load_modules(struct ldb_context *ldb, const char *options[])
 		}
 
 		ret = ldb_search(ldb, mods_dn, LDB_SCOPE_BASE, "", attrs, &res);
+		
+		if (ret != LDB_SUCCESS) {
+			ldb_debug(ldb, LDB_DEBUG_FATAL, "ldb error (%s) occurred searching for modules, bailing out\n", ldb_errstring(ldb));
+			talloc_free(mem_ctx);
+			return -1;
+		}
 		talloc_steal(mods_dn, res);
-		if (ret == LDB_SUCCESS && (res->count == 0 || res->msgs[0]->num_elements == 0)) {
+		if (res->count == 0 || res->msgs[0]->num_elements == 0) {
 			ldb_debug(ldb, LDB_DEBUG_TRACE, "no modules required by the db");
 		} else {
-			if (ret != LDB_SUCCESS) {
-				ldb_debug(ldb, LDB_DEBUG_FATAL, "ldb error (%s) occurred searching for modules, bailing out\n", ldb_errstring(ldb));
-				talloc_free(mem_ctx);
-				return -1;
-			}
 			if (res->count > 1) {
 				ldb_debug(ldb, LDB_DEBUG_FATAL, "Too many records found (%d), bailing out\n", res->count);
 				talloc_free(mem_ctx);
