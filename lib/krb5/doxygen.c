75675dd4363fccff9fc20323bf54d1261239dc01
Love Hörnquist Åstrand <lha@kth.se>
2007-11-11 11:21:24 +0000

    Add the krb5_v4compat group.
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@22051 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/krb5/doxygen.c
+++ lib/krb5/doxygen.c
@@ -60,5 +60,6 @@ RCSID("$Id$");
 /** @defgroup krb5_ccache Heimdal Kerberos 5 credential cache functions */
 /** @defgroup krb5_address Heimdal Kerberos 5 address functions */
 /** @defgroup krb5_error Heimdal Kerberos 5 error reporting functions */
+/** @defgroup krb5_v4compat Heimdal Kerberos 4 compatiblity functions */
 /** @defgroup krb5_support Heimdal Kerberos 5 support functions */
 
