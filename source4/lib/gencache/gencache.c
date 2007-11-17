7f3b941976e00455c4768731bb923267f717b39e
Stefan Metzmacher <metze@samba.org>
2007-11-17 22:46:39 +0100

    r26017: db_wrap.h doesn't exist anymore and we now need tdb_wrap.h
    metze
    (This used to be commit 88f11a88f32a0a5fe2702d24f1879881a5865365)

--- source4/lib/gencache/gencache.c
+++ source4/lib/gencache/gencache.c
@@ -23,7 +23,7 @@
 #include "includes.h"
 #include "system/time.h"
 #include "system/filesys.h"
-#include "db_wrap.h"
+#include "tdb_wrap.h"
 #include "lib/gencache/gencache.h"
 
 #define TIMEOUT_LEN 12
