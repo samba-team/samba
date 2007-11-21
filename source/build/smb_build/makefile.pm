1755adffecb8ed68047d6ad69087a680cc63ba8f
Jelmer Vernooij <jelmer@samba.org>
2007-11-21 16:04:13 +0100

    r26100: Also check for SWIG.

--- source/build/smb_build/makefile.pm
+++ source/build/smb_build/makefile.pm
@@ -183,6 +183,7 @@ SHELL=$self->{config}->{SHELL}
 
 PERL=$self->{config}->{PERL}
 PYTHON=$self->{config}->{PYTHON}
+SWIG=$self->{config}->{SWIG}
 
 CPP=$self->{config}->{CPP}
 CPPFLAGS=$builddir_headers-I\$(srcdir)/include -I\$(srcdir) -I\$(srcdir)/lib -I\$(srcdir)/lib/replace -I\$(srcdir)/lib/talloc -D_SAMBA_BUILD_=4 -DHAVE_CONFIG_H $self->{config}->{CPPFLAGS}
