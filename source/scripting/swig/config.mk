93eba0a3a984e9cd08ab2cf74b409938c991824a
Jelmer Vernooij <jelmer@samba.org>
2007-11-21 12:31:19 +0100

    r26071: Move DCE/RPC SWIG bindings closer to the code they're wrapping.

--- source/scripting/swig/config.mk
+++ source/scripting/swig/config.mk
@@ -1,13 +1,3 @@
-#######################
-# Start LIBRARY swig_dcerpc
-[LIBRARY::swig_dcerpc]
-ENABLE = NO
-LIBRARY_REALNAME = _dcerpc.$(SHLIBEXT)
-PUBLIC_DEPENDENCIES = LIBCLI_SMB NDR_MISC LIBSAMBA-UTIL LIBSAMBA-CONFIG dcerpc_samr RPC_NDR_LSA DYNCONFIG
-OBJ_FILES = dcerpc_wrap.o
-# End LIBRARY swig_dcerpc
-#######################
-
 # Swig extensions
 swig: lib/tdb/swig/_tdb.$(SHLIBEXT) lib/ldb/swig/_ldb.$(SHLIBEXT) \
 	libcli/swig/_libcli_nbt.$(SHLIBEXT) libcli/swig/_libcli_smb.$(SHLIBEXT)
@@ -17,15 +7,10 @@ swig: lib/tdb/swig/_tdb.$(SHLIBEXT) lib/ldb/swig/_ldb.$(SHLIBEXT) \
 .i_wrap.c:
 	swig -I$(srcdir)/scripting/swig -python $<
 
-SWIG_INCLUDES = librpc/gen_ndr/samr.i librpc/gen_ndr/lsa.i librpc/gen_ndr/spoolss.i
-
-scripting/swig/dcerpc_wrap.c: scripting/swig/dcerpc.i scripting/swig/samba.i scripting/swig/status_codes.i $(SWIG_INCLUDES)
-
 clean::
 	@echo "Removing SWIG output files"
 	@-rm -f scripting/swig/tdb.pyc scripting/swig/tdb.py
 
 # Swig testing
-
 swigtest: swig
 	./selftest/test_swig.sh
