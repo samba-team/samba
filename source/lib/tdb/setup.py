d8d04300d2c2613268e8bf9e6981d172471514da
Jelmer Vernooij <jelmer@samba.org>
2007-11-25 03:06:50 +0100

    r26108: Add makefile rules for building, installing and checking python bindings.

--- source/lib/tdb/setup.py
+++ source/lib/tdb/setup.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 from distutils.core import setup
 from distutils.extension import Extension
+
 setup(name='tdb',
       version='1.0',
+      url="http://tdb.samba.org/",
       ext_modules=[Extension('_tdb', ['tdb.i'], include_dirs=['include'],
-      libraries=['tdb'], swig_opts=["-noproxydel"])],
-      )
+          library_dirs=["."], libraries=['tdb'], swig_opts=["-noproxydel"])],
+)
