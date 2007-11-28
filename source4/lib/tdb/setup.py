7ab84481d6b0bee4d1820d9e70950fa126e5269c
Jelmer Vernooij <jelmer@samba.org>
2007-11-28 01:20:00 +0100

    r26180: Allow prebuilding swig output files. (This used to be commit 52106ad04edc3e80e3e3feb8d4b2d4e9e945cbbb)

--- source4/lib/tdb/setup.py
+++ source4/lib/tdb/setup.py
@@ -5,6 +5,7 @@ from distutils.extension import Extension
 setup(name='tdb',
       version='1.0',
       url="http://tdb.samba.org/",
-      ext_modules=[Extension('_tdb', ['tdb.i'], include_dirs=['include'],
-          library_dirs=["."], libraries=['tdb'], swig_opts=["-keyword"])],
+      py_modules=["tdb"],
+      ext_modules=[Extension('_tdb', ['tdb_wrap.c'], include_dirs=['include'],
+          library_dirs=["."], libraries=['tdb'])],
 )
