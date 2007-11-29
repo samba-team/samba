461ad00b1f090d5328c234e45eb7ab906252d93d
Jelmer Vernooij <jelmer@samba.org>
2007-11-29 14:49:47 +0100

    r26197: Add bindings for libsecurity.
    (cherry picked from parts of commit 8625cd403ba3a7d2b1b1fccfeb5efd7e21de0135)
    
    Michael
    (This used to be commit 22d48224d3235403b916c98c92915165fe7ef330)

--- source3/lib/talloc/config.mk
+++ source3/lib/talloc/config.mk
@@ -1,13 +1,8 @@
-################################################
-# Start LIBRARY LIBTALLOC
 [LIBRARY::LIBTALLOC]
-VERSION = 0.0.1
-SO_VERSION = 0
+VERSION = 1.0.0
+SO_VERSION = 1
 OBJ_FILES = talloc.o
 MANPAGE = talloc.3
 CFLAGS = -Ilib/talloc
 PUBLIC_HEADERS = talloc.h
 DESCRIPTION = A hierarchical pool based memory system with destructors
-#
-# End LIBRARY LIBTALLOC
-################################################
