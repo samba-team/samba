677ac6adc38b0747f825ee597e0502277a8f74b1
Jeremy Allison <jra@samba.org>
2007-12-09 14:59:07 -0800

    Many systems don't have sa_len as part of struct sockaddr. Revert 5c347cb46d85d04bbba7c99dca7ff9628f977d84 "Choose a better default for sockaddr length.". Jeremy.

--- source/lib/system.c
+++ source/lib/system.c
@@ -2475,7 +2475,7 @@ int sys_getnameinfo(const struct sockaddr *psa,
 
 int sys_connect(int fd, const struct sockaddr * addr)
 {
-	socklen_t salen = addr->sa_len;
+	socklen_t salen = -1;
 
 	if (addr->sa_family == AF_INET) {
 	    salen = sizeof(struct sockaddr_in);
