263092d3541a2d33e3ac8ddc5088e66aa9d1ccfb
Michael Adam <obnox@samba.org>
2007-12-11 12:47:28 +0100

    Streamline logic in cm_connect_netlogon()
    by retrieving trust password only, when it will be used.
    
    Michael

--- source/nsswitch/winbindd_cm.c
+++ source/nsswitch/winbindd_cm.c
@@ -1975,10 +1975,6 @@ NTSTATUS cm_connect_netlogon(struct winbindd_domain *domain,
 		return NT_STATUS_OK;
 	}
 
-	if ((IS_DC || domain->primary) && !get_trust_pw(domain->name, mach_pwd, &sec_chan_type)) {
-		return NT_STATUS_CANT_ACCESS_DOMAIN_INFO;
-	}
-
 	netlogon_pipe = cli_rpc_pipe_open_noauth(conn->cli, PI_NETLOGON,
 						 &result);
 	if (netlogon_pipe == NULL) {
@@ -1990,9 +1986,14 @@ NTSTATUS cm_connect_netlogon(struct winbindd_domain *domain,
 		neg_flags &= ~NETLOGON_NEG_SCHANNEL;		
 		goto no_schannel;
 	}
-	
+
 	if (lp_client_schannel() != False) {
 		neg_flags |= NETLOGON_NEG_SCHANNEL;
+	}
+
+	if (!get_trust_pw(domain->name, mach_pwd, &sec_chan_type)) {
+		cli_rpc_pipe_close(netlogon_pipe);
+		return NT_STATUS_CANT_ACCESS_DOMAIN_INFO;
 	}
 
 	/* if we are a DC and this is a trusted domain, then we need to use our
