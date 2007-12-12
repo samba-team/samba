749f699f871831e9ad5b2a57e498a32f959d23c6
Günther Deschner <gd@samba.org>
2007-12-12 18:14:54 +0100

    Build fixes for libnetapi.
    Guenther
    (This used to be commit 07d33557b95106ac57fdef8c767ab86192930a6d)

---
+++ source3/lib/netapi/netapi.c
@@ -0,0 +1,76 @@
+/*
+ *  Unix SMB/CIFS implementation.
+ *  NetApi Support
+ *  Copyright (C) Guenther Deschner 2007
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "includes.h"
+#include "lib/netapi/netapi.h"
+
+extern bool AllowDebugChange;
+
+static bool libnetapi_initialized = false;
+
+WERROR libnetapi_init(struct libnetapi_ctx **context)
+{
+	struct libnetapi_ctx *ctx = NULL;
+	TALLOC_CTX *frame = NULL;
+
+	if (libnetapi_initialized) {
+		return WERR_OK;
+	}
+
+	frame = talloc_stackframe();
+
+	ctx = talloc_zero(frame, struct libnetapi_ctx);
+	if (!ctx) {
+		TALLOC_FREE(frame);
+		return WERR_NOMEM;
+	}
+
+	DEBUGLEVEL = 0;
+	DEBUGLEVEL_CLASS[DBGC_ALL] = 0;
+	dbf = x_stderr;
+	x_setbuf(x_stderr, NULL);
+	AllowDebugChange = false;
+
+	load_case_tables();
+
+	setup_logging("libnetapi", true);
+
+	if (!lp_load(get_dyn_CONFIGFILE(), true, false, false, false)) {
+		TALLOC_FREE(frame);
+		return WERR_GENERAL_FAILURE;
+	}
+
+	init_names();
+	load_interfaces();
+	reopen_logs();
+
+	BlockSignals(True, SIGPIPE);
+
+	libnetapi_initialized = true;
+
+	*context = ctx;
+
+	return WERR_OK;
+}
+
+WERROR libnetapi_free(struct libnetapi_ctx *ctx)
+{
+	TALLOC_FREE(ctx);
+	return WERR_OK;
+}
---
+++ source3/lib/netapi/netapi.h
@@ -0,0 +1,26 @@
+/*
+ *  Unix SMB/CIFS implementation.
+ *  NetApi Support
+ *  Copyright (C) Guenther Deschner 2007
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+struct libnetapi_ctx {
+	int debuglevel;
+};
+
+WERROR libnetapi_init(struct libnetapi_ctx **ctx);
+
+#include "joindomain.h"
