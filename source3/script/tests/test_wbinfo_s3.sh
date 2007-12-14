28c7c831ed7b5ccf3567c7703964d6c1f6659d89
Stefan Metzmacher <metze@samba.org>
2007-12-14 16:54:01 +0100

    selftest: add a bunch of wbinfo based tests for winbindd
    metze
    (This used to be commit 88b6d2c0e83f515e2f83e5b4edc0728c434c7294)

---
+++ source3/script/tests/test_wbinfo_s3.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+if [ $# -lt 4 ]; then
+cat <<EOF
+Usage: test_wbinfo_s3.sh DOMAIN SERVER USERNAME PASSWORD <wbinfo args>
+EOF
+exit 1;
+fi
+
+domain="$1"
+server="$2"
+username="$3"
+password="$4"
+shift 4
+ADDARGS="$*"
+
+incdir=`dirname $0`
+. $incdir/test_functions.sh
+
+OLDIFS=$IFS;
+
+tests="--ping"
+tests="$tests:--separator"
+tests="$tests:--own-domain"
+tests="$tests:--all-domains"
+tests="$tests:--trusted-domains"
+tests="$tests:--domain-info=BUILTIN"
+tests="$tests:--domain-info=$server"
+tests="$tests:--sequence"
+tests="$tests:--sequence --domain=BUILTIN"
+tests="$tests:--sequence --domain=$server"
+#Didn't pass yet# tests="$tests:--domain-users"
+tests="$tests:--domain-groups"
+tests="$tests:--name-to-sid=$username"
+#Didn't pass yet# tests="$tests:--user-info=$username"
+tests="$tests:--user-groups=$username"
+
+OLDIFS=$IFS
+NEWIFS=$':'
+IFS=$NEWIFS
+for t in $tests; do
+   IFS=$OLDIFS
+   testit "wbinfo $t" $VALGRIND $BINDIR/wbinfo $ADDARGS $t || failed=`expr $failed + 1`
+   IFS=$NEWIFS
+done
+IFS=$OLDIFS
+
+testok $0 $failed
