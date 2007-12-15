a21a484a0153a339d275751cd077557d7cbf5208
Jelmer Vernooij <jelmer@samba.org>
2007-12-15 22:23:30 +0100

    r26460: Enable ldb python tests. (This used to be commit 25e982453142eb7d2f68c0751d641581b46fbb49)

--- source4/lib/samba3/config.mk
+++ source4/lib/samba3/config.mk
@@ -10,7 +10,7 @@ OBJ_FILES = tdbsam.o policy.o \
 		idmap.o winsdb.o samba3.o group.o \
 		registry.o secrets.o share_info.o
 PRIVATE_DEPENDENCIES = LIBSAMBA-UTIL LIBTDB NDR_SECURITY \
-					   CREDENTIALS SMBPASSWD
+					   CREDENTIALS SMBPASSWD LIBSECURITY
 # End SUBSYSTEM LIBSAMBA3
 ################################################
 
@@ -19,5 +19,6 @@ PRIVATE_DEPENDENCIES = LIBSAMBA-UTIL LIBTDB NDR_SECURITY \
 [SUBSYSTEM::SMBPASSWD]
 PRIVATE_PROTO_HEADER = samba3_smbpasswd_proto.h
 OBJ_FILES = smbpasswd.o
+PRIVATE_DEPENDENCIES = CHARSET LIBSAMBA-UTIL
 # End SUBSYSTEM LIBSAMBA3
 ################################################
