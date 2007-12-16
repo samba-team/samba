827632ab6e00bacef45c15efc626a1ef587c0915
Jelmer Vernooij <jelmer@samba.org>
2007-12-16 16:19:43 +0100

    r26476: Remove unnecessary hack.

--- source/lib/charset/config.mk
+++ source/lib/charset/config.mk
@@ -9,6 +9,5 @@ PUBLIC_HEADERS = charset.h
 PUBLIC_PROTO_HEADER = charset_proto.h
 PUBLIC_DEPENDENCIES = ICONV
 PRIVATE_DEPENDENCIES = DYNCONFIG
-LDFLAGS = bin/static/libsamba-config.a bin/static/libsamba-util.a
 # End SUBSYSTEM CHARSET
 ################################################
