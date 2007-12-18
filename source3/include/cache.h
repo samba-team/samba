b940e473dd15a8d6d670d241a7c9010791b66b1e
Volker Lendecke <vl@samba.org>
2007-12-18 10:07:08 +0100

    Add forgotten cache.h (This used to be commit 0dc4d6a8de84c191e339ee08c7f06ca63f83e6f3)

---
+++ source3/include/cache.h
@@ -0,0 +1,51 @@
+/*
+   Unix SMB/CIFS implementation.
+   In-memory cache
+   Copyright (C) Volker Lendecke 2005-2007
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef __CACHE_H__
+#define __CACHE_H__
+
+#include "includes.h"
+
+struct memcache;
+
+enum memcache_number {
+	STAT_CACHE,
+	UID_SID_CACHE,
+	SID_UID_CACHE,
+	GID_SID_CACHE,
+	SID_GID_CACHE,
+	GETWD_CACHE,
+	GETPWNAM_CACHE,
+	MANGLE_HASH2_CACHE
+};
+
+struct memcache *memcache_init(TALLOC_CTX *mem_ctx, size_t max_size);
+
+void memcache_add(struct memcache *cache, enum memcache_number n,
+		  DATA_BLOB key, DATA_BLOB value);
+
+void memcache_delete(struct memcache *cache, enum memcache_number n,
+		     DATA_BLOB key);
+
+bool memcache_lookup(struct memcache *cache, enum memcache_number n,
+		     DATA_BLOB key, DATA_BLOB *value);
+
+void memcache_flush(struct memcache *cache, enum memcache_number n);
+
+#endif
