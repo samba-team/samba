f76f994e471ff8f62714a51bdc15469e08aa7062
Michael Adam <obnox@samba.org>
2007-12-19 18:18:30 +0100

    Only retrieve password policies in pam_auth when WBFLAG_PAM_GET_PWD is set.
    This essentially re-establishes r14496 (2155bb0535656f294bd054d6a0a7d16a9a71c31b)
    which was undone in r17723 (43bd8c00abb38eb23a1497a255d194fb1bbffffb) for
    reasons that are unclear to me. Maybe I am being too naive.
    
    Now we do again only retrieve the password policy when called from
    the pam_winbind module. This fixes logons delegated to AD trusted
    domain controllers: We need to connect to the sam to retrieve the
    password policy. But auhtenticated session setup is not possible
    when contacting the trusted domain dc and afterwards, SamrConnect
    also fails with whatever credentials and method used.
    
    Michael

--- source/nsswitch/pam_winbind.c
+++ source/nsswitch/pam_winbind.c
@@ -1028,7 +1028,9 @@ static int winbind_auth_request(pam_handle_t * pamh,
 	request.data.auth.krb5_cc_type[0] = '\0';
 	request.data.auth.uid = -1;
 	
-	request.flags = WBFLAG_PAM_INFO3_TEXT | WBFLAG_PAM_CONTACT_TRUSTDOM;
+	request.flags = WBFLAG_PAM_INFO3_TEXT |
+			WBFLAG_PAM_GET_PWD_POLICY |
+			WBFLAG_PAM_CONTACT_TRUSTDOM;
 
 	if (ctrl & (WINBIND_KRB5_AUTH|WINBIND_CACHED_LOGIN)) {
 		struct passwd *pwd = NULL;
