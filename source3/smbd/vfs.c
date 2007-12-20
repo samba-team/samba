dea7d2223d0d7e5db44cb8df7bf8d6b2ad1eb91f
Volker Lendecke <vl@samba.org>
2007-12-20 20:59:44 +0100

    Fix a C++ warning (This used to be commit 7e1d9b561f6df233b8c7eaec83d1e4207b5a2fb0)

--- source3/smbd/vfs.c
+++ source3/smbd/vfs.c
@@ -280,7 +280,7 @@ void *vfs_fetch_fsp_extension(vfs_handle_struct *handle, files_struct *fsp)
 {
 	struct vfs_fsp_data *head;
 
-	head = vfs_memctx_fsp_extension(handle, fsp);
+	head = (struct vfs_fsp_data *)vfs_memctx_fsp_extension(handle, fsp);
 	if (head != NULL) {
 		return EXT_DATA_AREA(head);
 	}
