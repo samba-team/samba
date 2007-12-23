f5570dcf503a4b28ebf624d06fd090f3b6b0a560
Stefan Metzmacher <metze@samba.org>
2007-12-23 20:09:16 +0100

    selftest: also export LD_LIBRARY_PATH for samba4's smbtorture
    metze

--- source/script/tests/tests_all.sh
+++ source/script/tests/tests_all.sh
@@ -4,6 +4,9 @@ $SCRIPTDIR/test_smbtorture_s3.sh //$SERVER_IP/tmp $USERNAME $PASSWORD "" || fail
 $SCRIPTDIR/test_smbclient_s3.sh $SERVER $SERVER_IP || failed=`expr $failed + $?`
 $SCRIPTDIR/test_wbinfo_s3.sh $WORKGROUP $SERVER $USERNAME $PASSWORD || failed=`expr $failed + $?`
 
+LD_LIBRARY_PATH="$SAMBA4SHAREDDIR:$LD_LIBRARY_PATH"
+echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH
 SMBTORTURE4VERSION=`$SMBTORTURE4 --version`
 if [ -n "$SMBTORTURE4" -a -n "$SMBTORTURE4VERSION" ];then
 	echo "Running Tests with Samba4's smbtorture"
