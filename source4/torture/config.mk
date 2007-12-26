40309ed662fbdd9be690bedcff96350da51ffc9d
Jelmer Vernooij <jelmer@samba.org>
2007-12-26 04:04:23 -0600

    r26604: Don't depend on Python just yet, since the code that uses it is ifdeffed out. (This used to be commit b863fdf474503918f347883a578a8c6624fd615a)

--- source4/torture/config.mk
+++ source4/torture/config.mk
@@ -296,8 +296,7 @@ PRIVATE_DEPENDENCIES = \
 		LIBSAMBA-NET \
 		smbcalls \
 		POPT_CREDENTIALS \
-		torture_rpc \
-		LIBPYTHON
+		torture_rpc
 # End SUBSYSTEM TORTURE_NET
 #################################
 
