94ee39c23f6e4a8c31701240795c288299d6bb08
Volker Lendecke <vl@samba.org>
2007-12-28 00:12:14 +0100

    Fix the non-gcc branch of "likely" (This used to be commit 1e07368b5f96e4ada622682e38d260eb0c6185f2)

--- source3/include/debug.h
+++ source3/include/debug.h
@@ -172,8 +172,8 @@ extern bool *DEBUGLEVEL_CLASS_ISSET;
 #define likely(x)   __builtin_expect(!!(x), 1)
 #define unlikely(x) __builtin_expect(!!(x), 0)
 #else
-#define likely(x) x
-#define unlikely(x) x
+#define likely(x) (x)
+#define unlikely(x) (x)
 #endif
 
 #define DEBUGLVL( level ) \
