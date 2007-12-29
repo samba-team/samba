d674b95357b34a89b915af68fa12aa6b4169198d
Michael Adam <obnox@samba.org>
2007-12-29 22:29:33 +0100

    Make libnet_smbconf_reg_open_path() static.
    Michael
    (This used to be commit 6447bae71c99407485307dd508603c73d5bb9823)

--- source3/libnet/libnet_conf.c
+++ source3/libnet/libnet_conf.c
@@ -58,10 +58,10 @@ static WERROR libnet_smbconf_add_string_to_array(TALLOC_CTX *mem_ctx,
 /*
  * Open a subkey of KEY_SMBCONF (i.e a service)
  */
-WERROR libnet_smbconf_reg_open_path(TALLOC_CTX *ctx,
-				    const char *subkeyname,
-				    uint32 desired_access,
-				    struct registry_key **key)
+static WERROR libnet_smbconf_reg_open_path(TALLOC_CTX *ctx,
+					   const char *subkeyname,
+					   uint32 desired_access,
+					   struct registry_key **key)
 {
 	WERROR werr = WERR_OK;
 	char *path = NULL;
