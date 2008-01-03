547c3583e42e22e42432a10c79803219ee043dc7
Michael Adam <obnox@samba.org>
2008-01-03 13:43:34 +0100

    Rename libnet_smbconf_reg_set_value() to libnet_conf_reg_set_value().
    Michael
    (This used to be commit 96b2923bc3c57700352869627c38609529d53cd2)

--- source3/libnet/libnet_conf.c
+++ source3/libnet/libnet_conf.c
@@ -196,9 +196,9 @@ done:
 /*
  * add a value to a key.
  */
-static WERROR libnet_smbconf_reg_set_value(struct registry_key *key,
-					   const char *valname,
-					   const char *valstr)
+static WERROR libnet_conf_reg_set_value(struct registry_key *key,
+					const char *valname,
+					const char *valstr)
 {
 	struct registry_value val;
 	WERROR werr = WERR_OK;
@@ -697,7 +697,7 @@ WERROR libnet_smbconf_setparm(const char *service,
 		goto done;
 	}
 
-	werr = libnet_smbconf_reg_set_value(key, param, valstr);
+	werr = libnet_conf_reg_set_value(key, param, valstr);
 
 done:
 	TALLOC_FREE(mem_ctx);
