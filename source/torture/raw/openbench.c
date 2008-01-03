9bea9c0c4f62dabaaad4c56e7e55da96571b1e1d
Jelmer Vernooij <jelmer@samba.org>
2008-01-03 17:21:50 -0600

    r26650: torture/raw: Fix warnings in lockbench/openbench.
    I'm surprised this didn't cause any breakages. We should probably run (a subset of) RAW-LOCKBENCH + RAW-OPENBENCH in make test to make sure they don't regress.

--- source/torture/raw/openbench.c
+++ source/torture/raw/openbench.c
@@ -62,7 +62,7 @@ struct benchopen_state {
 	struct timed_event *te;
 
 	/* these are used for reconnections */
-	int dest_port;
+	const char **dest_ports;
 	const char *dest_host;
 	const char *called_name;
 	const char *service_type;
@@ -124,7 +124,7 @@ static void reopen_connection(struct event_context *ev, struct timed_event *te,
 	}
 
 	io->in.dest_host    = state->dest_host;
-	io->in.dest_ports   = state->dest_port;
+	io->in.dest_ports   = state->dest_ports;
 	io->in.called_name  = state->called_name;
 	io->in.service      = share;
 	io->in.service_type = state->service_type;
@@ -379,7 +379,11 @@ bool torture_bench_open(struct torture_context *torture)
 		state[i].tree = state[i].cli->tree;
 		state[i].dest_host = talloc_strdup(state[i].mem_ctx, 
 						   state[i].cli->tree->session->transport->socket->hostname);
-		state[i].dest_port = state[i].cli->tree->session->transport->socket->port;
+		state[i].dest_ports = talloc_array(state[i].mem_ctx, 
+						   const char *, 2);
+		state[i].dest_ports[0] = talloc_asprintf(state[i].dest_ports, 
+							 "%u", state[i].cli->tree->session->transport->socket->port);
+		state[i].dest_ports[1] = NULL;
 		state[i].called_name  = talloc_strdup(state[i].mem_ctx,
 						      state[i].cli->tree->session->transport->called.name);
 		state[i].service_type = talloc_strdup(state[i].mem_ctx,
