45b219642c529865a898625eeb0433c60b233867
Michael Adam <obnox@samba.org>
2008-01-09 01:34:21 +0100

    Fix talloctort: move size check after referenced ptr check.
    Michael

--- source/lib/talloc/talloc.c
+++ source/lib/talloc/talloc.c
@@ -787,14 +787,14 @@ void *_talloc_realloc(const void *context, void *ptr, size_t size, const char *n
 
 	tc = talloc_chunk_from_ptr(ptr);
 
-	if ((size < tc->size) && ((tc->size - size) < 1024)) {
-		tc->size = size;
-		return ptr;
-	}
-
 	/* don't allow realloc on referenced pointers */
 	if (unlikely(tc->refs)) {
 		return NULL;
+	}
+
+	if ((size < tc->size) && ((tc->size - size) < 1024)) {
+		tc->size = size;
+		return ptr;
 	}
 
 	/* by resetting magic we catch users of the old memory */
