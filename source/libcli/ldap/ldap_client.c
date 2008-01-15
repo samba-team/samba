44e1cfd2d0ef62e4ee541cec00581a7151d951b3
Jelmer Vernooij <jelmer@samba.org>
2008-01-15 01:04:38 +0100

    util: Move asn1 to lib/util to trim down the number of subsystems.

--- source/libcli/ldap/ldap_client.c
+++ source/libcli/ldap/ldap_client.c
@@ -23,7 +23,7 @@
 */
 
 #include "includes.h"
-#include "libcli/util/asn_1.h"
+#include "lib/util/asn1.h"
 #include "lib/util/dlinklist.h"
 #include "lib/events/events.h"
 #include "lib/socket/socket.h"
