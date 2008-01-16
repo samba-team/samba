7084854bd94c84250becc7e4e13035fde2d7a728
Volker Lendecke <vl@samba.org>
2008-01-16 21:04:52 +0100

    Next try to fix the max dead record calculation (This used to be commit 4f95c58040f46f343f55f9a5db2655a5e3b62c00)

--- source3/lib/tdb/common/open.c
+++ source3/lib/tdb/common/open.c
@@ -179,7 +179,7 @@ struct tdb_context *tdb_open_ex(const char *name, int hash_size, int tdb_flags,
 		tdb->page_size = 0x2000;
 	}
 
-	tdb->max_dead_records = (open_flags & TDB_VOLATILE) ? 5 : 0;
+	tdb->max_dead_records = (tdb_flags & TDB_VOLATILE) ? 5 : 0;
 
 	if ((open_flags & O_ACCMODE) == O_WRONLY) {
 		TDB_LOG((tdb, TDB_DEBUG_ERROR, "tdb_open_ex: can't open tdb %s write-only\n",
