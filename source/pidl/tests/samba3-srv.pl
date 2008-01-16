5b2ea43ed8613ac10ebe7feda0cf070c8079137a
Jelmer Vernooij <jelmer@samba.org>
2008-01-16 15:07:00 +0100

    pidl: Add simple test for ServerNDR.

--- source/pidl/tests/samba3-srv.pl
+++ source/pidl/tests/samba3-srv.pl
@@ -4,11 +4,15 @@
 use strict;
 use warnings;
 
-use Test::More tests => 0;
+use Test::More tests => 1;
 use FindBin qw($RealBin);
 use lib "$RealBin";
 use Util;
-use Parse::Pidl::Util qw(MyDumper);
-use Parse::Pidl::Samba3::ServerNDR;
+use Parse::Pidl::Util qw(MyDumper has_property);
+use Parse::Pidl::Samba3::ServerNDR qw(DeclLevel);
 
+my $l = { TYPE => "DATA", DATA_TYPE => "uint32" }; 
+my $e = { FILE => "foo", LINE => 0, PROPERTIES => { }, TYPE => "uint32",
+          LEVELS => [ $l ] };
 
+is("uint32_t", DeclLevel($e, 0));
