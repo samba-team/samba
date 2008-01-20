2925d8357319c9872d570fe045c883a25891b59e
Michael Adam <obnox@samba.org>
2008-01-20 03:24:13 +0100

    Add a registry backend current_version that replaces the former dynamic overlay.
    Make sure to only respond to the exact current version key since subkeys
    are registered by other backends (printing and - soon - perflib).
    
    Michael
    (This used to be commit 2c650bf63ccd9dc5dddbf4700831489544ded055)

--- source3/registry/reg_frontend.c
+++ source3/registry/reg_frontend.c
@@ -32,6 +32,7 @@ extern REGISTRY_OPS netlogon_params_reg_ops;
 extern REGISTRY_OPS prod_options_reg_ops;
 extern REGISTRY_OPS tcpip_params_reg_ops;
 extern REGISTRY_OPS hkpt_params_reg_ops;
+extern REGISTRY_OPS current_version_reg_ops;
 extern REGISTRY_OPS regdb_ops;		/* these are the default */
 
 /* array of REGISTRY_HOOK's which are read into a tree for easy access */
@@ -48,6 +49,7 @@ REGISTRY_HOOK reg_hooks[] = {
   { KEY_PROD_OPTIONS,		&prod_options_reg_ops },
   { KEY_TCPIP_PARAMS,		&tcpip_params_reg_ops },
   { KEY_HKPT,			&hkpt_params_reg_ops },
+  { KEY_CURRENT_VERSION,	&current_version_reg_ops },
 #endif
   { NULL, NULL }
 };
