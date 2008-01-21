1db41ff52565e9f336a22fb9ffd80d51677e023b
Kai Blin <kai@samba.org>
2008-01-21 13:18:38 +0100

    util_sock: Fix memcache bug in get_mydnsfullname.
    get_mydnsfullname relied on memcache_add(); memcache_lookup() working.
    When run from ntlm_auth, the global_cache variable in memcache is NULL, so
    the add and lookup both fail. In that case, just return the result of the
    getaddrinfo call.
    
    Jeremy, please check.

--- source/lib/util_sock.c
+++ source/lib/util_sock.c
@@ -2080,13 +2080,13 @@ const char *get_mydnsfullname(void)
 			data_blob_string_const("get_mydnsfullname"),
 			data_blob_string_const(res->ai_canonname));
 
-	freeaddrinfo(res);
-
 	if (!memcache_lookup(NULL, SINGLETON_CACHE,
 			data_blob_string_const("get_mydnsfullname"),
 			&tmp)) {
-		return NULL;
+		tmp = data_blob_string_const(res->ai_canonname);
 	}
+
+	freeaddrinfo(res);
 
 	return (const char *)tmp.data;
 }
