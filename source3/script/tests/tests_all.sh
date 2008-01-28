a626fa59cc1df3e154c062c433f3c1e1d6f87029
Kai Blin <kai@samba.org>
2008-01-28 11:28:38 +0100

    ntlm_auth: Add a blackbox test. (This used to be commit f02b74fc067074d8e48e186a7b7255a244592f7d)

--- source3/script/tests/tests_all.sh
+++ source3/script/tests/tests_all.sh
@@ -6,6 +6,7 @@ $SCRIPTDIR/test_smbclient_s3.sh $SERVER $SERVER_IP || failed=`expr $failed + $?`
 echo "Testing encrypted"
 $SCRIPTDIR/test_smbclient_s3.sh $SERVER $SERVER_IP "-e" || failed=`expr $failed + $?`
 $SCRIPTDIR/test_wbinfo_s3.sh $WORKGROUP $SERVER $USERNAME $PASSWORD || failed=`expr $failed + $?`
+$SCRIPTDIR/test_ntlm_auth_s3.sh || failed=`expr $failed + $?`
 
 eval "$LIB_PATH_VAR="\$SAMBA4SHAREDDIR:\$$LIB_PATH_VAR"; export $LIB_PATH_VAR"
 eval echo "$LIB_PATH_VAR=\$$LIB_PATH_VAR"
