beae25c808a3a03d645f247e9befcd05e3ecca2c
Günther Deschner <gd@samba.org>
2008-01-29 17:49:38 +0100

    Remove include/rpc_ds.h and all references to it completly.
    Jerry, please have a look if you're fine with that.
    
    Guenther

--- source/winbindd/winbindd_cm.c
+++ source/winbindd/winbindd_cm.c
@@ -1718,9 +1718,9 @@ static bool set_dc_type_and_flags_trustinfo( struct winbindd_domain *domain )
 	NTSTATUS result = NT_STATUS_UNSUCCESSFUL;
 	struct netr_DomainTrustList trusts;
 	int i;
-	uint32 flags = (DS_DOMAIN_IN_FOREST | 
-			DS_DOMAIN_DIRECT_OUTBOUND | 
-			DS_DOMAIN_DIRECT_INBOUND);
+	uint32 flags = (NETR_TRUST_FLAG_IN_FOREST |
+			NETR_TRUST_FLAG_OUTBOUND |
+			NETR_TRUST_FLAG_INBOUND);
 	struct rpc_pipe_client *cli;
 	TALLOC_CTX *mem_ctx = NULL;
 
@@ -1783,14 +1783,14 @@ static bool set_dc_type_and_flags_trustinfo( struct winbindd_domain *domain )
 			domain->domain_type           = trusts.array[i].trust_type;
 			domain->domain_trust_attribs  = trusts.array[i].trust_attributes;
 
-			if ( domain->domain_type == DS_DOMAIN_TRUST_TYPE_UPLEVEL )
+			if ( domain->domain_type == NETR_TRUST_TYPE_UPLEVEL )
 				domain->active_directory = True;
 
 			/* This flag is only set if the domain is *our* 
 			   primary domain and the primary domain is in
 			   native mode */
 
-			domain->native_mode = (domain->domain_flags & DS_DOMAIN_NATIVE_MODE);
+			domain->native_mode = (domain->domain_flags & NETR_TRUST_FLAG_NATIVE);
 
 			DEBUG(5, ("set_dc_type_and_flags_trustinfo: domain %s is %sin "
 				  "native mode.\n", domain->name, 
@@ -1937,7 +1937,7 @@ no_dssetup:
 			fstrcpy(domain->forest_name, forest_name);		
 
 			if (strequal(domain->forest_name, domain->alt_name)) {
-				domain->domain_flags = DS_DOMAIN_TREE_ROOT;
+				domain->domain_flags = NETR_TRUST_FLAG_TREEROOT;
 			}
 		}
 
