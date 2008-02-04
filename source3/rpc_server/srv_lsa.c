6a77f8d2d9333b33d4721a8b4043b5f58a9c0bb4
Günther Deschner <gd@samba.org>
2008-02-04 21:00:38 +0100

    Use pidl for _lsa_OpenPolicy2().
    Guenther
    (This used to be commit fc29364abe10d3ec249602cb3437e50294f5de76)

--- source3/rpc_server/srv_lsa.c
+++ source3/rpc_server/srv_lsa.c
@@ -52,29 +52,7 @@ static bool proxy_lsa_call(pipes_struct *p, uint8 opnum)
 
 static bool api_lsa_open_policy2(pipes_struct *p)
 {
-	LSA_Q_OPEN_POL2 q_u;
-	LSA_R_OPEN_POL2 r_u;
-	prs_struct *data = &p->in_data.data;
-	prs_struct *rdata = &p->out_data.rdata;
-
-	ZERO_STRUCT(q_u);
-	ZERO_STRUCT(r_u);
-
-	/* grab the server, object attributes and desired access flag...*/
-	if(!lsa_io_q_open_pol2("", &q_u, data, 0)) {
-		DEBUG(0,("api_lsa_open_policy2: unable to unmarshall LSA_Q_OPEN_POL2.\n"));
-		return False;
-	}
-
-	r_u.status = _lsa_open_policy2(p, &q_u, &r_u);
-
-	/* store the response in the SMB stream */
-	if(!lsa_io_r_open_pol2("", &r_u, rdata, 0)) {
-		DEBUG(0,("api_lsa_open_policy2: unable to marshall LSA_R_OPEN_POL2.\n"));
-		return False;
-	}
-
-	return True;
+	return proxy_lsa_call(p, NDR_LSA_OPENPOLICY2);
 }
 
 /***************************************************************************
