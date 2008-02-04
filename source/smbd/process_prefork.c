35313c0aa3fbfdd943edfb7bafd7645b1a0c54e9
Andrew Bartlett <abartlet@samba.org>
2008-02-04 17:59:16 +1100

    Remaining changes to implement the prefork process model
    To use, run 'smbd -M prefork'
    
    By default, only the smb service is preforked.  4 children are
    created, and all listen for new connections.  The Linux Kernel 'wake
    one' behaviour should ensure that only one is given the oportunity to
    accept.  We need to look into the ideal number of worker children, as
    well as load balancing behaviours.
    
    To change, set:
    
    prefork children : smb = 6
    
    valid service names (smb in this example) match those in 'server services'.
    
    Andrew Bartlett and David Disseldorp

--- source/smbd/process_prefork.c
+++ source/smbd/process_prefork.c
@@ -1,11 +1,13 @@
 /* 
    Unix SMB/CIFS implementation.
 
-   process model: standard (1 process per client connection)
+   process model: prefork (n client connections per process)
 
    Copyright (C) Andrew Tridgell 1992-2005
    Copyright (C) James J Myers 2003 <myersjj@samba.org>
    Copyright (C) Stefan (metze) Metzmacher 2004
+   Copyright (C) Andrew Bartlett 2008 <abartlet@samba.org>
+   Copyright (C) David Disseldorp 2008 <ddiss@sgi.com>
    
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -47,7 +49,7 @@ static int none_setproctitle(const char *fmt, ...)
 /*
   called when the process model is selected
 */
-static void standard_model_init(struct event_context *ev)
+static void prefork_model_init(struct event_context *ev)
 {
 	signal(SIGCHLD, SIG_IGN);
 }
@@ -55,7 +57,7 @@ static void standard_model_init(struct event_context *ev)
 /*
   called when a listening socket becomes readable. 
 */
-static void standard_accept_connection(struct event_context *ev, 
+static void prefork_accept_connection(struct event_context *ev, 
 				       struct loadparm_context *lp_ctx,
 				       struct socket_context *sock, 
 				       void (*new_conn)(struct event_context *,
@@ -65,91 +67,32 @@ static void standard_accept_connection(struct event_context *ev,
 {
 	NTSTATUS status;
 	struct socket_context *sock2;
-	pid_t pid;
-	struct event_context *ev2;
-	struct socket_address *c, *s;
+	pid_t pid = getpid();
 
 	/* accept an incoming connection. */
 	status = socket_accept(sock, &sock2);
 	if (!NT_STATUS_IS_OK(status)) {
-		DEBUG(0,("standard_accept_connection: accept: %s\n",
-			 nt_errstr(status)));
-		/* this looks strange, but is correct. We need to throttle things until
-		   the system clears enough resources to handle this new socket */
-		sleep(1);
 		return;
 	}
 
-	pid = fork();
+	talloc_steal(private, sock);
 
-	if (pid != 0) {
-		/* parent or error code ... */
-		talloc_free(sock2);
-		/* go back to the event loop */
-		return;
-	}
-
-	pid = getpid();
-
-	/* This is now the child code. We need a completely new event_context to work with */
-	ev2 = event_context_init(NULL);
-
-	/* the service has given us a private pointer that
-	   encapsulates the context it needs for this new connection -
-	   everything else will be freed */
-	talloc_steal(ev2, private);
-	talloc_steal(private, sock2);
-
-	/* this will free all the listening sockets and all state that
-	   is not associated with this new connection */
-	talloc_free(sock);
-	talloc_free(ev);
-
-	/* we don't care if the dup fails, as its only a select()
-	   speed optimisation */
-	socket_dup(sock2);
-			
-	/* tdb needs special fork handling */
-	if (tdb_reopen_all(1) == -1) {
-		DEBUG(0,("standard_accept_connection: tdb_reopen_all failed.\n"));
-	}
-
-	/* Ensure that the forked children do not expose identical random streams */
-	set_need_random_reseed();
-
-	/* setup the process title */
-	c = socket_get_peer_addr(sock2, ev2);
-	s = socket_get_my_addr(sock2, ev2);
-	if (s && c) {
-		setproctitle("conn c[%s:%u] s[%s:%u] server_id[%d]",
-			     c->addr, c->port, s->addr, s->port, pid);
-	}
-	talloc_free(c);
-	talloc_free(s);
-
-	/* setup this new connection.  Cluster ID is PID based for this process modal */
-	new_conn(ev2, lp_ctx, sock2, cluster_id(pid, 0), private);
-
-	/* we can't return to the top level here, as that event context is gone,
-	   so we now process events in the new event context until there are no
-	   more to process */	   
-	event_loop_wait(ev2);
-
-	talloc_free(ev2);
-	exit(0);
+	new_conn(ev, lp_ctx, sock2, cluster_id(pid, socket_get_fd(sock2)), private);
 }
 
 /*
   called to create a new server task
 */
-static void standard_new_task(struct event_context *ev, 
-			      struct loadparm_context *lp_ctx,
-			      const char *service_name,
-			      void (*new_task)(struct event_context *, struct loadparm_context *lp_ctx, struct server_id , void *), 
-			      void *private)
+static void prefork_new_task(struct event_context *ev, 
+			     struct loadparm_context *lp_ctx,
+			     const char *service_name,
+			     void (*new_task_fn)(struct event_context *, struct loadparm_context *lp_ctx, struct server_id , void *), 
+			     void *private)
 {
 	pid_t pid;
-	struct event_context *ev2;
+	int i, num_children;
+
+	struct event_context *ev2, *ev_parent;
 
 	pid = fork();
 
@@ -174,7 +117,7 @@ static void standard_new_task(struct event_context *ev,
 
 	/* tdb needs special fork handling */
 	if (tdb_reopen_all(1) == -1) {
-		DEBUG(0,("standard_accept_connection: tdb_reopen_all failed.\n"));
+		DEBUG(0,("prefork_accept_connection: tdb_reopen_all failed.\n"));
 	}
 
 	/* Ensure that the forked children do not expose identical random streams */
@@ -182,23 +125,65 @@ static void standard_new_task(struct event_context *ev,
 
 	setproctitle("task %s server_id[%d]", service_name, pid);
 
-	/* setup this new task.  Cluster ID is PID based for this process modal */
-	new_task(ev2, lp_ctx, cluster_id(pid, 0), private);
+	/* setup this new connection: process will bind to it's sockets etc */
+	new_task_fn(ev2, lp_ctx, cluster_id(pid, 0), private);
 
+	num_children = lp_parm_int(lp_ctx, NULL, "prefork children", service_name, 0);
+	if (num_children == 0) {
+
+		/* We don't want any kids hanging around for this one,
+		 * let the parent do all the work */
+		event_loop_wait(ev2);
+		
+		talloc_free(ev2);
+		exit(0);
+	}
+
+	/* We are now free to spawn some child proccesses */
+
+	for (i=0; i < num_children; i++) {
+
+		pid = fork();
+		if (pid > 0) {
+			continue;
+		} else if (pid == -1) {
+			return;
+		} else {
+			pid = getpid();
+			setproctitle("task %s server_id[%d]", service_name, pid);
+			/* we can't return to the top level here, as that event context is gone,
+			   so we now process events in the new event context until there are no
+			   more to process */	   
+			event_loop_wait(ev2);
+			
+			talloc_free(ev2);
+			exit(0);
+		}
+	}
+
+	/* Don't listen on the sockets we just gave to the children */
+	talloc_free(ev2);
+	
+	/* But we need a events system to handle reaping children */
+	ev_parent = event_context_init(NULL);
+	
+	/* TODO: Handle some events... */
+	
 	/* we can't return to the top level here, as that event context is gone,
 	   so we now process events in the new event context until there are no
 	   more to process */	   
-	event_loop_wait(ev2);
-
-	talloc_free(ev2);
+	event_loop_wait(ev_parent);
+	
+	talloc_free(ev_parent);
 	exit(0);
+
 }
 
 
 /* called when a task goes down */
-_NORETURN_ static void standard_terminate(struct event_context *ev, const char *reason) 
+_NORETURN_ static void prefork_terminate(struct event_context *ev, const char *reason) 
 {
-	DEBUG(2,("standard_terminate: reason[%s]\n",reason));
+	DEBUG(2,("prefork_terminate: reason[%s]\n",reason));
 
 	/* this reload_charcnv() has the effect of freeing the iconv context memory,
 	   which makes leak checking easier */
@@ -214,7 +199,7 @@ _NORETURN_ static void standard_terminate(struct event_context *ev, const char *
 }
 
 /* called to set a title of a task or connection */
-static void standard_set_title(struct event_context *ev, const char *title) 
+static void prefork_set_title(struct event_context *ev, const char *title) 
 {
 	if (title) {
 		setproctitle("%s", title);
@@ -223,19 +208,19 @@ static void standard_set_title(struct event_context *ev, const char *title)
 	}
 }
 
-static const struct model_ops standard_ops = {
-	.name			= "standard",
-	.model_init		= standard_model_init,
-	.accept_connection	= standard_accept_connection,
-	.new_task               = standard_new_task,
-	.terminate              = standard_terminate,
-	.set_title              = standard_set_title,
+static const struct model_ops prefork_ops = {
+	.name			= "prefork",
+	.model_init		= prefork_model_init,
+	.accept_connection	= prefork_accept_connection,
+	.new_task               = prefork_new_task,
+	.terminate              = prefork_terminate,
+	.set_title              = prefork_set_title,
 };
 
 /*
-  initialise the standard process model, registering ourselves with the process model subsystem
+  initialise the prefork process model, registering ourselves with the process model subsystem
  */
-NTSTATUS process_model_standard_init(void)
+NTSTATUS process_model_prefork_init(void)
 {
-	return register_process_model(&standard_ops);
+	return register_process_model(&prefork_ops);
 }
