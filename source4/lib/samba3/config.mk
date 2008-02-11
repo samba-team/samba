08c8d3b992d419db7b39a91fb42208beb5d273ba
Jelmer Vernooij <jelmer@samba.org>
2008-02-11 13:51:09 +0100

    Remove Samba 3 backwards compatibility code in C.
    This code is no longer used, and equivalent code already exists in Python
    (scripting/python/samba/samba3.py)
    (This used to be commit c16212e8bf5343496ea4b3afc30a8b4d3a0afe2d)

--- source4/lib/samba3/config.mk
+++ source4/lib/samba3/config.mk
@@ -1,19 +1,5 @@
 ################################################
 # Start SUBSYSTEM LIBSAMBA3
-[SUBSYSTEM::LIBSAMBA3]
-PRIVATE_PROTO_HEADER = samba3_proto.h
-PUBLIC_HEADERS = samba3.h
-OBJ_FILES = tdbsam.o policy.o \
-		idmap.o winsdb.o samba3.o group.o \
-		registry.o secrets.o share_info.o
-PRIVATE_DEPENDENCIES = LIBSAMBA-UTIL LIBTDB NDR_SECURITY \
-					   SMBPASSWD LIBSECURITY 
-PUBLIC_DEPENDENCIES = CREDENTIALS 
-# End SUBSYSTEM LIBSAMBA3
-################################################
-
-################################################
-# Start SUBSYSTEM LIBSAMBA3
 [SUBSYSTEM::SMBPASSWD]
 PRIVATE_PROTO_HEADER = samba3_smbpasswd_proto.h
 OBJ_FILES = smbpasswd.o
