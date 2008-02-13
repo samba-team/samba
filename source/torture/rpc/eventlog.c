samba-team/samba

commit 92b8bde561277a6b83048ce003cc29ff1b380255

r19850: Fix IDL warnings.

source/torture/rpc/eventlog.c
@@ -77,7 +77,7 @@ static bool test_GetNumRecords(struct torture_context *tctx, struct dcerpc_pipe 
 			dcerpc_eventlog_GetNumRecords(p, tctx, &r), 
 			"GetNumRecords failed");
 
-	torture_comment(tctx, talloc_asprintf(tctx, "%d records\n", r.out.number));
+	torture_comment(tctx, talloc_asprintf(tctx, "%d records\n", *r.out.number));
 
 	cr.in.handle = cr.out.handle = &handle;
 
@@ -125,7 +125,7 @@ static bool test_ReadEventLog(struct torture_context *tctx,
 		
 		/* Now read the actual record */
 
-		r.in.number_of_bytes = r.out.real_size;
+		r.in.number_of_bytes = *r.out.real_size;
 		r.out.data = talloc_size(tctx, r.in.number_of_bytes);
 
 		status = dcerpc_eventlog_ReadEventLogW(p, tctx, &r);
@@ -134,7 +134,7 @@ static bool test_ReadEventLog(struct torture_context *tctx,
 		
 		/* Decode a user-marshalled record */
 
-		blob.length = r.out.sent_size;
+		blob.length = *r.out.sent_size;
 		blob.data = talloc_steal(tctx, r.out.data);
 
 		ndr = ndr_pull_init_blob(&blob, tctx);

source/librpc/idl/eventlog.idl
@@ -81,7 +81,7 @@ import "lsa.idl", "security.idl";
 	/* Function: 0x04 */
 	NTSTATUS eventlog_GetNumRecords(
 		[in] policy_handle *handle,
-		[out] uint32 number
+		[out] uint32 *number
 	);
 
 	/******************/
@@ -119,8 +119,8 @@ import "lsa.idl", "security.idl";
 		[in] uint32 offset,
 		[in] uint32 number_of_bytes,
 		[out,size_is(number_of_bytes)] uint8 *data,
-		[out] uint32 sent_size,
-		[out] uint32 real_size
+		[out] uint32 *sent_size,
+		[out] uint32 *real_size
 	);
 
 	/*****************/

commit 3ec4bce0b64764e364aedea2800eed296805c51e

torture/eventlog: fix crash bugs!
This bug was introduced in 92b8bde561277a6b83048ce003cc29ff1b380255
and this shows that we need to be very, very careful in changing
idl elements from scalars to [ref] pointers!

metze

source/torture/rpc/eventlog.c
@@ -67,11 +67,14 @@ static bool test_GetNumRecords(struct torture_context *tctx, struct dcerpc_pipe 
 	struct eventlog_GetNumRecords r;
 	struct eventlog_CloseEventLog cr;
 	struct policy_handle handle;
+	uint32_t number = 0;
 
 	if (!get_policy_handle(tctx, p, &handle))
 		return false;
 
+	ZERO_STRUCT(r);
 	r.in.handle = &handle;
+	r.out.number = &number;
 
 	torture_assert_ntstatus_ok(tctx, 
 			dcerpc_eventlog_GetNumRecords(p, tctx, &r), 
@@ -98,6 +101,7 @@ static bool test_ReadEventLog(struct torture_context *tctx,
 	if (!get_policy_handle(tctx, p, &handle))
 		return false;
 
+	ZERO_STRUCT(r);
 	r.in.offset = 0;
 	r.in.handle = &handle;
 	r.in.flags = EVENTLOG_BACKWARDS_READ|EVENTLOG_SEQUENTIAL_READ;
@@ -107,11 +111,15 @@ static bool test_ReadEventLog(struct torture_context *tctx,
 		struct eventlog_Record rec;
 		struct ndr_pull *ndr;
 		enum ndr_err_code ndr_err;
+		uint32_t sent_size = 0;
+		uint32_t real_size = 0;
 
 		/* Read first for number of bytes in record */
 
 		r.in.number_of_bytes = 0;
 		r.out.data = NULL;
+		r.out.sent_size = &sent_size;
+		r.out.real_size = &real_size;
 
 		status = dcerpc_eventlog_ReadEventLogW(p, tctx, &r);
 