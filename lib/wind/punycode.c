b8a771bf3b3115c5d14dcf6fbb03d0bc02a7a0d5
Love Hörnquist Åstrand <lha@kth.se>
2008-02-23 10:58:43 +0000

    Make this pass the test cases again.
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@22620 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/wind/punycode.c
+++ lib/wind/punycode.c
@@ -108,22 +108,20 @@ wind_punycode_label_toascii(const uint32_t *in, size_t in_len,
 	    out[o++] = in[i];
 	}
     }
-    if (o == i) {
-	*out_len = o;
-	return 0;
-    }
     b = h;
     if (b > 0) {
 	if (o >= *out_len)
 	    return WIND_ERR_OVERRUN;
 	out[o++] = 0x2D;
     }
-
-    if (o + 4 >= *out_len)
-	return WIND_ERR_OVERRUN;
-    memmove(out + 4, out, o);
-    memcpy(out, "xn--", 4);
-    o += 4;
+    /* is this string punycoded */
+    if (h < in_len) {
+	if (o + 4 >= *out_len)
+	    return WIND_ERR_OVERRUN;
+	memmove(out + 4, out, o);
+	memcpy(out, "xn--", 4);
+	o += 4;
+    }
 
     while (h < in_len) {
 	m = (unsigned)-1;
