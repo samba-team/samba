6a84317aec9ae3fcd813390b0eec655387e036ec
Günther Deschner <gd@samba.org>
2008-02-28 00:03:00 +0100

    Re-run make idl.
    Guenther

--- source/librpc/gen_ndr/svcctl.h
+++ source/librpc/gen_ndr/svcctl.h
@@ -134,6 +134,14 @@ struct svcctl_LockServiceDatabase {
 
 struct svcctl_QueryServiceObjectSecurity {
 	struct {
+		struct policy_handle *handle;/* [ref] */
+		uint32_t security_flags;
+		uint32_t buffer_size;/* [range(0,0x40000)] */
+	} in;
+
+	struct {
+		uint8_t *buffer;/* [ref,size_is(buffer_size)] */
+		uint32_t *needed;/* [ref,range(0,0x40000)] */
 		WERROR result;
 	} out;
 
