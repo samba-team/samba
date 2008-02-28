bdebe9dc2a9dfeb922fe8827f97cc68d468e390d
Günther Deschner <gd@samba.org>
2008-02-28 11:12:33 +0100

    Re-run make idl.
    Guenther
    (This used to be commit 5895a03fd600745ec897d987910abca83d79de3e)

--- source3/librpc/gen_ndr/libnet_join.h
+++ source3/librpc/gen_ndr/libnet_join.h
@@ -63,6 +63,8 @@ struct libnet_UnjoinCtx {
 		const char * dns_domain_name;
 		uint8_t modified_config;
 		const char * error_string;
+		uint8_t disabled_machine_account;
+		uint8_t deleted_machine_account;
 		WERROR result;
 	} out;
 
