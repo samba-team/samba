93580bce833453ba512ee436d6dfdbdcd2c53777
Derrell Lipman <derrell.lipman@unwireduniverse.com>
2008-03-02 16:21:48 -0500

    Remove use of deprecated function

--- examples/libsmbclient/smbwrapper/smbw.c
+++ examples/libsmbclient/smbwrapper/smbw.c
@@ -176,9 +176,9 @@ static void do_init(StartupType startupType)
         
         smbc_setDebug(smbw_ctx, debug_level);
         smbc_setFunctionAuthData(smbw_ctx, get_auth_data_fn);
-        smbc_option_set(smbw_ctx, "browse_max_lmb_count", 0);
-        smbc_option_set(smbw_ctx, "urlencode_readdir_entries", 1);
-        smbc_option_set(smbw_ctx, "one_share_per_server", 1);
+        smbc_setOptionBrowseMaxLmbCount(smbw_ctx, 0);
+        smbc_setOptionUrlEncodeReaddirEntries(smbw_ctx, 1);
+        smbc_setOptionOneSharePerServer(smbw_ctx, 1);
         
         if (smbc_init_context(smbw_ctx) == NULL) {
                 fprintf(stderr, "Could not initialize context.\n");
