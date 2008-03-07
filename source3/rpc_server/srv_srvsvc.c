ad3a1b8475bc850578f0ee6a71747b204066340e
Günther Deschner <gd@samba.org>
2008-03-07 23:26:00 +0100

    Use pidl for _srvsvc_NetShareDel and _srvsvc_NetShareDelSticky.
    Guenther
    (This used to be commit 73b6587493d9a987a691ece495db9f42ce562efe)

--- source3/rpc_server/srv_srvsvc.c
+++ source3/rpc_server/srv_srvsvc.c
@@ -246,28 +246,7 @@ static bool api_srv_net_share_add(pipes_struct *p)
 
 static bool api_srv_net_share_del(pipes_struct *p)
 {
-	SRV_Q_NET_SHARE_DEL q_u;
-	SRV_R_NET_SHARE_DEL r_u;
-	prs_struct *data = &p->in_data.data;
-	prs_struct *rdata = &p->out_data.rdata;
-
-	ZERO_STRUCT(q_u);
-	ZERO_STRUCT(r_u);
-
-	/* Unmarshall the net server del info. */
-	if(!srv_io_q_net_share_del("", &q_u, data, 0)) {
-		DEBUG(0,("api_srv_net_share_del: Failed to unmarshall SRV_Q_NET_SHARE_DEL.\n"));
-		return False;
-	}
-
-	r_u.status = _srv_net_share_del(p, &q_u, &r_u);
-
-	if(!srv_io_r_net_share_del("", &r_u, rdata, 0)) {
-		DEBUG(0,("api_srv_net_share_del: Failed to marshall SRV_R_NET_SHARE_DEL.\n"));
-		return False;
-	}
-
-	return True;
+	return proxy_srvsvc_call(p, NDR_SRVSVC_NETSHAREDEL);
 }
 
 /*******************************************************************
@@ -276,28 +255,7 @@ static bool api_srv_net_share_del(pipes_struct *p)
 
 static bool api_srv_net_share_del_sticky(pipes_struct *p)
 {
-	SRV_Q_NET_SHARE_DEL q_u;
-	SRV_R_NET_SHARE_DEL r_u;
-	prs_struct *data = &p->in_data.data;
-	prs_struct *rdata = &p->out_data.rdata;
-
-	ZERO_STRUCT(q_u);
-	ZERO_STRUCT(r_u);
-
-	/* Unmarshall the net server del info. */
-	if(!srv_io_q_net_share_del("", &q_u, data, 0)) {
-		DEBUG(0,("api_srv_net_share_del_sticky: Failed to unmarshall SRV_Q_NET_SHARE_DEL.\n"));
-		return False;
-	}
-
-	r_u.status = _srv_net_share_del_sticky(p, &q_u, &r_u);
-
-	if(!srv_io_r_net_share_del("", &r_u, rdata, 0)) {
-		DEBUG(0,("api_srv_net_share_del_sticky: Failed to marshall SRV_R_NET_SHARE_DEL.\n"));
-		return False;
-	}
-
-	return True;
+	return proxy_srvsvc_call(p, NDR_SRVSVC_NETSHAREDELSTICKY);
 }
 
 /*******************************************************************
