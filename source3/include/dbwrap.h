541b8dec4e21e0a88ccc0d85bc01433b66eb3588
Volker Lendecke <vl@samba.org>
2008-03-10 10:17:05 +0100

    Add transactions to the dbwrap API
    Only filled in for tdb so far, for rbt it's pointless, and ctdb itself needs to
    be extended
    (This used to be commit 0a55e018dd68af06d84332d54148bbfb0b510b22)

--- source3/include/dbwrap.h
+++ source3/include/dbwrap.h
@@ -42,6 +42,9 @@ struct db_context {
 				      void *private_data),
 			     void *private_data);
 	int (*get_seqnum)(struct db_context *db);
+	int (*transaction_start)(struct db_context *db);
+	int (*transaction_commit)(struct db_context *db);
+	int (*transaction_cancel)(struct db_context *db);
 	void *private_data;
 	bool persistent;
 };
