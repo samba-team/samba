8099df79b624e9a4e36352a25a73f1f08b2d9b0c
Michael Adam <obnox@samba.org>
2008-03-19 12:37:17 +0100

    libsmbconf: put the smbconf context struct into a private header.
    Michael
    (cherry picked from commit a99ee5e536eee1563c90e5d7f251bfe9f5d1ffbb)

---
+++ source/lib/smbconf/smbconf_private.h
@@ -0,0 +1,27 @@
+/*
+ *  Unix SMB/CIFS implementation.
+ *  libsmbconf - Samba configuration library
+ *  Copyright (C) Michael Adam 2008
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __LIBSMBCONF_PRIVATE_H__
+#define __LIBSMBCONF_PRIVATE_H__
+
+struct smbconf_ctx {
+	NT_USER_TOKEN *token;
+};
+
+#endif
