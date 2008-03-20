427c60e92c79734b09bdf00aa807c558fdd1a029
Volker Lendecke <vl@samba.org>
2008-03-20 12:49:13 +0100

    Fix some warnings in the tsmsm module
    Alexander, please check!
    (This used to be commit df574fd2ee58f008b93b06f4d78c85cb909cc92c)

--- source3/modules/vfs_tsmsm.c
+++ source3/modules/vfs_tsmsm.c
@@ -148,8 +148,9 @@ static bool tsmsm_is_offline(struct vfs_handle_struct *handle,
         /* if the file has more than FILE_IS_ONLINE_RATIO of blocks available,
 	   then assume it is not offline (it may not be 100%, as it could be sparse) */
 	if (512 * (off_t)stbuf->st_blocks >= stbuf->st_size * tsmd->online_ratio) {
-		DEBUG(10,("%s not offline: st_blocks=%ld st_size=%ld online_ratio=%.2f\n", 
-			  path, stbuf->st_blocks, stbuf->st_size, tsmd->online_ratio));
+		DEBUG(10,("%s not offline: st_blocks=%ld st_size=%ld "
+			  "online_ratio=%.2f\n", path, (long)stbuf->st_blocks,
+			  (long)stbuf->st_size, tsmd->online_ratio));
 		return false;
 	}
 
@@ -226,8 +227,9 @@ static bool tsmsm_aio_force(struct vfs_handle_struct *handle, struct files_struc
 	   if the file might be offline
 	*/
 	if(SMB_VFS_FSTAT(fsp, &sbuf) == 0) {
-		DEBUG(10,("tsmsm_aio_force st_blocks=%ld st_size=%ld online_ratio=%.2f\n", 
-			  sbuf.st_blocks, sbuf.st_size, tsmd->online_ratio));
+		DEBUG(10,("tsmsm_aio_force st_blocks=%ld st_size=%ld "
+			  "online_ratio=%.2f\n", (long)sbuf.st_blocks,
+			  (long)sbuf.st_size, tsmd->online_ratio));
 		return !(512 * (off_t)sbuf.st_blocks >= sbuf.st_size * tsmd->online_ratio);
 	}
 	return false;
