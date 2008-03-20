8fbcccc4be4476a25307c1d288bf7755f0f71719
Michael Adam <obnox@samba.org>
2008-03-20 18:40:09 +0100

    libsmbconf: add open and close operations to the smbconf_ops.
    Note: currently, reg_init_smbconf opens the registry, but does
    not close it. This has to be changed. so that it is closed.
    And then libsmbconf will need these open/close functions
    
    Michael
    (cherry picked from commit 77dbdf82efa60c8a7c00e489c198775b2f66e56c)

--- source/lib/smbconf/smbconf_private.h
+++ source/lib/smbconf/smbconf_private.h
@@ -22,6 +22,8 @@
 
 struct smbconf_ops {
 	WERROR (*init)(struct smbconf_ctx *ctx);
+	WERROR (*open_conf)(struct smbconf_ctx *ctx);
+	int (*close_conf)(struct smbconf_ctx *ctx);
 	void (*get_csn)(struct smbconf_ctx *ctx, struct smbconf_csn *csn,
 			const char *service, const char *param);
 	WERROR (*drop)(struct smbconf_ctx *ctx);
