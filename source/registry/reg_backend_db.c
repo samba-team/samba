c1dd4a2caa4bf9d785401942ec83e9cd796e47df
Michael Adam <obnox@samba.org>
2008-03-20 14:35:41 +0100

    registry: use available string constant in debug statement.
    Michael
    (cherry picked from commit 6bac03a2ec6351ab9db457d904b8925c828086e8)

--- source/registry/reg_backend_db.c
+++ source/registry/reg_backend_db.c
@@ -342,7 +342,7 @@ bool regdb_init(void)
 
 	if ( vers_id != REGVER_V1 ) {
 		/* any upgrade code here if needed */
-		DEBUG(10, ("regdb_init: got INFO/version = %d != %d\n",
+		DEBUG(10, ("regdb_init: got %s = %d != %d\n", vstring,
 			   vers_id, REGVER_V1));
 		if (dbwrap_store_int32(regdb, vstring, REGVER_V1) != 0) {
 			DEBUG(0, ("regdb_init: error storing %s = %d\n",
