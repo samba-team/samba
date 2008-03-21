08213f706ed600efc6afa5f4bb2cb7e603ee193e
Günther Deschner <gd@samba.org>
2008-03-21 03:51:37 +0100

    Use pidl for _srvsvc_NetSessEnum().
    Guenther
    (This used to be commit 5d14938b14f8e59608771b1899fd473d63f3bde1)

--- source3/rpc_server/srv_srvsvc.c
+++ source3/rpc_server/srv_srvsvc.c
@@ -88,26 +88,7 @@ static bool api_srv_net_conn_enum(pipes_struct *p)
 
 static bool api_srv_net_sess_enum(pipes_struct *p)
 {
-	SRV_Q_NET_SESS_ENUM q_u;
-	SRV_R_NET_SESS_ENUM r_u;
-	prs_struct *data = &p->in_data.data;
-	prs_struct *rdata = &p->out_data.rdata;
-
-	ZERO_STRUCT(q_u);
-	ZERO_STRUCT(r_u);
-
-	/* grab the net server get enum */
-	if (!srv_io_q_net_sess_enum("", &q_u, data, 0))
-		return False;
-
-	/* construct reply.  always indicate success */
-	r_u.status = _srv_net_sess_enum(p, &q_u, &r_u);
-
-	/* store the response in the SMB stream */
-	if (!srv_io_r_net_sess_enum("", &r_u, rdata, 0))
-		return False;
-
-	return True;
+	return proxy_srvsvc_call(p, NDR_SRVSVC_NETSESSENUM);
 }
 
 /*******************************************************************
