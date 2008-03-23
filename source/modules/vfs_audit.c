1f475337c5cdf27862912264e4455b7a407897fe
Volker Lendecke <vl@samba.org>
2008-03-23 15:51:52 +0100

    Fix Coverity warnings
    ID 449, 448, 447, 446, 445, 444, 443, 442, 441, 440, 439, 438, 437
    (cherry picked from commit 0d50b4ee9068d04d1d9eb7786e08775d7ba16bc8)

--- source/modules/vfs_audit.c
+++ source/modules/vfs_audit.c
@@ -111,7 +111,11 @@ static int audit_syslog_priority(vfs_handle_struct *handle)
 
 	int priority;
 
-	priority = lp_parm_enum(SNUM(handle->conn), "audit", "priority", enum_log_priorities, LOG_NOTICE);
+	priority = lp_parm_enum(SNUM(handle->conn), "audit", "priority",
+				enum_log_priorities, LOG_NOTICE);
+	if (priority == -1) {
+		priority = LOG_WARNING;
+	}
 
 	return priority;
 }
