ecd4be81f0a7c2a19f2e7e7cb1d6cfd817daf23a
Love Hörnquist Åstrand <lha@kth.se>
2008-03-24 12:05:34 +0000

    set KRB5_GC_CANONICALIZE.
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@22710 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/krb5/krb5.h
+++ lib/krb5/krb5.h
@@ -363,6 +363,7 @@ typedef union {
 #define KRB5_GC_FORWARDABLE		(1U << 4)
 #define KRB5_GC_NO_TRANSIT_CHECK	(1U << 5)
 #define KRB5_GC_CONSTRAINED_DELEGATION	(1U << 6)
+#define KRB5_GC_CANONICALIZE		(1U << 7)
 
 /* constants for compare_creds (and cc_retrieve_cred) */
 #define KRB5_TC_DONT_MATCH_REALM	(1U << 31)
