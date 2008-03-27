225a9852eeee66f22a4135a660f18b70666c1c5f
Stefan Metzmacher <metze@samba.org>
2008-03-27 10:11:48 +0100

    opendb_tdb: enable oplocks per default
    metze

--- source/ntvfs/common/opendb_tdb.c
+++ source/ntvfs/common/opendb_tdb.c
@@ -102,7 +102,7 @@ static struct odb_context *odb_tdb_init(TALLOC_CTX *mem_ctx,
 	odb->ntvfs_ctx = ntvfs_ctx;
 
 	/* leave oplocks disabled by default until the code is working */
-	odb->oplocks = lp_parm_bool(ntvfs_ctx->lp_ctx, NULL, "opendb", "oplocks", false);
+	odb->oplocks = lp_parm_bool(ntvfs_ctx->lp_ctx, NULL, "opendb", "oplocks", true);
 
 	odb->lease_ctx = sys_lease_context_create(ntvfs_ctx->config, odb,
 						  ntvfs_ctx->event_ctx,
