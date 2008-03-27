e8b5e60014beb080158ba113b164a8fb10304119
Günther Deschner <gd@samba.org>
2008-03-27 11:04:01 +0100

    Re-run make idl.
    Guenther
    (cherry picked from commit e16ca32b8e0485ceafee6a2ff62dc88e96329ec4)

--- source/librpc/gen_ndr/netlogon.h
+++ source/librpc/gen_ndr/netlogon.h
@@ -1071,7 +1071,7 @@ struct netr_GetAnyDCName {
 struct netr_LogonControl2 {
 	struct {
 		const char *logon_server;/* [unique,charset(UTF16)] */
-		uint32_t function_code;
+		enum netr_LogonControlCode function_code;
 		uint32_t level;
 		union netr_CONTROL_DATA_INFORMATION *data;/* [ref,switch_is(function_code)] */
 	} in;
