0ea02e8126d3984da53da2d8f1557556f7c7ff1e
Andrew Kroeger <andrew@sprocks.gotdns.com>
2008-03-28 22:53:01 -0500

    mkrelease: Add checks to ensure run from top-level directory of repository. (This used to be commit 7d4c02fdfc9ca1a48a0a1e4672765636d4c4a717)

--- source4/script/mkrelease.sh
+++ source4/script/mkrelease.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+if [ ! -d ".git" -o `dirname $0` != "./source/script" ]; then
+	echo "Run this script from the top-level directory in the"
+	echo "repository as: ./source/script/mkrelease.sh"
+	exit 1
+fi
+
 TMPDIR=`mktemp -d samba-XXXXX`
 (git archive --format=tar HEAD | (cd $TMPDIR/ && tar xf -))
 
