0e371cf169e9a607fcbb3e65437ab9413935dd52
Jelmer Vernooij <jelmer@samba.org>
2008-04-02 16:47:17 +0200

    Reintroduce header previously autogenerated but ignored by git.
    Also fixed extra include in regpatch.

--- source/lib/util/util_ldb.h
+++ source/lib/util/util_ldb.h
@@ -1,19 +1,21 @@
 #ifndef __LIB_UTIL_UTIL_LDB_H__
 #define __LIB_UTIL_UTIL_LDB_H__
 
+/* The following definitions come from lib/util/util_ldb.c  */
+
 int gendb_search_v(struct ldb_context *ldb,
 		   TALLOC_CTX *mem_ctx,
 		   struct ldb_dn *basedn,
 		   struct ldb_message ***msgs,
 		   const char * const *attrs,
 		   const char *format,
-		   va_list ap)  _PRINTF_ATTRIBUTE(6,0);
+		   va_list ap)  PRINTF_ATTRIBUTE(6,0);
 int gendb_search(struct ldb_context *ldb,
 		 TALLOC_CTX *mem_ctx,
 		 struct ldb_dn *basedn,
 		 struct ldb_message ***res,
 		 const char * const *attrs,
-		 const char *format, ...) _PRINTF_ATTRIBUTE(6,7);
+		 const char *format, ...) PRINTF_ATTRIBUTE(6,7);
 int gendb_search_dn(struct ldb_context *ldb,
 		 TALLOC_CTX *mem_ctx,
 		 struct ldb_dn *dn,
@@ -23,4 +25,3 @@ int gendb_add_ldif(struct ldb_context *ldb, const char *ldif_string);
 char *wrap_casefold(void *context, void *mem_ctx, const char *s);
 
 #endif /* __LIB_UTIL_UTIL_LDB_H__ */
-
