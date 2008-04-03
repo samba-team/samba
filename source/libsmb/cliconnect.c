7d20487f9629f1eeb6da5a07a3d6fe9bb043b943
Günther Deschner <gd@samba.org>
2008-04-04 01:44:43 +0200

    Always uppercase cli->srv_name_slash.
    Not that I think it is of any importance...
    
    Guenther
    (cherry picked from commit 352f8440c74bc22416e21783e1dc5fecf5869902)

--- source/libsmb/cliconnect.c
+++ source/libsmb/cliconnect.c
@@ -1493,6 +1493,7 @@ NTSTATUS cli_connect(struct cli_state *cli,
 
 	fstrcpy(cli->desthost, host);
 	fstr_sprintf(cli->srv_name_slash, "\\\\%s", cli->desthost);
+	strupper_m(cli->srv_name_slash);
 
 	/* allow hostnames of the form NAME#xx and do a netbios lookup */
 	if ((p = strchr(cli->desthost, '#'))) {
