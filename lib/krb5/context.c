846471a408344fcbe3132867a3aaa58f7f5c054b
Love Hörnquist Åstrand <lha@kth.se>
2008-04-07 11:46:02 +0000

    register SCACHE.
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@22811 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/krb5/context.c
+++ lib/krb5/context.c
@@ -246,6 +246,7 @@ krb5_init_context(krb5_context *context)
     krb5_cc_register(p, &krb5_acc_ops, TRUE);
     krb5_cc_register(p, &krb5_fcc_ops, TRUE);
     krb5_cc_register(p, &krb5_mcc_ops, TRUE);
+    krb5_cc_register(p, &krb5_scc_ops, TRUE);
 #ifdef HAVE_KCM
     krb5_cc_register(p, &krb5_kcm_ops, TRUE);
 #endif
