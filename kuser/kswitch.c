e7cbe09808f71270e2e5ee29f18c1a6a317e9fce
Love Hörnquist Åstrand <lha@kth.se>
2008-04-07 11:49:04 +0000

    Implement --principal.
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@22848 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- kuser/kswitch.c
+++ kuser/kswitch.c
@@ -1,34 +1,34 @@
 /*
  * Copyright (c) 2008 Kungliga Tekniska Högskolan
- * (Royal Institute of Technology, Stockholm, Sweden). 
- * All rights reserved. 
+ * (Royal Institute of Technology, Stockholm, Sweden).
+ * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without 
- * modification, are permitted provided that the following conditions 
- * are met: 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
  *
- * 1. Redistributions of source code must retain the above copyright 
- *    notice, this list of conditions and the following disclaimer. 
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
  *
- * 2. Redistributions in binary form must reproduce the above copyright 
- *    notice, this list of conditions and the following disclaimer in the 
- *    documentation and/or other materials provided with the distribution. 
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
  *
- * 3. Neither the name of the Institute nor the names of its contributors 
- *    may be used to endorse or promote products derived from this software 
- *    without specific prior written permission. 
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND 
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE 
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS 
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY 
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
- * SUCH DAMAGE. 
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
  */
 
 #include "kuser_locl.h"
@@ -52,7 +52,7 @@ static struct getargs args[] = {
       "name of credential cache", "cache" },
     { "principal",		'p', arg_string, &principal,
       "name of principal", "principal" },
-    { "version", 		0,   arg_flag, &version_flag, 
+    { "version", 		0,   arg_flag, &version_flag,
       "print version", NULL },
     { "help",			0,   arg_flag, &help_flag, NULL, NULL}
 };
@@ -67,12 +67,10 @@ usage (int ret)
 int
 main (int argc, char **argv)
 {
-    const krb5_cc_ops *ops;
     krb5_context context;
     krb5_error_code ret;
     krb5_ccache id;
     int optidx = 0;
-    char *str;
 
     setprogname (argv[0]);
 
@@ -84,7 +82,7 @@ main (int argc, char **argv)
 
     if(getarg(args, sizeof(args) / sizeof(args[0]), argc, argv, &optidx))
 	usage(1);
-    
+
     if (help_flag)
 	usage (0);
 
@@ -99,22 +97,42 @@ main (int argc, char **argv)
     if (argc != 0)
 	usage (1);
 
-    if (cache == NULL)
-	krb5_errx(context, 1, "No cache name given");
+    if (cache && principal)
+	krb5_errx(context, 1, "Both --cache and --principal given, choose one");
 
-    ops = krb5_cc_get_prefix_ops(context, type);
-    if (ops == NULL)
-	krb5_err (context, 1, 0, "krb5_cc_get_prefix_ops");
+    if (principal) {
+	krb5_principal p;
 
-    asprintf(&str, "%s:%s", ops->prefix, cache);
-    if (str == NULL)
-	krb5_errx(context, 1, "out of memory");
+	ret = krb5_parse_name(context, principal, &p);
+	if (ret)
+	    krb5_err (context, 1, ret, "krb5_parse_name: %s", principal);
 
-    ret = krb5_cc_resolve(context, str, &id);
-    if (ret)
-	krb5_err (context, 1, ret, "krb5_cc_resolve: %s", str);
+	ret = krb5_cc_cache_match(context, p, type, &id);
+	if (ret)
+	    krb5_err (context, 1, ret,
+		      "Did not find principal: %s", principal);
 
-    free(str);
+	krb5_free_principal(context, p);
+
+    } else if (cache) {
+	const krb5_cc_ops *ops;
+	char *str;
+
+	ops = krb5_cc_get_prefix_ops(context, type);
+	if (ops == NULL)
+	    krb5_err (context, 1, 0, "krb5_cc_get_prefix_ops");
+	
+	asprintf(&str, "%s:%s", ops->prefix, cache);
+	if (str == NULL)
+	    krb5_errx(context, 1, "out of memory");
+	
+	ret = krb5_cc_resolve(context, str, &id);
+	if (ret)
+	    krb5_err (context, 1, ret, "krb5_cc_resolve: %s", str);
+	
+	free(str);
+    } else
+	usage(1);
 
     ret = krb5_cc_switch(context, id);
     if (ret)
