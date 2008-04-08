14e4eb9efb62daaadea6ee18c6424eb8bd7280a7
Michael Adam <obnox@samba.org>
2008-04-08 22:23:57 +0200

    libsmbconf: more sanely print multi_sz values in registry backend
    Michael
    (cherry picked from commit 382c623948abd1c6a5cf8ab7ee2be784fcef76ee)

--- source/lib/smbconf/smbconf_reg.c
+++ source/lib/smbconf/smbconf_reg.c
@@ -337,8 +337,8 @@ static char *smbconf_format_registry_value(TALLOC_CTX *mem_ctx,
 	case REG_MULTI_SZ: {
 		uint32 j;
 		for (j = 0; j < value->v.multi_sz.num_strings; j++) {
-			result = talloc_asprintf(mem_ctx, "%s \"%s\" ",
-						 result,
+			result = talloc_asprintf(mem_ctx, "%s\"%s\" ",
+						 result ? result : "" ,
 						 value->v.multi_sz.strings[j]);
 			if (result == NULL) {
 				break;
