aae4d91e726ef8dcad173cdd1d6f719d94462948
Günther Deschner <gd@samba.org>
2008-04-08 19:54:57 +0200

    Move is_valid_policy_hnd() out of net.
    Guenther

--- source/lib/util.c
+++ source/lib/util.c
@@ -3367,3 +3367,11 @@ NTSTATUS split_ntfs_stream_name(TALLOC_CTX *mem_ctx, const char *fname,
 	}
 	return NT_STATUS_OK;
 }
+
+bool is_valid_policy_hnd(const POLICY_HND *hnd)
+{
+	POLICY_HND tmp;
+	ZERO_STRUCT(tmp);
+	return (memcmp(&tmp, hnd, sizeof(tmp)) != 0);
+}
+
