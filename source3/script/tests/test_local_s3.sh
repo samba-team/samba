c575c85d587284eaf0afde269f6d4c2ae61423ee
Michael Adam <obnox@samba.org>
2008-04-11 10:57:50 +0200

    test: not only pretend to call smbconftort - really do it :-}
    Michael
    (This used to be commit e0947537e61cdc7aaaf1256a7f9a590f006cb3d9)

--- source3/script/tests/test_local_s3.sh
+++ source3/script/tests/test_local_s3.sh
@@ -23,7 +23,7 @@ testit "replacetort" $VALGRIND $BINDIR/replacetort || \
 testit "tdbtorture" $VALGRIND $BINDIR/tdbtorture || \
     failed=`expr $failed + 1`
 
-testit "smbconftort" $VALGRIND $BINDIR/tdbtorture $CONFIGURATION || \
+testit "smbconftort" $VALGRIND $BINDIR/smbconftort $CONFIGURATION || \
 	failed=`expr $failed + 1`
 
 testok $0 $failed
