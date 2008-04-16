b659e83cd6b627dd0ae04064bccff7220a5bd1ce
Jelmer Vernooij <jelmer@samba.org>
2008-04-17 01:37:42 +0200

    Add convenience function for getting at event context from ejs code.

--- source/scripting/ejs/smbcalls_auth.c
+++ source/scripting/ejs/smbcalls_auth.c
@@ -55,7 +55,7 @@ static int ejs_doauth(MprVarHandle eid,
 		msg = c->msg_ctx;
 	} else {
 		/* Hope we can find the event context somewhere up there... */
-		ev = event_context_find(tmp_ctx);
+		ev = mprEventCtx();
 		msg = messaging_client_init(tmp_ctx, lp_messaging_path(tmp_ctx, mprLpCtx()), 
 					    lp_iconv_convenience(mprLpCtx()), ev);
 	}
