66c0f331a231ea8897bd8f83658c86b1d2c85d62
Stefan Metzmacher <metze@samba.org>
2008-04-16 15:15:57 +0200

    torture/smb2: fix whitespaces
    metze

--- source/torture/smb2/smb2.c
+++ source/torture/smb2/smb2.c
@@ -47,9 +47,9 @@ static bool wrap_simple_1smb2_test(struct torture_context *torture_ctx,
 }
 
 struct torture_test *torture_suite_add_1smb2_test(struct torture_suite *suite,
-							   const char *name,
-							   bool (*run) (struct torture_context *,
-									struct smb2_tree *))
+						  const char *name,
+						  bool (*run)(struct torture_context *,
+							      struct smb2_tree *))
 {
 	struct torture_test *test; 
 	struct torture_tcase *tcase;
