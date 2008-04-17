194d5014d8c07a786acf94063a5c4b227f0b7d4b
Jelmer Vernooij <jelmer@samba.org>
2008-04-17 13:51:00 +0200

    Add some comments. (This used to be commit fd321fd77fcb9fbebcaa724c58a1beee606e87cd)

--- source3/librpc/rpc/dcerpc.h
+++ source3/librpc/rpc/dcerpc.h
@@ -33,9 +33,16 @@
 struct loadparm_context;
 struct cli_credentials;
 
+/**
+ * Connection to a particular DCE/RPC interface.
+ */
 struct dcerpc_pipe {
 	const struct ndr_interface_table *table;
+
+	/** SMB context used when transport is ncacn_np. */
 	struct cli_state *cli;
+
+	/** Samba 3 DCE/RPC client context. */
 	struct rpc_pipe_client *rpc_cli;
 };
 
@@ -53,7 +60,7 @@ enum dcerpc_transport_t {
 	NCADG_UNIX_DGRAM, NCACN_HTTP, NCADG_IPX, NCACN_SPX };
 
 
-/* this describes a binding to a particular transport/pipe */
+/** this describes a binding to a particular transport/pipe */
 struct dcerpc_binding {
 	enum dcerpc_transport_t transport;
 	struct ndr_syntax_id object;
@@ -64,8 +71,5 @@ struct dcerpc_binding {
 	uint32_t flags;
 	uint32_t assoc_group_id;
 };
-
-
-
 
 #endif /* __DCERPC_H__ */
