4d8f3f190215edcdbeb1725cccdc962dc68cc1a0
Andrew Bartlett <abartlet@samba.org>
2008-04-17 12:03:49 +0200

    Fix the expectations on the unixinfo test.
    Andrew Bartlett
    (This used to be commit 0df2b3e0b56007850cf83cfdcdb45ca29e162d34)

--- source4/scripting/python/samba/tests/dcerpc/unix.py
+++ source4/scripting/python/samba/tests/dcerpc/unix.py
@@ -27,8 +27,8 @@ class UnixinfoTests(RpcInterfaceTestCase):
     def test_getpwuid(self):
         infos = self.conn.GetPWUid(range(512))
         self.assertEquals(512, len(infos))
-        self.assertEquals("", infos[0].shell)
-        self.assertEquals("", infos[0].homedir)
+        self.assertEquals("/bin/false", infos[0].shell)
+        self.assertTrue(isinstance(infos[0].homedir, unicode))
 
     def test_gidtosid(self):
         self.conn.GidToSid(1000)
