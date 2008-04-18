08f83c59df7a0e490da55a536b6e04a32508d2b0
Stefan Metzmacher <metze@samba.org>
2008-04-18 22:20:49 +0200

    build: fix the build rule for .o files
    metze
    (This used to be commit c7f34f41c3f9f0c3f75a618dfaf566706014a6b4)

--- source4/build/make/rules.mk
+++ source4/build/make/rules.mk
@@ -211,7 +211,9 @@ include/includes.d: include/includes.h
 	@echo "Compiling $<"
 	@-mkdir -p `dirname $@`
 	@$(COMPILE) && exit 0 ; \
-	$(COMPILE) 
+		echo "The following command failed:" 1>&2;\
+		echo "$(COMPILE)" 1>&2;\
+		$(COMPILE) >/dev/null 2>&1
 
 
 
