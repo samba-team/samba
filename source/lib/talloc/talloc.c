14264b246406cdec728d124ade5a47e463d6677b
Volker Lendecke <vl@samba.org>
2008-04-19 16:56:44 +0200

    Fix bug 5400
    Thanks to Jason Mader!
    
    Volker
    (cherry picked from commit 87d8a63ce4e6dd91ea3193d0a2574520a5857be2)

--- source/lib/talloc/talloc.c
+++ source/lib/talloc/talloc.c
@@ -85,8 +85,8 @@
 #define likely(x)   __builtin_expect(!!(x), 1)
 #define unlikely(x) __builtin_expect(!!(x), 0)
 #else
-#define likely(x) x
-#define unlikely(x) x
+#define likely(x) (x)
+#define unlikely(x) (x)
 #endif
 
 /* this null_context is only used if talloc_enable_leak_report() or
