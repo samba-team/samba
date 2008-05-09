701b856c523acab3ca01b7d41329198aac85a20b
Jelmer Vernooij <jelmer@samba.org>
2008-05-09 13:27:06 +0200

    Fix COM headers.

--- source4/lib/com/config.mk
+++ source4/lib/com/config.mk
@@ -1,10 +1,8 @@
 [SUBSYSTEM::COM]
-PRIVATE_PROTO_HEADER = proto.h
 
 COM_OBJ_FILES = $(addprefix lib/com/, tables.o rot.o main.o)
 
 [SUBSYSTEM::DCOM]
-PRIVATE_PROTO_HEADER = dcom/proto.h
 PUBLIC_DEPENDENCIES = com DCOM_PROXY_DCOM RPC_NDR_REMACT \
 					  RPC_NDR_OXIDRESOLVER
 
