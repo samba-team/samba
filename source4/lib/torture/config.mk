d0096e90ba9d8aa6b339022bac194e57ae0a2b7a
Jelmer Vernooij <jelmer@samba.org>
2008-05-10 00:46:25 +0200

    Fix installation of pidl perl files, torture pc file. (This used to be commit 32da606e3759026c2744f853cd8948333ed0579c)

--- source4/lib/torture/config.mk
+++ source4/lib/torture/config.mk
@@ -5,6 +5,9 @@ PUBLIC_DEPENDENCIES = \
 		LIBSAMBA-UTIL \
 		LIBTALLOC
 
+TORTURE_VERSION = 0.0.1
+TORTURE_SO_VERSION = 0
+
 PC_FILES += lib/torture/torture.pc
 torture_OBJ_FILES = $(addprefix lib/torture/, torture.o)
 
