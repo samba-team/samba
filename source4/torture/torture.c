670122d724ff190d886a8eca86b6eeff64592708
Jelmer Vernooij <jelmer@samba.org>
2008-05-10 23:46:51 +0200

    Fix torture module initializion. (This used to be commit d5bf4e9065303b54421a6f35b213a22f062d27d4)

--- source4/torture/torture.c
+++ source4/torture/torture.c
@@ -59,7 +59,7 @@ _PUBLIC_ int torture_init(void)
 	extern NTSTATUS torture_raw_init(void);
 	extern NTSTATUS torture_unix_init(void);
 	extern NTSTATUS torture_winbind_init(void);
-	init_module_fn static_init[] = { STATIC_torture_MODULES };
+	init_module_fn static_init[] = { STATIC_smbtorture_MODULES };
 	init_module_fn *shared_init = load_samba_modules(NULL, cmdline_lp_ctx, "torture");
 
 	run_init_functions(static_init);
