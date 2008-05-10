bc4eacb5d89ca8dca123e2d6bd6eba47ed93b771
Jelmer Vernooij <jelmer@samba.org>
2008-05-10 14:26:08 +0200

    Fix case. (This used to be commit a90971ea0c43ce3b42b95aef5973139576b51959)

--- source4/lib/torture/config.mk
+++ source4/lib/torture/config.mk
@@ -5,8 +5,8 @@ PUBLIC_DEPENDENCIES = \
 		LIBSAMBA-UTIL \
 		LIBTALLOC
 
-TORTURE_VERSION = 0.0.1
-TORTURE_SO_VERSION = 0
+torture_VERSION = 0.0.1
+torture_SO_VERSION = 0
 
 PC_FILES += lib/torture/torture.pc
 torture_OBJ_FILES = $(addprefix lib/torture/, torture.o)
