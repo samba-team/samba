63f2b66f0e1120a516d3dfcd03b38db2e4aaddd0
Jelmer Vernooij <jelmer@samba.org>
2008-05-18 19:10:37 +0200

    Use src dir.

--- source/scripting/python/config.mk
+++ source/scripting/python/config.mk
@@ -3,26 +3,26 @@ PUBLIC_DEPENDENCIES = EXT_LIB_PYTHON
 PRIVATE_DEPENDENCIES = PYTALLOC
 INIT_FUNCTION_SENTINEL = { NULL, NULL }
 
-LIBPYTHON_OBJ_FILES = $(addprefix $(pyscriptdir)/, modules.o)
+LIBPYTHON_OBJ_FILES = $(addprefix $(pyscriptsrcdir)/, modules.o)
 
 [SUBSYSTEM::PYTALLOC]
 PUBLIC_DEPENDENCIES = EXT_LIB_PYTHON
 
-PYTALLOC_OBJ_FILES = $(addprefix $(pyscriptdir)/, pytalloc.o)
+PYTALLOC_OBJ_FILES = $(addprefix $(pyscriptsrcdir)/, pytalloc.o)
 
 [PYTHON::python_uuid]
 PRIVATE_DEPENDENCIES = LIBNDR 
 
-python_uuid_OBJ_FILES = $(pyscriptdir)/uuidmodule.o
+python_uuid_OBJ_FILES = $(pyscriptsrcdir)/uuidmodule.o
 
 [PYTHON::python_misc]
 PRIVATE_DEPENDENCIES = LIBNDR LIBLDB SAMDB CREDENTIALS
 SWIG_FILE = misc.i
 
-python_misc_OBJ_FILES = $(pyscriptdir)/misc_wrap.o
+python_misc_OBJ_FILES = $(pyscriptsrcdir)/misc_wrap.o
 
-_PY_FILES = $(shell find $(pyscriptdir) -name "*.py")
+_PY_FILES = $(shell find $(pyscriptsrcdir) -name "*.py")
 
-$(foreach pyfile, $(_PY_FILES),$(eval $(call python_py_module_template,$(patsubst $(pyscriptdir)/%,%,$(pyfile)),$(pyfile))))
+$(foreach pyfile, $(_PY_FILES),$(eval $(call python_py_module_template,$(patsubst $(pyscriptsrcdir)/%,%,$(pyfile)),$(pyfile))))
 
 install:: installpython
