dc2e8823d21ce8d6ba4b8a083badd270133fdc43
Volker Lendecke <vl@samba.org>
2008-05-19 04:20:56 +0200

    If no node number is given, default to the current node in smbcontrol (This used to be commit b4b3b6b1c6a336220c6afd68b153a769397ecded)

--- source3/lib/util.c
+++ source3/lib/util.c
@@ -3074,7 +3074,7 @@ struct server_id interpret_pid(const char *pid_string)
 		result.pid = pid;
 	}
 	else if (sscanf(pid_string, "%u", &pid) == 1) {
-		result.vnn = NONCLUSTER_VNN;
+		result.vnn = get_my_vnn();
 		result.pid = pid;
 	}
 	else {
