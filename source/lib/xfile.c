9c5c3eadf5aaf9821c0ebe1d2bb533eff22aab63
Jeremy Allison <jra@samba.org>
2008-05-21 13:02:40 -0700

    Don't error out on xflush if there's no buffer. Jeremy.

--- source/lib/xfile.c
+++ source/lib/xfile.c
@@ -256,14 +256,14 @@ int x_fflush(XFILE *f)
 {
 	int ret;
 
+	if (f->bufused == 0 || !f->buf) return 0;
+
 	if (f->flags & X_FLAG_ERROR) return -1;
 
 	if ((f->open_flags & O_ACCMODE) != O_WRONLY) {
 		errno = EINVAL;
 		return -1;
 	}
-
-	if (f->bufused == 0 || !f->buf) return 0;
 
 	ret = write(f->fd, f->buf, f->bufused);
 	if (ret == -1) return -1;
