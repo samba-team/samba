1e897b6148407e9921edd2c3db734930d79ccb42
Andrew Bartlett <abartlet@samba.org>
2008-05-21 14:28:50 +1000

    Include flags.h into samdb.h
    These flags are too useful just to hide away...
    
    Andrew Bartlett

--- source/dsdb/samdb/samdb.h
+++ source/dsdb/samdb/samdb.h
@@ -37,6 +37,7 @@ struct event_context;
 #include "dsdb/schema/schema.h"
 #include "dsdb/samdb/samdb_proto.h"
 #include "dsdb/common/proto.h"
+#include "dsdb/common/flags.h"
 
 #define DSDB_CONTROL_CURRENT_PARTITION_OID "1.3.6.1.4.1.7165.4.3.2"
 struct dsdb_control_current_partition {
