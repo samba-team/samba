d77745e6920038f3b6c7c25fa6cd6549e1c24d17
Jelmer Vernooij <jelmer@samba.org>
2008-05-22 02:21:25 +0200

    Move DCE/RPC python bindings into samba package. (This used to be commit 8dafd4ce40b7348fd5f2b1f2f5d2106ef4cc0dd1)

--- source4/scripting/python/samba/tests/dcerpc/registry.py
+++ source4/scripting/python/samba/tests/dcerpc/registry.py
@@ -17,7 +17,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-from dcerpc import winreg
+from samba.dcerpc import winreg
 import unittest
 from samba.tests import RpcInterfaceTestCase
 
