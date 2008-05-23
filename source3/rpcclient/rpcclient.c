0eae576b194a8e37a117b0bc4528e1ba4c4848f1
Günther Deschner <gd@samba.org>
2008-05-24 00:58:45 +0200

    drsuapi: fix the build.
    Guenther
    (This used to be commit b2421c81164da2a5ea4bb2391e4f2c410938db42)

--- source3/rpcclient/rpcclient.c
+++ source3/rpcclient/rpcclient.c
@@ -506,6 +506,7 @@ extern struct cmd_set shutdown_commands[];
 extern struct cmd_set test_commands[];
 extern struct cmd_set wkssvc_commands[];
 extern struct cmd_set ntsvcs_commands[];
+extern struct cmd_set drsuapi_commands[];
 
 static struct cmd_set *rpcclient_command_list[] = {
 	rpcclient_commands,
@@ -521,6 +522,7 @@ static struct cmd_set *rpcclient_command_list[] = {
  	test_commands,
 	wkssvc_commands,
 	ntsvcs_commands,
+	drsuapi_commands,
 	NULL
 };
 
