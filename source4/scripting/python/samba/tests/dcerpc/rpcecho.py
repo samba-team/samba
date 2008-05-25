928ecbaebbde00515d08fd530db7c99169c905ef
Jelmer Vernooij <jelmer@samba.org>
2008-05-25 04:23:03 +0200

    Add support for secondary contexts from Python. (This used to be commit 16d1ad050546ae6500153438db8d3c857e6f3ad5)

--- source4/scripting/python/samba/tests/dcerpc/rpcecho.py
+++ source4/scripting/python/samba/tests/dcerpc/rpcecho.py
@@ -26,6 +26,10 @@ class RpcEchoTests(RpcInterfaceTestCase):
     def setUp(self):
         self.conn = echo.rpcecho("ncalrpc:", self.get_loadparm())
 
+    def test_two_contexts(self):
+        self.conn2 = echo.rpcecho("ncalrpc", basis_connection=self.conn)
+        self.assertEquals(3, self.conn2.AddOne(2))
+
     def test_addone(self):
         self.assertEquals(2, self.conn.AddOne(1))
 
