9861cae1aebdef41f098df71be4e5a33f6af9bf1
Jelmer Vernooij <jelmer@samba.org>
2008-05-26 13:36:05 +0200

    remove unused credentials ejs module.

--- source/scripting/ejs/config.mk
+++ source/scripting/ejs/config.mk
@@ -71,7 +71,6 @@ PRIVATE_DEPENDENCIES = \
 smbcalls_OBJ_FILES = $(addprefix $(ejsscriptsrcdir)/, \
 		smbcalls.o \
 		smbcalls_options.o \
-		smbcalls_creds.o \
 		smbcalls_param.o \
 		mprutil.o \
 		literal.o)
