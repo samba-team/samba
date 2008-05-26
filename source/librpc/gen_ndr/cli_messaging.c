samba-team/samba

commit a67e96393fbf3a27f089ba0cf8050fa212680000

Re-run make idl.
Karolin

source/librpc/gen_ndr/cli_messaging.c
@@ -1,0 +1,8 @@
+/*
+ * Unix SMB/CIFS implementation.
+ * client auto-generated by pidl. DO NOT MODIFY!
+ */
+
+#include "includes.h"
+#include "librpc/gen_ndr/cli_messaging.h"
+

source/librpc/gen_ndr/cli_dfs.c
@@ -8,7 +8,7 @@
 
 NTSTATUS rpccli_dfs_GetManagerVersion(struct rpc_pipe_client *cli,
 				      TALLOC_CTX *mem_ctx,
-				      enum dfs_ManagerVersion *version)
+				      enum dfs_ManagerVersion *version /* [out] [ref] */)
 {
 	struct dfs_GetManagerVersion r;
 	NTSTATUS status;
@@ -47,11 +47,11 @@ NTSTATUS rpccli_dfs_GetManagerVersion(struct rpc_pipe_client *cli,
 
 NTSTATUS rpccli_dfs_Add(struct rpc_pipe_client *cli,
 			TALLOC_CTX *mem_ctx,
-			const char *path,
-			const char *server,
-			const char *share,
-			const char *comment,
-			uint32_t flags,
+			const char *path /* [in] [ref,charset(UTF16)] */,
+			const char *server /* [in] [ref,charset(UTF16)] */,
+			const char *share /* [in] [unique,charset(UTF16)] */,
+			const char *comment /* [in] [unique,charset(UTF16)] */,
+			uint32_t flags /* [in]  */,
 			WERROR *werror)
 {
 	struct dfs_Add r;
@@ -99,9 +99,9 @@ NTSTATUS rpccli_dfs_Add(struct rpc_pipe_client *cli,
 
 NTSTATUS rpccli_dfs_Remove(struct rpc_pipe_client *cli,
 			   TALLOC_CTX *mem_ctx,
-			   const char *dfs_entry_path,
-			   const char *servername,
-			   const char *sharename,
+			   const char *dfs_entry_path /* [in] [ref,charset(UTF16)] */,
+			   const char *servername /* [in] [unique,charset(UTF16)] */,
+			   const char *sharename /* [in] [unique,charset(UTF16)] */,
 			   WERROR *werror)
 {
 	struct dfs_Remove r;
@@ -147,11 +147,11 @@ NTSTATUS rpccli_dfs_Remove(struct rpc_pipe_client *cli,
 
 NTSTATUS rpccli_dfs_SetInfo(struct rpc_pipe_client *cli,
 			    TALLOC_CTX *mem_ctx,
-			    const char *dfs_entry_path,
-			    const char *servername,
-			    const char *sharename,
-			    uint32_t level,
-			    union dfs_Info *info,
+			    const char *dfs_entry_path /* [in] [charset(UTF16)] */,
+			    const char *servername /* [in] [unique,charset(UTF16)] */,
+			    const char *sharename /* [in] [unique,charset(UTF16)] */,
+			    uint32_t level /* [in]  */,
+			    union dfs_Info *info /* [in] [ref,switch_is(level)] */,
 			    WERROR *werror)
 {
 	struct dfs_SetInfo r;
@@ -199,11 +199,11 @@ NTSTATUS rpccli_dfs_SetInfo(struct rpc_pipe_client *cli,
 
 NTSTATUS rpccli_dfs_GetInfo(struct rpc_pipe_client *cli,
 			    TALLOC_CTX *mem_ctx,
-			    const char *dfs_entry_path,
-			    const char *servername,
-			    const char *sharename,
-			    uint32_t level,
-			    union dfs_Info *info,
+			    const char *dfs_entry_path /* [in] [charset(UTF16)] */,
+			    const char *servername /* [in] [unique,charset(UTF16)] */,
+			    const char *sharename /* [in] [unique,charset(UTF16)] */,
+			    uint32_t level /* [in]  */,
+			    union dfs_Info *info /* [out] [ref,switch_is(level)] */,
 			    WERROR *werror)
 {
 	struct dfs_GetInfo r;
@@ -251,10 +251,10 @@ NTSTATUS rpccli_dfs_GetInfo(struct rpc_pipe_client *cli,
 
 NTSTATUS rpccli_dfs_Enum(struct rpc_pipe_client *cli,
 			 TALLOC_CTX *mem_ctx,
-			 uint32_t level,
-			 uint32_t bufsize,
-			 struct dfs_EnumStruct *info,
-			 uint32_t *total,
+			 uint32_t level /* [in]  */,
+			 uint32_t bufsize /* [in]  */,
+			 struct dfs_EnumStruct *info /* [in,out] [unique] */,
+			 uint32_t *total /* [in,out] [unique] */,
 			 WERROR *werror)
 {
 	struct dfs_Enum r;
@@ -475,15 +475,15 @@ NTSTATUS rpccli_dfs_ManagerSendSiteInfo(struct rpc_pipe_client *cli,
 
 NTSTATUS rpccli_dfs_AddFtRoot(struct rpc_pipe_client *cli,
 			      TALLOC_CTX *mem_ctx,
-			      const char *servername,
-			      const char *dns_servername,
-			      const char *dfsname,
-			      const char *rootshare,
-			      const char *comment,
-			      const char *dfs_config_dn,
-			      uint8_t unknown1,
-			      uint32_t flags,
-			      struct dfs_UnknownStruct **unknown2,
+			      const char *servername /* [in] [charset(UTF16)] */,
+			      const char *dns_servername /* [in] [charset(UTF16)] */,
+			      const char *dfsname /* [in] [charset(UTF16)] */,
+			      const char *rootshare /* [in] [charset(UTF16)] */,
+			      const char *comment /* [in] [charset(UTF16)] */,
+			      const char *dfs_config_dn /* [in] [charset(UTF16)] */,
+			      uint8_t unknown1 /* [in]  */,
+			      uint32_t flags /* [in]  */,
+			      struct dfs_UnknownStruct **unknown2 /* [in,out] [unique] */,
 			      WERROR *werror)
 {
 	struct dfs_AddFtRoot r;
@@ -538,12 +538,12 @@ NTSTATUS rpccli_dfs_AddFtRoot(struct rpc_pipe_client *cli,
 
 NTSTATUS rpccli_dfs_RemoveFtRoot(struct rpc_pipe_client *cli,
 				 TALLOC_CTX *mem_ctx,
-				 const char *servername,
-				 const char *dns_servername,
-				 const char *dfsname,
-				 const char *rootshare,
-				 uint32_t flags,
-				 struct dfs_UnknownStruct **unknown,
+				 const char *servername /* [in] [charset(UTF16)] */,
+				 const char *dns_servername /* [in] [charset(UTF16)] */,
+				 const char *dfsname /* [in] [charset(UTF16)] */,
+				 const char *rootshare /* [in] [charset(UTF16)] */,
+				 uint32_t flags /* [in]  */,
+				 struct dfs_UnknownStruct **unknown /* [in,out] [unique] */,
 				 WERROR *werror)
 {
 	struct dfs_RemoveFtRoot r;
@@ -595,10 +595,10 @@ NTSTATUS rpccli_dfs_RemoveFtRoot(struct rpc_pipe_client *cli,
 
 NTSTATUS rpccli_dfs_AddStdRoot(struct rpc_pipe_client *cli,
 			       TALLOC_CTX *mem_ctx,
-			       const char *servername,
-			       const char *rootshare,
-			       const char *comment,
-			       uint32_t flags,
+			       const char *servername /* [in] [charset(UTF16)] */,
+			       const char *rootshare /* [in] [charset(UTF16)] */,
+			       const char *comment /* [in] [charset(UTF16)] */,
+			       uint32_t flags /* [in]  */,
 			       WERROR *werror)
 {
 	struct dfs_AddStdRoot r;
@@ -645,9 +645,9 @@ NTSTATUS rpccli_dfs_AddStdRoot(struct rpc_pipe_client *cli,
 
 NTSTATUS rpccli_dfs_RemoveStdRoot(struct rpc_pipe_client *cli,
 				  TALLOC_CTX *mem_ctx,
-				  const char *servername,
-				  const char *rootshare,
-				  uint32_t flags,
+				  const char *servername /* [in] [charset(UTF16)] */,
+				  const char *rootshare /* [in] [charset(UTF16)] */,
+				  uint32_t flags /* [in]  */,
 				  WERROR *werror)
 {
 	struct dfs_RemoveStdRoot r;
@@ -693,8 +693,8 @@ NTSTATUS rpccli_dfs_RemoveStdRoot(struct rpc_pipe_client *cli,
 
 NTSTATUS rpccli_dfs_ManagerInitialize(struct rpc_pipe_client *cli,
 				      TALLOC_CTX *mem_ctx,
-				      const char *servername,
-				      uint32_t flags,
+				      const char *servername /* [in] [ref,charset(UTF16)] */,
+				      uint32_t flags /* [in]  */,
 				      WERROR *werror)
 {
 	struct dfs_ManagerInitialize r;
@@ -739,10 +739,10 @@ NTSTATUS rpccli_dfs_ManagerInitialize(struct rpc_pipe_client *cli,
 
 NTSTATUS rpccli_dfs_AddStdRootForced(struct rpc_pipe_client *cli,
 				     TALLOC_CTX *mem_ctx,
-				     const char *servername,
-				     const char *rootshare,
-				     const char *comment,
-				     const char *store,
+				     const char *servername /* [in] [charset(UTF16)] */,
+				     const char *rootshare /* [in] [charset(UTF16)] */,
+				     const char *comment /* [in] [charset(UTF16)] */,
+				     const char *store /* [in] [charset(UTF16)] */,
 				     WERROR *werror)
 {
 	struct dfs_AddStdRootForced r;
@@ -789,10 +789,10 @@ NTSTATUS rpccli_dfs_AddStdRootForced(struct rpc_pipe_client *cli,
 
 NTSTATUS rpccli_dfs_GetDcAddress(struct rpc_pipe_client *cli,
 				 TALLOC_CTX *mem_ctx,
-				 const char *servername,
-				 const char **server_fullname,
-				 uint8_t *is_root,
-				 uint32_t *ttl,
+				 const char *servername /* [in] [charset(UTF16)] */,
+				 const char **server_fullname /* [in,out] [ref,charset(UTF16)] */,
+				 uint8_t *is_root /* [in,out] [ref] */,
+				 uint32_t *ttl /* [in,out] [ref] */,
 				 WERROR *werror)
 {
 	struct dfs_GetDcAddress r;
@@ -842,10 +842,10 @@ NTSTATUS rpccli_dfs_GetDcAddress(struct rpc_pipe_client *cli,
 
 NTSTATUS rpccli_dfs_SetDcAddress(struct rpc_pipe_client *cli,
 				 TALLOC_CTX *mem_ctx,
-				 const char *servername,
-				 const char *server_fullname,
-				 uint32_t flags,
-				 uint32_t ttl,
+				 const char *servername /* [in] [charset(UTF16)] */,
+				 const char *server_fullname /* [in] [charset(UTF16)] */,
+				 uint32_t flags /* [in]  */,
+				 uint32_t ttl /* [in]  */,
 				 WERROR *werror)
 {
 	struct dfs_SetDcAddress r;
@@ -892,8 +892,8 @@ NTSTATUS rpccli_dfs_SetDcAddress(struct rpc_pipe_client *cli,
 
 NTSTATUS rpccli_dfs_FlushFtTable(struct rpc_pipe_client *cli,
 				 TALLOC_CTX *mem_ctx,
-				 const char *servername,
-				 const char *rootshare,
+				 const char *servername /* [in] [charset(UTF16)] */,
+				 const char *rootshare /* [in] [charset(UTF16)] */,
 				 WERROR *werror)
 {
 	struct dfs_FlushFtTable r;
@@ -1022,11 +1022,11 @@ NTSTATUS rpccli_dfs_Remove2(struct rpc_pipe_client *cli,
 
 NTSTATUS rpccli_dfs_EnumEx(struct rpc_pipe_client *cli,
 			   TALLOC_CTX *mem_ctx,
-			   const char *dfs_name,
-			   uint32_t level,
-			   uint32_t bufsize,
-			   struct dfs_EnumStruct *info,
-			   uint32_t *total,
+			   const char *dfs_name /* [in] [charset(UTF16)] */,
+			   uint32_t level /* [in]  */,
+			   uint32_t bufsize /* [in]  */,
+			   struct dfs_EnumStruct *info /* [in,out] [unique] */,
+			   uint32_t *total /* [in,out] [unique] */,
 			   WERROR *werror)
 {
 	struct dfs_EnumEx r;

source/librpc/gen_ndr/cli_dfs.h
@@ -3,43 +3,43 @@
 #define __CLI_NETDFS__
 NTSTATUS rpccli_dfs_GetManagerVersion(struct rpc_pipe_client *cli,
 				      TALLOC_CTX *mem_ctx,
-				      enum dfs_ManagerVersion *version);
+				      enum dfs_ManagerVersion *version /* [out] [ref] */);
 NTSTATUS rpccli_dfs_Add(struct rpc_pipe_client *cli,
 			TALLOC_CTX *mem_ctx,
-			const char *path,
-			const char *server,
-			const char *share,
-			const char *comment,
-			uint32_t flags,
+			const char *path /* [in] [ref,charset(UTF16)] */,
+			const char *server /* [in] [ref,charset(UTF16)] */,
+			const char *share /* [in] [unique,charset(UTF16)] */,
+			const char *comment /* [in] [unique,charset(UTF16)] */,
+			uint32_t flags /* [in]  */,
 			WERROR *werror);
 NTSTATUS rpccli_dfs_Remove(struct rpc_pipe_client *cli,
 			   TALLOC_CTX *mem_ctx,
-			   const char *dfs_entry_path,
-			   const char *servername,
-			   const char *sharename,
+			   const char *dfs_entry_path /* [in] [ref,charset(UTF16)] */,
+			   const char *servername /* [in] [unique,charset(UTF16)] */,
+			   const char *sharename /* [in] [unique,charset(UTF16)] */,
 			   WERROR *werror);
 NTSTATUS rpccli_dfs_SetInfo(struct rpc_pipe_client *cli,
 			    TALLOC_CTX *mem_ctx,
-			    const char *dfs_entry_path,
-			    const char *servername,
-			    const char *sharename,
-			    uint32_t level,
-			    union dfs_Info *info,
+			    const char *dfs_entry_path /* [in] [charset(UTF16)] */,
+			    const char *servername /* [in] [unique,charset(UTF16)] */,
+			    const char *sharename /* [in] [unique,charset(UTF16)] */,
+			    uint32_t level /* [in]  */,
+			    union dfs_Info *info /* [in] [ref,switch_is(level)] */,
 			    WERROR *werror);
 NTSTATUS rpccli_dfs_GetInfo(struct rpc_pipe_client *cli,
 			    TALLOC_CTX *mem_ctx,
-			    const char *dfs_entry_path,
-			    const char *servername,
-			    const char *sharename,
-			    uint32_t level,
-			    union dfs_Info *info,
+			    const char *dfs_entry_path /* [in] [charset(UTF16)] */,
+			    const char *servername /* [in] [unique,charset(UTF16)] */,
+			    const char *sharename /* [in] [unique,charset(UTF16)] */,
+			    uint32_t level /* [in]  */,
+			    union dfs_Info *info /* [out] [ref,switch_is(level)] */,
 			    WERROR *werror);
 NTSTATUS rpccli_dfs_Enum(struct rpc_pipe_client *cli,
 			 TALLOC_CTX *mem_ctx,
-			 uint32_t level,
-			 uint32_t bufsize,
-			 struct dfs_EnumStruct *info,
-			 uint32_t *total,
+			 uint32_t level /* [in]  */,
+			 uint32_t bufsize /* [in]  */,
+			 struct dfs_EnumStruct *info /* [in,out] [unique] */,
+			 uint32_t *total /* [in,out] [unique] */,
 			 WERROR *werror);
 NTSTATUS rpccli_dfs_Rename(struct rpc_pipe_client *cli,
 			   TALLOC_CTX *mem_ctx,
@@ -55,68 +55,68 @@ NTSTATUS rpccli_dfs_ManagerSendSiteInfo(struct rpc_pipe_client *cli,
 					WERROR *werror);
 NTSTATUS rpccli_dfs_AddFtRoot(struct rpc_pipe_client *cli,
 			      TALLOC_CTX *mem_ctx,
-			      const char *servername,
-			      const char *dns_servername,
-			      const char *dfsname,
-			      const char *rootshare,
-			      const char *comment,
-			      const char *dfs_config_dn,
-			      uint8_t unknown1,
-			      uint32_t flags,
-			      struct dfs_UnknownStruct **unknown2,
+			      const char *servername /* [in] [charset(UTF16)] */,
+			      const char *dns_servername /* [in] [charset(UTF16)] */,
+			      const char *dfsname /* [in] [charset(UTF16)] */,
+			      const char *rootshare /* [in] [charset(UTF16)] */,
+			      const char *comment /* [in] [charset(UTF16)] */,
+			      const char *dfs_config_dn /* [in] [charset(UTF16)] */,
+			      uint8_t unknown1 /* [in]  */,
+			      uint32_t flags /* [in]  */,
+			      struct dfs_UnknownStruct **unknown2 /* [in,out] [unique] */,
 			      WERROR *werror);
 NTSTATUS rpccli_dfs_RemoveFtRoot(struct rpc_pipe_client *cli,
 				 TALLOC_CTX *mem_ctx,
-				 const char *servername,
-				 const char *dns_servername,
-				 const char *dfsname,
-				 const char *rootshare,
-				 uint32_t flags,
-				 struct dfs_UnknownStruct **unknown,
+				 const char *servername /* [in] [charset(UTF16)] */,
+				 const char *dns_servername /* [in] [charset(UTF16)] */,
+				 const char *dfsname /* [in] [charset(UTF16)] */,
+				 const char *rootshare /* [in] [charset(UTF16)] */,
+				 uint32_t flags /* [in]  */,
+				 struct dfs_UnknownStruct **unknown /* [in,out] [unique] */,
 				 WERROR *werror);
 NTSTATUS rpccli_dfs_AddStdRoot(struct rpc_pipe_client *cli,
 			       TALLOC_CTX *mem_ctx,
-			       const char *servername,
-			       const char *rootshare,
-			       const char *comment,
-			       uint32_t flags,
+			       const char *servername /* [in] [charset(UTF16)] */,
+			       const char *rootshare /* [in] [charset(UTF16)] */,
+			       const char *comment /* [in] [charset(UTF16)] */,
+			       uint32_t flags /* [in]  */,
 			       WERROR *werror);
 NTSTATUS rpccli_dfs_RemoveStdRoot(struct rpc_pipe_client *cli,
 				  TALLOC_CTX *mem_ctx,
-				  const char *servername,
-				  const char *rootshare,
-				  uint32_t flags,
+				  const char *servername /* [in] [charset(UTF16)] */,
+				  const char *rootshare /* [in] [charset(UTF16)] */,
+				  uint32_t flags /* [in]  */,
 				  WERROR *werror);
 NTSTATUS rpccli_dfs_ManagerInitialize(struct rpc_pipe_client *cli,
 				      TALLOC_CTX *mem_ctx,
-				      const char *servername,
-				      uint32_t flags,
+				      const char *servername /* [in] [ref,charset(UTF16)] */,
+				      uint32_t flags /* [in]  */,
 				      WERROR *werror);
 NTSTATUS rpccli_dfs_AddStdRootForced(struct rpc_pipe_client *cli,
 				     TALLOC_CTX *mem_ctx,
-				     const char *servername,
-				     const char *rootshare,
-				     const char *comment,
-				     const char *store,
+				     const char *servername /* [in] [charset(UTF16)] */,
+				     const char *rootshare /* [in] [charset(UTF16)] */,
+				     const char *comment /* [in] [charset(UTF16)] */,
+				     const char *store /* [in] [charset(UTF16)] */,
 				     WERROR *werror);
 NTSTATUS rpccli_dfs_GetDcAddress(struct rpc_pipe_client *cli,
 				 TALLOC_CTX *mem_ctx,
-				 const char *servername,
-				 const char **server_fullname,
-				 uint8_t *is_root,
-				 uint32_t *ttl,
+				 const char *servername /* [in] [charset(UTF16)] */,
+				 const char **server_fullname /* [in,out] [ref,charset(UTF16)] */,
+				 uint8_t *is_root /* [in,out] [ref] */,
+				 uint32_t *ttl /* [in,out] [ref] */,
 				 WERROR *werror);
 NTSTATUS rpccli_dfs_SetDcAddress(struct rpc_pipe_client *cli,
 				 TALLOC_CTX *mem_ctx,
-				 const char *servername,
-				 const char *server_fullname,
-				 uint32_t flags,
-				 uint32_t ttl,
+				 const char *servername /* [in] [charset(UTF16)] */,
+				 const char *server_fullname /* [in] [charset(UTF16)] */,
+				 uint32_t flags /* [in]  */,
+				 uint32_t ttl /* [in]  */,
 				 WERROR *werror);
 NTSTATUS rpccli_dfs_FlushFtTable(struct rpc_pipe_client *cli,
 				 TALLOC_CTX *mem_ctx,
-				 const char *servername,
-				 const char *rootshare,
+				 const char *servername /* [in] [charset(UTF16)] */,
+				 const char *rootshare /* [in] [charset(UTF16)] */,
 				 WERROR *werror);
 NTSTATUS rpccli_dfs_Add2(struct rpc_pipe_client *cli,
 			 TALLOC_CTX *mem_ctx,
@@ -126,11 +126,11 @@ NTSTATUS rpccli_dfs_Remove2(struct rpc_pipe_client *cli,
 			    WERROR *werror);
 NTSTATUS rpccli_dfs_EnumEx(struct rpc_pipe_client *cli,
 			   TALLOC_CTX *mem_ctx,
-			   const char *dfs_name,
-			   uint32_t level,
-			   uint32_t bufsize,
-			   struct dfs_EnumStruct *info,
-			   uint32_t *total,
+			   const char *dfs_name /* [in] [charset(UTF16)] */,
+			   uint32_t level /* [in]  */,
+			   uint32_t bufsize /* [in]  */,
+			   struct dfs_EnumStruct *info /* [in,out] [unique] */,
+			   uint32_t *total /* [in,out] [unique] */,
 			   WERROR *werror);
 NTSTATUS rpccli_dfs_SetInfo2(struct rpc_pipe_client *cli,
 			     TALLOC_CTX *mem_ctx,

source/librpc/gen_ndr/cli_dssetup.c
@@ -8,8 +8,8 @@
 
 NTSTATUS rpccli_dssetup_DsRoleGetPrimaryDomainInformation(struct rpc_pipe_client *cli,
 							  TALLOC_CTX *mem_ctx,
-							  enum dssetup_DsRoleInfoLevel level,
-							  union dssetup_DsRoleInfo *info,
+							  enum dssetup_DsRoleInfoLevel level /* [in]  */,
+							  union dssetup_DsRoleInfo *info /* [out] [unique,switch_is(level)] */,
 							  WERROR *werror)
 {
 	struct dssetup_DsRoleGetPrimaryDomainInformation r;

source/librpc/gen_ndr/cli_dssetup.h
@@ -3,8 +3,8 @@
 #define __CLI_DSSETUP__
 NTSTATUS rpccli_dssetup_DsRoleGetPrimaryDomainInformation(struct rpc_pipe_client *cli,
 							  TALLOC_CTX *mem_ctx,
-							  enum dssetup_DsRoleInfoLevel level,
-							  union dssetup_DsRoleInfo *info,
+							  enum dssetup_DsRoleInfoLevel level /* [in]  */,
+							  union dssetup_DsRoleInfo *info /* [out] [unique,switch_is(level)] */,
 							  WERROR *werror);
 NTSTATUS rpccli_dssetup_DsRoleDnsNameToFlatName(struct rpc_pipe_client *cli,
 						TALLOC_CTX *mem_ctx,

source/librpc/gen_ndr/cli_echo.c
@@ -8,8 +8,8 @@
 
 NTSTATUS rpccli_echo_AddOne(struct rpc_pipe_client *cli,
 			    TALLOC_CTX *mem_ctx,
-			    uint32_t in_data,
-			    uint32_t *out_data)
+			    uint32_t in_data /* [in]  */,
+			    uint32_t *out_data /* [out] [ref] */)
 {
 	struct echo_AddOne r;
 	NTSTATUS status;
@@ -49,9 +49,9 @@ NTSTATUS rpccli_echo_AddOne(struct rpc_pipe_client *cli,
 
 NTSTATUS rpccli_echo_EchoData(struct rpc_pipe_client *cli,
 			      TALLOC_CTX *mem_ctx,
-			      uint32_t len,
-			      uint8_t *in_data,
-			      uint8_t *out_data)
+			      uint32_t len /* [in]  */,
+			      uint8_t *in_data /* [in] [size_is(len)] */,
+			      uint8_t *out_data /* [out] [size_is(len)] */)
 {
 	struct echo_EchoData r;
 	NTSTATUS status;
@@ -92,8 +92,8 @@ NTSTATUS rpccli_echo_EchoData(struct rpc_pipe_client *cli,
 
 NTSTATUS rpccli_echo_SinkData(struct rpc_pipe_client *cli,
 			      TALLOC_CTX *mem_ctx,
-			      uint32_t len,
-			      uint8_t *data)
+			      uint32_t len /* [in]  */,
+			      uint8_t *data /* [in] [size_is(len)] */)
 {
 	struct echo_SinkData r;
 	NTSTATUS status;
@@ -133,8 +133,8 @@ NTSTATUS rpccli_echo_SinkData(struct rpc_pipe_client *cli,
 
 NTSTATUS rpccli_echo_SourceData(struct rpc_pipe_client *cli,
 				TALLOC_CTX *mem_ctx,
-				uint32_t len,
-				uint8_t *data)
+				uint32_t len /* [in]  */,
+				uint8_t *data /* [out] [size_is(len)] */)
 {
 	struct echo_SourceData r;
 	NTSTATUS status;
@@ -174,8 +174,8 @@ NTSTATUS rpccli_echo_SourceData(struct rpc_pipe_client *cli,
 
 NTSTATUS rpccli_echo_TestCall(struct rpc_pipe_client *cli,
 			      TALLOC_CTX *mem_ctx,
-			      const char *s1,
-			      const char **s2)
+			      const char *s1 /* [in] [ref,charset(UTF16)] */,
+			      const char **s2 /* [out] [ref,charset(UTF16)] */)
 {
 	struct echo_TestCall r;
 	NTSTATUS status;
@@ -215,8 +215,8 @@ NTSTATUS rpccli_echo_TestCall(struct rpc_pipe_client *cli,
 
 NTSTATUS rpccli_echo_TestCall2(struct rpc_pipe_client *cli,
 			       TALLOC_CTX *mem_ctx,
-			       uint16_t level,
-			       union echo_Info *info)
+			       uint16_t level /* [in]  */,
+			       union echo_Info *info /* [out] [ref,switch_is(level)] */)
 {
 	struct echo_TestCall2 r;
 	NTSTATUS status;
@@ -256,7 +256,7 @@ NTSTATUS rpccli_echo_TestCall2(struct rpc_pipe_client *cli,
 
 NTSTATUS rpccli_echo_TestSleep(struct rpc_pipe_client *cli,
 			       TALLOC_CTX *mem_ctx,
-			       uint32_t seconds)
+			       uint32_t seconds /* [in]  */)
 {
 	struct echo_TestSleep r;
 	NTSTATUS status;
@@ -295,9 +295,9 @@ NTSTATUS rpccli_echo_TestSleep(struct rpc_pipe_client *cli,
 
 NTSTATUS rpccli_echo_TestEnum(struct rpc_pipe_client *cli,
 			      TALLOC_CTX *mem_ctx,
-			      enum echo_Enum1 *foo1,
-			      struct echo_Enum2 *foo2,
-			      union echo_Enum3 *foo3)
+			      enum echo_Enum1 *foo1 /* [in,out] [ref] */,
+			      struct echo_Enum2 *foo2 /* [in,out] [ref] */,
+			      union echo_Enum3 *foo3 /* [in,out] [ref,switch_is(*foo1)] */)
 {
 	struct echo_TestEnum r;
 	NTSTATUS status;
@@ -341,7 +341,7 @@ NTSTATUS rpccli_echo_TestEnum(struct rpc_pipe_client *cli,
 
 NTSTATUS rpccli_echo_TestSurrounding(struct rpc_pipe_client *cli,
 				     TALLOC_CTX *mem_ctx,
-				     struct echo_Surrounding *data)
+				     struct echo_Surrounding *data /* [in,out] [ref] */)
 {
 	struct echo_TestSurrounding r;
 	NTSTATUS status;
@@ -381,7 +381,7 @@ NTSTATUS rpccli_echo_TestSurrounding(struct rpc_pipe_client *cli,
 
 NTSTATUS rpccli_echo_TestDoublePointer(struct rpc_pipe_client *cli,
 				       TALLOC_CTX *mem_ctx,
-				       uint16_t ***data)
+				       uint16_t ***data /* [in] [ref] */)
 {
 	struct echo_TestDoublePointer r;
 	NTSTATUS status;

source/librpc/gen_ndr/cli_echo.h
@@ -3,41 +3,41 @@
 #define __CLI_RPCECHO__
 NTSTATUS rpccli_echo_AddOne(struct rpc_pipe_client *cli,
 			    TALLOC_CTX *mem_ctx,
-			    uint32_t in_data,
-			    uint32_t *out_data);
+			    uint32_t in_data /* [in]  */,
+			    uint32_t *out_data /* [out] [ref] */);
 NTSTATUS rpccli_echo_EchoData(struct rpc_pipe_client *cli,
 			      TALLOC_CTX *mem_ctx,
-			      uint32_t len,
-			      uint8_t *in_data,
-			      uint8_t *out_data);
+			      uint32_t len /* [in]  */,
+			      uint8_t *in_data /* [in] [size_is(len)] */,
+			      uint8_t *out_data /* [out] [size_is(len)] */);
 NTSTATUS rpccli_echo_SinkData(struct rpc_pipe_client *cli,
 			      TALLOC_CTX *mem_ctx,
-			      uint32_t len,
-			      uint8_t *data);
+			      uint32_t len /* [in]  */,
+			      uint8_t *data /* [in] [size_is(len)] */);
 NTSTATUS rpccli_echo_SourceData(struct rpc_pipe_client *cli,
 				TALLOC_CTX *mem_ctx,
-				uint32_t len,
-				uint8_t *data);
+				uint32_t len /* [in]  */,
+				uint8_t *data /* [out] [size_is(len)] */);
 NTSTATUS rpccli_echo_TestCall(struct rpc_pipe_client *cli,
 			      TALLOC_CTX *mem_ctx,
-			      const char *s1,
-			      const char **s2);
+			      const char *s1 /* [in] [ref,charset(UTF16)] */,
+			      const char **s2 /* [out] [ref,charset(UTF16)] */);
 NTSTATUS rpccli_echo_TestCall2(struct rpc_pipe_client *cli,
 			       TALLOC_CTX *mem_ctx,
-			       uint16_t level,
-			       union echo_Info *info);
+			       uint16_t level /* [in]  */,
+			       union echo_Info *info /* [out] [ref,switch_is(level)] */);
 NTSTATUS rpccli_echo_TestSleep(struct rpc_pipe_client *cli,
 			       TALLOC_CTX *mem_ctx,
-			       uint32_t seconds);
+			       uint32_t seconds /* [in]  */);
 NTSTATUS rpccli_echo_TestEnum(struct rpc_pipe_client *cli,
 			      TALLOC_CTX *mem_ctx,
-			      enum echo_Enum1 *foo1,
-			      struct echo_Enum2 *foo2,
-			      union echo_Enum3 *foo3);
+			      enum echo_Enum1 *foo1 /* [in,out] [ref] */,
+			      struct echo_Enum2 *foo2 /* [in,out] [ref] */,
+			      union echo_Enum3 *foo3 /* [in,out] [ref,switch_is(*foo1)] */);
 NTSTATUS rpccli_echo_TestSurrounding(struct rpc_pipe_client *cli,
 				     TALLOC_CTX *mem_ctx,
-				     struct echo_Surrounding *data);
+				     struct echo_Surrounding *data /* [in,out] [ref] */);
 NTSTATUS rpccli_echo_TestDoublePointer(struct rpc_pipe_client *cli,
 				       TALLOC_CTX *mem_ctx,
-				       uint16_t ***data);
+				       uint16_t ***data /* [in] [ref] */);
 #endif /* __CLI_RPCECHO__ */

source/librpc/gen_ndr/cli_epmapper.c
@@ -8,9 +8,9 @@
 
 NTSTATUS rpccli_epm_Insert(struct rpc_pipe_client *cli,
 			   TALLOC_CTX *mem_ctx,
-			   uint32_t num_ents,
-			   struct epm_entry_t *entries,
-			   uint32_t replace)
+			   uint32_t num_ents /* [in]  */,
+			   struct epm_entry_t *entries /* [in] [size_is(num_ents)] */,
+			   uint32_t replace /* [in]  */)
 {
 	struct epm_Insert r;
 	NTSTATUS status;
@@ -51,8 +51,8 @@ NTSTATUS rpccli_epm_Insert(struct rpc_pipe_client *cli,
 
 NTSTATUS rpccli_epm_Delete(struct rpc_pipe_client *cli,
 			   TALLOC_CTX *mem_ctx,
-			   uint32_t num_ents,
-			   struct epm_entry_t *entries)
+			   uint32_t num_ents /* [in]  */,
+			   struct epm_entry_t *entries /* [in] [size_is(num_ents)] */)
 {
 	struct epm_Delete r;
 	NTSTATUS status;
@@ -92,14 +92,14 @@ NTSTATUS rpccli_epm_Delete(struct rpc_pipe_client *cli,
 
 NTSTATUS rpccli_epm_Lookup(struct rpc_pipe_client *cli,
 			   TALLOC_CTX *mem_ctx,
-			   uint32_t inquiry_type,
-			   struct GUID *object,
-			   struct rpc_if_id_t *interface_id,
-			   uint32_t vers_option,
-			   struct policy_handle *entry_handle,
-			   uint32_t max_ents,
-			   uint32_t *num_ents,
-			   struct epm_entry_t *entries)
+			   uint32_t inquiry_type /* [in]  */,
+			   struct GUID *object /* [in] [ptr] */,
+			   struct rpc_if_id_t *interface_id /* [in] [ptr] */,
+			   uint32_t vers_option /* [in]  */,
+			   struct policy_handle *entry_handle /* [in,out] [ref] */,
+			   uint32_t max_ents /* [in]  */,
+			   uint32_t *num_ents /* [out] [ref] */,
+			   struct epm_entry_t *entries /* [out] [length_is(*num_ents),size_is(max_ents)] */)
 {
 	struct epm_Lookup r;
 	NTSTATUS status;
@@ -146,12 +146,12 @@ NTSTATUS rpccli_epm_Lookup(struct rpc_pipe_client *cli,
 
 NTSTATUS rpccli_epm_Map(struct rpc_pipe_client *cli,
 			TALLOC_CTX *mem_ctx,
-			struct GUID *object,
-			struct epm_twr_t *map_tower,
-			struct policy_handle *entry_handle,
-			uint32_t max_towers,
-			uint32_t *num_towers,
-			struct epm_twr_p_t *towers)
+			struct GUID *object /* [in] [ptr] */,
+			struct epm_twr_t *map_tower /* [in] [ptr] */,
+			struct policy_handle *entry_handle /* [in,out] [ref] */,
+			uint32_t max_towers /* [in]  */,
+			uint32_t *num_towers /* [out] [ref] */,
+			struct epm_twr_p_t *towers /* [out] [length_is(*num_towers),size_is(max_towers)] */)
 {
 	struct epm_Map r;
 	NTSTATUS status;
@@ -196,7 +196,7 @@ NTSTATUS rpccli_epm_Map(struct rpc_pipe_client *cli,
 
 NTSTATUS rpccli_epm_LookupHandleFree(struct rpc_pipe_client *cli,
 				     TALLOC_CTX *mem_ctx,
-				     struct policy_handle *entry_handle)
+				     struct policy_handle *entry_handle /* [in,out] [ref] */)
 {
 	struct epm_LookupHandleFree r;
 	NTSTATUS status;
@@ -236,7 +236,7 @@ NTSTATUS rpccli_epm_LookupHandleFree(struct rpc_pipe_client *cli,
 
 NTSTATUS rpccli_epm_InqObject(struct rpc_pipe_client *cli,
 			      TALLOC_CTX *mem_ctx,
-			      struct GUID *epm_object)
+			      struct GUID *epm_object /* [in] [ref] */)
 {
 	struct epm_InqObject r;
 	NTSTATUS status;
@@ -275,9 +275,9 @@ NTSTATUS rpccli_epm_InqObject(struct rpc_pipe_client *cli,
 
 NTSTATUS rpccli_epm_MgmtDelete(struct rpc_pipe_client *cli,
 			       TALLOC_CTX *mem_ctx,
-			       uint32_t object_speced,
-			       struct GUID *object,
-			       struct epm_twr_t *tower)
+			       uint32_t object_speced /* [in]  */,
+			       struct GUID *object /* [in] [ptr] */,
+			       struct epm_twr_t *tower /* [in] [ptr] */)
 {
 	struct epm_MgmtDelete r;
 	NTSTATUS status;

commit 0101cd1ced03b1a644d14a3abfd158fae5bfe356

Remove source/librpc/gen_ndr/cli_messaging.c.
This was commited with a67e96393fbf3a accidentally.

Karolin

source/librpc/gen_ndr/cli_messaging.c
@@ -1,8 +1,0 @@
-/*
- * Unix SMB/CIFS implementation.
- * client auto-generated by pidl. DO NOT MODIFY!
- */
-
-#include "includes.h"
-#include "librpc/gen_ndr/cli_messaging.h"
-