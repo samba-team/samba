a6d28e2b330a20e95b745966bb5eb153bdc58ed1
Andrew Tridgell <tridge@samba.org>
2008-05-30 07:28:53 +1000

    the docs shows that this is a o16s32 blob

--- source/smb_server/smb2/fileinfo.c
+++ source/smb_server/smb2/fileinfo.c
@@ -55,8 +55,7 @@ static void smb2srv_getinfo_send(struct ntvfs_request *ntvfs)
 
 	SMB2SRV_CHECK(smb2srv_setup_reply(req, 0x08, true, op->info->out.blob.length));
 
-	/* TODO: this is maybe a o16s32_blob */
-	SMB2SRV_CHECK(smb2_push_o16s16_blob(&req->out, 0x02, op->info->out.blob));
+	SMB2SRV_CHECK(smb2_push_o16s32_blob(&req->out, 0x02, op->info->out.blob));
 	SSVAL(req->out.body,	0x06,	0);
 
 	smb2srv_send_reply(req);
