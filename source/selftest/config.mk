f67f3422b0fd319a3c6d3ccb3ea444a97c3ebaae
Jelmer Vernooij <jelmer@samba.org>
2008-05-29 18:19:19 +0200

    Add samba4-slow file listing tests that succeed but are just slow.
    Added a 'make slowtest' target that does run these tests.
    
    Also added some comments in the test lists files.

--- source/selftest/config.mk
+++ source/selftest/config.mk
@@ -7,66 +7,75 @@ SELFTEST = $(LD_LIBPATH_OVERRIDE) $(PERL) $(srcdir)/selftest/selftest.pl --prefi
     --exclude=$(srcdir)/samba4-skip --testlist="./selftest/samba4_tests.sh|" \
     $(TEST_OPTIONS) 
 
-test:: everything
+SELFTEST_NOSLOW_OPTS = --exclude=$(srcdir)/samba4-slow
+SELFTEST_QUICK_OPTS = $(SELFTEST_NOSLOW_OPTS) --quick 
+
+slowtest:: everything
 	$(SELFTEST) $(DEFAULT_TEST_OPTIONS) --immediate $(TESTS)
 
+test:: everything
+	$(SELFTEST) $(SELFTEST_NOSLOW_OPTS) $(DEFAULT_TEST_OPTIONS) --immediate \
+		$(TESTS)
+
 kvmtest:: everything
-	$(SELFTEST) $(DEFAULT_TEST_OPTIONS) --immediate --target=kvm --image=$(KVM_IMAGE)
+	$(SELFTEST) $(SELFTEST_NOSLOW_OPTS) $(DEFAULT_TEST_OPTIONS) --immediate \
+		--target=kvm --image=$(KVM_IMAGE)
 
 kvmquicktest:: everything
-	$(SELFTEST) $(DEFAULT_TEST_OPTIONS) --immediate --quick --target=kvm --image=$(KVM_IMAGE)
+	$(SELFTEST) $(DEFAULT_TEST_OPTIONS) --immediate \
+		$(SELFTEST_QUICK_OPTS) --target=kvm --image=$(KVM_IMAGE)
 
 testone:: everything
-	$(SELFTEST) $(DEFAULT_TEST_OPTIONS) --one $(TESTS)
+	$(SELFTEST) $(SELFTEST_NOSLOW_OPTS) $(DEFAULT_TEST_OPTIONS) --one $(TESTS)
 
 test-swrap:: everything
-	$(SELFTEST) --socket-wrapper --immediate $(TESTS)
+	$(SELFTEST) $(SELFTEST_NOSLOW_OPTS) --socket-wrapper --immediate $(TESTS)
 
 test-swrap-pcap:: everything
-	$(SELFTEST) --socket-wrapper-pcap --immediate $(TESTS)
+	$(SELFTEST) $(SELFTEST_NOSLOW_OPTS) --socket-wrapper-pcap --immediate $(TESTS)
 
 test-swrap-keep-pcap:: everything
-	$(SELFTEST) --socket-wrapper-keep-pcap --immediate $(TESTS)
+	$(SELFTEST) $(SELFTEST_NOSLOW_OPTS) --socket-wrapper-keep-pcap --immediate $(TESTS)
 
 test-noswrap:: everything
-	$(SELFTEST) --immediate $(TESTS)
+	$(SELFTEST) $(SELFTEST_NOSLOW_OPTS) --immediate $(TESTS)
 
 quicktest:: all
-	$(SELFTEST) --quick --socket-wrapper --immediate $(TESTS)
+	$(SELFTEST) $(SELFTEST_QUICK_OPTS) --socket-wrapper --immediate $(TESTS)
 
 quicktestone:: all
-	$(SELFTEST) --quick --socket-wrapper --one $(TESTS)
+	$(SELFTEST) $(SELFTEST_QUICK_OPTS) --socket-wrapper --one $(TESTS)
 
 testenv:: everything
-	$(SELFTEST) --socket-wrapper --testenv
+	$(SELFTEST) $(SELFTEST_NOSLOW_OPTS) --socket-wrapper --testenv
 
 valgrindtest:: valgrindtest-all
 
 valgrindtest-quick:: all
 	SMBD_VALGRIND="xterm -n smbd -e $(srcdir)/script/valgrind_run $(LD_LIBPATH_OVERRIDE)" \
 	VALGRIND="valgrind -q --num-callers=30 --log-file=${selftest_prefix}/valgrind.log" \
-	$(SELFTEST) --quick --immediate --socket-wrapper $(TESTS)
+	$(SELFTEST) $(SELFTEST_QUICK_OPTS) --immediate --socket-wrapper $(TESTS)
 
 valgrindtest-all:: everything
 	SMBD_VALGRIND="xterm -n smbd -e $(srcdir)/script/valgrind_run $(LD_LIBPATH_OVERRIDE)" \
 	VALGRIND="valgrind -q --num-callers=30 --log-file=${selftest_prefix}/valgrind.log" \
-	$(SELFTEST) --immediate --socket-wrapper $(TESTS)
+	$(SELFTEST) $(SELFTEST_NOSLOW_OPTS) --immediate --socket-wrapper $(TESTS)
 
 valgrindtest-env:: everything
 	SMBD_VALGRIND="xterm -n smbd -e $(srcdir)/script/valgrind_run $(LD_LIBPATH_OVERRIDE)" \
 	VALGRIND="valgrind -q --num-callers=30 --log-file=${selftest_prefix}/valgrind.log" \
-	$(SELFTEST) --socket-wrapper --testenv
+	$(SELFTEST) $(SELFTEST_NOSLOW_OPTS) --socket-wrapper --testenv
 
 gdbtest:: gdbtest-all
 
 gdbtest-quick:: all
 	SMBD_VALGRIND="xterm -n smbd -e $(srcdir)/script/gdb_run $(LD_LIBPATH_OVERRIDE)" \
-	$(SELFTEST) --immediate --quick --socket-wrapper $(TESTS)
+	$(SELFTEST) $(SELFTEST_QUICK_OPTS) --immediate --socket-wrapper $(TESTS)
 
 gdbtest-all:: everything
 	SMBD_VALGRIND="xterm -n smbd -e $(srcdir)/script/gdb_run $(LD_LIBPATH_OVERRIDE)" \
-	$(SELFTEST) --immediate --socket-wrapper $(TESTS)
+	$(SELFTEST) $(SELFTEST_NOSLOW_OPTS) --immediate --socket-wrapper $(TESTS)
 
 gdbtest-env:: everything
 	SMBD_VALGRIND="xterm -n smbd -e $(srcdir)/script/gdb_run $(LD_LIBPATH_OVERRIDE)" \
-	$(SELFTEST) --socket-wrapper --testenv
+	$(SELFTEST) $(SELFTEST_NOSLOW_OPTS)--socket-wrapper --testenv
