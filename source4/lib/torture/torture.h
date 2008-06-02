9835b4fde17526e18237f7adfa935f13cd5eb0a7
Andrew Tridgell <tridge@samba.org>
2008-06-02 11:02:37 +1000

    fixed a warning (This used to be commit 7a8ee50105265a4da1f2b89144094f2269c6b119)

--- source4/lib/torture/torture.h
+++ source4/lib/torture/torture.h
@@ -257,7 +257,7 @@ void torture_result(struct torture_context *test,
 	do { const void *__got = (got), *__expected = (expected); \
 	if (memcmp(__got, __expected, len) != 0) { \
 		torture_result(torture_ctx, TORTURE_FAIL, \
-					   __location__": "#got" of len %d did not match"#expected": %s", len, cmt); \
+			       __location__": "#got" of len %d did not match"#expected": %s", (int)len, cmt); \
 		return false; \
 	} \
 	} while(0)
