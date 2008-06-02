f68f75a6e4b4ba8b38cac7578c65dc7361315b28
Günther Deschner <gd@samba.org>
2008-06-02 19:02:52 +0200

    netapi: Implement NetLocalGroupAdd().
    Guenther

--- source/lib/netapi/localgroup.c
+++ source/lib/netapi/localgroup.c
@@ -30,7 +30,158 @@
 WERROR NetLocalGroupAdd_r(struct libnetapi_ctx *ctx,
 			  struct NetLocalGroupAdd *r)
 {
-	return WERR_NOT_SUPPORTED;
+	struct cli_state *cli = NULL;
+	struct rpc_pipe_client *pipe_cli = NULL;
+	NTSTATUS status;
+	WERROR werr;
+	struct lsa_String lsa_account_name;
+	struct policy_handle connect_handle, domain_handle, builtin_handle, alias_handle;
+	struct samr_Ids user_rids, name_types;
+	struct dom_sid2 *domain_sid = NULL;
+	uint32_t rid;
+
+	struct LOCALGROUP_INFO_0 *info0;
+	struct LOCALGROUP_INFO_1 *info1;
+
+	const char *alias_name = NULL;
+
+	if (!r->in.buf) {
+		return WERR_INVALID_PARAM;
+	}
+
+	switch (r->in.level) {
+		case 0:
+			info0 = (struct LOCALGROUP_INFO_0 *)r->in.buf;
+			alias_name = info0->lgrpi0_name;
+			break;
+		case 1:
+			info1 = (struct LOCALGROUP_INFO_1 *)r->in.buf;
+			alias_name = info1->lgrpi1_name;
+			break;
+		default:
+			werr = WERR_UNKNOWN_LEVEL;
+			goto done;
+	}
+
+	ZERO_STRUCT(connect_handle);
+	ZERO_STRUCT(builtin_handle);
+	ZERO_STRUCT(domain_handle);
+	ZERO_STRUCT(alias_handle);
+
+	werr = libnetapi_open_ipc_connection(ctx, r->in.server_name, &cli);
+	if (!W_ERROR_IS_OK(werr)) {
+		goto done;
+	}
+
+	werr = libnetapi_open_pipe(ctx, cli, PI_SAMR, &pipe_cli);
+	if (!W_ERROR_IS_OK(werr)) {
+		goto done;
+	}
+
+	status = rpccli_try_samr_connects(pipe_cli, ctx,
+					  SAMR_ACCESS_OPEN_DOMAIN |
+					  SAMR_ACCESS_ENUM_DOMAINS,
+					  &connect_handle);
+	if (!NT_STATUS_IS_OK(status)) {
+		werr = ntstatus_to_werror(status);
+		goto done;
+	}
+
+	status = rpccli_samr_OpenDomain(pipe_cli, ctx,
+					&connect_handle,
+					SAMR_DOMAIN_ACCESS_OPEN_ACCOUNT,
+					CONST_DISCARD(DOM_SID *, &global_sid_Builtin),
+					&builtin_handle);
+	if (!NT_STATUS_IS_OK(status)) {
+		werr = ntstatus_to_werror(status);
+		goto done;
+	}
+
+	init_lsa_String(&lsa_account_name, alias_name);
+
+	status = rpccli_samr_LookupNames(pipe_cli, ctx,
+					 &builtin_handle,
+					 1,
+					 &lsa_account_name,
+					 &user_rids,
+					 &name_types);
+	if (NT_STATUS_IS_OK(status)) {
+		status = rpccli_samr_OpenAlias(pipe_cli, ctx,
+					       &builtin_handle,
+					       SAMR_ALIAS_ACCESS_LOOKUP_INFO,
+					       user_rids.ids[0],
+					       &alias_handle);
+		if (NT_STATUS_IS_OK(status)) {
+			werr = WERR_ALIAS_EXISTS;
+			goto done;
+		}
+
+	}
+
+	rpccli_samr_Close(pipe_cli, ctx, &builtin_handle);
+
+	status = libnetapi_samr_open_domain(ctx, pipe_cli,
+					    SAMR_ACCESS_ENUM_DOMAINS |
+					    SAMR_ACCESS_OPEN_DOMAIN,
+					    SAMR_DOMAIN_ACCESS_CREATE_ALIAS |
+					    SAMR_DOMAIN_ACCESS_OPEN_ACCOUNT,
+					    &connect_handle,
+					    &domain_handle,
+					    &domain_sid);
+	if (!NT_STATUS_IS_OK(status)) {
+		werr = ntstatus_to_werror(status);
+		goto done;
+	}
+
+	status = rpccli_samr_CreateDomAlias(pipe_cli, ctx,
+					    &domain_handle,
+					    &lsa_account_name,
+					    SEC_STD_DELETE |
+					    SAMR_ALIAS_ACCESS_SET_INFO,
+					    &alias_handle,
+					    &rid);
+	if (!NT_STATUS_IS_OK(status)) {
+		werr = ntstatus_to_werror(status);
+		goto done;
+	}
+
+	if (r->in.level == 1) {
+
+		union samr_AliasInfo alias_info;
+
+		init_lsa_String(&alias_info.description, info1->lgrpi1_comment);
+
+		status = rpccli_samr_SetAliasInfo(pipe_cli, ctx,
+						  &alias_handle,
+						  ALIASINFODESCRIPTION,
+						  &alias_info);
+		if (!NT_STATUS_IS_OK(status)) {
+			werr = ntstatus_to_werror(status);
+			goto done;
+		}
+	}
+
+	werr = WERR_OK;
+
+ done:
+	if (!cli) {
+		return werr;
+	}
+
+	if (is_valid_policy_hnd(&alias_handle)) {
+		rpccli_samr_Close(pipe_cli, ctx, &alias_handle);
+	}
+	if (is_valid_policy_hnd(&domain_handle)) {
+		rpccli_samr_Close(pipe_cli, ctx, &domain_handle);
+	}
+	if (is_valid_policy_hnd(&builtin_handle)) {
+		rpccli_samr_Close(pipe_cli, ctx, &builtin_handle);
+	}
+	if (is_valid_policy_hnd(&connect_handle)) {
+		rpccli_samr_Close(pipe_cli, ctx, &connect_handle);
+	}
+
+	return werr;
 }
 
 /****************************************************************
@@ -39,5 +190,5 @@ WERROR NetLocalGroupAdd_r(struct libnetapi_ctx *ctx,
 WERROR NetLocalGroupAdd_l(struct libnetapi_ctx *ctx,
 			  struct NetLocalGroupAdd *r)
 {
-	return WERR_NOT_SUPPORTED;
+	return NetLocalGroupAdd_r(ctx, r);
 }
