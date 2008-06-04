2c8904be3bac2c696712cc3160fe95d62afde43a
Stefan Metzmacher <metze@samba.org>
2008-06-04 08:54:42 +0200

    lib/crypto: remove unused sha1 and hmac-sha1 code
    metze

--- source/lib/crypto/config.mk
+++ source/lib/crypto/config.mk
@@ -6,7 +6,7 @@
 
 LIBCRYPTO_OBJ_FILES = $(addprefix $(libcryptosrcdir)/, \
 					 crc32.o md5.o hmacmd5.o md4.o \
-					 arcfour.o sha1.o hmacsha1.o)
+					 arcfour.o)
 
 
 [MODULE::TORTURE_LIBCRYPTO]
@@ -14,6 +14,6 @@ SUBSYSTEM = smbtorture
 PRIVATE_DEPENDENCIES = LIBCRYPTO
 
 TORTURE_LIBCRYPTO_OBJ_FILES = $(addprefix $(libcryptosrcdir)/, \
-		md4test.o md5test.o hmacmd5test.o sha1test.o hmacsha1test.o)
+		md4test.o md5test.o hmacmd5test.o)
 
 $(eval $(call proto_header_template,$(libcryptosrcdir)/test_proto.h,$(TORTURE_LIBCRYPTO_OBJ_FILES:.o=.c)))
