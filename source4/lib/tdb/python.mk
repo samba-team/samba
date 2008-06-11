3fe58d839afd7b19cec33d84ba12a9e892ca5590
Jelmer Vernooij <jelmer@samba.org>
2008-06-11 02:47:09 +0200

    Use variables for source dir of python tdb bindings. (This used to be commit 22e7bcfbe4534c070ac2dd5f39773ea023dfa7f7)

--- source4/lib/tdb/python.mk
+++ source4/lib/tdb/python.mk
@@ -2,9 +2,9 @@
 LIBRARY_REALNAME = _tdb.$(SHLIBEXT)
 PUBLIC_DEPENDENCIES = LIBTDB DYNCONFIG
 
-swig_tdb_OBJ_FILES = lib/tdb/tdb_wrap.o
+swig_tdb_OBJ_FILES = $(tdbsrcdir)/tdb_wrap.o
 
-$(eval $(call python_py_module_template,tdb.py,lib/tdb/tdb.py))
+$(eval $(call python_py_module_template,tdb.py,$(tdbsrcdir)/tdb.py))
 
 $(swig_tdb_OBJ_FILES): CFLAGS+=$(CFLAG_NO_UNUSED_MACROS) $(CFLAG_NO_CAST_QUAL)
 
