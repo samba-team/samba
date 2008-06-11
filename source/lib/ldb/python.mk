cd4eddd31354f4ae45e8747da1a70340000716c3
Jelmer Vernooij <jelmer@samba.org>
2008-06-11 02:52:43 +0200

    Use variables for source dir of python ldb bindings.

--- source/lib/ldb/python.mk
+++ source/lib/ldb/python.mk
@@ -2,9 +2,9 @@
 LIBRARY_REALNAME = _ldb.$(SHLIBEXT)
 PUBLIC_DEPENDENCIES = LIBLDB
 
-swig_ldb_OBJ_FILES = $(ldbdir)/ldb_wrap.o
-$(swig_ldb_OBJ_FILES): CFLAGS+=-I$(ldbdir)/include
+swig_ldb_OBJ_FILES = $(ldbsrcdir)/ldb_wrap.o
+$(swig_ldb_OBJ_FILES): CFLAGS+=-I$(ldbsrcdir)/include
 
-$(eval $(call python_py_module_template,ldb.py,$(ldbdir)/ldb.py))
+$(eval $(call python_py_module_template,ldb.py,$(ldbsrcdir)/ldb.py))
 
 $(swig_ldb_OBJ_FILES): CFLAGS+=$(CFLAG_NO_UNUSED_MACROS) $(CFLAG_NO_CAST_QUAL)
