fd921aea286dd61a6e0bee1673bacb8948768f19
Ronnie Sahlberg <ronniesahlberg@gmail.com>
2008-06-13 13:45:23 +1000

    ban the node after 3 failed scripts by default
    (This used to be ctdb commit b4e6d8e37c7f985f357af82b4a524959bb97ec4c)

--- ctdb/server/ctdb_tunables.c
+++ ctdb/server/ctdb_tunables.c
@@ -38,7 +38,7 @@ static const struct {
 	{ "MonitorInterval",     15,  offsetof(struct ctdb_tunable, monitor_interval) },
 	{ "TickleUpdateInterval",20,  offsetof(struct ctdb_tunable, tickle_update_interval) },
 	{ "EventScriptTimeout",  20,  offsetof(struct ctdb_tunable, script_timeout) },
-	{ "EventScriptBanCount",  5,  offsetof(struct ctdb_tunable, script_ban_count) },
+	{ "EventScriptBanCount",  3,  offsetof(struct ctdb_tunable, script_ban_count) },
 	{ "RecoveryGracePeriod", 60,  offsetof(struct ctdb_tunable, recovery_grace_period) },
 	{ "RecoveryBanPeriod",  300,  offsetof(struct ctdb_tunable, recovery_ban_period) },
 	{ "DatabaseHashSize", 10000,  offsetof(struct ctdb_tunable, database_hash_size) },
