de33b264d18620ed7f91e759bcf80c9d64a99c17
Günther Deschner <gd@samba.org>
2008-06-17 21:44:30 +0200

    net_vampire: move out passdb routines to one file.
    Guenther
    (This used to be commit 74d431270d9b4cc1524f79fa2ad743420afef417)

--- source3/libnet/libnet_samsync.h
+++ source3/libnet/libnet_samsync.h
@@ -48,3 +48,8 @@ NTSTATUS fetch_sam_entries_ldif(TALLOC_CTX *mem_ctx,
 				struct netr_DELTA_ENUM_ARRAY *r,
 				NTSTATUS result,
 				struct samsync_context *ctx);
+NTSTATUS fetch_sam_entries(TALLOC_CTX *mem_ctx,
+			   enum netr_SamDatabaseID database_id,
+			   struct netr_DELTA_ENUM_ARRAY *r,
+			   NTSTATUS status,
+			   struct samsync_context *ctx);
