d621867bb8767e1c4236d28dd9294a61db6cbb10
Volker Lendecke <vl@samba.org>
2008-06-24 14:18:55 +0200

    Add server_info to pipes_struct

--- source/include/proto.h
+++ source/include/proto.h
@@ -122,6 +122,8 @@ NTSTATUS make_serverinfo_from_username(TALLOC_CTX *mem_ctx,
 struct auth_serversupplied_info *copy_serverinfo(TALLOC_CTX *mem_ctx,
 						 auth_serversupplied_info *src);
 bool init_guest_info(void);
+bool server_info_set_session_key(struct auth_serversupplied_info *info,
+				 DATA_BLOB session_key);
 NTSTATUS make_server_info_guest(TALLOC_CTX *mem_ctx,
 				auth_serversupplied_info **server_info);
 bool copy_current_user(struct current_user *dst, struct current_user *src);
