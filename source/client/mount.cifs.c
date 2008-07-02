8c12b31481d7c1f8acdec006977c556337adb98f
Jeremy Allison <jra@samba.org>
2008-07-02 11:58:07 -0700

    Fix bug #5578, reported by sendel2000@hotbox.ru. Bad (non-Samba) use of strlcat gives error. Jeremy.

--- source/client/mount.cifs.c
+++ source/client/mount.cifs.c
@@ -107,6 +107,9 @@ size_t strlcat(char *d, const char *s, size_t bufsize)
 	size_t ret = len1 + len2;
 
 	if (len1+len2 >= bufsize) {
+		if (bufsize < (len1+1)) {
+			return ret;
+		}
 		len2 = bufsize - (len1+1);
 	}
 	if (len2 > 0) {
