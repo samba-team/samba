4af2de3f038de1a553b620cb46436a05664e5e03
Stefan Metzmacher <metze@samba.org>
2008-07-07 18:04:44 +0200

    rap: trans->in.max_data should not depend on the smb max size
    We now support async multi fragment SMBtrans calls.
    
    metze
    (This used to be commit 66e0c1754f14cf0100ca2d3e9c0cd8c87f9dc1e6)

--- source4/torture/rap/rap.c
+++ source4/torture/rap/rap.c
@@ -206,7 +206,7 @@ static NTSTATUS rap_cli_do_call(struct smbcli_tree *tree,
 	params->flags = RAPNDR_FLAGS;
 
 	trans.in.max_param = call->rcv_paramlen;
-	trans.in.max_data = smb_raw_max_trans_data(tree, call->rcv_paramlen);
+	trans.in.max_data = call->rcv_datalen;
 	trans.in.max_setup = 0;
 	trans.in.flags = 0;
 	trans.in.timeout = 0;
