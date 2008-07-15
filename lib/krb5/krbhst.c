e3af27dfa463df713651c893573459c197ad5a84
Love Hörnquist Åstrand <lha@kth.se>
2008-07-15 11:22:26 +0000

    use constant KRB5_PLUGIN_LOCATE
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@23350 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/krb5/krbhst.c
+++ lib/krb5/krbhst.c
@@ -523,7 +523,7 @@ plugin_get_hosts(krb5_context context,
     struct krb5_plugin *list = NULL, *e;
     krb5_error_code ret;
 
-    ret = _krb5_plugin_find(context, PLUGIN_TYPE_DATA, "resolve", &list);
+    ret = _krb5_plugin_find(context, PLUGIN_TYPE_DATA, KRB5_PLUGIN_LOCATE, &list);
     if(ret != 0 || list == NULL)
 	return;
 
