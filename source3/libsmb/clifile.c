bf622cb8766fa3d66d597d3cf0971b081a8089dc
Volker Lendecke <vl@samba.org>
2008-08-01 15:29:06 +0200

    Fix a typo (This used to be commit 37bd2815c70176046bbe0232222b9f59dfa159c4)

--- source3/libsmb/clifile.c
+++ source3/libsmb/clifile.c
@@ -715,10 +715,10 @@ int cli_nt_delete_on_close(struct cli_state *cli, int fnum, bool flag)
 ****************************************************************************/
 
 int cli_nt_create_full(struct cli_state *cli, const char *fname,
-		 uint32 CreatFlags, uint32 DesiredAccess,
-		 uint32 FileAttributes, uint32 ShareAccess,
-		 uint32 CreateDisposition, uint32 CreateOptions,
-		 uint8 SecuityFlags)
+		       uint32 CreatFlags, uint32 DesiredAccess,
+		       uint32 FileAttributes, uint32 ShareAccess,
+		       uint32 CreateDisposition, uint32 CreateOptions,
+		       uint8 SecurityFlags)
 {
 	char *p;
 	int len;
@@ -744,7 +744,7 @@ int cli_nt_create_full(struct cli_state *cli, const char *fname,
 	SIVAL(cli->outbuf,smb_ntcreate_CreateDisposition, CreateDisposition);
 	SIVAL(cli->outbuf,smb_ntcreate_CreateOptions, CreateOptions);
 	SIVAL(cli->outbuf,smb_ntcreate_ImpersonationLevel, 0x02);
-	SCVAL(cli->outbuf,smb_ntcreate_SecurityFlags, SecuityFlags);
+	SCVAL(cli->outbuf,smb_ntcreate_SecurityFlags, SecurityFlags);
 
 	p = smb_buf(cli->outbuf);
 	/* this alignment and termination is critical for netapp filers. Don't change */
