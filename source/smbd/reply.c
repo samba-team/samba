377017b3a14140a8a74623c2fdf00299de441317
Andrew Tridgell <tridge@samba.org>
2008-08-10 10:43:36 +1000

    I found lots of places where we assume error will be set when calling one of our virtualised functions, such as db_open(), but error is only set when a system call fails, and it is not uncommon for us to fail a function internally without ever making a system call. That led to us passing back success when a function had in fact failed.
    I found two places where we relied on map_nt_error_from_unix()
    returning success when errno==0, but lots and lots of places where we
    relied on the reverse, so I fixed those two places.
    
    map_nt_error_from_unix() will now always return an error, returning
    NT_STATUS_UNSUCCESSFUL if errno is 0
    (cherry picked from commit 69d40ca4c1af925d4b0e59ddc69ef8c26e6501d1)
    (cherry picked from commit 8c66020a82d9b92fb10d14359b1381d58ad4972b)

--- source/smbd/reply.c
+++ source/smbd/reply.c
@@ -2497,7 +2497,7 @@ NTSTATUS unlink_internals(connection_struct *conn, struct smb_request *req,
 		TALLOC_FREE(dir_hnd);
 	}
 
-	if (count == 0 && NT_STATUS_IS_OK(status)) {
+	if (count == 0 && NT_STATUS_IS_OK(status) && errno != 0) {
 		status = map_nt_error_from_unix(errno);
 	}
 
@@ -5880,7 +5880,7 @@ NTSTATUS rename_internals(TALLOC_CTX *ctx,
 	}
 	TALLOC_FREE(dir_hnd);
 
-	if (count == 0 && NT_STATUS_IS_OK(status)) {
+	if (count == 0 && NT_STATUS_IS_OK(status) && errno != 0) {
 		status = map_nt_error_from_unix(errno);
 	}
 
