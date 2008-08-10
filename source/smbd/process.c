c59725892da965d09aa25e867c75c6aa3bfedf1d
Volker Lendecke <vl@samba.org>
2008-08-10 17:37:08 +0200

    Fix andx offset calculation for more than 2 chained requests
    Untested code is broken code.... Test follows later, it's quite an intrusive
    change to libsmb/
    (cherry picked from commit 2abeea64e15f0e8e8c413744de9194bdcedd6f16)

--- source/smbd/process.c
+++ source/smbd/process.c
@@ -1679,7 +1679,7 @@ void chain_reply(struct smb_request *req)
 	 * remember how much the caller added to the chain, only counting
 	 * stuff after the parameter words
 	 */
-	chain_size += outsize_padded - smb_wct;
+	chain_size += (outsize_padded - smb_wct);
 
 	/*
 	 * work out pointers into the original packets. The
@@ -1807,6 +1807,12 @@ void chain_reply(struct smb_request *req)
 
 	SAFE_FREE(caller_output);
 	TALLOC_FREE(req2);
+
+	/*
+	 * Reset the chain_size for our caller's offset calculations
+	 */
+
+	chain_size -= (outsize_padded - smb_wct);
 
 	return;
 }
