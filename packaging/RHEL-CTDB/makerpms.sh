fe7847f3e539ed737d7b1cd5b874e2d3add78129
Michael Adam <obnox@samba.org>
2008-08-12 21:28:10 +0200

    packaging(RHEL-CTDB): move to version 3.3.0 for the rpm build.
    Michael

--- packaging/RHEL-CTDB/makerpms.sh
+++ packaging/RHEL-CTDB/makerpms.sh
@@ -21,7 +21,7 @@ SRCDIR=`rpm --eval %_sourcedir`
 
 # At this point the SPECDIR and SRCDIR variables must have a value!
 
-VERSION='3.2.1'
+VERSION='3.3.0'
 REVISION='ctdb'
 SPECFILE="samba.spec"
 DOCS="docs.tar.bz2"
