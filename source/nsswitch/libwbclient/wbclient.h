4cb97ba397d06d7447956a5dc2f2b6a4f7a52009
Günther Deschner <gd@samba.org>
2008-08-15 13:53:23 +0200

    libwbclient: add wbcLogoffUserEx().
    Guenther
    (cherry picked from commit 9a24e73f5112cfb0a9378dee0d82340f3de8e363)

--- source/nsswitch/libwbclient/wbclient.h
+++ source/nsswitch/libwbclient/wbclient.h
@@ -407,6 +407,16 @@ enum wbcPasswordChangeRejectReason {
 	WBC_PWD_CHANGE_REJECT_COMPLEXITY=5
 };
 
+/**
+ * @brief Logoff User Parameters
+ **/
+
+struct wbcLogoffUserParams {
+	const char *username;
+	size_t num_blobs;
+	struct wbcNamedBlob *blobs;
+};
+
 /*
  * DomainControllerInfo struct
  */
@@ -618,6 +628,9 @@ wbcErr wbcAuthenticateUserEx(const struct wbcAuthUserParams *params,
 wbcErr wbcLogoffUser(const char *username,
 		     uid_t uid,
 		     const char *ccfilename);
+
+wbcErr wbcLogoffUserEx(const struct wbcLogoffUserParams *params,
+		       struct wbcAuthErrorInfo **error);
 
 wbcErr wbcChangeUserPassword(const char *username,
 			     const char *old_password,
