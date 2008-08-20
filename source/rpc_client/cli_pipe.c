e3ff1cb7709d93fb91602d3d25deca3f4ecce2ce
Günther Deschner <gd@samba.org>
2008-08-20 20:24:45 +0200

    fix another build warning.
    Guenther
    (cherry picked from commit 43693ce6c678b961fa516bbf502af92f87cd5346)
    (cherry picked from commit 2a5ae59f77c05c41c97747dee9bc8c196dfe6b89)

--- source/rpc_client/cli_pipe.c
+++ source/rpc_client/cli_pipe.c
@@ -823,8 +823,8 @@ static NTSTATUS rpc_api_pipe(struct rpc_pipe_client *cli,
 
 	while(1) {
 		RPC_HDR rhdr;
-		char *ret_data;
-		uint32 ret_data_len;
+		char *ret_data = NULL;
+		uint32 ret_data_len = 0;
 
 		/* Ensure we have enough data for a pdu. */
 		ret = cli_pipe_get_current_pdu(cli, &rhdr, &current_pdu);
