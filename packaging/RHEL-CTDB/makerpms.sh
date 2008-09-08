08721d60543cbab1403ca76bb0ee77ed9ef57dff
Michael Adam <obnox@samba.org>
2008-09-08 14:09:41 +0200

    packaging(RHEL-CTDB): add fix for versions containing "-" signs.
    Michael
    (cherry picked from commit 423105e29aaef449dbc40d124900a3f654b7266b)
    (cherry picked from commit 5c6628c1929312c9af487a5a48c61cfb9572eefb)

--- packaging/RHEL-CTDB/makerpms.sh
+++ packaging/RHEL-CTDB/makerpms.sh
@@ -62,6 +62,7 @@ vendor_version=`grep SAMBA_VERSION_VENDOR_SUFFIX ${VERSION_H} | awk '{print $3}'
 if test "x${vendor_version}"  != "x" ; then
 	VERSION="${VERSION}-${vendor_version}"
 fi
+VERSION=`echo ${VERSION} | sed 's/-/_/g'`
 VERSION=`echo ${VERSION} | sed 's/\"//g'`
 echo "VERSION: ${VERSION}"
 sed -e s/PVERSION/${VERSION}/g \
