db306bfee30437d2858788ea783ccb5acabd1ffa
Volker Lendecke <vl@sernet.de>
2008-09-08 16:42:06 +0200

    When setting an NFSv4 ACL, map generic bits (cherry picked from commit dbe7a61be2beac50d1665e38ac374cefbbabec00) (cherry picked from commit c2faa48abce1d656527b89f601e084e3ba70e36d)

--- source/modules/nfs4_acls.c
+++ source/modules/nfs4_acls.c
@@ -25,6 +25,8 @@
 
 #define SMBACL4_PARAM_TYPE_NAME "nfs4"
 
+extern const struct generic_mapping file_generic_mapping;
+
 #define SMB_ACE4_INT_MAGIC 0x76F8A967
 typedef struct _SMB_ACE4_INT_T
 {
@@ -528,6 +530,8 @@ static bool smbacl4_fill_ace4(
 	ace_v4->aceFlags = ace_nt->flags & SEC_ACE_FLAG_VALID_INHERIT;
 	ace_v4->aceMask = ace_nt->access_mask &
 		(STD_RIGHT_ALL_ACCESS | SA_RIGHT_FILE_ALL_ACCESS);
+
+	se_map_generic(&ace_v4->aceMask, &file_generic_mapping);
 
 	if (ace_v4->aceFlags!=ace_nt->flags)
 		DEBUG(9, ("ace_v4->aceFlags(0x%x)!=ace_nt->flags(0x%x)\n",
