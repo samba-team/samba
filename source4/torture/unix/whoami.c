d6130413965b53baa07b7b93c9b0d5199cff4437
Stefan Metzmacher <metze@samba.org>
2008-09-09 13:01:34 +0200

    UNIX-WHOAMI: fix compiler warnings
    metze
    (This used to be commit ec5d8ddadb76ff0d2cb72872e4d145a7527f0ec6)

--- source4/torture/unix/whoami.c
+++ source4/torture/unix/whoami.c
@@ -18,11 +18,11 @@
 */
 
 #include "includes.h"
-#include "torture/torture.h"
-#include "torture/basic/proto.h"
 #include "libcli/libcli.h"
 #include "libcli/raw/interfaces.h"
 #include "libcli/raw/raw_proto.h"
+#include "torture/torture.h"
+#include "torture/basic/proto.h"
 #include "lib/cmdline/popt_common.h"
 #include "auth/credentials/credentials.h"
 #include "param/param.h"
