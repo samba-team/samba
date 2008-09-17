ee67ed7256f6c52890cbb40f6e096669c8664259
Karolin Seeger <kseeger@samba.org>
2008-09-17 08:48:19 +0200

    find_missing_manpages.pl: Adapt script to changed directory structure.
    Karolin
    (cherry picked from commit b6b7aa023b62ab8994d13cb91d5185204c1500b2)

--- docs-xml/scripts/find_missing_manpages.pl
+++ docs-xml/scripts/find_missing_manpages.pl
@@ -1,14 +1,25 @@
 #!/usr/bin/perl
 
-my %doc;
-
 $invar = 0;
-
 $topdir = (shift @ARGV) or $topdir = ".";
-
+$makefile = "$topdir/source/Makefile.in";
+$mandir = "$topdir/docs-xml/manpages-3";
 $progs = "";
 
-open(IN, "$topdir/Makefile.in");
+chdir($topdir);
+
+if(! -e "$makefile") {
+	print "$makefile does not exist!\n";
+	print "Wrong directory?\n";
+	exit(1);
+}
+
+if(! -d "$mandir") {
+        print "$mandir does not exist!\n";
+        exit(1);
+}
+
+open(IN, "$makefile");
 while(<IN>) {
 	if($invar && /^([ \t]*)(.*?)([\\])$/) {
 		$progs.=" " . $2;
@@ -22,15 +33,17 @@ while(<IN>) {
 foreach(split(/bin\//, $progs)) {
 	next if($_ eq " ");
 	s/\@EXEEXT\@//g;
+	s/\@EXTRA_BIN_PROGS\@//g;
 	s/ //g;
 
 
 	$f = $_;
-	
+
 	$found = 0;
 
+
 	for($i = 0; $i < 9; $i++) {
-		if(-e "manpages/$f.$i.xml") { $found = 1; }
+		if(-e "$mandir/$f.$i.xml") { $found = 1; }
 	}
 
 	if(!$found) {
