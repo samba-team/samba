1f8fe94ff3a928063c91ded4571751da74d2c321
Volker Lendecke <vl@samba.org>
2008-09-23 22:44:21 +0200

    S3 puts smb.conf and lmhosts in lib/, s4 into etc/

--- source4/dynconfig/config.mk
+++ source4/dynconfig/config.mk
@@ -5,12 +5,12 @@ DYNCONFIG_OBJ_FILES = $(dynconfigsrcdir)/dynconfig.o
 # set these to where to find various files
 # These can be overridden by command line switches (see smbd(8))
 # or in smb.conf (see smb.conf(5))
-CONFIGFILE = $(sysconfdir)/smb.conf
+CONFIG4FILE = $(sysconfdir)/smb.conf
 PKGCONFIGDIR = $(libdir)/pkgconfig
-LMHOSTSFILE = $(sysconfdir)/lmhosts
+LMHOSTSFILE4 = $(sysconfdir)/lmhosts
 
-$(dynconfigsrcdir)/dynconfig.o: CFLAGS+=-DCONFIGFILE=\"$(CONFIGFILE)\" -DBINDIR=\"$(bindir)\" \
-	 -DLMHOSTSFILE=\"$(LMHOSTSFILE)\" \
+$(dynconfigsrcdir)/dynconfig.o: CFLAGS+=-DCONFIGFILE=\"$(CONFIG4FILE)\" -DBINDIR=\"$(bindir)\" \
+	 -DLMHOSTSFILE=\"$(LMHOSTSFILE4)\" \
 	 -DLOCKDIR=\"$(lockdir)\" -DPIDDIR=\"$(piddir)\" -DDATADIR=\"$(datadir)\" \
 	 -DLOGFILEBASE=\"$(logfilebase)\" \
 	 -DCONFIGDIR=\"$(sysconfdir)\" -DNCALRPCDIR=\"$(NCALRPCDIR)\" \
