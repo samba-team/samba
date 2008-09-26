9c1b5f27e40b1a30b20ffd674111fdfb709b0917
Günther Deschner <gd@samba.org>
2008-09-27 00:38:26 +0200

    libgpo: fix copyright.
    Guenther

--- source3/libgpo/gpext/security.c
+++ source3/libgpo/gpext/security.c
@@ -1,7 +1,7 @@
 /*
  *  Unix SMB/CIFS implementation.
  *  Group Policy Support
- *  Copyright (C) Guenther Deschner 2005-2007
+ *  Copyright (C) Guenther Deschner 2005-2008
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
