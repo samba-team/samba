3c9f3c32d1290b8e6c438a197602afe3e96ae828
Stefan Metzmacher <metze@samba.org>
2008-10-02 11:25:13 +0200

    s4:drsuapi.idl: fix some fields in drsuapi_DsRemoveDSServer()
    metze

--- source4/libnet/libnet_unbecome_dc.c
+++ source4/libnet/libnet_unbecome_dc.c
@@ -641,7 +641,7 @@ static void unbecomeDC_drsuapi_remove_ds_server_send(struct libnet_UnbecomeDC_st
 	r->in.level		= 1;
 	r->in.req.req1.server_dn= s->dest_dsa.server_dn_str;
 	r->in.req.req1.domain_dn= s->domain.dn_str;
-	r->in.req.req1.unknown	= 0x00000001;
+	r->in.req.req1.commit	= true;
 
 	req = dcerpc_drsuapi_DsRemoveDSServer_send(s->drsuapi.pipe, s, r);
 	composite_continue_rpc(c, req, unbecomeDC_drsuapi_remove_ds_server_recv, s);
@@ -664,11 +664,6 @@ static void unbecomeDC_drsuapi_remove_ds_server_recv(struct rpc_request *req)
 
 	if (r->out.level != 1) {
 		composite_error(c, NT_STATUS_INVALID_NETWORK_RESPONSE);
-		return;
-	}
-		
-	if (!W_ERROR_IS_OK(r->out.res.res1.status)) {
-		composite_error(c, werror_to_ntstatus(r->out.res.res1.status));
 		return;
 	}
 
