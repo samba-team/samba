c247506e8e311b7c4fb9a9c4cb5f1a96c377909c
Love Hörnquist Åstrand <lha@kth.se>
2008-10-13 03:01:53 +0000

    make krb5_ticket_get_flags compile
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@23898 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/krb5/ticket.c
+++ lib/krb5/ticket.c
@@ -119,7 +119,7 @@ unsigned long
 krb5_ticket_get_flags(krb5_context context,
 		      const krb5_ticket *ticket)
 {
-    return TicketFlags2int(ticket->ticket.flags.b);
+    return TicketFlags2int(ticket->ticket.flags);
 }
 
 static int
