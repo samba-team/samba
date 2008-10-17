4833f678ba194665e9c0554f9da37ddca269714e
Jeremy Allison <jra@samba.org>
2008-10-17 15:24:51 -0700

    Unify access checks for lsa server functions. Jeremy.

--- source/include/rpc_lsa.h
+++ source/include/rpc_lsa.h
@@ -39,6 +39,7 @@
 
 
 #define LSA_POLICY_READ       ( STANDARD_RIGHTS_READ_ACCESS      |\
+                            LSA_POLICY_VIEW_LOCAL_INFORMATION    |\
                             LSA_POLICY_VIEW_AUDIT_INFORMATION    |\
                             LSA_POLICY_GET_PRIVATE_INFORMATION)
 
