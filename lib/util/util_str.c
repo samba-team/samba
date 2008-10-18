7966b00b3a0614a0824aa7ef7089973f21734ea8
Günther Deschner <gd@samba.org>
2008-10-18 21:06:07 +0200

    lib/util: fix strhex_to_data_blob to use data_blob_talloc.
    Jelmer, I'm pretty sure you wanted to do this. Please check.
    
    Guenther

--- lib/util/util_str.c
+++ lib/util/util_str.c
@@ -222,7 +222,7 @@ _PUBLIC_ size_t strhex_to_str(char *p, size_t p_len, const char *strhex, size_t 
  */
 _PUBLIC_ _PURE_ DATA_BLOB strhex_to_data_blob(TALLOC_CTX *mem_ctx, const char *strhex) 
 {
-	DATA_BLOB ret_blob = data_blob(mem_ctx, strlen(strhex)/2+1);
+	DATA_BLOB ret_blob = data_blob_talloc(mem_ctx, NULL, strlen(strhex)/2+1);
 
 	ret_blob.length = strhex_to_str((char *)ret_blob.data, ret_blob.length,
 					strhex,
