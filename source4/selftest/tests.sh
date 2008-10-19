7e088ec35424fc2d6c64b8699d9d22b3215275e2
Jelmer Vernooij <jelmer@samba.org>
2008-10-19 14:41:10 +0200

    Fix comment after paths have changed.

--- source4/selftest/tests.sh
+++ source4/selftest/tests.sh
@@ -4,11 +4,11 @@
 
 # The output of this script is parsed by selftest.pl, which then decides 
 # which of the tests to actually run. It will, for example, skip all tests 
-# listed in samba4-skip or only run a subset during "make quicktest".
+# listed in selftest/skip or only run a subset during "make quicktest".
 
 # The idea is that this script outputs all of the tests of Samba 4, not 
 # just those that are known to pass, and list those that should be skipped 
-# or are known to file in samba4-skip/samba4-knownfail. This makes it 
+# or are known to fail in selftest/skip or selftest/knownfail. This makes it 
 # very easy to see what functionality is still missing in Samba 4 and makes 
 # it possible to run the testsuite against other servers, such as Samba 3 or 
 # Windows that have a different set of features.
