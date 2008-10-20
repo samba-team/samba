b6ce6dd1d82314ce3194dc450e67dec948e1a6b2
Volker Lendecke <vl@samba.org>
2008-10-20 11:05:31 -0700

    Use a direct compare instead of calling strncmp in valid_smb_header

--- source/smbd/process.c
+++ source/smbd/process.c
@@ -105,7 +105,11 @@ static bool valid_smb_header(const uint8_t *inbuf)
 	if (is_encrypted_packet(inbuf)) {
 		return true;
 	}
-	return (strncmp(smb_base(inbuf),"\377SMB",4) == 0);
+	/*
+	 * This used to be (strncmp(smb_base(inbuf),"\377SMB",4) == 0)
+	 * but it just looks weird to call strncmp for this one.
+	 */
+	return (IVAL(smb_base(inbuf), 0) == 0x424D53FF);
 }
 
 /* Socket functions for smbd packet processing. */
