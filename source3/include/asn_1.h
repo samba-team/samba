cef97c245955d0f125928740cd89984100635d29
Günther Deschner <gd@samba.org>
2008-10-22 14:01:53 +0200

    s3-spnego: move spnego defines to spnego.h
    Guenther

--- source3/include/asn_1.h
+++ source3/include/asn_1.h
@@ -51,21 +51,4 @@ typedef struct asn1_data {
 
 #define ASN1_MAX_OIDS 20
 
-/* some well known object IDs */
-#define OID_SPNEGO "1 3 6 1 5 5 2"
-#define OID_NTLMSSP "1 3 6 1 4 1 311 2 2 10"
-#define OID_KERBEROS5_OLD "1 2 840 48018 1 2 2"
-#define OID_KERBEROS5 "1 2 840 113554 1 2 2"
-
-#define SPNEGO_NEG_RESULT_ACCEPT 0
-#define SPNEGO_NEG_RESULT_INCOMPLETE 1
-#define SPNEGO_NEG_RESULT_REJECT 2
-
-/* not really ASN.1, but RFC 1964 */
-#define TOK_ID_KRB_AP_REQ	(uchar*)"\x01\x00"
-#define TOK_ID_KRB_AP_REP	(uchar*)"\x02\x00"
-#define TOK_ID_KRB_ERROR	(uchar*)"\x03\x00"
-#define TOK_ID_GSS_GETMIC	(uchar*)"\x01\x01"
-#define TOK_ID_GSS_WRAP		(uchar*)"\x02\x01"
-
 #endif /* _ASN_1_H */
--- source3/include/spnego.h
+++ source3/include/spnego.h
@@ -35,6 +35,23 @@
 #define SPNEGO_NEG_TOKEN_INIT 0
 #define SPNEGO_NEG_TOKEN_TARG 1
 
+/* some well known object IDs */
+#define OID_SPNEGO "1 3 6 1 5 5 2"
+#define OID_NTLMSSP "1 3 6 1 4 1 311 2 2 10"
+#define OID_KERBEROS5_OLD "1 2 840 48018 1 2 2"
+#define OID_KERBEROS5 "1 2 840 113554 1 2 2"
+
+#define SPNEGO_NEG_RESULT_ACCEPT 0
+#define SPNEGO_NEG_RESULT_INCOMPLETE 1
+#define SPNEGO_NEG_RESULT_REJECT 2
+
+/* not really ASN.1, but RFC 1964 */
+#define TOK_ID_KRB_AP_REQ	(uchar*)"\x01\x00"
+#define TOK_ID_KRB_AP_REP	(uchar*)"\x02\x00"
+#define TOK_ID_KRB_ERROR	(uchar*)"\x03\x00"
+#define TOK_ID_GSS_GETMIC	(uchar*)"\x01\x01"
+#define TOK_ID_GSS_WRAP		(uchar*)"\x02\x01"
+
 typedef enum _spnego_negResult {
 	SPNEGO_ACCEPT_COMPLETED = 0,
 	SPNEGO_ACCEPT_INCOMPLETE = 1,
