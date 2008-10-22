6c019b875515662f5c97d89d653e2708478581ad
Volker Lendecke <vl@samba.org>
2008-10-22 17:01:28 +0200

    Slightly simplify reply_sesssetup_blob(): Remove an else branch

--- source3/smbd/sesssetup.c
+++ source3/smbd/sesssetup.c
@@ -125,16 +125,17 @@ static void reply_sesssetup_blob(struct smb_request *req,
 	if (!NT_STATUS_IS_OK(nt_status) &&
 	    !NT_STATUS_EQUAL(nt_status, NT_STATUS_MORE_PROCESSING_REQUIRED)) {
 		reply_nterror(req, nt_status_squash(nt_status));
-	} else {
-		nt_status = nt_status_squash(nt_status);
-		SIVAL(req->outbuf, smb_rcls, NT_STATUS_V(nt_status));
-		SSVAL(req->outbuf, smb_vwv0, 0xFF); /* no chaining possible */
-		SSVAL(req->outbuf, smb_vwv3, blob.length);
+		return;
+	}
 
-		if ((message_push_blob(&req->outbuf, blob) == -1)
-		    || (push_signature(&req->outbuf) == -1)) {
-			reply_nterror(req, NT_STATUS_NO_MEMORY);
-		}
+	nt_status = nt_status_squash(nt_status);
+	SIVAL(req->outbuf, smb_rcls, NT_STATUS_V(nt_status));
+	SSVAL(req->outbuf, smb_vwv0, 0xFF); /* no chaining possible */
+	SSVAL(req->outbuf, smb_vwv3, blob.length);
+
+	if ((message_push_blob(&req->outbuf, blob) == -1)
+	    || (push_signature(&req->outbuf) == -1)) {
+		reply_nterror(req, NT_STATUS_NO_MEMORY);
 	}
 }
 
