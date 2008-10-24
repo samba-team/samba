239f815e30a4e5f64112a8da6401614eb3218eb5
Tim Prouty <tprouty@samba.org>
2008-10-23 20:49:55 -0700

    Add OneFS VFS module skeleton.
    Also set configure to autodetect OneFS OS and build this VFS module.

---
+++ source3/modules/vfs_onefs.c
@@ -0,0 +1,33 @@
+/*
+ * Support for OneFS
+ *
+ * Copyright (C) Tim Prouty, 2008
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "includes.h"
+
+#undef DBGC_CLASS
+#define DBGC_CLASS DBGC_VFS
+
+static vfs_op_tuple onefs_ops[] = {
+	{SMB_VFS_OP(NULL), SMB_VFS_OP_NOOP, SMB_VFS_LAYER_NOOP}
+};
+
+NTSTATUS vfs_onefs_init(void)
+{
+	return smb_register_vfs(SMB_VFS_INTERFACE_VERSION, "onefs",
+				onefs_ops);
+}
