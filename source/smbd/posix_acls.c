980e56109ba42703a658f6f983021843ddd06d16
Jeremy Allison <jra@samba.org>
2008-11-05 19:12:10 -0800

    Add reference to bug #4308 to remind me to add regression test to smbtorture. Jeremy. (cherry picked from commit 0de73eeb1a17b6af217babd626b203eca975541f)

--- source/smbd/posix_acls.c
+++ source/smbd/posix_acls.c
@@ -3426,6 +3426,10 @@ BOOL set_nt_acl(files_struct *fsp, uint32 security_info_sent, SEC_DESC *psd)
 
 	create_file_sids(&sbuf, &file_owner_sid, &file_grp_sid);
 
+	/* See here: http://www.codeproject.com/KB/winsdk/accessctrl2.aspx
+	 * for details and also the log trace in bug #4308. JRA.
+	*/
+
 	if ((security_info_sent & DACL_SECURITY_INFORMATION) &&
 		psd->dacl != NULL &&
 		(psd->type & (SE_DESC_DACL_AUTO_INHERITED|
