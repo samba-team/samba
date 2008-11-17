cc9940e432bf55839ec9ba25e1dd579ff7ce28a5
Karolin Seeger <kseeger@samba.org>
2008-11-17 14:02:45 +0100

    Revert "Fix bug 5840: Segfault in "rpcclient lsaaddacctrights""
    This reverts commit 369fecaeb2470a1f7e68417ccdddb61334e37d92.

--- source/rpcclient/cmd_lsarpc.c
+++ source/rpcclient/cmd_lsarpc.c
@@ -791,7 +791,7 @@ static NTSTATUS cmd_lsa_add_acct_rights(struct rpc_pipe_client *cli,
 		return NT_STATUS_NO_MEMORY;
 	}
 
-	for (i=0; i<argc-2; i++) {
+	for (i=0; i<argc-1; i++) {
 		init_lsa_StringLarge(&rights.names[i], argv[i+2]);
 	}
 
