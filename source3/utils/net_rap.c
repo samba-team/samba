5e5ccab372035a4e46e83775f40fca1ae5542ada
Kai Blin <kai@samba.org>
2008-11-17 08:37:21 +0100

    net: Fix net rap printq info documentation (bug #5892)
    Carsten Dumke <carsten@cdumke.de> reported and provided a patch
    for v3.0 and v3.2. (See 0a7fb721cdc)
    
    This fixes the same issue in v3.3 and also changes the auto-generated usage
    information to give better help about net rap printq info.

--- source3/utils/net_rap.c
+++ source3/utils/net_rap.c
@@ -612,7 +612,7 @@ int net_rap_printq_usage(struct net_context *c, int argc, const char **argv)
 	d_printf(
 	 "net rap printq [misc. options] [targets]\n"
 	 "\tor\n"
-	 "net rap printq list [<queue_name>] [misc. options] [targets]\n"
+	 "net rap printq info [<queue_name>] [misc. options] [targets]\n"
 	 "\tlists the specified queue and jobs on the target server.\n"
 	 "\tIf the queue name is not specified, all queues are listed.\n\n");
 	d_printf(
@@ -726,9 +726,10 @@ int net_rap_printq(struct net_context *c, int argc, const char **argv)
 			"info",
 			rap_printq_info,
 			NET_TRANSPORT_RAP,
-			"Display info about print job",
-			"net rap printq info\n"
-			"    Display info about print job"
+			"Display info about print queues and jobs",
+			"net rap printq info [queue]\n"
+			"    Display info about print jobs in queue.\n"
+			"    If queue is not specified, all queues are listed"
 		},
 		{
 			"delete",
