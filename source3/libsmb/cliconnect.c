691cf386fbc5d57a35a5838b99ef5cca1bfbf944
Volker Lendecke <vl@samba.org>
2008-11-30 13:49:14 +0100

    Slightly simplify cli_session_setup_ntlmssp
    Remove three pointless variables

--- source3/libsmb/cliconnect.c
+++ source3/libsmb/cliconnect.c
@@ -748,19 +748,11 @@ static NTSTATUS cli_session_setup_ntlmssp(struct cli_state *cli, const char *use
 
 	if (NT_STATUS_IS_OK(nt_status)) {
 
-		DATA_BLOB key = data_blob(ntlmssp_state->session_key.data,
-					  ntlmssp_state->session_key.length);
-		DATA_BLOB null_blob = data_blob_null;
-		bool res;
-
 		fstrcpy(cli->server_domain, ntlmssp_state->server_domain);
 		cli_set_session_key(cli, ntlmssp_state->session_key);
 
-		res = cli_simple_set_signing(cli, key, null_blob);
-
-		data_blob_free(&key);
-
-		if (res) {
+		if (cli_simple_set_signing(
+			    cli, ntlmssp_state->session_key, data_blob_null)) {
 
 			/* 'resign' the last message, so we get the right sequence numbers
 			   for checking the first reply from the server */
