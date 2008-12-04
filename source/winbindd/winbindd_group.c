68276aa400cb126513de06ad227febe737ec11b6
Michael Adam <obnox@samba.org>
2008-12-04 14:04:33 +0100

    winbindd: fix horrible mis-indentation of toplvl braces in getgrsid_sid2gid_r
    to make code more readable
    
    Michael
    (cherry picked from commit d28a0f970e06c67729d71d505c35ae1d49ad6ccc)

--- source/winbindd/winbindd_group.c
+++ source/winbindd/winbindd_group.c
@@ -833,7 +833,7 @@ struct getgrsid_state {
 };
 
 static void getgrsid_sid2gid_recv(void *private_data, bool success, gid_t gid)
-	{
+{
 	struct getgrsid_state *s =
 		(struct getgrsid_state *)private_data;
 	struct winbindd_domain *domain;
@@ -884,7 +884,7 @@ static void getgrsid_sid2gid_recv(void *private_data, bool success, gid_t gid)
 	s->state->response.extra_data.data = gr_mem;
 
 	request_ok(s->state);
-	}
+}
 
 static void getgrsid_lookupsid_recv( void *private_data, bool success,
 				     const char *dom_name, const char *name,
