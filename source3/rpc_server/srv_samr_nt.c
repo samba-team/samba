c4c6e1e463fe5ab8bb0e3e649d76ac5ec80900de
Jeremy Allison <jra@samba.org>
2008-12-10 15:29:14 -0800

    Fix usrmgr opening a user object as non-root. Jeremy.

--- source3/rpc_server/srv_samr_nt.c
+++ source3/rpc_server/srv_samr_nt.c
@@ -2636,7 +2636,7 @@ NTSTATUS _samr_QueryUserInfo(pipes_struct *p,
 		return NT_STATUS_INVALID_HANDLE;
 
 	status = access_check_samr_function(info->acc_granted,
-					    SAMR_DOMAIN_ACCESS_OPEN_ACCOUNT,
+					    SAMR_USER_ACCESS_GET_ATTRIBUTES,
 					    "_samr_QueryUserInfo");
 	if (!NT_STATUS_IS_OK(status)) {
 		return status;
