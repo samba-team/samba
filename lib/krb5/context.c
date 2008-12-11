c1a4666f81934cddc116a0156a1f7dd11f896eed
Love Hörnquist Åstrand <lha@kth.se>
2008-12-11 05:00:39 +0000

    mutex is always allocated, cid#142
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@24106 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/krb5/context.c
+++ lib/krb5/context.c
@@ -449,10 +449,10 @@ krb5_free_context(krb5_context context)
     krb5_set_extra_addresses(context, NULL);
     krb5_set_ignore_addresses(context, NULL);
     krb5_set_send_to_kdc_func(context, NULL, NULL);
-    if (context->mutex != NULL) {
-	HEIMDAL_MUTEX_destroy(context->mutex);
-	free(context->mutex);
-    }
+
+    HEIMDAL_MUTEX_destroy(context->mutex);
+    free(context->mutex);
+
     memset(context, 0, sizeof(*context));
     free(context);
 }
