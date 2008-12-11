7c4ee0b0e28c2f5b9100837c72f0ca49841e6132
Love Hörnquist Åstrand <lha@kth.se>
2008-12-11 04:51:59 +0000

    fix spelling
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@24066 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/gssapi/test_oid.c
+++ lib/gssapi/test_oid.c
@@ -62,7 +62,7 @@ main(int argc, char **argv)
     if (GSS_ERROR(maj_stat))
 	errx(1, "gss_oid_to_str failed");
 
-    ret = strnncmp(data.value, "1 3 6 1 5 6 4", data.length);
+    ret = strncmp(data.value, "1 3 6 1 5 6 4", data.length);
     gss_release_buffer(&maj_stat, &data);
     if (ret)
 	return 1;
