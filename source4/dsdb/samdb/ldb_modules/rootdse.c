d68ad8218ff104da35c2ed5b389c18926484c3af
Andrew Bartlett <abartlet@samba.org>
2008-12-16 09:01:35 +0100

    s4:rootdse: fix the logic to indentify a rootdse search
    Signed-off-by: Stefan Metzmacher <metze@samba.org>

--- source4/dsdb/samdb/ldb_modules/rootdse.c
+++ source4/dsdb/samdb/ldb_modules/rootdse.c
@@ -301,8 +301,7 @@ static int rootdse_search(struct ldb_module *module, struct ldb_request *req)
 	int ret;
 
 	/* see if its for the rootDSE - only a base search on the "" DN qualifies */
-	if (req->op.search.scope != LDB_SCOPE_BASE ||
-	    ( ! ldb_dn_is_null(req->op.search.base))) {
+	if (!(req->op.search.scope == LDB_SCOPE_BASE && ldb_dn_is_null(req->op.search.base))) {
 		/* Otherwise, pass down to the rest of the stack */
 		return ldb_next_request(module, req);
 	}
