31dc07ee2868c99792e0f71239f79ba49062e7e2
Jelmer Vernooij <jelmer@samba.org>
2008-12-23 20:13:17 +0100

    pytevent: Add missing file.

---
+++ lib/tevent/python.mk
@@ -0,0 +1,10 @@
+# TODO: Change python stuff to tevent
+[PYTHON::swig_events]
+LIBRARY_REALNAME = tevent.$(SHLIBEXT)
+PRIVATE_DEPENDENCIES = LIBTEVENT PYTALLOC LIBSAMBA-UTIL
+
+swig_events_OBJ_FILES = $(libteventsrcdir)/pytevent.o
+
+$(swig_events_OBJ_FILES): CFLAGS+=$(CFLAG_NO_CAST_QUAL)
+
+PC_FILES += $(libteventsrcdir)/tevent.pc
