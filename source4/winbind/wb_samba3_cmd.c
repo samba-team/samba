1180b6964f07c4f3526892a412b163348a137ccc
Kai Blin <kai@samba.org>
2008-12-26 12:45:50 +0100

    s4 winbind: Add support for WINBINDD_GETGRGID call

--- source4/winbind/wb_samba3_cmd.c
+++ source4/winbind/wb_samba3_cmd.c
@@ -939,11 +939,40 @@ static void getgrnam_recv(struct composite_context *ctx)
 	wbsrv_samba3_async_epilogue(status, s3call);
 }
 
+static void getgrgid_recv(struct composite_context *ctx);
+
 NTSTATUS wbsrv_samba3_getgrgid(struct wbsrv_samba3_call *s3call)
 {
+	struct composite_context *ctx;
+	struct wbsrv_service *service = s3call->wbconn->listen_socket->service;
+
 	DEBUG(5, ("wbsrv_samba3_getgrgid called\n"));
-	s3call->response.result = WINBINDD_ERROR;
+
+	ctx = wb_cmd_getgrgid_send(s3call, service,
+			s3call->request.data.gid);
+	NT_STATUS_HAVE_NO_MEMORY(ctx);
+
+	ctx->async.fn = getgrgid_recv;
+	ctx->async.private_data = s3call;
+	s3call->flags |= WBSRV_CALL_FLAGS_REPLY_ASYNC;
 	return NT_STATUS_OK;
+}
+
+static void getgrgid_recv(struct composite_context *ctx)
+{
+	struct wbsrv_samba3_call *s3call =
+		talloc_get_type(ctx->async.private_data,
+				struct wbsrv_samba3_call);
+	NTSTATUS status;
+	struct winbindd_gr *gr;
+
+	DEBUG(5, ("getgrgid_recv called\n"));
+
+	status = wb_cmd_getgrgid_recv(ctx, s3call, &gr);
+	if (NT_STATUS_IS_OK(status))
+		s3call->response.data.gr = *gr;
+
+	wbsrv_samba3_async_epilogue(status, s3call);
 }
 
 NTSTATUS wbsrv_samba3_getgroups(struct wbsrv_samba3_call *s3call)
