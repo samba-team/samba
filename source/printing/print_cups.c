5a14dee5f005ee4c0b8e0eb1e864589ab23a3ec9
Volker Lendecke <vl@samba.org>
2008-12-30 15:47:06 +0100

    Fix a memory leak in cups_pull_comment_location
    We allocated "request" with ippNew, so we also should ippDelete it.
    (cherry picked from commit 783247bdce9268d44b14005ec3eb84c35e0cd71a)

--- source/printing/print_cups.c
+++ source/printing/print_cups.c
@@ -1710,6 +1710,10 @@ bool cups_pull_comment_location(NT_PRINTER_INFO_LEVEL_2 *printer)
 	if (response)
 		ippDelete(response);
 
+	if (request) {
+		ippDelete(request);
+	}
+
 	if (language)
 		cupsLangFree(language);
 
