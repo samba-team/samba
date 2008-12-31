641772b605520ed3914ee36d5a953c247c1082c7
Volker Lendecke <vl@samba.org>
2008-12-31 18:37:39 +0100

    recvfile can't be used for printing so far

--- source3/smbd/reply.c
+++ source3/smbd/reply.c
@@ -3873,6 +3873,10 @@ bool is_valid_writeX_buffer(const uint8_t *inbuf)
 		DEBUG(10,("is_valid_writeX_buffer: IPC$ tid\n"));
 		return false;
 	}
+	if (IS_PRINT(conn)) {
+		DEBUG(10,("is_valid_writeX_buffer: printing tid\n"));
+		return false;
+	}
 	doff = SVAL(inbuf,smb_vwv11);
 
 	numtowrite = SVAL(inbuf,smb_vwv10);
