samba-team/samba

commit 888524623d179ceb4d540f5ac0f3b013d3ce5735

s3 build: Remove redundant #defines that are in idl generated headers
This fixes a series of samba3 build warnings in the merged build:
'error: "X" redefined', where X is one of the redundant #defines

source3/include/rpc_spoolss.h
@@ -20,6 +20,8 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include "../librpc/gen_ndr/spoolss.h"
+
 #ifndef _RPC_SPOOLSS_H		/* _RPC_SPOOLSS_H */
 #define _RPC_SPOOLSS_H
 
@@ -127,43 +129,6 @@
 #define PRINTER_CONTROL_SET_STATUS	0x00000004
 
 #define PRINTER_STATUS_OK               0x00000000
-#define PRINTER_STATUS_PAUSED		0x00000001
-#define PRINTER_STATUS_ERROR		0x00000002
-#define PRINTER_STATUS_PENDING_DELETION	0x00000004
-#define PRINTER_STATUS_PAPER_JAM	0x00000008
-
-#define PRINTER_STATUS_PAPER_OUT	0x00000010
-#define PRINTER_STATUS_MANUAL_FEED	0x00000020
-#define PRINTER_STATUS_PAPER_PROBLEM	0x00000040
-#define PRINTER_STATUS_OFFLINE		0x00000080
-
-#define PRINTER_STATUS_IO_ACTIVE	0x00000100
-#define PRINTER_STATUS_BUSY		0x00000200
-#define PRINTER_STATUS_PRINTING		0x00000400
-#define PRINTER_STATUS_OUTPUT_BIN_FULL	0x00000800
-
-#define PRINTER_STATUS_NOT_AVAILABLE	0x00001000
-#define PRINTER_STATUS_WAITING		0x00002000
-#define PRINTER_STATUS_PROCESSING	0x00004000
-#define PRINTER_STATUS_INITIALIZING	0x00008000
-
-#define PRINTER_STATUS_WARMING_UP	0x00010000
-#define PRINTER_STATUS_TONER_LOW	0x00020000
-#define PRINTER_STATUS_NO_TONER		0x00040000
-#define PRINTER_STATUS_PAGE_PUNT	0x00080000
-
-#define PRINTER_STATUS_USER_INTERVENTION	0x00100000
-#define PRINTER_STATUS_OUT_OF_MEMORY	0x00200000
-#define PRINTER_STATUS_DOOR_OPEN	0x00400000
-#define PRINTER_STATUS_SERVER_UNKNOWN	0x00800000
-
-#define PRINTER_STATUS_POWER_SAVE	0x01000000
-
-#define SERVER_ACCESS_ADMINISTER	0x00000001
-#define SERVER_ACCESS_ENUMERATE		0x00000002
-#define PRINTER_ACCESS_ADMINISTER	0x00000004
-#define PRINTER_ACCESS_USE		0x00000008
-#define JOB_ACCESS_ADMINISTER		0x00000010
 #define JOB_ACCESS_READ			0x00000020
 
 /* JOB status codes. */
@@ -180,31 +145,6 @@
 #define JOB_STATUS_DELETED		0x0100
 #define JOB_STATUS_BLOCKED		0x0200
 #define JOB_STATUS_USER_INTERVENTION	0x0400
-
-/* Access rights for print servers */
-#define SERVER_ALL_ACCESS	STANDARD_RIGHTS_REQUIRED_ACCESS|SERVER_ACCESS_ADMINISTER|SERVER_ACCESS_ENUMERATE
-#define SERVER_READ		STANDARD_RIGHTS_READ_ACCESS|SERVER_ACCESS_ENUMERATE
-#define SERVER_WRITE		STANDARD_RIGHTS_WRITE_ACCESS|SERVER_ACCESS_ADMINISTER|SERVER_ACCESS_ENUMERATE
-#define SERVER_EXECUTE		STANDARD_RIGHTS_EXECUTE_ACCESS|SERVER_ACCESS_ENUMERATE
-
-/* Access rights for printers */
-#define PRINTER_ALL_ACCESS	STANDARD_RIGHTS_REQUIRED_ACCESS|PRINTER_ACCESS_ADMINISTER|PRINTER_ACCESS_USE
-#define PRINTER_READ          STANDARD_RIGHTS_READ_ACCESS|PRINTER_ACCESS_USE
-#define PRINTER_WRITE         STANDARD_RIGHTS_WRITE_ACCESS|PRINTER_ACCESS_USE
-#define PRINTER_EXECUTE       STANDARD_RIGHTS_EXECUTE_ACCESS|PRINTER_ACCESS_USE
-
-/* Access rights for jobs */
-#define JOB_ALL_ACCESS	STANDARD_RIGHTS_REQUIRED_ACCESS|JOB_ACCESS_ADMINISTER|JOB_ACCESS_READ|PRINTER_ACCESS_USE
-#define JOB_READ	STANDARD_RIGHTS_READ_ACCESS|JOB_ACCESS_ADMINISTER|JOB_ACCESS_READ
-#define JOB_WRITE	STANDARD_RIGHTS_WRITE_ACCESS|JOB_ACCESS_ADMINISTER|PRINTER_ACCESS_USE
-#define JOB_EXECUTE	STANDARD_RIGHTS_EXECUTE_ACCESS|JOB_ACCESS_ADMINISTER|PRINTER_ACCESS_USE
-
-/* ACE masks for the various print permissions */
-
-#define PRINTER_ACE_FULL_CONTROL      (GENERIC_ALL_ACCESS|PRINTER_ALL_ACCESS)
-#define PRINTER_ACE_MANAGE_DOCUMENTS  (GENERIC_ALL_ACCESS|READ_CONTROL_ACCESS)
-#define PRINTER_ACE_PRINT             (GENERIC_EXECUTE_ACCESS|READ_CONTROL_ACCESS|PRINTER_ACCESS_USE)
-
 
 /* Notify field types */
 
@@ -366,24 +306,6 @@ PRINTER_MESSAGE_INFO;
  * RAW_ONLY _MUST_ be present otherwise NT will send an EMF file
  */
 
-#define PRINTER_ATTRIBUTE_QUEUED		0x00000001
-#define PRINTER_ATTRIBUTE_DIRECT		0x00000002
-#define PRINTER_ATTRIBUTE_DEFAULT		0x00000004
-#define PRINTER_ATTRIBUTE_SHARED		0x00000008
-
-#define PRINTER_ATTRIBUTE_NETWORK		0x00000010
-#define PRINTER_ATTRIBUTE_HIDDEN		0x00000020
-#define PRINTER_ATTRIBUTE_LOCAL			0x00000040
-#define PRINTER_ATTRIBUTE_ENABLE_DEVQ		0x00000080
-
-#define PRINTER_ATTRIBUTE_KEEPPRINTEDJOBS	0x00000100
-#define PRINTER_ATTRIBUTE_DO_COMPLETE_FIRST	0x00000200
-#define PRINTER_ATTRIBUTE_WORK_OFFLINE		0x00000400
-#define PRINTER_ATTRIBUTE_ENABLE_BIDI		0x00000800
-
-#define PRINTER_ATTRIBUTE_RAW_ONLY		0x00001000
-#define PRINTER_ATTRIBUTE_PUBLISHED		0x00002000
-
 #define PRINTER_ATTRIBUTE_SAMBA			(PRINTER_ATTRIBUTE_RAW_ONLY|\
 						 PRINTER_ATTRIBUTE_SHARED|\
 						 PRINTER_ATTRIBUTE_LOCAL)
@@ -394,29 +316,9 @@ PRINTER_MESSAGE_INFO;
 #define MIN_PRIORITY	 1
 #define DEF_PRIORITY	 1
 
-/* the flags of the query */
-#define PRINTER_ENUM_DEFAULT		0x00000001
-#define PRINTER_ENUM_LOCAL		0x00000002
-#define PRINTER_ENUM_CONNECTIONS	0x00000004
-#define PRINTER_ENUM_FAVORITE		0x00000004
-#define PRINTER_ENUM_NAME		0x00000008
-#define PRINTER_ENUM_REMOTE		0x00000010
-#define PRINTER_ENUM_SHARED		0x00000020
-#define PRINTER_ENUM_NETWORK		0x00000040
-
 /* the flags of each printers */
-#define PRINTER_ENUM_UNKNOWN_8		0x00000008
-#define PRINTER_ENUM_EXPAND		0x00004000
-#define PRINTER_ENUM_CONTAINER		0x00008000
+#define PRINTER_ENUM_UNKNOWN_8         0x00000008
 #define PRINTER_ENUM_ICONMASK		0x00ff0000
-#define PRINTER_ENUM_ICON1		0x00010000
-#define PRINTER_ENUM_ICON2		0x00020000
-#define PRINTER_ENUM_ICON3		0x00040000
-#define PRINTER_ENUM_ICON4		0x00080000
-#define PRINTER_ENUM_ICON5		0x00100000
-#define PRINTER_ENUM_ICON6		0x00200000
-#define PRINTER_ENUM_ICON7		0x00400000
-#define PRINTER_ENUM_ICON8		0x00800000
 
 /* FLAGS for SPOOLSS_DELETEPRINTERDRIVEREX */
 

commit cb5db966e34310af16499b657c83f30a1758fc06

s3 build: Revert previous patch to fix the build
888524623d179ceb4d540f5ac0f3b013d3ce5735 broke the build.  I'm
reverting it for the short term, while I work on a better fix.

source3/include/rpc_spoolss.h
@@ -20,8 +20,6 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "../librpc/gen_ndr/spoolss.h"
-
 #ifndef _RPC_SPOOLSS_H		/* _RPC_SPOOLSS_H */
 #define _RPC_SPOOLSS_H
 
@@ -129,6 +127,43 @@
 #define PRINTER_CONTROL_SET_STATUS	0x00000004
 
 #define PRINTER_STATUS_OK               0x00000000
+#define PRINTER_STATUS_PAUSED		0x00000001
+#define PRINTER_STATUS_ERROR		0x00000002
+#define PRINTER_STATUS_PENDING_DELETION	0x00000004
+#define PRINTER_STATUS_PAPER_JAM	0x00000008
+
+#define PRINTER_STATUS_PAPER_OUT	0x00000010
+#define PRINTER_STATUS_MANUAL_FEED	0x00000020
+#define PRINTER_STATUS_PAPER_PROBLEM	0x00000040
+#define PRINTER_STATUS_OFFLINE		0x00000080
+
+#define PRINTER_STATUS_IO_ACTIVE	0x00000100
+#define PRINTER_STATUS_BUSY		0x00000200
+#define PRINTER_STATUS_PRINTING		0x00000400
+#define PRINTER_STATUS_OUTPUT_BIN_FULL	0x00000800
+
+#define PRINTER_STATUS_NOT_AVAILABLE	0x00001000
+#define PRINTER_STATUS_WAITING		0x00002000
+#define PRINTER_STATUS_PROCESSING	0x00004000
+#define PRINTER_STATUS_INITIALIZING	0x00008000
+
+#define PRINTER_STATUS_WARMING_UP	0x00010000
+#define PRINTER_STATUS_TONER_LOW	0x00020000
+#define PRINTER_STATUS_NO_TONER		0x00040000
+#define PRINTER_STATUS_PAGE_PUNT	0x00080000
+
+#define PRINTER_STATUS_USER_INTERVENTION	0x00100000
+#define PRINTER_STATUS_OUT_OF_MEMORY	0x00200000
+#define PRINTER_STATUS_DOOR_OPEN	0x00400000
+#define PRINTER_STATUS_SERVER_UNKNOWN	0x00800000
+
+#define PRINTER_STATUS_POWER_SAVE	0x01000000
+
+#define SERVER_ACCESS_ADMINISTER	0x00000001
+#define SERVER_ACCESS_ENUMERATE		0x00000002
+#define PRINTER_ACCESS_ADMINISTER	0x00000004
+#define PRINTER_ACCESS_USE		0x00000008
+#define JOB_ACCESS_ADMINISTER		0x00000010
 #define JOB_ACCESS_READ			0x00000020
 
 /* JOB status codes. */
@@ -145,6 +180,31 @@
 #define JOB_STATUS_DELETED		0x0100
 #define JOB_STATUS_BLOCKED		0x0200
 #define JOB_STATUS_USER_INTERVENTION	0x0400
+
+/* Access rights for print servers */
+#define SERVER_ALL_ACCESS	STANDARD_RIGHTS_REQUIRED_ACCESS|SERVER_ACCESS_ADMINISTER|SERVER_ACCESS_ENUMERATE
+#define SERVER_READ		STANDARD_RIGHTS_READ_ACCESS|SERVER_ACCESS_ENUMERATE
+#define SERVER_WRITE		STANDARD_RIGHTS_WRITE_ACCESS|SERVER_ACCESS_ADMINISTER|SERVER_ACCESS_ENUMERATE
+#define SERVER_EXECUTE		STANDARD_RIGHTS_EXECUTE_ACCESS|SERVER_ACCESS_ENUMERATE
+
+/* Access rights for printers */
+#define PRINTER_ALL_ACCESS	STANDARD_RIGHTS_REQUIRED_ACCESS|PRINTER_ACCESS_ADMINISTER|PRINTER_ACCESS_USE
+#define PRINTER_READ          STANDARD_RIGHTS_READ_ACCESS|PRINTER_ACCESS_USE
+#define PRINTER_WRITE         STANDARD_RIGHTS_WRITE_ACCESS|PRINTER_ACCESS_USE
+#define PRINTER_EXECUTE       STANDARD_RIGHTS_EXECUTE_ACCESS|PRINTER_ACCESS_USE
+
+/* Access rights for jobs */
+#define JOB_ALL_ACCESS	STANDARD_RIGHTS_REQUIRED_ACCESS|JOB_ACCESS_ADMINISTER|JOB_ACCESS_READ|PRINTER_ACCESS_USE
+#define JOB_READ	STANDARD_RIGHTS_READ_ACCESS|JOB_ACCESS_ADMINISTER|JOB_ACCESS_READ
+#define JOB_WRITE	STANDARD_RIGHTS_WRITE_ACCESS|JOB_ACCESS_ADMINISTER|PRINTER_ACCESS_USE
+#define JOB_EXECUTE	STANDARD_RIGHTS_EXECUTE_ACCESS|JOB_ACCESS_ADMINISTER|PRINTER_ACCESS_USE
+
+/* ACE masks for the various print permissions */
+
+#define PRINTER_ACE_FULL_CONTROL      (GENERIC_ALL_ACCESS|PRINTER_ALL_ACCESS)
+#define PRINTER_ACE_MANAGE_DOCUMENTS  (GENERIC_ALL_ACCESS|READ_CONTROL_ACCESS)
+#define PRINTER_ACE_PRINT             (GENERIC_EXECUTE_ACCESS|READ_CONTROL_ACCESS|PRINTER_ACCESS_USE)
+
 
 /* Notify field types */
 
@@ -306,6 +366,24 @@ PRINTER_MESSAGE_INFO;
  * RAW_ONLY _MUST_ be present otherwise NT will send an EMF file
  */
 
+#define PRINTER_ATTRIBUTE_QUEUED		0x00000001
+#define PRINTER_ATTRIBUTE_DIRECT		0x00000002
+#define PRINTER_ATTRIBUTE_DEFAULT		0x00000004
+#define PRINTER_ATTRIBUTE_SHARED		0x00000008
+
+#define PRINTER_ATTRIBUTE_NETWORK		0x00000010
+#define PRINTER_ATTRIBUTE_HIDDEN		0x00000020
+#define PRINTER_ATTRIBUTE_LOCAL			0x00000040
+#define PRINTER_ATTRIBUTE_ENABLE_DEVQ		0x00000080
+
+#define PRINTER_ATTRIBUTE_KEEPPRINTEDJOBS	0x00000100
+#define PRINTER_ATTRIBUTE_DO_COMPLETE_FIRST	0x00000200
+#define PRINTER_ATTRIBUTE_WORK_OFFLINE		0x00000400
+#define PRINTER_ATTRIBUTE_ENABLE_BIDI		0x00000800
+
+#define PRINTER_ATTRIBUTE_RAW_ONLY		0x00001000
+#define PRINTER_ATTRIBUTE_PUBLISHED		0x00002000
+
 #define PRINTER_ATTRIBUTE_SAMBA			(PRINTER_ATTRIBUTE_RAW_ONLY|\
 						 PRINTER_ATTRIBUTE_SHARED|\
 						 PRINTER_ATTRIBUTE_LOCAL)
@@ -316,9 +394,29 @@ PRINTER_MESSAGE_INFO;
 #define MIN_PRIORITY	 1
 #define DEF_PRIORITY	 1
 
+/* the flags of the query */
+#define PRINTER_ENUM_DEFAULT		0x00000001
+#define PRINTER_ENUM_LOCAL		0x00000002
+#define PRINTER_ENUM_CONNECTIONS	0x00000004
+#define PRINTER_ENUM_FAVORITE		0x00000004
+#define PRINTER_ENUM_NAME		0x00000008
+#define PRINTER_ENUM_REMOTE		0x00000010
+#define PRINTER_ENUM_SHARED		0x00000020
+#define PRINTER_ENUM_NETWORK		0x00000040
+
 /* the flags of each printers */
-#define PRINTER_ENUM_UNKNOWN_8         0x00000008
+#define PRINTER_ENUM_UNKNOWN_8		0x00000008
+#define PRINTER_ENUM_EXPAND		0x00004000
+#define PRINTER_ENUM_CONTAINER		0x00008000
 #define PRINTER_ENUM_ICONMASK		0x00ff0000
+#define PRINTER_ENUM_ICON1		0x00010000
+#define PRINTER_ENUM_ICON2		0x00020000
+#define PRINTER_ENUM_ICON3		0x00040000
+#define PRINTER_ENUM_ICON4		0x00080000
+#define PRINTER_ENUM_ICON5		0x00100000
+#define PRINTER_ENUM_ICON6		0x00200000
+#define PRINTER_ENUM_ICON7		0x00400000
+#define PRINTER_ENUM_ICON8		0x00800000
 
 /* FLAGS for SPOOLSS_DELETEPRINTERDRIVEREX */
 