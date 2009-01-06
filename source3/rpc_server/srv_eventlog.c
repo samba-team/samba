2729c484fbc317687e537a1acc1d7ec83a96a1bd
Volker Lendecke <vl@samba.org>
2009-01-06 23:39:09 +0100

    Call rpc_srv_register instead of rpc_pipe_register_commands in eventlog and svcctl

--- source3/rpc_server/srv_eventlog.c
+++ source3/rpc_server/srv_eventlog.c
@@ -106,8 +106,8 @@ struct api_struct api_eventlog_cmds[] =
 
 NTSTATUS rpc_eventlog2_init(void)
 {
-	return rpc_pipe_register_commands(SMB_RPC_INTERFACE_VERSION, 
-		"eventlog", "eventlog", &ndr_table_eventlog.syntax_id,
+	return rpc_srv_register(SMB_RPC_INTERFACE_VERSION,
+		"eventlog", "eventlog", &ndr_table_eventlog,
 		api_eventlog_cmds,
 		sizeof(api_eventlog_cmds)/sizeof(struct api_struct));
 }
