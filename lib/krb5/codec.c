2854a221e8b5dbcf90a8bae6cfc152ee32e31d76
Love Hörnquist Åstrand <lha@kth.se>
2009-01-11 21:49:33 +0000

    use KRB5_DEPRECATED
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@24286 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/krb5/codec.c
+++ lib/krb5/codec.c
@@ -33,10 +33,10 @@
 
 #include "krb5_locl.h"
 
-RCSID("$Id$");
+#undef KRB5_DEPRECATED
+#define KRB5_DEPRECATED
 
-#undef __attribute__
-#define __attribute__(x)
+RCSID("$Id$");
 
 #ifndef HEIMDAL_SMALLER
 
@@ -46,7 +46,7 @@ krb5_decode_EncTicketPart (krb5_context context,
 			   size_t length,
 			   EncTicketPart *t,
 			   size_t *len)
-    __attribute__((deprecated))
+    KRB5_DEPRECATED
 {
     return decode_EncTicketPart(data, length, t, len);
 }
@@ -57,7 +57,7 @@ krb5_encode_EncTicketPart (krb5_context context,
 			   size_t length,
 			   EncTicketPart *t,
 			   size_t *len)
-    __attribute__((deprecated))
+    KRB5_DEPRECATED
 {
     return encode_EncTicketPart(data, length, t, len);
 }
@@ -68,7 +68,7 @@ krb5_decode_EncASRepPart (krb5_context context,
 			  size_t length,
 			  EncASRepPart *t,
 			  size_t *len)
-    __attribute__((deprecated))
+    KRB5_DEPRECATED
 {
     return decode_EncASRepPart(data, length, t, len);
 }
@@ -79,7 +79,7 @@ krb5_encode_EncASRepPart (krb5_context context,
 			  size_t length,
 			  EncASRepPart *t,
 			  size_t *len)
-    __attribute__((deprecated))
+    KRB5_DEPRECATED
 {
     return encode_EncASRepPart(data, length, t, len);
 }
@@ -90,7 +90,7 @@ krb5_decode_EncTGSRepPart (krb5_context context,
 			   size_t length,
 			   EncTGSRepPart *t,
 			   size_t *len)
-    __attribute__((deprecated))
+    KRB5_DEPRECATED
 {
     return decode_EncTGSRepPart(data, length, t, len);
 }
@@ -101,7 +101,7 @@ krb5_encode_EncTGSRepPart (krb5_context context,
 			   size_t length,
 			   EncTGSRepPart *t,
 			   size_t *len)
-    __attribute__((deprecated))
+    KRB5_DEPRECATED
 {
     return encode_EncTGSRepPart(data, length, t, len);
 }
@@ -112,7 +112,7 @@ krb5_decode_EncAPRepPart (krb5_context context,
 			  size_t length,
 			  EncAPRepPart *t,
 			  size_t *len)
-    __attribute__((deprecated))
+    KRB5_DEPRECATED
 {
     return decode_EncAPRepPart(data, length, t, len);
 }
@@ -123,7 +123,7 @@ krb5_encode_EncAPRepPart (krb5_context context,
 			  size_t length,
 			  EncAPRepPart *t,
 			  size_t *len)
-    __attribute__((deprecated))
+    KRB5_DEPRECATED
 {
     return encode_EncAPRepPart(data, length, t, len);
 }
@@ -134,7 +134,7 @@ krb5_decode_Authenticator (krb5_context context,
 			   size_t length,
 			   Authenticator *t,
 			   size_t *len)
-    __attribute__((deprecated))
+    KRB5_DEPRECATED
 {
     return decode_Authenticator(data, length, t, len);
 }
@@ -145,7 +145,7 @@ krb5_encode_Authenticator (krb5_context context,
 			   size_t length,
 			   Authenticator *t,
 			   size_t *len)
-    __attribute__((deprecated))
+    KRB5_DEPRECATED
 {
     return encode_Authenticator(data, length, t, len);
 }
@@ -156,7 +156,7 @@ krb5_decode_EncKrbCredPart (krb5_context context,
 			    size_t length,
 			    EncKrbCredPart *t,
 			    size_t *len)
-    __attribute__((deprecated))
+    KRB5_DEPRECATED
 {
     return decode_EncKrbCredPart(data, length, t, len);
 }
@@ -167,7 +167,7 @@ krb5_encode_EncKrbCredPart (krb5_context context,
 			    size_t length,
 			    EncKrbCredPart *t,
 			    size_t *len)
-    __attribute__((deprecated))
+    KRB5_DEPRECATED
 {
     return encode_EncKrbCredPart (data, length, t, len);
 }
@@ -178,7 +178,7 @@ krb5_decode_ETYPE_INFO (krb5_context context,
 			size_t length,
 			ETYPE_INFO *t,
 			size_t *len)
-    __attribute__((deprecated))
+    KRB5_DEPRECATED
 {
     return decode_ETYPE_INFO(data, length, t, len);
 }
@@ -189,7 +189,7 @@ krb5_encode_ETYPE_INFO (krb5_context context,
 			size_t length,
 			ETYPE_INFO *t,
 			size_t *len)
-    __attribute__((deprecated))
+    KRB5_DEPRECATED
 {
     return encode_ETYPE_INFO (data, length, t, len);
 }
@@ -200,7 +200,7 @@ krb5_decode_ETYPE_INFO2 (krb5_context context,
 			size_t length,
 			ETYPE_INFO2 *t,
 			size_t *len)
-    __attribute__((deprecated))
+    KRB5_DEPRECATED
 {
     return decode_ETYPE_INFO2(data, length, t, len);
 }
@@ -211,7 +211,7 @@ krb5_encode_ETYPE_INFO2 (krb5_context context,
 			 size_t length,
 			 ETYPE_INFO2 *t,
 			 size_t *len)
-    __attribute__((deprecated))
+    KRB5_DEPRECATED
 {
     return encode_ETYPE_INFO2 (data, length, t, len);
 }
