samba-team/samba

commit aaea68791cfea45c6fa69abc93ad6d0e832d0283

In gcc version 4.3.2 we get warnings for functions declared with attribute warn_unused_result. Start to fix these. Jeremy.

lib/socket_wrapper/socket_wrapper.c
@@ -899,7 +899,10 @@ static int swrap_get_pcap_fd(const char *fname)
 		file_hdr.frame_max_len	= SWRAP_FRAME_LENGTH_MAX;
 		file_hdr.link_type	= 0x0065; /* 101 RAW IP */
 
-		write(fd, &file_hdr, sizeof(file_hdr));
+		if (write(fd, &file_hdr, sizeof(file_hdr)) != sizeof(file_hdr)) {
+			close(fd);
+			return -1;
+		}
 		return fd;
 	}
 
@@ -1190,7 +1193,12 @@ static void swrap_dump_packet(struct socket_info *si,
 
 	fd = swrap_get_pcap_fd(file_name);
 	if (fd != -1) {
-		write(fd, packet, packet_len);
+		if (write(fd, packet, packet_len) != packet_len) {
+			close(fd);
+			free(packet);
+			return;
+		}
+		close(fd);
 	}
 
 	free(packet);

source3/libnet/libnet_join.c
@@ -1019,8 +1019,7 @@ NTSTATUS libnet_join_ok(const char *netbios_domain_name,
 		return NT_STATUS_NO_TRUST_LSA_SECRET;
 	}
 
-	asprintf(&machine_account, "%s$", machine_name);
-	if (!machine_account) {
+	if (asprintf(&machine_account, "%s$", machine_name) == -1) {
 		SAFE_FREE(machine_password);
 		return NT_STATUS_NO_MEMORY;
 	}

commit bd576b93bd8a136e1da8cbac154aa87ab1873c1c

socket_wrapper: fix PCAP support broken by aaea68791cfea45c6fa69abc93ad6d0e832d0283
We should note close the fd after writing the first packet.

metze

lib/socket_wrapper/socket_wrapper.c
@@ -901,7 +901,7 @@ static int swrap_get_pcap_fd(const char *fname)
 
 		if (write(fd, &file_hdr, sizeof(file_hdr)) != sizeof(file_hdr)) {
 			close(fd);
-			return -1;
+			fd = -1;
 		}
 		return fd;
 	}
@@ -1194,11 +1194,9 @@ static void swrap_dump_packet(struct socket_info *si,
 	fd = swrap_get_pcap_fd(file_name);
 	if (fd != -1) {
 		if (write(fd, packet, packet_len) != packet_len) {
-			close(fd);
 			free(packet);
 			return;
 		}
-		close(fd);
 	}
 
 	free(packet);