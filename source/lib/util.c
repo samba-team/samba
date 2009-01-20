918133544b535d12761b5d6242e59eb7d21bae45
Stefan Metzmacher <metze@samba.org>
2009-01-19 16:26:00 -0800

    s3: reinit_after_fork() should reinit the event context before the messaging context
    Because messaging_reinit() may add events to the event context, which will
    removed by event_context_reinit().
    
    metze
    (cherry picked from commit 56af6112d67aa9dd3757297fbe29ce1a3eabf00d)
    
    Signed-off-by: Michael Adam <obnox@samba.org>

--- source/lib/util.c
+++ source/lib/util.c
@@ -1052,6 +1052,10 @@ bool reinit_after_fork(struct messaging_context *msg_ctx,
 		return false;
 	}
 
+	if (ev_ctx) {
+		event_context_reinit(ev_ctx);
+	}
+
 	if (msg_ctx) {
 		/*
 		 * For clustering, we need to re-init our ctdbd connection after the
@@ -1063,10 +1067,6 @@ bool reinit_after_fork(struct messaging_context *msg_ctx,
 				 nt_errstr(status)));
 			return false;
 		}
-	}
-
-	if (ev_ctx) {
-		event_context_reinit(ev_ctx);
 	}
 
 	return true;
