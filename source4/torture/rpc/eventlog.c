ab2ee0711e128ac068e54a180fe2deb4b0c082be
Günther Deschner <gd@samba.org>
2009-01-21 00:33:01 +0100

    s4-smbtorture: add my copyright.
    Guenther

--- source4/torture/rpc/eventlog.c
+++ source4/torture/rpc/eventlog.c
@@ -4,6 +4,7 @@
 
    Copyright (C) Tim Potter 2003,2005
    Copyright (C) Jelmer Vernooij 2004
+   Copyright (C) Guenther Deschner 2009
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
