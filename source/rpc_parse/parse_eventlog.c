samba-team/samba

commit f6fa3080fee1b20df9f1968500840a88cf0ee592

r22542: Move over to using the _strict varients of the talloc calls. No functional changes. Looks bigger than it is :-). Jeremy.

source/rpc_parse/parse_eventlog.c
@@ -354,7 +354,9 @@ BOOL eventlog_io_r_read_eventlog(const char *desc,
 	/* Now pad with whitespace until the end of the response buffer */
 
 	if (q_u->max_read_size - r_u->num_bytes_in_resp) {
-		r_u->end_of_entries_padding = SMB_CALLOC_ARRAY(uint8, q_u->max_read_size - r_u->num_bytes_in_resp);
+		if (!r_u->end_of_entries_padding) {
+			return False;
+		}
 
 		if(!(prs_uint8s(False, "end of entries padding", ps, 
 				depth, r_u->end_of_entries_padding,

source/auth/auth_util.c
@@ -834,9 +834,9 @@ NTSTATUS create_token_from_username(TALLOC_CTX *mem_ctx, const char *username,
 			goto done;
 		}
 
-		group_sids = talloc_array(tmp_ctx, DOM_SID, num_group_sids);
+		group_sids = TALLOC_ARRAY(tmp_ctx, DOM_SID, num_group_sids);
 		if (group_sids == NULL) {
-			DEBUG(1, ("talloc_array failed\n"));
+			DEBUG(1, ("TALLOC_ARRAY failed\n"));
 			result = NT_STATUS_NO_MEMORY;
 			goto done;
 		}
@@ -864,9 +864,9 @@ NTSTATUS create_token_from_username(TALLOC_CTX *mem_ctx, const char *username,
 		uint32 dummy;
 
 		num_group_sids = 1;
-		group_sids = talloc_array(tmp_ctx, DOM_SID, num_group_sids);
+		group_sids = TALLOC_ARRAY(tmp_ctx, DOM_SID, num_group_sids);
 		if (group_sids == NULL) {
-			DEBUG(1, ("talloc_array failed\n"));
+			DEBUG(1, ("TALLOC_ARRAY failed\n"));
 			result = NT_STATUS_NO_MEMORY;
 			goto done;
 		}
@@ -1117,7 +1117,7 @@ static auth_serversupplied_info *copy_serverinfo(auth_serversupplied_info *src)
 	dst->gid = src->gid;
 	dst->n_groups = src->n_groups;
 	if (src->n_groups != 0) {
-		dst->groups = (gid_t *)talloc_memdup(
+		dst->groups = (gid_t *)TALLOC_MEMDUP(
 			dst, src->groups, sizeof(gid_t)*dst->n_groups);
 	} else {
 		dst->groups = NULL;

source/include/smb_macros.h
@@ -271,17 +271,19 @@ copy an IP address from one buffer to another
 
 /* The new talloc is paranoid malloc checker safe. */
 
-#define TALLOC(ctx, size) talloc_named_const(ctx, size, __location__)
-#define TALLOC_P(ctx, type) (type *)talloc_named_const(ctx, sizeof(type), #type)
-#define TALLOC_ARRAY(ctx, type, count) (type *)_talloc_array(ctx, sizeof(type), count, #type)
-#define TALLOC_MEMDUP(ctx, ptr, size) _talloc_memdup(ctx, ptr, size, __location__)
-#define TALLOC_ZERO(ctx, size) _talloc_zero(ctx, size, __location__)
-#define TALLOC_ZERO_P(ctx, type) (type *)_talloc_zero(ctx, sizeof(type), #type)
-#define TALLOC_ZERO_ARRAY(ctx, type, count) (type *)_talloc_zero_array(ctx, sizeof(type), count, #type)
+#define TALLOC(ctx, size) talloc_strict(ctx, size, __location__)
+#define TALLOC_P(ctx, type) (type *)talloc_strict(ctx, sizeof(type), #type)
+#define TALLOC_ARRAY(ctx, type, count) (type *)_talloc_array_strict(ctx, sizeof(type), count, #type)
+#define TALLOC_MEMDUP(ctx, ptr, size) _talloc_memdup_strict(ctx, ptr, size, __location__)
+#define TALLOC_ZERO(ctx, size) _talloc_zero_strict(ctx, size, __location__)
+#define TALLOC_ZERO_P(ctx, type) (type *)_talloc_zero_strict(ctx, sizeof(type), #type)
+#define TALLOC_ZERO_ARRAY(ctx, type, count) (type *)_talloc_zero_array_strict(ctx, sizeof(type), count, #type)
 #define TALLOC_REALLOC(ctx, ptr, count) _talloc_realloc(ctx, ptr, count, __location__)
 #define TALLOC_REALLOC_ARRAY(ctx, ptr, type, count) (type *)_talloc_realloc_array(ctx, ptr, sizeof(type), count, #type)
 #define talloc_destroy(ctx) talloc_free(ctx)
 #define TALLOC_FREE(ctx) do { if ((ctx) != NULL) {talloc_free(ctx); ctx=NULL;} } while(0)
+#define TALLOC_SIZE(ctx, size) talloc_strict(ctx, size)
+#define TALLOC_ZERO_SIZE(ctx, size) talloc_zero_size_strict(ctx, size)
 
 /* only define PARANOID_MALLOC_CHECKER with --enable-developer and not compiling
    the smbmount utils */

source/lib/data_blob.c
@@ -72,7 +72,7 @@ DATA_BLOB data_blob_talloc(TALLOC_CTX *mem_ctx, const void *p, size_t length)
 	if (p) {
 		ret.data = (uint8 *)TALLOC_MEMDUP(mem_ctx, p, length);
 		if (ret.data == NULL)
-			smb_panic("data_blob_talloc: talloc_memdup failed.\n");
+			smb_panic("data_blob_talloc: TALLOC_MEMDUP failed.\n");
 	} else {
 		ret.data = (uint8 *)TALLOC(mem_ctx, length);
 		if (ret.data == NULL)

source/lib/launchd.c
@@ -138,7 +138,7 @@ static void pull_launch_sockets(launch_data_t key,
 			    count, launch_data_dict_get_count(item)));
 		}
 
-		linfo->socket_list = talloc_array(NULL, int, count);
+		linfo->socket_list = TALLOC_ARRAY(NULL, int, count);
 		if (linfo->socket_list == NULL) {
 			goto done;
 		}
@@ -200,7 +200,7 @@ done:
 					linfo->num_sockets));
 
 		count = linfo->num_sockets;
-		linfo->socket_list = talloc_array(NULL, int, count);
+		linfo->socket_list = TALLOC_ARRAY(NULL, int, count);
 		if (linfo->socket_list == NULL) {
 			goto done;
 		}

source/libaddns/dns.h
@@ -88,13 +88,13 @@
 
 #include <talloc.h>
 
-#define TALLOC(ctx, size) talloc_named_const(ctx, size, __location__)
-#define TALLOC_P(ctx, type) (type *)talloc_named_const(ctx, sizeof(type), #type)
-#define TALLOC_ARRAY(ctx, type, count) (type *)_talloc_array(ctx, sizeof(type), count, #type)
-#define TALLOC_MEMDUP(ctx, ptr, size) _talloc_memdup(ctx, ptr, size, __location__)
-#define TALLOC_ZERO(ctx, size) _talloc_zero(ctx, size, __location__)
-#define TALLOC_ZERO_P(ctx, type) (type *)_talloc_zero(ctx, sizeof(type), #type)
-#define TALLOC_ZERO_ARRAY(ctx, type, count) (type *)_talloc_zero_array(ctx, sizeof(type), count, #type)
+#define TALLOC(ctx, size) talloc_strict(ctx, size, __location__)
+#define TALLOC_P(ctx, type) (type *)talloc_strict(ctx, sizeof(type), #type)
+#define TALLOC_ARRAY(ctx, type, count) (type *)_talloc_array_strict(ctx, sizeof(type), count, #type)
+#define TALLOC_MEMDUP(ctx, ptr, size) _talloc_memdup_strict(ctx, ptr, size, __location__)
+#define TALLOC_ZERO(ctx, size) _talloc_zero_strict(ctx, size, __location__)
+#define TALLOC_ZERO_P(ctx, type) (type *)_talloc_zero_strict(ctx, sizeof(type), #type)
+#define TALLOC_ZERO_ARRAY(ctx, type, count) (type *)_talloc_zero_array_strict(ctx, sizeof(type), count, #type)
 #define TALLOC_REALLOC(ctx, ptr, count) _talloc_realloc(ctx, ptr, count, __location__)
 #define TALLOC_REALLOC_ARRAY(ctx, ptr, type, count) (type *)_talloc_realloc_array(ctx, ptr, sizeof(type), count, #type)
 #define TALLOC_FREE(ctx) do { if ((ctx) != NULL) {talloc_free(ctx); ctx=NULL;} } while(0)

source/libmsrpc/cac_samr.c
@@ -176,7 +176,7 @@ DOM_SID *cac_get_domain_sid( CacServerHandle * hnd, TALLOC_CTX * mem_ctx,
 	if ( !fs.out.domain_sid )
 		return NULL;
 
-	sid = ( DOM_SID * ) talloc_memdup( mem_ctx,
+	sid = ( DOM_SID * ) TALLOC_MEMDUP( mem_ctx,
 					   &( fs.out.domain_sid->sid ),
 					   sizeof( DOM_SID ) );
 
@@ -718,13 +718,13 @@ int cac_SamGetGroupsForUser( CacServerHandle * hnd, TALLOC_CTX * mem_ctx,
 		return CAC_FAILURE;
 
 
-	rids_out = talloc_array( mem_ctx, uint32, num_groups_out );
+	rids_out = TALLOC_ARRAY( mem_ctx, uint32, num_groups_out );
 	if ( !rids_out ) {
 		hnd->status = NT_STATUS_NO_MEMORY;
 		return CAC_FAILURE;
 	}
 
-	attr_out = talloc_array( mem_ctx, uint32, num_groups_out );
+	attr_out = TALLOC_ARRAY( mem_ctx, uint32, num_groups_out );
 	if ( !attr_out ) {
 		hnd->status = NT_STATUS_NO_MEMORY;
 		return CAC_FAILURE;
@@ -1153,14 +1153,14 @@ int cac_SamEnumGroups( CacServerHandle * hnd, TALLOC_CTX * mem_ctx,
 		return CAC_FAILURE;
 	}
 
-	names_out = talloc_array( mem_ctx, char *, num_groups_out );
+	names_out = TALLOC_ARRAY( mem_ctx, char *, num_groups_out );
 	if ( !names_out ) {
 		hnd->status = NT_STATUS_NO_MEMORY;
 		TALLOC_FREE( acct_buf );
 		return CAC_FAILURE;
 	}
 
-	desc_out = talloc_array( mem_ctx, char *, num_groups_out );
+	desc_out = TALLOC_ARRAY( mem_ctx, char *, num_groups_out );
 	if ( !desc_out ) {
 		hnd->status = NT_STATUS_NO_MEMORY;
 		TALLOC_FREE( acct_buf );
@@ -1168,7 +1168,7 @@ int cac_SamEnumGroups( CacServerHandle * hnd, TALLOC_CTX * mem_ctx,
 		return CAC_FAILURE;
 	}
 
-	rids_out = talloc_array( mem_ctx, uint32, num_groups_out );
+	rids_out = TALLOC_ARRAY( mem_ctx, uint32, num_groups_out );
 	if ( !rids_out ) {
 		hnd->status = NT_STATUS_NO_MEMORY;
 		TALLOC_FREE( acct_buf );
@@ -1256,14 +1256,14 @@ int cac_SamEnumAliases( CacServerHandle * hnd, TALLOC_CTX * mem_ctx,
 	     NT_STATUS_V( STATUS_MORE_ENTRIES ) )
 		return CAC_FAILURE;
 
-	names_out = talloc_array( mem_ctx, char *, num_als_out );
+	names_out = TALLOC_ARRAY( mem_ctx, char *, num_als_out );
 	if ( !names_out ) {
 		hnd->status = NT_STATUS_NO_MEMORY;
 		TALLOC_FREE( acct_buf );
 		return CAC_FAILURE;
 	}
 
-	desc_out = talloc_array( mem_ctx, char *, num_als_out );
+	desc_out = TALLOC_ARRAY( mem_ctx, char *, num_als_out );
 	if ( !desc_out ) {
 		hnd->status = NT_STATUS_NO_MEMORY;
 		TALLOC_FREE( acct_buf );
@@ -1271,7 +1271,7 @@ int cac_SamEnumAliases( CacServerHandle * hnd, TALLOC_CTX * mem_ctx,
 		return CAC_FAILURE;
 	}
 
-	rids_out = talloc_array( mem_ctx, uint32, num_als_out );
+	rids_out = TALLOC_ARRAY( mem_ctx, uint32, num_als_out );
 	if ( !rids_out ) {
 		hnd->status = NT_STATUS_NO_MEMORY;
 		TALLOC_FREE( acct_buf );

source/libmsrpc/cac_winreg.c
@@ -775,14 +775,14 @@ int cac_RegEnumValues( CacServerHandle * hnd, TALLOC_CTX * mem_ctx,
 	}
 
 	/*we need to assume that the max number of values will be enumerated */
-	types_out = talloc_array( mem_ctx, uint32, op->in.max_values );
+	types_out = TALLOC_ARRAY( mem_ctx, uint32, op->in.max_values );
 
 	if ( !types_out ) {
 		hnd->status = NT_STATUS_NO_MEMORY;
 		return CAC_FAILURE;
 	}
 
-	values_out = talloc_array( mem_ctx, REG_VALUE_DATA *, 
+	values_out = TALLOC_ARRAY( mem_ctx, REG_VALUE_DATA *, 
 				   op->in.max_values );
 
 	if ( !values_out ) {
@@ -791,7 +791,7 @@ int cac_RegEnumValues( CacServerHandle * hnd, TALLOC_CTX * mem_ctx,
 		return CAC_FAILURE;
 	}
 
-	val_names_out = talloc_array( mem_ctx, char *, op->in.max_values );
+	val_names_out = TALLOC_ARRAY( mem_ctx, char *, op->in.max_values );
 
 	if ( !val_names_out ) {
 		TALLOC_FREE( types_out );

commit 547c70e896b6890dd5f7b911e4e0ca107c8a3fa3

s3-eventlog: fix buffer allocation in eventlog read call.
This broke with f6fa3080fee1b20df9f1968500840a88cf0ee592 back in April 2007...

Guenther

source/rpc_parse/parse_eventlog.c
@@ -166,6 +166,7 @@ bool eventlog_io_r_read_eventlog(const char *desc,
 	/* Now pad with whitespace until the end of the response buffer */
 
 	if (q_u->max_read_size - r_u->num_bytes_in_resp) {
+		r_u->end_of_entries_padding = PRS_ALLOC_MEM(ps, uint8_t, q_u->max_read_size - r_u->num_bytes_in_resp);
 		if (!r_u->end_of_entries_padding) {
 			return False;
 		}
@@ -173,11 +174,8 @@ bool eventlog_io_r_read_eventlog(const char *desc,
 		if(!(prs_uint8s(False, "end of entries padding", ps, 
 				depth, r_u->end_of_entries_padding,
 				(q_u->max_read_size - r_u->num_bytes_in_resp)))) {
-			free(r_u->end_of_entries_padding);
 			return False;
 		}
-
-		free(r_u->end_of_entries_padding);
 	}
 
 	/* We had better be DWORD aligned here */