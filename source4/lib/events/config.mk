d6497aa0894b20392d759e1762ad1bb6ab29f5c8
Jelmer Vernooij <jelmer@samba.org>
2009-01-28 17:06:18 +0100

    Stop installing events.h - it contains a broken include itself and should be considered internal now.

--- source4/lib/events/config.mk
+++ source4/lib/events/config.mk
@@ -3,5 +3,3 @@ PUBLIC_DEPENDENCIES = LIBTEVENT
 CFLAGS = -Ilib/events
 
 LIBEVENTS_OBJ_FILES = $(addprefix $(libeventssrcdir)/, tevent_s4.o)
-
-PUBLIC_HEADERS += $(addprefix $(libeventssrcdir)/, events.h)
