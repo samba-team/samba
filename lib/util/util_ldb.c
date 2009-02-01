10c907bcdb5856feb15bd6b096254ea5d112a8c9
Simo Sorce <idra@samba.org>
2009-02-01 13:06:13 -0500

    Fix headers, only ldb.h is required

--- lib/util/util_ldb.c
+++ lib/util/util_ldb.c
@@ -21,9 +21,7 @@
 */
 
 #include "includes.h"
-#include "lib/events/events.h"
 #include "lib/ldb/include/ldb.h"
-#include "lib/ldb/include/ldb_errors.h"
 #include "../lib/util/util_ldb.h"
 /*
   search the sam for the specified attributes - va_list variant
