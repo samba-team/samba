91c292249b6368a6212055692747ed818ce683cc
Stefan Metzmacher <metze@samba.org>
2009-02-02 14:55:47 +0100

    s4:lib/ldb: we should not silence warnings on handwritten python bindings
    metze

--- source4/lib/ldb/python.mk
+++ source4/lib/ldb/python.mk
@@ -4,5 +4,3 @@ PUBLIC_DEPENDENCIES = LIBLDB PYTALLOC
 
 pyldb_OBJ_FILES = $(ldbsrcdir)/pyldb.o
 $(pyldb_OBJ_FILES): CFLAGS+=-I$(ldbsrcdir)/include
-
-$(pyldb_OBJ_FILES): CFLAGS+=$(CFLAG_NO_CAST_QUAL)
