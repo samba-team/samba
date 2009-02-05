ef098bd621b544a651636db4b7d6cdd777c83d18
Günther Deschner <gd@samba.org>
2009-02-05 01:05:09 +0100

    s3-lsa: move remaining lsa policy access_mask defines to idl.
    Guenther

--- source3/include/includes.h
+++ source3/include/includes.h
@@ -619,7 +619,6 @@ struct smb_iconv_convenience *lp_iconv_convenience(void *lp_ctx);
 #include "ntlmssp.h"
 #include "auth.h"
 #include "ntdomain.h"
-#include "rpc_lsa.h"
 #include "reg_objects.h"
 #include "reg_db.h"
 #include "rpc_spoolss.h"
