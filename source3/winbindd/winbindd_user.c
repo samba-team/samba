a63f6024739e922e0bb6c929a1b9112e5e8e09de
Michael Adam <obnox@samba.org>
2009-02-09 11:07:11 +0100

    s3:winbindd_user: fix a debug message.
    find_domain_from_name_noinit() is no longer called only for
    name alias support.
    
    Michael

--- source3/winbindd/winbindd_user.c
+++ source3/winbindd/winbindd_user.c
@@ -82,8 +82,8 @@ static bool winbindd_fill_pwent(TALLOC_CTX *ctx, char *dom_name, char *user_name
 
 	domain = find_domain_from_name_noinit(dom_name);
 	if (domain == NULL) {
-		DEBUG(5,("winbindd_fill_pwent: Failed to find domain for %s.  "
-			 "Disabling name alias support\n", dom_name));
+		DEBUG(5,("winbindd_fill_pwent: Failed to find domain for %s.\n",
+			 dom_name));
 		nt_status = NT_STATUS_NO_SUCH_DOMAIN;
 		return false;
 	}
