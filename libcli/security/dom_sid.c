e304a623f10ac9587cad8cbfcfa83186a872e8ff
Volker Lendecke <vl@samba.org>
2009-02-14 17:42:51 +0100

    Add missing include to shut up missing prototype warnings

--- libcli/security/dom_sid.c
+++ libcli/security/dom_sid.c
@@ -22,6 +22,7 @@
 
 #include "includes.h"
 #include "librpc/gen_ndr/security.h"
+#include "dom_sid.h"
 
 /*****************************************************************
  Compare the auth portion of two sids.
