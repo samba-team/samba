4c5861c66715625ad6afce34fe24493d823ee9c6
Love Hörnquist Åstrand <lha@kth.se>
2009-02-22 23:24:25 +0000

    add conf_state to iov_length
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@24773 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/gssapi/mech/gss_aeap.c
+++ lib/gssapi/mech/gss_aeap.c
@@ -103,6 +103,7 @@ gss_wrap_iov_length(OM_uint32 * minor_status,
 		    gss_ctx_id_t context_handle,
 		    int conf_req_flag,
 		    gss_qop_t qop_req,
+		    int *conf_state,
 		    gss_iov_buffer_desc *iov,
 		    int iov_count)
 {
@@ -124,7 +125,7 @@ gss_wrap_iov_length(OM_uint32 * minor_status,
 	}
 
 	return (m->gm_wrap_iov_length)(minor_status, ctx->gc_ctx,
-				       conf_req_flag, qop_req,
+				       conf_req_flag, qop_req, conf_state,
 				       iov, iov_count);
 }
 
