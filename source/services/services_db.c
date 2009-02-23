733d4b54546d6c7c9350cf522d4980877edac6d4
Jeremy Allison <jra@samba.org>
2009-02-23 15:42:52 -0800

    More warning fixes for Solaris. Jeremy. (cherry picked from commit 6306dd0cab617930176fdde91641344ef403429b)

--- source/services/services_db.c
+++ source/services/services_db.c
@@ -91,7 +91,7 @@ static SEC_DESC* construct_service_sd( TALLOC_CTX *ctx )
 	SEC_ACE ace[4];
 	size_t i = 0;
 	SEC_DESC *sd = NULL;
-	SEC_ACL *acl = NULL;
+	SEC_ACL *theacl = NULL;
 	size_t sd_size;
 
 	/* basic access for Everyone */
@@ -109,12 +109,12 @@ static SEC_DESC* construct_service_sd( TALLOC_CTX *ctx )
 
 	/* create the security descriptor */
 
-	if ( !(acl = make_sec_acl(ctx, NT4_ACL_REVISION, i, ace)) )
+	if ( !(theacl = make_sec_acl(ctx, NT4_ACL_REVISION, i, ace)) )
 		return NULL;
 
 	if ( !(sd = make_sec_desc(ctx, SECURITY_DESCRIPTOR_REVISION_1,
 				  SEC_DESC_SELF_RELATIVE, NULL, NULL, NULL,
-				  acl, &sd_size)) )
+				  theacl, &sd_size)) )
 		return NULL;
 
 	return sd;
