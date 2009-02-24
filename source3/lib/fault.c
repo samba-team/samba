samba-team/samba

commit 07e0094365e8dc360a83eec2e7cf9b1d5d8d6d00

Fix all warnings in source3 with gcc4.3. Jeremy.

source3/lib/fault.c
@@ -119,7 +119,18 @@ void dump_core_setup(const char *progname)
 		SAFE_FREE(logbase);
 		return;
 	}
-	mkdir(corepath,0700);
+	if (mkdir(corepath,0700) == -1) {
+		if (errno != EEXIST) {
+			SAFE_FREE(corepath);
+			SAFE_FREE(logbase);
+			return;
+		}
+	}
+	if (chmod(corepath,0700) == -1) {
+		SAFE_FREE(corepath);
+		SAFE_FREE(logbase);
+		return;
+	}
 
 	SAFE_FREE(corepath);
 	if (asprintf(&corepath, "%s/cores/%s",
@@ -127,11 +138,26 @@ void dump_core_setup(const char *progname)
 		SAFE_FREE(logbase);
 		return;
 	}
-	mkdir(corepath,0700);
+	if (mkdir(corepath,0700) == -1) {
+		if (errno != EEXIST) {
+			SAFE_FREE(corepath);
+			SAFE_FREE(logbase);
+			return;
+		}
+	}
 
-	chown(corepath,getuid(),getgid());
-	chmod(corepath,0700);
+	if (chown(corepath,getuid(),getgid()) == -1) {
+		SAFE_FREE(corepath);
+		SAFE_FREE(logbase);
+		return;
+	}
+	if (chmod(corepath,0700) == -1) {
+		SAFE_FREE(corepath);
+		SAFE_FREE(logbase);
+		return;
+	}
 
+	SAFE_FREE(corepath);
 	SAFE_FREE(logbase);
 
 #ifdef HAVE_GETRLIMIT

lib/tdb/tools/tdbtool.c
@@ -506,7 +506,9 @@ static int do_command(void)
             return 0;
 	case CMD_SYSTEM:
 	    /* Shell command */
-	    system(arg1);
+	    if (system(arg1) == -1) {
+		terror("system() call failed\n");
+	    }
 	    return 0;
 	case CMD_QUIT:
 	    return 1;

librpc/ndr/ndr_basic.c
@@ -757,8 +757,7 @@ _PUBLIC_ void ndr_print_array_uint8(struct ndr_print *ndr, const char *name,
 	ndr->depth++;
 	for (i=0;i<count;i++) {
 		char *idx=NULL;
-		asprintf(&idx, "[%d]", i);
-		if (idx) {
+		if (asprintf(&idx, "[%d]", i) == -1) {
 			ndr_print_uint8(ndr, idx, data[i]);
 			free(idx);
 		}

source3/client/clitar.c
@@ -329,13 +329,13 @@ static int dotarbuf(int f, char *b, int n)
 
 		diff=tbufsiz-tp;
 		memcpy(tarbuf + tp, b, diff);
-		fail=fail && (1+write(f, tarbuf, tbufsiz));
+		fail=fail && (1+sys_write(f, tarbuf, tbufsiz));
 		n-=diff;
 		b+=diff;
 		tp=0;
 
 		while (n >= tbufsiz) {
-			fail=fail && (1 + write(f, b, tbufsiz));
+			fail=fail && (1 + sys_write(f, b, tbufsiz));
 			n-=tbufsiz;
 			b+=tbufsiz;
 		}
@@ -364,7 +364,10 @@ static void dozerobuf(int f, int n)
 
 	if (n+tp >= tbufsiz) {
 		memset(tarbuf+tp, 0, tbufsiz-tp);
-		write(f, tarbuf, tbufsiz);
+		if (sys_write(f, tarbuf, tbufsiz) != tbufsiz) {
+			DEBUG(0, ("dozerobuf: sys_write fail\n"));
+			return;
+		}
 		memset(tarbuf, 0, (tp+=n-tbufsiz));
 	} else {
 		memset(tarbuf+tp, 0, n);
@@ -408,8 +411,12 @@ static void dotareof(int f)
 
 	/* Could be a pipe, in which case S_ISREG should fail,
 		* and we should write out at full size */
-	if (tp > 0)
-		write(f, tarbuf, S_ISREG(stbuf.st_mode) ? tp : tbufsiz);
+	if (tp > 0) {
+		size_t towrite = S_ISREG(stbuf.st_mode) ? tp : tbufsiz;
+		if (sys_write(f, tarbuf, towrite) != towrite) {
+			DEBUG(0,("dotareof: sys_write fail\n"));
+		}
+	}
 }
 
 /****************************************************************************

source3/modules/vfs_netatalk.c
@@ -367,7 +367,9 @@ static int atalk_chown(struct vfs_handle_struct *handle, const char *path, uid_t
 		goto exit_chown;
 	}
 
-	chown(adbl_path, uid, gid);
+	if (chown(adbl_path, uid, gid) == -1) {
+		DEBUG(3, ("ATALK: chown error %s\n", strerror(errno)));
+	}
 
 exit_chown:	
 	talloc_destroy(ctx);
@@ -399,7 +401,9 @@ static int atalk_lchown(struct vfs_handle_struct *handle, const char *path, uid_
 		goto exit_lchown;
 	}
 
-	lchown(adbl_path, uid, gid);
+	if (lchown(adbl_path, uid, gid) == -1) {
+		DEBUG(3, ("ATALK: lchown error %s\n", strerror(errno)));
+	}
 
 exit_lchown:	
 	talloc_destroy(ctx);

source3/modules/vfs_recycle.c
@@ -525,7 +525,9 @@ static int recycle_unlink(vfs_handle_struct *handle, const char *file_name)
 	}
 
 	if (recycle_keep_dir_tree(handle) == True) {
-		asprintf(&temp_name, "%s/%s", repository, path_name);
+		if (asprintf(&temp_name, "%s/%s", repository, path_name) == -1) {
+			ALLOC_CHECK(temp_name, done);
+		}
 	} else {
 		temp_name = SMB_STRDUP(repository);
 	}
@@ -543,8 +545,9 @@ static int recycle_unlink(vfs_handle_struct *handle, const char *file_name)
 		}
 	}
 
-	asprintf(&final_name, "%s/%s", temp_name, base);
-	ALLOC_CHECK(final_name, done);
+	if (asprintf(&final_name, "%s/%s", temp_name, base) == -1) {
+		ALLOC_CHECK(final_name, done);
+	}
 	DEBUG(10, ("recycle: recycled file name: %s\n", final_name));		/* new filename with path */
 
 	/* check if we should delete file from recycle bin */
@@ -561,7 +564,9 @@ static int recycle_unlink(vfs_handle_struct *handle, const char *file_name)
 	i = 1;
 	while (recycle_file_exist(handle, final_name)) {
 		SAFE_FREE(final_name);
-		asprintf(&final_name, "%s/Copy #%d of %s", temp_name, i++, base);
+		if (asprintf(&final_name, "%s/Copy #%d of %s", temp_name, i++, base) == -1) {
+			ALLOC_CHECK(final_name, done);
+		}
 	}
 
 	DEBUG(10, ("recycle: Moving %s to %s\n", file_name, final_name));

source3/rpcclient/cmd_spoolss.c
@@ -1655,9 +1655,14 @@ static WERROR cmd_spoolss_addform(struct rpc_pipe_client *cli, TALLOC_CTX *mem_c
 	
 	/* Get a printer handle */
 
-	asprintf(&servername, "\\\\%s", cli->desthost);
+	if (asprintf(&servername, "\\\\%s", cli->desthost) == -1) {
+		return WERR_NOMEM;
+	}
 	strupper_m(servername);
-	asprintf(&printername, "%s\\%s", servername, argv[1]);
+	if (asprintf(&printername, "%s\\%s", servername, argv[1]) == -1) {
+		SAFE_FREE(servername);
+		return WERR_NOMEM;
+	}
 
 	werror = rpccli_spoolss_open_printer_ex(cli, mem_ctx, printername, "", 
 					     PRINTER_ALL_ACCESS, 
@@ -1716,9 +1721,14 @@ static WERROR cmd_spoolss_setform(struct rpc_pipe_client *cli, TALLOC_CTX *mem_c
 	
 	/* Get a printer handle */
 
-	asprintf(&servername, "\\\\%s", cli->desthost);
+	if (asprintf(&servername, "\\\\%s", cli->desthost)) {
+		return WERR_NOMEM;
+	}
 	strupper_m(servername);
-	asprintf(&printername, "%s\\%s", servername, argv[1]);
+	if (asprintf(&printername, "%s\\%s", servername, argv[1]) == -1) {
+		SAFE_FREE(servername);
+		return WERR_NOMEM;
+	}
 
 	werror = rpccli_spoolss_open_printer_ex(
 		cli, mem_ctx, printername, "", MAXIMUM_ALLOWED_ACCESS, 
@@ -1813,9 +1823,14 @@ static WERROR cmd_spoolss_getform(struct rpc_pipe_client *cli, TALLOC_CTX *mem_c
 	
 	/* Get a printer handle */
 
-	asprintf(&servername, "\\\\%s", cli->desthost);
+	if (asprintf(&servername, "\\\\%s", cli->desthost) == -1) {
+		return WERR_NOMEM;
+	}
 	strupper_m(servername);
-	asprintf(&printername, "%s\\%s", servername, argv[1]);
+	if (asprintf(&printername, "%s\\%s", servername, argv[1])) {
+		SAFE_FREE(servername);
+		return WERR_NOMEM;
+	}
 
 	werror = rpccli_spoolss_open_printer_ex(
 		cli, mem_ctx, printername, "", MAXIMUM_ALLOWED_ACCESS, 
@@ -1866,9 +1881,14 @@ static WERROR cmd_spoolss_deleteform(struct rpc_pipe_client *cli,
 	
 	/* Get a printer handle */
 
-	asprintf(&servername, "\\\\%s", cli->desthost);
+	if (asprintf(&servername, "\\\\%s", cli->desthost) == -1) {
+		return WERR_NOMEM;
+	}
 	strupper_m(servername);
-	asprintf(&printername, "%s\\%s", servername, argv[1]);
+	if (asprintf(&printername, "%s\\%s", servername, argv[1]) == -1) {
+		SAFE_FREE(servername);
+		return WERR_NOMEM;
+	}
 
 	werror = rpccli_spoolss_open_printer_ex(
 		cli, mem_ctx, printername, "", MAXIMUM_ALLOWED_ACCESS, 
@@ -1916,9 +1936,14 @@ static WERROR cmd_spoolss_enum_forms(struct rpc_pipe_client *cli,
 	
 	/* Get a printer handle */
 
-	asprintf(&servername, "\\\\%s", cli->desthost);
+	if (asprintf(&servername, "\\\\%s", cli->desthost) == -1) {
+		return WERR_NOMEM;
+	}
 	strupper_m(servername);
-	asprintf(&printername, "%s\\%s", servername, argv[1]);
+	if (asprintf(&printername, "%s\\%s", servername, argv[1]) == -1) {
+		SAFE_FREE(servername);
+		return WERR_NOMEM;
+	}
 
 	werror = rpccli_spoolss_open_printer_ex(
 		cli, mem_ctx, printername, "", MAXIMUM_ALLOWED_ACCESS, 

source3/utils/eventlogadm.c
@@ -111,7 +111,9 @@ static int DoWriteCommand( int argc, char **argv, bool debugflag, char *exename 
 	ZERO_STRUCT( ee );	/* MUST initialize between records */
 
 	while ( !feof( f1 ) ) {
-		fgets( linein, sizeof( linein ) - 1, f1 );
+		if (fgets( linein, sizeof( linein ) - 1, f1 ) == NULL) {
+			break;
+		}
 		linein[strlen( linein ) - 1] = 0;	/* whack the line delimiter */
 
 		if ( debugflag )

commit 9bb1bea53709dd32b8c3bf6032b38d9f95c8c379

s3: Fix a bug that prevent core files from being created
Removed an erroneous free() that was causing the corepath to be NULL
during dump_core().  This prevented dump_core() from actually calling
abort() to create a core file.  The bug was introduced in December by:
07e0094365e8dc360a83eec2e7cf9b1d5d8d6d00

source3/lib/fault.c
@@ -157,7 +157,6 @@ void dump_core_setup(const char *progname)
 		return;
 	}
 
-	SAFE_FREE(corepath);
 	SAFE_FREE(logbase);
 
 #ifdef HAVE_GETRLIMIT