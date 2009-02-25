9fe089291df30035974b750b4da7e6fa20b9c8a5
Jeremy Allison <jra@samba.org>
2009-02-25 13:01:04 -0800

    Fix bug in processing of open modes in POSIX open. Was missing case of "If file exists open. If file doesn't exist error." Damn damn damn. CIFSFS client will have to have fallback cases for this error for a long time.
    Make test for open modes more robust against other bits.
    
    Jeremy.
    (cherry picked from commit ac11d94f36e1878f3f5d86f2e7197fd8ecdd196b)

--- source/smbd/trans2.c
+++ source/smbd/trans2.c
@@ -5463,6 +5463,8 @@ static NTSTATUS smb_posix_open(connection_struct *conn,
 		create_disp = FILE_OVERWRITE_IF;
 	} else if((wire_open_mode & SMB_O_CREAT) == SMB_O_CREAT) {
 		create_disp = FILE_OPEN_IF;
+	} else if ((wire_open_mode & (SMB_O_CREAT | SMB_O_EXCL | SMB_O_TRUNC)) == 0) {
+		create_disp = FILE_OPEN;
 	} else {
 		DEBUG(5,("smb_posix_open: invalid create mode 0x%x\n",
 			(unsigned int)wire_open_mode ));
