80c1ff42ed5bb980641b5f1ce0b96bfccc83652b
Günther Deschner <gd@samba.org>
2009-02-25 14:33:59 +0100

    spoolss: add my copyright.
    Guenther
    (cherry picked from commit 9d1ab9a0013127d08bad01975a5204e8f63b9ddc)
    (cherry picked from commit ecc7537b6b0c7921160448b97a3cd8df1f4c77cf)

--- source3/utils/net_rpc_printer.c
+++ source3/utils/net_rpc_printer.c
@@ -1,7 +1,7 @@
 /*
    Samba Unix/Linux SMB client library
    Distributed SMB/CIFS Server Management Utility
-   Copyright (C) 2004 Guenther Deschner (gd@samba.org)
+   Copyright (C) 2004,2009 Guenther Deschner (gd@samba.org)
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
