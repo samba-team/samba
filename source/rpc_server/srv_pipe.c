803aa8da6d2a7f32359a090062ab468cf8d6ce32
Jeremy Allison <jra@samba.org>
2009-03-05 20:59:48 -0800

    Now we're allowing a lower bound for auth_len, ensure we also check for an upper one (integer wrap). Jeremy. (cherry picked from commit f03bacbf695f877d27186a39755ae726a22a61c8)

--- source/rpc_server/srv_pipe.c
+++ source/rpc_server/srv_pipe.c
@@ -2062,7 +2062,11 @@ BOOL api_pipe_schannel_process(pipes_struct *p, prs_struct *rpc_in, uint32 *p_ss
 
 	auth_len = p->hdr.auth_len;
 
-	if (auth_len < RPC_AUTH_SCHANNEL_SIGN_OR_SEAL_CHK_LEN) {
+	if (auth_len < RPC_AUTH_SCHANNEL_SIGN_OR_SEAL_CHK_LEN ||
+			auth_len < RPC_HEADER_LEN +
+					RPC_HDR_REQ_LEN +
+					RPC_HDR_AUTH_LEN +
+					auth_len) {
 		DEBUG(0,("Incorrect auth_len %u.\n", (unsigned int)auth_len ));
 		return False;
 	}
