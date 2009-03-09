f2c2265aefe27ccf701deff1766f6d6c502f6002
Günther Deschner <gd@samba.org>
2009-03-09 18:23:40 +0100

    s3-spoolss: use pidl for _spoolss_EnumPrinterDrivers.
    Guenther
    (cherry picked from commit d6e2ef51ecb430d9dfd7221b6d4a030325d05b4f)
    (cherry picked from commit 3ee427d0b18a986ed063572c6a25f130aa0e0d7b)

--- source3/rpc_server/srv_spoolss.c
+++ source3/rpc_server/srv_spoolss.c
@@ -375,27 +375,7 @@ static bool api_spoolss_setjob(pipes_struct *p)
 
 static bool api_spoolss_enumprinterdrivers(pipes_struct *p)
 {
-	SPOOL_Q_ENUMPRINTERDRIVERS q_u;
-	SPOOL_R_ENUMPRINTERDRIVERS r_u;
-	prs_struct *data = &p->in_data.data;
-	prs_struct *rdata = &p->out_data.rdata;
-
-	ZERO_STRUCT(q_u);
-	ZERO_STRUCT(r_u);
-
-	if (!spoolss_io_q_enumprinterdrivers("", &q_u, data, 0)) {
-		DEBUG(0,("spoolss_io_q_enumprinterdrivers: unable to unmarshall SPOOL_Q_ENUMPRINTERDRIVERS.\n"));
-		return False;
-	}
-
-	r_u.status = _spoolss_enumprinterdrivers(p, &q_u, &r_u);
-
-	if (!spoolss_io_r_enumprinterdrivers("",&r_u,rdata,0)) {
-		DEBUG(0,("spoolss_io_r_enumprinterdrivers: unable to marshall SPOOL_R_ENUMPRINTERDRIVERS.\n"));
-		return False;
-	}
-
-	return True;
+	return proxy_spoolss_call(p, NDR_SPOOLSS_ENUMPRINTERDRIVERS);
 }
 
 /****************************************************************************
