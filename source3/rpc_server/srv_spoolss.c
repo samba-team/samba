9b143e6de5c40a225c364b9377af52a8c7f6ca23
Günther Deschner <gd@samba.org>
2009-03-09 12:24:45 +0100

    s3-spoolss: use pidl for _spoolss_GetJob.
    Guenther

--- source3/rpc_server/srv_spoolss.c
+++ source3/rpc_server/srv_spoolss.c
@@ -569,27 +569,7 @@ static bool api_spoolss_enumprintmonitors(pipes_struct *p)
 
 static bool api_spoolss_getjob(pipes_struct *p)
 {
-	SPOOL_Q_GETJOB q_u;
-	SPOOL_R_GETJOB r_u;
-	prs_struct *data = &p->in_data.data;
-	prs_struct *rdata = &p->out_data.rdata;
-	
-	ZERO_STRUCT(q_u);
-	ZERO_STRUCT(r_u);
-	
-	if(!spoolss_io_q_getjob("", &q_u, data, 0)) {
-		DEBUG(0,("spoolss_io_q_getjob: unable to unmarshall SPOOL_Q_GETJOB.\n"));
-		return False;
-	}
-
-	r_u.status = _spoolss_getjob(p, &q_u, &r_u);
-	
-	if(!spoolss_io_r_getjob("",&r_u,rdata,0)) {
-		DEBUG(0,("spoolss_io_r_getjob: unable to marshall SPOOL_R_GETJOB.\n"));
-		return False;
-	}
-		
-	return True;
+	return proxy_spoolss_call(p, NDR_SPOOLSS_GETJOB);
 }
 
 /********************************************************************
