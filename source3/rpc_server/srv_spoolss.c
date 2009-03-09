33d0c7655b3623981abbb180099b84c56d59830a
Günther Deschner <gd@samba.org>
2009-03-10 00:51:38 +0100

    s3-spoolss: use pidl for _spoolss_EnumPrinters.
    Guenther
    (cherry picked from commit f6f703f16ef3b6a061801776e4bcc76fb04ad21a)

--- source3/rpc_server/srv_spoolss.c
+++ source3/rpc_server/srv_spoolss.c
@@ -190,27 +190,7 @@ static bool api_spoolss_rfnpcnex(pipes_struct *p)
 
 static bool api_spoolss_enumprinters(pipes_struct *p)
 {
-	SPOOL_Q_ENUMPRINTERS q_u;
-	SPOOL_R_ENUMPRINTERS r_u;
-	prs_struct *data = &p->in_data.data;
-	prs_struct *rdata = &p->out_data.rdata;
-
-	ZERO_STRUCT(q_u);
-	ZERO_STRUCT(r_u);
-
-	if (!spoolss_io_q_enumprinters("", &q_u, data, 0)) {
-		DEBUG(0,("spoolss_io_q_enumprinters: unable to unmarshall SPOOL_Q_ENUMPRINTERS.\n"));
-		return False;
-	}
-
-	r_u.status = _spoolss_enumprinters( p, &q_u, &r_u);
-
-	if (!spoolss_io_r_enumprinters("", &r_u, rdata, 0)) {
-		DEBUG(0,("spoolss_io_r_enumprinters: unable to marshall SPOOL_R_ENUMPRINTERS.\n"));
-		return False;
-	}
-
-	return True;
+	return proxy_spoolss_call(p, NDR_SPOOLSS_ENUMPRINTERS);
 }
 
 /********************************************************************
