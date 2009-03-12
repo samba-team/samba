0bf62f157063bf9ab46ab5b9417ee618c4f51203
Günther Deschner <gd@samba.org>
2009-03-12 10:57:13 +0100

    s3-spoolss: use pidl for _spoolss_GetPrinter.
    Guenther
    (cherry picked from commit 1ceb298ed62be9a40d1e323003e1a9916d5688db)
    (cherry picked from commit 1497620da3234a58ef6121b22d3e85bb913c1f0e)

--- source3/rpc_server/srv_spoolss.c
+++ source3/rpc_server/srv_spoolss.c
@@ -201,27 +201,7 @@ static bool api_spoolss_enumprinters(pipes_struct *p)
 
 static bool api_spoolss_getprinter(pipes_struct *p)
 {
-	SPOOL_Q_GETPRINTER q_u;
-	SPOOL_R_GETPRINTER r_u;
-	prs_struct *data = &p->in_data.data;
-	prs_struct *rdata = &p->out_data.rdata;
-
-	ZERO_STRUCT(q_u);
-	ZERO_STRUCT(r_u);
-
-	if(!spoolss_io_q_getprinter("", &q_u, data, 0)) {
-		DEBUG(0,("spoolss_io_q_getprinter: unable to unmarshall SPOOL_Q_GETPRINTER.\n"));
-		return False;
-	}
-
-	r_u.status = _spoolss_getprinter(p, &q_u, &r_u);
-
-	if(!spoolss_io_r_getprinter("",&r_u,rdata,0)) {
-		DEBUG(0,("spoolss_io_r_getprinter: unable to marshall SPOOL_R_GETPRINTER.\n"));
-		return False;
-	}
-
-	return True;
+	return proxy_spoolss_call(p, NDR_SPOOLSS_GETPRINTER);
 }
 
 /********************************************************************
