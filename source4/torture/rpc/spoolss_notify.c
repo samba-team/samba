2c789ad04a665f493b1fb1a55414eff0309e4dfe
Günther Deschner <gd@samba.org>
2009-03-17 18:03:36 +0100

    s4-smbtorture: fix spoolss notify test for notify type changes.
    Guenther
    (cherry picked from commit fac7208273a24fa35d3274df28aea3bb87374861)
    (cherry picked from commit 37141ad861d0bd4c215350c7611132a40748a8bf)

--- source4/torture/rpc/spoolss_notify.c
+++ source4/torture/rpc/spoolss_notify.c
@@ -252,12 +252,12 @@ static bool test_RFFPCNEx(struct torture_context *tctx,
 	t1.flags = 0;
 	t1.count = 2;
 	t1.types = talloc_zero_array(tctx, struct spoolss_NotifyOptionType, 2);
-	t1.types[0].type = SPOOLSS_NOTIFY_PRINTER;
+	t1.types[0].type = PRINTER_NOTIFY_TYPE;
 	t1.types[0].count = 1;
 	t1.types[0].fields = talloc_array(t1.types, enum spoolss_Field, 1);
 	t1.types[0].fields[0] = SPOOLSS_FIELD_SERVER_NAME;
 
-	t1.types[1].type = SPOOLSS_NOTIFY_JOB;
+	t1.types[1].type = JOB_NOTIFY_TYPE;
 	t1.types[1].count = 1;
 	t1.types[1].fields = talloc_array(t1.types, enum spoolss_Field, 1);
 	t1.types[1].fields[0] = SPOOLSS_FIELD_PRINTER_NAME;
