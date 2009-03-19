0c11c2deee7ab9d281df6df91b793b7f1c1764a4
Jeremy Allison <jra@samba.org>
2009-03-18 20:54:13 -0700

    Allow DFS client paths to work when POSIX pathnames have been selected (we need to path in pathname /that/look/like/this). Jeremy.

--- source/libsmb/clidfs.c
+++ source/libsmb/clidfs.c
@@ -665,13 +665,23 @@ static char *cli_dfs_make_full_path(TALLOC_CTX *ctx,
 					struct cli_state *cli,
 					const char *dir)
 {
+	char path_sep = '\\';
+
 	/* Ensure the extrapath doesn't start with a separator. */
 	while (IS_DIRECTORY_SEP(*dir)) {
 		dir++;
 	}
 
-	return talloc_asprintf(ctx, "\\%s\\%s\\%s",
-			cli->desthost, cli->share, dir);
+	if (cli->posix_capabilities & CIFS_UNIX_POSIX_PATHNAMES_CAP) {
+		path_sep = '/';
+	}
+	return talloc_asprintf(ctx, "%c%s%c%s%c%s",
+			path_sep,
+			cli->desthost,
+			path_sep,
+			cli->share,
+			path_sep,
+			dir);
 }
 
 /********************************************************************
