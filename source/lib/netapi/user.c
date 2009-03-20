9bf3e129605d51483fd5ce47c90642f7e82ac102
Günther Deschner <gd@samba.org>
2009-03-20 11:36:00 +0100

    s3-netapi: Fix Coverity #774 (REVERSE_INULL).
    Guenther

--- source/lib/netapi/user.c
+++ source/lib/netapi/user.c
@@ -2849,6 +2849,7 @@ WERROR NetUserGetGroups_r(struct libnetapi_ctx *ctx,
 
 	*r->out.buffer = NULL;
 	*r->out.entries_read = 0;
+	*r->out.total_entries = 0;
 
 	switch (r->in.level) {
 		case 0:
@@ -2943,12 +2944,8 @@ WERROR NetUserGetGroups_r(struct libnetapi_ctx *ctx,
 		}
 	}
 
-	if (r->out.entries_read) {
-		*r->out.entries_read = entries_read;
-	}
-	if (r->out.total_entries) {
-		*r->out.total_entries = entries_read;
-	}
+	*r->out.entries_read = entries_read;
+	*r->out.total_entries = entries_read;
 
  done:
 	if (!cli) {
