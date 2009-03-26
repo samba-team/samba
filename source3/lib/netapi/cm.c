samba-team/samba

commit 8dd1faaa2992851f6852ba7ea4498445af5faadd

Remove the global "struct cm_cred_struct" and associated calls, make callers pass in a struct user_auth_info * instead. This commit causes smbc_set_credentials() to print out a message telling callers to use smbc_set_credentials_with_fallback() instead, as smbc_set_credentials() has a broken API (no SMBCCTX * pointer). No more global variables used in the connection manager API for client dfs calls. Jeremy.

source3/lib/netapi/cm.c
@@ -29,36 +29,36 @@ static WERROR libnetapi_open_ipc_connection(struct libnetapi_ctx *ctx,
 					    const char *server_name,
 					    struct cli_state **cli)
 {
+	struct user_auth_info *auth_info = NULL;
 	struct cli_state *cli_ipc = NULL;
 
 	if (!ctx || !cli || !server_name) {
 		return WERR_INVALID_PARAM;
 	}
 
-	cli_cm_set_signing_state(Undefined);
-
-	if (ctx->use_kerberos) {
-		cli_cm_set_use_kerberos();
+	auth_info = user_auth_info_init(NULL);
+	if (!auth_info) {
+		return WERR_NOMEM;
 	}
-
-	if (ctx->password) {
-		cli_cm_set_password(ctx->password);
-	}
-	if (ctx->username) {
-		cli_cm_set_username(ctx->username);
-	}
+	auth_info->signing_state = Undefined;
+	set_cmdline_auth_info_use_kerberos(auth_info, ctx->use_kerberos);
+	set_cmdline_auth_info_password(auth_info, ctx->password);
+	set_cmdline_auth_info_username(auth_info, ctx->username);
 
 	if (ctx->username && ctx->username[0] &&
 	    ctx->password && ctx->password[0] &&
 	    ctx->use_kerberos) {
-		cli_cm_set_fallback_after_kerberos();
+		set_cmdline_auth_info_fallback_after_kerberos(auth_info, true);
 	}
 
 	cli_ipc = cli_cm_open(ctx, NULL,
-			      server_name, "IPC$",
-			      false, false,
-			      PROTOCOL_NT1,
-			      0, 0x20);
+				server_name, "IPC$",
+				auth_info,
+				false, false,
+				PROTOCOL_NT1,
+				0, 0x20);
+	TALLOC_FREE(auth_info);
+
 	if (!cli_ipc) {
 		libnetapi_set_error_string(ctx,
 			"Failed to connect to IPC$ share on %s", server_name);

source3/client/client.c
@@ -103,6 +103,9 @@ struct cli_state *cli;
 static char CLI_DIRSEP_CHAR = '\\';
 static char CLI_DIRSEP_STR[] = { '\\', '\0' };
 
+/* Authentication for client connections. */
+struct user_auth_info *auth_info;
+
 /* Accessor functions for directory paths. */
 static char *fileselection;
 static const char *client_get_fileselection(void)
@@ -299,7 +302,7 @@ static int do_dskattr(void)
 	char *targetpath = NULL;
 	TALLOC_CTX *ctx = talloc_tos();
 
-	if ( !cli_resolve_path(ctx, "", cli, client_get_cur_dir(), &targetcli, &targetpath)) {
+	if ( !cli_resolve_path(ctx, "", auth_info, cli, client_get_cur_dir(), &targetcli, &targetpath)) {
 		d_printf("Error in dskattr: %s\n", cli_errstr(cli));
 		return 1;
 	}
@@ -393,7 +396,7 @@ static int do_cd(const char *new_dir)
 	new_cd = clean_name(ctx, new_cd);
 	client_set_cur_dir(new_cd);
 
-	if ( !cli_resolve_path(ctx, "", cli, new_cd, &targetcli, &targetpath)) {
+	if ( !cli_resolve_path(ctx, "", auth_info, cli, new_cd, &targetcli, &targetpath)) {
 		d_printf("cd %s: %s\n", new_cd, cli_errstr(cli));
 		client_set_cur_dir(saved_dir);
 		goto out;
@@ -819,7 +822,7 @@ void do_list(const char *mask,
 
 			/* check for dfs */
 
-			if ( !cli_resolve_path(ctx, "", cli, head, &targetcli, &targetpath ) ) {
+			if ( !cli_resolve_path(ctx, "", auth_info, cli, head, &targetcli, &targetpath ) ) {
 				d_printf("do_list: [%s] %s\n", head, cli_errstr(cli));
 				remove_do_list_queue_head();
 				continue;
@@ -852,7 +855,7 @@ void do_list(const char *mask,
 		}
 	} else {
 		/* check for dfs */
-		if (cli_resolve_path(ctx, "", cli, mask, &targetcli, &targetpath)) {
+		if (cli_resolve_path(ctx, "", auth_info, cli, mask, &targetcli, &targetpath)) {
 			if (cli_list(targetcli, targetpath, attribute, do_list_helper, NULL) == -1) {
 				d_printf("%s listing %s\n",
 					cli_errstr(targetcli), targetpath);
@@ -1018,7 +1021,7 @@ static int do_get(const char *rname, const char *lname_in, bool reget)
 		strlower_m(lname);
 	}
 
-	if (!cli_resolve_path(ctx, "", cli, rname, &targetcli, &targetname ) ) {
+	if (!cli_resolve_path(ctx, "", auth_info, cli, rname, &targetcli, &targetname ) ) {
 		d_printf("Failed to open %s: %s\n", rname, cli_errstr(cli));
 		return 1;
 	}
@@ -1381,7 +1384,7 @@ static bool do_mkdir(const char *name)
 	struct cli_state *targetcli;
 	char *targetname = NULL;
 
-	if (!cli_resolve_path(ctx, "", cli, name, &targetcli, &targetname)) {
+	if (!cli_resolve_path(ctx, "", auth_info, cli, name, &targetcli, &targetname)) {
 		d_printf("mkdir %s: %s\n", name, cli_errstr(cli));
 		return false;
 	}
@@ -1464,7 +1467,7 @@ static int cmd_mkdir(void)
 			return 1;
 		}
 
-		if (!cli_resolve_path(ctx, "", cli, mask, &targetcli, &targetname)) {
+		if (!cli_resolve_path(ctx, "", auth_info, cli, mask, &targetcli, &targetname)) {
 			return 1;
 		}
 
@@ -1625,7 +1628,7 @@ static int do_put(const char *rname, const char *lname, bool reput)
 	struct push_state state;
 	NTSTATUS status;
 
-	if (!cli_resolve_path(ctx, "", cli, rname, &targetcli, &targetname)) {
+	if (!cli_resolve_path(ctx, "", auth_info, cli, rname, &targetcli, &targetname)) {
 		d_printf("Failed to open %s: %s\n", rname, cli_errstr(cli));
 		return 1;
 	}
@@ -2183,7 +2186,7 @@ static int cmd_wdel(void)
 		return 1;
 	}
 
-	if (!cli_resolve_path(ctx, "", cli, mask, &targetcli, &targetname)) {
+	if (!cli_resolve_path(ctx, "", auth_info, cli, mask, &targetcli, &targetname)) {
 		d_printf("cmd_wdel %s: %s\n", mask, cli_errstr(cli));
 		return 1;
 	}
@@ -2218,7 +2221,7 @@ static int cmd_open(void)
 		return 1;
 	}
 
-	if (!cli_resolve_path(ctx, "", cli, mask, &targetcli, &targetname)) {
+	if (!cli_resolve_path(ctx, "", auth_info, cli, mask, &targetcli, &targetname)) {
 		d_printf("open %s: %s\n", mask, cli_errstr(cli));
 		return 1;
 	}
@@ -2311,7 +2314,7 @@ static int cmd_posix_open(void)
 	}
 	mode = (mode_t)strtol(buf, (char **)NULL, 8);
 
-	if (!cli_resolve_path(ctx, "", cli, mask, &targetcli, &targetname)) {
+	if (!cli_resolve_path(ctx, "", auth_info, cli, mask, &targetcli, &targetname)) {
 		d_printf("posix_open %s: %s\n", mask, cli_errstr(cli));
 		return 1;
 	}
@@ -2359,7 +2362,7 @@ static int cmd_posix_mkdir(void)
 	}
 	mode = (mode_t)strtol(buf, (char **)NULL, 8);
 
-	if (!cli_resolve_path(ctx, "", cli, mask, &targetcli, &targetname)) {
+	if (!cli_resolve_path(ctx, "", auth_info, cli, mask, &targetcli, &targetname)) {
 		d_printf("posix_mkdir %s: %s\n", mask, cli_errstr(cli));
 		return 1;
 	}
@@ -2393,7 +2396,7 @@ static int cmd_posix_unlink(void)
 		return 1;
 	}
 
-	if (!cli_resolve_path(ctx, "", cli, mask, &targetcli, &targetname)) {
+	if (!cli_resolve_path(ctx, "", auth_info, cli, mask, &targetcli, &targetname)) {
 		d_printf("posix_unlink %s: %s\n", mask, cli_errstr(cli));
 		return 1;
 	}
@@ -2427,7 +2430,7 @@ static int cmd_posix_rmdir(void)
 		return 1;
 	}
 
-	if (!cli_resolve_path(ctx, "", cli, mask, &targetcli, &targetname)) {
+	if (!cli_resolve_path(ctx, "", auth_info, cli, mask, &targetcli, &targetname)) {
 		d_printf("posix_rmdir %s: %s\n", mask, cli_errstr(cli));
 		return 1;
 	}
@@ -2667,7 +2670,7 @@ static int cmd_rmdir(void)
 		return 1;
 	}
 
-	if (!cli_resolve_path(ctx, "", cli, mask, &targetcli, &targetname)) {
+	if (!cli_resolve_path(ctx, "", auth_info, cli, mask, &targetcli, &targetname)) {
 		d_printf("rmdir %s: %s\n", mask, cli_errstr(cli));
 		return 1;
 	}
@@ -2714,7 +2717,7 @@ static int cmd_link(void)
 		return 1;
 	}
 
-	if (!cli_resolve_path(ctx, "", cli, oldname, &targetcli, &targetname)) {
+	if (!cli_resolve_path(ctx, "", auth_info, cli, oldname, &targetcli, &targetname)) {
 		d_printf("link %s: %s\n", oldname, cli_errstr(cli));
 		return 1;
 	}
@@ -2765,7 +2768,7 @@ static int cmd_symlink(void)
 		return 1;
 	}
 
-	if (!cli_resolve_path(ctx, "", cli, oldname, &targetcli, &targetname)) {
+	if (!cli_resolve_path(ctx, "", auth_info, cli, oldname, &targetcli, &targetname)) {
 		d_printf("link %s: %s\n", oldname, cli_errstr(cli));
 		return 1;
 	}
@@ -2813,7 +2816,7 @@ static int cmd_chmod(void)
 
 	mode = (mode_t)strtol(buf, NULL, 8);
 
-	if (!cli_resolve_path(ctx, "", cli, src, &targetcli, &targetname)) {
+	if (!cli_resolve_path(ctx, "", auth_info, cli, src, &targetcli, &targetname)) {
 		d_printf("chmod %s: %s\n", src, cli_errstr(cli));
 		return 1;
 	}
@@ -2966,7 +2969,7 @@ static int cmd_getfacl(void)
 		return 1;
 	}
 
-	if (!cli_resolve_path(ctx, "", cli, src, &targetcli, &targetname)) {
+	if (!cli_resolve_path(ctx, "", auth_info, cli, src, &targetcli, &targetname)) {
 		d_printf("stat %s: %s\n", src, cli_errstr(cli));
 		return 1;
 	}
@@ -3132,7 +3135,7 @@ static int cmd_stat(void)
 		return 1;
 	}
 
-	if (!cli_resolve_path(ctx, "", cli, src, &targetcli, &targetname)) {
+	if (!cli_resolve_path(ctx, "", auth_info, cli, src, &targetcli, &targetname)) {
 		d_printf("stat %s: %s\n", src, cli_errstr(cli));
 		return 1;
 	}
@@ -3233,7 +3236,7 @@ static int cmd_chown(void)
 	if (!src) {
 		return 1;
 	}
-	if (!cli_resolve_path(ctx, "", cli, src, &targetcli, &targetname) ) {
+	if (!cli_resolve_path(ctx, "", auth_info, cli, src, &targetcli, &targetname) ) {
 		d_printf("chown %s: %s\n", src, cli_errstr(cli));
 		return 1;
 	}
@@ -3287,12 +3290,12 @@ static int cmd_rename(void)
 		return 1;
 	}
 
-	if (!cli_resolve_path(ctx, "", cli, src, &targetcli, &targetsrc)) {
+	if (!cli_resolve_path(ctx, "", auth_info, cli, src, &targetcli, &targetsrc)) {
 		d_printf("rename %s: %s\n", src, cli_errstr(cli));
 		return 1;
 	}
 
-	if (!cli_resolve_path(ctx, "", cli, dest, &targetcli, &targetdest)) {
+	if (!cli_resolve_path(ctx, "", auth_info, cli, dest, &targetcli, &targetdest)) {
 		d_printf("rename %s: %s\n", dest, cli_errstr(cli));
 		return 1;
 	}
@@ -3362,7 +3365,7 @@ static int cmd_hardlink(void)
 		return 1;
 	}
 
-	if (!cli_resolve_path(ctx, "", cli, src, &targetcli, &targetname)) {
+	if (!cli_resolve_path(ctx, "", auth_info, cli, src, &targetcli, &targetname)) {
 		d_printf("hardlink %s: %s\n", src, cli_errstr(cli));
 		return 1;
 	}
@@ -3829,7 +3832,7 @@ static int cmd_show_connect( void )
 	struct cli_state *targetcli;
 	char *targetpath;
 
-	if (!cli_resolve_path(ctx, "", cli, client_get_cur_dir(),
+	if (!cli_resolve_path(ctx, "", auth_info, cli, client_get_cur_dir(),
 				&targetcli, &targetpath ) ) {
 		d_printf("showconnect %s: %s\n", cur_dir, cli_errstr(cli));
 		return 1;
@@ -4051,7 +4054,8 @@ static int process_command_string(const char *cmd_in)
 	if (!cli) {
 		cli = cli_cm_open(talloc_tos(), NULL,
 				have_ip ? dest_ss_str : desthost,
-				service, true, smb_encrypt,
+				service, auth_info,
+				true, smb_encrypt,
 				max_protocol, port, name_type);
 		if (!cli) {
 			return 1;
@@ -4220,7 +4224,7 @@ static char **remote_completion(const char *text, int len)
 		goto cleanup;
 	}
 
-	if (!cli_resolve_path(ctx, "", cli, dirmask, &targetcli, &targetpath)) {
+	if (!cli_resolve_path(ctx, "", auth_info, cli, dirmask, &targetcli, &targetpath)) {
 		goto cleanup;
 	}
 	if (cli_list(targetcli, targetpath, aDIR | aSYSTEM | aHIDDEN,
@@ -4517,7 +4521,7 @@ static int process(const char *base_directory)
 
 	cli = cli_cm_open(talloc_tos(), NULL,
 			have_ip ? dest_ss_str : desthost,
-			service, true, smb_encrypt,
+			service, auth_info, true, smb_encrypt,
 			max_protocol, port, name_type);
 	if (!cli) {
 		return 1;
@@ -4550,7 +4554,7 @@ static int do_host_query(const char *query_host)
 	struct sockaddr_storage ss;
 
 	cli = cli_cm_open(talloc_tos(), NULL,
-			query_host, "IPC$", true, smb_encrypt,
+			query_host, "IPC$", auth_info, true, smb_encrypt,
 			max_protocol, port, name_type);
 	if (!cli)
 		return 1;
@@ -4570,7 +4574,7 @@ static int do_host_query(const char *query_host)
 
 		cli_shutdown(cli);
 		cli = cli_cm_open(talloc_tos(), NULL,
-				query_host, "IPC$", true, smb_encrypt,
+				query_host, "IPC$", auth_info, true, smb_encrypt,
 				max_protocol, 139, name_type);
 	}
 
@@ -4598,7 +4602,7 @@ static int do_tar_op(const char *base_directory)
 	if (!cli) {
 		cli = cli_cm_open(talloc_tos(), NULL,
 			have_ip ? dest_ss_str : desthost,
-			service, true, smb_encrypt,
+			service, auth_info, true, smb_encrypt,
 			max_protocol, port, name_type);
 		if (!cli)
 			return 1;
@@ -4625,7 +4629,7 @@ static int do_tar_op(const char *base_directory)
  Handle a message operation.
 ****************************************************************************/
 
-static int do_message_op(struct user_auth_info *auth_info)
+static int do_message_op(struct user_auth_info *a_info)
 {
 	struct sockaddr_storage ss;
 	struct nmb_name called, calling;
@@ -4667,7 +4671,7 @@ static int do_message_op(struct user_auth_info *auth_info)
 		return 1;
 	}
 
-	send_message(get_cmdline_auth_info_username(auth_info));
+	send_message(get_cmdline_auth_info_username(a_info));
 	cli_shutdown(cli);
 
 	return 0;
@@ -4714,7 +4718,6 @@ static int do_message_op(struct user_auth_info *auth_info)
 		POPT_TABLEEND
 	};
 	TALLOC_CTX *frame = talloc_stackframe();
-	struct user_auth_info *auth_info;
 
 	if (!client_set_cur_dir("\\")) {
 		exit(ENOMEM);
@@ -4970,11 +4973,10 @@ static int do_message_op(struct user_auth_info *auth_info)
 
 	poptFreeContext(pc);
 
-	/* Store the username and password for dfs support */
-
-	cli_cm_set_credentials(auth_info);
-
 	DEBUG(3,("Client started (version %s).\n", samba_version_string()));
+
+	/* Ensure we have a password (or equivalent). */
+	set_cmdline_auth_info_getpass(auth_info);
 
 	if (tar_type) {
 		if (cmdstr)

source3/include/libsmb_internal.h
@@ -181,6 +181,12 @@ struct SMBC_internal_data {
          */
         bool                                    case_sensitive;
 
+	/*
+	 * Auth info needed for DFS traversal.
+	 */
+
+	struct user_auth_info			*auth_info;
+
         struct smbc_server_cache * server_cache;
 
         /* POSIX emulation functions */

source3/include/popt_common.h
@@ -53,6 +53,7 @@ struct user_auth_info {
 	int signing_state;
 	bool smb_encrypt;
 	bool use_machine_account;
+	bool fallback_after_kerberos;
 };
 
 #endif /* _POPT_COMMON_H */

source3/include/proto.h
@@ -1084,6 +1084,9 @@ int get_cmdline_auth_info_signing_state(const struct user_auth_info *auth_info);
 void set_cmdline_auth_info_use_kerberos(struct user_auth_info *auth_info,
 					bool b);
 bool get_cmdline_auth_info_use_kerberos(const struct user_auth_info *auth_info);
+void set_cmdline_auth_info_fallback_after_kerberos(struct user_auth_info *auth_info,
+					bool b);
+bool get_cmdline_auth_info_fallback_after_kerberos(const struct user_auth_info *auth_info);
 void set_cmdline_auth_info_use_krb5_ticket(struct user_auth_info *auth_info);
 void set_cmdline_auth_info_smb_encrypt(struct user_auth_info *auth_info);
 void set_cmdline_auth_info_use_machine_account(struct user_auth_info *auth_info);
@@ -1093,6 +1096,7 @@ bool get_cmdline_auth_info_use_machine_account(const struct user_auth_info *auth
 struct user_auth_info *get_cmdline_auth_info_copy(TALLOC_CTX *mem_ctx,
 						 const struct user_auth_info *info);
 bool set_cmdline_auth_info_machine_account_creds(struct user_auth_info *auth_info);
+void set_cmdline_auth_info_getpass(struct user_auth_info *auth_info);
 bool add_gid_to_array_unique(TALLOC_CTX *mem_ctx, gid_t gid,
 			     gid_t **gids, size_t *num_gids);
 bool file_exist_stat(const char *fname,SMB_STRUCT_STAT *sbuf);
@@ -2359,21 +2363,13 @@ struct cli_state *cli_cm_open(TALLOC_CTX *ctx,
 				struct cli_state *referring_cli,
 				const char *server,
 				const char *share,
+				const struct user_auth_info *auth_info,
 				bool show_hdr,
 				bool force_encrypt,
 				int max_protocol,
 				int port,
 				int name_type);
 void cli_cm_display(const struct cli_state *c);
-void cli_cm_set_credentials(struct user_auth_info *auth_info);
-void cli_cm_set_port(int port_number);
-void cli_cm_set_dest_name_type(int type);
-void cli_cm_set_signing_state(int state);
-void cli_cm_set_username(const char *username);
-void cli_cm_set_password(const char *newpass);
-void cli_cm_set_use_kerberos(void);
-void cli_cm_set_fallback_after_kerberos(void);
-void cli_cm_set_dest_ss(struct sockaddr_storage *pss);
 bool cli_dfs_get_referral(TALLOC_CTX *ctx,
 			struct cli_state *cli,
 			const char *path,
@@ -2382,6 +2378,7 @@ bool cli_dfs_get_referral(TALLOC_CTX *ctx,
 			uint16 *consumed);
 bool cli_resolve_path(TALLOC_CTX *ctx,
 			const char *mountpt,
+			const struct user_auth_info *dfs_auth_info,
 			struct cli_state *rootcli,
 			const char *path,
 			struct cli_state **targetcli,

source3/lib/util.c
@@ -320,6 +320,9 @@ void set_cmdline_auth_info_password(struct user_auth_info *auth_info,
 				    const char *password)
 {
 	TALLOC_FREE(auth_info->password);
+	if (password == NULL) {
+		password = "";
+	}
 	auth_info->password = talloc_strdup(auth_info, password);
 	if (!auth_info->password) {
 		exit(ENOMEM);
@@ -360,6 +363,17 @@ void set_cmdline_auth_info_use_kerberos(struct user_auth_info *auth_info,
 bool get_cmdline_auth_info_use_kerberos(const struct user_auth_info *auth_info)
 {
 	return auth_info->use_kerberos;
+}
+
+void set_cmdline_auth_info_fallback_after_kerberos(struct user_auth_info *auth_info,
+					bool b)
+{
+	auth_info->fallback_after_kerberos = b;
+}
+
+bool get_cmdline_auth_info_fallback_after_kerberos(const struct user_auth_info *auth_info)
+{
+	return auth_info->fallback_after_kerberos;
 }
 
 /* This should only be used by lib/popt_common.c JRA */
@@ -453,6 +467,32 @@ bool set_cmdline_auth_info_machine_account_creds(struct user_auth_info *auth_inf
 	SAFE_FREE(pass);
 
 	return true;
+}
+
+/****************************************************************************
+ Ensure we have a password if one not given.
+****************************************************************************/
+
+void set_cmdline_auth_info_getpass(struct user_auth_info *auth_info)
+{
+	char *label = NULL;
+	char *pass;
+	TALLOC_CTX *frame;
+
+	if (get_cmdline_auth_info_got_pass(auth_info) ||
+			get_cmdline_auth_info_use_kerberos(auth_info)) {
+		/* Already got one... */
+		return;
+	}
+
+	frame = talloc_stackframe();
+	label = talloc_asprintf(frame, "Enter %s's password: ",
+			get_cmdline_auth_info_username(auth_info));
+	pass = getpass(label);
+	if (pass) {
+		set_cmdline_auth_info_password(auth_info, pass);
+	}
+	TALLOC_FREE(frame);
 }
 
 /****************************************************************************

source3/libsmb/clidfs.c
@@ -32,17 +32,6 @@
    as a separator when looking at the pathname part.... JRA.
 ********************************************************************/
 
-static struct cm_cred_struct {
-	char *username;
-	char *password;
-	bool got_pass;
-	bool use_kerberos;
-	bool fallback_after_kerberos;
-	int signing_state;
-} cm_creds;
-
-static void cm_set_password(const char *newpass);
-
 static bool cli_check_msdfs_proxy(TALLOC_CTX *ctx,
 				struct cli_state *cli,
 				const char *sharename,
@@ -96,6 +85,7 @@ NTSTATUS cli_cm_force_encryption(struct cli_state *c,
 static struct cli_state *do_connect(TALLOC_CTX *ctx,
 					const char *server,
 					const char *share,
+					const struct user_auth_info *auth_info,
 					bool show_sessetup,
 					bool force_encrypt,
 					int max_protocol,
@@ -143,7 +133,7 @@ static struct cli_state *do_connect(TALLOC_CTX *ctx,
 	zero_sockaddr(&ss);
 
 	/* have to open a new connection */
-	if (!(c=cli_initialise_ex(cm_creds.signing_state))) {
+	if (!(c=cli_initialise_ex(get_cmdline_auth_info_signing_state(auth_info)))) {
 		d_printf("Connection to %s failed\n", server_n);
 		if (c) {
 			cli_shutdown(c);
@@ -167,8 +157,9 @@ static struct cli_state *do_connect(TALLOC_CTX *ctx,
 		max_protocol = PROTOCOL_NT1;
 	}
 	c->protocol = max_protocol;
-	c->use_kerberos = cm_creds.use_kerberos;
-	c->fallback_after_kerberos = cm_creds.fallback_after_kerberos;
+	c->use_kerberos = get_cmdline_auth_info_use_kerberos(auth_info);
+	c->fallback_after_kerberos =
+		get_cmdline_auth_info_fallback_after_kerberos(auth_info);
 
 	if (!cli_session_request(c, &calling, &called)) {
 		char *p;
@@ -198,20 +189,8 @@ static struct cli_state *do_connect(TALLOC_CTX *ctx,
 		return NULL;
 	}
 
-	if (!cm_creds.got_pass && !cm_creds.use_kerberos) {
-		char *label = NULL;
-		char *pass;
-		label = talloc_asprintf(ctx, "Enter %s's password: ",
-			cm_creds.username);
-		pass = getpass(label);
-		if (pass) {
-			cm_set_password(pass);
-		}
-		TALLOC_FREE(label);
-	}
-
-	username = cm_creds.username ? cm_creds.username : "";
-	password = cm_creds.password ? cm_creds.password : "";
+	username = get_cmdline_auth_info_username(auth_info);
+	password = get_cmdline_auth_info_password(auth_info);
 
 	if (!NT_STATUS_IS_OK(cli_session_setup(c, username,
 					       password, strlen(password),
@@ -219,8 +198,9 @@ static struct cli_state *do_connect(TALLOC_CTX *ctx,
 					       lp_workgroup()))) {
 		/* If a password was not supplied then
 		 * try again with a null username. */
-		if (password[0] || !username[0] || cm_creds.use_kerberos ||
-		    !NT_STATUS_IS_OK(cli_session_setup(c, "",
+		if (password[0] || !username[0] ||
+			get_cmdline_auth_info_use_kerberos(auth_info) ||
+			!NT_STATUS_IS_OK(cli_session_setup(c, "",
 				    		"", 0,
 						"", 0,
 					       lp_workgroup()))) {
@@ -259,7 +239,7 @@ static struct cli_state *do_connect(TALLOC_CTX *ctx,
 				lp_workgroup())) {
 		cli_shutdown(c);
 		return do_connect(ctx, newserver,
-				newshare, false,
+				newshare, auth_info, false,
 				force_encrypt, max_protocol,
 				port, name_type);
 	}
@@ -313,6 +293,7 @@ static struct cli_state *cli_cm_connect(TALLOC_CTX *ctx,
 					struct cli_state *referring_cli,
 	 				const char *server,
 					const char *share,
+					const struct user_auth_info *auth_info,
 					bool show_hdr,
 					bool force_encrypt,
 					int max_protocol,
@@ -322,6 +303,7 @@ static struct cli_state *cli_cm_connect(TALLOC_CTX *ctx,
 	struct cli_state *cli;
 
 	cli = do_connect(ctx, server, share,
+				auth_info,
 				show_hdr, force_encrypt, max_protocol,
 				port, name_type);
 
@@ -389,6 +371,7 @@ struct cli_state *cli_cm_open(TALLOC_CTX *ctx,
 				struct cli_state *referring_cli,
 				const char *server,
 				const char *share,
+				const struct user_auth_info *auth_info,
 				bool show_hdr,
 				bool force_encrypt,
 				int max_protocol,
@@ -402,9 +385,25 @@ struct cli_state *cli_cm_open(TALLOC_CTX *ctx,
 		return c;
 	}
 
-	return cli_cm_connect(ctx, referring_cli,
-				server, share, show_hdr, force_encrypt,
-				max_protocol, port, name_type);
+	if (auth_info == NULL) {
+		/* Can't do a new connection
+		 * without auth info. */
+		d_printf("cli_cm_open() Unable to open connection [\\%s\\%s] "
+			"without auth info\n",
+			server, share );
+		return NULL;
+	}
+
+	return cli_cm_connect(ctx,
+				referring_cli,
+				server,
+				share,
+				auth_info,
+				show_hdr,
+				force_encrypt,
+				max_protocol,
+				port,
+				name_type);
 }
 
 /****************************************************************************
@@ -423,18 +422,10 @@ void cli_cm_display(const struct cli_state *cli)
 /****************************************************************************
 ****************************************************************************/
 
-static void cm_set_password(const char *newpass)
-{
-	SAFE_FREE(cm_creds.password);
-	cm_creds.password = SMB_STRDUP(newpass);
-	if (cm_creds.password) {
-		cm_creds.got_pass = true;
-	}
-}
-
 /****************************************************************************
 ****************************************************************************/
 
+#if 0
 void cli_cm_set_credentials(struct user_auth_info *auth_info)
 {
 	SAFE_FREE(cm_creds.username);
@@ -449,51 +440,7 @@ void cli_cm_set_credentials(struct user_auth_info *auth_info)
 	cm_creds.fallback_after_kerberos = false;
 	cm_creds.signing_state = get_cmdline_auth_info_signing_state(auth_info);
 }
-
-/****************************************************************************
-****************************************************************************/
-
-void cli_cm_set_signing_state(int state)
-{
-	cm_creds.signing_state = state;
-}
-
-/****************************************************************************
-****************************************************************************/
-
-void cli_cm_set_username(const char *username)
-{
-	SAFE_FREE(cm_creds.username);
-	cm_creds.username = SMB_STRDUP(username);
-}
-
-/****************************************************************************
-****************************************************************************/
-
-void cli_cm_set_password(const char *newpass)
-{
-	SAFE_FREE(cm_creds.password);
-	cm_creds.password = SMB_STRDUP(newpass);
-	if (cm_creds.password) {
-		cm_creds.got_pass = true;
-	}
-}
-
-/****************************************************************************
-****************************************************************************/
-
-void cli_cm_set_use_kerberos(void)
-{
-	cm_creds.use_kerberos = true;
-}
-
-/****************************************************************************
-****************************************************************************/
-
-void cli_cm_set_fallback_after_kerberos(void)
-{
-	cm_creds.fallback_after_kerberos = true;
-}
+#endif
 
 /**********************************************************************
  split a dfs path into the server, share name, and extrapath components
@@ -763,6 +710,7 @@ bool cli_dfs_get_referral(TALLOC_CTX *ctx,
 
 bool cli_resolve_path(TALLOC_CTX *ctx,
 			const char *mountpt,
+			const struct user_auth_info *dfs_auth_info,
 			struct cli_state *rootcli,
 			const char *path,
 			struct cli_state **targetcli,
@@ -843,13 +791,16 @@ bool cli_resolve_path(TALLOC_CTX *ctx,
 
 	/* Check for the referral. */
 
-	if (!(cli_ipc = cli_cm_open(ctx, rootcli,
-					rootcli->desthost,
-					"IPC$", false,
-					(rootcli->trans_enc_state != NULL),
-					rootcli->protocol,
-					0,
-					0x20))) {
+	if (!(cli_ipc = cli_cm_open(ctx,
+				rootcli,
+				rootcli->desthost,
+				"IPC$",
+				dfs_auth_info,
+				false,
+				(rootcli->trans_enc_state != NULL),
+				rootcli->protocol,
+				0,
+				0x20))) {
 		return false;
 	}
 
@@ -893,6 +844,7 @@ bool cli_resolve_path(TALLOC_CTX *ctx,
 	if ((*targetcli = cli_cm_open(ctx, rootcli,
 					server,
 					share,
+					dfs_auth_info,
 					false,
 					(rootcli->trans_enc_state != NULL),
 					rootcli->protocol,
@@ -952,6 +904,7 @@ bool cli_resolve_path(TALLOC_CTX *ctx,
 	if (!strequal(*pp_targetpath, "\\") && !strequal(*pp_targetpath, "/")) {
 		if (cli_resolve_path(ctx,
 					newmount,
+					dfs_auth_info,
 					*targetcli,
 					*pp_targetpath,
 					&newcli,

commit 56d74b62126083dd7e2a60d1b48b03e1b1798c90

s3-libnetapi: fix creds in libnetapi_open_ipc_connection().
Jeremy, this broke with 8dd1faaa2992851f6852ba7ea4498445af5faadd. Not sure if
other users broke as well with that change.

Guenther

source3/lib/netapi/cm.c
@@ -57,6 +57,11 @@ static WERROR libnetapi_open_ipc_connection(struct libnetapi_ctx *ctx,
 				false, false,
 				PROTOCOL_NT1,
 				0, 0x20);
+	if (cli_ipc) {
+		cli_set_username(cli_ipc, ctx->username);
+		cli_set_password(cli_ipc, ctx->password);
+		cli_set_domain(cli_ipc, ctx->workgroup);
+	}
 	TALLOC_FREE(auth_info);
 
 	if (!cli_ipc) {