gwr/samba-old

s3:net conf: reduce memory usage of "net conf import".
"net conf import" was wrapped in one big transaction.
This lead to MAX_TALLOC_SIZE being exceeded at roughly
1500 shares. This patch resolves that problem by
limiting the top level transactions in "net conf import"
to 100 shares.

Michael

source/utils/net_conf.c
@@ -314,12 +314,6 @@ static int net_conf_import(struct smbconf_ctx *conf_ctx,
 			 "would import the following configuration:\n\n");
 	}
 
-	werr = smbconf_transaction_start(conf_ctx);
-	if (!W_ERROR_IS_OK(werr)) {
-		d_printf("error starting transaction: %s\n", dos_errstr(werr));
-		goto done;
-	}
-
 	if (servicename != NULL) {
 		struct smbconf_service *service = NULL;
 
@@ -349,10 +343,43 @@ static int net_conf_import(struct smbconf_ctx *conf_ctx,
 				goto cancel;
 			}
 		}
+
+		/*
+		 * Wrap the importing of shares into a transaction,
+		 * but only 100 at a time, in order to serve memory.
+		 * The allocated memory accumulates across the actions
+		 * within the transaction, and for me, some 1500
+		 * imported shares, the MAX_TALLOC_SIZE of 256 MB
+		 * was exceeded.
+		 */
+		werr = smbconf_transaction_start(conf_ctx);
+		if (!W_ERROR_IS_OK(werr)) {
+			d_printf("error starting transaction: %s\n",
+				 dos_errstr(werr));
+			goto done;
+		}
+
 		for (sidx = 0; sidx < num_shares; sidx++) {
 			werr = import_process_service(conf_ctx, services[sidx]);
 			if (!W_ERROR_IS_OK(werr)) {
 				goto cancel;
+			}
+
+			if (sidx % 100) {
+				continue;
+			}
+
+			werr = smbconf_transaction_commit(conf_ctx);
+			if (!W_ERROR_IS_OK(werr)) {
+				d_printf("error committing transaction: %s\n",
+					 dos_errstr(werr));
+				goto done;
+			}
+			werr = smbconf_transaction_start(conf_ctx);
+			if (!W_ERROR_IS_OK(werr)) {
+				d_printf("error starting transaction: %s\n",
+					 dos_errstr(werr));
+				goto done;
 			}
 		}
 	}

samba-team/samba

Signed-off-by: Michael Adam <obnox@samba.org>
(cherry picked from commit 026b72903c852e46012ac60d9d42b14c6860a159)

source/utils/net_conf.c
@@ -331,12 +331,6 @@ static int net_conf_import(struct net_context *c, struct smbconf_ctx *conf_ctx,
 			 "would import the following configuration:\n\n");
 	}
 
-	werr = smbconf_transaction_start(conf_ctx);
-	if (!W_ERROR_IS_OK(werr)) {
-		d_printf("error starting transaction: %s\n", win_errstr(werr));
-		goto done;
-	}
-
 	if (servicename != NULL) {
 		struct smbconf_service *service = NULL;
 
@@ -366,11 +360,44 @@ static int net_conf_import(struct net_context *c, struct smbconf_ctx *conf_ctx,
 				goto cancel;
 			}
 		}
+
+		/*
+		 * Wrap the importing of shares into a transaction,
+		 * but only 100 at a time, in order to serve memory.
+		 * The allocated memory accumulates across the actions
+		 * within the transaction, and for me, some 1500
+		 * imported shares, the MAX_TALLOC_SIZE of 256 MB
+		 * was exceeded.
+		 */
+		werr = smbconf_transaction_start(conf_ctx);
+		if (!W_ERROR_IS_OK(werr)) {
+			d_printf("error starting transaction: %s\n",
+				 win_errstr(werr));
+			goto done;
+		}
+
 		for (sidx = 0; sidx < num_shares; sidx++) {
 			werr = import_process_service(c, conf_ctx,
 						      services[sidx]);
 			if (!W_ERROR_IS_OK(werr)) {
 				goto cancel;
+			}
+
+			if (sidx % 100) {
+				continue;
+			}
+
+			werr = smbconf_transaction_commit(conf_ctx);
+			if (!W_ERROR_IS_OK(werr)) {
+				d_printf("error committing transaction: %s\n",
+					 win_errstr(werr));
+				goto done;
+			}
+			werr = smbconf_transaction_start(conf_ctx);
+			if (!W_ERROR_IS_OK(werr)) {
+				d_printf("error starting transaction: %s\n",
+					 win_errstr(werr));
+				goto done;
 			}
 		}
 	}