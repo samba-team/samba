1033b89779725a4ae0645eabee085e07f4b71e99
Love Hörnquist Åstrand <lha@kth.se>
2009-04-03 04:05:28 +0000

    make comile
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@25048 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- kdc/hprop.c
+++ kdc/hprop.c
@@ -470,7 +470,7 @@ get_creds(krb5_context context, krb5_ccache *cache)
     ret = krb5_kt_close(context, keytab);
     if(ret) krb5_err(context, 1, ret, "krb5_kt_close");
 
-    ret = krb5_cc_new_unique(context, krb5_cc_type_memory, NULL cache);
+    ret = krb5_cc_new_unique(context, krb5_cc_type_memory, NULL, cache);
     if(ret) krb5_err(context, 1, ret, "krb5_cc_new_unique");
 
     ret = krb5_cc_initialize(context, *cache, client);
