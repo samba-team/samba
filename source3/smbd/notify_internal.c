e9569ae9250ac571c63fbb450709778a247e9ca3
Volker Lendecke <vl@samba.org>
2009-04-09 15:51:35 +0200

    Fix some nonempty blank lines

--- source3/smbd/notify_internal.c
+++ source3/smbd/notify_internal.c
@@ -2,17 +2,17 @@
    Unix SMB/CIFS implementation.
 
    Copyright (C) Andrew Tridgell 2006
-   
+
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -614,7 +614,7 @@ void notify_trigger(struct notify_context *notify,
 
 		/* see if there are any entries at this depth */
 		if (d->num_entries == 0) continue;
-		
+
 		/* try to skip based on the maximum mask. If next_p is
 		 NULL then we know it will be a 'this directory'
 		 match, otherwise it must be a subdir match */
