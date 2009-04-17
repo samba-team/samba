c5920e257dc2066e447620cc85f02241d408dcab
Günther Deschner <gd@samba.org>
2009-04-17 15:46:36 +0200

    s3-test: enable RPC-LSA-LOOKUPSIDS and RPC-JOIN during make test.
    Guenther
    (cherry picked from commit 79321cad54a4303abc27766101ffb52f50d64430)

--- source/script/tests/test_posix_s3.sh
+++ source/script/tests/test_posix_s3.sh
@@ -38,6 +38,7 @@ raw="$raw RAW-SAMBA3ROOTDIRFID"
 rpc="RPC-AUTHCONTEXT RPC-BINDSAMBA3 RPC-SAMBA3-SRVSVC RPC-SAMBA3-SHARESEC"
 rpc="$rpc RPC-SAMBA3-SPOOLSS RPC-SAMBA3-WKSSVC"
 rpc="$rpc RPC-NETLOGSAMBA3 RPC-SAMBA3SESSIONKEY RPC-SAMBA3-GETUSERNAME"
+rpc="$rpc RPC-LSA-LOOKUPSIDS RPC-JOIN"
 
 # NOTE: to enable the UNIX-WHOAMI test, we need to change the default share
 # config to allow guest access. I'm not sure whether this would break other
