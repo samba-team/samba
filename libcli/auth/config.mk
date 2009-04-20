4e1b633dab6fdd42425c43de07a57808c9003cb6
Jeremy Allison <jra@samba.org>
2009-04-20 09:06:21 -0700

    Stop autogenerated files from being created. Jeremy.

--- libcli/auth/config.mk
+++ libcli/auth/config.mk
@@ -7,8 +7,6 @@ ntlm_check_OBJ_FILES = $(addprefix $(libclicommonsrcdir)/auth/, ntlm_check.o)
 
 MSRPC_PARSE_OBJ_FILES = $(addprefix $(libclicommonsrcdir)/auth/, msrpc_parse.o)
 
-$(eval $(call proto_header_template,$(libclicommonsrcdir)/auth/msrpc_parse.h,$(MSRPC_PARSE_OBJ_FILES:.o=.c)))
-
 [SUBSYSTEM::LIBCLI_AUTH]
 PUBLIC_DEPENDENCIES = \
 		MSRPC_PARSE \
@@ -21,11 +19,8 @@ LIBCLI_AUTH_OBJ_FILES = $(addprefix $(libclicommonsrcdir)/auth/, \
 		smbdes.o)
 
 PUBLIC_HEADERS += ../libcli/auth/credentials.h
-$(eval $(call proto_header_template,$(libclicommonsrcdir)/auth/proto.h,$(LIBCLI_AUTH_OBJ_FILES:.o=.c)))
 
 [SUBSYSTEM::COMMON_SCHANNELDB]
 PRIVATE_DEPENDENCIES = LDB_WRAP
 
 COMMON_SCHANNELDB_OBJ_FILES = $(addprefix $(libclicommonsrcdir)/auth/, schannel_state.o)
-$(eval $(call proto_header_template,$(libclicommonsrcdir)/auth/schannel_state_proto.h,$(COMMON_SCHANNELDB_OBJ_FILES:.o=.c)))
-
