448d6cd32c793d04c3c509200bfaa75f466a0ee5
Jeremy Allison <jra@samba.org>
2009-04-22 02:55:47 -0700

    Fix bug #6279 - winbindd crash. Cope with LDAP libraries returning LDAP_SUCCESS but not returning a result. Jeremy

--- source/libads/ldap.c
+++ source/libads/ldap.c
@@ -112,6 +112,10 @@ static int ldap_search_with_timeout(LDAP *ld,
 	if (gotalarm != 0)
 		return LDAP_TIMELIMIT_EXCEEDED;
 
+	if (res == NULL) {
+		return LDAP_TIMELIMIT_EXCEEDED;
+	}
+
 	return result;
 }
 
