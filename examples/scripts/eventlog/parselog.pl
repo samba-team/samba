381f88f081788c5a49b763211a02f260cbd97e91
Günther Deschner <gd@samba.org>
2009-04-27 11:14:44 +0200

    s3-examples: fix usage for eventlog example script.
    Guenther

--- examples/scripts/eventlog/parselog.pl
+++ examples/scripts/eventlog/parselog.pl
@@ -20,7 +20,7 @@
 ##
 ######################################################################
 
-## usage: tail -f /var/log/syslog | parselog.pl | eventlogadm "Application"
+## usage: tail -f /var/log/syslog | parselog.pl | eventlogadm -o write "Application"
 
 while(<>) {
     chomp();
