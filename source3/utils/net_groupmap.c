974223b927119a1cd38fd58b215b14d81bfa0a8c
Volker Lendecke <vl@samba.org>
2009-05-07 10:09:32 +0200

    Fix bug 6336: "net groupmap set" segfaults

--- source3/utils/net_groupmap.c
+++ source3/utils/net_groupmap.c
@@ -619,11 +619,13 @@ static int net_groupmap_set(struct net_context *c, int argc, const char **argv)
 
 	/* The case (opt_domaingroup && opt_localgroup) was tested for above */
 
-	if (strlen(c->opt_comment) > 0)
+	if ((c->opt_comment != NULL) && (strlen(c->opt_comment) > 0)) {
 		fstrcpy(map.comment, c->opt_comment);
+	}
 
-	if (strlen(c->opt_newntname) > 0)
+	if ((c->opt_newntname != NULL) && (strlen(c->opt_newntname) > 0)) {
 		fstrcpy(map.nt_name, c->opt_newntname);
+	}
 
 	if (grp != NULL)
 		map.gid = grp->gr_gid;
