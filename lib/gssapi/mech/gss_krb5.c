61a2a0fe7a31e231b40091524f8f7001d96390d7
Love Hörnquist Åstrand <lha@kth.se>
2009-05-09 14:20:50 +0000

    set right return value
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@25200 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/gssapi/mech/gss_krb5.c
+++ lib/gssapi/mech/gss_krb5.c
@@ -278,7 +278,7 @@ gss_krb5_export_lucid_sec_context(OM_uint32 *minor_status,
 	|| *context_handle == GSS_C_NO_CONTEXT
 	|| version != 1)
     {
-	ret = EINVAL;
+	*minor_status = EINVAL;
 	return GSS_S_FAILURE;
     }
 
