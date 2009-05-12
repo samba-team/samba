b1701e09dfa3a1b494c766f2b3e36f6499d61ce9
Michael Adam <obnox@samba.org>
2009-05-12 22:59:08 +0200

    makerpms.sh: add GPL license header
    Michael
    
    (This used to be ctdb commit 7498e176817719eadd91201bbd0d9ceb91eefdae)

--- ctdb/packaging/RPM/makerpms.sh
+++ ctdb/packaging/RPM/makerpms.sh
@@ -1,10 +1,28 @@
 #!/bin/sh
+#
+# makerpms.sh  -  build RPM packages from the git sources
+#
 # Copyright (C) John H Terpstra 1998-2002
-#               Gerald (Jerry) Carter 2003
-#		Jim McDonough 2007
-#		Andrew Tridgell 2007
-#		Michael Adam 2008
+# Copyright (C) Gerald (Jerry) Carter 2003
+# Copyright (C) Jim McDonough 2007
+# Copyright (C) Andrew Tridgell 2007
+# Copyright (C) Michael Adam 2008-2009
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 3 of the License, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, see <http://www.gnu.org/licenses/>.
+#
 
+#
 # The following allows environment variables to override the target directories
 #   the alternative is to have a file in your home directory calles .rpmmacros
 #   containing the following:
