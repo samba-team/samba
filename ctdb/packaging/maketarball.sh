179d911826bf3c628d72835b24cb43a74b5d50eb
Michael Adam <obnox@samba.org>
2009-05-12 22:59:35 +0200

    maketarball.sh: add GPL license header
    Michael
    
    (This used to be ctdb commit 13270a011016bf20bbf721f6d083b2f113fdbc79)

--- ctdb/packaging/maketarball.sh
+++ ctdb/packaging/maketarball.sh
@@ -1,10 +1,28 @@
 #!/bin/sh
 #
+# maketarball.sh - create a tarball from the git branch HEAD
+#
 # Copyright (C) Michael Adam 2009
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 3 of the License, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, see <http://www.gnu.org/licenses/>.
+#
+
 #
 # Create CTDB source tarball of the current git branch HEAD.
 # The version is extracted from the spec file...
 # The first extra argument will be added as an additional version.
+#
 
 DIRNAME=$(dirname $0)
 TOPDIR=${DIRNAME}/..
