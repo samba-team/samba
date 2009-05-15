88876ac57156891e8107034e5d7f40076e032e58
Jelmer Vernooij <jelmer@samba.org>
2009-05-15 16:02:33 +0200

    errormap: Remove wbc -> NTSTATUS error map for now. It is not used.
    This was breaking the Samba 4 installation since it added dependencies
    on header files we weren't installing.
    
    Confirmed with Kai.

--- libcli/util/error.h
+++ libcli/util/error.h
@@ -22,7 +22,6 @@
 #include "libcli/util/werror.h"
 #include "libcli/util/doserr.h"
 #include "libcli/util/ntstatus.h"
-#include "nsswitch/libwbclient/wbclient.h"
 
 /** NT error on DOS connection! (NT_STATUS_OK) */
 bool ntstatus_dos_equal(NTSTATUS status1, NTSTATUS status2);
@@ -46,10 +45,5 @@ WERROR ntstatus_to_werror(NTSTATUS error);
  Map an NT error code from a Unix error code.
 *********************************************************************/
 NTSTATUS map_nt_error_from_unix(int unix_error);
-
-/*******************************************************************************
- Map between wbcErr and NT status.
-*******************************************************************************/
-NTSTATUS map_nt_error_from_wbcErr(wbcErr wbc_err);
 
 #endif /* _SAMBA_ERROR_H */
