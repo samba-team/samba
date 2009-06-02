8ca8dabe4615416153be9be7be16558e43d17381
Andrew Tridgell <tridge@samba.org>
2009-06-02 17:27:37 +1000

    add gendb_search_single_extended_dn()
    This function searches for a single record using a given filter,
    adding the extended-dn control so that any returned DNs will have the
    GUID and SID fields returned. This will be used in the sam auth code
    to prevent us doing a member= search for the groups, which invokes an
    unindexed search.

--- lib/util/util_ldb.h
+++ lib/util/util_ldb.h
@@ -26,4 +26,12 @@ int gendb_search_dn(struct ldb_context *ldb,
 int gendb_add_ldif(struct ldb_context *ldb, const char *ldif_string);
 char *wrap_casefold(void *context, void *mem_ctx, const char *s, size_t n);
 
+int gendb_search_single_extended_dn(struct ldb_context *ldb,
+				    TALLOC_CTX *mem_ctx,
+				    struct ldb_dn *basedn,
+				    enum ldb_scope scope,
+				    struct ldb_message **msg,
+				    const char * const *attrs,
+				    const char *format, ...)  PRINTF_ATTRIBUTE(7,8);
+
 #endif /* __LIB_UTIL_UTIL_LDB_H__ */
