b5ca5412d86bd054b0865a3644c71624f3a8d017
Günther Deschner <gd@samba.org>
2009-06-09 12:06:19 +0200

    s4-smbtorture: rename SECRETS to LSA-SECRETS.
    Guenther

--- source4/torture/rpc/session_key.c
+++ source4/torture/rpc/session_key.c
@@ -215,7 +215,7 @@ static const bool bool_vals[] = { true, false };
 /* TEST session key correctness by pushing and pulling secrets */
 struct torture_suite *torture_rpc_lsa_secrets(TALLOC_CTX *mem_ctx)
 {
-	struct torture_suite *suite = torture_suite_create(mem_ctx, "SECRETS");
+	struct torture_suite *suite = torture_suite_create(mem_ctx, "LSA-SECRETS");
 	int keyexchange, ntlm2, lm_key;
 
 	for (keyexchange = 0; keyexchange < ARRAY_SIZE(bool_vals); keyexchange++) {
