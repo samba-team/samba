fae921e93821329605c8fecacda8eebb768af2f8
Timur I Bakeyev <timur@com.bat.ru>
2009-06-09 20:45:03 +0200

    Fix installation path of the subunit python modules.
    Signed-off-by: Jelmer Vernooij <jelmer@samba.org>

--- source4/scripting/python/config.mk
+++ source4/scripting/python/config.mk
@@ -25,7 +25,7 @@ $(python_glue_OBJ_FILES): CFLAGS+=-I$(ldbsrcdir)
 
 _PY_FILES = $(shell find $(pyscriptsrcdir)/samba ../lib/subunit/python -name "*.py")
 
-$(eval $(foreach pyfile, $(_PY_FILES),$(call python_py_module_template,$(patsubst $(pyscriptsrcdir)/%,%,$(pyfile)),$(pyfile))))
+$(eval $(foreach pyfile, $(_PY_FILES),$(call python_py_module_template,$(patsubst $(pyscriptsrcdir)/%,%,$(subst ../lib/subunit/python,,$(pyfile))),$(pyfile))))
 
 EPYDOC_OPTIONS = --no-private --url http://www.samba.org/ --no-sourcecode
 
