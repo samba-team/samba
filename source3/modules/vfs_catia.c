f39232a8fb93cfccfe1533ab613867572ff7f848
Tim Prouty <tprouty@samba.org>
2009-07-02 13:13:14 -0700

    s3 catia: Remove pointless VOPs from catia

--- source3/modules/vfs_catia.c
+++ source3/modules/vfs_catia.c
@@ -336,65 +336,6 @@ static int catia_chdir(vfs_handle_struct *handle,
         return SMB_VFS_NEXT_CHDIR(handle, name);
 }
 
-static char *catia_getwd(vfs_handle_struct *handle, char *buf)
-{
-        return SMB_VFS_NEXT_GETWD(handle, buf);
-}
-
-static int catia_ntimes(vfs_handle_struct *handle,
-		       const char *path, struct smb_file_time *ft)
-{
-        return SMB_VFS_NEXT_NTIMES(handle, path, ft);
-}
-
-static bool catia_symlink(vfs_handle_struct *handle,
-			  const char *oldpath, const char *newpath)
-{
-        return SMB_VFS_NEXT_SYMLINK(handle, oldpath, newpath);
-}
-
-static bool catia_readlink(vfs_handle_struct *handle,
-			   const char *path, char *buf, size_t bufsiz)
-{
-        return SMB_VFS_NEXT_READLINK(handle, path, buf, bufsiz);
-}
-
-static int catia_link(vfs_handle_struct *handle,
-		      const char *oldpath, const char *newpath)
-{
-        return SMB_VFS_NEXT_LINK(handle, oldpath, newpath);
-}
-
-static int catia_mknod(vfs_handle_struct *handle,
-		       const char *path, mode_t mode, SMB_DEV_T dev)
-{
-        return SMB_VFS_NEXT_MKNOD(handle, path, mode, dev);
-}
-
-static char *catia_realpath(vfs_handle_struct *handle,
-			    const char *path, char *resolved_path)
-{
-        return SMB_VFS_NEXT_REALPATH(handle, path, resolved_path);
-}
-
-static NTSTATUS catia_get_nt_acl(vfs_handle_struct *handle,
-			       const char *name, uint32 security_info,
-			       struct  security_descriptor **ppdesc)
-{
-        return SMB_VFS_NEXT_GET_NT_ACL(handle, name, security_info, ppdesc);
-}
-
-static int catia_chmod_acl(vfs_handle_struct *handle,
-			   const char *name, mode_t mode)
-{
-        /* If the underlying VFS doesn't have ACL support... */
-        if (!handle->vfs_next.ops.chmod_acl) {
-                errno = ENOSYS;
-                return -1;
-        }
-        return SMB_VFS_NEXT_CHMOD_ACL(handle, name, mode);
-}
-
 /* VFS operations structure */
 
 static vfs_op_tuple catia_op_tuples[] = {
@@ -426,31 +367,6 @@ SMB_VFS_LAYER_TRANSPARENT},
 SMB_VFS_LAYER_TRANSPARENT},
         {SMB_VFS_OP(catia_chdir),                       SMB_VFS_OP_CHDIR,
 SMB_VFS_LAYER_TRANSPARENT},
-        {SMB_VFS_OP(catia_getwd),                       SMB_VFS_OP_GETWD,
-SMB_VFS_LAYER_TRANSPARENT},
-        {SMB_VFS_OP(catia_ntimes),                       SMB_VFS_OP_NTIMES,
-SMB_VFS_LAYER_TRANSPARENT},
-        {SMB_VFS_OP(catia_symlink), SMB_VFS_OP_SYMLINK,
-SMB_VFS_LAYER_TRANSPARENT},
-        {SMB_VFS_OP(catia_readlink), SMB_VFS_OP_READLINK,
-SMB_VFS_LAYER_TRANSPARENT},
-        {SMB_VFS_OP(catia_link), SMB_VFS_OP_LINK,
-SMB_VFS_LAYER_TRANSPARENT},
-        {SMB_VFS_OP(catia_mknod),                       SMB_VFS_OP_MKNOD,
-SMB_VFS_LAYER_TRANSPARENT},
-        {SMB_VFS_OP(catia_realpath), SMB_VFS_OP_REALPATH,
-SMB_VFS_LAYER_TRANSPARENT},
-
-        /* NT File ACL operations */
-
-        {SMB_VFS_OP(catia_get_nt_acl), SMB_VFS_OP_GET_NT_ACL,
-SMB_VFS_LAYER_TRANSPARENT},
-
-        /* POSIX ACL operations */
-
-        {SMB_VFS_OP(catia_chmod_acl), SMB_VFS_OP_CHMOD_ACL,
-SMB_VFS_LAYER_TRANSPARENT},
-
 
         {NULL,                                          SMB_VFS_OP_NOOP,
 SMB_VFS_LAYER_NOOP}
