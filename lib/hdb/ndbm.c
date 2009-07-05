a18db94691c34d666791d4b41142ffc06c6bb4ff
Love Hörnquist Åstrand <lha@kth.se>
2009-07-05 05:08:36 +0000

    Push enterprise support into the bdblayer.
    git-svn-id: svn://svn.h5l.se/heimdal/trunk/heimdal@25316 ec53bebd-3082-4978-b11e-865c3cabbd6b

--- lib/hdb/ndbm.c
+++ lib/hdb/ndbm.c
@@ -348,6 +348,7 @@ hdb_ndbm_create(krb5_context context, HDB **db,
     }
     (*db)->hdb_master_key_set = 0;
     (*db)->hdb_openp = 0;
+    (*db)->hdb_capability_flags = HDB_CAP_F_HANDLE_ENTERPRISE_PRINCIPAL;
     (*db)->hdb_open = NDBM_open;
     (*db)->hdb_close = NDBM_close;
     (*db)->hdb_fetch = _hdb_fetch;
