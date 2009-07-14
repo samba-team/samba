fafe2589e684e1946431722c8845d0dadd9ab525
Stefan Metzmacher <metze@samba.org>
2009-07-14 09:49:25 +0200

    s3:lib: map ENOSYS to NT_STATUS_NOT_SUPPORTED instead of NT_STATUS_ACCESS_DENIED
    Jeremy: please check and decide if we want to backport this.
    
    metze

--- source3/lib/errmap_unix.c
+++ source3/lib/errmap_unix.c
@@ -40,6 +40,7 @@ const struct unix_error_map unix_dos_nt_errmap[] = {
 	{ EISDIR, ERRDOS, ERRnoaccess, NT_STATUS_FILE_IS_A_DIRECTORY},
 	{ EMLINK, ERRDOS, ERRgeneral, NT_STATUS_TOO_MANY_LINKS },
 	{ EINTR,  ERRHRD, ERRgeneral, NT_STATUS_RETRY },
+	{ ENOSYS, ERRDOS, ERRunsup, NT_STATUS_NOT_SUPPORTED },
 #ifdef ELOOP
 	{ ELOOP, ERRDOS, ERRbadpath, NT_STATUS_OBJECT_PATH_NOT_FOUND },
 #endif
