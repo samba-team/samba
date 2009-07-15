2fca950d68bff3641ed3ac4bdaee1d16f0cca88a
Stefan Metzmacher <metze@samba.org>
2009-07-15 20:58:11 +0200

    s3:libsmb: we need to include "includes.h" as first header to let code build on all platforms
    This should fix the Tru64 build.
    
    metze

--- source3/libsmb/libsmb_thread_posix.c
+++ source3/libsmb/libsmb_thread_posix.c
@@ -17,8 +17,8 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <pthread.h>
 #include "includes.h"
+#include <pthread.h>
 #include "libsmbclient.h"
 #include "libsmb_internal.h"
 
