f6bed79ec6f287ac5da79151b97b6cf4776aab7a
Andrew Bartlett <abartlet@samba.org>
2009-07-17 08:39:51 +1000

    Revert "s4:heimdal_build: predefine GSSAPI_DEPRECATED depending on the compiler version"
    This is now handled correctly in the newly imported Heimdal
    
    This reverts commit 4a754d029b0eb229b23980aa4a80dae2b485a302.

--- source4/heimdal_build/krb5-types.h
+++ source4/heimdal_build/krb5-types.h
@@ -10,12 +10,4 @@
 typedef socklen_t krb5_socklen_t;
 typedef ssize_t krb5_ssize_t;
 
-#ifndef GSSAPI_DEPRECATED
-#if (__GNUC__ >= 3) && (__GNUC_MINOR__ >= 1 )
-#define GSSAPI_DEPRECATED __attribute__ ((deprecated))
-#else
-#define GSSAPI_DEPRECATED
-#endif
-#endif
-
 #endif /* __krb5_types_h__ */
