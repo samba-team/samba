0d9fdbceedddb08dbea8ed84e06a218d3ec562f4
Günther Deschner <gd@samba.org>
2009-07-16 11:07:14 +0200

    fix LSA-PRIVILEGES

--- source4/torture/rpc/lsa.c
+++ source4/torture/rpc/lsa.c
@@ -2842,8 +2842,9 @@ struct torture_suite *torture_rpc_lsa_privileges(TALLOC_CTX *mem_ctx)
 
 	suite = torture_suite_create(mem_ctx, "LSA-PRIVILEGES");
 
-	tcase = torture_suite_add_rpc_iface_tcase(suite, "lsa",
-						  &ndr_table_lsarpc);
+	tcase = torture_suite_add_machine_workstation_rpc_iface_tcase(suite, "lsa",
+								      &ndr_table_lsarpc,
+								      TEST_MACHINENAME);
 	torture_rpc_tcase_add_test(tcase, "Privileges",
 				   testcase_Privileges);
 
