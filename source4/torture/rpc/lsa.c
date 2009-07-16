106d43a1ddf1a9ad9369bde17acede2a6071fb6c
Günther Deschner <gd@samba.org>
2009-07-16 18:38:55 +0200

    Revert "fix LSA-TRUSTED-DOMAINS"
    This reverts commit 3c9b26276083002124674678ac757e859fb6b20e.

--- source4/torture/rpc/lsa.c
+++ source4/torture/rpc/lsa.c
@@ -2790,9 +2790,8 @@ struct torture_suite *torture_rpc_lsa_trusted_domains(TALLOC_CTX *mem_ctx)
 
 	suite = torture_suite_create(mem_ctx, "LSA-TRUSTED-DOMAINS");
 
-	tcase = torture_suite_add_machine_workstation_rpc_iface_tcase(suite, "lsa",
-								      &ndr_table_lsarpc,
-								      TEST_MACHINENAME);
+	tcase = torture_suite_add_rpc_iface_tcase(suite, "lsa",
+						  &ndr_table_lsarpc);
 	torture_rpc_tcase_add_test(tcase, "TrustedDomains",
 				   testcase_TrustedDomains);
 
