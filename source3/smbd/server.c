d53cb77ab21cc6edc2f1767bb56a75901c4c1709
Jeremy Allison <jra@samba.org>
2009-07-17 17:33:28 -0700

    Fix bug #6564 - SetPrinter fails (panics) as non root. Missing become_root()/unbecome_root() around reload_services. Jeremy.

--- source3/smbd/server.c
+++ source3/smbd/server.c
@@ -94,6 +94,7 @@ static void smb_conf_updated(struct messaging_context *msg,
 {
 	DEBUG(10,("smb_conf_updated: Got message saying smb.conf was "
 		  "updated. Reloading.\n"));
+	change_to_root_user();
 	reload_services(False);
 }
 
