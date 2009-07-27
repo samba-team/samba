a3dc832aa0b77d639ef3cc5f2a208b8765acdb4c
Stefan Metzmacher <metze@samba.org>
2009-07-27 17:34:37 +0200

    pidl: allow foo being on the wire after [length_is(foo)] uint8 *buffer
    metze
    (cherry picked from commit 92791ce9a8439ac06a22afdbeb0d0fc66c32cb31)
    (cherry picked from commit dd5faa13873fbdd92fa4ddd82dc69d34a73e4d1f)

--- pidl/lib/Parse/Pidl/Samba4/NDR/Parser.pm
+++ pidl/lib/Parse/Pidl/Samba4/NDR/Parser.pm
@@ -1069,6 +1069,10 @@ sub ParseElementPullLevel
 		my $counter = "cntr_$e->{NAME}_$l->{LEVEL_INDEX}";
 		my $array_name = $var_name;
 
+		if ($l->{IS_VARYING}) {
+			$length = "ndr_get_array_length($ndr, " . get_pointer_to($var_name) .")";
+		}
+
 		$var_name = get_array_element($var_name, $counter);
 
 		$self->ParseMemCtxPullStart($e, $l, $ndr, $array_name);
