48078dd24fdf87b6524f4052694e0a6e16dc86fb
Martin Schwenke <martin@meltin.net>
2009-07-29 18:01:07 +1000

    Test suite: Fixes for node state parsing plus new stop/continue tests.
    The parsing of "ctdb status -Y" output to determine various node
    states was implemented very strictly.  Therefore, the parsing broke
    due to the addition of the new "stopped" state to the output of "ctdb
    status -Y".  This relaxes the parsing so that it should work for
    versions prior to the introduction of the "stopped" state, as well as
    future versions that add new states to the end of the list of bits in
    output of "ctdb status -Y".
    
    Similarly the check for cluster unhealthy (in _cluster_is_healthy())
    now just checks for a single 1 in any bit in the "ctdb status -Y"
    output, rather than checking for a particular number of 0s.
    
    New tests
    tests/simple/{41_ctdb_stop.sh,42_ctdb_continue.sh,43_stop_recmaster_yield.sh}
    do rudimentary testing of the stop and continue functions.
    
    Remove tests tests/simple/41_ctdb_ban.sh and
    tests/simple/42_ctdb_unban.sh.  They were both unreliable.
    
    tests/simple/21_ctdb_disablemonitor.sh now schedules a restart, since
    one will be required.
    
    Signed-off-by: Martin Schwenke <martin@meltin.net>
    
    (This used to be ctdb commit 67c5bfb5f02c9d45a32d976021ede4fb2174dfe9)

---
+++ ctdb/tests/simple/42_ctdb_continue.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+test_info()
+{
+    cat <<EOF
+Verify the operation of the 'ctdb continue' command.
+
+This is a superficial test of the 'ctdb continue' command.  It trusts
+information from CTDB that indicates that the IP failover and failback
+has happened correctly.  Another test should check that the failover
+and failback has actually happened at the networking level.
+
+Prerequisites:
+
+* An active CTDB cluster with at least 2 active nodes.
+
+Steps:
+
+1. Verify that the status on all of the ctdb nodes is 'OK'.
+2. Stop one of the nodes using the 'ctdb stop' command.
+3. Verify that the status of the node changes to 'stopped'.
+4. Verify that the public IP addresses that were being served by
+   the node are failed over to one of the other nodes.
+5. Use 'ctdb continue' to bring the node back online.
+6. Verify that the status of the node changes back to 'OK' and that
+   the public IP addresses move back to the node.
+
+Expected results:
+
+* The 'ctdb continue' command successfully brings a stopped node online.
+EOF
+}
+
+. ctdb_test_functions.bash
+
+ctdb_test_init "$@"
+
+set -e
+
+cluster_is_healthy
+
+# Reset configuration
+ctdb_restart_when_done
+
+select_test_node_and_ips
+
+echo "Stopping node ${test_node}..."
+try_command_on_node 1 $CTDB stop -n $test_node
+
+onnode 0 $CTDB_TEST_WRAPPER wait_until_node_has_status $test_node stopped
+
+if wait_until_ips_are_on_nodeglob "[!${test_node}]" $ips ; then
+    echo "All IPs moved."
+else
+    echo "Some IPs didn't move."
+    testfailures=1
+fi
+
+echo "Continuing node $test_node"
+try_command_on_node 1 $CTDB continue -n $test_node
+
+onnode 0 $CTDB_TEST_WRAPPER wait_until_node_has_status $test_node notstopped
+
+# BUG: this is only guaranteed if DeterministicIPs is 1 and
+#      NoIPFailback is 0.
+if wait_until_ips_are_on_nodeglob "$test_node" $ips ; then
+    echo "All IPs moved."
+else
+    echo "Some IPs didn't move."
+    testfailures=1
+fi
