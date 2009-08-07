21d6f276fbb4f4c345c5bd9d4d0ee058cae7ea0b
Stefan Metzmacher <metze@samba.org>
2009-08-07 14:55:37 +0200

    s4:libcli/raw: we don't need to include "smb.h" explicit
    metze

--- source4/libcli/raw/raweas.c
+++ source4/libcli/raw/raweas.c
@@ -18,7 +18,6 @@
 */
 
 #include "includes.h"
-#include "smb.h"
 #include "libcli/raw/libcliraw.h"
 #include "libcli/raw/raw_proto.h"
 
--- source4/libcli/raw/smb_signing.c
+++ source4/libcli/raw/smb_signing.c
@@ -20,7 +20,6 @@
 */
 
 #include "includes.h"
-#include "smb.h"
 #include "libcli/raw/libcliraw.h"
 #include "libcli/raw/raw_proto.h"
 #include "../lib/crypto/crypto.h"
