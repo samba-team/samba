47c7063dc62dc06d0cdd50e1946c088f8bf1ee1d
Jeremy Allison <jra@samba.org>
2009-08-21 21:08:02 -0700

    Try and fix the buildfarm by using getpwnam(root) instead of getpwuid(0) if DEVELOPER is defined. I'm hoping the build farm defines DEVELOPER... Jeremy.

--- source3/auth/token_util.c
+++ source3/auth/token_util.c
@@ -91,10 +91,17 @@ NT_USER_TOKEN *get_root_nt_token( void )
 			cache_data, struct nt_user_token);
 	}
 
+#if defined(DEVELOPER)
+	if ( !(pw = sys_getpwnam("root")) ) {
+		DEBUG(0,("get_root_nt_token: sys_getpwnam(\"root\") failed!\n"));
+		return NULL;
+	}
+#else
 	if ( !(pw = sys_getpwuid(0)) ) {
 		DEBUG(0,("get_root_nt_token: sys_getpwuid(0) failed!\n"));
 		return NULL;
 	}
+#endif
 
 	/* get the user and primary group SIDs; although the
 	   BUILTIN\Administrators SId is really the one that matters here */
