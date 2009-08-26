cc248f7dfccf015586627ecef5fc5a475cc7f842
Michael Adam <obnox@samba.org>
2009-08-26 12:58:47 +0200

    util: fix comment and clarify argument name in DLIST_DEMOTE()
    Michael

--- lib/util/dlinklist.h
+++ lib/util/dlinklist.h
@@ -87,11 +87,11 @@ do { \
 	}\
 } while (0)
 
-/* demote an element to the end of the list, needs a tmp pointer */
-#define DLIST_DEMOTE(list, p, tmp) \
+/* demote an element to the end of the list, needs the entry type */
+#define DLIST_DEMOTE(list, p, type) \
 do { \
 		DLIST_REMOVE(list, p); \
-		DLIST_ADD_END(list, p, tmp); \
+		DLIST_ADD_END(list, p, type); \
 } while (0)
 
 /* concatenate two lists - putting all elements of the 2nd list at the
