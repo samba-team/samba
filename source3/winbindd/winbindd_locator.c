417927019856182c3bd09318def82a561ebc12a5
Volker Lendecke <vl@samba.org>
2009-08-30 11:12:17 +0200

    s3:winbind: Fix bug 5626
    Apparently the AIX compiler can't deal with sizeless array declarations

--- source3/winbindd/winbindd_locator.c
+++ source3/winbindd/winbindd_locator.c
@@ -27,16 +27,7 @@
 #define DBGC_CLASS DBGC_WINBIND
 
 
-static const struct winbindd_child_dispatch_table locator_dispatch_table[];
-
 static struct winbindd_child static_locator_child;
-
-void init_locator_child(void)
-{
-	setup_child(NULL, &static_locator_child,
-		    locator_dispatch_table,
-		    "log.winbindd", "locator");
-}
 
 struct winbindd_child *locator_child(void)
 {
@@ -56,3 +47,10 @@ static const struct winbindd_child_dispatch_table locator_dispatch_table[] = {
 		.name		= NULL,
 	}
 };
+
+void init_locator_child(void)
+{
+	setup_child(NULL, &static_locator_child,
+		    locator_dispatch_table,
+		    "log.winbindd", "locator");
+}
