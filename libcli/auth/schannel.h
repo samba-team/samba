f3979b50a9b2aacb0497df770f09950b78878fcc
Günther Deschner <gd@samba.org>
2009-09-15 18:29:10 +0200

    schannel: move schannel_sign to main directory.
    Guenther

--- libcli/auth/schannel.h
+++ libcli/auth/schannel.h
@@ -1,4 +1,4 @@
-/* 
+/*
    Unix SMB/CIFS implementation.
 
    dcerpc schannel operations
@@ -10,18 +10,17 @@
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "libcli/auth/libcli_auth.h"
-#include "auth/gensec/schannel_state.h"
 #include "libcli/auth/schannel_state.h"
 
 enum schannel_position {
@@ -36,4 +35,4 @@ struct schannel_state {
 	struct netlogon_creds_CredentialState *creds;
 };
 
-#include "auth/gensec/schannel_proto.h"
+#include "libcli/auth/schannel_proto.h"
