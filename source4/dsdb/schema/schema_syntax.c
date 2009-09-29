4fa2bf81843654ebad47cd36b441f337db95b1c6
Stefan Metzmacher <metze@samba.org>
2009-09-29 18:57:01 +0200

    s4:dsdb/schema: add dsdb_syntax_PRESENTATION_ADDRESS_validate_ldb()
    metze

--- source4/dsdb/schema/schema_syntax.c
+++ source4/dsdb/schema/schema_syntax.c
@@ -1942,6 +1942,17 @@ static WERROR dsdb_syntax_PRESENTATION_ADDRESS_ldb_to_drsuapi(struct ldb_context
 	return WERR_OK;
 }
 
+static WERROR dsdb_syntax_PRESENTATION_ADDRESS_validate_ldb(struct ldb_context *ldb,
+							    const struct dsdb_schema *schema,
+							    const struct dsdb_attribute *attr,
+							    const struct ldb_message_element *in)
+{
+	return dsdb_syntax_UNICODE_validate_ldb(ldb,
+						schema,
+						attr,
+						in);
+}
+
 #define OMOBJECTCLASS(val) { .length = sizeof(val) - 1, .data = discard_const_p(uint8_t, val) }
 
 static const struct dsdb_syntax dsdb_syntaxes[] = {
@@ -2169,7 +2180,7 @@ static const struct dsdb_syntax dsdb_syntaxes[] = {
 		.attributeSyntax_oid	= "2.5.5.13",
 		.drsuapi_to_ldb		= dsdb_syntax_PRESENTATION_ADDRESS_drsuapi_to_ldb,
 		.ldb_to_drsuapi		= dsdb_syntax_PRESENTATION_ADDRESS_ldb_to_drsuapi,
-		.validate_ldb		= dsdb_syntax_ALLOW_validate_ldb,
+		.validate_ldb		= dsdb_syntax_PRESENTATION_ADDRESS_validate_ldb,
 		.comment                = "Presentation Address",
 		.ldb_syntax             = LDB_SYNTAX_DIRECTORY_STRING,
 	},{
