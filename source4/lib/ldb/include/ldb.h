ee956c9172c25f40de8cc1b0f8598a4f6075dd44
Matthias Dieter Wallnöfer <mwallnoefer@yahoo.de>
2009-10-02 12:23:25 +0200

    s4:ldb.h - cosmetic - add whitespace

--- source4/lib/ldb/include/ldb.h
+++ source4/lib/ldb/include/ldb.h
@@ -462,8 +462,8 @@ typedef int (*ldb_qsort_cmp_fn_t) (void *v1, void *v2, void *opaque);
 
    \sa <a href="http://opends.dev.java.net/public/standards/draft-zeilenga-ldap-managedit.txt">draft managedit</a>.
 */
-
 #define LDB_CONTROL_RELAX_OID "1.3.6.1.4.1.4203.666.5.12"
+
 /**
    OID for the paged results control. This control is included in the
    searchRequest and searchResultDone messages as part of the controls
