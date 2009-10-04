1341d4509c715ab115425698c82e05ad4fc9f5f8
Volker Lendecke <vl@samba.org>
2009-10-04 16:52:08 +0200

    s3: Fix some nonempty blank lines

--- source3/passdb/util_unixsids.c
+++ source3/passdb/util_unixsids.c
@@ -2,17 +2,17 @@
    Unix SMB/CIFS implementation.
    Translate unix-defined names to SIDs and vice versa
    Copyright (C) Volker Lendecke 2005
-      
+
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -31,7 +31,7 @@ bool sid_check_is_in_unix_users(const DOM_SID *sid)
 
 	sid_copy(&dom_sid, sid);
 	sid_split_rid(&dom_sid, &rid);
-	
+
 	return sid_check_is_unix_users(&dom_sid);
 }
 
@@ -80,7 +80,7 @@ bool sid_check_is_in_unix_groups(const DOM_SID *sid)
 
 	sid_copy(&dom_sid, sid);
 	sid_split_rid(&dom_sid, &rid);
-	
+
 	return sid_check_is_unix_groups(&dom_sid);
 }
 
