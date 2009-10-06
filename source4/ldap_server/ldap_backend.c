ee0204cfccbd73050b2ec806f392bf5c4a549430
Matthias Dieter Wallnöfer <mwallnoefer@yahoo.de>
2009-10-06 21:53:05 +0200

    s4:ldap server - remove unused error handlings
    Those error cases should be handled by LDB itself to be available on all
    connection methods and not only over LDAP.

--- source4/ldap_server/ldap_backend.c
+++ source4/ldap_server/ldap_backend.c
@@ -534,26 +534,11 @@ static NTSTATUS ldapsrv_ModifyRequest(struct ldapsrv_call *call)
 				NT_STATUS_HAVE_NO_MEMORY(msg->elements[i].values);
 
 				for (j=0; j < msg->elements[i].num_values; j++) {
-					if (!(req->mods[i].attrib.values[j].length > 0)) {
-						result = LDAP_OTHER;
-
-						map_ldb_error(local_ctx,
-							LDB_ERR_OTHER, &errstr);
-						errstr = talloc_asprintf(local_ctx,
-							"%s. Empty attribute values not allowed", errstr);
-						goto reply;
-					}
 					msg->elements[i].values[j].length = req->mods[i].attrib.values[j].length;
 					msg->elements[i].values[j].data = req->mods[i].attrib.values[j].data;			
 				}
 			}
 		}
-	} else {
-		result = LDAP_OTHER;
-		map_ldb_error(local_ctx, LDB_ERR_OTHER, &errstr);
-		errstr = talloc_asprintf(local_ctx,
-			"%s. No mods are not allowed", errstr);
-		goto reply;
 	}
 
 reply:
@@ -628,31 +613,11 @@ static NTSTATUS ldapsrv_AddRequest(struct ldapsrv_call *call)
 				NT_STATUS_HAVE_NO_MEMORY(msg->elements[i].values);
 
 				for (j=0; j < msg->elements[i].num_values; j++) {
-					if (!(req->attributes[i].values[j].length > 0)) {
-						result = LDAP_OTHER;
-						map_ldb_error(local_ctx,
-							LDB_ERR_OTHER, &errstr);
-						errstr = talloc_asprintf(local_ctx,
-							"%s. Empty attribute values not allowed", errstr);
-						goto reply;
-					}
 					msg->elements[i].values[j].length = req->attributes[i].values[j].length;
 					msg->elements[i].values[j].data = req->attributes[i].values[j].data;			
 				}
-			} else {
-				result = LDAP_OTHER;
-				map_ldb_error(local_ctx, LDB_ERR_OTHER, &errstr);
-				errstr = talloc_asprintf(local_ctx,
-					"%s. No attribute values are not allowed", errstr);
-				goto reply;
 			}
 		}
-	} else {
-		result = LDAP_OTHER;
-		map_ldb_error(local_ctx, LDB_ERR_OTHER, &errstr);
-		errstr = talloc_asprintf(local_ctx, 
-			"%s. No attributes are not allowed", errstr);
-		goto reply;
 	}
 
 reply:
