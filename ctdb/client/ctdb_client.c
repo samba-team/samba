1c6829f3c2d5d9bcab8583fe9e8f814cad230099
Stefan Metzmacher <metze@samba.org>
2009-10-09 15:47:06 +0200

    ctdb_client: fix DEBUG statement in ctdb_ctrl_modflags()
    metze
    
    (This used to be ctdb commit a244b75ee49556b0ff51e254cc812594ee3b23a7)

--- ctdb/client/ctdb_client.c
+++ ctdb/client/ctdb_client.c
@@ -2386,7 +2386,7 @@ int ctdb_ctrl_modflags(struct ctdb_context *ctdb, struct timeval timeout, uint32
 					timeout, false, data,
 					NULL, NULL,
 					NULL) != 0) {
-		DEBUG(DEBUG_ERR, (__location__ " ctdb_control to disable node failed\n"));
+		DEBUG(DEBUG_ERR, (__location__ " Unable to update nodeflags on remote nodes\n"));
 
 		talloc_free(tmp_ctx);
 		return -1;
