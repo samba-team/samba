0c0eb14767f4e77780a531ff2873dcad9204a16a
Andrew Tridgell <tridge@samba.org>
2009-10-12 13:30:52 +1100

    s4-drs: make DsBind a bit less verbose

--- source4/rpc_server/drsuapi/dcesrv_drsuapi.c
+++ source4/rpc_server/drsuapi/dcesrv_drsuapi.c
@@ -60,7 +60,7 @@ static WERROR dcesrv_drsuapi_DsBind(struct dcesrv_call_state *dce_call, TALLOC_C
 	/* if this is a DC connecting, give them system level access */
 	werr = drs_security_level_check(dce_call, NULL);
 	if (W_ERROR_IS_OK(werr)) {
-		DEBUG(0,(__location__ ": doing DsBind with system_session\n"));
+		DEBUG(2,(__location__ ": doing DsBind with system_session\n"));
 		auth_info = system_session(b_state, dce_call->conn->dce_ctx->lp_ctx);
 	} else {
 		auth_info = dce_call->conn->auth_state.session_info;
