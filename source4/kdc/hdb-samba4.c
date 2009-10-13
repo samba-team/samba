11a8a54c825a52d7dd6ab78bc7aeff2d719327d2
Matthias Dieter Wallnöfer <mwallnoefer@yahoo.de>
2009-10-13 17:36:58 +0200

    s4:hdb-samba4 - Don't double-free "db"
    "db" is freed anyway after the destructor terminates so this does really make
    no sense here (rather it makes code crash).
    
    Should fix bug #6801.

--- source4/kdc/hdb-samba4.c
+++ source4/kdc/hdb-samba4.c
@@ -1442,7 +1442,6 @@ static krb5_error_code hdb_samba4_nextkey(krb5_context context, HDB *db, unsigne
 
 static krb5_error_code hdb_samba4_destroy(krb5_context context, HDB *db)
 {
-	talloc_free(db);
 	return 0;
 }
 
