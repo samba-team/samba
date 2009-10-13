e0e7ca4ee83dac62f5b1d61a711a852679e0c0a0
Andrew Bartlett <abartlet@samba.org>
2009-10-14 09:10:01 +1100

    Revert "s4:hdb-samba4 - Don't double-free "db""
    This reverts commit 11a8a54c825a52d7dd6ab78bc7aeff2d719327d2.
    
    The actual fix for bug 6801 is in hdb_end_seq_get() - this attempt
    leaks 'db' instead.
    
    Andrew Bartlett

--- source4/kdc/hdb-samba4.c
+++ source4/kdc/hdb-samba4.c
@@ -1442,6 +1442,7 @@ static krb5_error_code hdb_samba4_nextkey(krb5_context context, HDB *db, unsigne
 
 static krb5_error_code hdb_samba4_destroy(krb5_context context, HDB *db)
 {
+	talloc_free(db);
 	return 0;
 }
 
