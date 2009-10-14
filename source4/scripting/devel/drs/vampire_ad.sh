b41290c10dc3e6c783b7a51477be903d202ed074
Andrew Tridgell <tridge@samba.org>
2009-10-15 08:49:21 +1100

    s4-devel: for devel scripts its better to use bin/ than $PREFIX/bin
    This avoids having to do make install after each change when using the
    drs devel scripts

--- source4/scripting/devel/drs/vampire_ad.sh
+++ source4/scripting/devel/drs/vampire_ad.sh
@@ -9,8 +9,9 @@ cp `dirname $0`/named.conf.ad.template $namedtmp
 sed -i "s/DNSDOMAIN/$DNSDOMAIN/g" $namedtmp
 sed -i "s/SERVERIP/$server_ip/g" $namedtmp
 chmod a+r $namedtmp
-mv $namedtmp $PREFIX/private/named.conf
+mv -f $namedtmp $PREFIX/private/named.conf
 sudo rndc reconfig
 `dirname $0`/unvampire_ad.sh
-sudo $PREFIX/bin/net vampire $DNSDOMAIN -Uadministrator%$pass -s $PREFIX/etc/smb.conf -d2 || exit 1
+sudo bin/net vampire $DNSDOMAIN -Uadministrator%$pass -s $PREFIX/etc/smb.conf -d2 || exit 1
 PRIVATEDIR=$PREFIX/private sudo -E scripting/bin/setup_dns.sh $machine $DNSDOMAIN $machine_ip || exit 1
+sudo rndc flush
