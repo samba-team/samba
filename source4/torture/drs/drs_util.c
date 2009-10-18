1ecca31caa8c366264e8b15fdb6363a4a5338cde
Kamen Mazdrashki <kamen.mazdrashki@postpath.com>
2009-10-18 21:28:28 +0300

    s4/drs(tort): fixed '==' to '>='
    ATTID is marked as partial by adding 32768.
    Thus the check should be '>= 32768'

--- source4/torture/drs/drs_util.c
+++ source4/torture/drs/drs_util.c
@@ -78,7 +78,7 @@ bool drs_util_oid_from_attid(struct torture_context *tctx,
 		bin_oid.data[bin_oid.length-1] = lo_word;
 	}
 	else {
-		if (lo_word == 32768) {
+		if (lo_word >= 32768) {
 			lo_word -= 32768;
 		}
 		bin_oid.data[bin_oid.length-2] = ((lo_word / 128) % 128) + 128; // (0x80 | ((lo_word>>7)&0x7f))
