2b13c2108f8b746dbe866fc14dd5e6045b0d12ea
Rusty Russell <rusty@rustcorp.com.au>
2009-10-22 00:09:43 +1030

    lib/tdb: wean off TDB_ERRCODE.
    It was a regrettable hack which I used to reduce line count in tdb; in fact it caused confusion as can be seen in this patch.
    In particular, ecode now needs to be set before TDB_LOG anyway, and having it exposed in
    the header is useless (the struct tdb_context isn't defined, so it's doubly useless).
    Also, we should never set errno, as io.c was doing.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- lib/tdb/include/tdb.h
+++ lib/tdb/include/tdb.h
@@ -49,8 +49,6 @@ extern "C" {
 #define TDB_SEQNUM   128 /* maintain a sequence number */
 #define TDB_VOLATILE   256 /* Activate the per-hashchain freelist, default 5 */
 
-#define TDB_ERRCODE(code, ret) ((tdb->ecode = (code)), ret)
-
 /* error codes */
 enum TDB_ERROR {TDB_SUCCESS=0, TDB_ERR_CORRUPT, TDB_ERR_IO, TDB_ERR_LOCK, 
 		TDB_ERR_OOM, TDB_ERR_EXISTS, TDB_ERR_NOLOCK, TDB_ERR_LOCK_TIMEOUT,
