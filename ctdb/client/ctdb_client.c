c532347a450fc186051d5e087cb8ddd8ebe9d364
Michael Adam <obnox@samba.org>
2009-10-29 22:53:44 +0100

    client: randomize the transaction_start retry loop:
    instead of sleeping 1 second, sleep between 1 and 100 milliseconds
    
    Michael
    
    (This used to be ctdb commit a5d90d8ed8b44355c4ffb9c32ded772025fcc174)

--- ctdb/client/ctdb_client.c
+++ ctdb/client/ctdb_client.c
@@ -3226,11 +3226,13 @@ again:
 					      CTDB_CURRENT_NODE,
 					      ctdb_db->db_id);
 	if (status == 1) {
+		unsigned long int usec = (1000 + random()) % 100000;
 		DEBUG(DEBUG_NOTICE, (__location__ " transaction is active "
-				     "on db_id[0x%08x]. waiting for 1 second\n",
-				     ctdb_db->db_id));
+				     "on db_id[0x%08x]. waiting for %lu "
+				     "microseconds\n",
+				     ctdb_db->db_id, usec));
 		talloc_free(tmp_ctx);
-		sleep(1);
+		usleep(usec);
 		goto again;
 	}
 
