c9df4a3c28d21e8acc9df46def8805e56eb8b4ce
Matthias Dieter Wallnöfer <mwallnoefer@yahoo.de>
2009-11-06 15:21:56 +0100

    s4:dcesrv_samr_ValidatePassword - naturally this was only for debugging the failure

--- source4/rpc_server/samr/dcesrv_samr.c
+++ source4/rpc_server/samr/dcesrv_samr.c
@@ -4394,7 +4394,6 @@ static NTSTATUS dcesrv_samr_ValidatePassword(struct dcesrv_call_state *dce_call,
 		(*r->out.rep)->ctr2.status = res;
 	break;
 	case NetValidatePasswordReset:
-printf("Length/Size: %i|%i\n", r->in.req->req3.password.length, r->in.req->req3.password.size);
 		password = data_blob_const(r->in.req->req3.password.string,
 					   r->in.req->req3.password.length);
 		res = samdb_check_password(&password,
