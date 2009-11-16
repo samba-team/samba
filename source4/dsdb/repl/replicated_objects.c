e853dd763b56870643b135d3252ab0b3eaf25a00
Matthias Dieter Wallnöfer <mwallnoefer@yahoo.de>
2009-11-16 17:01:43 +0100

    Revert "s4:dsdb/repl/replicated_objects - Applicate also here the new "lDAPDisplayName" generator"
    This reverts commit df95d5c29292968b465bff24c3cf78800677a4d4.
    
    abartlet pointed out in a post on the samba-technical list that this isn't
    necessary at all (lDAPDisplayName normalisation algorithm). Rather it breaks
    functionality of the replication.

--- source4/dsdb/repl/replicated_objects.c
+++ source4/dsdb/repl/replicated_objects.c
@@ -153,10 +153,7 @@ static WERROR dsdb_convert_object_ex(struct ldb_context *ldb,
 		struct ldb_message_element *el;
 		el = ldb_msg_find_element(msg, rdn_attr->lDAPDisplayName);
 		if (!el) {
-			/* we assume that the RDN has prefix "CN" */
-			ret = ldb_msg_add_string(msg, rdn_attr->lDAPDisplayName,
-				samdb_cn_to_lDAPDisplayName(mem_ctx,
-					(const char *) rdn_value->data));
+			ret = ldb_msg_add_value(msg, rdn_attr->lDAPDisplayName, rdn_value, NULL);
 			if (ret != LDB_SUCCESS) {
 				return WERR_FOOBAR;
 			}
