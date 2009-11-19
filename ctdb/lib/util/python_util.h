c5827f2c2d98ccd9b345c4499733413e60ae9919
Stefan Metzmacher <metze@samba.org>
2009-11-19 16:49:13 +0100

    s3/s4 build: Fix Py_RETURN_NONE to work with python versions < 2.4 (cherry picked from samba commit 61a23c5eea2203721a70a4ad41bfdc6cd1d6a2a7)
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    
    (This used to be ctdb commit 4130c5dd10869b071124e2bf04d6807bbb11ab1f)

---
+++ ctdb/lib/util/python_util.h
@@ -0,0 +1,31 @@
+/*
+   Unix SMB/CIFS implementation.
+   Samba Python utility functions
+   Copyright (C) Tim Prouty <tprouty@samba.org> 2009
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _PYTHON_UTIL_H_
+#define _PYTHON_UTIL_H_
+
+#include <Python.h>
+
+#if PY_VERSION_HEX < 0x02040000
+#define Py_RETURN_NONE return Py_INCREF(Py_None), Py_None
+#define Py_RETURN_TRUE return Py_INCREF(Py_True), Py_True
+#define Py_RETURN_FALSE return Py_INCREF(Py_False), Py_False
+#endif
+
+#endif /* _PYTHON_UTIL_H_ */
