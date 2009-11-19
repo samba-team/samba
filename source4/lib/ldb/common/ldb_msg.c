51a59e8f9ed930eaa8f2936972b810fdd0acccbd
Andrew Tridgell <tridge@samba.org>
2009-11-19 16:03:59 -0200

    s4-ldb: added a warning about ldb_msg_add_dn
    ldb_msg_add_dn does not copy the dn linearized string

--- source4/lib/ldb/common/ldb_msg.c
+++ source4/lib/ldb/common/ldb_msg.c
@@ -260,6 +260,8 @@ int ldb_msg_add_steal_string(struct ldb_message *msg,
 
 /*
   add a DN element to a message
+  WARNING: this uses the linearized string from the dn, and does not
+  copy the string.
 */
 int ldb_msg_add_dn(struct ldb_message *msg, const char *attr_name,
 		   struct ldb_dn *dn)
