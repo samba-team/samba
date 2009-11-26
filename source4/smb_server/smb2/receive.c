b62964d6feb47ccf14e3d14120c04bc435c47cbd
Stefan Metzmacher <metze@samba.org>
2009-11-26 14:18:50 +0100

    s4:smb_server/smb2: don't reset highest_smb2_seqnum on SMB2 Cancel
    metze

--- source4/smb_server/smb2/receive.c
+++ source4/smb_server/smb2/receive.c
@@ -328,8 +328,10 @@ static NTSTATUS smb2srv_reply(struct smb2srv_request *req)
 		smbsrv_terminate_connection(req->smb_conn, "Invalid SMB2 sequence number");
 		return NT_STATUS_INVALID_PARAMETER;
 	}
-	req->smb_conn->highest_smb2_seqnum = req->seqnum;
-	
+	if (opcode != SMB2_OP_CANCEL) {
+		req->smb_conn->highest_smb2_seqnum = req->seqnum;
+	}
+
 	req->session	= smbsrv_session_find(req->smb_conn, uid, req->request_time);
 	req->tcon	= smbsrv_smb2_tcon_find(req->session, tid, req->request_time);
 
