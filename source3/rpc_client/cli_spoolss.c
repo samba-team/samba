5f60855ba2eb822dcb867378ff09278c42931b89
Günther Deschner <gd@samba.org>
2009-12-02 14:25:31 +0100

    samba-spoolss: use spoolss_StringArray2 in spoolss_EnumPrinterKey.
    This should finally resolve the endian issues we were seeing on sparc and is
    much cleaner for spoolss clients and servers.
    
    Guenther

--- source3/rpc_client/cli_spoolss.c
+++ source3/rpc_client/cli_spoolss.c
@@ -810,44 +810,28 @@ WERROR rpccli_spoolss_enumprinterkey(struct rpc_pipe_client *cli,
 	NTSTATUS status;
 	WERROR werror;
 	uint32_t needed;
-	uint16_t *buffer = NULL;
-
-	*key_buffer = NULL;
-
-	if (offered) {
-		buffer = talloc_array(mem_ctx, uint16_t, offered/2);
-		W_ERROR_HAVE_NO_MEMORY(buffer);
-	}
+	struct spoolss_StringArray2 _key_buffer;
 
 	status = rpccli_spoolss_EnumPrinterKey(cli, mem_ctx,
 					       handle,
 					       key_name,
-					       buffer,
+					       &_key_buffer,
 					       offered,
 					       &needed,
 					       &werror);
 
 	if (W_ERROR_EQUAL(werror, WERR_MORE_DATA)) {
 		offered = needed;
-		buffer = talloc_realloc(mem_ctx, buffer, uint16_t, needed/2);
-		W_ERROR_HAVE_NO_MEMORY(buffer);
 		status = rpccli_spoolss_EnumPrinterKey(cli, mem_ctx,
 						       handle,
 						       key_name,
-						       buffer,
+						       &_key_buffer,
 						       offered,
 						       &needed,
 						       &werror);
 	}
 
-	if (W_ERROR_IS_OK(werror)) {
-		const char **array;
-		DATA_BLOB blob = data_blob_const((uint8_t *)buffer, offered);
-		if (!pull_reg_multi_sz(mem_ctx, &blob, &array)) {
-			return WERR_NOMEM;
-		}
-		*key_buffer = array;
-	}
+	*key_buffer = _key_buffer.string;
 
 	return werror;
 }
