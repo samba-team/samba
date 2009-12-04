830f18c5f4fc85a1dc49198365ed6ed7061e5dee
Stefan Metzmacher <metze@samba.org>
2009-12-04 16:32:42 +0100

    s3: let tdb_validate_child() use tdb_check()
    metze
    (cherry picked from commit 6f6608c2aa6122c87ea1e8a937708d08e6c5785e)
    
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    (cherry picked from commit 66847adf072606e6519a1de1ede3aa26d9e041ba)

--- source3/lib/tdb_validate.c
+++ source3/lib/tdb_validate.c
@@ -44,6 +44,17 @@ static int tdb_validate_child(struct tdb_context *tdb,
 		goto out;
 	}
 
+	/*
+	 * we can simplify this by passing a check function,
+	 * but I don't want to change all the callers...
+	 */
+	ret = tdb_check(tdb, NULL, NULL);
+	if (ret == -1) {
+		v_status.tdb_error = True;
+		v_status.success = False;
+		goto out;
+	}
+
 	/* Check if the tdb's freelist is good. */
 	if (tdb_validate_freelist(tdb, &num_entries) == -1) {
 		v_status.bad_freelist = True;
