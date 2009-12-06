bf7cc3262e3cbd72a3603d3c648fccfe7ce9829f
Kamen Mazdrashki <kamen.mazdrashki@postpath.com>
2009-12-06 04:05:45 +0200

    krb5: Fix leaked hx509_context pointer
    Signed-off-by: Andrew Tridgell <tridge@samba.org>

--- source4/heimdal/lib/krb5/context.c
+++ source4/heimdal/lib/krb5/context.c
@@ -474,6 +474,10 @@ krb5_free_context(krb5_context context)
     krb5_set_ignore_addresses(context, NULL);
     krb5_set_send_to_kdc_func(context, NULL, NULL);
 
+#ifdef PKINIT
+    hx509_context_free(&context->hx509ctx);
+#endif
+
     HEIMDAL_MUTEX_destroy(context->mutex);
     free(context->mutex);
 
