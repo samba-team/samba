f8bb5a60e71bd51971ef05a23037cac4523dee25
Tim Prouty <tprouty@samba.org>
2009-12-16 14:31:54 -0800

    s4 torture: Parameterize streams delete behavior rather than commenting out the check.

--- source4/torture/raw/streams.c
+++ source4/torture/raw/streams.c
@@ -607,9 +607,13 @@ static bool test_stream_delete(struct torture_context *tctx,
 
 	status = smb_raw_fileinfo(cli->tree, tctx, &finfo);
 	CHECK_STATUS(status, NT_STATUS_OK);
-	/* w2k and w2k3 return 0 and w2k8 returns 1
-	CHECK_VALUE(finfo.all_info.out.delete_pending, 0);
-	*/
+
+	/* w2k and w2k3 return 0 and w2k8 returns 1 */
+	if (TARGET_IS_WINXP(tctx) || TARGET_IS_W2K3(tctx)) {
+		CHECK_VALUE(finfo.all_info.out.delete_pending, 0);
+	} else {
+		CHECK_VALUE(finfo.all_info.out.delete_pending, 1);
+	}
 
 	smbcli_close(cli->tree, fnum);
 
