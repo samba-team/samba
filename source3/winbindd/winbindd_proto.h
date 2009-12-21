01d794dfefc1e2c5763c8e20c7acbd7c54d592ab
Volker Lendecke <vl@samba.org>
2009-12-21 14:40:09 +0100

    s3: Remove unused winbindd_gid2sid_async (cherry picked from commit 83dac5e827eb5002e581f50662b23b3167a03bd0)

--- source3/winbindd/winbindd_proto.h
+++ source3/winbindd/winbindd_proto.h
@@ -403,9 +403,6 @@ enum winbindd_result winbindd_dual_sid2gid(struct winbindd_domain *domain,
 					   struct winbindd_cli_state *state);
 enum winbindd_result winbindd_dual_uid2sid(struct winbindd_domain *domain,
 					   struct winbindd_cli_state *state);
-void winbindd_gid2sid_async(TALLOC_CTX *mem_ctx, gid_t gid,
-			    void (*cont)(void *private_data, bool success, const char *sid),
-			    void *private_data);
 enum winbindd_result winbindd_dual_gid2sid(struct winbindd_domain *domain,
 					   struct winbindd_cli_state *state);
 
