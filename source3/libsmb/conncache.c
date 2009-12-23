50e5f9dc512a7356b16bb68bdd4a80ae9dd436b3
Volker Lendecke <vl@samba.org>
2009-12-23 15:22:09 +0100

    s3: Fix some nonempty blank lines

--- source3/libsmb/conncache.c
+++ source3/libsmb/conncache.c
@@ -13,12 +13,12 @@
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -191,7 +191,7 @@ void add_failed_connection_entry(const char *domain, const char *server,
 		DEBUG(1,("add_failed_connection_entry: failed to add "
 			  "domain %s (%s) to failed conn cache\n",
 			  domain, server));
-	
+
  done:
 	TALLOC_FREE(key);
 	TALLOC_FREE(value);
