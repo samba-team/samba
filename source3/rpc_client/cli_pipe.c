b58b359881c91ec382cfa1d6ba3007b8354b29cb
Bo Yang <boyang@samba.org>
2010-01-06 12:13:35 +0800

    s3: Fix infinite loop in NCACN_IP_TCP asa there is no timeout. Assume lsa_pipe_tcp is ok but network is down, then send request is ok, but select() on writeable fds loops forever since there is no response.
    Signed-off-by: Bo Yang <boyang@samba.org>
    (cherry picked from commit 36493bf2f6634b84c57107bcb86bcbf3e82e80fc)

--- source3/rpc_client/cli_pipe.c
+++ source3/rpc_client/cli_pipe.c
@@ -3034,12 +3034,30 @@ NTSTATUS rpc_pipe_bind(struct rpc_pipe_client *cli,
 unsigned int rpccli_set_timeout(struct rpc_pipe_client *rpc_cli,
 				unsigned int timeout)
 {
-	struct cli_state *cli = rpc_pipe_np_smb_conn(rpc_cli);
+	struct cli_state *cli;
 
-	if (cli == NULL) {
-		return 0;
+	if (rpc_cli->transport->transport == NCACN_NP) {
+		cli = rpc_pipe_np_smb_conn(rpc_cli);
+		if (cli == NULL) {
+			return 0;
+		}
+		return cli_set_timeout(cli, timeout);
 	}
-	return cli_set_timeout(cli, timeout);
+
+	if (rpc_cli->transport->transport == NCACN_IP_TCP ||
+	    rpc_cli->transport->transport == NCALRPC) {
+		return rpccli_set_sock_timeout(rpc_cli, timeout);
+	}
+
+	if (rpc_cli->transport->transport == NCACN_INTERNAL) {
+		cli = rpc_pipe_smbd_smb_conn(rpc_cli);
+		if (!cli) {
+			return 0;
+		}
+		return cli_set_timeout(cli, timeout);
+	}
+
+	return 0;
 }
 
 bool rpccli_get_pwd_hash(struct rpc_pipe_client *rpc_cli, uint8_t nt_hash[16])
