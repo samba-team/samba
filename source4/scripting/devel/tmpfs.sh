612eec4aa737de40cc18ef10722d35a36803816f
Andrew Bartlett <abartlet@samba.org>
2010-01-18 19:24:45 +1300

    s4:scripting/devel Allow tmpfs script to be re-run
    By doing the unmount, we can avoid double-mounting st and bin

--- source4/scripting/devel/tmpfs.sh
+++ source4/scripting/devel/tmpfs.sh
@@ -3,7 +3,10 @@
 # This sets up bin/ and st/ as tmpfs filesystems, which saves a lot of
 # time waiting on the disk!
 
-rm -rf bin st
+sudo echo "About to (re)mount bin and st as tmpfs"
+rm -rf bin st 
+sudo umount bin > /dev/null 2>&1 
+sudo umount st  > /dev/null 2>&1 
 mkdir -p bin st || exit 1
 sudo mount -t tmpfs /dev/null bin || exit 1
 sudo chown $USER bin || exit 1
