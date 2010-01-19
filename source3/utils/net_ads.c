samba-team/samba

commit bd3c922e2bc1a163efc1d8c9cb59578bebb79616

s3/net: split up some printable stings to ease i18n
If we put strings like "Usage:" into separate _() macros and not the whole
"Usage:..." string we can cover much more messages by only one single
translation. The drawback is that the message in the sources looks less pretty.

source3/utils/net_ads.c
@@ -128,9 +128,9 @@ static int net_ads_lookup(struct net_context *c, int argc, const char **argv)
 	int ret;
 
 	if (c->display_usage) {
-		d_printf(_("Usage:\n"
+		d_printf(_("Usage:\n"),
 			   "net ads lookup\n"
-			   "    Find the ADS DC using CLDAP lookup.\n"));
+			   "    ",_("Find the ADS DC using CLDAP lookup.\n"));
 		return 0;
 	}
 
@@ -158,9 +158,10 @@ static int net_ads_info(struct net_context *c, int argc, const char **argv)
 	char addr[INET6_ADDRSTRLEN];
 
 	if (c->display_usage) {
-		d_printf(_("Usage:\n"
-			   "net ads info\n"
-			   "    Display information about an Active Directory "
+		d_printf(_("Usage:\n"),
+			   "net ads info\n",
+			   "    ",
+			   _("Display information about an Active Directory "
 			   "server.\n"));
 		return 0;
 	}
@@ -372,9 +373,9 @@ static int net_ads_workgroup(struct net_context *c, int argc, const char **argv)
 	struct NETLOGON_SAM_LOGON_RESPONSE_EX reply;
 
 	if (c->display_usage) {
-		d_printf(_("Usage:\n"
+		d_printf(_("Usage:\n"),
 			   "net ads workgroup\n"
-			   "    Print the workgroup name\n"));
+			   "    ",_("Print the workgroup name\n"));
 		return 0;
 	}
 
@@ -697,9 +698,9 @@ int net_ads_user(struct net_context *c, int argc, const char **argv)
 
 	if (argc == 0) {
 		if (c->display_usage) {
-			d_printf(_("Usage:\n"
+			d_printf(_("Usage:\n"),
 			           "net ads user\n"
-				   "    List AD users\n"));
+				   "    ",_("List AD users\n"));
 			net_display_usage_from_functable(func);
 			return 0;
 		}
@@ -848,9 +849,9 @@ int net_ads_group(struct net_context *c, int argc, const char **argv)
 
 	if (argc == 0) {
 		if (c->display_usage) {
-			d_printf(_("Usage:\n"
+			d_printf(_("Usage:\n"),
 				   "net ads group\n"
-				   "    List AD groups\n"));
+				   "    ", _("List AD groups\n"));
 			net_display_usage_from_functable(func);
 			return 0;
 		}
@@ -882,9 +883,9 @@ static int net_ads_status(struct net_context *c, int argc, const char **argv)
 	LDAPMessage *res;
 
 	if (c->display_usage) {
-		d_printf(_("Usage:\n"
+		d_printf(_("Usage:\n"),
 			   "net ads status\n"
-			   "    Display machine account details\n"));
+			   "    ",_("Display machine account details\n"));
 		return 0;
 	}
 
@@ -924,9 +925,9 @@ static int net_ads_leave(struct net_context *c, int argc, const char **argv)
 	WERROR werr;
 
 	if (c->display_usage) {
-		d_printf(_("Usage:\n"
+		d_printf(_("Usage:\n"),
 			   "net ads leave\n"
-			   "    Leave an AD domain\n"));
+			   "    ", _("Leave an AD domain\n"));
 		return 0;
 	}
 
@@ -1037,9 +1038,9 @@ int net_ads_testjoin(struct net_context *c, int argc, const char **argv)
 	use_in_memory_ccache();
 
 	if (c->display_usage) {
-		d_printf(_("Usage:\n"
+		d_printf(_("Usage:\n"),
 			   "net ads testjoin\n"
-			   "    Test if the existing join is ok\n"));
+			   "    ", _("Test if the existing join is ok\n"));
 		return 0;
 	}
 
@@ -1416,9 +1417,9 @@ static int net_ads_dns_register(struct net_context *c, int argc, const char **ar
 #endif
 
 	if (argc > 0 || c->display_usage) {
-		d_printf(_("Usage:\n"
+		d_printf(_("Usage:\n"),
 			   "net ads dns register\n"
-			   "    Register hostname with DNS\n"));
+			   "    ", _("Register hostname with DNS\n"));
 		return -1;
 	}
 
@@ -1468,8 +1469,8 @@ static int net_ads_dns_gethostbyname(struct net_context *c, int argc, const char
 #endif
 
 	if (argc != 2 || c->display_usage) {
-		d_printf(_("Usage:\n"
-			   "net ads dns gethostbyname <server> <name>\n"
+		d_printf(_("Usage:\n"),
+			   _("net ads dns gethostbyname <server> <name>\n"),
 			   "  Look up hostname from the AD\n"
 			   "    server\tName server to use\n"
 			   "    name\tName to look up\n"));
@@ -1538,9 +1539,9 @@ static int net_ads_printer_search(struct net_context *c, int argc, const char **
 	LDAPMessage *res = NULL;
 
 	if (c->display_usage) {
-		d_printf(_("Usage:\n"
+		d_printf(_("Usage:\n"),
 			   "net ads printer search\n"
-			   "    List printers in the AD\n"));
+			   "    ", _("List printers in the AD\n"));
 		return 0;
 	}
 
@@ -1578,8 +1579,8 @@ static int net_ads_printer_info(struct net_context *c, int argc, const char **ar
 	LDAPMessage *res = NULL;
 
 	if (c->display_usage) {
-		d_printf(_("Usage:\n"
-			   "net ads printer info [printername [servername]]\n"
+		d_printf(_("Usage:\n"),
+			   _("net ads printer info [printername [servername]]\n"
 			   "  Display printer info from AD\n"
 			   "    printername\tPrinter name or wildcard\n"
 			   "    servername\tName of the print server\n"));
@@ -1642,8 +1643,8 @@ static int net_ads_printer_publish(struct net_context *c, int argc, const char *
 	LDAPMessage *res = NULL;
 
 	if (argc < 1 || c->display_usage) {
-		d_printf(_("Usage:\n"
-			   "net ads printer publish <printername> [servername]\n"
+		d_printf(_("Usage:\n"),
+			   _("net ads printer publish <printername> [servername]\n"
 			   "  Publish printer in AD\n"
 			   "    printername\tName of the printer\n"
 			   "    servername\tName of the print server\n"));
@@ -1768,8 +1769,8 @@ static int net_ads_printer_remove(struct net_context *c, int argc, const char **
 	LDAPMessage *res = NULL;
 
 	if (argc < 1 || c->display_usage) {
-		d_printf(_("Usage:\n"
-			   "net ads printer remove <printername> [servername]\n"
+		d_printf(_("Usage:\n"),
+			   _("net ads printer remove <printername> [servername]\n"
 			   "  Remove a printer from the AD\n"
 			   "    printername\tName of the printer\n"
 			   "    servername\tName of the print server\n"));
@@ -1871,8 +1872,8 @@ static int net_ads_password(struct net_context *c, int argc, const char **argv)
 	ADS_STATUS ret;
 
 	if (c->display_usage) {
-		d_printf(_("Usage:\n"
-			   "net ads password <username>\n"
+		d_printf(_("Usage:\n"),
+			   _("net ads password <username>\n"
 			   "  Change password for user\n"
 			   "    username\tName of user to change password for\n"));
 		return 0;
@@ -1954,9 +1955,9 @@ int net_ads_changetrustpw(struct net_context *c, int argc, const char **argv)
 	ADS_STATUS ret;
 
 	if (c->display_usage) {
-		d_printf(_("Usage:\n"
+		d_printf(_("Usage:\n"),
 			   "net ads changetrustpw\n"
-			   "    Change the machine account's trust password\n"));
+			   "    ", _("Change the machine account's trust password\n"));
 		return 0;
 	}
 
@@ -2195,9 +2196,9 @@ static int net_ads_keytab_flush(struct net_context *c, int argc, const char **ar
 	ADS_STRUCT *ads;
 
 	if (c->display_usage) {
-		d_printf(_("Usage:\n"
+		d_printf(_("Usage:\n"),
 			   "net ads keytab flush\n"
-			   "    Delete the whole keytab\n"));
+			   "    ", _("Delete the whole keytab\n"));
 		return 0;
 	}
 
@@ -2216,8 +2217,8 @@ static int net_ads_keytab_add(struct net_context *c, int argc, const char **argv
 	ADS_STRUCT *ads;
 
 	if (c->display_usage) {
-		d_printf(_("Usage:\n"
-			   "net ads keytab add <principal> [principal ...]\n"
+		d_printf(_("Usage:\n"),
+			   _("net ads keytab add <principal> [principal ...]\n"
 			   "  Add principals to local keytab\n"
 			   "    principal\tKerberos principal to add to "
 			   "keytab\n"));
@@ -2241,9 +2242,9 @@ static int net_ads_keytab_create(struct net_context *c, int argc, const char **a
 	int ret;
 
 	if (c->display_usage) {
-		d_printf(_("Usage:\n"
+		d_printf(_("Usage:\n"),
 			   "net ads keytab create\n"
-			   "    Create new default keytab\n"));
+			   "    ", _("Create new default keytab\n"));
 		return 0;
 	}
 
@@ -2260,8 +2261,8 @@ static int net_ads_keytab_list(struct net_context *c, int argc, const char **arg
 	const char *keytab = NULL;
 
 	if (c->display_usage) {
-		d_printf(_("Usage:\n"
-			   "net ads keytab list [keytab]\n"
+		d_printf(_("Usage:\n"),
+			   _("net ads keytab list [keytab]\n"
 			   "  List a local keytab\n"
 			   "    keytab\tKeytab to list\n"));
 		return 0;
@@ -2326,9 +2327,9 @@ static int net_ads_kerberos_renew(struct net_context *c, int argc, const char **
 	int ret = -1;
 
 	if (c->display_usage) {
-		d_printf(_("Usage:\n"
+		d_printf(_("Usage:\n"),
 			   "net ads kerberos renew\n"
-			   "    Renew TGT from existing credential cache\n"));
+			   "    ", _("Renew TGT from existing credential cache\n"));
 		return 0;
 	}
 
@@ -2350,9 +2351,9 @@ static int net_ads_kerberos_pac(struct net_context *c, int argc, const char **ar
 	const char *impersonate_princ_s = NULL;
 
 	if (c->display_usage) {
-		d_printf(_("Usage:\n"
+		d_printf(_("Usage:\n"),
 			   "net ads kerberos pac\n"
-			   "    Dump the Kerberos PAC\n"));
+			   "    ", _("Dump the Kerberos PAC\n"));
 		return 0;
 	}
 
@@ -2405,9 +2406,9 @@ static int net_ads_kerberos_kinit(struct net_context *c, int argc, const char **
 	NTSTATUS status;
 
 	if (c->display_usage) {
-		d_printf(_("Usage:\n"
+		d_printf(_("Usage:\n"),
 			   "net ads kerberos kinit\n"
-			   "    Get Ticket Granting Ticket (TGT) for the user\n"));
+			   "    ", _("Get Ticket Granting Ticket (TGT) for the user\n"));
 		return 0;
 	}
 

source3/utils/net.c
@@ -277,7 +277,7 @@ static int net_setlocalsid(struct net_context *c, int argc, const char **argv)
 	     || (strncmp(argv[0], "S-1-5-21-", strlen("S-1-5-21-")) != 0)
 	     || (!string_to_sid(&sid, argv[0]))
 	     || (sid.num_auths != 4)) {
-		d_printf(_("usage: net setlocalsid S-1-5-21-x-y-z\n"));
+		d_printf(_("usage:")," net setlocalsid S-1-5-21-x-y-z\n");
 		return 1;
 	}
 
@@ -297,7 +297,7 @@ static int net_setdomainsid(struct net_context *c, int argc, const char **argv)
 	     || (strncmp(argv[0], "S-1-5-21-", strlen("S-1-5-21-")) != 0)
 	     || (!string_to_sid(&sid, argv[0]))
 	     || (sid.num_auths != 4)) {
-		d_printf(_("usage: net setdomainsid S-1-5-21-x-y-z\n"));
+		d_printf(_("usage:")," net setdomainsid S-1-5-21-x-y-z\n");
 		return 1;
 	}
 
@@ -315,7 +315,7 @@ static int net_getdomainsid(struct net_context *c, int argc, const char **argv)
 	fstring sid_str;
 
 	if (argc > 0) {
-		d_printf(_("usage: net getdomainsid\n"));
+		d_printf(_("usage:")," net getdomainsid\n");
 		return 1;
 	}
 
@@ -399,7 +399,7 @@ static int net_maxrid(struct net_context *c, int argc, const char **argv)
 	uint32 rid;
 
 	if (argc != 0) {
-	        d_fprintf(stderr, _("usage: net maxrid\n"));
+	        d_fprintf(stderr, _("usage:")," net maxrid\n");
 		return 1;
 	}
 

source3/utils/net_ads_gpo.c
@@ -37,9 +37,9 @@ static int net_ads_gpo_refresh(struct net_context *c, int argc, const char **arg
 	struct nt_user_token *token = NULL;
 
 	if (argc < 1 || c->display_usage) {
-		d_printf(_("Usage:\n"
-			   "net ads gpo refresh <username|machinename>\n"
-			   "  Lists all GPOs assigned to an account and "
+		d_printf(_("Usage:\n"),
+			   _("net ads gpo refresh <username|machinename>\n"),
+			   _("  Lists all GPOs assigned to an account and "
 			   "downloads them\n"
 			   "    username\tUser to refresh GPOs for\n"
 			   "    machinename\tMachine to refresh GPOs for\n"));
@@ -223,9 +223,9 @@ static int net_ads_gpo_list_all(struct net_context *c, int argc, const char **ar
 	};
 
 	if (c->display_usage) {
-		d_printf(_("Usage:\n"
+		d_printf(_("Usage:\n"),
 			   "net ads gpo listall\n"
-			   "    List all GPOs on the DC\n"));
+			   "    ", _("List all GPOs on the DC\n"));
 		return 0;
 	}
 
@@ -297,9 +297,9 @@ static int net_ads_gpo_list(struct net_context *c, int argc, const char **argv)
 	struct nt_user_token *token = NULL;
 
 	if (argc < 1 || c->display_usage) {
-		d_printf(_("Usage:\n"
-			   "net ads gpo list <username|machinename>\n"
-			   "  Lists all GPOs for machine/user\n"
+		d_printf(_("Usage:\n"),
+			   _("net ads gpo list <username|machinename>\n"),
+			   _("  Lists all GPOs for machine/user\n"
 			   "    username\tUser to list GPOs for\n"
 			   "    machinename\tMachine to list GPOs for\n"));
 		return -1;
@@ -448,9 +448,9 @@ static int net_ads_gpo_link_get(struct net_context *c, int argc, const char **ar
 	struct GP_LINK gp_link;
 
 	if (argc < 1 || c->display_usage) {
-		d_printf(_("Usage:\n"
-			   "net ads gpo linkget <container>\n"
-			   "  Lists gPLink of a containter\n"
+		d_printf(_("Usage:\n"),
+			   _("net ads gpo linkget <container>\n"),
+			   _("  Lists gPLink of a containter\n"
 			   "    container\tContainer to get link for\n"));
 		return -1;
 	}
@@ -489,9 +489,9 @@ static int net_ads_gpo_link_add(struct net_context *c, int argc, const char **ar
 	TALLOC_CTX *mem_ctx;
 
 	if (argc < 2 || c->display_usage) {
-		d_printf(_("Usage:\n"
-			   "net ads gpo linkadd <linkdn> <gpodn> [options]\n"
-			   "  Link a container to a GPO\n"
+		d_printf(_("Usage:\n"),
+			   _("net ads gpo linkadd <linkdn> <gpodn> [options]\n"),
+			   _("  Link a container to a GPO\n"
 			   "    linkdn\tContainer to link to a GPO\n"
 			   "    gpodn\tGPO to link container to\n"));
 		d_printf(_("note: DNs must be provided properly escaped.\n"
@@ -576,9 +576,9 @@ static int net_ads_gpo_get_gpo(struct net_context *c, int argc, const char **arg
 	struct GROUP_POLICY_OBJECT gpo;
 
 	if (argc < 1 || c->display_usage) {
-		d_printf(_("Usage:\n"
-			   "net ads gpo getgpo <gpo>\n"
-			   "  List speciefied GPO\n"
+		d_printf(_("Usage:\n"),
+			   _("net ads gpo getgpo <gpo>\n"),
+			   _("  List speciefied GPO\n"
 			   "    gpo\t\tGPO to list\n"));
 		return -1;
 	}

source3/utils/net_afs.c
@@ -35,7 +35,7 @@ int net_afs_key(struct net_context *c, int argc, const char **argv)
 	struct afs_keyfile keyfile;
 
 	if (argc != 2) {
-		d_printf(_("usage: 'net afs key <keyfile> cell'\n"));
+		d_printf(_("usage:")," net afs key <keyfile> cell\n");
 		return -1;
 	}
 
@@ -68,7 +68,7 @@ int net_afs_impersonate(struct net_context *c, int argc,
 	char *token;
 
 	if (argc != 2) {
-		fprintf(stderr, _("Usage: net afs impersonate <user> <cell>\n"));
+		fprintf(stderr, _("Usage:")," net afs impersonate <user> <cell>\n");
 	        exit(1);
 	}
 

source3/utils/net_cache.c
@@ -155,7 +155,8 @@ static int net_cache_add(struct net_context *c, int argc, const char **argv)
 	time_t timeout;
 
 	if (argc < 3 || c->display_usage) {
-		d_printf(_("\nUsage:\nnet cache add <key string> <data string> "
+		d_printf("\n",_("Usage:\n"),
+			   _("net cache add <key string> <data string> "
 			   "<timeout>\n"));
 		return -1;
 	}
@@ -192,7 +193,7 @@ static int net_cache_del(struct net_context *c, int argc, const char **argv)
 	const char *keystr = argv[0];
 
 	if (argc < 1 || c->display_usage) {
-		d_printf(_("\nUsage: net cache del <key string>\n"));
+		d_printf("\n",_("Usage:"), _(" net cache del <key string>\n"));
 		return -1;
 	}
 
@@ -220,7 +221,7 @@ static int net_cache_get(struct net_context *c, int argc, const char **argv)
 	time_t timeout;
 
 	if (argc < 1 || c->display_usage) {
-		d_printf(_("\nUsage: net cache get <key>\n"));
+		d_printf("\n", _("Usage:"), _(" net cache get <key>\n"));
 		return -1;
 	}
 
@@ -247,7 +248,7 @@ static int net_cache_search(struct net_context *c, int argc, const char **argv)
 	const char* pattern;
 
 	if (argc < 1 || c->display_usage) {
-		d_printf(_("Usage: net cache search <pattern>\n"));
+		d_printf(_("Usage:"), _(" net cache search <pattern>\n"));
 		return -1;
 	}
 
@@ -269,9 +270,9 @@ static int net_cache_list(struct net_context *c, int argc, const char **argv)
 	const char* pattern = "*";
 
 	if (c->display_usage) {
-		d_printf(_("Usage:\n"
+		d_printf(_("Usage:\n"),
 			   "net cache list\n"
-			   "    List all cache entries.\n"));
+			   "    ", _("List all cache entries.\n"));
 		return 0;
 	}
 	gencache_iterate(print_cache_entry, NULL, pattern);
@@ -290,9 +291,9 @@ static int net_cache_flush(struct net_context *c, int argc, const char **argv)
 {
 	const char* pattern = "*";
 	if (c->display_usage) {
-		d_printf(_("Usage:\n"
+		d_printf(_("Usage:\n"),
 			   "net cache flush\n"
-			   "    Delete all cache entries.\n"));
+			   "    ", _("Delete all cache entries.\n"));
 		return 0;
 	}
 	gencache_iterate(delete_cache_entry, NULL, pattern);
@@ -303,9 +304,9 @@ static int net_cache_stabilize(struct net_context *c, int argc,
 			       const char **argv)
 {
 	if (c->display_usage) {
-		d_printf(_("Usage:\n"
+		d_printf(_("Usage:\n"),
 			   "net cache flush\n"
-			   "    Delete all cache entries.\n"));
+			   "    ", _("Delete all cache entries.\n"));
 		return 0;
 	}
 

source3/utils/net_conf.c
@@ -39,14 +39,14 @@
 static int net_conf_list_usage(struct net_context *c, int argc,
 			       const char **argv)
 {
-	d_printf(_("USAGE: net conf list\n"));
+	d_printf(_("Usage:"), " net conf list\n");
 	return -1;
 }
 
 static int net_conf_import_usage(struct net_context *c, int argc,
 				 const char**argv)
 {
-	d_printf(_("USAGE: net conf import [--test|-T] <filename> "
+	d_printf(_("Usage:"), _(" net conf import [--test|-T] <filename> "
 		   "[<servicename>]\n"
 		   "\t[--test|-T]    testmode - do not act, just print "
 			"what would be done\n"
@@ -58,28 +58,28 @@ static int net_conf_import_usage(struct net_context *c, int argc,
 static int net_conf_listshares_usage(struct net_context *c, int argc,
 				     const char **argv)
 {
-	d_printf(_("USAGE: net conf listshares\n"));
+	d_printf(_("Usage:"), " net conf listshares\n");
 	return -1;
 }
 
 static int net_conf_drop_usage(struct net_context *c, int argc,
 			       const char **argv)
 {
-	d_printf(_("USAGE: net conf drop\n"));
+	d_printf(_("Usage:"), " net conf drop\n");
 	return -1;
 }
 
 static int net_conf_showshare_usage(struct net_context *c, int argc,
 				    const char **argv)
 {
-	d_printf(_("USAGE: net conf showshare <sharename>\n"));
+	d_printf(_("Usage:"), _(" net conf showshare <sharename>\n"));
 	return -1;
 }
 
 static int net_conf_addshare_usage(struct net_context *c, int argc,
 				   const char **argv)
 {
-	d_printf(_("USAGE: net conf addshare <sharename> <path> "
+	d_printf(_("Usage:"), _(" net conf addshare <sharename> <path> "
 		   "[writeable={y|N} [guest_ok={y|N} [<comment>]]\n"
 		   "\t<sharename>      the new share name.\n"
 		   "\t<path>           the path on the filesystem to export.\n"
@@ -94,49 +94,49 @@ static int net_conf_addshare_usage(struct net_context *c, int argc,
 static int net_conf_delshare_usage(struct net_context *c, int argc,
 				   const char **argv)
 {
-	d_printf(_("USAGE: net conf delshare <sharename>\n"));
+	d_printf(_("Usage: "), _(" net conf delshare <sharename>\n"));
 	return -1;
 }
 
 static int net_conf_setparm_usage(struct net_context *c, int argc,
 				  const char **argv)
 {
-	d_printf(_("USAGE: net conf setparm <section> <param> <value>\n"));
+	d_printf(_("Usage:"), _(" net conf setparm <section> <param> <value>\n"));
 	return -1;
 }
 
 static int net_conf_getparm_usage(struct net_context *c, int argc,
 				  const char **argv)
 {
-	d_printf(_("USAGE: net conf getparm <section> <param>\n"));
+	d_printf(_("Usage:"), _(" net conf getparm <section> <param>\n"));
 	return -1;
 }
 
 static int net_conf_delparm_usage(struct net_context *c, int argc,
 				  const char **argv)
 {
-	d_printf(_("USAGE: net conf delparm <section> <param>\n"));
+	d_printf(_("Usage:"), _(" net conf delparm <section> <param>\n"));
 	return -1;
 }
 
 static int net_conf_getincludes_usage(struct net_context *c, int argc,
 				      const char **argv)
 {
-	d_printf(_("USAGE: net conf getincludes <section>\n"));
+	d_printf(_("Usage:"), _(" net conf getincludes <section>\n"));
 	return -1;
 }
 
 static int net_conf_setincludes_usage(struct net_context *c, int argc,
 				      const char **argv)
 {
-	d_printf(_("USAGE: net conf setincludes <section> [<filename>]*\n"));
+	d_printf(_("Usage:"), _(" net conf setincludes <section> [<filename>]*\n"));
 	return -1;
 }
 
 static int net_conf_delincludes_usage(struct net_context *c, int argc,
 				      const char **argv)
 {
-	d_printf(_("USAGE: net conf delincludes <section>\n"));
+	d_printf(_("Usage:"), _(" net conf delincludes <section>\n"));
 	return -1;
 }
 

source3/utils/net_dom.c
@@ -23,13 +23,13 @@
 
 int net_dom_usage(struct net_context *c, int argc, const char **argv)
 {
-	d_printf(_("usage: net dom join "
+	d_printf(_("Usage:"), _(" net dom join "
 		   "<domain=DOMAIN> <ou=OU> <account=ACCOUNT> "
 		   "<password=PASSWORD> <reboot>\n  Join a remote machine\n"));
-	d_printf(_("usage: net dom unjoin "
+	d_printf(_("Usage:"), _(" net dom unjoin "
 		   "<account=ACCOUNT> <password=PASSWORD> <reboot>\n"
 		   "  Unjoin a remote machine\n"));
-	d_printf(_("usage: net dom renamecomputer "
+	d_printf(_("Usage:"), _(" net dom renamecomputer "
 		   "<newname=NEWNAME> "
 		   "<account=ACCOUNT> <password=PASSWORD> <reboot>\n"
 		   "  Rename joined computer\n"));

source3/utils/net_eventlog.c
@@ -43,7 +43,7 @@ static int net_eventlog_dump(struct net_context *c, int argc,
 	char *s;
 
 	if (argc < 1 || c->display_usage) {
-		d_fprintf(stderr, _("usage: net eventlog dump <file.evt>\n"));
+		d_fprintf(stderr, _("usage:")," net eventlog dump <file.evt>\n");
 		goto done;
 	}
 
@@ -99,7 +99,7 @@ static int net_eventlog_import(struct net_context *c, int argc,
 
 	if (argc < 2 || c->display_usage) {
 		d_fprintf(stderr,
-			  _("usage: net eventlog import <file> <eventlog>\n"));
+			  _("usage:")," net eventlog import <file> <eventlog>\n");
 		goto done;
 	}
 
@@ -194,7 +194,7 @@ static int net_eventlog_export(struct net_context *c, int argc,
 
 	if (argc < 2 || c->display_usage) {
 		d_fprintf(stderr,
-			  _("usage: net eventlog export <file> <eventlog>\n"));
+			  _("usage:")," net eventlog export <file> <eventlog>\n");
 		goto done;
 	}
 

commit 28b512f904cee5a79aefd0aca2d839dbe37e5519

s3 net: Fix compile error with WITH_DNS_UPDATES
bd3c922e2bc1a163efc1d8c9cb59578bebb79616 introduced a compile-time error
when building with WITH_DNS_UPDATES.

source3/utils/net_ads.c
@@ -1470,8 +1470,8 @@ static int net_ads_dns_gethostbyname(struct net_context *c, int argc, const char
 
 	if (argc != 2 || c->display_usage) {
 		d_printf(_("Usage:\n"),
-			   _("net ads dns gethostbyname <server> <name>\n"),
-			   "  Look up hostname from the AD\n"
+			 _("net ads dns gethostbyname <server> <name>\n"),
+			 _("  Look up hostname from the AD\n"
 			   "    server\tName server to use\n"
 			   "    name\tName to look up\n"));
 		return -1;