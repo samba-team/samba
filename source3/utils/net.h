bff48738a88875c5279e70f2886e894584f0cd7f
Volker Lendecke <vl@samba.org>
2010-01-24 18:51:58 +0100

    s3: Fix a bug in net's use of popt
    In order to add --use-ccache to net, I added another "bool opt_ccache;" to
    struct net_context. popt did not like this, it took a while to figure out why.
    Popt has the lines
    
        /* XXX Check alignment, may fail on funky platforms. */
        if (arg == NULL || (((unsigned long)arg) & (sizeof(*arg)-1)))
            return POPT_ERROR_NULLARG;
    
    The "bool opt_ccache;" was not aligned anymore...

--- source3/utils/net.h
+++ source3/utils/net.h
@@ -70,7 +70,7 @@ struct net_context {
 	const char *opt_exclude;
 	const char *opt_destination;
 	int opt_testmode;
-	bool opt_kerberos;
+	int opt_kerberos;
 	int opt_force_full_repl;
 	int opt_single_obj_repl;
 	int opt_clean_old_entries;
