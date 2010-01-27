c81c109a6ce83741bb5149a51ceb4ab30855e9f9
Jeremy Allison <jra@samba.org>
2010-01-26 16:51:57 -0800

    Fix bug #7067 - Linux asynchronous IO (aio) can cause smbd to fail to respond to a read or write.
    Only works on Linux kernels 2.6.26 and above. Grants CAP_KILL capability
    to allow Linux threads under different euids to send signals to each other.
    
    Jeremy.
    (cherry picked from commit 899bd0005f56dcc1e95c3988d41ab3f628bb15db)
    (cherry picked from commit 246eba3b807e5ce50ee838c51823a9eb44f6b690)

--- source3/include/smb.h
+++ source3/include/smb.h
@@ -1690,7 +1690,8 @@ minimum length == 24.
 enum smbd_capability {
     KERNEL_OPLOCK_CAPABILITY,
     DMAPI_ACCESS_CAPABILITY,
-    LEASE_CAPABILITY
+    LEASE_CAPABILITY,
+    KILL_CAPABILITY
 };
 
 /*
