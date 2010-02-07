6b01ca95a8e2858f43726eacd59f1fab5da63865
Andrew Tridgell <tridge@samba.org>
2010-02-07 21:08:56 +1100

    nbt: don't reference the event_ctx in nbtsock
    This causes talloc_free with references errors

--- libcli/nbt/nbtsocket.c
+++ libcli/nbt/nbtsocket.c
@@ -341,7 +341,7 @@ _PUBLIC_ struct nbt_name_socket *nbt_name_socket_init(TALLOC_CTX *mem_ctx,
 	nbtsock = talloc(mem_ctx, struct nbt_name_socket);
 	if (nbtsock == NULL) goto failed;
 
-	nbtsock->event_ctx = talloc_reference(nbtsock, event_ctx);
+	nbtsock->event_ctx = event_ctx;
 	if (nbtsock->event_ctx == NULL) goto failed;
 
 	status = socket_create("ip", SOCKET_TYPE_DGRAM, &nbtsock->sock, 0);
