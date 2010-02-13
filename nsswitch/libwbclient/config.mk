b99d9f86e42fd5ffe660166b34d097eff25f18f8
Kai Blin <kai@samba.org>
2010-02-13 13:42:56 +0100

    libwbclient: Separate out the async functions

--- nsswitch/libwbclient/config.mk
+++ nsswitch/libwbclient/config.mk
@@ -1,15 +1,24 @@
 [SUBSYSTEM::LIBWBCLIENT]
-PUBLIC_DEPENDENCIES = LIBASYNC_REQ \
-		      LIBTEVENT \
-		      LIBTALLOC \
-		      UTIL_TEVENT
+PUBLIC_DEPENDENCIES = LIBTALLOC
 
-LIBWBCLIENT_OBJ_FILES = $(addprefix $(libwbclientsrcdir)/, wbc_async.o \
-								wbc_guid.o \
+LIBWBCLIENT_OBJ_FILES = $(addprefix $(libwbclientsrcdir)/, wbc_guid.o \
 								wbc_idmap.o \
 								wbclient.o \
 								wbc_pam.o \
 								wbc_pwd.o \
 								wbc_sid.o \
-								wbc_util.o \
+								wbc_util.o )
+
+[SUBSYSTEM::LIBWBCLIENT_ASYNC]
+PUBLIC_DEPENDENCIES = LIBASYNC_REQ \
+		      LIBTEVENT \
+		      LIBTALLOC \
+		      UTIL_TEVENT \
+		      LIBWBCLIENT
+
+LIBWBCLIENT_ASYNC_OBJ_FILES = $(addprefix $(libwbclientsrcdir)/, wbc_async.o \
+								wbc_idmap_async.o \
+								wbc_pam_async.o \
+								wbc_sid_async.o \
+								wbc_util_async.o \
 								wb_reqtrans.o )
