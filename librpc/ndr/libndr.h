31c7780c16651b284009874cf3da04587dc36d19
Stefan Metzmacher <metze@samba.org>
2010-02-17 21:24:40 +0100

    libndr: implement LIBNDR_RELATIVE_REVERSE handling
    This is based on Guenther's initial code.
    
    metze

--- librpc/ndr/libndr.h
+++ librpc/ndr/libndr.h
@@ -91,6 +91,7 @@ struct ndr_push {
 
 	struct ndr_token_list *switch_list;
 	struct ndr_token_list *relative_list;
+	struct ndr_token_list *relative_begin_list;
 	struct ndr_token_list *nbt_string_list;
 	struct ndr_token_list *full_ptr_list;
 
