c2edea5ccfb1eadabc3fd4a6d1eb9c5c0a8251e2
Andrew Tridgell <tridge@samba.org>
2010-02-26 18:19:23 +1100

    devel: get the ownership of the directories right in tmpfs.sh

--- source4/scripting/devel/tmpfs.sh
+++ source4/scripting/devel/tmpfs.sh
@@ -9,8 +9,8 @@ sudo umount bin > /dev/null 2>&1
 sudo umount st  > /dev/null 2>&1 
 mkdir -p bin st || exit 1
 sudo mount -t tmpfs /dev/null bin || exit 1
-sudo chown $USER bin || exit 1
+sudo chown $USER bin/. || exit 1
 echo "tmpfs setup for bin/"
 sudo mount -t tmpfs /dev/null st || exit 1
-sudo chown $USER st || exit 1
+sudo chown $USER st/. || exit 1
 echo "tmpfs setup for st/"
