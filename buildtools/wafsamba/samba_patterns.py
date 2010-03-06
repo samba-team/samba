5b9ca48d81a92a1b6c079654ce71038ab5f844b4
Andrew Tridgell <tridge@samba.org>
2010-03-07 10:59:01 +1100

    build: added generation of version.h

--- buildtools/wafsamba/samba_patterns.py
+++ buildtools/wafsamba/samba_patterns.py
@@ -21,3 +21,16 @@ def process_et(self, node):
     self.create_task('et', node, [c_node, h_node])
     self.allnodes.append(c_node)
 
+
+
+def SAMBA_MKVERSION(bld, target):
+    '''generate the version.h header for Samba'''
+    bld.SET_BUILD_GROUP('setup')
+    t = bld(rule="${SRC} ${TGT}",
+            source= [ "script/mkversion.sh", 'VERSION' ],
+            target=target,
+            before="cc")
+    # force this rule to be constructed now
+    t.post()
+Build.BuildContext.SAMBA_MKVERSION = SAMBA_MKVERSION
+
