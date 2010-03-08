1c9494c76cc9686c61e0966f38528d3318f3176f
Karolin Seeger <kseeger@samba.org>
2010-03-08 20:34:39 +0100

    Revert "Fix bug #7067 - Linux asynchronous IO (aio) can cause smbd to fail to respond to a read or write."
    This reverts commit a6ae7a552f851a399991262377cc0e062e40ac20.
    
    This fixes bug #7222 (All users have full rigths on all shares) (CVE-2010-0728).

--- source3/include/smb.h
+++ source3/include/smb.h
@@ -1723,8 +1723,7 @@ minimum length == 24.
 enum smbd_capability {
     KERNEL_OPLOCK_CAPABILITY,
     DMAPI_ACCESS_CAPABILITY,
-    LEASE_CAPABILITY,
-    KILL_CAPABILITY
+    LEASE_CAPABILITY
 };
 
 /*
