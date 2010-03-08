f4cb528ac510d3381a92a303e858edaf9e3d908a
Jeff Layton <jlayton@redhat.com>
2010-03-08 09:06:40 -0500

    samba: remove cifs-utils tools from build systems
    Now that cifs-utils are their own project, we need to go ahead and pull
    them out of the samba tree. This patch represents the first step toward
    that end.
    
    Remove the cifs-utilities from the source3 and source4 builds. Please
    pay particular attention to the source4 piece. I'm not at all familiar
    with the build system there, and would appreciate someone sanity
    checking my changes.
    
    It also adds a small README.cifs-utils file in the topdir. This is
    optional, but I think it's a good idea to carry this for a release or
    two.
    
    Once this patch looks ok, I'll plan to do another patch to actually
    remove the client dir and the relevant docs-xml files from the tree
    altogether.
    
    Signed-off-by: Jeff Layton <jlayton@redhat.com>

--- source4/client/config.mk
+++ source4/client/config.mk
@@ -20,22 +20,6 @@ PRIVATE_DEPENDENCIES = \
 
 smbclient_OBJ_FILES = $(clientsrcdir)/client.o
 
-[BINARY::mount.cifs]
-INSTALLDIR = BINDIR
-
-mount.cifs_OBJ_FILES = ../client/mount.cifs.o \
-					   ../client/mtab.o
-
-[BINARY::umount.cifs]
-INSTALLDIR = BINDIR
-
-umount.cifs_OBJ_FILES = ../client/umount.cifs.o \
-					   ../client/mtab.o
-
-#[BINARY::cifs.upcall]
-#INSTALLDIR = BINDIR
-#cifs.upcall_OBJ_FILES = ../client/cifs.upcall.o
-
 #################################
 # Start BINARY cifsdd
 [BINARY::cifsdd]
