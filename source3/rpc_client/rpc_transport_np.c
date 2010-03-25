b862351da8624df893ec77e020a456c1d23c58ed
Stefan Metzmacher <metze@samba.org>
2010-03-25 14:08:42 +0100

    s3:rpc_transport_np: use cli_state_is_connected() helper
    metze

--- source3/rpc_client/rpc_transport_np.c
+++ source3/rpc_client/rpc_transport_np.c
@@ -30,7 +30,7 @@ struct rpc_transport_np_state {
 
 static int rpc_transport_np_state_destructor(struct rpc_transport_np_state *s)
 {
-	if (s->cli->fd == -1) {
+	if (!cli_state_is_connected(s->cli)) {
 		DEBUG(10, ("socket was closed, no need to send close request.\n"));
 		return 0;
 	}
