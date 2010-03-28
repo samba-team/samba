28a05bc1b0ff7e090753c9d8e248f00136fb491a
Andrew Tridgell <tridge@samba.org>
2010-03-28 15:39:16 +1100

    build: better waf test script

--- buildtools/testwaf.sh
+++ buildtools/testwaf.sh
@@ -5,7 +5,15 @@ d=$(dirname $0)
 cd $d/..
 PREFIX=$HOME/testprefix
 
-for d in lib/replace lib/talloc lib/tevent lib/tdb source4/lib/ldb; do
+if [ $# -gt 0 ]; then
+    tests="$*"
+else
+    tests="lib/replace lib/talloc lib/tevent lib/tdb source4/lib/ldb"
+fi
+
+echo "testing in dirs $tests"
+
+for d in $tests; do
     echo "`date`: testing $d"
     pushd $d || exit 1
     rm -rf bin
@@ -14,6 +22,19 @@ for d in lib/replace lib/talloc lib/tevent lib/tdb source4/lib/ldb; do
     time waf build || exit 1
     time waf build || exit 1
     waf install || exit 1
+    case $d in
+	"source4/lib/ldb")
+	    ldd bin/ldbadd || exit 1
+	    ;;
+	"lib/replace")
+	    ldd bin/replace_testsuite || exit 1
+	    ;;
+	"lib/talloc")
+	    ldd bin/talloc_testsuite || exit 1
+	    ;;
+	"lib/tdb")
+	    ldd bin/tdbtool || exit 1
+	    ;;
+    esac
     popd
 done
-ldd source4/lib/ldb/bin/ldbadd
