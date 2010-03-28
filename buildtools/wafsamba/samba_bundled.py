3b380ea7ac9f33f208a9479d34bd514f8e3d5bf4
Andrew Tridgell <tridge@samba.org>
2010-03-28 15:41:49 +1100

    build: better control over bundled library extensions

--- buildtools/wafsamba/samba_bundled.py
+++ buildtools/wafsamba/samba_bundled.py
@@ -3,18 +3,15 @@
 from Configure import conf
 from samba_utils import *
 
-@conf
-def BUNDLED_LIBRARY_EXTENSION(conf, extension):
-    '''set extension to add to bundled libraries'''
-    if not 'BUNDLED_EXTENSION' in conf.env:
-        conf.env.BUNDLED_EXTENSION = extension
-
 def BUNDLED_NAME(bld, name, bundled_extension):
     '''possibly rename a library to include a bundled extension'''
-    if bld.env.DISABLE_SHARED:
+    if bld.env.DISABLE_SHARED or not bundled_extension:
         return name
-    if bundled_extension and 'BUNDLED_EXTENSION' in bld.env:
-        return name + '-' + bld.env.BUNDLED_EXTENSION
+    if name in bld.env.BUNDLED_EXTENSION_EXCEPTION:
+        return name
+    extension = getattr(bld.env, 'BUNDLED_EXTENSION', '')
+    if extension:
+        return name + '-' + extension
     return name
 
 
@@ -26,3 +23,20 @@ def BUILTIN_LIBRARY(bld, name):
     if name in bld.env.BUILTIN_LIBRARIES:
         return True
     return False
+
+
+def BUILTIN_DEFAULT(opt, builtins):
+    '''set a comma separated default list of builtin libraries for this package'''
+    if 'BUILTIN_LIBRARIES_DEFAULT' in Options.options:
+        return
+    Options.options['BUILTIN_LIBRARIES_DEFAULT'] = builtins
+Options.Handler.BUILTIN_DEFAULT = BUILTIN_DEFAULT
+
+
+def BUNDLED_EXTENSION_DEFAULT(opt, extension, noextenion=''):
+    '''set a default bundled library extension'''
+    if 'BUNDLED_EXTENSION_DEFAULT' in Options.options:
+        return
+    Options.options['BUNDLED_EXTENSION_DEFAULT'] = extension
+    Options.options['BUNDLED_EXTENSION_EXCEPTION'] = noextenion
+Options.Handler.BUNDLED_EXTENSION_DEFAULT = BUNDLED_EXTENSION_DEFAULT
