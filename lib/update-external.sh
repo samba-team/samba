6897be747594e385ee032ac1409289ce2d208548
Jelmer Vernooij <jelmer@samba.org>
2010-03-31 04:19:36 +0200

    testtools: Fix included testtools, for systems that don't have it.

--- lib/update-external.sh
+++ lib/update-external.sh
@@ -14,6 +14,6 @@ done
 
 echo "Updating testtools..."
 bzr export "$WORKDIR/testtools" lp:testtools 
-rsync -avz --delete "$WORKDIR/testtools/testtools/" "$TARGETDIR/testtools/"
+rsync -avz --delete "$WORKDIR/testtools/" "$TARGETDIR/testtools/"
 
 rm -rf "$WORKDIR"
