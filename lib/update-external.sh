a8ac7fda573a924debf165d39eff3c1837240d4f
Jelmer Vernooij <jelmer@samba.org>
2010-03-31 03:19:18 +0200

    Put testtools directly under lib/ to make it easier to install from Samba 4.

---
+++ lib/update-external.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# Pull in a new snapshot of external projects that are included in 
+# our source tree for users that don't have them installed on their system
+
+TARGETDIR="`dirname $0`"
+WORKDIR="`mktemp -d`"
+
+echo "Updating subunit..."
+bzr export "$WORKDIR/subunit" lp:subunit 
+for p in python/ filters/ perl/
+do
+	rsync -avz --delete "$WORKDIR/subunit/$p" "$TARGETDIR/subunit/$p"
+done
+
+echo "Updating testtools..."
+bzr export "$WORKDIR/testtools" lp:testtools 
+rsync -avz --delete "$WORKDIR/testtools/testtools/" "$TARGETDIR/testtools/"
+
+rm -rf "$WORKDIR"
