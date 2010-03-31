42e88dbf2a53ba4cc4edd1576bfc1fe6308fa22c
Jeremy Allison <jra@samba.org>
2010-03-30 22:16:37 -0700

    Fix bug 7310 - DOS attribute inconsistency with MS Office
    On rename we need to set the archive bit on the renamed file.
    
    Jeremy
    (cherry picked from commit 779c2052b3b13c8f1a793e59e6152c3704eb75c6)

--- source3/smbd/reply.c
+++ source3/smbd/reply.c
@@ -5846,6 +5846,21 @@ NTSTATUS rename_internals_fsp(connection_struct *conn,
 			  "%s -> %s\n", smb_fname_str_dbg(fsp->fsp_name),
 			  smb_fname_str_dbg(smb_fname_dst)));
 
+		if (lp_map_archive(SNUM(conn)) ||
+		    lp_store_dos_attributes(SNUM(conn))) {
+			/* We must set the archive bit on the newly
+			   renamed file. */
+			if (SMB_VFS_STAT(conn, smb_fname_dst) == 0) {
+				uint32_t old_dosmode = dos_mode(conn,
+							smb_fname_dst);
+				file_set_dosmode(conn,
+					smb_fname_dst,
+					old_dosmode | FILE_ATTRIBUTE_ARCHIVE,
+					NULL,
+					true);
+			}
+		}
+
 		notify_rename(conn, fsp->is_directory, fsp->fsp_name,
 			      smb_fname_dst);
 
