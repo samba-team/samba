1e35da61455ac1e94b1f64f661e7052113de4dfc
Andrew Tridgell <tridge@samba.org>
2010-04-04 18:27:30 +1000

    s4-waf: new autogen-waf.sh
    this one will work with the tdb, talloc, etc directories

--- source4/autogen-waf.sh
+++ source4/autogen-waf.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 
 echo "Setting up for waf build"
-ln -sf configure.waf configure
 
+echo "Looking for the buildtools directory"
+
+d="buildtools"
+while test \! -d $d; do d="../$d"; done
+
+echo "Found buildtools in $d"
+
+echo "Setting up configure"
+rm -f configure
+sed "s|BUILDTOOLS|$d|g" < "$d/scripts/configure.waf" > configure
+chmod +x configure
+
+echo "Setting up makefile"
 # this relies on the fact that make looks for 'makefile' before 'Makefile'
-ln -sf Makefile.waf makefile
+rm -f makefile
+sed "s|BUILDTOOLS|$d|g" < "$d/scripts/Makefile.waf" > makefile
 
-echo "done ... now run ./configure or ./configure.developer"
+echo "done. Now run ./configure or ./configure.developer then make"
