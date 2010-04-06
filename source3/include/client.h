8ff54339230d4cf6862e5c3dac59adff7178a869
Stefan Metzmacher <metze@samba.org>
2010-04-06 14:26:29 +0200

    s3:rpc_client: add rpccli_is_connected()
    metze
    (cherry picked from commit 4f41b53487ac9bc96c7960e8edab464558656373)
    (similar to commit 958b49323968740e2cbf69dc2a0a5dd57d5bcf87)
    
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    (cherry picked from commit a8a1a6c77a2148a729f6b285f3f945ffd5501256)

--- source3/include/client.h
+++ source3/include/client.h
@@ -110,6 +110,9 @@ struct rpc_cli_transport {
 	 */
 	NTSTATUS (*trans_recv)(struct async_req *req, TALLOC_CTX *mem_ctx,
 			       uint8_t **prdata, uint32_t *prdata_len);
+
+	bool (*is_connected)(void *priv);
+
 	void *priv;
 };
 
