5beaef7cde3c311e4543abf71e5fe9794d62cc6e
Stefan Metzmacher <metze@samba.org>
2010-04-10 10:58:27 +0200

    s4:autogen-waf: generate 'Makefile' instead of 'makefile'
    This hopefully fixes the build on MacOS 10.
    
    metze

--- source4/autogen-waf.sh
+++ source4/autogen-waf.sh
@@ -12,14 +12,13 @@ while test \! -d "$p/$d"; do d="../$d"; done
 echo "Found buildtools in $p/$d"
 
 echo "Setting up configure"
-rm -f $p/configure
+rm -f $p/configure $p/include/config*.h*
 sed "s|BUILDTOOLS|$d|g;s|BUILDPATH|$p|g" < "$p/$d/scripts/configure.waf" > $p/configure
 chmod +x $p/configure
 
-echo "Setting up makefile"
-# this relies on the fact that make looks for 'makefile' before 'Makefile'
-rm -f $p/makefile
-sed "s|BUILDTOOLS|$d|g" < "$p/$d/scripts/Makefile.waf" > $p/makefile
+echo "Setting up Makefile"
+rm -f $p/makefile $p/Makefile
+sed "s|BUILDTOOLS|$d|g" < "$p/$d/scripts/Makefile.waf" > $p/Makefile
 
 echo "done. Now run $p/configure or $p/configure.developer then make."
 if [ $p != "." ]; then
