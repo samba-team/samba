3d1c0f1a2152b456be112ce992d281e0234f1a96
Kai Blin <kai@samba.org>
2010-04-11 09:35:08 +0200

    s3-waf: Add helper that facilitates defining --with-x and --without-x options

---
+++ buildtools/wafsamba/samba3.py
@@ -0,0 +1,20 @@
+# a waf tool to add autoconf-like macros to the configure section
+# and for SAMBA_ macros for building libraries, binaries etc
+
+import Options
+from optparse import SUPPRESS_HELP
+
+def SAMBA3_ADD_OPTION(opt, option, help=(), dest=None, default=True):
+    if help == ():
+        help = ("Build with %s support" % option)
+    if dest is None:
+        dest = "with_%s" % option
+
+    with_val = "--with-%s" % option
+    without_val = "--without-%s" % option
+
+    opt.add_option(with_val, help=help, action="store_true", dest=dest,
+                   default=default)
+    opt.add_option(without_val, help=SUPPRESS_HELP, action="store_false",
+                   dest=dest)
+Options.Handler.SAMBA3_ADD_OPTION = SAMBA3_ADD_OPTION
