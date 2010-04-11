da7294ae0d1c9cdeb1c945f3eb51d2376e7dabf6
Kai Blin <kai@samba.org>
2010-04-11 22:38:38 +0200

    nsswitch/wbinfo: Fix the S4 header check

--- nsswitch/wbinfo.c
+++ nsswitch/wbinfo.c
@@ -26,7 +26,7 @@
 #include "libwbclient/wbclient.h"
 #include "lib/popt/popt.h"
 #include "../libcli/auth/libcli_auth.h"
-#if !(_SAMBA_VERSION_) < 4
+#if (_SAMBA_BUILD_) >= 4
 #include "lib/cmdline/popt_common.h"
 #endif
 
