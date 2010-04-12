a9e4abaa6e72425017953f95e2f22cc4af6d7455
Kamen Mazdrashki <kamenim@samba.org>
2010-04-12 03:45:12 +0300

    s4/libnet: Allocate mem in supplied mem_ctx
    In this case, 'libnet_Vampire *r' may not be a pointer to
    a talloced memory.

--- source4/libnet/libnet_vampire.c
+++ source4/libnet/libnet_vampire.c
@@ -760,8 +760,8 @@ NTSTATUS libnet_Vampire(struct libnet_context *ctx, TALLOC_CTX *mem_ctx,
 		return status;
 	}
 
-	r->out.domain_name = talloc_steal(r, join->out.domain_name);
-	r->out.domain_sid = dom_sid_dup(r, join->out.domain_sid);
+	r->out.domain_name = talloc_steal(mem_ctx, join->out.domain_name);
+	r->out.domain_sid = dom_sid_dup(mem_ctx, join->out.domain_sid);
 	
 	/* commit the transaction now we know the secrets were written
 	 * out properly
