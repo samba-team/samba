a901405b93a08c299569d0247bf76829d3258fad
Jim McDonough <jmcd@samba.org>
2010-04-21 20:53:51 +0200

    s3-net_conf: Display an error on net conf import failures.
    When something goes wrong, such as a typo in a parameter
    name, we'll now display the failure instead of just returning
    with -1 and no message.
    
    Fix bug #7378 ("net conf import" fails silently on parameter typo).
    (cherry picked from commit d983085cee01fcf6e6176d395400075e6d5df937)

--- source3/utils/net_conf.c
+++ source3/utils/net_conf.c
@@ -209,6 +209,9 @@ static WERROR import_process_service(struct net_context *c,
 						     service->param_names[idx],
 						     service->param_values[idx]);
 			if (!W_ERROR_IS_OK(werr)) {
+				d_printf("Error in section [%s], parameter \"%s\": %s\n",
+					 service->name, service->param_names[idx],
+					 win_errstr(werr));
 				goto done;
 			}
 		}
