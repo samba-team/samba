899fa60dd2cf24fc32c83f17080fa5f221861541
Andrew Tridgell <tridge@samba.org>
2010-04-21 11:32:58 +1000

    s4-devel: allow extra net command line options and gdb
    This allows you to run:
    
      GDB="gdb --args" vampire_ad.sh
    
    and also to add higher debug levels like this:
    
      vampire_ad.sh -d100
    
    Pair-Programmed-With: Andrew Bartlett <abartlet@samba.org>

--- source4/scripting/devel/drs/vampire_ad.sh
+++ source4/scripting/devel/drs/vampire_ad.sh
@@ -15,6 +15,6 @@ sudo rndc reconfig
 
 REALM="$(echo $DNSDOMAIN | tr '[a-z]' '[A-Z]')"
 
-sudo bin/net vampire $DNSDOMAIN -Uadministrator%$pass -s $PREFIX/etc/smb.conf --option=realm=$REALM --option="ads:dc function level=4" --option="ads:min function level=0" -d2 || exit 1
+sudo $GDB bin/net vampire $DNSDOMAIN -Uadministrator%$pass -s $PREFIX/etc/smb.conf --option=realm=$REALM --option="ads:dc function level=4" --option="ads:min function level=0" -d2 "$@" || exit 1
 PRIVATEDIR=$PREFIX/private sudo -E scripting/bin/setup_dns.sh $machine $DNSDOMAIN $machine_ip || exit 1
 sudo rndc flush
