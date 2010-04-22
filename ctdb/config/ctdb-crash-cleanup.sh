4f191982ca81c5ea5a7c4319f094c46eab1e5636
Ronnie Sahlberg <ronniesahlberg@gmail.com>
2010-04-22 14:02:11 +1000

    add an example script that can be called from crontab to cleanup and release public ip addresses if ctdbd is no longer running
    (This used to be ctdb commit 1cdaaa0a3f53d1b075340a33dfdc42b534e99187)

---
+++ ctdb/config/ctdb-crash-cleanup.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+#
+# This script can be called from a cronjob to automatically drop/release
+# all public ip addresses if CTDBD has crashed or stopped running.
+#
+
+[ -z "$CTDB_BASE" ] && {
+    CTDB_BASE=/etc/ctdb
+}
+
+[ -z "$CTDB_PUBLIC_ADDRESSES" ] && {
+	CTDB_PUBLIC_ADDRESSES=$CTDB_BASE/public_addresses
+}
+
+[ ! -f "$CTDB_PUBLIC_ADDRESSES" ] && {
+	echo "No public addresses file found. Cant cleanup."
+	exit 1
+}
+
+# if ctdb is running, just return
+ctdb status 2>/dev/null && {
+    exit 0
+}
+
+cat "$CTDB_PUBLIC_ADDRESSES" | cut -d/ -f1 | while read _IP; do
+	_IP_HELD=`/sbin/ip addr show | grep "inet $_IP/"`
+	[ -z "$_IP_HELD" ] || {
+		_IFACE=`echo $_IP_HELD | sed -e "s/.*\s//"`
+		_NM=`echo $_IP_HELD | sed -e "s/.*$_IP\///" -e "s/\s.*//"`
+		logger "Removing public address $_IP/$_NM from device $_IFACE"
+		/sbin/ip addr del $_IP/$_NM dev $_IFACE
+	}
+done
+
+
