eb28a761e910f0ffec118ffd3a16ef4971da1232
Günther Deschner <gd@samba.org>
2010-05-10 11:01:54 +0200

    testprogs: rename spoolss.exe to testspoolss.exe.
    Patch from Kurt Pfeifle <Kurt.Pfeifle@ricoh.de>.
    
    Guenther

--- testprogs/win32/spoolss/error.c
+++ testprogs/win32/spoolss/error.c
@@ -18,7 +18,7 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "spoolss.h"
+#include "testspoolss.h"
 
 const char *errstr(DWORD error)
 {
--- testprogs/win32/spoolss/torture.c
+++ testprogs/win32/spoolss/torture.c
@@ -18,7 +18,7 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "spoolss.h"
+#include "testspoolss.h"
 #include "torture.h"
 
 /****************************************************************************
