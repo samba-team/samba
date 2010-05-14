8608eaef76631666a41109317d2a9b20979c5025
Matthias Dieter Wallnöfer <mdw@samba.org>
2010-05-14 18:59:36 +0200

    s4:net domainlevel tool - fix up the error handling as Jelmer suggested
    Sorry, I've copied this from the "ldap.py" test and thought it would work.

--- source4/scripting/python/samba/netcmd/domainlevel.py
+++ source4/scripting/python/samba/netcmd/domainlevel.py
@@ -200,7 +200,8 @@ class cmd_domainlevel(Command):
                     try:
                         samdb.modify(m)
                     except LdbError, (num, _):
-                        self.assertEquals(num, ldb.ERR_UNWILLING_TO_PERFORM)
+                        if num != ldb.ERR_UNWILLING_TO_PERFORM:
+                            raise
 
                 # Directly on the base DN
                 m = ldb.Message()
@@ -219,7 +220,8 @@ class cmd_domainlevel(Command):
                 try:
                     samdb.modify(m)
                 except LdbError, (num, _):
-                    self.assertEquals(num, ldb.ERR_UNWILLING_TO_PERFORM)
+                    if num != ldb.ERR_UNWILLING_TO_PERFORM:
+                        raise
 
                 level_domain = new_level_domain
                 msgs.append("Domain function level changed!")
