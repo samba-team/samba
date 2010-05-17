20537d65166714f083256cf4f48b96d35786253a
Günther Deschner <gd@samba.org>
2010-05-17 18:22:37 +0200

    s3-pidfile: set the close on exec flag for the created pidfiles.
    Guenther

--- source3/lib/pidfile.c
+++ source3/lib/pidfile.c
@@ -145,6 +145,9 @@ void pidfile_create(const char *program_name)
 	}
 	/* Leave pid file open & locked for the duration... */
 	SAFE_FREE(name);
+
+	/* set the close on exec so that we don't leak the fd */
+	fcntl(fd, F_SETFD, FD_CLOEXEC);
 }
 
 void pidfile_unlink(void)
