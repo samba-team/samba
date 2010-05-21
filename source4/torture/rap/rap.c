6af5e7cde198ac21ed8030fd3744ab0ae7f15fd3
Matthias Dieter Wallnöfer <mdw@samba.org>
2010-05-21 23:23:58 +0200

    s4:torture/rap/rap.c - quiet discard const warning

--- source4/torture/rap/rap.c
+++ source4/torture/rap/rap.c
@@ -212,7 +212,7 @@ static NTSTATUS rap_pull_string(TALLOC_CTX *mem_ctx, struct ndr_pull *ndr,
 		return NT_STATUS_INVALID_PARAMETER;
 
 	*dest = talloc_zero_array(mem_ctx, char, len+1);
-	pull_string((char *)*dest, p, len+1, len, STR_ASCII);
+	pull_string(discard_const_p(char, *dest), p, len+1, len, STR_ASCII);
 
 	return NT_STATUS_OK;
 }
