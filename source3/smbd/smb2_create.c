d7542b58fcb0d9a67a20bde5bdf3aec6fd633237
Jeremy Allison <jra@samba.org>
2010-05-21 23:59:54 -0700

    If using fake oplocks, use the correct SMB2 type code for "no oplock".
    Jeremy.

--- source3/smbd/smb2_create.c
+++ source3/smbd/smb2_create.c
@@ -426,7 +426,7 @@ static struct tevent_req *smbd_smb2_create_send(TALLOC_CTX *mem_ctx,
 	ZERO_STRUCT(out_context_blobs);
 
 	if(lp_fake_oplocks(SNUM(smb2req->tcon->compat_conn))) {
-		requested_oplock_level = NO_OPLOCK;
+		requested_oplock_level = SMB2_OPLOCK_LEVEL_NONE;
 	} else {
 		requested_oplock_level = in_oplock_level;
 	}
