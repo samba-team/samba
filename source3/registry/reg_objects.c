af2cbaa700f8a56228131061ad10a1d219df1d83
Michael Adam <obnox@samba.org>
2010-05-25 10:53:17 +0200

    s3:registry: add C for substantial changes to reg_objects

--- source3/registry/reg_objects.c
+++ source3/registry/reg_objects.c
@@ -2,6 +2,7 @@
  *  Unix SMB/CIFS implementation.
  *  Virtual Windows Registry Layer
  *  Copyright (C) Gerald Carter                     2002-2005
+ *  Copyright (C) Michael Adam                      2007-2010
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
--- source3/registry/reg_objects.h
+++ source3/registry/reg_objects.h
@@ -3,6 +3,7 @@
    
    SMB parameters and setup
    Copyright (C) Gerald Carter                   2002-2006.
+   Copyright (C) Michael Adam                    2007-2010
    
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
