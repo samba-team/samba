2142d1728ac0b2957d1edaa4a95220012cc67089
Michael Adam <obnox@samba.org>
2010-06-01 15:35:01 +0200

    s3:loadparm: add new boolean parameter "idmap read only"
    This will be used to be able to put the default idmap config
    read only. This can make sense for instance with the tdb2
    idmap backend and using the idmap script feature.

--- source3/include/proto.h
+++ source3/include/proto.h
@@ -3656,6 +3656,7 @@ bool lp_winbind_normalize_names(void);
 bool lp_winbind_rpc_only(void);
 bool lp_create_krb5_conf(void);
 const char *lp_idmap_backend(void);
+bool lp_idmap_read_only(void);
 int lp_idmap_cache_time(void);
 int lp_idmap_negative_cache_time(void);
 int lp_keepalive(void);
