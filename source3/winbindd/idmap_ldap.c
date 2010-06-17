ccfb698c88cd3402992109e4443c1e835191b83b
Michael Adam <obnox@samba.org>
2010-06-17 20:19:46 +0200

    s3:idmap_ldap: add a idmap_ldap_new_mapping().
    High level function to create a new mapping for an unmapped sid.
    This builds logic that used to reside in the top level idmap code
    in the backend.

--- source3/winbindd/idmap_ldap.c
+++ source3/winbindd/idmap_ldap.c
@@ -750,6 +750,54 @@ done:
 	return ret;
 }
 
+/**
+ * Create a new mapping for an unmapped SID, also allocating a new ID.
+ * If possible, this should be run inside a transaction to make the
+ * action atomic.
+ */
+static NTSTATUS idmap_ldap_new_mapping(struct idmap_domain *dom, struct id_map *map)
+{
+	NTSTATUS ret;
+
+	if (map == NULL) {
+		ret = NT_STATUS_INVALID_PARAMETER;
+		goto done;
+	}
+
+	if ((map->xid.type != ID_TYPE_UID) && (map->xid.type != ID_TYPE_GID)) {
+		ret = NT_STATUS_INVALID_PARAMETER;
+		goto done;
+	}
+
+	if (map->sid == NULL) {
+		ret = NT_STATUS_INVALID_PARAMETER;
+		goto done;
+	}
+
+	ret = idmap_ldap_get_new_id(dom, &map->xid);
+	if (!NT_STATUS_IS_OK(ret)) {
+		DEBUG(3, ("Could not allocate id: %s\n", nt_errstr(ret)));
+		goto done;
+	}
+
+	DEBUG(10, ("Setting mapping: %s <-> %s %lu\n",
+		   sid_string_dbg(map->sid),
+		   (map->xid.type == ID_TYPE_UID) ? "UID" : "GID",
+		   (unsigned long)map->xid.id));
+
+	map->status = ID_MAPPED;
+
+	/* store the mapping */
+	ret = idmap_ldap_set_mapping(dom, map);
+	if (!NT_STATUS_IS_OK(ret)) {
+		DEBUG(3, ("Could not store the new mapping: %s\n",
+			  nt_errstr(ret)));
+	}
+
+done:
+	return ret;
+}
+
 
 /* max number of ids requested per batch query */
 #define IDMAP_LDAP_MAX_IDS 30
