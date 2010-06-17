aad8756daebf4eee03fb67e28324c723b81c3a02
Michael Adam <obnox@samba.org>
2010-06-17 09:39:24 +0200

    s3:idmap_tdb2: add my (C)

--- source3/winbindd/idmap_tdb2.c
+++ source3/winbindd/idmap_tdb2.c
@@ -14,6 +14,7 @@
    Copyright (C) Jim McDonough <jmcd@us.ibm.com> 2003
    Copyright (C) Jeremy Allison 2006
    Copyright (C) Simo Sorce 2003-2006
+   Copyright (C) Michael Adam 2009-2010
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
