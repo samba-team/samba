f41e71109744cbacfcbf6b8cf545e1f322d68494
Andrew Bartlett <abartlet@samba.org>
2010-06-24 10:03:29 +1000

    s4:auth Query LDB for msds-SupportedEncryptionTypes for the KDC
    The KDC needs this to determine what encryption types an entry supports
    
    Andrew Bartlett

--- source4/auth/sam.c
+++ source4/auth/sam.c
@@ -36,6 +36,7 @@
 	"userPrincipalName",			\
 	"servicePrincipalName",			\
 	"msDS-KeyVersionNumber",		\
+	"msDS-SupportedEncryptionTypes",	\
 	"supplementalCredentials",		\
 						\
 	/* passwords */				\
