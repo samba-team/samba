cb52b1ad99e17c09d4c429a780f30772efd6eaf9
Günther Deschner <gd@samba.org>
2010-07-05 15:24:27 +0200

    s3-idl: support --full for s3 build_idl.sh as well.
    Guenther

--- source3/script/build_idl.sh
+++ source3/script/build_idl.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+if [ "$1" = "--full" ]; then
+	FULL=1
+	shift 1
+else
+	FULL=0
+fi
+
 ARGS="--includedir=../librpc/idl --outputdir $PIDL_OUTPUTDIR --header --ndr-parser --samba3-ndr-server --samba3-ndr-client $PIDL_ARGS --"
 IDL_FILES="$*"
 
@@ -9,6 +16,12 @@ cd ${srcdir}
 [ -d $PIDL_OUTPUTDIR ] || mkdir -p $PIDL_OUTPUTDIR || exit 1
 
 PIDL="$PIDL $ARGS"
+
+if [ $FULL = 1 ]; then
+	echo "Rebuilding all idl files"
+	$PIDL $IDL_FILES || exit 1
+	exit 0
+fi
 
 ##
 ## Find newer files rather than rebuild all of them
