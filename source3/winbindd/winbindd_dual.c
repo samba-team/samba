samba-team/samba

commit c67cff0372d987d13105b81a7625ff42a3ceac43

s3-winbind: Create all logfiles in the same directory.
If log file is set in the config file, we should create the log files of
the winbind child processes in the same directory.

source3/winbindd/winbindd_dual.c
@@ -405,10 +405,31 @@ void setup_child(struct winbindd_domain *domain, struct winbindd_child *child,
 		 const char *logname)
 {
 	if (logprefix && logname) {
+		char *logbase = NULL;
+
+		if (lp_logfile()) {
+			char *end = NULL;
+
+			if (asprintf(&logbase, "%s", lp_logfile()) < 0) {
+				smb_panic("Internal error: asprintf failed");
+			}
+
+			if ((end = strrchr_m(logbase, '/'))) {
+				*end = '\0';
+			}
+		} else {
+			if (asprintf(&logbase, "%s", get_dyn_LOGFILEBASE()) < 0) {
+				smb_panic("Internal error: asprintf failed");
+			}
+		}
+
 		if (asprintf(&child->logfilename, "%s/%s-%s",
-			     get_dyn_LOGFILEBASE(), logprefix, logname) < 0) {
+			     logbase, logprefix, logname) < 0) {
+			SAFE_FREE(logbase);
 			smb_panic("Internal error: asprintf failed");
 		}
+
+		SAFE_FREE(logbase);
 	} else {
 		smb_panic("Internal error: logprefix == NULL && "
 			  "logname == NULL");

commit 76a084feeec208aff2bbfaa45c134592c1719600

s3-winbindd: Fix child logfile handling which broke with c67cff0372.
Andreas, please check.

Guenther

source3/winbindd/winbindd_dual.c
@@ -407,7 +407,7 @@ void setup_child(struct winbindd_domain *domain, struct winbindd_child *child,
 	if (logprefix && logname) {
 		char *logbase = NULL;
 
-		if (lp_logfile()) {
+		if (*lp_logfile()) {
 			char *end = NULL;
 
 			if (asprintf(&logbase, "%s", lp_logfile()) < 0) {