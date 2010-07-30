f73f2d758112857fe062416415687cd2e6a95a3b
Martin Schwenke <martin@meltin.net>
2010-07-30 16:45:36 +1000

    Testing: Add Python IP allocation simulation.
    Includes simulation module and example scenarios.  This allows you to
    test and perhaps tweak an algorithm that should be the same as the
    current CTDB IP reallocation one.
    
    Signed-off-by: Martin Schwenke <martin@meltin.net>
    
    (This used to be ctdb commit d148e7a7cb840febbdf56ba2e39c314cc2d7ac24)

---
+++ ctdb/tests/takeover/mgmt_simple.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+# This is an example showing a current SONAS configuration with 3
+# interface node and a management node.  When run with deterministic
+# IPs there are gratuitous IP reassignments.
+
+from ctdb_takeover import Cluster, Node
+
+addresses = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+
+c = Cluster()
+
+for i in range(3):
+    c.add_node(Node(addresses))
+
+c.add_node(Node([]))
+
+c.recover()
+
+c.random_iterations()
---
+++ ctdb/tests/takeover/nondet_path_01.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+# This is a contrived example that makes the balancing algorithm fail
+# for nondeterministic IPs (run with "-dv --nd" to see the failure).
+# The --hack option fixes the problem.
+
+from ctdb_takeover import Cluster, Node
+
+addresses1 = ['A', 'B', 'C', 'D']
+addresses2 = ['B', 'E', 'F']
+
+c = Cluster()
+
+for i in range(2):
+    c.add_node(Node(addresses1))
+
+c.add_node(Node(addresses2))
+
+c.recover()
+
+c.unhealthy(1)
+c.recover()
+c.healthy(1)
+c.recover()
