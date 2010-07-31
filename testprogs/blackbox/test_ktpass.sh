34692556bed72130acd2985cc734d74c8a57203e
Stefan Metzmacher <metze@samba.org>
2010-07-31 11:12:10 +0200

    s4:blackblock/ktpass: use test specific user name
    metze

--- testprogs/blackbox/test_ktpass.sh
+++ testprogs/blackbox/test_ktpass.sh
@@ -18,20 +18,19 @@ samba4bindir="$BUILDDIR/bin"
 samba4kinit="$samba4bindir/samba4kinit$EXEEXT"
 CONFIG="--configfile=$PREFIX/dc/etc/smb.conf"
 
-#two test for creating new user
-#newuser  account is created with cn=Given Name Initials. Surname
-#newuser1 account is created using cn=username
-testit "newuser" $net newuser $CONFIG --given-name="User" --surname="Tester" --initials="T" --profile-path="\\\\myserver\\my\\profile" --script-path="\\\\myserver\\my\\script" --home-directory="\\\\myserver\\my\\homedir" --job-title="Tester" --department="Testing" --company="Samba.org" --description="Description" --mail-address="tester@samba.org" --internet-address="http://samba.org" --telephone-number="001122334455" --physical-delivery-office="101" --home-drive="H:" testuser testp@ssw0Rd|| failed=`expr $failed + 1`
+TESTUSER="ktpassUser"
+
+testit "newuser" $net newuser $CONFIG $TESTUSER testp@ssw0Rd || failed=`expr $failed + 1`
 
 KRB5CCNAME="$PREFIX/tmpccache"
 export KRB5CCNAME
 echo "testp@ssw0Rd" >$PREFIX/tmppassfile
-testit "kinit with passwd" $samba4kinit -e arcfour-hmac-md5 --password-file=$PREFIX/tmppassfile   testuser@SAMBA.EXAMPLE.COM   || failed=`expr $failed + 1`
-testit "ktpass" $BUILDDIR/scripting/bin/ktpass.sh --host LOCALDC --out $PREFIX/testuser.kt --princ testuser --pass "testp@ssw0Rd" --path-to-ldbsearch=$BUILDDIR/bin|| failed=`expr $failed + 1`
+testit "kinit with passwd" $samba4kinit -e arcfour-hmac-md5 --password-file=$PREFIX/tmppassfile   $TESTUSER@SAMBA.EXAMPLE.COM   || failed=`expr $failed + 1`
+testit "ktpass" $BUILDDIR/scripting/bin/ktpass.sh --host LOCALDC --out $PREFIX/testuser.kt --princ $TESTUSER --pass "testp@ssw0Rd" --path-to-ldbsearch=$BUILDDIR/bin|| failed=`expr $failed + 1`
 
 rm -f $KRB5CCNAME
 
-testit "kinit with keytab" $samba4kinit -e arcfour-hmac-md5 --use-keytab -t $PREFIX/testuser.kt testuser@SAMBA.EXAMPLE.COM   || failed=`expr $failed + 1`
+testit "kinit with keytab" $samba4kinit -e arcfour-hmac-md5 --use-keytab -t $PREFIX/testuser.kt $TESTUSER@SAMBA.EXAMPLE.COM   || failed=`expr $failed + 1`
 
 rm -f $PREFIX/tmpccache $PREFIX/testuser.kt
 exit $failed
