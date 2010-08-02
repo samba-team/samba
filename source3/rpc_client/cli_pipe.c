558320cf582a5ebc50af28061f5e33f7e4158fba
Simo Sorce <idra@samba.org>
2010-08-02 09:47:01 -0400

    Add my (c)

--- source3/rpc_client/cli_pipe.c
+++ source3/rpc_client/cli_pipe.c
@@ -1,18 +1,19 @@
-/* 
+/*
  *  Unix SMB/CIFS implementation.
- *  RPC Pipe client / server routines
+ *  RPC Pipe client routines
  *  Largely rewritten by Jeremy Allison		    2005.
- *  
+ *  Heavily modified by Simo Sorce		    2010.
+ *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 3 of the License, or
  *  (at your option) any later version.
- *  
+ *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- *  
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
