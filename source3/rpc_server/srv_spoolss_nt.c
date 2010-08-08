9daa6a2501a1486aa78e7eed71d12a404df2e882
Volker Lendecke <vl@samba.org>
2010-08-08 14:11:39 +0200

    s3: Lift the smbd_messaging_context from enumprinters_level5

--- source3/rpc_server/srv_spoolss_nt.c
+++ source3/rpc_server/srv_spoolss_nt.c
@@ -4123,6 +4123,7 @@ static WERROR enumprinters_level4(TALLOC_CTX *mem_ctx,
 
 static WERROR enumprinters_level5(TALLOC_CTX *mem_ctx,
 				  struct auth_serversupplied_info *server_info,
+				  struct messaging_context *msg_ctx,
 				  uint32_t flags,
 				  const char *servername,
 				  union spoolss_PrinterInfo **info,
@@ -4130,8 +4131,7 @@ static WERROR enumprinters_level5(TALLOC_CTX *mem_ctx,
 {
 	DEBUG(4,("enum_all_printers_info_5\n"));
 
-	return enum_all_printers_info_level(mem_ctx, server_info,
-					    smbd_messaging_context(),
+	return enum_all_printers_info_level(mem_ctx, server_info, msg_ctx,
 					    5, flags, info, count);
 }
 
@@ -4198,7 +4198,7 @@ WERROR _spoolss_EnumPrinters(struct pipes_struct *p,
 		break;
 	case 5:
 		result = enumprinters_level5(p->mem_ctx, p->server_info,
-					     r->in.flags, name,
+					     p->msg_ctx, r->in.flags, name,
 					     r->out.info, r->out.count);
 		break;
 	default:
