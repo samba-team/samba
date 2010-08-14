2cadfe8f2a3c0a4cf200288f0b6123eca866c510
Matthieu Patou <mat@matws.net>
2010-08-14 18:51:55 +0400

    unit tests: debug to ease locating pb, remove dir if exists to avoid error

--- source4/setup/tests/blackbox_upgradeprovision.sh
+++ source4/setup/tests/blackbox_upgradeprovision.sh
@@ -13,13 +13,19 @@ shift 1
 . `dirname $0`/../../../testprogs/blackbox/subunit.sh
 
 upgradeprovision() {
+  if [ -d $PREFIX/upgradeprovision ]; then
+    rm -fr $PREFIX/upgradeprovision
+  fi
 	$PYTHON ./setup/provision --domain=FOO --realm=foo.example.com --targetdir="$PREFIX/upgradeprovision" --server-role="dc"
-	$PYTHON ./scripting/bin/upgradeprovision -s "$PREFIX/upgradeprovision/etc/smb.conf"
+	$PYTHON ./scripting/bin/upgradeprovision -s "$PREFIX/upgradeprovision/etc/smb.conf" --debugchange
 }
 
 upgradeprovision_full() {
+  if [ -d $PREFIX/upgradeprovision_full ]; then
+    rm -fr $PREFIX/upgradeprovision_full
+  fi
 	$PYTHON ./setup/provision --domain=FOO --realm=foo.example.com --targetdir="$PREFIX/upgradeprovision_full" --server-role="dc"
-	$PYTHON ./scripting/bin/upgradeprovision -s "$PREFIX/upgradeprovision_full/etc/smb.conf" --full
+	$PYTHON ./scripting/bin/upgradeprovision -s "$PREFIX/upgradeprovision_full/etc/smb.conf" --full --debugchange
 }
 
 testit "upgradeprovision" upgradeprovision
