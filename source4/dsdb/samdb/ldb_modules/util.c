896f10301cc3db10793cac115bcbca0f58f0f010
Andrew Tridgell <tridge@samba.org>
2010-08-17 14:55:23 +1000

    s4-dsdb: check the type of session_info from the opaque
    we saw a crash with a bad pointer here, and this may help track it
    down
    
    Pair-Programmed-With: Andrew Bartlett <abartlet@samba.org>

--- source4/dsdb/samdb/ldb_modules/util.c
+++ source4/dsdb/samdb/ldb_modules/util.c
@@ -906,7 +906,7 @@ bool dsdb_module_am_system(struct ldb_module *module)
 {
 	struct ldb_context *ldb = ldb_module_get_ctx(module);
 	struct auth_session_info *session_info
-		= (struct auth_session_info *)ldb_get_opaque(ldb, "sessionInfo");
+		= talloc_get_type(ldb_get_opaque(ldb, "sessionInfo"), struct auth_session_info);
 	return security_session_user_level(session_info, NULL) == SECURITY_SYSTEM;
 }
 
@@ -914,7 +914,7 @@ bool dsdb_module_am_administrator(struct ldb_module *module)
 {
 	struct ldb_context *ldb = ldb_module_get_ctx(module);
 	struct auth_session_info *session_info
-		= (struct auth_session_info *)ldb_get_opaque(ldb, "sessionInfo");
+		= talloc_get_type(ldb_get_opaque(ldb, "sessionInfo"), struct auth_session_info);
 	return security_session_user_level(session_info, NULL) == SECURITY_ADMINISTRATOR;
 }
 
