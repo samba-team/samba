e4afcd62bccf04f2afc779d7292f49e10c29b970
Matthias Dieter Wallnöfer <mdw@samba.org>
2010-08-26 22:30:04 +0200

    s4:credentials_krb5.c - quiet a Solaris warning

--- source4/auth/credentials/credentials_krb5.c
+++ source4/auth/credentials/credentials_krb5.c
@@ -479,7 +479,8 @@ _PUBLIC_ int cli_credentials_get_client_gss_creds(struct cli_credentials *cred,
 		for (num_ktypes = 0; etypes[num_ktypes]; num_ktypes++);
 
 		maj_stat = gss_krb5_set_allowable_enctypes(&min_stat, gcc->creds,
-							   num_ktypes, etypes);
+							   num_ktypes,
+							   (int32_t *) etypes);
 		krb5_xfree (etypes);
 		if (maj_stat) {
 			talloc_free(gcc);
