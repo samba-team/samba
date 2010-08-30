bb32a6cf7024fb20dc410c254257e3ddca180dd1
Martin Schwenke <martin@meltin.net>
2010-08-30 14:23:29 +1000

    Test suite: add time logging.
    We're seeing some weirdness with CTDB controls timing out.  We're
    wondering if time is jumping forward, so this creates a time log on
    each node that we can examine later if tests fail weirdly.
    
    Signed-off-by: Martin Schwenke <martin@meltin.net>
    
    (This used to be ctdb commit 5d82d89ee99f10bead101aebda645a80435ba246)

--- ctdb/tests/simple/00_ctdb_onnode.sh
+++ ctdb/tests/simple/00_ctdb_onnode.sh
@@ -27,3 +27,10 @@ ctdb_test_init "$@"
 
 echo "Checking connectivity between nodes..."
 onnode all onnode all true
+
+# We're seeing some weirdness with CTDB controls timing out.  We're
+# wondering if time is jumping forward, so this creates a time log on
+# each node that we can examine later if tests fail weirdly.
+echo "Starting time logging on each node..."
+f="/var/log/ctdb.test.time.log"
+onnode -p all "[ -f $f ] || while : ; do date '+%T.%N' ; sleep 1 ; done >$f 2>&1 </dev/null &"  &
