0b41ef789575e5194e9b499cf5fe0df4616f1bb8
Andrew Bartlett <abartlet@samba.org>
2010-08-31 09:20:39 +1000

    libcli/security Remove unused declarations from privileges.h
    Signed-off-by: Andrew Tridgell <tridge@samba.org>

--- libcli/security/privileges.h
+++ libcli/security/privileges.h
@@ -28,28 +28,10 @@
 #include "../librpc/gen_ndr/lsa.h"
 #include "../librpc/gen_ndr/security.h"
 
-/* privilege bitmask */
+/* common privilege bitmask defines */
 
-/* common privilege defines */
-
-#define SE_END				0x0
 #define SE_NONE				0x0
 #define SE_ALL_PRIVS                    (uint64_t)-1
-
-
-/* defined in lib/privilegs_basic.c */
-
-extern const uint64_t se_priv_all;
-
-extern const uint64_t se_priv_none;
-extern const uint64_t se_machine_account;
-extern const uint64_t se_print_operator;
-extern const uint64_t se_add_users;
-extern const uint64_t se_disk_operators;
-extern const uint64_t se_remote_shutdown;
-extern const uint64_t se_restore;
-extern const uint64_t se_take_ownership;
-
 
 /*
  * These are used in Lsa replies (srv_lsa_nt.c)
@@ -69,12 +51,6 @@ typedef struct {
 uint64_t sec_privilege_mask(enum sec_privilege privilege);
 
 /***************************************************************************
- copy an uint64_t structure
-****************************************************************************/
-
-bool se_priv_copy( uint64_t *dst, const uint64_t *src );
-
-/***************************************************************************
  put all privileges into a mask
 ****************************************************************************/
 
@@ -86,28 +62,7 @@ bool se_priv_put_all_privileges(uint64_t *privilege_mask);
 
 bool se_priv_from_name( const char *name, uint64_t *privilege_mask );
 
-/****************************************************************************
- check if the privilege is in the privilege list
-****************************************************************************/
-
-bool is_privilege_assigned(const uint64_t *privileges,
-			   const uint64_t *check);
-
 const char* get_privilege_dispname( const char *name );
-
-/****************************************************************************
- Does the user have the specified privilege ?  We only deal with one privilege
- at a time here.
-*****************************************************************************/
-
-bool user_has_privileges(const struct security_token *token, const uint64_t *privilege_bit);
-
-/****************************************************************************
- Does the user have any of the specified privileges ?  We only deal with one privilege
- at a time here.
-*****************************************************************************/
-
-bool user_has_any_privilege(struct security_token *token, const uint64_t *privilege_mask);
 
 /*******************************************************************
  return the number of elements in the 'short' privlege array (traditional source3 behaviour)
@@ -138,11 +93,6 @@ const char *sec_privilege_display_name(enum sec_privilege privilege, uint16_t *l
   map a privilege name to a privilege id. Return -1 if not found
 */
 enum sec_privilege sec_privilege_id(const char *name);
-
-/*
-  map a privilege name to a privilege id. Return -1 if not found
-*/
-enum sec_privilege sec_privilege_from_mask(uint64_t mask);
 
 /*
   assist in walking the table of privileges - return the LUID (low 32 bits) by index
