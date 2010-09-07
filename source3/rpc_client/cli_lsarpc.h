57a4b462b30b5c5718e5a5b25b408bf595555fb8
Andreas Schneider <asn@samba.org>
2010-09-07 11:24:52 +0200

    s3-rpc_client: Added dcerpc_lsa_open_policy.
    Signed-off-by: Günther Deschner <gd@samba.org>
    (cherry picked from commit 2f819505afe2a5f1bd244a625861f7b07c78ae8f)

--- source3/rpc_client/cli_lsarpc.h
+++ source3/rpc_client/cli_lsarpc.h
@@ -1,9 +1,33 @@
 /* The following definitions come from rpc_client/cli_lsarpc.c  */
 
+/**
+ * @brief Open a LSA policy.
+ *
+ * @param[in]  h        The dcerpc binding hanlde to use.
+ *
+ * @param[in]  mem_ctx  The memory context to use.
+ *
+ * @param[in]  sec_qos  Enable security quality of services.
+ *
+ * @param[in]  des_access The disired access rights to be granted.
+ *
+ * @param[out]  pol     A pointer to a rpc policy handle.
+ *
+ * @param[out]  result  A pointer for the NDR NTSTATUS error code.
+ *
+ * @return              A corresponding NTSTATUS error code for the connection.
+ */
+NTSTATUS dcerpc_lsa_open_policy(struct dcerpc_binding_handle *h,
+				TALLOC_CTX *mem_ctx,
+				bool sec_qos,
+				uint32_t des_access,
+				struct policy_handle *pol,
+				NTSTATUS *result);
 NTSTATUS rpccli_lsa_open_policy(struct rpc_pipe_client *cli,
 				TALLOC_CTX *mem_ctx,
 				bool sec_qos, uint32 des_access,
 				struct policy_handle *pol);
+
 NTSTATUS rpccli_lsa_open_policy2(struct rpc_pipe_client *cli,
 				 TALLOC_CTX *mem_ctx, bool sec_qos,
 				 uint32 des_access, struct policy_handle *pol);
