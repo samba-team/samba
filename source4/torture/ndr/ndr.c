81ece84b789c64243db1661df6c9fa590d19490f
Günther Deschner <gd@samba.org>
2010-09-08 11:18:40 +0200

    s4-smbtorture: add torture ndr nbt testsuite.
    Guenther

--- source4/torture/ndr/ndr.c
+++ source4/torture/ndr/ndr.c
@@ -267,6 +267,7 @@ struct torture_suite *torture_local_ndr(TALLOC_CTX *mem_ctx)
 	torture_suite_add_suite(suite, ndr_spoolss_suite(suite));
 	torture_suite_add_suite(suite, ndr_samr_suite(suite));
 	torture_suite_add_suite(suite, ndr_drsblobs_suite(suite));
+	torture_suite_add_suite(suite, ndr_nbt_suite(suite));
 
 	torture_suite_add_simple_test(suite, "string terminator", 
 								   test_check_string_terminator);
