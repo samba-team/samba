65bd8f881db3315c1369835fe6cfc2f27a04ae15
Andreas Schneider <asn@cynapses.org>
2010-09-13 11:29:31 +0200

    s3-printing: Document the printer list functions.

--- source3/printing/printer_list.h
+++ source3/printing/printer_list.h
@@ -22,23 +22,87 @@
 
 bool printer_list_parent_init(void);
 
+/**
+ * @brief Get the comment and the last refresh time from the printer list
+ *        database.
+ *
+ * @param[in]  mem_ctx  The talloc memory context to use.
+ *
+ * @param[in]  name     The printer name to lookup.
+ *
+ * @param[out] comment  A pointer to store the comment of the printer.
+ *
+ * @param[out] last_refresh A pointer to store the last refresh time of the
+ *                          printer.
+ *
+ * @return              NT_STATUS_OK on success, a correspoining NTSTATUS error
+ *                      code on a failure.
+ */
 NTSTATUS printer_list_get_printer(TALLOC_CTX *mem_ctx,
 				  const char *name,
 				  const char **comment,
 				  time_t *last_refresh);
 
+/**
+ * @brief Add a printer to the printer list database.
+ *
+ * @param[in]  mem_ctx  The talloc memory context to use.
+ *
+ * @param[in]  name     The printer name to store in the db.
+ *
+ * @param[in]  comment  The comment to store in the db.
+ *
+ * @param[in]  last_refresh The last refresh time of the printer to store in
+ *                          the db.
+ *
+ * @return              NT_STATUS_OK on success, a correspoining NTSTATUS error
+ *                      code on a failure.
+ */
 NTSTATUS printer_list_set_printer(TALLOC_CTX *mem_ctx,
 				  const char *name,
 				  const char *comment,
 				  time_t last_refresh);
 
+/**
+ * @brief Get the time of the last refresh of the printer database.
+ *
+ * @param[out] last_refresh The last refresh time in the db.
+ *
+ * @return              NT_STATUS_OK on success, a correspoining NTSTATUS error
+ *                      code on a failure.
+ */
 NTSTATUS printer_list_get_last_refresh(time_t *last_refresh);
+
+/**
+ * @brief Mark the database as reloaded.
+ *
+ * This sets the last refresh time to the current time. You can get the last
+ * reload/refresh time of the database with printer_list_get_last_refresh().
+ *
+ * @return              NT_STATUS_OK on success, a correspoining NTSTATUS error
+ *                      code on a failure.
+ */
 NTSTATUS printer_list_mark_reload(void);
+
+/**
+ * @brief Cleanup old entries in the database.
+ *
+ * Entries older than the last refresh times will be deleted.
+ *
+ * @return              NT_STATUS_OK on success, a correspoining NTSTATUS error
+ *                      code on a failure.
+ */
 NTSTATUS printer_list_clean_old(void);
 
 NTSTATUS printer_list_run_fn(void (*fn)(const char *, const char *, void *),
 			     void *private_data);
 
+/**
+ * @brief Check if the printer list needs to be refreshed.
+ *
+ * @return              True if the database needs to be refreshed, false if
+ *                      not.
+ */
 bool printer_list_need_refresh(void);
 
 #endif /* _PRINTER_LIST_H_ */
