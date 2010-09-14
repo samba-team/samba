55f465576c4602e74a6bc5c194adb27231c520f8
Günther Deschner <gd@samba.org>
2010-09-14 17:27:55 +0200

    s4-smbtorture: add LOCAL-NDR-NTLMSSP.
    Guenther

--- source4/torture/ndr/ndr.c
+++ source4/torture/ndr/ndr.c
@@ -353,6 +353,7 @@ struct torture_suite *torture_local_ndr(TALLOC_CTX *mem_ctx)
 	torture_suite_add_suite(suite, ndr_samr_suite(suite));
 	torture_suite_add_suite(suite, ndr_drsblobs_suite(suite));
 	torture_suite_add_suite(suite, ndr_nbt_suite(suite));
+	torture_suite_add_suite(suite, ndr_ntlmssp_suite(suite));
 
 	torture_suite_add_simple_test(suite, "string terminator",
 				      test_check_string_terminator);
