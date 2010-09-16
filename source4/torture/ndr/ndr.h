64cac0c36c2ddc2d3b1ecdc2de731b9bab72137a
Günther Deschner <gd@samba.org>
2010-09-16 16:22:32 +0200

    s3-smbtorture: fixup trailing whitespace in ndr testsuite.
    Guenther
    (cherry picked from commit 725126b1723438ba8b6f9dd41eac8730856da41e)

--- source4/torture/ndr/ndr.h
+++ source4/torture/ndr/ndr.h
@@ -1,18 +1,18 @@
-/* 
+/*
    Unix SMB/CIFS implementation.
    SMB torture tester
    Copyright (C) Jelmer Vernooij 2007
-   
+
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -25,9 +25,9 @@
 #include "libcli/security/security.h"
 
 _PUBLIC_ struct torture_test *_torture_suite_add_ndr_pull_test(
-					struct torture_suite *suite, 
+					struct torture_suite *suite,
 					const char *name, ndr_pull_flags_fn_t fn,
-					DATA_BLOB db, 
+					DATA_BLOB db,
 					size_t struct_size,
 					int ndr_flags,
 					bool (*check_fn) (struct torture_context *, void *data));
