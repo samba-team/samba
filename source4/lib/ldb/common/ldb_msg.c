0f163eb61113424e55887a9d0f2a7a89b109c4d0
Matthias Dieter Wallnöfer <mdw@samba.org>
2010-09-16 18:08:56 +0200

    ldb:ldb_msg_add_linearized_dn - handle NULL DNs
    Don't let the routine crash
    
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>

--- source4/lib/ldb/common/ldb_msg.c
+++ source4/lib/ldb/common/ldb_msg.c
@@ -307,8 +307,14 @@ int ldb_msg_add_steal_string(struct ldb_message *msg,
 int ldb_msg_add_linearized_dn(struct ldb_message *msg, const char *attr_name,
 			      struct ldb_dn *dn)
 {
-	return ldb_msg_add_steal_string(msg, attr_name,
-					ldb_dn_alloc_linearized(msg, dn));
+	char *str = ldb_dn_alloc_linearized(msg, dn);
+
+	if (str == NULL) {
+		/* we don't want to have unknown DNs added */
+		return LDB_ERR_OPERATIONS_ERROR;
+	}
+
+	return ldb_msg_add_steal_string(msg, attr_name, str);
 }
 
 /*
