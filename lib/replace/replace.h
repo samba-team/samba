fbd65b6c859905855c473b47101a4843048ee4a5
Björn Jacke <bj@sernet.de>
2010-09-22 11:16:47 -0700

    libreplace: fix endless strerror_r has been redefined warnings on AIX (cherry picked from commit 9b73bb0099d6b33d9e951895be3c877628b6c7dd)

--- lib/replace/replace.h
+++ lib/replace/replace.h
@@ -513,6 +513,7 @@ char *rep_get_current_dir_name(void);
 #endif
 
 #if !defined(HAVE_STRERROR_R) || !defined(STRERROR_R_PROTO_COMPATIBLE)
+#undef strerror_r
 #define strerror_r rep_strerror_r
 int rep_strerror_r(int errnum, char *buf, size_t buflen);
 #endif
