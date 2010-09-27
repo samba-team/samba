2921888a0808efbe765e3f52a83a91c2e03a44a6
Günther Deschner <gd@samba.org>
2010-09-28 00:10:17 +0200

    s3-spoolss: Fix _spoolss_EnumPrintProcessors error handling

--- source3/rpc_server/srv_spoolss_nt.c
+++ source3/rpc_server/srv_spoolss_nt.c
@@ -8501,6 +8501,10 @@ WERROR _spoolss_EnumPrintProcessors(struct pipes_struct *p,
 	*r->out.needed = 0;
 	*r->out.info = NULL;
 
+	if (!get_short_archi(r->in.environment)) {
+		return WERR_INVALID_ENVIRONMENT;
+	}
+
 	switch (r->in.level) {
 	case 1:
 		result = enumprintprocessors_level_1(p->mem_ctx, r->out.info,
