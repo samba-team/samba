d8d8778aaf8f6fa7056b915a2bb71076d33c8c2d
Jelmer Vernooij <jelmer@samba.org>
2010-10-02 23:40:19 +0200

    pytdb: Include Python.h first to prevent warning.
    (This used to be ctdb commit 247dacde0d0de1358cc2c27d08914be605272023)

--- ctdb/lib/tdb/pytdb.c
+++ ctdb/lib/tdb/pytdb.c
@@ -24,10 +24,10 @@
    License along with this library; if not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <Python.h>
 #include "replace.h"
 #include "system/filesys.h"
 
-#include <Python.h>
 #ifndef Py_RETURN_NONE
 #define Py_RETURN_NONE return Py_INCREF(Py_None), Py_None
 #endif
