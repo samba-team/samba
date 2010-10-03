8e5a9e7721a966ce1491f727d25ab3f0b8e8fa58
Jelmer Vernooij <jelmer@samba.org>
2010-10-03 16:01:58 +0200

    heimdal: Move autoproto code to heimdal_build.

--- buildtools/wafsamba/samba_autoproto.py
+++ buildtools/wafsamba/samba_autoproto.py
@@ -3,29 +3,6 @@
 import Build
 from samba_utils import *
 
-def HEIMDAL_AUTOPROTO(bld, header, source, options=None, group='prototypes'):
-    '''rule for heimdal prototype generation'''
-    bld.SET_BUILD_GROUP(group)
-    if options is None:
-        options='-q -P comment -o'
-    SET_TARGET_TYPE(bld, header, 'PROTOTYPE')
-    t = bld(rule='${PERL} ${HEIMDAL}/cf/make-proto.pl ${OPTIONS} ${TGT[0].abspath(env)} ${SRC}',
-            source=source,
-            target=header,
-            on_results=True,
-            ext_out='.c',
-            before='cc')
-    t.env.HEIMDAL = os.path.join(bld.srcnode.abspath(), 'source4/heimdal')
-    t.env.OPTIONS = options
-Build.BuildContext.HEIMDAL_AUTOPROTO = HEIMDAL_AUTOPROTO
-
-
-def HEIMDAL_AUTOPROTO_PRIVATE(bld, header, source):
-    '''rule for private heimdal prototype generation'''
-    bld.HEIMDAL_AUTOPROTO(header, source, options='-q -P comment -p')
-Build.BuildContext.HEIMDAL_AUTOPROTO_PRIVATE = HEIMDAL_AUTOPROTO_PRIVATE
-
-
 def SAMBA_AUTOPROTO(bld, header, source):
     '''rule for samba prototype generation'''
     bld.SET_BUILD_GROUP('prototypes')
