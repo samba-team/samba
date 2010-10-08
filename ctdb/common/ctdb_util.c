0aa2282c9c26c87e55db6f9df435d5d932f2cbd1
Ronnie Sahlberg <ronniesahlberg@gmail.com>
2010-10-08 13:14:14 +1100

    change the hash function to use the much better Jenkins hash from the tdb library
    cq S1020233
    
    (This used to be ctdb commit b86feb6fe463dfdb67b2798491df18a4c434a430)

--- ctdb/common/ctdb_util.c
+++ ctdb/common/ctdb_util.c
@@ -99,14 +99,7 @@ bool ctdb_same_address(struct ctdb_address *a1, struct ctdb_address *a2)
 */
 uint32_t ctdb_hash(const TDB_DATA *key)
 {
-	uint32_t value;	/* Used to compute the hash value.  */
-	uint32_t i;	/* Used to cycle through random values. */
-
-	/* Set the initial value from the key size. */
-	for (value = 0x238F13AF * key->dsize, i=0; i < key->dsize; i++)
-		value = (value + (key->dptr[i] << (i*5 % 24)));
-
-	return (1103515243 * value + 12345);  
+	return tdb_jenkins_hash(discard_const(key));
 }
 
 /*
