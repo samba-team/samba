57bf052e683e6ebc99dd85e3d1a6cf62d2fb89ea
Jelmer Vernooij <jelmer@samba.org>
2010-10-11 00:41:22 +0200

    smb_server: Split core out of service, since the service can be built as a .so against which we can't link.

--- source4/smb_server/smb_server.c
+++ source4/smb_server/smb_server.c
@@ -199,48 +199,4 @@ _PUBLIC_ NTSTATUS smbsrv_add_socket(struct tevent_context *event_context,
 }
 
 
-/*
-  open the smb server sockets
-*/
-static void smbsrv_task_init(struct task_server *task)
-{	
-	NTSTATUS status;
 
-	task_server_set_title(task, "task[smbsrv]");
-
-	if (lpcfg_interfaces(task->lp_ctx) && lpcfg_bind_interfaces_only(task->lp_ctx)) {
-		int num_interfaces;
-		int i;
-		struct interface *ifaces;
-
-		load_interfaces(task, lpcfg_interfaces(task->lp_ctx), &ifaces);
-
-		num_interfaces = iface_count(ifaces);
-
-		/* We have been given an interfaces line, and been 
-		   told to only bind to those interfaces. Create a
-		   socket per interface and bind to only these.
-		*/
-		for(i = 0; i < num_interfaces; i++) {
-			const char *address = iface_n_ip(ifaces, i);
-			status = smbsrv_add_socket(task->event_ctx, task->lp_ctx, task->model_ops, address);
-			if (!NT_STATUS_IS_OK(status)) goto failed;
-		}
-	} else {
-		/* Just bind to lpcfg_socket_address() (usually 0.0.0.0) */
-		status = smbsrv_add_socket(task->event_ctx, task->lp_ctx, task->model_ops,
-					   lpcfg_socket_address(task->lp_ctx));
-		if (!NT_STATUS_IS_OK(status)) goto failed;
-	}
-
-	return;
-failed:
-	task_server_terminate(task, "Failed to startup smb server task", true);	
-}
-
-/* called at smbd startup - register ourselves as a server service */
-NTSTATUS server_service_smb_init(void)
-{
-	share_init();
-	return register_server_service("smb", smbsrv_task_init);
-}
