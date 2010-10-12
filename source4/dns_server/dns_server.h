96195b2c0c239781c47f51bbf62156a8804f9525
Kai Blin <kai@samba.org>
2010-10-12 06:23:32 +0200

    s4 dns: Add a boilerplate DNS server implementation

---
+++ source4/dns_server/dns_server.h
@@ -0,0 +1,33 @@
+/*
+   Unix SMB/CIFS implementation.
+
+   DNS structures
+
+   Copyright (C) 2010 Kai Blin  <kai@samba.org>
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef __DNS_SERVER_H__
+#define __DNS_SERVER_H__
+
+#include "librpc/gen_ndr/dns.h"
+
+struct tsocket_address;
+
+struct dns_server {
+	struct task_server *task;
+};
+
+#endif /* __DNS_SERVER_H__ */
