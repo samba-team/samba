e675462b3cfc53d7fe0c6e07c13a386599c5afd9
Jeremy Allison <jra@samba.org>
2010-10-15 14:12:04 -0700

    Fix bug #7734 - When creating files with "inherit ACLs" set to true, we neglect to apply appropriate create masks.
    Jeremy.
    (cherry picked from commit 8cad5e23b6e2440a566def6fb138d484e3b47643)

--- source3/modules/vfs_default.c
+++ source3/modules/vfs_default.c
@@ -217,7 +217,7 @@ static int vfswrap_mkdir(vfs_handle_struct *handle,  const char *path, mode_t mo
 	if (lp_inherit_acls(SNUM(handle->conn))
 	    && parent_dirname(talloc_tos(), path, &parent, NULL)
 	    && (has_dacl = directory_has_default_acl(handle->conn, parent)))
-		mode = 0777;
+		mode = (0777 & lp_dir_mask(SNUM(handle->conn)));
 
 	TALLOC_FREE(parent);
 
