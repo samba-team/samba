9268e6028cf43f1ac0e3b073c32e364ea4ca4611
Jeremy Allison <jra@samba.org>
2010-10-22 16:04:53 -0700

    Add make_default_filesystem_acl() function to be used in following change to acl_xattr and acl_tdb module. (cherry picked from commit 2d84fce8f20c4eac70b02f0fc4333b15e278edfc)

--- source3/include/proto.h
+++ source3/include/proto.h
@@ -6767,6 +6767,10 @@ bool set_unix_posix_default_acl(connection_struct *conn, const char *fname,
 				uint16 num_def_acls, const char *pdata);
 bool set_unix_posix_acl(connection_struct *conn, files_struct *fsp, const char *fname, uint16 num_acls, const char *pdata);
 SEC_DESC *get_nt_acl_no_snum( TALLOC_CTX *ctx, const char *fname);
+NTSTATUS make_default_filesystem_acl(TALLOC_CTX *ctx,
+					const char *name,
+					SMB_STRUCT_STAT *psbuf,
+					SEC_DESC **ppdesc);
 
 /* The following definitions come from smbd/process.c  */
 
