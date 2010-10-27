64867330493c676cfe6a34ea462be45c638966ee
Kai Blin <kai@samba.org>
2010-10-27 22:15:10 +0200

    ndr: Make sure ndr_dns.c has no lines wider than 80 chars
    Autobuild-User: Kai Blin <kai@samba.org>
    Autobuild-Date: Wed Oct 27 21:17:50 UTC 2010 on sn-devel-104

--- librpc/ndr/ndr_dns.h
+++ librpc/ndr/ndr_dns.h
@@ -1,5 +1,15 @@
-_PUBLIC_ void ndr_print_dns_string(struct ndr_print *ndr, const char *name, const char *s);
-_PUBLIC_ enum ndr_err_code ndr_pull_dns_string(struct ndr_pull *ndr, int ndr_flags, const char **s);
-_PUBLIC_ enum ndr_err_code ndr_push_dns_string(struct ndr_push *ndr, int ndr_flags, const char *s);
-_PUBLIC_ enum ndr_err_code ndr_push_dns_res_rec(struct ndr_push *ndr, int ndr_flags, const struct dns_res_rec *r);
-_PUBLIC_ enum ndr_err_code ndr_pull_dns_res_rec(struct ndr_pull *ndr, int ndr_flags, struct dns_res_rec *r);
+void ndr_print_dns_string(struct ndr_print *ndr,
+			  const char *name,
+			  const char *s);
+enum ndr_err_code ndr_pull_dns_string(struct ndr_pull *ndr,
+				      int ndr_flags,
+				      const char **s);
+enum ndr_err_code ndr_push_dns_string(struct ndr_push *ndr,
+				      int ndr_flags,
+				      const char *s);
+enum ndr_err_code ndr_push_dns_res_rec(struct ndr_push *ndr,
+				       int ndr_flags,
+				       const struct dns_res_rec *r);
+enum ndr_err_code ndr_pull_dns_res_rec(struct ndr_pull *ndr,
+				       int ndr_flags,
+				       struct dns_res_rec *r);
