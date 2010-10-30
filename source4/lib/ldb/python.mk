7fc0aaba4b7383973bbc2e80addea11658d48fbf
Jelmer Vernooij <jelmer@samba.org>
2010-10-30 16:17:58 +0200

    ldb: Drop autoconf-based build system.

--- source4/lib/ldb/python.mk
+++
@@ -1,6 +0,0 @@
-[PYTHON::pyldb]
-LIBRARY_REALNAME = ldb.$(SHLIBEXT)
-PUBLIC_DEPENDENCIES = LIBLDB PYTALLOC
-
-pyldb_OBJ_FILES = $(ldbsrcdir)/pyldb.o 
-$(pyldb_OBJ_FILES): CFLAGS+=-I$(ldbsrcdir)/include
