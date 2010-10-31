f99c009b61c9e8b5968a9b074546408ce93db73c
Jelmer Vernooij <jelmer@samba.org>
2010-10-31 02:44:32 +0100

    replace: Build using waf by default.
    Autobuild-User: Jelmer Vernooij <jelmer@samba.org>
    Autobuild-Date: Sun Oct 31 02:45:21 UTC 2010 on sn-devel-104

--- lib/replace/autogen-waf.sh
+++ lib/replace/autogen-waf.sh
@@ -1,1 +1,11 @@
-../../buildtools/scripts/autogen-waf.sh
+#!/bin/sh
+
+p=`dirname $0`
+
+echo "Setting up for waf build"
+
+echo "done. Now run $p/configure or $p/configure.developer then make."
+if [ $p != "." ]; then
+	echo "Notice: The build invoke path is not the main directory! Use make with the parameter"
+	echo "-C $p. Example: make -C $p all"
+fi
