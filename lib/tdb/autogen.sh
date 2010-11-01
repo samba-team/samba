1aa8308c30962ac04a2997acaa7f2a7458729cc2
Jelmer Vernooij <jelmer@samba.org>
2010-11-01 06:18:55 +0100

    tdb: Use waf by default.
    Autobuild-User: Jelmer Vernooij <jelmer@samba.org>
    Autobuild-Date: Mon Nov  1 06:04:14 UTC 2010 on sn-devel-104

--- lib/tdb/autogen.sh
+++ lib/tdb/autogen.sh
@@ -1,14 +1,1 @@
-#!/bin/sh
-
-rm -rf autom4te.cache
-rm -f configure config.h.in
-
-IPATHS="-I libreplace -I lib/replace -I ../libreplace -I ../replace"
-autoconf $IPATHS || exit 1
-autoheader $IPATHS || exit 1
-
-rm -rf autom4te.cache
-
-echo "Now run ./configure and then make."
-exit 0
-
+autogen-waf.sh
