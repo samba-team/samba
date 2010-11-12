6a27fbbfc4c51ae1635b8a5fa51c470ebc9f01e2
Andrew Bartlett <abartlet@samba.org>
2010-11-12 15:27:43 +1100

    heimdal Extra files required for merge up to current heimdal

--- source4/heimdal_build/kpasswdd-glue.h
+++
@@ -1,6 +0,0 @@
-/* TODO: remove this file */
-struct _krb5_krb_auth_data;
-struct krb5_dh_moduli;
-struct AlgorithmIdentifier;
-#include "heimdal/lib/hcrypto/evp.h"
-#include "heimdal/lib/krb5/krb5-private.h"
