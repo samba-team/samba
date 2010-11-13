8e34df462c138440510b48a33ee8fd2730449e7d
Andrew Tridgell <tridge@samba.org>
2010-11-13 12:18:19 +1100

    s4-test: we need to import testtools before subunit/python
    subunit/python depends on testtools
    
    Autobuild-User: Andrew Tridgell <tridge@samba.org>
    Autobuild-Date: Sat Nov 13 02:02:45 UTC 2010 on sn-devel-104

--- source4/auth/credentials/tests/bind.py
+++ source4/auth/credentials/tests/bind.py
@@ -12,8 +12,8 @@ import time
 
 sys.path.append("bin/python")
 import samba
-samba.ensure_external_module("subunit", "subunit/python")
 samba.ensure_external_module("testtools", "testtools")
+samba.ensure_external_module("subunit", "subunit/python")
 
 import samba.getopt as options
 
