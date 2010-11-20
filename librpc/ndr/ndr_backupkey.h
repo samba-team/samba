25ae380fabfd2d7e5141dadb48b8e30b5b723c1f
Matthieu Patou <mat@matws.net>
2010-11-20 18:19:06 +0300

    idl: Add IDL for remote key backup protocol (rkbp)
    Signed-off-by: Stefan Metzmacher <metze@samba.org>

---
+++ librpc/ndr/ndr_backupkey.h
@@ -0,0 +1,2 @@
+_PUBLIC_ enum ndr_err_code ndr_push_bkrp_access_check_v2(struct ndr_push *ndr, int ndr_flags, const struct bkrp_access_check_v2 *r);
+_PUBLIC_ enum ndr_err_code ndr_pull_bkrp_access_check_v2(struct ndr_pull *ndr, int ndr_flags, struct bkrp_access_check_v2 *r);
