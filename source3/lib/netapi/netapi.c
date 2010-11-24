samba-team/samba

commit cf4de8ec2c8df2ceabbe3d836d296b058e7b19fb

s3-debug Remove 'AllowDebugChange' and use lp_set_cmdline() instead
By removing this global variable, the API between the two different
debug systems is made more similar.  Both s3 and s4 now have
lp_set_cmdline() which ensures that the smb.conf cannot overwrite
these the user-specified log level.

Andrew Bartlett

source3/lib/netapi/netapi.c
@@ -23,8 +23,6 @@
 #include "secrets.h"
 #include "krb5_env.h"
 
-extern bool AllowDebugChange;
-
 struct libnetapi_ctx *stat_ctx = NULL;
 TALLOC_CTX *frame = NULL;
 static bool libnetapi_initialized = false;
@@ -75,14 +73,10 @@ NET_API_STATUS libnetapi_init(struct libnetapi_ctx **context)
 		return W_ERROR_V(WERR_NOMEM);
 	}
 
-	if (!DEBUGLEVEL) {
-		DEBUGLEVEL = 0;
-	}
+	lp_set_cmdline("log level", "0");
 
 	/* prevent setup_logging() from closing x_stderr... */
 	setup_logging("libnetapi", DEBUG_STDERR);
-
-	AllowDebugChange = false;
 
 	load_case_tables();
 
@@ -91,8 +85,6 @@ NET_API_STATUS libnetapi_init(struct libnetapi_ctx **context)
 		fprintf(stderr, "error loading %s\n", get_dyn_CONFIGFILE() );
 		return W_ERROR_V(WERR_GENERAL_FAILURE);
 	}
-
-	AllowDebugChange = true;
 
 	init_names();
 	load_interfaces();
@@ -185,9 +177,8 @@ NET_API_STATUS libnetapi_free(struct libnetapi_ctx *ctx)
 NET_API_STATUS libnetapi_set_debuglevel(struct libnetapi_ctx *ctx,
 					const char *debuglevel)
 {
-	AllowDebugChange = true;
 	ctx->debuglevel = talloc_strdup(ctx, debuglevel);
-	if (!debug_parse_levels(debuglevel)) {
+	if (!lp_set_cmdline("log level", debuglevel)) {
 		return W_ERROR_V(WERR_GENERAL_FAILURE);
 	}
 	return NET_API_STATUS_SUCCESS;

nsswitch/wins.c
@@ -94,8 +94,7 @@ static int wins_lookup_open_socket_in(void)
 static void nss_wins_init(void)
 {
 	initialised = 1;
-	DEBUGLEVEL = 0;
-	AllowDebugChange = False;
+	lp_set_cmdline("log level", "0");
 
 	TimeInit();
 	setup_logging("nss_wins",False);

source3/client/client.c
@@ -5047,7 +5047,7 @@ static int do_message_op(struct user_auth_info *a_info)
 
         /* set default debug level to 1 regardless of what smb.conf sets */
 	setup_logging( "smbclient", DEBUG_DEFAULT_STDERR );
-	DEBUGLEVEL_CLASS[DBGC_ALL] = 1;
+	lp_set_cmdline("log level", "1");
 
 	load_case_tables();
 
@@ -5189,12 +5189,6 @@ static int do_message_op(struct user_auth_info *a_info)
 		set_cmdline_auth_info_password(auth_info,
 					       poptGetArg(pc));
 	}
-
-	/*
-	 * Don't load debug level from smb.conf. It should be
-	 * set by cmdline arg or remain default (0)
-	 */
-	AllowDebugChange = false;
 
 	/* save the workgroup...
 

source3/lib/debug.c
@@ -89,7 +89,6 @@ static struct {
 static char *debugf = NULL;
 bool    debug_warn_unknown_class = True;
 bool    debug_auto_add_unknown_class = True;
-bool    AllowDebugChange = True;
 
 /*
    used to check if the user specified a
@@ -468,9 +467,6 @@ bool debug_parse_levels(const char *params_str)
 
 	/* Just in case */
 	debug_init();
-
-	if (AllowDebugChange == False)
-		return True;
 
 	params = str_list_make_v3(talloc_tos(), params_str, NULL);
 

source3/lib/popt_common.c
@@ -36,7 +36,6 @@
 
 enum {OPT_OPTION=1};
 
-extern bool AllowDebugChange;
 extern bool override_logfile;
 
 static void set_logfile(poptContext con, const char * arg)
@@ -107,8 +106,7 @@ static void popt_common_callback(poptContext con,
 
 	case 'd':
 		if (arg) {
-			debug_parse_levels(arg);
-			AllowDebugChange = False;
+			lp_set_cmdline("log level", arg);
 		}
 		break;
 

source3/param/test_lp_load.c
@@ -20,8 +20,6 @@
 #include "includes.h"
 #include "popt_common.h"
 
-extern bool AllowDebugChange;
-
 int main(int argc, const char **argv)
 {
 	const char *config_file = get_dyn_CONFIGFILE();
@@ -41,7 +39,7 @@ int main(int argc, const char **argv)
 	TALLOC_CTX *frame = talloc_stackframe();
 
 	load_case_tables();
-	DEBUGLEVEL_CLASS[DBGC_ALL] = 0;
+	lp_set_cmdline("log level", "0");
 
 	pc = poptGetContext(NULL, argc, argv, long_options,
 			    POPT_CONTEXT_KEEP_FIRST);
@@ -60,9 +58,6 @@ int main(int argc, const char **argv)
 	if (count_str != NULL) {
 		count = atoi(count_str);
 	}
-
-	/* Don't let the debuglevel be changed by smb.conf. */
-	AllowDebugChange = False;
 
 	for (i=0; i < count; i++) {
 		printf("call lp_load() #%d: ", i+1);

source3/torture/masktest.c
@@ -34,7 +34,6 @@ static int ignore_dot_errors = 0;
 
 extern char *optarg;
 extern int optind;
-extern bool AllowDebugChange;
 
 /* a test fn for LANMAN mask support */
 static int ms_fnmatch_lanman_core(const char *pattern, const char *string)
@@ -484,8 +483,7 @@ static void usage(void)
 
 	setlinebuf(stdout);
 
-	DEBUGLEVEL = 0;
-	AllowDebugChange = False;
+	lp_set_cmdline("log level", "0");
 
 	if (argc < 2 || argv[1][0] == '-') {
 		usage();

source3/utils/dbwrap_tool.c
@@ -22,8 +22,6 @@
 #include "includes.h"
 #include "dbwrap.h"
 
-extern bool AllowDebugChange;
-
 typedef enum { OP_FETCH, OP_STORE, OP_DELETE, OP_ERASE, OP_LISTKEYS } dbwrap_op;
 
 typedef enum { TYPE_INT32, TYPE_UINT32 } dbwrap_type;
@@ -214,9 +212,8 @@ int main(int argc, const char **argv)
 	int ret = 1;
 
 	load_case_tables();
-	DEBUGLEVEL_CLASS[DBGC_ALL] = 0;
+	lp_set_cmdline("log level", "0");
 	setup_logging(argv[0], DEBUG_STDERR);
-	AllowDebugChange = false;
 	lp_load(get_dyn_CONFIGFILE(), true, false, false, true);
 
 	if ((argc < 3) || (argc > 6)) {

commit 88d020ade07bfe5cd7570b8c9b80a162adb39891

s3-netapi Add libnetapi_net_init(), don't double-init common Samba subsystems
The issue here is that libnet and net were both trying to load the
smb.conf files, the case tables and set the debug levels.  The set of
the debug levels caused problems, because it would force the level to
0, not (say) 10 as requested on the command line.

This regression was apparently introduced in
cf4de8ec2c8df2ceabbe3d836d296b058e7b19fb when eliminating
AllowDebugChange.

Andrew Bartlett

source3/lib/netapi/netapi.c
@@ -49,14 +49,14 @@ static NET_API_STATUS libnetapi_init_private_context(struct libnetapi_ctx *ctx)
 }
 
 /****************************************************************
+Create a libnetapi context, for use in non-Samba applications.  This
+loads the smb.conf file and sets the debug level to 0, so that
+applications are not flooded with debug logs at level 10, when they
+were not expecting it.
 ****************************************************************/
 
 NET_API_STATUS libnetapi_init(struct libnetapi_ctx **context)
 {
-	NET_API_STATUS status;
-	struct libnetapi_ctx *ctx = NULL;
-	char *krb5_cc_env = NULL;
-
 	if (stat_ctx && libnetapi_initialized) {
 		*context = stat_ctx;
 		return NET_API_STATUS_SUCCESS;
@@ -67,15 +67,10 @@ NET_API_STATUS libnetapi_init(struct libnetapi_ctx **context)
 #endif
 	frame = talloc_stackframe();
 
-	ctx = talloc_zero(frame, struct libnetapi_ctx);
-	if (!ctx) {
-		TALLOC_FREE(frame);
-		return W_ERROR_V(WERR_NOMEM);
-	}
-
+	/* When libnetapi is invoked from an application, it does not
+	 * want to be swamped with level 10 debug messages, even if
+	 * this has been set for the server in smb.conf */
 	lp_set_cmdline("log level", "0");
-
-	/* prevent setup_logging() from closing x_stderr... */
 	setup_logging("libnetapi", DEBUG_STDERR);
 
 	load_case_tables();
@@ -89,6 +84,33 @@ NET_API_STATUS libnetapi_init(struct libnetapi_ctx **context)
 	init_names();
 	load_interfaces();
 	reopen_logs();
+
+	BlockSignals(True, SIGPIPE);
+
+	return libnetapi_net_init(context);
+}
+
+/****************************************************************
+Create a libnetapi context, for use inside the 'net' binary.
+
+As we know net has already loaded the smb.conf file, and set the debug
+level etc, this avoids doing so again (which causes trouble with -d on
+the command line).
+****************************************************************/
+
+NET_API_STATUS libnetapi_net_init(struct libnetapi_ctx **context)
+{
+	NET_API_STATUS status;
+	struct libnetapi_ctx *ctx = NULL;
+	char *krb5_cc_env = NULL;
+
+	frame = talloc_stackframe();
+
+	ctx = talloc_zero(frame, struct libnetapi_ctx);
+	if (!ctx) {
+		TALLOC_FREE(frame);
+		return W_ERROR_V(WERR_NOMEM);
+	}
 
 	BlockSignals(True, SIGPIPE);
 

source3/lib/netapi/netapi_net.h
@@ -1,0 +1,24 @@
+/*
+ *  Unix SMB/CIFS implementation.
+ *  NetApi Support
+ *  Copyright (C) Andrew Bartlett 2010
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* This API header is private between the 'net' binary and and libnet */
+
+/* This function is to init the libnetapi subsystem, without
+ * re-reading config files or setting debug levels etc */
+NET_API_STATUS libnetapi_net_init(struct libnetapi_ctx **ctx);

source3/lib/netapi/netapi_private.h
@@ -20,6 +20,8 @@
 #ifndef __LIB_NETAPI_PRIVATE_H__
 #define __LIB_NETAPI_PRIVATE_H__
 
+#include "lib/netapi/netapi_net.h"
+
 #define LIBNETAPI_REDIRECT_TO_LOCALHOST(ctx, r, fn) \
 	DEBUG(10,("redirecting call %s to localhost\n", #fn)); \
 	if (!r->in.server_name) { \

source3/utils/net_dom.c
@@ -22,6 +22,7 @@
 #include "../librpc/gen_ndr/cli_initshutdown.h"
 #include "../librpc/gen_ndr/ndr_winreg.h"
 #include "lib/netapi/netapi.h"
+#include "lib/netapi/netapi_net.h"
 
 int net_dom_usage(struct net_context *c, int argc, const char **argv)
 {
@@ -372,7 +373,7 @@ int net_dom(struct net_context *c, int argc, const char **argv)
 		{NULL, NULL, 0, NULL, NULL}
 	};
 
-	status = libnetapi_init(&c->netapi_ctx);
+	status = libnetapi_net_init(&c->netapi_ctx);
 	if (status != 0) {
 		return -1;
 	}

source3/utils/net_rpc.c
@@ -35,6 +35,7 @@
 #include "../librpc/gen_ndr/cli_winreg.h"
 #include "secrets.h"
 #include "lib/netapi/netapi.h"
+#include "lib/netapi/netapi_net.h"
 #include "rpc_client/init_lsa.h"
 #include "../libcli/security/security.h"
 
@@ -1050,7 +1051,7 @@ int net_rpc_user(struct net_context *c, int argc, const char **argv)
 		{NULL, NULL, 0, NULL, NULL}
 	};
 
-	status = libnetapi_init(&c->netapi_ctx);
+	status = libnetapi_net_init(&c->netapi_ctx);
 	if (status != 0) {
 		return -1;
 	}
@@ -2899,7 +2900,7 @@ int net_rpc_group(struct net_context *c, int argc, const char **argv)
 		{NULL, NULL, 0, NULL, NULL}
 	};
 
-	status = libnetapi_init(&c->netapi_ctx);
+	status = libnetapi_net_init(&c->netapi_ctx);
 	if (status != 0) {
 		return -1;
 	}
@@ -4745,7 +4746,7 @@ int net_rpc_share(struct net_context *c, int argc, const char **argv)
 		{NULL, NULL, 0, NULL, NULL}
 	};
 
-	status = libnetapi_init(&c->netapi_ctx);
+	status = libnetapi_net_init(&c->netapi_ctx);
 	if (status != 0) {
 		return -1;
 	}
@@ -5021,7 +5022,7 @@ int net_rpc_file(struct net_context *c, int argc, const char **argv)
 		{NULL, NULL, 0, NULL, NULL}
 	};
 
-	status = libnetapi_init(&c->netapi_ctx);
+	status = libnetapi_net_init(&c->netapi_ctx);
 	if (status != 0) {
 		return -1;
 	}
@@ -7428,7 +7429,7 @@ int net_rpc(struct net_context *c, int argc, const char **argv)
 		{NULL, NULL, 0, NULL, NULL}
 	};
 
-	status = libnetapi_init(&c->netapi_ctx);
+	status = libnetapi_net_init(&c->netapi_ctx);
 	if (status != 0) {
 		return -1;
 	}

source3/utils/net_rpc_shell.c
@@ -23,6 +23,7 @@
 #include "utils/net.h"
 #include "../librpc/gen_ndr/ndr_samr.h"
 #include "lib/netapi/netapi.h"
+#include "lib/netapi/netapi_net.h"
 #include "../libcli/smbreadline/smbreadline.h"
 
 static NTSTATUS rpc_sh_info(struct net_context *c,
@@ -222,7 +223,7 @@ int net_rpc_shell(struct net_context *c, int argc, const char **argv)
 		return -1;
 	}
 
-	if (libnetapi_init(&c->netapi_ctx) != 0) {
+	if (libnetapi_net_init(&c->netapi_ctx) != 0) {
 		return -1;
 	}
 	libnetapi_set_username(c->netapi_ctx, c->opt_user_name);