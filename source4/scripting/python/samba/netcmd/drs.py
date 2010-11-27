2a4c6da7835cb6129a68e3e6e6c16e8f0a656eb2
Andrew Tridgell <tridge@samba.org>
2010-11-28 10:41:53 +1100

    s4-drs: fixed credit for C version

--- source4/scripting/python/samba/netcmd/drs.py
+++ source4/scripting/python/samba/netcmd/drs.py
@@ -4,6 +4,8 @@
 #
 # Copyright Andrew Tridgell 2010
 #
+# based on C implementation by Kamen Mazdrashki <kamen.mazdrashki@postpath.com>
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3 of the License, or
