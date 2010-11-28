97f0d6c27834975256fe21518007807400e61f4b
Jelmer Vernooij <jelmer@samba.org>
2010-11-28 16:38:27 +0100

    samba.tests.hostconfig: Fix import.
    Autobuild-User: Jelmer Vernooij <jelmer@samba.org>
    Autobuild-Date: Sun Nov 28 18:08:33 CET 2010 on sn-devel-104

--- source4/scripting/python/samba/tests/hostconfig.py
+++ source4/scripting/python/samba/tests/hostconfig.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-from samba.shares import SharesContainer
+from samba.hostconfig import SharesContainer
 from samba.tests import TestCase
 
 
