a762e9cab70f3ecc000b5dd20ce5f792963e8a52
Stefan Metzmacher <metze@samba.org>
2010-12-01 20:36:43 +0100

    s4:ldap_controls: allow DSDB_CONTROL_BYPASS_PASSWORD_HASH_OID over sockets.
    The DSDB_CONTROL_BYPASS_PASSWORD_HASH_OID control has to data attached to it.
    So we can allow it to be send over LDAP.
    
    We'll accept this control over the privileged ldapi socket only.
    
    metze

--- source4/libcli/ldap/ldap_controls.c
+++ source4/libcli/ldap/ldap_controls.c
@@ -1179,8 +1179,8 @@ static const struct ldap_control_handler ldap_known_controls[] = {
 	{ DSDB_CONTROL_PASSWORD_CHANGE_OID, NULL, NULL },
 /* DSDB_CONTROL_APPLY_LINKS is internal only, and has no network representation */
 	{ DSDB_CONTROL_APPLY_LINKS, NULL, NULL },
-/* DSDB_CONTROL_BYPASS_PASSWORD_HASH_OID is internal only, and has no network representation */
-	{ DSDB_CONTROL_BYPASS_PASSWORD_HASH_OID, NULL, NULL },
+/* DSDB_CONTROL_BYPASS_PASSWORD_HASH_OID is internal only, and has an empty network representation */
+	{ DSDB_CONTROL_BYPASS_PASSWORD_HASH_OID, decode_flag_request, encode_flag_request },
 /* LDB_CONTROL_BYPASS_OPERATIONAL_OID is internal only, and has no network representation */
 	{ LDB_CONTROL_BYPASS_OPERATIONAL_OID, NULL, NULL },
 /* DSDB_CONTROL_CHANGEREPLMETADATA_OID is internal only, and has no network representation */
