4a4d8e4b0fae1288cbdf6c8a95a2863c84676106
Andrew Bartlett <abartlet@samba.org>
2010-12-15 15:47:01 +1100

    libcli/security Add sid_blob_parse() to directly parse a binary SID blob

--- libcli/security/dom_sid.h
+++ libcli/security/dom_sid.h
@@ -81,6 +81,7 @@ bool sid_split_rid(struct dom_sid *sid, uint32_t *rid);
 bool sid_peek_rid(const struct dom_sid *sid, uint32_t *rid);
 bool sid_peek_check_rid(const struct dom_sid *exp_dom_sid, const struct dom_sid *sid, uint32_t *rid);
 void sid_copy(struct dom_sid *dst, const struct dom_sid *src);
+bool sid_blob_parse(DATA_BLOB in, struct dom_sid *sid);
 bool sid_parse(const char *inbuf, size_t len, struct dom_sid *sid);
 int sid_compare_domain(const struct dom_sid *sid1, const struct dom_sid *sid2);
 bool sid_equal(const struct dom_sid *sid1, const struct dom_sid *sid2);
