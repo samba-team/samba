58206fb9b8d1a66af5a1c08379ed5f6e3413c2f0
Jelmer Vernooij <jelmer@samba.org>
2010-12-21 19:31:40 +0100

    tevent: Remove reference to nonexistant configure.developer from autogen.sh.
    Autobuild-User: Jelmer Vernooij <jelmer@samba.org>
    Autobuild-Date: Tue Dec 21 20:29:21 CET 2010 on sn-devel-104

--- lib/tevent/autogen-waf.sh
+++ lib/tevent/autogen-waf.sh
@@ -4,7 +4,7 @@ p=`dirname $0`
 
 echo "Setting up for waf build"
 
-echo "done. Now run $p/configure or $p/configure.developer then make."
+echo "done. Now run $p/configure then make."
 if [ $p != "." ]; then
 	echo "Notice: The build invoke path is not the main directory! Use make with the parameter"
 	echo "-C $p. Example: make -C $p all"
