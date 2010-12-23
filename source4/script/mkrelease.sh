1a20d597f2c792fc7d6ba846a82d904ccc7705b3
Jelmer Vernooij <jelmer@samba.org>
2010-12-24 00:35:06 +0100

    mkrelease: Simplify further.
    Autobuild-User: Jelmer Vernooij <jelmer@samba.org>
    Autobuild-Date: Fri Dec 24 02:01:10 CET 2010 on sn-devel-104

--- source4/script/mkrelease.sh
+++ source4/script/mkrelease.sh
@@ -8,19 +8,13 @@ fi
 
 cd source4
 ../buildtools/bin/waf dist
-
 TGZFILE="`echo *.tar.gz`"
 gunzip $TGZFILE
 TARFILE="`echo *.tar`"
-tar xf $TARFILE
-DIRN="`echo -n $TARFILE | sed -e 's/\.tar//'`"
-cd $DIRN/source4 && ./autogen.sh && cd ../..
-tar cf $TARFILE $DIRN
-rm -r "$DIRN"
 
 echo "Now run: "
 echo "gpg --detach-sign --armor $TARFILE"
 echo "gzip $TARFILE"
 echo "And then upload "
-echo "$TARFILE.gz $TARFILE.asc" 
+echo "$TARFILE.gz $TARFILE.asc"
 echo "to pub/samba/samba4/ on samba.org"
