05dcfcaa6d2ad4ebd691eb37dee8e72ff805c64f
Volker Lendecke <vl@samba.org>
2010-12-31 14:00:43 +0100

    s3: Add some const to send_getdc_request (cherry picked from commit ba15b7af0708ed838a0589d3c9c6ade3dfadac9c)

--- source3/libsmb/clidgram.h
+++ source3/libsmb/clidgram.h
@@ -4,7 +4,7 @@
 
 bool send_getdc_request(TALLOC_CTX *mem_ctx,
 			struct messaging_context *msg_ctx,
-			struct sockaddr_storage *dc_ss,
+			const struct sockaddr_storage *dc_ss,
 			const char *domain_name,
 			const struct dom_sid *sid,
 			uint32_t nt_version,
