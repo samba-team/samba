f9520008dfbea495636fdf591b5c998056082633
Volker Lendecke <vl@samba.org>
2011-01-02 12:49:08 +0100

    s3: Add nbt_getdc
    This is a getdc version that uses /tmp/.nmbd/unexpected

--- source3/libsmb/clidgram.h
+++ source3/libsmb/clidgram.h
@@ -15,3 +15,23 @@ bool receive_getdc_response(TALLOC_CTX *mem_ctx,
 			    uint32_t *nt_version,
 			    const char **dc_name,
 			    struct netlogon_samlogon_response **reply);
+
+struct tevent_req *nbt_getdc_send(TALLOC_CTX *mem_ctx,
+				  struct tevent_context *ev,
+				  struct messaging_context *msg_ctx,
+				  const struct sockaddr_storage *dc_addr,
+				  const char *domain_name,
+				  const struct dom_sid *sid,
+				  uint32_t nt_version);
+NTSTATUS nbt_getdc_recv(struct tevent_req *req, TALLOC_CTX *mem_ctx,
+			uint32_t *nt_version, const char **dc_name,
+			struct netlogon_samlogon_response **samlogon_response);
+NTSTATUS nbt_getdc(struct messaging_context *msg_ctx,
+		   const struct sockaddr_storage *dc_addr,
+		   const char *domain_name,
+		   const struct dom_sid *sid,
+		   uint32_t nt_version,
+		   TALLOC_CTX *mem_ctx,
+		   uint32_t *pnt_version,
+		   const char **dc_name,
+		   struct netlogon_samlogon_response **samlogon_response);
