e3bdfd2d46cb09a42eda1012287bb82af5b8ee9b
Andreas Schneider <asn@samba.org>
2011-01-05 17:16:46 +0100

    s3-smbd: Added a function to setup rpc services.
    Move the complete setup of the rpc service to its own file and use
    callbacks to register at the endpoint mapper.

---
+++ source3/rpc_server/srv_rpc_register.h
@@ -0,0 +1,29 @@
+/*
+ *  Unix SMB/CIFS implementation.
+ *
+ *  SMBD RPC service callbacks
+ *
+ *  Copyright (c) 2011      Andreas Schneider <asn@samba.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _SMBD_RPC_CALLBACKS_H
+#define _SMBD_RPC_CALLBACKS_H
+
+bool srv_rpc_register(struct messaging_context *msg_ctx);
+
+#endif /* _SMBD_RPC_CALLBACKS_H */
+
+/* vim: set ts=8 sw=8 noet cindent ft=c.doxygen: */
