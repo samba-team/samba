8c6b0b61e5a19de3b1d710e84a7a773321b8a257
Volker Lendecke <vl@samba.org>
2011-01-12 18:12:29 +0100

    s3: Fix some nonempty blank lines
    Autobuild-User: Volker Lendecke <vlendec@samba.org>
    Autobuild-Date: Wed Jan 12 19:04:25 CET 2011 on sn-devel-104

--- source3/libsmb/namequery_dc.c
+++ source3/libsmb/namequery_dc.c
@@ -6,17 +6,17 @@
    Copyright (C) Tim Potter 2001
    Copyright (C) Andrew Bartlett 2002
    Copyright (C) Gerald Carter 2003
-   
+
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -48,7 +48,7 @@ static bool is_our_primary_domain(const char *domain)
 /**************************************************************************
  Find the name and IP address for a server in the realm/domain
  *************************************************************************/
- 
+
 static bool ads_dc_name(const char *domain,
 			const char *realm,
 			struct sockaddr_storage *dc_ss,
