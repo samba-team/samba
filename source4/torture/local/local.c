c9e6bf047dc92a75d31b686d4e759743fc7386f8
Stefan Metzmacher <metze@samba.org>
2011-01-18 17:00:26 +0100

    lib/util: add tests for anonymous_shared_allocate/free()
    metze
    
    Autobuild-User: Stefan Metzmacher <metze@samba.org>
    Autobuild-Date: Thu Jan 20 06:25:03 CET 2011 on sn-devel-104

--- source4/torture/local/local.c
+++ source4/torture/local/local.c
@@ -40,6 +40,7 @@
 	torture_local_util_time, 
 	torture_local_util_data_blob, 
 	torture_local_util_asn1,
+	torture_local_util_anonymous_shared,
 	torture_local_idtree, 
 	torture_local_dlinklist,
 	torture_local_genrand, 
