dd4f80a6dc99f3f811a909f0c37940774e03ef15
Björn Baumbach <bb@sernet.de>
2011-01-20 14:08:42 +0100

    s3-masktest: Fix cli_errstr() usage (part of bug #7864)
    Convert cli_errstr() to nt_errstr()
    
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    (cherry picked from commit 5d8ada549e629cfe53ed973d66f5fcf0a3bd0a8c)

--- source3/torture/masktest.c
+++ source3/torture/masktest.c
@@ -226,11 +226,12 @@ static struct cli_state *connect_one(char *share)
 		}
 	}
 
-	if (!NT_STATUS_IS_OK(cli_session_setup(c, username, 
-					       password, strlen(password),
-					       password, strlen(password),
-					       lp_workgroup()))) {
-		DEBUG(0,("session setup failed: %s\n", cli_errstr(c)));
+	status = cli_session_setup(c, username,
+				   password, strlen(password),
+				   password, strlen(password),
+				   lp_workgroup());
+	if (!NT_STATUS_IS_OK(status)) {
+		DEBUG(0, ("session setup failed: %s\n", nt_errstr(status)));
 		return NULL;
 	}
 
