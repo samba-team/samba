a152f20dd10288ce51ac987af8fb87ab30eb5654
Stefan Metzmacher <metze@samba.org>
2011-02-01 17:37:13 +0100

    s3:libnet/libnet_samsync_display: fix netr_DELTA_* display
    metze

--- source3/libnet/libnet_samsync_display.c
+++ source3/libnet/libnet_samsync_display.c
@@ -206,29 +206,32 @@ static NTSTATUS display_sam_entry(TALLOC_CTX *mem_ctx,
 		display_alias_mem(id.rid, u.alias_member);
 		break;
 	case NETR_DELTA_POLICY:
-		printf("Policy\n");
+		printf("Policy: %s\n",
+			sid_string_dbg(id.sid));
 		break;
 	case NETR_DELTA_TRUSTED_DOMAIN:
 		printf("Trusted Domain: %s\n",
 			u.trusted_domain->domain_name.string);
 		break;
 	case NETR_DELTA_DELETE_TRUST:
-		printf("Delete Trust: %d\n",
-			u.delete_trust.unknown);
+		printf("Delete Trust: %s\n",
+			sid_string_dbg(id.sid));
 		break;
 	case NETR_DELTA_ACCOUNT:
-		printf("Account\n");
+		printf("Account: %s\n",
+			sid_string_dbg(id.sid));
 		break;
 	case NETR_DELTA_DELETE_ACCOUNT:
-		printf("Delete Account: %d\n",
-			u.delete_account.unknown);
+		printf("Delete Account: %s\n",
+			sid_string_dbg(id.sid));
 		break;
 	case NETR_DELTA_SECRET:
-		printf("Secret\n");
+		printf("Secret: %s\n",
+			id.name);
 		break;
 	case NETR_DELTA_DELETE_SECRET:
-		printf("Delete Secret: %d\n",
-			u.delete_secret.unknown);
+		printf("Delete Secret: %s\n",
+			id.name);
 		break;
 	case NETR_DELTA_DELETE_GROUP2:
 		printf("Delete Group2: %s\n",
