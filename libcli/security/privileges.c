618c11cc15b0c7b16cd21e9bc10dbe474aa514ff
Matthias Dieter Wallnöfer <mdw@samba.org>
2011-02-16 09:12:17 +0100

    libcli/security/privileges.c - fix wrong counter type
    This strictly needs to be from type "uint32_t" since "privset->count"
    is defined with this type.

--- libcli/security/privileges.c
+++ libcli/security/privileges.c
@@ -296,7 +296,7 @@ bool se_priv_to_privilege_set( PRIVILEGE_SET *set, uint64_t privilege_mask )
 
 bool privilege_set_to_se_priv( uint64_t *privilege_mask, struct lsa_PrivilegeSet *privset )
 {
-	int i;
+	uint32_t i;
 
 	ZERO_STRUCTP( privilege_mask );
 
