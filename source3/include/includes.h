28b4b059386b4369d6ad85c82bbc150aa81609c4
Günther Deschner <gd@samba.org>
2011-02-22 19:45:24 +0100

    s3-includes: move some chgpasswd related defines to the locations where they are used.
    Guenther

--- source3/include/includes.h
+++ source3/include/includes.h
@@ -738,18 +738,6 @@ enum flush_reason_enum {
 #define OSF1_ENH_SEC 1
 #endif
 
-#ifndef ALLOW_CHANGE_PASSWORD
-#if (defined(HAVE_TERMIOS_H) && defined(HAVE_DUP2) && defined(HAVE_SETSID))
-#define ALLOW_CHANGE_PASSWORD 1
-#endif
-#endif
-
-/* what is the longest significant password available on your system? 
- Knowing this speeds up password searches a lot */
-#ifndef PASSWORD_LENGTH
-#define PASSWORD_LENGTH 8
-#endif
-
 #if defined(HAVE_CRYPT16) && defined(HAVE_GETAUTHUID)
 #define ULTRIX_AUTH 1
 #endif
