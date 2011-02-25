55f9579110f3c7706d8008714ae69b49f0b463de
Günther Deschner <gd@samba.org>
2011-02-25 16:48:10 +0100

    s3-includes: only include system/terminal.h when needed.
    Guenther

--- source3/rpc_server/samr/srv_samr_chgpasswd.c
+++ source3/rpc_server/samr/srv_samr_chgpasswd.c
@@ -46,6 +46,7 @@
    */
 
 #include "includes.h"
+#include "system/terminal.h"
 #include "../libcli/auth/libcli_auth.h"
 #include "../lib/crypto/arcfour.h"
 #include "rpc_server/samr/srv_samr_util.h"
