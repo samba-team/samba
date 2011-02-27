d2e976e32c552ba44596973628e3061f61a267b5
Matthieu Patou <mat@matws.net>
2011-02-27 12:16:43 +0300

    upgradeprovision: Fix typo

--- source4/scripting/python/samba/upgradehelpers.py
+++ source4/scripting/python/samba/upgradehelpers.py
@@ -21,7 +21,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Helers used for upgrading between different database formats."""
+"""Helpers used for upgrading between different database formats."""
 
 import os
 import string
