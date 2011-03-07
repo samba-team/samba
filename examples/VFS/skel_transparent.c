3d1069f1ae6e1665dddb75f90b4a1f941fb663d2
Jeremy Allison <jra@samba.org>
2011-03-07 03:53:11 -0800

    Fix examples/VFS to use correct prototypes for is_offline() and set_offline(). (cherry picked from commit 0faffd4556438f7add6312e58acca345abfbe7fb)

--- examples/VFS/skel_transparent.c
+++ examples/VFS/skel_transparent.c
@@ -706,14 +706,14 @@ static bool skel_aio_force(struct vfs_handle_struct *handle, struct files_struct
         return SMB_VFS_NEXT_AIO_FORCE(handle, fsp);
 }
 
-static bool skel_is_offline(struct vfs_handle_struct *handle, const char *path, SMB_STRUCT_STAT *sbuf)
+static bool skel_is_offline(struct vfs_handle_struct *handle, const struct smb_filename *fname, SMB_STRUCT_STAT *sbuf)
 {
-	return SMB_VFS_NEXT_IS_OFFLINE(handle, path, sbuf);
+	return SMB_VFS_NEXT_IS_OFFLINE(handle, fname, sbuf);
 }
 
-static int skel_set_offline(struct vfs_handle_struct *handle, const char *path)
+static int skel_set_offline(struct vfs_handle_struct *handle, const struct smb_filename *fname)
 {
-	return SMB_VFS_NEXT_SET_OFFLINE(handle, path);
+	return SMB_VFS_NEXT_SET_OFFLINE(handle, fname);
 }
 
 /* VFS operations structure */
