a4d7b3a1dfddc95c1825588cddaee5b4039fa5e2
Volker Lendecke <vl@samba.org>
2011-03-14 08:09:21 +0100

    s3: "hex_encode" does the same as "binary_string"

--- source3/lib/util_sid.c
+++ source3/lib/util_sid.c
@@ -121,7 +121,7 @@ char *sid_binstring_hex(const struct dom_sid *sid)
 	if (!buf)
 		return NULL;
 	sid_linearize(buf, len, sid);
-	s = binary_string(buf, len);
+	hex_encode(buf, len, &s);
 	free(buf);
 	return s;
 }
