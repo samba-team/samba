dca75c8e3ac23256d93269bdc13d1e67571bc9f2
Stefan Metzmacher <metze@samba.org>
2011-03-18 16:45:08 +0100

    s3:smbd: access checks should not depend on share mode flags
    metze
    (cherry picked from commit f0ec69b53544b7ff702f94d58b3d64c33eaabc7a)

--- source3/smbd/open.c
+++ source3/smbd/open.c
@@ -3140,7 +3140,6 @@ static NTSTATUS create_file_unixpath(connection_struct *conn,
 
 	if (lp_acl_check_permissions(SNUM(conn))
 	    && (create_disposition != FILE_CREATE)
-	    && (share_access & FILE_SHARE_DELETE)
 	    && (access_mask & DELETE_ACCESS)
 	    && (!(can_delete_file_in_directory(conn, smb_fname) ||
 		 can_access_file_acl(conn, smb_fname, DELETE_ACCESS)))) {
