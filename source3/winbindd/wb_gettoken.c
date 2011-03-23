e6d76ba832d87e59a50a5c044ecba5a6dc611958
Volker Lendecke <vl@samba.org>
2011-03-23 13:11:26 +0100

    s3: Fix Coverity ID 2203, NULL_RETURNS

--- source3/winbindd/wb_gettoken.c
+++ source3/winbindd/wb_gettoken.c
@@ -139,6 +139,10 @@ static void wb_gettoken_gotlocalgroups(struct tevent_req *subreq)
 		return;
 	}
 	domain = find_domain_from_sid_noinit(get_global_sam_sid());
+	if (domain == NULL) {
+		tevent_req_nterror(req, NT_STATUS_INTERNAL_ERROR);
+		return;
+	}
 	if (!wb_add_rids_to_sids(state, &state->num_sids, &state->sids,
 				 &domain->sid, num_rids, rids)) {
 		tevent_req_nterror(req, NT_STATUS_NO_MEMORY);
