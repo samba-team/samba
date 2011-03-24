4a4efc20b43df25ea69239383069e1ce98ed5901
Günther Deschner <gd@samba.org>
2011-03-24 22:29:18 +0100

    s3-rpc_server: remove two uneeded include registry/reg_parse_prs.h
    Guenther

--- source3/rpc_server/winreg/srv_winreg_nt.c
+++ source3/rpc_server/winreg/srv_winreg_nt.c
@@ -22,7 +22,6 @@
 
 #include "includes.h"
 #include "../librpc/gen_ndr/srv_winreg.h"
-#include "registry/reg_parse_prs.h"
 #include "registry.h"
 #include "registry/reg_api.h"
 #include "registry/reg_api_regf.h"
