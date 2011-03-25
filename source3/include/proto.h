de2848f45d5e5e39e7500bfaf65555115d6b3b1b
Günther Deschner <gd@samba.org>
2011-03-25 11:15:55 +0100

    s3: finally remove global include of smbd prototypes.
    Guenther
    (cherry picked from commit f064c8e4f23b7d427b6e0cb11a6d7ec04964e773)

--- source3/include/proto.h
+++ source3/include/proto.h
@@ -3637,7 +3637,4 @@ void contend_level2_oplocks_begin(files_struct *fsp,
 void contend_level2_oplocks_end(files_struct *fsp,
 				enum level2_contention_type type);
 
-/* FIXME */
-#include "smbd/proto.h"
-
 #endif /*  _PROTO_H_  */
