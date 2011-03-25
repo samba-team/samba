26253e04604d2ac6c747a87433a87476f834ac4f
Volker Lendecke <vl@samba.org>
2011-03-25 10:12:52 +0100

    s3: Compile IDL files in autogen, some configure tests need this
    Autobuild-User: Volker Lendecke <vlendec@samba.org>
    Autobuild-Date: Fri Mar 25 16:35:30 CET 2011 on sn-devel-104

--- source3/autogen.sh
+++ source3/autogen.sh
@@ -75,6 +75,17 @@ $AUTOCONF $IPATHS || exit 1
 
 rm -rf autom4te*.cache
 
+if gcc -E tests/preproc-dummy.c -o /dev/null ;
+then
+    PIDL_OUTPUTDIR="librpc/gen_ndr" CPP="gcc -E" PIDL=../pidl/pidl \
+	srcdir=. ../librpc/build_idl.sh ../librpc/idl/*.idl
+    PIDL_OUTPUTDIR="librpc/gen_ndr" CPP="gcc -E" PIDL=../pidl/pidl \
+	srcdir=. script/build_idl.sh librpc/idl/*.idl
+else
+   echo "Warning: Could not compile idl files in autogen, "
+   echo "some autconf tests might not work properly"
+fi
+
 echo "Now run ./configure (or ./configure.developer) and then make."
 exit 0
 
