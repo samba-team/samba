8742074eb23eea3001fbff894afa84a22373e048
Michael Adam <obnox@samba.org>
2011-04-02 00:29:51 +0200

    s3:waf:compare_config_h3.sh: specify autoconf-config.h as commandline parameter

--- buildtools/compare_config_h3.sh
+++ buildtools/compare_config_h3.sh
@@ -3,8 +3,13 @@
 # compare the generated config.h from a waf build with existing samba
 # build
 
+OLD_CONFIG=$HOME/samba_old/source3/include/config.h
+if test "x$1" != "x" ; then
+	OLD_CONFIG=$1
+fi
+
 grep "^.define" bin/default/source3/include/config.h | sort > waf-config.h
-grep "^.define" $HOME/samba_old/source3/include/config.h | sort > old-config.h
+grep "^.define" $OLD_CONFIG | sort > old-config.h
 
 comm -23 old-config.h waf-config.h
 
