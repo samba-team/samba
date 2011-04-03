304d472448a5c6b16c1746adc58d713411efef62
Björn Jacke <bj@sernet.de>
2011-04-03 16:19:11 +0200

    s3/vfs_gpfs: s/syncops/gpfs
    as pointed out by Metze in bug #8031
    
    cherry-picked from dca465fa53f4d16cdce1353685b11010aa8ff0c7
    
    The last two patches address bug #8031 - merge patc to make
    sharemodes/leases parameter a per share setting.
    (cherry picked from commit fbf1a26be29f3d78d09e5f4285c973db9e16327f)

--- source3/modules/vfs_gpfs.c
+++ source3/modules/vfs_gpfs.c
@@ -1164,7 +1164,7 @@ int vfs_gpfs_connect(struct vfs_handle_struct *handle, const char *service,
 					"leases", true);
 
 	SMB_VFS_HANDLE_SET_DATA(handle, config,
-				NULL, struct syncops_config_data,
+				NULL, struct gpfs_config_data,
 				return -1);
 
 	return 0;
