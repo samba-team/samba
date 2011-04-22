9a0676d5c947033ac41f700be9ba92ab1666814e
Volker Lendecke <vl@samba.org>
2011-04-22 09:34:35 +0200

    s3: Fix Coverity ID 986, BUFFER_SIZE_WARNING
    strncpy can leave the destination unterminated

--- source3/rpc_client/cli_pipe.c
+++ source3/rpc_client/cli_pipe.c
@@ -2691,7 +2691,7 @@ NTSTATUS rpc_pipe_open_ncalrpc(TALLOC_CTX *mem_ctx, const char *socket_path,
 
 	ZERO_STRUCT(addr);
 	addr.sun_family = AF_UNIX;
-	strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path));
+	strlcpy(addr.sun_path, socket_path, sizeof(addr.sun_path));
 
 	if (sys_connect(fd, (struct sockaddr *)(void *)&addr) == -1) {
 		DEBUG(0, ("connect(%s) failed: %s\n", socket_path,
