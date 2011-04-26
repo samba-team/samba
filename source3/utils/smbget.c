dfe0029521f67163bdea3f82ccd90d91f82d74e6
Günther Deschner <gd@samba.org>
2011-04-26 14:58:01 +0200

    s3-utils: Fix Bug #8106. Fix the build of smbget on HP NonStop.
    Guenther
    
    Autobuild-User: Günther Deschner <gd@samba.org>
    Autobuild-Date: Tue Apr 26 16:41:17 CEST 2011 on sn-devel-104
    (cherry picked from commit 633dbabe912f43fd4199453720fab299ccfefe03)

--- source3/utils/smbget.c
+++ source3/utils/smbget.c
@@ -17,6 +17,7 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
 #include "includes.h"
+#include "system/filesys.h"
 #include "popt_common.h"
 #include "libsmbclient.h"
 
