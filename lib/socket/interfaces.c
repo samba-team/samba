f346a737855bb5018978f0fcf1dcafbf5dc7e603
Andrew Bartlett <abartlet@samba.org>
2011-05-02 13:02:17 +1000

    lib/socket move interfaces code to the top level

--- lib/socket/interfaces.c
+++ lib/socket/interfaces.c
@@ -1,20 +1,20 @@
-/* 
+/*
    Unix SMB/CIFS implementation.
    return a list of network interfaces
    Copyright (C) Andrew Tridgell 1998
    Copyright (C) Jeremy Allison 2007
    Copyright (C) Jelmer Vernooij 2007
-   
+
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -33,7 +33,7 @@
 
 #include "includes.h"
 #include "system/network.h"
-#include "netif.h"
+#include "interfaces.h"
 #include "lib/util/tsort.h"
 
 /****************************************************************************
