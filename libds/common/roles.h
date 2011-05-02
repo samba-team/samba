b1055763d8b046b42580054e4bc15dc8cb414977
Andrew Tridgell <tridge@samba.org>
2011-05-02 12:04:32 +1000

    libds: added roles.h for server roles
    this will allow us to move the server roles in common, which will
    make it much easier to mix s3/s4 calls in the one C file
    
    Pair-Programmed-With: Andrew Bartlett <abartlet@samba.org>

---
+++ libds/common/roles.h
@@ -0,0 +1,42 @@
+/*
+   Unix SMB/CIFS implementation.
+
+   domain roles
+
+   Copyright (C) Andrew Tridgell 2011
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _LIBDS_ROLES_H_
+#define _LIBDS_ROLES_H_
+
+/* server roles. If you add new roles, please keep ensure that the
+ * existing role values match samr_Role from samr.idl
+ */
+enum server_role {
+	ROLE_STANDALONE    = 0,
+	ROLE_DOMAIN_MEMBER = 1,
+	ROLE_DOMAIN_BDC    = 2,
+	ROLE_DOMAIN_PDC    = 3
+};
+
+/* keep compatibility with the s4 'ROLE_DOMAIN_CONTROLLER' by mapping
+ * it to ROLE_DOMAIN_BDC. The PDC/BDC split is really historical from
+ * NT4 domains which were not multi-master, but even in AD there is
+ * only one machine that has the PDC FSMO role in a domain.
+*/
+#define ROLE_DOMAIN_CONTROLLER ROLE_DOMAIN_BDC
+
+#endif /* _LIBDS_ROLES_H_ */
