c3bb5b04e2b6535655dc03c870e02bd5b78a7dee
Andrew Bartlett <abartlet@samba.org>
2011-05-08 06:56:47 +0200

    s3-utils Set dyn_CONFIGFILE from -c on smbpasswd command line
    This is similar to the code in popt_common and allows the smb.conf to
    be re-loaded from this file later in the code (or for Samba4 plugins
    to attempt to parse the same smb.conf).
    
    Andrew Bartlett

--- source3/utils/smbpasswd.c
+++ source3/utils/smbpasswd.c
@@ -107,6 +107,7 @@ static int process_options(int argc, char **argv, int local_flags)
 			break;
 		case 'c':
 			configfile = optarg;
+			set_dyn_CONFIGFILE(optarg);
 			break;
 		case 'a':
 			local_flags |= LOCAL_ADD_USER;
