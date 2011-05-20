207a84d725b905c2b119d2ef0f4f4d4eb391140d
Andreas Schneider <asn@samba.org>
2011-05-20 15:42:46 +0200

    s3-winbind: Fix paranoia checks in winbindd_samr.c.
    This fixes looking up the correct unix user instead of allocation a new
    uid and creating it.
    
    Fix bug #8215 (winbind unix username lookup doesn't work correctly).
    (cherry picked from commit 531edfdd1924bfb2ef486820f7f5787098bd953a)
    
    Autobuild-User: Andreas Schneider <asn@cryptomilk.org>
    Autobuild-Date: Wed Jun 15 09:56:01 CEST 2011 on sn-devel-104

--- source3/winbindd/winbindd_samr.c
+++ source3/winbindd/winbindd_samr.c
@@ -762,8 +762,8 @@ static NTSTATUS sam_rids_to_names(struct winbindd_domain *domain,
 	ZERO_STRUCT(lsa_policy);
 
 	/* Paranoia check */
-	if (!sid_check_is_builtin(domain_sid) &&
-	    !sid_check_is_domain(domain_sid) &&
+	if (!sid_check_is_in_builtin(domain_sid) &&
+	    !sid_check_is_in_our_domain(domain_sid) &&
 	    !sid_check_is_unix_users(domain_sid) &&
 	    !sid_check_is_unix_groups(domain_sid) &&
 	    !sid_check_is_in_wellknown_domain(domain_sid)) {
