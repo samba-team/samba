df650fa8cf4954245eced7eccb26388c24acee82
Christian Ambach <ambi@samba.org>
2011-05-20 14:17:36 +0200

    s3:smbd remove unused code
    in the early CTDB days, the RELEASE_IP message was defined
    and some code was added to react on such a message to make
    smbd exit if the IP address it was using for the server socket
    is removed by CTDB.
    Later, it was discovered that we need to stop smbd immediately
    and logic was added to ctdb_conn to call release_ip() without
    going through the messaging system.
    
    So this code is not used and can be removed
    
    Autobuild-User: Christian Ambach <ambi@samba.org>
    Autobuild-Date: Fri May 20 16:18:24 CEST 2011 on sn-devel-104

--- source3/smbd/process.c
+++ source3/smbd/process.c
@@ -2375,15 +2375,6 @@ static void release_ip(const char *ip, void *priv)
 	}
 }
 
-static void msg_release_ip(struct messaging_context *msg_ctx, void *private_data,
-			   uint32_t msg_type, struct server_id server_id, DATA_BLOB *data)
-{
-	struct smbd_server_connection *sconn = talloc_get_type_abort(
-		private_data, struct smbd_server_connection);
-
-	release_ip((char *)data->data, sconn->client_id.addr);
-}
-
 #ifdef CLUSTER_SUPPORT
 static int client_get_tcp_info(int sock, struct sockaddr_storage *server,
 			       struct sockaddr_storage *client)
@@ -3040,8 +3031,6 @@ void smbd_process(struct smbd_server_connection *sconn)
 	/* register our message handlers */
 	messaging_register(sconn->msg_ctx, NULL,
 			   MSG_SMB_FORCE_TDIS, msg_force_tdis);
-	messaging_register(sconn->msg_ctx, sconn,
-			   MSG_SMB_RELEASE_IP, msg_release_ip);
 	messaging_register(sconn->msg_ctx, NULL,
 			   MSG_SMB_CLOSE_FILE, msg_close_file);
 
