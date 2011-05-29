c7c0e4ca71277a02bb9371aaf47ac1157f7ca542
Volker Lendecke <vl@samba.org>
2011-05-29 21:00:12 +0200

    s3: Remove unused cli_session_request
    Autobuild-User: Volker Lendecke <vlendec@samba.org>
    Autobuild-Date: Sun May 29 22:25:55 CEST 2011 on sn-devel-104

--- source3/libsmb/proto.h
+++ source3/libsmb/proto.h
@@ -68,8 +68,6 @@ struct tevent_req *cli_negprot_send(TALLOC_CTX *mem_ctx,
 				    struct event_context *ev,
 				    struct cli_state *cli);
 NTSTATUS cli_negprot_recv(struct tevent_req *req);
-bool cli_session_request(struct cli_state *cli,
-			 struct nmb_name *calling, struct nmb_name *called);
 NTSTATUS cli_connect_nb(const char *host, struct sockaddr_storage *pss,
 			uint16_t port, int name_type, const char *myname,
 			int signing_state, struct cli_state **pcli);
