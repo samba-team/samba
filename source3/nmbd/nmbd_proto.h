ad939a02cb746b6e2e5cf95f8360db4dbc2107eb
Michael Adam <obnox@samba.org>
2011-05-31 00:29:17 +0200

    s3:nmbd/nmbd_proto.h: add GPL/Copyright header (cherry picked from commit d8f269feefc3891c01788d5c74cf0954702c6022) (cherry picked from commit 5c7a01f246a19faeb3db5eb88543b11f04ee07c0)

--- source3/nmbd/nmbd_proto.h
+++ source3/nmbd/nmbd_proto.h
@@ -1,3 +1,28 @@
+/*
+ *  Unix SMB/CIFS implementation.
+ *  NBT netbios routines and daemon - version 2
+ *
+ *  Copyright (C) Andrew Tridgell			1994-1998
+ *  Copyright (C) Jeremy Allison			1994-2005
+ *  Copyright (C) Luke Kenneth Casson Leighton		1994-1998
+ *  Copyright (C) John H Terpstra			1995-1998
+ *  Copyright (C) Christopher R. Hertel			1998
+ *  Copyright (C) Jim McDonough <jmcd@us.ibm.com>	2002
+ *  Copyright (C) Jelmer Vernooij			2002,2003
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /* The following definitions come from nmbd/asyncdns.c  */
 
