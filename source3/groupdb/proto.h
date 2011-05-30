7182ce8b116152027fc58018f642e0fec72a454a
Michael Adam <obnox@samba.org>
2011-05-31 00:18:26 +0200

    s3:groupdb/proto.h: add GPL/Copyright header (cherry picked from commit f2cbced75e33c5467672b8eb5c9bc2fc82a2f7da)

--- source3/groupdb/proto.h
+++ source3/groupdb/proto.h
@@ -1,3 +1,25 @@
+/*
+ *  Unix SMB/CIFS implementation.
+ *  Group Mapping Database
+ *
+ *  Copyright (C) Andrew Tridgell              1992-2006
+ *  Copyright (C) Jean François Micouleau      1998-2001
+ *  Copyright (C) Gerald Carter                2006
+ *  Copyright (C) Volker Lendecke              2006
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
 
 /* The following definitions come from groupdb/mapping.c  */
 
