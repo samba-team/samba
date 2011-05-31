f22a39c8fc2f1501e8cc613765d0676cb22ecf51
Michael Adam <obnox@samba.org>
2011-05-31 13:30:34 +0200

    s3:nmbd: add _NMBD_NMBD_H_ guard to nmbd.h (cherry picked from commit fdd225c3e41ed5a81ef02d39ac037818bde49c03) (cherry picked from commit 7aeb6d8ffd19d58d5a0bcc7c1c700641a60d2066)

--- source3/nmbd/nmbd.h
+++ source3/nmbd/nmbd.h
@@ -18,9 +18,14 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#ifndef _NMBD_NMBD_H_
+#define _NMBD_NMBD_H_
+
 #ifndef HAVE_PIPE
 #define SYNC_DNS 1
 #endif
 
 #include "libsmb/nmblib.h"
 #include "nmbd/nmbd_proto.h"
+
+#endif /* _NMBD_NMBD_H_ */
