9549cf125a2e54a6bcaf4251b1f363775f8e7929
Michael Adam <obnox@samba.org>
2011-05-31 12:06:00 +0200

    s3:librpc: remove unused file librpc/ndr/util.h
    Autobuild-User: Michael Adam <obnox@samba.org>
    Autobuild-Date: Tue May 31 15:19:46 CEST 2011 on sn-devel-104

--- source3/librpc/ndr/util.h
+++
@@ -1,4 +0,0 @@
-
-/* The following definitions come from librpc/ndr/util.c  */
-
-_PUBLIC_ void ndr_print_sockaddr_storage(struct ndr_print *ndr, const char *name, const struct sockaddr_storage *ss);
