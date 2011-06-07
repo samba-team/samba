cd8dc47bf17d2cdb1558dc6ab49320ba12af8f34
Michael Adam <obnox@samba.org>
2011-06-07 15:53:49 +0200

    s3:idmap_autorid: fail initialization if the domain is not "*"
    autorid can only be used as a backend for the default idmap configuration.
    
    Autobuild-User: Michael Adam <obnox@samba.org>
    Autobuild-Date: Tue Jun  7 19:13:18 CEST 2011 on sn-devel-104

--- source3/winbindd/idmap_autorid.c
+++ source3/winbindd/idmap_autorid.c
@@ -438,6 +438,14 @@ static NTSTATUS idmap_autorid_initialize(struct idmap_domain *dom)
 	TALLOC_CTX *frame = talloc_stackframe();
 	char *config_option = NULL;
 
+	if (!strequal(dom->name, "*")) {
+		DEBUG(0, ("idmap_autorid_initialize: Error: autorid configured "
+			  "for domain '%s'. But autorid can only be used for "
+			  "the default idmap configuration.\n", dom->name));
+		status = NT_STATUS_INVALID_PARAMETER;
+		goto error;
+	}
+
 	config = TALLOC_ZERO_P(frame, struct autorid_global_config);
 	if (!config) {
 		DEBUG(0, ("Out of memory!\n"));
