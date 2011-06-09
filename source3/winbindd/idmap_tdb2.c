7f56b6269792300237193b69541f73fb32a986b3
Michael Adam <obnox@samba.org>
2011-06-09 13:07:54 +0200

    s3:idmap_tdb2: fix a legacy comment that does not apply any more (cherry picked from commit 7d3dc2164b3929c642127659593e69fc865a6184)

--- source3/winbindd/idmap_tdb2.c
+++ source3/winbindd/idmap_tdb2.c
@@ -110,8 +110,7 @@ static NTSTATUS idmap_tdb2_open_db(struct idmap_domain *dom)
 
 	db_path = lp_parm_talloc_string(-1, "tdb", "idmap2.tdb", NULL);
 	if (db_path == NULL) {
-		/* fall back to the private directory, which, despite
-		   its name, is usually on shared storage */
+		/* fall back to the private directory */
 		db_path = talloc_asprintf(NULL, "%s/idmap2.tdb", lp_private_dir());
 	}
 	NT_STATUS_HAVE_NO_MEMORY(db_path);
