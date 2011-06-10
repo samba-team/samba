e7df23090b0088a69edd25c95456a4a739585868
Günther Deschner <gd@samba.org>
2011-06-10 15:02:16 +0200

    librpc/ndr/ndr_dns.h: fix licence/copyright
    Guenther
    (cherry picked from commit 4dadee3adf4deb212f14c0a574dc0e91baa6589a)

--- librpc/ndr/ndr_dns.h
+++ librpc/ndr/ndr_dns.h
@@ -1,3 +1,28 @@
+/*
+   Unix SMB/CIFS implementation.
+
+   manipulate dns name structures
+
+   Copyright (C) 2010 Kai Blin  <kai@samba.org>
+
+   Heavily based on nbtname.c which is:
+
+   Copyright (C) Andrew Tridgell 2005
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 void ndr_print_dns_string(struct ndr_print *ndr,
 			  const char *name,
 			  const char *s);
