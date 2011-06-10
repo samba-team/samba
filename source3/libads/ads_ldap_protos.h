972d2ceaf98e622abf6c27d749c5d29a20c4f66c
Günther Deschner <gd@samba.org>
2011-06-10 15:05:09 +0200

    source3/libads/ads_ldap_protos.h: fix licence/copyright
    Guenther
    (cherry picked from commit f2d4252dfa790b65031fe3221afe89d0e285ebd7)
    (cherry picked from commit b589dcb6fc7249694d76c36b2988fe42ec9f6e85)

--- source3/libads/ads_ldap_protos.h
+++ source3/libads/ads_ldap_protos.h
@@ -1,4 +1,30 @@
 /*
+   Unix SMB/CIFS implementation.
+   ads (active directory) utility library
+   Copyright (C) Andrew Tridgell 2001
+   Copyright (C) Remus Koos 2001
+   Copyright (C) Jim McDonough <jmcd@us.ibm.com> 2002
+   Copyright (C) Guenther Deschner 2005
+   Copyright (C) Gerald Carter 2006
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _LIBADS_ADS_LDAP_PROTOS_H_
+#define _LIBADS_ADS_LDAP_PROTOS_H_
+
+/*
  * Prototypes for ads
  */
 
@@ -112,3 +138,5 @@ ADS_STATUS ads_get_joinable_ous(ADS_STRUCT *ads,
 				TALLOC_CTX *mem_ctx,
 				char ***ous,
 				size_t *num_ous);
+
+#endif /* _LIBADS_ADS_LDAP_PROTOS_H_ */
