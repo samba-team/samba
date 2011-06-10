806c54c159b95aa9ece40f953f64b9705defe69a
Günther Deschner <gd@samba.org>
2011-06-10 15:07:27 +0200

    source3/rpc_client/cli_spoolss.h: fix licence/copyright
    Guenther
    (cherry picked from commit b95b1813b9ab53b9490d8b37af4f6ced7fea7bdb)
    (cherry picked from commit e7026828be5943a846ee2d1ce396016acc7463ba)

--- source3/rpc_client/cli_spoolss.h
+++ source3/rpc_client/cli_spoolss.h
@@ -1,3 +1,30 @@
+/*
+   Unix SMB/CIFS implementation.
+   RPC pipe client
+
+   Copyright (C) Gerald Carter                2001-2005,
+   Copyright (C) Tim Potter                   2000-2002,
+   Copyright (C) Andrew Tridgell              1994-2000,
+   Copyright (C) Jean-Francois Micouleau      1999-2000.
+   Copyright (C) Jeremy Allison                         2005.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _RPC_CLIENT_CLI_SPOOLSS_H_
+#define _RPC_CLIENT_CLI_SPOOLSS_H_
+
 /* The following definitions come from rpc_client/cli_spoolss.c  */
 
 WERROR rpccli_spoolss_openprinter_ex(struct rpc_pipe_client *cli,
@@ -122,3 +149,5 @@ WERROR rpccli_spoolss_enumprinterdataex(struct rpc_pipe_client *cli,
 					uint32_t offered,
 					uint32_t *count,
 					struct spoolss_PrinterEnumValues **info);
+
+#endif /* _RPC_CLIENT_CLI_SPOOLSS_H_ */
