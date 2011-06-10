1bcd966ed1316ae94db05a0de0370670dd30ad41
Günther Deschner <gd@samba.org>
2011-06-10 15:05:37 +0200

    source3/libads/ldap_schema.h: fix licence/copyright
    Guenther
    (cherry picked from commit 183835d05515b5b437bbeff5ff7685a4f08415ad)

--- source3/libads/ldap_schema.h
+++ source3/libads/ldap_schema.h
@@ -1,3 +1,26 @@
+/*
+   Unix SMB/CIFS implementation.
+   ads (active directory) utility library
+   Copyright (C) Guenther Deschner 2005-2007
+   Copyright (C) Gerald (Jerry) Carter 2006
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _LIBADS_LDAP_SCHEMA_H_
+#define _LIBADS_LDAP_SCHEMA_H_
+
 /* used to remember the names of the posix attributes in AD */
 /* see the rfc2307 & sfu nss backends */
 
@@ -55,3 +78,5 @@ ADS_STATUS ads_check_posix_schema_mapping(TALLOC_CTX *mem_ctx,
 					  ADS_STRUCT *ads,
 					  enum wb_posix_mapping map_type,
 					  struct posix_schema **s ) ;
+
+#endif /* _LIBADS_LDAP_SCHEMA_H_ */
