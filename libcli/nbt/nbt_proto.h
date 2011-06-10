edb338aaa43c19e31d90fac5d891bb8da2ac6c37
Günther Deschner <gd@samba.org>
2011-06-10 15:01:03 +0200

    libcli/nbt/nbt_proto.h: fix licence/copyright
    Guenther
    (cherry picked from commit d41519f99e784f934e025f6d982e9ed681a4adc7)
    (cherry picked from commit 4802e605a89612ffc183fca8dd86405aa2ed22c3)

--- libcli/nbt/nbt_proto.h
+++ libcli/nbt/nbt_proto.h
@@ -1,3 +1,24 @@
+/*
+   Unix SMB/CIFS implementation.
+
+   manipulate nbt name structures
+
+   Copyright (C) Andrew Tridgell 2005
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef _____LIBCLI_NBT_NBT_PROTO_H__
 #define _____LIBCLI_NBT_NBT_PROTO_H__
 
