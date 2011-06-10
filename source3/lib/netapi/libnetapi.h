387ddc2c295e92363c83dcd29353cdd4c56984ab
Günther Deschner <gd@samba.org>
2011-06-10 15:04:48 +0200

    source3/lib/netapi/libnetapi.h: fix licence/copyright
    Guenther

--- source3/lib/netapi/libnetapi.h
+++ source3/lib/netapi/libnetapi.h
@@ -1,3 +1,22 @@
+/*
+ *  Unix SMB/CIFS implementation.
+ *  NetApi Support
+ *  Copyright (C) Guenther Deschner 2007-2008
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef __LIBNETAPI_LIBNETAPI__
 #define __LIBNETAPI_LIBNETAPI__
 NET_API_STATUS NetJoinDomain(const char * server /* [in] [unique] */,
