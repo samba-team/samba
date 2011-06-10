e21b603935e0b9a92bcda8d6fb79c9afd8b8e418
Günther Deschner <gd@samba.org>
2011-06-10 15:08:07 +0200

    source3/rpc_client/init_spoolss.h: fix licence/copyright
    Guenther
    (cherry picked from commit 4942aeb9b562f3621cbe6adc8e79366887f0d4b9)

--- source3/rpc_client/init_spoolss.h
+++ source3/rpc_client/init_spoolss.h
@@ -1,3 +1,24 @@
+/*
+ *  Unix SMB/CIFS implementation.
+ *  RPC Pipe client / server routines
+ *  Copyright (C) Guenther Deschner                  2009.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _RPC_CLIENT_INIT_SPOOLSS_H_
+#define _RPC_CLIENT_INIT_SPOOLSS_H_
 
 /* The following definitions come from rpc_client/init_spoolss.c  */
 
@@ -14,3 +35,4 @@ WERROR push_spoolss_PrinterData(TALLOC_CTX *mem_ctx, DATA_BLOB *blob,
 void spoolss_printerinfo2_to_setprinterinfo2(const struct spoolss_PrinterInfo2 *i,
 					     struct spoolss_SetPrinterInfo2 *s);
 
+#endif /* _RPC_CLIENT_INIT_SPOOLSS_H_ */
