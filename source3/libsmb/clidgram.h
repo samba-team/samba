13fd0465d9a463003e26d7c1e7fa015dfb88fc4d
Günther Deschner <gd@samba.org>
2011-06-10 15:05:57 +0200

    source3/libsmb/clidgram.h: fix licence/copyright
    Guenther
    (cherry picked from commit 1e9f2bdb2f7f9e809dd3044dc94018e0b5b6e8d3)

--- source3/libsmb/clidgram.h
+++ source3/libsmb/clidgram.h
@@ -1,3 +1,27 @@
+/*
+   Unix SMB/CIFS implementation.
+   client dgram calls
+   Copyright (C) Andrew Tridgell 1994-1998
+   Copyright (C) Richard Sharpe 2001
+   Copyright (C) John Terpstra 2001
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _LIBSMB_CLIDGRAM_H_
+#define _LIBSMB_CLIDGRAM_H_
+
 #include "../libcli/netlogon/netlogon.h"
 
 /* The following definitions come from libsmb/clidgram.c  */
@@ -21,3 +45,5 @@ NTSTATUS nbt_getdc(struct messaging_context *msg_ctx,
 		   uint32_t *pnt_version,
 		   const char **dc_name,
 		   struct netlogon_samlogon_response **samlogon_response);
+
+#endif /* _LIBSMB_CLIDGRAM_H_ */
