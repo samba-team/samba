59b7746d45b54bb7104f02b22ebb7f4951271b42
Günther Deschner <gd@samba.org>
2011-06-10 15:05:18 +0200

    source3/libads/ads_status.h: fix licence/copyright
    Guenther
    (cherry picked from commit d5c5aa1c5fcc0e3c74cf1216cce7a8c767a6df2a)

--- source3/libads/ads_status.h
+++ source3/libads/ads_status.h
@@ -1,3 +1,25 @@
+/*
+   Unix SMB/CIFS implementation.
+   ads (active directory) utility library
+   Copyright (C) Andrew Tridgell 2001
+   Copyright (C) Remus Koos 2001
+   Copyright (C) Andrew Bartlett 2001
+
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef _LIBADS_ADS_STATUS_H_
 #define _LIBADS_ADS_STATUS_H_
 
