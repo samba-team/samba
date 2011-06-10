3ec16489ecff6cf343c0eb99640e3048fd204c44
Günther Deschner <gd@samba.org>
2011-06-10 15:04:26 +0200

    source3/include/smb_ldap.h: fix licence/copyright
    Guenther
    (cherry picked from commit e14b60c7ee6a76c444f12b8987740602c7b5f1d3)
    (cherry picked from commit 5abff6867f8cc9856c81deffe7dbf38ee4e277eb)

--- source3/include/smb_ldap.h
+++ source3/include/smb_ldap.h
@@ -1,3 +1,25 @@
+/*
+   Unix SMB/CIFS implementation.
+   Copyright (C) Andrew Tridgell 2001
+   Copyright (C) Remus Koos 2001
+   Copyright (C) Jim McDonough <jmcd@us.ibm.com> 2002
+   Copyright (C) Guenther Deschner 2005
+   Copyright (C) Gerald Carter 2006
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef _SMB_LDAP_H
 #define _SMB_LDAP_H
 
