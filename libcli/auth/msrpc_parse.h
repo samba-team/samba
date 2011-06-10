f04b673aba26404fbf2c19eb57262fd1d9f3fd9d
Günther Deschner <gd@samba.org>
2011-06-10 15:00:32 +0200

    libcli/auth/msrpc_parse.h: fix licence/copyright
    Guenther

--- libcli/auth/msrpc_parse.h
+++ libcli/auth/msrpc_parse.h
@@ -1,3 +1,24 @@
+/*
+   Unix SMB/CIFS implementation.
+   simple kerberos5/SPNEGO routines
+   Copyright (C) Andrew Tridgell 2001
+   Copyright (C) Jim McDonough <jmcd@us.ibm.com> 2002
+   Copyright (C) Andrew Bartlett 2002-2003
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef _LIBCLI_AUTH_MSRPC_PARSE_H__
 #define _LIBCLI_AUTH_MSRPC_PARSE_H__
 
