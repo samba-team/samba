61bf43f5d17c9ac3fc9a36cab74a81f26ac97ccb
Rusty Russell <rusty@rustcorp.com.au>
2011-06-20 16:58:15 +0930

    lib/tdb_compat: header for tdb1 vs tdb2.
    TDB2's API is slightly different from TDB1.  In particular, all functions
    return 0 (TDB_SUCCESS) or a negative error number, rather than -1 or tdb_null
    and storing the error in tdb_error() (though TDB2 does that as well).
    
    The simplest fix is to replace all the different functions with a wrapper,
    and that is done here.
    
    Compatibility functions:
    
    tdb_null: not used as an error return, so not defined by tdb2.
    tdb_fetch_compat: TDB1-style data-returning tdb_fetch.
    tdb_firstkey_compat: TDB1-style data-returning tdb_firstkey
    tdb_nextkey_compat: TDB1-style data-returning tdb_nextkey, with
    	TDB2-style free of old key.
    tdb_errorstr_compat: TDB1-style tdb_errorstr() which takes TDB instead of ecode.
    TDB_CONTEXT: TDB1-style typedef for struct tdb_context.
    tdb_open_compat: Simplified open routine which takes log function, sets
    	TDB_ALLOW_NESTING as Samba expects, and adds TDB_CLEAR_IF_FIRST support.
    
    Things defined away in TDB2 wrappers:
    
    tdb_traverse_read: TDB2's tdb_traverse only uses read-locks anyway.
    tdb_reopen/tdb_reopen_all: TDB2 detects this error itself.
    TDB_INCOMPATIBLE_HASH: TDB2 uses the Jenkins hash already.
    TDB_VOLATILE: TDB2 shouldn't have freelist scaling issues.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

---
+++ lib/tdb_compat/tdb_compat.h
@@ -0,0 +1,136 @@
+/*
+   Unix SMB/CIFS implementation.
+
+   Compatibility layer for TDB1 vs TDB2.
+
+   Copyright (C) Rusty Russell 2011
+
+     ** NOTE! The following LGPL license applies to the tdb_compat
+     ** library. This does NOT imply that all of Samba is released
+     ** under the LGPL
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef TDB_COMPAT_H
+#define TDB_COMPAT_H
+
+#include "replace.h"
+#include <ccan/typesafe_cb/typesafe_cb.h>
+#if BUILD_TDB2
+#include <tdb2.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+extern TDB_DATA tdb_null;
+
+/* Old-style tdb_fetch. */
+static inline TDB_DATA tdb_fetch_compat(struct tdb_context *tdb, TDB_DATA k)
+{
+	TDB_DATA dbuf;
+	if (tdb_fetch(tdb, k, &dbuf) != TDB_SUCCESS) {
+		return tdb_null;
+	}
+	return dbuf;
+}
+
+static inline TDB_DATA tdb_firstkey_compat(struct tdb_context *tdb)
+{
+	TDB_DATA k;
+	if (tdb_firstkey(tdb, &k) != TDB_SUCCESS) {
+		return tdb_null;
+	}
+	return k;
+}
+
+/* Note: this frees the old key.dptr. */
+static inline TDB_DATA tdb_nextkey_compat(struct tdb_context *tdb, TDB_DATA k)
+{
+	if (tdb_nextkey(tdb, &k) != TDB_SUCCESS) {
+		return tdb_null;
+	}
+	return k;
+}
+
+/* tdb_traverse_read and tdb_traverse are equal: both only take read locks. */
+#define tdb_traverse_read tdb_traverse
+
+/* Old-style tdb_errorstr */
+#define tdb_errorstr_compat(tdb) tdb_errorstr(tdb_error(tdb))
+
+/* This typedef doesn't exist in TDB2. */
+typedef struct tdb_context TDB_CONTEXT;
+
+/* We don't need these any more. */
+#define tdb_reopen_all(flag) 0
+#define tdb_reopen(tdb) 0
+
+/* These no longer exist in tdb2. */
+#define TDB_CLEAR_IF_FIRST 1048576
+#define TDB_INCOMPATIBLE_HASH 0
+#define TDB_VOLATILE 0
+
+/* tdb2 does nonblocking functions via attibutes. */
+enum TDB_ERROR tdb_transaction_start_nonblock(struct tdb_context *tdb);
+
+/* Convenient (typesafe) wrapper for tdb open with logging */
+#define tdb_open_compat(name, hsize, tdb_fl, open_fl, mode, log_fn, log_data) \
+	tdb_open_compat_((name), (hsize), (tdb_fl), (open_fl), (mode),	\
+			 typesafe_cb_preargs(void, void *,		\
+					     (log_fn), (log_data),	\
+					     struct tdb_context *,	\
+					     enum tdb_log_level,	\
+					     const char *),		\
+			 (log_data))
+
+struct tdb_context *
+tdb_open_compat_(const char *name, int hash_size_unused,
+		 int tdb_flags, int open_flags, mode_t mode,
+		 void (*log_fn)(struct tdb_context *,
+				enum tdb_log_level,
+				const char *message,
+				void *data),
+		 void *log_data);
+#else
+#include <tdb.h>
+
+/* FIXME: Inlining this is a bit lazy, but eases S3 build. */
+static inline struct tdb_context *
+tdb_open_compat(const char *name, int hash_size,
+		int tdb_flags, int open_flags, mode_t mode,
+		tdb_log_func log_fn, void *log_private)
+{
+	struct tdb_logging_context lctx;
+	lctx.log_fn = log_fn;
+	lctx.log_private = log_private;
+
+	if (log_fn)
+		return tdb_open_ex(name, hash_size, tdb_flags, open_flags,
+				   mode, &lctx, NULL);
+	else
+		return tdb_open(name, hash_size, tdb_flags, open_flags, mode);
+}
+
+#define tdb_firstkey_compat tdb_firstkey
+/* Note: this frees the old key.dptr. */
+static inline TDB_DATA tdb_nextkey_compat(struct tdb_context *tdb, TDB_DATA k)
+{
+	TDB_DATA next = tdb_nextkey(tdb, k);
+	free(k.dptr);
+	return next;
+}
+#define tdb_errorstr_compat(tdb) tdb_errorstr(tdb)
+#define tdb_fetch_compat tdb_fetch
+#endif
+
+#endif /* TDB_COMPAT_H */
