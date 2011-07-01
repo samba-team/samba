b1aca06b1823601a5942ead53ed66ca95bd3b8c4
Günther Deschner <gd@samba.org>
2011-07-01 15:39:11 +0200

    s3-printing: open up a winreg pipe handle for the migration code.
    Guenther
    
    Autobuild-User: Günther Deschner <gd@samba.org>
    Autobuild-Date: Mon Jul  4 15:06:41 CEST 2011 on sn-devel-104
    (cherry picked from commit ff94539f276bdef73bd12c67bff1f34e697ccbfc)

--- source3/printing/nt_printing_migrate.h
+++ source3/printing/nt_printing_migrate.h
@@ -23,21 +23,25 @@
 
 NTSTATUS printing_tdb_migrate_form(TALLOC_CTX *mem_ctx,
 				   struct rpc_pipe_client *pipe_hnd,
+				   struct rpc_pipe_client *winreg_pipe,
 				   const char *key_name,
 				   unsigned char *data,
 				   size_t length);
 NTSTATUS printing_tdb_migrate_driver(TALLOC_CTX *mem_ctx,
 				     struct rpc_pipe_client *pipe_hnd,
+				     struct rpc_pipe_client *winreg_pipe,
 				     const char *key_name,
 				     unsigned char *data,
 				     size_t length);
 NTSTATUS printing_tdb_migrate_printer(TALLOC_CTX *mem_ctx,
 				      struct rpc_pipe_client *pipe_hnd,
+				      struct rpc_pipe_client *winreg_pipe,
 				      const char *key_name,
 				      unsigned char *data,
 				      size_t length);
 NTSTATUS printing_tdb_migrate_secdesc(TALLOC_CTX *mem_ctx,
 				      struct rpc_pipe_client *pipe_hnd,
+				      struct rpc_pipe_client *winreg_pipe,
 				      const char *key_name,
 				      unsigned char *data,
 				      size_t length);
