78f267df699a053a532a69cd4187917d6213deb1
Björn Baumbach <bb@sernet.de>
2011-07-22 16:51:10 +0200

    s3-client/client.c: replace cli_query_secdesc_old()
    Signed-off-by: Stefan Metzmacher <metze@samba.org>

--- source3/client/client.c
+++ source3/client/client.c
@@ -586,12 +586,12 @@ static NTSTATUS display_finfo(struct cli_state *cli_state, struct file_info *fin
 				   afname, nt_errstr(status)));
 		} else {
 			struct security_descriptor *sd = NULL;
-			sd = cli_query_secdesc_old(cli_state, fnum, ctx);
-			if (!sd) {
-				status = cli_nt_error(cli_state);
+			status = cli_query_secdesc(cli_state, fnum,
+						   ctx, &sd);
+			if (!NT_STATUS_IS_OK(status)) {
 				DEBUG( 0, ("display_finfo() failed to "
-					"get security descriptor: %s",
-					nt_errstr(status)));
+					   "get security descriptor: %s",
+					   nt_errstr(status)));
 			} else {
 				display_sec_desc(sd);
 			}
