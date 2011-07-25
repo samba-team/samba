8e2ee72bcd16d1fe3d2ac472ca3f7ce8cb264e75
Vicentiu Ciorbaru <cvicentiu@gmail.com>
2011-07-25 18:20:01 +0300

    s3-net: Added net rpc conf getparm command to net rpc conf
    The function only appears in the commands list usage of net rpc conf.
    
    Signed-off-by: Michael Adam <obnox@samba.org>

--- source3/utils/net_rpc_conf.c
+++ source3/utils/net_rpc_conf.c
@@ -894,6 +894,12 @@ static int rpc_conf_delshare(struct net_context *c, int argc,
 		rpc_conf_delshare_internal, argc, argv );
 }
 
+static int rpc_conf_getparm(struct net_context *c, int argc,
+			     const char **argv)
+{
+
+	return 0;
+}
 /* function calls */
 int net_rpc_conf(struct net_context *c, int argc,
 		 const char **argv)
@@ -944,6 +950,14 @@ int net_rpc_conf(struct net_context *c, int argc,
 			N_("Delete a remote share."),
 			N_("net rpc conf delshare\n"
 			   "    Delete a remote share.")
+		},
+		{
+			"getparm",
+			rpc_conf_getparm,
+			NET_TRANSPORT_RPC,
+			N_("Retrieve the value of a parameter."),
+			N_("net rpc conf getparm\n"
+			   "    Retrieve the value of a parameter.")
 		},
 		{NULL, NULL, 0, NULL, NULL}
 	};
