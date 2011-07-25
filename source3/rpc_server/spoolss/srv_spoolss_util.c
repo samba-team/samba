72ac68e7467f9a28389f9487b93096bb2e7c1622
Andreas Schneider <asn@samba.org>
2011-07-25 21:15:36 +0200

    s3-spoolss: Use tmp_ctx in winreg_get_driver_list_internal.

--- source3/rpc_server/spoolss/srv_spoolss_util.c
+++ source3/rpc_server/spoolss/srv_spoolss_util.c
@@ -429,15 +429,28 @@ WERROR winreg_get_driver_list_internal(TALLOC_CTX *mem_ctx,
 {
 	WERROR result;
 	struct dcerpc_binding_handle *b;
+	TALLOC_CTX *tmp_ctx;
 
-	result = winreg_printer_binding_handle(mem_ctx, session_info, msg_ctx, &b);
-	W_ERROR_NOT_OK_RETURN(result);
+	tmp_ctx = talloc_stackframe();
+	if (tmp_ctx == NULL) {
+		return WERR_NOMEM;
+	}
 
-	return winreg_get_driver_list(mem_ctx, b,
-				      architecture,
-				      version,
-				      num_drivers,
-				      drivers_p);
+	result = winreg_printer_binding_handle(tmp_ctx, session_info, msg_ctx, &b);
+	if (!W_ERROR_IS_OK(result)) {
+		talloc_free(tmp_ctx);
+		return result;
+	}
+
+	result = winreg_get_driver_list(mem_ctx,
+					b,
+					architecture,
+					version,
+					num_drivers,
+					drivers_p);
+
+	talloc_free(tmp_ctx);
+	return result;
 }
 
 WERROR winreg_del_driver_internal(TALLOC_CTX *mem_ctx,
