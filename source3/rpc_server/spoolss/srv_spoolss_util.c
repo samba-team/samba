81eae6ebd4746e4c14fe86e7a77ce78a172d9234
Andreas Schneider <asn@samba.org>
2011-07-25 21:35:24 +0200

    s3-spoolss: Use tmp_ctx in winreg_set_printer_secdesc_internal.

--- source3/rpc_server/spoolss/srv_spoolss_util.c
+++ source3/rpc_server/spoolss/srv_spoolss_util.c
@@ -553,13 +553,26 @@ WERROR winreg_set_printer_secdesc_internal(TALLOC_CTX *mem_ctx,
 {
 	WERROR result;
 	struct dcerpc_binding_handle *b;
+	TALLOC_CTX *tmp_ctx;
 
-	result = winreg_printer_binding_handle(mem_ctx, session_info, msg_ctx, &b);
-	W_ERROR_NOT_OK_RETURN(result);
+	tmp_ctx = talloc_stackframe();
+	if (tmp_ctx == NULL) {
+		return WERR_NOMEM;
+	}
 
-	return winreg_set_printer_secdesc(mem_ctx, b,
-					  sharename,
-					  secdesc);
+	result = winreg_printer_binding_handle(tmp_ctx, session_info, msg_ctx, &b);
+	if (!W_ERROR_IS_OK(result)) {
+		talloc_free(tmp_ctx);
+		return result;
+	}
+
+	result = winreg_set_printer_secdesc(mem_ctx,
+					    b,
+					    sharename,
+					    secdesc);
+
+	talloc_free(tmp_ctx);
+	return result;
 }
 
 WERROR winreg_printer_enumforms1_internal(TALLOC_CTX *mem_ctx,
