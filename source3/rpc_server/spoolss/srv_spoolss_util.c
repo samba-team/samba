f49917ade0c4dda99d7188e8556b35a76f3f39b0
Andreas Schneider <asn@samba.org>
2011-07-25 21:36:34 +0200

    s3-spoolss: Use tmp_ctx in winreg_printer_enumforms1_internal.

--- source3/rpc_server/spoolss/srv_spoolss_util.c
+++ source3/rpc_server/spoolss/srv_spoolss_util.c
@@ -583,13 +583,26 @@ WERROR winreg_printer_enumforms1_internal(TALLOC_CTX *mem_ctx,
 {
 	WERROR result;
 	struct dcerpc_binding_handle *b;
+	TALLOC_CTX *tmp_ctx;
 
-	result = winreg_printer_binding_handle(mem_ctx, session_info, msg_ctx, &b);
-	W_ERROR_NOT_OK_RETURN(result);
+	tmp_ctx = talloc_stackframe();
+	if (tmp_ctx == NULL) {
+		return WERR_NOMEM;
+	}
 
-	return winreg_printer_enumforms1(mem_ctx, b,
-					 pnum_info,
-					 pinfo);
+	result = winreg_printer_binding_handle(tmp_ctx, session_info, msg_ctx, &b);
+	if (!W_ERROR_IS_OK(result)) {
+		talloc_free(tmp_ctx);
+		return result;
+	}
+
+	result = winreg_printer_enumforms1(mem_ctx,
+					   b,
+					   pnum_info,
+					   pinfo);
+
+	talloc_free(tmp_ctx);
+	return result;
 }
 
 WERROR winreg_printer_getform1_internal(TALLOC_CTX *mem_ctx,
