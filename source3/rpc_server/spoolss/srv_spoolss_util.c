f9359a2dc9cc94bf52d568825a79995732299759
Andreas Schneider <asn@samba.org>
2011-07-25 21:07:21 +0200

    s3-spoolss: Use tmp_ctx in winreg_get_printer_internal.

--- source3/rpc_server/spoolss/srv_spoolss_util.c
+++ source3/rpc_server/spoolss/srv_spoolss_util.c
@@ -157,14 +157,26 @@ WERROR winreg_get_printer_internal(TALLOC_CTX *mem_ctx,
 {
 	WERROR result;
 	struct dcerpc_binding_handle *b;
+	TALLOC_CTX *tmp_ctx;
 
-	result = winreg_printer_binding_handle(mem_ctx, session_info, msg_ctx, &b);
-	W_ERROR_NOT_OK_RETURN(result);
+	tmp_ctx = talloc_stackframe();
+	if (tmp_ctx == NULL) {
+		return WERR_NOMEM;
+	}
 
-	return winreg_get_printer(mem_ctx, b,
-				  printer,
-				  pinfo2);
+	result = winreg_printer_binding_handle(tmp_ctx, session_info, msg_ctx, &b);
+	if (!W_ERROR_IS_OK(result)) {
+		talloc_free(tmp_ctx);
+		return result;
+	}
 
+	result = winreg_get_printer(mem_ctx,
+				    b,
+				    printer,
+				    pinfo2);
+
+	talloc_free(tmp_ctx);
+	return result;
 }
 
 WERROR winreg_create_printer_internal(TALLOC_CTX *mem_ctx,
