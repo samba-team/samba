9f98ec85d9142651331745a284511a6b9eafa7f9
Martin Schwenke <martin@meltin.net>
2011-08-11 15:32:28 +1000

    Revert "Tests: tweak some samba tests to cope with debug from ctdb_check_tcp_ports()."
    This reverts commit 557ac30e60516742da10b83bfbbbb41430c977a2.
    
    (This used to be ctdb commit 9600cc7a6b7b854fac1a5b080129e3df8fcbd84e)

--- ctdb/tests/eventscripts/simple/50.samba.monitor.104.sh
+++ ctdb/tests/eventscripts/simple/50.samba.monitor.104.sh
@@ -7,12 +7,6 @@ define_test "port 139 down"
 setup_samba
 tcp_port_down 139
 
-required_result 1 <<EOF
-ERROR: samba tcp port 139 is not responding
-netstat -l -t -n shows this output:
-Active Internet connections (servers only)
-Proto Recv-Q Send-Q Local Address           Foreign Address         State
-tcp        0      0 0.0.0.0:445             0.0.0.0:*               LISTEN
-EOF
+required_result 1 "ERROR: samba tcp port 139 is not responding"
 
 simple_test
