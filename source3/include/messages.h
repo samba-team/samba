samba-team/samba

commit ff0ac5b0e7f9163760b2863f48389e4943ac2154

r23112: Trim down the message.c API slightly: The messages_pending_for_pid is now replaced by MSG_FLAG_LOWPRIORITY or'ed into the msg_type. To enable this, changed the msg_type definitions to hexadecimal.
This way we could theoretically add the MSG_FLAG_NODUPLICATES again, but I
would rather not do this, because that one is racy and can't be guaranteed at
all.
(This used to be commit 3f5eb8a9600839a9f9c44c553f0bda6df22b30b0)

source3/include/messages.h
@@ -22,79 +22,93 @@
 #ifndef _MESSAGES_H_
 #define _MESSAGES_H_
 
+#define MSG_TYPE_MASK			0xFFFF
+
 /* general messages */
-#define MSG_DEBUG		1
-#define MSG_PING		2
-#define MSG_PONG		3
-#define MSG_PROFILE		4
-#define MSG_REQ_DEBUGLEVEL	5
-#define MSG_DEBUGLEVEL		6
-#define MSG_REQ_PROFILELEVEL	7
-#define MSG_PROFILELEVEL	8
-#define MSG_REQ_POOL_USAGE	9
-#define MSG_POOL_USAGE		10
+#define MSG_DEBUG			0x0001
+#define MSG_PING			0x0002
+#define MSG_PONG			0x0003
+#define MSG_PROFILE			0x0004
+#define MSG_REQ_DEBUGLEVEL		0x0005
+#define MSG_DEBUGLEVEL			0x0006
+#define MSG_REQ_PROFILELEVEL		0x0007
+#define MSG_PROFILELEVEL		0x0008
+#define MSG_REQ_POOL_USAGE		0x0009
+#define MSG_POOL_USAGE			0x000A
 
 /* If dmalloc is included, set a steady-state mark */
-#define MSG_REQ_DMALLOC_MARK	11
+#define MSG_REQ_DMALLOC_MARK		0x000B
 
 /* If dmalloc is included, dump to the dmalloc log a description of
  * what has changed since the last MARK */
-#define MSG_REQ_DMALLOC_LOG_CHANGED	12
+#define MSG_REQ_DMALLOC_LOG_CHANGED	0x000C
 
-#define MSG_SHUTDOWN		13
+#define MSG_SHUTDOWN			0x000D
 
 /* nmbd messages */
-#define MSG_FORCE_ELECTION 1001
-#define MSG_WINS_NEW_ENTRY 1002
-#define MSG_SEND_PACKET    1003
+#define MSG_FORCE_ELECTION		0x0101
+#define MSG_WINS_NEW_ENTRY		0x0102
+#define MSG_SEND_PACKET			0x0103
 
 /* printing messages */
 /* #define MSG_PRINTER_NOTIFY  2001*/ /* Obsolete */
-#define MSG_PRINTER_NOTIFY2		2002
+#define MSG_PRINTER_NOTIFY2		0x0202
 
-#define MSG_PRINTER_DRVUPGRADE		2101
-#define MSG_PRINTERDATA_INIT_RESET	2102
-#define MSG_PRINTER_UPDATE		2103
-#define MSG_PRINTER_MOD			2104
+#define MSG_PRINTER_DRVUPGRADE		0x0203
+#define MSG_PRINTERDATA_INIT_RESET	0x0204
+#define MSG_PRINTER_UPDATE		0x0205
+#define MSG_PRINTER_MOD			0x0206
 
 /* smbd messages */
-#define MSG_SMB_CONF_UPDATED 3001
-#define MSG_SMB_FORCE_TDIS   3002
-#define MSG_SMB_SAM_SYNC     3003
-#define MSG_SMB_SAM_REPL     3004
-#define MSG_SMB_UNLOCK       3005
-#define MSG_SMB_BREAK_REQUEST 3006
-#define MSG_SMB_BREAK_RESPONSE 3007
-#define MSG_SMB_ASYNC_LEVEL2_BREAK 3008
-#define MSG_SMB_OPEN_RETRY   3009
-#define MSG_SMB_KERNEL_BREAK 3010
-#define MSG_SMB_FILE_RENAME  3011
-#define MSG_SMB_INJECT_FAULT 3012
-#define MSG_SMB_BLOCKING_LOCK_CANCEL 3013
-#define MSG_SMB_NOTIFY       3014
-#define MSG_SMB_STAT_CACHE_DELETE 3015
+#define MSG_SMB_CONF_UPDATED		0x0301
+#define MSG_SMB_FORCE_TDIS		0x0302
+#define MSG_SMB_SAM_SYNC		0x0303
+#define MSG_SMB_SAM_REPL		0x0304
+#define MSG_SMB_UNLOCK			0x0305
+#define MSG_SMB_BREAK_REQUEST		0x0306
+#define MSG_SMB_BREAK_RESPONSE		0x0307
+#define MSG_SMB_ASYNC_LEVEL2_BREAK	0x0308
+#define MSG_SMB_OPEN_RETRY		0x0309
+#define MSG_SMB_KERNEL_BREAK		0x030A
+#define MSG_SMB_FILE_RENAME		0x030B
+#define MSG_SMB_INJECT_FAULT		0x030C
+#define MSG_SMB_BLOCKING_LOCK_CANCEL	0x030D
+#define MSG_SMB_NOTIFY			0x030E
+#define MSG_SMB_STAT_CACHE_DELETE	0x030F
 /*
  * Samba4 compatibility
  */
-#define MSG_PVFS_NOTIFY       3016
+#define MSG_PVFS_NOTIFY			0x0310
 
 /* winbind messages */
-#define MSG_WINBIND_FINISHED     4001
-#define MSG_WINBIND_FORGET_STATE 4002
-#define MSG_WINBIND_ONLINE       4003
-#define MSG_WINBIND_OFFLINE      4004
-#define MSG_WINBIND_ONLINESTATUS 4005
-#define MSG_WINBIND_TRY_TO_GO_ONLINE 4006
-#define MSG_WINBIND_FAILED_TO_GO_ONLINE 4007
+#define MSG_WINBIND_FINISHED		0x0401
+#define MSG_WINBIND_FORGET_STATE	0x0402
+#define MSG_WINBIND_ONLINE		0x0403
+#define MSG_WINBIND_OFFLINE		0x0404
+#define MSG_WINBIND_ONLINESTATUS	0x0405
+#define MSG_WINBIND_TRY_TO_GO_ONLINE	0x0406
+#define MSG_WINBIND_FAILED_TO_GO_ONLINE 0x0407
+
+/*
+ * Special flags passed to message_send. Allocated from the top, lets see when
+ * it collides with the message types in the lower 16 bits :-)
+ */
+
+/*
+ * Under high load, this message can be dropped. Use for notify-style
+ * messages that are not critical for correct operation.
+ */
+#define MSG_FLAG_LOWPRIORITY		0x80000000
+
 
 /* Flags to classify messages - used in message_send_all() */
 /* Sender will filter by flag. */
 
-#define FLAG_MSG_GENERAL 	0x0001
-#define FLAG_MSG_SMBD		0x0002
-#define FLAG_MSG_NMBD		0x0004
-#define FLAG_MSG_PRINT_NOTIFY	0x0008
-#define FLAG_MSG_PRINT_GENERAL	0x0010
+#define FLAG_MSG_GENERAL		0x0001
+#define FLAG_MSG_SMBD			0x0002
+#define FLAG_MSG_NMBD			0x0004
+#define FLAG_MSG_PRINT_NOTIFY		0x0008
+#define FLAG_MSG_PRINT_GENERAL		0x0010
 
 struct server_id {
 	pid_t pid;
@@ -103,8 +117,6 @@ struct server_id {
 struct messaging_context;
 struct data_blob;
 
-unsigned int messages_pending_for_pid(struct messaging_context *msg_ctx,
-				      struct server_id pid);
 void message_dispatch(struct messaging_context *msg_ctx);
 BOOL message_send_all(struct messaging_context *msg_ctx,
 		      int msg_type,

source3/lib/messages.c
@@ -70,6 +70,8 @@ struct messaging_context {
 	struct server_id id;
 	struct event_context *event_ctx;
 	struct messaging_callback *callbacks;
+
+	
 };
 
 /****************************************************************************
@@ -103,7 +105,7 @@ static void ping_message(struct messaging_context *msg_ctx,
  Initialise the messaging functions. 
 ****************************************************************************/
 
-static BOOL message_init(struct messaging_context *msg_ctx)
+static BOOL message_tdb_init(struct messaging_context *msg_ctx)
 {
 	sec_init();
 
@@ -120,14 +122,6 @@ static BOOL message_init(struct messaging_context *msg_ctx)
 	tdb_set_max_dead(msg_ctx->tdb, 5);
 
 	CatchSignal(SIGUSR1, SIGNAL_CAST sig_usr1);
-
-	messaging_register(msg_ctx, NULL, MSG_PING, ping_message);
-
-	/* Register some debugging related messages */
-
-	register_msg_pool_usage(msg_ctx);
-	register_dmalloc_msgs(msg_ctx);
-	debug_register_msgs(msg_ctx);
 
 	return True;
 }
@@ -300,9 +294,9 @@ static NTSTATUS message_notify(struct server_id procid)
  Send a message to a particular pid.
 ****************************************************************************/
 
-static NTSTATUS messaging_tdb_send(TDB_CONTEXT *msg_tdb,
+static NTSTATUS messaging_tdb_send(struct messaging_context *msg_ctx,
 				   struct server_id pid, int msg_type,
-				   const void *buf, size_t len)
+				   const DATA_BLOB *data)
 {
 	struct messaging_array *msg_array;
 	struct messaging_rec *rec;
@@ -311,8 +305,8 @@ static NTSTATUS messaging_tdb_send(TDB_CONTEXT *msg_tdb,
 	TDB_DATA key = message_key_pid(pid);
 
 	/* NULL pointer means implicit length zero. */
-	if (!buf) {
-		SMB_ASSERT(len == 0);
+	if (!data->data) {
+		SMB_ASSERT(data->length == 0);
 	}
 
 	/*
@@ -326,42 +320,45 @@ static NTSTATUS messaging_tdb_send(TDB_CONTEXT *msg_tdb,
 		return NT_STATUS_NO_MEMORY;
 	}
 
-	if (tdb_chainlock(msg_tdb, key) == -1) {
+	if (tdb_chainlock(msg_ctx->tdb, key) == -1) {
+		TALLOC_FREE(mem_ctx);
 		return NT_STATUS_LOCK_NOT_GRANTED;
 	}
 
-	status = messaging_tdb_fetch(msg_tdb, key, mem_ctx, &msg_array);
+	status = messaging_tdb_fetch(msg_ctx->tdb, key, mem_ctx, &msg_array);
 
 	if (!NT_STATUS_IS_OK(status)) {
-		tdb_chainunlock(msg_tdb, key);
-		TALLOC_FREE(mem_ctx);
-		return status;
+		goto done;
+	}
+
+	if ((msg_type & MSG_FLAG_LOWPRIORITY)
+	    && (msg_array->num_messages > 1000)) {
+		DEBUG(5, ("Dropping message for PID %s\n",
+			  procid_str_static(&pid)));
+		status = NT_STATUS_INSUFFICIENT_RESOURCES;
+		goto done;
 	}
 
 	if (!(rec = TALLOC_REALLOC_ARRAY(mem_ctx, msg_array->messages,
 					 struct messaging_rec,
 					 msg_array->num_messages+1))) {
-		tdb_chainunlock(msg_tdb, key);
-		TALLOC_FREE(mem_ctx);
-		return NT_STATUS_NO_MEMORY;
+		status = NT_STATUS_NO_MEMORY;
+		goto done;
 	}
 
 	rec[msg_array->num_messages].msg_version = MESSAGE_VERSION;
-	rec[msg_array->num_messages].msg_type = msg_type;
+	rec[msg_array->num_messages].msg_type = msg_type & MSG_TYPE_MASK;
 	rec[msg_array->num_messages].dest = pid;
 	rec[msg_array->num_messages].src = procid_self();
-	rec[msg_array->num_messages].buf = data_blob_const(buf, len);
+	rec[msg_array->num_messages].buf = *data;
 
 	msg_array->messages = rec;
 	msg_array->num_messages += 1;
 
-	status = messaging_tdb_store(msg_tdb, key, msg_array);
-
-	tdb_chainunlock(msg_tdb, key);
-	TALLOC_FREE(mem_ctx);
+	status = messaging_tdb_store(msg_ctx->tdb, key, msg_array);
 
 	if (!NT_STATUS_IS_OK(status)) {
-		return status;
+		goto done;
 	}
 	
 	status = message_notify(pid);
@@ -369,33 +366,14 @@ static NTSTATUS messaging_tdb_send(TDB_CONTEXT *msg_tdb,
 	if (NT_STATUS_EQUAL(status, NT_STATUS_INVALID_HANDLE)) {
 		DEBUG(2, ("pid %s doesn't exist - deleting messages record\n",
 			  procid_str_static(&pid)));
-		tdb_delete(msg_tdb, message_key_pid(pid));
+		tdb_delete(msg_ctx->tdb, message_key_pid(pid));
 	}
 
+ done:
+	tdb_chainunlock(msg_ctx->tdb, key);
+	TALLOC_FREE(mem_ctx);
 	return status;
 }
-
-/****************************************************************************
- Count the messages pending for a particular pid. Expensive....
-****************************************************************************/
-
-unsigned int messages_pending_for_pid(struct messaging_context *msg_ctx,
-				      struct server_id pid)
-{
-	struct messaging_array *msg_array;
-	unsigned int result;
-
-	if (!NT_STATUS_IS_OK(messaging_tdb_fetch(msg_ctx->tdb,
-						 message_key_pid(pid), NULL,
-						 &msg_array))) {
-		DEBUG(10, ("messaging_tdb_fetch failed\n"));
-		return 0;
-	}
-
-	result = msg_array->num_messages;
-	TALLOC_FREE(msg_array);
-	return result;
-}	
 
 /****************************************************************************
  Retrieve all messages for the current process.
@@ -619,10 +597,18 @@ struct messaging_context *messaging_init(TALLOC_CTX *mem_ctx,
 	ctx->id = server_id;
 	ctx->event_ctx = ev;
 
-	if (!message_init(ctx)) {
+	if (!message_tdb_init(ctx)) {
 		DEBUG(0, ("message_init failed: %s\n", strerror(errno)));
 		TALLOC_FREE(ctx);
 	}
+
+	messaging_register(ctx, NULL, MSG_PING, ping_message);
+
+	/* Register some debugging related messages */
+
+	register_msg_pool_usage(ctx);
+	register_dmalloc_msgs(ctx);
+	debug_register_msgs(ctx);
 
 	return ctx;
 }
@@ -688,11 +674,10 @@ void messaging_deregister(struct messaging_context *ctx, uint32_t msg_type,
   Send a message to a particular server
 */
 NTSTATUS messaging_send(struct messaging_context *msg_ctx,
-			struct server_id server, 
-			uint32_t msg_type, const DATA_BLOB *data)
+			struct server_id server, uint32_t msg_type,
+			const DATA_BLOB *data)
 {
-	return messaging_tdb_send(msg_ctx->tdb, server, msg_type,
-				  data->data, data->length);
+	return messaging_tdb_send(msg_ctx, server, msg_type, data);
 }
 
 NTSTATUS messaging_send_buf(struct messaging_context *msg_ctx,

source3/Makefile.in
@@ -984,7 +984,7 @@ cac: SHOWFLAGS $(LIBMSRPC)
 ## Perl IDL Compiler
 IDL_FILES = unixinfo.idl lsa.idl dfs.idl echo.idl winreg.idl initshutdown.idl \
 	srvsvc.idl svcctl.idl eventlog.idl wkssvc.idl netlogon.idl notify.idl \
-	epmapper.idl
+	epmapper.idl messaging.idl
 
 idl:
 	@IDL_FILES="$(IDL_FILES)" CPP="$(CPP)" PERL="$(PERL)" \

source3/printing/notify.c
@@ -183,16 +183,9 @@ static void print_notify_send_messages_to_printer(struct messaging_context *msg_
 	}
 
 	for (i = 0; i < num_pids; i++) {
-		unsigned int q_len = messages_pending_for_pid(
-			msg_ctx, pid_to_procid(pid_list[i]));
-		if (q_len > 1000) {
-			DEBUG(5, ("print_notify_send_messages_to_printer: discarding notify to printer %s as queue length = %u\n",
-				printer, q_len ));
-			continue;
-		}
 		messaging_send_buf(msg_ctx,
 				   pid_to_procid(pid_list[i]),
-				   MSG_PRINTER_NOTIFY2,
+				   MSG_PRINTER_NOTIFY2 | MSG_FLAG_LOWPRIORITY,
 				   (uint8 *)buf, offset);
 
 		if ((timeout != 0) && timeval_expired(&end_time)) {

commit b706fd37f6d0e7e66718488d069053687ad0664c

s3-messaging: Fix messaging classes.
This has been broken since ff0ac5b0 (May 2007).
Basically all messages were belonging to the General class except for CTDB
messages.
This fixed the message_send_all() function to correctly compute the class, and
fixes registrations to include all they need to cope with the fact not all
messages are of calss general (registrations rotted a bit because as long as
FLAG_MSG_GENERAL was defined the process woould receive all messages).

Signed-off-by: Andreas Schneider <asn@samba.org>

source3/include/messages.h
@@ -42,7 +42,7 @@
 #define FLAG_MSG_GENERAL		0x0001
 #define FLAG_MSG_SMBD			0x0002
 #define FLAG_MSG_NMBD			0x0004
-/* #define FLAG_MSG_PRINT_NOTIFY		0x0008  Obsolete */
+#define FLAG_MSG_WINBIND		0x0008
 #define FLAG_MSG_PRINT_GENERAL		0x0010
 /* dbwrap messages 4001-4999 */
 #define FLAG_MSG_DBWRAP			0x0020

source3/lib/messages.c
@@ -146,18 +146,21 @@ bool message_send_all(struct messaging_context *msg_ctx,
 	struct msg_all msg_all;
 
 	msg_all.msg_type = msg_type;
-	if (msg_type < 1000)
+	if (msg_type < 0x100) {
 		msg_all.msg_flag = FLAG_MSG_GENERAL;
-	else if (msg_type > 1000 && msg_type < 2000)
+	} else if (msg_type > 0x100 && msg_type < 0x200) {
 		msg_all.msg_flag = FLAG_MSG_NMBD;
-	else if (msg_type > 2100 && msg_type < 3000)
+	} else if (msg_type > 0x200 && msg_type < 0x300) {
 		msg_all.msg_flag = FLAG_MSG_PRINT_GENERAL;
-	else if (msg_type > 3000 && msg_type < 4000)
+	} else if (msg_type > 0x300 && msg_type < 0x400) {
 		msg_all.msg_flag = FLAG_MSG_SMBD;
-	else if (msg_type > 4000 && msg_type < 5000)
+	} else if (msg_type > 0x400 && msg_type < 0x600) {
+		msg_all.msg_flag = FLAG_MSG_WINBIND;
+	} else if (msg_type > 4000 && msg_type < 5000) {
 		msg_all.msg_flag = FLAG_MSG_DBWRAP;
-	else
-		return False;
+	} else {
+		return false;
+	}
 
 	msg_all.buf = buf;
 	msg_all.len = len;
@@ -167,7 +170,7 @@ bool message_send_all(struct messaging_context *msg_ctx,
 	serverid_traverse(traverse_fn, &msg_all);
 	if (n_sent)
 		*n_sent = msg_all.n_sent;
-	return True;
+	return true;
 }
 
 struct event_context *messaging_event_context(struct messaging_context *msg_ctx)

source3/librpc/idl/messaging.idl
@@ -94,7 +94,7 @@ interface messaging
 		/* event messages */
 		MSG_DUMP_EVENT_LIST		= 0x0500,
 
-		/* dbwrap messages 4001-4999 */
+		/* dbwrap messages 4001-4999 (0x0FA0 - 0x1387) */
 		MSG_DBWRAP_TDB2_CHANGES		= 4001,
 		MSG_DBWRAP_G_LOCK_RETRY		= 4002
 	} messaging_type;

source3/nmbd/nmbd.c
@@ -932,7 +932,9 @@ static bool open_sockets(bool isdaemon, int port)
 	/* get broadcast messages */
 
 	if (!serverid_register(procid_self(),
-			       FLAG_MSG_GENERAL|FLAG_MSG_DBWRAP)) {
+				FLAG_MSG_GENERAL |
+				FLAG_MSG_NMBD |
+				FLAG_MSG_DBWRAP)) {
 		DEBUG(1, ("Could not register myself in serverid.tdb\n"));
 		exit(1);
 	}

source3/winbindd/winbindd.c
@@ -1045,7 +1045,9 @@ void winbindd_register_handlers(void)
 	/* get broadcast messages */
 
 	if (!serverid_register(procid_self(),
-			       FLAG_MSG_GENERAL|FLAG_MSG_DBWRAP)) {
+			       FLAG_MSG_GENERAL |
+			       FLAG_MSG_WINBIND |
+			       FLAG_MSG_DBWRAP)) {
 		DEBUG(1, ("Could not register myself in serverid.tdb\n"));
 		exit(1);
 	}