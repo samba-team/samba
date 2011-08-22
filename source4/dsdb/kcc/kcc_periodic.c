484fb303ff2b7e5becd2ad9502ae8645657a3ced
Andrew Tridgell <tridge@samba.org>
2011-08-22 17:29:54 +1000

    s4-kcc: fixed _msdcs DNS name
    we need to base this DNS name on the forest DNS name for multi-domain
    support
    
    Pair-Programmed-With: Amitay Isaacs <amitay@gmail.com>

--- source4/dsdb/kcc/kcc_periodic.c
+++ source4/dsdb/kcc/kcc_periodic.c
@@ -356,9 +356,7 @@ NTSTATUS kccsrv_simple_update(struct kccsrv_service *s, TALLOC_CTX *mem_ctx)
 		r1 = &reps[count].ctr.ctr1;
 
 		r1->other_info               = talloc_zero(reps, struct repsFromTo1OtherInfo);
-		r1->other_info->dns_name     = talloc_asprintf(r1->other_info, "%s._msdcs.%s",
-							       GUID_string(mem_ctx, &ntds_guid),
-							       lpcfg_dnsdomain(s->task->lp_ctx));
+		r1->other_info->dns_name     = samdb_ntds_msdcs_dns_name(s->samdb, reps, &ntds_guid);
 		r1->source_dsa_obj_guid      = ntds_guid;
 		r1->source_dsa_invocation_id = invocation_id;
 		r1->replica_flags = kccsrv_replica_flags(s);
