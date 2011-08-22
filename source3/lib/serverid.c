76eb41461c670c23838e27e3cc7b7d6d4fd661a4
Ira Cooper <ira@wakeful.net>
2011-08-22 12:46:41 -0700

    Fix bug #8395 - optimize serverid_exists() for Solaris.
    jra asked:
    
        Do you have any idea on how many calls it saves,
        and what help the optimization gives ?
    
    As far as calls: Not 100% sure, dtrace pointed the issue out to me, and when I
    saw 10-50%(Or more), I looked for an easy fix.
    
    The real expense here is the fnctl locks on the database.  That's pretty
    painful.  1000ns per call, and probably 3-4 calls in that chain.  So it doesn't
    take much to come out ahead was my math.
    (cherry picked from commit 9bf2b3034b4c9523bfac779e518a2a1a6d76ef46)

--- source3/lib/serverid.c
+++ source3/lib/serverid.c
@@ -233,7 +233,11 @@ bool serverid_exists(const struct server_id *id)
 	struct serverid_key key;
 	TDB_DATA tdbkey;
 
-	if (lp_clustering() && !process_exists(*id)) {
+	if (procid_is_me(id)) {
+		return true;
+	}
+
+	if (!process_exists(*id)) {
 		return false;
 	}
 
