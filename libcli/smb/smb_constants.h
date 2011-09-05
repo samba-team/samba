7fd075f91b3b7c5d5a95cde54c5206d763ff8d65
Stefan Metzmacher <metze@samba.org>
2011-09-05 13:12:38 +0200

    libcli/smb: remove PROTOCOL_SMB2 alias of PROTOCOL_SMB2_02
    metze

--- libcli/smb/smb_constants.h
+++ libcli/smb/smb_constants.h
@@ -43,7 +43,6 @@ enum protocol_types {
 	PROTOCOL_NT1,
 	PROTOCOL_SMB2_02
 };
-#define PROTOCOL_SMB2 PROTOCOL_SMB2_02
 
 /* NT Flags2 bits - cifs6.txt section 3.1.2 */
 #define FLAGS2_LONG_PATH_COMPONENTS    0x0001
