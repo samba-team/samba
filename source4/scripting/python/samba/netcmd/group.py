87a6102c62f2b726b8ecda72e68643eeac479c7d
Giampaolo Lauria <lauria2@yahoo.com>
2011-09-06 13:12:36 -0400

    samba-tool: Fixed "group" subcommands syntax
    Signed-off-by: Amitay Isaacs <amitay@gmail.com>
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>

--- source4/scripting/python/samba/netcmd/group.py
+++ source4/scripting/python/samba/netcmd/group.py
@@ -43,7 +43,7 @@ distribution_group = dict({"Domain": GTYPE_DISTRIBUTION_DOMAIN_LOCAL_GROUP, "Glo
 class cmd_group_add(Command):
     """Creates a new group"""
 
-    synopsis = "%prog group add [options] <groupname>"
+    synopsis = "%prog group add <groupname> [options]"
 
     takes_options = [
         Option("-H", "--URL", help="LDB URL for database or target server", type=str,
@@ -87,7 +87,7 @@ class cmd_group_add(Command):
 class cmd_group_delete(Command):
     """Delete a group"""
 
-    synopsis = "%prog group delete <groupname>"
+    synopsis = "%prog group delete <groupname> [options]"
 
     takes_options = [
         Option("-H", "--URL", help="LDB URL for database or target server", type=str,
@@ -113,7 +113,7 @@ class cmd_group_delete(Command):
 class cmd_group_add_members(Command):
     """Add (comma-separated list of) group members"""
 
-    synopsis = "%prog group addmembers <groupname> <listofmembers>"
+    synopsis = "%prog group addmembers <groupname> <listofmembers> [options]"
 
     takes_options = [
         Option("-H", "--URL", help="LDB URL for database or target server", type=str,
@@ -140,7 +140,7 @@ class cmd_group_add_members(Command):
 class cmd_group_remove_members(Command):
     """Remove (comma-separated list of) group members"""
 
-    synopsis = "%prog group removemembers <groupname> <listofmembers>"
+    synopsis = "%prog group removemembers <groupname> <listofmembers> [options]"
 
     takes_options = [
         Option("-H", "--URL", help="LDB URL for database or target server", type=str,
