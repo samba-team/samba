27f3f2617a5de7797458b6d39ddf7ab6212d0949
Stefan Metzmacher <metze@samba.org>
2011-09-06 14:01:43 +0200

    s3:smb2_server: use smbd_smb2_request_verify_sizes() in smb2_close.c
    metze
    (cherry picked from commit e09b3940a769806dcc17d24079375f5d53eca26a)

--- source3/smbd/smb2_close.c
+++ source3/smbd/smb2_close.c
@@ -30,29 +30,20 @@ static NTSTATUS smbd_smb2_close(struct smbd_smb2_request *req,
 
 NTSTATUS smbd_smb2_request_process_close(struct smbd_smb2_request *req)
 {
-	const uint8_t *inhdr;
 	const uint8_t *inbody;
 	int i = req->current_idx;
 	uint8_t *outhdr;
 	DATA_BLOB outbody;
-	size_t expected_body_size = 0x18;
-	size_t body_size;
 	uint16_t in_flags;
 	uint64_t in_file_id_persistent;
 	uint64_t in_file_id_volatile;
 	NTSTATUS status;
 
-	inhdr = (const uint8_t *)req->in.vector[i+0].iov_base;
-	if (req->in.vector[i+1].iov_len != (expected_body_size & 0xFFFFFFFE)) {
-		return smbd_smb2_request_error(req, NT_STATUS_INVALID_PARAMETER);
+	status = smbd_smb2_request_verify_sizes(req, 0x18);
+	if (!NT_STATUS_IS_OK(status)) {
+		return smbd_smb2_request_error(req, status);
 	}
-
 	inbody = (const uint8_t *)req->in.vector[i+1].iov_base;
-
-	body_size = SVAL(inbody, 0x00);
-	if (body_size != expected_body_size) {
-		return smbd_smb2_request_error(req, NT_STATUS_INVALID_PARAMETER);
-	}
 
 	outbody = data_blob_talloc(req->out.vector, NULL, 0x3C);
 	if (outbody.data == NULL) {
