ef38cf1d4b04cb71d7a36d843ca7af8d4e6de1f0
Stefan Metzmacher <metze@samba.org>
2011-09-06 14:01:43 +0200

    s3:smb2_server: use smbd_smb2_request_verify_sizes() in smb2_write.c
    metze
    (cherry picked from commit 1a726b88ec74962d0317740bbdf576ddcffb52bc)
    (cherry picked from commit 10e5d1c6361fb309de0b2dd291deefb69c6506e6)

--- source3/smbd/smb2_write.c
+++ source3/smbd/smb2_write.c
@@ -39,11 +39,10 @@ static NTSTATUS smbd_smb2_write_recv(struct tevent_req *req,
 static void smbd_smb2_request_write_done(struct tevent_req *subreq);
 NTSTATUS smbd_smb2_request_process_write(struct smbd_smb2_request *req)
 {
+	NTSTATUS status;
 	const uint8_t *inhdr;
 	const uint8_t *inbody;
 	int i = req->current_idx;
-	size_t expected_body_size = 0x31;
-	size_t body_size;
 	uint32_t in_smbpid;
 	uint16_t in_data_offset;
 	uint32_t in_data_length;
@@ -54,17 +53,12 @@ NTSTATUS smbd_smb2_request_process_write(struct smbd_smb2_request *req)
 	uint32_t in_flags;
 	struct tevent_req *subreq;
 
+	status = smbd_smb2_request_verify_sizes(req, 0x31);
+	if (!NT_STATUS_IS_OK(status)) {
+		return smbd_smb2_request_error(req, status);
+	}
 	inhdr = (const uint8_t *)req->in.vector[i+0].iov_base;
-	if (req->in.vector[i+1].iov_len != (expected_body_size & 0xFFFFFFFE)) {
-		return smbd_smb2_request_error(req, NT_STATUS_INVALID_PARAMETER);
-	}
-
 	inbody = (const uint8_t *)req->in.vector[i+1].iov_base;
-
-	body_size = SVAL(inbody, 0x00);
-	if (body_size != expected_body_size) {
-		return smbd_smb2_request_error(req, NT_STATUS_INVALID_PARAMETER);
-	}
 
 	in_smbpid = IVAL(inhdr, SMB2_HDR_PID);
 
@@ -75,7 +69,7 @@ NTSTATUS smbd_smb2_request_process_write(struct smbd_smb2_request *req)
 	in_file_id_volatile	= BVAL(inbody, 0x18);
 	in_flags		= IVAL(inbody, 0x2C);
 
-	if (in_data_offset != (SMB2_HDR_BODY + (body_size & 0xFFFFFFFE))) {
+	if (in_data_offset != (SMB2_HDR_BODY + req->in.vector[i+1].iov_len)) {
 		return smbd_smb2_request_error(req, NT_STATUS_INVALID_PARAMETER);
 	}
 
