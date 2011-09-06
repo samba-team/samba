fb1c61880c962f26a28a45ffc9c8680edad65488
Stefan Metzmacher <metze@samba.org>
2011-09-06 14:14:52 +0200

    s3:smb2_server: return BAD_NETWORK_NAME if the path is terminated in SMB2_TCON
    metze
    (cherry picked from commit 68b33aa61ac393c2737969f8449adce3e3096d73)

--- source3/smbd/smb2_tcon.c
+++ source3/smbd/smb2_tcon.c
@@ -81,6 +81,14 @@ NTSTATUS smbd_smb2_request_process_tcon(struct smbd_smb2_request *req)
 		return smbd_smb2_request_error(req, NT_STATUS_ILLEGAL_CHARACTER);
 	}
 
+	if (in_path_buffer.length == 0) {
+		in_path_string_size = 0;
+	}
+
+	if (strlen(in_path_string) != in_path_string_size) {
+		return smbd_smb2_request_error(req, NT_STATUS_BAD_NETWORK_NAME);
+	}
+
 	status = smbd_smb2_tree_connect(req, in_path_string,
 					&out_share_type,
 					&out_share_flags,
