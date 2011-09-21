e68ebe600d9349e16e83aeb8e6ae8647c117d098
Jeremy Allison <jra@samba.org>
2011-09-21 11:30:06 -0700

    Fix bug #8473 - smb2_find uses a hard coded max reply size of 0x10000 instead of smb2_max_trans.
    Use lp_smb2_max_trans() instead of 0x10000.

--- source3/smbd/smb2_find.c
+++ source3/smbd/smb2_find.c
@@ -281,7 +281,7 @@ static struct tevent_req *smbd_smb2_find_send(TALLOC_CTX *mem_ctx,
 		return tevent_req_post(req, ev);
 	}
 
-	if (in_output_buffer_length > 0x10000) {
+	if (in_output_buffer_length > lp_smb2_max_trans()) {
 		tevent_req_nterror(req, NT_STATUS_INVALID_PARAMETER);
 		return tevent_req_post(req, ev);
 	}
