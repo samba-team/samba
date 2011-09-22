949f12d5921fbdd84cc46fab4a6788aff03cbfb1
Stefan Metzmacher <metze@samba.org>
2011-09-22 21:23:02 +0200

    Revert "Fix bug #8453 - smbclient segfaults when dialect option -m is used for legacy dialects"
    This reverts commit f261ac1932ecdae925b27301aa3e907757845a85.
    
    We now handle that in cli_state_create().
    
    metze
    
    Autobuild-User: Stefan Metzmacher <metze@samba.org>
    Autobuild-Date: Wed Nov 16 15:44:05 CET 2011 on sn-devel-104

--- source3/libsmb/cliconnect.c
+++ source3/libsmb/cliconnect.c
@@ -1976,19 +1976,6 @@ NTSTATUS cli_session_setup(struct cli_state *cli,
 	}
 
 	if (cli_state_protocol(cli) < PROTOCOL_LANMAN1) {
-		/*
-		 * Ensure cli->server_domain,
-		 * cli->server_os and cli->server_type
-		 * are valid pointers.
-		 */
-		cli->server_domain = talloc_strdup(cli, "");
-		cli->server_os = talloc_strdup(cli, "");
-		cli->server_type = talloc_strdup(cli, "");
-		if (cli->server_domain == NULL ||
-				cli->server_os == NULL ||
-				cli->server_type == NULL) {
-			return NT_STATUS_NO_MEMORY;
-		}
 		return NT_STATUS_OK;
 	}
 
