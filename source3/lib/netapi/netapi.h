7df9932ac8c7355603d3e34b3e7f6cf16ec94af3
Günther Deschner <gd@samba.org>
2011-09-26 16:25:12 +0200

    s3-netapi: allow to use default krb5 credential cache for libnetapi users.
    Guenther
    (cherry picked from commit 1f9875f435d1a9cf6d0476f476f8af9480b87bdb)

--- source3/lib/netapi/netapi.h
+++ source3/lib/netapi/netapi.h
@@ -1356,6 +1356,7 @@ struct libnetapi_ctx {
 	char *krb5_cc_env;
 	int use_kerberos;
 	int use_ccache;
+	int use_memory_krb5_ccache;
 	int disable_policy_handle_cache;
 
 	void *private_data;
@@ -1404,6 +1405,11 @@ NET_API_STATUS libnetapi_set_workgroup(struct libnetapi_ctx *ctx,
 ****************************************************************/
 
 NET_API_STATUS libnetapi_set_use_kerberos(struct libnetapi_ctx *ctx);
+
+/****************************************************************
+****************************************************************/
+
+NET_API_STATUS libnetapi_set_use_memory_krb5_ccache(struct libnetapi_ctx *ctx);
 
 /****************************************************************
 ****************************************************************/
