3c4eb2cab68d4b60888dfb557734cdfaeef50b4c
Stefan Metzmacher <metze@samba.org>
2011-10-11 15:32:36 +0200

    script/autobuild.py: split 'samba3' 'make -j 4 everything' into multiple steps
    Calling 'waf configure' while compiling .c files makes it very hard to
    find compile errors. We do 'make -j 4 bin/smbtorture4' before 'make -j 4 everything'.
    
    metze

--- script/autobuild.py
+++ script/autobuild.py
@@ -34,7 +34,11 @@ tasks = {
     "samba3" : [ ("autogen", "./autogen.sh", "text/plain"),
                  ("configure", "./configure.developer ${PREFIX}", "text/plain"),
                  ("make basics", "make basics", "text/plain"),
-                 ("make", "make -j 4 everything", "text/plain"), # don't use too many processes
+                 # we split 'make -j 4', 'make bin/smbtorture4' and 'make -j 4 everything'
+                 # because it makes it much easier to find errors.
+                 ("make", "make -j 4", "text/plain"), # don't use too many processes
+                 ("make bin/smbtorture4", "make -j 4 bin/smbtorture4", "text/plain"),
+                 ("make everything", "make -j 4 everything", "text/plain"),
                  ("install", "make install", "text/plain"),
                  ("test", "TDB_NO_FSYNC=1 make test FAIL_IMMEDIATELY=1", "text/plain"),
                  ("check-clean-tree", "../script/clean-source-tree.sh", "text/plain"),
