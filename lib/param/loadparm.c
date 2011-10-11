e425a17cb4d05d7f98dac9d9ab868bab76eced98
Stefan Metzmacher <metze@samba.org>
2011-10-11 15:38:43 +0200

    lib/param: fix typo _SAMBA_BUILD => _SAMBA_BUILD_ causing segfaults
    If lpcfg_dns_update_command() returns NULL 'samba' segfaults on startup.
    
    metze
    
    Autobuild-User: Stefan Metzmacher <metze@samba.org>
    Autobuild-Date: Tue Oct 11 17:25:19 CEST 2011 on sn-devel-104

--- lib/param/loadparm.c
+++ lib/param/loadparm.c
@@ -3369,7 +3369,7 @@ struct loadparm_context *loadparm_init(TALLOC_CTX *mem_ctx)
 	lpcfg_do_global_parameter(lp_ctx, "winbind separator", "\\");
 	lpcfg_do_global_parameter(lp_ctx, "winbind sealed pipes", "True");
 	lpcfg_do_global_parameter(lp_ctx, "winbindd socket directory", dyn_WINBINDD_SOCKET_DIR);
-#if _SAMBA_BUILD >= 4
+#if _SAMBA_BUILD_ >= 4
 	lpcfg_do_global_parameter(lp_ctx, "winbindd privileged socket directory", dyn_WINBINDD_PRIVILEGED_SOCKET_DIR);
 	lpcfg_do_global_parameter(lp_ctx, "ntp signd socket directory", dyn_NTP_SIGND_SOCKET_DIR);
 	lpcfg_do_global_parameter_var(lp_ctx, "dns update command", "%s/samba_dnsupdate", dyn_SCRIPTSBINDIR);
