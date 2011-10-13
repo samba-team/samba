d47c25401a9a553f39be9adf079dc7c462244d81
Günther Deschner <gd@samba.org>
2011-10-13 14:54:51 +0200

    s3-passdb: add define guards for machine_sid.h.
    Guenther

--- source3/passdb/machine_sid.h
+++ source3/passdb/machine_sid.h
@@ -22,7 +22,12 @@
 
 /* The following definitions come from passdb/machine_sid.c  */
 
+#ifndef _PASSDB_MACHINE_SID_H_
+#define _PASSDB_MACHINE_SID_H_
+
 struct dom_sid  *get_global_sam_sid(void);
 void reset_global_sam_sid(void) ;
 bool sid_check_is_domain(const struct dom_sid  *sid);
 bool sid_check_is_in_our_domain(const struct dom_sid  *sid);
+
+#endif /* _PASSDB_MACHINE_SID_H_ */
