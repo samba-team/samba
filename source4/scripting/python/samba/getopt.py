aa7240e6cf9d2fcec660116f891fc9c7d6ce39bc
Jelmer Vernooij <jelmer@samba.org>
2011-10-19 01:30:40 +0200

    samba.getopt: Keep exception message when setting a lp option fails.

--- source4/scripting/python/samba/getopt.py
+++ source4/scripting/python/samba/getopt.py
@@ -75,13 +75,14 @@ class SambaOptions(optparse.OptionGroup):
 
     def _set_option(self, option, opt_str, arg, parser):
         if arg.find('=') == -1:
-            raise optparse.OptionValueError("--option option takes a 'a=b' argument")
+            raise optparse.OptionValueError(
+                "--option option takes a 'a=b' argument")
         a = arg.split('=')
         try:
             self._lp.set(a[0], a[1])
-        except Exception:
-            raise optparse.OptionValueError("invalid --option option value: %s"
-                                            % arg)
+        except Exception, e:
+            raise optparse.OptionValueError(
+                "invalid --option option value %r: %s" % (arg, e))
 
     def get_loadparm(self):
         """Return loadparm object with data specified on the command line."""
