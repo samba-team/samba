926b3394b653a9bef561fea9c89a18a1850df6f9
Stefan Metzmacher <metze@samba.org>
2011-10-27 22:55:17 +0200

    s4:librpc/dcerpc_smb: fix smb_write_callback()
    The should use smb_raw_write_recv() to get the result.
    
    metze

--- source4/librpc/rpc/dcerpc_smb.c
+++ source4/librpc/rpc/dcerpc_smb.c
@@ -308,13 +308,18 @@ static NTSTATUS smb_send_trans_request(struct dcecli_connection *c, DATA_BLOB *b
 static void smb_write_callback(struct smbcli_request *req)
 {
 	struct dcecli_connection *c = (struct dcecli_connection *)req->async.private_data;
+	union smb_write io;
+	NTSTATUS status;
 
-	if (!NT_STATUS_IS_OK(req->status)) {
-		DEBUG(0,("dcerpc_smb: write callback error\n"));
-		pipe_dead(c, req->status);
+	ZERO_STRUCT(io);
+	io.generic.level = RAW_WRITE_WRITEX;
+
+	status = smb_raw_write_recv(req, &io);
+	if (!NT_STATUS_IS_OK(status)) {
+		DEBUG(0,("dcerpc_smb: write callback error: %s\n",
+			nt_errstr(status)));
+		pipe_dead(c, status);
 	}
-
-	smbcli_request_destroy(req);
 }
 
 /* 
