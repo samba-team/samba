151bb290d12f6727fce4b6be29175d5ad5551bef
Günther Deschner <gd@samba.org>
2011-10-28 12:03:58 +0200

    s3-net: Make sure to always re-use the "good" dc for the DNS updates as well.
    Guenther
    
    Autobuild-User: Günther Deschner <gd@samba.org>
    Autobuild-Date: Fri Oct 28 19:13:49 CEST 2011 on sn-devel-104

--- source3/utils/net_ads.c
+++ source3/utils/net_ads.c
@@ -1466,7 +1466,7 @@ int net_ads_join(struct net_context *c, int argc, const char **argv)
 		/* We enter this block with user creds */
 		ADS_STRUCT *ads_dns = NULL;
 
-		if ( (ads_dns = ads_init( lp_realm(), NULL, NULL )) != NULL ) {
+		if ( (ads_dns = ads_init( lp_realm(), NULL, r->in.dc_name )) != NULL ) {
 			/* kinit with the machine password */
 
 			use_in_memory_ccache();
