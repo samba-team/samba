d5fe21c0037bdf7c95b1219911a3a5af6ee74b83
Volker Lendecke <vl@samba.org>
2011-10-31 12:41:09 +0100

    s3: Fix some nonempty blank lines
    Autobuild-User: Volker Lendecke <vlendec@samba.org>
    Autobuild-Date: Mon Oct 31 14:25:14 CET 2011 on sn-devel-104

--- source3/lib/messages.c
+++ source3/lib/messages.c
@@ -5,17 +5,17 @@
    Copyright (C) 2001 by Martin Pool
    Copyright (C) 2002 by Jeremy Allison
    Copyright (C) 2007 by Volker Lendecke
-   
+
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -24,7 +24,7 @@
   @defgroup messages Internal messaging framework
   @{
   @file messages.c
-  
+
   @brief  Module for internal messaging between Samba daemons. 
 
    The idea is that if a part of Samba wants to do communication with
@@ -115,7 +115,7 @@ static int traverse_fn(struct db_record *rec, const struct server_id *id,
 				    (const uint8 *)msg_all->buf, msg_all->len);
 
 	if (NT_STATUS_EQUAL(status, NT_STATUS_INVALID_HANDLE)) {
-		
+
 		/*
 		 * If the pid was not found delete the entry from
 		 * serverid.tdb
