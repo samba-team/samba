2f2913349146e03b498f305f046c92344508da69
Jelmer Vernooij <jelmer@samba.org>
2011-11-12 16:56:01 +0100

    wafsambaa: Support libraries that don't have the same name as their pkg-config file.

--- buildtools/wafsamba/samba_bundled.py
+++ buildtools/wafsamba/samba_bundled.py
@@ -126,7 +126,7 @@ def CHECK_BUNDLED_SYSTEM_PKG(conf, libname, minversion='0.0.0',
         pkg = libname
 
     if conf.check_cfg(package=pkg,
-                      args='"%s >= %s" --cflags --libs' % (libname, minversion),
+                      args='"%s >= %s" --cflags --libs' % (pkg, minversion),
                       msg=msg):
         conf.SET_TARGET_TYPE(libname, 'SYSLIB')
         conf.env[found] = True
