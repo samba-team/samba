0cd67698ca69d82936da29658103ce449d797c25
Stefan Metzmacher <metze@samba.org>
2011-11-14 15:29:37 +0100

    s3:smb2_ioctl: STATUS_PENDING is defered by 1 millisecond for SMB2_IOCTL
    metze

--- source3/smbd/smb2_ioctl.c
+++ source3/smbd/smb2_ioctl.c
@@ -110,7 +110,7 @@ NTSTATUS smbd_smb2_request_process_ioctl(struct smbd_smb2_request *req)
 	}
 	tevent_req_set_callback(subreq, smbd_smb2_request_ioctl_done, req);
 
-	return smbd_smb2_request_pending_queue(req, subreq, 500);
+	return smbd_smb2_request_pending_queue(req, subreq, 1000);
 }
 
 static void smbd_smb2_request_ioctl_done(struct tevent_req *subreq)
