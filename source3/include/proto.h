c40c0d187427ca221d55c673019233375000f1af
Jeremy Allison <jra@samba.org>
2011-11-15 13:30:22 -0800

    Fix bug #8561 - Password change settings not fully observed. (cherry picked from commit 467a586b3b25f632a5b9ca58abf428a546d97740)

--- source3/include/proto.h
+++ source3/include/proto.h
@@ -4492,6 +4492,7 @@ bool pdb_set_group_sid_from_rid (struct samu *sampass, uint32 grid, enum pdb_val
 
 /* The following definitions come from passdb/pdb_get_set.c  */
 
+bool pdb_is_password_change_time_max(time_t test_time);
 uint32 pdb_get_acct_ctrl(const struct samu *sampass);
 time_t pdb_get_logon_time(const struct samu *sampass);
 time_t pdb_get_logoff_time(const struct samu *sampass);
