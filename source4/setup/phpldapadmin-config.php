0a10b3260212982825d2bc78739ffef795ac63cb
Amitay Isaacs <amitay@gmail.com>
2011-12-07 12:26:46 +1100

    setup: Fix phpldapadmin configuration for version 1.2.x

--- source4/setup/phpldapadmin-config.php
+++ source4/setup/phpldapadmin-config.php
@@ -1,28 +1,20 @@
 <?php
 /**
  * The phpLDAPadmin config file, customised for use with Samba4
- * This overrides phpLDAPadmin defaults
- * that are defined in config_default.php.
  *
- * DONT change config_default.php, you changes will be lost by the next release
- * of PLA. Instead change this file - as it will NOT be replaced by a new
- * version of phpLDAPadmin.
+ * Use config.php.example to create config.php, if you don't have one.
+ *
+ * Append this file to config.php.
  */
 
-/*********************************************/
-/* Useful important configuration overrides  */
-/*********************************************/
-
-/* phpLDAPadmin can encrypt the content of sensitive cookies if you set this
-   to a big random string. */
-
-$i=0;
-$ldapservers = new LDAPServers;
+/* Create a new LDAP server for SAMBA4 */
+$servers->newServer('ldap_pla');
 
 /* A convenient name that will appear in the tree viewer and throughout
    phpLDAPadmin to identify this LDAP server to users. */
-$ldapservers->SetValue($i,'server','name','Samba4 LDAP Server');
-$ldapservers->SetValue($i,'server','host','${S4_LDAPI_URI}');
-$ldapservers->SetValue($i,'server','auth_type','session');
-$ldapservers->SetValue($i,'login','attr','dn');
+$servers->setValue('server','name','Samba4 LDAP Server');
+$servers->setValue('server','host','${S4_LDAPI_URI}');
+$servers->setValue('login','auth_type','session');
+$servers->setValue('login','attr','dn');
+
 ?>
