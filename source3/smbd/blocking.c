d1e0997614ac76bf4d2efc59d9a49472a3da3d4c
Stefan Metzmacher <metze@samba.org>
2011-12-12 15:45:07 +0100

    s3:smbd/blocking: pass smbd_server_connection as private_data to received_unlock_msg()
    metze

--- source3/smbd/blocking.c
+++ source3/smbd/blocking.c
@@ -248,7 +248,7 @@ bool push_blocking_lock_request( struct byte_range_lock *br_lck,
 
 	/* Ensure we'll receive messages when this is unlocked. */
 	if (!sconn->smb1.locks.blocking_lock_unlock_state) {
-		messaging_register(sconn->msg_ctx, NULL,
+		messaging_register(sconn->msg_ctx, sconn,
 				   MSG_SMB_UNLOCK, received_unlock_msg);
 		sconn->smb1.locks.blocking_lock_unlock_state = true;
 	}
@@ -702,11 +702,11 @@ static void received_unlock_msg(struct messaging_context *msg,
 				struct server_id server_id,
 				DATA_BLOB *data)
 {
-	struct smbd_server_connection *sconn;
+	struct smbd_server_connection *sconn =
+		talloc_get_type(private_data,
+		struct smbd_server_connection);
 
-	sconn = msg_ctx_to_sconn(msg);
 	if (sconn == NULL) {
-		DEBUG(1, ("could not find sconn\n"));
 		return;
 	}
 
