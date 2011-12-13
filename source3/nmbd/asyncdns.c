5c3194cc41c68ce4aacf339225b0b0d3a4f42963
Volker Lendecke <vl@samba.org>
2011-12-13 16:21:37 +0100

    s3: Fix some nonempty blank lines

--- source3/nmbd/asyncdns.c
+++ source3/nmbd/asyncdns.c
@@ -2,17 +2,17 @@
    Unix SMB/CIFS implementation.
    a async DNS handler
    Copyright (C) Andrew Tridgell 1997-1998
-   
+
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
    */
@@ -30,7 +30,7 @@ static struct name_record *add_dns_result(struct nmb_name *question, struct in_a
 	unstring qname;
 
 	pull_ascii_nstring(qname, sizeof(qname), question->name);
-  
+
 	if (!addr.s_addr) {
 		/* add the fail to WINS cache of names. give it 1 hour in the cache */
 		DEBUG(3,("add_dns_result: Negative DNS answer for %s\n", qname));
