8115c99a9755dad0befb1b5184a29068ffb5be57
Andrew Bartlett <abartlet@samba.org>
2011-12-16 13:19:06 +1100

    s3-rpc_server: Add my copyright for my previous work here
    Autobuild-User: Andrew Bartlett <abartlet@samba.org>
    Autobuild-Date: Thu Dec 22 09:02:57 CET 2011 on sn-devel-104

--- source3/rpc_server/dcesrv_ntlmssp.c
+++ source3/rpc_server/dcesrv_ntlmssp.c
@@ -2,6 +2,7 @@
  *  NTLMSSP Acceptor
  *  DCERPC Server functions
  *  Copyright (C) Simo Sorce 2010.
+ *  Copyright (C) Andrew Bartlett 2011.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
