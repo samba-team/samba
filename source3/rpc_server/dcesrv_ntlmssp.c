2dd23e900ac8873f121be57ce87d4e3a8878dc6c
Andrew Bartlett <abartlet@samba.org>
2011-12-16 17:05:12 +1100

    s3-rpc_server remove unused header
    Signed-off-by: Stefan Metzmacher <metze@samba.org>

--- source3/rpc_server/dcesrv_ntlmssp.c
+++ source3/rpc_server/dcesrv_ntlmssp.c
@@ -21,7 +21,6 @@
 
 #include "includes.h"
 #include "rpc_server/dcesrv_ntlmssp.h"
-#include "../auth/ntlmssp/ntlmssp.h"
 #include "ntlmssp_wrap.h"
 #include "auth.h"
 #include "auth/gensec/gensec.h"
