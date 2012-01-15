9f2ce614a792994ffcf2dc13b693429b6b17afcc
Martin Schwenke <martin@meltin.net>
2012-01-15 14:55:52 +1100

    Tests: initial ctdb tool testcases
    Signed-off-by: Martin Schwenke <martin@meltin.net>
    
    (This used to be ctdb commit 47e9975fa75a7bb2628a751dc19d35e2c5c57ce3)

---
+++ ctdb/tests/tool/run_tests.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Run some IP allocation unit tests.
+
+cd $(dirname "$0")
+export TESTS_SUBDIR=$(pwd)
+
+test_dir=$(dirname "$TESTS_SUBDIR")
+
+opts="-d"
+
+for i ; do
+    case "$i" in
+	-*)
+	    opts="$opts $i"
+	    shift
+	    ;;
+	*)
+	    break
+    esac
+done
+
+tests=""
+if [ -z "$*" ] ; then
+    tests=$(ls testcases/*.[0-9][0-9][0-9].sh 2>/dev/null)
+fi
+
+"$test_dir/scripts/run_tests" $opts "$@" $tests || exit 1
+
+echo "All OK"
+exit 0
