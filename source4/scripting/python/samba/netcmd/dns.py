4bba5fcecd1f7071aa0529527a11f0082f8d625f
Amitay Isaacs <amitay@gmail.com>
2012-02-16 10:17:25 +1100

    samba-tool: dns: Update the copyright
    Autobuild-User: Amitay Isaacs <amitay@samba.org>
    Autobuild-Date: Tue Feb 21 09:55:07 CET 2012 on sn-devel-104

--- source4/scripting/python/samba/netcmd/dns.py
+++ source4/scripting/python/samba/netcmd/dns.py
@@ -2,7 +2,7 @@
 #
 # DNS management tool
 #
-# Copyright (C) Amitay Isaacs 2011
+# Copyright (C) Amitay Isaacs 2011-2012
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
