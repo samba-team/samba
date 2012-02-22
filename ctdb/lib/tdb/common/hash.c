94b7a84cb118b0139c29bc247ead43c25ec60415
Ronnie Sahlberg <ronniesahlberg@gmail.com>
2012-02-22 16:07:17 +1100

    change TDB to be valgrind nice
    (This used to be ctdb commit d306c3c9a53e012c412c96ab9743de6cd96826e0)

--- ctdb/lib/tdb/common/hash.c
+++ ctdb/lib/tdb/common/hash.c
@@ -22,6 +22,8 @@
    You should have received a copy of the GNU Lesser General Public
    License along with this library; if not, see <http://www.gnu.org/licenses/>.
 */
+#define VALGRIND
+
 #include "tdb_private.h"
 
 /* This is based on the hash algorithm from gdbm */
