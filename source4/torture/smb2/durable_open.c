8e947f0f5d56969527a40d8148fb773e8c69ea84
Michael Adam <obnox@samba.org>
2012-02-23 15:49:51 +0100

    s4:torture:smb2:durable-open: prevent using a disconnected handle in reopen4
    Autobuild-User: Michael Adam <obnox@samba.org>
    Autobuild-Date: Mon Feb 27 03:44:16 CET 2012 on sn-devel-104

--- source4/torture/smb2/durable_open.c
+++ source4/torture/smb2/durable_open.c
@@ -588,6 +588,7 @@ bool test_durable_open_reopen4(struct torture_context *tctx,
 	ZERO_STRUCT(io2);
 	io2.in.fname = fname;
 	io2.in.durable_handle = h;
+	h = NULL;
 
 	status = smb2_create(tree2, mem_ctx, &io2);
 	CHECK_STATUS(status, NT_STATUS_OK);
