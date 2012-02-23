samba-team/samba

commit 70f4a96c68e91e407651e2487cc3c66a80262fa2

selftest: Allow setup_env() to signal that an environment name is unknown
This will allow us to skip samba4 environments that may be mentioned in
the source3/selftest/tests.py file.

Andrew Bartlett

selftest/selftest.pl
@@ -882,7 +882,9 @@ sub setup_env($$)
 		}
 	} else {
 		$testenv_vars = $target->setup_env($envname, $prefix);
-		if (defined($testenv_vars) && not defined($testenv_vars->{target})) {
+		if (defined($testenv_vars) and $testenv_vars eq "UNKNOWN") {
+		    return $testenv_vars;
+		} elsif (defined($testenv_vars) && not defined($testenv_vars->{target})) {
 		        $testenv_vars->{target} = $target;
 		}
 		if (not defined($testenv_vars)) {
@@ -1023,6 +1025,11 @@ $envvarstr
 			Subunit::start_testsuite($name);
 			Subunit::end_testsuite($name, "error",
 				"unable to set up environment $envname - exiting");
+			next;
+		} elsif ($envvars eq "UNKNOWN") {
+			Subunit::start_testsuite($name);
+			Subunit::end_testsuite($name, "skip",
+				"environment $envname is unknown in this test backend - skipping");
 			next;
 		}
 

selftest/target/Samba.pm
@@ -27,13 +27,13 @@ sub setup_env($$$)
 	$ENV{ENVNAME} = $envname;
 
 	my $env = $self->{samba4}->setup_env($envname, $path);
-	if (defined($env)) {
+	if (defined($env) and $env ne "UNKNOWN") {
 	    if (not defined($env->{target})) {
 		$env->{target} = $self->{samba4};
 	    }
 	} else {
 	   	$env = $self->{samba3}->setup_env($envname, $path);
-		if (defined($env)) {
+		if (defined($env) and $env ne "UNKNOWN") {
 		    if (not defined($env->{target})) {
 			$env->{target} = $self->{samba3};
 		    }

selftest/target/Samba3.pm
@@ -112,7 +112,7 @@ sub setup_env($$$)
 		}
 		return $self->setup_member("$path/member", $self->{vars}->{s3dc});
 	} else {
-		return undef;
+		return "UNKNOWN";
 	}
 }
 

selftest/target/Samba4.pm
@@ -1429,7 +1429,7 @@ sub setup_env($$$)
 	} elsif ($envname eq "plugin_s4_dc") {
 		return $self->setup_plugin_s4_dc("$path/plugin_s4_dc");
 	} else {
-		return undef;
+		return "UNKNOWN";
 	}
 }
 

commit 6f8f24c5f2564cf0d0f742af556e3f641803efbd

selftest: Do not skip environments that fail to start up
This is a regression in 70f4a96c68e91e407651e2487cc3c66a80262fa2.

Andrew Bartlett

Autobuild-User: Andrew Bartlett <abartlet@samba.org>
Autobuild-Date: Thu Feb 23 10:55:20 CET 2012 on sn-devel-104

selftest/selftest.pl
@@ -888,7 +888,7 @@ sub setup_env($$)
 		        $testenv_vars->{target} = $target;
 		}
 		if (not defined($testenv_vars)) {
-			warn("$opt_target can't provide environment '$envname'");
+			warn("$opt_target can't start up known environment '$envname'");
 		}
 	}
 

selftest/target/Samba.pm
@@ -31,7 +31,7 @@ sub setup_env($$$)
 	    if (not defined($env->{target})) {
 		$env->{target} = $self->{samba4};
 	    }
-	} else {
+	} elsif (defined($env) and $env eq "UNKNOWN") {
 	   	$env = $self->{samba3}->setup_env($envname, $path);
 		if (defined($env) and $env ne "UNKNOWN") {
 		    if (not defined($env->{target})) {
@@ -39,8 +39,12 @@ sub setup_env($$$)
 		    }
 		}
 	}
-	if (not defined $env) {
+	if (defined($env) and ($env eq "UNKNOWN")) {
 		warn("Samba can't provide environment '$envname'");
+		return "UNKNOWN";
+	}
+	if (not defined $env) {
+		warn("failed to start up environment '$envname'");
 		return undef;
 	}
 	return $env;