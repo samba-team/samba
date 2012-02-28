a0d51949abde68134eb35150d797387a1fb57ab7
Brad Smith <brad@comstyle.com>
2012-02-28 20:45:41 +0100

    s3: Add sys_statvfs() wrapper support for OpenBSD/FreeBSD/DragonFly.
    Fix bug #8777.

--- source3/modules/vfs_default.c
+++ source3/modules/vfs_default.c
@@ -107,7 +107,7 @@ static uint32_t vfswrap_fs_capabilities(struct vfs_handle_struct *handle,
 	NTSTATUS status;
 	int ret = -1;
 
-#if defined(DARWINOS)
+#if defined(DARWINOS) || (defined(BSD) && defined(MNT_RDONLY))
 	struct vfs_statvfs_struct statbuf;
 	ZERO_STRUCT(statbuf);
 	sys_statvfs(conn->connectpath, &statbuf);
