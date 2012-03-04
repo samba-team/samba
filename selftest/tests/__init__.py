1d71ca98b7d7ace2c8b27566b3e7d332dbd1cbdc
Jelmer Vernooij <jelmer@samba.org>
2012-03-04 01:31:44 +0100

    Add test infrastructure for selftest.

---
+++ selftest/tests/__init__.py
@@ -0,0 +1,32 @@
+# __init__.py -- The tests for selftest
+# Copyright (C) 2012 Jelmer Vernooij <jelmer@samba.org>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; version 3
+# of the License or (at your option) any later version of
+# the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA  02110-1301, USA.
+
+"""Tests for selftest."""
+
+import unittest
+
+def test_suite():
+    result = unittest.TestSuite()
+    names = ['socketwrapper']
+    module_names = ['selftest.tests.test_' + name for name in names]
+    loader = unittest.TestLoader()
+    result.addTests(loader.loadTestsFromNames(module_names))
+    from dulwich.tests.compat import test_suite as compat_test_suite
+    result.addTests(compat_test_suite())
+    return result
---
+++ selftest/tests/test_socket_wrapper.py
@@ -0,0 +1,20 @@
+# test_socket_wraper.py -- The tests for selftest socket wrapper routines
+# Copyright (C) 2012 Jelmer Vernooij <jelmer@samba.org>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; version 3
+# of the License or (at your option) any later version of
+# the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA  02110-1301, USA.
+
+"""Tests for selftest/socket_wrapper."""
