16ecda3f11e7398bf3d493b89abb5afabc714f1a
Andrew Bartlett <abartlet@samba.org>
2012-03-11 04:45:05 +1100

    selftest: fix running additional smbclient test for non-spnego session setup
    Autobuild-User: Andrew Bartlett <abartlet@samba.org>
    Autobuild-Date: Sat Mar 10 20:46:05 CET 2012 on sn-devel-104

--- source3/selftest/tests.py
+++ source3/selftest/tests.py
@@ -129,7 +129,7 @@ tests=["--ping", "--separator",
 
 for options in ["--option=clientusespnego=no", " --option=clientntlmv2auth=no --option=clientlanmanauth=yes --max-protocol=LANMAN2", ""]:
     env = "s3dc"
-    plantestsuite("samba3.blackbox.smbclient_auth.plain (%s) %s" % (env, options), env, [os.path.join(samba3srcdir, "script/tests/test_smbclient_auth.sh"), '$SERVER', '$SERVER_IP', '$DC_USERNAME', '$DC_PASSWORD', binpath('smbclient3'), configuration])
+    plantestsuite("samba3.blackbox.smbclient_auth.plain (%s) %s" % (env, options), env, [os.path.join(samba3srcdir, "script/tests/test_smbclient_auth.sh"), '$SERVER', '$SERVER_IP', '$DC_USERNAME', '$DC_PASSWORD', binpath('smbclient3'), configuration, options])
 
 for env in ["s3dc", "member", "s3member"]:
     plantestsuite("samba3.blackbox.smbclient_auth.plain (%s)" % env, env, [os.path.join(samba3srcdir, "script/tests/test_smbclient_auth.sh"), '$SERVER', '$SERVER_IP', '$DC_USERNAME', '$DC_PASSWORD', binpath('smbclient3'), configuration])
