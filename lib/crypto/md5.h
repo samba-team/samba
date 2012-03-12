b68f72c7f58c05870100d0d993c9baf0fa80a891
Matthieu Patou <mat@matws.net>
2012-03-12 16:21:28 -0700

    lib/crypto: Detect CommonCrypto and use it if available
    CommonCrypto/CommonDigest is available on Mac and there is function in
    the libc for MD5 calculation. MD5Final is a C define of CC_MD5_Final.
    Under some circumstance we have the symbol defined twice in samba
    binaries on Snow Leopard at least.
    
    By detecting CommonCrypto/CommonDigest we end up always using the system
    version if available.

--- lib/crypto/md5.h
+++ lib/crypto/md5.h
@@ -10,6 +10,16 @@
 /* Try to avoid clashes with BSD MD5 implementation */
 #include <bsd/md5.h>
 #else
+/* Try to use CommonCrypto on Mac as otherwise we can get MD5Final twice */
+#ifdef HAVE_COMMONCRYPTO_COMMONDIGEST_H
+#include <CommonCrypto/CommonDigest.h>
+
+#define MD5Init(c)					CC_MD5_Init(c)
+#define MD5Update(c,d,l)			CC_MD5_Update(c,d,l)
+#define MD5Final(m, c)				CC_MD5_Final((unsigned char *)m,c)
+#define MD5Context CC_MD5state_st
+
+#else
 typedef struct MD5Context {
 	uint32_t buf[4];
 	uint32_t bits[2];
@@ -22,6 +32,8 @@ void MD5Init(MD5_CTX *context);
 void MD5Update(MD5_CTX *context, const uint8_t *buf,
 	       size_t len);
 void MD5Final(uint8_t digest[MD5_DIGEST_LENGTH], MD5_CTX *context);
+#endif /* HAVE_COMMONCRYPTO_COMMONDIGEST_H */
+
 #endif /* HAVE_BSD_MD5_H */
 
 #endif /* !MD5_H */
