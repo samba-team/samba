5ecc34807296793dbe9b50145fc91174bbe0451d
Martin Schwenke <martin@meltin.net>
2012-03-20 17:06:51 +1100

    Tests - eventscripts - fix hardcoding error
    Signed-off-by: Martin Schwenke <martin@meltin.net>
    
    (This used to be ctdb commit 9c30bf7fd3d94884e27a258571a28dca5e460177)

--- ctdb/tests/eventscripts/simple/10.interface.takeip.003.sh
+++ ctdb/tests/eventscripts/simple/10.interface.takeip.003.sh
@@ -7,6 +7,10 @@ define_test "error - add same IP twice"
 setup_ctdb
 
 public_address=$(ctdb_get_1_public_address)
+dev="${public_address%% *}"
+t="${public_address#* }"
+ip="${t% *}"
+bits="${t#* }"
 
 # This is a bit gross and contrived.  The method of quoting the error
 # message so it makes it to required_result() is horrible.  Hopefully
@@ -14,7 +18,7 @@ public_address=$(ctdb_get_1_public_address)
 
 err2="\
 RTNETLINK answers: File exists
-Failed to add 10.0.0.1/24 on dev dev123"
+Failed to add $ip/$bits on dev $dev"
 
 #EVENTSCRIPTS_TESTS_TRACE="sh -x"
 iterate_test -- $public_address -- 2 "ok_null" \
