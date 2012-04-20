466ff20156bf220b6d67ac18898082635513d658
Martin Schwenke <martin@meltin.net>
2012-04-20 14:10:34 +1000

    tests: Update top-level wrapper scripts
    * Add the unit tests to "make test"
    * Make scripts run from anywhere
    
    Signed-off-by: Martin Schwenke <martin@meltin.net>
    
    (This used to be ctdb commit 95947123eee687ed83eebc567397a5bc9d55a945)

--- ctdb/tests/run_cluster_tests.sh
+++ ctdb/tests/run_cluster_tests.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
-if [ -n "$*" ]; then
-    tests/scripts/run_tests -s $* || exit 1
+test_dir=$(dirname "$0")
+
+if [ -n "$1" ] ; then
+    "${test_dir}/scripts/run_tests" -l -s "$@" || exit 1
 else
-    tests/scripts/run_tests -s tests/simple/*.sh tests/complex/*.sh || exit 1
+    cd "$test_dir"
+
+    # By default, run all unit tests and the tests against local
+    # daemons
+    dirs="simple complex"
+
+    ./scripts/run_tests -s $dirs || exit 1
 fi
 
 echo "All OK"
--- ctdb/tests/run_tests.sh
+++ ctdb/tests/run_tests.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
-tests/scripts/run_tests -l -s ${*:-tests/simple/*.sh} || exit 1
+test_dir=$(dirname "$0")
+
+if [ -n "$1" ] ; then
+    "${test_dir}/scripts/run_tests" -l -s "$@" || exit 1
+else
+    cd "$test_dir"
+
+    # By default, run all unit tests and the tests against local
+    # daemons
+    dirs="onnode takeover tool eventscripts simple"
+
+    ./scripts/run_tests -l -s $dirs || exit 1
+fi
 
 echo "All OK"
 exit 0
