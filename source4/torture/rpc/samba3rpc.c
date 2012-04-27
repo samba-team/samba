12ee7933327e99c0a5db8c7b6273775a6fc9c8fd
Michael Adam <obnox@samba.org>
2012-04-28 00:35:56 +0200

    s4:torture: add a check for talloc success in rpc.samba3.randomauth2
    Autobuild-User: Michael Adam <obnox@samba.org>
    Autobuild-Date: Sat Apr 28 02:10:39 CEST 2012 on sn-devel-104

--- source4/torture/rpc/samba3rpc.c
+++ source4/torture/rpc/samba3rpc.c
@@ -2142,7 +2142,7 @@ static bool torture_samba3_rpc_randomauth2(struct torture_context *torture)
 						 r.in.credentials,
 						 r.out.return_credentials, &mach_pw,
 						 &netr_cred, negotiate_flags);
-
+	torture_assert(torture, (creds_state != NULL), "memory allocation failed");
 
 	status = dcerpc_netr_ServerAuthenticate2_r(net_handle, mem_ctx, &a);
 	if (!NT_STATUS_IS_OK(status)) {
