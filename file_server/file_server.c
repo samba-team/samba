c3be5240e980c6bb523ba6ff95d5abf691ad2adf
Andrew Bartlett <abartlet@samba.org>
2012-04-30 15:22:04 +1000

    file_server: use embedded ntsvcs server

--- file_server/file_server.c
+++ file_server/file_server.c
@@ -54,6 +54,7 @@ static const char *generate_smb_conf(struct task_server *task)
         fdprintf(fd, "rpc_server:default = external\n");
 	fdprintf(fd, "rpc_server:svcctl = embedded\n");
 	fdprintf(fd, "rpc_server:eventlog = embedded\n");
+	fdprintf(fd, "rpc_server:ntsvcs = embedded\n");
 	fdprintf(fd, "rpc_server:spoolss = embedded\n");
 	fdprintf(fd, "rpc_daemon:spoolssd = disabled\n");
 	fdprintf(fd, "rpc_server:tcpip = no\n");
