74ca9cc957cfd0f9a6c33b6c9cb8d7822ff7e7ff
Volker Lendecke <vl@samba.org>
2012-05-09 10:56:54 +0200

    s3: Fix Coverity ID 242719 Uninitialized scalar variable
    In an error path we are closing domain_pol without opening it

--- source3/rpcclient/cmd_samr.c
+++ source3/rpcclient/cmd_samr.c
@@ -1054,7 +1054,8 @@ static NTSTATUS cmd_samr_enum_als_groups(struct rpc_pipe_client *cli,
                                          TALLOC_CTX *mem_ctx,
                                          int argc, const char **argv)
 {
-	struct policy_handle connect_pol, domain_pol;
+	struct policy_handle connect_pol;
+	struct policy_handle domain_pol = { 0, };
 	NTSTATUS status, result;
 	uint32 start_idx, num_als_groups, i;
 	uint32 access_mask = MAXIMUM_ALLOWED_ACCESS;
