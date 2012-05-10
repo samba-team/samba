af2ab74beb5da03e84e760ac92d45c9453a5e75d
Martin Schwenke <martin@meltin.net>
2012-05-10 14:55:19 +1000

    tests: Allow run_tests.sh to take options
    However, options must be followed by "--".
    
    Signed-off-by: Martin Schwenke <martin@meltin.net>
    
    (This used to be ctdb commit db8cf8f5e644a0b21a6040287887fee40f38d4db)

--- ctdb/tests/run_tests.sh
+++ ctdb/tests/run_tests.sh
@@ -2,8 +2,22 @@
 
 test_dir=$(dirname "$0")
 
+# Allow options to be passed to this script.  However, if any options
+# are passed there must be a "--" between the options and the tests.
+# This makes it easy to handle options that take arguments.
+opts=""
+case "$1" in
+    -*)
+	while [ -n "$1" ] ; do
+	    case "$1" in
+		--) shift ; break ;;
+		*) opts="$opts $1" ; shift ;;
+	    esac
+	done
+esac
+
 if [ -n "$1" ] ; then
-    "${test_dir}/scripts/run_tests" -l -s "$@" || exit 1
+    "${test_dir}/scripts/run_tests" -l -s $opts "$@" || exit 1
 else
     cd "$test_dir"
 
@@ -11,7 +25,7 @@ else
     # daemons
     dirs="onnode takeover tool eventscripts simple"
 
-    ./scripts/run_tests -l -s $dirs || exit 1
+    ./scripts/run_tests -l -s $opts $dirs || exit 1
 fi
 
 echo "All OK"
