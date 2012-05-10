b713c11621bd9e1fcae535888421c0b3a09d014b
Martin Schwenke <martin@meltin.net>
2012-05-10 14:32:06 +1000

    tests/eventscripts: Fix a policy routing test
    The previous commit 55006ea8999ab3721fcde81b92692661065f0688
    highlighted an error in this test.
    
    Signed-off-by: Martin Schwenke <martin@meltin.net>
    
    (This used to be ctdb commit 9f20fbf91706db94f65f62dbd6a4e087890c1da9)

--- ctdb/tests/eventscripts/13.per_ip_routing.002.sh
+++ ctdb/tests/eventscripts/13.per_ip_routing.002.sh
@@ -8,7 +8,7 @@ setup_ctdb
 setup_ctdb_policy_routing
 
 required_result 1 <<EOF
-error: CTDB_PER_IP_ROUTING_CONF=${TEST_SUBDIR}/etc-ctdb/policy_routing file not found
+error: CTDB_PER_IP_ROUTING_CONF=${CTDB_BASE}/policy_routing file not found
 EOF
 
 simple_test_event "ipreallocated"
