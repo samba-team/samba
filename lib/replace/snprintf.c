dcd11b2f2c523a2f80295ad993dfd1f2b6403fba
Jelmer Vernooij <jelmer@samba.org>
2012-05-13 03:21:34 +0200

    libreplace: Fix symbol names for snprintf/asprintf/vasprintf.
    Autobuild-User: Jelmer Vernooij <jelmer@samba.org>
    Autobuild-Date: Sun May 13 05:16:28 CEST 2012 on sn-devel-104
    (cherry picked from commit cf67da70c9a63c4dc63f287059321d6c36d1e19e)
    (cherry picked from commit 27405fb8cfaa56f3a39cdcd2fd635fd37af629f9)

--- lib/replace/snprintf.c
+++ lib/replace/snprintf.c
@@ -1187,7 +1187,7 @@ static int add_cnk_list_entry(struct pr_chunk_x **list,
 	return max;
 }
 
- int vsnprintf (char *str, size_t count, const char *fmt, va_list args)
+ int rep_vsnprintf (char *str, size_t count, const char *fmt, va_list args)
 {
 	return dopr(str, count, fmt, args);
 }
@@ -1200,7 +1200,7 @@ static int add_cnk_list_entry(struct pr_chunk_x **list,
  * that doesn't work properly according to the autoconf test.
  */
 #if !defined(HAVE_SNPRINTF) || !defined(HAVE_C99_VSNPRINTF)
- int snprintf(char *str,size_t count,const char *fmt,...)
+ int rep_snprintf(char *str,size_t count,const char *fmt,...)
 {
 	size_t ret;
 	va_list ap;
@@ -1213,7 +1213,7 @@ static int add_cnk_list_entry(struct pr_chunk_x **list,
 #endif
 
 #ifndef HAVE_C99_VSNPRINTF
- int printf(const char *fmt, ...)
+ int rep_printf(const char *fmt, ...)
 {
 	va_list ap;
 	int ret;
@@ -1234,7 +1234,7 @@ static int add_cnk_list_entry(struct pr_chunk_x **list,
 #endif
 
 #ifndef HAVE_C99_VSNPRINTF
- int fprintf(FILE *stream, const char *fmt, ...)
+ int rep_fprintf(FILE *stream, const char *fmt, ...)
 {
 	va_list ap;
 	int ret;
@@ -1257,7 +1257,7 @@ static int add_cnk_list_entry(struct pr_chunk_x **list,
 #endif 
 
 #ifndef HAVE_VASPRINTF
- int vasprintf(char **ptr, const char *format, va_list ap)
+ int rep_vasprintf(char **ptr, const char *format, va_list ap)
 {
 	int ret;
 	va_list ap2;
@@ -1280,7 +1280,7 @@ static int add_cnk_list_entry(struct pr_chunk_x **list,
 
 
 #ifndef HAVE_ASPRINTF
- int asprintf(char **ptr, const char *format, ...)
+ int rep_asprintf(char **ptr, const char *format, ...)
 {
 	va_list ap;
 	int ret;
