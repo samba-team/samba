bf8829b56f75019036c76be1bd46ad816586e3d9
Stefan Metzmacher <metze@samba.org>
2012-05-14 11:50:52 +0200

    s4:lib/events: no longer define TEVENT_COMPAT_DEFINES
    metze

--- source4/lib/events/events.h
+++ source4/lib/events/events.h
@@ -1,8 +1,5 @@
 #ifndef __LIB_EVENTS_H__
 #define __LIB_EVENTS_H__
-#ifndef TEVENT_COMPAT_DEFINES
-#define TEVENT_COMPAT_DEFINES 1
-#endif
 #include <tevent.h>
 struct tevent_context *s4_event_context_init(TALLOC_CTX *mem_ctx);
 void s4_event_context_set_default(struct tevent_context *ev);
