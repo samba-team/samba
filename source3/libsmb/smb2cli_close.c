b2dcc7a061cb7438a51f886d7cd649b4c577ae04
Stefan Metzmacher <metze@samba.org>
2012-05-14 09:56:47 +0200

    s3:libsmb: s/\<event_context/tevent_context/gc in smb2cli_close.c
    metze

--- source3/libsmb/smb2cli_close.c
+++ source3/libsmb/smb2cli_close.c
@@ -106,7 +106,7 @@ NTSTATUS smb2cli_close(struct smbXcli_conn *conn,
 		       uint64_t fid_volatile)
 {
 	TALLOC_CTX *frame = talloc_stackframe();
-	struct event_context *ev;
+	struct tevent_context *ev;
 	struct tevent_req *req;
 	NTSTATUS status = NT_STATUS_NO_MEMORY;
 
@@ -117,7 +117,7 @@ NTSTATUS smb2cli_close(struct smbXcli_conn *conn,
 		status = NT_STATUS_INVALID_PARAMETER;
 		goto fail;
 	}
-	ev = event_context_init(frame);
+	ev = tevent_context_init(frame);
 	if (ev == NULL) {
 		goto fail;
 	}
