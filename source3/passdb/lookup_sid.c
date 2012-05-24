c4fd9ff890e2640ef90b4d7129621e3fe800a61e
Ira Cooper <ira@samba.org>
2012-05-23 21:39:03 -0400

    s3-passdb: Fix negative SID->uid/gid cache handling. (bug #8952)
    -1 uid/gid signals a non existent uid/gid.
    
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    (cherry picked from commit baac32c43460ed5e78e7f8bb2304f79cae2b15ef)

--- source3/passdb/lookup_sid.c
+++ source3/passdb/lookup_sid.c
@@ -1490,7 +1490,24 @@ bool sids_to_unix_ids(const struct dom_sid *sids, uint32_t num_sids,
 			continue;
 		}
 	}
+
 done:
+	for (i=0; i<num_sids; i++) {
+		switch(ids[i].type) {
+		case WBC_ID_TYPE_GID:
+			if (ids[i].id.gid == (gid_t)-1) {
+				ids[i].type = WBC_ID_TYPE_NOT_SPECIFIED;
+			}
+			break;
+		case WBC_ID_TYPE_UID:
+			if (ids[i].id.uid == (uid_t)-1) {
+				ids[i].type = WBC_ID_TYPE_NOT_SPECIFIED;
+			}
+			break;
+		case WBC_ID_TYPE_NOT_SPECIFIED:
+			break;
+		}
+	}
 	ret = true;
 fail:
 	TALLOC_FREE(wbc_ids);
