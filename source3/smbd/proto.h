2a804a359af5bb171afc0d04bb91e48601b8c067
Stefan Metzmacher <metze@samba.org>
2012-06-08 10:11:57 +0200

    s3:smbd: add file_fsp_smb2()
    metze

--- source3/smbd/proto.h
+++ source3/smbd/proto.h
@@ -387,6 +387,9 @@ bool file_find_subpath(files_struct *dir_fsp);
 void file_sync_all(connection_struct *conn);
 void file_free(struct smb_request *req, files_struct *fsp);
 files_struct *file_fsp(struct smb_request *req, uint16 fid);
+struct files_struct *file_fsp_smb2(struct smbd_smb2_request *smb2req,
+				   uint64_t persistent_id,
+				   uint64_t volatile_id);
 NTSTATUS dup_file_fsp(struct smb_request *req, files_struct *from,
 		      uint32 access_mask, uint32 share_access,
 		      uint32 create_options, files_struct *to);
