91be6b42f1a23d716d5f1116ebbb0ad71619ed1a
Volker Lendecke <vl@samba.org>
2012-06-08 14:25:23 +0200

    s3: Assigning ?True:False to a bool is a bit pointless
    Signed-off-by: Michael Adam <obnox@samba.org>

--- source3/smbd/files.c
+++ source3/smbd/files.c
@@ -603,11 +603,11 @@ NTSTATUS dup_file_fsp(struct smb_request *req, files_struct *from,
 	to->share_access = share_access;
 	to->oplock_type = from->oplock_type;
 	to->can_lock = from->can_lock;
-	to->can_read = (access_mask & FILE_READ_DATA) ? True : False;
+	to->can_read = ((access_mask & FILE_READ_DATA) != 0);
 	if (!CAN_WRITE(from->conn)) {
 		to->can_write = False;
 	} else {
-		to->can_write = (access_mask & (FILE_WRITE_DATA | FILE_APPEND_DATA)) ? True : False;
+		to->can_write = ((access_mask & (FILE_WRITE_DATA | FILE_APPEND_DATA)) != 0);
 	}
 	to->modified = from->modified;
 	to->is_directory = from->is_directory;
