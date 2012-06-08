fac6191f3c1f719a482f04e60ce7828197858e9a
Volker Lendecke <vl@samba.org>
2012-06-08 11:59:18 +0200

    s3: Make vfswrap_audit_file static
    Signed-off-by: Jeremy Allison <jra@samba.org>
    
    Autobuild-User(master): Jeremy Allison <jra@samba.org>
    Autobuild-Date(master): Tue Jun 12 02:26:31 CEST 2012 on sn-devel-104

--- source3/modules/vfs_default.c
+++ source3/modules/vfs_default.c
@@ -1874,11 +1874,11 @@ static NTSTATUS vfswrap_fset_nt_acl(vfs_handle_struct *handle, files_struct *fsp
 	return result;
 }
 
-NTSTATUS vfswrap_audit_file(struct vfs_handle_struct *handle,
-			    struct smb_filename *file,
-			    struct security_acl *sacl,
-			    uint32_t access_requested,
-			    uint32_t access_denied)
+static NTSTATUS vfswrap_audit_file(struct vfs_handle_struct *handle,
+				   struct smb_filename *file,
+				   struct security_acl *sacl,
+				   uint32_t access_requested,
+				   uint32_t access_denied)
 {
 	return NT_STATUS_OK; /* Nothing to do here ... */
 }
