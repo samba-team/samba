d91182c5ae0466773d59894abc5bb29cfd2c80ad
Michael Adam <obnox@samba.org>
2012-06-11 17:50:22 +0200

    s4:heimdal: fix use of a non-existent word (existant)

--- source4/heimdal/lib/krb5/fcache.c
+++ source4/heimdal/lib/krb5/fcache.c
@@ -925,7 +925,7 @@ fcc_get_cache_next(krb5_context context, krb5_cc_cursor cursor, krb5_ccache *id)
 	    return ret;
 	fn = expandedfn;
     }
-    /* check if file exists, don't return a non existant "next" */
+    /* check if file exists, don't return a non existent "next" */
     if (strncasecmp(fn, "FILE:", 5) == 0) {
 	struct stat sb;
 	ret = stat(fn + 5, &sb);
