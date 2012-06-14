3d76007f84194f7da2cc4c063aa1d6c47a002832
Volker Lendecke <Volker.Lendecke@SerNet.DE>
2012-06-14 11:24:01 -0700

    Part 1 of fix for bug #8998 - Notify code can miss a ChDir.
    Factor out notify_parent_dir.
    (cherry picked from commit 2b92491ba7cc5fd541e3f8988ecf831c3949fb00)

--- source3/smbd/notify.c
+++ source3/smbd/notify.c
@@ -345,28 +345,40 @@ void remove_pending_change_notify_requests_by_fid(files_struct *fsp,
 	}
 }
 
+static void notify_parent_dir(connection_struct *conn,
+			      uint32 action, uint32 filter,
+			      const char *path)
+{
+	struct smb_filename smb_fname_parent;
+	char *parent;
+	const char *name;
+
+	if (!parent_dirname(talloc_tos(), path, &parent, &name)) {
+		return;
+	}
+
+	ZERO_STRUCT(smb_fname_parent);
+	smb_fname_parent.base_name = parent;
+
+	if (SMB_VFS_STAT(conn, &smb_fname_parent) == -1) {
+		goto done;
+	}
+	notify_onelevel(conn->notify_ctx, action, filter,
+			SMB_VFS_FILE_ID_CREATE(conn, &smb_fname_parent.st),
+			name);
+done:
+	TALLOC_FREE(parent);
+}
+
 void notify_fname(connection_struct *conn, uint32 action, uint32 filter,
 		  const char *path)
 {
 	char *fullpath;
-	char *parent;
-	const char *name;
 
 	if (path[0] == '.' && path[1] == '/') {
 		path += 2;
 	}
-	if (parent_dirname(talloc_tos(), path, &parent, &name)) {
-		struct smb_filename smb_fname_parent;
-
-		ZERO_STRUCT(smb_fname_parent);
-		smb_fname_parent.base_name = parent;
-
-		if (SMB_VFS_STAT(conn, &smb_fname_parent) != -1) {
-			notify_onelevel(conn->notify_ctx, action, filter,
-			    SMB_VFS_FILE_ID_CREATE(conn, &smb_fname_parent.st),
-			    name);
-		}
-	}
+	notify_parent_dir(conn, action, filter, path);
 
 	fullpath = talloc_asprintf(talloc_tos(), "%s/%s", conn->connectpath,
 				   path);
