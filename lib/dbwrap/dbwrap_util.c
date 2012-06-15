8b99d40520875ad89e14d9397cff902f8198fd25
Volker Lendecke <vl@samba.org>
2012-06-15 09:43:13 +0200

    dbwrap: dbwrap_change_int32_atomic->dbwrap_change_int32_atomic_bystring
    Signed-off-by: Michael Adam <obnox@samba.org>

--- lib/dbwrap/dbwrap_util.c
+++ lib/dbwrap/dbwrap_util.c
@@ -272,8 +272,10 @@ done:
 	return ret;
 }
 
-NTSTATUS dbwrap_change_int32_atomic(struct db_context *db, const char *keystr,
-				    int32_t *oldval, int32_t change_val)
+NTSTATUS dbwrap_change_int32_atomic_bystring(struct db_context *db,
+					     const char *keystr,
+					     int32_t *oldval,
+					     int32_t change_val)
 {
 	NTSTATUS ret;
 	struct dbwrap_change_int32_atomic_context state;
