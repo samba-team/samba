e89150597ac659f738f24fe06ef3a17559a81862
Stefan Metzmacher <metze@samba.org>
2012-06-19 11:34:04 +0200

    Revert "s3-lib Remove unused sys_fcntl_long()"
    This reverts commit 846a697e20478798288afb43cdb7a9f389a15c69.
    
    This is still used in source3/smbd/oplock_irix.c
    
    metze

--- source3/include/proto.h
+++ source3/include/proto.h
@@ -272,6 +272,7 @@ ssize_t sys_pwrite(int fd, const void *buf, size_t count, off_t off);
 ssize_t sys_send(int s, const void *msg, size_t len, int flags);
 ssize_t sys_recvfrom(int s, void *buf, size_t len, int flags, struct sockaddr *from, socklen_t *fromlen);
 int sys_fcntl_ptr(int fd, int cmd, void *arg);
+int sys_fcntl_long(int fd, int cmd, long arg);
 void update_stat_ex_mtime(struct stat_ex *dst, struct timespec write_ts);
 void update_stat_ex_create_time(struct stat_ex *dst, struct timespec create_time);
 int sys_stat(const char *fname, SMB_STRUCT_STAT *sbuf,
