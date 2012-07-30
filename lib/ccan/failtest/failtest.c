5b4792d9d29ce27a5e69aedf0fcb65373e8aae31
Andrew Bartlett <abartlet@samba.org>
2012-07-30 13:39:41 +1000

    lib/ccan: Fix format string errors found by -Werror=format in ccan failtest

--- lib/ccan/failtest/failtest.c
+++ lib/ccan/failtest/failtest.c
@@ -1259,7 +1259,7 @@ static ssize_t failtest_add_read(int fd, void *buf, size_t count, off_t off,
 				set_cleanup(p, cleanup_read, struct read_call);
 		}
 	}
-	trace("%sread %s:%u fd %i %zu@%llu -> %i\n",
+	trace("%sread %s:%u fd %i %zu@%llu -> %zd\n",
 	      is_pread ? "p" : "", file, line, fd, count, (long long)off,
 	      p->u.read.ret);
 	errno = p->error;
@@ -1361,7 +1361,7 @@ static ssize_t failtest_add_write(int fd, const void *buf,
 		else
 			p->u.write.ret = write(fd, buf, count);
 	}
-	trace("%swrite %s:%i %zu@%llu on fd %i -> %i\n",
+	trace("%swrite %s:%i %zu@%llu on fd %i -> %zd\n",
 	      p->u.write.is_pwrite ? "p" : "",
 	      file, line, count, (long long)off, fd, p->u.write.ret);
 	errno = p->error;
