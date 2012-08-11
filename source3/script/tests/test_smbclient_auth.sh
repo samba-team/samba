14285005ec5f208327ab8236e9c33bfefe38126a
Salvador I. Gonzalez <sgonzalez@codejunkie.net>
2012-08-11 10:47:58 -0400

    s3-selftest: Fix copy/paste error in test usage string
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>

--- source3/script/tests/test_smbclient_auth.sh
+++ source3/script/tests/test_smbclient_auth.sh
@@ -4,7 +4,7 @@
 
 if [ $# -lt 4 ]; then
 cat <<EOF
-Usage: test_smbclient_s3.sh SERVER SERVER_IP USERNAME PASSWORD SMBCLIENT <smbclient arguments>
+Usage: test_smbclient_auth.sh SERVER SERVER_IP USERNAME PASSWORD SMBCLIENT <smbclient arguments>
 EOF
 exit 1;
 fi
