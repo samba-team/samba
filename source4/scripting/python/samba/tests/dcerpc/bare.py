f9b9433b752a663cdfda03967bd969cac5cf16bf
Andrew Bartlett <abartlet@samba.org>
2012-08-15 20:33:46 +1000

    s4-selftest: Fix test name for samba.tests.dcerpc.bare

--- source4/scripting/python/samba/tests/dcerpc/bare.py
+++ source4/scripting/python/samba/tests/dcerpc/bare.py
@@ -17,7 +17,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-"""Tests for samba.dcerpc.bare."""
+"""Tests for samba.tests.dcerpc.bare."""
 
 from samba.dcerpc import ClientConnection
 import samba.tests
