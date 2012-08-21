02c4886863e9a4066b89f2dcb8ff853bfbda7e86
Jeremy Allison <jra@samba.org>
2012-08-21 11:24:58 -0700

    Fix bug #9098 - winbind does not refresh kerberos tickets.
    Based on work from Ian Gordon <ian.gordon@strath.ac.uk>.
    (cherry picked from commit 3f60bff699223a8895d060585f765706e167da37)
    (cherry picked from commit ecf4d5e79c4dad452600498958ebe7a8e1c81fcd)

--- source3/winbindd/winbindd_pam.c
+++ source3/winbindd/winbindd_pam.c
@@ -640,6 +640,7 @@ static NTSTATUS winbindd_raw_kerberos_login(TALLOC_CTX *mem_ctx,
 					    cc,
 					    service,
 					    user,
+					    pass,
 					    realm,
 					    uid,
 					    time(NULL),
@@ -957,6 +958,7 @@ static NTSTATUS winbindd_dual_pam_auth_cached(struct winbindd_domain *domain,
 							    cc,
 							    service,
 							    state->request->data.auth.user,
+							    state->request->data.auth.pass,
 							    domain->alt_name,
 							    uid,
 							    time(NULL),
@@ -2104,6 +2106,13 @@ enum winbindd_result winbindd_dual_pam_logoff(struct winbindd_domain *domain,
 			nt_errstr(result)));
 		goto process_result;
 	}
+
+	/*
+	 * Remove any mlock'ed memory creds in the child
+	 * we might be using for krb5 ticket renewal.
+	 */
+
+	winbindd_delete_memory_creds(state->request->data.logoff.user);
 
 #else
 	result = NT_STATUS_NOT_SUPPORTED;
