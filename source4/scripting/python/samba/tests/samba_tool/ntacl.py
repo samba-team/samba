7b86c18f38412c621b3c316776067d949b0b0bbb
Andrew Bartlett <abartlet@samba.org>
2012-09-05 18:13:53 +1000

    selftest: Add python blackbox tests for samba-tool ntacl get/set
    Autobuild-User(master): Andrew Bartlett <abartlet@samba.org>
    Autobuild-Date(master): Wed Sep  5 15:47:55 CEST 2012 on sn-devel-104

--- source4/scripting/python/samba/tests/samba_tool/ntacl.py
+++ source4/scripting/python/samba/tests/samba_tool/ntacl.py
@@ -22,9 +22,10 @@ import os
 import time
 import ldb
 from samba.tests.samba_tool.base import SambaToolCmdTest
+import random
 
-class NtACLCmdTestCase(SambaToolCmdTest):
-    """Tests for samba-tool ntacl subcommands"""
+class NtACLCmdSysvolTestCase(SambaToolCmdTest):
+    """Tests for samba-tool ntacl sysvol* subcommands"""
 
 
     def test_ntvfs(self):
@@ -68,3 +69,67 @@ class NtACLCmdTestCase(SambaToolCmdTest):
         self.assertEquals(err,"","Shouldn't be any error messages")
         self.assertEquals(out,"","Shouldn't be any output messages")
 
+class NtACLCmdGetSetTestCase(SambaToolCmdTest):
+    """Tests for samba-tool ntacl get/set subcommands"""
+
+    acl = "O:DAG:DUD:P(A;OICI;0x001f01ff;;;DA)(A;OICI;0x001f01ff;;;EA)(A;OICIIO;0x001f01ff;;;CO)(A;OICI;0x001f01ff;;;DA)(A;OICI;0x001f01ff;;;SY)(A;OICI;0x001200a9;;;AU)(A;OICI;0x001200a9;;;ED)S:AI(OU;CIIDSA;WP;f30e3bbe-9ff0-11d1-b603-0000f80367c1;bf967aa5-0de6-11d0-a285-00aa003049e2;WD)(OU;CIIDSA;WP;f30e3bbf-9ff0-11d1-b603-0000f80367c1;bf967aa5-0de6-11d0-a285-00aa003049e2;WD)"
+
+
+    def test_ntvfs(self):
+        path = os.environ['SELFTEST_PREFIX']
+        tempf = os.path.join(path,"pytests"+str(int(100000*random.random())))
+        open(tempf, 'w').write("empty")
+
+        (result, out, err) =  self.runsubcmd("ntacl", "set", self.acl, tempf,
+                                             "--use-ntvfs")
+        self.assertCmdSuccess(result)
+        self.assertEquals(out,"","Shouldn't be any output messages")
+        self.assertIn("Please note that POSIX permissions have NOT been changed, only the stored NT ACL", err)
+
+    def test_s3fs(self):
+        path = os.environ['SELFTEST_PREFIX']
+        tempf = os.path.join(path,"pytests"+str(int(100000*random.random())))
+        open(tempf, 'w').write("empty")
+
+        (result, out, err) =  self.runsubcmd("ntacl", "set", self.acl, tempf,
+                                             "--use-s3fs")
+
+        self.assertCmdSuccess(result)
+        self.assertEquals(err,"","Shouldn't be any error messages")
+        self.assertEquals(out,"","Shouldn't be any output messages")
+
+    def test_ntvfs_check(self):
+        path = os.environ['SELFTEST_PREFIX']
+        tempf = os.path.join(path,"pytests"+str(int(100000*random.random())))
+        open(tempf, 'w').write("empty")
+
+        (result, out, err) =  self.runsubcmd("ntacl", "set", self.acl, tempf,
+                                             "--use-ntvfs")
+        self.assertCmdSuccess(result)
+        self.assertEquals(out,"","Shouldn't be any output messages")
+        self.assertIn("Please note that POSIX permissions have NOT been changed, only the stored NT ACL", err)
+
+        # Now check they were set correctly
+        (result, out, err) =  self.runsubcmd("ntacl",  "get", tempf,
+                                             "--use-ntvfs", "--as-sddl")
+        self.assertCmdSuccess(result)
+        self.assertEquals(err,"","Shouldn't be any error messages")
+        self.assertEquals(self.acl+"\n", out, "Output should be the ACL")
+
+    def test_s3fs_check(self):
+        path = os.environ['SELFTEST_PREFIX']
+        tempf = os.path.join(path,"pytests"+str(int(100000*random.random())))
+        open(tempf, 'w').write("empty")
+
+        (result, out, err) =  self.runsubcmd("ntacl", "set", self.acl, tempf,
+                                             "--use-s3fs")
+        self.assertCmdSuccess(result)
+        self.assertEquals(out,"","Shouldn't be any output messages")
+        self.assertEquals(err,"","Shouldn't be any error messages")
+
+        # Now check they were set correctly
+        (result, out, err) =  self.runsubcmd("ntacl",  "get", tempf,
+                                             "--use-s3fs", "--as-sddl")
+        self.assertCmdSuccess(result)
+        self.assertEquals(err,"","Shouldn't be any error messages")
+        self.assertEquals(self.acl+"\n", out,"Output should be the ACL")
