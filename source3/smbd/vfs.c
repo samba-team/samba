56b752512597acaffa94a979ef9140760e0f5cf3
Jeremy Allison <jra@samba.org>
2012-09-13 10:13:21 -0700

    Sigh :-(. Removing optimization prematurely is the root of all evil :-(.
    Sorry for the mistake, but the LastDir singleton cache in vfs_ChDir()
    actually plays an important role. When we're processing a stream of
    SMB1/SMB2/SMB3 requests we don't want to add a chdir()/getcwd()
    system call pair on every request if they're all on the same connection
    and dealing with the same base path.
    
    I did some testing with a program that times 1,000,000 chdir()
    requests vs. 1,000,000 strcmp requests and it's a penalty of 10x
    doing the system calls.
    
    Just because it's old code, doesn't mean it's bad :-(.
    
    Autobuild-User(master): Jeremy Allison <jra@samba.org>
    Autobuild-Date(master): Thu Sep 13 21:31:42 CEST 2012 on sn-devel-104

--- source3/smbd/vfs.c
+++ source3/smbd/vfs.c
@@ -801,7 +801,15 @@ int vfs_ChDir(connection_struct *conn, const char *path)
 {
 	int ret;
 
+	if (!LastDir) {
+		LastDir = SMB_STRDUP("");
+	}
+
 	if (strcsequal(path,".")) {
+		return 0;
+	}
+
+	if (*path == '/' && strcsequal(LastDir,path)) {
 		return 0;
 	}
 
@@ -809,6 +817,11 @@ int vfs_ChDir(connection_struct *conn, const char *path)
 
 	ret = SMB_VFS_CHDIR(conn,path);
 	if (ret == 0) {
+		/* Global cache. */
+		SAFE_FREE(LastDir);
+		LastDir = SMB_STRDUP(path);
+
+		/* conn cache. */
 		TALLOC_FREE(conn->cwd);
 		conn->cwd = vfs_GetWd(conn, conn);
 		DEBUG(4,("vfs_ChDir got %s\n",conn->cwd));
