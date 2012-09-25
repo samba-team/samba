83f60672e1b3069e6b1b90b376460da895e37df3
Jeremy Allison <jra@samba.org>
2012-09-25 16:35:09 -0700

    Fix bug #9213 - Bad ASN.1 NegTokenInit packet can cause invalid free.
    Not the correct fix for the specific issue, but a general fix to
    make sure this can never happen again.
    
    Autobuild-User(master): Jeremy Allison <jra@samba.org>
    Autobuild-Date(master): Wed Sep 26 04:07:57 CEST 2012 on sn-devel-104

--- source3/libsmb/clispnego.c
+++ source3/libsmb/clispnego.c
@@ -103,6 +103,10 @@ bool spnego_parse_negTokenInit(TALLOC_CTX *ctx,
 	bool ret;
 	ASN1_DATA *data;
 
+	for (i = 0; i < ASN1_MAX_OIDS; i++) {
+		OIDs[i] = NULL;
+	}
+
 	data = asn1_init(talloc_tos());
 	if (data == NULL) {
 		return false;
