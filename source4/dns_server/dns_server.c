4b83c43ac27040585fefd61f18d3d111fe3d8a21
Matthieu Patou <mat@matws.net>
2012-10-03 00:37:34 -0700

    s4-dns: fix a warning
    Autobuild-User(master): Matthieu Patou <mat@samba.org>
    Autobuild-Date(master): Mon Oct  8 10:45:41 CEST 2012 on sn-devel-104

--- source4/dns_server/dns_server.c
+++ source4/dns_server/dns_server.c
@@ -566,10 +566,9 @@ static void dns_udp_call_sendto_done(struct tevent_req *subreq)
 {
 	struct dns_udp_call *call = tevent_req_callback_data(subreq,
 				       struct dns_udp_call);
-	ssize_t ret;
 	int sys_errno;
 
-	ret = tdgram_sendto_queue_recv(subreq, &sys_errno);
+	tdgram_sendto_queue_recv(subreq, &sys_errno);
 
 	/* We don't care about errors */
 
