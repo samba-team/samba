76b38f75e98ed68de9e649c2a0ef3b4b317292ae
Andrew Bartlett <abartlet@samba.org>
2012-10-10 16:53:22 +1100

    vfs: Use posix_sys_acl_blob_get_file in vfs_default for posix ACLs

--- source3/modules/vfs_default.c
+++ source3/modules/vfs_default.c
@@ -2365,6 +2365,8 @@ static struct vfs_fn_pointers vfs_default_fns = {
 
 	.sys_acl_get_file_fn = vfswrap_sys_acl_get_file,
 	.sys_acl_get_fd_fn = vfswrap_sys_acl_get_fd,
+	.sys_acl_blob_get_file_fn = posix_sys_acl_blob_get_file,
+	.sys_acl_blob_get_fd_fn = posix_sys_acl_blob_get_fd,
 	.sys_acl_set_file_fn = vfswrap_sys_acl_set_file,
 	.sys_acl_set_fd_fn = vfswrap_sys_acl_set_fd,
 	.sys_acl_delete_def_file_fn = vfswrap_sys_acl_delete_def_file,
