598b565c632aae8792dc2917eda2e2b7365d4cec
Andrew Bartlett <abartlet@samba.org>
2012-10-10 16:59:25 +1100

    vfs: Use posix_sys_acl_blob_get_file in vfs_irix for posix ACLs

--- source3/modules/vfs_irixacl.c
+++ source3/modules/vfs_irixacl.c
@@ -73,6 +73,8 @@ int irixacl_sys_acl_delete_def_file(vfs_handle_struct *handle,
 static struct vfs_fn_pointers irixacl_fns = {
 	.sys_acl_get_file_fn = irixacl_sys_acl_get_file,
 	.sys_acl_get_fd_fn = irixacl_sys_acl_get_fd,
+	.sys_acl_blob_get_file_fn = posix_sys_acl_blob_get_file,
+	.sys_acl_blob_get_fd_fn = posix_sys_acl_blob_get_fd,
 	.sys_acl_set_file_fn = irixacl_sys_acl_set_file,
 	.sys_acl_set_fd_fn = irixacl_sys_acl_set_fd,
 	.sys_acl_delete_def_file_fn = irixacl_sys_acl_delete_def_file,
