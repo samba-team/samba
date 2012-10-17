1d488270a116b3140de1355c1492aa2652a57e08
Volker Lendecke <vl@samba.org>
2012-10-17 12:11:37 +0200

    s3-shadow-copy2: Add more debugs
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Christof Schmitt <cs@samba.org>

--- source3/modules/vfs_shadow_copy2.c
+++ source3/modules/vfs_shadow_copy2.c
@@ -1673,21 +1673,30 @@ static int shadow_copy2_get_real_filename(struct vfs_handle_struct *handle,
 	int saved_errno;
 	char *conv;
 
+	DEBUG(10, ("shadow_copy2_get_real_filename called for path=[%s], "
+		   "name=[%s]\n", path, name));
+
 	if (!shadow_copy2_strip_snapshot(talloc_tos(), handle, path,
 					 &timestamp, &stripped)) {
+		DEBUG(10, ("shadow_copy2_strip_snapshot failed\n"));
 		return -1;
 	}
 	if (timestamp == 0) {
+		DEBUG(10, ("timestamp == 0\n"));
 		return SMB_VFS_NEXT_GET_REAL_FILENAME(handle, path, name,
 						      mem_ctx, found_name);
 	}
 	conv = shadow_copy2_convert(talloc_tos(), handle, stripped, timestamp);
 	TALLOC_FREE(stripped);
 	if (conv == NULL) {
+		DEBUG(10, ("shadow_copy2_convert failed\n"));
 		return -1;
 	}
+	DEBUG(10, ("Calling NEXT_GET_REAL_FILE_NAME for conv=[%s], "
+		   "name=[%s]\n", conv, name));
 	ret = SMB_VFS_NEXT_GET_REAL_FILENAME(handle, conv, name,
 					     mem_ctx, found_name);
+	DEBUG(10, ("NEXT_REAL_FILE_NAME returned %d\n", (int)ret));
 	saved_errno = errno;
 	TALLOC_FREE(conv);
 	errno = saved_errno;
