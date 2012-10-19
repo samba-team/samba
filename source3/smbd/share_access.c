3da5d964b9821a589be39ad076ed56000952382b
Volker Lendecke <vl@samba.org>
2012-10-19 21:48:20 +0200

    s3: Fix some blank line endings
    Autobuild-User(master): Volker Lendecke <vl@samba.org>
    Autobuild-Date(master): Sat Oct 20 09:06:12 CEST 2012 on sn-devel-104

--- source3/smbd/share_access.c
+++ source3/smbd/share_access.c
@@ -1,18 +1,18 @@
-/* 
+/*
    Unix SMB/CIFS implementation.
    Check access based on valid users, read list and friends
    Copyright (C) Volker Lendecke 2005
-   
+
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -90,7 +90,7 @@ static bool token_contains_name(TALLOC_CTX *mem_ctx,
 		 * result that might be interpreted in a wrong way. */
 		smb_panic("substitutions failed");
 	}
-	
+
 	/* check to see is we already have a SID */
 
 	if ( string_to_sid( &sid, name ) ) {
