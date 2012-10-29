b59fed0d5d7dceff3e5ab47a6f219849a24cff1b
Michael Adam <obnox@samba.org>
2012-10-29 14:52:50 +0100

    lib/param: fix function name (set_variable) in debug statements
    Signed-off-by: Michael Adam <obnox@samba.org>
    Reviewed-by: Ira Cooper <ira@samba.org>

--- lib/param/loadparm.c
+++ lib/param/loadparm.c
@@ -1300,7 +1300,7 @@ static bool set_variable(TALLOC_CTX *mem_ctx, int parmnum, void *parm_ptr,
 		case P_BOOL: {
 			bool b;
 			if (!set_boolean(pszParmValue, &b)) {
-				DEBUG(0,("lp_do_parameter(%s): value is not boolean!\n", pszParmValue));
+				DEBUG(0,("set_variable(%s): value is not boolean!\n", pszParmValue));
 				return false;
 			}
 			*(bool *)parm_ptr = b;
@@ -1310,7 +1310,7 @@ static bool set_variable(TALLOC_CTX *mem_ctx, int parmnum, void *parm_ptr,
 		case P_BOOLREV: {
 			bool b;
 			if (!set_boolean(pszParmValue, &b)) {
-				DEBUG(0,("lp_do_parameter(%s): value is not boolean!\n", pszParmValue));
+				DEBUG(0,("set_variable(%s): value is not boolean!\n", pszParmValue));
 				return false;
 			}
 			*(bool *)parm_ptr = !b;
@@ -1339,8 +1339,8 @@ static bool set_variable(TALLOC_CTX *mem_ctx, int parmnum, void *parm_ptr,
 				}
 			}
 
-			DEBUG(0,("lp_do_parameter(%s): value is not "
-			    "a valid size specifier!\n", pszParmValue));
+			DEBUG(0, ("set_variable(%s): value is not "
+			          "a valid size specifier!\n", pszParmValue));
 			return false;
 		}
 
