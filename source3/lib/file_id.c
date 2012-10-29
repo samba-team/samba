e44b231f5823bd7a08a65391094db1b7b1eb94e8
Volker Lendecke <vl@samba.org>
2012-10-29 16:35:49 +0100

    s3: Fix some nonempty line endings
    Reviewed by Jeremy Allison <jra@samba.org>

--- source3/lib/file_id.c
+++ source3/lib/file_id.c
@@ -1,20 +1,20 @@
-/* 
+/*
    Unix SMB/CIFS implementation.
 
    file_id structure handling
 
    Copyright (C) Andrew Tridgell 2007
-   
+
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -37,7 +37,7 @@ bool file_id_equal(const struct file_id *id1, const struct file_id *id2)
 const char *file_id_string_tos(const struct file_id *id)
 {
 	char *result = talloc_asprintf(talloc_tos(), "%llx:%llx:%llx",
-				       (unsigned long long)id->devid, 
+				       (unsigned long long)id->devid,
 				       (unsigned long long)id->inode,
 				       (unsigned long long)id->extid);
 	SMB_ASSERT(result != NULL);
