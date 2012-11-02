775d0a78c496af8ddbde9eb0f8c2f6d5dc5bcc81
Christian Ambach <ambi@samba.org>
2012-11-02 08:41:40 +0100

    s3:vfs_gpfs fix a memory leak in gpfsacl_get_posix_acl
    Signed-off-by: Christian Ambach <ambi@samba.org>
    
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- source3/modules/vfs_gpfs.c
+++ source3/modules/vfs_gpfs.c
@@ -663,6 +663,9 @@ static SMB_ACL_T gpfsacl_get_posix_acl(const char *path, gpfs_aclType_t type,
 
  done:
 
+	if (pacl != NULL) {
+		talloc_free(pacl);
+	}
 	if (errno != 0) {
 		TALLOC_FREE(result);
 	}
