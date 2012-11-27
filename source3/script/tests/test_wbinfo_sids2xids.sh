b356ad6c24f1c94dcb4274b1b4c994a5ce4f3582
Michael Adam <obnox@samba.org>
2012-11-27 12:08:33 +0100

    s3:test:wbinfo_sids2xids: test the results with singular calls with filled and with empty cache
    Signed-off-by: Michael Adam <obnox@samba.org>
    Reviewed-by: Stefan Metzmacher <metze@samba.org>
    (cherry picked from commit 7f2f29647a5d5906db5a267f614f30607d9162e3)

--- source3/script/tests/test_wbinfo_sids2xids.sh
+++ source3/script/tests/test_wbinfo_sids2xids.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 WBINFO="$VALGRIND ${WBINFO:-$BINDIR/wbinfo} $CONFIGURATION"
+NET="$VALGRIND ${NET:-$BINDIR/net} $CONFIGURATION"
 TEST_INT=`dirname $0`/test_wbinfo_sids2xids_int.py
 
 incdir=`dirname $0`/../../../testprogs/blackbox
 . $incdir/subunit.sh
 
-testit "sids2xids" ${TEST_INT} ${WBINFO} || failed=`expr $failed + 1`
+testit "sids2xids" ${TEST_INT} ${WBINFO} ${NET} || failed=`expr $failed + 1`
 
 testok $0 $failed
