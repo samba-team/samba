2b73dfbc8e994ae8b7eea6cf17fcbcfb2e2a074f
Stefan Metzmacher <metze@samba.org>
2012-11-28 09:05:23 +0100

    s3:vfs_modules: fix *sys_acl_blob_get_{file,fd} and only return ENOSYS
    We should not segfault if some callers starts to call this.
    
    This is a 4.0 patch only, if you try to backport the real implementation
    just revert this patch...
    
    Signed-off-by: Stefan Metzmacher <metze@samba.org>

--- source3/modules/vfs_default.c
+++ source3/modules/vfs_default.c
@@ -2147,6 +2147,26 @@ static SMB_ACL_T vfswrap_sys_acl_get_fd(vfs_handle_struct *handle,
 	return sys_acl_get_fd(handle, fsp, mem_ctx);
 }
 
+static int vfswrap_sys_acl_blob_get_file(vfs_handle_struct *handle,
+					 const char *path_p,
+					 TALLOC_CTX *mem_ctx,
+					 char **blob_description,
+					 DATA_BLOB *blob)
+{
+	errno = ENOSYS;
+	return -1;
+}
+
+static int vfswrap_sys_acl_blob_get_fd(vfs_handle_struct *handle,
+				       files_struct *fsp,
+				       TALLOC_CTX *mem_ctx,
+				       char **blob_description,
+				       DATA_BLOB *blob)
+{
+	errno = ENOSYS;
+	return -1;
+}
+
 static int vfswrap_sys_acl_set_file(vfs_handle_struct *handle,  const char *name, SMB_ACL_TYPE_T acltype, SMB_ACL_T theacl)
 {
 	return sys_acl_set_file(handle, name, acltype, theacl);
@@ -2382,6 +2402,8 @@ static struct vfs_fn_pointers vfs_default_fns = {
 
 	.sys_acl_get_file_fn = vfswrap_sys_acl_get_file,
 	.sys_acl_get_fd_fn = vfswrap_sys_acl_get_fd,
+	.sys_acl_blob_get_file_fn = vfswrap_sys_acl_blob_get_file,
+	.sys_acl_blob_get_fd_fn = vfswrap_sys_acl_blob_get_fd,
 	.sys_acl_set_file_fn = vfswrap_sys_acl_set_file,
 	.sys_acl_set_fd_fn = vfswrap_sys_acl_set_fd,
 	.sys_acl_delete_def_file_fn = vfswrap_sys_acl_delete_def_file,
