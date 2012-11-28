457c933ff04e8ffc11ad1b94bbd9d28c48109066
Günther Deschner <gd@samba.org>
2012-11-28 14:53:27 +0100

    s3-rpc_cli: Remove some unused wrapping code.
    Guenther
    
    Signed-off-by: Günther Deschner <gd@samba.org>
    Reviewed-by: Andreas Schneider <asn@samba.org>

--- source3/rpc_client/cli_lsarpc.h
+++ source3/rpc_client/cli_lsarpc.h
@@ -158,15 +158,6 @@ NTSTATUS dcerpc_lsa_lookup_sids3(struct dcerpc_binding_handle *h,
 				 char ***pnames,
 				 enum lsa_SidType **ptypes,
 				 NTSTATUS *result);
-NTSTATUS rpccli_lsa_lookup_sids3(struct rpc_pipe_client *cli,
-				 TALLOC_CTX *mem_ctx,
-				 struct policy_handle *pol,
-				 int num_sids,
-				 const struct dom_sid *sids,
-				 char ***pdomains,
-				 char ***pnames,
-				 enum lsa_SidType **ptypes);
-
 NTSTATUS dcerpc_lsa_lookup_names(struct dcerpc_binding_handle *h,
 				 TALLOC_CTX *mem_ctx,
 				 struct policy_handle *pol,
@@ -196,14 +187,6 @@ NTSTATUS dcerpc_lsa_lookup_names4(struct dcerpc_binding_handle *h,
 				  struct dom_sid **sids,
 				  enum lsa_SidType **types,
 				  NTSTATUS *result);
-NTSTATUS rpccli_lsa_lookup_names4(struct rpc_pipe_client *cli,
-				  TALLOC_CTX *mem_ctx,
-				  struct policy_handle *pol, int num_names,
-				  const char **names,
-				  const char ***dom_names,
-				  int level,
-				  struct dom_sid **sids,
-				  enum lsa_SidType **types);
 
 bool fetch_domain_sid( char *domain, char *remote_machine, struct dom_sid *psid);
 
