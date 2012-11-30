ecb567fb4a4c5e1c8b8c9ef53548c2e427305fa4
Michael Adam <obnox@samba.org>
2012-11-30 15:27:15 +0100

    s3:winbindd: remove unused function idmap_backends_sid_to_unixid()
    Signed-off-by: Michael Adam <obnox@samba.org>
    Reviewed-by: Stefan Metzmacher <metze@samba.org>
    (cherry picked from commit e3ee3971403c7dac4e8e3578a60973b97451af68)

--- source3/winbindd/idmap_proto.h
+++ source3/winbindd/idmap_proto.h
@@ -36,8 +36,6 @@ NTSTATUS idmap_allocate_uid(struct unixid *id);
 NTSTATUS idmap_allocate_gid(struct unixid *id);
 NTSTATUS idmap_backends_unixid_to_sid(const char *domname,
 				      struct id_map *id);
-NTSTATUS idmap_backends_sid_to_unixid(const char *domname,
-				      struct id_map *id);
 
 /* The following definitions come from winbindd/idmap_nss.c  */
 
