fecdf48aaf514e6cda5cd0412d7407319a3ff89f
Michael Adam <obnox@samba.org>
2012-11-30 12:27:00 +0100

    s3:lib: add utility function sid_check_is_for_passdb()
    This function checks whether the given sid should be treated
    by passdb (e.g. for id mapping).
    
    Signed-off-by: Michael Adam <obnox@samba.org>
    Reviewed-by: Stefan Metzmacher <metze@samba.org>

---
+++ source3/lib/util_sid_passdb.c
@@ -0,0 +1,72 @@
+/*
+   Unix SMB/CIFS implementation.
+   sid utility functions
+
+   Copyright (C) Michael Adam 2012
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "includes.h"
+#include "lib/util_sid_passdb.h"
+#include "passdb/machine_sid.h"
+
+/**
+ * check whether this is an object- or domain-sid that should
+ * be treated by the passdb, e.g. for id-mapping.
+ */
+bool sid_check_is_for_passdb(const struct dom_sid *sid)
+{
+	if (sid_check_is_our_sam(sid)) {
+		return true;
+	}
+
+	if (sid_check_is_in_our_sam(sid)) {
+		return true;
+	}
+
+	if (sid_check_is_builtin(sid)) {
+		return true;
+	}
+
+	if (sid_check_is_in_builtin(sid)) {
+		return true;
+	}
+
+	if (sid_check_is_wellknown_domain(sid, NULL)) {
+		return true;
+	}
+
+	if (sid_check_is_in_wellknown_domain(sid)) {
+		return true;
+	}
+
+	if (sid_check_is_unix_users(sid)) {
+		return true;
+	}
+
+	if (sid_check_is_in_unix_users(sid)) {
+		return true;
+	}
+
+	if (sid_check_is_unix_groups(sid)) {
+		return true;
+	}
+
+	if (sid_check_is_in_unix_groups(sid)) {
+		return true;
+	}
+
+	return false;
+}
