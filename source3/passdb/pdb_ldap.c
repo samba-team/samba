0ec17f634f8023a40ad7f0523ad570f3c7d233c9
Michael Adam <obnox@samba.org>
2012-12-03 01:44:49 +0100

    s3:passdb:pdb_ldap: treat "Unix User" and "Unix Group" in sid_to_id()
    Signed-off-by: Michael Adam <obnox@samba.org>
    Reviewed-by: Stefan Metzmacher <metze@samba.org>
    (cherry picked from commit 5fbdc5f35a122ff040c6120e2aa2cf5485e32097)

--- source3/passdb/pdb_ldap.c
+++ source3/passdb/pdb_ldap.c
@@ -4943,6 +4943,11 @@ static bool ldapsam_sid_to_id(struct pdb_methods *methods,
 		return false;
 	}
 
+	ret = pdb_sid_to_id_unix_users_and_groups(sid, id);
+	if (ret == true) {
+		return true;
+	}
+
 	mem_ctx = talloc_new(NULL);
 	if (mem_ctx == NULL) {
 		DEBUG(0, ("talloc_new failed\n"));
