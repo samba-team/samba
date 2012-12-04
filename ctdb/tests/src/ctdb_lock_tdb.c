1d9d1d8cf9b5a9b2ce5e21634d465ec3a06a3a3d
Amitay Isaacs <amitay@gmail.com>
2012-12-04 18:27:10 +1100

    tests: Add a test program to hold a lock on a database
    Signed-off-by: Amitay Isaacs <amitay@gmail.com>
    
    (This used to be ctdb commit f6b066a23610fb0092298861c21a9b354b91e2f1)

---
+++ ctdb/tests/src/ctdb_lock_tdb.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <fcntl.h>
+
+#include "includes.h"
+
+const char *tdb_file;
+TDB_CONTEXT *tdb;
+
+void signal_handler(int signum)
+{
+	tdb_close(tdb);
+}
+
+
+int
+main(int argc, char *argv[])
+{
+	if (argc != 2) {
+		printf("Usage: %s <tdb file>\n", argv[0]);
+		exit(1);
+	}
+
+	tdb_file = argv[1];
+
+	tdb = tdb_open(tdb_file, 0, 0, O_RDWR, 0);
+	if (tdb == NULL) {
+		fprintf(stderr, "Failed to open TDB file %s\n", tdb_file);
+		exit(1);
+	}
+
+	signal(SIGINT, signal_handler);
+
+	if (tdb_lockall(tdb) != 0) {
+		fprintf(stderr, "Failed to lock database %s\n", tdb_file);
+		tdb_close(tdb);
+		exit(1);
+	}
+
+	sleep(999999);
+
+	return 0;
+}
