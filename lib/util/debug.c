25eb1af07cc09e5e019a0702c60a763cc3266196
Tsukasa Hamano <hamano@osstech.co.jp>
2012-12-06 13:01:33 -0800

    Fix bug #9471 - SEGV when using second vfs module.
    Don't use default_classname_table when we obviously shoud be using
    classname_table.
    
    Reviewed by: Jeremy Allison <jra@samba.org>
    Reviewed-by: Michael Adam <obnox@samba.org>
    
    Autobuild-User(master): Michael Adam <obnox@samba.org>
    Autobuild-Date(master): Fri Dec  7 17:51:50 CET 2012 on sn-devel-104
    (cherry picked from commit 16d725b4f5ed77db865e2a3c27ae0eb4accca5a8)

--- lib/util/debug.c
+++ lib/util/debug.c
@@ -1007,7 +1007,7 @@ bool dbghdrclass(int level, int cls, const char *location, const char *func)
 			slprintf(header_str + hs_len,
 				 sizeof(header_str) -1 - hs_len,
 				 ", class=%s",
-				 default_classname_table[cls]);
+				 classname_table[cls]);
 		}
 
 		/* Print it all out at once to prevent split syslog output. */
