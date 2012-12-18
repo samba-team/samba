53f54432831ba1c369beaed7d4ca95749eb2ee39
Andreas Schneider <asn@samba.org>
2012-12-18 16:39:40 +0100

    s4-dsdb: Make it clear that we want to fall trough here.
    Found by Coverity.
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Günther Deschner <gd@samba.org>

--- source4/dsdb/samdb/cracknames.c
+++ source4/dsdb/samdb/cracknames.c
@@ -1029,6 +1029,7 @@ static WERROR DsCrackNameOneFilter(struct ldb_context *sam_ctx, TALLOC_CTX *mem_
 				return WERR_OK;
 			}
 		}
+		/* FALL TROUGH */
 		default:
 			info1->status = DRSUAPI_DS_NAME_STATUS_NOT_UNIQUE;
 			return WERR_OK;
