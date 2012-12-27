0615f68096569d00b1f262529024ad40136d445e
Ira Cooper <ira@samba.org>
2012-12-27 19:57:14 +0000

    s3: Fix vfs_zfsacl to compile.
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- source3/modules/vfs_zfsacl.c
+++ source3/modules/vfs_zfsacl.c
@@ -201,13 +201,13 @@ static NTSTATUS zfsacl_fget_nt_acl(struct vfs_handle_struct *handle,
 	NTSTATUS status;
 
 	status = zfs_get_nt_acl_common(fsp->fsp_name->base_name,
-				       mem_ctx, security_info,
+				       security_info,
 				       &pacl);
 	if (!NT_STATUS_IS_OK(status)) {
 		return status;
 	}
 
-	return smb_fget_nt_acl_nfs4(fsp, security_info, ppdesc, pacl);
+	return smb_fget_nt_acl_nfs4(fsp, security_info, mem_ctx, ppdesc, pacl);
 }
 
 static NTSTATUS zfsacl_get_nt_acl(struct vfs_handle_struct *handle,
@@ -306,7 +306,7 @@ static int zfsacl_fail__sys_acl_blob_get_file(vfs_handle_struct *handle, const c
 	return -1;
 }
 
-static int zfsacl_fail__skel_sys_acl_blob_get_fd(vfs_handle_struct *handle, files_struct *fsp, TALLOC_CTX *mem_ctx, char **blob_description, DATA_BLOB *blob)
+static int zfsacl_fail__sys_acl_blob_get_fd(vfs_handle_struct *handle, files_struct *fsp, TALLOC_CTX *mem_ctx, char **blob_description, DATA_BLOB *blob)
 {
 	return -1;
 }
