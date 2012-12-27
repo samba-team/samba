samba-team/samba

commit 5b1d95046c8ea624419d94dd7d9e2785ba86f556

param: Correctly create directory and create common function.
Found by Coverity.

Signed-off-by: Andreas Schneider <asn@samba.org>
Reviewed-by: Günther Deschner <gd@samba.org>

lib/param/util.c
@@ -71,14 +71,13 @@ bool lpcfg_is_myname(struct loadparm_context *lp_ctx, const char *name)
 	return false;
 }
 
-
-/**
- A useful function for returning a path in the Samba lock directory.
-**/
-char *lpcfg_lock_path(TALLOC_CTX* mem_ctx, struct loadparm_context *lp_ctx,
-			 const char *name)
+static char *lpcfg_common_path(TALLOC_CTX* mem_ctx,
+			       struct loadparm_context *lp_ctx,
+			       const char *name)
 {
 	char *fname, *dname;
+	bool ok;
+
 	if (name == NULL) {
 		return NULL;
 	}
@@ -87,19 +86,35 @@ char *lpcfg_lock_path(TALLOC_CTX* mem_ctx, struct loadparm_context *lp_ctx,
 	}
 
 	dname = talloc_strdup(mem_ctx, lpcfg_lockdir(lp_ctx));
-	trim_string(dname,"","/");
-	
-	if (!directory_exist(dname)) {
-		if (!mkdir(dname,0755))
-			DEBUG(1, ("Unable to create directory %s for file %s. "
-			      "Error was %s\n", dname, name, strerror(errno)));
+	if (dname == NULL) {
+		return NULL;
 	}
-	
-	fname = talloc_asprintf(mem_ctx, "%s/%s", dname, name);
+	trim_string(dname,"","/");
 
+	ok = directory_create_or_exist(dname, geteuid(), 0755);
+	if (!ok) {
+		DEBUG(1, ("Unable to create directory %s for file %s. "
+			  "Error was %s\n", dname, name, strerror(errno)));
+		return NULL;
+	}
+
+	fname = talloc_asprintf(mem_ctx, "%s/%s", dname, name);
+	if (fname == NULL) {
+		return dname;
+	}
 	talloc_free(dname);
 
 	return fname;
+}
+
+
+/**
+ A useful function for returning a path in the Samba lock directory.
+**/
+char *lpcfg_lock_path(TALLOC_CTX* mem_ctx, struct loadparm_context *lp_ctx,
+			 const char *name)
+{
+	return lpcfg_common_path(mem_ctx, lp_ctx, name);
 }
 
 /**
@@ -108,26 +123,7 @@ char *lpcfg_lock_path(TALLOC_CTX* mem_ctx, struct loadparm_context *lp_ctx,
 char *lpcfg_state_path(TALLOC_CTX* mem_ctx, struct loadparm_context *lp_ctx,
 		       const char *name)
 {
-	char *fname, *dname;
-	if (name == NULL) {
-		return NULL;
-	}
-	if (name[0] == 0 || name[0] == '/' || strstr(name, ":/")) {
-		return talloc_strdup(mem_ctx, name);
-	}
-
-	dname = talloc_strdup(mem_ctx, lpcfg_statedir(lp_ctx));
-	trim_string(dname,"","/");
-
-	if (!directory_exist(dname)) {
-		mkdir(dname,0755);
-	}
-
-	fname = talloc_asprintf(mem_ctx, "%s/%s", dname, name);
-
-	talloc_free(dname);
-
-	return fname;
+	return lpcfg_common_path(mem_ctx, lp_ctx, name);
 }
 
 /**
@@ -136,26 +132,7 @@ char *lpcfg_state_path(TALLOC_CTX* mem_ctx, struct loadparm_context *lp_ctx,
 char *lpcfg_cache_path(TALLOC_CTX* mem_ctx, struct loadparm_context *lp_ctx,
 		       const char *name)
 {
-	char *fname, *dname;
-	if (name == NULL) {
-		return NULL;
-	}
-	if (name[0] == 0 || name[0] == '/' || strstr(name, ":/")) {
-		return talloc_strdup(mem_ctx, name);
-	}
-
-	dname = talloc_strdup(mem_ctx, lpcfg_cachedir(lp_ctx));
-	trim_string(dname,"","/");
-
-	if (!directory_exist(dname)) {
-		mkdir(dname,0755);
-	}
-
-	fname = talloc_asprintf(mem_ctx, "%s/%s", dname, name);
-
-	talloc_free(dname);
-
-	return fname;
+	return lpcfg_common_path(mem_ctx, lp_ctx, name);
 }
 
 /**
@@ -222,10 +199,16 @@ char *smbd_tmp_path(TALLOC_CTX *mem_ctx,
 			     const char *name)
 {
 	char *fname, *dname;
+	bool ok;
 
 	dname = lpcfg_private_path(mem_ctx, lp_ctx, "smbd.tmp");
-	if (!directory_exist(dname)) {
-		mkdir(dname,0755);
+	if (dname == NULL) {
+		return NULL;
+	}
+
+	ok = directory_create_or_exist(dname, geteuid(), 0755);
+	if (!ok) {
+		return NULL;
 	}
 
 	if (name == NULL) {
@@ -233,6 +216,9 @@ char *smbd_tmp_path(TALLOC_CTX *mem_ctx,
 	}
 
 	fname = talloc_asprintf(mem_ctx, "%s/%s", dname, name);
+	if (fname == NULL) {
+		return dname;
+	}
 	talloc_free(dname);
 
 	return fname;

commit 24957527e0250a9a5d4c51ff47db9ff2eb11ee7b

lib/param: use the correct path names again
This fixes a regression which was introduced by commit
5b1d95046c8ea624419d94dd7d9e2785ba86f556.

Signed-off-by: Stefan Metzmacher <metze@samba.org>
Reviewed-by: Michael Adam <obnox@samba.org>

Autobuild-User(master): Michael Adam <obnox@samba.org>
Autobuild-Date(master): Thu Dec 27 19:19:55 CET 2012 on sn-devel-104

lib/param/util.c
@@ -72,7 +72,7 @@ bool lpcfg_is_myname(struct loadparm_context *lp_ctx, const char *name)
 }
 
 static char *lpcfg_common_path(TALLOC_CTX* mem_ctx,
-			       struct loadparm_context *lp_ctx,
+			       const char *parent,
 			       const char *name)
 {
 	char *fname, *dname;
@@ -85,7 +85,7 @@ static char *lpcfg_common_path(TALLOC_CTX* mem_ctx,
 		return talloc_strdup(mem_ctx, name);
 	}
 
-	dname = talloc_strdup(mem_ctx, lpcfg_lockdir(lp_ctx));
+	dname = talloc_strdup(mem_ctx, parent);
 	if (dname == NULL) {
 		return NULL;
 	}
@@ -114,7 +114,7 @@ static char *lpcfg_common_path(TALLOC_CTX* mem_ctx,
 char *lpcfg_lock_path(TALLOC_CTX* mem_ctx, struct loadparm_context *lp_ctx,
 			 const char *name)
 {
-	return lpcfg_common_path(mem_ctx, lp_ctx, name);
+	return lpcfg_common_path(mem_ctx, lpcfg_lockdir(lp_ctx), name);
 }
 
 /**
@@ -123,7 +123,7 @@ char *lpcfg_lock_path(TALLOC_CTX* mem_ctx, struct loadparm_context *lp_ctx,
 char *lpcfg_state_path(TALLOC_CTX* mem_ctx, struct loadparm_context *lp_ctx,
 		       const char *name)
 {
-	return lpcfg_common_path(mem_ctx, lp_ctx, name);
+	return lpcfg_common_path(mem_ctx, lpcfg_statedir(lp_ctx), name);
 }
 
 /**
@@ -132,7 +132,7 @@ char *lpcfg_state_path(TALLOC_CTX* mem_ctx, struct loadparm_context *lp_ctx,
 char *lpcfg_cache_path(TALLOC_CTX* mem_ctx, struct loadparm_context *lp_ctx,
 		       const char *name)
 {
-	return lpcfg_common_path(mem_ctx, lp_ctx, name);
+	return lpcfg_common_path(mem_ctx, lpcfg_cachedir(lp_ctx), name);
 }
 
 /**