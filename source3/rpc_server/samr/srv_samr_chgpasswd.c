4063f9e46a370800aac5242cdc60a80997773749
Volker Lendecke <vl@samba.org>
2013-01-11 10:31:42 +0100

    samr: Split up an assignment from an if condition

--- source3/rpc_server/samr/srv_samr_chgpasswd.c
+++ source3/rpc_server/samr/srv_samr_chgpasswd.c
@@ -74,7 +74,8 @@ static int findpty(char **slave)
 
 #if defined(HAVE_GRANTPT)
 	/* Try to open /dev/ptmx. If that fails, fall through to old method. */
-	if ((master = open("/dev/ptmx", O_RDWR, 0)) >= 0) {
+	master = open("/dev/ptmx", O_RDWR, 0);
+	if (master >= 0) {
 		grantpt(master);
 		unlockpt(master);
 		line = (char *)ptsname(master);
