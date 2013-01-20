6762959a45bfc78183055162ae583733e589062f
Kai Blin <kai@samba.org>
2013-01-20 08:58:08 +0100

    swat: Use additional nonce on XSRF protection
    If the user had a weak password on the root account of a machine running
    SWAT, there still was a chance of being targetted by an XSRF on a
    malicious web site targetting the SWAT setup.
    
    Use a random nonce stored in secrets.tdb to close this possible attack
    window. Thanks to Jann Horn for reporting this issue.
    
    Signed-off-by: Kai Blin <kai@samba.org>
    
    Fix bug #9577 - CVE-2013-0214: Potential XSRF in SWAT.

--- source3/web/swat.c
+++ source3/web/swat.c
@@ -156,6 +156,7 @@ void get_xsrf_token(const char *username, const char *pass,
 	struct MD5Context md5_ctx;
 	uint8_t token[16];
 	int i;
+	char *nonce = cgi_nonce();
 
 	token_str[0] = '\0';
 	ZERO_STRUCT(md5_ctx);
@@ -169,6 +170,7 @@ void get_xsrf_token(const char *username, const char *pass,
 	if (pass != NULL) {
 		MD5Update(&md5_ctx, (uint8_t *)pass, strlen(pass));
 	}
+	MD5Update(&md5_ctx, (uint8_t *)nonce, strlen(nonce));
 
 	MD5Final(token, &md5_ctx);
 
