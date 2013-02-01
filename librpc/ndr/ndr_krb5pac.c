499008021051e5b01e4c1c151403b18a9dcbcf24
Günther Deschner <gd@samba.org>
2013-02-01 17:41:00 +0100

    krb5pac: make sure to correctly store the highest relative pointer offset.
    Guenther
    
    Signed-off-by: Günther Deschner <gd@samba.org>
    Reviewed-by: Stefan Metzmacher <metze@samba.org>
    
    Autobuild-User(master): Günther Deschner <gd@samba.org>
    Autobuild-Date(master): Fri Feb  1 21:19:28 CET 2013 on sn-devel-104

--- librpc/ndr/ndr_krb5pac.c
+++ librpc/ndr/ndr_krb5pac.c
@@ -115,6 +115,9 @@ enum ndr_err_code ndr_pull_PAC_BUFFER(struct ndr_pull *ndr, int ndr_flags, struc
 					NDR_CHECK(ndr_pull_subcontext_end(ndr, _ndr_info, 0, r->_ndr_size));
 				}
 				NDR_PULL_SET_MEM_CTX(ndr, _mem_save_info_0, 0);
+				if (ndr->offset > ndr->relative_highest_offset) {
+					ndr->relative_highest_offset = ndr->offset;
+				}
 				ndr->offset = _relative_save_offset;
 			}
 			ndr->flags = _flags_save_PAC_INFO;
