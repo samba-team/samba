29aeaab1d9bf2441f4a3a9f4a8554fa9af2bd5c2
Stefan Metzmacher <metze@samba.org>
2013-02-01 03:13:35 -0500

    lib/dbwrap: talloc_strdup() name in db_open_file()
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Michael Adam <obnox@samba.org>

--- lib/dbwrap/dbwrap_file.c
+++ lib/dbwrap/dbwrap_file.c
@@ -371,8 +371,13 @@ struct db_context *db_open_file(TALLOC_CTX *mem_ctx,
 	result->traverse = db_file_traverse;
 	result->traverse_read = db_file_traverse;
 	result->persistent = ((tdb_flags & TDB_CLEAR_IF_FIRST) == 0);
-	result->name = name;
 	result->hash_size = 0;
+	result->name = talloc_strdup(result, name);
+	if (result->name == NULL) {
+		DEBUG(0, ("talloc failed\n"));
+		TALLOC_FREE(result);
+		return NULL;
+	}
 
 	ctx->locked_record = NULL;
 	if (!(ctx->dirname = talloc_strdup(ctx, name))) {
