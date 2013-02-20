22cc4599f1a5ab1092383275b03fc389cc3dfb83
Andreas Schneider <asn@samba.org>
2013-02-20 09:51:43 +0100

    winbind: Don't leak centry memory. Reviewed-by: Alexander Bokovoy <ab@samba.org>
    The last two patches address bug #9684 - Fix two resource leaks in winbindd.
    (cherry picked from commit eb657c324f83e94d46f80b9c1b97fa0228c3a47a)

--- source3/winbindd/winbindd_cache.c
+++ source3/winbindd/winbindd_cache.c
@@ -2130,6 +2130,7 @@ static NTSTATUS rids_to_names(struct winbindd_domain *domain,
 				} else {
 					/* something's definitely wrong */
 					result = centry->status;
+					centry_free(centry);
 					goto error;
 				}
 
