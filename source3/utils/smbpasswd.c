312355ad59001fcc1eaa30344d69fc8c91acebc8
Andreas Schneider <asn@samba.org>
2013-02-20 10:25:55 +0100

    smbpasswd: Don't leak memory. Reviewed-by: Alexander Bokovoy <ab@samba.org>

--- source3/utils/smbpasswd.c
+++ source3/utils/smbpasswd.c
@@ -137,6 +137,7 @@ static int process_options(int argc, char **argv, int local_flags)
 		case 'n':
 			local_flags |= LOCAL_SET_NO_PASSWORD;
 			local_flags &= ~LOCAL_SET_PASSWORD;
+			SAFE_FREE(new_passwd);
 			new_passwd = smb_xstrdup("NO PASSWORD");
 			break;
 		case 'r':
