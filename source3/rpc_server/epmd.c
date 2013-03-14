6a5cae96788640ddce89a047bbc63b4dbc94ca89
Andrew Bartlett <abartlet@samba.org>
2013-03-15 10:02:53 +1100

    s3-epmd: Cast getpid() result to unsigned int for GNU/Solaris build
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/rpc_server/epmd.c
+++ source3/rpc_server/epmd.c
@@ -222,7 +222,7 @@ void start_epmd(struct tevent_context *ev_ctx,
 		exit(1);
 	}
 
-	DEBUG(1, ("Endpoint Mapper Daemon Started (%d)\n", getpid()));
+	DEBUG(1, ("Endpoint Mapper Daemon Started (%u)\n", (unsigned int)getpid()));
 
 	/* loop forever */
 	rc = tevent_loop_wait(ev_ctx);
