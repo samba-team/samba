a5ddcbc15be038b28213c4a095b8a327eee9833d
Jeremy Allison <jra@samba.org>
2013-03-26 16:46:51 -0700

    Ensure we don't return uninitialized memory in the pad bytes.
    Signed-off-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: David Disseldorp <ddiss@suse.de>

--- source3/smbd/trans2.c
+++ source3/smbd/trans2.c
@@ -479,6 +479,7 @@ static NTSTATUS fill_ea_chained_buffer(TALLOC_CTX *mem_ctx,
 		size_t dos_namelen;
 		fstring dos_ea_name;
 		size_t this_size;
+		size_t pad = 0;
 
 		if (last_start != NULL && do_store_data) {
 			SIVAL(last_start, 0, PTR_DIFF(p, last_start));
@@ -497,7 +498,7 @@ static NTSTATUS fill_ea_chained_buffer(TALLOC_CTX *mem_ctx,
 		this_size = 0x08 + dos_namelen + 1 + ea_list->ea.value.length;
 
 		if (ea_list->next) {
-			size_t pad = 4 - (this_size % 4);
+			pad = 4 - (this_size % 4);
 			this_size += pad;
 		}
 
@@ -513,6 +514,11 @@ static NTSTATUS fill_ea_chained_buffer(TALLOC_CTX *mem_ctx,
 			SSVAL(p, 0x06, ea_list->ea.value.length);
 			strlcpy((char *)(p+0x08), dos_ea_name, dos_namelen+1);
 			memcpy(p + 0x08 + dos_namelen + 1, ea_list->ea.value.data, ea_list->ea.value.length);
+			if (pad) {
+				memset(p + 0x08 + dos_namelen + 1 + ea_list->ea.value.length,
+					'\0',
+					pad);
+			}
 			total_data_size -= this_size;
 		}
 
