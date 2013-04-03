b5d5b9a40eb13f89a552e29096deed52229c147e
Volker Lendecke <vl@samba.org>
2013-04-03 13:24:53 +0200

    libsmbclient: Fix cli_session_setup_guest_send
    This worked fine so far, but for the wrong reason: We only ever called
    this through the sync wrapper. The tevent_req_nterror in the NT_STATUS_OK
    case does not call tevent_req_finish. So the tevent_loop_poll did call
    into the core send/receive smb code. This is a fix for the case when
    smb1cli_req_chain_submit fails for some reason.
    
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Stefan Metzmacher <metze@samba.org>
    
    Autobuild-User(master): Stefan Metzmacher <metze@samba.org>
    Autobuild-Date(master): Wed Apr  3 18:39:30 CEST 2013 on sn-devel-104

--- source3/libsmb/cliconnect.c
+++ source3/libsmb/cliconnect.c
@@ -446,7 +446,7 @@ struct tevent_req *cli_session_setup_guest_send(TALLOC_CTX *mem_ctx,
 	}
 
 	status = smb1cli_req_chain_submit(&subreq, 1);
-	if (NT_STATUS_IS_OK(status)) {
+	if (!NT_STATUS_IS_OK(status)) {
 		tevent_req_nterror(req, status);
 		return tevent_req_post(req, ev);
 	}
