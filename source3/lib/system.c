abff441e445431970d1e25fa79e10276e576d9e3
Jeremy Allison <jra@samba.org>
2013-04-10 16:30:10 -0700

    Remove dependency on detection of HAVE_DIRFD for use of fdopendir().
    Signed-off-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: Andreas Schneider <asn@samba.org>
    
    Autobuild-User(master): Andreas Schneider <asn@cryptomilk.org>
    Autobuild-Date(master): Fri Apr 12 16:21:10 CEST 2013 on sn-devel-104
    (cherry picked from commit 7a4dd845958f1411daa8031ca242987001ab2f26)

--- source3/lib/system.c
+++ source3/lib/system.c
@@ -882,16 +882,14 @@ SMB_STRUCT_DIR *sys_opendir(const char *name)
 }
 
 /*******************************************************************
- An fdopendir wrapper that will deal with 64 bit filesizes.
- Ugly hack - we need dirfd for this to work correctly in the
- calling code.. JRA.
+ An fdopendir wrapper.
 ********************************************************************/
 
 SMB_STRUCT_DIR *sys_fdopendir(int fd)
 {
-#if defined(HAVE_EXPLICIT_LARGEFILE_SUPPORT) && defined(HAVE_FDOPENDIR64) && defined(HAVE_DIRFD)
+#if defined(HAVE_EXPLICIT_LARGEFILE_SUPPORT) && defined(HAVE_FDOPENDIR64)
 	return fdopendir64(fd);
-#elif defined(HAVE_FDOPENDIR) && defined(HAVE_DIRFD)
+#elif defined(HAVE_FDOPENDIR)
 	return fdopendir(fd);
 #else
 	errno = ENOSYS;
