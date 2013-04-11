7d274641726d1572243968b708a102bb9a15a15c
Volker Lendecke <vl@samba.org>
2013-04-11 16:11:26 +0200

    vfs: Convert catia_unlink to cp_smb_filename
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/modules/vfs_catia.c
+++ source3/modules/vfs_catia.c
@@ -491,9 +491,9 @@ static int catia_unlink(vfs_handle_struct *handle,
 	}
 
 	/* Setup temporary smb_filename structs. */
-	status = copy_smb_filename(talloc_tos(), smb_fname, &smb_fname_tmp);
-	if (!NT_STATUS_IS_OK(status)) {
-		errno = map_errno_from_nt_status(status);
+	smb_fname_tmp = cp_smb_filename(talloc_tos(), smb_fname);
+	if (smb_fname_tmp == NULL) {
+		errno = ENOMEM;
 		return -1;
 	}
 
