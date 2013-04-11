e2e45bd9cd6fa9a48fe8be15da4406dfcc55057a
Volker Lendecke <vl@samba.org>
2013-04-11 16:27:48 +0200

    smbd: Convert call_trans2qfilepathinfo to cp_smb_filename
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/smbd/trans2.c
+++ source3/smbd/trans2.c
@@ -5216,10 +5216,9 @@ static void call_trans2qfilepathinfo(connection_struct *conn,
 			return;
 		}
 
-		status = copy_smb_filename(talloc_tos(), fsp->fsp_name,
-					   &smb_fname);
-		if (!NT_STATUS_IS_OK(status)) {
-			reply_nterror(req, status);
+		smb_fname = cp_smb_filename(talloc_tos(), fsp->fsp_name);
+		if (smb_fname == NULL) {
+			reply_nterror(req, NT_STATUS_NO_MEMORY);
 			return;
 		}
 
