2b97eb13ff7d97ddd7a4400a8073ea7b00119103
Volker Lendecke <vl@samba.org>
2013-04-15 11:49:54 +0200

    vfstest: Convert cmd_set_nt_acl to synthetic_smb_fname
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/torture/cmd_vfs.c
+++ source3/torture/cmd_vfs.c
@@ -1462,11 +1462,10 @@ static NTSTATUS cmd_set_nt_acl(struct vfs_state *vfs, TALLOC_CTX *mem_ctx, int a
 	}
 	fsp->conn = vfs->conn;
 
-	status = create_synthetic_smb_fname_split(NULL, argv[1], NULL,
-						  &smb_fname);
-	if (!NT_STATUS_IS_OK(status)) {
+	smb_fname = synthetic_smb_fname_split(NULL, argv[1], NULL);
+	if (smb_fname == NULL) {
 		TALLOC_FREE(fsp);
-		return status;
+		return NT_STATUS_NO_MEMORY;
 	}
 
 	fsp->fsp_name = smb_fname;
