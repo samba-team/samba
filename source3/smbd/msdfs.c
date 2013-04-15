576df8742828c676997021cc8a58c2b7168cb0a9
Volker Lendecke <vl@samba.org>
2013-04-15 11:06:57 +0200

    smbd: Convert create_msdfs_link to synthetic_smb_fname
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/smbd/msdfs.c
+++ source3/smbd/msdfs.c
@@ -1279,14 +1279,12 @@ bool create_msdfs_link(const struct junction_map *jucn)
 
 	if(SMB_VFS_SYMLINK(conn, msdfs_link, path) < 0) {
 		if (errno == EEXIST) {
-			struct smb_filename *smb_fname = NULL;
-			NTSTATUS status;
+			struct smb_filename *smb_fname;
 
-			status = create_synthetic_smb_fname(talloc_tos(), path,
-							    NULL, NULL,
-							    &smb_fname);
-			if (!NT_STATUS_IS_OK(status)) {
-				errno = map_errno_from_nt_status(status);
+			smb_fname = synthetic_smb_fname(talloc_tos(), path,
+							NULL, NULL);
+			if (smb_fname == NULL) {
+				errno = ENOMEM;
 				goto out;
 			}
 
