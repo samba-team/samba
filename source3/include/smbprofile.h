bdc3e9acaf1b03af0e523f60b3260c6fdc62523c
Christian Ambach <ambi@samba.org>
2013-04-23 11:20:42 +0200

    s3:include remove non-blank line endings
    Signed-off-by: Christian Ambach <ambi@samba.org>
    
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- source3/include/smbprofile.h
+++ source3/include/smbprofile.h
@@ -1,6 +1,6 @@
 #ifndef _PROFILE_H_
 #define _PROFILE_H_
-/* 
+/*
    Unix SMB/CIFS implementation.
    store smbd profiling information in shared memory
    Copyright (C) Andrew Tridgell 1999
@@ -10,12 +10,12 @@
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
