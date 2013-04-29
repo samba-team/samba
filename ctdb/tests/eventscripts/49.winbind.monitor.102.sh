6e2863a4f9ae476727b05105d0f6e2cf24e68957
Martin Schwenke <martin@meltin.net>
2013-04-30 03:54:17 +1000

    eventscripts: Clean up ctdb_check_command()
    * Command is now multiple arguments, preserving quoting
    * $service_name no longer printed, no longer an argument
    * Debug output from failed command
    
    Signed-off-by: Martin Schwenke <martin@meltin.net>
    
    (This used to be ctdb commit 9e25fb261447a196de05937052779b36e75e7215)

--- ctdb/tests/eventscripts/49.winbind.monitor.102.sh
+++ ctdb/tests/eventscripts/49.winbind.monitor.102.sh
@@ -7,6 +7,6 @@ define_test "winbind down"
 setup_winbind
 wbinfo_down
 
-required_result 1 "ERROR: winbind - wbinfo -p returned error"
+required_result 1 "ERROR: wbinfo -p returned error"
 
 simple_test
