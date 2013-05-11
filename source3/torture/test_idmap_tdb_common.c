1cd20441a76bde78196124b925a67183614103d8
Volker Lendecke <vl@samba.org>
2013-05-11 10:51:22 +0200

    torture: Remove some unused code
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Christian Ambach <ambi@samba.org>

--- source3/torture/test_idmap_tdb_common.c
+++ source3/torture/test_idmap_tdb_common.c
@@ -62,11 +62,6 @@ bool idmap_is_online(void)
 	return true;
 }
 
-NTSTATUS idmap_backends_sid_to_unixid(const char *domain, struct id_map *id)
-{
-	return NT_STATUS_OK;
-}
-
 NTSTATUS idmap_backends_unixid_to_sid(const char *domname, struct id_map *id)
 {
 	return NT_STATUS_OK;
