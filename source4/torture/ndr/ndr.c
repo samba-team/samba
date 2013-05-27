886f062e72f8ad9cddfe5fab4b2b1a36be0ea30b
Günther Deschner <gd@samba.org>
2013-05-27 14:15:08 +0200

    s4-torture: Always compile backupkey ndr testsuite.
    This removes another incorrect AD_DC_BUILD_IS_ENABLED check ifdef check. The ndr
    test is not at all heimdal specific.
    
    Guenther
    
    Signed-off-by: Günther Deschner <gd@samba.org>
    Reviewed-by: Alexander Bokovoy <ab@samba.org>
    
    Autobuild-User(master): Alexander Bokovoy <ab@samba.org>
    Autobuild-Date(master): Wed May 29 11:29:51 CEST 2013 on sn-devel-104

--- source4/torture/ndr/ndr.c
+++ source4/torture/ndr/ndr.c
@@ -398,9 +398,7 @@ struct torture_suite *torture_local_ndr(TALLOC_CTX *mem_ctx)
 	torture_suite_add_suite(suite, ndr_drsblobs_suite(suite));
 	torture_suite_add_suite(suite, ndr_nbt_suite(suite));
 	torture_suite_add_suite(suite, ndr_ntlmssp_suite(suite));
-#ifdef AD_DC_BUILD_IS_ENABLED /* Add Heimdal-specific KDC test */
 	torture_suite_add_suite(suite, ndr_backupkey_suite(suite));
-#endif
 	torture_suite_add_suite(suite, ndr_string_suite(suite));
 
 	torture_suite_add_simple_test(suite, "string terminator",
