5de011be3f0ebb527678d681573f4def8335f6d4
Andreas Schneider <asn@samba.org>
2013-07-03 14:17:00 +0200

    s4-torture: Remove socket_wrapper testsuite.
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Stefan Metzmacher <metze@samba.org>

--- source4/torture/local/local.c
+++ source4/torture/local/local.c
@@ -47,9 +47,6 @@
 	torture_local_genrand, 
 	torture_local_iconv,
 	torture_local_socket, 
-#ifdef SOCKET_WRAPPER
-	torture_local_socket_wrapper, 
-#endif
 	torture_pac, 
 	torture_local_resolve,
 	torture_local_sddl,
