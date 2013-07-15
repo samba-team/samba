85400325276b37d4e3086bbfc8190d27f52d5671
Aurélien Aptel <aurelien.aptel@gmail.com>
2013-07-15 23:41:15 +0200

    test_smbclient_tarmode.pl: add copyright header
    Signed-off-by: Aurélien Aptel <aurelien.aptel@gmail.com>
    Reviewed-by: David Disseldorp <ddiss@samba.org>
    Reviewed-by: Jim McDonough <jmcd@samba.org>

--- source3/script/tests/test_smbclient_tarmode.pl
+++ source3/script/tests/test_smbclient_tarmode.pl
@@ -1,5 +1,22 @@
 #!/usr/bin/perl
 
+# Unix SMB/CIFS implementation.
+# Test suite for the tar backup mode of smbclient.
+# Copyright (C) Aurélien Aptel 2013
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 =head1 NAME
 
 C<test_smbclient_tarmode.pl> - Test for smbclient tar backup feature
