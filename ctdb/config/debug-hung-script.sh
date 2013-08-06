98163e01a9f112d2fb1c92a3eccd40b7869a37aa
Martin Schwenke <martin@meltin.net>
2013-08-06 16:11:40 +1000

    scripts: Do not run ctdb tool commands when debugging hung "init" event
    CTDB daemon is not ready to accept clients in INIT runstate (init event).
    CTDB daemon will start accepting connections in SETUP runstate (setup event)
    and later.
    
    Also, minor log formatting changes.
    
    Signed-off-by: Martin Schwenke <martin@meltin.net>
    
    (This used to be ctdb commit 81d7ce03b28d592a1337639e14d9ea141e20bfff)

--- ctdb/config/debug-hung-script.sh
+++ ctdb/config/debug-hung-script.sh
@@ -3,10 +3,14 @@
 (
     flock --wait 2 9 || exit 1
 
-    echo "===== Start of hung script debug for PID=\"$1\", event\"$2\" ====="
+    echo "===== Start of hung script debug for PID=\"$1\", event=\"$2\" ====="
 
     echo "pstree -p -a ${1}:"
     pstree -p -a $1
+
+    if [ "$2" = "init" ] ; then
+	exit 0
+    fi
 
     echo "ctdb scriptstatus ${2}:"
     # No use running several of these in parallel if, say, "releaseip"
@@ -14,6 +18,6 @@
     # interleaved in the log and would just be confusing.
     ctdb scriptstatus "$2"
 
-    echo "===== End of hung script debug for PID=\"$1\", event\"$2\" ====="
+    echo "===== End of hung script debug for PID=\"$1\", event=\"$2\" ====="
 
 ) 9>"${CTDB_VARDIR}/debug-hung-script.lock"
