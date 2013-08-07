04d396566b90f32dc239450596ab50502f9cbb02
Jeremy Allison <jra@samba.org>
2013-08-07 16:03:00 -0700

    s3:libsmb: Plumb cli_smb2_rmdir() inside cli_rmdir().
    Signed-off-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: Stefan Metzmacher <metze@samba.org>

--- source3/libsmb/clifile.c
+++ source3/libsmb/clifile.c
@@ -1635,10 +1635,16 @@ NTSTATUS cli_rmdir_recv(struct tevent_req *req)
 
 NTSTATUS cli_rmdir(struct cli_state *cli, const char *dname)
 {
-	TALLOC_CTX *frame = talloc_stackframe();
+	TALLOC_CTX *frame = NULL;
 	struct tevent_context *ev;
 	struct tevent_req *req;
 	NTSTATUS status = NT_STATUS_OK;
+
+	if (smbXcli_conn_protocol(cli->conn) >= PROTOCOL_SMB2_02) {
+		return cli_smb2_rmdir(cli, dname);
+	}
+
+	frame = talloc_stackframe();
 
 	if (smbXcli_conn_has_async_calls(cli->conn)) {
 		/*
