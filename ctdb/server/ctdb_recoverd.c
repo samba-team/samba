de6b97ce4f57ea64d6825039ea346f031d36d6d3
Amitay Isaacs <amitay@gmail.com>
2013-08-13 13:55:47 +1000

    Revert "recoverd: Use correct tdb flags when creating missing databases"
    This reverts commit 10a057d8e15c8c18e540598a940d3548c731b0b4.
    
    This approach would not work when creating local databases since currently
    there is no control to receive TDB flags for remote databases.
    
    Signed-off-by: Amitay Isaacs <amitay@gmail.com>
    
    (This used to be ctdb commit ca61eb776ab862bd269e45ee0f9f96e7e1e0e001)

--- ctdb/server/ctdb_recoverd.c
+++ ctdb/server/ctdb_recoverd.c
@@ -468,7 +468,7 @@ static int create_missing_remote_databases(struct ctdb_context *ctdb, struct ctd
 			}
 			ctdb_ctrl_createdb(ctdb, CONTROL_TIMEOUT(), nodemap->nodes[j].pnn, 
 					   mem_ctx, name,
-					   dbmap->dbs[db].flags);
+					   dbmap->dbs[db].flags & CTDB_DB_FLAGS_PERSISTENT);
 			if (ret != 0) {
 				DEBUG(DEBUG_ERR, (__location__ " Unable to create remote db:%s\n", name));
 				return -1;
@@ -531,7 +531,7 @@ static int create_missing_local_databases(struct ctdb_context *ctdb, struct ctdb
 				return -1;
 			}
 			ctdb_ctrl_createdb(ctdb, CONTROL_TIMEOUT(), pnn, mem_ctx, name, 
-					   remote_dbmap->dbs[db].flags);
+					   remote_dbmap->dbs[db].flags & CTDB_DB_FLAGS_PERSISTENT);
 			if (ret != 0) {
 				DEBUG(DEBUG_ERR, (__location__ " Unable to create local db:%s\n", name));
 				return -1;
