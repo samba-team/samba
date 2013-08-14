2fb817c55b0dca708b316e920f1be1d1ed708e68
Stefan Metzmacher <metze@samba.org>
2013-08-14 15:18:47 -0700

    s3:libsmb: call smb2cli_logoff() from cli_ulogoff()
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>
    
    Autobuild-User(master): Stefan Metzmacher <metze@samba.org>
    Autobuild-Date(master): Thu Aug 15 10:53:21 CEST 2013 on sn-devel-104
    
    (cherry picked from commit b706ca6e55aa76d4da901c69d991969602facc3b)

--- source3/libsmb/cliconnect.c
+++ source3/libsmb/cliconnect.c
@@ -2299,6 +2299,18 @@ NTSTATUS cli_ulogoff(struct cli_state *cli)
 	struct tevent_req *req;
 	NTSTATUS status = NT_STATUS_NO_MEMORY;
 
+	if (smbXcli_conn_protocol(cli->conn) >= PROTOCOL_SMB2_02) {
+		status = smb2cli_logoff(cli->conn,
+					cli->timeout,
+					cli->smb2.session);
+		if (!NT_STATUS_IS_OK(status)) {
+			return status;
+		}
+		smb2cli_session_set_id_and_flags(cli->smb2.session,
+						 UINT64_MAX, 0);
+		return NT_STATUS_OK;
+	}
+
 	if (smbXcli_conn_has_async_calls(cli->conn)) {
 		return NT_STATUS_INVALID_PARAMETER;
 	}
