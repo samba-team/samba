7a8337a01d83232f72ff5d1cefd7394c50dde348
Amitay Isaacs <amitay@gmail.com>
2013-08-25 21:44:59 +1000

    libctdb: Remove incomplete libctdb
    Signed-off-by: Amitay Isaacs <amitay@gmail.com>
    
    (This used to be ctdb commit c5a7f2b4ff011e1393c4ff34864f85e6b472ff07)

--- ctdb/libctdb/messages.h
+++
@@ -1,9 +0,0 @@
-#ifndef _LIBCTDB_MESSAGE_H
-#define _LIBCTDB_MESSAGE_H
-struct message_handler_info;
-struct ctdb_connection;
-struct ctdb_req_header;
-
-void deliver_message(struct ctdb_connection *ctdb, struct ctdb_req_header *hdr);
-void remove_message_handlers(struct ctdb_connection *ctdb);
-#endif /* _LIBCTDB_MESSAGE_H */
