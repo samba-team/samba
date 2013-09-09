05375cde8354a785c190cd75b051e3e508312b37
Andrew Bartlett <abartlet@samba.org>
2013-09-09 17:14:45 +1200

    selftest: Pass DC_REALM to the subdom_dc environment
    This allows 'samba-tool drs kcc' to be run during the environment setup.
    
    Andrew Bartlett
    
    Change-Id: I5d25470f1530b28be0a9413d13c48442fabb1a84
    
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-By: Jelmer Vernooij <jelmer@samba.org>

--- selftest/target/Samba4.pm
+++ selftest/target/Samba4.pm
@@ -1222,6 +1222,7 @@ sub provision_vampire_dc($$$)
 	$ret->{DC_NETBIOSNAME} = $dcvars->{DC_NETBIOSNAME};
 	$ret->{DC_USERNAME} = $dcvars->{DC_USERNAME};
 	$ret->{DC_PASSWORD} = $dcvars->{DC_PASSWORD};
+	$ret->{DC_REALM} = $dcvars->{DC_REALM};
 
 	return $ret;
 }
@@ -1321,6 +1322,7 @@ sub provision_dc($$)
 	$ret->{DC_NETBIOSNAME} = $ret->{NETBIOSNAME};
 	$ret->{DC_USERNAME} = $ret->{USERNAME};
 	$ret->{DC_PASSWORD} = $ret->{PASSWORD};
+	$ret->{DC_REALM} = $ret->{REALM};
 
 	return $ret;
 }
