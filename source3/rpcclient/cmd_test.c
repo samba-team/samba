a79547cac620eaafd7fabe4264e58eb9276b549d
Stefan Metzmacher <metze@samba.org>
2013-09-13 11:06:30 +0200

    s3:rpcclient: make use of rpcclient_cli_state
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- source3/rpcclient/cmd_test.c
+++ source3/rpcclient/cmd_test.c
@@ -35,14 +35,14 @@ static NTSTATUS cmd_testme(struct rpc_pipe_client *cli, TALLOC_CTX *mem_ctx,
 
 	d_printf("testme\n");
 
-	status = cli_rpc_pipe_open_noauth(rpc_pipe_np_smb_conn(cli),
+	status = cli_rpc_pipe_open_noauth(rpcclient_cli_state,
 					  &ndr_table_lsarpc,
 					  &lsa_pipe);
 	if (!NT_STATUS_IS_OK(status)) {
 		goto done;
 	}
 
-	status = cli_rpc_pipe_open_noauth(rpc_pipe_np_smb_conn(cli),
+	status = cli_rpc_pipe_open_noauth(rpcclient_cli_state,
 					  &ndr_table_samr,
 					  &samr_pipe);
 	if (!NT_STATUS_IS_OK(status)) {
