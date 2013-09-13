66d3064333391bbd7e07e504c5d246498c1f748a
Stefan Metzmacher <metze@samba.org>
2013-09-13 11:27:39 +0200

    lib/param: add PROTOCOL_SMB3_02 handling
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Michael Adam <obnox@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- lib/param/param_table.c
+++ lib/param/param_table.c
@@ -30,6 +30,7 @@
 static const struct enum_list enum_protocol[] = {
 	{PROTOCOL_SMB2_10, "SMB2"}, /* for now keep PROTOCOL_SMB2_10 */
 	{PROTOCOL_SMB3_00, "SMB3"}, /* for now keep PROTOCOL_SMB3_00 */
+	{PROTOCOL_SMB3_02, "SMB3_02"},
 	{PROTOCOL_SMB3_00, "SMB3_00"},
 	{PROTOCOL_SMB2_24, "SMB2_24"},
 	{PROTOCOL_SMB2_22, "SMB2_22"},
