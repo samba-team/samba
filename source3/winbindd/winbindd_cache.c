f0d454d768a4b4e91dbea8c920d0fe0293c3167c
Christian Ambach <ambi@samba.org>
2013-09-16 13:18:17 +0200

    s3:winbindd fix use of uninitialized variables
    Bug: https://bugzilla.samba.org/show_bug.cgi?id=10280
    
    Signed-off-by: Christian Ambach <ambi@samba.org>
    Reviewed-by: Stefan Metzmacher <metze@samba.org>
    (cherry picked from commit 7393781a57891687b464762b0954e6c936f750bb)
    
    Autobuild-User(v4-0-test): Karolin Seeger <kseeger@samba.org>
    Autobuild-Date(v4-0-test): Tue Jan  7 12:25:51 CET 2014 on sn-devel-104

--- source3/winbindd/winbindd_cache.c
+++ source3/winbindd/winbindd_cache.c
@@ -2120,6 +2120,19 @@ static NTSTATUS rids_to_names(struct winbindd_domain *domain,
 			old_status) {
 			have_mapped = have_unmapped = false;
 
+			*names = talloc_array(mem_ctx, char *, num_rids);
+			if (*names != NULL) {
+				result = NT_STATUS_NO_MEMORY;
+				goto error;
+			}
+
+			*types = talloc_array(mem_ctx, enum lsa_SidType,
+					      num_rids);
+			if (*types != NULL) {
+				result = NT_STATUS_NO_MEMORY;
+				goto error;
+			}
+
 			for (i=0; i<num_rids; i++) {
 				struct dom_sid sid;
 				struct cache_entry *centry;
