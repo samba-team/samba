8fd1e540d90380875f0a2048b467b419434e0ee0
Jeremy Allison <jra@samba.org>
2013-09-16 19:16:52 -0700

    s3: libsmb : The short name length is only a one byte field.
    The next byte is "undefined" and some vendors set this to 0xff
    (discovered in SNIA SDC lab tests).
    
    Signed-off-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: Stefan Metzmacher <metze@samba.org>
    
    Autobuild-User(master): Stefan Metzmacher <metze@samba.org>
    Autobuild-Date(master): Tue Sep 17 12:27:18 CEST 2013 on sn-devel-104
    (cherry picked from commit 1c41feb7893ae4a4f42c035f3c83f8b2950b7816)
    
    Fix bug #10145 - Samba SMB2 client code reads the wrong short name length in a
    directory listing reply.
    
    Autobuild-User(v4-1-test): Karolin Seeger <kseeger@samba.org>
    Autobuild-Date(v4-1-test): Wed Sep 18 11:15:35 CEST 2013 on sn-devel-104

--- source3/libsmb/cli_smb2_fnum.c
+++ source3/libsmb/cli_smb2_fnum.c
@@ -412,7 +412,7 @@ static NTSTATUS parse_finfo_id_both_directory_info(uint8_t *dir_data,
 	if (namelen > (dir_data_length - 104)) {
 		return NT_STATUS_INFO_LENGTH_MISMATCH;
 	}
-	slen = SVAL(dir_data + 68, 0);
+	slen = CVAL(dir_data + 68, 0);
 	if (slen > 24) {
 		return NT_STATUS_INFO_LENGTH_MISMATCH;
 	}
