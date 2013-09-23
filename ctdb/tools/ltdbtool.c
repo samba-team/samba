613313fa52f06d9821680296515d1d5ea30262cb
Martin Schwenke <martin@meltin.net>
2013-09-23 16:23:36 +1000

    tool/ltdbtool: -h option does not require an argument
    Signed-off-by: Martin Schwenke <martin@meltin.net>
    
    (This used to be ctdb commit 9e18f3c173863919587e25d704f66372624ed8ed)

--- ctdb/tools/ltdbtool.c
+++ ctdb/tools/ltdbtool.c
@@ -212,7 +212,7 @@ int main(int argc, char* argv[])
 	int opt;
 	const char *cmd, *idb, *odb;
 
-	while ((opt = getopt(argc, argv, "s:o:S:O:ph:e")) != -1) {
+	while ((opt = getopt(argc, argv, "s:o:S:O:phe")) != -1) {
 		switch (opt) {
 		case 's':
 		case 'S':
