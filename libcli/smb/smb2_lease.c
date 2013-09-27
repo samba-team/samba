c944d2ea41456d27e1f327f9e50b70c0bbbde824
Volker Lendecke <vl@samba.org>
2013-09-26 17:10:50 -0700

    libcli: Correct smb2_lease_pull
    We don't really use leases yet, so so far this went by unnoticed. It's
    the V2 lease requests that hold the parent lease key, not the V1 ones.
    
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Stefan Metzmacher <metze@samba.org>

--- libcli/smb/smb2_lease.c
+++ libcli/smb/smb2_lease.c
@@ -46,12 +46,12 @@ ssize_t smb2_lease_pull(const uint8_t *buf, size_t len,
 
 	switch (version) {
 	case 1:
-		memcpy(&lease->parent_lease_key, buf+32, 16);
-		lease->lease_epoch = SVAL(buf, 48);
-		break;
-	case 2:
 		ZERO_STRUCT(lease->parent_lease_key);
 		lease->lease_epoch = 0;
+		break;
+	case 2:
+		memcpy(&lease->parent_lease_key, buf+32, 16);
+		lease->lease_epoch = SVAL(buf, 48);
 		break;
 	}
 
