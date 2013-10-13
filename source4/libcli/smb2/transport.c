064433f265d2215389f2a377b6e8243318669b65
Volker Lendecke <vl@samba.org>
2013-10-13 12:20:29 +0200

    libcli4: Remove an unused variable
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Stefan Metzmacher <metze@samba.org>
    
    Autobuild-User(master): Stefan Metzmacher <metze@samba.org>
    Autobuild-Date(master): Sun Oct 13 17:58:23 CEST 2013 on sn-devel-104

--- source4/libcli/smb2/transport.c
+++ source4/libcli/smb2/transport.c
@@ -48,7 +48,6 @@ struct smb2_transport *smb2_transport_init(struct smbcli_socket *sock,
 					   struct smbcli_options *options)
 {
 	struct smb2_transport *transport;
-	uint32_t smb2_capabilities = 0;
 
 	transport = talloc_zero(parent_ctx, struct smb2_transport);
 	if (!transport) return NULL;
