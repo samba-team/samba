06be5962aba761ec0ac02fb2b8e108ec1918bfed
Michael Adam <obnox@samba.org>
2013-11-13 14:53:45 +0100

    random-sleep: add possiblity to override via environment variable
    set AUTOBUILD_RANDOM_SLEEP_OVERRIDE=<x>
    
    to the number x of seconds that you want to sleep.
    This is added to make it possible to run one autobuild
    target without having to wait for several minutes.
    
    Signed-off-by: Michael Adam <obnox@samba.org>
    Signed-off-by: Stefan Metzmacher <metze@samba.org>

--- script/random-sleep.sh
+++ script/random-sleep.sh
@@ -16,6 +16,10 @@ r=$(head --bytes=2 /dev/urandom | od -l | head -n 1 | sed -e 's/^[^ ]*  *//')
 v=$(expr $r % $s)
 d=$(expr $l + $v)
 
+if test "x${AUTOBUILD_RANDOM_SLEEP_OVERRIDE}" != "x" ; then
+	d="${AUTOBUILD_RANDOM_SLEEP_OVERRIDE}"
+fi
+
 echo "$0: sleep $d ... start"
 sleep $d
 echo "$0: sleep $d ... end"
