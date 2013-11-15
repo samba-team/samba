97554854439e83ebb9e99ce4ebb6e49de8e7a5a9
Martin Schwenke <martin@meltin.net>
2013-11-15 12:22:05 +1100

    ctdb:tests: remove unused setup_nmap_output_filter()
    The tests that used it have gone.
    
    Signed-off-by: Martin Schwenke <martin@meltin.net>
    Reviewed-by: Michael Adam <obnox@samba.org>

--- ctdb/tests/eventscripts/scripts/local.sh
+++ ctdb/tests/eventscripts/scripts/local.sh
@@ -200,11 +200,6 @@ ethtool_interfaces_up ()
     done
 }
 
-setup_nmap_output_filter ()
-{
-    OUT_FILTER="-e 's@^(DEBUG: # Nmap 5.21 scan initiated) .+ (as:)@\1 DATE \2@' -e 's@^(DEBUG: # Nmap done at) .+ (--)@\1 DATE \2@'"
-}
-
 dump_routes ()
 {
     echo "# ip rule show"
