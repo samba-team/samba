979d9dbbb709c99c77b02fbc7429a0d0472ac2f4
Andreas Schneider <asn@samba.org>
2013-11-19 18:14:24 +0100

    replace: Fix developer build on BSD.
    This fixes bsd_attr_list() calling geteuid().
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>
    
    Autobuild-User(master): Jeremy Allison <jra@samba.org>
    Autobuild-Date(master): Thu Nov 21 03:37:59 CET 2013 on sn-devel-104

--- lib/replace/xattr.c
+++ lib/replace/xattr.c
@@ -25,6 +25,7 @@
    License along with this library; if not, see <http://www.gnu.org/licenses/>.
 */
 
+#define UID_WRAPPER_NOT_REPLACE
 #include "replace.h"
 #include "system/filesys.h"
 #include "system/dir.h"
