ffae8a13b62433e5b74f1bc317db6c9979718cbb
Volker Lendecke <vl@samba.org>
2013-11-28 09:37:47 +0100

    pam_winbind: Use strlcat in safe_append_string
    We have that available via libreplace, so use it.
    
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: David Disseldorp <ddiss@samba.org>
    
    Autobuild-User(master): David Disseldorp <ddiss@samba.org>
    Autobuild-Date(master): Thu Nov 28 14:33:32 CET 2013 on sn-devel-104

--- nsswitch/pam_winbind.c
+++ nsswitch/pam_winbind.c
@@ -1043,15 +1043,9 @@ static bool safe_append_string(char *dest,
 			       const char *src,
 			       int dest_buffer_size)
 {
-	int dest_length = strlen(dest);
-	int src_length = strlen(src);
-
-	if (dest_length + src_length + 1 > dest_buffer_size) {
-		return false;
-	}
-
-	memcpy(dest + dest_length, src, src_length + 1);
-	return true;
+	size_t len;
+	len = strlcat(dest, src, dest_buffer_size);
+	return (len < dest_buffer_size);
 }
 
 /**
