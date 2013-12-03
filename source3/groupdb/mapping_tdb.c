044f8f71903084717271038d931d7dd47e8e74c3
Volker Lendecke <vl@samba.org>
2013-12-03 16:01:35 +0100

    group_mapping: Avoid a talloc
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Christof Schmitt <cs@samba.org>
    
    Autobuild-User(master): Jeremy Allison <jra@samba.org>
    Autobuild-Date(master): Thu Jan  9 20:41:15 CET 2014 on sn-devel-104

--- source3/groupdb/mapping_tdb.c
+++ source3/groupdb/mapping_tdb.c
@@ -119,17 +119,15 @@ static bool init_group_mapping(void)
 
 static char *group_mapping_key(TALLOC_CTX *mem_ctx, const struct dom_sid *sid)
 {
-	char *sidstr, *result;
+	char sidstr[DOM_SID_STR_BUFLEN];
+	int len;
 
-	sidstr = sid_string_talloc(talloc_tos(), sid);
-	if (sidstr == NULL) {
+	len = dom_sid_string_buf(sid, sidstr, sizeof(sidstr));
+	if (len >= sizeof(sidstr)) {
 		return NULL;
 	}
 
-	result = talloc_asprintf(mem_ctx, "%s%s", GROUP_PREFIX, sidstr);
-
-	TALLOC_FREE(sidstr);
-	return result;
+	return talloc_asprintf(mem_ctx, "%s%s", GROUP_PREFIX, sidstr);
 }
 
 /****************************************************************************
