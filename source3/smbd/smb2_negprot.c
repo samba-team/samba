59c185bf549b359728183c8e61c165823f6112ef
Stefan Metzmacher <metze@samba.org>
2013-12-04 14:59:07 +0100

    s3:smb2_negprot: make use of smbd_smb2_generate_outbody()
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/smbd/smb2_negprot.c
+++ source3/smbd/smb2_negprot.c
@@ -320,7 +320,7 @@ NTSTATUS smbd_smb2_request_process_negprot(struct smbd_smb2_request *req)
 		return smbd_smb2_request_error(req, status);
 	}
 
-	outbody = data_blob_talloc(req->out.vector, NULL, 0x40);
+	outbody = smbd_smb2_generate_outbody(req, 0x40);
 	if (outbody.data == NULL) {
 		return smbd_smb2_request_error(req, NT_STATUS_NO_MEMORY);
 	}
