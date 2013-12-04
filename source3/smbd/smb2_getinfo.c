a021a25104aa570efd8b69920b26fc2dcc6b5c04
Stefan Metzmacher <metze@samba.org>
2013-12-04 14:59:07 +0100

    s3:smb2_getinfo: make use of smbd_smb2_generate_outbody()
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/smbd/smb2_getinfo.c
+++ source3/smbd/smb2_getinfo.c
@@ -178,7 +178,7 @@ static void smbd_smb2_request_getinfo_done(struct tevent_req *subreq)
 
 	out_output_buffer_offset = SMB2_HDR_BODY + 0x08;
 
-	outbody = data_blob_talloc(req->out.vector, NULL, 0x08);
+	outbody = smbd_smb2_generate_outbody(req, 0x08);
 	if (outbody.data == NULL) {
 		error = smbd_smb2_request_error(req, NT_STATUS_NO_MEMORY);
 		if (!NT_STATUS_IS_OK(error)) {
