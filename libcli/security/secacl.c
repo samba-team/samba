2b113599f8a95b9d5c031511a2515ce9ffe98af2
Volker Lendecke <vl@samba.org>
2013-12-06 09:25:20 +0000

    secacl: Fix whitespace
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- libcli/security/secacl.c
+++ libcli/security/secacl.c
@@ -1,21 +1,21 @@
-/* 
+/*
  *  Unix SMB/Netbios implementation.
  *  SEC_ACL handling routines
  *  Copyright (C) Andrew Tridgell              1992-1998,
  *  Copyright (C) Jeremy R. Allison            1995-2003.
  *  Copyright (C) Luke Kenneth Casson Leighton 1996-1998,
  *  Copyright (C) Paul Ashton                  1997-1998.
- *  
+ *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 3 of the License, or
  *  (at your option) any later version.
- *  
+ *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- *  
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
@@ -28,12 +28,12 @@
 #define  SEC_ACL_HEADER_SIZE (2 * sizeof(uint16_t) + sizeof(uint32_t))
 
 /*******************************************************************
- Create a SEC_ACL structure.  
+ Create a SEC_ACL structure.
 ********************************************************************/
 
-struct security_acl *make_sec_acl(TALLOC_CTX *ctx, 
-								  enum security_acl_revision revision,
-								  int num_aces, struct security_ace *ace_list)
+struct security_acl *make_sec_acl(TALLOC_CTX *ctx,
+				  enum security_acl_revision revision,
+				  int num_aces, struct security_ace *ace_list)
 {
 	struct security_acl *dst;
 	int i;
@@ -51,12 +51,12 @@ struct security_acl *make_sec_acl(TALLOC_CTX *ctx,
 	   entries in it.  This is achieved by checking that num_aces is a
 	   positive number. */
 
-	if ((num_aces) && 
+	if ((num_aces) &&
             ((dst->aces = talloc_array(dst, struct security_ace, num_aces))
              == NULL)) {
 		return NULL;
 	}
-        
+
 	for (i = 0; i < num_aces; i++) {
 		dst->aces[i] = ace_list[i]; /* Structure copy. */
 		dst->size += ace_list[i].size;
@@ -66,7 +66,7 @@ struct security_acl *make_sec_acl(TALLOC_CTX *ctx,
 }
 
 /*******************************************************************
- Duplicate a SEC_ACL structure.  
+ Duplicate a SEC_ACL structure.
 ********************************************************************/
 
 struct security_acl *dup_sec_acl(TALLOC_CTX *ctx, struct security_acl *src)
