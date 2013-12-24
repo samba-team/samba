9db648789278128af2b065443caed255e4ebc79c
Andrew Bartlett <abartlet@samba.org>
2013-12-24 16:03:50 +1300

    param: Rename variable used for lp_ms_add_printer_wizard bMsAddPrinterWizard
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>
    Signed-off-by: Garming Sam <garming@catalyst.net.nz>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/param/loadparm.c
+++ source3/param/loadparm.c
@@ -941,7 +941,7 @@ static void init_globals(bool reinit_globals)
 
 */
 
-	Globals.bMsAddPrinterWizard = true;
+	Globals.ms_add_printer_wizard = true;
 	Globals.os_level = 20;
 	Globals.local_master = true;
 	Globals.domain_master = Auto;	/* depending on _domain_logons */
