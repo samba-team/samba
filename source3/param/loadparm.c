c7a09766f1a15489001915f4c9863ca72cc4c52b
Andrew Bartlett <abartlet@samba.org>
2013-12-24 16:04:04 +1300

    param: Rename variable used for lp_template_shell szTemplateShell
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>
    Signed-off-by: Garming Sam <garming@catalyst.net.nz>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/param/loadparm.c
+++ source3/param/loadparm.c
@@ -958,7 +958,7 @@ static void init_globals(bool reinit_globals)
 	Globals.allow_trusted_domains = true;
 	string_set(&Globals.szIdmapBackend, "tdb");
 
-	string_set(&Globals.szTemplateShell, "/bin/false");
+	string_set(&Globals.template_shell, "/bin/false");
 	string_set(&Globals.template_homedir, "/home/%D/%U");
 	string_set(&Globals.szWinbindSeparator, "\\");
 	string_set(&Globals.szWinbinddSocketDirectory, dyn_WINBINDD_SOCKET_DIR);
