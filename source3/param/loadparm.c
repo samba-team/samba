7198e8bb8cb07361006c373e46784fc95b9d7667
Andrew Bartlett <abartlet@samba.org>
2013-12-24 16:03:23 +1300

    param: Rename variable used for lp_lpresumecommand szLpresumecommand
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>
    Signed-off-by: Garming Sam <garming@catalyst.net.nz>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/param/loadparm.c
+++ source3/param/loadparm.c
@@ -162,7 +162,7 @@ static struct loadparm_service sDefault =
 	.lpqcommand = NULL,
 	.lprmcommand = NULL,
 	.lppausecommand = NULL,
-	.szLpresumecommand = NULL,
+	.lpresumecommand = NULL,
 	.szQueuepausecommand = NULL,
 	.szQueueresumecommand = NULL,
 	.szPrintername = NULL,
@@ -398,7 +398,7 @@ static void init_printer_values(struct loadparm_service *pService)
 			string_set(&pService->szQueuepausecommand, "lpc stop '%p'");
 			string_set(&pService->szQueueresumecommand, "lpc start '%p'");
 			string_set(&pService->lppausecommand, "lpc hold '%p' %j");
-			string_set(&pService->szLpresumecommand, "lpc release '%p' %j");
+			string_set(&pService->lpresumecommand, "lpc release '%p' %j");
 			break;
 
 		case PRINT_CUPS:
@@ -409,7 +409,7 @@ static void init_printer_values(struct loadparm_service *pService)
 			string_set(&pService->lprmcommand, "");
 			string_set(&pService->printcommand, "");
 			string_set(&pService->lppausecommand, "");
-			string_set(&pService->szLpresumecommand, "");
+			string_set(&pService->lpresumecommand, "");
 			string_set(&pService->szQueuepausecommand, "");
 			string_set(&pService->szQueueresumecommand, "");
 			break;
@@ -423,7 +423,7 @@ static void init_printer_values(struct loadparm_service *pService)
 			string_set(&pService->szQueueresumecommand, "enable %p");
 #ifndef HPUX
 			string_set(&pService->lppausecommand, "lp -i %p-%j -H hold");
-			string_set(&pService->szLpresumecommand, "lp -i %p-%j -H resume");
+			string_set(&pService->lpresumecommand, "lp -i %p-%j -H resume");
 #endif /* HPUX */
 			break;
 
@@ -471,7 +471,7 @@ static void init_printer_values(struct loadparm_service *pService)
 
 		tmp = talloc_asprintf(tmp_ctx, "vlp %s lpresume %%p %%j",
 				      tdbfile);
-		string_set(&pService->szLpresumecommand,
+		string_set(&pService->lpresumecommand,
 			   tmp ? tmp : "vlp lpresume %p %j");
 
 		tmp = talloc_asprintf(tmp_ctx, "vlp %s queuepause %%p",
