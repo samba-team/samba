abe9cb8b69eb2598b72076f24553cb24945f5223
Andrew Bartlett <abartlet@samba.org>
2013-12-24 16:03:53 +1300

    param: Rename variable used for lp_reset_on_zero_vc bResetOnZeroVC
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>
    Signed-off-by: Garming Sam <garming@catalyst.net.nz>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/param/loadparm.c
+++ source3/param/loadparm.c
@@ -892,7 +892,7 @@ static void init_globals(bool reinit_globals)
 #endif
 	Globals.bUnicode = true;
 	Globals.bUnixExtensions = true;
-	Globals.bResetOnZeroVC = false;
+	Globals.reset_on_zero_vc = false;
 	Globals.log_writeable_files_on_exit = false;
 	Globals.create_krb5_conf = true;
 	Globals.winbindMaxDomainConnections = 1;
