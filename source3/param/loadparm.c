d3ff81f1acf49ea6aeef92405c10abedf0a77ad5
Andrew Bartlett <abartlet@samba.org>
2013-12-24 16:03:57 +1300

    param: Rename variable used for lp_winbind_enum_users bWinbindEnumUsers
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>
    Signed-off-by: Garming Sam <garming@catalyst.net.nz>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/param/loadparm.c
+++ source3/param/loadparm.c
@@ -980,7 +980,7 @@ static void init_globals(bool reinit_globals)
 	Globals.winbind_cache_time = 300;	/* 5 minutes */
 	Globals.winbind_reconnect_delay = 30;	/* 30 seconds */
 	Globals.winbind_max_clients = 200;
-	Globals.bWinbindEnumUsers = false;
+	Globals.winbind_enum_users = false;
 	Globals.winbind_enum_groups = false;
 	Globals.bWinbindUseDefaultDomain = false;
 	Globals.bWinbindTrustedDomainsOnly = false;
