603282d5f56bbcdb1f913dbdc8a6a568706d3bf5
Andrew Bartlett <abartlet@samba.org>
2013-12-24 16:04:01 +1300

    param: Rename variable used for lp_guestaccount szGuestaccount
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>
    Signed-off-by: Garming Sam <garming@catalyst.net.nz>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/param/loadparm.c
+++ source3/param/loadparm.c
@@ -757,7 +757,7 @@ static void init_globals(bool reinit_globals)
 	string_set(&Globals.szManglingMethod, "hash2");
 	Globals.mangle_prefix = 1;
 
-	string_set(&Globals.szGuestaccount, GUEST_ACCOUNT);
+	string_set(&Globals.guestaccount, GUEST_ACCOUNT);
 
 	/* using UTF8 by default allows us to support all chars */
 	string_set(&Globals.unix_charset, DEFAULT_UNIX_CHARSET);
