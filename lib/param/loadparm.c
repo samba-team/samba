1f568678d6a0ccd6c2e89d40844451cb53b577d3
Garming Sam <garming@catalyst.net.nz>
2014-01-08 09:55:44 +1300

    lib/param: add default for max open files
    Signed-off-by: Garming Sam <garming@catalyst.net.nz>
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Alexander Bokovoy <ab@samba.org>

--- lib/param/loadparm.c
+++ lib/param/loadparm.c
@@ -2240,6 +2240,8 @@ struct loadparm_context *loadparm_init(TALLOC_CTX *mem_ctx)
 
 	lpcfg_do_global_parameter(lp_ctx, "short preserve case", "True");
 
+	lpcfg_do_global_parameter(lp_ctx, "max open files", "16384");
+
 	for (i = 0; parm_table[i].label; i++) {
 		if (!(lp_ctx->flags[i] & FLAG_CMDLINE)) {
 			lp_ctx->flags[i] |= FLAG_DEFAULT;
