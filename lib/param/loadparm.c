51a5942eb5c92282fdbddb1a9042938a3b237cd9
Garming Sam <garming@catalyst.net.nz>
2014-01-08 10:10:43 +1300

    lib/param: add default for ldap replication sleep
    Signed-off-by: Garming Sam <garming@catalyst.net.nz>
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Alexander Bokovoy <ab@samba.org>

--- lib/param/loadparm.c
+++ lib/param/loadparm.c
@@ -2256,6 +2256,8 @@ struct loadparm_context *loadparm_init(TALLOC_CTX *mem_ctx)
 
 	lpcfg_do_global_parameter(lp_ctx, "defer sharing violations", "True");
 
+	lpcfg_do_global_parameter(lp_ctx, "ldap replication sleep", "1000");
+
 	for (i = 0; parm_table[i].label; i++) {
 		if (!(lp_ctx->flags[i] & FLAG_CMDLINE)) {
 			lp_ctx->flags[i] |= FLAG_DEFAULT;
