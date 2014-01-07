51a5180941fc26b08a23a6b11985a3d36ce30657
Garming Sam <garming@catalyst.net.nz>
2014-01-08 10:41:21 +1300

    lib/param: add default for lm interval
    Signed-off-by: Garming Sam <garming@catalyst.net.nz>
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Alexander Bokovoy <ab@samba.org>

--- lib/param/loadparm.c
+++ lib/param/loadparm.c
@@ -2278,6 +2278,8 @@ struct loadparm_context *loadparm_init(TALLOC_CTX *mem_ctx)
 
 	lpcfg_do_global_parameter(lp_ctx, "ldap deref", "auto");
 
+	lpcfg_do_global_parameter(lp_ctx, "lm interval", "60");
+
 	for (i = 0; parm_table[i].label; i++) {
 		if (!(lp_ctx->flags[i] & FLAG_CMDLINE)) {
 			lp_ctx->flags[i] |= FLAG_DEFAULT;
