9fd38d1dbe5b248728395dff55d77a21e8d6203a
Garming Sam <garming@catalyst.net.nz>
2014-01-08 10:21:56 +1300

    lib/param: add default for winbind nested groups parameter
    Signed-off-by: Garming Sam <garming@catalyst.net.nz>
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Alexander Bokovoy <ab@samba.org>

--- lib/param/loadparm.c
+++ lib/param/loadparm.c
@@ -2268,6 +2268,8 @@ struct loadparm_context *loadparm_init(TALLOC_CTX *mem_ctx)
 
 	lpcfg_do_global_parameter(lp_ctx, "getwd cache", "True");
 
+	lpcfg_do_global_parameter(lp_ctx, "winbind nested groups", "True");
+
 	for (i = 0; parm_table[i].label; i++) {
 		if (!(lp_ctx->flags[i] & FLAG_CMDLINE)) {
 			lp_ctx->flags[i] |= FLAG_DEFAULT;
