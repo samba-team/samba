543599fca0d55bdc4273275f3b0d9fa10df2fe84
Garming Sam <garming@catalyst.net.nz>
2014-01-08 10:45:55 +1300

    lib/param: add default for browse list parameter
    Signed-off-by: Garming Sam <garming@catalyst.net.nz>
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Alexander Bokovoy <ab@samba.org>

--- lib/param/loadparm.c
+++ lib/param/loadparm.c
@@ -2284,6 +2284,8 @@ struct loadparm_context *loadparm_init(TALLOC_CTX *mem_ctx)
 
 	lpcfg_do_global_parameter(lp_ctx, "hide dot files", "True");
 
+	lpcfg_do_global_parameter(lp_ctx, "browse list", "True");
+
 	for (i = 0; parm_table[i].label; i++) {
 		if (!(lp_ctx->flags[i] & FLAG_CMDLINE)) {
 			lp_ctx->flags[i] |= FLAG_DEFAULT;
