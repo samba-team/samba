83e13ac05fc236d3480e19d26f5988873b061872
Garming Sam <garming@catalyst.net.nz>
2014-01-08 10:25:05 +1300

    lib/param: add default for smb2 max credits
    Signed-off-by: Garming Sam <garming@catalyst.net.nz>
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Alexander Bokovoy <ab@samba.org>

--- lib/param/loadparm.c
+++ lib/param/loadparm.c
@@ -2272,6 +2272,8 @@ struct loadparm_context *loadparm_init(TALLOC_CTX *mem_ctx)
 
 	lpcfg_do_global_parameter(lp_ctx, "mangled names", "True");
 
+	lpcfg_do_global_parameter(lp_ctx, "smb2 max credits", "8192");
+
 	for (i = 0; parm_table[i].label; i++) {
 		if (!(lp_ctx->flags[i] & FLAG_CMDLINE)) {
 			lp_ctx->flags[i] |= FLAG_DEFAULT;
