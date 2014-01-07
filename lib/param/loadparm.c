7273d9b766fde1090da5947b0fff8bce32e1a986
Garming Sam <garming@catalyst.net.nz>
2014-01-08 10:14:22 +1300

    lib/param: add default for enable privileges parameter
    Signed-off-by: Garming Sam <garming@catalyst.net.nz>
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Alexander Bokovoy <ab@samba.org>

--- lib/param/loadparm.c
+++ lib/param/loadparm.c
@@ -2260,6 +2260,8 @@ struct loadparm_context *loadparm_init(TALLOC_CTX *mem_ctx)
 
 	lpcfg_do_global_parameter(lp_ctx, "idmap backend", "tdb");
 
+	lpcfg_do_global_parameter(lp_ctx, "enable privileges", "True");
+
 	for (i = 0; parm_table[i].label; i++) {
 		if (!(lp_ctx->flags[i] & FLAG_CMDLINE)) {
 			lp_ctx->flags[i] |= FLAG_DEFAULT;
