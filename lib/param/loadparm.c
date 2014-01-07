cf6aac3125f5edc0a5231d6f3bcada4196258854
Garming Sam <garming@catalyst.net.nz>
2014-01-08 10:07:22 +1300

    lib/param: add default for name cache timeout
    Signed-off-by: Garming Sam <garming@catalyst.net.nz>
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Alexander Bokovoy <ab@samba.org>

--- lib/param/loadparm.c
+++ lib/param/loadparm.c
@@ -2252,6 +2252,8 @@ struct loadparm_context *loadparm_init(TALLOC_CTX *mem_ctx)
 
 	lpcfg_do_global_parameter(lp_ctx, "change notify", "True");
 
+	lpcfg_do_global_parameter(lp_ctx, "name cache timeout", "660");
+
 	for (i = 0; parm_table[i].label; i++) {
 		if (!(lp_ctx->flags[i] & FLAG_CMDLINE)) {
 			lp_ctx->flags[i] |= FLAG_DEFAULT;
