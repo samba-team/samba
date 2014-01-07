aab21403b2e793825f096c0b81a28a4b75108a8c
Garming Sam <garming@catalyst.net.nz>
2014-01-08 10:39:33 +1300

    lib/param: add default for ldap deref parameter
    Signed-off-by: Garming Sam <garming@catalyst.net.nz>
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Alexander Bokovoy <ab@samba.org>

--- lib/param/loadparm.c
+++ lib/param/loadparm.c
@@ -2276,6 +2276,8 @@ struct loadparm_context *loadparm_init(TALLOC_CTX *mem_ctx)
 
 	lpcfg_do_global_parameter(lp_ctx, "ldap ssl", "start tls");
 
+	lpcfg_do_global_parameter(lp_ctx, "ldap deref", "auto");
+
 	for (i = 0; parm_table[i].label; i++) {
 		if (!(lp_ctx->flags[i] & FLAG_CMDLINE)) {
 			lp_ctx->flags[i] |= FLAG_DEFAULT;
