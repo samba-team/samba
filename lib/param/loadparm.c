5a729cae41f4b5ff79028e5ac8baddcaf80ad923
Garming Sam <garming@catalyst.net.nz>
2014-01-07 18:12:47 +1300

    lib/param: add default for server schannel
    Signed-off-by: Garming Sam <garming@catalyst.net.nz>
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Alexander Bokovoy <ab@samba.org>

--- lib/param/loadparm.c
+++ lib/param/loadparm.c
@@ -2236,6 +2236,8 @@ struct loadparm_context *loadparm_init(TALLOC_CTX *mem_ctx)
 
 	lpcfg_do_global_parameter(lp_ctx, "winbind nss info", "template");
 
+	lpcfg_do_global_parameter(lp_ctx, "server schannel", "Auto");
+
 	for (i = 0; parm_table[i].label; i++) {
 		if (!(lp_ctx->flags[i] & FLAG_CMDLINE)) {
 			lp_ctx->flags[i] |= FLAG_DEFAULT;
