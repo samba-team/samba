6f273ccd863672c998ffd0e26bdcc355a52969bf
Garming Sam <garming@catalyst.net.nz>
2014-01-08 10:05:35 +1300

    lib/param: add default for change notify parameter
    Signed-off-by: Garming Sam <garming@catalyst.net.nz>
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Alexander Bokovoy <ab@samba.org>

--- lib/param/loadparm.c
+++ lib/param/loadparm.c
@@ -2250,6 +2250,8 @@ struct loadparm_context *loadparm_init(TALLOC_CTX *mem_ctx)
 
 	lpcfg_do_global_parameter(lp_ctx, "client use spnego", "True");
 
+	lpcfg_do_global_parameter(lp_ctx, "change notify", "True");
+
 	for (i = 0; parm_table[i].label; i++) {
 		if (!(lp_ctx->flags[i] & FLAG_CMDLINE)) {
 			lp_ctx->flags[i] |= FLAG_DEFAULT;
