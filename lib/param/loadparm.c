37f7d07d17a0f443f96e42503d0ac19ee9da6d0d
Garming Sam <garming@catalyst.net.nz>
2014-01-08 09:54:13 +1300

    lib/param: add default for short preserve case
    Signed-off-by: Garming Sam <garming@catalyst.net.nz>
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Alexander Bokovoy <ab@samba.org>

--- lib/param/loadparm.c
+++ lib/param/loadparm.c
@@ -2238,6 +2238,8 @@ struct loadparm_context *loadparm_init(TALLOC_CTX *mem_ctx)
 
 	lpcfg_do_global_parameter(lp_ctx, "server schannel", "Auto");
 
+	lpcfg_do_global_parameter(lp_ctx, "short preserve case", "True");
+
 	for (i = 0; parm_table[i].label; i++) {
 		if (!(lp_ctx->flags[i] & FLAG_CMDLINE)) {
 			lp_ctx->flags[i] |= FLAG_DEFAULT;
