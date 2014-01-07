f2c262800a57b6bc92ab9b7ae904cd0e06f0629c
Garming Sam <garming@catalyst.net.nz>
2014-01-07 17:36:57 +1300

    lib/param: add default for enhanced browsing parameter
    Signed-off-by: Garming Sam <garming@catalyst.net.nz>
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Alexander Bokovoy <ab@samba.org>

--- lib/param/loadparm.c
+++ lib/param/loadparm.c
@@ -2232,6 +2232,8 @@ struct loadparm_context *loadparm_init(TALLOC_CTX *mem_ctx)
 
 	lpcfg_do_global_parameter(lp_ctx, "algorithmic rid base", "1000");
 
+	lpcfg_do_global_parameter(lp_ctx, "enhanced browsing", "True");
+
 	for (i = 0; parm_table[i].label; i++) {
 		if (!(lp_ctx->flags[i] & FLAG_CMDLINE)) {
 			lp_ctx->flags[i] |= FLAG_DEFAULT;
