8f40b755aea4819fb054e02ca4d51e75692f8d5d
Garming Sam <garming@catalyst.net.nz>
2014-01-08 10:44:35 +1300

    lib/param: add default for hide dot files parameter
    Signed-off-by: Garming Sam <garming@catalyst.net.nz>
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Alexander Bokovoy <ab@samba.org>

--- lib/param/loadparm.c
+++ lib/param/loadparm.c
@@ -2282,6 +2282,8 @@ struct loadparm_context *loadparm_init(TALLOC_CTX *mem_ctx)
 
 	lpcfg_do_global_parameter(lp_ctx, "mangling method", "hash2");
 
+	lpcfg_do_global_parameter(lp_ctx, "hide dot files", "True");
+
 	for (i = 0; parm_table[i].label; i++) {
 		if (!(lp_ctx->flags[i] & FLAG_CMDLINE)) {
 			lp_ctx->flags[i] |= FLAG_DEFAULT;
