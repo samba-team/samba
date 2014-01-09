223bd6ed110deb8c9c302e337c8a8099d2efa846
Andrew Bartlett <abartlet@samba.org>
2014-01-09 13:52:44 +1300

    param: Add defaults for tls parameters to s3 loadparm
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Alexander Bokovoy <ab@samba.org>

--- source3/param/loadparm.c
+++ source3/param/loadparm.c
@@ -1047,6 +1047,10 @@ static void init_globals(bool reinit_globals)
 
 	Globals.tls_enabled = true;
 
+	string_set(&Globals.tls_keyfile, "tls/key.pem");
+	string_set(&Globals.tls_certfile, "tls/cert.pem");
+	string_set(&Globals.tls_cafile, "tls/ca.pem");
+
 	string_set(&Globals.share_backend, "classic");
 
 	Globals.iPreferredMaster = Auto;
