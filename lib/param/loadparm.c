158ce93f4f50f3b10bcadef9eadd613c55bb6cae
Andrew Bartlett <abartlet@samba.org>
2014-01-09 13:36:24 +1300

    lib/param: Fix default of logon home to match s3
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Michael Adam <obnox@samba.org>

--- lib/param/loadparm.c
+++ lib/param/loadparm.c
@@ -2374,6 +2374,8 @@ struct loadparm_context *loadparm_init(TALLOC_CTX *mem_ctx)
 
 	lpcfg_do_global_parameter(lp_ctx, "smb passwd file", get_dyn_SMB_PASSWD_FILE());
 
+	lpcfg_do_global_parameter(lp_ctx, "logon home", "\\\\%N\\%U");
+
 	for (i = 0; parm_table[i].label; i++) {
 		if (!(lp_ctx->flags[i] & FLAG_CMDLINE)) {
 			lp_ctx->flags[i] |= FLAG_DEFAULT;
