53522ac9946c78b00d4beb081777c80190b2f100
Andrew Bartlett <abartlet@samba.org>
2014-01-14 13:07:52 +1300

    docs: Allow smb.conf.5 to still be generated outside the waf build
    This will be needed once we start to use the XML entities for the
    configure-dependent paths.
    
    Change-Id: I2b64f02bb365cc099178b63b2b24bdb239dbb7c1
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Michael Adam <obnox@samba.org>

--- docs-xml/smbdotconf/generate-file-list.sh
+++ docs-xml/smbdotconf/generate-file-list.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
+#
+# This is the fallback table for when we use the docs-xml build
+# system.  When build as part of the main waf build, these are set to
+# the full correct path for the system.
+#
+echo "<!DOCTYPE section [
+<!ENTITY pathconfig.SCRIPTSBINDIR       '\${prefix}/sbin'>
+<!ENTITY pathconfig.LOCKDIR             '\${prefix}/var/lock'>
+<!ENTITY pathconfig.NCALRPCDIR          '\${prefix}/var/run/ncalrpc'>
+<!ENTITY pathconfig.PIDDIR              '\${prefix}/var/run'>
+<!ENTITY pathconfig.STATEDIR            '\${prefix}/var/locks'>
+<!ENTITY pathconfig.PRIVATE_DIR         '\${prefix}/private'>
+<!ENTITY pathconfig.SMB_PASSWD_FILE     '\${prefix}/private/smbpasswd'>
+<!ENTITY pathconfig.WINBINDD_SOCKET_DIR '\${prefix}/var/run/ncalrpc'>
+]>"
+
 DIR=.
 if [ "x$1" != "x" ]
 then
@@ -8,12 +24,12 @@ fi
 OLD=`pwd`
 cd $DIR
 
-echo "<section xmlns:xi=\"http://www.w3.org/2003/XInclude\">"
+echo "<section>"
 for I in `find . -mindepth 2 -type f -name '*.xml' | sort -t/ -k3 | xargs`
 do 
-	echo "<xi:include href='$I' parse='xml'/>"
+	cat $I
 done
-                
 echo "</section>"
+
 
 cd $OLD
