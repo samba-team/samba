020fab300d2f4f19301eff19ad810c71f77bbb78
Michael Adam <obnox@samba.org>
2014-01-27 16:21:14 +0100

    s3:rpc_client: optimize the netlogon_creds_cli.tdb for read-only access
    Usually a record in this DB will be written once and then read
    many times by winbindd processes on multiple nodes (when run in
    a cluster). In order not to introduce a big performance penalty
    with the increased correctness achieved by storing the netlogon
    creds, in a cluster setup, we should activate ctdb's read only
    record copies on this db.
    
    Signed-off-by: Michael Adam <obnox@samba.org>
    Reviewed-by: Stefan Metzmacher <metze@samba.org>

--- source3/rpc_client/cli_netlogon.c
+++ source3/rpc_client/cli_netlogon.c
@@ -70,7 +70,7 @@ NTSTATUS rpccli_pre_open_netlogon_creds(void)
 	global_db = db_open(talloc_autofree_context(), fname,
 			    0, TDB_CLEAR_IF_FIRST|TDB_INCOMPATIBLE_HASH,
 			    O_RDWR|O_CREAT, 0600, DBWRAP_LOCK_ORDER_2,
-			    DBWRAP_FLAG_NONE);
+			    DBWRAP_FLAG_OPTIMIZE_READONLY_ACCESS);
 	if (global_db == NULL) {
 		TALLOC_FREE(frame);
 		return NT_STATUS_NO_MEMORY;
