5e5a31bfb2de9ffa9829db1002118773a22ac9e4
Michael Adam <obnox@samba.org>
2014-01-28 11:54:06 +0100

    dbwrap: add DBWRAP_LOCK_ORDER_VALID()
    Signed-off-by: Michael Adam <obnox@samba.org>
    Reviewed-by: Stefan Metzmacher <metze@samba.org>

--- lib/dbwrap/dbwrap_private.h
+++ lib/dbwrap/dbwrap_private.h
@@ -72,5 +72,9 @@ struct db_context {
 #define DBWRAP_LOCK_ORDER_MIN DBWRAP_LOCK_ORDER_1
 #define DBWRAP_LOCK_ORDER_MAX DBWRAP_LOCK_ORDER_3
 
+#define DBWRAP_LOCK_ORDER_VALID(order) \
+	(((order) >= DBWRAP_LOCK_ORDER_MIN) && \
+	 ((order) <= DBWRAP_LOCK_ORDER_MAX))
+
 #endif /* __DBWRAP_PRIVATE_H__ */
 
