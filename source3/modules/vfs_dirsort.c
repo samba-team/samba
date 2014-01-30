c86c98d18e2b0719238341d6b8d5554a9abaa350
Jeremy Allison <jra@samba.org>
2014-01-29 17:01:30 -0800

    s3: vfs_dirsort module.
    Allow dirsort to work when multiple simultaneous
    directories are open. The old code only keeps one
    active private data pointer on the connection struct, opening
    a second directory on the same connection will overwrite it.
    
    This modification turns the private data pointer
    into a linked list of open directories on the
    connection struct, and finds the correct one by searching
    on the passed in DIR *.
    
    With this code in place, smbd passes raw.search
    torture test on a share definition with:
    
    vfs objects = dirsort
    
    https://bugzilla.samba.org/show_bug.cgi?id=10406
    
    Signed-off-by: Jeremy Allison <jra@samba.org>
    
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>
    (back-ported from commit fd79652b0e598882e0c4d156bd897c29dae8ec04)

--- source3/modules/vfs_dirsort.c
+++ source3/modules/vfs_dirsort.c
@@ -28,6 +28,7 @@ static int compare_dirent (const struct dirent *da, const struct dirent *db)
 }
 
 struct dirsort_privates {
+	struct dirsort_privates *prev, *next;
 	long pos;
 	struct dirent *directory_list;
 	unsigned int number_of_entries;
@@ -36,10 +37,6 @@ struct dirsort_privates {
 	files_struct *fsp; /* If open via FDOPENDIR. */
 	struct smb_filename *smb_fname; /* If open via OPENDIR */
 };
-
-static void free_dirsort_privates(void **datap) {
-	TALLOC_FREE(*datap);
-}
 
 static bool get_sorted_dir_mtime(vfs_handle_struct *handle,
 				struct dirsort_privates *data,
@@ -120,7 +117,14 @@ static DIR *dirsort_opendir(vfs_handle_struct *handle,
 				       uint32 attr)
 {
 	NTSTATUS status;
+	struct dirsort_privates *list_head = NULL;
 	struct dirsort_privates *data = NULL;
+
+	if (SMB_VFS_HANDLE_TEST_DATA(handle)) {
+		/* Find the list head of all open directories. */
+		SMB_VFS_HANDLE_GET_DATA(handle, list_head, struct dirsort_privates,
+				return NULL);
+	}
 
 	/* set up our private data about this directory */
 	data = talloc_zero(handle->conn, struct dirsort_privates);
@@ -153,7 +157,9 @@ static DIR *dirsort_opendir(vfs_handle_struct *handle,
 		return NULL;
 	}
 
-	SMB_VFS_HANDLE_SET_DATA(handle, data, free_dirsort_privates,
+	/* Add to the private list of all open directories. */
+	DLIST_ADD(list_head, data);
+	SMB_VFS_HANDLE_SET_DATA(handle, list_head, NULL,
 				struct dirsort_privates, return NULL);
 
 	return data->source_directory;
@@ -164,7 +170,14 @@ static DIR *dirsort_fdopendir(vfs_handle_struct *handle,
 					const char *mask,
 					uint32 attr)
 {
+	struct dirsort_privates *list_head = NULL;
 	struct dirsort_privates *data = NULL;
+
+	if (SMB_VFS_HANDLE_TEST_DATA(handle)) {
+		/* Find the list head of all open directories. */
+		SMB_VFS_HANDLE_GET_DATA(handle, list_head, struct dirsort_privates,
+				return NULL);
+	}
 
 	/* set up our private data about this directory */
 	data = talloc_zero(handle->conn, struct dirsort_privates);
@@ -191,7 +204,9 @@ static DIR *dirsort_fdopendir(vfs_handle_struct *handle,
 		return NULL;
 	}
 
-	SMB_VFS_HANDLE_SET_DATA(handle, data, free_dirsort_privates,
+	/* Add to the private list of all open directories. */
+	DLIST_ADD(list_head, data);
+	SMB_VFS_HANDLE_SET_DATA(handle, list_head, NULL,
 				struct dirsort_privates, return NULL);
 
 	return data->source_directory;
@@ -207,12 +222,20 @@ static struct dirent *dirsort_readdir(vfs_handle_struct *handle,
 	SMB_VFS_HANDLE_GET_DATA(handle, data, struct dirsort_privates,
 				return NULL);
 
+	while(data && (data->source_directory != dirp)) {
+		data = data->next;
+	}
+	if (data == NULL) {
+		return NULL;
+	}
+
 	if (get_sorted_dir_mtime(handle, data, &current_mtime) == false) {
 		return NULL;
 	}
 
 	/* throw away cache and re-read the directory if we've changed */
-	if (timespec_compare(&current_mtime, &data->mtime) > 1) {
+	if (timespec_compare(&current_mtime, &data->mtime)) {
+		SMB_VFS_NEXT_REWINDDIR(handle, data->source_directory);
 		open_and_sort_dir(handle, data);
 	}
 
@@ -226,10 +249,53 @@ static struct dirent *dirsort_readdir(vfs_handle_struct *handle,
 static void dirsort_seekdir(vfs_handle_struct *handle, DIR *dirp,
 			    long offset)
 {
+	struct timespec current_mtime;
 	struct dirsort_privates *data = NULL;
+
 	SMB_VFS_HANDLE_GET_DATA(handle, data, struct dirsort_privates, return);
 
+	/* Find the entry holding dirp. */
+	while(data && (data->source_directory != dirp)) {
+		data = data->next;
+	}
+	if (data == NULL) {
+		return;
+	}
+	if (offset > data->number_of_entries) {
+		return;
+	}
 	data->pos = offset;
+
+	if (get_sorted_dir_mtime(handle, data, &current_mtime) == false) {
+		return;
+	}
+
+	if (timespec_compare(&current_mtime, &data->mtime)) {
+		/* Directory changed. We must re-read the
+		   cache and search for the name that was
+		   previously stored at the offset being
+		   requested, otherwise after the re-sort
+		   we will point to the wrong entry. The
+		   OS/2 incremental delete code relies on
+		   this. */
+		unsigned int i;
+		char *wanted_name = talloc_strdup(handle->conn,
+					data->directory_list[offset].d_name);
+		if (wanted_name == NULL) {
+			return;
+		}
+		SMB_VFS_NEXT_REWINDDIR(handle, data->source_directory);
+		open_and_sort_dir(handle, data);
+		/* Now search for where we were. */
+		data->pos = 0;
+		for (i = 0; i < data->number_of_entries; i++) {
+			if(strcmp(wanted_name, data->directory_list[i].d_name) == 0) {
+				data->pos = i;
+				break;
+			}
+		}
+		TALLOC_FREE(wanted_name);
+	}
 }
 
 static long dirsort_telldir(vfs_handle_struct *handle, DIR *dirp)
@@ -238,6 +304,13 @@ static long dirsort_telldir(vfs_handle_struct *handle, DIR *dirp)
 	SMB_VFS_HANDLE_GET_DATA(handle, data, struct dirsort_privates,
 				return -1);
 
+	/* Find the entry holding dirp. */
+	while(data && (data->source_directory != dirp)) {
+		data = data->next;
+	}
+	if (data == NULL) {
+		return -1;
+	}
 	return data->pos;
 }
 
@@ -246,7 +319,38 @@ static void dirsort_rewinddir(vfs_handle_struct *handle, DIR *dirp)
 	struct dirsort_privates *data = NULL;
 	SMB_VFS_HANDLE_GET_DATA(handle, data, struct dirsort_privates, return);
 
+	/* Find the entry holding dirp. */
+	while(data && (data->source_directory != dirp)) {
+		data = data->next;
+	}
+	if (data == NULL) {
+		return;
+	}
 	data->pos = 0;
+}
+
+static int dirsort_closedir(vfs_handle_struct *handle, DIR *dirp)
+{
+	struct dirsort_privates *list_head = NULL;
+	struct dirsort_privates *data = NULL;
+	int ret;
+
+	SMB_VFS_HANDLE_GET_DATA(handle, list_head, struct dirsort_privates, return -1);
+	/* Find the entry holding dirp. */
+	for(data = list_head; data && (data->source_directory != dirp); data = data->next) {
+		;
+	}
+	if (data == NULL) {
+		return -1;
+	}
+	/* Remove from the list and re-store the list head. */
+	DLIST_REMOVE(list_head, data);
+	SMB_VFS_HANDLE_SET_DATA(handle, list_head, NULL,
+				struct dirsort_privates, return -1);
+
+	ret = SMB_VFS_NEXT_CLOSEDIR(handle, dirp);
+	TALLOC_FREE(data);
+	return ret;
 }
 
 static struct vfs_fn_pointers vfs_dirsort_fns = {
@@ -256,6 +360,7 @@ static struct vfs_fn_pointers vfs_dirsort_fns = {
 	.seekdir_fn = dirsort_seekdir,
 	.telldir_fn = dirsort_telldir,
 	.rewind_dir_fn = dirsort_rewinddir,
+	.closedir_fn = dirsort_closedir,
 };
 
 NTSTATUS vfs_dirsort_init(void)
