0aa080b0f7c4950d29187c2f531e1018d545d007
Volker Lendecke <vl@samba.org>
2014-02-02 15:54:53 +0100

    unixuid: Use the tevent_context from the ntvfs_context
    Signed-off-by: Volker Lendecke <vl@samba.org>
    
    Change-Id: I4edb0ee4cefdc2f1b309202c9ec70c7c7bbac0b8
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Andreas Schneider <asn@samba.org>

--- source4/ntvfs/unixuid/vfs_unixuid.c
+++ source4/ntvfs/unixuid/vfs_unixuid.c
@@ -154,10 +154,8 @@ static NTSTATUS nt_token_to_unix_security(struct ntvfs_module_context *ntvfs,
 					  struct security_token *token,
 					  struct security_unix_token **sec)
 {
-	struct unixuid_private *priv = ntvfs->private_data;
-
 	return security_token_to_unix_token(req,
-					    priv->wbc_ctx->event_ctx,
+					    ntvfs->ctx->event_ctx,
 					    token, sec);
 }
 
