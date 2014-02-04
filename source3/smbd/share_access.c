8256cb5a37f15ce22dcfee19806a3323a4b9c34c
Garming Sam <garming@catalyst.net.nz>
2014-02-04 15:09:10 +1300

    param: rename lp function and variable from "readlist" to "read_list"
    Signed-off-by: Garming Sam <garming@catalyst.net.nz>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/smbd/share_access.c
+++ source3/smbd/share_access.c
@@ -261,11 +261,11 @@ bool is_share_read_only_for_token(const char *username,
 	int snum = SNUM(conn);
 	bool result = conn->read_only;
 
-	if (lp_readlist(snum) != NULL) {
+	if (lp_read_list(snum) != NULL) {
 		if (token_contains_name_in_list(username, domain,
 						lp_servicename(talloc_tos(), snum),
 						token,
-						lp_readlist(snum))) {
+						lp_read_list(snum))) {
 			result = True;
 		}
 	}
