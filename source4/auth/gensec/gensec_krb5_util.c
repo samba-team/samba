04c8c65e4cf0d8e8953f63148beef2e2a6569887
Stefan Metzmacher <metze@samba.org>
2014-02-26 20:16:26 +0100

    s4:auth/gensec: include gensec_krb5_util.h in gensec_krb5_util.c
    This avoids missing prototype warnings.
    
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- source4/auth/gensec/gensec_krb5_util.c
+++ source4/auth/gensec/gensec_krb5_util.c
@@ -37,6 +37,7 @@
 #include "includes.h"
 #include "system/kerberos.h"
 #include "auth/kerberos/kerberos.h"
+#include "gensec_krb5_util.h"
 
 /* Taken from  accept_sec_context.c,v 1.65 */
 krb5_error_code smb_rd_req_return_stuff(krb5_context context, 
