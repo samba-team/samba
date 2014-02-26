a5e5547ad46905a0a32912e489a8c09a5bac1629
Björn Baumbach <bb@sernet.de>
2014-02-26 15:26:28 +0100

    docs-smbdotconf: Fix build - add missing XML entities (config paths)
    Signed-off-by: Björn Baumbach <bb@sernet.de>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- docs-xml/smbdotconf/generate-file-list.sh
+++ docs-xml/smbdotconf/generate-file-list.sh
@@ -5,14 +5,17 @@
 # the full correct path for the system.
 #
 echo "<!DOCTYPE section [
-<!ENTITY pathconfig.SCRIPTSBINDIR       '\${prefix}/sbin'>
-<!ENTITY pathconfig.LOCKDIR             '\${prefix}/var/lock'>
-<!ENTITY pathconfig.NCALRPCDIR          '\${prefix}/var/run/ncalrpc'>
-<!ENTITY pathconfig.PIDDIR              '\${prefix}/var/run'>
-<!ENTITY pathconfig.STATEDIR            '\${prefix}/var/locks'>
-<!ENTITY pathconfig.PRIVATE_DIR         '\${prefix}/private'>
-<!ENTITY pathconfig.SMB_PASSWD_FILE     '\${prefix}/private/smbpasswd'>
-<!ENTITY pathconfig.WINBINDD_SOCKET_DIR '\${prefix}/var/run/ncalrpc'>
+<!ENTITY pathconfig.SCRIPTSBINDIR        '\${prefix}/sbin'>
+<!ENTITY pathconfig.LOCKDIR              '\${prefix}/var/lock'>
+<!ENTITY pathconfig.NCALRPCDIR           '\${prefix}/var/run/ncalrpc'>
+<!ENTITY pathconfig.PIDDIR               '\${prefix}/var/run'>
+<!ENTITY pathconfig.STATEDIR             '\${prefix}/var/locks'>
+<!ENTITY pathconfig.PRIVATE_DIR          '\${prefix}/private'>
+<!ENTITY pathconfig.SMB_PASSWD_FILE      '\${prefix}/private/smbpasswd'>
+<!ENTITY pathconfig.WINBINDD_SOCKET_DIR  '\${prefix}/var/run/ncalrpc'>
+<!ENTITY pathconfig.WINBINDD_PRIVILEGED_SOCKET_DIR '\${prefix}/var/lib/winbindd_privileged'>
+<!ENTITY pathconfig.CACHEDIR             '\${prefix}/var/cache'>
+<!ENTITY pathconfig.NTP_SIGND_SOCKET_DIR '\${prefix}/var/lib/ntp_signd'>
 ]>"
 
 DIR=.
