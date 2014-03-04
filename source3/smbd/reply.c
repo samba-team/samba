b86f90611820bcf7826bd1de3c7b05488a8f1b0e
Stefan Metzmacher <metze@samba.org>
2014-03-04 14:07:26 +0100

    s3:smbd: pass the final numtoread reply_outbuf() for the lockread reply.
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/smbd/reply.c
+++ source3/smbd/reply.c
@@ -3492,10 +3492,6 @@ void reply_lockread(struct smb_request *req)
 
 	numtoread = MIN(BUFFER_SIZE - (smb_size + 5*2 + 3), numtoread);
 
-	reply_outbuf(req, 5, numtoread + 3);
-
-	data = smb_buf(req->outbuf) + 3;
-
 	/*
 	 * NB. Discovered by Menny Hamburger at Mainsoft. This is a core+
 	 * protocol request that predates the read/write lock concept. 
@@ -3534,6 +3530,11 @@ Returning short read of maximum allowed for compatibility with Windows 2000.\n",
 			(unsigned int)sconn->smb1.negprot.max_recv));
 		numtoread = MIN(numtoread, sconn->smb1.negprot.max_recv);
 	}
+
+	reply_outbuf(req, 5, numtoread + 3);
+
+	data = smb_buf(req->outbuf) + 3;
+
 	nread = read_file(fsp,data,startpos,numtoread);
 
 	if (nread < 0) {
