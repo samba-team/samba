c0e239473b532cebd31ef5e53d8cbf23c262c18d
Martin Schwenke <martin@meltin.net>
2014-03-17 06:15:39 +1100

    ctdb-tests: Add a test for NAT gateway sanity checking
    Signed-off-by: Martin Schwenke <martin@meltin.net>

---
+++ ctdb/tests/eventscripts/11.natgw.002.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+. "${TEST_SCRIPTS_DIR}/unit.sh"
+
+define_test "missing config file"
+
+setup_ctdb
+setup_ctdb_natgw <<EOF
+192.168.1.21 master
+192.168.1.22
+192.168.1.23
+192.168.1.24
+EOF
+
+rm -f "$CTDB_NATGW_NODES"
+
+required_result 1 <<EOF
+error: CTDB_NATGW_NODES=${CTDB_NATGW_NODES} unreadable
+EOF
+
+for i in "startup" "ipreallocated" "monitor" ; do
+    simple_test_event "$i"
+done
+
