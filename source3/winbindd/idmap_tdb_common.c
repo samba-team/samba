26062b4a9d3c8684f8d1d3cf39656cb89f7a959c
Michael Adam <obnox@samba.org>
2014-04-11 23:52:20 +0200

    idmap_tdb_common: remove legacy comment.
    Signed-off-by: Michael Adam <obnox@samba.org>
    Reviewed-by: Stefan Metzmacher <metze@samba.org>

--- source3/winbindd/idmap_tdb_common.c
+++ source3/winbindd/idmap_tdb_common.c
@@ -383,11 +383,7 @@ NTSTATUS idmap_tdb_common_unixid_to_sid(struct idmap_domain * dom,
 	if (!dom || !map) {
 		return NT_STATUS_INVALID_PARAMETER;
 	}
-/* TODO: do we really make sure the database is open again? should have been
- * done while initing the module
-	status = idmap_tdb2_open_db(dom);
-	NT_STATUS_NOT_OK_RETURN(status);
-*/
+
 	ctx =
 	    talloc_get_type_abort(dom->private_data,
 				  struct idmap_tdb_common_context);
