ffaa2849e5afc573c4b0d82086b233f4675bed85
Jeremy Allison <jra@samba.org>
2014-05-01 11:11:20 -0700

    s3: smbd: Remove open_file_fchmod().
    No longer used (hurrah!).
    
    Bug 10564 - Lock order violation and file lost
    
    https://bugzilla.samba.org/show_bug.cgi?id=10564
    
    Signed-off-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: Volker Lendecke <vl@samba.org>
    
    Autobuild-User(master): Jeremy Allison <jra@samba.org>
    Autobuild-Date(master): Fri May  2 23:47:38 CEST 2014 on sn-devel-104

--- source3/smbd/open.c
+++ source3/smbd/open.c
@@ -2870,39 +2870,6 @@ static NTSTATUS open_file_ntcreate(connection_struct *conn,
 	return NT_STATUS_OK;
 }
 
-
-/****************************************************************************
- Open a file for for write to ensure that we can fchmod it.
-****************************************************************************/
-
-NTSTATUS open_file_fchmod(connection_struct *conn,
-			  struct smb_filename *smb_fname,
-			  files_struct **result)
-{
-	if (!VALID_STAT(smb_fname->st)) {
-		return NT_STATUS_INVALID_PARAMETER;
-	}
-
-        return SMB_VFS_CREATE_FILE(
-		conn,					/* conn */
-		NULL,					/* req */
-		0,					/* root_dir_fid */
-		smb_fname,				/* fname */
-		FILE_WRITE_DATA,			/* access_mask */
-		(FILE_SHARE_READ | FILE_SHARE_WRITE |	/* share_access */
-		    FILE_SHARE_DELETE),
-		FILE_OPEN,				/* create_disposition*/
-		0,					/* create_options */
-		0,					/* file_attributes */
-		INTERNAL_OPEN_ONLY,			/* oplock_request */
-		0,					/* allocation_size */
-		0,					/* private_flags */
-		NULL,					/* sd */
-		NULL,					/* ea_list */
-		result,					/* result */
-		NULL);					/* pinfo */
-}
-
 static NTSTATUS mkdir_internal(connection_struct *conn,
 			       struct smb_filename *smb_dname,
 			       uint32 file_attributes)
