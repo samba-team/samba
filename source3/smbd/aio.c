25aacde2983eb12537bf0723ec3031d3776012d8
Jeremy Allison <jra@samba.org>
2014-05-01 10:58:51 -0700

    s3 : smbd : Protect all possible code paths from fsp->op == NULL.
    In changes to come this will be possible for an INTERNAL_OPEN_ONLY.
    The protection was already in place for some code paths, this
    makes the coverage compete.
    
    Bug 10564 - Lock order violation and file lost
    
    https://bugzilla.samba.org/show_bug.cgi?id=10564
    
    Signed-off-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: Volker Lendecke <vl@samba.org>

--- source3/smbd/aio.c
+++ source3/smbd/aio.c
@@ -688,6 +688,11 @@ NTSTATUS schedule_smb2_aio_read(connection_struct *conn,
 		return NT_STATUS_RETRY;
 	}
 
+	if (fsp->op == NULL) {
+		/* No AIO on internal opens. */
+		return NT_STATUS_RETRY;
+	}
+
 	if ((!min_aio_read_size || (smb_maxcnt < min_aio_read_size))
 	    && !SMB_VFS_AIO_FORCE(fsp)) {
 		/* Too small a read for aio request. */
@@ -836,6 +841,11 @@ NTSTATUS schedule_aio_smb2_write(connection_struct *conn,
 	if (fsp->base_fsp != NULL) {
 		/* No AIO on streams yet */
 		DEBUG(10, ("AIO on streams not yet supported\n"));
+		return NT_STATUS_RETRY;
+	}
+
+	if (fsp->op == NULL) {
+		/* No AIO on internal opens. */
 		return NT_STATUS_RETRY;
 	}
 
