d51422661dcb6e2badd9f35a7654571e3b6216cf
Jeremy Allison <jra@samba.org>
2014-05-01 11:11:20 -0700

    s3: smbd: Remove open_file_fchmod().
    No longer used (hurrah!).
    
    Bug 10564 - Lock order violation and file lost
    
    https://bugzilla.samba.org/show_bug.cgi?id=10564
    
    Signed-off-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: Volker Lendecke <vl@samba.org>
    
    Autobuild-User(master): Jeremy Allison <jra@samba.org>
    Autobuild-Date(master): Fri May  2 23:47:38 CEST 2014 on sn-devel-104

--- source3/smbd/proto.h
+++ source3/smbd/proto.h
@@ -620,9 +620,6 @@ NTSTATUS change_dir_owner_to_parent(connection_struct *conn,
 				    SMB_STRUCT_STAT *psbuf);
 bool is_stat_open(uint32 access_mask);
 bool is_deferred_open_async(const void *ptr);
-NTSTATUS open_file_fchmod(connection_struct *conn,
-			  struct smb_filename *smb_fname,
-			  files_struct **result);
 NTSTATUS create_directory(connection_struct *conn, struct smb_request *req,
 			  struct smb_filename *smb_dname);
 void msg_file_was_renamed(struct messaging_context *msg,
