23848f35474f96f101da20644205099c0b1d3c9e
Andrew Bartlett <abartlet@samba.org>
2014-05-08 12:16:24 +1200

    s3-rpc_server: Use C99 types in rpc_pipes.h
    Change-Id: Ic282f02f421870ff8a8623005979f8a034902d88
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Andreas Schneider <asn@samba.org>
    
    Autobuild-User(master): Andrew Bartlett <abartlet@samba.org>
    Autobuild-Date(master): Wed Jun  4 05:48:29 CEST 2014 on sn-devel-104

--- source3/rpc_server/rpc_pipes.h
+++ source3/rpc_server/rpc_pipes.h
@@ -33,7 +33,7 @@ typedef struct _output_data {
 	DATA_BLOB rdata;
 
 	/* The amount of data sent from the current rdata struct. */
-	uint32 data_sent_length;
+	uint32_t data_sent_length;
 
 	/*
 	 * The current fragment being returned. This inclues
@@ -42,7 +42,7 @@ typedef struct _output_data {
 	DATA_BLOB frag;
 
 	/* The amount of data sent from the current PDU. */
-	uint32 current_pdu_sent;
+	uint32_t current_pdu_sent;
 } output_data;
 
 typedef struct _input_data {
@@ -61,7 +61,7 @@ typedef struct _input_data {
 	 * If this is zero, then we are at the start of a new
 	 * pdu.
 	 */
-	uint32 pdu_needed_len;
+	uint32_t pdu_needed_len;
 
 	/*
 	 * This is the collection of input data with all
@@ -79,7 +79,7 @@ struct pipes_struct;
 
 struct api_struct {
 	const char *name;
-	uint8 opnum;
+	uint8_t opnum;
 	bool (*fn) (struct pipes_struct *);
 };
 
