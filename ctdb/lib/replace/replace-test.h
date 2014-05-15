91274d97a644ece172ad99937967cec3d7258b0e
Amitay Isaacs <amitay@gmail.com>
2014-05-16 00:04:35 +1000

    ctdb-build: Remove duplicate replace library
    Signed-off-by: Amitay Isaacs <amitay@gmail.com>
    Reviewed-by: Michael Adam <obnox@samba.org>

--- ctdb/lib/replace/replace-test.h
+++
@@ -1,9 +0,0 @@
-#ifndef __LIB_REPLACE_REPLACE_TEST_H__
-#define __LIB_REPLACE_REPLACE_TEST_H__
-
-int libreplace_test_strptime(void);
-int test_readdir_os2_delete(void);
-int getifaddrs_test(void);
-
-#endif /* __LIB_REPLACE_REPLACE_TEST_H__ */
-
