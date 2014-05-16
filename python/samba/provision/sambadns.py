eb0e0b13600f9f26250ee118c04dd347456423d2
Kai Blin <kai@samba.org>
2014-05-16 18:33:42 +0200

    provision: Correctly provision the SOA record minimum TTL
    This fixes bug #10466
    
    Signed-off-by: Kai Blin <kai@samba.org>
    Reviewed-by: Guenter Kukkukk <kukks@samba.org>
    
    Autobuild-User(master): Kai Blin <kai@samba.org>
    Autobuild-Date(master): Wed May 21 10:55:00 CEST 2014 on sn-devel-104
    
    (cherry picked from commit 634f116fbb89d723a627f4501b4cd89342cecb8e)

--- python/samba/provision/sambadns.py
+++ python/samba/provision/sambadns.py
@@ -125,6 +125,7 @@ class SOARecord(dnsp.DnssrvRpcRecord):
         soa.expire = expire
         soa.mname = mname
         soa.rname = rname
+        soa.minimum = minimum
         self.data = soa
 
 
