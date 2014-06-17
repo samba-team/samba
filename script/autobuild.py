46924f68c1db00a402b419b4f3c71d60d473ea77
Amitay Isaacs <amitay@gmail.com>
2014-06-17 09:24:48 -0700

    autobuild: Remove unsupported --enable-socket-wrapper option from the "ctdb" target.
    Socket wrapper now works with LD_PRELOAD.
    
    Signed-off-by: Amitay Isaacs <amitay@gmail.com>
    Reviewed-by: Michael Adam <obnox@samba.org>

--- script/autobuild.py
+++ script/autobuild.py
@@ -36,7 +36,7 @@ defaulttasks = [ "ctdb", "samba", "samba-ctdb", "samba-libs", "ldb", "tdb", "ntd
 
 tasks = {
     "ctdb" : [ ("random-sleep", "../script/random-sleep.sh 60 600", "text/plain"),
-               ("configure", "./configure ${PREFIX} --enable-socket-wrapper ", "text/plain"),
+               ("configure", "./configure ${PREFIX}", "text/plain"),
                ("make", "make all", "text/plain"),
                ("install", "make install", "text/plain"),
                ("test", "make autotest", "text/plain"),
