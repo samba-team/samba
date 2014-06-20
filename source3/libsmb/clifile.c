700b43940a6984fb4e4e4b7ad96804501cf71bca
Volker Lendecke <vl@samba.org>
2014-06-20 08:52:45 +0000

    libsmb: remove smb2 switch from cli_ntcreate
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/libsmb/clifile.c
+++ source3/libsmb/clifile.c
@@ -2020,19 +2020,6 @@ NTSTATUS cli_ntcreate(struct cli_state *cli,
 	struct tevent_req *req;
 	NTSTATUS status = NT_STATUS_OK;
 
-	if (smbXcli_conn_protocol(cli->conn) >= PROTOCOL_SMB2_02) {
-		return cli_smb2_create_fnum(cli,
-					fname,
-					CreatFlags,
-					DesiredAccess,
-					FileAttributes,
-					ShareAccess,
-					CreateDisposition,
-					CreateOptions,
-					pfid,
-					cr);
-	}
-
 	frame = talloc_stackframe();
 
 	if (smbXcli_conn_has_async_calls(cli->conn)) {
