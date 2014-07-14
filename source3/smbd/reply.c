d27f3ef318820d18214b91a6d33cfa537a6f41d7
Volker Lendecke <vl@samba.org>
2014-07-14 15:10:53 +0000

    smbd: Fix a typo
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Stefan Metzmacher <metze@samba.org>

--- source3/smbd/reply.c
+++ source3/smbd/reply.c
@@ -7649,7 +7649,7 @@ NTSTATUS smbd_do_locking(struct smb_request *req,
 			if (num_locks > 1) {
 				/*
 				 * MS-CIFS (2.2.4.32.1) states that a cancel is honored if and only
-				 * if the lock vector contains one entry. When given mutliple cancel
+				 * if the lock vector contains one entry. When given multiple cancel
 				 * requests in a single PDU we expect the server to return an
 				 * error. Windows servers seem to accept the request but only
 				 * cancel the first lock.
