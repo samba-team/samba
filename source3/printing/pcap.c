5a647c5258f57c93563a4e962aa712221874bf2d
David Disseldorp <ddiss@samba.org>
2014-07-23 12:12:34 +0200

    printing: return last change time with pcap_cache_loaded()
    Bug: https://bugzilla.samba.org/show_bug.cgi?id=10652
    
    Signed-off-by: David Disseldorp <ddiss@samba.org>
    Reviewed-by: Andreas Schneider <asn@samba.org>
    (cherry picked from commit 30ce835670a6aeca6fb960ea7c4fe1b982bdd5b0)

--- source3/printing/pcap.c
+++ source3/printing/pcap.c
@@ -83,13 +83,19 @@ void pcap_cache_destroy_specific(struct pcap_cache **pp_cache)
 	*pp_cache = NULL;
 }
 
-bool pcap_cache_loaded(void)
+bool pcap_cache_loaded(time_t *_last_change)
 {
 	NTSTATUS status;
 	time_t last;
 
 	status = printer_list_get_last_refresh(&last);
-	return NT_STATUS_IS_OK(status);
+	if (!NT_STATUS_IS_OK(status)) {
+		return false;
+	}
+	if (_last_change != NULL) {
+		*_last_change = last;
+	}
+	return true;
 }
 
 bool pcap_cache_replace(const struct pcap_cache *pcache)
