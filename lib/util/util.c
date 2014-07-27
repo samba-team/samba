2b9c35da12f6892e22253e8beb6a7ec95ba17c58
Volker Lendecke <vl@samba.org>
2014-07-27 19:08:52 +0200

    lib: Simplify directory_create_or_exist with an early return
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- lib/util/util.c
+++ lib/util/util.c
@@ -196,35 +196,36 @@ _PUBLIC_ bool directory_create_or_exist(const char *dname,
 {
 	int ret;
 	struct stat st;
+	mode_t old_umask;
+
+	ret = lstat(dname, &st);
+	if (ret == 0) {
+		return true;
+	}
+
+	if (errno != ENOENT) {
+		DEBUG(0, ("lstat failed on directory %s: %s\n",
+			  dname, strerror(errno)));
+		return false;
+	}
+
+	/* Create directory */
+	old_umask = umask(0);
+	ret = mkdir(dname, dir_perms);
+	if (ret == -1 && errno != EEXIST) {
+		DEBUG(0, ("mkdir failed on directory "
+			  "%s: %s\n", dname,
+			  strerror(errno)));
+		umask(old_umask);
+		return false;
+	}
+	umask(old_umask);
 
 	ret = lstat(dname, &st);
 	if (ret == -1) {
-		mode_t old_umask;
-
-		if (errno != ENOENT) {
-			DEBUG(0, ("lstat failed on directory %s: %s\n",
-				  dname, strerror(errno)));
-			return false;
-		}
-
-		/* Create directory */
-		old_umask = umask(0);
-		ret = mkdir(dname, dir_perms);
-		if (ret == -1 && errno != EEXIST) {
-			DEBUG(0, ("mkdir failed on directory "
-				  "%s: %s\n", dname,
-				  strerror(errno)));
-			umask(old_umask);
-			return false;
-		}
-		umask(old_umask);
-
-		ret = lstat(dname, &st);
-		if (ret == -1) {
-			DEBUG(0, ("lstat failed on created directory %s: %s\n",
-				  dname, strerror(errno)));
-			return false;
-		}
+		DEBUG(0, ("lstat failed on created directory %s: %s\n",
+			  dname, strerror(errno)));
+		return false;
 	}
 
 	return true;
