222996931d5ef8661ec69f34a9a0e2f4e8a980fb
Volker Lendecke <vl@samba.org>
2014-07-29 11:20:27 +0000

    debug: Add some readability to debug_logtype
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Michael Adam <obnox@samba.org>

--- lib/util/debug.h
+++ lib/util/debug.h
@@ -207,7 +207,14 @@ extern int  *DEBUGLEVEL_CLASS;
  * for example.  This makes it easy to override for debug to stderr on
  * the command line, as the smb.conf cannot reset it back to
  * file-based logging */
-enum debug_logtype {DEBUG_DEFAULT_STDERR = 0, DEBUG_DEFAULT_STDOUT = 1, DEBUG_FILE = 2, DEBUG_STDOUT = 3, DEBUG_STDERR = 4, DEBUG_CALLBACK = 5};
+enum debug_logtype {
+	DEBUG_DEFAULT_STDERR = 0,
+	DEBUG_DEFAULT_STDOUT = 1,
+	DEBUG_FILE = 2,
+	DEBUG_STDOUT = 3,
+	DEBUG_STDERR = 4,
+	DEBUG_CALLBACK = 5
+};
 
 struct debug_settings {
 	size_t max_log_size;
