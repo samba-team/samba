7fce187a0dc7b0d53cae1d544a645d6d8d440045
Martin Schwenke <martin@meltin.net>
2014-08-18 14:58:53 +1000

    ctdb-includes: Clean up includes.h
    Remove unused #defines.  CTDB doesn't use these.  _PUBLIC_ is defined
    in replace.h anyway.
    
    Remove unnecessary #includes.
    
    Signed-off-by: Martin Schwenke <martin@meltin.net>
    Reviewed-by: Amitay Isaacs <amitay@gmail.com>

--- ctdb/include/internal/includes.h
+++ ctdb/include/internal/includes.h
@@ -1,14 +1,11 @@
 #ifndef _CTDB_INCLUDES_H
 #define _CTDB_INCLUDES_H
 
-#define HAVE_UNIXSOCKET 1
-
 #include "replace.h"
 #include "talloc.h"
 #include "system/wait.h"
 #include "system/network.h"
 #include "tdb.h"
-#include "idtree.h"
 #include "ctdb_client.h"
 
 /* Allow use of deprecated function tevent_loop_allow_nesting() */
@@ -19,24 +16,6 @@
 #include "tevent.h"
 
 #include "ctdb_logging.h"
-
-#ifndef _PUBLIC_
-#define _PUBLIC_
-#endif /* _PUBLIC_ */
-#ifndef _NORETURN_
-#define _NORETURN_
-#endif /* _NORETURN_ */
-#ifndef _PURE_
-#define _PURE_
-#endif /* _PURE_ */
-
-#ifndef ZERO_STRUCT
-#define ZERO_STRUCT(x) memset((char *)&(x), 0, sizeof(x))
-#endif /* ZERO_STRUCT */
-
-#ifndef discard_const
-#define discard_const(ptr) ((void *)((intptr_t)(ptr)))
-#endif
 
 #include "lib/util/debug.h"
 #include "lib/util/samba_util.h"
