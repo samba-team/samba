6975ff05ab952ddde9dd9044663e434b35b56fe0
Stefan Metzmacher <metze@samba.org>
2014-08-28 13:26:44 +0200

    s3:selftest: run the smb2.replay test against the //$SERVER_IP/durable share
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Michael Adam <obnox@samba.org>

--- source3/selftest/tests.py
+++ source3/selftest/tests.py
@@ -334,7 +334,7 @@ for t in tests:
     elif t == "rpc.samr.passwords.validate":
         plansmbtorture4testsuite(t, "s3dc", 'ncacn_ip_tcp:$SERVER_IP -U$USERNAME%$PASSWORD', 'over ncacn_ip_tcp ')
         plansmbtorture4testsuite(t, "plugin_s4_dc", 'ncacn_ip_tcp:$SERVER_IP -U$USERNAME%$PASSWORD', 'over ncacn_ip_tcp ')
-    elif t == "smb2.durable-open" or t == "smb2.durable-v2-open":
+    elif t == "smb2.durable-open" or t == "smb2.durable-v2-open" or t == "smb2.replay":
         plansmbtorture4testsuite(t, "s3dc", '//$SERVER_IP/durable -U$USERNAME%$PASSWORD')
         plansmbtorture4testsuite(t, "plugin_s4_dc", '//$SERVER_IP/durable -U$USERNAME%$PASSWORD')
     elif t == "base.rw1":
