c6e726f719e8c510fc51e681532183803f766ca0
Volker Lendecke <vl@samba.org>
2014-09-09 10:53:52 +0000

    s3: smbd: streams - Ensure share mode validation ignores internal opens (op_mid == 0).
    Fixes bug 10797 - smbd panic at find_oplock_types
    
    https://bugzilla.samba.org/show_bug.cgi?id=10797
    
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/smbd/open.c
+++ source3/smbd/open.c
@@ -1110,7 +1110,7 @@ static void validate_my_share_entries(struct smbd_server_connection *sconn,
 		return;
 	}
 
-	if (share_entry->share_file_id == 0) {
+	if (share_entry->op_mid == 0) {
 		/* INTERNAL_OPEN_ONLY */
 		return;
 	}
@@ -1288,6 +1288,11 @@ static bool validate_oplock_types(struct share_mode_lock *lck)
 		struct share_mode_entry *e = &d->share_modes[i];
 
 		if (!is_valid_share_mode_entry(e)) {
+			continue;
+		}
+
+		if (e->op_mid == 0) {
+			/* INTERNAL_OPEN_ONLY */
 			continue;
 		}
 
