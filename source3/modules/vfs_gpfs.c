c56a88da6ec7be1a0f042270a5750c4834a9e95a
Volker Lendecke <vl@samba.org>
2014-09-12 13:03:52 +0200

    vfs_gpfs: Fix the build with profiling-data
    This fails with -Werror=declaration-after-statement
    
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>
    
    Autobuild-User(master): Jeremy Allison <jra@samba.org>
    Autobuild-Date(master): Tue Sep 16 01:55:57 CEST 2014 on sn-devel-104

--- source3/modules/vfs_gpfs.c
+++ source3/modules/vfs_gpfs.c
@@ -78,6 +78,8 @@ static int vfs_gpfs_kernel_flock(vfs_handle_struct *handle, files_struct *fsp,
 	struct gpfs_config_data *config;
 	int ret = 0;
 
+	START_PROFILE(syscall_kernel_flock);
+
 	SMB_VFS_HANDLE_GET_DATA(handle, config,
 				struct gpfs_config_data,
 				return -1);
@@ -85,8 +87,6 @@ static int vfs_gpfs_kernel_flock(vfs_handle_struct *handle, files_struct *fsp,
 	if(!config->sharemodes) {
 		return 0;
 	}
-
-	START_PROFILE(syscall_kernel_flock);
 
 	kernel_flock(fsp->fh->fd, share_mode, access_mask);
 
@@ -121,14 +121,14 @@ static int vfs_gpfs_setlease(vfs_handle_struct *handle, files_struct *fsp,
 	struct gpfs_config_data *config;
 	int ret=0;
 
+	START_PROFILE(syscall_linux_setlease);
+
 	SMB_VFS_HANDLE_GET_DATA(handle, config,
 				struct gpfs_config_data,
 				return -1);
 
 	if (linux_set_lease_sighandler(fsp->fh->fd) == -1)
 		return -1;
-
-	START_PROFILE(syscall_linux_setlease);
 
 	if (config->leases) {
 		/*
