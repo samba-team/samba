31e5faa008c4e1acdf25c12ef03183896596a88b
Michael Adam <obnox@samba.org>
2014-09-22 20:13:39 +0200

    selftest: run smbtorture3 LOCAL-MESSAGING-FDPASS1 test
    Signed-off-by: Michael Adam <obnox@samba.org>
    Reviewed-by: Stefan Metzmacher <metze@samba.org>

--- source3/selftest/tests.py
+++ source3/selftest/tests.py
@@ -109,6 +109,7 @@ local_tests = [
     "LOCAL-MESSAGING-READ1",
     "LOCAL-MESSAGING-READ2",
     "LOCAL-MESSAGING-READ3",
+    "LOCAL-MESSAGING-FDPASS1",
     "LOCAL-hex_encode_buf",
     "LOCAL-sprintf_append",
     "LOCAL-remove_duplicate_addrs2"]
