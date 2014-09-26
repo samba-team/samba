c8dca765a0c984602389bbd707eca7c58cd41b41
Stefan Metzmacher <metze@samba.org>
2014-09-26 03:14:53 +0200

    s3:lib/netapi: avoid calling cli_set_username()
    Change-Id: I3ab768d2df06749187555a16d7b930f7cc8f8b9f
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- source3/lib/netapi/cm.c
+++ source3/lib/netapi/cm.c
@@ -113,9 +113,7 @@ static WERROR libnetapi_open_ipc_connection(struct libnetapi_ctx *ctx,
 			     false, false,
 			     lp_client_max_protocol(),
 			     0, 0x20, &cli_ipc);
-	if (NT_STATUS_IS_OK(status)) {
-		cli_set_username(cli_ipc, ctx->username);
-	} else {
+	if (!NT_STATUS_IS_OK(status)) {
 		cli_ipc = NULL;
 	}
 	TALLOC_FREE(auth_info);
