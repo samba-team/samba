samba-team/samba

commit 0ce46318715577bd8273ed9c0880be54542f227f

winbind3: Simplify fillup_pw_field
Signed-off-by: Volker Lendecke <vl@samba.org>
Reviewed-by: Andrew Bartlett <abartlet@samba.org>

Autobuild-User(master): Volker Lendecke <vl@samba.org>
Autobuild-Date(master): Wed Feb  5 13:47:52 CET 2014 on sn-devel-104

source3/winbindd/wb_fill_pwent.c
@@ -214,32 +214,31 @@ static bool fillup_pw_field(const char *lp_template,
 			    const char *in,
 			    fstring out)
 {
-	char *templ;
+	const char *templ;
+	char *result;
 
 	if (out == NULL)
 		return False;
 
-	/* The substitution of %U and %D in the 'template
-	   homedir' is done by talloc_sub_specified() below.
-	   If we have an in string (which means the value has already
-	   been set in the nss_info backend), then use that.
-	   Otherwise use the template value passed in. */
+	templ = lp_template;
 
 	if ((in != NULL) && (in[0] != '\0') && (lp_security() == SEC_ADS)) {
-		templ = talloc_sub_specified(talloc_tos(), in,
-					     username, grpname, domname,
-					     uid, gid);
-	} else {
-		templ = talloc_sub_specified(talloc_tos(), lp_template,
-					     username, grpname, domname,
-					     uid, gid);
+		/*
+		 * The backend has already filled in the required value. Use
+		 * that instead of the template.
+		 */
+		templ = in;
 	}
 
-	if (!templ)
+	result = talloc_sub_specified(talloc_tos(), templ,
+				      username, grpname, domname,
+				      uid, gid);
+	if (result == NULL) {
 		return False;
+	}
 
 	fstrcpy(out, templ);
-	TALLOC_FREE(templ);
+	TALLOC_FREE(result);
 
 	return True;
 

commit 902086d0d4c688236ad4a8248a50de8639e1e524

winbind3: Fix pwent variable substitution
Commit 0ce46318 (winbind3: Simplify fillup_pw_field) broke variable
substitution by copying from the wrong (unsubstituted) buffer.  Fix it.

Signed-off-by: Justin Maggard <jmaggard10@gmail.com>
Reviewed-by: Jeremy Allison <jra@samba.org>
Reviewed-by: Richard Sharpe <rsharpe@samba.org>

source3/winbindd/wb_fill_pwent.c
@@ -240,7 +240,7 @@ static bool fillup_pw_field(const char *lp_template,
 		return False;
 	}
 
-	fstrcpy(out, templ);
+	fstrcpy(out, result);
 	TALLOC_FREE(result);
 
 	return True;