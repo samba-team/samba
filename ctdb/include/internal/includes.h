d558281205a9a3c29cd10fdf94284caf37067fb7
Martin Schwenke <martin@meltin.net>
2014-10-08 19:59:10 +1100

    ctdb-includes: Reorder includes.h more logically
    For now replace.h needs to be before tdb.h, since tdb.h needs
    stdbool.h.  This is fixed upstream but not yet in various packaged
    versions.
    
    Signed-off-by: Martin Schwenke <martin@meltin.net>
    Reviewed-by: Amitay Isaacs <amitay@gmail.com>
    
    Autobuild-User(master): Amitay Isaacs <amitay@samba.org>
    Autobuild-Date(master): Fri Oct 31 03:46:40 CET 2014 on sn-devel-104

--- ctdb/include/internal/includes.h
+++ ctdb/include/internal/includes.h
@@ -1,23 +1,24 @@
 #ifndef _CTDB_INCLUDES_H
 #define _CTDB_INCLUDES_H
 
+/* Replace must be before broken tdb.h to define bool */
 #include "replace.h"
-#include "talloc.h"
 #include "system/wait.h"
 #include "system/network.h"
-#include "tdb.h"
-#include "ctdb_client.h"
+
+#include <talloc.h>
+#include <tdb.h>
 
 /* Allow use of deprecated function tevent_loop_allow_nesting() */
 #define TEVENT_DEPRECATED
 /* Saves ctdb from massive churn. */
 #define TEVENT_COMPAT_DEFINES 1
-
-#include "tevent.h"
-
-#include "ctdb_logging.h"
+#include <tevent.h>
 
 #include "lib/util/debug.h"
 #include "lib/util/samba_util.h"
+
+#include "ctdb_client.h"
+#include "ctdb_logging.h"
 
 #endif /* _CTDB_INCLUDES_H */
