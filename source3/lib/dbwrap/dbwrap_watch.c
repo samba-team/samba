176259f2c47d3a32dc105e211cc8097c60031dbf
David Disseldorp <ddiss@samba.org>
2014-11-02 20:21:39 +0100

    dbwrap_watch: don't leak lock_path onto talloc tos
    Also check for allocation failures.
    
    Signed-off-by: David Disseldorp <ddiss@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/lib/dbwrap/dbwrap_watch.c
+++ source3/lib/dbwrap/dbwrap_watch.c
@@ -30,11 +30,17 @@ static struct db_context *dbwrap_record_watchers_db(void)
 	static struct db_context *watchers_db;
 
 	if (watchers_db == NULL) {
+		char *db_path = lock_path("dbwrap_watchers.tdb");
+		if (db_path == NULL) {
+			return NULL;
+		}
+
 		watchers_db = db_open(
-			NULL, lock_path("dbwrap_watchers.tdb"),	0,
+			NULL, db_path,	0,
 			TDB_CLEAR_IF_FIRST | TDB_INCOMPATIBLE_HASH,
 			O_RDWR|O_CREAT, 0600, DBWRAP_LOCK_ORDER_3,
 			DBWRAP_FLAG_NONE);
+		TALLOC_FREE(db_path);
 	}
 	return watchers_db;
 }
