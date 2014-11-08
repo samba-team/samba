e2b5ae135b740c7a6d04c2ef8137ce4064d83dff
Jelmer Vernooij <jelmer@samba.org>
2014-11-08 14:15:41 +0000

    data_blob.h: Add space between literal and string constant.
    C++11 compilers warn if there is a missing space:
    
    invalid suffix on literal; C++11 requires a space between literal and string macro [-Wliteral-suffix]
    
    Change-Id: Ib7e11c72c09de588ec558cfbf9dded8c625bf49b
    Signed-Off-By: Jelmer Vernooij <jelmer@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- lib/util/data_blob.h
+++ lib/util/data_blob.h
@@ -49,9 +49,9 @@ struct data_blob_list_item {
    a fair bit of code */
 #define ldb_val datablob
 
-#define data_blob(ptr, size) data_blob_named(ptr, size, "DATA_BLOB: "__location__)
-#define data_blob_talloc(ctx, ptr, size) data_blob_talloc_named(ctx, ptr, size, "DATA_BLOB: "__location__)
-#define data_blob_dup_talloc(ctx, blob) data_blob_talloc_named(ctx, (blob).data, (blob).length, "DATA_BLOB: "__location__)
+#define data_blob(ptr, size) data_blob_named(ptr, size, "DATA_BLOB: " __location__)
+#define data_blob_talloc(ctx, ptr, size) data_blob_talloc_named(ctx, ptr, size, "DATA_BLOB: " __location__)
+#define data_blob_dup_talloc(ctx, blob) data_blob_talloc_named(ctx, (blob).data, (blob).length, "DATA_BLOB: " __location__)
 
 /**
  construct a data blob, must be freed with data_blob_free()
