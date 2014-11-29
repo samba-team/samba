816751a3a8ed564f2cf880fd1ca3b1e8f9c85471
Stefan Metzmacher <metze@samba.org>
2014-11-29 10:52:05 +0100

    s3:idmap_cache: remove unused idmap_cache_set_sid2[u|g]id()
    Change-Id: I40bcfacb812b0dac7917533c9baf82a79f598efd
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Garming Sam <garming@catalyst.net.nz>
    
    Autobuild-User(master): Garming Sam <garming@samba.org>
    Autobuild-Date(master): Wed Dec  3 06:44:29 CET 2014 on sn-devel-104

--- source3/lib/idmap_cache.h
+++ source3/lib/idmap_cache.h
@@ -32,8 +32,6 @@ bool idmap_cache_find_sid2gid(const struct dom_sid *sid, gid_t *pgid,
 bool idmap_cache_find_uid2sid(uid_t uid, struct dom_sid *sid, bool *expired);
 bool idmap_cache_find_gid2sid(gid_t gid, struct dom_sid *sid, bool *expired);
 void idmap_cache_set_sid2unixid(const struct dom_sid *sid, struct unixid *unix_id);
-void idmap_cache_set_sid2uid(const struct dom_sid *sid, uid_t uid);
-void idmap_cache_set_sid2gid(const struct dom_sid *sid, gid_t gid);
 
 bool idmap_cache_del_uid(uid_t uid);
 bool idmap_cache_del_gid(gid_t gid);
