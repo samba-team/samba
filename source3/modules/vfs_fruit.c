3e8a1681b613608e2c282a07b76387f9fbf549e0
Jeremy Allison <jra@samba.org>
2014-12-05 15:37:11 -0800

    s3: modules: Fix *allocate* calls to follow POSIX error return convention.
    Fix up the ceph, fruit, time_audit and streams_xattr modules to follow
    the -1,errno convention for errors.
    
    Reported by Jones <jones.kstw@gmail.com> who provided the
    initial patch. This patch tested and confirmed working
    by him as well.
    
    Signed-off-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: David Disseldorp <ddiss@suse.de>
    
    Autobuild-User(master): Jeremy Allison <jra@samba.org>
    Autobuild-Date(master): Mon Dec  8 02:59:43 CET 2014 on sn-devel-104
    
    (cherry picked from commit 2845e1c29f8bce6306a73d546184c401bf89cfea)
    
    Autobuild-User(v4-2-test): Karolin Seeger <kseeger@samba.org>
    Autobuild-Date(v4-2-test): Mon Dec  8 12:37:27 CET 2014 on sn-devel-104

--- source3/modules/vfs_fruit.c
+++ source3/modules/vfs_fruit.c
@@ -3049,11 +3049,12 @@ static int fruit_fallocate(struct vfs_handle_struct *handle,
 	}
 
 	if (!fruit_fsp_recheck(ad, fsp)) {
-		return errno;
+		return -1;
 	}
 
 	/* Let the pwrite code path handle it. */
-	return ENOSYS;
+	errno = ENOSYS;
+	return -1;
 }
 
 static int fruit_ftruncate(struct vfs_handle_struct *handle,
