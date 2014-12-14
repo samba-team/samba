80e387adfac7a67ce91a87c656bfc5eded132c6d
Jelmer Vernooij <jelmer@samba.org>
2014-12-14 19:55:55 +0000

    Use iso8601 from the system, rather than the one bundled with subunit.
    Change-Id: I681bf79eb9ebe45b6972b3783c8e799eb612400b
    Signed-Off-By: Jelmer Vernooij <jelmer@samba.org>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- selftest/subunithelper.py
+++ selftest/subunithelper.py
@@ -21,7 +21,7 @@ import re
 import sys
 from samba import subunit
 from samba.subunit.run import TestProtocolClient
-from subunit import iso8601
+import iso8601
 import unittest
 
 VALID_RESULTS = ['success', 'successful', 'failure', 'fail', 'skip', 'knownfail', 'error', 'xfail', 'skip-testsuite', 'testsuite-failure', 'testsuite-xfail', 'testsuite-success', 'testsuite-error', 'uxsuccess', 'testsuite-uxsuccess']
