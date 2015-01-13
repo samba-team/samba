ecb145cb1400aa68903b2ee25476f6a4768f193f
Volker Lendecke <vl@samba.org>
2015-01-13 12:51:13 +0100

    net: Fix sam addgroupmem
    Domain local groups come across as SID_TYPE_ALIAS and are sent to us in the
    PAC/Info3 struct. We should allow this in net sam addgroupmem.
    
    Volker
    
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Guenther Deschner <gd@samba.org>
    Bug: https://bugzilla.samba.org/show_bug.cgi?id=11051
    
    Autobuild-User(master): Günther Deschner <gd@samba.org>
    Autobuild-Date(master): Tue Jan 13 15:28:16 CET 2015 on sn-devel-104

--- source3/utils/net_sam.c
+++ source3/utils/net_sam.c
@@ -1216,10 +1216,12 @@ static int net_sam_addmem(struct net_context *c, int argc, const char **argv)
 
 	if ((grouptype == SID_NAME_ALIAS) || (grouptype == SID_NAME_WKN_GRP)) {
 		if ((membertype != SID_NAME_USER) &&
+		    (membertype != SID_NAME_ALIAS) &&
 		    (membertype != SID_NAME_DOM_GRP)) {
-			d_fprintf(stderr, _("%s is a local group, only users "
-				  "and domain groups can be added.\n"
-				  "%s is a %s\n"), argv[0], argv[1],
+			d_fprintf(stderr, _("Can't add %s: only users, domain "
+					    "groups and domain local groups "
+					    "can be added. %s is a %s\n"),
+				  argv[0], argv[1],
 				  sid_type_lookup(membertype));
 			return -1;
 		}
