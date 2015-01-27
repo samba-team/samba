41f08b19642780a49f14b547b7a775a3f0904166
Stefan Metzmacher <metze@samba.org>
2015-01-27 21:45:47 +0000

    python/samba: add on optional 'special_name' argument to CredentialsOptions()
    This way we have have two sets or credentials on the command line,
    while at least one uses some prefix (special_name) for the arguments.
    
    The default options without special_name are:
    
      Credentials Options:
        --simple-bind-dn=DN
                            DN to use for a simple bind
        --password=PASSWORD
                            Password
        -U USERNAME, --username=USERNAME
                            Username
        -W WORKGROUP, --workgroup=WORKGROUP
                            Workgroup
        -N, --no-pass       Don't ask for a password
        -k KERBEROS, --kerberos=KERBEROS
                            Use Kerberos
        --ipaddress=IPADDRESS
                            IP address of server
        -P, --machine-pass  Use stored machine account password
    
    With special_name='local-dc' it's:
    
      Credentials Options (local-dc):
        --local-dc-simple-bind-dn=DN
                            DN to use for a simple bind
        --local-dc-password=PASSWORD
                            Password
        --local-dc-username=USERNAME
                            Username
        --local-dc-workgroup=WORKGROUP
                            Workgroup
        --local-dc-no-pass  Don't ask for a password
        --local-dc-kerberos=KERBEROS
                            Use Kerberos
        --local-dc-ipaddress=IPADDRESS
                            IP address of server
        --local-dc-machine-pass
                            Use stored machine account password
    
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- python/samba/getopt.py
+++ python/samba/getopt.py
@@ -125,37 +125,54 @@ def parse_kerberos_arg(arg, opt_str):
 class CredentialsOptions(optparse.OptionGroup):
     """Command line options for specifying credentials."""
 
-    def __init__(self, parser):
+    def __init__(self, parser, special_name=None):
+        self.special_name = special_name
+        if special_name is not None:
+            self.section = "Credentials Options (%s)" % special_name
+        else:
+            self.section = "Credentials Options"
+
         self.ask_for_password = True
         self.ipaddress = None
         self.machine_pass = False
-        optparse.OptionGroup.__init__(self, parser, "Credentials Options")
-        self.add_option("--simple-bind-dn", metavar="DN", action="callback",
+        optparse.OptionGroup.__init__(self, parser, self.section)
+        self._add_option("--simple-bind-dn", metavar="DN", action="callback",
                         callback=self._set_simple_bind_dn, type=str,
                         help="DN to use for a simple bind")
-        self.add_option("--password", metavar="PASSWORD", action="callback",
+        self._add_option("--password", metavar="PASSWORD", action="callback",
                         help="Password", type=str, callback=self._set_password)
-        self.add_option("-U", "--username", metavar="USERNAME",
+        self._add_option("-U", "--username", metavar="USERNAME",
                         action="callback", type=str,
                         help="Username", callback=self._parse_username)
-        self.add_option("-W", "--workgroup", metavar="WORKGROUP",
+        self._add_option("-W", "--workgroup", metavar="WORKGROUP",
                         action="callback", type=str,
                         help="Workgroup", callback=self._parse_workgroup)
-        self.add_option("-N", "--no-pass", action="callback",
+        self._add_option("-N", "--no-pass", action="callback",
                         help="Don't ask for a password",
                         callback=self._set_no_password)
-        self.add_option("-k", "--kerberos", metavar="KERBEROS",
+        self._add_option("-k", "--kerberos", metavar="KERBEROS",
                         action="callback", type=str,
                         help="Use Kerberos", callback=self._set_kerberos)
-        self.add_option("", "--ipaddress", metavar="IPADDRESS",
+        self._add_option("", "--ipaddress", metavar="IPADDRESS",
                         action="callback", type=str,
                         help="IP address of server",
                         callback=self._set_ipaddress)
-        self.add_option("-P", "--machine-pass",
+        self._add_option("-P", "--machine-pass",
                         action="callback",
                         help="Use stored machine account password",
                         callback=self._set_machine_pass)
         self.creds = Credentials()
+
+    def _add_option(self, *args1, **kwargs):
+        if self.special_name is None:
+            return self.add_option(*args1, **kwargs)
+
+        args2 = ()
+        for a in args1:
+            if not a.startswith("--"):
+                continue
+            args2 += (a.replace("--", "--%s-" % self.special_name),)
+        self.add_option(*args2, **kwargs)
 
     def _parse_username(self, option, opt_str, arg, parser):
         self.creds.parse_string(arg)
