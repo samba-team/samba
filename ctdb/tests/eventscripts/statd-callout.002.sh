bc1e3a83441d4b50ddb97d120b535ee9db6a902f
Martin Schwenke <martin@meltin.net>
2015-03-04 11:51:20 +1100

    ctdb-tests: Unit tests for statd-callout
    With improvements to unit test infrastructure to support.  This
    includes linking the real statd-callout into etc-ctdb/ in place of the
    placeholder script.
    
    Signed-off-by: Martin Schwenke <martin@meltin.net>
    Reviewed-by: Amitay Isaacs <amitay@gmail.com>
    (cherry picked from commit 22602f76bc1ec91e807a8f1cd45ba6fb4c05e622)

---
+++ ctdb/tests/eventscripts/statd-callout.002.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+. "${TEST_SCRIPTS_DIR}/unit.sh"
+
+define_test "2 x add-client"
+
+setup_ctdb
+
+FAKE_DATE_OUTPUT="1234565789"
+
+ok_null
+simple_test_event "add-client" "192.168.123.45"
+simple_test_event "add-client" "192.168.123.46"
+
+check_ctdb_tdb_statd_state "192.168.123.45" "192.168.123.46"
