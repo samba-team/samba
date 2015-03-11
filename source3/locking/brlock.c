7c48f5316c0e56afc98966c5a9bfc4b424cfe696
Christof Schmitt <cs@samba.org>
2015-03-11 15:54:55 -0700

    brlock: Use 0 instead of empty initializer list
    C does not allow empty initializer lists. Although gcc accepts that, the
    SunOS compiler fails in this case with an error.
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=11153
    
    Signed-off-by: Christof Schmitt <cs@samba.org>
    Reviewed-by: Michael Adam <obnox@samba.org>
    
    Autobuild-User(master): Christof Schmitt <cs@samba.org>
    Autobuild-Date(master): Thu Mar 12 02:49:36 CET 2015 on sn-devel-104
    (cherry picked from commit ddcf3614ce14e7cb03195e36f872043542bc4ec1)

--- source3/locking/brlock.c
+++ source3/locking/brlock.c
@@ -2005,7 +2005,7 @@ static void brl_get_locks_readonly_parser(TDB_DATA key, TDB_DATA data,
 		*state->br_lock = NULL;
 		return;
 	}
-	*br_lck = (struct byte_range_lock) {};
+	*br_lck = (struct byte_range_lock) { 0 };
 	if (!brl_parse_data(br_lck, data)) {
 		*state->br_lock = NULL;
 		return;
