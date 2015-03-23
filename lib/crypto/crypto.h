59cce7cbebee66df1789b9d3c2fc0b8311f9e78b
Michael Adam <obnox@samba.org>
2015-03-23 01:41:57 +0100

    lib/crypto: fix header guard for crypto.h
    Signed-off-by: Michael Adam <obnox@samba.org>
    Reviewed-by: Volker Lendecke <vl@samba.org>

--- lib/crypto/crypto.h
+++ lib/crypto/crypto.h
@@ -17,7 +17,7 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef _SAMBA_CRYPTO_H
+#ifndef _SAMBA_CRYPTO_H_
 #define _SAMBA_CRYPTO_H_
 
 #include "../lib/crypto/crc32.h"
