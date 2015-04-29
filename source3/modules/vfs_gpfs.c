5e65ae14ddb74c648f31b4dfbacd4af9c02ca058
Ralph Boehme <slow@samba.org>
2015-04-29 20:48:08 +0200

    vfp_gpfs: ensure END_PROFILE is always called
    Bug: https://bugzilla.samba.org/show_bug.cgi?id=11244
    
    Signed-off-by: Ralph Boehme <slow@samba.org>
    Reviewed-by: Christof Schmitt <cs@samba.org>
    
    Autobuild-User(master): Ralph Böhme <slow@samba.org>
    Autobuild-Date(master): Thu Apr 30 19:34:41 CEST 2015 on sn-devel-104

--- source3/modules/vfs_gpfs.c
+++ source3/modules/vfs_gpfs.c
@@ -125,8 +125,6 @@ static int vfs_gpfs_kernel_flock(vfs_handle_struct *handle, files_struct *fsp,
 	struct gpfs_config_data *config;
 	int ret = 0;
 
-	START_PROFILE(syscall_kernel_flock);
-
 	SMB_VFS_HANDLE_GET_DATA(handle, config,
 				struct gpfs_config_data,
 				return -1);
@@ -144,6 +142,8 @@ static int vfs_gpfs_kernel_flock(vfs_handle_struct *handle, files_struct *fsp,
 		DEBUG(2,("%s: kernel_flock on stream\n", fsp_str_dbg(fsp)));
 		return 0;
 	}
+
+	START_PROFILE(syscall_kernel_flock);
 
 	kernel_flock(fsp->fh->fd, share_mode, access_mask);
 
@@ -198,14 +198,16 @@ static int vfs_gpfs_setlease(vfs_handle_struct *handle, files_struct *fsp,
 	struct gpfs_config_data *config;
 	int ret=0;
 
-	START_PROFILE(syscall_linux_setlease);
-
 	SMB_VFS_HANDLE_GET_DATA(handle, config,
 				struct gpfs_config_data,
 				return -1);
 
-	if (linux_set_lease_sighandler(fsp->fh->fd) == -1)
-		return -1;
+	START_PROFILE(syscall_linux_setlease);
+
+	if (linux_set_lease_sighandler(fsp->fh->fd) == -1) {
+		ret = -1;
+		goto failure;
+	}
 
 	if (config->leases) {
 		/*
@@ -219,6 +221,7 @@ static int vfs_gpfs_setlease(vfs_handle_struct *handle, files_struct *fsp,
 
 	END_PROFILE(syscall_linux_setlease);
 
+failure:
 	return ret;
 }
 
