aa658dc7832738efecb1088b55be10367be0703a
Christian Ambach <ambi@samba.org>
2015-06-08 23:48:54 +0200

    s3:param/loadparm fix testparm --show-all-parameters
    it didn't know about the new types of config parameters
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=11170
    
    Signed-off-by: Christian Ambach <ambi@samba.org>
    Reviewed-by: Michael Adam <obnox@samba.org>
    
    Autobuild-User(master): Michael Adam <obnox@samba.org>
    Autobuild-Date(master): Tue Jun  9 11:49:18 CEST 2015 on sn-devel-104

--- source3/param/loadparm.c
+++ source3/param/loadparm.c
@@ -1726,7 +1726,7 @@ static void show_parameter(int parmIndex)
 	bool inverse;
 	const char *type[] = { "P_BOOL", "P_BOOLREV", "P_CHAR", "P_INTEGER",
 		"P_OCTAL", "P_LIST", "P_STRING", "P_USTRING",
-		"P_ENUM", "P_SEP"};
+		"P_ENUM", "P_BYTES", "P_CMDLIST", "P_SEP" };
 	unsigned flags[] = { FLAG_BASIC, FLAG_SHARE, FLAG_PRINT, FLAG_GLOBAL,
 		FLAG_WIZARD, FLAG_ADVANCED, FLAG_DEVELOPER, FLAG_DEPRECATED,
 		FLAG_HIDE};
