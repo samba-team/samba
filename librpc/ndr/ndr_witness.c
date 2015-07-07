117e87d8040c6a7e625aa2c52cbc7ab350bf669a
Michael Adam <obnox@samba.org>
2015-07-07 09:50:21 +0200

    librpc:ndr:witness: remove an unneeded block, reducing indentation.
    Check with "git diff -w".
    
    Signed-off-by: Michael Adam <obnox@samba.org>
    Reviewed-by: Guenther Deschner <gd@samba.org>
    
    Autobuild-User(master): Günther Deschner <gd@samba.org>
    Autobuild-Date(master): Tue Jul  7 23:37:05 CEST 2015 on sn-devel-104

--- librpc/ndr/ndr_witness.c
+++ librpc/ndr/ndr_witness.c
@@ -60,52 +60,51 @@ _PUBLIC_ enum ndr_err_code ndr_push_witness_notifyResponse(struct ndr_push *ndr,
 
 _PUBLIC_ enum ndr_err_code ndr_pull_witness_notifyResponse(struct ndr_pull *ndr, int ndr_flags, struct witness_notifyResponse *r)
 {
-	{
-		uint32_t _flags_save_STRUCT = ndr->flags;
-		ndr_set_flags(&ndr->flags, LIBNDR_PRINT_ARRAY_HEX);
-		NDR_PULL_CHECK_FLAGS(ndr, ndr_flags);
-		if (ndr_flags & NDR_SCALARS) {
-			NDR_CHECK(ndr_pull_align(ndr, 4));
-			NDR_CHECK(ndr_pull_witness_notifyResponse_type(ndr, NDR_SCALARS, &r->type));
-			NDR_CHECK(ndr_pull_uint32(ndr, NDR_SCALARS, &r->length));
-			NDR_CHECK(ndr_pull_uint32(ndr, NDR_SCALARS, &r->num));
-			{
-				uint32_t _flags_save_witness_notifyResponse_message = ndr->flags;
-				uint32_t _ptr_messages;
-				ndr_set_flags(&ndr->flags, LIBNDR_FLAG_REMAINING);
-				NDR_CHECK(ndr_pull_generic_ptr(ndr, &_ptr_messages));
-				if (_ptr_messages) {
-					NDR_PULL_ALLOC(ndr, r->messages);
-				} else {
-					r->messages = NULL;
-				}
-				if (r->messages) {
-					uint32_t size_messages_0 = 0;
-					uint32_t cntr_messages_0;
-					TALLOC_CTX *_mem_save_messages_0;
-
-					size_messages_0 = r->num;
-					NDR_PULL_ALLOC_N(ndr, r->messages, size_messages_0);
-					_mem_save_messages_0 = NDR_PULL_GET_MEM_CTX(ndr);
-					NDR_PULL_SET_MEM_CTX(ndr, r->messages, 0);
-					{
-						struct ndr_pull *_ndr_messages;
-						NDR_CHECK(ndr_pull_subcontext_start(ndr, &_ndr_messages, 4, r->length));
-						for (cntr_messages_0 = 0; cntr_messages_0 < (size_messages_0); cntr_messages_0++) {
-							NDR_CHECK(ndr_pull_set_switch_value(_ndr_messages, &r->messages[cntr_messages_0], r->type));
-							NDR_CHECK(ndr_pull_witness_notifyResponse_message(_ndr_messages, NDR_SCALARS, &r->messages[cntr_messages_0]));
-						}
-						NDR_CHECK(ndr_pull_subcontext_end(ndr, _ndr_messages, 4, r->length));
-					}
-					NDR_PULL_SET_MEM_CTX(ndr, _mem_save_messages_0, 0);
-				}
-				ndr->flags = _flags_save_witness_notifyResponse_message;
+	uint32_t _flags_save_STRUCT = ndr->flags;
+	ndr_set_flags(&ndr->flags, LIBNDR_PRINT_ARRAY_HEX);
+	NDR_PULL_CHECK_FLAGS(ndr, ndr_flags);
+	if (ndr_flags & NDR_SCALARS) {
+		NDR_CHECK(ndr_pull_align(ndr, 4));
+		NDR_CHECK(ndr_pull_witness_notifyResponse_type(ndr, NDR_SCALARS, &r->type));
+		NDR_CHECK(ndr_pull_uint32(ndr, NDR_SCALARS, &r->length));
+		NDR_CHECK(ndr_pull_uint32(ndr, NDR_SCALARS, &r->num));
+		{
+			uint32_t _flags_save_witness_notifyResponse_message = ndr->flags;
+			uint32_t _ptr_messages;
+			ndr_set_flags(&ndr->flags, LIBNDR_FLAG_REMAINING);
+			NDR_CHECK(ndr_pull_generic_ptr(ndr, &_ptr_messages));
+			if (_ptr_messages) {
+				NDR_PULL_ALLOC(ndr, r->messages);
+			} else {
+				r->messages = NULL;
 			}
-			NDR_CHECK(ndr_pull_trailer_align(ndr, 4));
+			if (r->messages) {
+				uint32_t size_messages_0 = 0;
+				uint32_t cntr_messages_0;
+				TALLOC_CTX *_mem_save_messages_0;
+
+				size_messages_0 = r->num;
+				NDR_PULL_ALLOC_N(ndr, r->messages, size_messages_0);
+				_mem_save_messages_0 = NDR_PULL_GET_MEM_CTX(ndr);
+				NDR_PULL_SET_MEM_CTX(ndr, r->messages, 0);
+				{
+					struct ndr_pull *_ndr_messages;
+					NDR_CHECK(ndr_pull_subcontext_start(ndr, &_ndr_messages, 4, r->length));
+					for (cntr_messages_0 = 0; cntr_messages_0 < (size_messages_0); cntr_messages_0++) {
+						NDR_CHECK(ndr_pull_set_switch_value(_ndr_messages, &r->messages[cntr_messages_0], r->type));
+						NDR_CHECK(ndr_pull_witness_notifyResponse_message(_ndr_messages, NDR_SCALARS, &r->messages[cntr_messages_0]));
+					}
+					NDR_CHECK(ndr_pull_subcontext_end(ndr, _ndr_messages, 4, r->length));
+				}
+				NDR_PULL_SET_MEM_CTX(ndr, _mem_save_messages_0, 0);
+			}
+			ndr->flags = _flags_save_witness_notifyResponse_message;
 		}
-		if (ndr_flags & NDR_BUFFERS) {
-		}
-		ndr->flags = _flags_save_STRUCT;
+		NDR_CHECK(ndr_pull_trailer_align(ndr, 4));
 	}
+	if (ndr_flags & NDR_BUFFERS) {
+	}
+	ndr->flags = _flags_save_STRUCT;
+
 	return NDR_ERR_SUCCESS;
 }
