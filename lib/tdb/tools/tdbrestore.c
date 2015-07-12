5768ca652360e24262200a30a45e137d85cfa245
Uri Simchoni <urisimchoni@gmail.com>
2015-07-12 09:29:13 +0300

    tdbrestore: include config.h before any glibc headers
    config.h may have some flags which affect glibc behavior, e.g.
    _FILE_OFFSET_BITS=64. To make sure these flags have the desired
    effect, config.h must be included before any glibc header files.
    
    This commit does not fix a specific known bug. It changes the code to
    comply with coding conventions.
    
    Signed-off-by: Uri Simchoni <urisimchoni@gmail.com>
    Reviewed-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: "Stefan Metzmacher" <metze@samba.org>

--- lib/tdb/tools/tdbrestore.c
+++ lib/tdb/tools/tdbrestore.c
@@ -17,8 +17,8 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <assert.h>
 #include "replace.h"
+#include <assert.h>
 #include "system/locale.h"
 #include "system/time.h"
 #include "system/filesys.h"
