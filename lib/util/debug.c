689d4a5796f185cbec407fd2dc8204b12f76f753
Uri Simchoni <urisimchoni@gmail.com>
2015-07-12 09:30:36 +0300

    lib/util: include config.h before any glibc headers
    config.h may have some flags which affect glibc behavior, e.g.
    _FILE_OFFSET_BITS=64. To make sure these flags have the desired
    effect, config.h must be included before any glibc header files.
    
    This commit does not fix a specific known bug. It changes the code to
    comply with coding conventions.
    
    Signed-off-by: Uri Simchoni <urisimchoni@gmail.com>
    Reviewed-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: "Stefan Metzmacher" <metze@samba.org>

--- lib/util/debug.c
+++ lib/util/debug.c
@@ -19,8 +19,8 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <talloc.h>
 #include "replace.h"
+#include <talloc.h>
 #include "system/filesys.h"
 #include "system/syslog.h"
 #include "system/locale.h"
