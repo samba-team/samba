3aaeaea13b5019f0d0d062a825cd2250de77c287
Günther Deschner <gd@samba.org>
2015-07-21 20:41:34 +0200

    s4-torture: remove trailing whitespace from remact test.
    Guenther
    
    Signed-off-by: Günther Deschner <gd@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source4/torture/rpc/remact.c
+++ source4/torture/rpc/remact.c
@@ -1,19 +1,19 @@
-/* 
+/*
    Unix SMB/CIFS implementation.
    test suite for remoteactivation operations
 
    Copyright (C) Jelmer Vernooij 2004
-   
+
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -27,7 +27,7 @@
 #define DCERPC_IUNKNOWN_UUID "00000000-0000-0000-c000-000000000046"
 #define DCERPC_ICLASSFACTORY_UUID "00000001-0000-0000-c000-000000000046"
 
-static bool test_RemoteActivation(struct torture_context *tctx, 
+static bool test_RemoteActivation(struct torture_context *tctx,
 								 struct dcerpc_pipe *p)
 {
 	struct RemoteActivation r;
@@ -62,15 +62,15 @@ static bool test_RemoteActivation(struct torture_context *tctx,
 	r.in.Mode = MODE_GET_CLASS_OBJECT;
 
 	status = dcerpc_RemoteActivation_r(b, tctx, &r);
-	torture_assert_ntstatus_ok(tctx, status, 
+	torture_assert_ntstatus_ok(tctx, status,
 							   "RemoteActivation(GetClassObject)");
 
-	torture_assert_werr_ok(tctx, r.out.result, 
+	torture_assert_werr_ok(tctx, r.out.result,
 						   "RemoteActivation(GetClassObject)");
 
 	torture_assert_werr_ok(tctx, *r.out.hr, "RemoteActivation(GetClassObject)");
 
-	torture_assert_werr_ok(tctx, r.out.results[0], 
+	torture_assert_werr_ok(tctx, r.out.results[0],
 						   "RemoteActivation(GetClassObject)");
 
 	return true;
