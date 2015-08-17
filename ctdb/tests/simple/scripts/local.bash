b933b91a0b03969eea73bb986f94e6c1d2e4887c
Martin Schwenke <martin@meltin.net>
2015-08-17 20:47:58 +1000

    ctdb-scripts: Update important installed files to use build-time defaults
    In this case: ctdbd_wrapper, onnode, ctdb_diagnostics, ctdb.sudoers.
    Set sensible defaults from configure options.
    
    Update documentation to match, trying to fix up anything that has been
    missed before.
    
    The onnode unit tests need a symlink to the functions file.
    The simple integration tests need to set CTDB_BASE and also
    need symlinks to functions/nodes.
    
    Signed-off-by: Martin Schwenke <martin@meltin.net>
    Reviewed-by: Amitay Isaacs <amitay@gmail.com>
    Reviewed-by: Jose A. Rivera <jarrpa@samba.org>
    Reviewed-by: Michael Adam <obnox@samba.org>
    
    fixup
    
    Signed-off-by: Martin Schwenke <martin@meltin.net>

--- ctdb/tests/simple/scripts/local.bash
+++ ctdb/tests/simple/scripts/local.bash
@@ -1,3 +1,8 @@
+# onnode needs CTDB_BASE to be set when run in-tree
+if [ -z "$CTDB_BASE" ] ; then
+    export CTDB_BASE="$TEST_SUBDIR"
+fi
+
 if [ -n "$TEST_LOCAL_DAEMONS" ] ; then
     . "${TEST_SUBDIR}/scripts/local_daemons.bash"
 fi
