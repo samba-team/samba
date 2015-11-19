65bd884149a68c01336c5af462b1e5b2ec66b3d5
Stefan Metzmacher <metze@samba.org>
2015-11-19 16:02:58 +0100

    CVE-2016-2110(<=4.2): auth/ntlmssp: implement new_spnego support including MIC checking (as server)
    This fixes the build in 4.2 and older versions.
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=11644
    
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Günther Deschner <gd@samba.org>

--- auth/ntlmssp/ntlmssp_server.c
+++ auth/ntlmssp/ntlmssp_server.c
@@ -608,10 +608,8 @@ static NTSTATUS ntlmssp_server_preauth(struct gensec_security *gensec_security,
 						 "user=[%s] domain=[%s] "
 						 "workstation=[%s]\n",
 						 __func__,
-						 timeval_str_buf(&ct, false,
-								 true, &tmp1),
-						 timeval_str_buf(&st, false,
-								 true, &tmp2),
+						 timeval_str_buf(&ct, true, &tmp1),
+						 timeval_str_buf(&st, true, &tmp2),
 						 ntlmssp_state->user,
 						 ntlmssp_state->domain,
 						 ntlmssp_state->client.netbios_name));
@@ -635,7 +633,7 @@ static NTSTATUS ntlmssp_server_preauth(struct gensec_security *gensec_security,
 		DEBUG(1,("%s: challenge invalid (expired %s) for "
 			 "user=[%s] domain=[%s] workstation=[%s]\n",
 			 __func__,
-			 timeval_str_buf(&endtime, false, true, &tmp),
+			 timeval_str_buf(&endtime, true, &tmp),
 			 ntlmssp_state->user, ntlmssp_state->domain,
 			 ntlmssp_state->client.netbios_name));
 		return NT_STATUS_INVALID_PARAMETER;
