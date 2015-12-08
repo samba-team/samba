2a906f88008779d2fbf1e676c20d931ed50d299a
RG72 <alexey@yourtask.ru>
2015-12-08 09:51:04 +0500

    Update loadparm.c

--- source3/param/loadparm.c
+++ source3/param/loadparm.c
@@ -902,6 +902,8 @@ static void init_globals(struct loadparm_context *lp_ctx, bool reinit_globals)
 	Globals.web_port = 901;
 
 	Globals.aio_max_threads = 100;
+	
+	Globals.disable_openfile_inode_check = false;
 
 	/* Now put back the settings that were set with lp_set_cmdline() */
 	apply_lp_set_cmdline();
