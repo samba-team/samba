7ba9f8239dc6be25ff8bf3473393184c7333eaa2
Günther Deschner <gd@samba.org>
2015-12-18 13:16:28 +0100

    s4-torture: add a negoex ndr pullpush test.
    Guenther
    
    Signed-off-by: Guenther Deschner <gd@samba.org>
    Reviewed-by: Stefan Metzmacher <metze@samba.org>
    
    Autobuild-User(master): Stefan Metzmacher <metze@samba.org>
    Autobuild-Date(master): Mon Dec 21 01:37:29 CET 2015 on sn-devel-144

--- source4/torture/ndr/ndr.c
+++ source4/torture/ndr/ndr.c
@@ -418,6 +418,7 @@ struct torture_suite *torture_local_ndr(TALLOC_CTX *mem_ctx)
 	torture_suite_add_suite(suite, ndr_backupkey_suite(suite));
 	torture_suite_add_suite(suite, ndr_witness_suite(suite));
 	torture_suite_add_suite(suite, ndr_clusapi_suite(suite));
+	torture_suite_add_suite(suite, ndr_negoex_suite(suite));
 	torture_suite_add_suite(suite, ndr_string_suite(suite));
 
 	torture_suite_add_simple_test(suite, "string terminator",
