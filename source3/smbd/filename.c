657610a0e423aa0cd17f9137845a335b02217884
Volker Lendecke <vl@samba.org>
2016-01-07 21:33:18 +0100

    smbd: Fix 240393 Uninitialized pointer read
    If we run into the "This is a stream on the root of the share" case,
    in old line 409 (new line 417) we "goto done;". If then in old line 1027
    (new line 1035) "build_stream_path" fails, "start" is uninitialized.
    
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>
    
    Autobuild-User(master): Jeremy Allison <jra@samba.org>
    Autobuild-Date(master): Fri Jan  8 05:44:15 CET 2016 on sn-devel-144

--- source3/smbd/filename.c
+++ source3/smbd/filename.c
@@ -235,7 +235,15 @@ NTSTATUS unix_convert(TALLOC_CTX *ctx,
 		      uint32_t ucf_flags)
 {
 	struct smb_filename *smb_fname = NULL;
-	char *start, *end;
+
+	/*
+	 * This looks strange. But we need "start" initialized to "" here but
+	 * it can't be a const char *, so 'char *start = "";' does not work.
+	 */
+	char cnull = '\0';
+	char *start = &cnull;
+
+	char *end;
 	char *dirpath = NULL;
 	char *stream = NULL;
 	bool component_was_mangled = False;
