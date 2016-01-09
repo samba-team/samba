1311631f611c1eb5dd9ecfdaf6d58aa09dcaf599
Stefan Metzmacher <metze@samba.org>
2016-01-09 21:21:25 +0100

    selftest: set tls crlfile if it exist
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=11804
    
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Günther Deschner <gd@samba.org>
    (cherry picked from commit b2c0f71db026353060ad47fd0a85241a3df8c703)

--- selftest/target/Samba4.pm
+++ selftest/target/Samba4.pm
@@ -501,6 +501,8 @@ sub provision_raw_step1($$)
 	}
 
 	Samba::prepare_keyblobs($ctx);
+	my $crlfile = "$ctx->{tlsdir}/crl.pem";
+	$crlfile = "" unless -e ${crlfile};
 
 	print CONFFILE "
 [global]
@@ -520,6 +522,7 @@ sub provision_raw_step1($$)
 	winbind separator = /
 	interfaces = $ctx->{interfaces}
 	tls dh params file = $ctx->{tlsdir}/dhparms.pem
+	tls crlfile = ${crlfile}
 	panic action = $RealBin/gdb_backtrace \%d
 	wins support = yes
 	server role = $ctx->{server_role}
