44b5d2d481e1fd9a960e15c3e91e74904e6dc5de
Stefan Metzmacher <metze@samba.org>
2016-01-09 01:08:02 +0100

    selftest: add config and script to create a samba.example.com CA
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=11804
    
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Günther Deschner <gd@samba.org>
    (cherry picked from commit bdc1f036a8a66256afe8dc88f8a9dc47655640bd)

---
+++ selftest/manage-ca/manage-CA-samba.example.com.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+
+set -e
+set -u
+set -x
+
+#
+# All passwords are "1234"
+#
+
+./manage-ca.sh manage-CA-samba.example.com.cnf init_ca
+
+./manage-ca.sh manage-CA-samba.example.com.cnf create_dc localdc.samba.example.com 0123456789ABCDEF
+./manage-ca.sh manage-CA-samba.example.com.cnf create_user administrator@samba.example.com
+
+./manage-ca.sh manage-CA-samba.example.com.cnf create_dc plugindc.plugindom.samba.example.com 0123456789ABCDEF
+./manage-ca.sh manage-CA-samba.example.com.cnf create_user administrator@plugindom.samba.example.com
