9e60cbfc7cff553c6e5ecfaa73a2e69d58c0e85a
Uri Simchoni <uri@samba.org>
2016-01-10 15:38:49 +0200

    vfs_ceph: do not call disk_norm() on disk_free_fn
    This is handled at SMB layer now.
    
    Signed-off-by: Uri Simchoni <uri@samba.org>
    Reviewed-by: Volker Lendecke <vl@samba.org>

--- source3/modules/vfs_ceph.c
+++ source3/modules/vfs_ceph.c
@@ -175,7 +175,6 @@ static uint64_t cephwrap_disk_free(struct vfs_handle_struct *handle,
 		*bsize = statvfs_buf.f_bsize;
 		*dfree = statvfs_buf.f_bavail;
 		*dsize = statvfs_buf.f_blocks;
-		disk_norm(bsize, dfree, dsize);
 		DEBUG(10, ("[CEPH] bsize: %llu, dfree: %llu, dsize: %llu\n",
 			llu(*bsize), llu(*dfree), llu(*dsize)));
 		return *dfree;
