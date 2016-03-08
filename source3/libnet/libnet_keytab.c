eb880ccc7c39aea3896f68c66feca473ece56606
Andreas Schneider <asn@samba.org>
2016-03-08 17:08:22 +0100

    s3-libnet: Allow the keytab function to use a relative path
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Stefan Metzmacher <metze@samba.org>

--- source3/libnet/libnet_keytab.c
+++ source3/libnet/libnet_keytab.c
@@ -81,7 +81,10 @@ krb5_error_code libnet_keytab_init(TALLOC_CTX *mem_ctx,
 		return ret;
 	}
 
-	ret = smb_krb5_open_keytab(context, keytab_name, true, &keytab);
+	ret = smb_krb5_open_keytab_relative(context,
+					    keytab_name,
+					    true, /* write_access */
+					    &keytab);
 	if (ret) {
 		DEBUG(1,("keytab_init: smb_krb5_open_keytab failed (%s)\n",
 			error_message(ret)));
