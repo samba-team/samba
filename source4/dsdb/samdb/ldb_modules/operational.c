e822b312389a1a41e1a384f298cb667ffed41edd
Andrew Bartlett <abartlet@samba.org>
2016-03-21 11:29:04 +1300

    dsdb: Improve debug messages in operational module
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Garming Sam <garming@catalyst.net.nz>
    Reviewed-by: Douglas Bagnall <douglas.bagnall@catalyst.net.nz>

--- source4/dsdb/samdb/ldb_modules/operational.c
+++ source4/dsdb/samdb/ldb_modules/operational.c
@@ -362,7 +362,8 @@ static int construct_parent_guid(struct ldb_module *module,
 	/* not NC, so the object should have a parent*/
 	if (ret == LDB_ERR_NO_SUCH_OBJECT) {
 		return ldb_error(ldb_module_get_ctx(module), LDB_ERR_OPERATIONS_ERROR, 
-				 talloc_asprintf(msg, "Parent dn for %s does not exist", 
+				 talloc_asprintf(msg, "Parent dn %s for %s does not exist",
+						 ldb_dn_get_linearized(parent_dn),
 						 ldb_dn_get_linearized(msg->dn)));
 	} else if (ret != LDB_SUCCESS) {
 		return ret;
@@ -1037,7 +1038,7 @@ static int operational_search_post_process(struct ldb_module *module,
 failed:
 	ldb_debug_set(ldb, LDB_DEBUG_WARNING,
 		      "operational_search_post_process failed for attribute '%s' - %s",
-		      attrs_from_user[a], ldb_errstring(ldb));
+		      list_replace[a].attr, ldb_errstring(ldb));
 	return -1;
 }
 
