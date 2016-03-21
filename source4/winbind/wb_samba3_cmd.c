8dcd3cb2bbe935a7db000ebb8455bd5a3efd234d
Stefan Metzmacher <metze@samba.org>
2016-03-21 23:07:12 +0100

    CVE-2016-2110(<=4.2): s4:winbind: implement the WBFLAG_BIG_NTLMV2_BLOB flag
    NTLMv2 blobs can become large...
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=11644
    
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Günther Deschner <gd@samba.org>

--- source4/winbind/wb_samba3_cmd.c
+++ source4/winbind/wb_samba3_cmd.c
@@ -643,8 +643,13 @@ NTSTATUS wbsrv_samba3_pam_auth_crap(struct wbsrv_samba3_call *s3call)
 
 	chal.data       = s3call->request->data.auth_crap.chal;
 	chal.length     = sizeof(s3call->request->data.auth_crap.chal);
-	nt_resp.data    = (uint8_t *)s3call->request->data.auth_crap.nt_resp;
-	nt_resp.length  = s3call->request->data.auth_crap.nt_resp_len;
+	if (s3call->request->flags & WBFLAG_BIG_NTLMV2_BLOB) {
+		nt_resp.data    = (uint8_t *)s3call->request->extra_data.data;
+		nt_resp.length  = s3call->request->extra_len;
+	} else {
+		nt_resp.data    = (uint8_t *)s3call->request->data.auth_crap.nt_resp;
+		nt_resp.length  = s3call->request->data.auth_crap.nt_resp_len;
+	}
 	lm_resp.data    = (uint8_t *)s3call->request->data.auth_crap.lm_resp;
 	lm_resp.length  = s3call->request->data.auth_crap.lm_resp_len;
 
