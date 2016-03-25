6e22abd9775e69aed018d04e5488757910862436
Stefan Metzmacher <metze@samba.org>
2016-03-25 19:24:20 +0100

    CVE-2016-2112: docs-xml: change the default of "ldap server require strong auth" to "yes"
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=11644
    
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Alexander Bokovoy <ab@samba.org>

--- source3/param/loadparm.c
+++ source3/param/loadparm.c
@@ -742,7 +742,7 @@ static void init_globals(struct loadparm_context *lp_ctx, bool reinit_globals)
 	Globals.client_ldap_sasl_wrapping = ADS_AUTH_SASL_SIGN;
 
 	Globals.ldap_server_require_strong_auth =
-		LDAP_SERVER_REQUIRE_STRONG_AUTH_NO;
+		LDAP_SERVER_REQUIRE_STRONG_AUTH_YES;
 
 	/* This is what we tell the afs client. in reality we set the token 
 	 * to never expire, though, when this runs out the afs client will 
