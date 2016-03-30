0124d3e6a537374c6639025934903ca03a08691e
Uri Simchoni <uri@samba.org>
2016-03-30 13:21:58 +0300

    nfs-quota: do not fail on ECONNREFUSED
    Trying to differentiate between "no quota" and real
    error conditions - if the connection to rpc.quotad
    is refused it could simply mean that the remote host
    has no quota and therefore report this as success with
    no quota.
    
    Signed-off-by: Uri Simchoni <uri@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/lib/sysquotas_nfs.c
+++ source3/lib/sysquotas_nfs.c
@@ -180,8 +180,20 @@ int sys_get_nfs_quota(const char *path, const char *bdev,
 			      timeout);
 
 	if (clnt_stat != RPC_SUCCESS) {
-		DEBUG(3, ("sys_get_nfs_quotas: clnt_call failed\n"));
-		ret = -1;
+		if (errno == ECONNREFUSED) {
+			/* If we cannot connect with rpc.quotad, it may
+			 * simply be because there's no quota on the remote
+			 * system
+			 */
+			DBG_INFO("clnt_call failed with ECONNREFUSED - "
+				 "assuming no quotas on server\n");
+			ret = 0;
+		} else {
+			int save_errno = errno;
+			DBG_NOTICE("clnt_call failed - %s\n", strerror(errno));
+			errno = save_errno;
+			ret = -1;
+		}
 		goto out;
 	}
 
