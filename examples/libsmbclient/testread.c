b279aa1f7cab0dcdf8466f0bea13faa0cbabacba
Michael Adam <obnox@samba.org>
2016-04-05 22:57:51 +0200

    examples:smbclient:read: fix O3 error unused result of fgets
    Signed-off-by: Michael Adam <obnox@samba.org>
    Reviewed-by: Christian Ambach <ambi@samba.org>

--- examples/libsmbclient/testread.c
+++ examples/libsmbclient/testread.c
@@ -24,7 +24,11 @@ int main(int argc, char * argv[])
     {
         fprintf(stdout, "Path: ");
         *path = '\0';
-        fgets(path, sizeof(path) - 1, stdin);
+	p = fgets(path, sizeof(path) - 1, stdin);
+	if (p == NULL) {
+		fprintf(stderr, "failed to read from stdin\n");
+		return 1;
+	}
         if (strlen(path) == 0)
         {
             return 0;
