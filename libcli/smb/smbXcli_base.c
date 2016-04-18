ad94c11330f361115e478f73cb28132ff737aaa0
Stefan Metzmacher <metze@samba.org>
2016-04-18 17:38:46 +0200

    libcli/smb: add smbXcli_session_is_guest() helper function
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=11841
    
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Günther Deschner <gd@samba.org>
    (cherry picked from commit 8f4a4bec089b46bbeb0e0f37bb682acb88702bf2)

--- libcli/smb/smbXcli_base.c
+++ libcli/smb/smbXcli_base.c
@@ -5302,6 +5302,30 @@ struct smbXcli_session *smbXcli_session_copy(TALLOC_CTX *mem_ctx,
 	return session;
 }
 
+bool smbXcli_session_is_guest(struct smbXcli_session *session)
+{
+	if (session == NULL) {
+		return false;
+	}
+
+	if (session->conn == NULL) {
+		return false;
+	}
+
+	if (session->conn->protocol >= PROTOCOL_SMB2_02) {
+		if (session->smb2->session_flags & SMB2_SESSION_FLAG_IS_GUEST) {
+			return true;
+		}
+		return false;
+	}
+
+	if (session->smb1.action & SMB_SETUP_GUEST) {
+		return true;
+	}
+
+	return false;
+}
+
 bool smbXcli_session_is_authenticated(struct smbXcli_session *session)
 {
 	const DATA_BLOB *application_key;
