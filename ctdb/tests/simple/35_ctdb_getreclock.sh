a3c2a39ea2fb6aed3791e36efe0c0998d7ea83af
Martin Schwenke <martin@meltin.net>
2016-05-10 19:54:09 +1000

    ctdb-tests: Replace "ctdb setrelock" test with "ctdb getreclock" test
    Signed-off-by: Martin Schwenke <martin@meltin.net>
    Reviewed-by: Amitay Isaacs <amitay@gmail.com>

---
+++ ctdb/tests/simple/35_ctdb_getreclock.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+test_info()
+{
+    cat <<EOF
+Verify that "ctdb getreclock" gets the recovery lock correctly.
+
+Make sure the recovery lock is consistent across all nodes.
+EOF
+}
+
+. "${TEST_SCRIPTS_DIR}/integration.bash"
+
+ctdb_test_init "$@"
+
+set -e
+
+cluster_is_healthy
+
+echo "Check that recovery lock is set the same on all nodes..."
+try_command_on_node -v -q all $CTDB getreclock
+
+if [ -z "$out" ] ; then
+    echo "GOOD: Recovery lock is unset on all nodes"
+    exit 0
+fi
+
+n=$(echo "$out" | sort -u | wc -l)
+if [ "$n" = 1 ] ; then
+	echo "GOOD: All nodes have the same recovery lock setting"
+else
+	echo "BAD: Recovery lock setting differs across nodes"
+	exit 1
+fi
