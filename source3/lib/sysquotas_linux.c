9fa8b5cf84e3d13250bf24728ee7e4db5f551b79
Uri Simchoni <uri@samba.org>
2016-05-23 22:07:39 +0300

    s3-sysquotas-linux: remove support for old interfaces
    Remove support for Linux quota interfaces that predate the
    2.6.x kernel series. Glibc has been supporting the "current"
    quota interface for over 10 years now...
    
    Signed-off-by: Uri Simchoni <uri@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/lib/sysquotas_linux.c
+++ source3/lib/sysquotas_linux.c
@@ -31,267 +31,7 @@
 
 #ifdef HAVE_QUOTACTL_LINUX 
 
-#include "samba_linux_quota.h"
-
-/****************************************************************************
- Abstract out the v1 Linux quota get calls.
-****************************************************************************/
-static int sys_get_linux_v1_quota(const char *path, const char *bdev, enum SMB_QUOTA_TYPE qtype, unid_t id, SMB_DISK_QUOTA *dp)
-{
-	int ret = -1;
-	uint32_t qflags = 0;
-	struct v1_kern_dqblk D;
-	uint64_t bsize = (uint64_t)QUOTABLOCK_SIZE;
-
-	ZERO_STRUCT(D);
-
-	switch (qtype) {
-		case SMB_USER_QUOTA_TYPE:
-			DEBUG(10,("sys_get_linux_v1_quota: path[%s] bdev[%s] SMB_USER_QUOTA_TYPE uid[%u]\n",
-				path, bdev, (unsigned)id.uid));
-
-			if ((ret = quotactl(QCMD(Q_V1_GETQUOTA,USRQUOTA), bdev, id.uid, (caddr_t)&D))&&errno != EDQUOT) {
-				return ret;
-			}
-
-			break;
-		case SMB_GROUP_QUOTA_TYPE:
-			DEBUG(10,("sys_get_linux_v1_quota: path[%s] bdev[%s] SMB_GROUP_QUOTA_TYPE gid[%u]\n",
-				path, bdev, (unsigned)id.gid));
-
-			if ((ret = quotactl(QCMD(Q_V1_GETQUOTA,GRPQUOTA), bdev, id.gid, (caddr_t)&D))&&errno != EDQUOT) {
-				return ret;
-			}
-
-			break;
-		case SMB_USER_FS_QUOTA_TYPE:
-			DEBUG(10,("sys_get_linux_v1_quota: path[%s] bdev[%s] SMB_USER_FS_QUOTA_TYPE (uid[%u])\n",
-				path, bdev, (unsigned)id.uid));
-
-			if ((ret = quotactl(QCMD(Q_V1_GETQUOTA,USRQUOTA), bdev, id.uid, (caddr_t)&D))==0) {
-				qflags |= QUOTAS_DENY_DISK;
-			}
-
-			break;
-		case SMB_GROUP_FS_QUOTA_TYPE:
-			DEBUG(10,("sys_get_linux_v1_quota: path[%s] bdev[%s] SMB_GROUP_FS_QUOTA_TYPE (gid[%u])\n",
-				path, bdev, (unsigned)id.gid));
-
-			if ((ret = quotactl(QCMD(Q_V1_GETQUOTA,GRPQUOTA), bdev, id.gid, (caddr_t)&D))==0) {
-				qflags |= QUOTAS_DENY_DISK;
-			}
-
-			break;
-		default:
-			errno = ENOSYS;
-			return -1;
-	}
-
-	dp->bsize = bsize;
-	dp->softlimit = (uint64_t)D.dqb_bsoftlimit;
-	dp->hardlimit = (uint64_t)D.dqb_bhardlimit;
-	dp->ihardlimit = (uint64_t)D.dqb_ihardlimit;
-	dp->isoftlimit = (uint64_t)D.dqb_isoftlimit;
-	dp->curinodes = (uint64_t)D.dqb_curinodes;
-	dp->curblocks = (uint64_t)D.dqb_curblocks;
-
-
-	dp->qflags = qflags;
-
-	return ret;
-}
-
-/****************************************************************************
- Abstract out the v1 Linux quota set calls.
-****************************************************************************/
-static int sys_set_linux_v1_quota(const char *path, const char *bdev, enum SMB_QUOTA_TYPE qtype, unid_t id, SMB_DISK_QUOTA *dp)
-{
-	int ret = -1;
-	struct v1_kern_dqblk D;
-	uint64_t bsize = (uint64_t)QUOTABLOCK_SIZE;
-
-	ZERO_STRUCT(D);
-
-	if (bsize == dp->bsize) {
-		D.dqb_bsoftlimit = dp->softlimit;
-		D.dqb_bhardlimit = dp->hardlimit;
-		D.dqb_ihardlimit = dp->ihardlimit;
-		D.dqb_isoftlimit = dp->isoftlimit;
-	} else {
-		D.dqb_bsoftlimit = (dp->softlimit*dp->bsize)/bsize;
-		D.dqb_bhardlimit = (dp->hardlimit*dp->bsize)/bsize;
-		D.dqb_ihardlimit = (dp->ihardlimit*dp->bsize)/bsize;
-		D.dqb_isoftlimit = (dp->isoftlimit*dp->bsize)/bsize;
-	}
-
-	switch (qtype) {
-		case SMB_USER_QUOTA_TYPE:
-			DEBUG(10,("sys_set_linux_v1_quota: path[%s] bdev[%s] SMB_USER_QUOTA_TYPE uid[%u]\n",
-				path, bdev, (unsigned)id.uid));
-
-			ret = quotactl(QCMD(Q_V1_SETQUOTA,USRQUOTA), bdev, id.uid, (caddr_t)&D);
-			break;
-		case SMB_GROUP_QUOTA_TYPE:
-			DEBUG(10,("sys_set_linux_v1_quota: path[%s] bdev[%s] SMB_GROUP_QUOTA_TYPE gid[%u]\n",
-				path, bdev, (unsigned)id.gid));
-
-			ret = quotactl(QCMD(Q_V1_SETQUOTA,GRPQUOTA), bdev, id.gid, (caddr_t)&D);
-			break;
-		case SMB_USER_FS_QUOTA_TYPE:
-			DEBUG(10,("sys_set_linux_v1_quota: path[%s] bdev[%s] SMB_USER_FS_QUOTA_TYPE (uid[%u])\n",
-				path, bdev, (unsigned)id.uid));
-
-			if ((ret = quotactl(QCMD(Q_V1_GETQUOTA,USRQUOTA), bdev, id.uid, (caddr_t)&D))==0) {
-				dp->qflags |= QUOTAS_DENY_DISK;
-			}
-
-			break;
-		case SMB_GROUP_FS_QUOTA_TYPE:
-			DEBUG(10,("sys_set_linux_v1_quota: path[%s] bdev[%s] SMB_GROUP_FS_QUOTA_TYPE (gid[%u])\n",
-				path, bdev, (unsigned)id.gid));
-
-			if ((ret = quotactl(QCMD(Q_V1_GETQUOTA,GRPQUOTA), bdev, id.gid, (caddr_t)&D))==0) {
-				dp->qflags |= QUOTAS_DENY_DISK;
-			}
-
-			break;
-		default:
-			errno = ENOSYS;
-			return -1;
-	}
-
-	return ret;
-}
-
-/****************************************************************************
- Abstract out the v2 Linux quota get calls.
-****************************************************************************/
-static int sys_get_linux_v2_quota(const char *path, const char *bdev, enum SMB_QUOTA_TYPE qtype, unid_t id, SMB_DISK_QUOTA *dp)
-{
-	int ret = -1;
-	uint32_t qflags = 0;
-	struct v2_kern_dqblk D;
-	uint64_t bsize = (uint64_t)QUOTABLOCK_SIZE;
-
-	ZERO_STRUCT(D);
-
-	switch (qtype) {
-		case SMB_USER_QUOTA_TYPE:
-			DEBUG(10,("sys_get_linux_v2_quota: path[%s] bdev[%s] SMB_USER_QUOTA_TYPE uid[%u]\n",
-				path, bdev, (unsigned)id.uid));
-
-			if ((ret = quotactl(QCMD(Q_V2_GETQUOTA,USRQUOTA), bdev, id.uid, (caddr_t)&D))&&errno != EDQUOT) {
-				return ret;
-			}
-
-			break;
-		case SMB_GROUP_QUOTA_TYPE:
-			DEBUG(10,("sys_get_linux_v2_quota: path[%s] bdev[%s] SMB_GROUP_QUOTA_TYPE gid[%u]\n",
-				path, bdev, (unsigned)id.gid));
-
-			if ((ret = quotactl(QCMD(Q_V2_GETQUOTA,GRPQUOTA), bdev, id.gid, (caddr_t)&D))&&errno != EDQUOT) {
-				return ret;
-			}
-
-			break;
-		case SMB_USER_FS_QUOTA_TYPE:
-			DEBUG(10,("sys_get_linux_v2_quota: path[%s] bdev[%s] SMB_USER_FS_QUOTA_TYPE (uid[%u])\n",
-				path, bdev, (unsigned)id.uid));
-
-			if ((ret = quotactl(QCMD(Q_V2_GETQUOTA,USRQUOTA), bdev, id.uid, (caddr_t)&D))==0) {
-				qflags |= QUOTAS_DENY_DISK;
-			}
-
-			break;
-		case SMB_GROUP_FS_QUOTA_TYPE:
-			DEBUG(10,("sys_get_linux_v2_quota: path[%s] bdev[%s] SMB_GROUP_FS_QUOTA_TYPE (gid[%u])\n",
-				path, bdev, (unsigned)id.gid));
-
-			if ((ret = quotactl(QCMD(Q_V2_GETQUOTA,GRPQUOTA), bdev, id.gid, (caddr_t)&D))==0) {
-				qflags |= QUOTAS_DENY_DISK;
-			}
-
-			break;
-		default:
-			errno = ENOSYS;
-			return -1;
-	}
-
-	dp->bsize = bsize;
-	dp->softlimit = (uint64_t)D.dqb_bsoftlimit;
-	dp->hardlimit = (uint64_t)D.dqb_bhardlimit;
-	dp->ihardlimit = (uint64_t)D.dqb_ihardlimit;
-	dp->isoftlimit = (uint64_t)D.dqb_isoftlimit;
-	dp->curinodes = (uint64_t)D.dqb_curinodes;
-	dp->curblocks = (uint64_t)D.dqb_curspace/bsize;
-
-
-	dp->qflags = qflags;
-
-	return ret;
-}
-
-/****************************************************************************
- Abstract out the v2 Linux quota set calls.
-****************************************************************************/
-static int sys_set_linux_v2_quota(const char *path, const char *bdev, enum SMB_QUOTA_TYPE qtype, unid_t id, SMB_DISK_QUOTA *dp)
-{
-	int ret = -1;
-	struct v2_kern_dqblk D;
-	uint64_t bsize = (uint64_t)QUOTABLOCK_SIZE;
-
-	ZERO_STRUCT(D);
-
-	if (bsize == dp->bsize) {
-		D.dqb_bsoftlimit = dp->softlimit;
-		D.dqb_bhardlimit = dp->hardlimit;
-		D.dqb_ihardlimit = dp->ihardlimit;
-		D.dqb_isoftlimit = dp->isoftlimit;
-	} else {
-		D.dqb_bsoftlimit = (dp->softlimit*dp->bsize)/bsize;
-		D.dqb_bhardlimit = (dp->hardlimit*dp->bsize)/bsize;
-		D.dqb_ihardlimit = (dp->ihardlimit*dp->bsize)/bsize;
-		D.dqb_isoftlimit = (dp->isoftlimit*dp->bsize)/bsize;
-	}
-
-	switch (qtype) {
-		case SMB_USER_QUOTA_TYPE:
-			DEBUG(10,("sys_set_linux_v2_quota: path[%s] bdev[%s] SMB_USER_QUOTA_TYPE uid[%u]\n",
-				path, bdev, (unsigned)id.uid));
-
-			ret = quotactl(QCMD(Q_V2_SETQUOTA,USRQUOTA), bdev, id.uid, (caddr_t)&D);
-			break;
-		case SMB_GROUP_QUOTA_TYPE:
-			DEBUG(10,("sys_set_linux_v2_quota: path[%s] bdev[%s] SMB_GROUP_QUOTA_TYPE gid[%u]\n",
-				path, bdev, (unsigned)id.gid));
-
-			ret = quotactl(QCMD(Q_V2_SETQUOTA,GRPQUOTA), bdev, id.gid, (caddr_t)&D);
-			break;
-		case SMB_USER_FS_QUOTA_TYPE:
-			DEBUG(10,("sys_set_linux_v2_quota: path[%s] bdev[%s] SMB_USER_FS_QUOTA_TYPE (uid[%u])\n",
-				path, bdev, (unsigned)id.uid));
-
-			if ((ret = quotactl(QCMD(Q_V2_GETQUOTA,USRQUOTA), bdev, id.uid, (caddr_t)&D))==0) {
-				dp->qflags |= QUOTAS_DENY_DISK;
-			}
-
-			break;
-		case SMB_GROUP_FS_QUOTA_TYPE:
-			DEBUG(10,("sys_set_linux_v2_quota: path[%s] bdev[%s] SMB_GROUP_FS_QUOTA_TYPE (gid[%u])\n",
-				path, bdev, (unsigned)id.gid));
-
-			if ((ret = quotactl(QCMD(Q_V2_GETQUOTA,GRPQUOTA), bdev, id.gid, (caddr_t)&D))==0) {
-				dp->qflags |= QUOTAS_DENY_DISK;
-			}
-
-			break;
-		default:
-			errno = ENOSYS;
-			return -1;
-	}
-
-	return ret;
-}
+#include <sys/quota.h>
 
 /****************************************************************************
  Abstract out the generic Linux quota get calls.
@@ -300,7 +40,7 @@ static int sys_get_linux_gen_quota(const char *path, const char *bdev, enum SMB_
 {
 	int ret = -1;
 	uint32_t qflags = 0;
-	struct if_dqblk D;
+	struct dqblk D;
 	uint64_t bsize = (uint64_t)QUOTABLOCK_SIZE;
 
 	ZERO_STRUCT(D);
@@ -367,7 +107,7 @@ static int sys_get_linux_gen_quota(const char *path, const char *bdev, enum SMB_
 static int sys_set_linux_gen_quota(const char *path, const char *bdev, enum SMB_QUOTA_TYPE qtype, unid_t id, SMB_DISK_QUOTA *dp)
 {
 	int ret = -1;
-	struct if_dqblk D;
+	struct dqblk D;
 	uint64_t bsize = (uint64_t)QUOTABLOCK_SIZE;
 
 	ZERO_STRUCT(D);
@@ -441,33 +181,21 @@ int sys_get_vfs_quota(const char *path, const char *bdev, enum SMB_QUOTA_TYPE qt
 		case SMB_USER_QUOTA_TYPE:
 		case SMB_GROUP_QUOTA_TYPE:
 			if ((ret=sys_get_linux_gen_quota(path, bdev, qtype, id, dp))&&errno != EDQUOT) {
-				if ((ret=sys_get_linux_v2_quota(path, bdev, qtype, id, dp))&&errno != EDQUOT) {
-					if ((ret=sys_get_linux_v1_quota(path, bdev, qtype, id, dp))&&errno != EDQUOT) {
-						return ret;
-					}
-				}
+				return ret;
 			}
 			ret = 0;
 			break;
 		case SMB_USER_FS_QUOTA_TYPE:
 			id.uid = getuid();
 
-			if ((ret=sys_get_linux_gen_quota(path, bdev, qtype, id, dp))&&errno != EDQUOT) {
-				if ((ret=sys_get_linux_v2_quota(path, bdev, qtype, id, dp))&&errno != EDQUOT) {
-					ret=sys_get_linux_v1_quota(path, bdev, qtype, id, dp);
-				}
-			}
+			sys_get_linux_gen_quota(path, bdev, qtype, id, dp);
 
 			ret = 0;
 			break;
 		case SMB_GROUP_FS_QUOTA_TYPE:
 			id.gid = getgid();
 
-			if ((ret=sys_get_linux_gen_quota(path, bdev, qtype, id, dp))&&errno != EDQUOT) {
-				if ((ret=sys_get_linux_v2_quota(path, bdev, qtype, id, dp))&&errno != EDQUOT) {
-					ret=sys_get_linux_v1_quota(path, bdev, qtype, id, dp);
-				}
-			}
+			sys_get_linux_gen_quota(path, bdev, qtype, id, dp);
 
 			ret = 0;
 			break;
@@ -496,21 +224,13 @@ int sys_set_vfs_quota(const char *path, const char *bdev, enum SMB_QUOTA_TYPE qt
 		case SMB_USER_QUOTA_TYPE:
 		case SMB_GROUP_QUOTA_TYPE:
 			if ((ret=sys_set_linux_gen_quota(path, bdev, qtype, id, dp))) {
-				if ((ret=sys_set_linux_v2_quota(path, bdev, qtype, id, dp))) {
-					if ((ret=sys_set_linux_v1_quota(path, bdev, qtype, id, dp))) {
-						return ret;
-					}
-				}
+				return ret;
 			}
 			break;
 		case SMB_USER_FS_QUOTA_TYPE:
 			id.uid = getuid();
 
-			if ((ret=sys_set_linux_gen_quota(path, bdev, qtype, id, dp))) {
-				if ((ret=sys_set_linux_v2_quota(path, bdev, qtype, id, dp))) {
-					ret=sys_set_linux_v1_quota(path, bdev, qtype, id, dp);
-				}
-			}
+			sys_set_linux_gen_quota(path, bdev, qtype, id, dp);
 
 			if (oldqflags == dp->qflags) {
 				ret = 0;
@@ -521,11 +241,7 @@ int sys_set_vfs_quota(const char *path, const char *bdev, enum SMB_QUOTA_TYPE qt
 		case SMB_GROUP_FS_QUOTA_TYPE:
 			id.gid = getgid();
 
-			if ((ret=sys_set_linux_gen_quota(path, bdev, qtype, id, dp))) {
-				if ((ret=sys_set_linux_v2_quota(path, bdev, qtype, id, dp))) {
-					ret=sys_set_linux_v1_quota(path, bdev, qtype, id, dp);
-				}
-			}
+			sys_set_linux_gen_quota(path, bdev, qtype, id, dp);
 
 			if (oldqflags == dp->qflags) {
 				ret = 0;
