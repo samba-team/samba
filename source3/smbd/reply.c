f2dfba87f17c185a0e27286c480a4a5ff847a5b6
Jeremy Allison <jra@samba.org>
2016-06-13 09:22:56 -0700

    s3: smbd: Make setup_readX_header() externally accessible
    https://bugzilla.samba.org/show_bug.cgi?id=11845
    
    Signed-off-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: Alexander Bokovoy <ab@samba.org>
    (cherry picked from commit 2ff3171fba931e621581336c975bae146a2ea3a9)

--- source3/smbd/reply.c
+++ source3/smbd/reply.c
@@ -3757,7 +3757,7 @@ strict_unlock:
  Setup readX header.
 ****************************************************************************/
 
-static int setup_readX_header(char *outbuf, size_t smb_maxcnt)
+int setup_readX_header(char *outbuf, size_t smb_maxcnt)
 {
 	int outsize;
 
