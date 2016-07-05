3175d5f4bfb399049ad6b8da2a1d578620b25802
Andrew Bartlett <abartlet@samba.org>
2016-07-05 16:01:38 +1200

    ldb: Avoid use-after-free when one error message is printed into another
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Garming Sam <garming@catalyst.net.nz>
    Reviewed-by: Andreas Schneider <asn@samba.org>

--- lib/ldb/common/ldb.c
+++ lib/ldb/common/ldb.c
@@ -284,15 +284,17 @@ void ldb_set_errstring(struct ldb_context *ldb, const char *err_string)
 void ldb_asprintf_errstring(struct ldb_context *ldb, const char *format, ...)
 {
 	va_list ap;
-
+	char *old_err_string = NULL;
 	if (ldb->err_string) {
-		talloc_free(ldb->err_string);
+		old_err_string = ldb->err_string;
 	}
 
 	va_start(ap, format);
 	ldb->err_string = talloc_vasprintf(ldb, format, ap);
 	va_end(ap);
 
+	TALLOC_FREE(old_err_string);
+	
 	if (ldb->flags & LDB_FLG_ENABLE_TRACING) {
 		ldb_debug(ldb, LDB_DEBUG_TRACE, "ldb_asprintf/set_errstring: %s",
 			  ldb->err_string);
