93be59ee0ab5cddaad7b5d1e6d6d593acc47597c
Garming Sam <garming@catalyst.net.nz>
2016-07-13 13:30:35 +1200

    dbcheck.sh: Fix the arguments supplied as $@
    Signed-off-by: Garming Sam <garming@samba.org>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- testprogs/blackbox/dbcheck.sh
+++ testprogs/blackbox/dbcheck.sh
@@ -9,34 +9,35 @@ fi
 
 PREFIX="$1"
 shift 1
+ARGS=$@
 
 . `dirname $0`/subunit.sh
 
 dbcheck() {
-	$BINDIR/samba-tool dbcheck --cross-ncs $@
+	$BINDIR/samba-tool dbcheck --cross-ncs $ARGS
 }
 
 # This list of attributes can be freely extended
 dbcheck_fix_one_way_links() {
-	$BINDIR/samba-tool dbcheck --quiet --fix --yes fix_all_string_dn_component_mismatch --attrs="lastKnownParent defaultObjectCategory" --cross-ncs $@
+	$BINDIR/samba-tool dbcheck --quiet --fix --yes fix_all_string_dn_component_mismatch --attrs="lastKnownParent defaultObjectCategory fromServer rIDSetReferences" --cross-ncs $ARGS
 }
 
 # This test shows that this does not do anything to a current
 # provision (that would be a bug)
 dbcheck_reset_well_known_acls() {
-	$BINDIR/samba-tool dbcheck --cross-ncs --reset-well-known-acls $@
+	$BINDIR/samba-tool dbcheck --cross-ncs --reset-well-known-acls $ARGS
 }
 
 reindex() {
-	$BINDIR/samba-tool dbcheck --reindex
+	$BINDIR/samba-tool dbcheck --reindex $ARGS
 }
 
 fixed_attrs() {
-	$BINDIR/samba-tool dbcheck --attrs=cn
+	$BINDIR/samba-tool dbcheck --attrs=cn $ARGS
 }
 
 force_modules() {
-	$BINDIR/samba-tool dbcheck --force-modules
+	$BINDIR/samba-tool dbcheck --force-modules $ARGS
 }
 
 dbcheck_fix_one_way_links
