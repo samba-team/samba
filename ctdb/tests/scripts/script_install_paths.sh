b51f604f753abc094bf0151165173c5a27b5711f
Martin Schwenke <martin@meltin.net>
2016-07-14 11:58:51 +1000

    ctdb-tests: Add new test support script for script install paths
    This helps unit tests locate CTDB's scripts.  It is being created to
    support some new shellcheck unit tests.
    
    Hopefully, it can also be used to simplify some of the symlink
    complexity in some other unit tests suites, such as eventscripts.
    
    Signed-off-by: Martin Schwenke <martin@meltin.net>
    Reviewed-by: Amitay Isaacs <amitay@gmail.com>

---
+++ ctdb/tests/scripts/script_install_paths.sh
@@ -0,0 +1,21 @@
+# Hey Emacs, this is a -*- shell-script -*- !!!  :-)
+
+# $ctdb_dir is set in common.sh
+if [  -n "$ctdb_dir" ] ; then
+	# Running in-tree
+	CTDB_SCRIPTS_BASE="${ctdb_dir}/config"
+	CTDB_SCRIPTS_INIT_SCRIPT="${ctdb_dir}/config/ctdb.init"
+	CTDB_SCRIPTS_SBIN_DIR="${ctdb_dir}/config"
+	CTDB_SCRIPTS_TOOLS_BIN_DIR="${ctdb_dir}/tools"
+	CTDB_SCRIPTS_TOOLS_HELPER_DIR="${ctdb_dir}/tools"
+else
+	# Installed
+	CTDB_SCRIPTS_BASE="/usr/local/etc/ctdb"
+	CTDB_SCRIPTS_INIT_SCRIPT="" # No ideas here... this is a packaging choice
+	CTDB_SCRIPTS_SBIN_DIR="/usr/local/sbin"
+	CTDB_SCRIPTS_TOOLS_BIN_DIR="/usr/local/bin"
+	CTDB_SCRIPTS_TOOLS_HELPER_DIR="/usr/local/libexec/ctdb"
+fi
+
+export CTDB_SCRIPTS_BASE CTDB_SCRIPTS_BIN_DIR CTDB_SCRIPTS_INIT_DIR \
+	CTDB_SCRIPTS_SBIN_DIR CTDB_SCRIPTS_TOOLS_HELPER_DIR
