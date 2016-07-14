ff5521838a474d6ff9489bb92438797db65671a6
Michael Adam <obnox@samba.org>
2016-07-14 09:58:15 +0200

    Revert "tevent: avoid -Wtautological-compare errors with gcc6"
    This reverts commit 2991f7709973fdcc2c0b83bbe15dda3f1ceae9b3.
    
    Breaks compile for older (<= 4.4) gccs.
    Needs to be done differently.
    
    Signed-off-by: Michael Adam <obnox@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- lib/tevent/tevent_util.h
+++ lib/tevent/tevent_util.h
@@ -79,9 +79,6 @@ do { \
 */
 #define DLIST_REMOVE(list, p) \
 do { \
-	_Pragma ("GCC diagnostic push") \
-	_Pragma ("GCC diagnostic ignored \"-Wpragmas\"") \
-	_Pragma ("GCC diagnostic ignored \"-Wtautological-compare\"") \
 	if ((p) == (list)) { \
 		if ((p)->next) (p)->next->prev = (p)->prev; \
 		(list) = (p)->next; \
@@ -93,7 +90,6 @@ do { \
 		if ((p)->next) (p)->next->prev = (p)->prev; \
 	} \
 	if ((p) != (list)) (p)->next = (p)->prev = NULL;	\
-	_Pragma ("GCC diagnostic pop") \
 } while (0)
 
 /*
@@ -124,11 +120,7 @@ do { \
 		(p)->next = (el)->next;		\
 		(el)->next = (p);		\
 		if ((p)->next) (p)->next->prev = (p);	\
-		_Pragma ("GCC diagnostic push") \
-		_Pragma ("GCC diagnostic ignored \"-Wpragmas\"") \
-		_Pragma ("GCC diagnostic ignored \"-Wtautological-compare\"") \
 		if ((list)->prev == (el)) (list)->prev = (p); \
-		_Pragma ("GCC diagnostic pop") \
 	}\
 } while (0)
 
