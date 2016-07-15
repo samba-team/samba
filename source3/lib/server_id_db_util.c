fddee66e20cddc81b5078f2ea9163383ab6c10c6
Volker Lendecke <vl@samba.org>
2016-07-15 10:55:54 +0200

    lib: Fix a signed/unsigned mixup
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>
    
    Autobuild-User(master): Jeremy Allison <jra@samba.org>
    Autobuild-Date(master): Sat Jul 16 00:17:03 CEST 2016 on sn-devel-144

--- source3/lib/server_id_db_util.c
+++ source3/lib/server_id_db_util.c
@@ -72,7 +72,7 @@ static int server_id_db_check_exclusive(
 	unsigned num_servers, struct server_id *servers)
 {
 	struct server_id me = server_id_db_pid(db);
-	int i;
+	unsigned i;
 
 	for (i=0; i<num_servers; i++) {
 		int ret;
