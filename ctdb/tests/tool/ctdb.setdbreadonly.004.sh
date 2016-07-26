5d34755be384b4a5b6f627cb1b35539e6e8da31d
Amitay Isaacs <amitay@gmail.com>
2016-07-26 14:18:35 +1000

    ctdb-tests: Add more tests for ctdb setdbsticky and setdbreadonly
    Signed-off-by: Amitay Isaacs <amitay@gmail.com>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Martin Schwenke <martin@meltin.net>

---
+++ ctdb/tests/tool/ctdb.setdbreadonly.004.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+. "${TEST_SCRIPTS_DIR}/unit.sh"
+
+define_test "set volatile sticky to sticky and read-only by name"
+
+setup_ctdbd <<EOF
+NODEMAP
+0       192.168.20.41   0x0     CURRENT RECMASTER
+1       192.168.20.42   0x0
+2       192.168.20.43   0x0
+
+DBMAP
+0x7a19d84d locking.tdb STICKY
+0x4e66c2b2 brlock.tdb
+0x4d2a432b g_lock.tdb
+0x7132c184 secrets.tdb PERSISTENT
+0x6cf2837d registry.tdb PERSISTENT 42
+EOF
+
+ok_null
+simple_test locking.tdb
+
+ok <<EOF
+Number of databases:5
+dbid:0x7a19d84d name:locking.tdb path:/var/run/ctdb/DB_DIR/locking.tdb.0 STICKY READONLY
+dbid:0x4e66c2b2 name:brlock.tdb path:/var/run/ctdb/DB_DIR/brlock.tdb.0
+dbid:0x4d2a432b name:g_lock.tdb path:/var/run/ctdb/DB_DIR/g_lock.tdb.0
+dbid:0x7132c184 name:secrets.tdb path:/var/lib/ctdb/persistent/secrets.tdb.0 PERSISTENT
+dbid:0x6cf2837d name:registry.tdb path:/var/lib/ctdb/persistent/registry.tdb.0 PERSISTENT
+EOF
+
+simple_test_other getdbmap
