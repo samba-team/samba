a772738363bdf59a9b25ae961a8219d31025da67
Martin Schwenke <martin@meltin.net>
2016-08-01 14:07:04 +1000

    ctdb-tests: Stop cross-talk between reclock tests
    On a busy system the backgrounded counter reset can survive into the
    next test and interfere with its result.
    
    To avoid this, wait until all forks of 01.reclock exit before
    continuing on to the next test.
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=12109
    
    Signed-off-by: Martin Schwenke <martin@meltin.net>
    Reviewed-by: Volker Lendecke <vl@samba.org>
    
    Autobuild-User(master): Volker Lendecke <vl@samba.org>
    Autobuild-Date(master): Mon Aug  1 17:53:27 CEST 2016 on sn-devel-144
    
    (cherry picked from commit 19ed8165bbe374e1ea277fd4dd5a65ee932b4f05)

--- ctdb/tests/eventscripts/scripts/local.sh
+++ ctdb/tests/eventscripts/scripts/local.sh
@@ -1132,10 +1132,21 @@ program $_rpc_service${_ver:+ version }${_ver} is not available"
 
 # Recovery lock fakery
 
+cleanup_reclock ()
+{
+	_pattern="${script_dir}/${script}"
+	while pgrep -f "$_pattern" >/dev/null ; do
+		echo "Waiting for backgrounded ${script} to exit..."
+		(FAKE_SLEEP_REALLY=yes sleep 1)
+	done
+}
+
 setup_reclock ()
 {
 	CTDB_RECOVERY_LOCK=$(mktemp --tmpdir="$EVENTSCRIPTS_TESTS_VAR_DIR")
 	export CTDB_RECOVERY_LOCK
+
+	test_cleanup cleanup_reclock
 }
 
 ######################################################################
