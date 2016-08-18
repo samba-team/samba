4276ffbab4efdece293b8ce78fe333316045a68a
Jeremy Allison <jra@samba.org>
2016-08-18 14:02:24 -0700

    s3: libsmb: Plumb in additional_flags2 = FLAGS2_REPARSE_PATH to cli_getatr_send().
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=12165
    
    Signed-off-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: Uri Simchoni <uri@samba.org>
    (cherry picked from commit 461000c8812a99a81a9f3fc87f93c1b7c67f42bd)

--- source3/libsmb/clifile.c
+++ source3/libsmb/clifile.c
@@ -3743,6 +3743,7 @@ struct tevent_req *cli_getatr_send(TALLOC_CTX *mem_ctx,
 	struct tevent_req *req = NULL, *subreq = NULL;
 	struct cli_getatr_state *state = NULL;
 	uint8_t additional_flags = 0;
+	uint16_t additional_flags2 = 0;
 	uint8_t *bytes = NULL;
 
 	req = tevent_req_create(mem_ctx, &state, struct cli_getatr_state);
@@ -3764,8 +3765,13 @@ struct tevent_req *cli_getatr_send(TALLOC_CTX *mem_ctx,
 		return tevent_req_post(req, ev);
 	}
 
-	subreq = cli_smb_send(state, ev, cli, SMBgetatr, additional_flags, 0,
-			      0, NULL, talloc_get_size(bytes), bytes);
+	if (clistr_is_previous_version_path(fname)) {
+		additional_flags2 = FLAGS2_REPARSE_PATH;
+	}
+
+	subreq = cli_smb_send(state, ev, cli, SMBgetatr, additional_flags,
+			additional_flags2,
+			0, NULL, talloc_get_size(bytes), bytes);
 	if (tevent_req_nomem(subreq, req)) {
 		return tevent_req_post(req, ev);
 	}
