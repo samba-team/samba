599d3f7f8358f6107e1d13ab0a92c3143f32435e
Andrew Bartlett <abartlet@samba.org>
2016-08-31 11:39:24 +1200

    selftest: Correct name of samba4.blackbox.dbcheck.release-4-5-0-pre1
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Garming Sam <garming@catalyst.net.nz>

--- selftest/tests.py
+++ selftest/tests.py
@@ -91,6 +91,11 @@ plantestsuite(
     ["PYTHON=%s" % python, os.path.join(bbdir, "dbcheck-oldrelease.sh"),
      '$PREFIX_ABS/provision', 'release-4-1-6-partial-object', configuration])
 plantestsuite(
+    "samba4.blackbox.dbcheck.release-4-5-0-pre1", "none",
+    ["PYTHON=%s" % python,
+     os.path.join(bbdir, "dbcheck-oldrelease.sh"),
+     '$PREFIX_ABS/provision', 'release-4-5-0-pre1', configuration])
+plantestsuite(
     "samba4.blackbox.upgradeprovision.alpha13", "none",
     ["PYTHON=%s" % python,
      os.path.join(bbdir, "upgradeprovision-oldrelease.sh"),
@@ -100,11 +105,6 @@ plantestsuite(
     ["PYTHON=%s" % python,
      os.path.join(bbdir, "upgradeprovision-oldrelease.sh"),
      '$PREFIX_ABS/provision', 'release-4-0-0', configuration])
-plantestsuite(
-    "samba4.blackbox.upgradeprovision.release-4-5-0-pre1", "none",
-    ["PYTHON=%s" % python,
-     os.path.join(bbdir, "dbcheck-oldrelease.sh"),
-     '$PREFIX_ABS/provision', 'release-4-5-0-pre1', configuration])
 planpythontestsuite("none", "samba.tests.upgradeprovision")
 planpythontestsuite("none", "samba.tests.xattr")
 planpythontestsuite("none", "samba.tests.ntacls")
