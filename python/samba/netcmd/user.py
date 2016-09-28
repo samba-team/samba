22da0887b26f090dd39e864c139908ea33738a7b
Rowland Penny <rpenny@samba.org>
2016-09-28 15:39:52 +0100

    bug 12292: stop user.py throwing errors if user is unknown
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=12292
    
    Signed-off-by: Rowland Penny <rpenny@samba.org>
    Reviewed-by: Alexander Bokovoy <ab@samba.org>

--- python/samba/netcmd/user.py
+++ python/samba/netcmd/user.py
@@ -406,10 +406,23 @@ Example2 shows how to delete a user in the domain against the local server.   su
         lp = sambaopts.get_loadparm()
         creds = credopts.get_credentials(lp, fallback_machine=True)
 
+        samdb = SamDB(url=H, session_info=system_session(),
+                      credentials=creds, lp=lp)
+
+        filter = ("(&(sAMAccountName=%s)(sAMAccountType=805306368))" %
+                   username)
+
         try:
-            samdb = SamDB(url=H, session_info=system_session(),
-                          credentials=creds, lp=lp)
-            samdb.deleteuser(username)
+            res = samdb.search(base=samdb.domain_dn(),
+                               scope=ldb.SCOPE_SUBTREE,
+                               expression=filter,
+                               attrs=["dn"])
+            user_dn = res[0].dn
+        except IndexError:
+            raise CommandError('Unable to find user "%s"' % (username))
+
+        try:
+            samdb.delete(user_dn)
         except Exception, e:
             raise CommandError('Failed to remove user "%s"' % username, e)
         self.outf.write("Deleted user %s\n" % username)
