48e65a15ac306051c0f37520a86d38f254b04321
Günther Deschner <gd@samba.org>
2016-10-31 18:07:50 +0100

    s3-spoolss: add winreg_add_core_driver_internal()
    Guenther
    
    Signed-off-by: Guenther Deschner <gd@samba.org>
    Reviewed-by: Andreas Schneider <asn@samba.org>

--- source3/rpc_server/spoolss/srv_spoolss_util.c
+++ source3/rpc_server/spoolss/srv_spoolss_util.c
@@ -548,6 +548,35 @@ WERROR winreg_get_core_driver_internal(TALLOC_CTX *mem_ctx,
 	return result;
 }
 
+WERROR winreg_add_core_driver_internal(TALLOC_CTX *mem_ctx,
+				       const struct auth_session_info *session_info,
+				       struct messaging_context *msg_ctx,
+				       const char *architecture,
+				       const struct spoolss_CorePrinterDriver *core_printer_driver)
+{
+	WERROR result;
+	struct dcerpc_binding_handle *b;
+	TALLOC_CTX *tmp_ctx;
+
+	tmp_ctx = talloc_stackframe();
+	if (tmp_ctx == NULL) {
+		return WERR_NOT_ENOUGH_MEMORY;
+	}
+
+	result = winreg_printer_binding_handle(tmp_ctx, session_info, msg_ctx, &b);
+	if (!W_ERROR_IS_OK(result)) {
+		talloc_free(tmp_ctx);
+		return result;
+	}
+
+	result = winreg_add_core_driver(mem_ctx,
+					b,
+					architecture,
+					core_printer_driver);
+
+	talloc_free(tmp_ctx);
+	return result;
+}
 
 WERROR winreg_get_printer_secdesc_internal(TALLOC_CTX *mem_ctx,
 					   const struct auth_session_info *session_info,
