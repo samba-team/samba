samba-team/samba

commit f227c26178c2eaf3fae49f32a90ec905f85005f4

ctdb-scripts: Update script boilerplate to avoid shellcheck warnings
* Assign the output of dirname to temporary variable to avoid word
  splitting when directory name contains whitespace

* Drop export of CTDB_BASE to avoid masking broken return value -
  functions file does the export anyway

* Quote path when including functions file

Signed-off-by: Martin Schwenke <martin@meltin.net>
Reviewed-by: Amitay Isaacs <amitay@gmail.com>

ctdb/config/ctdb-crash-cleanup.sh
@@ -5,9 +5,9 @@
 #
 
 [ -n "$CTDB_BASE" ] || \
-    export CTDB_BASE=$(cd -P $(dirname "$0") ; echo "$PWD")
+    CTDB_BASE=$(d=$(dirname "$0") ; cd -P "$d" ; dirname "$PWD")
 
-. "$CTDB_BASE/functions"
+. "${CTDB_BASE}/functions"
 
 # If ctdb is running, just exit
 if service ctdb status >/dev/null 2>&1 ; then

ctdb/config/debug-hung-script.sh
@@ -4,9 +4,9 @@
 # for other operating systems.
 
 [ -n "$CTDB_BASE" ] || \
-    export CTDB_BASE=$(cd -P $(dirname "$0") ; echo "$PWD")
+    CTDB_BASE=$(d=$(dirname "$0") ; cd -P "$d" ; dirname "$PWD")
 
-. "$CTDB_BASE/functions"
+. "${CTDB_BASE}/functions"
 
 loadconfig ctdb
 

ctdb/config/debug_locks.sh
@@ -8,9 +8,9 @@
 # rather than mutex locks.
 
 [ -n "$CTDB_BASE" ] || \
-    export CTDB_BASE=$(cd -P $(dirname "$0") ; echo "$PWD")
+    CTDB_BASE=$(d=$(dirname "$0") ; cd -P "$d" ; dirname "$PWD")
 
-. "$CTDB_BASE/functions"
+. "${CTDB_BASE}/functions"
 
 # Default fallback location for database directories.
 # These can be overwritten from CTDB configuration

ctdb/config/statd-callout
@@ -9,9 +9,9 @@
 #   STATD_HOSTNAME="myhostname -H /etc/ctdb/statd-callout"
 
 [ -n "$CTDB_BASE" ] || \
-    export CTDB_BASE=$(cd -P $(dirname "$0") ; echo "$PWD")
+    CTDB_BASE=$(d=$(dirname "$0") ; cd -P "$d" ; dirname "$PWD")
 
-. $CTDB_BASE/functions
+. "${CTDB_BASE}/functions"
 
 # Overwrite this so we get some logging
 die ()

ctdb/config/events.d/00.ctdb
@@ -4,9 +4,10 @@
 # elsewhere.
 
 [ -n "$CTDB_BASE" ] || \
-    export CTDB_BASE=$(cd -P $(dirname "$0") ; dirname "$PWD")
+    CTDB_BASE=$(d=$(dirname "$0") ; cd -P "$d" ; dirname "$PWD")
 
-. $CTDB_BASE/functions
+. "${CTDB_BASE}/functions"
+
 loadconfig
 
 ctdb_setup_service_state_dir "ctdb"

ctdb/config/events.d/01.reclock
@@ -2,9 +2,10 @@
 # script to check accessibility to the reclock file on a node
 
 [ -n "$CTDB_BASE" ] || \
-    export CTDB_BASE=$(cd -P $(dirname "$0") ; dirname "$PWD")
+    CTDB_BASE=$(d=$(dirname "$0") ; cd -P "$d" ; dirname "$PWD")
 
-. $CTDB_BASE/functions
+. "${CTDB_BASE}/functions"
+
 loadconfig
 
 # If CTDB_RECOVERY_LOCK specifies a helper then exit because this

ctdb/config/events.d/05.system
@@ -2,9 +2,10 @@
 # ctdb event script for checking local file system utilization
 
 [ -n "$CTDB_BASE" ] || \
-    export CTDB_BASE=$(cd -P $(dirname "$0") ; dirname "$PWD")
+    CTDB_BASE=$(d=$(dirname "$0") ; cd -P "$d" ; dirname "$PWD")
 
-. $CTDB_BASE/functions
+. "${CTDB_BASE}/functions"
+
 loadconfig
 
 ctdb_setup_service_state_dir "system-monitoring"

ctdb/config/events.d/06.nfs
@@ -2,9 +2,9 @@
 # script to manage nfs in a clustered environment
 
 [ -n "$CTDB_BASE" ] || \
-    export CTDB_BASE=$(cd -P $(dirname "$0") ; dirname "$PWD")
+    CTDB_BASE=$(d=$(dirname "$0") ; cd -P "$d" ; dirname "$PWD")
 
-. $CTDB_BASE/functions
+. "${CTDB_BASE}/functions"
 
 service_name="nfs"
 loadconfig

commit 9c6f0dc69bd9eb2ca2bc1ca18c81762791958f88

ctdb-scripts: Fix calculation of CTDB_BASE
This is a regression introduced in f227c26178c2eaf3fae49f32a90ec905f85005f4.

BUG: https://bugzilla.samba.org/show_bug.cgi?id=12407

Signed-off-by: Amitay Isaacs <amitay@gmail.com>
Reviewed-by: Martin Schwenke <martin@meltin.net>

Autobuild-User(master): Martin Schwenke <martins@samba.org>
Autobuild-Date(master): Thu Nov  3 10:10:31 CET 2016 on sn-devel-144

ctdb/config/ctdb-crash-cleanup.sh
@@ -5,7 +5,7 @@
 #
 
 [ -n "$CTDB_BASE" ] || \
-    CTDB_BASE=$(d=$(dirname "$0") ; cd -P "$d" ; dirname "$PWD")
+    CTDB_BASE=$(d=$(dirname "$0") ; cd -P "$d" ; echo "$PWD")
 
 . "${CTDB_BASE}/functions"
 

ctdb/config/debug-hung-script.sh
@@ -4,7 +4,7 @@
 # for other operating systems.
 
 [ -n "$CTDB_BASE" ] || \
-    CTDB_BASE=$(d=$(dirname "$0") ; cd -P "$d" ; dirname "$PWD")
+    CTDB_BASE=$(d=$(dirname "$0") ; cd -P "$d" ; echo "$PWD")
 
 . "${CTDB_BASE}/functions"
 

ctdb/config/debug_locks.sh
@@ -8,7 +8,7 @@
 # rather than mutex locks.
 
 [ -n "$CTDB_BASE" ] || \
-    CTDB_BASE=$(d=$(dirname "$0") ; cd -P "$d" ; dirname "$PWD")
+    CTDB_BASE=$(d=$(dirname "$0") ; cd -P "$d" ; echo "$PWD")
 
 . "${CTDB_BASE}/functions"
 

ctdb/config/statd-callout
@@ -9,7 +9,7 @@
 #   STATD_HOSTNAME="myhostname -H /etc/ctdb/statd-callout"
 
 [ -n "$CTDB_BASE" ] || \
-    CTDB_BASE=$(d=$(dirname "$0") ; cd -P "$d" ; dirname "$PWD")
+    CTDB_BASE=$(d=$(dirname "$0") ; cd -P "$d" ; echo "$PWD")
 
 . "${CTDB_BASE}/functions"
 