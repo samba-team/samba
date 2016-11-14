4024d20819a1e3e0f7158730f8ef4b327f927bc2
Kevin Anderson <andersonkw2@gmail.com>
2016-11-14 19:14:44 +0100

    vfs_fruit: Add Time Machine support
    Add a configuration option to disable/enable Time Machine support via
    the FULLSYNC AAPL flag.
    
    Signed-off-by: Kevin Anderson <andersonkw2@gmail.com>

--- libcli/smb/smb2_create_ctx.h
+++ libcli/smb/smb2_create_ctx.h
@@ -42,5 +42,6 @@
 /* "AAPL" Volume Capabilities bitmap */
 #define SMB2_CRTCTX_AAPL_SUPPORT_RESOLVE_ID 1
 #define SMB2_CRTCTX_AAPL_CASE_SENSITIVE     2
+#define SMB2_CRTCTX_AAPL_FULL_SYNC          4
 
 #endif
