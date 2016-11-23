f464f69b33b40c81d6ee57bebf9d59837431739b
Andreas Schneider <asn@samba.org>
2016-11-23 14:39:47 +0100

    s3:param: Add an 'include system krb5 conf' option
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=12441
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Alexander Bokovoy <ab@samba.org>

--- source3/param/loadparm.c
+++ source3/param/loadparm.c
@@ -711,6 +711,7 @@ static void init_globals(struct loadparm_context *lp_ctx, bool reinit_globals)
 	Globals.reset_on_zero_vc = false;
 	Globals.log_writeable_files_on_exit = false;
 	Globals.create_krb5_conf = true;
+	Globals.include_system_krb5_conf = true;
 	Globals._winbind_max_domain_connections = 1;
 
 	/* hostname lookups can be very expensive and are broken on
