82b2bb254b8008f692450ec850efff87b9b96e7b
Ralph Boehme <slow@samba.org>
2016-12-02 07:42:07 +0100

    selftest: also run vfs_fruit tests with streams_depot
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=12427
    
    Signed-off-by: Ralph Boehme <slow@samba.org>
    Reviewed-by: Uri Simchoni <uri@samba.org>
    (cherry picked from commit f7b0982826b58cfcdaf8782e15c2440f31a45ba2)

--- selftest/target/Samba3.pm
+++ selftest/target/Samba3.pm
@@ -1668,6 +1668,13 @@ sub provision($$$$$$$$)
 	fruit:resource = file
 	fruit:metadata = stream
 
+[vfs_fruit_stream_depot]
+	path = $shrdir
+	vfs objects = fruit streams_depot acl_xattr
+	ea support = yes
+	fruit:resource = stream
+	fruit:metadata = stream
+
 [badname-tmp]
 	path = $badnames_shrdir
 	guest ok = yes
