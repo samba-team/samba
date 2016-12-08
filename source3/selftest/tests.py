7fb2f57b587b42eb32afdd31e7a77c5ab464cadd
Ralph Boehme <slow@samba.org>
2016-12-08 20:43:21 +0100

    selftest: reenable vfs_fruit tests
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=12427
    
    Signed-off-by: Ralph Boehme <slow@samba.org>
    Reviewed-by: Uri Simchoni <uri@samba.org>
    (cherry picked from commit 51189bd4758a140bad6abf3300c931302432a687)

--- source3/selftest/tests.py
+++ source3/selftest/tests.py
@@ -329,7 +329,7 @@ nbt = ["nbt.dgram" ]
 
 libsmbclient = ["libsmbclient"]
 
-vfs = ["vfs.acl_xattr"]
+vfs = ["vfs.fruit", "vfs.acl_xattr"]
 
 tests= base + raw + smb2 + rpc + unix + local + rap + nbt + libsmbclient + idmap + vfs
 
