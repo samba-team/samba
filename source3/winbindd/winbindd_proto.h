7981c6f9b5ce7ce294bfc9932286b7da03390c01
Volker Lendecke <vl@samba.org>
2016-12-30 11:47:45 +0000

    winbind: Remove wb_fill_pwent
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Uri Simchoni <uri@samba.org>
    Reviewed-by: Andreas Schneider <asn@samba.org>

--- source3/winbindd/winbindd_proto.h
+++ source3/winbindd/winbindd_proto.h
@@ -783,13 +783,6 @@ NTSTATUS wb_query_group_list_recv(struct tevent_req *req, TALLOC_CTX *mem_ctx,
 				  int *num_users,
 				  struct wbint_Principal **groups);
 
-
-struct tevent_req *wb_fill_pwent_send(TALLOC_CTX *mem_ctx,
-				      struct tevent_context *ev,
-				      const struct wbint_userinfo *info,
-				      struct winbindd_pw *pw);
-NTSTATUS wb_fill_pwent_recv(struct tevent_req *req);
-
 struct tevent_req *wb_next_pwent_send(TALLOC_CTX *mem_ctx,
 				      struct tevent_context *ev,
 				      struct getpwent_state *gstate,
