0865fea4fcd8c540b59accdf24c27d09f580ae02
Volker Lendecke <vl@samba.org>
2017-01-08 19:52:47 +0000

    lib: Avoid an includes.h
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- lib/util/util_pw.c
+++ lib/util/util_pw.c
@@ -24,7 +24,8 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "includes.h"
+#include "replace.h"
+#include <talloc.h>
 #include "system/passwd.h"
 #include "lib/util/util_pw.h"
 
