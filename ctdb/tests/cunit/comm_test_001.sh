e7856f051b311ee52ad0a3a5cd580cf35d368e98
Amitay Isaacs <amitay@gmail.com>
2017-02-07 15:18:02 +1100

    ctdb-tests: Add more comm tests
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=12580
    
    Signed-off-by: Amitay Isaacs <amitay@gmail.com>
    Reviewed-by: Martin Schwenke <martin@meltin.net>
    (cherry picked from commit 024a2c20d2bcdbcc43d16d492c7cd2d09b93c8f0)

--- ctdb/tests/cunit/comm_test_001.sh
+++ ctdb/tests/cunit/comm_test_001.sh
@@ -2,6 +2,12 @@
 
 . "${TEST_SCRIPTS_DIR}/unit.sh"
 
-ok "100 2048 500 4096 1024 8192 200 16384 300 32768 400 65536 1048576 "
 
-unit_test comm_test
+ok_null
+unit_test comm_test 1
+
+ok_null
+unit_test comm_test 2
+
+ok "100 2048 500 4096 1024 8192 200 16384 300 32768 400 65536 1048576 "
+unit_test comm_test 3
