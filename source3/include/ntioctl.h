a091a30a5bd50bc02df3e9c01b11f70c94dbd053
Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
2017-02-09 13:02:52 +1300

    shadow_copy_get_shadow_copy_data: fix GCC snprintf warning
    GCC 7 warns about snprintf truncating a dirent d_name (potentially 255 bytes) to 25 bytes,
    even though we have checked that it is 25 long in shadow_copy_match_name().
    
    Using strlcpy instead of snprintf lets us check it again, JUST TO BE SURE.
    
    Signed-off-by: Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- source3/include/ntioctl.h
+++ source3/include/ntioctl.h
@@ -30,7 +30,7 @@
 
 
 /* For FSCTL_GET_SHADOW_COPY_DATA ...*/
-typedef char SHADOW_COPY_LABEL[25];
+typedef char SHADOW_COPY_LABEL[25]; /* sizeof("@GMT-2004.02.18-15.44.00") + 1 */
 
 struct shadow_copy_data {
 	/* Total number of shadow volumes currently mounted */
