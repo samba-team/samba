59b580f6c72d4d296a22c95fc1ef572ed2c696b0
Volker Lendecke <vl@samba.org>
2017-02-12 19:20:07 +0100

    auth_ntdomain3: Correctly handle !authoritative
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- source3/auth/auth_domain.c
+++ source3/auth/auth_domain.c
@@ -229,6 +229,10 @@ static NTSTATUS domain_client_validate(TALLOC_CTX *mem_ctx,
 		if (NT_STATUS_EQUAL(nt_status, NT_STATUS_UNSUCCESSFUL)) {
 			nt_status = NT_STATUS_NO_LOGON_SERVERS;
 		}
+		if (NT_STATUS_EQUAL(nt_status, NT_STATUS_NO_SUCH_USER) &&
+		    (authoritative == 0)) {
+			nt_status = NT_STATUS_NOT_IMPLEMENTED;
+		}
 	} else {
 		nt_status = make_server_info_info3(mem_ctx,
 						   user_info->client.account_name,
