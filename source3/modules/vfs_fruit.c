samba-team/samba

commit e4d1f8354f97ab9007e4c5f7d164937bdc5cd6f1

vfs_fruit: cleanup metadata and resource xattr name defines
Just some cleanup, no change in behaviour. This also removes the hokey
tag. :)

Bug: https://bugzilla.samba.org/show_bug.cgi?id=12490

Signed-off-by: Ralph Boehme <slow@samba.org>
Reviewed-by: Jeremy Allison <jra@samba.org>

source3/modules/vfs_fruit.c
@@ -100,17 +100,15 @@ static int vfs_fruit_debug_level = DBGC_VFS;
 #define FRUIT_PARAM_TYPE_NAME "fruit"
 #define ADOUBLE_NAME_PREFIX "._"
 
-/*
- * REVIEW:
- * This is hokey, but what else can we do?
- */
 #define NETATALK_META_XATTR "org.netatalk.Metadata"
+#define NETATALK_RSRC_XATTR "org.netatalk.Metadata"
+
 #if defined(HAVE_ATTROPEN)
 #define AFPINFO_EA_NETATALK NETATALK_META_XATTR
-#define AFPRESOURCE_EA_NETATALK "org.netatalk.ResourceFork"
+#define AFPRESOURCE_EA_NETATALK NETATALK_RSRC_XATTR
 #else
 #define AFPINFO_EA_NETATALK "user." NETATALK_META_XATTR
-#define AFPRESOURCE_EA_NETATALK "user.org.netatalk.ResourceFork"
+#define AFPRESOURCE_EA_NETATALK "user." NETATALK_RSRC_XATTR
 #endif
 
 enum apple_fork {APPLE_FORK_DATA, APPLE_FORK_RSRC};

commit 221faba006e84b05004500d2261307f269858a18

vfs_fruit: fix resource fork xattr name
Fix resource fork xattr name broken in
e4d1f8354f97ab9007e4c5f7d164937bdc5cd6f1.

Bug: https://bugzilla.samba.org/show_bug.cgi?id=12490

Signed-off-by: Ralph Boehme <slow@samba.org>
Reviewed-by: Ralph Boehme <slow@samba.org>

Autobuild-User(master): Ralph Böhme <slow@samba.org>
Autobuild-Date(master): Tue Feb 14 21:26:01 CET 2017 on sn-devel-144

source3/modules/vfs_fruit.c
@@ -101,7 +101,7 @@ static int vfs_fruit_debug_level = DBGC_VFS;
 #define ADOUBLE_NAME_PREFIX "._"
 
 #define NETATALK_META_XATTR "org.netatalk.Metadata"
-#define NETATALK_RSRC_XATTR "org.netatalk.Metadata"
+#define NETATALK_RSRC_XATTR "org.netatalk.ResourceFork"
 
 #if defined(HAVE_ATTROPEN)
 #define AFPINFO_EA_NETATALK NETATALK_META_XATTR