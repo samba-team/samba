529142a7595a885541238ecba92c776927c243ae
Amitay Isaacs <amitay@gmail.com>
2017-03-02 11:15:26 +1100

    ctdb-daemon: Once database is attached, do not modify tdb flags
    Signed-off-by: Amitay Isaacs <amitay@gmail.com>
    Reviewed-by: Martin Schwenke <martin@meltin.net>

--- ctdb/server/ctdb_ltdb_server.c
+++ ctdb/server/ctdb_ltdb_server.c
@@ -1193,7 +1193,6 @@ int32_t ctdb_control_db_attach(struct ctdb_context *ctdb, TDB_DATA indata,
 		}
 		outdata->dptr  = (uint8_t *)&db->db_id;
 		outdata->dsize = sizeof(db->db_id);
-		tdb_add_flags(db->ltdb->tdb, tdb_flags);
 		return 0;
 	}
 
