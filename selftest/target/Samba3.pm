c5c8e6aa11d9eb75874f34014137ccf7b63ac101
Jeremy Allison <jra@samba.org>
2017-03-27 11:48:25 -0700

    s3: Test for CVE-2017-2619 regression with "follow symlinks = no".
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=12721
    
    Signed-off-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: Uri Simchoni <uri@samba.org>
    
    Back-ported from commit 782172a9bef0040981d20e49519b13dd744df6a0

--- selftest/target/Samba3.pm
+++ selftest/target/Samba3.pm
@@ -1207,6 +1207,9 @@ sub provision($$$$$$$$)
 	my $shadow_shrdir="$shadow_basedir/share";
 	push(@dirs,$shadow_shrdir);
 
+	my $nosymlinks_shrdir="$shrdir/nosymlinks";
+	push(@dirs,$nosymlinks_shrdir);
+
 	# this gets autocreated by winbindd
 	my $wbsockdir="$prefix_abs/winbindd";
 	my $wbsockprivdir="$lockdir/winbindd_privileged";
@@ -1824,6 +1827,10 @@ sub provision($$$$$$$$)
 	copy = tmp
 	acl_xattr:ignore system acls = yes
 	acl_xattr:default acl style = windows
+[nosymlinks]
+	copy = tmp
+	path = $nosymlinks_shrdir
+	follow symlinks = no
 [kernel_oplocks]
 	copy = tmp
 	kernel oplocks = yes
