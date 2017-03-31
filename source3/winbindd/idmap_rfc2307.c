c73b49e16f2a7d27886d9914d49bb1462cc7b76a
Volker Lendecke <vl@samba.org>
2017-03-31 15:23:39 +0000

    idmap_rfc2307: "ldap_next_entry" needs the previous entry, not the start
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=12757
    
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Christof Schmitt <cs@samba.org>
    (cherry picked from commit 17563f295ffa7379daa5bf7cc89540df4ae4f7b3)

--- source3/winbindd/idmap_rfc2307.c
+++ source3/winbindd/idmap_rfc2307.c
@@ -553,7 +553,7 @@ static void idmap_rfc2307_map_xid_results(struct idmap_rfc2307_context *ctx,
 		if (i == 0) {
 			entry = ldap_first_entry(ctx->ldap, result);
 		} else {
-			entry = ldap_next_entry(ctx->ldap, result);
+			entry = ldap_next_entry(ctx->ldap, entry);
 		}
 		if (!entry) {
 			DEBUG(2, ("Unable to fetch entry.\n"));
