ff5865a927d29c9e6b7d98c16adc23b8e9dbfc80
Ralph Boehme <slow@samba.org>
2017-04-04 14:23:03 +0200

    winbindd: remove unused single_domains array
    This was added as part of 9be918116e356c358ef77cc2933e471090088293, but
    is not needed anymore as the previous commit changed the logic.
    
    Bug: https://bugzilla.samba.org/show_bug.cgi?id=11961
    
    Pair-Programmed-With: Stefan Metzmacher <metze@samba.org>
    
    Signed-off-by: Ralph Boehme <slow@samba.org>
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    (cherry picked from commit 9671811da8ad3f91ba7bb0fa868f806bc5afe863)

--- source3/winbindd/wb_lookupsids.c
+++ source3/winbindd/wb_lookupsids.c
@@ -73,8 +73,6 @@ struct wb_lookupsids_state {
 	 * wbint_LookupSid. Preallocated with num_sids.
 	 */
 	uint32_t *single_sids;
-	/* Pointer into the "domains" array above*/
-	struct wb_lookupsids_domain **single_domains;
 	uint32_t num_single_sids;
 	uint32_t single_sids_done;
 
@@ -128,12 +126,6 @@ struct tevent_req *wb_lookupsids_send(TALLOC_CTX *mem_ctx,
 
 	state->single_sids = talloc_array(state, uint32_t, num_sids);
 	if (tevent_req_nomem(state->single_sids, req)) {
-		return tevent_req_post(req, ev);
-	}
-	state->single_domains = talloc_zero_array(state,
-						  struct wb_lookupsids_domain *,
-						  num_sids);
-	if (tevent_req_nomem(state->single_domains, req)) {
 		return tevent_req_post(req, ev);
 	}
 
@@ -463,7 +455,6 @@ static void wb_lookupsids_done(struct tevent_req *subreq)
 
 			state->single_sids[state->num_single_sids] =
 				res_sid_index;
-			state->single_domains[state->num_single_sids] = d;
 			state->num_single_sids += 1;
 		}
 		state->domains_done += 1;
