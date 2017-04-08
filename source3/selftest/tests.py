5e2937966fe20e3747c080ce171c106af157d7f8
Uri Simchoni <uri@samba.org>
2017-04-09 00:20:40 +0300

    selftest: test fetching a large ACL from vfs_acl_xattr
    Add a test that fetches an ACL whose size is larger than 4K.
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=12737
    
    Signed-off-by: Uri Simchoni <uri@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>
    (back-ported from commit 5017dfeef24b8d568e0146c085f3f979d688acf2)

--- source3/selftest/tests.py
+++ source3/selftest/tests.py
@@ -219,6 +219,7 @@ for env in ["fileserver"]:
     plantestsuite("samba3.blackbox.netshareenum (%s)" % env, env, [os.path.join(samba3srcdir, "script/tests/test_shareenum.sh"), '$SERVER', '$USERNAME', '$PASSWORD', rpcclient])
     plantestsuite("samba3.blackbox.acl_xattr (%s)" % env, env, [os.path.join(samba3srcdir, "script/tests/test_acl_xattr.sh"), '$SERVER', '$USERNAME', '$PASSWORD', '$PREFIX', smbclient3, smbcacls])
     plantestsuite("samba3.blackbox.smb2.not_casesensitive (%s)" % env, env, [os.path.join(samba3srcdir, "script/tests/test_smb2_not_casesensitive.sh"), '//$SERVER/tmp', '$SERVER_IP', '$USERNAME', '$PASSWORD', '$LOCAL_PATH', smbclient3])
+    plantestsuite("samba3.blackbox.large_acl (%s)" % env, env, [os.path.join(samba3srcdir, "script/tests/test_large_acl.sh"), '$SERVER', '$USERNAME', '$PASSWORD', smbclient3, smbcacls])
 
     #
     # tar command tests
