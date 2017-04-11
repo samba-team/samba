bf8f7a36bfbe5c2018dbd9a317c8791c56b4114e
Jeremy Allison <jra@samba.org>
2017-04-11 16:06:08 -0700

    lib:charset: Remove use of talloc_autofree_context() for global_iconv_handle
    All other callers use NULL here anyway, so there's no
    need to use a special context for get_iconv_handle().
    
    Signed-off-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: Andreas Schneider <asn@samba.org>

--- lib/util/charset/codepoints.c
+++ lib/util/charset/codepoints.c
@@ -16506,9 +16506,15 @@ static struct smb_iconv_handle *global_iconv_handle = NULL;
 
 struct smb_iconv_handle *get_iconv_handle(void)
 {
-	if (global_iconv_handle == NULL)
-		global_iconv_handle = smb_iconv_handle_reinit(talloc_autofree_context(),
-							      "ASCII", "UTF-8", true, NULL);
+	if (global_iconv_handle == NULL) {
+		global_iconv_handle =
+			smb_iconv_handle_reinit(NULL,
+						"ASCII",
+						"UTF-8",
+						true,
+						NULL);
+	}
+
 	return global_iconv_handle;
 }
 
