9f7ed6b82c68592dc32590dea3a0752ce6e69896
Andrew Bartlett <abartlet@samba.org>
2017-04-19 22:44:24 +1200

    autobuild: Do not require cmocka to be installed for samba-libs to build
    cmocka does not have an install target, but is a depencency of ldb for the tests
    
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Andrew Bartlet <abartlet@samba.org>
    
    Autobuild-User(master): Andreas Schneider <asn@cryptomilk.org>
    Autobuild-Date(master): Wed Apr 19 19:43:15 CEST 2017 on sn-devel-144

--- script/autobuild.py
+++ script/autobuild.py
@@ -56,7 +56,7 @@ samba_libs_envvars =  "PYTHONPATH=${PYTHON_PREFIX}/site-packages:$PYTHONPATH"
 samba_libs_envvars += " PKG_CONFIG_PATH=$PKG_CONFIG_PATH:${PREFIX_DIR}/lib/pkgconfig"
 samba_libs_envvars += " ADDITIONAL_CFLAGS='-Wmissing-prototypes'"
 samba_libs_configure_base = samba_libs_envvars + " ./configure --abi-check --enable-debug --picky-developer -C ${PREFIX} ${EXTRA_PYTHON}"
-samba_libs_configure_libs = samba_libs_configure_base + " --bundled-libraries=NONE"
+samba_libs_configure_libs = samba_libs_configure_base + " --bundled-libraries=cmocka,NONE"
 samba_libs_configure_samba = samba_libs_configure_base + " --bundled-libraries=!talloc,!pytalloc-util,!tdb,!pytdb,!ldb,!pyldb,!pyldb-util,!tevent,!pytevent"
 
 if os.environ.get("AUTOBUILD_NO_EXTRA_PYTHON", "0") == "1":
