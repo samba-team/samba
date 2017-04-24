67557dceeb577a7c5625afe245d5d2421a20c802
Jeremy Allison <jra@samba.org>
2017-04-24 15:45:02 -0700

    s4: torture: Add a TALLOC_CTX * to torture_smb2_streams_init().
    Signed-off-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: Ralph Böhme <slow@samba.org>

--- source4/torture/smb2/smb2.c
+++ source4/torture/smb2/smb2.c
@@ -168,7 +168,7 @@ NTSTATUS torture_smb2_init(TALLOC_CTX *ctx)
 	torture_suite_add_suite(suite, torture_smb2_compound_find_init(suite));
 	torture_suite_add_suite(suite, torture_smb2_oplocks_init(suite));
 	torture_suite_add_suite(suite, torture_smb2_kernel_oplocks_init(suite));
-	torture_suite_add_suite(suite, torture_smb2_streams_init());
+	torture_suite_add_suite(suite, torture_smb2_streams_init(suite));
 	torture_suite_add_suite(suite, torture_smb2_ioctl_init(suite));
 	torture_suite_add_suite(suite, torture_smb2_rename_init(suite));
 	torture_suite_add_1smb2_test(suite, "bench-oplock", test_smb2_bench_oplock);
--- source4/torture/smb2/streams.c
+++ source4/torture/smb2/streams.c
@@ -1834,10 +1834,10 @@ done:
 /*
    basic testing of streams calls SMB2
 */
-struct torture_suite *torture_smb2_streams_init(void)
+struct torture_suite *torture_smb2_streams_init(TALLOC_CTX *ctx)
 {
 	struct torture_suite *suite =
-		torture_suite_create(talloc_autofree_context(), "streams");
+		torture_suite_create(ctx, "streams");
 
 	torture_suite_add_1smb2_test(suite, "dir", test_stream_dir);
 	torture_suite_add_1smb2_test(suite, "io", test_stream_io);
