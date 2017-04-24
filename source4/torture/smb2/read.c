c08d43c699546f3805f1e53fe1ce00dc7374bfb2
Jeremy Allison <jra@samba.org>
2017-04-24 15:38:38 -0700

    s4: torture: Add a TALLOC_CTX * to torture_smb2_read_init().
    Signed-off-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: Ralph Böhme <slow@samba.org>

--- source4/torture/smb2/read.c
+++ source4/torture/smb2/read.c
@@ -303,9 +303,9 @@ done:
 /* 
    basic testing of SMB2 read
 */
-struct torture_suite *torture_smb2_read_init(void)
+struct torture_suite *torture_smb2_read_init(TALLOC_CTX *ctx)
 {
-	struct torture_suite *suite = torture_suite_create(talloc_autofree_context(), "read");
+	struct torture_suite *suite = torture_suite_create(ctx, "read");
 
 	torture_suite_add_1smb2_test(suite, "eof", test_read_eof);
 	torture_suite_add_1smb2_test(suite, "position", test_read_position);
--- source4/torture/smb2/smb2.c
+++ source4/torture/smb2/smb2.c
@@ -151,7 +151,7 @@ NTSTATUS torture_smb2_init(TALLOC_CTX *ctx)
 	torture_suite_add_suite(suite, torture_smb2_getinfo_init());
 	torture_suite_add_simple_test(suite, "setinfo", torture_smb2_setinfo);
 	torture_suite_add_suite(suite, torture_smb2_lock_init(suite));
-	torture_suite_add_suite(suite, torture_smb2_read_init());
+	torture_suite_add_suite(suite, torture_smb2_read_init(suite));
 	torture_suite_add_suite(suite, torture_smb2_create_init(suite));
 	torture_suite_add_suite(suite, torture_smb2_acls_init(suite));
 	torture_suite_add_suite(suite, torture_smb2_notify_init(suite));
