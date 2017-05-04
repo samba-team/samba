88360c7a8553261159ad81d64c96c79c6aacecac
Ralph Boehme <slow@samba.org>
2017-05-04 11:50:56 +0200

    s3/locking: helper functions for lease types
    Add some helper functions that will be used to update a bunch of checks
    for exclusive oplocks to the lease area.
    
    Bug: https://bugzilla.samba.org/show_bug.cgi?id=12766
    
    Signed-off-by: Ralph Boehme <slow@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>
    (cherry picked from commit f631e95e2de857ea98204609a71e6db00993994b)

--- source3/locking/leases_util.c
+++ source3/locking/leases_util.c
@@ -53,3 +53,20 @@ uint32_t fsp_lease_type(const struct files_struct *fsp)
 	}
 	return map_oplock_to_lease_type(fsp->oplock_type);
 }
+
+uint32_t lease_type_is_exclusive(uint32_t lease_type)
+{
+	if ((lease_type & (SMB2_LEASE_READ | SMB2_LEASE_WRITE)) ==
+	    (SMB2_LEASE_READ | SMB2_LEASE_WRITE)) {
+		return true;
+	}
+
+	return false;
+}
+
+bool fsp_lease_type_is_exclusive(const struct files_struct *fsp)
+{
+	uint32_t lease_type = fsp_lease_type(fsp);
+
+	return lease_type_is_exclusive(lease_type);
+}
