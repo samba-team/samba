5098636c784df917fb038ac4822f7a863d9543de
Jeremy Allison <jra@samba.org>
2017-05-11 16:15:56 -0700

    s4: nmblookup: Allocate event context off NULL instead of talloc_autofree_context().
    It's already correctly freed on exit.
    
    Signed-off-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: Ralph Boehme <slow@samba.org>
    
    Autobuild-User(master): Ralph Böhme <slow@samba.org>
    Autobuild-Date(master): Sat May 13 21:01:25 CEST 2017 on sn-devel-144

--- libcli/nbt/tools/nmblookup.c
+++ libcli/nbt/tools/nmblookup.c
@@ -372,7 +372,7 @@ int main(int argc, const char *argv[])
 
 	load_interface_list(NULL, cmdline_lp_ctx, &ifaces);
 
-	ev = s4_event_context_init(talloc_autofree_context());
+	ev = s4_event_context_init(NULL);
 
 	while (poptPeekArg(pc)) {
 		const char *name = poptGetArg(pc);
