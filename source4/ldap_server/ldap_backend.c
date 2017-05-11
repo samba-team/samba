ace49db796a121bd35741e86e3e0aa98be48e81f
Stefan Metzmacher <metze@samba.org>
2017-05-11 19:07:04 +0200

    s4:ldap_server: use talloc_zero() in ldapsrv_init_reply()
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- source4/ldap_server/ldap_backend.c
+++ source4/ldap_server/ldap_backend.c
@@ -237,11 +237,11 @@ struct ldapsrv_reply *ldapsrv_init_reply(struct ldapsrv_call *call, uint8_t type
 {
 	struct ldapsrv_reply *reply;
 
-	reply = talloc(call, struct ldapsrv_reply);
+	reply = talloc_zero(call, struct ldapsrv_reply);
 	if (!reply) {
 		return NULL;
 	}
-	reply->msg = talloc(reply, struct ldap_message);
+	reply->msg = talloc_zero(reply, struct ldap_message);
 	if (reply->msg == NULL) {
 		talloc_free(reply);
 		return NULL;
