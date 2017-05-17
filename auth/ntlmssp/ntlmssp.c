458495d604719dd323ecd6b8aa91919ccb555bd6
Stefan Metzmacher <metze@samba.org>
2017-05-17 20:21:19 +0200

    auth/ntlmssp: remove unused variable from gensec_ntlmssp_update()
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- auth/ntlmssp/ntlmssp.c
+++ auth/ntlmssp/ntlmssp.c
@@ -162,7 +162,6 @@ NTSTATUS gensec_ntlmssp_update(struct gensec_security *gensec_security,
 	struct gensec_ntlmssp_context *gensec_ntlmssp =
 		talloc_get_type_abort(gensec_security->private_data,
 				      struct gensec_ntlmssp_context);
-	struct ntlmssp_state *ntlmssp_state = gensec_ntlmssp->ntlmssp_state;
 	NTSTATUS status;
 	uint32_t i;
 
