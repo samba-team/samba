e3a151e2472d97891c97cc898f27f3ccf712bf35
Uri Simchoni <uri@samba.org>
2017-06-07 20:33:24 +0300

    winbindd: cache name-to-sid from PAC based on lookup domain
    The name-to-sid lookup for trusted domains is not necessarily
    done against the domain - in AD member case it is done
    against the primary domain. Therefore the caching should also
    be done against the lookup domain.
    
    Signed-off-by: Uri Simchoni <uri@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/winbindd/winbindd_pam.c
+++ source3/winbindd/winbindd_pam.c
@@ -2664,7 +2664,7 @@ NTSTATUS winbindd_pam_auth_pac_send(struct winbindd_cli_state *state,
 		 * We're in the parent here, so find the child
 		 * pointer from the PAC domain name.
 		 */
-		domain = find_domain_from_name_noinit(
+		domain = find_lookup_domain_from_name(
 				info3_copy->base.logon_domain.string);
 		if (domain && domain->primary ) {
 			struct dom_sid user_sid;
