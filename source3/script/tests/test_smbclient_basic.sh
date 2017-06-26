26fb6350fa82dfd61a3246bf822558885f8d37da
Stefan Metzmacher <metze@samba.org>
2017-06-26 11:56:40 +0200

    s3:test_smbclient_basic.sh: make use of $incdir/common_test_fns.inc
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Andreas Schneider <asn@samba.org>

--- source3/script/tests/test_smbclient_basic.sh
+++ source3/script/tests/test_smbclient_basic.sh
@@ -20,30 +20,15 @@ ADDARGS="$@"
 
 incdir=`dirname $0`/../../../testprogs/blackbox
 . $incdir/subunit.sh
-
-test_smbclient() {
-	name="$1"
-	cmd="$2"
-	shift
-	shift
-	echo "test: $name"
-	$VALGRIND $smbclient $CONFIGURATION //$SERVER/tmp -c "$cmd" ${ADDARGS} $@
-	status=$?
-	if [ x$status = x0 ]; then
-		echo "success: $name"
-	else
-		echo "failure: $name"
-	fi
-	return $status
-}
+. $incdir/common_test_fns.inc
 
 # TEST using \ as the separator (default)
-test_smbclient "smbclient as $DOMAIN\\$USERNAME" 'ls' -U$DOMAIN\\$USERNAME%$PASSWORD $CONFIGURATION || failed=`expr $failed + 1`
+test_smbclient "smbclient as $DOMAIN\\$USERNAME" 'ls' "//$SERVER/tmp" -U$DOMAIN\\$USERNAME%$PASSWORD $ADDARGS || failed=`expr $failed + 1`
 # TEST using / as the separator (default)
-test_smbclient "smbclient as $DOMAIN/$USERNAME" 'ls' -U$DOMAIN/$USERNAME%$PASSWORD $CONFIGURATION || failed=`expr $failed + 1`
+test_smbclient "smbclient as $DOMAIN/$USERNAME" 'ls' "//$SERVER/tmp" -U$DOMAIN/$USERNAME%$PASSWORD $ADDARGS || failed=`expr $failed + 1`
 
 # TEST using 'winbind separator = +'
-test_smbclient "smbclient as $DOMAIN+$USERNAME" 'ls' -U$DOMAIN+$USERNAME%$PASSWORD $CONFIGURATION --option=winbindseparator=+ || failed=`expr $failed + 1`
+test_smbclient "smbclient as $DOMAIN+$USERNAME" 'ls' "//$SERVER/tmp" -U$DOMAIN+$USERNAME%$PASSWORD $ADDARGS --option=winbindseparator=+ || failed=`expr $failed + 1`
 
 # TEST using 'winbind separator = +' set in a config file
 smbclient_config="$PREFIX/tmpsmbconf"
@@ -55,7 +40,7 @@ EOF
 
 SAVE_CONFIGURATION="$CONFIGURATION"
 CONFIGURATION="--configfile=$smbclient_config"
-test_smbclient "smbclient as $DOMAIN+$USERNAME" 'ls' -U$DOMAIN+$USERNAME%$PASSWORD || failed=`expr $failed + 1`
+test_smbclient "smbclient as $DOMAIN+$USERNAME" 'ls' "//$SERVER/tmp" -U$DOMAIN+$USERNAME%$PASSWORD $ADDARGS || failed=`expr $failed + 1`
 CONFIGURATION="$SAVE_CONFIGURATION"
 rm -rf $smbclient_config
 
