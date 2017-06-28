1714d0cfa22d7deb93d90ef134c522a9e57d1064
Günther Deschner <gd@samba.org>
2017-06-28 18:10:28 +0200

    vfs_fruit: add fruit:model = <modelname> parametric option
    fruit:model = iMac
    fruit:model = MacBook
    fruit:model = MacPro
    fruit:model = Xserve
    
    will all display a different icon inside Finder.
    
    Formerly, we used "Samba" which resulted in a "?" icon in Finder, with
    the new default "MacSamba" we appear with a computer box icon at least.
    
    Guenther
    
    Bug: https://bugzilla.samba.org/show_bug.cgi?id=12840
    
    Signed-off-by: Guenther Deschner <gd@samba.org>
    Signed-off-by: Ralph Boehme <slow@samba.org>
    Reviewed-by: Ralph Boehme <slow@samba.org>
    
    Autobuild-User(master): Günther Deschner <gd@samba.org>
    Autobuild-Date(master): Wed Jul 12 03:17:57 CEST 2017 on sn-devel-144
    
    (cherry picked from commit 259e1706e3206b215e136ea9d5beef4c9e3fcdee)

--- source4/torture/vfs/fruit.c
+++ source4/torture/vfs/fruit.c
@@ -1252,7 +1252,13 @@ static bool enable_aapl(struct torture_context *tctx,
 	torture_assert_goto(tctx, aapl != NULL, ret, done, "missing AAPL context");
 
 	if (!is_osx_server) {
-		torture_assert_goto(tctx, aapl->data.length == 50, ret, done, "bad AAPL size");
+		size_t exptected_aapl_ctx_size;
+
+		exptected_aapl_ctx_size = strlen("MacSamba") * 2 + 40;
+
+		torture_assert_goto(
+			tctx, aapl->data.length == exptected_aapl_ctx_size,
+			ret, done, "bad AAPL size");
 	}
 
 	aapl_server_caps = BVAL(aapl->data.data, 16);
