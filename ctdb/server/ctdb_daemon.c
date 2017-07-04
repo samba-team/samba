a004c934104b620c3d4b7dc44fdaa6f1717449bb
Amitay Isaacs <amitay@gmail.com>
2017-07-04 15:49:19 +1000

    ctdb-daemon: Increase priority of logs when ctdb starts up disabled/stopped
    Signed-off-by: Amitay Isaacs <amitay@gmail.com>
    Reviewed-by: Martin Schwenke <martin@meltin.net>

--- ctdb/server/ctdb_daemon.c
+++ ctdb/server/ctdb_daemon.c
@@ -1060,12 +1060,14 @@ static void initialise_node_flags (struct ctdb_context *ctdb)
 
 	/* do we start out in DISABLED mode? */
 	if (ctdb->start_as_disabled != 0) {
-		DEBUG(DEBUG_NOTICE, ("This node is configured to start in DISABLED state\n"));
+		DEBUG(DEBUG_ERR,
+		      ("This node is configured to start in DISABLED state\n"));
 		ctdb->nodes[ctdb->pnn]->flags |= NODE_FLAGS_DISABLED;
 	}
 	/* do we start out in STOPPED mode? */
 	if (ctdb->start_as_stopped != 0) {
-		DEBUG(DEBUG_NOTICE, ("This node is configured to start in STOPPED state\n"));
+		DEBUG(DEBUG_ERR,
+		      ("This node is configured to start in STOPPED state\n"));
 		ctdb->nodes[ctdb->pnn]->flags |= NODE_FLAGS_STOPPED;
 	}
 }
