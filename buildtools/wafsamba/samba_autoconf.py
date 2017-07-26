33fa6778e1d95c41733c43b2b694cd22d440e4c6
Andreas Schneider <asn@samba.org>
2017-07-26 16:29:06 +0200

    wafsamba: Build with -Wimplicit-fallthrough if supported
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>
    
    Autobuild-User(master): Andrew Bartlett <abartlet@samba.org>
    Autobuild-Date(master): Thu Mar  1 19:38:12 CET 2018 on sn-devel-144

--- buildtools/wafsamba/samba_autoconf.py
+++ buildtools/wafsamba/samba_autoconf.py
@@ -711,6 +711,8 @@ def SAMBA_CONFIG_H(conf, path=None):
                         testflags=True)
         conf.ADD_CFLAGS('-Werror=uninitialized -Wuninitialized',
                         testflags=True)
+        conf.ADD_CFLAGS('-Wimplicit-fallthrough',
+                        testflags=True)
 
         conf.ADD_CFLAGS('-Wformat=2 -Wno-format-y2k', testflags=True)
         conf.ADD_CFLAGS('-Wno-format-zero-length', testflags=True)
