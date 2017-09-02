7665d999d276f4d6776b237968dc6aded940f4be
Andrew Bartlett <abartlet@samba.org>
2017-09-02 16:31:21 +1200

    provision: Add a fixed objectGUID to the tmp DB used for LDAP backend schema work
    This DB holds a copy of the schema, but now needs to have an objectGUID on each record.
    
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Garming Sam <garming@catalyst.net.nz>

--- python/samba/schema.py
+++ python/samba/schema.py
@@ -136,8 +136,12 @@ dn: @INDEXLIST
 @IDXATTR: attributeSyntax
 @IDXGUID: objectGUID
 """)
+
+            schema_dn_add = self.schema_dn_add \
+                            + "objectGUID: 24e2ca70-b093-4ae8-84c0-2d7ac652a1b8\n"
+
             # These bits of LDIF are supplied when the Schema object is created
-            self.ldb.add_ldif(self.schema_dn_add)
+            self.ldb.add_ldif(schema_dn_add)
             self.ldb.modify_ldif(self.schema_dn_modify)
             self.ldb.add_ldif(self.schema_data)
         except:
