de2279df55dea426f73c485917676976f7a6e28c
Volker Lendecke <vl@samba.org>
2017-09-17 14:28:44 -0700

    rpcclient3: Factor out cli_rpc_pipe_open_bind_schannel()
    This will be used for the "fast path" to netlogon when we already have
    credentials.
    
    This slightly widens the area of code covered by the netlogon_creds
    lock: cli_rpc_pipe_open is now also covered by the lock.
    
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- source3/rpc_client/cli_pipe.h
+++ source3/rpc_client/cli_pipe.h
@@ -99,6 +99,12 @@ NTSTATUS cli_rpc_pipe_open_generic_auth(struct cli_state *cli,
 					const char *password,
 					struct rpc_pipe_client **presult);
 
+NTSTATUS cli_rpc_pipe_open_bind_schannel(
+	struct cli_state *cli,
+	const struct ndr_interface_table *table,
+	enum dcerpc_transport_t transport,
+	struct netlogon_creds_cli_context *netlogon_creds,
+	struct rpc_pipe_client **_rpccli);
 NTSTATUS cli_rpc_pipe_open_schannel_with_creds(struct cli_state *cli,
 					       const struct ndr_interface_table *table,
 					       enum dcerpc_transport_t transport,
