8a148193da0ba801f5302a6a2285b39ab6f4d247
Gary Lockyer <gary@catalyst.net.nz>
2017-09-25 14:16:09 +1300

    heimdal_build: provide a prototype with the dummy afs header-only function stubs
    We do not do AFS in Samba
    
    Signed-off-by: Gary Lockyer <gary@catalyst.net.nz>
    Reviewed-by: Douglas Bagnall <douglas.bagnall@catalyst.net.nz>

--- source4/heimdal_build/kafs.h
+++ source4/heimdal_build/kafs.h
@@ -1,16 +1,29 @@
+int k_hasafs (void);
+
 int k_hasafs (void) {
 	return 0;
 };
 
+int krb_afslog (const char *cell, const char *realm);
+
 int krb_afslog (const char *cell, const char *realm) {
 	return 0;
 };
+int k_unlog (void);
+
 int k_unlog (void) {
 	return 0;
 };
+int k_setpag (void);
+
 int k_setpag (void) {
 	return 0;
 };
+krb5_error_code krb5_afslog (krb5_context context,
+				 krb5_ccache id,
+				 const char *cell,
+			     krb5_const_realm realm);
+
 krb5_error_code krb5_afslog (krb5_context context,
 				 krb5_ccache id, 
 				 const char *cell,
