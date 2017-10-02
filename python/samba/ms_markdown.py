d66cbca4e1c4d68a40fb117efc3e7e357690fc5d
Garming Sam <garming@catalyst.net.nz>
2017-10-03 10:01:30 +1300

    adprep: Add the LDF data needed to upgrade to 2012R2 schema
    This patch adds the LDF files corresponding to the changes that the
    Windows Adprep.exe tool makes when upgrading a AD schema to Windows
    2012R2.
    
    This is based on information Microsoft has made public on github
    (Schema-Updates.md - see the README.txt for more details).
    
    The LDF files 48-56 are for upgrading to Windows Server 2012, and 57-69
    are for Windows Server 2012 R2.
    
    Unfortunately, the raw LDF information from Microsoft wasn't enough to
    get the schema working. The .diff files contain changes we needed to
    make on top of the raw LDF content from Microsoft.
    
    The basic steps to regenerate the .LDF files are documented in the
    README.txt file. The files used to generate the .LDF files are in the
    WindowsServerDocs/ sub-directory. (The .LDF generation is done at runtime
    during provision).
    
    Signed-off-by: Garming Sam <garming@catalyst.net.nz>
    Signed-off-by: Tim Beale <timbeale@catalyst.net.nz>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

---
+++ python/samba/ms_markdown.py
@@ -0,0 +1,71 @@
+# Create schema.ldif from Github markdown
+#
+# Each LDF section in the markdown file then gets written to a corresponding
+# .LDF output file.
+#
+# Copyright (C) Andrew Bartlett 2017
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Generate LDIF from Github documentation."""
+
+import re
+import os
+import markdown
+import xml.etree.ElementTree as ET
+
+def innertext(tag):
+    return (tag.text or '') + \
+            ''.join(innertext(e) for e in tag) + \
+            (tag.tail or '')
+
+def read_ms_markdown(in_file, out_folder):
+    """Read Github documentation-derived schema files."""
+
+    with open(in_file) as update_file:
+        # Remove any comments from the raw LDF files
+        html = markdown.markdown(re.sub(r'(?m)^# .*\n?', '', update_file.read()),
+                                 output_format='xhtml')
+
+    tree = ET.fromstring('<root>' + html + '</root>')
+
+    ldf = None
+    try:
+        for node in tree:
+            if node.tag == 'h3':
+                if ldf is not None:
+                    ldf.close()
+
+                out_path = os.path.join(out_folder, innertext(node).strip())
+                ldf = open(out_path, 'w')
+            elif node.tag == 'p' and ldf is not None:
+                ldf.write(innertext(node).replace('```', '') + '\n')
+    finally:
+        if ldf is not None:
+            ldf.close()
+
+if __name__ == '__main__':
+    import sys
+
+    out_folder = ''
+
+    if len(sys.argv) == 0:
+        print >>sys.stderr, "Usage: %s <Schema-Update.md> [<output folder>]" % (sys.argv[0])
+        sys.exit(1)
+
+    in_file = sys.argv[1]
+    if len(sys.argv) > 2:
+        out_folder = sys.argv[2]
+
+    read_ms_markdown(in_file, out_folder)
---
+++ source4/setup/adprep/README.txt
@@ -0,0 +1,23 @@
+The Schema updates in this directory were based on information retrieved
+from Microsoft's Github:
+
+file: WindowsServerDocs/identity/ad-ds/deploy/Schema-Updates.md
+project: https://github.com/MicrosoftDocs/windowsserverdocs/
+
+./WindowsServerDocs/Schema-Updates.md is the version of the file we used (last updated
+Jun 1, 2017, commit SHA f79755b75d2810b8a4).
+
+License files (LICENSE and LICENSE-CODE) have also been included in the ./gen
+directory for reference.
+
+The ms_markdown.py script was then used to produce the .ldf files.
+
+However, this schema didn't work. The ./gen/*.diff files are the changes
+we made on top of this to get the schema working on Samba. If you are
+re-generating the .ldf files, to apply the patches, use:
+
+for p in `ls gen/*.diff` ; do patch -p 1 < $p ; done
+
+All this is handled at runtime in the provision code, so that we do
+not store patched generated files in git (an alternative would have
+been to patch the original markdown).
