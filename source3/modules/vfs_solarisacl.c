d774aeba5e04eacb98cf938d356e4cc502caa2e7
Jorge Schrauwen via samba-technical <samba-technical@lists.samba.org>
2017-10-03 19:55:28 +0200

    vfs_solarisacl: fix build for samba 4.7 and up
    Bug: https://bugzilla.samba.org/show_bug.cgi?id=13049
    
    Signed-off-by: Jorge Schrauwen <sjorge@blackdot.be>
    Reviewed-by: Ralph Boehme <slow@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/modules/vfs_solarisacl.c
+++ source3/modules/vfs_solarisacl.c
@@ -306,7 +306,7 @@ int solarisacl_sys_acl_set_fd(vfs_handle_struct *handle,
  * check is considered unnecessary. --- Agreed? XXX
  */
 int solarisacl_sys_acl_delete_def_file(vfs_handle_struct *handle,
-				struct smb_filename *smb_fname)
+				const struct smb_filename *smb_fname)
 {
 	SMB_ACL_T smb_acl;
 	int ret = -1;
--- source3/modules/vfs_solarisacl.h
+++ source3/modules/vfs_solarisacl.h
@@ -41,7 +41,7 @@ int solarisacl_sys_acl_set_fd(vfs_handle_struct *handle,
 int solarisacl_sys_acl_delete_def_file(vfs_handle_struct *handle,
 				const struct smb_filename *smb_fname);
 
-NTSTATUS vfs_solarisacl_init(void);
+NTSTATUS vfs_solarisacl_init(TALLOC_CTX *);
 
 #endif
 
