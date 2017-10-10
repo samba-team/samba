8e958708c348023d09767cb5432a0c62d4ebdded
Ralph Boehme <slow@samba.org>
2017-10-10 16:15:49 +0200

    vfs_fruit: pass path to ad_convert
    This will be needed in a later commit when converting xattrs in sidecar
    AppleDouble files.
    
    Bug: https://bugzilla.samba.org/show_bug.cgi?id=13076
    
    Signed-off-by: Ralph Boehme <slow@samba.org>
    Reviewed-by: Volker Lendecke <vl@samba.org>
    (cherry picked from commit 386249eea3a13303744dbab88480e80790138329)

--- source3/modules/vfs_fruit.c
+++ source3/modules/vfs_fruit.c
@@ -924,7 +924,9 @@ static bool ad_unpack(struct adouble *ad, const size_t nentries,
  * @return -1 in case an error occured, 0 if no conversion was done, 1
  * otherwise
  **/
-static int ad_convert(struct adouble *ad, int fd)
+static int ad_convert(struct adouble *ad,
+		      const char *path,
+		      int fd)
 {
 	int rc = 0;
 	char *map = MAP_FAILED;
@@ -1219,7 +1221,7 @@ static ssize_t ad_read_rsrc_adouble(struct adouble *ad,
 	 * there is lost.
 	 */
 
-	ret = ad_convert(ad, ad->ad_fd);
+	ret = ad_convert(ad, path, ad->ad_fd);
 	if (ret != 0) {
 		DBG_WARNING("Failed to convert [%s]\n", path);
 		return len;
