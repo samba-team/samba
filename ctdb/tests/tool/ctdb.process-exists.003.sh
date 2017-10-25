88752411daf64af59bd00c60c36ca1ced3c044f0
Martin Schwenke <martin@meltin.net>
2017-10-25 12:15:23 +1100

    ctdb-tests: Process-exists unit tests should wait until PID is registered
    Otherwise the client registration can race with the check in the test.
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=13097
    
    Signed-off-by: Martin Schwenke <martin@meltin.net>
    Reviewed-by: Amitay Isaacs <amitay@gmail.com>
    
    Autobuild-User(master): Amitay Isaacs <amitay@samba.org>
    Autobuild-Date(master): Thu Oct 26 13:32:24 CEST 2017 on sn-devel-144
    
    (cherry picked from commit 0e8b781e0740310d251bf1fa7db7a467d4f7f9b5)

--- ctdb/tests/tool/ctdb.process-exists.003.sh
+++ ctdb/tests/tool/ctdb.process-exists.003.sh
@@ -16,6 +16,8 @@ srvid="0xaebbccdd12345678"
 dummy_client -d INFO -s "$ctdbd_socket" -n 10 -S "$srvid" &
 pid=$!
 
+wait_until 10 $CTDB process-exists "$pid"
+
 srvid2="0x1234567812345678"
 required_result 1 "PID $pid with SRVID $srvid2 does not exist"
 simple_test "$pid" "$srvid2"
