5b414d94fe7aa05d1fb3ca8ee35c346ca6797bae
Andreas Schneider <asn@samba.org>
2017-10-25 19:39:34 +0200

    s3:vfs_glusterfs: Fix a double free in vfs_gluster_getwd()
    Found by cppcheck.
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=13100
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>
    
    (cherry picked from commit 16389bed0773952ca563b7bf1fecc2a737587257)

--- source3/modules/vfs_glusterfs.c
+++ source3/modules/vfs_glusterfs.c
@@ -1093,8 +1093,9 @@ static char *vfs_gluster_getwd(struct vfs_handle_struct *handle)
 	}
 
 	ret = glfs_getcwd(handle->data, cwd, PATH_MAX - 1);
-	if (ret == 0) {
+	if (ret == NULL) {
 		free(cwd);
+		return NULL;
 	}
 	return ret;
 }
