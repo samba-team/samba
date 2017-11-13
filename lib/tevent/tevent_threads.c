d4f07b4e2289ace7f5a548d143f59c532dfc31cc
Stefan Metzmacher <metze@samba.org>
2017-11-13 23:29:42 +0100

    Revert "tevent: Fix a race condition"
    This reverts commit 6a43b1b17902c8fbc319e13f31f6c9177f38371c.
    
    This will reapplied shortly in the correct order relative to
    other backports.
    
    Signed-off-by: Stefan Metzmacher <metze@samba.org>

--- lib/tevent/tevent_threads.c
+++ lib/tevent/tevent_threads.c
@@ -434,7 +434,7 @@ void _tevent_threaded_schedule_immediate(struct tevent_threaded_context *tctx,
 {
 #ifdef HAVE_PTHREAD
 	struct tevent_context *ev;
-	int ret, wakeup_fd;
+	int ret;
 
 	ret = pthread_mutex_lock(&tctx->event_ctx_mutex);
 	if (ret != 0) {
@@ -442,7 +442,6 @@ void _tevent_threaded_schedule_immediate(struct tevent_threaded_context *tctx,
 	}
 
 	ev = tctx->event_ctx;
-	wakeup_fd = tctx->wakeup_fd;
 
 	ret = pthread_mutex_unlock(&tctx->event_ctx_mutex);
 	if (ret != 0) {
@@ -490,7 +489,7 @@ void _tevent_threaded_schedule_immediate(struct tevent_threaded_context *tctx,
 	 * than a noncontended one. So I'd opt for the lower footprint
 	 * initially. Maybe we have to change that later.
 	 */
-	tevent_common_wakeup_fd(wakeup_fd);
+	tevent_common_wakeup_fd(tctx->wakeup_fd);
 #else
 	/*
 	 * tevent_threaded_context_create() returned NULL with ENOSYS...
