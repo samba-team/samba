8d90f2a03e58f34f36d345d31c5ca6090d37ff54
Andreas Schneider <asn@samba.org>
2018-01-11 11:00:43 +0100

    s3:test: Always validate the join after changing the secret
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Signed-off-by: Ralph Boehme <slow@samba.org>
    Pair-Programmed-With: Ralph Boehme <slow@samba.org>

--- source3/script/tests/test_net_cred_change.sh
+++ source3/script/tests/test_net_cred_change.sh
@@ -9,8 +9,9 @@ fi
 
 incdir=`dirname $0`/../../../testprogs/blackbox
 . $incdir/subunit.sh
-testit "first change" $VALGRIND $BINDIR/wbinfo -c || failed=`expr $failed + 1`
-testit "first join" $VALGRIND $BINDIR/net rpc testjoin $@ || failed=`expr $failed + 1`
-testit "second change" $VALGRIND $BINDIR/wbinfo -c || failed=`expr $failed + 1`
+testit "1: change machine secret" $VALGRIND $BINDIR/wbinfo --change-secret || failed=`expr $failed + 1`
+testit "1: validate secret" $VALGRIND $BINDIR/net rpc testjoin $@ || failed=`expr $failed + 1`
+testit "2: change machine secret" $VALGRIND $BINDIR/wbinfo --change-secret || failed=`expr $failed + 1`
+testit "2: validate secret" $VALGRIND $BINDIR/net rpc testjoin $@ || failed=`expr $failed + 1`
 
 testok $0 $failed
