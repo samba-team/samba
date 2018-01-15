9fef5d1891e6c1aebea29fbfbb90e77631b7836c
Stefan Metzmacher <metze@samba.org>
2018-01-15 14:30:48 +0100

    winbindd: add missing can_do_ncacn_ip_tcp initialisation
    Bug: https://bugzilla.samba.org/show_bug.cgi?id=13232
    
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Ralph Boehme <slow@samba.org>

--- source3/winbindd/winbindd_util.c
+++ source3/winbindd/winbindd_util.c
@@ -275,6 +275,8 @@ static NTSTATUS add_trusted_domain(const char *domain_name,
 		}
 	}
 
+	domain->can_do_ncacn_ip_tcp = domain->active_directory;
+
 	/* Link to domain list */
 	DLIST_ADD_END(_domain_list, domain);
 
