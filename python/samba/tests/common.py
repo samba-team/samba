c56eb49119117a1a06afb0a76630ae5c7a1ca30c
Stefan Metzmacher <metze@samba.org>
2018-01-30 11:09:55 +0100

    python:tests: add test_dsdb_Dn_sorted() to "samba.tests.common"
    Failing until dsdb_Dn implements the correct __cmp__() function.
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=13228
    
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Ralph Boehme <slow@samba.org>

--- python/samba/tests/common.py
+++ python/samba/tests/common.py
@@ -39,3 +39,27 @@ class CommonTests(samba.tests.TestCaseInTempDir):
         self.assertEquals(dn2.binary, "0000000D")
         self.assertEquals(13, dn2.get_binary_integer())
         os.unlink(url)
+
+    def test_dsdb_Dn_sorted(self):
+        url = self.tempdir + "/test_dsdb_Dn_sorted.ldb"
+        sam = samba.Ldb(url=url)
+        try:
+            dn1 = dsdb_Dn(sam, "B:8:0000000D:<GUID=b3f0ec29-17f4-452a-b002-963e1909d101>;OU=dn1,DC=samba,DC=example,DC=com")
+            dn2 = dsdb_Dn(sam, "B:8:0000000C:<GUID=b3f0ec29-17f4-452a-b002-963e1909d101>;OU=dn1,DC=samba,DC=example,DC=com")
+            dn3 = dsdb_Dn(sam, "B:8:0000000F:<GUID=00000000-17f4-452a-b002-963e1909d101>;OU=dn3,DC=samba,DC=example,DC=com")
+            dn4 = dsdb_Dn(sam, "B:8:00000000:<GUID=ffffffff-17f4-452a-b002-963e1909d101>;OU=dn4,DC=samba,DC=example,DC=com")
+            dn5 = dsdb_Dn(sam, "<GUID=ffffffff-27f4-452a-b002-963e1909d101>;OU=dn5,DC=samba,DC=example,DC=com")
+            dn6 = dsdb_Dn(sam, "<GUID=00000000-27f4-452a-b002-963e1909d101>;OU=dn6,DC=samba,DC=example,DC=com")
+            unsorted_links14 = [dn1,dn2,dn3,dn4]
+            sorted_vals14 = [str(dn) for dn in sorted(unsorted_links14)]
+            self.assertEquals(sorted_vals14[0], str(dn3))
+            self.assertEquals(sorted_vals14[1], str(dn2))
+            self.assertEquals(sorted_vals14[2], str(dn1))
+            self.assertEquals(sorted_vals14[3], str(dn4))
+            unsorted_links56 = [dn5,dn6]
+            sorted_vals56 = [str(dn) for dn in sorted(unsorted_links56)]
+            self.assertEquals(sorted_vals56[0], str(dn6))
+            self.assertEquals(sorted_vals56[1], str(dn5))
+        finally:
+            del sam
+            os.unlink(url)
