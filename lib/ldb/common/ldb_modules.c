3450dd6a50d1d08f49de5ff98f2ce20f31081636
Timur I. Bakeyev <timur@iXsystems.com>
2018-02-14 02:31:58 +0100

    Don't load LDB_MODULESDIR as a module file
    We are setting modules directory here(LDB_MODULESDIR) so treat it this
    way, no need to attempt to load it as a module file.
    
    Signed-off-by: Timur I. Bakeyev <timur@iXsystems.com>
    Reviewed-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Uri Simchoni <uri@samba.org>

--- lib/ldb/common/ldb_modules.c
+++ lib/ldb/common/ldb_modules.c
@@ -1103,7 +1103,7 @@ static int ldb_modules_load_dir(const char *modules_dir, const char *version)
 */
 void ldb_set_modules_dir(struct ldb_context *ldb, const char *path)
 {
-	int ret = ldb_modules_load_path(path, LDB_VERSION);
+	int ret = ldb_modules_load_dir(path, LDB_VERSION);
 	if (ret != LDB_SUCCESS) {
 		ldb_asprintf_errstring(ldb, "Failed to load modules from: %s\n", path);
 	}
