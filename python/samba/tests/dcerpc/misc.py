c2c7781b238ffd3793c2bb60db8b51dc7b193945
Noel Power <noel.power@suse.com>
2018-02-28 14:40:26 +0000

    samba python tests: Ensure GUIDTests run with python2/python3
    Signed-off-by: Noel Power <noel.power@suse.com>

--- python/samba/tests/dcerpc/misc.py
+++ python/samba/tests/dcerpc/misc.py
@@ -21,8 +21,9 @@ from samba.dcerpc import misc
 import samba.tests
 from samba.compat import PY3
 
-text1 = "76f53846-a7c2-476a-ae2c-20e2b80d7b34"
-text2 = "344edffa-330a-4b39-b96e-2c34da52e8b1"
+text1_b = b"76f53846-a7c2-476a-ae2c-20e2b80d7b34"
+text2_b = b"344edffa-330a-4b39-b96e-2c34da52e8b1"
+text1_s = text1_b.decode('utf8')
 
 
 if PY3:
@@ -34,23 +35,23 @@ if PY3:
 class GUIDTests(samba.tests.TestCase):
 
     def test_str(self):
-        guid = misc.GUID(text1)
-        self.assertEquals(text1, str(guid))
+        guid = misc.GUID(text1_b)
+        self.assertEquals(text1_s, str(guid))
 
     def test_repr(self):
-        guid = misc.GUID(text1)
-        self.assertEquals("GUID('%s')" % text1, repr(guid))
+        guid = misc.GUID(text1_b)
+        self.assertEquals("GUID('%s')" % text1_s, repr(guid))
 
     def test_compare_different(self):
-        guid1 = misc.GUID(text1)
-        guid2 = misc.GUID(text2)
+        guid1 = misc.GUID(text1_b)
+        guid2 = misc.GUID(text2_b)
         self.assertFalse(guid1 == guid2)
         self.assertGreater(guid1, guid2)
         self.assertTrue(cmp(guid1, guid2) > 0)
 
     def test_compare_same(self):
-        guid1 = misc.GUID(text1)
-        guid2 = misc.GUID(text1)
+        guid1 = misc.GUID(text1_b)
+        guid2 = misc.GUID(text1_b)
         self.assertTrue(guid1 == guid2)
         self.assertEquals(guid1, guid2)
         self.assertEquals(0, cmp(guid1, guid2))
@@ -59,14 +60,14 @@ class GUIDTests(samba.tests.TestCase):
 class PolicyHandleTests(samba.tests.TestCase):
 
     def test_init(self):
-        x = misc.policy_handle(text1, 1)
+        x = misc.policy_handle(text1_s, 1)
         self.assertEquals(1, x.handle_type)
-        self.assertEquals(text1, str(x.uuid))
+        self.assertEquals(text1_s, str(x.uuid))
 
     def test_repr(self):
-        x = misc.policy_handle(text1, 42)
-        self.assertEquals("policy_handle(%d, '%s')" % (42, text1), repr(x))
+        x = misc.policy_handle(text1_s, 42)
+        self.assertEquals("policy_handle(%d, '%s')" % (42, text1_s), repr(x))
 
     def test_str(self):
-        x = misc.policy_handle(text1, 42)
-        self.assertEquals("%d, %s" % (42, text1), str(x))
+        x = misc.policy_handle(text1_s, 42)
+        self.assertEquals("%d, %s" % (42, text1_s), str(x))
