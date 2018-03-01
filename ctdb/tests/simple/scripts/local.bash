ee9051b7f7884a3beead841ef2a7684d8ab48784
Martin Schwenke <martin@meltin.net>
2018-03-01 15:39:44 +1100

    ctdb-tests: New directory for simple test state
    Signed-off-by: Martin Schwenke <martin@meltin.net>
    Reviewed-by: Amitay Isaacs <amitay@gmail.com>

--- ctdb/tests/simple/scripts/local.bash
+++ ctdb/tests/simple/scripts/local.bash
@@ -1,3 +1,11 @@
+if [ -z "$TEST_VAR_DIR" ] ; then
+	die "TEST_VAR_DIR unset"
+fi
+
+export SIMPLE_TESTS_VAR_DIR="${TEST_VAR_DIR}/simple"
+# Don't remove old directory since state is retained between tests
+mkdir -p "$SIMPLE_TESTS_VAR_DIR"
+
 # onnode needs CTDB_BASE to be set when run in-tree
 if [ -z "$CTDB_BASE" ] ; then
     export CTDB_BASE="$TEST_SUBDIR"
