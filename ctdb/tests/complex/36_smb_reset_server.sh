9e954bcbf43d67a18ee55f84cda0b09028f96b92
Martin Schwenke <martin@meltin.net>
2018-03-08 11:49:56 +1100

    ctdb-tests: Don't use nc -d or -w options
    nmap-ncat is used in some distributions to replace netcat.  It has a
    different meaning for these options.
    
    We can get the same effect as the current combination of -d and -w by
    piping a sleep process to nc.  Subsequent use of $! works because it
    gets the last process in pipeline.
    
    Note that redirecting from /dev/null doesn't work with some versions
    of nc.  They just exit when they get EOF.
    
    Signed-off-by: Martin Schwenke <martin@meltin.net>
    Reviewed-by: Amitay Isaacs <amitay@gmail.com>
    
    Autobuild-User(master): Martin Schwenke <martins@samba.org>
    Autobuild-Date(master): Fri Mar  9 12:24:13 CET 2018 on sn-devel-144

--- ctdb/tests/complex/36_smb_reset_server.sh
+++ ctdb/tests/complex/36_smb_reset_server.sh
@@ -52,7 +52,7 @@ sleep_for 5
 
 echo "Connecting to node ${test_node} on IP ${test_ip}:${test_port} with nc..."
 
-nc -d -w $(($monitor_interval * 4)) $test_ip $test_port &
+sleep $((monitor_interval * 4)) | nc $test_ip $test_port &
 nc_pid=$!
 ctdb_test_exit_hook_add "kill $nc_pid >/dev/null 2>&1"
 
