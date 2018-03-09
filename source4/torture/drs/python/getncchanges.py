699d1c14183f9c44c474fb11340db2234632d0f2
Noel Power <noel.power@suse.com>
2018-03-09 14:07:31 +0000

    lib/tdb: convert print func to be py2/py3 compatible
    Signed-off-by: Noel Power <noel.power@suse.com>

--- source4/torture/drs/python/getncchanges.py
+++ source4/torture/drs/python/getncchanges.py
@@ -297,7 +297,7 @@ class DrsReplicaSyncIntegrityTestCase(drs_base.DrsBaseTestCase):
                 # we receive the parents before the child
                 self.assertFalse(get_anc, "Unknown parent for object %s" % dn)
 
-                print("Unknown parent for %s - try GET_ANC" % d)
+                print("Unknown parent for %s - try GET_ANC" % dn)
 
                 # try the same thing again with the GET_ANC flag set this time
                 return self.repl_get_next(get_anc=True, get_tgt=get_tgt,
