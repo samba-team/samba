b526968bcf920c895a24d4bc447716721132166d
Martin Schwenke <martin@meltin.net>
2018-03-20 20:38:13 +1100

    ctdb-tests: 20.multipathd.sh 80 column fixups
    Signed-off-by: Martin Schwenke <martin@meltin.net>
    Reviewed-by: Amitay Isaacs <amitay@gmail.com>

--- ctdb/tests/eventscripts/scripts/20.multipathd.sh
+++ ctdb/tests/eventscripts/scripts/20.multipathd.sh
@@ -1,18 +1,21 @@
 setup_multipathd ()
 {
+	_failures=""
+	_devices=""
 	for i ; do
 		case "$i" in
 		\!*)
 			_t="${i#!}"
 			echo "Marking ${_t} as having no active paths"
-			FAKE_MULTIPATH_FAILURES="${FAKE_MULTIPATH_FAILURES}${FAKE_MULTIPATH+FAILURES:+ }${_t}"
+			_failures="${_failures}${_failures:+ }${_t}"
 		;;
 		*)
 			_t="$i"
 		esac
-		CTDB_MONITOR_MPDEVICES="${CTDB_MONITOR_MPDEVICES}${CTDB_MONITOR_MPDEVICES:+ }${_t}"
+		_devices="${_devices}${_devices:+ }${_t}"
 	done
 
-	export CTDB_MONITOR_MPDEVICES FAKE_MULTIPATH_FAILURES
+	export CTDB_MONITOR_MPDEVICES="$_devices"
+	export FAKE_MULTIPATH_FAILURES="$_failures"
 	export FAKE_SLEEP_FORCE=0.1
 }
