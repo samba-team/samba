e4a4532e5c417146da3a30c00d74b2032e16dc66
Martin Schwenke <martin@meltin.net>
2018-03-21 18:43:35 +1100

    ctdb-tests: Make date handling consistent in 00.ctdb tests
    This duplicates the filter function but that will be fixed later.
    
    Signed-off-by: Martin Schwenke <martin@meltin.net>
    Reviewed-by: Amitay Isaacs <amitay@gmail.com>

--- ctdb/tests/eventscripts/00.ctdb.init.004.sh
+++ ctdb/tests/eventscripts/00.ctdb.init.004.sh
@@ -8,15 +8,22 @@ setup_ctdb
 
 FAKE_TDBTOOL_SUPPORTS_CHECK="yes"
 
+result_filter ()
+{
+	_date="[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]"
+	_time="[0-9][0-9][0-9][0-9][0-9][0-9]"
+	_nanos="[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]"
+	_date_time="${_date}\.${_time}\.${_nanos}"
+	sed -e "s|\.${_date_time}\.|.DATE.TIME.|"
+}
+
 db="${CTDB_DBDIR}/foo.tdb.0"
 touch "$db"
 FAKE_TDB_IS_OK="no"
 
-FAKE_DATE_OUTPUT="19690818.103000.000000001"
-
 ok <<EOF
 WARNING: database ${db} is corrupted.
- Moving to backup ${db}.${FAKE_DATE_OUTPUT}.corrupt for later analysis.
+ Moving to backup ${db}.DATE.TIME.corrupt for later analysis.
 EOF
 
 simple_test
