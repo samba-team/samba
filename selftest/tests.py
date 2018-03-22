5fd49e895637ca491c7d716c3c18033129211f34
Noel Power <noel.power@suse.com>
2018-03-22 12:39:43 +0000

    python selftest: enable samba.tests.s3windb to run with py3
    Signed-off-by: Noel Power <noel.power@suse.com>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Douglas Bagnall <douglas.bagnall@catalyst.net.nz>

--- selftest/tests.py
+++ selftest/tests.py
@@ -78,7 +78,7 @@ planpythontestsuite("ad_dc_ntvfs:local", "samba.tests.messaging",
 planpythontestsuite("none", "samba.tests.s3param", py3_compatible=True)
 planpythontestsuite("none", "samba.tests.s3passdb", py3_compatible=True)
 planpythontestsuite("none", "samba.tests.s3registry", py3_compatible=True)
-planpythontestsuite("none", "samba.tests.s3windb")
+planpythontestsuite("none", "samba.tests.s3windb", py3_compatible=True)
 planpythontestsuite("none", "samba.tests.s3idmapdb")
 planpythontestsuite("none", "samba.tests.samba3sam")
 planpythontestsuite(
