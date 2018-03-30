76babda4ef13111e402fcfac10d6f9c3d7bb75e9
Volker Lendecke <vl@samba.org>
2018-03-30 12:19:24 -0500

    groupdb: Fix CID 1167984 Ignoring number of bytes read
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: Stefan Metzmacher <metze@samba.org>

--- source3/groupdb/mapping.c
+++ source3/groupdb/mapping.c
@@ -237,9 +237,13 @@ int smb_create_group(const char *unix_group, gid_t *new_gid)
 
 		if (fd != 0) {
 			fstring output;
+			ssize_t nread;
 
 			*new_gid = 0;
-			if (read(fd, output, sizeof(output)) > 0) {
+
+			nread = read(fd, output, sizeof(output)-1);
+			if (nread > 0) {
+				output[nread] = '\0';
 				*new_gid = (gid_t)strtoul(output, NULL, 10);
 			}
 
