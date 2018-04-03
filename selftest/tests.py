a35dfa017ba47f98a42a14adc70fd5430ca73e0e
Joe Guo <joeg@catalyst.net.nz>
2018-04-03 15:13:09 +1200

    selftest: enable py3 for samba.tests.dcerpc.array
    No change needed.
    
    Signed-off-by: Joe Guo <joeg@catalyst.net.nz>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Douglas Bagnall <douglas.bagnall@catalyst.net.nz>

--- selftest/tests.py
+++ selftest/tests.py
@@ -70,7 +70,7 @@ planpythontestsuite("none", "samba.tests.password_quality", py3_compatible=True)
 planpythontestsuite("none", "samba.tests.strings")
 planpythontestsuite("none", "samba.tests.netcmd")
 planpythontestsuite("none", "samba.tests.dcerpc.rpc_talloc", py3_compatible=True)
-planpythontestsuite("none", "samba.tests.dcerpc.array")
+planpythontestsuite("none", "samba.tests.dcerpc.array", py3_compatible=True)
 planpythontestsuite("none", "samba.tests.dcerpc.string")
 planpythontestsuite("none", "samba.tests.hostconfig")
 planpythontestsuite("ad_dc_ntvfs:local", "samba.tests.messaging",
