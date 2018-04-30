fa179595865ace92999b6349dba86ba6d543ef32
Noel Power <noel.power@suse.com>
2018-04-30 15:47:05 +0100

    s4/selftest: Enable samba.tests.samba_tool.sites for py3

--- source4/selftest/tests.py
+++ source4/selftest/tests.py
@@ -627,7 +627,7 @@ planpythontestsuite("none", "samba.tests.samba_tool.help", py3_compatible=True)
 planpythontestsuite("ad_dc_ntvfs:local", "samba.tests.samba_tool.passwordsettings")
 
 # Run these against chgdcpass to share the runtime load
-planpythontestsuite("chgdcpass:local", "samba.tests.samba_tool.sites")
+planpythontestsuite("chgdcpass:local", "samba.tests.samba_tool.sites", py3_compatible=True)
 planpythontestsuite("chgdcpass:local", "samba.tests.samba_tool.dnscmd")
 
 planpythontestsuite("ad_dc_ntvfs:local", "samba.tests.dcerpc.rpcecho", py3_compatible=True)
