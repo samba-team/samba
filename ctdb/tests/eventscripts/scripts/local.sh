ff3d0e70b69d47e62478b3f5a6b8bc3957a01444
Martin Schwenke <martin@meltin.net>
2018-05-02 12:37:21 +1000

    ctdb-tests: Add setup of ctdb.conf database directory settings
    Signed-off-by: Martin Schwenke <martin@meltin.net>
    Reviewed-by: Amitay Isaacs <amitay@gmail.com>

--- ctdb/tests/eventscripts/scripts/local.sh
+++ ctdb/tests/eventscripts/scripts/local.sh
@@ -102,6 +102,12 @@ setup_dbdir ()
 	export CTDB_DBDIR="${CTDB_DBDIR_BASE}/volatile"
 	export CTDB_DBDIR_PERSISTENT="${CTDB_DBDIR_BASE}/persistent"
 	export CTDB_DBDIR_STATE="${CTDB_DBDIR_BASE}/state"
+	cat >>"${CTDB_BASE}/ctdb.conf" <<EOF
+[database]
+	volatile database directory = ${CTDB_DBDIR}
+	persistent database directory = ${CTDB_DBDIR_PERSISTENT}
+	state database directory = ${CTDB_DBDIR_STATE}
+EOF
 	mkdir -p "$CTDB_DBDIR"
 	mkdir -p "$CTDB_DBDIR_PERSISTENT"
 	mkdir -p "$CTDB_DBDIR_STATE"
