7e4f7778a786d569cde9a6ce83ca26df6555cfd0
Noel Power <noel.power@suse.com>
2018-05-03 20:31:07 +0100

    s4/selftest: enable samba.tests.samba_tool.user_virtualCryptSHA for py3

--- source4/selftest/tests.py
+++ source4/selftest/tests.py
@@ -607,7 +607,7 @@ planpythontestsuite("ad_dc_ntvfs:local", "samba.tests.samba_tool.processes", py3
 planpythontestsuite("ad_dc_ntvfs:local", "samba.tests.samba_tool.user", py3_compatible=True)
 planpythontestsuite("ad_dc_ntvfs:local", "samba.tests.samba_tool.user_wdigest",  py3_compatible=True)
 planpythontestsuite("ad_dc:local", "samba.tests.samba_tool.user", py3_compatible=True)
-planpythontestsuite("ad_dc:local", "samba.tests.samba_tool.user_virtualCryptSHA")
+planpythontestsuite("ad_dc:local", "samba.tests.samba_tool.user_virtualCryptSHA", py3_compatible=True)
 planpythontestsuite("chgdcpass:local", "samba.tests.samba_tool.user_check_password_script", py3_compatible=True)
 planpythontestsuite("ad_dc_ntvfs:local", "samba.tests.samba_tool.group", py3_compatible=True)
 planpythontestsuite("ad_dc_ntvfs:local", "samba.tests.samba_tool.ou", py3_compatible=True)
