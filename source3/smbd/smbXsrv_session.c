15d2f4f8178e9cd10deaace19268b0659806340a
Mathieu Parent <math.parent@gmail.com>
2018-05-04 22:24:25 +0200

    Fix spelling s/unsuported/unsupported/
    Signed-off-by: Mathieu Parent <math.parent@gmail.com>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Garming Sam <garming@catalyst.net.nz>

--- source3/smbd/smbXsrv_session.c
+++ source3/smbd/smbXsrv_session.c
@@ -1918,7 +1918,7 @@ static int smbXsrv_session_global_traverse_fn(struct db_record *rec, void *data)
 
 	if (global_blob.version != SMBXSRV_VERSION_0) {
 		DEBUG(1,("Invalid record in smbXsrv_session_global.tdb:"
-			 "key '%s' unsuported version - %d\n",
+			 "key '%s' unsupported version - %d\n",
 			 hex_encode_talloc(frame, key.dptr, key.dsize),
 			 (int)global_blob.version));
 		goto done;
