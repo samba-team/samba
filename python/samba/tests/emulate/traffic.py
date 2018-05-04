12a2bdab56a73fb4708fceb97ab0d735d832acc2
Noel Power <noel.power@suse.com>
2018-05-04 11:49:23 +0100

    python/samba/tests: Ensure StringIO usage is py2/py3 compatible
    Signed-off-by: Noel Power <noel.power@suse.com>

--- python/samba/tests/emulate/traffic.py
+++ python/samba/tests/emulate/traffic.py
@@ -16,7 +16,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # from pprint import pprint
-from cStringIO import StringIO
+from samba.compat import StringIO
 
 import samba.tests
 
