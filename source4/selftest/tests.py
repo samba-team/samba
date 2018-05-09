b7909eb29dc4be813561d84c2205ba38d5c06109
Noel Power <noel.power@suse.com>
2018-05-09 11:37:42 +0100

    s4/selftest: enable samba.tests.password_hash_ldap for python3
    Signed-off-by: Noel Power <noel.power@suse.com>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- source4/selftest/tests.py
+++ source4/selftest/tests.py
@@ -752,11 +752,11 @@ planoldpythontestsuite("ad_dc_ntvfs:local",
 # fl2003dc:local functional level < 2008, gpg keys not available
 planoldpythontestsuite("fl2003dc:local",
                        "samba.tests.password_hash_fl2003",
-                       extra_args=['-U"$USERNAME%$PASSWORD"'])
+                       extra_args=['-U"$USERNAME%$PASSWORD"'], py3_compatible=True)
 # ad_dc: wDigest values over ldap
 planoldpythontestsuite("ad_dc",
                        "samba.tests.password_hash_ldap",
-                       extra_args=['-U"$USERNAME%$PASSWORD"'])
+                       extra_args=['-U"$USERNAME%$PASSWORD"'], py3_compatible=True)
 planoldpythontestsuite("ad_dc:local",
                        "samba.tests.domain_backup",
                        extra_args=['-U"$USERNAME%$PASSWORD"'])
