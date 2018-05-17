7afa2d93260bdf74fec108ceca4e6add357454c0
Noel Power <noel.power@suse.com>
2018-05-17 14:51:40 +0100

    s4/selftest: enable samba.tests.samba_tool.passwordsetting for Py3
    Signed-off-by: Noel Power <noel.power@suse.com>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- source4/selftest/tests.py
+++ source4/selftest/tests.py
@@ -648,7 +648,7 @@ planpythontestsuite("ad_dc_ntvfs:local", "samba.tests.samba_tool.schema")
 planpythontestsuite("ad_dc:local", "samba.tests.samba_tool.ntacl", py3_compatible=True)
 planpythontestsuite("none", "samba.tests.samba_tool.provision_password_check",  py3_compatible=True)
 planpythontestsuite("none", "samba.tests.samba_tool.help", py3_compatible=True)
-planpythontestsuite("ad_dc_ntvfs:local", "samba.tests.samba_tool.passwordsettings")
+planpythontestsuite("ad_dc_ntvfs:local", "samba.tests.samba_tool.passwordsettings", py3_compatible=True)
 
 # Run these against chgdcpass to share the runtime load
 planpythontestsuite("chgdcpass:local", "samba.tests.samba_tool.sites", py3_compatible=True)
