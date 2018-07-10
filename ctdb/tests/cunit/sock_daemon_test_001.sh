23952c9165bbdcae8f34b7dfefdbb4a499a55362
Amitay Isaacs <amitay@gmail.com>
2018-07-10 19:11:18 +1000

    ctdb-tests: Use sigcode to match signals
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=13520
    
    Signed-off-by: Amitay Isaacs <amitay@gmail.com>
    Reviewed-by: Martin Schwenke <martin@meltin.net>

--- ctdb/tests/cunit/sock_daemon_test_001.sh
+++ ctdb/tests/cunit/sock_daemon_test_001.sh
@@ -38,20 +38,20 @@ ok <<EOF
 test2[PID]: daemon started, pid=PID
 test2[PID]: startup completed successfully
 test2[PID]: listening on $sockpath
-test2[PID]: Received signal 1
+test2[PID]: Received signal $(sigcode SIGHUP)
 test2[PID]: reconfigure failed, ret=1
-test2[PID]: Received signal 10
+test2[PID]: Received signal $(sigcode SIGUSR1)
 test2[PID]: reconfigure completed successfully
-test2[PID]: Received signal 15
+test2[PID]: Received signal $(sigcode SIGTERM)
 test2[PID]: Shutting down
 test2[PID]: daemon started, pid=PID
 test2[PID]: startup completed successfully
 test2[PID]: listening on $sockpath
-test2[PID]: Received signal 10
+test2[PID]: Received signal $(sigcode SIGUSR1)
 test2[PID]: reconfigure failed, ret=2
-test2[PID]: Received signal 1
+test2[PID]: Received signal $(sigcode SIGHUP)
 test2[PID]: reconfigure completed successfully
-test2[PID]: Received signal 15
+test2[PID]: Received signal $(sigcode SIGTERM)
 test2[PID]: Shutting down
 EOF
 unit_test sock_daemon_test "$pidfile" "$sockpath" 2
@@ -73,7 +73,7 @@ unit_test sock_daemon_test "$pidfile" "$sockpath" 4
 ok <<EOF
 test5[PID]: daemon started, pid=PID
 test5[PID]: listening on $sockpath
-test5[PID]: Received signal 15
+test5[PID]: Received signal $(sigcode SIGTERM)
 test5[PID]: Shutting down
 EOF
 unit_test sock_daemon_test "$pidfile" "$sockpath" 5
@@ -88,7 +88,7 @@ unit_test sock_daemon_test "$pidfile" "$sockpath" 6
 ok <<EOF
 test7[PID]: daemon started, pid=PID
 test7[PID]: startup completed successfully
-test7[PID]: Received signal 15
+test7[PID]: Received signal $(sigcode SIGTERM)
 test7[PID]: Shutting down
 EOF
 unit_test sock_daemon_test "$pidfile" "$sockpath" 7
@@ -96,11 +96,11 @@ unit_test sock_daemon_test "$pidfile" "$sockpath" 7
 ok <<EOF
 test8[PID]: daemon started, pid=PID
 test8[PID]: startup completed successfully
-test8[PID]: Received signal 15
+test8[PID]: Received signal $(sigcode SIGTERM)
 test8[PID]: Shutting down
 test8[PID]: daemon started, pid=PID
 test8[PID]: startup completed successfully
-test8[PID]: Received signal 15
+test8[PID]: Received signal $(sigcode SIGTERM)
 test8[PID]: Shutting down
 EOF
 unit_test sock_daemon_test "$pidfile" "$sockpath" 8
@@ -108,11 +108,11 @@ unit_test sock_daemon_test "$pidfile" "$sockpath" 8
 ok <<EOF
 test9[PID]: daemon started, pid=PID
 test9[PID]: startup completed successfully
-test9[PID]: Received signal 15
+test9[PID]: Received signal $(sigcode SIGTERM)
 test9[PID]: Shutting down
 test9[PID]: daemon started, pid=PID
 test9[PID]: startup completed successfully
-test9[PID]: Received signal 15
+test9[PID]: Received signal $(sigcode SIGTERM)
 test9[PID]: Shutting down
 EOF
 unit_test sock_daemon_test "$pidfile" "$sockpath" 9
@@ -122,7 +122,7 @@ test10[PID]: daemon started, pid=PID
 test10[PID]: listening on $sockpath
 test10[PID]: daemon started, pid=PID
 test10[PID]: listening on $sockpath
-test10[PID]: Received signal 15
+test10[PID]: Received signal $(sigcode SIGTERM)
 test10[PID]: Shutting down
 EOF
 unit_test sock_daemon_test "$pidfile" "$sockpath" 10
