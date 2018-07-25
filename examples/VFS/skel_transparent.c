5ab0b4af9edbd50c0c7747840f9b3aca9cb01da8
Stefan Metzmacher <metze@samba.org>
2018-07-25 12:30:37 +0200

    examples/VFS/skel_transparent: make vfs_fn_pointers static
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Ralph Boehme <slow@samba.org>
    
    Autobuild-User(master): Stefan Metzmacher <metze@samba.org>
    Autobuild-Date(master): Wed Jul 25 20:44:12 CEST 2018 on sn-devel-144

--- examples/VFS/skel_transparent.c
+++ examples/VFS/skel_transparent.c
@@ -1119,7 +1119,7 @@ static NTSTATUS skel_durable_reconnect(struct vfs_handle_struct *handle,
 
 /* VFS operations structure */
 
-struct vfs_fn_pointers skel_transparent_fns = {
+static struct vfs_fn_pointers skel_transparent_fns = {
 	/* Disk operations */
 
 	.connect_fn = skel_connect,
