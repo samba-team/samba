d28b4fa3611a8b2aaac4c37ec79907a724d6f0f7
Noel Power <noel.power@suse.com>
2018-07-31 17:02:54 +0100

    PY3: md5 related functions need to be passed bytes
    Signed-off-by: Noel Power <noel.power@suse.com>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- buildtools/wafsamba/samba_utils.py
+++ buildtools/wafsamba/samba_utils.py
@@ -358,13 +358,13 @@ try:
     # Even if hashlib.md5 exists, it may be unusable.
     # Try to use MD5 function. In FIPS mode this will cause an exception
     # and we'll get to the replacement code
-    foo = md5('abcd')
+    foo = md5(b'abcd')
 except:
     try:
         import md5
         # repeat the same check here, mere success of import is not enough.
         # Try to use MD5 function. In FIPS mode this will cause an exception
-        foo = md5.md5('abcd')
+        foo = md5.md5(b'abcd')
     except:
         Context.SIG_NIL = hash('abcd')
         class replace_md5(object):
