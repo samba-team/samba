1a86733d034019083cadb137cb81dba661f96b66
Amitay Isaacs <amitay@gmail.com>
2018-08-08 19:44:38 +1000

    provision: Add support for BIND 9.12.x
    Signed-off-by: Amitay Isaacs <amitay@gmail.com>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>
    
    Autobuild-User(master): Andrew Bartlett <abartlet@samba.org>
    Autobuild-Date(master): Fri Aug 10 05:36:19 CEST 2018 on sn-devel-144

--- python/samba/provision/sambadns.py
+++ python/samba/provision/sambadns.py
@@ -953,6 +953,7 @@ def create_named_conf(paths, realm, dnsdomain, dns_backend, logger):
         bind9_9 = '#'
         bind9_10 = '#'
         bind9_11 = '#'
+        bind9_12 = '#'
         if bind_info.upper().find('BIND 9.8') != -1:
             bind9_8 = ''
         elif bind_info.upper().find('BIND 9.9') != -1:
@@ -961,6 +962,8 @@ def create_named_conf(paths, realm, dnsdomain, dns_backend, logger):
             bind9_10 = ''
         elif bind_info.upper().find('BIND 9.11') != -1:
             bind9_11 = ''
+        elif bind_info.upper().find('BIND 9.12') != -1:
+            bind9_12 = ''
         elif bind_info.upper().find('BIND 9.7') != -1:
             raise ProvisioningError("DLZ option incompatible with BIND 9.7.")
         else:
@@ -971,7 +974,8 @@ def create_named_conf(paths, realm, dnsdomain, dns_backend, logger):
                     "BIND9_8" : bind9_8,
                     "BIND9_9" : bind9_9,
                     "BIND9_10" : bind9_10,
-                    "BIND9_11" : bind9_11
+                    "BIND9_11" : bind9_11,
+                    "BIND9_12" : bind9_12
                     })
 
 
