83b79f5bfb5af906f23ca81e13e4d3682e6a3a58
Martin Schwenke <martin@meltin.net>
2018-08-21 11:44:03 +1000

    ctdb-config: Integrate failover options into conf-tool
    Update and add tests accordingly.
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=13589
    
    Signed-off-by: Martin Schwenke <martin@meltin.net>
    Reviewed-by: Amitay Isaacs <amitay@gmail.com>
    (cherry picked from commit d003a41a9cb9ea97a7da9dbb5bd3138f82da6cf1)

--- ctdb/common/conf_tool.c
+++ ctdb/common/conf_tool.c
@@ -32,6 +32,7 @@
 #include "cluster/cluster_conf.h"
 #include "database/database_conf.h"
 #include "event/event_conf.h"
+#include "failover/failover_conf.h"
 #include "server/legacy_conf.h"
 
 #include "common/conf_tool.h"
@@ -243,6 +244,7 @@ int conf_tool_run(struct conf_tool_context *ctx, int *result)
 	cluster_conf_init(ctx->conf);
 	database_conf_init(ctx->conf);
 	event_conf_init(ctx->conf);
+	failover_conf_init(ctx->conf);
 	legacy_conf_init(ctx->conf);
 
 	if (! conf_valid(ctx->conf)) {
