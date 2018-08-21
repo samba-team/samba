893dd623dfdec4d5c5da07f933069e4534fe58ae
Martin Schwenke <martin@meltin.net>
2018-08-21 11:30:39 +1000

    ctdb-failover: Add failover configuration options
    Only a "disabled" option for now.  Not documented because it isn't
    used yet.
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=13589
    
    Signed-off-by: Martin Schwenke <martin@meltin.net>
    Reviewed-by: Amitay Isaacs <amitay@gmail.com>

---
+++ ctdb/failover/failover_conf.h
@@ -0,0 +1,31 @@
+/*
+   CTDB failover config handling
+
+   Copyright (C) Martin Schwenke  2018
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef __CTDB_FAILOVER_CONF_H__
+#define __CTDB_FAILOVER_CONF_H__
+
+#include "common/conf.h"
+
+#define FAILOVER_CONF_SECTION "failover"
+
+#define FAILOVER_CONF_DISABLED "disabled"
+
+void failover_conf_init(struct conf_context *conf);
+
+#endif /* __CTDB_FAILOVER_CONF_H__ */
