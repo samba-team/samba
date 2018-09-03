8385a0c84bb71304584c3742a21998d2b07fa6c0
Martin Schwenke <martin@meltin.net>
2018-09-03 16:12:16 +1000

    ctdb-daemon: Log complete eventd startup command
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=13592
    
    Signed-off-by: Martin Schwenke <martin@meltin.net>
    Reviewed-by: Amitay Isaacs <amitay@gmail.com>
    (cherry picked from commit 6d3d9a85e5630ba398ac953ad1515155f10224d9)

--- ctdb/server/eventscript.c
+++ ctdb/server/eventscript.c
@@ -310,10 +310,11 @@ int ctdb_start_eventd(struct ctdb_context *ctdb)
 		return -1;
 	}
 
-	DEBUG(DEBUG_NOTICE,
-	      ("Starting event daemon %s %s %s %s %s %s %s %s %s %s %s\n",
-	       argv[0], argv[1], argv[2], argv[3], argv[4], argv[5],
-	       argv[6], argv[7], argv[8], argv[9], argv[10]));
+	D_NOTICE("Starting event daemon "
+		 "%s %s %s %s %s %s %s %s %s %s %s %s %s\n",
+		 argv[0], argv[1], argv[2], argv[3], argv[4], argv[5],
+		 argv[6], argv[7], argv[8], argv[9], argv[10],
+		 argv[11], argv[12]);
 
 	pid = ctdb_fork(ctdb);
 	if (pid == -1) {
