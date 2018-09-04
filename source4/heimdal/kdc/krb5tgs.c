4ab222049fd788255dc93a316b87d3615f2752db
Isaac Boukris <iboukris@gmail.com>
2018-09-04 11:21:40 +0300

    heimdal: kdc: split check_PAC function
    Introduction commit, no change intended.
    
    Signed-off-by: Isaac Boukris <iboukris@gmail.com>

--- source4/heimdal/kdc/krb5tgs.c
+++ source4/heimdal/kdc/krb5tgs.c
@@ -276,19 +276,9 @@ check_KRB5SignedPath(krb5_context context,
  */
 
 static krb5_error_code
-check_PAC(krb5_context context,
-	  krb5_kdc_configuration *config,
-	  const krb5_principal client_principal,
-	  const krb5_principal delegated_proxy_principal,
-	  hdb_entry_ex *client,
-	  hdb_entry_ex *server,
-	  hdb_entry_ex *krbtgt,
-	  const EncryptionKey *server_check_key,
-	  const EncryptionKey *server_sign_key,
-	  const EncryptionKey *krbtgt_sign_key,
-	  EncTicketPart *tkt,
-	  krb5_data *rspac,
-	  int *signedpath)
+get_PAC(krb5_context context,
+	EncTicketPart *tkt,
+	krb5_pac *pac)
 {
     AuthorizationData *ad = tkt->authorization_data;
     unsigned i, j;
@@ -315,47 +305,13 @@ check_PAC(krb5_context context,
 	for (j = 0; j < child.len; j++) {
 
 	    if (child.val[j].ad_type == KRB5_AUTHDATA_WIN2K_PAC) {
-		int signed_pac = 0;
-		krb5_pac pac;
 
 		/* Found PAC */
 		ret = krb5_pac_parse(context,
 				     child.val[j].ad_data.data,
 				     child.val[j].ad_data.length,
-				     &pac);
+				     pac);
 		free_AuthorizationData(&child);
-		if (ret)
-		    return ret;
-
-		ret = krb5_pac_verify(context, pac, tkt->authtime,
-				      client_principal,
-				      server_check_key, NULL);
-		if (ret) {
-		    krb5_pac_free(context, pac);
-		    return ret;
-		}
-
-		ret = _kdc_pac_verify(context, client_principal,
-				      delegated_proxy_principal,
-				      client, server, krbtgt, &pac, &signed_pac);
-		if (ret) {
-		    krb5_pac_free(context, pac);
-		    return ret;
-		}
-
-		/*
-		 * Only re-sign PAC if we could verify it with the PAC
-		 * function. The no-verify case happens when we get in
-		 * a PAC from cross realm from a Windows domain and
-		 * that there is no PAC verification function.
-		 */
-		if (signed_pac) {
-		    *signedpath = 1;
-		    ret = _krb5_pac_sign(context, pac, tkt->authtime,
-					 client_principal,
-					 server_sign_key, krbtgt_sign_key, rspac);
-		}
-		krb5_pac_free(context, pac);
 
 		return ret;
 	    }
@@ -363,6 +319,65 @@ check_PAC(krb5_context context,
 	free_AuthorizationData(&child);
     }
     return 0;
+}
+
+static krb5_error_code
+check_PAC(krb5_context context,
+	  krb5_kdc_configuration *config,
+	  const krb5_principal client_principal,
+	  const krb5_principal delegated_proxy_principal,
+	  hdb_entry_ex *client,
+	  hdb_entry_ex *server,
+	  hdb_entry_ex *krbtgt,
+	  const EncryptionKey *server_check_key,
+	  const EncryptionKey *server_sign_key,
+	  const EncryptionKey *krbtgt_sign_key,
+	  EncTicketPart *tkt,
+	  krb5_data *rspac,
+	  int *signedpath)
+{
+    int signed_pac = 0;
+    krb5_pac pac = NULL;
+    krb5_error_code ret;
+
+    ret = get_PAC(context, tkt, &pac);
+    if (ret) {
+	return ret;
+    }
+
+    if (pac != NULL) {
+	ret = krb5_pac_verify(context, pac, tkt->authtime,
+			      client_principal, server_check_key, NULL);
+	if (ret) {
+	    krb5_pac_free(context, pac);
+	    return ret;
+	}
+
+	ret = _kdc_pac_verify(context, client_principal,
+			      delegated_proxy_principal,
+			      client, server, krbtgt, &pac, &signed_pac);
+	if (ret) {
+	    krb5_pac_free(context, pac);
+	    return ret;
+	}
+
+	/*
+	 * Only re-sign PAC if we could verify it with the PAC
+	 * function. The no-verify case happens when we get in
+	 * a PAC from cross realm from a Windows domain and
+	 * that there is no PAC verification function.
+	 */
+	if (signed_pac) {
+	    *signedpath = 1;
+	    ret = _krb5_pac_sign(context, pac, tkt->authtime,
+				 client_principal,
+				 server_sign_key, krbtgt_sign_key, rspac);
+	}
+
+	krb5_pac_free(context, pac);
+    }
+
+    return ret;
 }
 
 /*
