samba-team/samba

commit cb40e2bbc8a34a1ec3584ab585c5bf44c037ef0e

autobuild: do not try to send email to no recipient
Signed-off-by: Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
Reviewed-by: Gary Lockyer <gary@catalyst.net.nz>

script/autobuild.py
@@ -692,6 +692,11 @@ parser.add_option("", "--restrict-tests", help="run as make test with this TESTS
                   default='')
 
 def send_email(subject, text, log_tar):
+    if options.email is None:
+        do_print("not sending email because the recipient is not set")
+        do_print("the text content would have been:\n\nSubject: %s\n\nTs" %
+                 (subject, text))
+        return
     outer = MIMEMultipart()
     outer['Subject'] = subject
     outer['To'] = options.email

commit 8f022a0be81333fcaa7ac20147028eef73e2179e

script/autobuild: Fix formatting in send_email
Commit cb40e2bbc8a34a1ec3584ab585c5bf44c037ef0e introduced a print
statement with a broken formatting. Reported by pylint.

Signed-off-by: Alexander Bokovoy <ab@samba.org>
Reviewed-by: Andrew Bartlett <abartlet@samba.org>

script/autobuild.py
@@ -777,7 +777,7 @@ parser.add_option("", "--restrict-tests", help="run as make test with this TESTS
 def send_email(subject, text, log_tar):
     if options.email is None:
         do_print("not sending email because the recipient is not set")
-        do_print("the text content would have been:\n\nSubject: %s\n\nTs" %
+        do_print("the text content would have been:\n\nSubject: %s\n\n%s" %
                  (subject, text))
         return
     outer = MIMEMultipart()