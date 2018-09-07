0cd9452ea6b6e402124bee970df5484c4a264368
Stefan Metzmacher <metze@samba.org>
2018-09-07 16:27:58 +0200

    wafsamba: fix generic_cc.py to work with waf 2
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- buildtools/wafsamba/samba_autoconf.py
+++ buildtools/wafsamba/samba_autoconf.py
@@ -897,9 +897,6 @@ def CHECK_CC_ENV(conf):
     The build farm sometimes puts a space at the start"""
     if os.environ.get('CC'):
         conf.env.CC = TO_LIST(os.environ.get('CC'))
-        if len(conf.env.CC) == 1:
-            # make for nicer logs if just a single command
-            conf.env.CC = conf.env.CC[0]
 
 
 @conf
