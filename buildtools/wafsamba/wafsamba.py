samba-team/samba

commit 26ea0f58daace4adef7c5bb17f19476083bf3b7b

wafsamba: Make INSTALL_DIR() respect $DESTDIR
INSTALL_DIR() currently ignores $DESTDIR and just installs directories
into the final destination.

Signed-off-by: Martin Schwenke <martin@meltin.net>
Reviewed-by: Alexander Bokovoy <ab@samba.org>

buildtools/wafsamba/wafsamba.py
@@ -897,7 +897,8 @@ def INSTALL_DIR(bld, path, chmod=0o755, env=None):
     if not path:
         return []
 
-    destpath = bld.EXPAND_VARIABLES(path)
+    destpath = os.path.join(Options.options.destdir,
+                            bld.EXPAND_VARIABLES(path).lstrip(os.sep))
 
     if bld.is_install > 0:
         if not os.path.isdir(destpath):

commit 93d72bcfb0dea3d7734771bbb550c45d642955c9

wafsamba: Only use $DESTDIR in INSTALL_DIR() if it is set
Otherwise the leading '/' is stripped and directories are created
relative to the current directory.

This fixes a regression introduced in recent commit
26ea0f58daace4adef7c5bb17f19476083bf3b7b.

Reported-by: Ralph Böhme <slow@samba.org>
Signed-off-by: Martin Schwenke <martin@meltin.net>
Reviewed-by: Alexander Bokovoy <ab@samba.org>

Autobuild-User(master): Martin Schwenke <martins@samba.org>
Autobuild-Date(master): Thu Sep 13 09:37:23 CEST 2018 on sn-devel-144

buildtools/wafsamba/wafsamba.py
@@ -897,8 +897,9 @@ def INSTALL_DIR(bld, path, chmod=0o755, env=None):
     if not path:
         return []
 
-    destpath = os.path.join(Options.options.destdir,
-                            bld.EXPAND_VARIABLES(path).lstrip(os.sep))
+    destpath = bld.EXPAND_VARIABLES(path)
+    if Options.options.destdir:
+        destpath = os.path.join(Options.options.destdir, destpath.lstrip(os.sep))
 
     if bld.is_install > 0:
         if not os.path.isdir(destpath):