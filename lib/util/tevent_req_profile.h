a1a4e54cf0ee5c753425fbf7c3b1f78e98ed78c0
Volker Lendecke <vl@samba.org>
2018-10-07 14:47:26 +0200

    lib: Avoid the use of open_memstream in tevent_req_profile_string
    Solaris does not have it.
    
    Bug: https://bugzilla.samba.org/show_bug.cgi?id=13629
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>
    (cherry picked from commit e7d3678ddb5916416193d553f3cc562627e7d8ab)
    
    Autobuild-User(v4-9-test): Karolin Seeger <kseeger@samba.org>
    Autobuild-Date(v4-9-test): Wed Oct 10 18:57:59 CEST 2018 on sn-devel-144

--- lib/util/tevent_req_profile.h
+++ lib/util/tevent_req_profile.h
@@ -29,12 +29,8 @@
 #include "replace.h"
 #include <tevent.h>
 
-int tevent_req_profile_print(const struct tevent_req_profile *profile,
-			     FILE *fp,
-			     unsigned indent,
-			     unsigned max_indent);
-char *tevent_req_profile_string(const struct tevent_req_profile *profile,
-				TALLOC_CTX *mem_ctx,
+char *tevent_req_profile_string(TALLOC_CTX *mem_ctx,
+				const struct tevent_req_profile *profile,
 				unsigned indent,
 				unsigned max_indent);
 ssize_t tevent_req_profile_pack(
