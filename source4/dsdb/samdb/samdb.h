60131b4452d43b3792e7f27a4190c88e7aabb1b4
Stefan Metzmacher <metze@samba.org>
2018-10-08 17:13:13 +0200

    s4:dsdb: fix comment on DSDB_CONTROL_DBCHECK_FIX_LINK_DN_NAME
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- source4/dsdb/samdb/samdb.h
+++ source4/dsdb/samdb/samdb.h
@@ -132,7 +132,7 @@ struct dsdb_control_password_change {
 /* passed by dbcheck to fix duplicate linked attributes (bug #13095) */
 #define DSDB_CONTROL_DBCHECK_FIX_DUPLICATE_LINKS "1.3.6.1.4.1.7165.4.3.19.2"
 
-/* passed by dbcheck to fix the DN strong of a one-way-link (bug #13495) */
+/* passed by dbcheck to fix the DN string of a one-way-link (bug #13495) */
 #define DSDB_CONTROL_DBCHECK_FIX_LINK_DN_NAME "1.3.6.1.4.1.7165.4.3.19.3"
 
 /* passed when importing plain text password on upgrades */
