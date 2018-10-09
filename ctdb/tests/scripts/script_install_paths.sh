bde73c7a044a9508bf08bb8c55a37f38d034afe0
Martin Schwenke <martin@meltin.net>
2018-10-09 16:18:49 +1100

    ctdb-tests: Add new variable CTDB_SCRIPTS_TESTS_BINDIR
    Signed-off-by: Martin Schwenke <martin@meltin.net>
    Reviewed-by: Amitay Isaacs <amitay@gmail.com>

--- ctdb/tests/scripts/script_install_paths.sh
+++ ctdb/tests/scripts/script_install_paths.sh
@@ -10,6 +10,7 @@ if [  -n "$ctdb_dir" ] ; then
 	CTDB_SCRIPTS_TOOLS_HELPER_DIR="${ctdb_dir}/tools"
 	CTDB_SCRIPTS_HELPER_BINDIR="${top_dir}/bin"
 	CTDB_SCRIPTS_DATA_DIR="${ctdb_dir}/config"
+	CTDB_SCRIPTS_TESTS_BINDIR="${top_dir}/bin"
 else
 	# Installed
 	CTDB_SCRIPTS_BASE="/usr/local/etc/ctdb"
@@ -19,6 +20,7 @@ else
 	CTDB_SCRIPTS_TOOLS_HELPER_DIR="/usr/local/libexec/ctdb"
 	CTDB_SCRIPTS_HELPER_BINDIR="/usr/local/libexec/ctdb"
 	CTDB_SCRIPTS_DATA_DIR="/usr/local/share/ctdb"
+	CTDB_SCRIPTS_TESTS_BINDIR="/usr/local/libexec/ctdb/tests"
 fi
 
 export CTDB_SCRIPTS_BASE \
@@ -27,4 +29,5 @@ export CTDB_SCRIPTS_BASE \
        CTDB_SCRIPTS_SBIN_DIR \
        CTDB_SCRIPTS_TOOLS_HELPER_DIR \
        CTDB_SCRIPTS_HELPER_BINDIR \
-       CTDB_SCRIPTS_DATA_DIR
+       CTDB_SCRIPTS_DATA_DIR \
+       CTDB_SCRIPTS_TESTS_BINDIR
