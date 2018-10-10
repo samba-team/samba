15b2ab04b7a97fe948aec9169d5d49f22fdb8289
Noel Power <noel.power@suse.com>
2018-10-10 10:15:31 +0100

    s4/scripting: PY3 Ensure python scripts are run with correct python ver.
    As part of port samba4.blackbox.samba3dump to python2/3
    make sure test_samba3dump.sh runs samba3dump with $PYTHON which should
    define the correct python version.
    
    Signed-off-by: Noel Power <noel.power@suse.com>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- source4/selftest/test_samba3dump.sh
+++ source4/selftest/test_samba3dump.sh
@@ -7,7 +7,7 @@ subunit_start_test samba3dump
 
 SRCDIR=`dirname $0`/../..
 
-if $SRCDIR/source4/scripting/bin/samba3dump $SRCDIR/testdata/samba3; then
+if $PYTHON $SRCDIR/source4/scripting/bin/samba3dump $SRCDIR/testdata/samba3; then
 	subunit_pass_test samba3dump
 else
 	echo | subunit_fail_test samba3dump
