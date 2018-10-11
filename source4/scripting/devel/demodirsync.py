105e14cbd1865578bdaad150f2254ceed88abab1
Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
2018-10-11 18:33:48 +1300

    s4/scripting/demodirsync: fix syntax error
    Signed-off-by: Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
    Reviewed-by: Noel Power <npower@samba.org>

--- source4/scripting/devel/demodirsync.py
+++ source4/scripting/devel/demodirsync.py
@@ -138,7 +138,7 @@ cookie.blob.highwatermark.tmp_highest_usn = usn - 2
 if cookie.blob.extra_length > 0:
     cookie.blob.extra.ctr.cursors[0].source_dsa_invocation_id = misc.GUID("128a99bf-e2df-4832-ac0a-1fb625e530db")
     cookie.blob.extra.ctr.cursors[0].highest_usn = bigusn - 1
-controls = ["dirsync:1:1:50:%s" % base64.b64encode(ndr_pack(cookie)).decode('utf8'))
+controls = ["dirsync:1:1:50:%s" % base64.b64encode(ndr_pack(cookie)).decode('utf8')]
 (msgs, ctrls) = remote_ldb.searchex(expression="(objectclass=*)", base=base, controls=controls)
 if (len(ctrls)):
     for ctl in ctrls:
