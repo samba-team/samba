74a5448bd0464173353971ac158c732a46006649
Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
2018-10-11 13:05:32 +1300

    python/gp_ext_loader: remove unused imports
    Signed-off-by: Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
    Reviewed-by: Noel Power <npower@samba.org>

--- python/samba/gp_ext_loader.py
+++ python/samba/gp_ext_loader.py
@@ -14,7 +14,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import os
 from samba.gpclass import list_gp_extensions
 from samba.gpclass import gp_ext
 
