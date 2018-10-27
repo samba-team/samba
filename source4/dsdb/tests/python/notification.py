4e694079e5f3a662c678cce0e87769c62ddeefeb
Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
2018-10-28 11:02:51 +1300

    tests/python/notification: safer use of super()
    Signed-off-by: Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
    Reviewed-by: Noel Power <noel.power@suse.com>

--- source4/dsdb/tests/python/notification.py
+++ source4/dsdb/tests/python/notification.py
@@ -67,7 +67,7 @@ creds.set_gensec_features(creds.get_gensec_features() | gensec.FEATURE_SEAL)
 class LDAPNotificationTest(samba.tests.TestCase):
 
     def setUp(self):
-        super(samba.tests.TestCase, self).setUp()
+        super(LDAPNotificationTest, self).setUp()
         self.ldb = SamDB(url, credentials=creds, session_info=system_session(lp), lp=lp)
         self.base_dn = self.ldb.domain_dn()
 
