samba-team/samba

commit da68a1b2f417ec82ea4ed3e7a4d867cef8ca8f93

lib:socket: Return early if we have only one interface
This fixes compilation with -Wstrict-overflow=2

Signed-off-by: Andreas Schneider <asn@samba.org>
Reviewed-by: Andrew Bartlett <abartlet@samba.org>

lib/socket/interfaces.c
@@ -367,7 +367,10 @@ int get_interfaces(TALLOC_CTX *mem_ctx, struct iface_struct **pifaces)
 	int total, i, j;
 
 	total = _get_interfaces(mem_ctx, &ifaces);
-	if (total <= 0) return total;
+	/* If we have an error, no interface or just one we can leave */
+	if (total <= 1) {
+		return total;
+	}
 
 	/* now we need to remove duplicates */
 	TYPESAFE_QSORT(ifaces, total, iface_comp);

commit 5391e21dea3168691cee1e6ef6c5959e46d62d1e

lib:socket: If returning early, set ifaces
Prevents a segfault in load_interfaces() when total interfaces == 1.
Fixes regression caused by da68a1b2f417ec82ea4ed3e7a4d867cef8ca8f93.

BUG: https://bugzilla.samba.org/show_bug.cgi?id=13665

Signed-off-by: David Mulder <dmulder@suse.com>
Reviewed-by: Ralph Böhme <slow@samba.org>
Reviewed-by: Volker Lendecke <vl@samba.org>
Reviewed-by: Jeremy Allison <jra@samba.org>

Autobuild-User(master): Volker Lendecke <vl@samba.org>
Autobuild-Date(master): Sun Oct 28 00:35:35 CEST 2018 on sn-devel-144

lib/socket/interfaces.c
@@ -363,12 +363,13 @@ static int iface_comp(struct iface_struct *i1, struct iface_struct *i2)
    above */
 int get_interfaces(TALLOC_CTX *mem_ctx, struct iface_struct **pifaces)
 {
-	struct iface_struct *ifaces;
+	struct iface_struct *ifaces = NULL;
 	int total, i, j;
 
 	total = _get_interfaces(mem_ctx, &ifaces);
 	/* If we have an error, no interface or just one we can leave */
 	if (total <= 1) {
+		*pifaces = ifaces;
 		return total;
 	}
 