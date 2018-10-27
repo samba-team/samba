d1777971010621e750d352d219a344fef4a11c33
Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
2018-10-28 10:29:06 +1300

    python/upgradehelpers: remove unused provision imports
    Signed-off-by: Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
    Reviewed-by: Noel Power <noel.power@suse.com>

--- python/samba/upgradehelpers.py
+++ python/samba/upgradehelpers.py
@@ -33,9 +33,9 @@ from samba import Ldb, version, ntacls
 from ldb import SCOPE_SUBTREE, SCOPE_ONELEVEL, SCOPE_BASE
 import ldb
 from samba.provision import (provision_paths_from_lp,
-                             getpolicypath, set_gpos_acl, create_gpo_struct,
+                             getpolicypath, create_gpo_struct,
                              provision, ProvisioningError,
-                             setsysvolacl, secretsdb_self_join)
+                             secretsdb_self_join)
 from samba.provision.common import FILL_FULL
 from samba.dcerpc import xattr, drsblobs, security
 from samba.dcerpc.misc import SEC_CHAN_BDC
