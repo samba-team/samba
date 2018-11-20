be04480e0872a70c7ddca0eb14ba28b9bee98990
Andreas Schneider <asn@samba.org>
2018-11-20 14:10:36 +0100

    s3:auth: Use #ifdef instead of #if for config.h definitions
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Gary Lockyer <gary@catalyst.net.nz>

--- source3/auth/pampass.c
+++ source3/auth/pampass.c
@@ -488,7 +488,7 @@ static bool smb_pam_start(pam_handle_t **pamh, const char *user, const char *rho
 		return False;
 	}
 
-#if HAVE_PAM_RHOST
+#ifdef HAVE_PAM_RHOST
 	DEBUG(4,("smb_pam_start: PAM: setting rhost to: %s\n", rhost));
 	pam_error = pam_set_item(*pamh, PAM_RHOST, rhost);
 	if(!smb_pam_error_handler(*pamh, pam_error, "set rhost failed", 0)) {
@@ -497,7 +497,7 @@ static bool smb_pam_start(pam_handle_t **pamh, const char *user, const char *rho
 		return False;
 	}
 #endif
-#if HAVE_PAM_TTY
+#ifdef HAVE_PAM_TTY
 	DEBUG(4,("smb_pam_start: PAM: setting tty\n"));
 	pam_error = pam_set_item(*pamh, PAM_TTY, "samba");
 	if (!smb_pam_error_handler(*pamh, pam_error, "set tty failed", 0)) {
@@ -642,7 +642,7 @@ static bool smb_internal_pam_session(pam_handle_t *pamh, const char *user, const
 {
 	int pam_error;
 
-#if HAVE_PAM_TTY
+#ifdef HAVE_PAM_TTY
 	DEBUG(4,("smb_internal_pam_session: PAM: tty set to: %s\n", tty));
 	pam_error = pam_set_item(pamh, PAM_TTY, tty);
 	if (!smb_pam_error_handler(pamh, pam_error, "set tty failed", 0))
