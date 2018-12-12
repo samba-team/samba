338413ad5aba0b6fa2f0419eb99b4d00ebf6ebc7
Joe Guo <joeg@catalyst.net.nz>
2018-12-12 13:39:24 +1300

    PY3: change shebang to python3 in source4/scripting dir
    Signed-off-by: Joe Guo <joeg@catalyst.net.nz>
    Reviewed-by: Noel Power <npower@samba.org>

--- source4/scripting/devel/pfm_verify.py
+++ source4/scripting/devel/pfm_verify.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
 # script to verify cached prefixMap on remote
--- source4/scripting/devel/repl_cleartext_pwd.py
+++ source4/scripting/devel/repl_cleartext_pwd.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #
 # Copyright Stefan Metzmacher 2011-2012
 #
