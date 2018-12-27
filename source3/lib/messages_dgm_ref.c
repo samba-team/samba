26107832cd9d200fb171ef1f991d7ef5478cac18
Ralph Boehme <slow@samba.org>
2018-12-27 12:45:28 +0100

    Revert "s3:messages: allow messaging_dgm_ref() to use wrapper tevent_context"
    This reverts commit 9dc332060cf5f249ea887dbc60ec7a39b6f91120.
    
    See the discussion in
    
    https://lists.samba.org/archive/samba-technical/2018-December/131731.html
    
    for the reasoning behind this revert.
    
    Signed-off-by: Ralph Boehme <slow@samba.org>
    Reviewed-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Stefan Metzmacher <metze@samba.org>

--- source3/lib/messages_dgm_ref.c
+++ source3/lib/messages_dgm_ref.c
@@ -55,6 +55,18 @@ void *messaging_dgm_ref(TALLOC_CTX *mem_ctx, struct tevent_context *ev,
 {
 	struct msg_dgm_ref *result, *tmp_refs;
 
+	if (tevent_context_is_wrapper(ev)) {
+		/*
+		 * This is really a programmer error!
+		 *
+		 * The main/raw tevent context should
+		 * have been registered first!
+		 */
+		DBG_ERR("Should not be used with a wrapper tevent context\n");
+		*err = EINVAL;
+		return NULL;
+	}
+
 	result = talloc(mem_ctx, struct msg_dgm_ref);
 	if (result == NULL) {
 		*err = ENOMEM;
@@ -74,18 +86,6 @@ void *messaging_dgm_ref(TALLOC_CTX *mem_ctx, struct tevent_context *ev,
 
 	if (refs == NULL) {
 		int ret;
-
-		if (tevent_context_is_wrapper(ev)) {
-			/*
-			 * This is really a programmer error!
-			 *
-			 * The main/raw tevent context should
-			 * have been registered first!
-			 */
-			DBG_ERR("Should not be used with a wrapper tevent context\n");
-			*err = EINVAL;
-			return NULL;
-		}
 
 		ret = messaging_dgm_init(ev, unique, socket_dir, lockfile_dir,
 					 msg_dgm_ref_recv, NULL);
