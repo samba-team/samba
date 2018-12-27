ac597b0706926850f8d599c54077946b3a726a9a
Ralph Boehme <slow@samba.org>
2018-12-27 15:21:06 +0100

    Revert "smbd: explain that/why we use the raw tevent_context for linux_oplock_signal_handler()"
    This reverts commit dc517b20f60a156d73fdd551557eb1d4366dfdeb.
    
    See the discussion in
    
    https://lists.samba.org/archive/samba-technical/2018-December/131731.html
    
    for the reasoning behind this revert.
    
    Signed-off-by: Ralph Boehme <slow@samba.org>
    Reviewed-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Stefan Metzmacher <metze@samba.org>

--- source3/smbd/oplock_linux.c
+++ source3/smbd/oplock_linux.c
@@ -125,12 +125,6 @@ static void linux_oplock_signal_handler(struct tevent_context *ev_ctx,
 	int fd = info->si_fd;
 	files_struct *fsp;
 
-	/*
-	 * This function doesn't expect any specific impersonation, as it only
-	 * sends messages to other smbd processes. And messaging_send_iov_from()
-	 * already handles EACCES.
-	 */
-
 	fsp = file_find_fd(sconn, fd);
 	if (fsp == NULL) {
 		DEBUG(0,("linux_oplock_signal_handler: failed to find fsp for file fd=%d (file was closed ?)\n", fd ));
@@ -241,13 +235,7 @@ struct kernel_oplocks *linux_init_kernel_oplocks(struct smbd_server_connection *
 	ctx->ops = &linux_koplocks;
 	ctx->private_data = sconn;
 
-	/*
-	 * linux_oplock_signal_handler() only
-	 * sends messages to other smbd processes
-	 * and doesn't require any impersonation.
-	 * So we can just use the raw tevent_context.
-	 */
-	se = tevent_add_signal(sconn->raw_ev_ctx,
+	se = tevent_add_signal(sconn->ev_ctx,
 			       ctx,
 			       RT_SIGNAL_LEASE, SA_SIGINFO,
 			       linux_oplock_signal_handler,
