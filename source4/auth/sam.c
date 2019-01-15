ce83b21be8aaa537ce3cb8e2ed0bc86f33024159
Isaac Boukris <iboukris@gmail.com>
2019-01-15 13:58:52 +0200

    sam.c: fix incorrect check of talloc_new() allocation
    Signed-off-by: Isaac Boukris <iboukris@gmail.com>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Garming Sam <garming@catalyst.net.nz>

--- source4/auth/sam.c
+++ source4/auth/sam.c
@@ -364,7 +364,7 @@ _PUBLIC_ NTSTATUS authsam_make_user_info_dc(TALLOC_CTX *mem_ctx,
 	NT_STATUS_HAVE_NO_MEMORY(user_info_dc);
 
 	tmp_ctx = talloc_new(user_info_dc);
-	if (user_info_dc == NULL) {
+	if (tmp_ctx == NULL) {
 		TALLOC_FREE(user_info_dc);
 		return NT_STATUS_NO_MEMORY;
 	}
