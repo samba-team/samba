95db9a81db093488e625b4ef385a184a5e517ede
Andreas Schneider <asn@samba.org>
2019-01-16 13:15:08 +0100

    s3:rpc_client: Use GnuTLS RC4 in init_samr_CryptPassword()
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=14031
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- source3/rpc_client/init_samr.c
+++ source3/rpc_client/init_samr.c
@@ -19,7 +19,6 @@
 
 #include "includes.h"
 #include "../libcli/auth/libcli_auth.h"
-#include "../lib/crypto/arcfour.h"
 #include "rpc_client/init_samr.h"
 
 #include "lib/crypto/gnutls_helpers.h"
@@ -77,13 +76,33 @@ NTSTATUS init_samr_CryptPassword(const char *pwd,
 				 struct samr_CryptPassword *pwd_buf)
 {
 	/* samr_CryptPassword */
+	gnutls_cipher_hd_t cipher_hnd = NULL;
+	gnutls_datum_t sess_key = {
+		.data = session_key->data,
+		.size = session_key->length,
+	};
 	bool ok;
+	int rc;
 
 	ok = encode_pw_buffer(pwd_buf->data, pwd, STR_UNICODE);
 	if (!ok) {
 		return NT_STATUS_INTERNAL_ERROR;
 	}
-	arcfour_crypt_blob(pwd_buf->data, 516, session_key);
+
+	rc = gnutls_cipher_init(&cipher_hnd,
+				GNUTLS_CIPHER_ARCFOUR_128,
+				&sess_key,
+				NULL);
+	if (rc != 0) {
+		return gnutls_error_to_ntstatus(rc, NT_STATUS_ACCESS_DISABLED_BY_POLICY_OTHER);
+	}
+	rc = gnutls_cipher_encrypt(cipher_hnd,
+				   pwd_buf->data,
+				   516);
+	gnutls_cipher_deinit(cipher_hnd);
+	if (rc != 0) {
+		return gnutls_error_to_ntstatus(rc, NT_STATUS_ACCESS_DISABLED_BY_POLICY_OTHER);
+	}
 
 	return NT_STATUS_OK;
 }
