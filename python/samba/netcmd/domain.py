e51de1d48a4656d08d1276477fe48cb7c9a15603
Tim Beale <timbeale@catalyst.net.nz>
2019-01-16 15:17:38 +1300

    join: Fix TypeError when handling exception
    When we can't resolve a domain name, we were inadvertently throwing a
    TypeError whilst trying to output a helpful message. E.g.
    
    ERROR(<class 'TypeError'>): uncaught exception - 'NTSTATUSError' object
    does not support indexing
    
    Instead of indexing the object, we want to index the Exception.args so
    that we just display the string portion of the exception error.
    
    The same problem is also present for the domain trust commands.
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=13747
    
    Signed-off-by: Tim Beale <timbeale@catalyst.net.nz>
    Reviewed-by: Rowland Penny <rpenny@samba.org>
    Reviewed-by: Jeremy Allison <rpenny@samba.org>
    (cherry picked from commit 3bb7808984c163a7bba66fb983411d1281589722)

--- python/samba/netcmd/domain.py
+++ python/samba/netcmd/domain.py
@@ -1818,7 +1818,7 @@ class DomainTrustCommand(Command):
             remote_info = remote_net.finddc(flags=remote_flags, domain=domain, address=remote_server)
         except NTSTATUSError as error:
             raise CommandError("Failed to find a writeable DC for domain '%s': %s" %
-                               (domain, error[1]))
+                               (domain, error.args[1]))
         except Exception:
             raise CommandError("Failed to find a writeable DC for domain '%s'" % domain)
         flag_map = {
