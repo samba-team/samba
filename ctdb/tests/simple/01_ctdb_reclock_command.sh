c39430b0ae6218dd987410b045fed65e18f39709
Martin Schwenke <martin@meltin.net>
2019-01-21 12:15:33 +1100

    ctdb-tests: Add a test for configuring the recovery lock as a command
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=13800
    
    Signed-off-by: Martin Schwenke <martin@meltin.net>
    Reviewed-by: Amitay Isaacs <amitay@gmail.com>
    (cherry picked from commit ebc082122fb34ffb8cbcafde9ad39bcc241d33ed)

---
+++ ctdb/tests/simple/01_ctdb_reclock_command.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+test_info()
+{
+    cat <<EOF
+Check that CTDB operates correctly if the recovery lock is configured
+as a command.
+
+This test only does anything with local daemons.  On a real cluster it
+has no way of updating configuration.
+EOF
+}
+
+. "${TEST_SCRIPTS_DIR}/integration.bash"
+
+set -e
+
+if [ -z "$TEST_LOCAL_DAEMONS" ] ; then
+	echo "SKIPPING this test - only runs against local daemons"
+	exit 0
+fi
+
+echo "Starting CTDB with recovery lock command configured..."
+ctdb_test_init --reclock-use-command
+
+cluster_is_healthy
+
+echo "Good, that seems to work!"
