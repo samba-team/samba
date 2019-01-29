1e0db9726bb91ff967c896686d129aa30563697d
Tim Beale <timbeale@catalyst.net.nz>
2019-01-30 10:07:26 +1300

    selftest: Remove unnecessary tests.py options from proclimit tests
    It seems like these extra options were just copy-n-pasted from another
    test. The process_limits test doesn't actually try to use these env
    variables at all. All the test is doing is creating LDAP connections to
    the DC. The SOCKET_WRAPPER_DEFAULT_IFACE may have perhaps been needed,
    but we can avoid this by dropping ':local' from the testenv and running
    the test as a "client" instead.
    
    Signed-off-by: Tim Beale <timbeale@catalyst.net.nz>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- source4/selftest/tests.py
+++ source4/selftest/tests.py
@@ -1268,12 +1268,10 @@ planoldpythontestsuite("preforkrestartdc:local",
                        extra_args=['-U"$USERNAME%$PASSWORD"'],
                        name="samba.tests.blackbox.smbcontrol_process",
                        py3_compatible=True)
-planoldpythontestsuite("proclimitdc:local",
+planoldpythontestsuite("proclimitdc",
                        "samba.tests.process_limits",
                        extra_path=[
                            os.path.join(srcdir(), 'python/samba/tests')],
                        extra_args=['-U"$USERNAME%$PASSWORD"'],
-                       environ={'CLIENT_IP': '127.0.0.11',
-                                'SOCKET_WRAPPER_DEFAULT_IFACE': 11},
                        name="samba.tests.process_limits",
                        py3_compatible=True)
