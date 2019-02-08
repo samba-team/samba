0732499f23cebe6c5e24f596ff4fe41f8ab1d156
Aurelien Aptel <aaptel@suse.com>
2019-02-08 12:04:42 +0100

    docs-xml: add "debug encryption" global parm
    Add debug option to dump in the log the session id & keys in smbd and
    libsmb-based code for offline decryption.
    
    Wireshark can make use of this to decrypt encrypted traffic.
    
    Signed-off-by: Aurelien Aptel <aaptel@suse.com>
    Reviewed-by: Noel Power <npower@samba.org>
    Reviewed-by: Ralph Boehme <slow@samba.org>
    Reviewed-by: David Disseldorp <ddiss@samba.org>

--- lib/param/loadparm.c
+++ lib/param/loadparm.c
@@ -3008,6 +3008,8 @@ struct loadparm_context *loadparm_init(TALLOC_CTX *mem_ctx)
 
 	lpcfg_do_global_parameter(lp_ctx, "store dos attributes", "yes");
 
+	lpcfg_do_global_parameter(lp_ctx, "debug encryption", "no");
+
 	for (i = 0; parm_table[i].label; i++) {
 		if (!(lp_ctx->flags[i] & FLAG_CMDLINE)) {
 			lp_ctx->flags[i] |= FLAG_DEFAULT;
