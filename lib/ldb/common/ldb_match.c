8ddaf853404f3cddef84b77b38951526d73ffbda
Gary Lockyer <gary@catalyst.net.nz>
2019-02-19 10:26:56 +1300

    CVE-2019-3824 ldb: wildcard_match end of data check
    ldb_handler_copy and ldb_val_dup over allocate by one and add a trailing '\0'
    to the data, to make them safe to use the C string functions on.
    
    However testing for the trailing '\0' is not the correct way to test for
    the end of a value, the length should be checked instead.
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=13773
    
    Signed-off-by: Gary Lockyer <gary@catalyst.net.nz>

--- lib/ldb/common/ldb_match.c
+++ lib/ldb/common/ldb_match.c
@@ -353,7 +353,7 @@ static int ldb_wildcard_compare(struct ldb_context *ldb,
 	}
 
 	/* last chunk may not have reached end of string */
-	if ( (! tree->u.substring.end_with_wildcard) && (*(val.data) != 0) ) goto mismatch;
+	if ( (! tree->u.substring.end_with_wildcard) && (val.length != 0) ) goto mismatch;
 	talloc_free(save_p);
 	*matched = true;
 	return LDB_SUCCESS;
