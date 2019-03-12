aebf46d957fa2d39c6575a79458d4a819a92ddda
Stefan Metzmacher <metze@samba.org>
2019-03-12 10:25:40 +0100

    python/samba/netcmd: provide SUPPRESS_HELP via Option class
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=13816
    
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>
    (cherry picked from commit b61d580fb7dba8ff94e9e98c958e324865cd2f1d)

--- python/samba/netcmd/__init__.py
+++ python/samba/netcmd/__init__.py
@@ -24,6 +24,7 @@ import sys, traceback
 import textwrap
 
 class Option(optparse.Option):
+    SUPPRESS_HELP = optparse.SUPPRESS_HELP
     pass
 
 # This help formatter does text wrapping and preserves newlines
