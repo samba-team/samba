65a175aac08bc69eaaf6b4e011eb59b262e3417b
Andrew Bartlett <abartlet@samba.org>
2019-03-21 17:21:58 +1300

    CVE-2019-3870 pysmbd: Include tests to show the outside umask has no impact
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=13834
    
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- python/samba/tests/ntacls_backup.py
+++ python/samba/tests/ntacls_backup.py
@@ -112,6 +112,12 @@ class NtaclsBackupRestoreTests(SmbdBaseTests):
 
         dirpath = os.path.join(self.service_root, 'a-dir')
         smbd.mkdir(dirpath, self.service)
+        mode = os.stat(dirpath).st_mode
+
+        # This works in conjunction with the TEST_UMASK in smbd_base
+        # to ensure that permissions are not related to the umask
+        # but instead the smb.conf settings
+        self.assertEquals(mode & 0o777, 0o755)
         self.assertTrue(os.path.isdir(dirpath))
 
     def test_smbd_create_file(self):
@@ -122,6 +128,13 @@ class NtaclsBackupRestoreTests(SmbdBaseTests):
         filepath = os.path.join(self.service_root, 'a-file')
         smbd.create_file(filepath, self.service)
         self.assertTrue(os.path.isfile(filepath))
+
+        mode = os.stat(filepath).st_mode
+
+        # This works in conjunction with the TEST_UMASK in smbd_base
+        # to ensure that permissions are not related to the umask
+        # but instead the smb.conf settings
+        self.assertEquals(mode & 0o777, 0o644)
 
         # As well as checking that unlink works, this removes the
         # fake xattrs from the dev/inode based DB
