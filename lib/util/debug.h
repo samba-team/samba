823d57094b992d4287487932c72d308f4d94d6a3
Stefan Metzmacher <metze@samba.org>
2019-04-26 13:32:43 +0200

    lib/util: remove unused prototypes in debug.h
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=13915
    
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Ralph Boehme <slow@samba.org>
    (cherry picked from commit d98a971247450d494c581c5454e6c270ad1b6880)

--- lib/util/debug.h
+++ lib/util/debug.h
@@ -45,7 +45,6 @@
 bool dbgtext_va(const char *, va_list ap) PRINTF_ATTRIBUTE(1,0);
 bool dbgtext( const char *, ... ) PRINTF_ATTRIBUTE(1,2);
 bool dbghdrclass( int level, int cls, const char *location, const char *func);
-bool dbghdr( int level, const char *location, const char *func);
 
 /*
  * Define all new debug classes here. A class is represented by an entry in
@@ -318,7 +317,6 @@ void force_check_log_size( void );
 bool need_to_check_log_size( void );
 void check_log_size( void );
 void dbgflush( void );
-bool dbghdrclass(int level, int cls, const char *location, const char *func);
 bool debug_get_output_is_stderr(void);
 bool debug_get_output_is_stdout(void);
 void debug_schedule_reopen_logs(void);
