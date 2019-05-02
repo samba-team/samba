9f4cc1ff94e0f44906f3b6f3e6c67c0d501bd44f
Robert Sander <r.sander@heinlein-support.de>
2019-05-02 11:20:44 -0700

    s3: modules: ceph: use current working directory instead of share path
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=13918
    
    Signed-off-by: Robert Sander <r.sander@heinlein-support.de>
    Reviewed-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: Ralph Böhme <slow@samba.org>
    
    Autobuild-User(master): Jeremy Allison <jra@samba.org>
    Autobuild-Date(master): Thu May  2 19:34:11 UTC 2019 on sn-devel-184
    
    (cherry picked from commit 966fc0dc604299a87e40e7170d80911475317db5)

--- source3/modules/vfs_ceph.c
+++ source3/modules/vfs_ceph.c
@@ -1216,14 +1216,14 @@ static struct smb_filename *cephwrap_realpath(struct vfs_handle_struct *handle,
 	} else if ((len >= 2) && (path[0] == '.') && (path[1] == '/')) {
 		if (len == 2) {
 			r = asprintf(&result, "%s",
-					handle->conn->connectpath);
+					handle->conn->cwd_fname->base_name);
 		} else {
 			r = asprintf(&result, "%s/%s",
-					handle->conn->connectpath, &path[2]);
+					handle->conn->cwd_fname->base_name, &path[2]);
 		}
 	} else {
 		r = asprintf(&result, "%s/%s",
-				handle->conn->connectpath, path);
+				handle->conn->cwd_fname->base_name, path);
 	}
 
 	if (r < 0) {
