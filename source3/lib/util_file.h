19583f44bb44dddc61ba486cec0b2cc1eb801e33
Jeremy Allison <jra@samba.org>
2019-05-18 11:18:19 -0700

    s3: lib: Add file_ploadv_send().
    Not yet used. Preparing to remove file_pload_send()
    with this safer alternative.
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=13964
    
    Signed-off-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: Ralph Boehme <slow@samba.org>
    (cherry picked from commit 61054e53f53e5884902b566b1f9b454a3ff4741f)

--- source3/lib/util_file.h
+++ source3/lib/util_file.h
@@ -26,6 +26,9 @@
 struct tevent_req *file_pload_send(TALLOC_CTX *mem_ctx,
 				   struct tevent_context *ev,
 				   const char *syscmd, size_t maxsize);
+struct tevent_req *file_ploadv_send(TALLOC_CTX *mem_ctx,
+				   struct tevent_context *ev,
+				   char * const argl[], size_t maxsize);
 int file_pload_recv(struct tevent_req *req, TALLOC_CTX *mem_ctx,
 		    uint8_t **buf);
 char **file_lines_ploadv(TALLOC_CTX *mem_ctx,
