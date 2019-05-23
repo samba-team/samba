dc82866dd76c043f73a93cde061af730cc010695
Noel Power <noel.power@suse.com>
2019-05-23 13:56:39 +0000

    nsswitch: cppcheck: Fix ctunullpointer error
    Fixes
    
    nsswitch/pam_winbind.c:1379: error: ctunullpointer: Null pointer dereference: info <--[cppcheck]
    
    Signed-off-by: Noel Power <noel.power@suse.com>
    Reviewed-by: Andreas Schneider <asn@samba.org>

--- nsswitch/pam_winbind.c
+++ nsswitch/pam_winbind.c
@@ -1375,14 +1375,16 @@ static void _pam_set_data_string(struct pwb_context *ctx,
 static void _pam_set_data_info3(struct pwb_context *ctx,
 				const struct wbcAuthUserInfo *info)
 {
-	_pam_set_data_string(ctx, PAM_WINBIND_HOMEDIR,
+	if (info != NULL) {
+		_pam_set_data_string(ctx, PAM_WINBIND_HOMEDIR,
 			     info->home_directory);
-	_pam_set_data_string(ctx, PAM_WINBIND_LOGONSCRIPT,
+		_pam_set_data_string(ctx, PAM_WINBIND_LOGONSCRIPT,
 			     info->logon_script);
-	_pam_set_data_string(ctx, PAM_WINBIND_LOGONSERVER,
+		_pam_set_data_string(ctx, PAM_WINBIND_LOGONSERVER,
 			     info->logon_server);
-	_pam_set_data_string(ctx, PAM_WINBIND_PROFILEPATH,
+		_pam_set_data_string(ctx, PAM_WINBIND_PROFILEPATH,
 			     info->profile_path);
+	}
 }
 
 /**
