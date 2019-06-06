88d62288dd4a3c7eb00603c3a9fc5d1c9c73e51f
Andrew Bartlett <abartlet@samba.org>
2019-06-06 06:02:49 +0200

    py3: Remove unused PyInt_AsUnsignedLongLongMask macro from py3compat.h
    Now that we are no longer developing new py2/py3 compatible code we can remove
    any aspects of this header we do not use.  This will make the eventual removal
    easier.
    
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>

--- python/py3compat.h
+++ python/py3compat.h
@@ -87,7 +87,6 @@
 #define PyInt_Check PyLong_Check
 #define PyInt_FromLong PyLong_FromLong
 #define PyInt_AsLong PyLong_AsLong
-#define PyInt_AsUnsignedLongLongMask PyLong_AsUnsignedLongLongMask
 #define PyInt_AsSsize_t PyLong_AsSsize_t
 
 /* Module init */
