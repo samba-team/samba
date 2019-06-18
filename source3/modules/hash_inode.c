cf0442bff13e37507bc37455f2823d0179f783aa
Andreas Schneider <asn@samba.org>
2019-06-18 16:56:43 +0200

    s3:modules: Add hash_inode() function based on SHA1
    This should use SHA1 as modern CPUs have SHA NI instruction support.
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Ralph Boehme <slow@samba.org>

---
+++ source3/modules/hash_inode.c
@@ -0,0 +1,83 @@
+/*
+ * Unix SMB/Netbios implementation.
+ *
+ * Copyright (c) 2019      Andreas Schneider <asn@samba.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "includes.h"
+#include "hash_inode.h"
+
+#include <gnutls/gnutls.h>
+#include <gnutls/crypto.h>
+
+SMB_INO_T hash_inode(const SMB_STRUCT_STAT *sbuf, const char *sname)
+{
+	gnutls_hash_hd_t hash_hnd = NULL;
+	uint8_t digest[gnutls_hash_get_len(GNUTLS_DIG_SHA1)];
+	char *upper_sname = NULL;
+	SMB_INO_T result = 0;
+	int rc;
+
+	DBG_DEBUG("hash_inode called for %ju/%ju [%s]\n",
+		  (uintmax_t)sbuf->st_ex_dev,
+		  (uintmax_t)sbuf->st_ex_ino,
+		  sname);
+
+	upper_sname = talloc_strdup_upper(talloc_tos(), sname);
+	SMB_ASSERT(upper_sname != NULL);
+
+	rc = gnutls_hash_init(&hash_hnd, GNUTLS_DIG_SHA1);
+	if (rc < 0) {
+		goto out;
+	}
+
+	rc = gnutls_hash(hash_hnd,
+			 &(sbuf->st_ex_dev),
+			 sizeof(sbuf->st_ex_dev));
+	if (rc < 0) {
+		gnutls_hash_deinit(hash_hnd, NULL);
+		goto out;
+	}
+	rc = gnutls_hash(hash_hnd,
+			 &(sbuf->st_ex_ino),
+			 sizeof(sbuf->st_ex_ino));
+	if (rc < 0) {
+		gnutls_hash_deinit(hash_hnd, NULL);
+		goto out;
+	}
+	rc = gnutls_hash(hash_hnd,
+			 upper_sname,
+			 talloc_get_size(upper_sname) - 1);
+	if (rc < 0) {
+		gnutls_hash_deinit(hash_hnd, NULL);
+		goto out;
+	}
+
+	gnutls_hash_deinit(hash_hnd, digest);
+
+	memcpy(&result, digest, sizeof(result));
+	DBG_DEBUG("fruit_inode \"%s\": ino=%ju\n",
+		  sname, (uintmax_t)result);
+
+out:
+	TALLOC_FREE(upper_sname);
+
+	DBG_DEBUG("hash_inode '%s': ino=%ju\n",
+		  sname,
+		  (uintmax_t)result);
+
+	return result;
+}
---
+++ source3/modules/hash_inode.h
@@ -0,0 +1,25 @@
+/*
+ * Unix SMB/Netbios implementation.
+ *
+ * Copyright (c) 2019      Andreas Schneider <asn@samba.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _HASH_INODE_H
+#define _HASH_INODE_H
+
+SMB_INO_T hash_inode(const SMB_STRUCT_STAT *sbuf, const char *sname);
+
+#endif /* _HASH_INODE_H */
