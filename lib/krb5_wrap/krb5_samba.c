samba-team/samba

commit 8b92539997a9df8e0d217790fd82fc8f59ea50cc

lib/krb5_wrap: clang: Fix warning: Call to function 'mktemp' is insecure
Fixes:

lib/krb5_wrap/krb5_samba.c:2012:2: warning: Call to function 'mktemp' is insecure as it always creates or uses insecure temporary file.  Use 'mkstemp' instead <--[clang]
        mktemp(tmp_name);
        ^~~~~~

Signed-off-by: Noel Power <noel.power@suse.com>
Reviewed-by: Gary Lockyer <gary@catalyst.net.nz>

lib/krb5_wrap/krb5_samba.c
@@ -2009,7 +2009,7 @@ krb5_error_code smb_krb5_kinit_keyblock_ccache(krb5_context ctx,
 
 	memcpy(tmp_name, SMB_CREDS_KEYTAB, sizeof(SMB_CREDS_KEYTAB));
 	mask = umask(S_IRWXO | S_IRWXG);
-	mktemp(tmp_name);
+	mkstemp(tmp_name);
 	umask(mask);
 	if (tmp_name[0] == 0) {
 		return KRB5_KT_BADNAME;

commit c3bad08e2b0913e5e49ffadb4780bb51154a3215

lib/krb5_wrap: Fix leaking using mkstemp
commit: 8b92539997a9df8e0d217790fd82fc8f59ea50cc introduced a leak
by replacing mktemp with mkstemp (mkstemp returns an open fd)

Signed-off-by: Noel Power <noel.power@suse.com>
Reviewed-by: Andreas Schneider <asn@samba.org>

lib/krb5_wrap/krb5_samba.c
@@ -2001,6 +2001,7 @@ krb5_error_code smb_krb5_kinit_keyblock_ccache(krb5_context ctx,
 	char tmp_name[sizeof(SMB_CREDS_KEYTAB)];
 	krb5_keytab_entry entry;
 	krb5_keytab keytab;
+	int tmpfd;
 	mode_t mask;
 
 	memset(&entry, 0, sizeof(entry));
@@ -2009,11 +2010,13 @@ krb5_error_code smb_krb5_kinit_keyblock_ccache(krb5_context ctx,
 
 	memcpy(tmp_name, SMB_CREDS_KEYTAB, sizeof(SMB_CREDS_KEYTAB));
 	mask = umask(S_IRWXO | S_IRWXG);
-	mkstemp(tmp_name);
+	tmpfd = mkstemp(tmp_name);
 	umask(mask);
-	if (tmp_name[0] == 0) {
+	if (tmpfd == -1) {
+		DBG_ERR("Failed to mkstemp %s\n", tmp_name);
 		return KRB5_KT_BADNAME;
 	}
+	close(tmpfd);
 	code = krb5_kt_resolve(ctx, tmp_name, &keytab);
 	if (code) {
 		return code;