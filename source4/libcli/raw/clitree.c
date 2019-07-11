7b8ec24ccfb06c1d499bf500183c0dc24de553a0
Noel Power <noel.power@suse.com>
2019-07-11 12:00:27 +0000

    s4/libcli/raw: Fix 'Value stored to 'p' is never read'
    Fixes:
    
    source4/libcli/raw/clitree.c:138:3: warning: Value stored to 'p' is never read <--[clang]
                    p += smbcli_req_pull_string(&req->in.bufinfo, mem_ctx, &parms->tconx.out.fs_type,
                    ^    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    1 warning generated.
    
    Signed-off-by: Noel Power <noel.power@suse.com>
    Reviewed-by: Gary Lockyer <gary@catalyst.net.nz>

--- source4/libcli/raw/clitree.c
+++ source4/libcli/raw/clitree.c
@@ -135,7 +135,7 @@ NTSTATUS smb_raw_tcon_recv(struct smbcli_request *req, TALLOC_CTX *mem_ctx,
 
 		p += smbcli_req_pull_string(&req->in.bufinfo, mem_ctx, &parms->tconx.out.dev_type, 
 					    p, -1, STR_ASCII | STR_TERMINATE);
-		p += smbcli_req_pull_string(&req->in.bufinfo, mem_ctx, &parms->tconx.out.fs_type, 
+		smbcli_req_pull_string(&req->in.bufinfo, mem_ctx, &parms->tconx.out.fs_type,
 					 p, -1, STR_TERMINATE);
 		break;
 
