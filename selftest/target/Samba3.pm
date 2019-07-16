9ab5a51a6e770c8bd95ceca892808cf2cf670852
Stefan Metzmacher <metze@samba.org>
2019-07-16 15:40:38 +0200

    CVE-2019-10197: test_smbclient_s3.sh: add regression test for the no permission on share root problem
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=14035
    
    Signed-off-by: Stefan Metzmacher <metze@samba.org>

--- selftest/target/Samba3.pm
+++ selftest/target/Samba3.pm
@@ -1434,6 +1434,9 @@ sub provision($$$$$$$$$)
 	my $ro_shrdir="$shrdir/root-tmp";
 	push(@dirs,$ro_shrdir);
 
+	my $noperm_shrdir="$shrdir/noperm-tmp";
+	push(@dirs,$noperm_shrdir);
+
 	my $msdfs_shrdir="$shrdir/msdfsshare";
 	push(@dirs,$msdfs_shrdir);
 
@@ -1503,6 +1506,11 @@ sub provision($$$$$$$$$)
 	chmod 0755, $lockdir;
 	chmod 0755, $piddir;
 
+
+	##
+	## Create a directory without permissions to enter
+	##
+	chmod 0000, $noperm_shrdir;
 
 	##
 	## create ro and msdfs share layout
@@ -1826,6 +1834,10 @@ sub provision($$$$$$$$$)
         guest ok = yes
 [ro-tmp]
 	path = $ro_shrdir
+	guest ok = yes
+[noperm]
+	path = $noperm_shrdir
+	wide links = yes
 	guest ok = yes
 [write-list-tmp]
 	path = $shrdir
