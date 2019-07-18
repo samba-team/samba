de4a11589f15bd983d9f610f532636ac6233d05c
Christof Schmitt <cs@samba.org>
2019-07-18 12:06:06 -0700

    nfs4_acls: Update copyright header
    Signed-off-by: Christof Schmitt <cs@samba.org>
    Reviewed-by: Ralph Boehme <slow@samba.org>

--- source3/modules/nfs4_acls.c
+++ source3/modules/nfs4_acls.c
@@ -2,6 +2,7 @@
  * NFS4 ACL handling
  *
  * Copyright (C) Jim McDonough, 2006
+ * Copyright (C) Christof Schmitt 2019
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
