e94ebc705542ffca536d15e28fc99e023f4748bc
Volker Lendecke <vl@samba.org>
2019-07-26 16:20:17 +0200

    smbd: Slightly optimize reply_open_and_X()
    Well, this is just a drive-by patch: We don't use "mtime" if we
    exit early. So it's not really a worthwhile optimization, to me
    it's more a code clarity thing.
    
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/smbd/reply.c
+++ source3/smbd/reply.c
@@ -2525,12 +2525,12 @@ void reply_open_and_X(struct smb_request *req)
 	}
 
 	fattr = dos_mode(conn, fsp->fsp_name);
-	mtime = convert_timespec_to_time_t(fsp->fsp_name->st.st_ex_mtime);
 	if (fattr & FILE_ATTRIBUTE_DIRECTORY) {
 		close_file(req, fsp, ERROR_CLOSE);
 		reply_nterror(req, NT_STATUS_ACCESS_DENIED);
 		goto out;
 	}
+	mtime = convert_timespec_to_time_t(fsp->fsp_name->st.st_ex_mtime);
 
 	/* If the caller set the extended oplock request bit
 		and we granted one (by whatever means) - set the
