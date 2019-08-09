ba25d8fbb16a1c11880e7434803b6c92b23647ee
Jeremy Allison <jra@samba.org>
2019-08-09 15:59:54 -0700

    s3: VFS: vfs_crossrename. Remove rename_fn(). No longer used.
    Signed-off-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: Ralph Boehme <slow@samba.org>

--- source3/modules/vfs_crossrename.c
+++ source3/modules/vfs_crossrename.c
@@ -167,32 +167,6 @@ static int copy_reg(const char *source, const char *dest)
 	return -1;
 }
 
-
-static int crossrename_rename(vfs_handle_struct *handle,
-			  const struct smb_filename *smb_fname_src,
-			  const struct smb_filename *smb_fname_dst)
-{
-	int result = -1;
-
-	START_PROFILE(syscall_rename);
-
-	if (smb_fname_src->stream_name || smb_fname_dst->stream_name) {
-		errno = ENOENT;
-		goto out;
-	}
-
-	result = rename(smb_fname_src->base_name, smb_fname_dst->base_name);
-	if ((result == -1) && (errno == EXDEV)) {
-		/* Rename across filesystems needed. */
-		result = copy_reg(smb_fname_src->base_name,
-				  smb_fname_dst->base_name);
-	}
-
- out:
-	END_PROFILE(syscall_rename);
-	return result;
-}
-
 static int crossrename_renameat(vfs_handle_struct *handle,
 			files_struct *srcfsp,
 			const struct smb_filename *smb_fname_src,
@@ -223,7 +197,6 @@ static int crossrename_renameat(vfs_handle_struct *handle,
 
 static struct vfs_fn_pointers vfs_crossrename_fns = {
 	.connect_fn = crossrename_connect,
-	.rename_fn = crossrename_rename,
 	.renameat_fn = crossrename_renameat
 };
 
