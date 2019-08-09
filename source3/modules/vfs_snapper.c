0b64e84c01a36302a2298c1d1af4c7f4c41ed6eb
Jeremy Allison <jra@samba.org>
2019-08-09 10:07:04 -0700

    s3: VFS: vfs_snapper: Make unlink return errno = EROFS on a shadow copy path.
    smbd has no business modifying a shadow copy filesystem, it should be read-only.
    
    Signed-off-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: David Disseldorp <ddiss@samba.org>

--- source3/modules/vfs_snapper.c
+++ source3/modules/vfs_snapper.c
@@ -2204,35 +2204,18 @@ static int snapper_gmt_open(vfs_handle_struct *handle,
 static int snapper_gmt_unlink(vfs_handle_struct *handle,
 			      const struct smb_filename *smb_fname)
 {
-	time_t timestamp;
-	char *stripped;
-	int ret, saved_errno;
-	struct smb_filename *conv;
+	time_t timestamp = 0;
 
 	if (!snapper_gmt_strip_snapshot(talloc_tos(), handle,
 					smb_fname->base_name,
-					&timestamp, &stripped)) {
+					&timestamp, NULL)) {
 		return -1;
 	}
-	if (timestamp == 0) {
-		return SMB_VFS_NEXT_UNLINK(handle, smb_fname);
-	}
-	conv = cp_smb_filename(talloc_tos(), smb_fname);
-	if (conv == NULL) {
-		errno = ENOMEM;
+	if (timestamp != 0) {
+		errno = EROFS;
 		return -1;
 	}
-	conv->base_name = snapper_gmt_convert(conv, handle,
-					      stripped, timestamp);
-	TALLOC_FREE(stripped);
-	if (conv->base_name == NULL) {
-		return -1;
-	}
-	ret = SMB_VFS_NEXT_UNLINK(handle, conv);
-	saved_errno = errno;
-	TALLOC_FREE(conv);
-	errno = saved_errno;
-	return ret;
+	return SMB_VFS_NEXT_UNLINK(handle, smb_fname);
 }
 
 static int snapper_gmt_chmod(vfs_handle_struct *handle,
