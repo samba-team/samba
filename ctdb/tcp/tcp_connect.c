f2ce6c745cff004afee2576043274984205a3104
Martin Schwenke <martin@meltin.net>
2019-08-15 15:45:16 +1000

    ctdb-tcp: Use TALLOC_FREE()
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=14084
    
    Signed-off-by: Martin Schwenke <martin@meltin.net>
    Reviewed-by: Amitay Isaacs <amitay@gmail.com>
    (cherry picked from commit d80d9edb4dc107b15a35a39e5c966a3eaed6453a)

--- ctdb/tcp/tcp_connect.c
+++ ctdb/tcp/tcp_connect.c
@@ -46,10 +46,8 @@ void ctdb_tcp_stop_connection(struct ctdb_node *node)
 		node->private_data, struct ctdb_tcp_node);
 	
 	ctdb_queue_set_fd(tnode->out_queue, -1);
-	talloc_free(tnode->connect_te);
-	talloc_free(tnode->connect_fde);
-	tnode->connect_fde = NULL;
-	tnode->connect_te = NULL;
+	TALLOC_FREE(tnode->connect_te);
+	TALLOC_FREE(tnode->connect_fde);
 	if (tnode->out_fd != -1) {
 		close(tnode->out_fd);
 		tnode->out_fd = -1;
