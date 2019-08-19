7155d3a2c5d7237f00cccb1802c1341cf295864e
Stefan Metzmacher <metze@samba.org>
2019-08-19 17:38:30 +0200

    s4:torture/smb2: add smb2.samba3misc.localposixlock1
    This demonstrates that the SMB2 code path doesn't do
    any retry for local posix locks.
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=14113
    
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Volker Lendecke <vl@samba.org>

--- source3/selftest/tests.py
+++ source3/selftest/tests.py
@@ -546,7 +546,7 @@ for t in tests:
             plansmbtorture4testsuite(t, env, '//$SERVER/tmp -k yes -U$DC_USERNAME@$REALM%$DC_PASSWORD --option=torture:addc=$DC_SERVER', description='kerberos connection')
             plansmbtorture4testsuite(t, env, '//$SERVER/tmpguest -U% --option=torture:addc=$DC_SERVER', description='anonymous connection')
             plansmbtorture4testsuite(t, env, '//$SERVER/tmp -k no -U$DC_USERNAME@$REALM%$DC_PASSWORD', description='ntlm user@realm')
-    elif t == "raw.samba3posixtimedlock":
+    elif t == "raw.samba3posixtimedlock" or t == "smb2.samba3misc":
         plansmbtorture4testsuite(t, "nt4_dc", '//$SERVER_IP/tmpguest -U$USERNAME%$PASSWORD --option=torture:localdir=$SELFTEST_PREFIX/nt4_dc/share')
         plansmbtorture4testsuite(t, "ad_dc", '//$SERVER_IP/tmpguest -U$USERNAME%$PASSWORD --option=torture:localdir=$SELFTEST_PREFIX/ad_dc/share')
     elif t == "raw.chkpath":
