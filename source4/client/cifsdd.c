72d3604a9c4ee4325fef9b3a160445dc961c863e
Swen Schillig <swen@linux.ibm.com>
2019-08-19 14:29:22 +0200

    s4: free popt context in client
    If done with popt context it should be free'd.
    
    Signed-off-by: Swen Schillig <swen@linux.ibm.com>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Ralph Böhme <slow@samba.org>

--- source4/client/cifsdd.c
+++ source4/client/cifsdd.c
@@ -689,6 +689,8 @@ int main(int argc, const char ** argv)
 	CatchSignal(SIGINT, dd_handle_signal);
 	CatchSignal(SIGUSR1, dd_handle_signal);
 	rc = copy_files(ev, cmdline_lp_ctx);
+
+	poptFreeContext(pctx);
 	talloc_free(ev);
 	return rc;
 }
