2c6b098c7220a3e6d78f6040c912fc22a02f6e46
Martin Schwenke <martin@meltin.net>
2019-09-09 16:58:06 +1000

    ctdb-tests: Don't run setup_ctdb_base() for real cluster
    This has not been needed for some time.
    
    Signed-off-by: Martin Schwenke <martin@meltin.net>
    Reviewed-by: Amitay Isaacs <amitay@gmail.com>

--- ctdb/tests/simple/scripts/local.bash
+++ ctdb/tests/simple/scripts/local.bash
@@ -6,11 +6,6 @@ export SIMPLE_TESTS_VAR_DIR="${TEST_VAR_DIR}/simple"
 # Don't remove old directory since state is retained between tests
 mkdir -p "$SIMPLE_TESTS_VAR_DIR"
 
-if [ -z "$TEST_LOCAL_DAEMONS" ] ; then
-	# Running against a real cluster
-	setup_ctdb_base "$SIMPLE_TESTS_VAR_DIR" "ctdb-etc" \
-			functions \
-			nodes
-else
+if [ -n "$TEST_LOCAL_DAEMONS" ] ; then
 	. "${TEST_SUBDIR}/scripts/local_daemons.bash"
 fi
