1e6a8a3093ab118a4dc21344bfb4f41da18adc25
Jeremy Allison <jra@samba.org>
2019-09-13 13:16:19 -0700

    s3: VFS: vfs_fruit. Wrap Convert fruit_unlink() -> fruit_unlink_internal() so we can add parameters.
    Make fruit_unlink() a wrapper calling the internal version.
    
    Signed-off-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: Ralph Boehme <slow@samba.org>

--- source3/modules/vfs_fruit.c
+++ source3/modules/vfs_fruit.c
@@ -1951,7 +1951,7 @@ static int fruit_unlink_rsrc(vfs_handle_struct *handle,
 	return rc;
 }
 
-static int fruit_unlink(vfs_handle_struct *handle,
+static int fruit_unlink_internal(vfs_handle_struct *handle,
 			const struct smb_filename *smb_fname)
 {
 	int rc;
@@ -2165,6 +2165,12 @@ exit_rmdir:
 	return SMB_VFS_NEXT_RMDIR(handle, smb_fname);
 }
 
+static int fruit_unlink(vfs_handle_struct *handle,
+			const struct smb_filename *smb_fname)
+{
+	return fruit_unlink_internal(handle, smb_fname);
+}
+
 static int fruit_unlinkat(vfs_handle_struct *handle,
 			struct files_struct *dirfsp,
 			const struct smb_filename *smb_fname,
@@ -2176,7 +2182,7 @@ static int fruit_unlinkat(vfs_handle_struct *handle,
 	if (flags & AT_REMOVEDIR) {
 		ret = fruit_rmdir(handle, smb_fname);
 	} else {
-		ret = fruit_unlink(handle, smb_fname);
+		ret = fruit_unlink_internal(handle, smb_fname);
 	}
 	return ret;
 }
