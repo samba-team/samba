samba-team/samba

commit 15a8af075a2d7b59f4d97d9804599b26325e5640

lib: Enable watching and storing dbwrap_watch records.
Samba so far on a dbwrap_watch record either watches or stores a
record, but never both from the same db_record instance acquired from
a dbwrap_fetch_locked(). In one of the next commits this will change,
we will watch a record and at the same time store data into it. This
patch enables a watch_send() followed by a storev() by properly
keeping the watchers around.

Signed-off-by: Volker Lendecke <vl@samba.org>
Reviewed-by: Jeremy Allison <jra@samba.org>

source3/lib/dbwrap/dbwrap_watch.c
@@ -223,6 +223,7 @@ static struct db_record *dbwrap_watched_fetch_locked(
 		db->private_data, struct db_watched_ctx);
 	struct db_record *rec;
 	struct db_watched_subrec *subrec;
+	uint8_t *watchers = NULL;
 	TDB_DATA subrec_value;
 	bool ok;
 
@@ -251,6 +252,26 @@ static struct db_record *dbwrap_watched_fetch_locked(
 	subrec_value = dbwrap_record_get_value(subrec->subrec);
 
 	ok = dbwrap_watch_rec_parse(subrec_value, &subrec->wrec);
+	if (!ok) {
+		return rec;	/* fresh record */
+	}
+
+	/*
+	 * subrec->wrec.watchers points *directly* into the
+	 * returned data in the record. We need to talloc a
+	 * copy of this so it belongs to subrec.
+	 */
+
+	watchers = talloc_memdup(
+		subrec,
+		subrec->wrec.watchers,
+		subrec->wrec.num_watchers * SERVER_ID_BUF_LENGTH);
+	if (watchers == NULL) {
+		TALLOC_FREE(rec);
+		return NULL;
+	}
+	subrec->wrec.watchers = watchers;
+
 	if (ok) {
 		rec->value = subrec->wrec.data;
 	}
@@ -314,6 +335,20 @@ static void dbwrap_watched_do_locked_fn(struct db_record *subrec,
 
 	ok = dbwrap_watch_rec_parse(subrec_value, &state->subrec.wrec);
 	if (ok) {
+		uint8_t *watchers = NULL;
+
+		/*
+		 * state->subrec.wrec.watchers points *directly* into the
+		 * returned data in the record. We need to talloc a
+		 * copy of this so it belongs to state->mem_ctx.
+		 */
+		watchers = talloc_memdup(
+			state->mem_ctx,
+			state->subrec.wrec.watchers,
+			state->subrec.wrec.num_watchers * SERVER_ID_BUF_LENGTH);
+		SMB_ASSERT(watchers != NULL);
+		state->subrec.wrec.watchers = watchers;
+
 		rec.value = state->subrec.wrec.data;
 	}
 
@@ -407,32 +442,38 @@ static NTSTATUS dbwrap_watched_save(struct db_record *rec,
 {
 	uint32_t num_watchers_buf;
 	uint8_t sizebuf[4];
-	uint8_t addbuf[SERVER_ID_BUF_LENGTH];
 	NTSTATUS status;
-	struct TDB_DATA dbufs[num_databufs+3];
+	struct TDB_DATA dbufs[num_databufs+2];
 
 	dbufs[0] = (TDB_DATA) {
 		.dptr = sizebuf, .dsize = sizeof(sizebuf)
 	};
+
+	if (addwatch != NULL) {
+		uint8_t *watchers, *new_watch;
+
+		watchers = talloc_realloc(
+			NULL,
+			wrec->watchers,
+			uint8_t,
+			(wrec->num_watchers+1) * SERVER_ID_BUF_LENGTH);
+		if (watchers == NULL) {
+			return NT_STATUS_NO_MEMORY;
+		}
+		wrec->watchers = watchers;
+
+		new_watch = &watchers[wrec->num_watchers*SERVER_ID_BUF_LENGTH];
+		server_id_put(new_watch, *addwatch);
+		wrec->num_watchers += 1;
+	}
 
 	dbufs[1] = (TDB_DATA) {
 		.dptr = wrec->watchers,
 		.dsize = wrec->num_watchers * SERVER_ID_BUF_LENGTH
 	};
 
-	if (addwatch != NULL) {
-		server_id_put(addbuf, *addwatch);
-
-		dbufs[2] = (TDB_DATA) {
-			.dptr = addbuf, .dsize = SERVER_ID_BUF_LENGTH
-		};
-		wrec->num_watchers += 1;
-	} else {
-		dbufs[2] = (TDB_DATA) { 0 };
-	}
-
 	if (num_databufs != 0) {
-		memcpy(&dbufs[3], databufs, sizeof(TDB_DATA) * num_databufs);
+		memcpy(&dbufs[2], databufs, sizeof(TDB_DATA) * num_databufs);
 	}
 
 	num_watchers_buf = wrec->num_watchers;

commit 32d6cc84cf8e0cf278b5715b8a9d66b7c0a2a6d2

dbwrap_watch: Don't alert ourselves, fix raw.oplock.batch26 race
This fixes the following flaky test:

UNEXPECTED(failure): samba3.raw.oplock.batch26(nt4_dc)
REASON: Exception: Exception: (../../source4/torture/raw/oplock.c:3718): wrong value for break_info.count got 0x2 - should be 0x1

You can reproduce it with two small msleeps, which means it's a race
condition:

diff --git a/source3/smbd/open.c b/source3/smbd/open.c
index 20b5a3e294c..126c7fc021d 100644
--- a/source3/smbd/open.c
+++ b/source3/smbd/open.c
@@ -1917,6 +1917,14 @@ NTSTATUS send_break_message(struct messaging_context *msg_ctx,
 	DATA_BLOB blob;
 	NTSTATUS status;

+	{
+		static bool sent = false;
+		if (sent) {
+			smb_msleep(500);
+		}
+		sent = true;
+	}
+
 	if (DEBUGLVL(10)) {
 		struct server_id_buf buf;
 		DBG_DEBUG("Sending break message to %s\n",
diff --git a/source3/smbd/oplock.c b/source3/smbd/oplock.c
index b3da84b1269..d9c4dbb9487 100644
--- a/source3/smbd/oplock.c
+++ b/source3/smbd/oplock.c
@@ -858,6 +858,8 @@ static void process_oplock_break_message(struct messaging_context *msg_ctx,
 	uint16_t break_to;
 	bool break_needed = true;

+	smb_msleep(100);
+
 	msg = talloc(talloc_tos(), struct oplock_break_message);
 	if (msg == NULL) {
 		DBG_WARNING("talloc failed\n");

15a8af075a2 introduced a bug where we immediately wake up ourselves
after doing a watch_send, leading to two inter-smbd oplock break
messages for this case. In theory, this should not matter, as in the
oplock break handler in the destination smbd we check

(fsp->sent_oplock_break != NO_BREAK_SENT)

so that the break does not get sent twice. However, with the above two
sleeps the oplock holding client could send out its oplock downgrade
while the second inter-smbd break messages was on its way.

The real fix would be to note in the share mode array that the
inter-smbd message has already been sent, but as other users of
dbwrap_watched_watch_send might also be affected by this bug, this fix

source3/lib/dbwrap/dbwrap_watch.c
@@ -196,6 +196,7 @@ struct db_watched_ctx {
 struct db_watched_subrec {
 	struct db_record *subrec;
 	struct dbwrap_watch_rec wrec;
+	bool added_watcher;
 };
 
 static NTSTATUS dbwrap_watched_subrec_storev(
@@ -385,7 +386,7 @@ static void dbwrap_watched_subrec_wakeup(
 	struct db_context *db = rec->db;
 	struct db_watched_ctx *ctx = talloc_get_type_abort(
 		db->private_data, struct db_watched_ctx);
-	size_t i;
+	size_t i, num_to_wakeup;
 	size_t db_id_len = dbwrap_db_id(db, NULL, 0);
 	uint8_t db_id[db_id_len];
 	uint8_t len_buf[4];
@@ -403,7 +404,17 @@ static void dbwrap_watched_subrec_wakeup(
 
 	i = 0;
 
-	while (i < wrec->num_watchers) {
+	num_to_wakeup = wrec->num_watchers;
+
+	if (subrec->added_watcher) {
+		/*
+		 * Don't alert our own watcher that we just added to
+		 * the end of the array.
+		 */
+		num_to_wakeup -= 1;
+	}
+
+	while (i < num_to_wakeup) {
 		struct server_id watcher;
 		NTSTATUS status;
 		struct server_id_buf tmp;
@@ -916,6 +927,7 @@ struct tevent_req *dbwrap_watched_watch_send(TALLOC_CTX *mem_ctx,
 		      state->me);
 	wrec->watchers = watchers;
 	wrec->num_watchers += 1;
+	subrec->added_watcher = true;
 
 	status = dbwrap_watched_save(
 		subrec->subrec, wrec, &wrec->data, 1, 0);