8a18518edead4c828a62615365c99624e4315e69
Jeremy Allison <jra@samba.org>
2019-10-04 14:41:26 -0700

    s3: VFS: vfs_audit: Remove rmdir_fn(). No longer used.
    Signed-off-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: Ralph Boehme <slow@samba.org>

--- source3/modules/vfs_audit.c
+++ source3/modules/vfs_audit.c
@@ -214,21 +214,6 @@ static int audit_mkdirat(vfs_handle_struct *handle,
 	return result;
 }
 
-static int audit_rmdir(vfs_handle_struct *handle,
-		const struct smb_filename *smb_fname)
-{
-	int result;
-
-	result = SMB_VFS_NEXT_RMDIR(handle, smb_fname);
-
-	syslog(audit_syslog_priority(handle), "rmdir %s %s%s\n", 
-	       smb_fname->base_name,
-	       (result < 0) ? "failed: " : "",
-	       (result < 0) ? strerror(errno) : "");
-
-	return result;
-}
-
 static int audit_open(vfs_handle_struct *handle,
 		      struct smb_filename *smb_fname, files_struct *fsp,
 		      int flags, mode_t mode)
@@ -338,7 +323,6 @@ static struct vfs_fn_pointers vfs_audit_fns = {
 	.disconnect_fn = audit_disconnect,
 	.opendir_fn = audit_opendir,
 	.mkdirat_fn = audit_mkdirat,
-	.rmdir_fn = audit_rmdir,
 	.open_fn = audit_open,
 	.close_fn = audit_close,
 	.renameat_fn = audit_renameat,
