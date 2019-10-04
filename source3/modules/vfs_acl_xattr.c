9290510fe4d2b5ce88a6085e7047f1e6fafd6f5a
Jeremy Allison <jra@samba.org>
2019-10-04 14:31:59 -0700

    s3: VFS: vfs_acl_xattr. Wrap rmdir_acl_common() by acl_xattr_rmdir() so we don't call it directly.
    This will allow adding the extra dirfsp parameter to
    rmdir_acl_common().
    
    Signed-off-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: Ralph Boehme <slow@samba.org>

--- source3/modules/vfs_acl_xattr.c
+++ source3/modules/vfs_acl_xattr.c
@@ -296,6 +296,12 @@ static int acl_xattr_unlinkat(vfs_handle_struct *handle,
 	return ret;
 }
 
+static int acl_xattr_rmdir(vfs_handle_struct *handle,
+			const struct smb_filename *smb_dname)
+{
+	return rmdir_acl_common(handle, smb_dname);
+}
+
 static NTSTATUS acl_xattr_fget_nt_acl(vfs_handle_struct *handle,
 				      files_struct *fsp,
 				      uint32_t security_info,
@@ -334,7 +340,7 @@ static NTSTATUS acl_xattr_fset_nt_acl(vfs_handle_struct *handle,
 
 static struct vfs_fn_pointers vfs_acl_xattr_fns = {
 	.connect_fn = connect_acl_xattr,
-	.rmdir_fn = rmdir_acl_common,
+	.rmdir_fn = acl_xattr_rmdir,
 	.unlinkat_fn = acl_xattr_unlinkat,
 	.chmod_fn = chmod_acl_module_common,
 	.fchmod_fn = fchmod_acl_module_common,
