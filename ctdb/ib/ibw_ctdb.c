540325d3cb4eee1964fc59a63d51289552293902
Björn Jacke <bj@sernet.de>
2019-10-26 02:41:08 +0200

    ctdb/ib/ibw_ctdb.c: typo fixes
    Signed-off-by: Bjoern Jacke <bjacke@samba.org>
    Reviewed-by: Martin Schwenke <martin@meltin.net>

--- ctdb/ib/ibw_ctdb.c
+++ ctdb/ib/ibw_ctdb.c
@@ -44,7 +44,7 @@ int ctdb_ibw_get_address(struct ctdb_context *ctdb,
 	if (inet_pton(AF_INET, address, addr) <= 0) {
 		struct hostent *he = gethostbyname(address);
 		if (he == NULL || he->h_length > sizeof(*addr)) {
-			ctdb_set_error(ctdb, "invalid nework address '%s'\n", 
+			ctdb_set_error(ctdb, "invalid network address '%s'\n", 
 				       address);
 			return -1;
 		}
