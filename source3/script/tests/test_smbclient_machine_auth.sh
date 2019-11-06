b05be655a9218add833d41a1765166de92431897
Andreas Schneider <asn@samba.org>
2019-11-06 14:54:45 +0000

    s3:tests: Remove the -I SERVER_IP so that Kerberos auth works
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Gary Lockyer <gary@samba.org>

--- source3/script/tests/test_smbclient_machine_auth.sh
+++ source3/script/tests/test_smbclient_machine_auth.sh
@@ -18,8 +18,8 @@ ADDARGS="$*"
 incdir=`dirname $0`/../../../testprogs/blackbox
 . $incdir/subunit.sh
 
-testit "smbclient //$SERVER/tmp" $SMBCLIENT //$SERVER/tmp --machine-pass -I $SERVER_IP -p 139 -c quit $ADDARGS
+testit "smbclient //$SERVER/tmp" $SMBCLIENT //$SERVER/tmp --machine-pass -p 139 -c quit $ADDARGS
 
 # Testing these here helps because we know the machine account isn't already this user/group
-testit "smbclient //$SERVER/forceuser" $SMBCLIENT //$SERVER/tmp --machine-pass -I $SERVER_IP -p 139 -c quit $ADDARGS
-testit "smbclient //$SERVER/forcegroup" $SMBCLIENT //$SERVER/tmp --machine-pass -I $SERVER_IP -p 139 -c quit $ADDARGS
+testit "smbclient //$SERVER/forceuser" $SMBCLIENT //$SERVER/tmp --machine-pass -p 139 -c quit $ADDARGS
+testit "smbclient //$SERVER/forcegroup" $SMBCLIENT //$SERVER/tmp --machine-pass -p 139 -c quit $ADDARGS
