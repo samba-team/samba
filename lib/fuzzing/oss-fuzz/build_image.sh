4946811eb67c6e4e6abdfd64a552e7a2686feeec
Andrew Bartlett <abartlet@samba.org>
2019-11-07 17:17:40 +1300

    lib/fuzzing/oss-fuzz: Install chrpath as we use it in the build.sh script to set -rpath
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
    Reviewed-by: Uri Simchoni <uri@samba.org>

--- lib/fuzzing/oss-fuzz/build_image.sh
+++ lib/fuzzing/oss-fuzz/build_image.sh
@@ -6,4 +6,6 @@ SCRIPT_DIR=`dirname $0`
 $SCRIPT_DIR/../../../bootstrap/generated-dists/$DIST/bootstrap.sh
 $SCRIPT_DIR/../../../bootstrap/generated-dists/$DIST/locale.sh
 
+apt-get install chrpath
+
 cp $SCRIPT_DIR/build.sh $SRC/
