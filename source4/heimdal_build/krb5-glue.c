d84c4f68f00d4f2b941531235d3d5ba6da73ca6f
Stefan Metzmacher <metze@samba.org>
2019-11-22 16:01:07 +0100

    heimdal_build: Add C99 struct initializer in source4/heimdal_build/krb5-glue.c
    This avoids uninitiliased structure members in this dummy
    structure we include to avoid including more of Heimdal.
    
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- source4/heimdal_build/krb5-glue.c
+++ source4/heimdal_build/krb5-glue.c
@@ -22,20 +22,6 @@
 #include "heimdal/lib/krb5/krb5_locl.h"
 
 const krb5_cc_ops krb5_scc_ops = {
-    KRB5_CC_OPS_VERSION,
-    "_NOTSUPPORTED_SDB",
-    NULL, /* scc_retrieve */
-    NULL, /* scc_get_principal */
-    NULL, /* scc_get_first */
-    NULL, /* scc_get_next */
-    NULL, /* scc_end_get */
-    NULL, /* scc_remove_cred */
-    NULL, /* scc_set_flags */
-    NULL,
-    NULL, /* scc_get_cache_first */
-    NULL, /* scc_get_cache_next */
-    NULL, /* scc_end_cache_get */
-    NULL, /* scc_move */
-    NULL, /* scc_get_default_name */
-    NULL  /* scc_set_default */
+    .version = KRB5_CC_OPS_VERSION,
+    .prefix = "_NOTSUPPORTED_SDB",
 };
