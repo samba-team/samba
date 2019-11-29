3e5f1be8eace95cffa9d7e4cfd55eedca9ce2ceb
Volker Lendecke <vl@samba.org>
2019-11-29 15:46:20 +0100

    net: Use share_mode_count_entries()
    Avoid a reference to share_mode_data->num_share_modes
    
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/utils/net_tdb.c
+++ source3/utils/net_tdb.c
@@ -95,10 +95,20 @@ static int net_tdb_locking(struct net_context *c, int argc, const char **argv)
 	if (argc == 2 && strequal(argv[1], "dump")) {
 		ret = net_tdb_locking_dump(mem_ctx, lock->data);
 	} else {
+		NTSTATUS status;
+		size_t num_share_modes = 0;
+
+		status = share_mode_count_entries(
+			lock->data->id, &num_share_modes);
+		if (!NT_STATUS_IS_OK(status)) {
+			d_fprintf(stderr,
+				  "Could not count share entries: %s\n",
+				  nt_errstr(status));
+		}
+
 		d_printf("Share path:            %s\n", lock->data->servicepath);
 		d_printf("Name:                  %s\n", lock->data->base_name);
-		d_printf("Number of share modes: %" PRIu32 "\n",
-			 lock->data->num_share_modes);
+		d_printf("Number of share modes: %zu\n", num_share_modes);
 	}
 
 out:
