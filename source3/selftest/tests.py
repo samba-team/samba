0f83d9153cd4d84aa293a4e2aae9d212fdfd9b33
Noel Power <noel.power@suse.com>
2019-12-09 10:12:17 +0000

    s3/selftest: Fix typo in test generator script
    Introduced in a64516099936 but only makes a difference when
    environments limit protocols for testing.
    
    Reviewed-by: Ralph Boehme <slow@samba.org>
    Signed-off-by: Noel Power <noel.power@suse.com>

--- source3/selftest/tests.py
+++ source3/selftest/tests.py
@@ -324,7 +324,7 @@ for env in ["maptoguest", "simpleserver"]:
 env = "maptoguest"
 plantestsuite("samba3.blackbox.smbclient_auth.plain.bad_username", env, [os.path.join(samba3srcdir, "script/tests/test_smbclient_auth.sh"), '$SERVER', '$SERVER_IP', 'notmy$USERNAME', '$PASSWORD', smbclient3, configuration + " --option=clientntlmv2auth=no --option=clientlanmanauth=yes"])
 plantestsuite("samba3.blackbox.smbclient_ntlm.plain.NT1", env, [os.path.join(samba3srcdir, "script/tests/test_smbclient_ntlm.sh"), '$SERVER', '$USERNAME', '$PASSWORD', "baduser", smbclient3, "NT1", configuration])
-plantestsuite("samba3.blackbox.smbclient_ntlm.plain.SMB3", env, [os.path.join(samba3srcdir, "script/tests/test_smbclient_ntlm.sh"), '$SERVER', '$USERNAME', '$PASSWORD', "baduser", smbclient3, "NT1", configuration])
+plantestsuite("samba3.blackbox.smbclient_ntlm.plain.SMB3", env, [os.path.join(samba3srcdir, "script/tests/test_smbclient_ntlm.sh"), '$SERVER', '$USERNAME', '$PASSWORD', "baduser", smbclient3, "SMB3", configuration])
 
 # plain
 for env in ["nt4_dc"]:
