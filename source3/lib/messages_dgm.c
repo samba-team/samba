adfdcc4791b9a5706c48789bfbb46f256ee10538
Andreas Schneider <asn@samba.org>
2019-12-09 10:47:46 +0100

    s3:lib: Move NULL check before messaging_dgm_out_rearm_idle_timer()
    We dereference out in messaging_dgm_out_rearm_idle_timer().
    
    Found by covscan.
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Richard Sharpe <realrichardsharpe@gmail.com>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- source3/lib/messages_dgm.c
+++ source3/lib/messages_dgm.c
@@ -322,7 +322,6 @@ static int messaging_dgm_out_get(struct messaging_dgm_context *ctx, pid_t pid,
 		}
 	}
 
-	messaging_dgm_out_rearm_idle_timer(out);
 	/*
 	 * shouldn't be possible, should be set if messaging_dgm_out_create
 	 * succeeded. This check is to satisfy static checker
@@ -330,6 +329,7 @@ static int messaging_dgm_out_get(struct messaging_dgm_context *ctx, pid_t pid,
 	if (out == NULL) {
 		return EINVAL;
 	}
+	messaging_dgm_out_rearm_idle_timer(out);
 
 	*pout = out;
 	return 0;
