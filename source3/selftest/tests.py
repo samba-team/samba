25efadd5f36b9d8b01b22c1613ba0b9afeff5e18
Noel Power <noel.power@suse.com>
2019-12-11 18:06:47 +0000

    s3/selftest: Move samba3.rap.basic & samba3.rap.rpc to _smb1 env
    Tests:
      samba3.rap.basic
      samba3.rap.rpc
    
    have been moved to ad_dc_smb1, nt4_dc_smb1
    
    verified by
      python3 source3/selftest/tests.py  | grep ad_dc_smb1 | grep "^samba3.rap"
    
    these tests have been removed from skip_smb1_fails
    Additionally
    samba3.rap.basic.netsessiongetinfo was already marked as knownfail
    in ad_dc so in ad_dc_smb1 also true
    
    Signed-off-by: Noel Power <noel.power@suse.com>
    Reviewed-by: Ralph Boehme <slow@samba.org>

--- source3/selftest/tests.py
+++ source3/selftest/tests.py
@@ -750,7 +750,8 @@ for t in tests:
         plansmbtorture4testsuite(t, "ad_dc", '//$SERVER_IP/tmp -U$USERNAME%$PASSWORD')
     elif t == "rpc.spoolss.notify":
         plansmbtorture4testsuite(t, "ad_member", '//$SERVER_IP/tmp -U$DC_USERNAME%$DC_PASSWORD')
-    elif (t in base and t != "base.charset"):
+    elif (t in base and t != "base.charset") \
+        or (t in rap and t != "rap.printing"):
         plansmbtorture4testsuite(t, "nt4_dc_smb1", '//$SERVER_IP/tmp -U$USERNAME%$PASSWORD')
         plansmbtorture4testsuite(t, "ad_dc_smb1", '//$SERVER/tmp -U$USERNAME%$PASSWORD')
     else:
