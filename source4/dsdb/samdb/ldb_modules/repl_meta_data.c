16b377276ee82c04d069666e53deaa95a7633dd4
Andrew Bartlett <abartlet@samba.org>
2019-12-12 14:44:57 +1300

    CVE-2019-14902 dsdb: Change basis of descriptor module deferred processing to be GUIDs
    We can not process on the basis of a DN, as the DN may have changed in a rename,
    not only that this module can see, but also from repl_meta_data below.
    
    Therefore remove all the complex tree-based change processing, leaving only
    a tree-based sort of the possible objects to be changed, and a single
    stopped_dn variable containing the DN to stop processing below (after
    a no-op change).
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=12497
    
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>

--- source4/dsdb/samdb/ldb_modules/repl_meta_data.c
+++ source4/dsdb/samdb/ldb_modules/repl_meta_data.c
@@ -5538,7 +5538,8 @@ static int replmd_replicated_apply_add(struct replmd_replicated_request *ar)
 		 */
 		ret = dsdb_module_schedule_sd_propagation(ar->module,
 							  ar->objs->partition_dn,
-							  msg->dn, true);
+							  ar->objs->objects[ar->index_current].object_guid,
+							  true);
 		if (ret != LDB_SUCCESS) {
 			return replmd_replicated_request_error(ar, ret);
 		}
@@ -6323,7 +6324,7 @@ static int replmd_replicated_apply_merge(struct replmd_replicated_request *ar)
 		 */
 		ret = dsdb_module_schedule_sd_propagation(ar->module,
 							  ar->objs->partition_dn,
-							  msg->dn,
+							  ar->objs->objects[ar->index_current].object_guid,
 							  true);
 		if (ret != LDB_SUCCESS) {
 			return ldb_operr(ldb);
@@ -6343,7 +6344,7 @@ static int replmd_replicated_apply_merge(struct replmd_replicated_request *ar)
 		 */
 		ret = dsdb_module_schedule_sd_propagation(ar->module,
 							  ar->objs->partition_dn,
-							  msg->dn,
+							  ar->objs->objects[ar->index_current].object_guid,
 							  false);
 		if (ret != LDB_SUCCESS) {
 			return ldb_operr(ldb);
