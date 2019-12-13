628ff67b72385c31d6310bfa7547a8f151f6d2fb
Noel Power <noel.power@suse.com>
2019-12-13 14:15:51 +0000

    s3/selftest: Move samba3.unix.whoami to nt4_dc_smb1
    Also remove the associated entry from skip_smb1_fail
    
    Signed-off-by: Noel Power <noel.power@suse.com>
    Reviewed-by: Ralph Boehme <slow@samba.org>

--- source3/selftest/tests.py
+++ source3/selftest/tests.py
@@ -578,7 +578,7 @@ for t in tests:
     elif t == "unix.whoami":
         plansmbtorture4testsuite(t, "nt4_member:local", '//$SERVER/tmp --machine-pass', description="machine account")
         plansmbtorture4testsuite(t, "ad_member:local", '//$SERVER/tmp --machine-pass --option=torture:addc=$DC_SERVER', description="machine account")
-        for env in ["nt4_dc", "nt4_member"]:
+        for env in ["nt4_dc_smb1", "nt4_member"]:
             plansmbtorture4testsuite(t, env, '//$SERVER/tmp -U$DC_USERNAME%$DC_PASSWORD')
             plansmbtorture4testsuite(t, env, '//$SERVER/tmpguest -U%', description='anonymous connection')
         for env in ["ad_dc_smb1", "ad_member"]:
