f8799bf076d647d73aa749f3864d022a366c3904
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2019-12-15 17:08:24 +0100

    lib/ldb/tests: include stdint.h before cmocka.h
    This fix the following build failures:
    
    [2466/3864] Linking bin/default/lib/ldb/ldbmodify
    In file included from /home/buildroot/autobuild/instance-0/output-1/host/opt/ext-toolchain/lib/gcc/mips64el-buildroot-linux-uclibc/5.5.0/include/stdint.h:9:0,
                     from ../../lib/tevent/tevent.h:31,
                     from ../../lib/ldb/include/ldb.h:51,
                     from ../../lib/ldb/tests/test_ldb_dn.c:25:
    /home/buildroot/autobuild/instance-0/output-1/host/mips64el-buildroot-linux-uclibc/sysroot/usr/include/stdint.h:122:27: error: conflicting types for 'uintptr_t'
     typedef unsigned long int uintptr_t;
                               ^
    In file included from ../../lib/ldb/tests/test_ldb_dn.c:23:0:
    /home/buildroot/autobuild/instance-0/output-1/host/mips64el-buildroot-linux-uclibc/sysroot/usr/include/cmocka.h:132:28: note: previous declaration of 'uintptr_t' was here
           typedef unsigned int uintptr_t;
                                ^
    
    In file included from /home/buildroot/autobuild/instance-0/output-1/host/opt/ext-toolchain/lib/gcc/mips64el-buildroot-linux-uclibc/5.5.0/include/stdint.h:9:0,
                     from ../../lib/tevent/tevent.h:31,
                     from ../../lib/ldb/tests/ldb_key_value_test.c:48:
    /home/buildroot/autobuild/instance-0/output-1/host/mips64el-buildroot-linux-uclibc/sysroot/usr/include/stdint.h:122:27: error: conflicting types for 'uintptr_t'
     typedef unsigned long int uintptr_t;
                               ^
    In file included from ../../lib/ldb/tests/ldb_key_value_test.c:43:0:
    /home/buildroot/autobuild/instance-0/output-1/host/mips64el-buildroot-linux-uclibc/sysroot/usr/include/cmocka.h:132:28: note: previous declaration of 'uintptr_t' was here
           typedef unsigned int uintptr_t;
                                ^
    
    Fixes:
     - http://autobuild.buildroot.org/results/9507739b3d5d51024ee9c60b74c2f85d5004e7e2
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=14218
    
    Signed-off-by: Fabrice Fontaine <fontaine.fabrice@gmail.com>
    Reviewed-by: Uri Simchoni <uri@samba.org>
    Reviewed-by: Andreas Schneider <asn@samba.org>

--- lib/ldb/tests/ldb_key_value_test.c
+++ lib/ldb/tests/ldb_key_value_test.c
@@ -39,6 +39,7 @@
  */
 #include <stdarg.h>
 #include <stddef.h>
+#include <stdint.h>
 #include <setjmp.h>
 #include <cmocka.h>
 
--- lib/ldb/tests/ldb_lmdb_size_test.c
+++ lib/ldb/tests/ldb_lmdb_size_test.c
@@ -43,6 +43,7 @@
  */
 #include <stdarg.h>
 #include <stddef.h>
+#include <stdint.h>
 #include <setjmp.h>
 #include <cmocka.h>
 
