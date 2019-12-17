4d9753dfbd8a2d66d86f60aabee627826247aa38
Gary Lockyer <gary@catalyst.net.nz>
2019-12-18 11:17:51 +1300

    lib ldb: fix use after free
    Fix ASAN detected use after free.  No security implications as  the
    talloc_free is followed immediately by the print statement and the value
    printed is an integer
    
    Signed-off-by: Gary Lockyer <gary@catalyst.net.nz>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- lib/ldb/common/ldb.c
+++ lib/ldb/common/ldb.c
@@ -1392,8 +1392,8 @@ int ldb_op_default_callback(struct ldb_request *req, struct ldb_reply *ares)
 	}
 
 	if (ares->type != LDB_REPLY_DONE) {
-		talloc_free(ares);
 		ldb_asprintf_errstring(req->handle->ldb, "Invalid LDB reply type %d", ares->type);
+		TALLOC_FREE(ares);
 		return ldb_request_done(req, LDB_ERR_OPERATIONS_ERROR);
 	}
 
