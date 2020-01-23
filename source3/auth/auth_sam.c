2db313bdb57acb67733e51021a19bd42d245ea75
Stefan Metzmacher <metze@samba.org>
2020-01-23 15:48:39 +0100

    s3:auth_sam: replace confusing FALL_THROUGH; with break;
    There's no real logic change here, but is makes it easier to
    understand.
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=14247
    
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Andreas Schneider <asn@samba.org>
    (cherry picked from commit 85b168c6dac88f5065c0ec6e925937439f2c12ed)

--- source3/auth/auth_sam.c
+++ source3/auth/auth_sam.c
@@ -89,7 +89,7 @@ static NTSTATUS auth_samstrict_auth(const struct auth_context *auth_context,
 				return NT_STATUS_NOT_IMPLEMENTED;
 			}
 
-			FALL_THROUGH;
+			break;
 		case ROLE_DOMAIN_PDC:
 		case ROLE_DOMAIN_BDC:
 			if ( !is_local_name && !is_my_domain ) {
@@ -98,7 +98,7 @@ static NTSTATUS auth_samstrict_auth(const struct auth_context *auth_context,
 				return NT_STATUS_NOT_IMPLEMENTED;
 			}
 
-			FALL_THROUGH;
+			break;
 		default: /* name is ok */
 			break;
 	}
