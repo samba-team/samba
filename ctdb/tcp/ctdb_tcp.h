8cfc1a398d1ada22b7d48114700cc7caf7399a32
Ralph Boehme <slow@samba.org>
2020-02-28 11:36:00 +0100

    ctdb-tcp: rename ctdb_tcp_stop_connection() to ctdb_tcp_stop_outgoing()
    No change in behaviour.  This makes the code self-documenting.
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=14295
    
    Signed-off-by: Ralph Boehme <slow@samba.org>
    Reviewed-by: Martin Schwenke <martin@meltin.net>
    (cherry picked from commit 1e2a967ff41cc29c3a0d7f61a46937c68fdb90ba)

--- ctdb/tcp/ctdb_tcp.h
+++ ctdb/tcp/ctdb_tcp.h
@@ -48,7 +48,7 @@ void ctdb_tcp_node_connect(struct tevent_context *ev, struct tevent_timer *te,
 			   struct timeval t, void *private_data);
 void ctdb_tcp_read_cb(uint8_t *data, size_t cnt, void *args);
 void ctdb_tcp_tnode_cb(uint8_t *data, size_t cnt, void *private_data);
-void ctdb_tcp_stop_connection(struct ctdb_node *node);
+void ctdb_tcp_stop_outgoing(struct ctdb_node *node);
 
 #define CTDB_TCP_ALIGNMENT 8
 
