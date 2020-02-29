d80e1985da727055fab85e51aa0d38eaf4456294
Noel Power <noel.power@suse.com>
2020-02-29 15:49:28 +0000

    ctdb-tcp: move free of inbound queue to TCP restart
    Since commit 77deaadca8e8dbc3c92ea16893099c72f6dc874e, a nodeA which
    had previously accepted a connection from nodeB (where nodeB dies
    e.g. as as result of fencing) when nodeB attempts to connect again
    after restarting is always rejected with
    
     ctdb_listen_event: Incoming queue active, rejecting connection from w.x.y.z
    
    messages.
    
    Consolidate dead node handling in the TCP restart handling.
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=14295
    
    Signed-off-by: Noel Power <noel.power@suse.com>
    Reviewed-by: Ralph Boehme <slow@samba.org>
    Reviewed-by: Martin Schwenke <martin@meltin.net>
    (cherry picked from commit 0ff1b78fc2f0491f9e11131d0040bdaba8873770)

--- ctdb/tcp/tcp_init.c
+++ ctdb/tcp/tcp_init.c
@@ -121,7 +121,7 @@ static void ctdb_tcp_restart(struct ctdb_node *node)
 		node->transport_data, struct ctdb_tcp_node);
 
 	DEBUG(DEBUG_NOTICE,("Tearing down connection to dead node :%d\n", node->pnn));
-
+	TALLOC_FREE(tnode->in_queue);
 	ctdb_tcp_stop_connection(node);
 
 	tnode->connect_te = tevent_add_timer(node->ctdb->ev, tnode,
