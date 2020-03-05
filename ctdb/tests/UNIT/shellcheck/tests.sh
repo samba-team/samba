be3065ea958b27ec297078008c2ce33b4a419820
Martin Schwenke <martin@meltin.net>
2020-03-06 05:58:26 +1100

    ctdb-tests: Make integration.bash pass shellcheck
    Apart from the non-constant sourcing of include files.
    
    Mostly avoidance of quoting warnings.
    
    One subtle change is to simply pass "120" to wait_until_ready() to
    stop warnings that it expects arguments but none are passed (both
    SC2119 and SC2120).  There seems no way to indicate to structure
    function argument handling so that shellcheck realises arguments are
    optional.  In later shellcheck versions, disabling SC2120 for a
    function also silences complaints about its callers... but not all of
    our testing uses "later" shellcheck versions.
    
    Signed-off-by: Martin Schwenke <martin@meltin.net>
    Reviewed-by: Amitay Isaacs <amitay@gmail.com>

--- ctdb/tests/UNIT/shellcheck/tests.sh
+++ ctdb/tests/UNIT/shellcheck/tests.sh
@@ -25,5 +25,6 @@ shellcheck_test -s sh \
 
 shellcheck_test -s bash \
 	"${TEST_SCRIPTS_DIR}/cluster.bash" \
+	"${TEST_SCRIPTS_DIR}/integration.bash" \
 	"${TEST_SCRIPTS_DIR}/integration_local_daemons.bash" \
 	"${TEST_SCRIPTS_DIR}/integration_real_cluster.bash"
