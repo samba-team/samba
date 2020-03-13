097e48fa2c190e79a14730d054e163f6c01a2663
Jeremy Allison <jra@samba.org>
2020-03-13 10:37:30 -0700

    Revert "s3: VFS: vfs_glusterfs. Pass in struct vfs_gluster_pread_state as the callback data to the subreq."
    This reverts commit c6c4e2de22cd3d84f45f5c21e6b09b09274f7f7b.
    
    Now we wait for all aio to finish on all SHUTDOWN_CLOSE
    cases, this is no longer needed.
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=14301
    
    Signed-off-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: Ralph Boehme <slow@samba.org>

--- source3/modules/vfs_glusterfs.c
+++ source3/modules/vfs_glusterfs.c
@@ -766,7 +766,7 @@ static struct tevent_req *vfs_gluster_pread_send(struct vfs_handle_struct
 	if (tevent_req_nomem(subreq, req)) {
 		return tevent_req_post(req, ev);
 	}
-	tevent_req_set_callback(subreq, vfs_gluster_pread_done, state);
+	tevent_req_set_callback(subreq, vfs_gluster_pread_done, req);
 
 	talloc_set_destructor(state, vfs_gluster_pread_state_destructor);
 
@@ -812,9 +812,10 @@ static int vfs_gluster_pread_state_destructor(struct vfs_gluster_pread_state *st
 
 static void vfs_gluster_pread_done(struct tevent_req *subreq)
 {
-	struct vfs_gluster_pread_state *state = tevent_req_callback_data(
-		subreq, struct vfs_gluster_pread_state);
-	struct tevent_req *req = state->req;
+	struct tevent_req *req = tevent_req_callback_data(
+		subreq, struct tevent_req);
+	struct vfs_gluster_pread_state *state = tevent_req_data(
+		req, struct vfs_gluster_pread_state);
 	int ret;
 
 	ret = pthreadpool_tevent_job_recv(subreq);
