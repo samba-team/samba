85cb86d7c6371bb025ee9fb821d29fdf828b53d6
Volker Lendecke <vl@samba.org>
2020-03-26 10:39:24 +0100

    vfs_fruit: Use ARRAY_DEL_ELEMENT() in filter_empty_rsrc_stream()
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Ralph Boehme <slow@samba.org>

--- source3/modules/vfs_fruit.c
+++ source3/modules/vfs_fruit.c
@@ -377,11 +377,7 @@ static bool filter_empty_rsrc_stream(unsigned int *num_streams,
 	}
 
 	TALLOC_FREE(tmp[i].name);
-	if (*num_streams - 1 > i) {
-		memmove(&tmp[i], &tmp[i+1],
-			(*num_streams - i - 1) * sizeof(struct stream_struct));
-	}
-
+	ARRAY_DEL_ELEMENT(tmp, i, *num_streams);
 	*num_streams -= 1;
 	return true;
 }
