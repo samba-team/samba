f2a4eecbb3576b2790613cddc571f0486af9f91f
Volker Lendecke <vl@samba.org>
2020-03-26 11:11:35 +0100

    ldb: Use ARRAY_DEL_ELEMENT() in ldb_dn_extended_filter()
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Ralph Boehme <slow@samba.org>

--- lib/ldb/common/ldb_dn.c
+++ lib/ldb/common/ldb_dn.c
@@ -908,9 +908,8 @@ void ldb_dn_extended_filter(struct ldb_dn *dn, const char * const *accept_list)
 	unsigned int i;
 	for (i=0; i<dn->ext_comp_num; i++) {
 		if (!ldb_attr_in_list(accept_list, dn->ext_components[i].name)) {
-			memmove(&dn->ext_components[i],
-				&dn->ext_components[i+1],
-				(dn->ext_comp_num-(i+1))*sizeof(dn->ext_components[0]));
+			ARRAY_DEL_ELEMENT(
+				dn->ext_components, i, dn->ext_comp_num);
 			dn->ext_comp_num--;
 			i--;
 		}
