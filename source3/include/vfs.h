34b4effc04b44e22809227479da2d794ac88e447
Ralph Boehme <slow@samba.org>
2020-04-03 08:01:45 +0200

    smbd: move files_struct.backup_intent to a bitfield
    Signed-off-by: Ralph Boehme <slow@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/include/vfs.h
+++ source3/include/vfs.h
@@ -371,6 +371,7 @@ typedef struct files_struct {
 		bool initial_delete_on_close : 1;
 		bool delete_on_close : 1;
 		bool is_sparse : 1;
+		bool backup_intent : 1;
 	} fsp_flags;
 
 	struct tevent_timer *update_write_time_event;
@@ -392,8 +393,6 @@ typedef struct files_struct {
 	int current_lock_count; /* Count the number of outstanding locks and pending locks. */
 
 	uint64_t posix_flags;
-	bool backup_intent; /* Handle was successfully opened with backup intent
-				and opener has privilege to do so. */
 	bool use_ofd_locks; /* Are we using open file description locks ? */
 	struct smb_filename *fsp_name;
 	uint32_t name_hash;		/* Jenkins hash of full pathname. */
