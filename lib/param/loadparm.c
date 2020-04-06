4ec90ccda8d4a751bfc1cb03b01d729833be2ad6
Gary Lockyer <gary@catalyst.net.nz>
2020-04-07 09:09:01 +1200

    CVE-2020-10704: smb.conf: Add max ldap request sizes
    Add two new smb.conf parameters to control the maximum permitted ldap
    request size.
    
    Adds:
       ldap max anonymous request size       default 250Kb
       ldap max authenticated request size   default 16Mb
    
    Credit to OSS-Fuzz
    
    REF: https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=20454
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=14334
    
    Signed-off-by: Gary Lockyer <gary@catalyst.net.nz>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- lib/param/loadparm.c
+++ lib/param/loadparm.c
@@ -3008,6 +3008,11 @@ struct loadparm_context *loadparm_init(TALLOC_CTX *mem_ctx)
 
 	lpcfg_do_global_parameter(lp_ctx, "store dos attributes", "yes");
 
+	lpcfg_do_global_parameter(
+		lp_ctx, "ldap max anonymous request size", "256000");
+	lpcfg_do_global_parameter(
+		lp_ctx, "ldap max authenticated request size", "16777216");
+
 	for (i = 0; parm_table[i].label; i++) {
 		if (!(lp_ctx->flags[i] & FLAG_CMDLINE)) {
 			lp_ctx->flags[i] |= FLAG_DEFAULT;
