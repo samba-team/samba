2840bf3700f6bbfa8c1cc851fd5bdf661d131f11
Jeremy Allison <jra@samba.org>
2020-04-13 13:32:07 -0700

    s3: VFS: acl_xattr: Add acl_xattr_get_nt_acl_at().
    Not yet used.
    
    Signed-off-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: Ralph Boehme <slow@samba.org>

--- source3/modules/vfs_acl_xattr.c
+++ source3/modules/vfs_acl_xattr.c
@@ -385,6 +385,24 @@ static NTSTATUS acl_xattr_get_nt_acl(vfs_handle_struct *handle,
 	return status;
 }
 
+static NTSTATUS acl_xattr_get_nt_acl_at(vfs_handle_struct *handle,
+				struct files_struct *dirfsp,
+				const struct smb_filename *smb_fname,
+				uint32_t security_info,
+				TALLOC_CTX *mem_ctx,
+				struct security_descriptor **ppdesc)
+{
+	NTSTATUS status;
+	status = get_nt_acl_common_at(get_acl_blob_at,
+				handle,
+				dirfsp,
+				smb_fname,
+				security_info,
+				mem_ctx,
+				ppdesc);
+	return status;
+}
+
 static NTSTATUS acl_xattr_fset_nt_acl(vfs_handle_struct *handle,
 				      files_struct *fsp,
 				      uint32_t security_info_sent,
@@ -404,6 +422,7 @@ static struct vfs_fn_pointers vfs_acl_xattr_fns = {
 	.fchmod_fn = fchmod_acl_module_common,
 	.fget_nt_acl_fn = acl_xattr_fget_nt_acl,
 	.get_nt_acl_fn = acl_xattr_get_nt_acl,
+	.get_nt_acl_at_fn = acl_xattr_get_nt_acl_at,
 	.fset_nt_acl_fn = acl_xattr_fset_nt_acl,
 	.sys_acl_set_file_fn = sys_acl_set_file_xattr,
 	.sys_acl_set_fd_fn = sys_acl_set_fd_xattr
