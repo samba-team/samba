9df4d6ab4944c6c606502981517f4e345c5c797e
Gary Lockyer <gary@catalyst.net.nz>
2020-05-01 10:42:03 +1200

    Fix clang 9 parentheses-equality warnings
    Signed-off-by: Gary Lockyer <gary@catalyst.net.nz>
    Reviewed-by: Andreas Schneider <asn@samba.org>

--- lib/ldb/ldb_mdb/ldb_mdb.c
+++ lib/ldb/ldb_mdb/ldb_mdb.c
@@ -183,7 +183,7 @@ static int lmdb_store(struct ldb_kv_private *ldb_kv,
 
 	if (flags == TDB_INSERT) {
 		mdb_flags = MDB_NOOVERWRITE;
-	} else if ((flags == TDB_MODIFY)) {
+	} else if (flags == TDB_MODIFY) {
 		/*
 		 * Modifying a record, ensure that it exists.
 		 * This mimics the TDB semantics
