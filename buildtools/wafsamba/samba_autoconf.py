c2b0071460c09eb24d12880e54e2f80c77834ebb
Gary Lockyer <gary@catalyst.net.nz>
2020-05-01 15:03:10 +1200

    Fix clang 9 unused-function warnings
    Signed-off-by: Gary Lockyer <gary@catalyst.net.nz>
    Reviewed-by: Andreas Schneider <asn@samba.org>

--- buildtools/wafsamba/samba_autoconf.py
+++ buildtools/wafsamba/samba_autoconf.py
@@ -794,7 +794,6 @@ int main(void) {
             conf.ADD_NAMED_CFLAGS('PICKY_CFLAGS', '-Wno-error=format-nonliteral', testflags=True)
             conf.ADD_NAMED_CFLAGS('PICKY_CFLAGS', '-Wno-error=for-loop-analysis', testflags=True)
             conf.ADD_NAMED_CFLAGS('PICKY_CFLAGS', '-Wno-error=missing-field-initializers', testflags=True)
-            conf.ADD_NAMED_CFLAGS('PICKY_CFLAGS', '-Wno-error=unused-function', testflags=True)
 
     if Options.options.fatal_errors:
         conf.ADD_CFLAGS('-Wfatal-errors', testflags=True)
