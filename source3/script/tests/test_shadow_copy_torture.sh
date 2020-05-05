c3377b4c02d0eebb1917c5306f9dd6407cadaee0
Jeremy Allison <jra@samba.org>
2020-05-04 17:08:30 -0700

    s3: torture: Add call to smbtorture smb2.twrp.openroot
    Signed-off-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: Ralph Boehme <slow@samba.org>

--- source3/script/tests/test_shadow_copy_torture.sh
+++ source3/script/tests/test_shadow_copy_torture.sh
@@ -75,6 +75,7 @@ test_shadow_copy_write()
         failed=`expr $failed + 1`
 }
 
+
 test_shadow_copy_stream()
 {
     local msg
@@ -104,11 +105,32 @@ EOF
         failed=`expr $failed + 1`
 }
 
+test_shadow_copy_openroot()
+{
+    local msg
+
+    msg=$1
+
+    #delete snapshots from previous tests
+    find $WORKDIR -name ".snapshots" -exec rm -rf {} \; 1>/dev/null 2>&1
+    build_snapshots
+
+    testit "opening shadow copy root of share" \
+	   $SMBTORTURE \
+	   -U$USERNAME%$PASSWORD \
+	   "//$SERVER/shadow_write" \
+	   --option="torture:twrp_snapshot=$SNAPSHOT" \
+	   smb2.twrp.openroot || \
+        failed=`expr $failed + 1`
+}
+
 build_files $WORKDIR
 
 # test open for writing and write behaviour of snapshoted files
 test_shadow_copy_write "write behaviour of snapshoted files"
 
 test_shadow_copy_stream "reading stream of snapshotted file"
+
+test_shadow_copy_openroot "opening root of shadow copy share"
 
 exit $failed
