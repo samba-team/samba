f8eb7545612377216e8265c7de6544856797bf7d
Volker Lendecke <vl@samba.org>
2020-05-25 18:29:26 +0200

    libsmb: Use tevent_req_simple_finish_ntstatus() in cli_unlink_done()
    Save a few lines of code
    
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/libsmb/clifile.c
+++ source3/libsmb/clifile.c
@@ -1920,16 +1920,9 @@ struct tevent_req *cli_unlink_send(TALLOC_CTX *mem_ctx,
 
 static void cli_unlink_done(struct tevent_req *subreq)
 {
-	struct tevent_req *req = tevent_req_callback_data(
-		subreq, struct tevent_req);
-	NTSTATUS status;
-
-	status = cli_smb_recv(subreq, NULL, NULL, 0, NULL, NULL, NULL, NULL);
-	TALLOC_FREE(subreq);
-	if (tevent_req_nterror(req, status)) {
-		return;
-	}
-	tevent_req_done(req);
+	NTSTATUS status = cli_smb_recv(
+		subreq, NULL, NULL, 0, NULL, NULL, NULL, NULL);
+	tevent_req_simple_finish_ntstatus(subreq, status);
 }
 
 static void cli_unlink_done2(struct tevent_req *subreq)
