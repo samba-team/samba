824fa5f45c82e1d0daf801d227ca2caa272ec45e
Björn Baumbach <bb@sernet.de>
2020-05-29 16:05:10 +0200

    python: fix slow's mail address
    Signed-off-by: Björn Baumbach <bb@sernet.de>
    Reviewed-by: Ralph Boehme <slow@samba.org>

--- python/samba/auth_util.py
+++ python/samba/auth_util.py
@@ -1,7 +1,7 @@
 # Unix SMB/CIFS implementation.
 # auth util helpers
 #
-# Copyright (C) Ralph Boehme <slow@sambba.org> 2019
+# Copyright (C) Ralph Boehme <slow@samba.org> 2019
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
