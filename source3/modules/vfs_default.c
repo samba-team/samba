176290eb68f57f953821b33130f595d2cdb79089
Ralph Boehme <slow@samba.org>
2020-06-10 16:01:18 +0200

    vfs_default: realign vfswrap_fgetxattr() args
    Signed-off-by: Ralph Boehme <slow@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/modules/vfs_default.c
+++ source3/modules/vfs_default.c
@@ -3556,7 +3556,11 @@ static ssize_t vfswrap_getxattrat_recv(struct tevent_req *req,
 	return xattr_size;
 }
 
-static ssize_t vfswrap_fgetxattr(struct vfs_handle_struct *handle, struct files_struct *fsp, const char *name, void *value, size_t size)
+static ssize_t vfswrap_fgetxattr(struct vfs_handle_struct *handle,
+				 struct files_struct *fsp,
+				 const char *name,
+				 void *value,
+				 size_t size)
 {
 	return fgetxattr(fsp->fh->fd, name, value, size);
 }
