samba-team/samba

commit c83ce5f4f99aef94530411ec82cc03e9935b352d

lib:util: Fix smbclient -l basename dir
BUG: https://bugzilla.samba.org/show_bug.cgi?id=14345

Pair-Programmed-With: Andreas Schneider <asn@samba.org>
Signed-off-by: Andreas Schneider <asn@samba.org>
Signed-off-by: Amit Kumar <amitkuma@redhat.com>
Reviewed-by: Alexander Bokovoy <ab@samba.org>

Autobuild-User(master): Andreas Schneider <asn@cryptomilk.org>
Autobuild-Date(master): Thu May  7 10:38:29 UTC 2020 on sn-devel-184

lib/util/debug.c
@@ -1140,8 +1140,14 @@ bool reopen_logs_internal(void)
 		dbgc_config[DBGC_ALL].fd = 2;
 		return true;
 
-	case DEBUG_FILE:
+	case DEBUG_FILE: {
+		struct debug_backend *b = debug_find_backend("file");
+
+		if (b != NULL) {
+			b->log_level = dbgc_config[DBGC_ALL].loglevel;
+		}
 		break;
+	}
 	}
 
 	oldumask = umask( 022 );

selftest/knownfail.d/smbclog
@@ -1,1 +1,0 @@
- ^samba3.blackbox.smbclient_log_basename

commit 71488b812fe737df2d3439a6ff3f95bb69b4a5bd

lib/debug: set the correct default backend loglevel to MAX_DEBUG_LEVEL
This fixed a regression introduced by commit
c83ce5f4f99aef94530411ec82cc03e9935b352d for bug #14345.

The backend loglevel globally restricts logging of a particular backend. If this
value is smaller then any explicitly configured logging class, logging for this
class is skipped.

Eg, given the following logging config in smb.conf:

  log level = 1 auth_json_audit:3@/var/log/samba/samba_auth_audit.log

the default class loglevel of 1 (dbgc_config[DBGC_ALL].loglevel) will be
assigned to the backend loglevel.

So even though the logging class auth_json_audit is configured at level 3, this
doesn't become effective as the file backend drops all log messages with a level
below 1.

BUG: https://bugzilla.samba.org/show_bug.cgi?id=14426

Signed-off-by: Ralph Boehme <slow@samba.org>
Reviewed-by: Andreas Schneider <asn@samba.org>

Autobuild-User(master): Andreas Schneider <asn@cryptomilk.org>
Autobuild-Date(master): Thu Jul  2 13:25:29 UTC 2020 on sn-devel-184

lib/util/debug.c
@@ -1146,7 +1146,7 @@ bool reopen_logs_internal(void)
 		b = debug_find_backend("file");
 		assert(b != NULL);
 
-		b->log_level = dbgc_config[DBGC_ALL].loglevel;
+		b->log_level = MAX_DEBUG_LEVEL;
 		break;
 	}
 