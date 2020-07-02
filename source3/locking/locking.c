58bc493c7794923b845647ab16bbfd3aafe07798
Ralph Boehme <slow@samba.org>
2020-07-02 14:09:15 +0200

    smbd: increase loglevel when leases_db_del() with anything then NT_STATUS_NOT_FOUND
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=14428
    
    Signed-off-by: Ralph Boehme <slow@samba.org>
    Reviewed-by: Stefan Metzmacher <metze@samba.org>
    (backported from commit fbb8bbe1243eb2a0351dc2422929278f85a99e26)
    [slow@samba.org: remove_lease_if_stale() does not exist in 4.11]

--- source3/locking/locking.c
+++ source3/locking/locking.c
@@ -704,13 +704,16 @@ static void remove_share_mode_lease(struct share_mode_data *d,
 	}
 
 	{
+		int level = DBGLVL_DEBUG;
 		NTSTATUS status;
 
 		status = leases_db_del(&e->client_guid,
 				       &e->lease_key,
 				       &d->id);
-
-		DEBUG(10, ("%s: leases_db_del returned %s\n", __func__,
+		if (!NT_STATUS_EQUAL(status, NT_STATUS_NOT_FOUND)) {
+			level = DBGLVL_ERR;
+		}
+		DBG_PREFIX(level, ("leases_db_del failed: %s\n",
 			   nt_errstr(status)));
 	}
 }
