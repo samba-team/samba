a4f598fde8cfa564613108397b0a645277cf0ace
David Mulder <dmulder@suse.com>
2020-07-08 15:29:42 -0600

    gpo: Apply Group Policy Message of the day
    Signed-off-by: David Mulder <dmulder@suse.com>
    Reviewed-by: Douglas Bagnall <douglas.bagnall@catalyst.net.nz>

--- python/samba/gp_msgs_ext.py
+++ python/samba/gp_msgs_ext.py
@@ -14,9 +14,51 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
 from samba.gpclass import gp_pol_ext
 
 class gp_msgs_ext(gp_pol_ext):
+    def __str__(self):
+        return 'Unix Settings/Messages'
+
     def process_group_policy(self, deleted_gpo_list, changed_gpo_list,
                              cdir='/etc'):
-        pass
+        for guid, settings in deleted_gpo_list:
+            self.gp_db.set_guid(guid)
+            if str(self) in settings:
+                for attribute, msg in settings[str(self)].items():
+                    if attribute == 'motd':
+                        mfile = os.path.join(cdir, 'motd')
+                    else:
+                        continue
+                    with open(mfile, 'w') as w:
+                        if msg:
+                            w.write(msg)
+                        else:
+                            w.truncate()
+                    self.gp_db.delete(str(self), attribute)
+            self.gp_db.commit()
+
+        for gpo in changed_gpo_list:
+            if gpo.file_sys_path:
+                section_name = 'Software\\Policies\\Samba\\Unix Settings\\Messages'
+                self.gp_db.set_guid(gpo.name)
+                pol_file = 'MACHINE/Registry.pol'
+                path = os.path.join(gpo.file_sys_path, pol_file)
+                pol_conf = self.parse(path)
+                if not pol_conf:
+                    continue
+                for e in pol_conf.entries:
+                    if e.keyname == section_name and e.data.strip():
+                        if e.valuename == 'motd':
+                            mfile = os.path.join(cdir, 'motd')
+                        else:
+                            continue
+                        if os.path.exists(mfile):
+                            old_val = open(mfile, 'r').read()
+                        else:
+                            old_val = ''
+                        with open(mfile, 'w') as w:
+                            w.write(e.data)
+                            self.gp_db.store(str(self), e.valuename, old_val)
+                        self.gp_db.commit()
