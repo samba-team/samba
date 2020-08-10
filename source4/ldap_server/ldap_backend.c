3824ce0de3bc53a35f414685dc78719429abfc19
Volker Lendecke <vl@samba.org>
2020-08-10 16:24:04 +0200

    ldap_server: Terminate LDAP connections on krb ticket expiry
    See RFC4511 section 4.4.1 and
    
    https://lists.samba.org/archive/cifs-protocol/2020-August/003515.html
    
    for details: Windows terminates LDAP connections when the krb5 ticket
    expires, Samba should do the same. This patch slightly deviates from
    Windows behaviour by sending a LDAP exop response with msgid 0 that is
    ASN1-encoded conforming to RFC4511.
    
    Bug: https://bugzilla.samba.org/show_bug.cgi?id=14465
    
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Stefan Metzmacher <metze@samba.org>
    (cherry picked from commit eb72f887b0bf91c050fd5d911f58a1b3ff9b8bcc)

--- source4/ldap_server/ldap_backend.c
+++ source4/ldap_server/ldap_backend.c
@@ -1384,11 +1384,48 @@ static NTSTATUS ldapsrv_AbandonRequest(struct ldapsrv_call *call)
 	return NT_STATUS_OK;
 }
 
+static NTSTATUS ldapsrv_expired(struct ldapsrv_call *call)
+{
+	struct ldapsrv_reply *reply = NULL;
+	struct ldap_ExtendedResponse *r = NULL;
+
+	DBG_DEBUG("Sending connection expired message\n");
+
+	reply = ldapsrv_init_reply(call, LDAP_TAG_ExtendedResponse);
+	if (reply == NULL) {
+		return NT_STATUS_NO_MEMORY;
+	}
+
+	/*
+	 * According to RFC4511 section 4.4.1 this has a msgid of 0
+	 */
+	reply->msg->messageid = 0;
+
+	r = &reply->msg->r.ExtendedResponse;
+	r->response.resultcode = LDB_ERR_UNAVAILABLE;
+	r->response.errormessage = "The server has timed out this connection";
+	r->oid = "1.3.6.1.4.1.1466.20036"; /* see rfc4511 section 4.4.1 */
+
+	ldapsrv_queue_reply(call, reply);
+	return NT_STATUS_OK;
+}
+
 NTSTATUS ldapsrv_do_call(struct ldapsrv_call *call)
 {
 	unsigned int i;
 	struct ldap_message *msg = call->request;
+	struct ldapsrv_connection *conn = call->conn;
 	NTSTATUS status;
+	bool expired;
+
+	expired = timeval_expired(&conn->limits.expire_time);
+	if (expired) {
+		status = ldapsrv_expired(call);
+		if (!NT_STATUS_IS_OK(status)) {
+			return status;
+		}
+		return NT_STATUS_NETWORK_SESSION_EXPIRED;
+	}
 
 	/* Check for undecoded critical extensions */
 	for (i=0; msg->controls && msg->controls[i]; i++) {
