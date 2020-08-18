5cfe884b39327d5208d4d7059b242e9c3dd881e5
Christof Schmitt <cs@samba.org>
2020-08-18 13:54:07 -0700

    test_vfs_gpfs: Add test for lease mapping function
    Signed-off-by: Christof Schmitt <cs@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/modules/test_vfs_gpfs.c
+++ source3/modules/test_vfs_gpfs.c
@@ -43,10 +43,18 @@ static void test_share_deny_mapping(void **state)
 			 0); /* GPFS limitation, cannot deny only delete. */
 }
 
+static void test_gpfs_lease_mapping(void **state)
+{
+	assert_int_equal(lease_type_to_gpfs(F_RDLCK), GPFS_LEASE_READ);
+	assert_int_equal(lease_type_to_gpfs(F_WRLCK), GPFS_LEASE_WRITE);
+	assert_int_equal(lease_type_to_gpfs(F_UNLCK), GPFS_LEASE_NONE);
+}
+
 int main(int argc, char **argv)
 {
 	const struct CMUnitTest tests[] = {
 		cmocka_unit_test(test_share_deny_mapping),
+		cmocka_unit_test(test_gpfs_lease_mapping),
 	};
 
 	cmocka_set_message_output(CM_OUTPUT_SUBUNIT);
