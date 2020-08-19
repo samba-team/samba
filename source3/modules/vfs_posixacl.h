ff40135a7cbc39a26436a6c35a6b3f32d912e555
Christof Schmitt <cs@samba.org>
2020-08-19 15:43:44 -0700

    s3:VFS: Remove function declaration for vfs_posixacl_init
    Signed-off-by: Christof Schmitt <cs@samba.org>
    Reviewed-by: Volker Lendecke <vl@samba.org>

--- source3/modules/vfs_posixacl.h
+++ source3/modules/vfs_posixacl.h
@@ -42,7 +42,5 @@ int posixacl_sys_acl_set_fd(vfs_handle_struct *handle,
 int posixacl_sys_acl_delete_def_file(vfs_handle_struct *handle,
 				const struct smb_filename *smb_fname);
 
-NTSTATUS vfs_posixacl_init(TALLOC_CTX *);
-
 #endif
 
