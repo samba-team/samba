e759ecdae11aff98847236cab46dd26fbb7ba90b
Martin Schwenke <martin@meltin.net>
2020-09-30 10:48:38 +1000

    ctdb-tests: Strengthen node state checking in ctdb disable/enable test
    Check that the desired state is set on all nodes instead of just the
    test node.  This ensures that node flags have correctly propagated
    across the cluster.
    
    RN: Fix remaining ctdb disable/enable bug
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=14513
    Signed-off-by: Martin Schwenke <martin@meltin.net>
    Reviewed-by: Amitay Isaacs <amitay@gmail.com>
    
    Autobuild-User(master): Amitay Isaacs <amitay@samba.org>
    Autobuild-Date(master): Tue Oct  6 04:32:06 UTC 2020 on sn-devel-184
    
    (cherry picked from commit b68105b8f7c20692d23d457f2777edcf44f12bb8)
    Signed-off-by: Martin Schwenke <martin@meltin.net>

--- ctdb/tests/INTEGRATION/failover/pubips.030.disable_enable.sh
+++ ctdb/tests/INTEGRATION/failover/pubips.030.disable_enable.sh
@@ -21,10 +21,10 @@ select_test_node_and_ips
 
 echo "Disabling node $test_node"
 try_command_on_node 1 $CTDB disable -n $test_node
-wait_until_node_has_status $test_node disabled
+wait_until_node_has_status $test_node disabled 30 all
 wait_until_node_has_no_ips "$test_node"
 
 echo "Re-enabling node $test_node"
 try_command_on_node 1 $CTDB enable -n $test_node
-wait_until_node_has_status $test_node enabled
+wait_until_node_has_status $test_node enabled 30 all
 wait_until_node_has_some_ips "$test_node"
