945bdc7c2350ba2edc6fd40bfcf2d5ac3f451b5c
Ralph Boehme <slow@samba.org>
2020-10-27 19:21:48 +0100

    smbd: we DO NEED the low level fd
    In order to make everything handle based, we will need the basefile handle when
    eg the client requests setting any of the filemetadata that is common across all
    streams, eg the file's timestamps.
    
    Signed-off-by: Ralph Boehme <slow@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/smbd/open.c
+++ source3/smbd/open.c
@@ -5629,8 +5629,6 @@ static NTSTATUS create_file_unixpath(connection_struct *conn,
 				   nt_errstr(status)));
 			goto fail;
 		}
-		/* we don't need the low level fd */
-		fd_close(base_fsp);
 	}
 
 	status = file_new(req, conn, &fsp);
