d1be9eee67412d02169fdef83adca16f37f7e37e
Volker Lendecke <vl@samba.org>
2020-11-25 16:33:32 +0100

    samldb: Align two integer types
    ARRAY_SIZE is size_t
    
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source4/dsdb/samdb/ldb_modules/samldb.c
+++ source4/dsdb/samdb/ldb_modules/samldb.c
@@ -1878,7 +1878,8 @@ static int samldb_prim_group_trigger(struct samldb_ctx *ac)
 static int samldb_check_user_account_control_invariants(struct samldb_ctx *ac,
 						    uint32_t user_account_control)
 {
-	int i, ret = 0;
+	size_t i;
+	int ret = 0;
 	bool need_check = false;
 	const struct uac_to_guid {
 		uint32_t uac;
@@ -1988,7 +1989,8 @@ static int samldb_check_user_account_control_acl(struct samldb_ctx *ac,
 						 uint32_t user_account_control,
 						 uint32_t user_account_control_old)
 {
-	int i, ret = 0;
+	size_t i;
+	int ret = 0;
 	bool need_acl_check = false;
 	struct ldb_result *res;
 	const char * const sd_attrs[] = {"ntSecurityDescriptor", NULL};
