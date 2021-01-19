6f4a013677d499b91a103c4f33d8f200e27e44aa
Jeremy Allison <jra@samba.org>
2021-01-19 12:57:51 -0800

    s3: smbd: Change hardlink_internals() to use a real directory fsp for SMB_VFS_LINKAT().
    New VFS change.
    
    Signed-off-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: Ralph Böhme <slow@samba.org>

--- source3/smbd/trans2.c
+++ source3/smbd/trans2.c
@@ -6466,10 +6466,52 @@ NTSTATUS hardlink_internals(TALLOC_CTX *ctx,
 	NTSTATUS status = NT_STATUS_OK;
 	int ret;
 	bool ok;
+	struct smb_filename *parent_fname_old = NULL;
+	struct smb_filename *base_name_old = NULL;
+	struct smb_filename *parent_fname_new = NULL;
+	struct smb_filename *base_name_new = NULL;
 
 	/* source must already exist. */
 	if (!VALID_STAT(smb_fname_old->st)) {
 		status = NT_STATUS_OBJECT_NAME_NOT_FOUND;
+		goto out;
+	}
+
+	/* No links from a directory. */
+	if (S_ISDIR(smb_fname_old->st.st_ex_mode)) {
+		status = NT_STATUS_FILE_IS_A_DIRECTORY;
+		goto out;
+	}
+
+	/* Setting a hardlink to/from a stream isn't currently supported. */
+	ok = is_ntfs_stream_smb_fname(smb_fname_old);
+	if (ok) {
+		DBG_DEBUG("Old name has streams\n");
+		status = NT_STATUS_INVALID_PARAMETER;
+		goto out;
+	}
+	ok = is_ntfs_stream_smb_fname(smb_fname_new);
+	if (ok) {
+		DBG_DEBUG("New name has streams\n");
+		status = NT_STATUS_INVALID_PARAMETER;
+		goto out;
+	}
+
+	status = parent_pathref(talloc_tos(),
+				conn->cwd_fsp,
+				smb_fname_old,
+				&parent_fname_old,
+				&base_name_old);
+	if (!NT_STATUS_IS_OK(status)) {
+		goto out;
+	}
+
+	status = parent_pathref(talloc_tos(),
+				conn->cwd_fsp,
+				smb_fname_new,
+				&parent_fname_new,
+				&base_name_new);
+	if (!NT_STATUS_IS_OK(status)) {
 		goto out;
 	}
 
@@ -6494,34 +6536,14 @@ NTSTATUS hardlink_internals(TALLOC_CTX *ctx,
 		}
 	}
 
-	/* No links from a directory. */
-	if (S_ISDIR(smb_fname_old->st.st_ex_mode)) {
-		status = NT_STATUS_FILE_IS_A_DIRECTORY;
-		goto out;
-	}
-
-	/* Setting a hardlink to/from a stream isn't currently supported. */
-	ok = is_ntfs_stream_smb_fname(smb_fname_old);
-	if (ok) {
-		DBG_DEBUG("Old name has streams\n");
-		status = NT_STATUS_INVALID_PARAMETER;
-		goto out;
-	}
-	ok = is_ntfs_stream_smb_fname(smb_fname_new);
-	if (ok) {
-		DBG_DEBUG("New name has streams\n");
-		status = NT_STATUS_INVALID_PARAMETER;
-		goto out;
-	}
-
 	DEBUG(10,("hardlink_internals: doing hard link %s -> %s\n",
 		  smb_fname_old->base_name, smb_fname_new->base_name));
 
 	ret = SMB_VFS_LINKAT(conn,
-			conn->cwd_fsp,
-			smb_fname_old,
-			conn->cwd_fsp,
-			smb_fname_new,
+			parent_fname_old->fsp,
+			base_name_old,
+			parent_fname_new->fsp,
+			base_name_new,
 			0);
 
 	if (ret != 0) {
@@ -6533,6 +6555,8 @@ NTSTATUS hardlink_internals(TALLOC_CTX *ctx,
 
   out:
 
+	TALLOC_FREE(parent_fname_old);
+	TALLOC_FREE(parent_fname_new);
 	return status;
 }
 
