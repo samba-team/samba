b1ba5d58727eff385eb2d3a570a655376b441ee6
Volker Lendecke <vl@samba.org>
2021-01-27 14:53:15 +0100

    librpc: Fix a typo, while there linewrap the comment
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- librpc/rpc/dcesrv_handles.c
+++ librpc/rpc/dcesrv_handles.c
@@ -49,7 +49,8 @@ struct dcesrv_handle *dcesrv_handle_create(struct dcesrv_call_state *call,
 	struct dom_sid *sid;
 
 	/*
-	 * For simplicty, ensure we abort here for an interface that has no handles (programmer error)
+	 * For simplicity, ensure we abort here for an interface that
+	 * has no handles (programmer error)
 	 */
 	SMB_ASSERT((context->iface->flags & DCESRV_INTERFACE_FLAGS_HANDLES_NOT_USED) == 0);
 
