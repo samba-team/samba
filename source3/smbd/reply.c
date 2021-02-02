acd2c1fed8db0e0acee5158c81fdf93952bcbbd0
Ralph Boehme <slow@samba.org>
2021-02-02 15:58:42 +0100

    smbd: remove NT_STATUS_STOPPED_ON_SYMLINK status code check from rename_internals()
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=14730
    
    Signed-off-by: Ralph Boehme <slow@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>
    (cherry picked from commit 8999c7d69c6312c7e7bef93417ecef93ddbdabf5)

--- source3/smbd/reply.c
+++ source3/smbd/reply.c
@@ -8210,9 +8210,6 @@ NTSTATUS rename_internals(TALLOC_CTX *ctx,
 		vfs_stat(conn, smb_fname_src);
 
 		status = openat_pathref_fsp(conn->cwd_fsp, smb_fname_src);
-		if (NT_STATUS_EQUAL(status, NT_STATUS_STOPPED_ON_SYMLINK)) {
-			status = NT_STATUS_OBJECT_NAME_NOT_FOUND;
-		}
 		if (!NT_STATUS_IS_OK(status)) {
 			DBG_INFO("openat_pathref_fsp [%s] failed: %s\n",
 				 smb_fname_str_dbg(smb_fname_src),
