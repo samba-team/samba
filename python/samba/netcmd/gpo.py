d22196117cd3f87ca1e3790013909d6e72fdb942
David Mulder <dmulder@suse.com>
2021-02-09 06:16:32 -0700

    samba-tool: Add a gpo command for listing VGP Startup Scripts Group Policy
    Signed-off-by: David Mulder <dmulder@suse.com>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- python/samba/netcmd/gpo.py
+++ python/samba/netcmd/gpo.py
@@ -3064,7 +3064,54 @@ samba-tool gpo manage scripts startup list {31B2F340-016D-11D2-945F-00C04FB984F9
     takes_args = ["gpo"]
 
     def run(self, gpo, H=None, sambaopts=None, credopts=None, versionopts=None):
-        pass
+        self.lp = sambaopts.get_loadparm()
+        self.creds = credopts.get_credentials(self.lp, fallback_machine=True)
+
+        # We need to know writable DC to setup SMB connection
+        if H and H.startswith('ldap://'):
+            dc_hostname = H[7:]
+            self.url = H
+        else:
+            dc_hostname = netcmd_finddc(self.lp, self.creds)
+            self.url = dc_url(self.lp, self.creds, dc=dc_hostname)
+
+        # SMB connect to DC
+        conn = smb_connection(dc_hostname,
+                              'sysvol',
+                              lp=self.lp,
+                              creds=self.creds)
+
+        realm = self.lp.get('realm')
+        vgp_xml = '\\'.join([realm.lower(), 'Policies', gpo,
+                                'MACHINE\\VGP\\VTLA\\Unix',
+                                'Scripts\\Startup\\manifest.xml'])
+        try:
+            xml_data = ET.fromstring(conn.loadfile(vgp_xml))
+        except NTSTATUSError as e:
+            # STATUS_OBJECT_NAME_INVALID, STATUS_OBJECT_NAME_NOT_FOUND,
+            # STATUS_OBJECT_PATH_NOT_FOUND
+            if e.args[0] in [0xC0000033, 0xC0000034, 0xC000003A]:
+                return # The file doesn't exist, so there is nothing to list
+            if e.args[0] == 0xC0000022: # STATUS_ACCESS_DENIED
+                raise CommandError("The authenticated user does "
+                                   "not have sufficient privileges")
+            raise
+
+        policy = xml_data.find('policysetting')
+        data = policy.find('data')
+        for listelement in data.findall('listelement'):
+            script = listelement.find('script')
+            script_path = '\\'.join(['\\', realm.lower(), 'Policies', gpo,
+                                     'MACHINE\\VGP\\VTLA\\Unix\\Scripts',
+                                     'Startup', script.text])
+            parameters = listelement.find('parameters')
+            run_as = listelement.find('run_as')
+            if run_as is not None:
+                run_as = run_as.text
+            else:
+                run_as = 'root'
+            self.outf.write('@reboot %s %s %s' % (run_as, script_path,
+                                                  parameters.text))
 
 class cmd_startup(SuperCommand):
     """Manage Startup Scripts Group Policy Objects"""
