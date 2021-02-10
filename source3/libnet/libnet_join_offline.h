402d9032cbf1ac495202bfd2aabad386ac3e454d
Günther Deschner <gd@samba.org>
2021-02-10 15:56:43 +0100

    s3-libnet_join: add some libnet_JoinCtx-to-ODJ helpers
    Guenther
    
    Signed-off-by: Guenther Deschner <gd@samba.org>
    Reviewed-by: Alexander Bokovoy <ab@samba.org>

---
+++ source3/libnet/libnet_join_offline.h
@@ -0,0 +1,22 @@
+/*
+ *  Unix SMB/CIFS implementation.
+ *  libnet Join offline support
+ *  Copyright (C) Guenther Deschner 2021
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+WERROR libnet_odj_compose_ODJ_PROVISION_DATA(TALLOC_CTX *mem_ctx,
+					     const struct libnet_JoinCtx *r,
+					     struct ODJ_PROVISION_DATA **b_p);
