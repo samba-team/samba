721c604d289b0d1784299d372253653b86cc35d9
Jeremy Allison <jra@samba.org>
2021-03-22 12:21:00 -0700

    s3: VFS: unityed_media: Remove um_removexattr(). No longer called.
    Signed-off-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: Ralph Boehme <slow@samba.org>

--- source3/modules/vfs_unityed_media.c
+++ source3/modules/vfs_unityed_media.c
@@ -1731,34 +1731,6 @@ err:
 	return ret;
 }
 
-static int um_removexattr(struct vfs_handle_struct *handle,
-			  const struct smb_filename *smb_fname,
-			  const char *name)
-{
-	int status;
-	struct smb_filename *client_fname = NULL;
-
-	DEBUG(10, ("Entering um_removexattr\n"));
-
-	if (!is_in_media_files(smb_fname->base_name)) {
-		return SMB_VFS_NEXT_REMOVEXATTR(handle, smb_fname, name);
-	}
-
-	status = alloc_get_client_smb_fname(handle,
-				talloc_tos(),
-				smb_fname,
-				&client_fname);
-	if (status != 0) {
-		goto err;
-	}
-
-	status = SMB_VFS_NEXT_REMOVEXATTR(handle, client_fname, name);
-
-err:
-	TALLOC_FREE(client_fname);
-	return status;
-}
-
 static int um_connect(vfs_handle_struct *handle,
 			 const char *service,
 			 const char *user)
@@ -1848,7 +1820,6 @@ static struct vfs_fn_pointers vfs_um_fns = {
 	.getxattr_fn = um_getxattr,
 	.getxattrat_send_fn = vfs_not_implemented_getxattrat_send,
 	.getxattrat_recv_fn = vfs_not_implemented_getxattrat_recv,
-	.removexattr_fn = um_removexattr,
 };
 
 static_decl_vfs;
