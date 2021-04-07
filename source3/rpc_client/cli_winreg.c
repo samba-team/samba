111118354a1ad956d36bf7e4963dc9c4ac969849
Volker Lendecke <vl@samba.org>
2021-04-07 15:45:18 +0000

    rpc_client: talloc_stackframe() aborts on failure
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/rpc_client/cli_winreg.c
+++ source3/rpc_client/cli_winreg.c
@@ -711,7 +711,7 @@ NTSTATUS dcerpc_winreg_enumvals(TALLOC_CTX *mem_ctx,
 					DATA_BLOB **pdata,
 					WERROR *pwerr)
 {
-	TALLOC_CTX *tmp_ctx;
+	TALLOC_CTX *tmp_ctx = talloc_stackframe();
 	uint32_t num_subkeys = 0, max_subkeylen = 0, max_classlen = 0;
 	uint32_t num_values = 0, max_valnamelen = 0, max_valbufsize = 0;
 	uint32_t secdescsize = 0;
@@ -726,14 +726,6 @@ NTSTATUS dcerpc_winreg_enumvals(TALLOC_CTX *mem_ctx,
 
 	WERROR result = WERR_OK;
 	NTSTATUS status;
-
-	tmp_ctx = talloc_stackframe();
-	if (tmp_ctx == NULL) {
-
-		status = NT_STATUS_NO_MEMORY;
-		*pwerr = ntstatus_to_werror(status);
-		return status;
-	}
 
 	status = dcerpc_winreg_QueryInfoKey(h,
 					    tmp_ctx,
