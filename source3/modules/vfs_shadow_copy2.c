04d6f4a153f322fa1f0055bf59f47e2a218657d1
Noel Power <noel.power@suse.com>
2021-04-09 15:58:42 +0200

    s3/modules: VFS: shadow_copy2: Remove shadow_copy2_chmod
    Signed-off-by: Noel Power <noel.power@suse.com>
    Reviewed-by: Ralph Boehme <slow@samba.org>

--- source3/modules/vfs_shadow_copy2.c
+++ source3/modules/vfs_shadow_copy2.c
@@ -1428,26 +1428,6 @@ static int shadow_copy2_unlinkat(vfs_handle_struct *handle,
 			flags);
 }
 
-static int shadow_copy2_chmod(vfs_handle_struct *handle,
-			const struct smb_filename *smb_fname,
-			mode_t mode)
-{
-	time_t timestamp = 0;
-
-	if (!shadow_copy2_strip_snapshot(talloc_tos(),
-				handle,
-				smb_fname,
-				&timestamp,
-				NULL)) {
-		return -1;
-	}
-	if (timestamp != 0) {
-		errno = EROFS;
-		return -1;
-	}
-	return SMB_VFS_NEXT_CHMOD(handle, smb_fname, mode);
-}
-
 static int shadow_copy2_fchmod(vfs_handle_struct *handle,
                        struct files_struct *fsp,
                        mode_t mode)
@@ -3192,7 +3172,6 @@ static struct vfs_fn_pointers vfs_shadow_copy2_fns = {
 	.fstat_fn = shadow_copy2_fstat,
 	.openat_fn = shadow_copy2_openat,
 	.unlinkat_fn = shadow_copy2_unlinkat,
-	.chmod_fn = shadow_copy2_chmod,
 	.fchmod_fn = shadow_copy2_fchmod,
 	.chdir_fn = shadow_copy2_chdir,
 	.ntimes_fn = shadow_copy2_ntimes,
