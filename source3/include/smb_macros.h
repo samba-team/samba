44f6258ad080939921908d553ca19e982ee729fc
Volker Lendecke <vl@samba.org>
2021-04-13 15:00:39 +0000

    lib: Remove two unused historic macros
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/include/smb_macros.h
+++ source3/include/smb_macros.h
@@ -228,10 +228,7 @@ copy an IP address from one buffer to another
 #define SMB_XMALLOC_ARRAY(type,count) (type *)smb_xmalloc_array(sizeof(type),(count))
 
 #define TALLOC(ctx, size) talloc_named_const(ctx, size, __location__)
-#define TALLOC_ZERO(ctx, size) _talloc_zero(ctx, size, __location__)
 #define TALLOC_SIZE(ctx, size) talloc_named_const(ctx, size, __location__)
-#define TALLOC_ZERO_SIZE(ctx, size) _talloc_zero(ctx, size, __location__)
-
 #define TALLOC_REALLOC(ctx, ptr, count) _talloc_realloc(ctx, ptr, count, __location__)
 #define talloc_destroy(ctx) talloc_free(ctx)
 #ifndef TALLOC_FREE
