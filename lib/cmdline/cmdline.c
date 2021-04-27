9f69e93bad38d45a53219cf248ba92097298b7e7
Andreas Schneider <asn@samba.org>
2021-04-27 16:19:31 +0200

    lib:cmdline: Ignore the return code of cli_credentials_guess()
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- lib/cmdline/cmdline.c
+++ lib/cmdline/cmdline.c
@@ -757,8 +757,15 @@ static void popt_common_credentials_callback(poptContext popt_ctx,
 		/*
 		 * This calls cli_credentials_set_conf() to get the defaults
 		 * form smb.conf and set the winbind separator.
+		 *
+		 * Just warn that we can't read the smb.conf. The might not be
+		 * one available or we want to ignore it.
 		 */
-		cli_credentials_guess(creds, lp_ctx);
+		ok = cli_credentials_guess(creds, lp_ctx);
+		if (!ok) {
+			fprintf(stderr,
+				"Unable to read defaults from smb.conf\n");
+		}
 
 		(void)cli_credentials_get_password_and_obtained(creds,
 								&password_obtained);
