de29a2a07686dfbc0a7c1aba30fb137ff0684b29
Noel Power <noel.power@suse.com>
2021-04-27 16:14:27 +0100

    s3: VFS: glusterfs: Initialise fstreaminfo_fn to NULL
    Signed-off-by: Noel Power <noel.power@suse.com>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/modules/vfs_glusterfs.c
+++ source3/modules/vfs_glusterfs.c
@@ -2352,6 +2352,7 @@ static struct vfs_fn_pointers glusterfs_fns = {
 	.chflags_fn = vfs_gluster_chflags,
 	.file_id_create_fn = NULL,
 	.streaminfo_fn = NULL,
+	.fstreaminfo_fn = NULL,
 	.get_real_filename_fn = vfs_gluster_get_real_filename,
 	.connectpath_fn = vfs_gluster_connectpath,
 	.create_dfs_pathat_fn = vfs_gluster_create_dfs_pathat,
