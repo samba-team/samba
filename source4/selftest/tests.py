8667e6bcdd39c7757283fb1e4c44458e04c5363c
Joseph Sutton <josephsutton@catalyst.net.nz>
2021-04-29 20:58:11 +1200

    python: Add LDAP credentials cache test
    Test that we can use a credentials cache with a user's service ticket
    obtained with our Python code to connect to a service through LDAP.
    
    Signed-off-by: Joseph Sutton <josephsutton@catalyst.net.nz>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=14817
    (cherry picked from commit 7663b5c37fa3413f7c67c018107322494e4a6fd9)

--- source4/selftest/tests.py
+++ source4/selftest/tests.py
@@ -820,6 +820,7 @@ planoldpythontestsuite("ad_dc_default:local", "samba.tests.krb5.s4u_tests",
 planoldpythontestsuite("fl2008r2dc:local", "samba.tests.krb5.xrealm_tests")
 
 planoldpythontestsuite("ad_dc_default", "samba.tests.krb5.test_ccache")
+planoldpythontestsuite("ad_dc_default", "samba.tests.krb5.test_ldap")
 
 for env in ["ad_dc", smbv1_disabled_testenv]:
     planoldpythontestsuite(env, "samba.tests.smb", extra_args=['-U"$USERNAME%$PASSWORD"'])
