b5344570b8ca75da104871c0451b377263b1b823
Jeremy Allison <jra@samba.org>
2021-05-14 14:29:17 -0700

    s3: VFS: aixacl: Add aixacl_sys_acl_delete_def_fd().
    Signed-off-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: Noel Power <noel.power@suse.com>

--- source3/modules/vfs_aixacl.c
+++ source3/modules/vfs_aixacl.c
@@ -190,6 +190,12 @@ int aixacl_sys_acl_delete_def_file(vfs_handle_struct *handle,
 	return 0; /* otherwise you can't set acl at upper level */
 }
 
+int aixacl_sys_acl_delete_def_fd(vfs_handle_struct *handle,
+				 files_struct *fsp)
+{
+	return 0; /* otherwise you can't set acl at upper level */
+}
+
 static struct vfs_fn_pointers vfs_aixacl_fns = {
 	.sys_acl_get_file_fn = aixacl_sys_acl_get_file,
 	.sys_acl_get_fd_fn = aixacl_sys_acl_get_fd,
@@ -197,6 +203,7 @@ static struct vfs_fn_pointers vfs_aixacl_fns = {
 	.sys_acl_blob_get_fd_fn = posix_sys_acl_blob_get_fd,
 	.sys_acl_set_fd_fn = aixacl_sys_acl_set_fd,
 	.sys_acl_delete_def_file_fn = aixacl_sys_acl_delete_def_file,
+	.sys_acl_delete_def_fd_fn = aixacl_sys_acl_delete_def_fd,
 };
 
 static_decl_vfs;
