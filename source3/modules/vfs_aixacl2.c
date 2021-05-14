ca733d8b47a1f1a5b5251cd0549d3ecd7f0eb905
Jeremy Allison <jra@samba.org>
2021-05-14 14:30:29 -0700

    s3: VFS: aixacl2: Add aixjfs2_sys_acl_delete_def_fd().
    Signed-off-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: Noel Power <noel.power@suse.com>

--- source3/modules/vfs_aixacl2.c
+++ source3/modules/vfs_aixacl2.c
@@ -531,6 +531,15 @@ int aixjfs2_sys_acl_delete_def_file(vfs_handle_struct *handle,
 	return 0;
 }
 
+int aixjfs2_sys_acl_delete_def_fd(vfs_handle_struct *handle,
+				files_struct *fsp)
+{
+	/* Not available under AIXC ACL */
+	/* Don't report here any error otherwise */
+	/* upper layer will break the normal execution */
+	return 0;
+}
+
 static struct vfs_fn_pointers vfs_aixacl2_fns = {
 	.fget_nt_acl_fn = aixjfs2_fget_nt_acl,
 	.get_nt_acl_at_fn = aixjfs2_get_nt_acl_at,
@@ -540,7 +549,8 @@ static struct vfs_fn_pointers vfs_aixacl2_fns = {
 	.sys_acl_blob_get_file_fn = aixjfs2_sys_acl_blob_get_file,
 	.sys_acl_blob_get_fd_fn = aixjfs2_sys_acl_blob_get_fd,
 	.sys_acl_set_fd_fn = aixjfs2_sys_acl_set_fd,
-	.sys_acl_delete_def_file_fn = aixjfs2_sys_acl_delete_def_file
+	.sys_acl_delete_def_file_fn = aixjfs2_sys_acl_delete_def_file,
+	.sys_acl_delete_def_fd_fn = aixjfs2_sys_acl_delete_def_fd
 };
 
 static_decl_vfs;
