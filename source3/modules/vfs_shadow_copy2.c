b3d54e20da45f0d00865092ca4df1b5f708b5b7f
Jeremy Allison <jra@samba.org>
2021-06-07 09:56:19 -0700

    s3: VFS: vfs_shadow_copy2: Remove check_access_snapdir().
    No longer used.
    
    Signed-off-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: Ralph Boehme <slow@samba.org>

--- source3/modules/vfs_shadow_copy2.c
+++ source3/modules/vfs_shadow_copy2.c
@@ -1738,45 +1738,6 @@ static char *have_snapdir(struct vfs_handle_struct *handle,
 	return NULL;
 }
 
-#if 0
-static bool check_access_snapdir(struct vfs_handle_struct *handle,
-				const char *path)
-{
-	struct smb_filename smb_fname;
-	int ret;
-	NTSTATUS status;
-
-	ZERO_STRUCT(smb_fname);
-	smb_fname.base_name = talloc_asprintf(talloc_tos(),
-						"%s",
-						path);
-	if (smb_fname.base_name == NULL) {
-		return false;
-	}
-
-	ret = SMB_VFS_NEXT_STAT(handle, &smb_fname);
-	if (ret != 0 || !S_ISDIR(smb_fname.st.st_ex_mode)) {
-		TALLOC_FREE(smb_fname.base_name);
-		return false;
-	}
-
-	status = smbd_check_access_rights(handle->conn,
-					handle->conn->cwd_fsp,
-					&smb_fname,
-					false,
-					SEC_DIR_LIST);
-	if (!NT_STATUS_IS_OK(status)) {
-		DEBUG(0,("user does not have list permission "
-			"on snapdir %s\n",
-			smb_fname.base_name));
-		TALLOC_FREE(smb_fname.base_name);
-		return false;
-	}
-	TALLOC_FREE(smb_fname.base_name);
-	return true;
-}
-#endif
-
 /**
  * Find the snapshot directory (if any) for the given
  * filename (which is relative to the share).
