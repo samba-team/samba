fbfd4183d64e0173550963269a83732c4c3cfd74
Jeremy Allison <jra@samba.org>
2021-06-22 17:19:08 -0700

    s3: VFS: streams_xattr: In streams_xattr_pread() - use the fsp->base_fsp argument for get_ea_value().
    We know it must be valid here.
    
    Signed-off-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: Ralph Boehme <slow@samba.org>

--- source3/modules/vfs_streams_xattr.c
+++ source3/modules/vfs_streams_xattr.c
@@ -1071,8 +1071,12 @@ static ssize_t streams_xattr_pread(vfs_handle_struct *handle,
 		return -1;
 	}
 
-	status = get_ea_value(talloc_tos(), handle->conn, NULL,
-			      smb_fname_base, sio->xattr_name, &ea);
+	status = get_ea_value(talloc_tos(),
+			      handle->conn,
+			      fsp->base_fsp,
+			      NULL,
+			      sio->xattr_name,
+			      &ea);
 	if (!NT_STATUS_IS_OK(status)) {
 		return -1;
 	}
