samba-team/samba

commit 43c808f2ff907497dfff0988ff90a48fdcfc16ef

gensec: Fix CID 1458421 OVERRUN
Signed-off-by: Volker Lendecke <vl@samba.org>
Reviewed-by: Stefan Metzmacher <metze@samba.org>

source4/auth/gensec/gensec_krb5.c
@@ -148,9 +148,8 @@ static NTSTATUS gensec_krb5_start(struct gensec_security *gensec_security, bool 
 		struct samba_sockaddr addr;
 		bool ok;
 
-		addr.sa_socklen = tsocket_address_bsd_sockaddr(tlocal_addr,
-							       &addr.u.sa,
-							       sizeof(addr.u));
+		addr.sa_socklen = tsocket_address_bsd_sockaddr(
+			tlocal_addr, &addr.u.sa, sizeof(addr.u.sa));
 		if (addr.sa_socklen < 0) {
 			talloc_free(gensec_krb5_state);
 			return NT_STATUS_INTERNAL_ERROR;
@@ -168,9 +167,8 @@ static NTSTATUS gensec_krb5_start(struct gensec_security *gensec_security, bool 
 		struct samba_sockaddr addr;
 		bool ok;
 
-		addr.sa_socklen = tsocket_address_bsd_sockaddr(tremote_addr,
-							       &addr.u.sa,
-							       sizeof(addr.u));
+		addr.sa_socklen = tsocket_address_bsd_sockaddr(
+			tremote_addr, &addr.u.sa, sizeof(addr.u.sa));
 		if (addr.sa_socklen < 0) {
 			talloc_free(gensec_krb5_state);
 			return NT_STATUS_INTERNAL_ERROR;

commit 0388a8f33bdde49f1cc805a0291859203c1a52b4

gensec_krb5: restore ipv6 support for kpasswd
We need to offer as much space we have in order to
get the address out of tsocket_address_bsd_sockaddr().

This fixes a regression in commit
43c808f2ff907497dfff0988ff90a48fdcfc16ef.

BUG: https://bugzilla.samba.org/show_bug.cgi?id=14750

Signed-off-by: Stefan Metzmacher <metze@samba.org>
Reviewed-by: Andrew Bartlett <abartlet@samba.org>

source4/auth/gensec/gensec_krb5.c
@@ -149,8 +149,9 @@ static NTSTATUS gensec_krb5_start(struct gensec_security *gensec_security, bool 
 		struct samba_sockaddr addr;
 		bool ok;
 
+		addr.sa_socklen = sizeof(addr.u);
 		sockaddr_ret = tsocket_address_bsd_sockaddr(
-			tlocal_addr, &addr.u.sa, sizeof(addr.u.sa));
+			tlocal_addr, &addr.u.sa, addr.sa_socklen);
 		if (sockaddr_ret < 0) {
 			talloc_free(gensec_krb5_state);
 			return NT_STATUS_INTERNAL_ERROR;
@@ -170,8 +171,9 @@ static NTSTATUS gensec_krb5_start(struct gensec_security *gensec_security, bool 
 		struct samba_sockaddr addr;
 		bool ok;
 
+		addr.sa_socklen = sizeof(addr.u);
 		sockaddr_ret = tsocket_address_bsd_sockaddr(
-			tremote_addr, &addr.u.sa, sizeof(addr.u.sa));
+			tremote_addr, &addr.u.sa, addr.sa_socklen);
 		if (sockaddr_ret < 0) {
 			talloc_free(gensec_krb5_state);
 			return NT_STATUS_INTERNAL_ERROR;