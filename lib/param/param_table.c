41cf9f8966e22a391fa484872a48d9c89abc5db0
Stefan Metzmacher <metze@samba.org>
2021-07-09 12:03:06 +0200

    docs-xml: remove support for "SMB3_10"
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- lib/param/param_table.c
+++ lib/param/param_table.c
@@ -46,7 +46,6 @@ static const struct enum_list enum_protocol[] = {
 	{PROTOCOL_SMB2_10, "SMB2"}, /* for now keep PROTOCOL_SMB2_10 */
 	{PROTOCOL_SMB3_11, "SMB3"}, /* for now keep PROTOCOL_SMB3_11 */
 	{PROTOCOL_SMB3_11, "SMB3_11"},
-	{PROTOCOL_SMB3_10, "SMB3_10"},
 	{PROTOCOL_SMB3_02, "SMB3_02"},
 	{PROTOCOL_SMB3_00, "SMB3_00"},
 	{PROTOCOL_SMB2_10, "SMB2_10"},
