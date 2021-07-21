e52ce697d9ed1962a4fae1dbb5ad09b1883b8dfc
Volker Lendecke <vl@samba.org>
2021-07-21 08:28:20 +0200

    rpcclient: Align integer types
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/rpcclient/cmd_winreg.c
+++ source3/rpcclient/cmd_winreg.c
@@ -99,7 +99,7 @@ static void display_winreg_data(const char *v,
 				uint8_t *data,
 				uint32_t length)
 {
-	int i;
+	size_t i;
 	union winreg_Data r;
 	DATA_BLOB blob = data_blob_const(data, length);
 	WERROR result;
@@ -162,7 +162,7 @@ static WERROR cmd_winreg_querymultiplevalues_ex(struct rpc_pipe_client *cli,
 	struct QueryMultipleValue *values_in, *values_out;
 	uint32_t num_values;
 	uint8_t *buffer = NULL;
-	int i;
+	uint32_t i;
 
 
 	if (argc < 2) {
