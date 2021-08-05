2b5c86118d4c0bba23a0db61413fa288425cc8ba
Pavel Filipenský <pfilipen@redhat.com>
2021-08-05 15:17:57 +0200

    s3:winbind: Move up some code in winbindd_getusersids.c
    Signed-off-by: Pavel Filipenský <pfilipen@redhat.com>
    Reviewed-by: Andreas Schneider <asn@samba.org>

--- source3/winbindd/winbindd_getusersids.c
+++ source3/winbindd/winbindd_getusersids.c
@@ -101,6 +101,9 @@ NTSTATUS winbindd_getusersids_recv(struct tevent_req *req,
 	}
 
 	result = talloc_strdup(response, "");
+	if (result == NULL) {
+		return NT_STATUS_NO_MEMORY;
+	}
 
 	for (i=0; i<state->num_sids; i++) {
 		talloc_asprintf_addbuf(
@@ -109,9 +112,6 @@ NTSTATUS winbindd_getusersids_recv(struct tevent_req *req,
 			dom_sid_str_buf(&state->sids[i], &sidbuf));
 	}
 
-	if (result == NULL) {
-		return NT_STATUS_NO_MEMORY;
-	}
 
 	response->data.num_entries = state->num_sids;
 	response->extra_data.data = result;
