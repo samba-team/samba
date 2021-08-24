af86793af77ab0dfe1c0a9740820c52b435d993d
Stefan Metzmacher <metze@samba.org>
2021-08-24 17:11:24 +0200

    CVE-2020-25719 CVE-2020-25717 tests/krb5: Add tests for connecting to services anonymously and without a PAC
    At the end of the patchset we assume NT_STATUS_NO_IMPERSONATION_TOKEN if
    no PAC is available.
    
    For now we want to look for ACCESS_DENIED as this allows
    the test to pass (showing that gensec:require_pac = true
    is a useful partial mitigation).
    
    This will also help others doing backports that do not
    take the full patch set.
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=14801
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=14799
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=14561
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=14556
    
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- python/samba/tests/krb5/test_rpc.py
+++ python/samba/tests/krb5/test_rpc.py
@@ -20,7 +20,9 @@
 import sys
 import os
 
+from samba import NTSTATUSError, credentials
 from samba.dcerpc import lsa
+from samba.ntstatus import NT_STATUS_ACCESS_DENIED
 
 from samba.tests.krb5.kdc_base_test import KDCBaseTest
 
@@ -37,13 +39,20 @@ class RpcTests(KDCBaseTest):
     """
 
     def test_rpc(self):
+        self._run_rpc_test("rpcusr")
+
+    def test_rpc_no_pac(self):
+        self._run_rpc_test("rpcusr_nopac", include_pac=False,
+                           expect_anon=True, allow_error=True)
+
+    def _run_rpc_test(self, user_name, include_pac=True,
+                      expect_anon=False, allow_error=False):
         # Create a user account and a machine account, along with a Kerberos
         # credentials cache file where the service ticket authenticating the
         # user are stored.
 
         samdb = self.get_samdb()
 
-        user_name = "rpcusr"
         mach_name = samdb.host_dns_name()
         service = "cifs"
 
@@ -59,20 +68,45 @@ class RpcTests(KDCBaseTest):
         (creds, cachefile) = self.create_ccache_with_user(user_credentials,
                                                           mach_credentials,
                                                           service,
-                                                          mach_name)
+                                                          mach_name,
+                                                          pac=include_pac)
+        # Remove the cached credentials file.
+        self.addCleanup(os.remove, cachefile.name)
 
         # Authenticate in-process to the machine account using the user's
         # cached credentials.
 
         binding_str = "ncacn_np:%s[\\pipe\\lsarpc]" % mach_name
-        conn = lsa.lsarpc(binding_str, self.get_lp(), creds)
+        try:
+            conn = lsa.lsarpc(binding_str, self.get_lp(), creds)
+        except NTSTATUSError as e:
+            if not allow_error:
+                self.fail()
+
+            enum, _ = e.args
+            self.assertEqual(NT_STATUS_ACCESS_DENIED, enum)
+            return
 
         (account_name, _) = conn.GetUserName(None, None, None)
 
-        self.assertEqual(user_name, account_name.string)
+        if expect_anon:
+            self.assertNotEqual(user_name, account_name.string)
+        else:
+            self.assertEqual(user_name, account_name.string)
 
-        # Remove the cached credentials file.
-        os.remove(cachefile.name)
+    def test_rpc_anonymous(self):
+        samdb = self.get_samdb()
+        mach_name = samdb.host_dns_name()
+
+        anon_creds = credentials.Credentials()
+        anon_creds.set_anonymous()
+
+        binding_str = "ncacn_np:%s[\\pipe\\lsarpc]" % mach_name
+        conn = lsa.lsarpc(binding_str, self.get_lp(), anon_creds)
+
+        (account_name, _) = conn.GetUserName(None, None, None)
+
+        self.assertEqual('ANONYMOUS LOGON', account_name.string)
 
 
 if __name__ == "__main__":
