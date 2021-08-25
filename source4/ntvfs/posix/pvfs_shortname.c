a41425ebd0f6f2e9e84e2462e7458d04267cd524
Jeremy Allison <jra@samba.org>
2021-08-25 09:57:05 -0700

    s4: ntvfs: Missed comma in 24c09f913d82528ada14013e3d673d277cf04a93, string would be concatenated.
    Sorry for the mistake, I missed that in the review.
    
    Caught by Coverity.
    
    Signed-off-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: Björn Jacke <bjacke@samba.org>
    
    Autobuild-User(master): Jeremy Allison <jra@samba.org>
    Autobuild-Date(master): Wed Aug 25 18:02:05 UTC 2021 on sn-devel-184

--- source4/ntvfs/posix/pvfs_shortname.c
+++ source4/ntvfs/posix/pvfs_shortname.c
@@ -81,7 +81,7 @@
 
 static const char *reserved_names[] = 
 { "AUX", "CON",
-  "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9"
+  "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9",
   "LPT1", "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9",
   "NUL", "PRN", NULL };
 
