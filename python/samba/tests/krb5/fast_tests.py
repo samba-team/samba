57800189c5f4a92058ff293f8583805ebcf9928d
Joseph Sutton <josephsutton@catalyst.net.nz>
2021-08-27 13:26:45 +1200

    tests/krb5: Allow specifying parameters specific to the inner FAST request body
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=14770
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=14817
    
    Signed-off-by: Joseph Sutton <josephsutton@catalyst.net.nz>
    Reviewed-by: Andreas Schneider <asn@samba.org>
    (cherry picked from commit c6d7e19ecfb264c6f79df5a20e830e4ea6fdb340)

--- python/samba/tests/krb5/fast_tests.py
+++ python/samba/tests/krb5/fast_tests.py
@@ -1325,7 +1325,9 @@ class FAST_Tests(KDCBaseTest):
                 auth_data = None
 
             if not use_fast:
+                self.assertNotIn('inner_req', kdc_dict)
                 self.assertNotIn('outer_req', kdc_dict)
+            inner_req = kdc_dict.pop('inner_req', None)
             outer_req = kdc_dict.pop('outer_req', None)
 
             if rep_type == KRB_AS_REP:
@@ -1355,6 +1357,7 @@ class FAST_Tests(KDCBaseTest):
                     armor_tgt=armor_tgt,
                     armor_subkey=armor_subkey,
                     kdc_options=kdc_options,
+                    inner_req=inner_req,
                     outer_req=outer_req)
             else:  # KRB_TGS_REP
                 kdc_exchange_dict = self.tgs_exchange_dict(
@@ -1383,6 +1386,7 @@ class FAST_Tests(KDCBaseTest):
                     auth_data=auth_data,
                     body_checksum_type=None,
                     kdc_options=kdc_options,
+                    inner_req=inner_req,
                     outer_req=outer_req)
 
             repeat = kdc_dict.pop('repeat', 1)
