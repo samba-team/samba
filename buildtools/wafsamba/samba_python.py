5950fc66e017e5712a5bafbce16c92484eee4c16
Andrew Bartlett <abartlet@samba.org>
2021-09-09 20:43:11 +1200

    build: Make Python 3.6 the minimum to build now oss-fuzz is upgraded
    The exception to allow building, but not operating, with Python 3.5
    was only because oss-fuzz provided only Python 3.5 on Ubuntu 16.04.
    
    Ubuntu 20.04 is now the base image provided, so this exception can
    be removed.
    
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Uri Simchoni <uri@samba.org>

--- buildtools/wafsamba/samba_python.py
+++ buildtools/wafsamba/samba_python.py
@@ -7,9 +7,6 @@ from waflib.Configure import conf
 @conf
 def SAMBA_CHECK_PYTHON(conf, version=(3,6,0)):
 
-    if conf.env.enable_fuzzing:
-        version=(3,5,0)
-
     # enable tool to build python extensions
     if conf.env.HAVE_PYTHON_H:
         conf.check_python_version(version)
