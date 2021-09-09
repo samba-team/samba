98c977f44b6086e2c5cec52451078a6ade81d4a8
Ralph Boehme <slow@samba.org>
2021-09-09 18:15:51 +0200

    nmblookup: don't ignore unknown options
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=14828
    
    Signed-off-by: Ralph Boehme <slow@samba.org>
    Reviewed-by: Stefan Metzmacher <metze@samba.org>

--- libcli/nbt/tools/nmblookup.c
+++ libcli/nbt/tools/nmblookup.c
@@ -433,6 +433,11 @@ int main(int argc, const char *argv[])
 		case OPT_CASE_SENSITIVE:
 			options.case_sensitive = true;
 			break;
+		case POPT_ERROR_BADOPT:
+			fprintf(stderr, "\nInvalid option %s: %s\n\n",
+				poptBadOption(pc, 0), poptStrerror(opt));
+			poptPrintUsage(pc, stderr, 0);
+			exit(1);
 		}
 	}
 
