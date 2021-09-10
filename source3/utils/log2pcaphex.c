72a6cf1a8a2903518488cff1bdadd001c5b0b281
Ralph Boehme <slow@samba.org>
2021-09-10 07:05:58 +0200

    log2pcaphex: don't ignore unknown options
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=14828
    
    Signed-off-by: Ralph Boehme <slow@samba.org>
    Reviewed-by: Stefan Metzmacher <metze@samba.org>

--- source3/utils/log2pcaphex.c
+++ source3/utils/log2pcaphex.c
@@ -334,6 +334,11 @@ int main(int argc, const char **argv)
 
 	while((opt = poptGetNextOpt(pc)) != -1) {
 		switch (opt) {
+		case POPT_ERROR_BADOPT:
+			fprintf(stderr, "\nInvalid option %s: %s\n\n",
+				poptBadOption(pc, 0), poptStrerror(opt));
+			poptPrintUsage(pc, stderr, 0);
+			exit(1);
 		}
 	}
 
