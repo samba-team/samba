3755304b6efb98739aca3aa121c095302b09e631
Ralph Boehme <slow@samba.org>
2021-09-10 07:11:43 +0200

    smbcacls: don't ignore unknown options
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=14828
    
    Signed-off-by: Ralph Boehme <slow@samba.org>
    Reviewed-by: Stefan Metzmacher <metze@samba.org>

--- source3/utils/smbcacls.c
+++ source3/utils/smbcacls.c
@@ -1786,6 +1786,11 @@ int main(int argc, char *argv[])
 		case 'x':
 			want_mxac = true;
 			break;
+		case POPT_ERROR_BADOPT:
+			fprintf(stderr, "\nInvalid option %s: %s\n\n",
+				poptBadOption(pc, 0), poptStrerror(opt));
+			poptPrintUsage(pc, stderr, 0);
+			exit(1);
 		}
 	}
 	if (inheritance && !the_acl) {
