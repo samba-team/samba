4056bebf05f4d1e0bfcbc5fe53d63b3bab9e031f
Ralph Boehme <slow@samba.org>
2021-09-10 07:05:02 +0200

    s3/async-tracker: don't ignore unknown options
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=14828
    
    Signed-off-by: Ralph Boehme <slow@samba.org>
    Reviewed-by: Stefan Metzmacher <metze@samba.org>

--- source3/utils/async-tracker.c
+++ source3/utils/async-tracker.c
@@ -267,6 +267,11 @@ int main(int argc, const char **argv)
 		case 't':
 			state->loop_type = TEVENT_LOOP;
 			break;
+		case POPT_ERROR_BADOPT:
+			fprintf(stderr, "\nInvalid option %s: %s\n\n",
+				poptBadOption(pc, 0), poptStrerror(c));
+			poptPrintUsage(pc, stderr, 0);
+			exit(1);
 		}
 	}
 
