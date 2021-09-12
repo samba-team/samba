a99a76722d6046a5d63032e3d2bb3f791da948a6
Joseph Sutton <josephsutton@catalyst.net.nz>
2021-09-13 11:34:56 +1200

    pytest:segfault: Add test for ldb.msg_diff()
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=14642
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=14836
    
    Signed-off-by: Joseph Sutton <josephsutton@catalyst.net.nz>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Douglas Bagnall <douglas.bagnall@catalyst.net.nz>

--- python/samba/tests/segfault.py
+++ python/samba/tests/segfault.py
@@ -210,3 +210,15 @@ class SegfaultTests(samba.tests.TestCase):
         rec = TXTRecord(["a", "b", "c"])
         rec.wType = dnsp.DNS_TYPE_A
         rec.data
+
+    @no_gdb_backtrace
+    @segfault_detector
+    def test_ldb_msg_diff(self):
+        samdb = self.get_samdb()
+
+        msg = ldb.Message()
+        msg.dn = ldb.Dn(samdb, '')
+        diff = samdb.msg_diff(msg, msg)
+
+        del msg
+        diff.dn
