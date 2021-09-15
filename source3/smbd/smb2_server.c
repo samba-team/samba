1744dd8c5bc342a74e397951506468636275fe45
Stefan Metzmacher <metze@samba.org>
2021-09-15 20:27:12 +0200

    smb2_server: don't let SMB2_OP_IOCTL force FILE_CLOSED for invalid file ids
    smbd_smb2_request_process_ioctl() already detailed checks for file_ids,
    which not reached before.
    
    .allow_invalid_fileid = true was only used for SMB2_OP_IOCTL.
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=14788
    
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Ralph Boehme <slow@samba.org>

--- source3/smbd/smb2_server.c
+++ source3/smbd/smb2_server.c
@@ -67,7 +67,6 @@ static const struct smbd_smb2_dispatch_table {
 	bool need_tcon;
 	bool as_root;
 	uint16_t fileid_ofs;
-	bool allow_invalid_fileid;
 	bool modify;
 } smbd_smb2_table[] = {
 #define _OP(o) .opcode = o, .name = #o
@@ -132,7 +131,6 @@ static const struct smbd_smb2_dispatch_table {
 		.need_session = true,
 		.need_tcon = true,
 		.fileid_ofs = 0x08,
-		.allow_invalid_fileid = true,
 		.modify = true,
 	},{
 		_OP(SMB2_OP_CANCEL),
@@ -3323,16 +3321,12 @@ skipped_signing:
 				return smbd_smb2_request_error(req,
 						req->compound_create_err);
 			}
-			if (!call->allow_invalid_fileid) {
-				return smbd_smb2_request_error(req,
-						NT_STATUS_FILE_CLOSED);
-			}
-
-			if (file_id_persistent != UINT64_MAX) {
-				return smbd_smb2_request_error(req,
-						NT_STATUS_FILE_CLOSED);
-			}
-			if (file_id_volatile != UINT64_MAX) {
+			/*
+			 * smbd_smb2_request_process_ioctl()
+			 * has more checks in order to return more
+			 * detailed error codes...
+			 */
+			if (opcode != SMB2_OP_IOCTL) {
 				return smbd_smb2_request_error(req,
 						NT_STATUS_FILE_CLOSED);
 			}
