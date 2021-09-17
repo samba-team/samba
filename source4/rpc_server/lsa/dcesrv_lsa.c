ad462c27a4bcc8aa7262ee75b069841c16c4f0f5
Volker Lendecke <vl@samba.org>
2021-09-17 15:00:36 +0200

    rpc_server4: Fix a typo
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source4/rpc_server/lsa/dcesrv_lsa.c
+++ source4/rpc_server/lsa/dcesrv_lsa.c
@@ -2258,7 +2258,7 @@ done:
 }
 
 /*
-  lsa_SetInfomrationTrustedDomain
+  lsa_SetInformationTrustedDomain
 */
 static NTSTATUS dcesrv_lsa_SetInformationTrustedDomain(
 				struct dcesrv_call_state *dce_call,
