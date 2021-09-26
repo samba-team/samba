eac75fb3b601880f8692dfd4674c49b843cf7275
Andrew Bartlett <abartlet@samba.org>
2021-09-27 12:10:02 +1300

    CVE-2020-25721 auth: Fill in the new HAS_SAM_NAME_AND_SID values
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=14835
    
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Joseph Sutton <josephsutton@catalyst.net.nz>

--- python/samba/tests/krb5/s4u_tests.py
+++ python/samba/tests/krb5/s4u_tests.py
@@ -309,7 +309,6 @@ class S4UKerberosTests(KDCBaseTest):
             tgt=service_tgt,
             authenticator_subkey=authenticator_subkey,
             kdc_options=str(kdc_options),
-            expect_upn_dns_info_ex=False,
             expect_claims=False)
 
         self._generic_kdc_exchange(kdc_exchange_dict,
@@ -611,7 +610,6 @@ class S4UKerberosTests(KDCBaseTest):
             kdc_options=kdc_options,
             pac_options=pac_options,
             expect_edata=expect_edata,
-            expect_upn_dns_info_ex=False,
             expected_proxy_target=expected_proxy_target,
             expected_transited_services=expected_transited_services,
             expect_pac=expect_pac)
