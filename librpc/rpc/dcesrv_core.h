7b62fa967d02f771d4afa9eaeef2f6b2d9f6ccd0
Volker Lendecke <vl@samba.org>
2021-09-27 13:13:11 +0200

    dcesrv_core: Remove unused dcesrv_reinit_context()
    This was only used in the prefork source3 rpc servers
    
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Samuel Cabrero <scabrero@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: Stefan Metzmacher <metze@samba.org>

--- librpc/rpc/dcesrv_core.h
+++ librpc/rpc/dcesrv_core.h
@@ -482,7 +482,6 @@ NTSTATUS dcesrv_init_context(TALLOC_CTX *mem_ctx,
 			     struct loadparm_context *lp_ctx,
 			     struct dcesrv_context_callbacks *cb,
 			     struct dcesrv_context **_dce_ctx);
-NTSTATUS dcesrv_reinit_context(struct dcesrv_context *dce_ctx);
 void dcesrv_context_set_callbacks(
 	struct dcesrv_context *dce_ctx,
 	struct dcesrv_context_callbacks *cb);
