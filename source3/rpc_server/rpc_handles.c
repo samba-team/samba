81a7b2e08ddd4efd330dfeeada5577192f8281bf
Volker Lendecke <vl@samba.org>
2021-10-04 10:25:14 +0200

    rpc_server3: Remove an outdated comment
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/rpc_server/rpc_handles.c
+++ source3/rpc_server/rpc_handles.c
@@ -35,16 +35,6 @@
 
 static size_t num_handles = 0;
 
-/* TODO
- * the following prototypes are declared here to avoid
- * code being moved about too much for a patch to be
- * disrupted / less obvious.
- *
- * these functions, and associated functions that they
- * call, should be moved behind a .so module-loading
- * system _anyway_.  so that's the next step...
- */
-
 int make_base_pipes_struct(TALLOC_CTX *mem_ctx,
 			   struct messaging_context *msg_ctx,
 			   const char *pipe_name,
