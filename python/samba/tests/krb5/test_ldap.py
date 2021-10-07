d46f0d1793bed59026cb517dc7056a51fca1d5cb
Joseph Sutton <josephsutton@catalyst.net.nz>
2021-10-08 11:48:41 +1300

    tests/krb5: Disable debugging output for tests
    This reduces the time spent running the tests in a testenv.
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=14642
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=14881
    
    Signed-off-by: Joseph Sutton <josephsutton@catalyst.net.nz>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>
    (cherry picked from commit dfd613661eec4b81e162f2d86a8fa9266c2fdc03)

--- python/samba/tests/krb5/test_ldap.py
+++ python/samba/tests/krb5/test_ldap.py
@@ -90,7 +90,7 @@ class LdapTests(KDCBaseTest):
 
 
 if __name__ == "__main__":
-    global_asn1_print = True
-    global_hexdump = True
+    global_asn1_print = False
+    global_hexdump = False
     import unittest
     unittest.main()
--- python/samba/tests/krb5/xrealm_tests.py
+++ python/samba/tests/krb5/xrealm_tests.py
@@ -181,7 +181,7 @@ class XrealmKerberosTests(RawKerberosTest):
 
 
 if __name__ == "__main__":
-    global_asn1_print = True
-    global_hexdump = True
+    global_asn1_print = False
+    global_hexdump = False
     import unittest
     unittest.main()
