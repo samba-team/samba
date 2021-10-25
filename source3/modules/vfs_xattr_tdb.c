f254be19d6501a4f573843af97963e350a9ee2ed
Jeremy Allison <jra@samba.org>
2021-10-25 12:02:43 -0700

    s3: VFS: xattr_tdb. Allow unlinkat to cope with dangling symlinks.
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=14879
    
    Signed-off-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: Ralph Boehme <slow@samba.org>

--- source3/modules/vfs_xattr_tdb.c
+++ source3/modules/vfs_xattr_tdb.c
@@ -520,6 +520,16 @@ static int xattr_tdb_unlinkat(vfs_handle_struct *handle,
 		ret = SMB_VFS_NEXT_LSTAT(handle, full_fname);
 	} else {
 		ret = SMB_VFS_NEXT_STAT(handle, full_fname);
+		if (ret == -1 && (errno == ENOENT || errno == ELOOP)) {
+			if (VALID_STAT(smb_fname->st) &&
+					S_ISLNK(smb_fname->st.st_ex_mode)) {
+				/*
+				 * Original name was a link - Could be
+				 * trying to remove a dangling symlink.
+				 */
+				ret = SMB_VFS_NEXT_LSTAT(handle, full_fname);
+			}
+		}
 	}
 	if (ret == -1) {
 		goto out;
