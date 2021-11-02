77a36f23facc22ffcd5ca1e0b04b59b16b5eae2c
Andrew Bartlett <abartlet@samba.org>
2021-11-02 14:02:14 +1300

    CVE-2020-25719 selftest: Always expect a PAC in TGS replies with Heimdal
    This is tested in other places already, but this ensures a global
    check that a TGS-REP has a PAC, regardless.
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=14561
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Joseph Sutton <josephsutton@catalyst.net.nz>

--- source4/selftest/tests.py
+++ source4/selftest/tests.py
@@ -811,7 +811,7 @@ planoldpythontestsuite("ad_dc:local", "samba.tests.dckeytab", extra_args=['-U"$U
 
 have_fast_support = int('SAMBA_USES_MITKDC' in config_hash)
 tkt_sig_support = int('SAMBA4_USES_HEIMDAL' in config_hash)
-expect_pac = 0
+expect_pac = int('SAMBA4_USES_HEIMDAL' in config_hash)
 planoldpythontestsuite("none", "samba.tests.krb5.kcrypto")
 planoldpythontestsuite("ad_dc_default", "samba.tests.krb5.simple_tests",
                        environ={'SERVICE_USERNAME':'$SERVER',
