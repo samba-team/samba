962b7b0f92d37867296b8e30c5ae659e9544a16f
Günther Deschner <gd@samba.org>
2021-11-04 22:22:44 +0100

    s3-winexe: Fix winexe core dump (use-after-free)
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=14893
    
    Guenther
    
    Signed-off-by: Guenther Deschner <gd@samba.org>
    Reviewed-by: Andreas Schneider <asn@samba.org>
    
    Autobuild-User(master): Günther Deschner <gd@samba.org>
    Autobuild-Date(master): Fri Nov  5 11:43:57 UTC 2021 on sn-devel-184
    
    (cherry picked from commit e9495d2ed28a26899dc3dd77bdfe56e284980218)
    
    Autobuild-User(v4-15-test): Jule Anger <janger@samba.org>
    Autobuild-Date(v4-15-test): Wed Nov 10 17:05:18 UTC 2021 on sn-devel-184

--- examples/winexe/winexe.c
+++ examples/winexe/winexe.c
@@ -220,8 +220,6 @@ static void parse_args(int argc, const char *argv[],
 		*port_str = '\0';
 	}
 
-	poptFreeContext(pc);
-
 	if (options->runas == NULL && options->runas_file != NULL) {
 		struct cli_credentials *runas_cred;
 		const char *user;
@@ -253,9 +251,19 @@ static void parse_args(int argc, const char *argv[],
 
 	options->credentials = samba_cmdline_get_creds();
 
-	options->hostname = argv_new[0] + 2;
+	options->hostname = talloc_strdup(mem_ctx, argv_new[0] + 2);
+	if (options->hostname == NULL) {
+		DBG_ERR("Out of memory\n");
+		exit(1);
+	}
 	options->port = port;
-	options->cmd = argv_new[1];
+	options->cmd = talloc_strdup(mem_ctx, argv_new[1]);
+	if (options->cmd == NULL) {
+		DBG_ERR("Out of memory\n");
+		exit(1);
+	}
+
+	poptFreeContext(pc);
 
 	options->flags = flag_interactive;
 	if (flag_reinstall) {
