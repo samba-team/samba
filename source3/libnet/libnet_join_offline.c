3397e04d7149f22604e6213e833190da7ce1a5ac
Andreas Schneider <asn@samba.org>
2021-12-10 15:10:39 +0100

    s3:libnet: Initialize struct ODJ_POLICY_DNS_DOMAIN_INFO
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/libnet/libnet_join_offline.c
+++ source3/libnet/libnet_join_offline.c
@@ -30,7 +30,9 @@ static WERROR libnet_odj_compose_ODJ_WIN7BLOB(TALLOC_CTX *mem_ctx,
 {
 	char *samaccount;
 	uint32_t len;
-	struct ODJ_POLICY_DNS_DOMAIN_INFO i;
+	struct ODJ_POLICY_DNS_DOMAIN_INFO i = {
+		.Sid = NULL,
+	};
 
 	ZERO_STRUCTP(b);
 
