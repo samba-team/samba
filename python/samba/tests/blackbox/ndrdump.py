12464bd4c222d996aac6d6250b7945d63f20f4bc
Stefan Metzmacher <metze@samba.org>
2022-01-21 20:42:45 +0100

    blackbox.ndrdump: fix test_ndrdump_fuzzed_NULL_struct_ntlmssp_CHALLENGE_MESSAGE test
    This actually reveals that ndr_push_string() for TargetName="" was
    failing before because it resulted in 1 byte for a subcontext with
    TargetLen=0.
    
    This is fixed now and we no longer expect ndrdump to exit with 1.
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=14956
    
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Andreas Schneider <asn@samba.org>
    
    Autobuild-User(master): Stefan Metzmacher <metze@samba.org>
    Autobuild-Date(master): Mon Jan 24 16:18:34 UTC 2022 on sn-devel-184

--- python/samba/tests/blackbox/ndrdump.py
+++ python/samba/tests/blackbox/ndrdump.py
@@ -499,10 +499,9 @@ dump OK
     def test_ndrdump_fuzzed_NULL_struct_ntlmssp_CHALLENGE_MESSAGE(self):
         expected =  open(self.data_path("fuzzed_ntlmssp-CHALLENGE_MESSAGE.txt")).read().encode('utf8')
         try:
-            actual = self.check_exit_code(
+            actual = self.check_output(
                 "ndrdump --debug-stdout ntlmssp CHALLENGE_MESSAGE struct --validate --input " +\
-                "'AAAACwIAAAAAJwIAAAAAAAcAAAAAAAAAAIAbhG8uyk9dAL0mQE73MAAAAAAAAAAA' --base64-input",
-                1)
+                "'AAAACwIAAAAAJwIAAAAAAAcAAAAAAAAAAIAbhG8uyk9dAL0mQE73MAAAAAAAAAAA' --base64-input")
         except BlackboxProcessError as e:
             self.fail(e)
 
