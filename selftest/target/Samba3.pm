2fd16c0cbf6bbf8ec9ca349b7a30a7eb1dc49f72
Andreas Schneider <asn@samba.org>
2022-02-09 16:33:10 +0100

    selftest: Do not force -d0 for smbd/nmbd/winbindd
    We have the env variable SERVER_LOG_LEVEL which allows you to change
    the log level on the command line. If we force -d0 this will not work.
    
    make test TESTS="samba" SERVER_LOG_LEVEL=10
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>
    (cherry picked from commit 9693f7ea7383c6a51ab58b7c8255b30206f18a3b)

--- selftest/target/Samba3.pm
+++ selftest/target/Samba3.pm
@@ -2158,7 +2158,7 @@ sub make_bin_cmd
 {
 	my ($self, $binary, $env_vars, $options, $valgrind, $dont_log_stdout) = @_;
 
-	my @optargs = ("-d0");
+	my @optargs = ();
 	if (defined($options)) {
 		@optargs = split(/ /, $options);
 	}
