cb10b8704e8640dbbb4b8c3ca876b890833e54ef
Andreas Schneider <asn@samba.org>
2022-02-21 14:11:19 +0100

    s3:script: Reformat shell scripts
    shfmt -f source3/script/ | xargs shfmt -w -p -i 0 -fn
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>
    
    Autobuild-User(master): Andrew Bartlett <abartlet@samba.org>
    Autobuild-Date(master): Thu Mar  3 01:53:16 UTC 2022 on sn-devel-184

--- source3/script/mksyms.sh
+++ source3/script/mksyms.sh
@@ -11,14 +11,16 @@
 # Copyright (C) 2008 Michael Adam <obnox@samba.org>
 #
 
-LANG=C; export LANG
-LC_ALL=C; export LC_ALL
-LC_COLLATE=C; export LC_COLLATE
+LANG=C
+export LANG
+LC_ALL=C
+export LC_ALL
+LC_COLLATE=C
+export LC_COLLATE
 
-if [ $# -lt 2 ]
-then
-  echo "Usage: $0 awk output_file header_files"
-  exit 1
+if [ $# -lt 2 ]; then
+	echo "Usage: $0 awk output_file header_files"
+	exit 1
 fi
 
 awk="$1"
@@ -28,18 +30,17 @@ symsfile="$1"
 shift
 symsfile_tmp="$symsfile.$$.tmp~"
 
-proto_src="`echo $@ | tr ' ' '\n' | sort | uniq `"
+proto_src="$(echo $@ | tr ' ' '\n' | sort | uniq)"
 
 echo creating $symsfile
 
-mkdir -p `dirname $symsfile`
+mkdir -p $(dirname $symsfile)
 
-${awk} -f `dirname $0`/mksyms.awk $proto_src > $symsfile_tmp
+${awk} -f $(dirname $0)/mksyms.awk $proto_src >$symsfile_tmp
 
-if cmp -s $symsfile $symsfile_tmp 2>/dev/null
-then
-  echo "$symsfile unchanged"
-  rm $symsfile_tmp
+if cmp -s $symsfile $symsfile_tmp 2>/dev/null; then
+	echo "$symsfile unchanged"
+	rm $symsfile_tmp
 else
-  mv $symsfile_tmp $symsfile
+	mv $symsfile_tmp $symsfile
 fi
