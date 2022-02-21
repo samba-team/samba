7fab6d53737dd66f941a6877b86e059e7bc5bc63
Andreas Schneider <asn@samba.org>
2022-02-21 11:17:52 +0100

    lib:tdb: Reformat shell scripts
    shfmt -f lib/tdb/ | xargs shfmt -w -p -i 0 -fn
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- lib/tdb/test/test_tdbbackup.sh
+++ lib/tdb/test/test_tdbbackup.sh
@@ -4,29 +4,32 @@
 
 if [ $# -lt 1 ]; then
 	echo "Usage: $0 LDBFILE"
-	exit 1;
+	exit 1
 fi
 
 LDBFILE=$1
 
-timestamp() {
-  date -u +'time: %Y-%m-%d %H:%M:%S.%6NZ' | sed 's/\..*NZ$/.000000Z/'
+timestamp()
+{
+	date -u +'time: %Y-%m-%d %H:%M:%S.%6NZ' | sed 's/\..*NZ$/.000000Z/'
 }
 
-subunit_fail_test () {
-  timestamp
-  printf 'failure: %s [\n' "$1"
-  cat -
-  echo "]"
+subunit_fail_test()
+{
+	timestamp
+	printf 'failure: %s [\n' "$1"
+	cat -
+	echo "]"
 }
 
-testit () {
+testit()
+{
 	name="$1"
 	shift
 	cmdline="$@"
 	timestamp
 	printf 'test: %s\n' "$1"
-	output=`$cmdline 2>&1`
+	output=$($cmdline 2>&1)
 	status=$?
 	if [ x$status = x0 ]; then
 		timestamp
@@ -37,16 +40,16 @@ testit () {
 	return $status
 }
 
-$BINDIR/tdbdump $LDBFILE | sort > orig_dump
+$BINDIR/tdbdump $LDBFILE | sort >orig_dump
 
 testit "normal tdbbackup on tdb file" $BINDIR/tdbbackup $LDBFILE -s .bak
-$BINDIR/tdbdump $LDBFILE.bak | sort > bak_dump
+$BINDIR/tdbdump $LDBFILE.bak | sort >bak_dump
 testit "cmp between tdbdumps of original and backup" cmp orig_dump bak_dump
 rm $LDBFILE.bak
 rm bak_dump
 
 testit "readonly tdbbackup on tdb file" $BINDIR/tdbbackup $LDBFILE -s .bak -r
-$BINDIR/tdbdump $LDBFILE.bak | sort > bak_dump
+$BINDIR/tdbdump $LDBFILE.bak | sort >bak_dump
 testit "cmp between tdbdumps of original and back dbs" cmp orig_dump bak_dump
 rm $LDBFILE.bak
 rm bak_dump
