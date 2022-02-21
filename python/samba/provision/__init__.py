8c58c14cd66504ffde4cd49e6fb4a4c681957a2f
Stefan Metzmacher <metze@samba.org>
2022-02-21 15:08:34 +0100

    provision: use 120 characters for the dns account password
    We should use the same as for the computer account.
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=14984
    
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Andreas Schneider <asn@samba.org>
    (cherry picked from commit 3b91be36581de1007427d539daffdaa62752412d)

--- python/samba/provision/__init__.py
+++ python/samba/provision/__init__.py
@@ -1928,7 +1928,7 @@ def provision_fill(samdb, secrets_ldb, logger, names, paths,
     if machinepass is None:
         machinepass = samba.generate_random_machine_password(120, 120)
     if dnspass is None:
-        dnspass = samba.generate_random_password(128, 255)
+        dnspass = samba.generate_random_password(120, 120)
 
     samdb.transaction_start()
     try:
