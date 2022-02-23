bf9b2d7ae1c3fd1d56c52c776d1bc0002e325e7e
Andreas Schneider <asn@samba.org>
2022-02-23 12:01:11 +0100

    s4:librpc: Reformat shell scripts
    shfmt -f source4/librpc/ | xargs shfmt -w -p -i 0 -fn
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Pavel Filipenský <pfilipensky@samba.org>

--- source4/librpc/scripts/build_idl.sh
+++ source4/librpc/scripts/build_idl.sh
@@ -10,28 +10,28 @@ IDL_FILES="$*"
 PIDL="$PIDL --outputdir $OUTDIR --header --ndr-parser --server --client --python --dcom-proxy --com-header --includedir ../librpc/idl -- "
 
 if [ x$FULLBUILD = xFULL ]; then
-      echo Rebuilding all idl files in $IDLDIR
-      $PIDL $IDL_FILES || exit 1
-      exit 0
+	echo Rebuilding all idl files in $IDLDIR
+	$PIDL $IDL_FILES || exit 1
+	exit 0
 fi
 
 list=""
 
-for f in $IDL_FILES ; do
-    basename=`basename $f .idl`
-    ndr="$OUTDIR/ndr_$basename.c"
-    # blergh - most shells don't have the -nt function
-    if [ -f $ndr ]; then
-	if [ x`find $f -newer $ndr -print` = x$f ]; then
-	    list="$list $f"
+for f in $IDL_FILES; do
+	basename=$(basename $f .idl)
+	ndr="$OUTDIR/ndr_$basename.c"
+	# blergh - most shells don't have the -nt function
+	if [ -f $ndr ]; then
+		if [ x$(find $f -newer $ndr -print) = x$f ]; then
+			list="$list $f"
+		fi
+	else
+		list="$list $f"
 	fi
-    else 
-        list="$list $f"
-    fi
 done
 
 if [ "x$list" != x ]; then
-    $PIDL $list || exit 1
+	$PIDL $list || exit 1
 fi
 
 exit 0
