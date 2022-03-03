8b359fa4df803a8e5790865476f217e15a31c3d1
Andreas Schneider <asn@samba.org>
2022-03-03 15:18:45 +0100

    s3:script: Fix shellcheck errors in test_net_cred_change.sh
    source3/script/tests/test_net_cred_change.sh:13:64: error: Double quote
    array expansions to avoid re-splitting elements. [SC2068]
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Pavel Filipenský <pfilipensky@samba.org>

--- source3/script/tests/test_net_cred_change.sh
+++ source3/script/tests/test_net_cred_change.sh
@@ -10,8 +10,8 @@ fi
 incdir=$(dirname $0)/../../../testprogs/blackbox
 . $incdir/subunit.sh
 testit "1: change machine secret" $VALGRIND $BINDIR/wbinfo --change-secret || failed=$(expr $failed + 1)
-testit "1: validate secret" $VALGRIND $BINDIR/net rpc testjoin $@ || failed=$(expr $failed + 1)
+testit "1: validate secret" $VALGRIND $BINDIR/net rpc testjoin "$@" || failed=$(expr $failed + 1)
 testit "2: change machine secret" $VALGRIND $BINDIR/wbinfo --change-secret || failed=$(expr $failed + 1)
-testit "2: validate secret" $VALGRIND $BINDIR/net rpc testjoin $@ || failed=$(expr $failed + 1)
+testit "2: validate secret" $VALGRIND $BINDIR/net rpc testjoin "$@" || failed=$(expr $failed + 1)
 
 testok $0 $failed
