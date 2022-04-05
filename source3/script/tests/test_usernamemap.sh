26a7e8fa7a36820ce8cda66998f0a4b6aadd2296
Pavel Filipenský <pfilipen@redhat.com>
2022-04-05 14:04:52 +0200

    s3:tests Test "username map" for UNIX groups
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=15041
    
    Signed-off-by: Pavel Filipenský <pfilipen@redhat.com>
    Reviewed-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: Noel Power <npower@samba.org>
    (cherry picked from commit af8747a28bd62937a01fa4648f404bd0b09a44c0)

---
+++ source3/script/tests/test_usernamemap.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# Copyright (c) 2022 Pavel Filipenský <pfilipen@redhat.com>
+#
+# Tests for "username map" smb.conf parameter for UNIX groups
+
+if [ $# -lt 2 ]; then
+cat <<EOF
+Usage: test_usernamemap.sh SERVER SMBCLIENT
+EOF
+exit 1;
+fi
+
+SERVER="$1"
+SMBCLIENT="$2"
+SMBCLIENT="${VALGRIND} ${SMBCLIENT}"
+
+incdir=$(dirname "$0")/../../../testprogs/blackbox
+. "${incdir}"/subunit.sh
+
+failed=0
+
+# jackthemapper is mapped to jacknomapper, so we need jacknomapper password
+testit "jackthemapper" "${SMBCLIENT}" //"${SERVER}"/tmp -U"${SERVER}/jackthemapper%nOmApsEcrEt" -c ls || failed=$((failed + 1))
+# jacknomapper is not mapped, so we need jacknomapper password
+testit "jacknomapper"  "${SMBCLIENT}" //"${SERVER}"/tmp -U"${SERVER}/jacknomapper%nOmApsEcrEt"  -c ls || failed=$((failed + 1))
+
+testok "$0" "${failed}"
