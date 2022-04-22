f1e40238e433846ea14f584cc62f525d9019ba18
Andreas Schneider <asn@samba.org>
2022-04-22 15:34:09 +0200

    s3:tests: Reformat test_force_user_unlink.sh
    shfmt -f source3/script/| xargs shfmt -w -p -i 0 -fn
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- source3/script/tests/test_force_user_unlink.sh
+++ source3/script/tests/test_force_user_unlink.sh
@@ -12,25 +12,26 @@ smbclient="$BINDIR/smbclient"
 error_inject_conf=$(dirname ${SMB_CONF_PATH})/error_inject.conf
 failed=0
 
-test_forced_user_can_delete() {
-    out=$($smbclient -U $DOMAIN/$USERNAME%$PASSWORD //$SERVER_IP/force_user_error_inject -c "rm dir/file")
-    if [ $? -ne 0 ] ; then
-	echo $out
-	return 1
-    fi
-    tmp=$(echo $out | grep NT_STATUS_ )
-    if [ $? -eq 0 ] ; then
-	return 1
-    fi
-    return 0
+test_forced_user_can_delete()
+{
+	out=$($smbclient -U $DOMAIN/$USERNAME%$PASSWORD //$SERVER_IP/force_user_error_inject -c "rm dir/file")
+	if [ $? -ne 0 ]; then
+		echo $out
+		return 1
+	fi
+	tmp=$(echo $out | grep NT_STATUS_)
+	if [ $? -eq 0 ]; then
+		return 1
+	fi
+	return 0
 }
 
-echo "error_inject:unlinkat = EACCES" > ${error_inject_conf}
+echo "error_inject:unlinkat = EACCES" >${error_inject_conf}
 
-$smbclient -U $DOMAIN/$USERNAME%$PASSWORD //$SERVER_IP/force_user_error_inject -c "mkdir dir" || failed=`expr $failed + 1`
-$smbclient -U $DOMAIN/$USERNAME%$PASSWORD //$SERVER_IP/force_user_error_inject -c "put WHATSNEW.txt dir/file" || failed=`expr $failed + 1`
+$smbclient -U $DOMAIN/$USERNAME%$PASSWORD //$SERVER_IP/force_user_error_inject -c "mkdir dir" || failed=$(expr $failed + 1)
+$smbclient -U $DOMAIN/$USERNAME%$PASSWORD //$SERVER_IP/force_user_error_inject -c "put WHATSNEW.txt dir/file" || failed=$(expr $failed + 1)
 
-testit "test_forced_user_can_delete" test_forced_user_can_delete || failed=`expr $failed + 1`
+testit "test_forced_user_can_delete" test_forced_user_can_delete || failed=$(expr $failed + 1)
 
 rm ${error_inject_conf}
 
