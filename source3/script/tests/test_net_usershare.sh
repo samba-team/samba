fdc98ff5560903d64b10980d66826e8c50ef204a
Andreas Schneider <asn@samba.org>
2022-04-22 15:34:09 +0200

    s3:tests: Reformat test_net_usershare.sh
    shfmt -f source3/script/| xargs shfmt -w -p -i 0 -fn
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>
    
    Autobuild-User(master): Andreas Schneider <asn@cryptomilk.org>
    Autobuild-Date(master): Wed Jun  8 07:28:08 UTC 2022 on sn-devel-184

--- source3/script/tests/test_net_usershare.sh
+++ source3/script/tests/test_net_usershare.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 if [ $# -lt 5 ]; then
-cat <<EOF
+	cat <<EOF
 Usage: test_net_usershare.sh SERVER SERVER_IP DOMAIN USERNAME PASSWORD SMBCLIENT <smbclient arguments>
 EOF
-exit 1;
+	exit 1
 fi
 
 SERVER="$1"
@@ -24,11 +24,11 @@ samba_smbcontrol="$samba_bindir/smbcontrol"
 samba_share_dir="$LOCAL_PATH"
 samba_usershare_dir="$samba_share_dir/usershares"
 
-incdir=`dirname $0`/../../../testprogs/blackbox
+incdir=$(dirname $0)/../../../testprogs/blackbox
 . $incdir/subunit.sh
 
-
-test_smbclient() {
+test_smbclient()
+{
 	name="$1"
 	share="$2"
 	cmd="$3"
@@ -44,7 +44,8 @@ test_smbclient() {
 	return $status
 }
 
-test_net_usershare() {
+test_net_usershare()
+{
 	name="$1"
 	cmd="$2"
 	shift
@@ -67,16 +68,16 @@ test_net_usershare() {
 samba_usershare_name="test_usershare_1"
 samba_usershare_path="$samba_usershare_dir/$samba_usershare_name"
 
-testit "create usershare dir for $samba_usershare_name" mkdir --mode=0755 --verbose $samba_usershare_path || failed=`expr $failed + 1`
+testit "create usershare dir for $samba_usershare_name" mkdir --mode=0755 --verbose $samba_usershare_path || failed=$(expr $failed + 1)
 
 test_net_usershare "net usershare add $samba_usershare_name" "add" "$samba_usershare_name" "$samba_usershare_path" "$samba_usershare_name"
 
 test_net_usershare "net usershare info $samba_usershare_name" "info" "$samba_usershare_name"
 
-test_smbclient "smbclient to $samba_usershare_name" "$samba_usershare_name" 'ls' -U$USERNAME%$PASSWORD || failed=`expr $failed + 1`
+test_smbclient "smbclient to $samba_usershare_name" "$samba_usershare_name" 'ls' -U$USERNAME%$PASSWORD || failed=$(expr $failed + 1)
 
 # CLEANUP
 test_net_usershare "net usershare delete $samba_usershare_name" "delete" "$samba_usershare_name"
-testit "remove usershare dir for $samba_usershare_name" rm -rf $samba_usershare_path || failed=`expr $failed + 1`
+testit "remove usershare dir for $samba_usershare_name" rm -rf $samba_usershare_path || failed=$(expr $failed + 1)
 
 exit $failed
