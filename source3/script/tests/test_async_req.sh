a3d0655ee09d9954dd901730372968d5a22e6ccd
Andreas Schneider <asn@samba.org>
2022-04-22 15:34:08 +0200

    s3:tests: Reformat test_async_req.sh
    shfmt -f source3/script/| xargs shfmt -w -p -i 0 -fn
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- source3/script/tests/test_async_req.sh
+++ source3/script/tests/test_async_req.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-incdir=`dirname $0`/../../../testprogs/blackbox
+incdir=$(dirname $0)/../../../testprogs/blackbox
 . $incdir/subunit.sh
 
 failed=0
@@ -9,6 +9,6 @@ SOCKET_WRAPPER_IPV4_NETWORK="127.0.0.0"
 export SOCKET_WRAPPER_IPV4_NETWORK
 
 testit "async_connect_send" $VALGRIND $BINDIR/async_connect_send_test ||
-	failed=`expr $failed + 1`
+	failed=$(expr $failed + 1)
 
 testok $0 $failed
