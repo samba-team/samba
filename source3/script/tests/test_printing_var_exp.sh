07875d856969847d017e40fcefc123188c9838b7
Andreas Schneider <asn@samba.org>
2022-04-22 15:34:10 +0200

    s3:tests: Reformat test_printing_var_exp.sh
    shfmt -f source3/script/| xargs shfmt -w -p -i 0 -fn
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Guenther Deschner <gd@samba.org>

--- source3/script/tests/test_printing_var_exp.sh
+++ source3/script/tests/test_printing_var_exp.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 if [ $# -lt 4 ]; then
-cat <<EOF
+	cat <<EOF
 Usage: test_smbspool.sh SERVER SERVER_IP DOMAIN USERNAME PASSWORD
 EOF
-exit 1;
+	exit 1
 fi
 
 SERVER="$1"
@@ -15,38 +15,39 @@ PASSWORD="$5"
 shift 5
 ADDARGS="$@"
 
-incdir=`dirname $0`/../../../testprogs/blackbox
+incdir=$(dirname $0)/../../../testprogs/blackbox
 . $incdir/subunit.sh
 . $incdir/common_test_fns.inc
 
 smbclient="$BINDIR/smbclient"
 
-test_var_expansion() {
+test_var_expansion()
+{
 	logfile="${SELFTEST_TMPDIR}/${USER}_printing_var_exp.log"
 
 	$smbclient -U $DOMAIN/$USERNAME%$PASSWORD \
-		   //$SERVER_IP/print_var_exp \
-		   -c "print $SRCDIR/testdata/printing/example.ps"
-	if [ $? -ne 0 ] ; then
-	    rm -f "$logfile"
-	    return 1
+		//$SERVER_IP/print_var_exp \
+		-c "print $SRCDIR/testdata/printing/example.ps"
+	if [ $? -ne 0 ]; then
+		rm -f "$logfile"
+		return 1
 	fi
 	cat "$logfile"
 
 	grep "Windows user: $USERNAME" "$logfile"
-	if [ $? -ne 0 ] ; then
-	    rm -f "$logfile"
-	    return 1
+	if [ $? -ne 0 ]; then
+		rm -f "$logfile"
+		return 1
 	fi
 	grep "UNIX user: $USERNAME" "$logfile"
-	if [ $? -ne 0 ] ; then
-	    rm -f "$logfile"
-	    return 1
+	if [ $? -ne 0 ]; then
+		rm -f "$logfile"
+		return 1
 	fi
 	grep "Domain: $DOMAIN" "$logfile"
-	if [ $? -ne 0 ] ; then
-	    rm -f "$logfile"
-	    return 1
+	if [ $? -ne 0 ]; then
+		rm -f "$logfile"
+		return 1
 	fi
 
 	rm -f "$logfile"
@@ -54,7 +55,7 @@ test_var_expansion() {
 }
 
 testit "Test variable expansion for '%U', '%u' and '%D'" \
-	test_var_expansion \
-	|| failed=$(expr $failed + 1)
+	test_var_expansion ||
+	failed=$(expr $failed + 1)
 
 exit $failed
