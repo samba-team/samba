d637255f6bf100c18f3171126d72e5ba570e54fd
Andreas Schneider <asn@samba.org>
2022-04-22 15:34:10 +0200

    s3:tests: Reformat test_smb1_shadow_copy_torture.sh
    shfmt -f source3/script/| xargs shfmt -w -p -i 0 -fn
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Guenther Deschner <gd@samba.org>

--- source3/script/tests/test_smb1_shadow_copy_torture.sh
+++ source3/script/tests/test_smb1_shadow_copy_torture.sh
@@ -4,10 +4,10 @@
 #
 
 if [ $# -lt 7 ]; then
-cat <<EOF
+	cat <<EOF
 Usage: test_shadow_copy SERVER SERVER_IP DOMAIN USERNAME PASSWORD WORKDIR SMBTORTURE
 EOF
-exit 1;
+	exit 1
 fi
 
 SERVER=${1}
@@ -19,7 +19,7 @@ WORKDIR=${6}
 SMBTORTURE="$VALGRIND ${7}"
 shift 7
 
-incdir=`dirname $0`/../../../testprogs/blackbox
+incdir=$(dirname $0)/../../../testprogs/blackbox
 . $incdir/subunit.sh
 
 SNAPSHOT="@GMT-2015.10.31-19.40.30"
@@ -31,42 +31,42 @@ cd $SELFTEST_TMPDIR || exit 1
 # build a hierarchy of files, symlinks, and directories
 build_files()
 {
-    local destdir
-    destdir=$1
+	local destdir
+	destdir=$1
 
-    echo "$content" > $destdir/foo
+	echo "$content" >$destdir/foo
 }
 
 # build a snapshots directory
 build_snapshots()
 {
-    local snapdir
+	local snapdir
 
-    snapdir=$WORKDIR/.snapshots
+	snapdir=$WORKDIR/.snapshots
 
-    mkdir -p $snapdir
-    mkdir $snapdir/$SNAPSHOT
+	mkdir -p $snapdir
+	mkdir $snapdir/$SNAPSHOT
 
-    build_files $snapdir/$SNAPSHOT
+	build_files $snapdir/$SNAPSHOT
 }
 
 test_shadow_copy_openroot()
 {
-    local msg
+	local msg
 
-    msg=$1
+	msg=$1
 
-    #delete snapshots from previous tests
-    find $WORKDIR -name ".snapshots" -exec rm -rf {} \; 1>/dev/null 2>&1
-    build_snapshots
+	#delete snapshots from previous tests
+	find $WORKDIR -name ".snapshots" -exec rm -rf {} \; 1>/dev/null 2>&1
+	build_snapshots
 
-    testit "opening shadow copy root of share over SMB1" \
-	   $SMBTORTURE \
-	   -U$USERNAME%$PASSWORD \
-	   "//$SERVER/shadow_write" \
-	   --option="torture:twrp_snapshot=$SNAPSHOT" \
-           base.smb1-twrp-openroot || \
-        failed=`expr $failed + 1`
+	testit "opening shadow copy root of share over SMB1" \
+		$SMBTORTURE \
+		-U$USERNAME%$PASSWORD \
+		"//$SERVER/shadow_write" \
+		--option="torture:twrp_snapshot=$SNAPSHOT" \
+		base.smb1-twrp-openroot ||
+		failed=$(expr $failed + 1)
 }
 
 build_files $WORKDIR
