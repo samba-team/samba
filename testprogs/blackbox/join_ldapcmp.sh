65b3797352c5f158c0c9c9bd624fed712d86792f
Andreas Schneider <asn@samba.org>
2022-04-22 15:46:04 +0200

    testprogs: Reformat join_ldapcmp.sh
    shfmt -w -p -i 0 -fn testprogs/blackbox/join_ldapcmp.sh
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Pavel Filipenský <pfilipensky@samba.org>

--- testprogs/blackbox/join_ldapcmp.sh
+++ testprogs/blackbox/join_ldapcmp.sh
@@ -1,36 +1,39 @@
 #!/bin/sh
 # Does a join against the testenv's DC and then runs ldapcmp on the resulting DB
 
-. `dirname $0`/subunit.sh
+. $(dirname $0)/subunit.sh
 
 TARGET_DIR="$PREFIX_ABS/join_$SERVER"
 
 cleanup_output_dir()
 {
-    if [ -d $TARGET_DIR ]; then
-        rm -fr $TARGET_DIR
-    fi
+	if [ -d $TARGET_DIR ]; then
+		rm -fr $TARGET_DIR
+	fi
 }
 
 SAMBA_TOOL="$PYTHON $BINDIR/samba-tool"
 
-join_dc() {
-    JOIN_ARGS="--targetdir=$TARGET_DIR --server=$SERVER -U$USERNAME%$PASSWORD"
-    $SAMBA_TOOL domain join $REALM dc $JOIN_ARGS --option="netbios name = TESTJOINDC"
+join_dc()
+{
+	JOIN_ARGS="--targetdir=$TARGET_DIR --server=$SERVER -U$USERNAME%$PASSWORD"
+	$SAMBA_TOOL domain join $REALM dc $JOIN_ARGS --option="netbios name = TESTJOINDC"
 }
 
-demote_joined_dc() {
-    DEMOTE_ARGS="--remove-other-dead-server=TESTJOINDC --server=$SERVER -U$USERNAME%$PASSWORD"
-    $SAMBA_TOOL domain demote $DEMOTE_ARGS
+demote_joined_dc()
+{
+	DEMOTE_ARGS="--remove-other-dead-server=TESTJOINDC --server=$SERVER -U$USERNAME%$PASSWORD"
+	$SAMBA_TOOL domain demote $DEMOTE_ARGS
 }
 
-ldapcmp_result() {
-    DB1_PATH="tdb://$PREFIX_ABS/$SERVER/private/sam.ldb"
-    DB2_PATH="tdb://$TARGET_DIR/private/sam.ldb"
+ldapcmp_result()
+{
+	DB1_PATH="tdb://$PREFIX_ABS/$SERVER/private/sam.ldb"
+	DB2_PATH="tdb://$TARGET_DIR/private/sam.ldb"
 
-    # interSiteTopologyGenerator gets periodically updated. With the restored
-    # testenvs, it can sometimes point to the old/deleted DC object still
-    $SAMBA_TOOL ldapcmp $DB1_PATH $DB2_PATH --filter=interSiteTopologyGenerator
+	# interSiteTopologyGenerator gets periodically updated. With the restored
+	# testenvs, it can sometimes point to the old/deleted DC object still
+	$SAMBA_TOOL ldapcmp $DB1_PATH $DB2_PATH --filter=interSiteTopologyGenerator
 }
 
 cleanup_output_dir
