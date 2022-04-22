c4d0c741ebea571776d4686fe3e46c54c2727995
Andreas Schneider <asn@samba.org>
2022-04-22 15:34:11 +0200

    s3:tests: Reformat test_smbclient_mget.sh
    shfmt -f source3/script/| xargs shfmt -w -p -i 0 -fn
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Guenther Deschner <gd@samba.org>

--- source3/script/tests/test_smbclient_mget.sh
+++ source3/script/tests/test_smbclient_mget.sh
@@ -1,23 +1,29 @@
 #!/bin/sh
 
 if [ $# -lt 6 ]; then
-cat <<EOF
+	cat <<EOF
 Usage: $0 smbclient3 server share user password directory
 EOF
-exit 1;
+	exit 1
 fi
 
-incdir=`dirname $0`/../../../testprogs/blackbox
+incdir=$(dirname $0)/../../../testprogs/blackbox
 . $incdir/subunit.sh
 
 failed=0
 
-SMBCLIENT3="$1"; shift
-SERVER="$1"; shift
-SHARE="$1"; shift
-USERNAME="$1"; shift
-PASSWORD="$1"; shift
-DIRECTORY="$1"; shift
+SMBCLIENT3="$1"
+shift
+SERVER="$1"
+shift
+SHARE="$1"
+shift
+USERNAME="$1"
+shift
+PASSWORD="$1"
+shift
+DIRECTORY="$1"
+shift
 
 cd $SELFTEST_TMPDIR || exit 1
 
@@ -27,15 +33,15 @@ cd $SELFTEST_TMPDIR || exit 1
 name="smbclient mget"
 subunit_start_test "$name"
 output=$("$SMBCLIENT3" //"$SERVER"/"$SHARE" \
-         -U"$USERNAME"%"$PASSWORD" -c "recurse;prompt;mget $DIRECTORY")
+	-U"$USERNAME"%"$PASSWORD" -c "recurse;prompt;mget $DIRECTORY")
 status=$?
 if [ x$status = x0 ]; then
-    subunit_pass_test "$name"
+	subunit_pass_test "$name"
 else
-    echo "$output" | subunit_fail_test "$name"
+	echo "$output" | subunit_fail_test "$name"
 fi
 
-testit "rm foo" rm "$DIRECTORY"/foo || failed=`expr $failed + 1`
-testit "rmdir $DIRECTORY" rmdir "$DIRECTORY" || failed=`expr $failed + 1`
+testit "rm foo" rm "$DIRECTORY"/foo || failed=$(expr $failed + 1)
+testit "rmdir $DIRECTORY" rmdir "$DIRECTORY" || failed=$(expr $failed + 1)
 
 testok $0 $failed
