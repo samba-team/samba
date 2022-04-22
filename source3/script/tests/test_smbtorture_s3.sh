ce6a31d218886679a82153b3f8d5fd42d266896d
Andreas Schneider <asn@samba.org>
2022-04-22 15:34:11 +0200

    s3:tests: Reformat test_smbtorture_s3.sh
    shfmt -f source3/script/| xargs shfmt -w -p -i 0 -fn
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- source3/script/tests/test_smbtorture_s3.sh
+++ source3/script/tests/test_smbtorture_s3.sh
@@ -3,10 +3,10 @@
 # this runs the file serving tests that are expected to pass with samba3
 
 if [ $# -lt 5 ]; then
-cat <<EOF
+	cat <<EOF
 Usage: test_smbtorture_s3.sh TEST UNC USERNAME PASSWORD SMBTORTURE <smbtorture args>
 EOF
-exit 1;
+	exit 1
 fi
 
 t="$1"
@@ -17,12 +17,10 @@ SMBTORTURE="$5"
 shift 5
 ADDARGS="$*"
 
-incdir=`dirname $0`/../../../testprogs/blackbox
+incdir=$(dirname $0)/../../../testprogs/blackbox
 . $incdir/subunit.sh
 
-
-
 failed=0
-testit "smbtorture" $VALGRIND $SMBTORTURE $unc -U"$username"%"$password" $ADDARGS $t || failed=`expr $failed + 1`
+testit "smbtorture" $VALGRIND $SMBTORTURE $unc -U"$username"%"$password" $ADDARGS $t || failed=$(expr $failed + 1)
 
 testok $0 $failed
