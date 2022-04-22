e862d7e7a2975dcfb7246a3e19fdb643c006bb0b
Andreas Schneider <asn@samba.org>
2022-04-22 15:34:08 +0200

    s3:tests: Reformat stream-depot/run.sh
    shfmt -f source3/script/| xargs shfmt -w -p -i 0 -fn
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Guenther Deschner <gd@samba.org>

--- source3/script/tests/stream-depot/run.sh
+++ source3/script/tests/stream-depot/run.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 if [ $# -lt 2 ]; then
-cat <<EOF
+	cat <<EOF
 Usage: run.sh VFSTEST PREFIX
 EOF
-exit 1;
+	exit 1
 fi
 
-TESTBASE=`dirname $0`
+TESTBASE=$(dirname $0)
 VFSTEST=$1
 PREFIX=$2
 shift 2
@@ -15,22 +15,22 @@ ADDARGS="$*"
 VFSTEST_PREFIX=vfstest
 VFSTEST_TMPDIR=$(mktemp -d ${PREFIX}/${VFSTEST_PREFIX}_XXXXXX)
 
-incdir=`dirname $0`/../../../../testprogs/blackbox
+incdir=$(dirname $0)/../../../../testprogs/blackbox
 . $incdir/subunit.sh
 
 failed=0
 
 cd $VFSTEST_TMPDIR || exit 1
 
-testit "vfstest" $VFSTEST -f $TESTBASE/vfstest.cmd $ADDARGS || failed=`expr $failed + 1`
+testit "vfstest" $VFSTEST -f $TESTBASE/vfstest.cmd $ADDARGS || failed=$(expr $failed + 1)
 testname=".streams check"
 subunit_start_test $testname
-NUM=`find .streams | wc -l`
-if [ $NUM -ne 3 ] ; then
-echo "streams_depot left ${NUM} in .streams, expected 3" | subunit_fail_test $testname
-    failed=`expr $failed + 1`
-else 
-    subunit_pass_test $testname
+NUM=$(find .streams | wc -l)
+if [ $NUM -ne 3 ]; then
+	echo "streams_depot left ${NUM} in .streams, expected 3" | subunit_fail_test $testname
+	failed=$(expr $failed + 1)
+else
+	subunit_pass_test $testname
 fi
 
 exit $failed
