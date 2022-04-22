e8938c566795c88cbe1d70e6d9ed0b9b19f0f9d2
Andreas Schneider <asn@samba.org>
2022-04-22 15:34:09 +0200

    s3:tests: Reformat test_local_s3.sh
    shfmt -f source3/script/| xargs shfmt -w -p -i 0 -fn
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Anoop C S <anoopcs@samba.org>
    Reviewed-by: Alexander Bokovoy <ab@samba.org>

--- source3/script/tests/test_local_s3.sh
+++ source3/script/tests/test_local_s3.sh
@@ -3,37 +3,37 @@
 # this runs the file serving tests that are expected to pass with samba3
 
 if [ $# != 0 ]; then
-cat <<EOF
+	cat <<EOF
 Usage: test_local_s3.sh
 EOF
-exit 1;
+	exit 1
 fi
 
-incdir=`dirname $0`/../../../testprogs/blackbox
+incdir=$(dirname $0)/../../../testprogs/blackbox
 . $incdir/subunit.sh
 
 failed=0
 
 cd $SELFTEST_TMPDIR || exit 1
 
-if test -x $BINDIR/talloctort ; then
-	testit "talloctort" $VALGRIND $BINDIR/talloctort || \
-		failed=`expr $failed + 1`
+if test -x $BINDIR/talloctort; then
+	testit "talloctort" $VALGRIND $BINDIR/talloctort ||
+		failed=$(expr $failed + 1)
 else
 	echo "Skipping talloctort"
 fi
 
-testit "replace_testsuite" $VALGRIND $BINDIR/replace_testsuite || \
-    failed=`expr $failed + 1`
+testit "replace_testsuite" $VALGRIND $BINDIR/replace_testsuite ||
+	failed=$(expr $failed + 1)
 
-if test -x $BINDIR/tdbtorture ; then
-	testit "tdbtorture" $VALGRIND $BINDIR/tdbtorture || \
-		failed=`expr $failed + 1`
+if test -x $BINDIR/tdbtorture; then
+	testit "tdbtorture" $VALGRIND $BINDIR/tdbtorture ||
+		failed=$(expr $failed + 1)
 else
 	echo "Skipping tdbtorture"
 fi
 
-testit "smbconftort" $VALGRIND $BINDIR/smbconftort $CONFIGURATION || \
-	failed=`expr $failed + 1`
+testit "smbconftort" $VALGRIND $BINDIR/smbconftort $CONFIGURATION ||
+	failed=$(expr $failed + 1)
 
 testok $0 $failed
