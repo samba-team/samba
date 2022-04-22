7366bd117831c1043b986dd08ec3a0c7b8486cca
Andreas Schneider <asn@samba.org>
2022-04-22 15:34:08 +0200

    s3:tests: Reformat test_dropbox.sh
    shfmt -f source3/script/| xargs shfmt -w -p -i 0 -fn
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- source3/script/tests/test_dropbox.sh
+++ source3/script/tests/test_dropbox.sh
@@ -4,10 +4,10 @@
 #
 
 if [ $# -lt 7 ]; then
-cat <<EOF
+	cat <<EOF
 Usage: $0 SERVER DOMAIN USERNAME PASSWORD PREFIX TARGET_ENV SMBCLIENT
 EOF
-exit 1;
+	exit 1
 fi
 
 SERVER=${1}
@@ -21,7 +21,7 @@ shift 7
 SMBCLIENT="$VALGRIND ${SMBCLIENT}"
 ADDARGS="$@"
 
-incdir=`dirname $0`/../../../testprogs/blackbox
+incdir=$(dirname $0)/../../../testprogs/blackbox
 . $incdir/subunit.sh
 
 failed=0
@@ -29,60 +29,60 @@ failed=0
 # Test listing a share with valid users succeeds
 test_dropbox()
 {
-    local filename="wurst.$$"
-    local filename_path="$PREFIX/$filename"
-    local dropbox_path="$PREFIX/$TARGET_ENV/share/$1/dirmode733"
+	local filename="wurst.$$"
+	local filename_path="$PREFIX/$filename"
+	local dropbox_path="$PREFIX/$TARGET_ENV/share/$1/dirmode733"
 
-    local tmpfile=$PREFIX/smbclient.in.$$
+	local tmpfile=$PREFIX/smbclient.in.$$
 
-    echo "wurstbar" > $filename_path
+	echo "wurstbar" >$filename_path
 
-    cat > $tmpfile <<EOF
+	cat >$tmpfile <<EOF
 lcd $PREFIX
 put $filename dirmode733\\$filename
 quit
 EOF
 
-    # Create dropbox directory and set permissions
-    mkdir -p $dropbox_path
-    chmod 0333 $dropbox_path
+	# Create dropbox directory and set permissions
+	mkdir -p $dropbox_path
+	chmod 0333 $dropbox_path
 
-    local cmd='CLI_FORCE_INTERACTIVE=yes $SMBCLIENT //$SERVER/$1 -U$USERNAME%$PASSWORD $ADDARGS < $tmpfile 2>&1'
-    eval echo "$cmd"
-    out=`eval $cmd`
-    ret=$?
+	local cmd='CLI_FORCE_INTERACTIVE=yes $SMBCLIENT //$SERVER/$1 -U$USERNAME%$PASSWORD $ADDARGS < $tmpfile 2>&1'
+	eval echo "$cmd"
+	out=$(eval $cmd)
+	ret=$?
 
-    # Reset dropbox permissions
-    chmod 0755 $dropbox_path
-    rm -f $tmpfile
+	# Reset dropbox permissions
+	chmod 0755 $dropbox_path
+	rm -f $tmpfile
 
-    if [ $ret != 0 ] ; then
-        echo "Failed accessing share $ret"
-        echo "$out"
+	if [ $ret != 0 ]; then
+		echo "Failed accessing share $ret"
+		echo "$out"
 
-        return 1
-    fi
-    rm -f $filename_path
+		return 1
+	fi
+	rm -f $filename_path
 
-    dropped_file="$dropbox_path/$filename"
-    if [ ! -r "$dropped_file" ]; then
-        echo "Failed to drop file $filename"
-        echo "$out"
-        return 1
-    fi
+	dropped_file="$dropbox_path/$filename"
+	if [ ! -r "$dropped_file" ]; then
+		echo "Failed to drop file $filename"
+		echo "$out"
+		return 1
+	fi
 
-    content=`cat $dropped_file`
-    if [ "$content" != "wurstbar" ]; then
-        echo "Invalid file content: $content"
-        echo "$out"
-        return 1
-    fi
+	content=$(cat $dropped_file)
+	if [ "$content" != "wurstbar" ]; then
+		echo "Invalid file content: $content"
+		echo "$out"
+		return 1
+	fi
 
-    return 0
+	return 0
 }
 
 testit "dropbox dirmode 0733" \
-   test_dropbox dropbox || \
-   failed=`expr $failed + 1`
+	test_dropbox dropbox ||
+	failed=$(expr $failed + 1)
 
 exit $failed
