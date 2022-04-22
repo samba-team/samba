6170f97bb11ac510508afd95927b6762f7bd8cf9
Andreas Schneider <asn@samba.org>
2022-04-22 15:34:09 +0200

    s3:tests: Reformat test_net_rpc_share_allowedusers.sh
    shfmt -f source3/script/| xargs shfmt -w -p -i 0 -fn
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- source3/script/tests/test_net_rpc_share_allowedusers.sh
+++ source3/script/tests/test_net_rpc_share_allowedusers.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 if [ $# -lt 4 ]; then
-cat <<EOF
+	cat <<EOF
 Usage: test_net_rpc_share_allowedusers.sh  SERVER USERNAME PASSWORD PREFIX
 EOF
-exit 1;
+	exit 1
 fi
 
 SERVER="$1"
@@ -14,18 +14,18 @@ PREFIX="$4"
 shift 4
 ADDARGS="$*"
 
-incdir=`dirname $0`/../../../testprogs/blackbox
+incdir=$(dirname $0)/../../../testprogs/blackbox
 . $incdir/subunit.sh
 mkdir -p $PREFIX/private
 net=$BINDIR/net
 # Check for the SID for group "Everyone" as a basic test things are working.
-testit_grep "net_usersidlist" '^ S-1-1-0$' $VALGRIND $net usersidlist $ADDARGS || failed=`expr $failed + 1`
+testit_grep "net_usersidlist" '^ S-1-1-0$' $VALGRIND $net usersidlist $ADDARGS || failed=$(expr $failed + 1)
 # Check "print$" share is listed by default.
-testit_grep "net_rpc_share_allowedusers" '^print\$$' $net usersidlist | $VALGRIND $net rpc share allowedusers -S$SERVER -U$USERNAME%$PASSWORD $ADDARGS || failed=`expr $failed + 1`
+testit_grep "net_rpc_share_allowedusers" '^print\$$' $net usersidlist | $VALGRIND $net rpc share allowedusers -S$SERVER -U$USERNAME%$PASSWORD $ADDARGS || failed=$(expr $failed + 1)
 # Check "print$" share is listed if we ask for it.
-testit_grep "net_rpc_share_allowedusers" '^print\$$' $net usersidlist | $VALGRIND $net rpc share allowedusers -S$SERVER -U$USERNAME%$PASSWORD $ADDARGS - 'print$' || failed=`expr $failed + 1`
+testit_grep "net_rpc_share_allowedusers" '^print\$$' $net usersidlist | $VALGRIND $net rpc share allowedusers -S$SERVER -U$USERNAME%$PASSWORD $ADDARGS - 'print$' || failed=$(expr $failed + 1)
 # Check user "user1" is allowed to read share "tmp".
-testit_grep "net_rpc_share_allowedusers" '^ user1$' $net usersidlist | $VALGRIND $net rpc share allowedusers -S$SERVER -U$USERNAME%$PASSWORD $ADDARGS || failed=`expr $failed + 1`
+testit_grep "net_rpc_share_allowedusers" '^ user1$' $net usersidlist | $VALGRIND $net rpc share allowedusers -S$SERVER -U$USERNAME%$PASSWORD $ADDARGS || failed=$(expr $failed + 1)
 #
 # Subtle extra test for bug https://bugzilla.samba.org/show_bug.cgi?id=13992
 #
@@ -36,14 +36,13 @@ testit_grep "net_rpc_share_allowedusers" '^ user1$' $net usersidlist | $VALGRIND
 # user1 to read from it.
 #
 subunit_start_test "net_rpc_share_allowedusers"
-multi_userout=`$net usersidlist | $VALGRIND $net rpc share allowedusers -S$SERVER -U$USERNAME%$PASSWORD $ADDARGS`
-num_matches=`echo "$multi_userout" | grep -c '^ user1$'`
-if [ "$num_matches" -gt "1" ]
-then
+multi_userout=$($net usersidlist | $VALGRIND $net rpc share allowedusers -S$SERVER -U$USERNAME%$PASSWORD $ADDARGS)
+num_matches=$(echo "$multi_userout" | grep -c '^ user1$')
+if [ "$num_matches" -gt "1" ]; then
 	subunit_pass_test "net_rpc_share_allowedusers"
 else
 	echo "net_rpc_share_allowedusers only found $num_matches shares readable by user1. Should be greater than one.\n"
-	failed=`expr $failed + 1`
+	failed=$(expr $failed + 1)
 	echo "$multi_userout" | subunit_fail_test "net_rpc_share_allowedusers"
 fi
 
