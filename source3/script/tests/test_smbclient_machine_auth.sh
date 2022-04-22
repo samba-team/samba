07d07b70eb8e91c333969962c46b29241b0e98e9
Andreas Schneider <asn@samba.org>
2022-04-22 15:34:11 +0200

    s3:tests: Reformat test_smbclient_machine_auth.sh
    shfmt -f source3/script/| xargs shfmt -w -p -i 0 -fn
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Guenther Deschner <gd@samba.org>

--- source3/script/tests/test_smbclient_machine_auth.sh
+++ source3/script/tests/test_smbclient_machine_auth.sh
@@ -3,10 +3,10 @@
 # this runs the file serving tests that are expected to pass with samba3 against shares with various options
 
 if [ $# -lt 2 ]; then
-cat <<EOF
+	cat <<EOF
 Usage: test_smbclient_machine_auth.sh SERVER SMBCLIENT <smbclient arguments>
 EOF
-exit 1;
+	exit 1
 fi
 
 SERVER="$1"
@@ -15,7 +15,7 @@ SMBCLIENT="$VALGRIND ${SMBCLIENT}"
 shift 2
 ADDARGS="$*"
 
-incdir=`dirname $0`/../../../testprogs/blackbox
+incdir=$(dirname $0)/../../../testprogs/blackbox
 . $incdir/subunit.sh
 
 testit "smbclient //$SERVER/tmp" $SMBCLIENT //$SERVER/tmp --machine-pass -p 139 -c quit $ADDARGS
