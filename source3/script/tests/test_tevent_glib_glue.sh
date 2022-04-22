7dce28b712f227ed2943a2a71546307f8eeb8d23
Andreas Schneider <asn@samba.org>
2022-04-22 15:34:11 +0200

    s3:tests: Reformat test_tevent_glib_glue.sh
    shfmt -f source3/script/| xargs shfmt -w -p -i 0 -fn
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Guenther Deschner <gd@samba.org>

--- source3/script/tests/test_tevent_glib_glue.sh
+++ source3/script/tests/test_tevent_glib_glue.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-incdir=`dirname $0`/../../../testprogs/blackbox
+incdir=$(dirname $0)/../../../testprogs/blackbox
 . $incdir/subunit.sh
 
 failed=0
 TESTNAME="tevent_glib_glue_test"
 
-if [ ! -x $BINDIR/tevent_glib_glue_test ] ; then
+if [ ! -x $BINDIR/tevent_glib_glue_test ]; then
 	subunit_start_test "$TESTNAME"
 	subunit_skip_test "$TESTNAME" <<EOF
 Test needs glib2-devel
@@ -14,8 +14,7 @@ EOF
 	testok $0 $failed
 fi
 
-
 testit "$TESTNAME" $VALGRIND $BINDIR/tevent_glib_glue_test ||
-	failed=`expr $failed + 1`
+	failed=$(expr $failed + 1)
 
 testok $0 $failed
