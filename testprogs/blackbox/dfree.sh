eab5cdb66f4c9760e9313fa160a4a973e75685e5
Andreas Schneider <asn@samba.org>
2022-04-22 15:46:04 +0200

    testprogs: Reformat dfree.sh
    shfmt -w -p -i 0 -fn testprogs/blackbox/dfree.sh
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Pavel Filipenský <pfilipensky@samba.org>

--- testprogs/blackbox/dfree.sh
+++ testprogs/blackbox/dfree.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-if [ "$1" = "." ] ; then
-    echo "1000 10 2048"
-elif [ "$1" = "subdir1" ] ; then
-    echo "2000 20 4096"
+if [ "$1" = "." ]; then
+	echo "1000 10 2048"
+elif [ "$1" = "subdir1" ]; then
+	echo "2000 20 4096"
 else
-    echo "4000 40 8192"
+	echo "4000 40 8192"
 fi
