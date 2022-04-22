58e815f2a2f65da094acf1a769aed3dbfcc44c76
Andreas Schneider <asn@samba.org>
2022-04-22 15:34:11 +0200

    s3:tests: Reformat test_smbclient_log_basename.sh
    shfmt -f source3/script/| xargs shfmt -w -p -i 0 -fn
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Guenther Deschner <gd@samba.org>

--- source3/script/tests/test_smbclient_log_basename.sh
+++ source3/script/tests/test_smbclient_log_basename.sh
@@ -3,10 +3,10 @@
 # this test checks whether smbclient can log into -l log-basename
 
 if [ $# -lt 2 ]; then
-cat <<EOF
+	cat <<EOF
 Usage: test_smbclient_log_basename.sh SERVER SMBCLIENT PREFIX <smbclient arguments>
 EOF
-exit 1;
+	exit 1
 fi
 
 SERVER="$1"
@@ -15,7 +15,7 @@ PREFIX="$3"
 shift 3
 ADDARGS="$*"
 
-incdir=`dirname $0`/../../../testprogs/blackbox
+incdir=$(dirname $0)/../../../testprogs/blackbox
 . $incdir/subunit.sh
 
 failed=0
@@ -27,10 +27,10 @@ test_smbclient_log_basename()
 	rm -rf $LOG_DIR
 	mkdir $LOG_DIR
 	cmd='$VALGRIND $SMBCLIENT -l $LOG_DIR -d3 //$SERVER/IPC\$ $CONFIGURATION -U%badpassword -c quit $ADDARGS'
-	out=`eval $cmd 2>&1`
+	out=$(eval $cmd 2>&1)
 	grep 'Client started' $LOG_DIR/log.smbclient
 }
 
-testit "smbclient log-basename" test_smbclient_log_basename || failed=`expr $failed + 1`
+testit "smbclient log-basename" test_smbclient_log_basename || failed=$(expr $failed + 1)
 
 testok $0 $failed
