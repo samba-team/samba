ce1a0119b7707aed6e47ef15c6bd797711f2a88a
Andreas Schneider <asn@samba.org>
2022-04-22 15:34:08 +0200

    s3:tests: Reformat test_failure.sh
    shfmt -f source3/script/| xargs shfmt -w -p -i 0 -fn
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- source3/script/tests/test_failure.sh
+++ source3/script/tests/test_failure.sh
@@ -4,7 +4,7 @@
 #
 # Copyright (C) 2011 Michael Adam <obnox@samba.org>
 
-incdir=`dirname $0`/../../../testprogs/blackbox
+incdir=$(dirname $0)/../../../testprogs/blackbox
 . $incdir/subunit.sh
 
 failed=0
@@ -20,16 +20,15 @@ test_success()
 }
 
 testit "success" \
-	test_success || \
-	failed=`expr $failed + 1`
+	test_success ||
+	failed=$(expr $failed + 1)
 
 testit "failure" \
-	test_failure || \
-	failed=`expr $failed + 1`
+	test_failure ||
+	failed=$(expr $failed + 1)
 
 testit "success" \
-	test_success || \
-	failed=`expr $failed + 1`
+	test_success ||
+	failed=$(expr $failed + 1)
 
 testok $0 $failed
-
