b1cddccf12c45e48570242c6c6a03ff5974c93d8
Andreas Schneider <asn@samba.org>
2022-04-22 15:34:10 +0200

    s3:tests: Reformat test_sacl_set_get.sh
    shfmt -f source3/script/| xargs shfmt -w -p -i 0 -fn
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Guenther Deschner <gd@samba.org>

--- source3/script/tests/test_sacl_set_get.sh
+++ source3/script/tests/test_sacl_set_get.sh
@@ -6,8 +6,8 @@
 #
 
 if [ $# -lt 7 ]; then
-    echo "Usage: $0 SERVER SERVER_IP USERNAME PASSWORD SMBTORTURE3 NET SHARE"
-    exit 1
+	echo "Usage: $0 SERVER SERVER_IP USERNAME PASSWORD SMBTORTURE3 NET SHARE"
+	exit 1
 fi
 
 SERVER="$1"
@@ -20,25 +20,26 @@ SHARE="$7"
 
 failed=0
 
-incdir=`dirname $0`/../../../testprogs/blackbox
+incdir=$(dirname $0)/../../../testprogs/blackbox
 . $incdir/subunit.sh
 
-sacl_set_get() {
-    out=$($SMBTORTURE3 //$SERVER_IP/$SHARE -U $USERNAME%$PASSWORD SMB2-SACL)
-    if [ $? -ne 0 ] ; then
-	echo "SMB2-SACL failed"
-	echo "$out"
-	return 1
-    fi
+sacl_set_get()
+{
+	out=$($SMBTORTURE3 //$SERVER_IP/$SHARE -U $USERNAME%$PASSWORD SMB2-SACL)
+	if [ $? -ne 0 ]; then
+		echo "SMB2-SACL failed"
+		echo "$out"
+		return 1
+	fi
 }
 
 # Grant SeSecurityPrivilege to the user
-testit "grant SeSecurityPrivilege" $NET rpc rights grant $USERNAME SeSecurityPrivilege -U $USERNAME%$PASSWORD -I $SERVER_IP || failed=`expr $failed + 1`
+testit "grant SeSecurityPrivilege" $NET rpc rights grant $USERNAME SeSecurityPrivilege -U $USERNAME%$PASSWORD -I $SERVER_IP || failed=$(expr $failed + 1)
 
 # Run the tests.
-testit "SACL set_get" sacl_set_get || failed=`expr $failed + 1`
+testit "SACL set_get" sacl_set_get || failed=$(expr $failed + 1)
 
 # Revoke SeSecurityPrivilege
-testit "revoke SeSecurityPrivilege" $NET rpc rights revoke $USERNAME SeSecurityPrivilege -U $USERNAME%$PASSWORD -I $SERVER_IP || failed=`expr $failed + 1`
+testit "revoke SeSecurityPrivilege" $NET rpc rights revoke $USERNAME SeSecurityPrivilege -U $USERNAME%$PASSWORD -I $SERVER_IP || failed=$(expr $failed + 1)
 
 exit $failed
