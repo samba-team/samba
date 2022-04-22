9bc3ba8f3793d02f9ae529fd0992ed534a419e40
Andreas Schneider <asn@samba.org>
2022-04-22 15:46:04 +0200

    testprogs: Reformat demote-saveddb.sh
    shfmt -w -p -i 0 -fn testprogs/blackbox/demote-saveddb.sh
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Pavel Filipenský <pfilipensky@samba.org>

--- testprogs/blackbox/demote-saveddb.sh
+++ testprogs/blackbox/demote-saveddb.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 if [ $# -lt 1 ]; then
-cat <<EOF
+	cat <<EOF
 Usage: demote.sh PREFIX RELEASE
 EOF
-exit 1;
+	exit 1
 fi
 
 PREFIX_ABS="$1"
@@ -12,66 +12,68 @@ shift 1
 
 failed=0
 
-. `dirname $0`/subunit.sh
-. `dirname $0`/common_test_fns.inc
+. $(dirname $0)/subunit.sh
+. $(dirname $0)/common_test_fns.inc
 
 samba_tree_dir="$SRCDIR_ABS/source4/selftest/provisions/multi-dc-samba-master-c596ac6"
 
 samba_tdbrestore="tdbrestore"
 if [ -x $BINDIR/tdbrestore ]; then
-    samba_tdbrestore="$BINDIR/tdbrestore"
+	samba_tdbrestore="$BINDIR/tdbrestore"
 fi
 
 # The undump script and the provision data is not part of release tarballs,
 # skip the tests in this case!
 samba_undump="$SRCDIR_ABS/source4/selftest/provisions/undump.sh"
 if [ ! -x $samba_undump ] || [ ! -d $samba_tree_dir ]; then
-    subunit_start_test "undump"
-    subunit_skip_test "undump" <<EOF
+	subunit_start_test "undump"
+	subunit_skip_test "undump" <<EOF
 EOF
 
-    subunit_start_test "undump"
-    subunit_skip_test "undump" <<EOF
-Skipping tests - no provision!
-EOF
-
-    subunit_start_test "demote-q-0-0"
-    subunit_skip_test "demote-q-0-0" <<EOF
-Skipping tests - no provision!
-EOF
-    subunit_start_test "demote-q-0-1"
-    subunit_skip_test "demote-q-0-1" <<EOF
-Skipping tests - no provision!
-EOF
-    subunit_start_test "demote-q-1-0"
-    subunit_skip_test "demote-q-1-0" <<EOF
-Skipping tests - no provision!
-EOF
-    subunit_start_test "demote-q-1-1"
-    subunit_skip_test "demote-q-1-1" <<EOF
+	subunit_start_test "undump"
+	subunit_skip_test "undump" <<EOF
 Skipping tests - no provision!
 EOF
 
-    exit 0
+	subunit_start_test "demote-q-0-0"
+	subunit_skip_test "demote-q-0-0" <<EOF
+Skipping tests - no provision!
+EOF
+	subunit_start_test "demote-q-0-1"
+	subunit_skip_test "demote-q-0-1" <<EOF
+Skipping tests - no provision!
+EOF
+	subunit_start_test "demote-q-1-0"
+	subunit_skip_test "demote-q-1-0" <<EOF
+Skipping tests - no provision!
+EOF
+	subunit_start_test "demote-q-1-1"
+	subunit_skip_test "demote-q-1-1" <<EOF
+Skipping tests - no provision!
+EOF
+
+	exit 0
 fi
 
-undump() {
-    $SRCDIR_ABS/source4/selftest/provisions/undump.sh $samba_tree_dir $PREFIX_ABS $samba_tdbrestore
+undump()
+{
+	$SRCDIR_ABS/source4/selftest/provisions/undump.sh $samba_tree_dir $PREFIX_ABS $samba_tdbrestore
 }
 
-demote() {
-       $PYTHON $BINDIR/samba-tool domain demote -H tdb://$PREFIX_ABS/private/sam.ldb --remove-other-dead-server=$1
+demote()
+{
+	$PYTHON $BINDIR/samba-tool domain demote -H tdb://$PREFIX_ABS/private/sam.ldb --remove-other-dead-server=$1
 }
 
 remove_directory $PREFIX_ABS
 
-testit "undump" undump || failed=`expr $failed + 1`
-testit "demote-q-0-0" demote "q-0-0" || failed=`expr $failed + 1`
+testit "undump" undump || failed=$(expr $failed + 1)
+testit "demote-q-0-0" demote "q-0-0" || failed=$(expr $failed + 1)
 # The database was copied of q-0-1 so this will fail
 # as we can't remove our own name
-testit_expect_failure "demote-q-0-1" demote "q-0-1" || failed=`expr $failed + 1`
-testit "demote-q-1-0" demote "q-1-0" || failed=`expr $failed + 1`
-testit "demote-q-1-1" demote "q-1-1" || failed=`expr $failed + 1`
+testit_expect_failure "demote-q-0-1" demote "q-0-1" || failed=$(expr $failed + 1)
+testit "demote-q-1-0" demote "q-1-0" || failed=$(expr $failed + 1)
+testit "demote-q-1-1" demote "q-1-1" || failed=$(expr $failed + 1)
 
 remove_directory $PREFIX_ABS
 
