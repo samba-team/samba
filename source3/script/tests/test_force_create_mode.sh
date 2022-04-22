c72b48051b5c3f8d908b3a731ec0d3c236aaa1a8
Andreas Schneider <asn@samba.org>
2022-04-22 15:34:09 +0200

    s3:tests: Reformat test_force_create_mode.sh
    shfmt -f source3/script/| xargs shfmt -w -p -i 0 -fn
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- source3/script/tests/test_force_create_mode.sh
+++ source3/script/tests/test_force_create_mode.sh
@@ -4,10 +4,10 @@
 #
 
 if [ $# -lt 7 ]; then
-cat <<EOF
+	cat <<EOF
 Usage: $0 SERVER DOMAIN USERNAME PASSWORD PREFIX TARGET_ENV SMBCLIENT
 EOF
-exit 1;
+	exit 1
 fi
 
 SERVER=${1}
@@ -21,52 +21,52 @@ shift 7
 SMBCLIENT="$VALGRIND ${SMBCLIENT}"
 ADDARGS="$@"
 
-incdir=`dirname $0`/../../../testprogs/blackbox
+incdir=$(dirname $0)/../../../testprogs/blackbox
 . $incdir/subunit.sh
 
 failed=0
 
 test_force_create_mode()
 {
-    local filename="wurst.$$"
-    local filename_path="$PREFIX/$filename"
+	local filename="wurst.$$"
+	local filename_path="$PREFIX/$filename"
 
-    local tmpfile=$PREFIX/smbclient_interactive_prompt_commands
+	local tmpfile=$PREFIX/smbclient_interactive_prompt_commands
 
-    echo wurstbar > $filename_path
+	echo wurstbar >$filename_path
 
-    cat > $tmpfile <<EOF
+	cat >$tmpfile <<EOF
 lcd $PREFIX
 put $filename
 quit
 EOF
-    cmd='CLI_FORCE_INTERACTIVE=yes $SMBCLIENT -U$USERNAME%$PASSWORD //$SERVER/$1 $ADDARGS < $tmpfile 2>&1'
-    eval echo "$cmd"
-    out=`eval $cmd`
-    ret=$?
-    rm -f $tmpfile
+	cmd='CLI_FORCE_INTERACTIVE=yes $SMBCLIENT -U$USERNAME%$PASSWORD //$SERVER/$1 $ADDARGS < $tmpfile 2>&1'
+	eval echo "$cmd"
+	out=$(eval $cmd)
+	ret=$?
+	rm -f $tmpfile
 
-    if [ $ret -ne 0 ] ; then
-       echo "$out"
-       echo "Failed to connect - error: $ret"
-       return 1
-    fi
-    rm -f $filename_path
+	if [ $ret -ne 0 ]; then
+		echo "$out"
+		echo "Failed to connect - error: $ret"
+		return 1
+	fi
+	rm -f $filename_path
 
-    share_filename="$PREFIX/$TARGET_ENV/share/$filename"
-    file_perms=$(stat --format=%a $share_filename)
-    if [ "$file_perms" != "664" ]; then
-        echo "Invalid file permissions: $file_perms"
-        return 1
-    fi
+	share_filename="$PREFIX/$TARGET_ENV/share/$filename"
+	file_perms=$(stat --format=%a $share_filename)
+	if [ "$file_perms" != "664" ]; then
+		echo "Invalid file permissions: $file_perms"
+		return 1
+	fi
 
-    rm -f $share_filename
+	rm -f $share_filename
 
-    return 0
+	return 0
 }
 
 testit "test_mode=0664" \
-   test_force_create_mode create_mode_664 || \
-   failed=`expr $failed + 1`
+	test_force_create_mode create_mode_664 ||
+	failed=$(expr $failed + 1)
 
 exit $failed
