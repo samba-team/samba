6395813ca092228c3e28ad624c385c519efa5f92
Andreas Schneider <asn@samba.org>
2022-04-22 15:34:10 +0200

    s3:tests: Reformat test_shadow_copy_torture.sh
    shfmt -f source3/script/| xargs shfmt -w -p -i 0 -fn
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Guenther Deschner <gd@samba.org>

--- source3/script/tests/test_shadow_copy_torture.sh
+++ source3/script/tests/test_shadow_copy_torture.sh
@@ -4,10 +4,10 @@
 #
 
 if [ $# -lt 7 ]; then
-cat <<EOF
+	cat <<EOF
 Usage: test_shadow_copy SERVER SERVER_IP DOMAIN USERNAME PASSWORD WORKDIR SMBTORTURE SMBCLIENT
 EOF
-exit 1;
+	exit 1
 fi
 
 SERVER=${1}
@@ -20,7 +20,7 @@ SMBTORTURE="$VALGRIND ${7}"
 SMBCLIENT="$VALGRIND ${8}"
 shift 7
 
-incdir=`dirname $0`/../../../testprogs/blackbox
+incdir=$(dirname $0)/../../../testprogs/blackbox
 . $incdir/subunit.sh
 
 SNAPSHOT="@GMT-2015.10.31-19.40.30"
@@ -32,182 +32,181 @@ cd $SELFTEST_TMPDIR || exit 1
 # build a hierarchy of files, symlinks, and directories
 build_files()
 {
-    local destdir
-    destdir=$1
+	local destdir
+	destdir=$1
 
-    echo "$content" > $destdir/foo
+	echo "$content" >$destdir/foo
 
-    mkdir -p $WORKDIR/subdir/
-    touch $WORKDIR/subdir/hardlink
+	mkdir -p $WORKDIR/subdir/
+	touch $WORKDIR/subdir/hardlink
 }
 
 # build a snapshots directory
 build_snapshots()
 {
-    local snapdir
+	local snapdir
 
-    snapdir=$WORKDIR/.snapshots
+	snapdir=$WORKDIR/.snapshots
 
-    mkdir -p $snapdir/$SNAPSHOT
+	mkdir -p $snapdir/$SNAPSHOT
 
-    build_files $snapdir/$SNAPSHOT
+	build_files $snapdir/$SNAPSHOT
 
-    mkdir -p $snapdir/$SNAPSHOT/subdir
-    ln "$WORKDIR"/subdir/hardlink "$snapdir"/$SNAPSHOT/subdir/hardlink
+	mkdir -p $snapdir/$SNAPSHOT/subdir
+	ln "$WORKDIR"/subdir/hardlink "$snapdir"/$SNAPSHOT/subdir/hardlink
 }
 
 build_stream_on_snapshot()
 {
-    file=$WORKDIR/.snapshots/$SNAPSHOT/foo
+	file=$WORKDIR/.snapshots/$SNAPSHOT/foo
 
-    setfattr -n 'user.DosStream.bar:$DATA' -v baz $file || return 1
+	setfattr -n 'user.DosStream.bar:$DATA' -v baz $file || return 1
 }
 
 test_shadow_copy_write()
 {
-    local msg
+	local msg
 
-    msg=$1
+	msg=$1
 
-    #delete snapshots from previous tests
-    find $WORKDIR -name ".snapshots" -exec rm -rf {} \; 1>/dev/null 2>&1
-    build_snapshots
+	#delete snapshots from previous tests
+	find $WORKDIR -name ".snapshots" -exec rm -rf {} \; 1>/dev/null 2>&1
+	build_snapshots
 
-    testit "writing to shadow copy of a file" \
-	   $SMBTORTURE \
-	   -U$USERNAME%$PASSWORD \
-	   "//$SERVER/shadow_write" \
-	   --option="torture:twrp_file=foo" \
-	   --option="torture:twrp_snapshot=$SNAPSHOT" \
-	   smb2.twrp.write || \
-        failed=`expr $failed + 1`
+	testit "writing to shadow copy of a file" \
+		$SMBTORTURE \
+		-U$USERNAME%$PASSWORD \
+		"//$SERVER/shadow_write" \
+		--option="torture:twrp_file=foo" \
+		--option="torture:twrp_snapshot=$SNAPSHOT" \
+		smb2.twrp.write ||
+		failed=$(expr $failed + 1)
 }
-
 
 test_shadow_copy_stream()
 {
-    local msg
+	local msg
 
-    msg=$1
+	msg=$1
 
-    #delete snapshots from previous tests
-    find $WORKDIR -name ".snapshots" -exec rm -rf {} \; 1>/dev/null 2>&1
-    build_snapshots
-    build_stream_on_snapshot || {
-	subunit_start_test msg
-	subunit_skip_test msg <<EOF
+	#delete snapshots from previous tests
+	find $WORKDIR -name ".snapshots" -exec rm -rf {} \; 1>/dev/null 2>&1
+	build_snapshots
+	build_stream_on_snapshot || {
+		subunit_start_test msg
+		subunit_skip_test msg <<EOF
 test_shadow_copy_stream needs an fs with xattrs
 EOF
-	return 0
-    }
+		return 0
+	}
 
-    testit "reading stream of a shadow copy of a file" \
-	   $SMBTORTURE \
-	   -U$USERNAME%$PASSWORD \
-	   "//$SERVER/shadow_write" \
-	   --option="torture:twrp_file=foo" \
-	   --option="torture:twrp_stream=bar" \
-	   --option="torture:twrp_stream_size=3" \
-	   --option="torture:twrp_snapshot=$SNAPSHOT" \
-	   smb2.twrp.stream || \
-        failed=`expr $failed + 1`
+	testit "reading stream of a shadow copy of a file" \
+		$SMBTORTURE \
+		-U$USERNAME%$PASSWORD \
+		"//$SERVER/shadow_write" \
+		--option="torture:twrp_file=foo" \
+		--option="torture:twrp_stream=bar" \
+		--option="torture:twrp_stream_size=3" \
+		--option="torture:twrp_snapshot=$SNAPSHOT" \
+		smb2.twrp.stream ||
+		failed=$(expr $failed + 1)
 }
 
 test_shadow_copy_openroot()
 {
-    local msg
+	local msg
 
-    msg=$1
+	msg=$1
 
-    #delete snapshots from previous tests
-    find $WORKDIR -name ".snapshots" -exec rm -rf {} \; 1>/dev/null 2>&1
-    build_snapshots
+	#delete snapshots from previous tests
+	find $WORKDIR -name ".snapshots" -exec rm -rf {} \; 1>/dev/null 2>&1
+	build_snapshots
 
-    testit "opening shadow copy root of share" \
-	   $SMBTORTURE \
-	   -U$USERNAME%$PASSWORD \
-	   "//$SERVER/shadow_write" \
-	   --option="torture:twrp_snapshot=$SNAPSHOT" \
-	   smb2.twrp.openroot || \
-        failed=`expr $failed + 1`
+	testit "opening shadow copy root of share" \
+		$SMBTORTURE \
+		-U$USERNAME%$PASSWORD \
+		"//$SERVER/shadow_write" \
+		--option="torture:twrp_snapshot=$SNAPSHOT" \
+		smb2.twrp.openroot ||
+		failed=$(expr $failed + 1)
 }
 
 test_shadow_copy_fix_inodes()
 {
-    local msg
+	local msg
 
-    msg=$1
+	msg=$1
 
-    #delete snapshots from previous tests
-    find $WORKDIR -name ".snapshots" -exec rm -rf {} \; 1>/dev/null 2>&1
-    build_snapshots
+	#delete snapshots from previous tests
+	find $WORKDIR -name ".snapshots" -exec rm -rf {} \; 1>/dev/null 2>&1
+	build_snapshots
 
-    out=$($SMBCLIENT \
-	   -U $USERNAME%$PASSWORD \
-	   "//$SERVER/shadow_write" \
-	   -c "open $SNAPSHOT/subdir/hardlink") || failed=`expr $failed + 1`
-    echo $out
-    echo $out | grep "hardlink: for read/write fnum 1" || return 1
+	out=$($SMBCLIENT \
+		-U $USERNAME%$PASSWORD \
+		"//$SERVER/shadow_write" \
+		-c "open $SNAPSHOT/subdir/hardlink") || failed=$(expr $failed + 1)
+	echo $out
+	echo $out | grep "hardlink: for read/write fnum 1" || return 1
 }
 
 build_hiddenfile()
 {
-    local snapdir
+	local snapdir
 
-    snapdir=$WORKDIR/.snapshots
+	snapdir=$WORKDIR/.snapshots
 
-    #delete snapshots from previous tests
-    find $WORKDIR -name ".snapshots" -exec rm -rf {} \; 1>/dev/null 2>&1
-    build_snapshots
+	#delete snapshots from previous tests
+	find $WORKDIR -name ".snapshots" -exec rm -rf {} \; 1>/dev/null 2>&1
+	build_snapshots
 
-    touch $WORKDIR/hiddenfile
+	touch $WORKDIR/hiddenfile
 
-    # Create a file with hidden attribute
-    $SMBCLIENT -U $USERNAME%$PASSWORD \
-	       "//$SERVER/shadow_write" \
-	       -c "put $WORKDIR/hiddenfile hiddenfile; setmode hiddenfile +h"
-    # ...and move it to the snapshot directory
-    mv $WORKDIR/hiddenfile $snapdir/$SNAPSHOT/
+	# Create a file with hidden attribute
+	$SMBCLIENT -U $USERNAME%$PASSWORD \
+		"//$SERVER/shadow_write" \
+		-c "put $WORKDIR/hiddenfile hiddenfile; setmode hiddenfile +h"
+	# ...and move it to the snapshot directory
+	mv $WORKDIR/hiddenfile $snapdir/$SNAPSHOT/
 }
 
 test_hiddenfile()
 {
-    build_hiddenfile
+	build_hiddenfile
 
-    out=$($SMBCLIENT \
-	   -U $USERNAME%$PASSWORD \
-	   "//$SERVER/shadow_write" \
-	   -c "allinfo $SNAPSHOT/hiddenfile") || return 1
-    echo $out
-    echo $out | grep "attributes: HA (22)" || return 1
+	out=$($SMBCLIENT \
+		-U $USERNAME%$PASSWORD \
+		"//$SERVER/shadow_write" \
+		-c "allinfo $SNAPSHOT/hiddenfile") || return 1
+	echo $out
+	echo $out | grep "attributes: HA (22)" || return 1
 
-    out=$($SMBCLIENT \
-	   -U $USERNAME%$PASSWORD \
-	   "//$SERVER/shadow_write" \
-	   -c "ls $SNAPSHOT/hiddenfile") || return 1
-    echo $out
-    echo $out | grep "hiddenfile[[:blank:]]*AH" || return 1
+	out=$($SMBCLIENT \
+		-U $USERNAME%$PASSWORD \
+		"//$SERVER/shadow_write" \
+		-c "ls $SNAPSHOT/hiddenfile") || return 1
+	echo $out
+	echo $out | grep "hiddenfile[[:blank:]]*AH" || return 1
 
-    return 0
+	return 0
 }
 
 test_shadow_copy_listdir_fix_inodes()
 {
-    local msg
+	local msg
 
-    msg=$1
+	msg=$1
 
-    #delete snapshots from previous tests
-    find $WORKDIR -name ".snapshots" -exec rm -rf {} \; 1>/dev/null 2>&1
-    build_snapshots
+	#delete snapshots from previous tests
+	find $WORKDIR -name ".snapshots" -exec rm -rf {} \; 1>/dev/null 2>&1
+	build_snapshots
 
-    testit "$msg" \
-	   $SMBTORTURE \
-	   -U$USERNAME%$PASSWORD \
-	   "//$SERVER/shadow_write" \
-	   --option="torture:twrp_snapshot=$SNAPSHOT" \
-	   smb2.twrp.listdir || \
-        failed=`expr $failed + 1`
+	testit "$msg" \
+		$SMBTORTURE \
+		-U$USERNAME%$PASSWORD \
+		"//$SERVER/shadow_write" \
+		--option="torture:twrp_snapshot=$SNAPSHOT" \
+		smb2.twrp.listdir ||
+		failed=$(expr $failed + 1)
 }
 
 build_files $WORKDIR
@@ -219,9 +218,9 @@ test_shadow_copy_stream "reading stream of snapshotted file"
 
 test_shadow_copy_openroot "opening root of shadow copy share"
 
-testit "fix inodes with hardlink" test_shadow_copy_fix_inodes || failed=`expr $failed + 1`
+testit "fix inodes with hardlink" test_shadow_copy_fix_inodes || failed=$(expr $failed + 1)
 
-testit "Test reading DOS attribute" test_hiddenfile || failed=`expr $failed + 1`
+testit "Test reading DOS attribute" test_hiddenfile || failed=$(expr $failed + 1)
 
 test_shadow_copy_listdir_fix_inodes "fix inodes when listing directory"
 
