d0f27918308fca625c5cecfad205698fcdc8a8bc
Andreas Schneider <asn@samba.org>
2022-04-22 15:46:04 +0200

    testprogs: Reformat nsstest.sh
    shfmt -w -p -i 0 -fn testprogs/blackbox/nsstest.sh
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Pavel Filipenský <pfilipensky@samba.org>

--- testprogs/blackbox/nsstest.sh
+++ testprogs/blackbox/nsstest.sh
@@ -4,10 +4,10 @@
 # Copyright (C) 2006-2008 Andrew Bartlett <abartlet@samba.org>
 
 if [ $# -lt 2 ]; then
-cat <<EOF
+	cat <<EOF
 Usage: nsstest.sh NSSTEST LIBNSS_WINBIND
 EOF
-exit 1;
+	exit 1
 fi
 
 nsstest=$1
@@ -15,8 +15,8 @@ libnss_winbind=$2
 shift 2
 failed=0
 
-. `dirname $0`/subunit.sh
+. $(dirname $0)/subunit.sh
 
-testit "run nsstest" $VALGRIND $nsstest $libnss_winbind || failed=`expr $failed + 1`
+testit "run nsstest" $VALGRIND $nsstest $libnss_winbind || failed=$(expr $failed + 1)
 
 exit $failed
