534ae9349ab4b5df7cdcb7fcd49875ba332b9045
Andreas Schneider <asn@samba.org>
2022-04-22 15:34:09 +0200

    s3:tests: Reformat test_net_lookup.sh
    shfmt -f source3/script/| xargs shfmt -w -p -i 0 -fn
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- source3/script/tests/test_net_lookup.sh
+++ source3/script/tests/test_net_lookup.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 
-if [ $# != 6 ] ; then
-    echo "Usage: $0 SERVER USERNAME PASSWORD NET SAMBA-TOOL DNS-ZONE"
-    exit 1
+if [ $# != 6 ]; then
+	echo "Usage: $0 SERVER USERNAME PASSWORD NET SAMBA-TOOL DNS-ZONE"
+	exit 1
 fi
 
-SERVER="$1"; shift 1
-USERNAME="$1"; shift 1
-PASSWORD="$1"; shift 1
-NET="$1"; shift 1
-SAMBATOOL="$1"; shift 1
-DNSZONE="$1"; shift 1
+SERVER="$1"
+shift 1
+USERNAME="$1"
+shift 1
+PASSWORD="$1"
+shift 1
+NET="$1"
+shift 1
+SAMBATOOL="$1"
+shift 1
+DNSZONE="$1"
+shift 1
 
 SITE="mysite"
 
@@ -20,29 +26,29 @@ incdir=$(dirname $0)/../../../testprogs/blackbox
 failed=0
 
 $SAMBATOOL dns add "$SERVER" -U "$USERNAME"%"$PASSWORD" \
-	   _msdcs."$DNSZONE" _ldap._tcp."$SITE"._sites.dc \
-	   SRV "mydc.$DNSZONE 389 100 100"
+	_msdcs."$DNSZONE" _ldap._tcp."$SITE"._sites.dc \
+	SRV "mydc.$DNSZONE 389 100 100"
 $SAMBATOOL dns add "$SERVER" -U "$USERNAME"%"$PASSWORD" \
-	   "$DNSZONE" mydc \
-	   A "1.2.3.4"
+	"$DNSZONE" mydc \
+	A "1.2.3.4"
 
 # global lookup
 testit_grep global 10.53.57.30:389 $NET lookup ldap "$DNSZONE" ||
-    failed = `expr $failed + 1`
+	failed = $(expr $failed + 1)
 
 # correct site-aware lookup
 testit_grep site-aware 1.2.3.4:389 $NET lookup ldap "$DNSZONE" "$SITE" ||
-    failed = `expr $failed + 1`
+	failed = $(expr $failed + 1)
 
 # lookup with nonexisting site -- global fallback
 testit_grep global 10.53.57.30:389 $NET lookup ldap "$DNSZONE" nosite ||
-    failed = `expr $failed + 1`
+	failed = $(expr $failed + 1)
 
 $SAMBATOOL dns delete "$SERVER" -U "$USERNAME"%"$PASSWORD" \
-	   "$DNSZONE" mydc \
-	   A "1.2.3.4"
+	"$DNSZONE" mydc \
+	A "1.2.3.4"
 $SAMBATOOL dns delete "$SERVER" -U "$USERNAME"%"$PASSWORD" \
-	   _msdcs."$DNSZONE" _ldap._tcp."$SITE"._sites.dc \
-	   SRV "mydc.$DNSZONE 389 100 100"
+	_msdcs."$DNSZONE" _ldap._tcp."$SITE"._sites.dc \
+	SRV "mydc.$DNSZONE 389 100 100"
 
 testok $0 $failed
