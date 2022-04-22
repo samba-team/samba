b4ee11d083da33ec68bd5e95af3c481656b57d51
Andreas Schneider <asn@samba.org>
2022-04-22 15:34:08 +0200

    s3:tests: Reformat printing_var_exp_lpr_cmd.sh
    shfmt -f source3/script/| xargs shfmt -w -p -i 0 -fn
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- source3/script/tests/printing/printing_var_exp_lpr_cmd.sh
+++ source3/script/tests/printing/printing_var_exp_lpr_cmd.sh
@@ -4,6 +4,6 @@ logfile="${SELFTEST_TMPDIR}/${USER}_printing_var_exp.log"
 
 rm -f "$logfile"
 
-for i in $(seq 1 $#) ; do
-    eval echo "arg $i: \$$i" >> "$logfile"
+for i in $(seq 1 $#); do
+	eval echo "arg $i: \$$i" >>"$logfile"
 done
