a8c6b30e72373e13cb64f3503ef1237cefa7e753
Andreas Schneider <asn@samba.org>
2022-04-22 15:34:08 +0200

    s3:tests: Reformat full_audit_segfault/run.sh
    shfmt -f source3/script/| xargs shfmt -w -p -i 0 -fn
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Guenther Deschner <gd@samba.org>

--- source3/script/tests/full_audit_segfault/run.sh
+++ source3/script/tests/full_audit_segfault/run.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
 if [ $# -lt 1 ]; then
-cat <<EOF
+	cat <<EOF
 Usage: run.sh VFSTEST
 EOF
-exit 1;
+	exit 1
 fi
 
-TALLOC_FILL_FREE=0; export TALLOC_FILL_FREE
+TALLOC_FILL_FREE=0
+export TALLOC_FILL_FREE
 
 TESTBASE="$(dirname $0)"
-VFSTEST="$VALGRIND $1"; shift 1;
+VFSTEST="$VALGRIND $1"
+shift 1
 ADDARGS="$*"
 
-incdir=`dirname $0`/../../../../testprogs/blackbox
+incdir=$(dirname $0)/../../../../testprogs/blackbox
 . $incdir/subunit.sh
 
 failed=0
 
 testit "vfstest" "$VFSTEST" -f "$TESTBASE/vfstest.cmd" "$ADDARGS" ||
-    failed=$(expr $failed + 1)
+	failed=$(expr $failed + 1)
 
 exit $failed
