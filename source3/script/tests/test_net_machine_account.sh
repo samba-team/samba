a0802f556f23b585db6cac2d639e412aa83f47f2
Andreas Schneider <asn@samba.org>
2022-04-22 15:34:09 +0200

    s3:tests: Reformat test_net_machine_account.sh
    shfmt -f source3/script/| xargs shfmt -w -p -i 0 -fn
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- source3/script/tests/test_net_machine_account.sh
+++ source3/script/tests/test_net_machine_account.sh
@@ -2,21 +2,24 @@
 # Reproducer for https://bugzilla.samba.org/show_bug.cgi?id=14908
 
 if [ $# -lt 2 ]; then
-    echo "Usage: $0 NET CONFFILE SERVER_IP"
-    exit 1
+	echo "Usage: $0 NET CONFFILE SERVER_IP"
+	exit 1
 fi
 
-NET="$1"; shift
-CONFFILE="$1"; shift
-SERVER_IP="$1"; shift
+NET="$1"
+shift
+CONFFILE="$1"
+shift
+SERVER_IP="$1"
+shift
 
 export UID_WRAPPER_ROOT=1
 
-incdir=`dirname $0`/../../../testprogs/blackbox
+incdir=$(dirname $0)/../../../testprogs/blackbox
 . $incdir/subunit.sh
 
 failed=0
 
-testit "net_ads_user" $VALGRIND $NET rpc user --configfile="$CONFFILE" -S "$SERVER_IP" -P || failed=`expr $failed + 1`
+testit "net_ads_user" $VALGRIND $NET rpc user --configfile="$CONFFILE" -S "$SERVER_IP" -P || failed=$(expr $failed + 1)
 
 testok $0 $failed
