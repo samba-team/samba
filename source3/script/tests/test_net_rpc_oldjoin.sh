7a916f886f335296de86bbfe3fe74d5951f09b50
Andreas Schneider <asn@samba.org>
2022-04-22 15:34:09 +0200

    s3:tests: Reformat test_net_rpc_oldjoin.sh
    shfmt -f source3/script/| xargs shfmt -w -p -i 0 -fn
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- source3/script/tests/test_net_rpc_oldjoin.sh
+++ source3/script/tests/test_net_rpc_oldjoin.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 if [ $# -lt 3 ]; then
-cat <<EOF
+	cat <<EOF
 Usage: test_net_rpc_oldjoin.sh SERVER PREFIX SMB_CONF_PATH
 EOF
-exit 1;
+	exit 1
 fi
 
 SERVER="$1"
@@ -12,7 +12,7 @@ PREFIX="$2"
 SMB_CONF_PATH="$3"
 shift 3
 
-incdir=`dirname $0`/../../../testprogs/blackbox
+incdir=$(dirname $0)/../../../testprogs/blackbox
 . $incdir/subunit.sh
 maccount="OLDJOINTEST"
 privatedir="$PREFIX/private"
@@ -40,14 +40,13 @@ test_smbpasswd()
 	fi
 }
 
-
-testit "mkdir -p $privatedir" mkdir -p $privatedir || failed=`expr $failed + 1`
+testit "mkdir -p $privatedir" mkdir -p $privatedir || failed=$(expr $failed + 1)
 testit "smbpasswd -a -m" \
-	test_smbpasswd $maccount \
-	|| failed=$(expr $failed + 1)
-testit "net_rpc_oldjoin" $VALGRIND $BINDIR/net rpc oldjoin -S $SERVER $OPTIONS || failed=`expr $failed + 1`
-testit "net_rpc_testjoin1" $VALGRIND $BINDIR/net rpc testjoin -S $SERVER $OPTIONS || failed=`expr $failed + 1`
-testit "net_rpc_changetrustpw" $VALGRIND $BINDIR/net rpc changetrustpw -S $SERVER $OPTIONS || failed=`expr $failed + 1`
-testit "net_rpc_testjoin2" $VALGRIND $BINDIR/net rpc testjoin -S $SERVER $OPTIONS || failed=`expr $failed + 1`
+	test_smbpasswd $maccount ||
+	failed=$(expr $failed + 1)
+testit "net_rpc_oldjoin" $VALGRIND $BINDIR/net rpc oldjoin -S $SERVER $OPTIONS || failed=$(expr $failed + 1)
+testit "net_rpc_testjoin1" $VALGRIND $BINDIR/net rpc testjoin -S $SERVER $OPTIONS || failed=$(expr $failed + 1)
+testit "net_rpc_changetrustpw" $VALGRIND $BINDIR/net rpc changetrustpw -S $SERVER $OPTIONS || failed=$(expr $failed + 1)
+testit "net_rpc_testjoin2" $VALGRIND $BINDIR/net rpc testjoin -S $SERVER $OPTIONS || failed=$(expr $failed + 1)
 
 testok $0 $failed
