b7a024e4ef8f0cbc40a125f91d03e5ce526f1052
Andreas Schneider <asn@samba.org>
2022-06-13 11:26:38 +0200

    s4:selftest: Fix shellcheck errors in wintest_net.sh
    source4/selftest/win/wintest_net.sh:57:27: error: Use braces when
    expanding arrays, e.g. ${array[idx]} (or ${var}[.. to quiet). [SC1087]
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Pavel Filipenský <pfilipensky@samba.org>
    
    Autobuild-User(master): Pavel Filipensky <pfilipensky@samba.org>
    Autobuild-Date(master): Mon Aug 22 15:15:11 UTC 2022 on sn-devel-184

--- source4/selftest/win/wintest_net.sh
+++ source4/selftest/win/wintest_net.sh
@@ -54,7 +54,7 @@ for o in $bind_options; do
 		for t in $net_test; do
 			test_name="$t on $transport with $o"
 			$SMBTORTURE_BIN_PATH -U $username%$password \
-				-W $domain $transport:$server[$o] \
+				-W $domain ${transport}:${server}[$o] \
 				$t || on_error "\n$test_name failed."
 		done
 	done
