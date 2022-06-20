45094bd891318f650ac5f0a1bb88e9cbb08729fd
Andrew Bartlett <abartlet@samba.org>
2022-06-20 14:35:19 +1200

    docs-xml: Use &pathconfig.WINBINDD_SOCKET_DIR; to avoid reference to old /tmp/.winbindd
    We can now write docs that follow how the software on this system was
    built, which is much less confusing for users.  Also /tmp/.winbindd
    has not been used for a long time.
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=15101
    
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Andreas Schneider <asn@samba.org>

--- docs-xml/smbdotconf/generate-file-list.sh
+++ docs-xml/smbdotconf/generate-file-list.sh
@@ -1,24 +1,10 @@
 #!/bin/sh
 #
-# This is the fallback table for when we use the docs-xml build
-# system.  When build as part of the main waf build, these are set to
-# the full correct path for the system.
-#
-echo "<!DOCTYPE section [
-<!ENTITY pathconfig.SCRIPTSBINDIR        '\${prefix}/sbin'>
-<!ENTITY pathconfig.LOCKDIR              '\${prefix}/var/lock'>
-<!ENTITY pathconfig.NCALRPCDIR           '\${prefix}/var/run/ncalrpc'>
-<!ENTITY pathconfig.PIDDIR               '\${prefix}/var/run'>
-<!ENTITY pathconfig.STATEDIR             '\${prefix}/var/locks'>
-<!ENTITY pathconfig.PRIVATE_DIR          '\${prefix}/private'>
-<!ENTITY pathconfig.BINDDNS_DIR          '\${prefix}/bind-dns'>
-<!ENTITY pathconfig.SMB_PASSWD_FILE      '\${prefix}/private/smbpasswd'>
-<!ENTITY pathconfig.WINBINDD_SOCKET_DIR  '\${prefix}/var/run/winbindd'>
-<!ENTITY pathconfig.CACHEDIR             '\${prefix}/var/cache'>
-<!ENTITY pathconfig.NTP_SIGND_SOCKET_DIR '\${prefix}/var/lib/ntp_signd'>
-<!ENTITY pathconfig.MITKDCPATH           '\${prefix}/sbin/krb5kdc'>
-<!ENTITY pathconfig.SAMBA_DATADIR        '\${prefix}/var/samba'>
-]>"
+set -e
+echo "<!DOCTYPE section ["
+$(dirname $0)/../generate-pathconf-entities.sh
+
+echo "]>"
 
 DIR=.
 if [ "x$1" != "x" ]; then
