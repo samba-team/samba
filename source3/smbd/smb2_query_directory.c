df4c3f0f28e2385cfdace905da4ad3cc4a59dd43
Volker Lendecke <vl@samba.org>
2022-07-29 14:57:54 +0200

    smbd: Save a line with tevent_req_nomem()
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/smbd/smb2_query_directory.c
+++ source3/smbd/smb2_query_directory.c
@@ -409,8 +409,7 @@ static struct tevent_req *smbd_smb2_query_directory_send(TALLOC_CTX *mem_ctx,
 						conn,
 						state->in_file_name,
 						0);
-		if (state->in_file_name == NULL) {
-			tevent_req_oom(req);
+		if (tevent_req_nomem(state->in_file_name, req)) {
 			return tevent_req_post(req, ev);
 		}
 	}
