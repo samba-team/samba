f0a52d433730f38845cb0870a1afec04cf7a2ccf
Ralph Boehme <slow@samba.org>
2022-07-29 14:54:07 +0200

    smbd: use metadata_fsp() with SMB_VFS_FSET_NT_ACL()
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=15126
    MR: https://gitlab.com/samba-team/samba/-/merge_requests/2643
    
    Signed-off-by: Ralph Boehme <slow@samba.org>
    Reviewed-by: Volker Lendecke <vl@samba.org>
    (cherry picked from commit 4ab29e2a345b48ebba652d5154e96adf954a6757)

--- source3/smbd/pysmbd.c
+++ source3/smbd/pysmbd.c
@@ -280,7 +280,7 @@ static NTSTATUS set_nt_acl_conn(const char *fname,
 		return status;
 	}
 
-	status = SMB_VFS_FSET_NT_ACL(fsp, security_info_sent, sd);
+	status = SMB_VFS_FSET_NT_ACL(metadata_fsp(fsp), security_info_sent, sd);
 	if (!NT_STATUS_IS_OK(status)) {
 		DEBUG(0,("set_nt_acl_no_snum: fset_nt_acl returned %s.\n", nt_errstr(status)));
 	}
