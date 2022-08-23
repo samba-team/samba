c31df02cb231d4d975bd796020a250dac36a6bf1
Volker Lendecke <vl@samba.org>
2022-08-23 13:13:48 +0200

    libsmb: Remove unused code
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/libsmb/clidfs.c
+++ source3/libsmb/clidfs.c
@@ -441,29 +441,6 @@ void cli_cm_display(struct cli_state *cli)
 	}
 }
 
-/****************************************************************************
-****************************************************************************/
-
-/****************************************************************************
-****************************************************************************/
-
-#if 0
-void cli_cm_set_credentials(struct user_auth_info *auth_info)
-{
-	SAFE_FREE(cm_creds.username);
-	cm_creds.username = SMB_STRDUP(get_cmdline_auth_info_username(
-					       auth_info));
-
-	if (get_cmdline_auth_info_got_pass(auth_info)) {
-		cm_set_password(get_cmdline_auth_info_password(auth_info));
-	}
-
-	cm_creds.use_kerberos = get_cmdline_auth_info_use_kerberos(auth_info);
-	cm_creds.fallback_after_kerberos = false;
-	cm_creds.signing_state = get_cmdline_auth_info_signing_state(auth_info);
-}
-#endif
-
 /**********************************************************************
  split a dfs path into the server, share name, and extrapath components
 **********************************************************************/
