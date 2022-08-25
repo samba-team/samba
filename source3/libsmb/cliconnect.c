a4f9f7c825c216d25bd14beeb2e547d0fff74c51
Volker Lendecke <vl@samba.org>
2022-08-25 10:43:54 +0200

    libsmb: Introduce helper var to cli_tree_connect_*_done()
    README.Coding, makes it easier to debug
    
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/libsmb/cliconnect.c
+++ source3/libsmb/cliconnect.c
@@ -2357,14 +2357,14 @@ static struct tevent_req *cli_tree_connect_send(
 
 static void cli_tree_connect_smb2_done(struct tevent_req *subreq)
 {
-	tevent_req_simple_finish_ntstatus(
-		subreq, smb2cli_tcon_recv(subreq));
+	NTSTATUS status = smb2cli_tcon_recv(subreq);
+	tevent_req_simple_finish_ntstatus(subreq, status);
 }
 
 static void cli_tree_connect_andx_done(struct tevent_req *subreq)
 {
-	tevent_req_simple_finish_ntstatus(
-		subreq, cli_tcon_andx_recv(subreq));
+	NTSTATUS status = cli_tcon_andx_recv(subreq);
+	tevent_req_simple_finish_ntstatus(subreq, status);
 }
 
 static void cli_tree_connect_raw_done(struct tevent_req *subreq)
