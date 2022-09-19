7efe673fbdcd27ddd23f36281c5f5338681a68fe
Christian Merten <christian@merten.dev>
2022-09-19 22:47:10 +0200

    libcli security_descriptor: Add function to delete a given ace from a security descriptor
    Two functions have been added to delete a given ace from the SACL or the DACL of a security descriptor.
    
    Signed-off-by: Christian Merten <christian@merten.dev>
    Reviewed-by: Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- libcli/security/security_descriptor.h
+++ libcli/security/security_descriptor.h
@@ -39,6 +39,10 @@ NTSTATUS security_descriptor_dacl_del(struct security_descriptor *sd,
 				      const struct dom_sid *trustee);
 NTSTATUS security_descriptor_sacl_del(struct security_descriptor *sd,
 				      const struct dom_sid *trustee);
+NTSTATUS security_descriptor_dacl_del_ace(struct security_descriptor *sd,
+					  const struct security_ace *ace);
+NTSTATUS security_descriptor_sacl_del_ace(struct security_descriptor *sd,
+					  const struct security_ace *ace);
 bool security_ace_equal(const struct security_ace *ace1, 
 			const struct security_ace *ace2);
 bool security_acl_equal(const struct security_acl *acl1, 
