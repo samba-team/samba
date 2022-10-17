e69d2b3f9d2c8f38a4d93413d563ad5241d35383
Noel Power <noel.power@suse.com>
2022-10-17 10:17:34 +0100

    s3/param: Check return of talloc_strdup
    followup to commit ff003fc87b8164610dfd6572347c05308c4b2fd7
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=15205
    
    Signed-off-by: Noel Power <noel.power@suse.com>
    Reviewed-by: Jeremy Allison <jra@samba.org>
    (cherry picked from commit 19eb88bc53e481327bbd437b0c145d5765c6dcec)

--- source3/param/test_lp_load.c
+++ source3/param/test_lp_load.c
@@ -83,6 +83,11 @@ int main(int argc, const char **argv)
 
 	if (poptPeekArg(pc)) {
 		config_file = talloc_strdup(frame, poptGetArg(pc));
+		if (config_file == NULL) {
+			DBG_ERR("out of memory\n");
+			TALLOC_FREE(frame);
+			exit(1);
+		}
 	} else {
 		config_file = get_dyn_CONFIGFILE();
 	}
