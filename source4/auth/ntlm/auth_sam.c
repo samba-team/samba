samba-team/samba

commit 7b8e32efc336fb728e0c7e3dd6fbe2ed54122124

CVE-2021-20251 auth4: Reread the user record if a bad password is noticed.
As is, this is pointless, as we need a transaction to make this
any less of a race, but this provides the steps towards that goal.

BUG: https://bugzilla.samba.org/show_bug.cgi?id=14611

Signed-off-by: Andrew Bartlett <abartlet@samba.org>
Reviewed-by: Joseph Sutton <josephsutton@catalyst.net.nz>
Reviewed-by: Andreas Schneider <asn@samba.org>

source4/auth/sam.c
@@ -827,6 +827,68 @@ static int authsam_get_user_pso(struct ldb_context *sam_ctx,
 	return LDB_SUCCESS;
 }
 
+/*
+ * Re-read the bad password and successful logon data for a user.
+ *
+ * The DN in the passed user record should contain the "objectGUID" in case the
+ * object DN has changed.
+ */
+NTSTATUS authsam_reread_user_logon_data(
+	struct ldb_context *sam_ctx,
+	TALLOC_CTX *mem_ctx,
+	const struct ldb_message *user_msg,
+	struct ldb_message **current)
+{
+	const struct ldb_val *v = NULL;
+	struct ldb_result *res = NULL;
+	uint16_t acct_flags = 0;
+	const char *attr_name = "msDS-User-Account-Control-Computed";
+
+	int ret;
+
+	/*
+	 * Re-read the account details, using the GUID in case the DN
+	 * is being changed (this is automatic in LDB because the
+	 * original search also used DSDB_SEARCH_SHOW_EXTENDED_DN)
+	 *
+	 * We re read all the attributes in user_attrs, rather than using a
+	 * subset to ensure that we can reuse existing validation code.
+	 */
+	ret = dsdb_search_dn(sam_ctx,
+			     mem_ctx,
+			     &res,
+			     user_msg->dn,
+			     user_attrs,
+			     DSDB_SEARCH_SHOW_EXTENDED_DN);
+	if (ret != LDB_SUCCESS) {
+		DBG_ERR("Unable to re-read account control data for %s\n",
+			ldb_dn_get_linearized(user_msg->dn));
+		return NT_STATUS_INTERNAL_ERROR;
+	}
+
+	/*
+	 * Ensure the account has not been locked out by another request
+	 */
+	v = ldb_msg_find_ldb_val(res->msgs[0], attr_name);
+	if (v == NULL || v->data == NULL) {
+		DBG_ERR("No %s attribute for %s\n",
+			attr_name,
+			ldb_dn_get_linearized(user_msg->dn));
+		TALLOC_FREE(res);
+		return NT_STATUS_INTERNAL_ERROR;
+	}
+	acct_flags = samdb_result_acct_flags(res->msgs[0], attr_name);
+	if (acct_flags & ACB_AUTOLOCK) {
+		DBG_WARNING(
+			"Account for user %s was locked out.\n",
+			ldb_dn_get_linearized(user_msg->dn));
+		TALLOC_FREE(res);
+		return NT_STATUS_ACCOUNT_LOCKED_OUT;
+	}
+	*current = res->msgs[0];
+	return NT_STATUS_OK;
+}
+
 static struct db_context *authsam_get_bad_password_db(
 	TALLOC_CTX *mem_ctx,
 	struct ldb_context *sam_ctx)
@@ -1245,6 +1307,26 @@ NTSTATUS authsam_logon_success_accounting(struct ldb_context *sam_ctx,
 		sam_ctx, mem_ctx, &need_db_reread, msg);
 	if (!NT_STATUS_IS_OK(status)) {
 		return status;
+	}
+
+	if (need_db_reread) {
+		struct ldb_message *current = NULL;
+
+		/*
+		 * Re-read the account details, using the GUID
+		 * embedded in DN so this is safe against a race where
+		 * it is being renamed.
+		 */
+		status = authsam_reread_user_logon_data(
+			sam_ctx, mem_ctx, msg, &current);
+		if (!NT_STATUS_IS_OK(status)) {
+			/*
+			 * The re-read can return account locked out, as well
+			 * as an internal error
+			 */
+			return status;
+		}
+		msg = current;
 	}
 
 	lockoutTime = ldb_msg_find_attr_as_int64(msg, "lockoutTime", 0);

commit 1414269dccfd7cb831889cc92df35920b034457c

CVE-2021-20251: s4:auth: fix use after free in authsam_logon_success_accounting()
This fixes a use after free problem introduced by
commit 7b8e32efc336fb728e0c7e3dd6fbe2ed54122124,
which has msg = current; which means the lifetime
of the 'msg' memory is no longer in the scope of th
caller.

BUG: https://bugzilla.samba.org/show_bug.cgi?id=14611
BUG: https://bugzilla.samba.org/show_bug.cgi?id=15253

Signed-off-by: Stefan Metzmacher <metze@samba.org>
Reviewed-by: Andrew Bartlett <abartlet@samba.org>

source4/auth/ntlm/auth_sam.c
@@ -787,6 +787,7 @@ static NTSTATUS authsam_authenticate(struct auth4_context *auth_context,
 	nt_status = authsam_logon_success_accounting(auth_context->sam_ctx,
 						     msg, domain_dn,
 						     interactive,
+						     tmp_ctx,
 						     &send_to_sam);
 
 	if (send_to_sam != NULL) {

source4/auth/ntlm/auth_winbind.c
@@ -256,7 +256,7 @@ static void winbind_check_password_done(struct tevent_req *subreq)
 				ctx->auth_ctx->sam_ctx, msg,
 				domain_dn,
 				user_info->flags & USER_INFO_INTERACTIVE_LOGON,
-				NULL);
+				NULL, NULL);
 			if (tevent_req_nterror(req, status)) {
 				return;
 			}

source4/auth/sam.c
@@ -1396,6 +1396,7 @@ NTSTATUS authsam_logon_success_accounting(struct ldb_context *sam_ctx,
 					  const struct ldb_message *msg,
 					  struct ldb_dn *domain_dn,
 					  bool interactive_or_kerberos,
+					  TALLOC_CTX *send_to_sam_mem_ctx,
 					  struct netr_SendToSamBase **send_to_sam)
 {
 	int ret;
@@ -1612,7 +1613,13 @@ get_transaction:
 		if (dbBadPwdCount != 0 && send_to_sam != NULL) {
 			struct netr_SendToSamBase *base_msg;
 			struct GUID guid = samdb_result_guid(msg, "objectGUID");
-			base_msg = talloc_zero(msg, struct netr_SendToSamBase);
+
+			base_msg = talloc_zero(send_to_sam_mem_ctx,
+					       struct netr_SendToSamBase);
+			if (base_msg == NULL) {
+				status = NT_STATUS_NO_MEMORY;
+				goto error;
+			}
 
 			base_msg->message_type = SendToSamResetBadPasswordCount;
 			base_msg->message_size = 16;

source4/kdc/hdb-samba4.c
@@ -653,7 +653,7 @@ static krb5_error_code hdb_samba4_audit(krb5_context context,
 			 * in the PAC here or re-calculate it.
 			 */
 			status = authsam_logon_success_accounting(kdc_db_ctx->samdb, p->msg,
-								  domain_dn, true, &send_to_sam);
+								  domain_dn, true, frame, &send_to_sam);
 			if (NT_STATUS_EQUAL(status, NT_STATUS_ACCOUNT_LOCKED_OUT)) {
 				final_ret = KRB5KDC_ERR_CLIENT_REVOKED;
 				r->error_code = final_ret;

source4/kdc/mit_samba.c
@@ -1035,7 +1035,7 @@ out:
 
 void mit_samba_zero_bad_password_count(krb5_db_entry *db_entry)
 {
-	struct netr_SendToSamBase *send_to_sam = NULL;
+	/* struct netr_SendToSamBase *send_to_sam = NULL; */
 	struct samba_kdc_entry *p =
 		talloc_get_type_abort(db_entry->e_data, struct samba_kdc_entry);
 	struct ldb_dn *domain_dn;
@@ -1046,7 +1046,7 @@ void mit_samba_zero_bad_password_count(krb5_db_entry *db_entry)
 					 p->msg,
 					 domain_dn,
 					 true,
-					 &send_to_sam);
+					 NULL, NULL);
 	/* TODO: RODC support */
 }
 

source4/auth/tests/sam.c
@@ -1446,7 +1446,7 @@ static void test_success_accounting_start_txn_failed(void **state) {
 	ldb_transaction_start_ret = LDB_ERR_OPERATIONS_ERROR;
 
 	status = authsam_logon_success_accounting(
-		ldb, msg, domain_dn, true, NULL);
+		ldb, msg, domain_dn, true, NULL, NULL);
 	assert_true(NT_STATUS_EQUAL(status, NT_STATUS_INTERNAL_ERROR));
 
 	/*
@@ -1502,7 +1502,7 @@ static void test_success_accounting_reread_failed(void **state) {
 	will_return(__wrap_dsdb_search_dn, LDB_ERR_NO_SUCH_OBJECT);
 
 	status = authsam_logon_success_accounting(
-		ldb, msg, domain_dn, true, NULL);
+		ldb, msg, domain_dn, true, NULL, NULL);
 	assert_true(NT_STATUS_EQUAL(status, NT_STATUS_INTERNAL_ERROR));
 	assert_true(transaction_cancelled);
 
@@ -1561,7 +1561,7 @@ static void test_success_accounting_ldb_msg_new_failed(void **state) {
 	ldb_msg_new_fail = true;
 
 	status = authsam_logon_success_accounting(
-		ldb, msg, domain_dn, true, NULL);
+		ldb, msg, domain_dn, true, NULL, NULL);
 	assert_true(NT_STATUS_EQUAL(status, NT_STATUS_NO_MEMORY));
 	assert_true(transaction_cancelled);
 
@@ -1612,7 +1612,7 @@ static void test_success_accounting_samdb_rodc_failed(void **state) {
 	samdb_rodc_ret = LDB_ERR_OPERATIONS_ERROR;
 
 	status = authsam_logon_success_accounting(
-		ldb, msg, domain_dn, true, NULL);
+		ldb, msg, domain_dn, true, NULL, NULL);
 	assert_true(NT_STATUS_EQUAL(status, NT_STATUS_INTERNAL_ERROR));
 	assert_false(in_transaction);
 	assert_false(transaction_cancelled);
@@ -1675,7 +1675,7 @@ static void test_success_accounting_update_lastlogon_failed(void **state) {
 	will_return(__wrap_samdb_msg_add_int64, LDB_ERR_OPERATIONS_ERROR);
 
 	status = authsam_logon_success_accounting(
-		ldb, msg, domain_dn, true, NULL);
+		ldb, msg, domain_dn, true, NULL, NULL);
 	assert_true(NT_STATUS_EQUAL(status, NT_STATUS_NO_MEMORY));
 	assert_true(transaction_cancelled);
 
@@ -1737,7 +1737,7 @@ static void test_success_accounting_build_mod_req_failed(void **state) {
 	will_return(__wrap_samdb_msg_add_int64, LDB_SUCCESS);
 
 	status = authsam_logon_success_accounting(
-		ldb, msg, domain_dn, true, NULL);
+		ldb, msg, domain_dn, true, NULL, NULL);
 	assert_true(NT_STATUS_EQUAL(status, NT_STATUS_INTERNAL_ERROR));
 	assert_true(transaction_cancelled);
 
@@ -1800,7 +1800,7 @@ static void test_success_accounting_add_control_failed(void **state) {
 	will_return(__wrap_samdb_msg_add_int64, LDB_SUCCESS);
 
 	status = authsam_logon_success_accounting(
-		ldb, msg, domain_dn, true, NULL);
+		ldb, msg, domain_dn, true, NULL, NULL);
 	assert_true(NT_STATUS_EQUAL(status, NT_STATUS_INTERNAL_ERROR));
 	assert_true(transaction_cancelled);
 
@@ -1863,7 +1863,7 @@ static void test_success_accounting_ldb_request_failed(void **state) {
 	will_return(__wrap_samdb_msg_add_int64, LDB_SUCCESS);
 
 	status = authsam_logon_success_accounting(
-		ldb, msg, domain_dn, true, NULL);
+		ldb, msg, domain_dn, true, NULL, NULL);
 	assert_true(NT_STATUS_EQUAL(status, NT_STATUS_INTERNAL_ERROR));
 	assert_true(transaction_cancelled);
 
@@ -1926,7 +1926,7 @@ static void test_success_accounting_ldb_wait_failed(void **state) {
 	will_return(__wrap_samdb_msg_add_int64, LDB_SUCCESS);
 
 	status = authsam_logon_success_accounting(
-		ldb, msg, domain_dn, true, NULL);
+		ldb, msg, domain_dn, true, NULL, NULL);
 	assert_true(NT_STATUS_EQUAL(status, NT_STATUS_INTERNAL_ERROR));
 	assert_true(transaction_cancelled);
 
@@ -1989,7 +1989,7 @@ static void test_success_accounting_commit_failed(void **state) {
 	will_return(__wrap_samdb_msg_add_int64, LDB_SUCCESS);
 
 	status = authsam_logon_success_accounting(
-		ldb, msg, domain_dn, true, NULL);
+		ldb, msg, domain_dn, true, NULL, NULL);
 	assert_true(NT_STATUS_EQUAL(status, NT_STATUS_INTERNAL_ERROR));
 	assert_true(in_transaction);
 	assert_false(transaction_cancelled);
@@ -2055,7 +2055,7 @@ static void test_success_accounting_rollback_failed(void **state) {
 	will_return(__wrap_samdb_msg_add_int64, LDB_SUCCESS);
 
 	status = authsam_logon_success_accounting(
-		ldb, msg, domain_dn, true, NULL);
+		ldb, msg, domain_dn, true, NULL, NULL);
 	assert_true(NT_STATUS_EQUAL(status, NT_STATUS_INTERNAL_ERROR));
 	assert_true(in_transaction);
 	assert_false(transaction_cancelled);
@@ -2124,7 +2124,7 @@ static void test_success_accounting_spurious_bad_pwd_indicator(void **state) {
 	ldb_build_mod_req_res = talloc_zero(ctx, struct ldb_request);
 
 	status = authsam_logon_success_accounting(
-		ldb, msg, domain_dn, true, NULL);
+		ldb, msg, domain_dn, true, NULL, NULL);
 	assert_true(NT_STATUS_EQUAL(status, NT_STATUS_OK));
 	assert_false(in_transaction);
 	assert_false(transaction_cancelled);