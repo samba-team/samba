6b6f8debb5dedf1520a7bedfa0277849f70683dd
Volker Lendecke <vl@samba.org>
2022-11-29 10:46:42 +0100

    libcldap: Save lines in cldap_netlogon_create_filter() with talloc_asprintf_addbuf()
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- libcli/cldap/cldap.c
+++ libcli/cldap/cldap.c
@@ -906,54 +906,36 @@ char *cldap_netlogon_create_filter(TALLOC_CTX *mem_ctx,
 
 	filter = talloc_asprintf(mem_ctx, "(&(NtVer=%s)",
 				 ldap_encode_ndr_uint32(mem_ctx, io->in.version));
-	if (filter == NULL)
-		return NULL;
 
 	if (io->in.user) {
-		filter = talloc_asprintf_append_buffer(filter, "(User=%s)", io->in.user);
-		if (filter == NULL) {
-			return NULL;
-		}
+		talloc_asprintf_addbuf(&filter, "(User=%s)", io->in.user);
 	}
 	if (io->in.host) {
-		filter = talloc_asprintf_append_buffer(filter, "(Host=%s)", io->in.host);
-		if (filter == NULL) {
-			return NULL;
-		}
+		talloc_asprintf_addbuf(&filter, "(Host=%s)", io->in.host);
 	}
 	if (io->in.realm) {
-		filter = talloc_asprintf_append_buffer(filter, "(DnsDomain=%s)", io->in.realm);
-		if (filter == NULL) {
-			return NULL;
-		}
+		talloc_asprintf_addbuf(&filter, "(DnsDomain=%s)", io->in.realm);
 	}
 	if (io->in.acct_control != -1) {
-		filter = talloc_asprintf_append_buffer(filter, "(AAC=%s)", 
-						ldap_encode_ndr_uint32(mem_ctx, io->in.acct_control));
-		if (filter == NULL) {
-			return NULL;
-		}
+		talloc_asprintf_addbuf(
+			&filter,
+			"(AAC=%s)",
+			ldap_encode_ndr_uint32(mem_ctx, io->in.acct_control));
 	}
 	if (io->in.domain_sid) {
 		struct dom_sid *sid = dom_sid_parse_talloc(mem_ctx, io->in.domain_sid);
 
-		filter = talloc_asprintf_append_buffer(filter, "(domainSid=%s)",
-						ldap_encode_ndr_dom_sid(mem_ctx, sid));
-		if (filter == NULL) {
-			return NULL;
-		}
+		 talloc_asprintf_addbuf(&filter, "(domainSid=%s)",
+					ldap_encode_ndr_dom_sid(mem_ctx, sid));
 	}
 	if (io->in.domain_guid) {
 		struct GUID guid;
 		GUID_from_string(io->in.domain_guid, &guid);
 
-		filter = talloc_asprintf_append_buffer(filter, "(DomainGuid=%s)",
-						ldap_encode_ndr_GUID(mem_ctx, &guid));
-		if (filter == NULL) {
-			return NULL;
-		}
+		talloc_asprintf_addbuf(&filter, "(DomainGuid=%s)",
+				       ldap_encode_ndr_GUID(mem_ctx, &guid));
 	}
-	filter = talloc_asprintf_append_buffer(filter, ")");
+	talloc_asprintf_addbuf(&filter, ")");
 
 	return filter;
 }
