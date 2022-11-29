8d58174cddeb2a5e62557ca23724b7acbb56e4c5
Ralph Boehme <slow@samba.org>
2022-11-29 12:07:19 +0100

    lib/torture: fix tctx arg usage in torture_assert_nttime_equal() macro
    Signed-off-by: Ralph Boehme <slow@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- lib/torture/torture.h
+++ lib/torture/torture.h
@@ -685,7 +685,7 @@ static inline void torture_dump_data_str_cb(const char *buf, void *private_data)
 #define torture_assert_nttime_equal(torture_ctx,got,expected,cmt) \
 	do { NTTIME __got = got, __expected = expected; \
 	if (!nt_time_equal(&__got, &__expected)) { \
-		torture_result(torture_ctx, TORTURE_FAIL, __location__": "#got" was %s, expected %s: %s", nt_time_string(tctx, __got), nt_time_string(tctx, __expected), cmt); \
+		torture_result(torture_ctx, TORTURE_FAIL, __location__": "#got" was %s, expected %s: %s", nt_time_string(torture_ctx, __got), nt_time_string(torture_ctx, __expected), cmt); \
 		return false; \
 	}\
 	} while(0)
