f9c982b5a96bed750bdff0a5243dda73c3d17587
Volker Lendecke <vl@samba.org>
2022-12-06 10:54:48 +0100

    smbd: Simplify dos_mode_msdfs()
    Use ISDOT[DOT]
    
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/smbd/dosmode.c
+++ source3/smbd/dosmode.c
@@ -554,8 +554,7 @@ uint32_t dos_mode_msdfs(connection_struct *conn,
 		}
 
 		/* Only . and .. are not hidden. */
-		if (p[0] == '.' && !((p[1] == '\0') ||
-				(p[1] == '.' && p[2] == '\0'))) {
+		if ((p[0] == '.') && !(ISDOT(p) || ISDOTDOT(p))) {
 			result |= FILE_ATTRIBUTE_HIDDEN;
 		}
 	}
