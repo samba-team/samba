samba-team/samba

commit dd9cdfb3b1488a5f262767089d456ae9269f72f0

s3: libsmb: For SMB2 opens on a DFS share, convert to a DFS path if not already done.
Signed-off-by: Jeremy Allison <jra@samba.org>
Reviewed-by: Noel Power <npower@samba.org>

source3/libsmb/cli_smb2_fnum.c
@@ -154,6 +154,42 @@ static uint8_t flags_to_smb2_oplock(uint32_t create_flags)
 }
 
 /***************************************************************
+ If we're on a DFS share, ensure we convert to a full DFS path
+ if this hasn't already been done.
+***************************************************************/
+
+static const char *smb2_dfs_share_path(TALLOC_CTX *ctx,
+				       struct cli_state *cli,
+				       const char *path)
+{
+	bool is_dfs = smbXcli_conn_dfs_supported(cli->conn) &&
+			smbXcli_tcon_is_dfs_share(cli->smb2.tcon);
+	bool is_already_dfs_path = false;
+
+	if (!is_dfs) {
+		return path;
+	}
+	is_already_dfs_path = cli_dfs_is_already_full_path(cli, path);
+	if (is_already_dfs_path) {
+		return path;
+	}
+	if (path[0] == '\0') {
+		return talloc_asprintf(ctx,
+				       "%s\\%s",
+					smbXcli_conn_remote_name(cli->conn),
+					cli->share);
+	}
+	while (*path == '\\') {
+		path++;
+	}
+	return talloc_asprintf(ctx,
+			       "%s\\%s\\%s",
+			       smbXcli_conn_remote_name(cli->conn),
+			       cli->share,
+			       path);
+}
+
+/***************************************************************
  Small wrapper that allows SMB2 create to return a uint16_t fnum.
 ***************************************************************/
 
@@ -252,6 +288,11 @@ struct tevent_req *cli_smb2_create_fnum_send(
 				return tevent_req_post(req, ev);
 			}
 		}
+	}
+
+	fname = smb2_dfs_share_path(state, cli, fname);
+	if (tevent_req_nomem(fname, req)) {
+		return tevent_req_post(req, ev);
 	}
 
 	/* SMB2 is pickier about pathnames. Ensure it doesn't

commit 7fcbae4e4940c8d89717c4fa4199d57d69c1d3a4

libsmb: Don't mess up pathnames in cli_smb2_create_fnum_send()
Master-only bug introduced with dd9cdfb3b14: smb2_dfs_share_path() can
change the length of fname, and if it happens that the original length
hits a \ in the enlarged filename, we cut it off.

Found by accident, this really made me scratch my head when looking at
traces :-)

Signed-off-by: Volker Lendecke <vl@samba.org>
Reviewed-by: Jeremy Allison <jra@samba.org>

source3/libsmb/cli_smb2_fnum.c
@@ -287,6 +287,7 @@ struct tevent_req *cli_smb2_create_fnum_send(
 	if (tevent_req_nomem(fname, req)) {
 		return tevent_req_post(req, ev);
 	}
+	fname_len = strlen(fname);
 
 	/* SMB2 is pickier about pathnames. Ensure it doesn't
 	   start in a '\' */