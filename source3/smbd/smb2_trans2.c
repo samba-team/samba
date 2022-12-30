0cfea6079270de7a9b420cb3ad34e31cfdd5d037
Volker Lendecke <vl@samba.org>
2022-12-30 22:22:31 +0100

    smbd: Remove an unnecessary if-statement
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Ralph Boehme <slow@samba.org>

--- source3/smbd/smb2_trans2.c
+++ source3/smbd/smb2_trans2.c
@@ -2936,10 +2936,7 @@ static unsigned int count_acl_entries(connection_struct *conn, SMB_ACL_T posix_a
 	SMB_ACL_ENTRY_T entry;
 
 	while ( posix_acl && (sys_acl_get_entry(posix_acl, entry_id, &entry) == 1)) {
-		/* get_next... */
-		if (entry_id == SMB_ACL_FIRST_ENTRY) {
-			entry_id = SMB_ACL_NEXT_ENTRY;
-		}
+		entry_id = SMB_ACL_NEXT_ENTRY;
 		ace_count++;
 	}
 	return ace_count;
