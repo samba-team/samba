8f53f32bffff61c35cbd06b9e74408e680fb6abb
Björn Baumbach <bb@sernet.de>
2023-02-24 16:27:17 +0100

    testprogs: use more unique names in "net ads dns" tests
    ADMINNAME can be used for records, created by the AD admin
    MACHINENAME for records, created by the machine (-P)
    UNPRIVNAME for records, created by the unprivileged user
    
    Signed-off-by: Björn Baumbach <bb@sernet.de>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- testprogs/blackbox/test_net_ads_dns.sh
+++ testprogs/blackbox/test_net_ads_dns.sh
@@ -42,7 +42,8 @@ IPADDRESS=10.1.4.111
 IP6ADDRESS=fd00:1a1a::1:5ee:bad:c0de
 IPADDRMAC=10.1.4.124
 UNPRIVIP=10.1.4.130
-NAME=testname.$$
+ADMINNAME=testname.$$
+MACHINENAME=membername.$$
 UNPRIVNAME=unprivname.$$
 UNPRIVUSER=unprivuser.$$
 UNPRIVPASS=UnPrivPass1
@@ -51,35 +52,39 @@ UNPRIVPASS=UnPrivPass1
 # unprivileged users cannot do so.
 echo "Starting ..."
 
-testit "admin user should be able to add a DNS entry $NAME.$REALM $IPADDRESS $IP6ADDRESS" $VALGRIND $net_tool ads dns register $NAME.$REALM $IPADDRESS $IP6ADDRESS -U$DC_USERNAME%$DC_PASSWORD || failed=$(expr $failed + 1)
+testit "admin user should be able to add a DNS entry $ADMINNAME.$REALM $IPADDRESS $IP6ADDRESS" \
+	$VALGRIND $net_tool ads dns register $ADMINNAME.$REALM $IPADDRESS $IP6ADDRESS -U$DC_USERNAME%$DC_PASSWORD ||
+	failed=$(expr $failed + 1)
 
 testit_grep_count \
-	"We should be able to see the new name $NAME.$REALM $IPADDRESS" \
+	"We should be able to see the new name $ADMINNAME.$REALM $IPADDRESS" \
 	"$IPADDRESS" \
 	1 \
-	dig @$SERVER +short -t a $NAME.$REALM ||
+	dig @$SERVER +short -t a $ADMINNAME.$REALM ||
 	failed=$(expr $failed + 1)
 testit_grep_count \
-	"We should be able to see the new name $NAME.$REALM $IP6ADDRESS" \
+	"We should be able to see the new name $ADMINNAME.$REALM $IP6ADDRESS" \
 	"$IP6ADDRESS" \
 	1 \
-	dig @$SERVER +short -t aaaa $NAME.$REALM ||
+	dig @$SERVER +short -t aaaa $ADMINNAME.$REALM ||
 	failed=$(expr $failed + 1)
 
-testit "We should be able to unregister the name $NAME.$REALM" $VALGRIND $net_tool ads dns unregister $NAME.$REALM -U$DC_USERNAME%$DC_PASSWORD || failed=$(expr $failed + 1)
+testit "We should be able to unregister the name $ADMINNAME.$REALM" \
+	$VALGRIND $net_tool ads dns unregister $ADMINNAME.$REALM -U$DC_USERNAME%$DC_PASSWORD ||
+	failed=$(expr $failed + 1)
 
 testit_grep_count \
-	"The name $NAME.$REALM $IPADDRESS should not be there any longer" \
+	"The name $ADMINNAME.$REALM $IPADDRESS should not be there any longer" \
 	"$IPADDRESS" \
 	0 \
-	dig @$SERVER +short -t a $NAME.$REALM ||
+	dig @$SERVER +short -t a $ADMINNAME.$REALM ||
 	failed=$(expr $failed + 1)
 
 testit_grep_count \
-	"The name $NAME.$REALM $IP6ADDRESS should not be there any longer" \
+	"The name $ADMINNAME.$REALM $IP6ADDRESS should not be there any longer" \
 	"$IP6ADDRESS" \
 	0 \
-	dig @$SERVER +short -t aaaa $NAME.$REALM ||
+	dig @$SERVER +short -t aaaa $ADMINNAME.$REALM ||
 	failed=$(expr $failed + 1)
 
 # prime the kpasswd server, see "git blame" for an explanation
@@ -102,17 +107,21 @@ testit "We should have enabled the account" test $STATUS -eq 0 || failed=$(expr 
 testit "Unprivileged users should be able to add new names" $net_tool ads dns register $UNPRIVNAME.$REALM $UNPRIVIP -U$UNPRIVUSER%$UNPRIVPASS || failed=$(expr $failed + 1)
 
 # This should work as well
-testit "machine account should be able to add a DNS entry net ads dns register membername.$REALM $IPADDRMAC -P " $net_tool ads dns register membername.$REALM $IPADDRMAC -P || failed=$(expr $failed + 1)
+testit "machine account should be able to add a DNS entry net ads dns register $MACHINENAME.$REALM $IPADDRMAC -P" \
+	$net_tool ads dns register $MACHINENAME.$REALM $IPADDRMAC -P ||
+	failed=$(expr $failed + 1)
 
 testit_grep_count \
-	"We should be able to see the new name membername.$REALM" \
+	"We should be able to see the new name $MACHINENAME.$REALM" \
 	"$IPADDRMAC" \
 	1 \
-	dig @$SERVER +short -t a membername.$REALM ||
+	dig @$SERVER +short -t a $MACHINENAME.$REALM ||
 	failed=$(expr $failed + 1)
 
 #Unprivileged users should not be able to overwrite other's names
-testit_expect_failure "Unprivileged users should not be able to modify existing names" $net_tool ads dns register membername.$REALM $UNPRIVIP -U$UNPRIVUSER%$UNPRIVPASS || failed=$(expr $failed + 1)
+testit_expect_failure "Unprivileged users should not be able to modify existing names" \
+	$net_tool ads dns register $MACHINENAME.$REALM $UNPRIVIP -U$UNPRIVUSER%$UNPRIVPASS ||
+	failed=$(expr $failed + 1)
 
 testit "We should be able to unregister the name $UNPRIVNAME.$REALM $IPADDRESS" \
 	$VALGRIND $net_tool ads dns unregister $UNPRIVNAME.$REALM -U$UNPRIVUSER%$UNPRIVPASS ||
