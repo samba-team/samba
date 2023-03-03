9447c4e81e04df5b8d775fb62f3440f0d9076002
Joseph Sutton <josephsutton@catalyst.net.nz>
2023-03-03 17:34:29 +1300

    CVE-2023-0614 ldb: Prevent disclosure of confidential attributes
    Add a hook, acl_redact_msg_for_filter(), in the aclread module, that
    marks inaccessible any message elements used by an LDAP search filter
    that the user has no right to access. Make the various ldb_match_*()
    functions check whether message elements are accessible, and refuse to
    match any that are not. Remaining message elements, not mentioned in the
    search filter, are checked in aclread_callback(), and any inaccessible
    elements are removed at this point.
    
    Certain attributes, namely objectClass, distinguishedName, name, and
    objectGUID, are always present, and hence the presence of said
    attributes is always allowed to be checked in a search filter. This
    corresponds with the behaviour of Windows.
    
    Further, we unconditionally allow the attributes isDeleted and
    isRecycled in a check for presence or equality. Windows is not known to
    make this special exception, but it seems mostly harmless, and should
    mitigate the performance impact on searches made by the show_deleted
    module.
    
    As a result of all these changes, our behaviour regarding confidential
    attributes happens to match Windows more closely. For the test in
    confidential_attr.py, we can now model our attribute handling with
    DC_MODE_RETURN_ALL, which corresponds to the behaviour exhibited by
    Windows.
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=15270
    
    Pair-Programmed-With: Andrew Bartlett <abartlet@samba.org>
    
    Signed-off-by: Joseph Sutton <josephsutton@catalyst.net.nz>
    Signed-off-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>
    
    [abartlet@samba.org adapted due to Samba 4.17 and lower
     not having the patches for CVE-2020-25720 and 4.16 and lower
     not having the patches for CVE-2022-32743 ]

--- source4/dsdb/samdb/samdb.h
+++ source4/dsdb/samdb/samdb.h
@@ -221,6 +221,8 @@ struct dsdb_control_transaction_identifier {
 	struct GUID transaction_guid;
 };
 
+#define DSDB_CONTROL_ACL_READ_OID "1.3.6.1.4.1.7165.4.3.37"
+
 #define DSDB_EXTENDED_REPLICATED_OBJECTS_OID "1.3.6.1.4.1.7165.4.4.1"
 struct dsdb_extended_replicated_object {
 	struct ldb_message *msg;
