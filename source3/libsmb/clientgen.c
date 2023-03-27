ec3229ed5c892eee53fe2d7aefac5a1efd7ab731
Andreas Schneider <asn@samba.org>
2023-03-27 09:59:22 +0200

    s3:libsmb: Remove trailing whitespaces in clientgen.c
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/libsmb/clientgen.c
+++ source3/libsmb/clientgen.c
@@ -1,19 +1,19 @@
-/* 
+/*
    Unix SMB/CIFS implementation.
    SMB client generic functions
    Copyright (C) Andrew Tridgell 1994-1998
    Copyright (C) Jeremy Allison 2007.
-   
+
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -119,7 +119,7 @@ struct cli_state *cli_state_create(TALLOC_CTX *mem_ctx,
 
 	/* Set the CLI_FORCE_DOSERR environment variable to test
 	   client routines using DOS errors instead of STATUS32
-	   ones.  This intended only as a temporary hack. */	
+	   ones.  This intended only as a temporary hack. */
 	if (getenv("CLI_FORCE_DOSERR")) {
 		force_dos_errors = true;
 	}
