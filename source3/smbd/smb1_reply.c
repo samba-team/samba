0cb37f78c1c7d381c7b271ec2147094182c9aa03
Ralph Boehme <slow@samba.org>
2023-03-30 16:01:42 +0200

    smbd: use smb1_strip_dfs_path() in reply_setatr()
    Signed-off-by: Ralph Boehme <slow@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/smbd/smb1_reply.c
+++ source3/smbd/smb1_reply.c
@@ -921,6 +921,11 @@ void reply_setatr(struct smb_request *req)
 	if (ucf_flags & UCF_GMT_PATHNAME) {
 		extract_snapshot_token(fname, &twrp);
 	}
+	status = smb1_strip_dfs_path(ctx, &ucf_flags, &fname);
+	if (!NT_STATUS_IS_OK(status)) {
+		reply_nterror(req, status);
+		goto out;
+	}
 	status = filename_convert_dirfsp(ctx,
 					 conn,
 					 fname,
