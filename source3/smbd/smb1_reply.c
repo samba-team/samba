c44491676ea6ac288b901983923b0ed6e8712eda
Ralph Boehme <slow@samba.org>
2023-03-30 16:05:04 +0200

    smbd: use smb1_strip_dfs_path() in reply_rmdir()
    Signed-off-by: Ralph Boehme <slow@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/smbd/smb1_reply.c
+++ source3/smbd/smb1_reply.c
@@ -6074,6 +6074,12 @@ void reply_rmdir(struct smb_request *req)
 	if (ucf_flags & UCF_GMT_PATHNAME) {
 		extract_snapshot_token(directory, &twrp);
 	}
+	status = smb1_strip_dfs_path(ctx, &ucf_flags, &directory);
+	if (!NT_STATUS_IS_OK(status)) {
+		reply_nterror(req, status);
+		goto out;
+	}
+
 	status = filename_convert_dirfsp(ctx,
 					 conn,
 					 directory,
