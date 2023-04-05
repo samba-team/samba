bfae42620365e8caf41f181286268e8f18470aaa
Andreas Schneider <asn@samba.org>
2023-04-05 08:47:16 +0200

    s3:tests: Use the CONFIGURATION passed down to the test
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Stefan Metzmacher <metze@samba.org>

--- source3/script/tests/test_smbclient_machine_auth.sh
+++ source3/script/tests/test_smbclient_machine_auth.sh
@@ -11,15 +11,28 @@ fi
 
 SERVER="$1"
 SMBCLIENT="$2"
-SMBCLIENT="$VALGRIND ${SMBCLIENT}"
-shift 2
+# This is used by test_smbclient()
+# shellcheck disable=2034
+CONFIGURATION="${3}"
+shift 3
 ADDARGS="$*"
 
-incdir=$(dirname $0)/../../../testprogs/blackbox
-. $incdir/subunit.sh
+# This is used by test_smbclient()
+# shellcheck disable=2034
+smbclient="${VALGRIND} ${SMBCLIENT}"
 
-testit "smbclient //$SERVER/tmp" $SMBCLIENT //$SERVER/tmp --machine-pass -p 139 -c quit $ADDARGS
+incdir="$(dirname "${0}")/../../../testprogs/blackbox"
+. "${incdir}/subunit.sh"
+. "${incdir}/common_test_fns.inc"
+
+failed=0
+
+test_smbclient "smbclient //${SERVER}/tmp" \
+	"quit" "//${SERVER}/tmp" --machine-pass -p 139 "${ADDARGS}" || \
+	failed=$((failed + 1))
 
 # Testing these here helps because we know the machine account isn't already this user/group
 testit "smbclient //$SERVER/forceuser" $SMBCLIENT //$SERVER/tmp --machine-pass -p 139 -c quit $ADDARGS
 testit "smbclient //$SERVER/forcegroup" $SMBCLIENT //$SERVER/tmp --machine-pass -p 139 -c quit $ADDARGS
+
+exit ${failed}
