9cbd4a3abdd7d4065da312b6ce7bbb5ff4615a61
Andreas Schneider <asn@samba.org>
2023-04-11 15:29:01 +0200

    s3:tests: Do not export UID_WRAPPER_ROOT in test_net_machine_account
    Just set it for the test.
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- source3/script/tests/test_net_machine_account.sh
+++ source3/script/tests/test_net_machine_account.sh
@@ -13,13 +13,22 @@ shift
 SERVER_IP="$1"
 shift
 
-export UID_WRAPPER_ROOT=1
-
 incdir=$(dirname $0)/../../../testprogs/blackbox
 . $incdir/subunit.sh
 
 failed=0
 
-testit "net_ads_user" $VALGRIND $NET rpc user --configfile="$CONFFILE" -S "$SERVER_IP" -P || failed=$(expr $failed + 1)
+net_ads_user() {
+	_out=$(UID_WRAPPER_INITIAL_RUID=0 UID_WRAPPER_INITIAL_EUID=0 \
+		${VALGRIND} "${NET}" rpc user \
+		--configfile="${CONFFILE}" -S "${SERVER_IP}" -P)
+	_ret=$?
+
+	echo "${_out}"
+
+	return ${_ret}
+}
+
+testit "net_ads_user" net_ads_user || failed=$((failed + 1))
 
 testok $0 $failed
