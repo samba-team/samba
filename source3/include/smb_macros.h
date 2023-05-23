af9dc206043809a706063019112a191e81456ed7
Ralph Boehme <slow@samba.org>
2023-05-23 17:26:03 +0200

    smbd: also reset struct stat_ex.cached_dos_attributes in SET_STAT_INVALID()
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=15375
    
    Signed-off-by: Ralph Boehme <slow@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>
    
    Autobuild-User(master): Jeremy Allison <jra@samba.org>
    Autobuild-Date(master): Wed May 24 21:42:50 UTC 2023 on atb-devel-224
    
    (cherry picked from commit 412373984db6d0c20ba38076d06d0a87631890d0)
    
    Autobuild-User(v4-18-test): Jule Anger <janger@samba.org>
    Autobuild-Date(v4-18-test): Wed May 31 10:47:15 UTC 2023 on atb-devel-224

--- source3/include/smb_macros.h
+++ source3/include/smb_macros.h
@@ -108,7 +108,10 @@
 
 #define VALID_STAT(st) ((st).st_ex_nlink != 0)
 #define VALID_STAT_OF_DIR(st) (VALID_STAT(st) && S_ISDIR((st).st_ex_mode))
-#define SET_STAT_INVALID(st) ((st).st_ex_nlink = 0)
+#define SET_STAT_INVALID(st) { \
+		(st).st_ex_nlink = 0;					\
+		(st).cached_dos_attributes = FILE_ATTRIBUTES_INVALID;	\
+};
 
 /* Macros to get at offsets within smb_lkrng and smb_unlkrng
    structures. We cannot define these as actual structures
