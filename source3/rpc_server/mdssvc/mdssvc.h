578e434a94147dc2d7dbfc006d2ab84807859c1d
Ralph Boehme <slow@samba.org>
2023-06-05 18:02:20 +0200

    CVE-2023-34968: mdssvc: return a fake share path
    Instead of returning the real server-side absolute path of shares and search
    results, return a fake absolute path replacing the path of the share with the
    share name, iow for a share "test" with a server-side path of "/foo/bar", we
    previously returned
    
      /foo/bar and
      /foo/bar/search/result
    
    and now return
    
      /test and
      /test/search/result
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=15388
    
    Signed-off-by: Ralph Boehme <slow@samba.org>
    Reviewed-by: Stefan Metzmacher <metze@samba.org>

--- source3/rpc_server/mdssvc/mdssvc.h
+++ source3/rpc_server/mdssvc/mdssvc.h
@@ -127,6 +127,7 @@ struct mds_ctx {
 	int snum;
 	const char *sharename;
 	const char *spath;
+	size_t spath_len;
 	struct connection_struct *conn;
 	struct sl_query *query_list;     /* list of active queries */
 	struct db_context *ino_path_map; /* dbwrap rbt for storing inode->path mappings */
