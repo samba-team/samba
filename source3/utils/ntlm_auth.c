2eabbe31f64a8456813a502afb05907beb46ffad
Ralph Boehme <slow@samba.org>
2023-06-16 12:28:47 +0200

    CVE-2022-2127: ntlm_auth: cap lanman response length value
    We already copy at most sizeof(request.data.auth_crap.lm_resp) bytes to the
    lm_resp buffer, but we don't cap the length indicator.
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=15072
    
    Signed-off-by: Ralph Boehme <slow@samba.org>

--- source3/utils/ntlm_auth.c
+++ source3/utils/ntlm_auth.c
@@ -576,10 +576,14 @@ NTSTATUS contact_winbind_auth_crap(const char *username,
 	memcpy(request.data.auth_crap.chal, challenge->data, MIN(challenge->length, 8));
 
 	if (lm_response && lm_response->length) {
+		size_t capped_lm_response_len = MIN(
+			lm_response->length,
+			sizeof(request.data.auth_crap.lm_resp));
+
 		memcpy(request.data.auth_crap.lm_resp,
 		       lm_response->data,
-		       MIN(lm_response->length, sizeof(request.data.auth_crap.lm_resp)));
-		request.data.auth_crap.lm_resp_len = lm_response->length;
+		       capped_lm_response_len);
+		request.data.auth_crap.lm_resp_len = capped_lm_response_len;
 	}
 
 	if (nt_response && nt_response->length) {
