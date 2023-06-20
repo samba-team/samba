0e43d11e39bf57dccebd661e028a717be2b8803c
Joseph Sutton <josephsutton@catalyst.net.nz>
2023-06-20 16:41:05 +1200

    s4:kdc: Remove useless sdb → hdb error code translation
    samba_kdc_check_s4u2proxy() is never going to return an SDB_* error
    code, so these conditions can never be hit.
    
    Signed-off-by: Joseph Sutton <josephsutton@catalyst.net.nz>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Stefan Metzmacher <metze@samba.org>

--- source4/kdc/hdb-samba4.c
+++ source4/kdc/hdb-samba4.c
@@ -305,33 +305,15 @@ hdb_samba4_check_constrained_delegation(krb5_context context, HDB *db,
 {
 	struct samba_kdc_db_context *kdc_db_ctx = NULL;
 	struct samba_kdc_entry *skdc_entry = NULL;
-	krb5_error_code ret;
 
 	kdc_db_ctx = talloc_get_type_abort(db->hdb_db,
 					   struct samba_kdc_db_context);
 	skdc_entry = talloc_get_type_abort(entry->context,
 					   struct samba_kdc_entry);
 
-	ret = samba_kdc_check_s4u2proxy(context, kdc_db_ctx,
-					skdc_entry,
-					target_principal);
-	switch (ret) {
-	case 0:
-		break;
-	case SDB_ERR_WRONG_REALM:
-		ret = HDB_ERR_WRONG_REALM;
-		break;
-	case SDB_ERR_NOENTRY:
-		ret = HDB_ERR_NOENTRY;
-		break;
-	case SDB_ERR_NOT_FOUND_HERE:
-		ret = HDB_ERR_NOT_FOUND_HERE;
-		break;
-	default:
-		break;
-	}
-
-	return ret;
+	return samba_kdc_check_s4u2proxy(context, kdc_db_ctx,
+					 skdc_entry,
+					 target_principal);
 }
 
 static krb5_error_code
