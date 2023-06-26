14cbe1b86716a83a2619671b65c72d522405e38e
Volker Lendecke <vl@samba.org>
2023-06-26 13:17:19 +0200

    error_inject: Enable returning EROFS for O_CREAT
    Bug: https://bugzilla.samba.org/show_bug.cgi?id=15402
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Ralph Boehme <slow@samba.org>
    (cherry picked from commit 37b3667f65d10a39b95dd84c002677d16f8c0776)

--- source3/modules/vfs_error_inject.c
+++ source3/modules/vfs_error_inject.c
@@ -116,6 +116,7 @@ static int vfs_error_inject_openat(struct vfs_handle_struct *handle,
 				   const struct vfs_open_how *how)
 {
 	int error = inject_unix_error("openat", handle);
+	int create_error = inject_unix_error("openat_create", handle);
 	int dirfsp_flags = (O_NOFOLLOW|O_DIRECTORY);
 	bool return_error;
 
@@ -126,6 +127,24 @@ static int vfs_error_inject_openat(struct vfs_handle_struct *handle,
 	dirfsp_flags |= O_SEARCH;
 #endif
 #endif
+
+	if ((create_error != 0) && (how->flags & O_CREAT)) {
+		struct stat_ex st = {
+			.st_ex_nlink = 0,
+		};
+		int ret;
+
+		ret = SMB_VFS_FSTATAT(handle->conn,
+				      dirfsp,
+				      smb_fname,
+				      &st,
+				      AT_SYMLINK_NOFOLLOW);
+
+		if ((ret == -1) && (errno == ENOENT)) {
+			errno = create_error;
+			return -1;
+		}
+	}
 
 	return_error = (error != 0);
 	return_error &= !fsp->fsp_flags.is_pathref;
