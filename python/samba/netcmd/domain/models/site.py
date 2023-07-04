b9d01c6420760e65012af8beaf46f2bfb5a7b33e
Rob van der Linde <rob@catalyst.net.nz>
2023-07-04 21:34:38 +1200

    netcmd: add Subnet and Site models
    Signed-off-by: Rob van der Linde <rob@catalyst.net.nz>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>
    Reviewed-by: Douglas Bagnall <douglas.bagnall@catalyst.net.nz>

--- python/samba/netcmd/domain/models/site.py
+++ python/samba/netcmd/domain/models/site.py
@@ -1,6 +1,6 @@
 # Unix SMB/CIFS implementation.
 #
-# Samba domain models.
+# Site model.
 #
 # Copyright (C) Catalyst.Net Ltd. 2023
 #
@@ -20,12 +20,26 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-from .auth_policy import AuthenticationPolicy
-from .auth_silo import AuthenticationSilo
-from .claim_type import ClaimType
-from .model import MODELS
-from .schema import AttributeSchema, ClassSchema
-from .site import Site
-from .subnet import Subnet
-from .user import User
-from .value_type import ValueType
+from .fields import BooleanField, DnField, IntegerField
+from .model import Model
+
+
+class Site(Model):
+    show_in_advanced_view_only = BooleanField("showInAdvancedViewOnly")
+    site_object_bl = DnField("siteObjectBL")
+    system_flags = IntegerField("systemFlags")
+
+    @staticmethod
+    def get_base_dn(ldb):
+        """Return the base DN for the Site model.
+
+        :param ldb: Ldb connection
+        :return: Dn to use for new objects
+        """
+        base_dn = ldb.get_config_basedn()
+        base_dn.add_child("CN=Sites")
+        return base_dn
+
+    @staticmethod
+    def get_object_class():
+        return "site"
