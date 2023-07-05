ba5b1f633aaad654a399c815c2a7001d560eb2fc
Andreas Schneider <asn@samba.org>
2023-07-05 11:11:20 +0200

    s3:include: Remove trailing whitepaces in MacExtensions.h
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Martin Schwenke <mschwenke@ddn.com>

--- source3/include/MacExtensions.h
+++ source3/include/MacExtensions.h
@@ -1,21 +1,21 @@
-/* 
+/*
    Unix SMB/CIFS implementation.
    SMB parameters and setup
    Copyright (C) Andrew Tridgell 1992-1998
    Copyright (C) John H Terpstra 1996-1998
    Copyright (C) Luke Kenneth Casson Leighton 1996-1998
    Copyright (C) Paul Ashton 1998
-    
+
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -47,7 +47,7 @@
 ** NT's AFP_AfpInfo stream structure
 */
 #define AFP_INFO_SIZE		0x3c
-#define AFP_Signature		0x41465000 
+#define AFP_Signature		0x41465000
 #define AFP_Version			0x00000100
 #define AFP_BackupTime		0x80000000
 #define AFP_FinderSize		32
@@ -73,7 +73,7 @@ typedef struct _AfpInfo
 
 typedef struct _SambaAfpInfo
 {
-	 AfpInfo       	afp; 
+	 AfpInfo       	afp;
 	 unsigned long 	createtime;
 } SambaAfpInfo;
 
@@ -102,30 +102,30 @@ typedef struct _SambaAfpInfo
 
 
 
-/* 
-** The server will return folder access control in the Trans2_Find_First2 
-** and Trans2_Find_Next2 message described later in this document. 
+/*
+** The server will return folder access control in the Trans2_Find_First2
+** and Trans2_Find_Next2 message described later in this document.
 */
 #define SUPPORT_MAC_ACCESS_CNTRL	0x0010
 /*
-** The server supports setting/getting comments using the mechanism in this 
-** document instead of using the NTFS format described in the Introduction. 
+** The server supports setting/getting comments using the mechanism in this
+** document instead of using the NTFS format described in the Introduction.
 */
 #define SUPPORT_MAC_GETSETCOMMENTS	0x0020
 /*
 ** The Server supports setting and getting Macintosh desktop database information
-** using the mechanism in this document. 
+** using the mechanism in this document.
 */
 #define SUPPORT_MAC_DESKTOPDB_CALLS	0x0040
 /*
-** The server will return a unique id for files and directories in the 
-** Trans2_Find_First2 and Trans2_Find_Next2 message described later in this document. 
+** The server will return a unique id for files and directories in the
+** Trans2_Find_First2 and Trans2_Find_Next2 message described later in this document.
 */
 #define SUPPORT_MAC_UNIQUE_IDS		0x0080
 /*
 ** The server will return this flag telling the client that the server does
 ** not support streams or the Macintosh extensions. The rest of this message
-** will be ignored by the client. 
+** will be ignored by the client.
 */
 #define NO_STREAMS_OR_MAC_SUPPORT	0x0100
 
@@ -150,7 +150,7 @@ enum {
 	otherSearch	= 0x0001,
 	Owner		= 0x0800
 };
-	
+
 
 /*
 ** We will be adding a new info level to the Trans2_Set_Path_Information.
@@ -178,20 +178,20 @@ enum {
 */
 
 /*
-** We need to be able to store an application name and its creator in a database. We send a 
-** Trans2_Set_Path_Information call with the full path of the application in the path field. 
+** We need to be able to store an application name and its creator in a database. We send a
+** Trans2_Set_Path_Information call with the full path of the application in the path field.
 ** We will send an info level that represents adding an application name and creator to the database.
-** We will pass the File Creator in the data message. 
+** We will pass the File Creator in the data message.
 **
 ** The server should just respond  with no error or an error.
 */
 #define SMB_MAC_DT_ADD_APPL	  0x304
 
 /*
-** We need to be able to remove an application name and its creator from a database. We send a 
-** Trans2_Set_Path_Information call with the full path of the application in the path field. 
+** We need to be able to remove an application name and its creator from a database. We send a
+** Trans2_Set_Path_Information call with the full path of the application in the path field.
 ** We will send an info level that represents removing an application name and creator from the database.
-** We will pass the File Creator in the data message. 
+** We will pass the File Creator in the data message.
 **
 ** The server should just respond  with no error or an error.
 */
@@ -199,9 +199,9 @@ enum {
 
 
 /*
-** We need to be able to get an application name and its creator from a database. We send a 
-** Trans2_Query_Path_Information call in which the name field is just ignore. 
-** We will send an info level that represents getting an application name with a structure that 
+** We need to be able to get an application name and its creator from a database. We send a
+** Trans2_Query_Path_Information call in which the name field is just ignore.
+** We will send an info level that represents getting an application name with a structure that
 ** contains the File Creator and index. Were index has the following meaning.
 **		Index = 0; Get the application path from the database with the most current date.
 **		Index > 0; Use the index to find the application path from the database.
@@ -215,35 +215,35 @@ enum {
 
 
 /*
-** We need to be able to get an icon from a database. We send a Trans2_Query_Path_Information call in 
+** We need to be able to get an icon from a database. We send a Trans2_Query_Path_Information call in
 ** which the path name is ignore. We will send an info level that represents getting an icon with a structure
 ** that contains the Requested size of the icon, the Icon type, File Creator, and File Type.
 **
-** The server returns with a structure that contains the actual size of the icon 
-** (must be less than requested length) and the icon bit map. 
+** The server returns with a structure that contains the actual size of the icon
+** (must be less than requested length) and the icon bit map.
 */
 #define SMB_MAC_DT_GET_ICON	  0x307
 
 
 /*
-** We need to be able to get an icon from a database. We send a Trans2_Query_Path_Information call in 
+** We need to be able to get an icon from a database. We send a Trans2_Query_Path_Information call in
 ** which the path name is ignore. We will send an info level that represents getting an icon with a structure
 ** that contains the index and File Creator. The index allows the client to make repeated calls to the server
 ** gathering all icon stored by this file creator.
-**		
 **
-** The server returns with a structure that contains the actual size of the icon 
-** (must be less than requested length) and the icon bit map, File Type, and Icon Type. 
+**
+** The server returns with a structure that contains the actual size of the icon
+** (must be less than requested length) and the icon bit map, File Type, and Icon Type.
 */
 #define SMB_MAC_DT_GET_ICON_INFO	  0x308
 
 
 
 /*
-** We need to be able to add an icon to a database. We send a Trans2_Set_Path_Information call in 
+** We need to be able to add an icon to a database. We send a Trans2_Set_Path_Information call in
 ** which the path name is ignore. We will send an info level that represents setting an icon with a structure
 ** that contains the icon data, icon size, icon type, the file type, and file creator.
-**		
+**
 **
 ** The server returns only that the call was successful or not.
 */
