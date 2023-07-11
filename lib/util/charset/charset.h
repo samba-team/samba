355fd3c7bf8af31800c351d70302ab48c72694c0
Joseph Sutton <josephsutton@catalyst.net.nz>
2023-07-11 13:50:53 +1200

    lib:charset: Update NUM_CHARSETS to reflect true value
    CH_DISPLAY was removed in commit
    125a2ff262aa312df20eec68802fd5f8a47f492f, but NUM_CHARSETS was not
    updated to match.
    
    By assigning to NUM_CHARSETS the last enumeration value in charset_t, we
    guard against its falling out of sync again.
    
    Signed-off-by: Joseph Sutton <josephsutton@catalyst.net.nz>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- lib/util/charset/charset.h
+++ lib/util/charset/charset.h
@@ -28,9 +28,17 @@
 #include <talloc.h>
 
 /* this defines the charset types used in samba */
-typedef enum {CH_UTF16LE=0, CH_UTF16=0, CH_UNIX, CH_DOS, CH_UTF8, CH_UTF16BE, CH_UTF16MUNGED} charset_t;
-
-#define NUM_CHARSETS 7
+typedef enum {
+	CH_UTF16LE = 0,
+	CH_UTF16 = 0,
+	CH_UNIX,
+	CH_DOS,
+	CH_UTF8,
+	CH_UTF16BE,
+	CH_UTF16MUNGED,
+	/* The number of distinct character sets. */
+	NUM_CHARSETS
+} charset_t;
 
 /*
  * SMB UCS2 (16-bit unicode) internal type.
