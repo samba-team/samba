feee201888367404dc1302888c76628c027de7ed
Andreas Schneider <asn@samba.org>
2023-07-18 11:30:56 +0200

    s3:smbd: Fix trailing white spaces in quotas.c
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Joseph Sutton <josephsutton@catalyst.net.nz>

--- source3/smbd/quotas.c
+++ source3/smbd/quotas.c
@@ -1,26 +1,26 @@
-/* 
+/*
    Unix SMB/CIFS implementation.
    support for quotas
    Copyright (C) Andrew Tridgell 1992-1998
-   
+
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 
-/* 
+/*
  * This is one of the most system dependent parts of Samba, and its
- * done a litle differently. Each system has its own way of doing 
+ * done a litle differently. Each system has its own way of doing
  * things :-(
  */
 
@@ -429,7 +429,7 @@ bool disk_quotas(connection_struct *conn, struct smb_filename *fname,
 	}
 
 	return True;
-	
+
 try_group_quota:
 	/*
 	 * First of all, check whether group quota is
