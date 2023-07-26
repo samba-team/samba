c39f90a12496fea74a11cbd8b34ad4074d2529db
Jeremy Allison <jra@samba.org>
2023-07-25 17:49:21 -0700

    CVE-2023-3961:s3:torture: Add test SMB2-INVALID-PIPENAME to show we allow bad pipenames with unix separators through to the UNIX domain socket code.
    The raw SMB2-INVALID-PIPENAME test passes against Windows 2022,
    as it just returns NT_STATUS_OBJECT_NAME_NOT_FOUND.
    
    Add the knownfail.
    
    BUG:https://bugzilla.samba.org/show_bug.cgi?id=15422
    
    Signed-off-by: Jeremy Allison <jra@samba.org>

--- source3/torture/torture.c
+++ source3/torture/torture.c
@@ -15845,6 +15845,10 @@ static struct {
 		.fn    = run_smb2_quota1,
 	},
 	{
+		.name  = "SMB2-INVALID-PIPENAME",
+		.fn    = run_smb2_invalid_pipename,
+	},
+	{
 		.name  = "SMB2-STREAM-ACL",
 		.fn    = run_smb2_stream_acl,
 	},
