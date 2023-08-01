867b192cadbca101761ecf7ee116dd35016f4b45
Joseph Sutton <josephsutton@catalyst.net.nz>
2023-08-02 09:44:18 +1200

    s3:rpc_server: Fix typo
    Signed-off-by: Joseph Sutton <josephsutton@catalyst.net.nz>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- source3/rpc_server/lsa/srv_lsa_nt.c
+++ source3/rpc_server/lsa/srv_lsa_nt.c
@@ -2056,7 +2056,7 @@ NTSTATUS _lsa_DeleteTrustedDomain(struct pipes_struct *p,
 
 	if (td->netbios_name == NULL || *td->netbios_name == '\0') {
 		struct dom_sid_buf buf;
-		DEBUG(10, ("Missing netbios name for for trusted domain %s.\n",
+		DEBUG(10, ("Missing netbios name for trusted domain %s.\n",
 			   dom_sid_str_buf(r->in.dom_sid, &buf)));
 		return NT_STATUS_UNSUCCESSFUL;
 	}
