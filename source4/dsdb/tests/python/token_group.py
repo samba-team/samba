5313a30714889ba96c68a8809a16697f99febb55
Andreas Schneider <asn@samba.org>
2023-08-02 10:44:32 +0200

    CVE-2023-4154 s4:dsdb:tests: Fix code spelling
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=15424
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Joseph Sutton <josephsutton@catalyst.net.nz>
    
    (cherry picked from commit b29793ffdee5d9b9c1c05830622e80f7faec7670)

--- source4/dsdb/tests/python/token_group.py
+++ source4/dsdb/tests/python/token_group.py
@@ -151,7 +151,7 @@ class StaticTokenTest(samba.tests.TestCase):
         extra_sidset = set(extra_sids)
 
         if len(missing_sidset.symmetric_difference(extra_sidset)):
-            print("dn token sids unexpeted")
+            print("dn token sids unexpected")
             print("tokengroups: %s" % dn_tokengroups)
             print("user sids: %s" % self.user_sids)
             print("actual difference: %s" % missing_sidset)
@@ -423,7 +423,7 @@ class DynamicTokenTest(samba.tests.TestCase):
         extra_sidset = set(extra_sids)
 
         if len(missing_sidset.symmetric_difference(extra_sidset)):
-            print("dn token sids unexpeted")
+            print("dn token sids unexpected")
             print("tokengroups: %s" % dn_tokengroups)
             print("user sids: %s" % self.user_sids)
             print("actual difference: %s" % missing_sidset)
