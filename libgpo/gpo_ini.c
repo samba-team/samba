c86038095e3aa18ef965cd6b7922fd85f758523e
Joseph Sutton <josephsutton@catalyst.net.nz>
2023-08-09 16:52:46 +1200

    libgpo: Remove unnecessary cast
    Signed-off-by: Joseph Sutton <josephsutton@catalyst.net.nz>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- libgpo/gpo_ini.c
+++ libgpo/gpo_ini.c
@@ -119,7 +119,7 @@ static NTSTATUS convert_file_from_ucs2(TALLOC_CTX *mem_ctx,
 	}
 
 	if (!convert_string_talloc(mem_ctx, CH_UTF16LE, CH_UNIX, data_in, n,
-				   (void *)&data_out, &converted_size))
+				   &data_out, &converted_size))
 	{
 		status = NT_STATUS_INVALID_BUFFER_SIZE;
 		goto out;
