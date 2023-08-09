329bfe47d9e59323ef5ec0fae014939a5ae5f440
Joseph Sutton <josephsutton@catalyst.net.nz>
2023-08-09 16:50:44 +1200

    auth: Remove unnecessary casts
    Signed-off-by: Joseph Sutton <josephsutton@catalyst.net.nz>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- auth/credentials/credentials_ntlm.c
+++ auth/credentials/credentials_ntlm.c
@@ -374,7 +374,7 @@ _PUBLIC_ bool cli_credentials_set_utf16_password(struct cli_credentials *cred,
 					   CH_UTF16MUNGED, CH_UTF8,
 					   password_utf16->data,
 					   password_utf16->length,
-					   (void *)&password_talloc,
+					   &password_talloc,
 					   &password_len);
 		if (!ok) {
 			TALLOC_FREE(nt_hash);
@@ -424,7 +424,7 @@ _PUBLIC_ bool cli_credentials_set_old_utf16_password(struct cli_credentials *cre
 				   CH_UTF16MUNGED, CH_UTF8,
 				   password_utf16->data,
 				   password_utf16->length,
-				   (void *)&password_talloc,
+				   &password_talloc,
 				   &password_len);
 	if (!ok) {
 		TALLOC_FREE(nt_hash);
