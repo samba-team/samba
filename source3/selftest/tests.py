963fd8aa9b76361ab9aeb63307773f2498b17879
Jeremy Allison <jra@samba.org>
2023-08-11 10:39:36 -0700

    s3: torture: Add SMB1-TRUNCATED-SESSSETUP test.
    Shows that we indirect through an uninitialized pointer and the client crashes
    it's own smbd.
    
    Add knownfail.
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=15420
    
    Signed-off-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: Volker Lendecke <vl@samba.org>

--- source3/selftest/tests.py
+++ source3/selftest/tests.py
@@ -219,6 +219,17 @@ plantestsuite("samba3.smbtorture_s3.hidenewfiles_showdirs",
                "",
                "-l $LOCAL_PATH"])
 
+plantestsuite("samba3.smbtorture_s3.smb1.SMB1-TRUNCATED-SESSSETUP",
+                "fileserver_smb1",
+                [os.path.join(samba3srcdir,
+                              "script/tests/test_smbtorture_s3.sh"),
+                'SMB1-TRUNCATED-SESSSETUP',
+                '//$SERVER_IP/tmp',
+                '$USERNAME',
+                '$PASSWORD',
+                smbtorture3,
+                "-mNT1"])
+
 #
 # MSDFS attribute tests.
 #
