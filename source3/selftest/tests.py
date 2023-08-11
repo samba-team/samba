122afc377246f722306df2d8c1b4ca5eb0aa7bb0
Jeremy Allison <jra@samba.org>
2023-08-11 15:12:05 -0700

    s3: torture: Add a test doing an SMB1 negotiate+exit.
    Robert Morris <rtm@lcs.mit.edu> noticed a missing
    return in reply_exit_done().
    
    Adds knownfail.
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=15430
    
    Signed-off-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: Noel Power <npower@samba.org>
    (cherry picked from commit 63895e03c4e8ed79a3b2cda928f58ec278cd6608)

--- source3/selftest/tests.py
+++ source3/selftest/tests.py
@@ -228,6 +228,17 @@ plantestsuite("samba3.smbtorture_s3.smb1.SMB1-TRUNCATED-SESSSETUP",
                 smbtorture3,
                 "-mNT1"])
 
+plantestsuite("samba3.smbtorture_s3.smb1.SMB1-NEGOTIATE-EXIT",
+                "fileserver_smb1",
+                [os.path.join(samba3srcdir,
+                              "script/tests/test_smbtorture_s3.sh"),
+                'SMB1-NEGOTIATE-EXIT',
+                '//$SERVER_IP/tmp',
+                '$USERNAME',
+                '$PASSWORD',
+                smbtorture3,
+                "-mNT1"])
+
 #
 # MSDFS attribute tests.
 #
