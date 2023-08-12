4145bfb1b5a3639caf26a310d612aec29fc00117
Jeremy Allison <jra@samba.org>
2023-08-11 17:28:53 -0700

    s3: smbd: Ensure init_smb1_request() zeros out what the incoming pointer points to.
    Remove the now unneeded req->xxx = NULL assignments (and the
    deliberately bogus req->session = (void *)0xDEADBEEF one
    used to demonstrate the bug).
    
    Remove knownfail.
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=15432
    
    Signed-off-by: Jeremy Allison <jra@samba.org>
    Reviewed-by: Noel Power <npower@samba.org>
    
    Autobuild-User(master): Noel Power <npower@samba.org>
    Autobuild-Date(master): Tue Aug 15 12:06:36 UTC 2023 on atb-devel-224

--- source3/smbd/smb2_process.c
+++ source3/smbd/smb2_process.c
@@ -731,6 +731,8 @@ bool init_smb1_request(struct smb_request *req,
 		return false;
 	}
 
+	*req = (struct smb_request) { .cmd = 0};
+
 	req->request_time = timeval_current();
 	now = timeval_to_nttime(&req->request_time);
 
@@ -749,18 +751,13 @@ bool init_smb1_request(struct smb_request *req,
 	req->encrypted = encrypted;
 	req->sconn = sconn;
 	req->xconn = xconn;
-	req->conn = NULL;
 	if (xconn != NULL) {
 		status = smb1srv_tcon_lookup(xconn, req->tid, now, &tcon);
 		if (NT_STATUS_IS_OK(status)) {
 			req->conn = tcon->compat;
 		}
 	}
-	req->chain_fsp = NULL;
-	req->smb2req = NULL;
-	req->chain = NULL;
 	req->posix_pathnames = lp_posix_pathnames();
-	req->session = (void *)0xDEADBEEF;
 
 	/* Ensure we have at least wct words and 2 bytes of bcc. */
 	if (smb_size + req->wct*2 > req_size) {
@@ -779,7 +776,6 @@ bool init_smb1_request(struct smb_request *req,
 		return false;
 	}
 
-	req->outbuf = NULL;
 	return true;
 }
 
