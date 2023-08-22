b6a665cc8e8bcc771df513ce005a04fe5f03a441
Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
2023-08-23 11:24:46 +1200

    librpc/ndr:ndr_sec_helper: fix a typo
    Signed-off-by: Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- librpc/ndr/ndr_sec_helper.c
+++ librpc/ndr/ndr_sec_helper.c
@@ -50,7 +50,7 @@ static size_t ndr_size_security_ace_core(const struct security_ace *ace, int fla
 			ret += 16; /* GUID ace->object.object.type.type */
 		}
 		if (ace->object.object.flags & SEC_ACE_INHERITED_OBJECT_TYPE_PRESENT) {
-			ret += 16; /* GUID ace->object.object.inherited_typeinherited_type */
+			ret += 16; /* GUID ace->object.object.inherited_type.inherited_type */
 		}
 		break;
 	default:
