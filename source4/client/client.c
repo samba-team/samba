20ab21e698fc20da24bce8a66c073924c8f0fc77
Joseph Sutton <josephsutton@catalyst.net.nz>
2023-09-07 16:12:03 +1200

    s4:client: Fix code spelling
    Signed-off-by: Joseph Sutton <josephsutton@catalyst.net.nz>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- source4/client/client.c
+++ source4/client/client.c
@@ -80,7 +80,7 @@ static unsigned int get_total_time_ms = 0;
 static uint64_t put_total_size = 0;
 static unsigned int put_total_time_ms = 0;
 
-/* Unfortunately, there is no way to pass the a context to the completion function as an argument */
+/* Unfortunately, there is no way to pass a context to the completion function as an argument */
 static struct smbclient_context *rl_ctx; 
 
 /* totals globals */
