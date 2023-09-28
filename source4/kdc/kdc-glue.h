45e8e197198a37488ee188a79e7c70ec964baa1a
Joseph Sutton <josephsutton@catalyst.net.nz>
2023-09-29 12:08:54 +1300

    s4:kdc: Remove unused declaration
    Signed-off-by: Joseph Sutton <josephsutton@catalyst.net.nz>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- source4/kdc/kdc-glue.h
+++ source4/kdc/kdc-glue.h
@@ -32,16 +32,6 @@
 #include "kdc/samba_kdc.h"
 #include "kdc/kdc-server.h"
 
-struct tsocket_address;
-
-kdc_code kpasswdd_process(struct kdc_server *kdc,
-			  TALLOC_CTX *mem_ctx,
-			  DATA_BLOB *input,
-			  DATA_BLOB *reply,
-			  struct tsocket_address *peer_addr,
-			  struct tsocket_address *my_addr,
-			  int datagram_reply);
-
 /* from hdb-samba4.c */
 NTSTATUS hdb_samba4_create_kdc(struct samba_kdc_base_context *base_ctx,
 			       krb5_context context, struct HDB **db);
