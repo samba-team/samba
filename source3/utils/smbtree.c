4d411cf5cecd4e584f500335310164ed22c199d7
Andreas Schneider <asn@samba.org>
2023-10-24 12:04:19 +0200

    s3:utils: Call gfree_all() before exit in smbtree
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- source3/utils/smbtree.c
+++ source3/utils/smbtree.c
@@ -1,20 +1,20 @@
-/* 
+/*
    Unix SMB/CIFS implementation.
    Network neighbourhood browser.
-   
+
    Copyright (C) Tim Potter      2000
    Copyright (C) Jelmer Vernooij 2003
-   
+
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -288,6 +288,7 @@ fail:
 		smbc_free_context(ctx, 0);
 		ctx = NULL;
 	}
+	gfree_all();
 	poptFreeContext(pc);
 	TALLOC_FREE(frame);
 	return result;
