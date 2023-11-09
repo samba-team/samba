11523b490c5f8eee5cd9e7faa1c348f71675623f
Christof Schmitt <cs@samba.org>
2023-11-09 12:39:57 -0700

    nfs4_acls: Make fstat_with_cap_dac_override static
    No other module is calling this function.
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=15507
    
    Signed-off-by: Christof Schmitt <cs@samba.org>
    Reviewed-by: Björn Jacke <bjacke@samba.org>
    (cherry picked from commit bffd8bd8c32fea738824b807eb9e5f97a609493e)

--- source3/modules/nfs4_acls.c
+++ source3/modules/nfs4_acls.c
@@ -192,8 +192,8 @@ int nfs4_acl_stat(struct vfs_handle_struct *handle,
 	return ret;
 }
 
-int fstat_with_cap_dac_override(int fd, SMB_STRUCT_STAT *sbuf,
-				bool fake_dir_create_times)
+static int fstat_with_cap_dac_override(int fd, SMB_STRUCT_STAT *sbuf,
+				       bool fake_dir_create_times)
 {
 	int ret;
 
