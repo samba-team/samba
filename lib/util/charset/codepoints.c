1947bd6d6d9b774e811ea0ad20ad282bc5fc2e2b
Joseph Sutton <josephsutton@catalyst.net.nz>
2023-11-14 11:02:15 +1300

    util/charset: Remove trailing whitespace
    Signed-off-by: Joseph Sutton <josephsutton@catalyst.net.nz>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- lib/util/charset/codepoints.c
+++ lib/util/charset/codepoints.c
@@ -1,21 +1,21 @@
-/* 
+/*
    Unix SMB/CIFS implementation.
    Character set conversion Extensions
    Copyright (C) Igor Vergeichik <iverg@mail.ru> 2001
    Copyright (C) Andrew Tridgell 2001
    Copyright (C) Simo Sorce 2001
    Copyright (C) Jelmer Vernooij 2007
-   
+
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -16512,8 +16512,8 @@ struct smb_iconv_handle *get_iconv_handle(void)
 	return global_iconv_handle;
 }
 
-struct smb_iconv_handle *get_iconv_testing_handle(TALLOC_CTX *mem_ctx, 
-						  const char *dos_charset, 
+struct smb_iconv_handle *get_iconv_testing_handle(TALLOC_CTX *mem_ctx,
+						  const char *dos_charset,
 						  const char *unix_charset,
 						  bool use_builtin_handlers)
 {
