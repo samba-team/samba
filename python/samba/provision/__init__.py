beefbb277a95b876e1fa323f9401da531d73ab9b
Joseph Sutton <josephsutton@catalyst.net.nz>
2023-11-23 20:59:59 +1300

    python: Remove unused parameters ‘backend_store’ and ‘backend_store_size’
    Signed-off-by: Joseph Sutton <josephsutton@catalyst.net.nz>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- python/samba/provision/__init__.py
+++ python/samba/provision/__init__.py
@@ -1342,9 +1342,7 @@ def setup_samdb(path, session_info, provision_backend, lp, names,
 def fill_samdb(samdb, lp, names, logger, policyguid,
                policyguid_dc, fill, adminpass, krbtgtpass, machinepass, dns_backend,
                dnspass, invocationid, ntdsguid, serverrole, am_rodc=False,
-               dom_for_fun_level=None, schema=None, next_rid=None, dc_rid=None,
-               backend_store=None,
-               backend_store_size=None):
+               dom_for_fun_level=None, schema=None, next_rid=None, dc_rid=None):
 
     if next_rid is None:
         next_rid = 1000
@@ -1956,9 +1954,7 @@ def provision_fill(samdb, secrets_ldb, logger, names, paths,
                            dns_backend=dns_backend, dnspass=dnspass,
                            ntdsguid=ntdsguid, serverrole=serverrole,
                            dom_for_fun_level=dom_for_fun_level, am_rodc=am_rodc,
-                           next_rid=next_rid, dc_rid=dc_rid,
-                           backend_store=backend_store,
-                           backend_store_size=backend_store_size)
+                           next_rid=next_rid, dc_rid=dc_rid)
 
         # Set up group policies (domain policy and domain controller
         # policy)
