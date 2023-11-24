78ec47a6674db65d738305cf00861aa711886a43
Stefan Metzmacher <metze@samba.org>
2023-11-24 16:28:38 +0100

    tdb: fix python/tdbdump.py example
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Günther Deschner <gd@samba.org>
    Reviewed-by: Andreas Schneider <asn@samba.org>

--- lib/tdb/python/tdbdump.py
+++ lib/tdb/python/tdbdump.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 # Trivial reimplementation of tdbdump in Python
 
-import tdb, sys
+import sys
+
+sys.path.insert(0, "bin/python")
+
+import tdb
 
 if len(sys.argv) < 2:
     print("Usage: tdbdump.py <tdb-file>")
     sys.exit(1)
 
 db = tdb.Tdb(sys.argv[1])
-for (k, v) in db.items():
+for k in db.keys():
+    v = db.get(k)
     print("{\nkey(%d) = %r\ndata(%d) = %r\n}" % (len(k), k, len(v), v))
