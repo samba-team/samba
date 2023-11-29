99c61fa85131af2d21d53dc0e949607833fb3a0c
Volker Lendecke <vl@samba.org>
2023-11-29 16:35:20 +0100

    lib: Fix some whitespace
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Guenther Deschner <gd@samba.org>

--- source3/lib/system_smbd.c
+++ source3/lib/system_smbd.c
@@ -1,4 +1,4 @@
-/* 
+/*
    Unix SMB/CIFS implementation.
    system call wrapper interface.
    Copyright (C) Andrew Tridgell 2002
@@ -8,19 +8,19 @@
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-/* 
+/*
    This file may assume linkage with smbd - for things like become_root()
-   etc. 
+   etc.
 */
 
 #include "includes.h"
