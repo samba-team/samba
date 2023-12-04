0b287f2e4e9a7935b8b07872fe06454a5a85e489
Jo Sutton <josutton@catalyst.net.nz>
2023-12-04 14:29:08 +1300

    lib:util: Remove trailing whitespace
    Signed-off-by: Jo Sutton <josutton@catalyst.net.nz>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- lib/util/time.h
+++ lib/util/time.h
@@ -1,4 +1,4 @@
-/* 
+/*
    Unix SMB/CIFS implementation.
    time utility functions
 
@@ -11,12 +11,12 @@
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -273,7 +273,7 @@ struct timeval timeval_current_ofs_msec(uint32_t msecs);
 struct timeval timeval_current_ofs_usec(uint32_t usecs);
 
 /**
-  compare two timeval structures. 
+  compare two timeval structures.
   Return -1 if tv1 < tv2
   Return 0 if tv1 == tv2
   Return 1 if tv1 > tv2
