b6c2c26e9ba02b6b7b0843e8a01344c774974dee
Ralph Boehme <slow@samba.org>
2023-12-21 19:40:21 +0100

    selftest: remove error_inject from shadow_write share
    Frankly, I can't remember why I added this as part of bug 13688. The
    goal of the corresponding test is to verify a write on a read-only
    file handle fails. As the file is opened O_RDONLY, the write will fail
    anyway and there's no need to inject the error.
    
    To make things worse, having the error injected meant we didn't notice
    when the underlying logic of forcing the open to be done with O_RDONLY
    was done as O_RDWR, resulting in the write on the handle to succeed.
    
    This happened when we introduced reopen_from_fsp(): the initial
    pathref open of a path with a twrp value was correctly detected and
    handled by shadow_copy2_openat(). However, when converting the pathref
    open to a real one via reopen_from_fsp(), shadow_copy2_openat() only
    sees the magic /proc/fd path and has no way of inferring that this was
    originating from a prevous version open with a twrp value.
    
    Tl;dr: we can just remove this error injection, it is not needed, the
    correct fix is to implement this in the SMB layer which is done in the
    subsequent commits.
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=13688
    
    Signed-off-by: Ralph Boehme <slow@samba.org>
    Reviewed-by: Stefan Metzmacher <metze@samba.org>
    (cherry picked from commit 78119edba013583555069271bb61134c12c2c135)

--- selftest/target/Samba3.pm
+++ selftest/target/Samba3.pm
@@ -3437,9 +3437,7 @@ sub provision($$)
 [shadow_write]
 	path = $shadow_tstdir
 	comment = previous versions snapshots under mount point
-	vfs objects = shadow_copy2 streams_xattr error_inject
-	aio write size = 0
-	error_inject:pwrite = EBADF
+	vfs objects = shadow_copy2 streams_xattr
 	shadow:mountpoint = $shadow_tstdir
 	shadow:fixinodes = yes
 	smbd async dosmode = yes
