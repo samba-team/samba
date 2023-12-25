b9f32b32e0e1463b8ca3e696d682ecf86503464b
Björn Jacke <bjacke@samba.org>
2023-12-25 19:50:55 +0100

    docs-xml: use XML_CATALOG_FILES env var if defined
    Thanks to Thierry LARONDE for the fix.
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=15540
    
    Signed-off-by: Björn Jacke <bjacke@samba.org>
    Reviewed-by: Stefan Metzmacher <metze@samba.org>

--- buildtools/wafsamba/wafsamba.py
+++ buildtools/wafsamba/wafsamba.py
@@ -1178,7 +1178,7 @@ def SAMBAMANPAGES(bld, manpages, extra_source=None):
     bld.env.SAMBA_EXPAND_XSL = bld.srcnode.abspath() + '/docs-xml/xslt/expand-sambadoc.xsl'
     bld.env.SAMBA_MAN_XSL = bld.srcnode.abspath() + '/docs-xml/xslt/man.xsl'
     bld.env.SAMBA_CATALOG = bld.bldnode.abspath() + '/docs-xml/build/catalog.xml'
-    bld.env.SAMBA_CATALOGS = 'file:///etc/xml/catalog file:///usr/local/share/xml/catalog file://' + bld.env.SAMBA_CATALOG
+    bld.env.SAMBA_CATALOGS = os.getenv('XML_CATALOG_FILES', 'file:///etc/xml/catalog file:///usr/local/share/xml/catalog') + ' file://' + bld.env.SAMBA_CATALOG
 
     for m in manpages.split():
         source = [m + '.xml']
