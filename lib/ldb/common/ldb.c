debc4bc31f796636352f478ef9f887392752bd42
Jo Sutton <josutton@catalyst.net.nz>
2024-01-25 10:28:31 +1300

    ldb: Fix code spelling
    Signed-off-by: Jo Sutton <josutton@catalyst.net.nz>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- lib/ldb/common/ldb.c
+++ lib/ldb/common/ldb.c
@@ -1969,7 +1969,7 @@ const char *ldb_errstring(struct ldb_context *ldb)
 }
 
 /*
-  return a string explaining what a ldb error constant meancs
+  return a string explaining what a ldb error constant means
 */
 const char *ldb_strerror(int ldb_err)
 {
