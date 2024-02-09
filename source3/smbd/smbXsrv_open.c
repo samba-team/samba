45200770fe6862b609adab3f620ef892bb9c27ff
Volker Lendecke <vl@samba.org>
2024-02-09 13:05:28 +0100

    smbd: Simplify smbXsrv_open_clear_replay_cache()
    GUID_buf_string() is designed to never fail
    
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/smbd/smbXsrv_open.c
+++ source3/smbd/smbXsrv_open.c
@@ -676,7 +676,6 @@ static NTSTATUS smbXsrv_open_clear_replay_cache(struct smbXsrv_open *op)
 {
 	struct GUID *create_guid;
 	struct GUID_txt_buf buf;
-	char *guid_string;
 	struct db_context *db;
 	NTSTATUS status;
 
@@ -695,12 +694,7 @@ static NTSTATUS smbXsrv_open_clear_replay_cache(struct smbXsrv_open *op)
 		return NT_STATUS_OK;
 	}
 
-	guid_string = GUID_buf_string(create_guid, &buf);
-	if (guid_string == NULL) {
-		return NT_STATUS_INVALID_PARAMETER;
-	}
-
-	status = dbwrap_purge_bystring(db, guid_string);
+	status = dbwrap_purge_bystring(db, GUID_buf_string(create_guid, &buf));
 
 	if (NT_STATUS_IS_OK(status)) {
 		op->flags &= ~SMBXSRV_OPEN_HAVE_REPLAY_CACHE;
