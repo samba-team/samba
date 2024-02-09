7107b233346f8540384a39b50c4f01ce3f5d2dc3
Martin Schwenke <mschwenke@ddn.com>
2024-02-09 17:29:46 +1100

    ctdb-protocol: Add missing push support for new controls
    CTDB_CONTROL_TCP_CLIENT_DISCONNECTED and
    CTDB_CONTROL_TCP_CLIENT_PASSED were added in commits
    c6602b686b4e50d93272667ef86d3904181fb1ab and
    037e8e449deb136ad5ed5e4de05439411b545b6d.  They were missing test
    support for the packet push/pull.  While adding the testing (for
    completeness, before adding another new control) I noticed that the
    push functionality was absent.  This adds that, along with the test
    support.
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=15580
    
    Signed-off-by: Martin Schwenke <mschwenke@ddn.com>
    Reviewed-by: Volker Lendecke <vl@samba.org>
    
    Autobuild-User(master): Volker Lendecke <vl@samba.org>
    Autobuild-Date(master): Mon Feb 19 10:21:48 UTC 2024 on atb-devel-224
    
    (cherry picked from commit dd9b11acbc4fbde1941719968aeb463b853b0ffb)
    
    Autobuild-User(v4-20-test): Jule Anger <janger@samba.org>
    Autobuild-Date(v4-20-test): Tue Feb 20 13:46:47 UTC 2024 on atb-devel-224

--- ctdb/protocol/protocol_control.c
+++ ctdb/protocol/protocol_control.c
@@ -693,6 +693,14 @@ static void ctdb_req_control_data_push(struct ctdb_req_control_data *cd,
 	case CTDB_CONTROL_ECHO_DATA:
 		ctdb_echo_data_push(cd->data.echo_data, buf, &np);
 		break;
+
+	case CTDB_CONTROL_TCP_CLIENT_DISCONNECTED:
+		ctdb_connection_push(cd->data.conn, buf, &np);
+		break;
+
+	case CTDB_CONTROL_TCP_CLIENT_PASSED:
+		ctdb_connection_push(cd->data.conn, buf, &np);
+		break;
 	}
 
 	*npush = np;
