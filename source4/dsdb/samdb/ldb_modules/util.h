e0e2126face6d0a46dff348a8c690eb943dc2930
Jo Sutton <josutton@catalyst.net.nz>
2024-02-15 16:48:29 +1300

    s4:dsdb: Add include guard to dsdb/samdb/ldb_modules/util.h
    Signed-off-by: Jo Sutton <josutton@catalyst.net.nz>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- source4/dsdb/samdb/ldb_modules/util.h
+++ source4/dsdb/samdb/ldb_modules/util.h
@@ -19,6 +19,9 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#ifndef DSDB_SAMDB_LDB_MODULES_UTIL_H
+#define DSDB_SAMDB_LDB_MODULES_UTIL_H
+
 /* predeclare some structures used by utility functions */
 struct dsdb_schema;
 struct dsdb_attribute;
@@ -41,3 +44,5 @@ struct netlogon_samlogon_response;
 #define DSDB_FLAG_TRUSTED		      0x01000000
 #define DSDB_FLAG_REPLICATED_UPDATE           0x02000000
 #define DSDB_FLAG_FORCE_ALLOW_VALIDATED_DNS_HOSTNAME_SPN_WRITE 0x04000000
+
+#endif /* DSDB_SAMDB_LDB_MODULES_UTIL_H */
