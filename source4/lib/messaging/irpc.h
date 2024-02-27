2d4aa599943a5860ebc0cc54d85012b360453bdc
Volker Lendecke <vl@samba.org>
2024-02-27 15:39:16 +0100

    lib: Fix whitespace
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Andreas Schneider <asn@samba.org>

--- source4/lib/messaging/irpc.h
+++ source4/lib/messaging/irpc.h
@@ -1,20 +1,20 @@
-/* 
+/*
    Unix SMB/CIFS implementation.
 
    Samba internal rpc code - header
 
    Copyright (C) Andrew Tridgell 2005
-   
+
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -59,7 +59,7 @@ typedef NTSTATUS (*irpc_function_t)(struct irpc_message *, void *r);
 struct ndr_interface_table;
 
 NTSTATUS irpc_register(struct imessaging_context *msg_ctx,
-		       const struct ndr_interface_table *table, 
+		       const struct ndr_interface_table *table,
 		       int call, irpc_function_t fn, void *private_data);
 
 struct dcerpc_binding_handle *irpc_binding_handle(TALLOC_CTX *mem_ctx,
@@ -84,4 +84,3 @@ void irpc_remove_name(struct imessaging_context *msg_ctx, const char *name);
 NTSTATUS irpc_send_reply(struct irpc_message *m, NTSTATUS status);
 
 #endif
-
