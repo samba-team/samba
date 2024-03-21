01940ae7afa21a6b70da5bdd5c4b8c3352c30c06
Andreas Schneider <asn@samba.org>
2024-03-21 10:31:36 +0100

    buildtools: Fix PYTHONPATH and print it
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- buildtools/devel_env.sh
+++ buildtools/devel_env.sh
@@ -3,5 +3,7 @@
 # source buildtools/devel_env.sh
 
 # Setup python path for lsp server
-PYTHONPATH="$(pwd)/third_party/waf:$(pwd)/python:$(pwd)/bin/python:$(pwd)/selftest:${PYTHONPATH}"
+echo "Old PYTHONPATH: ${PYTHONPATH}"
+PYTHONPATH="$(pwd)/third_party/waf:$(pwd)/bin/python:$(pwd)/python:$(pwd)/selftest:${PYTHONPATH}"
 export PYTHONPATH
+echo "New PYTHONPATH: ${PYTHONPATH}"
