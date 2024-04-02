0f8e25b897f84a9f62a1be7f65a7f97b4eb9d718
Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
2024-04-03 12:54:09 +1300

    s4:wins: use NUMERIC_CMP in winsdb_addr_sort_list()
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=15625
    
    Signed-off-by: Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>
    (cherry picked from commit 66d47537e42caa528c7fab670d9c35d27c513cce)

--- source4/nbt_server/wins/winsdb.c
+++ source4/nbt_server/wins/winsdb.c
@@ -32,6 +32,7 @@
 #include "lib/socket/netif.h"
 #include "param/param.h"
 #include "lib/util/smb_strtox.h"
+#include "lib/util/tsort.h"
 
 #undef strcasecmp
 
@@ -360,7 +361,7 @@ static int winsdb_addr_sort_list (struct winsdb_addr **p1, struct winsdb_addr **
 		a1_owned = true;
 	}
 
-	return a2_owned - a1_owned;
+	return NUMERIC_CMP(a2_owned, a1_owned);
 }
 
 struct winsdb_addr **winsdb_addr_list_add(struct winsdb_handle *h, const struct winsdb_record *rec,
