b2dbe57c977595714f8ae6c7d21b3c55b8e649c9
Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
2024-04-04 11:22:58 +1300

    ldb: avoid non-transitive comparison in ldb_val_cmp()
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=15625
    
    Signed-off-by: Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>
    (cherry picked from commit 5c36bc82415b246fccec9eae693da82b7aa45b81)

--- lib/ldb/common/ldb_msg.c
+++ lib/ldb/common/ldb_msg.c
@@ -93,7 +93,7 @@ struct ldb_val *ldb_msg_find_val(const struct ldb_message_element *el,
 static int ldb_val_cmp(const struct ldb_val *v1, const struct ldb_val *v2)
 {
 	if (v1->length != v2->length) {
-		return v1->length - v2->length;
+		return NUMERIC_CMP(v1->length, v2->length);
 	}
 	return memcmp(v1->data, v2->data, v1->length);
 }
