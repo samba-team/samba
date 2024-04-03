838c68470299045c5b1c9bdbd527edbeedebf2d6
Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
2024-04-03 17:43:03 +1300

    ldb:attrib_handlers: ldb_comparison_binary uses NUMERIC_CMP()
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=15625
    
    Signed-off-by: Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>

--- lib/ldb/common/attrib_handlers.c
+++ lib/ldb/common/attrib_handlers.c
@@ -300,7 +300,7 @@ int ldb_comparison_binary(struct ldb_context *ldb, void *mem_ctx,
 			  const struct ldb_val *v1, const struct ldb_val *v2)
 {
 	if (v1->length != v2->length) {
-		return v1->length - v2->length;
+		return NUMERIC_CMP(v1->length, v2->length);
 	}
 	return memcmp(v1->data, v2->data, v1->length);
 }
