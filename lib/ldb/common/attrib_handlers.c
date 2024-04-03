9de4a5d3bbc31e23c570afce98c8c0092d92fe8c
Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
2024-04-03 17:32:48 +1300

    ldb:attrib_handlers: ldb_comparison_Boolean uses NUMERIC_CMP()
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=15625
    
    Signed-off-by: Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
    Reviewed-by: Andrew Bartlett <abartlet@samba.org>
    (cherry picked from commit f78b964cd81db11097e78099c0699f571f20e126)

--- lib/ldb/common/attrib_handlers.c
+++ lib/ldb/common/attrib_handlers.c
@@ -287,7 +287,7 @@ static int ldb_comparison_Boolean(struct ldb_context *ldb, void *mem_ctx,
 			   const struct ldb_val *v1, const struct ldb_val *v2)
 {
 	if (v1->length != v2->length) {
-		return v1->length - v2->length;
+		return NUMERIC_CMP(v1->length, v2->length);
 	}
 	return strncasecmp((char *)v1->data, (char *)v2->data, v1->length);
 }
