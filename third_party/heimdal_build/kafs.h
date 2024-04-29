f3934fb9d0930b152d59a5a080f8695d069b35ca
Volker Lendecke <vl@samba.org>
2024-04-29 13:11:36 +0200

    heimdal_build: Fix whitespace
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- third_party/heimdal_build/kafs.h
+++ third_party/heimdal_build/kafs.h
@@ -25,7 +25,7 @@ krb5_error_code krb5_afslog (krb5_context context,
 			     krb5_const_realm realm);
 
 krb5_error_code krb5_afslog (krb5_context context,
-				 krb5_ccache id, 
+				 krb5_ccache id,
 				 const char *cell,
 			     krb5_const_realm realm) {
 	return 0;
