deced5eaae54cb9d8fbbd7598be1550e4946a3a0
Andreas Schneider <asn@samba.org>
2024-06-24 10:09:24 +0200

    s3:torture: Remove trailing spaces in query.c
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Noel Power <npower@samba.org>

--- source4/torture/nbt/query.c
+++ source4/torture/nbt/query.c
@@ -1,20 +1,20 @@
-/* 
+/*
    Unix SMB/CIFS implementation.
 
    NBT name query testing
 
    Copyright (C) Andrew Tridgell 2005
-   
+
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -80,7 +80,7 @@ static bool bench_namequery(struct torture_context *tctx)
 			num_sent++;
 			if (num_sent % 1000 == 0) {
 				if (torture_setting_bool(tctx, "progress", true)) {
-					torture_comment(tctx, "%.1f queries per second (%d failures)  \r", 
+					torture_comment(tctx, "%.1f queries per second (%d failures)  \r",
 					       result->num_pass / timeval_elapsed(&tv),
 					       result->num_fail);
 					fflush(stdout);
@@ -95,7 +95,7 @@ static bool bench_namequery(struct torture_context *tctx)
 		tevent_loop_once(nbtsock->event_ctx);
 	}
 
-	torture_comment(tctx, "%.1f queries per second (%d failures)  \n", 
+	torture_comment(tctx, "%.1f queries per second (%d failures)  \n",
 	       result->num_pass / timeval_elapsed(&tv),
 	       result->num_fail);
 
