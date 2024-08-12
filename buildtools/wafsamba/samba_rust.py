4a4304ecf7828ef80daa2134dcff234e32dc5caf
David Mulder <dmulder@samba.org>
2024-08-12 14:25:12 -0600

    Modify rust build to share target dir w/ cargo test
    The build needs to share the target directory
    with the cargo test command to prevent duplicate
    dependency downloads.
    
    Signed-off-by: David Mulder <dmulder@samba.org>
    Reviewed-by: Alexander Bokovoy <ab@samba.org>

--- buildtools/wafsamba/samba_rust.py
+++ buildtools/wafsamba/samba_rust.py
@@ -7,7 +7,7 @@ def SAMBA_CHECK_RUST(conf):
     conf.find_program('cargo', var='CARGO',
                       mandatory=not conf.env.disable_rust)
 
-def SAMBA_RUST(bld, name, source, enabled=True):
+def SAMBA_RUST(bld, rust_subdir, target_name, source, enabled=True):
     # force-disable when we can't build rust modules, so
     # every single call doesn't need to pass this in.
     if bld.env.disable_rust:
@@ -19,18 +19,21 @@ def SAMBA_RUST(bld, name, source, enabled=True):
 
     release_flag = ''
     if bld.env.debug or bld.env.developer:
-        target = os.path.join('debug', name)
+        target = os.path.join('debug', target_name)
     else:
         release_flag = '--release'
-        target = os.path.join('release', name)
+        target = os.path.join('release', target_name)
     target = bld.path.find_or_declare(target)
-    rust_vars = 'CARGO_TARGET_DIR=%s' % bld.path.find_or_declare('./')
+    # The Rust target directory is one directory above the located target
+    target_dir = os.path.join(os.path.dirname('%s' % target), '../')
 
-    rule = [rust_vars, '${CARGO}', 'build',
-            '--manifest-path=${SRC[0].abspath(env)}', release_flag]
-    bld.SAMBA_GENERATOR(name,
+    rule = ['${CARGO}', 'build',
+            '--manifest-path=${SRC[0].abspath(env)}',
+            '--target-dir=%s' % target_dir,
+            release_flag]
+    bld.SAMBA_GENERATOR(target_name,
                         ' '.join(rule),
-                        source='Cargo.toml %s' % source,
+                        source='%s/Cargo.toml %s' % (rust_subdir, source),
                         target=target,
                         group='final',
                         enabled=enabled)
