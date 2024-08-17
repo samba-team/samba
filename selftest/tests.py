081125f3b319ca8807f0f4d9b5f279ab4dacced5
Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
2024-08-17 14:46:05 +1200

    ldb:tests: make api_simple module
    Signed-off-by: Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
    Reviewed-by: Andreas Schneider <asn@samba.org>

--- selftest/tests.py
+++ selftest/tests.py
@@ -81,6 +81,10 @@ planpythontestsuite("none", "api_add_modify",
                     name="ldb.python.api_add_modify",
                     extra_path=['lib/ldb/tests/python'],
                     environ={'HAVE_LMDB': str(int(have_lmdb))})
+planpythontestsuite("none", "api_simple",
+                    name="ldb.python.api_simple",
+                    extra_path=['lib/ldb/tests/python'],
+                    environ={'HAVE_LMDB': str(int(have_lmdb))})
 planpythontestsuite("none", "crash",
                     name="ldb.python.crash",
                     extra_path=['lib/ldb/tests/python'],
@@ -109,6 +113,11 @@ planpythontestsuite("none", "api_search",
                              'HAVE_LMDB': str(int(have_lmdb))})
 planpythontestsuite("none", "api_add_modify",
                     name="ldb.python.api_add_modify.tr",
+                    extra_path=['lib/ldb/tests/python'],
+                    environ={'LC_ALL': 'tr_TR.UTF-8',
+                             'HAVE_LMDB': str(int(have_lmdb))})
+planpythontestsuite("none", "api_simple",
+                    name="ldb.python.api_simple.tr",
                     extra_path=['lib/ldb/tests/python'],
                     environ={'LC_ALL': 'tr_TR.UTF-8',
                              'HAVE_LMDB': str(int(have_lmdb))})
