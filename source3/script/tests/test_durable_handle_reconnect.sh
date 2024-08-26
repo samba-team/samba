153f9027c9682f8860fe0c39bdc7659089d7c4a4
Stefan Metzmacher <metze@samba.org>
2024-08-26 14:42:12 +0200

    s3:tests: let test_durable_handle_reconnect.sh run smb2.durable-v2-regressions.durable_v2_reconnect_bug15624
    This demonstrates the dead lock after a durable reconnect failed
    because the stat info changed, the file can't be accessed anymore
    as we leak the incomplete share mode entry in a still running
    process.
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=15624
    
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Guenther Deschner <gd@samba.org>
    (cherry picked from commit 14875448ca06a3a28800343a3a326f1a66bccec0)

--- source3/script/tests/test_durable_handle_reconnect.sh
+++ source3/script/tests/test_durable_handle_reconnect.sh
@@ -33,4 +33,22 @@ testit "durable_v2_delay.durable_v2_reconnect_delay_msec" $VALGRIND \
 
 rm $delay_inject_conf
 
+error_inject_conf=$(dirname $SMB_CONF_PATH)/error_inject.conf
+
+cat > $error_inject_conf << _EOF
+	kernel share modes = no
+	kernel oplocks = no
+	posix locking = no
+	error_inject:durable_reconnect = st_ex_nlink
+_EOF
+
+testit "durable-v2-regressions.durable_v2_reconnect_bug15624" \
+	$VALGRIND $BINDIR/smbtorture //$SERVER_IP/error_inject \
+	-U$USERNAME%$PASSWORD \
+	--option=torture:bug15624=yes \
+	smb2.durable-v2-regressions.durable_v2_reconnect_bug15624 ||
+	failed=$(expr $failed + 1)
+
+rm $error_inject_conf
+
 testok $0 $failed
