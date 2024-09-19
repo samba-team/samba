samba-team/samba

commit 55d98b29eb294542cc4947573f233e0d5e6966cb

smbd: check is POSIX is enabled on the fsp in fsinfo_unix_valid_level()
Signed-off-by: Ralph Boehme <slow@samba.org>
Reviewed-by: Jeremy Allison <jra@samba.org>

source3/smbd/smb2_trans2.c
@@ -1952,7 +1952,9 @@ static bool fsinfo_unix_valid_level(connection_struct *conn,
 				    uint16_t info_level)
 {
 	if (conn->sconn->using_smb2 &&
-	    info_level == SMB2_FS_POSIX_INFORMATION_INTERNAL) {
+	    fsp->posix_flags == FSP_POSIX_FLAGS_OPEN &&
+	    info_level == SMB2_FS_POSIX_INFORMATION_INTERNAL)
+	{
 		return true;
 	}
 #if defined(SMB1SERVER)

commit 7a440864dde9e9de8748b1e930bfa59f25eaf08a

smbd: SMB3 POSIX Extensions statfs() is broken
Regression introduced by 55d98b29eb294542cc4947573f233e0d5e6966cb. D'oh!

We'll add a regression test for this soon,
but for now we want to unblocking testing from the linux
client...

BUG: https://bugzilla.samba.org/show_bug.cgi?id=15639

Signed-off-by: Ralph Boehme <slow@samba.org>
Reviewed-by: Volker Lendecke <vl@samba.org>
Reviewed-by: Stefan Metzmacher <metze@samba.org>

Autobuild-User(master): Stefan Metzmacher <metze@samba.org>
Autobuild-Date(master): Thu Sep 19 21:27:07 UTC 2024 on atb-devel-224

source3/smbd/smb2_trans2.c
@@ -2002,7 +2002,7 @@ static bool fsinfo_unix_valid_level(connection_struct *conn,
 				    uint16_t info_level)
 {
 	if (conn_using_smb2(conn->sconn) &&
-	    fsp->posix_flags == FSP_POSIX_FLAGS_OPEN &&
+	    (fsp->posix_flags & FSP_POSIX_FLAGS_OPEN) &&
 	    info_level == SMB2_FS_POSIX_INFORMATION_INTERNAL)
 	{
 		return true;