1edb984810b3c0f80c7050bd2c34e49895b3dc4d
Stefan Metzmacher <metze@samba.org>
2024-10-29 09:46:07 +0100

    libcli/auth: pass auth_{type,level} to netlogon_creds_server_step_check()
    This will make it easier to implement netr_ServerAuthenticateKerberos() later...
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=15425
    
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
    (cherry picked from commit 2956c7eb3c9fc2161fd2748e5aac1fc94478e8c7)

--- libcli/auth/credentials.c
+++ libcli/auth/credentials.c
@@ -798,7 +798,9 @@ struct netlogon_creds_CredentialState *netlogon_creds_server_init(TALLOC_CTX *me
 
 NTSTATUS netlogon_creds_server_step_check(struct netlogon_creds_CredentialState *creds,
 				 const struct netr_Authenticator *received_authenticator,
-				 struct netr_Authenticator *return_authenticator)
+				 struct netr_Authenticator *return_authenticator,
+				 enum dcerpc_AuthType auth_type,
+				 enum dcerpc_AuthLevel auth_level)
 {
 	NTSTATUS status;
 
