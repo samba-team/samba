70b3699f8bdfd95b40076f631af1335206ec2797
Volker Lendecke <vl@samba.org>
2024-12-10 16:57:47 +0100

    Fix whitespace
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Jeremy Allison <jra@samba.org>

--- source3/include/trans2.h
+++ source3/include/trans2.h
@@ -1,4 +1,4 @@
-/* 
+/*
    Unix SMB/CIFS implementation.
    SMB transaction2 handling
 
@@ -11,12 +11,12 @@
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -208,7 +208,7 @@ Byte offset   Type     name                description
 #define SMB_QUERY_FS_DEVICE_INFO        0x104
 #define SMB_QUERY_FS_ATTRIBUTE_INFO     0x105
 #if 0
-#define SMB_QUERY_FS_QUOTA_INFO		
+#define SMB_QUERY_FS_QUOTA_INFO
 #endif
 
 #define l2_vol_fdateCreation 0
