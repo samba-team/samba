bbd30dc1d5def60dcac33d95af75bdc1211f1eb7
Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
2024-12-11 14:30:04 +1300

    util: add a crypt strerror helper
    This will be used by Python also.
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=15756
    
    Signed-off-by: Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
    Reviewed-by: Andreas Schneider <asn@samba.org>
    (cherry picked from commit 5f365e71c1fa8cdc533159283a5977164b5d39f2)

--- lib/util/util_crypt.h
+++ lib/util/util_crypt.h
@@ -3,3 +3,5 @@ int talloc_crypt_blob(TALLOC_CTX *mem_ctx,
 		      const char *phrase,
 		      const char *cmd,
 		      DATA_BLOB *blob);
+
+char *talloc_crypt_errstring(TALLOC_CTX *mem_ctx, int error);
