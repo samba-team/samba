26001cecf4b293fd62adc1f6007268ce0313789b
Stefan Metzmacher <metze@samba.org>
2025-01-28 10:19:09 +0100

    pidl:Python: initialize pointers and add 'result' at the end
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=14213
    
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
    (cherry picked from commit 9dfb0ed8d29bd4a9146cf38bd63c4bb55b5faf73)

--- pidl/lib/Parse/Pidl/Samba4/Python.pm
+++ pidl/lib/Parse/Pidl/Samba4/Python.pm
@@ -970,12 +970,11 @@ sub PythonFunctionUnpackOut($$$)
 	$self->pidl("static PyObject *$outfnname(struct $fn->{NAME} *r)");
 	$self->pidl("{");
 	$self->indent;
-	$self->pidl("PyObject *result;");
 	foreach my $e (@{$fn->{ELEMENTS}}) {
 		next unless (grep(/out/,@{$e->{DIRECTION}}));
 		next if (($metadata_args->{in}->{$e->{NAME}} and grep(/in/, @{$e->{DIRECTION}})) or
 		         ($metadata_args->{out}->{$e->{NAME}}) and grep(/out/, @{$e->{DIRECTION}}));
-		$self->pidl("PyObject *py_$e->{NAME};");
+		$self->pidl("PyObject *py_$e->{NAME} = NULL;");
 		$result_size++;
 	}
 
@@ -988,7 +987,8 @@ sub PythonFunctionUnpackOut($$$)
 		$result_size++;
 	}
 
-	my $i = 0;
+	$self->pidl("PyObject *result = NULL;");
+	$self->pidl("");
 
 	if ($result_size > 1) {
 		$self->pidl("result = PyTuple_New($result_size);");
@@ -998,6 +998,8 @@ sub PythonFunctionUnpackOut($$$)
 		$self->pidl("Py_INCREF(result);");
 		$signature .= "None";
 	}
+
+	my $i = 0;
 
 	foreach my $e (@{$fn->{ELEMENTS}}) {
 		next if ($metadata_args->{out}->{$e->{NAME}});
