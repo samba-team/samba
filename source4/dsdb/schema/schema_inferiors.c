865e37b4a18d670e8287b1bf913ba1e21b3dc681
Jennifer Sutton <jennifersutton@catalyst.net.nz>
2025-02-04 19:18:28 +1300

    s4:dsdb: Remove trailing whitespace
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=15852
    
    Signed-off-by: Jennifer Sutton <jennifersutton@catalyst.net.nz>
    Reviewed-by: Douglas Bagnall <douglas.bagnall@catalyst.net.nz>

--- source4/dsdb/schema/schema_inferiors.c
+++ source4/dsdb/schema/schema_inferiors.c
@@ -1,8 +1,8 @@
-/* 
+/*
    Unix SMB/CIFS implementation.
 
    implement possibleInferiors calculation
-   
+
    Copyright (C) Andrew Tridgell 2009
    Copyright (C) Andrew Bartlett <abartlet@samba.org> 2009
 
@@ -10,15 +10,15 @@
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-   
+
 */
 /*
   This module is a C implementation of the logic in the
@@ -93,7 +93,7 @@ static const char **schema_subclasses(const struct dsdb_schema *schema,
 }
 
 
-/* 
+/*
    equivalent of the POSSSUPERIORS() python function
  */
 static const char **schema_posssuperiors(const struct dsdb_schema *schema,
