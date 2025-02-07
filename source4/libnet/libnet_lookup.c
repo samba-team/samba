753c9bfdad987442b17e3877dbd3f8b384ce34e2
Björn Baumbach <bb@sernet.de>
2025-02-07 12:03:18 +0100

    libnet4: check return value of DC lookup
    Avoids possible segmentation fault when the lookup fails.
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=15798
    
    Pair-programmed-with: Volker Lendecke <vl@samba.org>
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Signed-off-by: Björn Baumbach <bb@sernet.de>
    Reviewed-by: Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
    
    Autobuild-User(master): Douglas Bagnall <dbagnall@samba.org>
    Autobuild-Date(master): Sat Feb  8 03:30:27 UTC 2025 on atb-devel-224
    
    (cherry picked from commit c5511056708fb1be3c2e1b2ad61af6643f92051c)

--- source4/libnet/libnet_lookup.c
+++ source4/libnet/libnet_lookup.c
@@ -223,6 +223,9 @@ NTSTATUS libnet_LookupDCs_recv(struct tevent_req *req, TALLOC_CTX *mem_ctx,
 	NTSTATUS status;
 	struct finddcs finddcs_io;
 	status = finddcs_cldap_recv(req, mem_ctx, &finddcs_io);
+	if (!NT_STATUS_IS_OK(status)) {
+		return status;
+	}
 	talloc_free(req);
 	io->out.num_dcs = 1;
 	io->out.dcs = talloc(mem_ctx, struct nbt_dc_name);
