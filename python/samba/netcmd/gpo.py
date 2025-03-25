samba-team/samba

commit ce56d336f234febfd4cb3da11dd584842c24ce1d

gpo.py: avoid inefficient string concatenations
Signed-off-by: Bjoern Jacke <bjacke@samba.org>
Reviewed-by: Andrew Bartlett <abartlet@samba.org>

python/samba/netcmd/gpo.py
@@ -110,9 +110,7 @@ def parse_gplink(gplink):
 
 def encode_gplink(gplist):
     '''Encode an array of dn and options into gPLink string'''
-    ret = ''
-    for g in gplist:
-        ret += "[LDAP://%s;%d]" % (g['dn'], g['options'])
+    ret = "".join("[LDAP://%s;%d]" % (g['dn'], g['options']) for g in gplist)
     return ret
 
 
@@ -1079,9 +1077,8 @@ class cmd_backup(GPOCommand):
             entities = cmd_backup.generalize_xml_entities(self.outf, gpodir,
                                                           gpodir)
             import operator
-            ents = ''
-            for ent in sorted(entities.items(), key=operator.itemgetter(1)):
-                ents += '<!ENTITY {} "{}">\n'.format(ent[1].strip('&;'), ent[0])
+            ents = "".join('<!ENTITY {} "{}\n">'.format(ent[1].strip('&;'), ent[0]) \
+                             for ent in sorted(entities.items(), key=operator.itemgetter(1)))
 
             if ent_file:
                 with open(ent_file, 'w') as f:

commit 65751f2562f98bd7fd0734dc00784e6395d76322

samba-tool gpo backup fix --generalize
This was broken with commit ce56d336f234febfd4cb3da11dd584842c24ce1d
but we didn't notice because the test was already broken.

BUG: https://bugzilla.samba.org/show_bug.cgi?id=15829

Signed-off-by: Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
Reviewed-by: Jennifer Sutton <jennifersutton@catalyst.net.nz>

python/samba/netcmd/gpo.py
@@ -1323,9 +1323,11 @@ class cmd_backup(GPOCommand):
             self.outf.write('\nAttempting to generalize XML entities:\n')
             entities = cmd_backup.generalize_xml_entities(self.outf, gpodir,
                                                           gpodir)
-            import operator
-            ents = "".join('<!ENTITY {} "{}\n">'.format(ent[1].strip('&;'), ent[0]) \
-                             for ent in sorted(entities.items(), key=operator.itemgetter(1)))
+
+            ent_list = [(v, k) for k, v in entities.items()]
+            ent_list.sort()
+            ents = "".join(f'<!ENTITY {ent.strip("&;")} "{val}">\n'
+                           for ent, val in ent_list)
 
             if ent_file:
                 with open(ent_file, 'w') as f:

selftest/knownfail.d/samba-tool-gpo
@@ -1,2 +1,0 @@
-^samba.tests.samba_tool.gpo.+GpoCmdTestCase.test_backup_restore_generalize
-