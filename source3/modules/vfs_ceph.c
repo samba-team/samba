458fd85d5aa2b3c41a0656b47d8c770c7f6c9957
Samuel Cabrero <scabrero@suse.de>
2025-05-02 12:11:01 +0200

    vfs:ceph: Allow VFS_OPEN_HOW_RESOLVE_NO_XDEV flag
    Don't return ENOSYS if the flag is set. It will be ignored,
    does not make sense in a ceph virtual filesystem.
    
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=15805
    
    Signed-off-by: Samuel Cabrero <scabrero@samba.org>
    Reviewed-by: Ralph Boehme <slow@samba.org>

--- source3/modules/vfs_ceph.c
+++ source3/modules/vfs_ceph.c
@@ -472,7 +472,9 @@ static int cephwrap_openat(struct vfs_handle_struct *handle,
 	int result = -ENOENT;
 	int dirfd = -1;
 
-	if ((how->resolve & ~VFS_OPEN_HOW_WITH_BACKUP_INTENT) != 0) {
+	if ((how->resolve & ~(VFS_OPEN_HOW_WITH_BACKUP_INTENT |
+			      VFS_OPEN_HOW_RESOLVE_NO_XDEV)) != 0)
+	{
 		errno = ENOSYS;
 		return -1;
 	}
