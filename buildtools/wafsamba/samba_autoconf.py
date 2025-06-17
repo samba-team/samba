25452830370fec2d7fe963f644c7fff60b8810ad
Gary Lockyer <gary@catalyst.net.nz>
2025-06-18 09:03:55 +1200

    Enable clang 20 cast-qual warnings
    Now that pam_wrapper has been upgraded to 1.1.8, it compiles without
    cast-qual warnings.  So lets re-enable the warnings.
    
    Signed-off-by: Gary Lockyer <gary@catalyst.net.nz>
    Reviewed-by: Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
    Reviewed-by: Anoop C S <anoopcs@samba.org>
    
    Autobuild-User(master): Anoop C S <anoopcs@samba.org>
    Autobuild-Date(master): Wed Jun 18 06:26:53 UTC 2025 on atb-devel-224

--- buildtools/wafsamba/samba_autoconf.py
+++ buildtools/wafsamba/samba_autoconf.py
@@ -843,7 +843,6 @@ int main(void) {
             conf.ADD_NAMED_CFLAGS('PICKY_CFLAGS', '-Werror -Wno-error=deprecated-declarations', testflags=True)
             conf.ADD_NAMED_CFLAGS('PICKY_CFLAGS', '-Wno-error=tautological-compare', testflags=True)
             conf.ADD_NAMED_CFLAGS('PICKY_CFLAGS', '-Wno-error=cast-align', testflags=True)
-            conf.ADD_NAMED_CFLAGS('PICKY_CFLAGS', '-Wno-error=cast-qual', testflags=True)
 
     if Options.options.fatal_errors:
         conf.ADD_CFLAGS('-Wfatal-errors', testflags=True)
