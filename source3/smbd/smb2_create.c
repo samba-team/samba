09e859756bb6fc9deee403212503fb4a567d1382
Ralph Boehme <slow@samba.org>
2025-07-10 15:51:12 +0200

    smbd: move create_action to smbXsrv_open_global0
    In preperation for making create replay disk backed.
    
    Signed-off-by: Ralph Boehme <slow@samba.org>
    Reviewed-by: Stefan Metzmacher <metze@samba.org>

--- source3/smbd/smb2_create.c
+++ source3/smbd/smb2_create.c
@@ -1082,7 +1082,7 @@ static struct tevent_req *smbd_smb2_create_send(TALLOC_CTX *mem_ctx,
 		state->result = state->op->compat;
 		state->result->op = state->op;
 		state->update_open = false;
-		state->info = state->op->create_action;
+		state->info = state->op->global->create_action;
 
 		smbd_smb2_create_after_exec(req);
 		if (!tevent_req_is_in_progress(req)) {
@@ -1708,7 +1708,7 @@ static void smbd_smb2_create_after_exec(struct tevent_req *req)
 
 	DBG_DEBUG("response construction phase\n");
 
-	state->op->create_action = state->info;
+	state->op->global->create_action = state->info;
 	state->out_create_action = state->info;
 
 	state->out_file_attributes = fdos_mode(state->result);
