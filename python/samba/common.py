a579efadaadbd12f1debb70e4383db87cf20c86a
Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
2025-07-30 11:57:02 +1200

    py:common: normalise_int32 checks bit size
    Signed-off-by: Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
    Reviewed-by: Gary Lockyer <gary@catalyst.net.nz>

--- python/samba/common.py
+++ python/samba/common.py
@@ -65,7 +65,10 @@ def confirm(msg, forced=False, allow_all=False):
 
 def normalise_int32(ivalue):
     """normalise a ldap integer to signed 32 bit"""
-    if int(ivalue) & 0x80000000 and int(ivalue) > 0:
+    ivalue = int(ivalue)
+    if ivalue > 0xffffffff or ivalue < -0x80000000:
+        raise ValueError(f"{ivalue} (0x{ivalue:x}) does not fit in 32 bits.")
+    if ivalue >= 0x80000000:
         return str(int(ivalue) - 0x100000000)
     return str(ivalue)
 
