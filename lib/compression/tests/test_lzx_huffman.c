1bceb8ff3f854e10345c7e92b37cf9f1fac8cb9e
Jennifer Sutton <jennifersutton@catalyst.net.nz>
2025-08-13 10:17:46 +1200

    lib:compression: Fix code spelling
    Signed-off-by: Jennifer Sutton <jennifersutton@catalyst.net.nz>
    Reviewed-by: Douglas Bagnall <douglas.bagnall@catalyst.net.nz>

--- lib/compression/tests/test_lzx_huffman.c
+++ lib/compression/tests/test_lzx_huffman.c
@@ -1137,7 +1137,7 @@ static void test_lzxpress_huffman_short_boring_strings(void **state)
 	size_t i;
 	/*
 	 * How do short repetitive strings work? We're poking at the limit
-	 * around which LZ77 comprssion is turned on.
+	 * around which LZ77 compression is turned on.
 	 *
 	 * For this test we don't change the blob memory between runs, just
 	 * the declared length.
