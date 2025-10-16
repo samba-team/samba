samba-team/samba

commit b6757378be238985d2b5d514219e8fc9d0ab04ee

s4:torture/smb2: let smb2.bench tests start the loop only when everything is ready
Signed-off-by: Stefan Metzmacher <metze@samba.org>
Reviewed-by: Andreas Schneider <asn@samba.org>

Autobuild-User(master): Stefan Metzmacher <metze@samba.org>
Autobuild-Date(master): Fri May 16 13:41:56 UTC 2025 on atb-devel-224

source4/torture/smb2/bench.c
@@ -442,6 +442,14 @@ static bool test_smb2_bench_echo(struct torture_context *tctx,
 			loop->conn = &state->conns[i];
 			loop->im = tevent_create_immediate(state->loops);
 			torture_assert(tctx, loop->im != NULL, __location__);
+		}
+	}
+
+	for (li = 0, i = 0; i <state->num_conns; i++) {
+		size_t pcli;
+
+		for (pcli = 0; pcli < torture_qdepth; pcli++) {
+			struct test_smb2_bench_echo_loop *loop = &state->loops[li];
 
 			tevent_schedule_immediate(loop->im,
 						  tctx->ev,
@@ -916,6 +924,14 @@ bool test_smb2_bench_path_contention_shared(struct torture_context *tctx,
 			torture_assert(tctx, loop->im != NULL, __location__);
 			loop->opens.io = open_io;
 			loop->closes.io = close_io;
+		}
+	}
+
+	for (li = 0, i = 0; i <state->num_conns; i++) {
+		size_t pcli;
+
+		for (pcli = 0; pcli < torture_qdepth; pcli++) {
+			struct test_smb2_bench_path_contention_shared_loop *loop = &state->loops[li];
 
 			tevent_schedule_immediate(loop->im,
 						  tctx->ev,
@@ -1328,6 +1344,14 @@ static bool test_smb2_bench_read(struct torture_context *tctx,
 			sfinfo.end_of_file_info.in.size = state->io_size;
 			status = smb2_setinfo_file(state->conns[i].tree, &sfinfo);
 			CHECK_STATUS(status, NT_STATUS_OK);
+		}
+	}
+
+	for (li = 0, i = 0; i <state->num_conns; i++) {
+		size_t pcli;
+
+		for (pcli = 0; pcli < torture_qdepth; pcli++) {
+			struct test_smb2_bench_read_loop *loop = &state->loops[li];
 
 			tevent_schedule_immediate(loop->im,
 						  tctx->ev,
@@ -1805,6 +1829,14 @@ static bool test_smb2_bench_session_setup(struct torture_context *tctx,
 			loop->conn = &state->conns[i];
 			loop->im = tevent_create_immediate(state->loops);
 			torture_assert(tctx, loop->im != NULL, __location__);
+		}
+	}
+
+	for (li = 0, i = 0; i <state->num_conns; i++) {
+		size_t pcli;
+
+		for (pcli = 0; pcli < torture_qdepth; pcli++) {
+			struct test_smb2_bench_session_setup_shared_loop *loop = &state->loops[li];
 
 			tevent_schedule_immediate(loop->im,
 						  tctx->ev,

commit e28dc4e98f100d73e93490a04993ca773d21cfad

smbtorture: fix regression in smb2.bench
The changes in b6757378be23 caused a regression where only the first
connection was used.

BUG: https://bugzilla.samba.org/show_bug.cgi?id=15934

Signed-off-by: Ralph Boehme <slow@samba.org>
Reviewed-by: Stefan Metzmacher <metze@samba.org>

Autobuild-User(master): Ralph Böhme <slow@samba.org>
Autobuild-Date(master): Thu Oct 16 18:42:05 UTC 2025 on atb-devel-224

source4/torture/smb2/bench.c
@@ -445,17 +445,13 @@ static bool test_smb2_bench_echo(struct torture_context *tctx,
 		}
 	}
 
-	for (li = 0, i = 0; i <state->num_conns; i++) {
-		size_t pcli;
+	for (li = 0; li <state->num_loops; li++) {
+		struct test_smb2_bench_echo_loop *loop = &state->loops[li];
 
-		for (pcli = 0; pcli < torture_qdepth; pcli++) {
-			struct test_smb2_bench_echo_loop *loop = &state->loops[li];
-
-			tevent_schedule_immediate(loop->im,
-						  tctx->ev,
-						  test_smb2_bench_echo_loop_start,
-						  loop);
-		}
+		tevent_schedule_immediate(loop->im,
+					  tctx->ev,
+					  test_smb2_bench_echo_loop_start,
+					  loop);
 	}
 
 	torture_comment(tctx, "Opened %zu connections with qdepth=%d => %zu loops\n",
@@ -927,17 +923,13 @@ bool test_smb2_bench_path_contention_shared(struct torture_context *tctx,
 		}
 	}
 
-	for (li = 0, i = 0; i <state->num_conns; i++) {
-		size_t pcli;
+	for (li = 0; li <state->num_loops; li++) {
+		struct test_smb2_bench_path_contention_shared_loop *loop = &state->loops[li];
 
-		for (pcli = 0; pcli < torture_qdepth; pcli++) {
-			struct test_smb2_bench_path_contention_shared_loop *loop = &state->loops[li];
-
-			tevent_schedule_immediate(loop->im,
-						  tctx->ev,
-						  test_smb2_bench_path_contention_loop_start,
-						  loop);
-		}
+		tevent_schedule_immediate(loop->im,
+					  tctx->ev,
+					  test_smb2_bench_path_contention_loop_start,
+					  loop);
 	}
 
 	torture_comment(tctx, "Opened %zu connections with qdepth=%d => %zu loops\n",
@@ -1347,17 +1339,13 @@ static bool test_smb2_bench_read(struct torture_context *tctx,
 		}
 	}
 
-	for (li = 0, i = 0; i <state->num_conns; i++) {
-		size_t pcli;
+	for (li = 0; li <state->num_loops; li++) {
+		struct test_smb2_bench_read_loop *loop = &state->loops[li];
 
-		for (pcli = 0; pcli < torture_qdepth; pcli++) {
-			struct test_smb2_bench_read_loop *loop = &state->loops[li];
-
-			tevent_schedule_immediate(loop->im,
-						  tctx->ev,
-						  test_smb2_bench_read_loop_start,
-						  loop);
-		}
+		tevent_schedule_immediate(loop->im,
+					  tctx->ev,
+					  test_smb2_bench_read_loop_start,
+					  loop);
 	}
 
 	torture_comment(tctx, "Opened %zu connections with qdepth=%d => %zu loops\n",
@@ -1833,17 +1821,13 @@ static bool test_smb2_bench_session_setup(struct torture_context *tctx,
 		}
 	}
 
-	for (li = 0, i = 0; i <state->num_conns; i++) {
-		size_t pcli;
+	for (li = 0; li <state->num_loops; li++) {
+		struct test_smb2_bench_session_setup_shared_loop *loop = &state->loops[li];
 
-		for (pcli = 0; pcli < torture_qdepth; pcli++) {
-			struct test_smb2_bench_session_setup_shared_loop *loop = &state->loops[li];
-
-			tevent_schedule_immediate(loop->im,
-						  tctx->ev,
-						  test_smb2_bench_session_setup_loop_start,
-						  loop);
-		}
+		tevent_schedule_immediate(loop->im,
+					  tctx->ev,
+					  test_smb2_bench_session_setup_loop_start,
+					  loop);
 	}
 
 	torture_comment(tctx, "Opened %zu connections with qdepth=%d => %zu loops\n",