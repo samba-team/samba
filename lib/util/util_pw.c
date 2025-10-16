1416ad9d66597543539ebdf57d01b4b9877aaa5e
Volker Lendecke <vl@samba.org>
2025-10-16 12:54:14 +0200

    lib: Fix whitespace
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Anoop C S <anoopcs@samba.org>

--- lib/util/util_pw.c
+++ lib/util/util_pw.c
@@ -1,4 +1,4 @@
-/* 
+/*
    Unix SMB/CIFS implementation.
 
    Safe versions of getpw* calls
@@ -9,17 +9,17 @@
    Copyright (C) Timur Bakeyev        2005
    Copyright (C) Bjoern Jacke    2006-2007
 
-   
+
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -63,7 +63,7 @@ struct passwd *getpwnam_alloc(TALLOC_CTX *mem_ctx, const char *name)
 	struct passwd *temp;
 
 	temp = getpwnam(name);
-	
+
 	if (!temp) {
 #if 0
 		if (errno == ENOMEM) {
@@ -85,7 +85,7 @@ struct passwd *getpwuid_alloc(TALLOC_CTX *mem_ctx, uid_t uid)
 	struct passwd *temp;
 
 	temp = getpwuid(uid);
-	
+
 	if (!temp) {
 #if 0
 		if (errno == ENOMEM) {
