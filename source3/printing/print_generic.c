dd8fff458e1621f5ba1a334bc7e5ff53c2fa7218
Volker Lendecke <vl@samba.org>
2025-10-17 14:12:12 +0200

    printing: Fix trailing whitespace
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Anoop C S <anoopcs@samba.org>

--- source3/printing/print_generic.c
+++ source3/printing/print_generic.c
@@ -1,18 +1,18 @@
-/* 
+/*
    Unix SMB/CIFS implementation.
    printing command routines
    Copyright (C) Andrew Tridgell 1992-2000
-   
+
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -125,7 +125,7 @@ static int generic_job_pause(int snum, struct printjob *pjob)
 	const struct loadparm_substitution *lp_sub =
 		loadparm_s3_global_substitution();
 	fstring jobstr;
-	
+
 	/* need to pause the spooled entry */
 	fstr_sprintf(jobstr, "%d", pjob->sysjob);
 	return print_run_command(snum, lp_printername(talloc_tos(), lp_sub, snum), True,
@@ -356,4 +356,3 @@ struct printif	generic_printif =
 	generic_job_resume,
 	generic_job_submit,
 };
-
