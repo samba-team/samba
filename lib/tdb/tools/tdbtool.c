samba-team/samba

commit fd056127944182bf1fa96d025a4418d9c05d1982

tdb: Use hex_byte() in parse_hex()
Signed-off-by: Volker Lendecke <vl@samba.org>
Reviewed-by: Ralph Boehme <slow@samba.org>
Reviewed-by: Jeremy Allison <jra@samba.org>

lib/tdb/tools/tdbtool.c
@@ -349,23 +349,6 @@ static void store_tdb(char *keyname, size_t keylen, char* data, size_t datalen)
 	}
 }
 
-static bool hexchar(char c, uint8_t *v)
-{
-	if ((c >= '0') && (c <= '9')) {
-		*v = (c - '0');
-		return true;
-	}
-	if ((c >= 'A') && (c <= 'F')) {
-		*v = (c - 'A' + 10);
-		return true;
-	}
-	if ((c >= 'a') && (c <= 'f')) {
-		*v = (c - 'a' + 10);
-		return true;
-	}
-	return false;
-}
-
 static bool parse_hex(const char *src, size_t srclen, uint8_t *dst)
 {
 	size_t i=0;
@@ -375,14 +358,11 @@ static bool parse_hex(const char *src, size_t srclen, uint8_t *dst)
 	}
 
 	while (i<srclen) {
-		bool ok;
-		uint8_t hi,lo;
-
-		ok = (hexchar(src[i++], &hi) && hexchar(src[i++], &lo));
+		bool ok = hex_byte(src, dst);
 		if (!ok) {
 			return false;
 		}
-		*dst = (hi<<4)|lo;
+		src += 2;
 		dst += 1;
 	}
 

commit ba7f4b1167471cea56f069972f78541b76b58657

tdb: Fix parse_hex during `tdbtool storehex`
Fixes: fd0561279

During `tdbtool storehex`, tdbtool check whether the input
string in hex format.

However, during the check, the index is never moved forward,
resulting in checking beyond the valid input string.

This patch fix the issue by checking the valid string

Signed-off-by: Lin Liu <lin.liu01@citrix.com>
Reviewed-by: Jennifer Sutton <jennifersutton@catalyst.net.nz>
Reviewed-by: Andreas Schneider <asn@samba.org>

Autobuild-User(master): Jennifer Sutton <jsutton@samba.org>
Autobuild-Date(master): Thu Nov 20 22:29:03 UTC 2025 on atb-devel-224

lib/tdb/tools/tdbtool.c
@@ -363,13 +363,13 @@ static int store_tdb(char *keyname, size_t keylen, char* data, size_t datalen)
 
 static bool parse_hex(const char *src, size_t srclen, uint8_t *dst)
 {
-	size_t i=0;
+	const char *end = src + srclen;
 
 	if ((srclen % 2) != 0) {
 		return false;
 	}
 
-	while (i<srclen) {
+	while (src < end) {
 		bool ok = hex_byte(src, dst);
 		if (!ok) {
 			return false;