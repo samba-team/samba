2f3b17b35c1d18a007587b616a5833df3c2c1c26
Volker Lendecke <vl@samba.org>
2025-11-22 10:08:12 +0100

    smbd: Reduce indentation with an early return;
    Review with "git show -w"
    
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Anoop C S <anoopcs@samba.org>

--- source3/smbd/statvfs.c
+++ source3/smbd/statvfs.c
@@ -82,79 +82,83 @@ static int bsd_statvfs(const char *path, struct vfs_statvfs_struct *statbuf)
 	int ret;
 
 	ret = statfs(path, &sbuf);
-	if (ret == 0) {
-		statbuf->OptimalTransferSize = sbuf.f_iosize;
-		statbuf->BlockSize = sbuf.f_bsize;
-		statbuf->TotalBlocks = sbuf.f_blocks;
-		statbuf->BlocksAvail = sbuf.f_bfree;
-		statbuf->UserBlocksAvail = sbuf.f_bavail;
-		statbuf->TotalFileNodes = sbuf.f_files;
-		statbuf->FreeFileNodes = sbuf.f_ffree;
-		statbuf->FsIdentifier =
-			(((uint64_t) sbuf.f_fsid.val[0] << 32) & 0xffffffff00000000LL) |
-			    (uint64_t) sbuf.f_fsid.val[1];
-#ifdef DARWINOS
-		statbuf->FsCapabilities = darwin_fs_capabilities(sbuf.f_mntonname);
-#else
-		/* Try to extrapolate some of the fs flags into the
-		 * capabilities
-		 */
-		statbuf->FsCapabilities =
-		    FILE_CASE_SENSITIVE_SEARCH | FILE_CASE_PRESERVED_NAMES;
-#ifdef MNT_ACLS
-		if (sbuf.f_flags & MNT_ACLS)
-			statbuf->FsCapabilities |= FILE_PERSISTENT_ACLS;
-#endif
-#endif
-		if (sbuf.f_flags & MNT_QUOTA)
-			statbuf->FsCapabilities |= FILE_VOLUME_QUOTAS;
-		if (sbuf.f_flags & MNT_RDONLY)
-			statbuf->FsCapabilities |= FILE_READ_ONLY_VOLUME;
+	if (ret != 0) {
+		return ret;
 	}
 
-	return ret;
+	statbuf->OptimalTransferSize = sbuf.f_iosize;
+	statbuf->BlockSize = sbuf.f_bsize;
+	statbuf->TotalBlocks = sbuf.f_blocks;
+	statbuf->BlocksAvail = sbuf.f_bfree;
+	statbuf->UserBlocksAvail = sbuf.f_bavail;
+	statbuf->TotalFileNodes = sbuf.f_files;
+	statbuf->FreeFileNodes = sbuf.f_ffree;
+	statbuf->FsIdentifier =
+		(((uint64_t) sbuf.f_fsid.val[0] << 32) & 0xffffffff00000000LL) |
+		(uint64_t) sbuf.f_fsid.val[1];
+#ifdef DARWINOS
+	statbuf->FsCapabilities = darwin_fs_capabilities(sbuf.f_mntonname);
+#else
+	/* Try to extrapolate some of the fs flags into the
+	 * capabilities
+	 */
+	statbuf->FsCapabilities =
+		FILE_CASE_SENSITIVE_SEARCH | FILE_CASE_PRESERVED_NAMES;
+#ifdef MNT_ACLS
+	if (sbuf.f_flags & MNT_ACLS)
+		statbuf->FsCapabilities |= FILE_PERSISTENT_ACLS;
+#endif
+#endif
+	if (sbuf.f_flags & MNT_QUOTA)
+		statbuf->FsCapabilities |= FILE_VOLUME_QUOTAS;
+	if (sbuf.f_flags & MNT_RDONLY)
+		statbuf->FsCapabilities |= FILE_READ_ONLY_VOLUME;
+
+	return 0;
 }
 #elif defined(STAT_STATVFS) && defined(HAVE_FSID_INT)
 static int posix_statvfs(const char *path, struct vfs_statvfs_struct *statbuf)
 {
 	struct statvfs statvfs_buf;
-	int result;
+	int ret;
 
-	result = statvfs(path, &statvfs_buf);
+	ret = statvfs(path, &statvfs_buf);
 
-	if (!result) {
-		/* statvfs bsize is not the statfs bsize, the naming is terrible,
-		 * see bug 11810 */
-		statbuf->OptimalTransferSize = statvfs_buf.f_bsize;
-		statbuf->BlockSize = statvfs_buf.f_frsize;
-		statbuf->TotalBlocks = statvfs_buf.f_blocks;
-		statbuf->BlocksAvail = statvfs_buf.f_bfree;
-		statbuf->UserBlocksAvail = statvfs_buf.f_bavail;
-		statbuf->TotalFileNodes = statvfs_buf.f_files;
-		statbuf->FreeFileNodes = statvfs_buf.f_ffree;
-		statbuf->FsIdentifier = statvfs_buf.f_fsid;
-		/* Try to extrapolate some of the fs flags into the
-		 * capabilities
-		 */
-		statbuf->FsCapabilities =
-		    FILE_CASE_SENSITIVE_SEARCH | FILE_CASE_PRESERVED_NAMES;
+	if (ret != 0) {
+		return ret;
+	}
+
+	/* statvfs bsize is not the statfs bsize, the naming is terrible,
+	 * see bug 11810 */
+	statbuf->OptimalTransferSize = statvfs_buf.f_bsize;
+	statbuf->BlockSize = statvfs_buf.f_frsize;
+	statbuf->TotalBlocks = statvfs_buf.f_blocks;
+	statbuf->BlocksAvail = statvfs_buf.f_bfree;
+	statbuf->UserBlocksAvail = statvfs_buf.f_bavail;
+	statbuf->TotalFileNodes = statvfs_buf.f_files;
+	statbuf->FreeFileNodes = statvfs_buf.f_ffree;
+	statbuf->FsIdentifier = statvfs_buf.f_fsid;
+	/* Try to extrapolate some of the fs flags into the
+	 * capabilities
+	 */
+	statbuf->FsCapabilities =
+		FILE_CASE_SENSITIVE_SEARCH | FILE_CASE_PRESERVED_NAMES;
 #ifdef ST_QUOTA
-		if (statvfs_buf.f_flag & ST_QUOTA)
-			statbuf->FsCapabilities |= FILE_VOLUME_QUOTAS;
+	if (statvfs_buf.f_flag & ST_QUOTA)
+		statbuf->FsCapabilities |= FILE_VOLUME_QUOTAS;
 #endif
-		if (statvfs_buf.f_flag & ST_RDONLY)
-			statbuf->FsCapabilities |= FILE_READ_ONLY_VOLUME;
+	if (statvfs_buf.f_flag & ST_RDONLY)
+		statbuf->FsCapabilities |= FILE_READ_ONLY_VOLUME;
 
 #if defined(HAVE_FALLOC_FL_PUNCH_HOLE) && defined(HAVE_LSEEK_HOLE_DATA)
-		/*
-		 * Only flag sparse file support if ZERO_DATA can be used to
-		 * deallocate blocks, and SEEK_HOLE / SEEK_DATA can be used
-		 * to provide QUERY_ALLOCATED_RANGES information.
-		 */
-		statbuf->FsCapabilities |= FILE_SUPPORTS_SPARSE_FILES;
+	/*
+	 * Only flag sparse file support if ZERO_DATA can be used to
+	 * deallocate blocks, and SEEK_HOLE / SEEK_DATA can be used
+	 * to provide QUERY_ALLOCATED_RANGES information.
+	 */
+	statbuf->FsCapabilities |= FILE_SUPPORTS_SPARSE_FILES;
 #endif
-	}
-	return result;
+	return 0;
 }
 #endif
 
