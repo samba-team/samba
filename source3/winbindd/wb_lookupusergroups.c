a0589bc5994a133d2a52abd5a7dd613935df17a6
Günther Deschner <gd@samba.org>
2025-11-28 22:51:06 +0100

    s3-winbindd: attempt S4U2SELF impersonation for usergroup lookup
    As there are limitations in the krb5 libraries,
    e.g. blocking without timeouts or only support
    for the primary realm, this is off by default.
    
    Pair-Programmed-With: Stefan Metzmacher <metze@samba.org>
    
    Signed-off-by: Guenther Deschner <gd@samba.org>
    Signed-off-by: Stefan Metzmacher <metze@samba.org>

--- source3/winbindd/wb_lookupusergroups.c
+++ source3/winbindd/wb_lookupusergroups.c
@@ -21,14 +21,18 @@
 #include "winbindd.h"
 #include "librpc/gen_ndr/ndr_winbind_c.h"
 #include "../libcli/security/security.h"
+#include "rpc_client/util_netlogon.h"
 
 struct wb_lookupusergroups_state {
 	struct tevent_context *ev;
 	struct dom_sid sid;
 	struct wbint_SidArray sids;
+	struct wbint_KerberosImpersonationTokenValidation validation;
+	struct winbindd_domain *domain;
 };
 
 static void wb_lookupusergroups_done(struct tevent_req *subreq);
+static void wb_lookupusergroups_impersonated_done(struct tevent_req *subreq);
 
 struct tevent_req *wb_lookupusergroups_send(TALLOC_CTX *mem_ctx,
 					    struct tevent_context *ev,
@@ -39,8 +43,11 @@ struct tevent_req *wb_lookupusergroups_send(TALLOC_CTX *mem_ctx,
 	struct tevent_req *req, *subreq;
 	struct wb_lookupusergroups_state *state;
 	struct winbindd_domain *domain;
+	struct winbindd_domain *our_domain = NULL;
 	NTSTATUS status;
 	struct dom_sid_buf buf;
+	int global_winbind_use_s4u2self = false;
+	bool attempt_s4u2self = false;
 
 	req = tevent_req_create(mem_ctx, &state,
 				struct wb_lookupusergroups_state);
@@ -88,6 +95,144 @@ struct tevent_req *wb_lookupusergroups_send(TALLOC_CTX *mem_ctx,
 		return tevent_req_post(req, ev);
 	}
 
+	state->ev = ev;
+	state->domain = domain;
+
+	/*
+	 * allow overwrite per domain
+	 * winbind use s4u2self:<netbios_domain>
+	 */
+	global_winbind_use_s4u2self = lp_winbind_use_s4u2self();
+	attempt_s4u2self = global_winbind_use_s4u2self;
+	if (!domain->primary) {
+		/*
+		 * As winbind use s4u2self = yes, is
+		 * still experimental, it should only
+		 * apply to the primary domain.
+		 *
+		 * winbind use s4u2self:NBTDOMAIN = yes,
+		 * can be used to activate it for other
+		 * domains as well, but depending on
+		 * the krb5 library it can only work
+		 * for the primary domain.
+		 * E.g. heimdal only supports the primary domain.
+		 */
+		attempt_s4u2self = false;
+	}
+	attempt_s4u2self = lp_parm_bool(-1,
+					"winbind use s4u2self",
+					domain->name,
+					attempt_s4u2self);
+
+	our_domain = find_our_domain();
+	SMB_ASSERT(our_domain != NULL);
+
+	/*
+	 * Only if our domain is active directory
+	 * we can try s4u2self, as we contact the
+	 * kdc of our domain first.
+	 */
+	if (!our_domain->active_directory) {
+		attempt_s4u2self = false;
+	}
+
+	/*
+	 * For now we only try s4u2self on
+	 * members of AD domains, but not AD DCs yet.
+	 */
+	if (IS_AD_DC) {
+		attempt_s4u2self = false;
+	}
+
+	/*
+	 * If the krb5 library doesn't support
+	 * s4u2self, we can skip the attempt to
+	 * use it here, instead of having the
+	 * child rejecting it
+	 */
+	if (!winbind_s4u2self_krb5_api_support()) {
+		attempt_s4u2self = false;
+	}
+
+	if (attempt_s4u2self) {
+		const char *impersonate_principal = NULL;
+		const char *our_realm = lp_realm();
+
+		if (strequal(domain->alt_name, our_realm)) {
+			/*
+			 * For our own domain we don't
+			 * need an enterprise principal.
+			 *
+			 * That also avoids strange AS-REQ
+			 * done by the MIT library in order
+			 * to find a realm for NBTDOMAIN as
+			 * user realm.
+			 */
+			impersonate_principal = talloc_asprintf(
+				state,
+				"%s@%s",
+				acct_name,
+				our_realm);
+			if (tevent_req_nomem(impersonate_principal, req)) {
+				return tevent_req_post(req, ev);
+			}
+		} else {
+			const char *u_realm = NULL;
+
+			/*
+			 * If we have enough
+			 * information about the
+			 * users domain, we can use
+			 * the realm directly.
+			 *
+			 * Otherwise we use the
+			 * netbios domain name
+			 * and let our KDC
+			 * canonicalize it while
+			 * returning referrals
+			 */
+			if (domain->alt_name != NULL) {
+				u_realm = talloc_strdup_upper(state,
+							      domain->alt_name);
+				if (tevent_req_nomem(u_realm, req)) {
+					return tevent_req_post(req, ev);
+				}
+			} else {
+				/*
+				 * This should only be upper
+				 */
+				u_realm = domain->name;
+			}
+
+			impersonate_principal = talloc_asprintf(
+				state,
+				"%s@%s@%s",
+				acct_name,
+				u_realm,
+				our_realm);
+			if (tevent_req_nomem(impersonate_principal, req)) {
+				return tevent_req_post(req, ev);
+			}
+		}
+
+		D_INFO("WB command lookupusergroups impersonate %s\n",
+		       impersonate_principal);
+
+		subreq = dcerpc_wbint_KerberosImpersonationToken_send(
+			state,
+			ev,
+			dom_child_handle(our_domain),
+			impersonate_principal,
+			&state->validation);
+		if (tevent_req_nomem(subreq, req)) {
+			return tevent_req_post(req, ev);
+		}
+		tevent_req_set_callback(subreq,
+					wb_lookupusergroups_impersonated_done,
+					req);
+		return req;
+	}
+
 	subreq = dcerpc_wbint_LookupUserGroups_send(
 		state, ev, dom_child_handle(domain), &state->sid, &state->sids);
 	if (tevent_req_nomem(subreq, req)) {
@@ -112,6 +257,103 @@ static void wb_lookupusergroups_done(struct tevent_req *subreq)
 		tevent_req_nterror(req, status);
 		return;
 	}
+	tevent_req_done(req);
+}
+
+static void wb_lookupusergroups_impersonated_done(struct tevent_req *subreq)
+{
+	struct tevent_req *req = tevent_req_callback_data(subreq,
+							  struct tevent_req);
+	struct wb_lookupusergroups_state *state = tevent_req_data(
+		req, struct wb_lookupusergroups_state);
+	NTSTATUS status, result;
+	bool ok;
+	struct netr_SamInfo3 *info3;
+	bool retry_lookupusergroups = false;
+
+	status = dcerpc_wbint_KerberosImpersonationToken_recv(subreq,
+							      state,
+							      &result);
+	TALLOC_FREE(subreq);
+
+	if (any_nt_status_not_ok(status, result, &status)) {
+		D_WARNING("Failed S4U2SELF impersonation with %s.\n",
+			  nt_errstr(status));
+	}
+
+	if (NT_STATUS_EQUAL(status, NT_STATUS_NO_S4U_PROT_SUPPORT)) {
+		/* no s4u2self krb5 api support */
+		retry_lookupusergroups = true;
+	}
+
+	if (NT_STATUS_EQUAL(status, NT_STATUS_LOGON_FAILURE) ||
+	    NT_STATUS_EQUAL(status, NT_STATUS_TIME_DIFFERENCE_AT_DC))
+	{
+		/* clock skew or disabled account in AD */
+		retry_lookupusergroups = true;
+	}
+
+	if (NT_STATUS_EQUAL(status, NT_STATUS_ACCOUNT_LOCKED_OUT)) {
+		/* disabled account when not using enterprise principals */
+		retry_lookupusergroups = true;
+	}
+
+	if (retry_lookupusergroups) {
+		subreq = dcerpc_wbint_LookupUserGroups_send(
+			state,
+			state->ev,
+			dom_child_handle(state->domain),
+			&state->sid,
+			&state->sids);
+		if (tevent_req_nomem(subreq, req)) {
+			return;
+		}
+		tevent_req_set_callback(subreq, wb_lookupusergroups_done, req);
+		return;
+	}
+
+	if (any_nt_status_not_ok(status, result, &status)) {
+		tevent_req_nterror(req, status);
+		return;
+	}
+
+	/*
+	 * TODO: replace the next two calls with a new
+	 * sid_array_from_validation() call - gd
+	 */
+
+	status = map_validation_to_info3(state,
+					 state->validation.level,
+					 state->validation.validation,
+					 &info3);
+	if (!NT_STATUS_IS_OK(status)) {
+		tevent_req_nterror(req, status);
+		return;
+	}
+
+	status = sid_array_from_info3(
+		state, info3, &state->sids.sids, &state->sids.num_sids, true);
+	if (!NT_STATUS_IS_OK(status)) {
+		tevent_req_nterror(req, status);
+		return;
+	}
+
+	/*
+	 * Cross-check state->sid with token primary sid to make sure we don't
+	 * process incorrect information from a PAC.
+	 */
+
+	ok = dom_sid_equal(&state->sid, &state->sids.sids[0]);
+	if (!ok) {
+		struct dom_sid_buf buf;
+		D_WARNING("SID mismatch after S4U2SELF impersonation "
+			  "(requested: %s, returned: %s)\n",
+			  dom_sid_str_buf(&state->sid, &buf),
+			  dom_sid_str_buf(&state->sids.sids[0], &buf));
+		tevent_req_nterror(req, NT_STATUS_SERVER_SID_MISMATCH);
+		return;
+	}
+
 	tevent_req_done(req);
 }
 
