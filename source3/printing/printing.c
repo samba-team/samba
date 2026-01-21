samba-team/samba

commit e9a7dce599eb12d7749783c1809c33313fc5d81e

printing: Define and use methods to fetch/store time in share cache
Signed-off-by: Vinit Agnihotri <vagnihot@redhat.com>
Reviewed-by: Volker Lendecke <vl@samba.org>
Reviewed-by: Anoop C S <anoopcs@samba.org>

source3/printing/printing.c
@@ -43,12 +43,61 @@
 #include "source3/printing/rap_jobid.h"
 #include "source3/lib/substitute.h"
 
+#define CACHE_LAST_SCAN_TIME "CACHE"
+#define MSG_PENDING_TIME "MSG_PENDING"
+
 extern userdom_struct current_user_info;
 
 /* Current printer interface */
 static bool remove_from_jobs_added(const char* sharename, uint32_t jobid);
 
 static int get_queue_status(const char* sharename, print_status_struct *);
+
+static int fetch_share_cache_time(const char *key_name,
+				  const char *sharename,
+				  struct tdb_context *tdb,
+				  time_t *curr_time)
+{
+	char *key = NULL;
+
+	key = talloc_asprintf(NULL, "%s/%s", key_name, sharename);
+	if (key == NULL) {
+		DBG_ERR("Failed to format key\n");
+		return -1;
+	}
+
+	if (tdb_fetch_int64(tdb, key, curr_time) != 0) {
+		DBG_ERR("No timing record found for[%s]!\n", sharename);
+		TALLOC_FREE(key);
+		return -1;
+	}
+
+	TALLOC_FREE(key);
+	return 0;
+}
+
+static int update_share_cache_time(const char *key_name,
+				   const char *sharename,
+				   struct tdb_context *tdb,
+				   time_t curr_time)
+{
+	char *key = NULL;
+
+	key = talloc_asprintf(NULL, "%s/%s", key_name, sharename);
+	if (key == NULL) {
+		DBG_ERR("Failed to format key.\n");
+		return -1;
+	}
+
+	if (tdb_store_int64(tdb, key, (int64_t)curr_time) != 0) {
+		DBG_ERR("Unable to update print cache for %s\n", sharename);
+		TALLOC_FREE(key);
+		return -1;
+	}
+
+	TALLOC_FREE(key);
+	return 0;
+}
 
 /****************************************************************************
  Initialise the printing backend. Called once at startup before the fork().
@@ -1001,13 +1050,12 @@ static int traverse_fn_delete(TDB_CONTEXT *t, TDB_DATA key, TDB_DATA data, void 
 
 static void print_cache_flush(const char *sharename)
 {
-	fstring key;
 	struct tdb_print_db *pdb = get_print_db_byname(sharename);
 
 	if (!pdb)
 		return;
-	slprintf(key, sizeof(key)-1, "CACHE/%s", sharename);
-	tdb_store_int32(pdb->tdb, key, -1);
+
+	update_share_cache_time(CACHE_LAST_SCAN_TIME, sharename, pdb->tdb, -1);
 	release_print_db(pdb);
 }
 
@@ -1211,8 +1259,11 @@ static bool print_cache_expired(const char *sharename, bool check_pending)
 	if (!pdb)
 		return False;
 
-	snprintf(key, sizeof(key), "CACHE/%s", sharename);
-	last_qscan_time = (time_t)tdb_fetch_int32(pdb->tdb, key);
+	if (fetch_share_cache_time(CACHE_LAST_SCAN_TIME, sharename,
+				   pdb->tdb, &last_qscan_time) != 0) {
+		DBG_ERR("Unable to get last scan timing for %s\n", sharename);
+		goto done;
+	}
 
 	/*
 	 * Invalidate the queue for 3 reasons.

commit e2825363b3accd01e2f5db033c8a0de0e7f532f9

printing: Fix compilation error for native 32-bit time_t
commit#e9a7dce599eb12d broke samba compilation for 32-bit time_t.

BUG: https://bugzilla.samba.org/show_bug.cgi?id=15976
Used correct pointer type to fix the warning to fix compialtion.

Pair-Programmed-With: Vinit Agnihotri <vagnihot@redhat.com>
Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>
Signed-off-by: Vinit Agnihotri <vagnihot@redhat.com>
Reviewed-by: Günther Deschner <gd@samba.org>
Reviewed-by: Volker Lendecke <vl@samba.org>

Autobuild-User(master): Volker Lendecke <vl@samba.org>
Autobuild-Date(master): Wed Jan 21 19:23:29 UTC 2026 on atb-devel-224

source3/printing/printing.c
@@ -59,6 +59,7 @@ static int fetch_share_cache_time(const char *key_name,
 				  time_t *curr_time)
 {
 	char *key = NULL;
+	int64_t curr_time64 = -1;
 
 	key = talloc_asprintf(NULL, "%s/%s", key_name, sharename);
 	if (key == NULL) {
@@ -66,11 +67,12 @@ static int fetch_share_cache_time(const char *key_name,
 		return -1;
 	}
 
-	if (tdb_fetch_int64(tdb, key, curr_time) != 0) {
+	if (tdb_fetch_int64(tdb, key, &curr_time64) != 0) {
 		DBG_ERR("No timing record found for[%s]!\n", sharename);
 		TALLOC_FREE(key);
 		return -1;
 	}
+	*curr_time = curr_time64;
 
 	TALLOC_FREE(key);
 	return 0;
@@ -82,6 +84,7 @@ static int update_share_cache_time(const char *key_name,
 				   time_t curr_time)
 {
 	char *key = NULL;
+	int64_t curr_time64 = curr_time;
 
 	key = talloc_asprintf(NULL, "%s/%s", key_name, sharename);
 	if (key == NULL) {
@@ -89,7 +92,7 @@ static int update_share_cache_time(const char *key_name,
 		return -1;
 	}
 
-	if (tdb_store_int64(tdb, key, (int64_t)curr_time) != 0) {
+	if (tdb_store_int64(tdb, key, curr_time64) != 0) {
 		DBG_ERR("Unable to update print cache for %s\n", sharename);
 		TALLOC_FREE(key);
 		return -1;