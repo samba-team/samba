5e5426528ab1f3269f2b680e09441b4bdf2c242a
John Mulligan <jmulligan@redhat.com>
2026-02-12 17:44:59 -0500

    ctdb-tools: add some tests cases for ctdb status --json
    Signed-off-by: John Mulligan <jmulligan@redhat.com>
    Reviewed-by: Martin Schwenke <martin@meltin.net>
    Reviewed-by: Anoop C S <anoopcs@samba.org>

--- ctdb/tests/UNIT/tool/ctdb.status.001.sh
+++ ctdb/tests/UNIT/tool/ctdb.status.001.sh
@@ -44,3 +44,90 @@ required_result 0 <<EOF
 |2|192.168.20.43|0|0|0|0|0|0|0|0|N|
 EOF
 simple_test -X
+
+required_result 0 <<EOF
+{
+  "node_status": {
+    "node_count": 3,
+    "deleted_node_count": 0,
+    "nodes": {
+      "0": {
+        "pnn": 0,
+        "address": "192.168.20.41",
+        "partially_online": false,
+        "flags_raw": 0,
+        "flags_ok": true,
+        "flags": {
+          "disconnected": false,
+          "unknown": false,
+          "disabled": false,
+          "banned": false,
+          "unhealthy": false,
+          "deleted": false,
+          "stopped": false,
+          "inactive": false
+        },
+        "this_node": true
+      },
+      "1": {
+        "pnn": 1,
+        "address": "192.168.20.42",
+        "partially_online": false,
+        "flags_raw": 0,
+        "flags_ok": true,
+        "flags": {
+          "disconnected": false,
+          "unknown": false,
+          "disabled": false,
+          "banned": false,
+          "unhealthy": false,
+          "deleted": false,
+          "stopped": false,
+          "inactive": false
+        },
+        "this_node": false
+      },
+      "2": {
+        "pnn": 2,
+        "address": "192.168.20.43",
+        "partially_online": false,
+        "flags_raw": 0,
+        "flags_ok": true,
+        "flags": {
+          "disconnected": false,
+          "unknown": false,
+          "disabled": false,
+          "banned": false,
+          "unhealthy": false,
+          "deleted": false,
+          "stopped": false,
+          "inactive": false
+        },
+        "this_node": false
+      }
+    }
+  },
+  "vnn_status": {
+    "generation": 654321,
+    "size": 3,
+    "vnn_map": [
+      {
+        "hash": 0,
+        "lmaster": 0
+      },
+      {
+        "hash": 1,
+        "lmaster": 1
+      },
+      {
+        "hash": 2,
+        "lmaster": 2
+      }
+    ]
+  },
+  "recovery_mode": "NORMAL",
+  "recovery_mode_raw": 0,
+  "leader": 0
+}
+EOF
+simple_json_test
