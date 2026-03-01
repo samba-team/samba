9d244ba7c0d40875566e43f5969683da82cd2ddf
Martin Schwenke <mschwenke@ddn.com>
2026-03-01 15:18:00 +1100

    ctdb-scripts: Put output under the control of failure counting
    The port checking output should be under the control of the failure
    counting.  It shouldn't be printed every time.
    
    Duplicate most of the associated test to ensure that the counter is
    correctly reset.
    
    Signed-off-by: Martin Schwenke <mschwenke@ddn.com>
    Reviewed-by: Amitay Isaacs <amitay@gmail.com>

--- ctdb/tests/UNIT/eventscripts/40.vsftpd.monitor.002.sh
+++ ctdb/tests/UNIT/eventscripts/40.vsftpd.monitor.002.sh
@@ -12,8 +12,8 @@ simple_test
 setup "down"
 
 ok <<EOF
-vsftpd not listening on TCP port 21
 WARNING: vsftpd listening on TCP port 21: fail count 1 >= threshold 1
+vsftpd not listening on TCP port 21
 EOF
 simple_test
 
@@ -27,19 +27,44 @@ simple_test
 setup "down"
 
 ok <<EOF
-vsftpd not listening on TCP port 21
 WARNING: vsftpd listening on TCP port 21: fail count 1 >= threshold 1
+vsftpd not listening on TCP port 21
 EOF
 simple_test
 
 required_result 1 <<EOF
-vsftpd not listening on TCP port 21
 ERROR: vsftpd listening on TCP port 21: fail count 2 >= threshold 2
+vsftpd not listening on TCP port 21
 EOF
 simple_test
 
 required_result 1 <<EOF
+ERROR: vsftpd listening on TCP port 21: fail count 3 >= threshold 2
+EOF
+simple_test
+
+setup "up"
+
+ok <<EOF
+NOTICE: vsftpd listening on TCP port 21: no longer failing
+EOF
+simple_test
+
+setup "down"
+
+ok <<EOF
+WARNING: vsftpd listening on TCP port 21: fail count 1 >= threshold 1
 vsftpd not listening on TCP port 21
+EOF
+simple_test
+
+required_result 1 <<EOF
+ERROR: vsftpd listening on TCP port 21: fail count 2 >= threshold 2
+vsftpd not listening on TCP port 21
+EOF
+simple_test
+
+required_result 1 <<EOF
 ERROR: vsftpd listening on TCP port 21: fail count 3 >= threshold 2
 EOF
 simple_test
