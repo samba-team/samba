65a9ac413b03eefc7a48d5536e54177319ca30e3
Stefan Metzmacher <metze@samba.org>
2026-05-08 23:27:35 +0200

    CVE-2026-4408: s3:testparm: warn about 'check password script' %u usage
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=16034
    
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Reviewed-by: Douglas Bagnall <douglas.bagnall@catalyst.net.nz>

--- source3/utils/testparm.c
+++ source3/utils/testparm.c
@@ -359,6 +359,7 @@ static int do_global_checks(void)
 	const char **lp_ptr = NULL;
 	const struct loadparm_substitution *lp_sub =
 		loadparm_s3_global_substitution();
+	const char *check_pw_script = NULL;
 	int ival;
 
 	fprintf(stderr, "\n");
@@ -819,6 +820,17 @@ static int do_global_checks(void)
 		while (*lp_ptr) {
 			ret |= pw2kt_check_line(*lp_ptr++);
 		}
+	}
+
+	check_pw_script = lp_check_password_script(talloc_tos(), lp_sub);
+	if (talloc_string_sub_mixed_quoting(check_pw_script, 'u')) {
+		fprintf(stderr,
+			"WARNING: You are using 'check password script' "
+			"with mixed quoting and %%u.\n"
+			"CVE-2026-4408 changed the way %%u substitution works. \n"
+			"You should use the SAMBA_CPS_ACCOUNT_NAME "
+			"environment variable exported to the script, or\n"
+			"at least use single quotes (directly) around '%%u'.\n\n");
 	}
 
 	return ret;
