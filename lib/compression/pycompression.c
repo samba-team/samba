4d64a48b59d8117cde2eb568ce2bb74cf707d4ce
Andreas Schneider <asn@samba.org>
2026-05-20 18:29:12 +0200

    lib:compression: Fix _POSIX_C_SOURCE/_XOPEN_SOURCE redefinition issue
    BUG: https://bugzilla.samba.org/show_bug.cgi?id=16006
    
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Stefan Metzmacher <metze@samba.org>
    (cherry picked from commit 39d53421d3b739f0c9911c18ad7c1251e84dd514)

--- lib/compression/pycompression.c
+++ lib/compression/pycompression.c
@@ -24,9 +24,9 @@
    License along with this library; if not, see <http://www.gnu.org/licenses/>.
 */
 
+#include "lib/replace/system/python.h"
 #include "includes.h"
 #include <talloc.h>
-#include "lib/replace/system/python.h"
 #include "lzxpress.h"
 #include "lzxpress_huffman.h"
 
