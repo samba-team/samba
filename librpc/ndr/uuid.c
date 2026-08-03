4c7f6f93d2c16cd5cad5cf0c82c00ffd8b5ffeb1
Volker Lendecke <vl@samba.org>
2026-08-03 11:49:50 +0200

    lib: Fix whitespace
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Anoop C S <anoopcs@samba.org>

--- librpc/ndr/uuid.c
+++ librpc/ndr/uuid.c
@@ -1,4 +1,4 @@
-/* 
+/*
    Unix SMB/CIFS implementation.
 
    UUID/GUID functions
@@ -6,17 +6,17 @@
    Copyright (C) Theodore Ts'o               1996, 1997,
    Copyright (C) Jim McDonough                     2002.
    Copyright (C) Andrew Tridgell                   2003.
-   
+
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -136,7 +136,7 @@ _PUBLIC_ struct GUID GUID_random(void)
 }
 
 /**
- * generate an empty GUID 
+ * generate an empty GUID
  */
 _PUBLIC_ struct GUID GUID_zero(void)
 {
