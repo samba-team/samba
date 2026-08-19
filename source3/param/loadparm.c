c514c8f0cda5486376cd891d8812824495478613
Günther Deschner <gd@samba.org>
2026-08-19 17:54:31 +0200

    docs-xml: add new "winbind use s4u2self" boolean option.
    Pair-Programmed-With: Stefan Metzmacher <metze@samba.org>
    
    Signed-off-by: Guenther Deschner <gd@samba.org>
    Signed-off-by: Stefan Metzmacher <metze@samba.org>

--- source3/param/loadparm.c
+++ source3/param/loadparm.c
@@ -847,6 +847,7 @@ void loadparm_s3_init_globals(struct loadparm_context *lp_ctx,
 	Globals.winbind_refresh_tickets = false;
 	Globals.winbind_offline_logon = false;
 	Globals.winbind_scan_trusted_domains = false;
+	Globals.winbind_use_s4u2self = false;
 
 	Globals.idmap_cache_time = 86400 * 7; /* a week by default */
 	Globals.idmap_negative_cache_time = 120; /* 2 minutes by default */
