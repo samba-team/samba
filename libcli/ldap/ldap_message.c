d889a397c951c44f33df1a9257eb68218eabdc83
Volker Lendecke <vl@samba.org>
2026-08-31 11:09:37 +0200

    libcli: Avoid a ZERO_STRUCT with a direct initialization
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Anoop C S <anoopcs@samba.org>

--- libcli/ldap/ldap_message.c
+++ libcli/ldap/ldap_message.c
@@ -1152,8 +1152,7 @@ bool ldap_decode_attribs_bare(TALLOC_CTX *mem_ctx, struct asn1_data *data,
 			      int *num_attributes)
 {
 	while (asn1_peek_tag(data, ASN1_SEQUENCE(0))) {
-		struct ldb_message_element attrib;
-		ZERO_STRUCT(attrib);
+		struct ldb_message_element attrib = {};
 		if (!ldap_decode_attrib(mem_ctx, data, &attrib)) return false;
 		add_attrib_to_array_talloc(mem_ctx, &attrib,
 					   attributes, num_attributes);
