b63c5abd58ff8c208b57dd70476a4bf8d772e342
Volker Lendecke <vl@samba.org>
2026-08-31 11:11:37 +0200

    libcli: Check for allocation error
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Anoop C S <anoopcs@samba.org>
    
    Autobuild-User(master): Volker Lendecke <vl@samba.org>
    Autobuild-Date(master): Mon Sep 14 12:16:35 UTC 2026 on atb-devel-224

--- libcli/ldap/ldap_message.c
+++ libcli/ldap/ldap_message.c
@@ -1153,9 +1153,18 @@ bool ldap_decode_attribs_bare(TALLOC_CTX *mem_ctx, struct asn1_data *data,
 {
 	while (asn1_peek_tag(data, ASN1_SEQUENCE(0))) {
 		struct ldb_message_element attrib = {};
-		if (!ldap_decode_attrib(mem_ctx, data, &attrib)) return false;
-		add_attrib_to_array_talloc(mem_ctx, &attrib,
-					   attributes, num_attributes);
+		bool ok;
+
+		ok = ldap_decode_attrib(mem_ctx, data, &attrib);
+		if (!ok) {
+			return false;
+		}
+
+		ok = add_attrib_to_array_talloc(
+			mem_ctx, &attrib, attributes, num_attributes);
+		if (!ok) {
+			return false;
+		}
 	}
 	return true;
 }
